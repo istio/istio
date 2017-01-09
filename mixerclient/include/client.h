3a0ec2cbe06ee55f49b8afcbb04681c4c6e8715a
Qian Sun <hahasunqian@gmail.com>
2017-01-09 10:36:25 -0800

    Define interface for client. (#2)
    * Define interface for client.
    
    * Remove per_request transport.
    
    * Add quota interface.
    
    * Update copyright headers.
    
    * Update namespace from google to istio.

---
+++ mixerclient/include/client.h
@@ -0,0 +1,116 @@
+/* Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MIXERCLIENT_CLIENT_H
+#define MIXERCLIENT_CLIENT_H
+
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "google/protobuf/stubs/status.h"
+#include "mixer/api/v1/service.pb.h"
+#include "options.h"
+
+namespace istio {
+namespace mixer_client {
+
+// Defines a function prototype used when an asynchronous transport call
+// is completed.
+using DoneFunc = std::function<void(const ::google::protobuf::util::Status&)>;
+
+// Defines a function prototype to make an asynchronous Check call to
+// the mixer server.
+using TransportCheckFunc = std::function<void(
+    const ::istio::mixer::v1::CheckRequest& request,
+    ::istio::mixer::v1::CheckResponse* response, DoneFunc on_done)>;
+
+// Defines a function prototype to make an asynchronous Report call to
+// the mixer server.
+using TransportReportFunc = std::function<void(
+    const ::istio::mixer::v1::ReportRequest& request,
+    ::istio::mixer::v1::ReportResponse* response, DoneFunc on_done)>;
+
+// Defines a function prototype to make an asynchronous Quota call to
+// the mixer server.
+using TransportQuotaFunc = std::function<void(
+    const ::istio::mixer::v1::QuotaRequest& request,
+    ::istio::mixer::v1::QuotaResponse* response, DoneFunc on_done)>;
+
+// Defines the options to create an instance of MixerClient interface.
+struct MixerClientOptions {
+  // Default constructor with default values.
+  MixerClientOptions() {}
+
+  // Constructor with specified option values.
+  MixerClientOptions(const CheckOptions& check_options,
+                     const ReportOptions& report_options,
+                     const QuotaOptions& quota_options)
+      : check_options(check_options),
+        report_options(report_options),
+        quota_options(quota_options) {}
+
+  // Check options.
+  CheckOptions check_options;
+
+  // Report options.
+  ReportOptions report_options;
+
+  // Quota options.
+  QuotaOptions quota_options;
+
+  // Transport functions are used to send request to mixer server.
+  // It can be implemented many ways based on the environments.
+  // If not provided, the GRPC transport will be used.
+  TransportCheckFunc check_transport;
+  TransportReportFunc report_transport;
+  TransportQuotaFunc quota_transport;
+};
+
+class MixerClient {
+ public:
+  // Destructor
+  virtual ~MixerClient() {}
+
+  // The async call.
+  // on_check_done is called with the check status after cached
+  // check_response is returned in case of cache hit, otherwise called after
+  // check_response is returned from the Controller service.
+  //
+  // check_response must be alive until on_check_done is called.
+  virtual void Check(const ::istio::mixer::v1::CheckRequest& check_request,
+                     ::istio::mixer::v1::CheckResponse* check_response,
+                     DoneFunc on_check_done) = 0;
+
+  // This is async call. on_report_done is always called when the
+  // report request is finished.
+  virtual void Report(const ::istio::mixer::v1::ReportRequest& report_request,
+                      ::istio::mixer::v1::ReportResponse* report_response,
+                      DoneFunc on_report_done) = 0;
+
+  // This is async call. on_quota_done is always called when the
+  // quota request is finished.
+  virtual void Quota(const ::istio::mixer::v1::QuotaRequest& quota_request,
+                      ::istio::mixer::v1::QuotaResponse* quota_response,
+                      DoneFunc on_quota_done) = 0;
+};
+
+// Creates a MixerClient object.
+std::unique_ptr<MixerClient> CreateMixerClient(MixerClientOptions& options);
+
+}  // namespace mixer_client
+}  // namespace istio
+
+#endif  // MIXERCLIENT_CLIENT_H
---
+++ mixerclient/include/options.h
@@ -0,0 +1,122 @@
+/* Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MIXERCLIENT_OPTIONS_H
+#define MIXERCLIENT_OPTIONS_H
+
+#include <memory>
+
+namespace istio {
+namespace mixer_client {
+
+// Options controlling check behavior.
+struct CheckOptions {
+  // Default constructor.
+  // Default options are chosen from experience.
+  CheckOptions()
+      : num_entries(10000), flush_interval_ms(500), expiration_ms(1000) {}
+
+  // Constructor.
+  // cache_entries is the maximum number of cache entries that can be kept in
+  // the cache. Cache is disabled when cache_entries <= 0.
+  // flush_cache_entry_interval_ms is the maximum milliseconds before an
+  // check request needs to send to remote server again.
+  // response_expiration_ms is the maximum milliseconds before a cached check
+  // response is invalidated. We make sure that it is at least
+  // flush_cache_entry_interval_ms + 1.
+  CheckOptions(int cache_entries, int flush_cache_entry_interval_ms,
+               int response_expiration_ms)
+      : num_entries(cache_entries),
+        flush_interval_ms(flush_cache_entry_interval_ms),
+        expiration_ms(std::max(flush_cache_entry_interval_ms + 1,
+                               response_expiration_ms)) {}
+
+  // Maximum number of cache entries kept in the cache.
+  // Set to 0 will disable caching.
+  const int num_entries;
+
+  // Maximum milliseconds before check requests are flushed to the
+  // server. The flush is triggered by a check request.
+  const int flush_interval_ms;
+
+  // Maximum milliseconds before a cached check response should be deleted. The
+  // deletion is triggered by a timer. This value must be larger than
+  // flush_interval_ms.
+  const int expiration_ms;
+};
+
+// Options controlling report behavior.
+struct ReportOptions {
+  // Default constructor.
+  ReportOptions() : num_entries(10000), flush_interval_ms(1000) {}
+
+  // Constructor.
+  // cache_entries is the maximum number of cache entries that can be kept in
+  // the cache. Cache is disabled when cache_entries <= 0.
+  // flush_cache_entry_interval_ms is the maximum milliseconds before
+  // report requests are flushed to the server. The cache entry is deleted after
+  // the flush.
+  ReportOptions(int cache_entries, int flush_cache_entry_interval_ms)
+      : num_entries(cache_entries),
+        flush_interval_ms(flush_cache_entry_interval_ms) {}
+
+  // Maximum number of cache entries kept in the cache.
+  // Set to 0 will disable caching.
+  const int num_entries;
+
+  // Maximum milliseconds before report requests are flushed to the
+  // server. The flush is triggered by a timer.
+  const int flush_interval_ms;
+};
+
+// Options controlling quota behavior.
+struct QuotaOptions {
+  // Default constructor.
+  QuotaOptions()
+      : num_entries(10000), flush_interval_ms(500), expiration_ms(1000) {}
+
+  // Constructor.
+  // cache_entries is the maximum number of cache entries that can be kept in
+  // the cache. Cache is disabled when cache_entries <= 0.
+  // flush_cache_entry_interval_ms is the maximum milliseconds before an
+  // quota request needs to send to remote server again.
+  // response_expiration_ms is the maximum milliseconds before a cached quota
+  // response is invalidated. We make sure that it is at least
+  // flush_cache_entry_interval_ms + 1.
+  QuotaOptions(int cache_entries, int flush_cache_entry_interval_ms,
+               int response_expiration_ms)
+      : num_entries(cache_entries),
+        flush_interval_ms(flush_cache_entry_interval_ms),
+        expiration_ms(std::max(flush_cache_entry_interval_ms + 1,
+                               response_expiration_ms)) {}
+
+  // Maximum number of cache entries kept in the cache.
+  // Set to 0 will disable caching.
+  const int num_entries;
+
+  // Maximum milliseconds before quota requests are flushed to the
+  // server. The flush is triggered by a quota request.
+  const int flush_interval_ms;
+
+  // Maximum milliseconds before a cached quota response should be deleted. The
+  // deletion is triggered by a timer. This value must be larger than
+  // flush_interval_ms.
+  const int expiration_ms;
+};
+
+}  // namespace mixer_client
+}  // namespace istio
+
+#endif  // MIXERCLIENT_OPTIONS_H
