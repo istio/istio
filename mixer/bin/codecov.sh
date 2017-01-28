f4a62833114505b9a2dec7f698df638b8fea70b9
mandarjog <mandarjog@gmail.com>
2017-01-27 16:54:50 -0800

    Only run linters on the changed code, except for main build (#207)
    * Only run linters on the changed code, except for main build
    
    * use vendored linters
    
    * break coverage and race as 2 test, runs faster
    
    
    Former-commit-id: 34169c89106ff26ee56dc4a519467cecf4d5905b

--- mixer/bin/codecov.sh
+++ mixer/bin/codecov.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
 set -e
+
+echo "Code coverage test"
 echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
-    go test -race -coverprofile=profile.out -covermode=atomic $d
+    go test -coverprofile=profile.out $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
     fi
 done
+
+echo "Race test"
+go test -race ./...
