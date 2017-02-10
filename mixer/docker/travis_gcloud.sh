e65e51f9355c0f088d823e9531c027ab31ca8d9e
Douglas Reid <douglas-reid@users.noreply.github.com>
2017-02-09 21:21:09 -0800

    Tag docker images with date (and build number). (#251)
    Since bazel strips out the date information (to give us repeatable builds!), this
    will add a helpful way to distinguish between images, etc.
    
    Former-commit-id: acba551b05f619bd925c960591698482dfc504ca

--- mixer/docker/travis_gcloud.sh
+++ mixer/docker/travis_gcloud.sh
@@ -4,6 +4,9 @@ set -ev
 export PROJECT="istio-testing"
 export BAZEL_OUTBASE=$HOME/bazel/outbase
 
+today=$(date +%F)
+export DOCKER_TAG=$today-${TRAVIS_BUILD_NUMBER}
+
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 	openssl aes-256-cbc -K $encrypted_2f660428f0db_key -iv $encrypted_2f660428f0db_iv -in $PROJECT.json.enc -out $PROJECT.json -d
 
