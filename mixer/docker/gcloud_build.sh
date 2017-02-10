b4f21c6cba0bbfd10e3fa0075ae2ccae1f2ad47c
Douglas Reid <douglas-reid@users.noreply.github.com>
2017-02-09 21:21:09 -0800

    Tag docker images with date (and build number). (#251)
    Since bazel strips out the date information (to give us repeatable builds!), this
    will add a helpful way to distinguish between images, etc.
    
    Former-commit-id: 714bad29aa1a27bfbd5cc840711467e432a2006d

--- mixer/docker/gcloud_build.sh
+++ mixer/docker/gcloud_build.sh
@@ -4,11 +4,19 @@ set -ex
 
 gcloud docker --authorize-only
 
-if [ -z $BAZEL_OUTBASE ]
+if [ -z $DOCKER_TAG ]
 then
-    bazel run //docker:mixer gcr.io/$PROJECT/mixer:experiment
-else
-    bazel --output_base=$BAZEL_OUTBASE run //docker:mixer gcr.io/$PROJECT/mixer:experiment
+    export DOCKER_TAG=experiment
 fi
 
-gcloud docker -- push gcr.io/$PROJECT/mixer:experiment
+if [ -z $BAZEL_OUTBASE ]
+then
+    bazel run //docker:mixer gcr.io/$PROJECT/mixer:$DOCKER_TAG
+else
+    bazel --output_base=$BAZEL_OUTBASE run //docker:mixer gcr.io/$PROJECT/mixer:$DOCKER_TAG
+fi
+
+docker tag gcr.io/$PROJECT/mixer:$DOCKER_TAG gcr.io/$PROJECT/mixer:latest
+
+gcloud docker -- push gcr.io/$PROJECT/mixer:$DOCKER_TAG
+gcloud docker -- push gcr.io/$PROJECT/mixer:latest
