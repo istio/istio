7f74446f42fa034f3115d292b4d90a5de40af4d1
Yang Guan <yangguan@google.com>
2017-02-28 21:07:46 -0800

    Enable codecov report (#31)
    This closes #29 .

--- security/bin/linters.sh
+++ security/bin/linters.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -ex
 
-# Create symlinks from bazel directory to the vendor folder.
-bin/bazel_to_go.py
-
-# Remove nested vendor/ directories.
-find -L vendor/ -mindepth 1 -name vendor | xargs rm -rf
-
-go install ./...
-
 gometalinter --concurrency=4 --enable-gc --deadline=300s --disable-all\
   --enable=aligncheck\
   --enable=deadcode\
