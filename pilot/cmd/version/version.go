333742e0ed71bc5bd7dfd6688e53e1e77ff57187
Kuat <kyessenov@users.noreply.github.com>
2017-03-08 14:57:05 -0800

    Set Golang compiler version (it is go1.8) (#276)

--- pilot/cmd/version/version.go
+++ pilot/cmd/version/version.go
@@ -15,17 +15,18 @@
 // Package version provides build time version information.
 package version
 
+import "runtime"
+
 // The following fields are populated at buildtime with bazel's linkstamp
 // feature. This is equivalent to using golang directly with -ldflags -X.
 // Note that DATE is omitted as bazel aims for reproducible builds and
 // seems to strip date information from the build process.
 var (
-	buildAppVersion    string
-	buildGitRevision   string
-	buildGitBranch     string
-	buildUser          string
-	buildHost          string
-	buildGolangVersion string
+	buildAppVersion  string
+	buildGitRevision string
+	buildGitBranch   string
+	buildUser        string
+	buildHost        string
 )
 
 // BuildInfo describes version information about the binary build.
@@ -47,5 +48,5 @@ func init() {
 	Info.GitBranch = buildGitBranch
 	Info.User = buildUser
 	Info.Host = buildHost
-	Info.GolangVersion = buildGolangVersion
+	Info.GolangVersion = runtime.Version()
 }
