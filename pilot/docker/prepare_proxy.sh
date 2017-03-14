903eedb1d68f3e7c60de2eb13655ecb4c64bef6b
Andra Cismaru <andraxylia@users.noreply.github.com>
2017-03-14 09:37:43 -0700

    Change kernel core_pattern so we can get core files (#319)

--- pilot/docker/prepare_proxy.sh
+++ pilot/docker/prepare_proxy.sh
@@ -11,5 +11,8 @@ ISTIO_PROXY_UID=1337
 iptables -t nat -A PREROUTING -p tcp -j REDIRECT --to-port $ISTIO_PROXY_PORT
 iptables -t nat -A OUTPUT -p tcp -j REDIRECT ! -s 127.0.0.1/32 --to-port $ISTIO_PROXY_PORT -m owner '!' --uid-owner $ISTIO_PROXY_UID
 
+# Enable core dumps to writable directory
+sysctl -w  kernel.core_pattern=/tmp/core.%e.%p.%t
+
 exit 0
 
