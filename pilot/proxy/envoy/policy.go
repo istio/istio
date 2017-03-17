bbbe07f69377bcab35b922446262bbd1145495d9
Kuat <kyessenov@users.noreply.github.com>
2017-03-17 14:59:45 -0700

    Add service attributes to Mixer (#354)
    * Add service attributes to Mixer
    
    * Fix tests

--- pilot/proxy/envoy/policy.go
+++ pilot/proxy/envoy/policy.go
@@ -18,6 +18,7 @@
 package envoy
 
 import (
+	"sort"
 	"strings"
 
 	"istio.io/manager/model"
@@ -25,32 +26,50 @@ import (
 )
 
 func insertMixerFilter(listeners []*Listener, context *ProxyContext) {
-	if context.MeshConfig.MixerAddress != "" {
-		// join IPs with a comma
-		ips := make([]string, 0)
-		for ip := range context.Addrs {
-			ips = append(ips, ip)
-		}
-		id := strings.Join(ips, ",")
+	if context.MeshConfig.MixerAddress == "" {
+		return
+	}
 
-		for _, l := range listeners {
-			for _, f := range l.Filters {
-				if f.Name == HTTPConnectionManager {
-					http := (f.Config).(*HTTPFilterConfig)
-					http.Filters = append([]HTTPFilter{{
-						Type: "decoder",
-						Name: "mixer",
-						Config: &FilterMixerConfig{
-							MixerServer: context.MeshConfig.MixerAddress,
-							MixerAttributes: map[string]string{
-								"target.uid": id,
-							},
-							ForwardAttributes: map[string]string{
-								"source.uid": id,
-							},
+	// join IPs with a comma
+	ips := make([]string, 0)
+	for ip := range context.Addrs {
+		ips = append(ips, ip)
+	}
+	sort.Strings(ips)
+	id := strings.Join(ips, ",")
+
+	// join service names with a comma
+	instances := context.Discovery.HostInstances(context.Addrs)
+	serviceSet := make(map[string]bool)
+	for _, instance := range instances {
+		serviceSet[instance.Service.Hostname] = true
+	}
+	services := make([]string, 0)
+	for service := range serviceSet {
+		services = append(services, service)
+	}
+	sort.Strings(services)
+	service := strings.Join(services, ",")
+
+	for _, l := range listeners {
+		for _, f := range l.Filters {
+			if f.Name == HTTPConnectionManager {
+				http := (f.Config).(*HTTPFilterConfig)
+				http.Filters = append([]HTTPFilter{{
+					Type: "decoder",
+					Name: "mixer",
+					Config: &FilterMixerConfig{
+						MixerServer: context.MeshConfig.MixerAddress,
+						MixerAttributes: map[string]string{
+							"target.uid":     id,
+							"target.service": service,
 						},
-					}}, http.Filters...)
-				}
+						ForwardAttributes: map[string]string{
+							"source.uid":     id,
+							"source.service": service,
+						},
+					},
+				}}, http.Filters...)
 			}
 		}
 	}
