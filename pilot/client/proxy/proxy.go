270b6f3cf5f4c146a8fdb14e53a6391672ee84a8
Liam White <liamwhite@uk.ibm.com>
2017-04-14 13:26:33 +0100

    API client initial functionality (#504)
    * API client initial functionality
    
    Signed-off-by: Liam White <liamwhite@uk.ibm.com>
    
    * PR review updates
    
    Signed-off-by: Liam White <liamwhite@uk.ibm.com>
    
    * Comments, linting and more PR review amendments
    
    Signed-off-by: Liam White <liamwhite@uk.ibm.com>
    
    * Add proxy client BUILD file
    
    Signed-off-by: Liam White <liamwhite@uk.ibm.com>

---
+++ pilot/client/proxy/proxy.go
@@ -0,0 +1,146 @@
+package proxy
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"istio.io/manager/apiserver"
+	"istio.io/manager/model"
+)
+
+// ManagerClient is a client wrapper that contains the base URL and API version
+type ManagerClient struct {
+	base             url.URL
+	versionedAPIPath string
+	client           *http.Client
+}
+
+// NewManagerClient creates a new ManagerClient instance. It trims the apiVersion of leading and trailing slashes
+// and the base path of trailing slashes to ensure consistency
+func NewManagerClient(base url.URL, apiVersion string, client *http.Client) *ManagerClient {
+	base.Path = strings.TrimSuffix(base.Path, "/")
+	return &ManagerClient{
+		base:             base,
+		versionedAPIPath: strings.TrimPrefix(strings.TrimSuffix(apiVersion, "/"), "/"),
+		client:           client,
+	}
+}
+
+func (m *ManagerClient) do(request *http.Request) (*http.Response, error) {
+	fullURL, err := url.Parse(fmt.Sprintf("%s/%s/%s",
+		m.base.String(), m.versionedAPIPath, request.URL.String()))
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse URL: %v", err)
+	}
+	request.URL = fullURL
+	response, err := m.client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	if response.StatusCode < 200 || response.StatusCode >= 300 {
+		return nil, fmt.Errorf("received non-success status code %v", response.StatusCode)
+	}
+	return response, nil
+}
+
+// GetConfig retrieves the configuration resource for the passed key
+func (m *ManagerClient) GetConfig(key model.Key) (config *apiserver.Config, err error) {
+	response, err := m.doConfigCRUD(key, http.MethodGet, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() { _ = response.Body.Close() }() // #nosec
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	config = &apiserver.Config{}
+	if err = json.Unmarshal(body, config); err != nil {
+		return nil, err
+	}
+	return config, nil
+}
+
+// AddConfig creates a configuration resources for the passed key using the passed configuration
+// It is idempotent
+func (m *ManagerClient) AddConfig(key model.Key, config apiserver.Config) error {
+	bodyIn, err := json.Marshal(config)
+	if err != nil {
+		return err
+	}
+	if _, err = m.doConfigCRUD(key, http.MethodPost, bodyIn); err != nil {
+		return err
+	}
+	return nil
+}
+
+// UpdateConfig updates the configuration resource for the passed key using the passed configuration
+// It is idempotent
+func (m *ManagerClient) UpdateConfig(key model.Key, config apiserver.Config) error {
+	bodyIn, err := json.Marshal(config)
+	if err != nil {
+		return err
+	}
+	if _, err = m.doConfigCRUD(key, http.MethodPut, bodyIn); err != nil {
+		return err
+	}
+	return nil
+}
+
+// DeleteConfig deletes the configuration resource for the passed key
+func (m *ManagerClient) DeleteConfig(key model.Key) error {
+	_, err := m.doConfigCRUD(key, http.MethodDelete, nil)
+	return err
+}
+
+// ListConfig retrieves all configuration resources of the passed kind in the given namespace
+// If namespace is an empty string it retrieves all configs of the passed kind across all namespaces
+func (m *ManagerClient) ListConfig(kind, namespace string) (config []apiserver.Config, err error) {
+	var reqURL string
+	if namespace != "" {
+		reqURL = fmt.Sprintf("config/%v/%v", kind, namespace)
+	} else {
+		reqURL = fmt.Sprintf("config/%v", kind)
+	}
+	request, err := http.NewRequest(http.MethodGet, reqURL, nil)
+	if err != nil {
+		return nil, err
+	}
+	response, err := m.do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() { _ = response.Body.Close() }() // #nosec
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	config = []apiserver.Config{}
+	if err = json.Unmarshal(body, &config); err != nil {
+		return nil, err
+	}
+	return config, nil
+}
+
+func (m *ManagerClient) doConfigCRUD(key model.Key, method string, inBody []byte) (*http.Response, error) {
+	reqURL := fmt.Sprintf("config/%v/%v/%v", key.Kind, key.Namespace, key.Name)
+	var body io.Reader
+	if len(inBody) > 0 {
+		body = bytes.NewBuffer(inBody)
+	}
+	request, err := http.NewRequest(method, reqURL, body)
+	if err != nil {
+		return nil, err
+	}
+	return m.do(request)
+}
---
+++ pilot/client/proxy/proxy_test.go
@@ -0,0 +1,302 @@
+package proxy
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"reflect"
+	"testing"
+
+	"istio.io/manager/apiserver"
+	"istio.io/manager/model"
+)
+
+type FakeHandler struct {
+	wantResponse   string
+	wantHeaders    http.Header
+	wantStatusCode int
+}
+
+func (f *FakeHandler) HandlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(f.wantStatusCode)
+	for k, v := range f.wantHeaders {
+		w.Header().Set(k, v[0])
+	}
+	_, _ = w.Write([]byte(f.wantResponse))
+}
+
+func TestNewManagerClient(t *testing.T) {
+	cases := []struct {
+		name           string
+		baseURL        string
+		wantBaseURL    string
+		apiVersion     string
+		wantAPIVersion string
+	}{
+		{
+			name:        "TestBaseURL",
+			baseURL:     "http://host:port",
+			wantBaseURL: "http://host:port",
+		},
+		{
+			name:        "TestBaseURLTrailingSlash",
+			baseURL:     "http://host:port/",
+			wantBaseURL: "http://host:port",
+		},
+		{
+			name:           "TestAPIVersion",
+			apiVersion:     "test",
+			wantAPIVersion: "test",
+		},
+		{
+			name:           "TestAPIVersionTrailingSlash",
+			apiVersion:     "test/",
+			wantAPIVersion: "test",
+		},
+		{
+			name:           "TestAPIVersionLeadingSlash",
+			apiVersion:     "/test",
+			wantAPIVersion: "test",
+		},
+	}
+
+	for _, c := range cases {
+		url, _ := url.Parse(c.baseURL)
+		client := NewManagerClient(*url, c.apiVersion, nil)
+		if c.baseURL != "" && client.base.String() != c.wantBaseURL {
+			t.Errorf("%s: got baseURL %v, want %v", c.name, client.base.String(), c.wantBaseURL)
+		}
+		if c.apiVersion != "" && client.versionedAPIPath != c.wantAPIVersion {
+			t.Errorf("%s: got apiVersion %v, want %v", c.name, client.versionedAPIPath, c.wantAPIVersion)
+		}
+	}
+}
+
+func TestGetAddUpdateDeleteListConfig(t *testing.T) {
+	cases := []struct {
+		name            string
+		function        string
+		key             model.Key
+		kind            string
+		namespace       string
+		config          *apiserver.Config
+		wantConfig      *apiserver.Config
+		wantConfigSlice []apiserver.Config
+		wantHeaders     http.Header
+		wantStatus      int
+		wantError       bool
+	}{
+		{
+			name:        "TestConfigGet",
+			function:    "get",
+			key:         model.Key{Name: "name", Namespace: "namespace", Kind: "route-rule"},
+			wantConfig:  &apiserver.Config{Type: "type", Name: "name", Spec: "spec"},
+			wantHeaders: http.Header{"Content-Type": []string{"application/json"}},
+			wantStatus:  http.StatusOK,
+		},
+		{
+			name:        "TestConfigGetNotFound",
+			function:    "get",
+			key:         model.Key{Name: "name", Namespace: "namespace", Kind: "route-rule"},
+			wantError:   true,
+			wantHeaders: http.Header{"Content-Type": []string{"text/plain"}},
+			wantStatus:  http.StatusNotFound,
+		},
+		{
+			name:        "TestConfigGetInvalidConfigType",
+			function:    "get",
+			key:         model.Key{Name: "name", Namespace: "namespace", Kind: "route-rule"},
+			wantError:   true,
+			wantHeaders: http.Header{"Content-Type": []string{"text/plain"}},
+			wantStatus:  http.StatusBadRequest,
+		},
+		{
+			name:        "TestConfigGetInvalidRespBody",
+			function:    "get",
+			key:         model.Key{Name: "name", Namespace: "namespace", Kind: "route-rule"},
+			wantError:   true,
+			wantHeaders: http.Header{"Content-Type": []string{"text/plain"}},
+			wantStatus:  http.StatusBadRequest,
+		},
+		{
+			name:        "TestConfigAdd",
+			function:    "add",
+			key:         model.Key{Name: "name", Namespace: "namespace", Kind: "route-rule"},
+			config:      &apiserver.Config{Type: "type", Name: "name", Spec: "spec"},
+			wantHeaders: http.Header{"Content-Type": []string{"application/json"}},
+			wantStatus:  http.StatusCreated,
+		},
+		{
+			name:        "TestAddConfigConflict",
+			function:    "add",
+			key:         model.Key{Name: "name", Namespace: "namespace", Kind: "route-rule"},
+			config:      &apiserver.Config{Type: "type", Name: "name", Spec: "spec"},
+			wantError:   true,
+			wantHeaders: http.Header{"Content-Type": []string{"text/plain"}},
+			wantStatus:  http.StatusConflict,
+		},
+		{
+			name:        "TestConfigAddInvalidConfigType",
+			function:    "add",
+			key:         model.Key{Name: "name", Namespace: "namespace", Kind: "route-rule"},
+			config:      &apiserver.Config{Type: "NOTATYPE", Name: "name", Spec: "spec"},
+			wantError:   true,
+			wantHeaders: http.Header{"Content-Type": []string{"text/plain"}},
+			wantStatus:  http.StatusBadRequest,
+		},
+		{
+			name:        "TestAddConfigInvalidSpec",
+			function:    "add",
+			key:         model.Key{Name: "name", Namespace: "namespace", Kind: "route-rule"},
+			config:      &apiserver.Config{Type: "type", Name: "name", Spec: "NOTASPEC"},
+			wantError:   true,
+			wantHeaders: http.Header{"Content-Type": []string{"text/plain"}},
+			wantStatus:  http.StatusBadRequest,
+		},
+		{
+			name:        "TestConfigUpdate",
+			function:    "update",
+			key:         model.Key{Name: "name", Namespace: "namespace", Kind: "route-rule"},
+			config:      &apiserver.Config{Type: "type", Name: "name", Spec: "spec"},
+			wantHeaders: http.Header{"Content-Type": []string{"application/json"}},
+			wantStatus:  http.StatusOK,
+		},
+		{
+			name:        "TestConfigUpdateNotFound",
+			function:    "update",
+			key:         model.Key{Name: "name", Namespace: "namespace", Kind: "route-rule"},
+			config:      &apiserver.Config{Type: "type", Name: "name", Spec: "spec"},
+			wantError:   true,
+			wantHeaders: http.Header{"Content-Type": []string{"text/plain"}},
+			wantStatus:  http.StatusNotFound,
+		},
+		{
+			name:        "TestConfigUpdateInvalidConfigType",
+			function:    "update",
+			key:         model.Key{Name: "name", Namespace: "namespace", Kind: "route-rule"},
+			config:      &apiserver.Config{Type: "NOTATYPE", Name: "name", Spec: "spec"},
+			wantError:   true,
+			wantHeaders: http.Header{"Content-Type": []string{"text/plain"}},
+			wantStatus:  http.StatusBadRequest,
+		},
+		{
+			name:        "TestUpdateConfigInvalidSpec",
+			function:    "update",
+			key:         model.Key{Name: "name", Namespace: "namespace", Kind: "route-rule"},
+			config:      &apiserver.Config{Type: "type", Name: "name", Spec: "NOTASPEC"},
+			wantError:   true,
+			wantHeaders: http.Header{"Content-Type": []string{"text/plain"}},
+			wantStatus:  http.StatusBadRequest,
+		},
+		{
+			name:        "TestConfigDelete",
+			function:    "delete",
+			key:         model.Key{Name: "name", Namespace: "namespace", Kind: "route-rule"},
+			wantHeaders: http.Header{"Content-Type": []string{"application/json"}},
+			wantStatus:  http.StatusOK,
+		},
+		{
+			name:        "TestConfigDeleteNotFound",
+			function:    "delete",
+			key:         model.Key{Name: "name", Namespace: "namespace", Kind: "route-rule"},
+			wantError:   true,
+			wantHeaders: http.Header{"Content-Type": []string{"text/plain"}},
+			wantStatus:  http.StatusNotFound,
+		},
+		{
+			name:        "TestConfigDeleteInvalidConfigType",
+			function:    "delete",
+			key:         model.Key{Name: "name", Namespace: "namespace", Kind: "route-rule"},
+			wantError:   true,
+			wantHeaders: http.Header{"Content-Type": []string{"text/plain"}},
+			wantStatus:  http.StatusBadRequest,
+		},
+		{
+			name:      "TestConfigListWithNamespace",
+			function:  "list",
+			kind:      "kind",
+			namespace: "namespace",
+			wantConfigSlice: []apiserver.Config{
+				{Type: "type", Name: "name", Spec: "spec"},
+				{Type: "type", Name: "name2", Spec: "spec"},
+			},
+			wantHeaders: http.Header{"Content-Type": []string{"application/json"}},
+			wantStatus:  http.StatusOK,
+		},
+		{
+			name:     "TestConfigListWithoutNamespace",
+			function: "list",
+			kind:     "kind",
+			wantConfigSlice: []apiserver.Config{
+				{Type: "type", Name: "name", Spec: "spec"},
+				{Type: "type", Name: "name2", Spec: "spec"},
+			},
+			wantHeaders: http.Header{"Content-Type": []string{"application/json"}},
+			wantStatus:  http.StatusOK,
+		},
+		{
+			name:        "TestConfigListWithNamespaceInvalidConfigType",
+			function:    "list",
+			kind:        "kind",
+			namespace:   "namespace",
+			wantError:   true,
+			wantHeaders: http.Header{"Content-Type": []string{"application/json"}},
+			wantStatus:  http.StatusBadRequest,
+		},
+	}
+	for _, c := range cases {
+
+		// Setup test server
+		var want string
+		if c.wantConfigSlice != nil {
+			e, _ := json.Marshal(c.wantConfigSlice)
+			want = string(e)
+		} else {
+			e, _ := json.Marshal(c.wantConfig)
+			want = string(e)
+		}
+
+		fh := &FakeHandler{
+			wantResponse:   want,
+			wantHeaders:    c.wantHeaders,
+			wantStatusCode: c.wantStatus,
+		}
+		ts := httptest.NewServer(http.HandlerFunc(fh.HandlerFunc))
+		defer ts.Close()
+		tsURL, _ := url.Parse(ts.URL)
+
+		// Setup Client
+		var config *apiserver.Config
+		var configSlice []apiserver.Config
+		var err error
+		client := NewManagerClient(*tsURL, "test", &http.Client{})
+		switch c.function {
+		case "get":
+			config, err = client.GetConfig(c.key)
+		case "add":
+			err = client.AddConfig(c.key, *c.config)
+		case "update":
+			err = client.UpdateConfig(c.key, *c.config)
+		case "delete":
+			err = client.DeleteConfig(c.key)
+		case "list":
+			configSlice, err = client.ListConfig(c.kind, c.namespace)
+		default:
+			t.Fatal("didn't supply function to test case, don't know which client function to call")
+		}
+
+		// Verify
+		if !c.wantError && err != nil {
+			t.Errorf("%s: unexpected error: %v", c.name, err)
+		} else if c.function == "get" {
+			if !reflect.DeepEqual(config, c.wantConfig) {
+				t.Errorf("%s: wanted config: %+v, but received: %+v", c.name, c.wantConfig, config)
+			}
+		} else if c.function == "list" {
+			if !reflect.DeepEqual(configSlice, c.wantConfigSlice) {
+				t.Errorf("%s: wanted config slice: %+v, but received: %+v", c.name, c.wantConfigSlice, configSlice)
+			}
+		}
+	}
+}
