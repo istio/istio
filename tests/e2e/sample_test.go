e3af41788d6483056e36e8269bcb4f4737053887
Sebastien Vas <sebastienvas@users.noreply.github.com>
2017-04-19 13:47:27 -0700

    Separate TestInfo to another module (#144)
    * Separete TestInfo to another module
    
    Implemented status file creation
    Implemented log upload to cloud storage
    Rename SetUp to Setup and TearDown to Teardown
    
    * Add more info in TestStatus
    
    * Rename InitLogging to InitGlog
    
    * Resolving comments
    
    * Return skipDir error on err

--- tests/e2e/sample_test.go
+++ tests/e2e/sample_test.go
@@ -17,30 +17,39 @@ type testConfig struct {
 	sampleValue string
 }
 
-func (c *testConfig) SetUp() error {
+func (c *testConfig) Setup() error {
 	glog.Info("Sample test Setup")
 	c.sampleValue = "sampleValue"
 	return nil
 }
 
-func (c *testConfig) TearDown() error {
+func (c *testConfig) Teardown() error {
 	glog.Info("Sample test Tear Down")
 	return nil
 }
 
 func TestSample(t *testing.T) {
-	t.Log("Value is ", c.sampleValue)
+	t.Logf("Value is %s", c.sampleValue)
 }
 
-func NewTestConfig() *testConfig {
-	return &testConfig{
-		CommonConfig: framework.NewCommonConfig("sample_test"),
+func NewTestConfig() (*testConfig, error) {
+	cc, err := framework.NewCommonConfig("sample_test")
+	if err != nil {
+		return nil, err
 	}
+	t := new(testConfig)
+	t.CommonConfig = cc
+	return t, nil
 }
 
 func TestMain(m *testing.M) {
 	flag.Parse()
-	c = NewTestConfig()
-	c.CommonConfig.Cleanup.RegisterCleanable(c)
+	framework.InitGlog()
+	var err error
+	c, err = NewTestConfig()
+	if err != nil {
+		glog.Fatalf("Could not create TestConfig %s", err)
+	}
+	c.Cleanup.RegisterCleanable(c)
 	os.Exit(c.RunTest(m))
 }
