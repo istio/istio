8e7cd3df27e40721e64e873a2f3c5bb7751f69ff
Costin Manolache <costin@google.com>
2017-04-21 11:19:23 -0700

    Add small comment

--- kubernetes/rbac.sh
+++ kubernetes/rbac.sh
@@ -7,7 +7,11 @@ SP=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 set -ex
 
-cat $SP/istio-install/istio-ca.yaml > $SP/istio-rbac.yaml
+# TODO(costin): run part of the presubmit
+echo "# GENERATED FILE, DO NOT EDIT" > $SP/istio-rbac.yaml
+echo "# TO UPDATE, modify files in istio-install and run rbac.sh" >> $SP/istio-rbac.yaml
+
+cat $SP/istio-install/istio-ca.yaml >> $SP/istio-rbac.yaml
 cat $SP/istio-install/istio-mixer.yaml >> $SP/istio-rbac.yaml
 cat $SP/istio-rbac/istio-rbac.yaml >> $SP/istio-rbac.yaml
 sed 's/# RBAC: //' $SP/istio-install/istio-manager.yaml >> $SP/istio-rbac.yaml
