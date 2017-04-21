8fc9e12a56cb1b3dd7c9d5975f567804ce9caf45
Zack <zbutcher@google.com>
2017-04-21 11:47:30 -0700

    No-op adapter (#581)
    * Add an adapter that implements every aspect but does no work.
    
    * Add in an empty params proto so we don't run into issues returning nil
    
    * Dump the empty params proto in favor of using proto.Empty. Add a test for verifying that the no-op adapter is registered for every aspect kind.
    
    * Return a failed precondition in the noop Deny aspect impl.
    
    
    Former-commit-id: 7ff9e1a200b431da305c89833773e825623bea8a

--- mixer/adapter/inventory.go
+++ mixer/adapter/inventory.go
@@ -20,6 +20,7 @@ import (
 	"istio.io/mixer/adapter/ipListChecker"
 	"istio.io/mixer/adapter/kubernetes"
 	"istio.io/mixer/adapter/memQuota"
+	"istio.io/mixer/adapter/noop"
 	"istio.io/mixer/adapter/prometheus"
 	"istio.io/mixer/adapter/redisquota"
 	"istio.io/mixer/adapter/statsd"
@@ -39,5 +40,6 @@ func Inventory() []adapter.RegisterFn {
 		statsd.Register,
 		stdioLogger.Register,
 		kubernetes.Register,
+		noop.Register,
 	}
 }
