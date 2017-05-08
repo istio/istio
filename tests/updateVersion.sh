7f0b3c199f1a01afe849b0857275d71116492c11
Andra Cismaru <andracis@google.com>
2017-05-08 11:48:25 -0700

    Re-unite 1.6 and 1.5 installations
    Former-commit-id: 3ce2c184656a11f3bb91cf59abd6b9b862136a28

--- tests/updateVersion.sh
+++ tests/updateVersion.sh
@@ -104,39 +104,20 @@ function merge_files() {
   SRC=$ROOT/kubernetes/istio-install
   AUTH_SRC=$ROOT/kubernetes/istio-auth
 
-  ISTIO_15=$ROOT/kubernetes/istio-15.yaml
-  ISTIO_AUTH_15=$ROOT/kubernetes/istio-auth-15.yaml
-  echo "# GENERATED FILE. Use for Kubernetes 1.5 or earlier." > $ISTIO_15
-  echo "# TO UPDATE, modify files in istio-install and run updateVersion.sh" >> $ISTIO_15
-  cat $SRC/istio-mixer.yaml >> $ISTIO_15
-  cat $SRC/istio-manager.yaml >> $ISTIO_15
-  cp $ISTIO_15 $ISTIO_AUTH_15
-  cat $SRC/istio-ingress.yaml >> $ISTIO_15
-  cat $SRC/istio-egress.yaml >> $ISTIO_15
-
-  sed -i "s/# authPolicy: MUTUAL_TLS/authPolicy: MUTUAL_TLS/" $ISTIO_AUTH_15
-  cat $AUTH_SRC/istio-ingress-auth.yaml >> $ISTIO_AUTH_15
-  cat $AUTH_SRC/istio-egress-auth.yaml >> $ISTIO_AUTH_15
-  cat $AUTH_SRC/istio-namespace-ca.yaml >> $ISTIO_AUTH_15
-
-  ISTIO_16=$ROOT/kubernetes/istio-16.yaml
-  ISTIO_AUTH_16=$ROOT/kubernetes/istio-auth-16.yaml
-  echo "# GENERATED FILE. Use with Kubernetes 1.6+" > $ISTIO_16
-  echo "# TO UPDATE, modify files in istio-install and run updateVersion.sh" >> $ISTIO_16
-  cat $SRC/istio-mixer.yaml >> $ISTIO_16
-  cat $SRC/istio-manager.yaml >> $ISTIO_16
-  cat $SRC/../istio-rbac/istio-rbac.yaml >> $ISTIO_16
-  cp $ISTIO_16 $ISTIO_AUTH_16
-  cat $SRC/istio-ingress.yaml >> $ISTIO_16
-  cat $SRC/istio-egress.yaml >> $ISTIO_16
-
-  sed -i "s/# authPolicy: MUTUAL_TLS/authPolicy: MUTUAL_TLS/" $ISTIO_AUTH_16
-  cat $AUTH_SRC/istio-ingress-auth.yaml >> $ISTIO_AUTH_16
-  cat $AUTH_SRC/istio-egress-auth.yaml >> $ISTIO_AUTH_16
-  cat $AUTH_SRC/istio-namespace-ca.yaml >> $ISTIO_AUTH_16
-
+  ISTIO=$ROOT/kubernetes/istio.yaml
   ISTIO_AUTH=$ROOT/kubernetes/istio-auth.yaml
-  cp $ISTIO_AUTH_15 $ISTIO_AUTH
+  echo "# GENERATED FILE. Use with Kubernetes 1.6+" > $ISTIO
+  echo "# TO UPDATE, modify files in istio-install and run updateVersion.sh" >> $ISTIO
+  cat $SRC/istio-mixer.yaml >> $ISTIO
+  cat $SRC/istio-manager.yaml >> $ISTIO
+  cp $ISTIO $ISTIO_AUTH
+  cat $SRC/istio-ingress.yaml >> $ISTIO
+  cat $SRC/istio-egress.yaml >> $ISTIO
+
+  sed -i "s/# authPolicy: MUTUAL_TLS/authPolicy: MUTUAL_TLS/" $ISTIO_AUTH
+  cat $AUTH_SRC/istio-ingress-auth.yaml >> $ISTIO_AUTH
+  cat $AUTH_SRC/istio-egress-auth.yaml >> $ISTIO_AUTH
+  cat $AUTH_SRC/istio-namespace-ca.yaml >> $ISTIO_AUTH
 }
 
 function update_version_file() {
