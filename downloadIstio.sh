357db28c1efa6f5ae59a33eee8694982c83d865e
Laurent Demailly <ldemailly@gmail.com>
2017-05-12 09:28:22 -0700

    make the download shell a bit more verbose
    about what it's doing
    
    Former-commit-id: cbe05977ce58f1c1e751b634701d917512066328

--- downloadIstio.sh
+++ downloadIstio.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-## Temporary place holder that gets the job done.
-#We can do arbitrary things in this script over time.
-## This file will be fetched as : curl git.io/getIstio.sh|sh -
-## The script fetches the latest Istio release and untars it.
+# Temporary place holder that gets the job done.
+# We can do arbitrary things in this script over time.
+# This file will be fetched as: curl git.io/getIstio.sh | sh -
+# The script fetches the latest Istio release and untars it.
 
-## Automate me.
+# TODO: Automate me.
 ISTIO_VERSION=0.1.1
-
-curl -L https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio.tar.gz | tar xz
+URL=https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio.tar.gz
+echo "Downloading istio-$ISTIO_VERSION from $URL ..."
+curl -L $URL | tar xz
+# TODO: change this so the version is in the tgz/directory name (users trying multiple versions)
+echo "Downloaded into istio:"
+ls istio
