cbf21f303ec26e89d32e05c2f59cab8be677c405
Laurent Demailly <ldemailly@gmail.com>
2017-05-15 10:10:59 -0700

    fixing the curl to working one
    Former-commit-id: e6a35bd6502ea85b1fba99b87838b1007a2b7144

--- downloadIstio.sh
+++ downloadIstio.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Temporary place holder that gets the job done.
 # We can do arbitrary things in this script over time.
-# This file will be fetched as: curl git.io/getIstio.sh | sh -
+# This file will be fetched as: curl -L https://git.io/getIstio | sh -
 # The script fetches the latest Istio release and untars it.
 
 # TODO: Automate me.
