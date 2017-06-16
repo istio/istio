8296e274f923aca4a4f777826b7d9d96304126ce
Kuat <kyessenov@users.noreply.github.com>
2017-06-15 17:25:20 -0700

    remove race test (#878)

--- pilot/bin/codecov.sh
+++ pilot/bin/codecov.sh
@@ -4,18 +4,7 @@ set -e
 echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
-    options="-coverprofile=profile.out -covermode=atomic"
-    case $d in
-        istio.io/pilot/adapter/config/tpr)
-            echo "Skipping race detection on $d (see https://github.com/istio/pilot/issues/173)"
-            ;;
-        istio.io/pilot/adapter/config/ingress)
-            echo "Skipping race detection on $d (see https://github.com/istio/pilot/issues/173)"
-            ;;
-        *)
-            options=$options" -race"
-            ;;
-    esac
+    options="-coverprofile=profile.out"
     go test $options $d
 
     if [ -f profile.out ]; then
