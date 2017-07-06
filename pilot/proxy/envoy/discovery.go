0fdacf4c523cbb8a7c66f4b8eabccff0ec87e725
Liam White <liamwhite@uk.ibm.com>
2017-07-06 14:11:53 +0100

    correct sds data structure (#906)
    Signed-off-by: Liam White <liamwhite@uk.ibm.com>

--- pilot/proxy/envoy/discovery.go
+++ pilot/proxy/envoy/discovery.go
@@ -151,6 +151,12 @@ type hosts struct {
 type host struct {
 	Address string `json:"ip_address"`
 	Port    int    `json:"port"`
+	Tags    *tags  `json:"tags,omitempty"`
+}
+
+type tags struct {
+	AZ     string `json:"az,omitempty"`
+	Canary bool   `json:"canary,omitempty"`
 
 	// Weight is an integer in the range [1, 100] or empty
 	Weight int `json:"load_balancing_weight,omitempty"`
