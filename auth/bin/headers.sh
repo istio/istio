ef418d378267be546a7299c50c9675c7cf8122b6
Sebastien Vas <sebastienvas@users.noreply.github.com>
2017-07-24 10:46:05 -0700

    Fixes linters issues with bazel proto genfiles (#184)
    * Fixes linters issues with bazel proto genfiles
    
    * Only checking headers from checked in files

--- auth/bin/headers.sh
+++ auth/bin/headers.sh
@@ -2,7 +2,7 @@
 
 exit_status=0
 
-for file in $(find . -name "*.go"); do
+for file in $(git ls-files | grep  -e '.*\.go'); do
   head -n 1 $file | grep -P '^// Copyright 20\d{2} Istio Authors' > /dev/null
   if [[ $? -ne 0 ]]; then
     echo $file does not have a copyright license header
