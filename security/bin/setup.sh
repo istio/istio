de9639326b7042b487b3d0c52962c2753fba5108
Sebastien Vas <sebastienvas@users.noreply.github.com>
2017-07-24 10:46:05 -0700

    Fixes linters issues with bazel proto genfiles (#184)
    * Fixes linters issues with bazel proto genfiles
    
    * Only checking headers from checked in files

--- security/bin/setup.sh
+++ security/bin/setup.sh
@@ -5,6 +5,15 @@ set -ex
 # Create symlinks from bazel directory to the vendor folder.
 bin/bazel_to_go.py
 
+# Link generated proto files
+ln -sf ${PWD}/bazel-genfiles/proto/ca_service.pb.go proto/
+
+
+mkdir -p vendor/github.com/googleapis/googleapis/google/rpc
+for f in $(pwd)/bazel-genfiles/external/com_github_googleapis_googleapis/google/rpc/*.pb.go; do
+  ln -sf $f vendor/github.com/googleapis/googleapis/google/rpc/
+done
+
 # Remove nested vendor/ directories.
 find -L vendor/ -mindepth 1 -name vendor | xargs rm -rf
 
