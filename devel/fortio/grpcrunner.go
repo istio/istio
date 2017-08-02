0e0d5fb2b75fede57f96bea2255cadc3068cc340
Laurent Demailly <ldemailly@google.com>
2017-08-01 19:05:40 -0700

    Grpc runner as library

---
+++ devel/fortio/grpcrunner.go
@@ -0,0 +1,137 @@
+// Copyright 2017 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fortio
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"runtime"
+	"runtime/pprof"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health/grpc_health_v1"
+)
+
+// TODO: refactor common parts between http and grpc runners
+
+// GrpcRunnerResults is the aggregated result of an GrpcRunner.
+// Also is the internal type used per thread/goroutine.
+type GrpcRunnerResults struct {
+	client            grpc_health_v1.HealthClient
+	req               grpc_health_v1.HealthCheckRequest
+	RetCodes          map[grpc_health_v1.HealthCheckResponse_ServingStatus]int64
+	DurationHistogram *Histogram
+	ActualQPS         float64
+}
+
+// Used globally / in TestGrpc() TODO: change periodic.go to carry caller defined context
+var (
+	grpcstate []GrpcRunnerResults
+)
+
+// TestGrpc exercises Grpc health check at the target QPS.
+// To be set as the Function in RunnerOptions.
+func TestGrpc(t int) {
+	Debugf("Calling in %d", t)
+	res, err := grpcstate[t].client.Check(context.Background(), &grpcstate[t].req)
+	Debugf("Got %v %v", res, err)
+	if err != nil {
+		Errf("Error making health check %v", err)
+	} else {
+		grpcstate[t].RetCodes[res.Status]++
+	}
+}
+
+// GrpcRunnerOptions includes the base RunnerOptions plus http specific
+// options.
+type GrpcRunnerOptions struct {
+	RunnerOptions
+	Destination string
+	Service     string
+	Profiler    string // file to save profiles to. defaults to no profiling
+}
+
+// RunGrpcTest runs an http test and returns the aggregated stats.
+func RunGrpcTest(o *GrpcRunnerOptions) (*GrpcRunnerResults, error) {
+	// TODO lock
+	if o.Function == nil {
+		o.Function = TestGrpc
+	}
+	Infof("Starting grpc test for %s with %d threads at %.1f qps", o.Destination, o.NumThreads, o.QPS)
+	r := NewPeriodicRunner(&o.RunnerOptions)
+	numThreads := r.Options().NumThreads
+	total := GrpcRunnerResults{
+		RetCodes: make(map[grpc_health_v1.HealthCheckResponse_ServingStatus]int64),
+	}
+	grpcstate = make([]GrpcRunnerResults, numThreads)
+	for i := 0; i < numThreads; i++ {
+		conn, err := grpc.Dial(o.Destination, grpc.WithInsecure())
+		if err != nil {
+			Errf("Error in grpc dial for %s %v", o.Destination, err)
+			return nil, err
+		}
+		grpcstate[i].client = grpc_health_v1.NewHealthClient(conn)
+		if grpcstate[i].client == nil {
+			return nil, fmt.Errorf("unable to create client %d for %s", i, o.Destination)
+		}
+		grpcstate[i].req = grpc_health_v1.HealthCheckRequest{Service: o.Service}
+		_, err = grpcstate[i].client.Check(context.Background(), &grpcstate[i].req)
+		if err != nil {
+			Errf("Error in first grpc health check call for %s %v", o.Destination, err)
+			return nil, err
+		}
+		// Setup the stats for each 'thread'
+		grpcstate[i].RetCodes = make(map[grpc_health_v1.HealthCheckResponse_ServingStatus]int64)
+	}
+
+	if o.Profiler != "" {
+		fc, err := os.Create(o.Profiler + ".cpu")
+		if err != nil {
+			Critf("Unable to create .cpu profile: %v", err)
+			return nil, err
+		}
+		pprof.StartCPUProfile(fc) //nolint: gas,errcheck
+	}
+	total.ActualQPS, total.DurationHistogram = r.Run()
+	if o.Profiler != "" {
+		pprof.StopCPUProfile()
+		fm, err := os.Create(o.Profiler + ".mem")
+		if err != nil {
+			Critf("Unable to create .mem profile: %v", err)
+			return nil, err
+		}
+		runtime.GC()               // get up-to-date statistics
+		pprof.WriteHeapProfile(fm) // nolint:gas,errcheck
+		fm.Close()                 // nolint:gas,errcheck
+		fmt.Printf("Wrote profile data to %s.{cpu|mem}\n", o.Profiler)
+	}
+	// Numthreads may have reduced
+	numThreads = r.Options().NumThreads
+	keys := []grpc_health_v1.HealthCheckResponse_ServingStatus{}
+	for i := 0; i < numThreads; i++ {
+		// Q: is there some copying each time stats[i] is used?
+		for k := range grpcstate[i].RetCodes {
+			if _, exists := total.RetCodes[k]; !exists {
+				keys = append(keys, k)
+			}
+			total.RetCodes[k] += grpcstate[i].RetCodes[k]
+		}
+	}
+	for _, k := range keys {
+		fmt.Printf("Health %s : %d\n", k.String(), total.RetCodes[k])
+	}
+	return &total, nil
+}
---
+++ devel/fortio/grpcrunner_test.go
@@ -0,0 +1,75 @@
+// Copyright 2017 Istio Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+// Adapted from istio/proxy/test/backend/echo with error handling and
+// concurrency fixes and making it as low overhead as possible
+// (no std output by default)
+
+package fortio
+
+import (
+	"fmt"
+	"net"
+	"testing"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
+)
+
+// DynamicGrpcHealthServer starts and returns the port where a Grpc Health
+// server is running.
+func DynamicGrpcHealthServer() int {
+	socket, err := net.Listen("tcp", ":0")
+	if err != nil {
+		Fatalf("failed to listen: %v", err)
+	}
+	addr := socket.Addr()
+	grpcServer := grpc.NewServer()
+	healthServer := health.NewServer()
+	healthServer.SetServingStatus("ping", grpc_health_v1.HealthCheckResponse_SERVING)
+	grpc_health_v1.RegisterHealthServer(grpcServer, healthServer)
+	fmt.Printf("Fortio %s grpc health server listening on port %v\n", Version, addr)
+	go func(socket net.Listener) {
+		if e := grpcServer.Serve(socket); e != nil {
+			Fatalf("failed to start grpc server: %v", e)
+		}
+	}(socket)
+	return addr.(*net.TCPAddr).Port
+}
+
+func TestGrpcRunner(t *testing.T) {
+	SetLogLevel(Info)
+	port := DynamicGrpcHealthServer()
+	destination := fmt.Sprintf("localhost:%d", port)
+
+	opts := GrpcRunnerOptions{
+		RunnerOptions: RunnerOptions{
+			QPS:        100,
+			Resolution: 0.00001,
+		},
+		Destination: destination,
+	}
+	res, err := RunGrpcTest(&opts)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	totalReq := res.DurationHistogram.Count
+	ok := res.RetCodes[grpc_health_v1.HealthCheckResponse_SERVING]
+	if totalReq != ok {
+		t.Errorf("Mismatch between requests %d and ok %v", totalReq, res.RetCodes)
+	}
+}
