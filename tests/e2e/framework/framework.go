0e76d182ad49a4e8a4515eb71d97105527477716
Charles Xu <chx@google.com>
2017-08-02 16:48:48 -0700

    Add err print back

--- tests/e2e/framework/framework.go
+++ tests/e2e/framework/framework.go
@@ -203,7 +203,7 @@ func (c *CommonConfig) RunTest(m runnable) int {
 		ret = m.Run()
 	}
 	if err := c.saveLogs(ret); err != nil {
-		glog.Warning("Log saving incomplete")
+		glog.Warningf("Log saving incomplete: %v", err)
 	}
 	c.Cleanup.cleanup()
 	return ret
