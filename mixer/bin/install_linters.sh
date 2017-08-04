e85e2d8c7ff34d38ca10a48f6a166de46cc65a33
Sebastien Vas <sebastienvas@users.noreply.github.com>
2017-08-04 11:25:57 -0700

    Enable Prow and Simplify Jenkins setup (#1004)
    * Enable Prow
    
    * Fix typo in Jenkinsfile
    
    * Fix linter to use 1.2 stable version
    
    * Attempt to fix the github.com issue with bazel to go
    
    * Fix postsubmit as well
    
    * Check race conditions
    
    * Replacing test with race test for pre and post submit
    
    * Updating e2e-smoketest for bazel cache
    
    * Fix codecov
    
    * Fix directory creation
    
    * Fix comments
    
    
    Former-commit-id: 07595d4f115a5f07c1f5108e625268f1815f1eaf

--- mixer/bin/install_linters.sh
+++ mixer/bin/install_linters.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Installing or updating linters"
-go get -u github.com/alecthomas/gometalinter
+go get -u gopkg.in/alecthomas/gometalinter.v1
 go get -u github.com/bazelbuild/buildifier/buildifier
 go get -u github.com/3rf/codecoroner
-gometalinter --install --vendored-linters >/dev/null
+gometalinter.v1 --install --vendored-linters >/dev/null
 
 echo Done installing linters
