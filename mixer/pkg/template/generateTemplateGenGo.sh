0b0d91c476ca27d31d6de58e520c842845fd9722
Martin Taillefer <geeknoid@users.noreply.github.com>
2017-08-09 18:04:17 -0700

    Add initial set of templates (#1043)
    Automatic merge from submit-queue
    
    Add initial set of templates.
    
    Also improves comments in generated code.
    
    [Sorry, due to technical difficulty, I had to close my previous PR and open this new one]
    
    **Release note**:
    
    ```release-note
    NONE
    ```
    
    Former-commit-id: 10e5a3ee7f15d31f2487b55e4d0d1b8cdf31dac9

--- mixer/pkg/template/generateTemplateGenGo.sh
+++ mixer/pkg/template/generateTemplateGenGo.sh
@@ -8,6 +8,12 @@ bazel-bin/tools/codegen/cmd/mixgenbootstrap/mixgenbootstrap \
 bazel-genfiles/template/sample/report/go_default_library_proto.descriptor_set:istio.io/mixer/template/sample/report \
 bazel-genfiles/template/sample/check/go_default_library_proto.descriptor_set:istio.io/mixer/template/sample/check \
 bazel-genfiles/template/sample/quota/go_default_library_proto.descriptor_set:istio.io/mixer/template/sample/quota \
+bazel-genfiles/template/listentry/go_default_library_proto.descriptor_set:istio.io/mixer/template/listentry \
+bazel-genfiles/template/logentry/go_default_library_proto.descriptor_set:istio.io/mixer/template/logentry \
+bazel-genfiles/template/metric/go_default_library_proto.descriptor_set:istio.io/mixer/template/metric \
+bazel-genfiles/template/quota/go_default_library_proto.descriptor_set:istio.io/mixer/template/quota \
+bazel-genfiles/template/reportnothing/go_default_library_proto.descriptor_set:istio.io/mixer/template/reportnothing \
+bazel-genfiles/template/checknothing/go_default_library_proto.descriptor_set:istio.io/mixer/template/checknothing \
 -o $MIXERPATH/pkg/template/template.gen.go
 
 bazel build ...
