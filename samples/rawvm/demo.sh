70cc0ff6322ef9a9a6572f6d48fa7c4b0d27a001
Laurent Demailly <ldemailly@google.com>
2017-08-10 14:50:11 -0700

    Using podIP

--- samples/rawvm/demo.sh
+++ samples/rawvm/demo.sh
@@ -6,7 +6,11 @@ FORTIO_IMAGE='gcr.io/istio-testing/fortio:0.2.1-pre1' # escape / needed
 sed -e s!FORTIO_IMAGE!$FORTIO_IMAGE!g < k8services.yaml.in | kubectl apply -f -
 kubectl get -o wide pods
 cli=$(kubectl get pod -l app=fortio -o jsonpath='{.items[0].metadata.name}')
-srv1=$(kubectl get pod -l app=echosrv -o jsonpath='{.items[0].metadata.name}')
-srv2=$(kubectl get pod -l app=echosrv -o jsonpath='{.items[1].metadata.name}')
-url1="http://$srv1:8080/debug?env=dump"
-kubectl exec $cli /usr/local/bin/fortio -- load -loglevel verbose "$url1"
+srv1=$(kubectl get pod -l app=echosrv -o jsonpath='{.items[0].status.podIP}')
+srv2=$(kubectl get pod -l app=echosrv -o jsonpath='{.items[1].status.podIP}')
+debugurlsuffix=":8080/debug?env=dump"
+url1="http://$srv1$debugurlsuffix"
+url2="http://$srv2$debugurlsuffix"
+singlecall="/usr/local/bin/fortio -- load -loglevel verbose -c 1 -qps 0 -t 1ns"
+kubectl exec $cli $singlecall "$url1"
+kubectl exec $cli $singlecall "$url2"
