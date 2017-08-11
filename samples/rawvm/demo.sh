9d7055f1cab972e84ca1feaace1745813aa17104
Laurent Demailly <ldemailly@google.com>
2017-08-10 18:09:26 -0700

    Adding Makefile to process yamls and more
    Doing the istio inject
    
    Current status: 404 from envoy - to be debugged

--- samples/rawvm/demo.sh
+++ samples/rawvm/demo.sh
@@ -1,16 +1,23 @@
 #! /bin/bash
 set -x
 set -e
-FORTIO_IMAGE='gcr.io/istio-testing/fortio:0.2.1-pre1' # escape / needed
 
-sed -e s!FORTIO_IMAGE!$FORTIO_IMAGE!g < k8services.yaml.in | kubectl apply -f -
-kubectl get -o wide pods
+make # default target is istio injected svc and normal client
 cli=$(kubectl get pod -l app=fortio -o jsonpath='{.items[0].metadata.name}')
+cliIp=$(kubectl get pod -l app=fortio -o jsonpath='{.items[0].status.podIP}')
 srv1=$(kubectl get pod -l app=echosrv -o jsonpath='{.items[0].status.podIP}')
 srv2=$(kubectl get pod -l app=echosrv -o jsonpath='{.items[1].status.podIP}')
 debugurlsuffix=":8080/debug?env=dump"
+
+# Direct pod ip to pod ip access:
 url1="http://$srv1$debugurlsuffix"
 url2="http://$srv2$debugurlsuffix"
 singlecall="/usr/local/bin/fortio -- load -loglevel verbose -c 1 -qps 0 -t 1ns"
 kubectl exec $cli $singlecall "$url1"
 kubectl exec $cli $singlecall "$url2"
+grpcping="/usr/local/bin/fortio -- grpcping -loglevel warning -n 100"
+kubectl exec $cli $grpcping $cliIp
+kubectl exec $cli $grpcping $srv1
+kubectl exec $cli $grpcping $srv2
+
+# svc access:
