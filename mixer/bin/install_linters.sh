872607eb78ea9adcfa684de2671ce6c2b01e2770
Laurent Demailly <ldemailly@gmail.com>
2017-08-15 13:46:09 -0700

    (Linters) Adding missing --update which should ... (#1081)
    (Linters) Adding missing --update which should enhance the chance that
    everyone sees the same/latest linter errors
    
    Former-commit-id: b75d02dcdd9a92e7cd02d8408588d22e766706d0

--- mixer/bin/install_linters.sh
+++ mixer/bin/install_linters.sh
@@ -4,6 +4,6 @@ echo "Installing or updating linters"
 go get -u gopkg.in/alecthomas/gometalinter.v1
 go get -u github.com/bazelbuild/buildifier/buildifier
 go get -u github.com/3rf/codecoroner
-gometalinter.v1 --install --vendored-linters >/dev/null
+gometalinter.v1 --update --install --vendored-linters >/dev/null
 
 echo Done installing linters
