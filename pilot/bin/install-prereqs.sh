8fac3a5c1d291ada3c6e7cd0a75ae7396dab2825
Laurent Demailly <ldemailly@gmail.com>
2017-08-15 13:52:35 -0700

    (Linters) Adding missing --update which should ... (#1035)
    (Linters) Adding missing --update which should enhance the chance that
    everyone sees the same/latest linter errors

--- pilot/bin/install-prereqs.sh
+++ pilot/bin/install-prereqs.sh
@@ -4,7 +4,7 @@ set -ex
 
 # Install linters
 go get -u github.com/alecthomas/gometalinter
-gometalinter --install --vendored-linters
+gometalinter --install --update --vendored-linters
 
 # Install buildifier BUILD file validator
 go get -u github.com/bazelbuild/buildifier/buildifier
