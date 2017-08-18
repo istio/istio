c1131c951f31f870a706d9148f22694743e48c9f
Tao Li <taotao12@gmail.com>
2017-08-18 09:44:33 -0700

    Fix lint (#224)

--- security/cmd/istio_ca/main.go
+++ security/cmd/istio_ca/main.go
@@ -160,7 +160,7 @@ func createCA() ca.CertificateAuthority {
 
 	var certChainBytes []byte
 	if opts.certChainFile != "" {
-	        certChainBytes = readFile(opts.certChainFile)
+		certChainBytes = readFile(opts.certChainFile)
 	}
 	caOpts := &ca.IstioCAOptions{
 		CertChainBytes:   certChainBytes,
