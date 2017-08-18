ee6cd93b5163090fb11a5a26c64b9c319c6e3fb1
Zijun Wang <zijun.wang@ibm.com>
2017-08-18 17:27:04 -0400

    Add controller and eventing support for memory configstore  (#1032)
    * add monitor for inmem config store
    
    * update monitor
    
    * update memory config store
    
    * update changes to controller and monitor
    
    * sequential update of monitor
    
    * Async Implementation
    
    * Add conformation tests and update monitor
    
    * formatting + update controller/monitor + update tests
    
    * Update monitor

---
+++ pilot/adapter/config/memory/controller.go
@@ -0,0 +1,93 @@
+// Copyright 2017 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package memory
+
+import (
+	"errors"
+
+	"istio.io/pilot/model"
+)
+
+type controller struct {
+	monitor     Monitor
+	configStore model.ConfigStore
+}
+
+// NewController return an implementation of model.ConfigStoreCache
+func NewController(cs model.ConfigStore) model.ConfigStoreCache {
+	out := &controller{
+		configStore: cs,
+		monitor:     NewConfigStoreMonitor(cs),
+	}
+	return out
+}
+
+func (c *controller) RegisterEventHandler(typ string, f func(model.Config, model.Event)) {
+	c.monitor.AppendEventHandler(typ, f)
+}
+
+// Memory implementation is always synchronized with cache
+func (c *controller) HasSynced() bool {
+	return true
+}
+
+func (c *controller) Run(stop <-chan struct{}) {
+	c.monitor.Start(stop)
+}
+
+func (c *controller) ConfigDescriptor() model.ConfigDescriptor {
+	return c.configStore.ConfigDescriptor()
+}
+
+func (c *controller) Get(typ, key, namespace string) (*model.Config, bool) {
+	return c.configStore.Get(typ, key, namespace)
+}
+
+func (c *controller) Create(config model.Config) (revision string, err error) {
+	if revision, err = c.configStore.Create(config); err == nil {
+		c.monitor.ScheduleProcessEvent(ConfigEvent{
+			config: config,
+			event:  model.EventAdd,
+		})
+	}
+	return
+}
+
+func (c *controller) Update(config model.Config) (newRevision string, err error) {
+	if newRevision, err = c.configStore.Update(config); err == nil {
+		c.monitor.ScheduleProcessEvent(ConfigEvent{
+			config: config,
+			event:  model.EventUpdate,
+		})
+	}
+	return
+}
+
+func (c *controller) Delete(typ, key, namespace string) (err error) {
+	if config, exists := c.Get(typ, key, namespace); exists {
+		if err = c.configStore.Delete(typ, key, namespace); err == nil {
+			c.monitor.ScheduleProcessEvent(ConfigEvent{
+				config: *config,
+				event:  model.EventDelete,
+			})
+			return
+		}
+	}
+	return errors.New("Delete failure: config" + key + "does not exist")
+}
+
+func (c *controller) List(typ, namespace string) ([]model.Config, error) {
+	return c.configStore.List(typ, namespace)
+}
---
+++ pilot/adapter/config/memory/monitor.go
@@ -0,0 +1,105 @@
+// Copyright 2017 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package memory
+
+import (
+	"github.com/golang/glog"
+
+	"istio.io/pilot/model"
+)
+
+const (
+	// BufferSize specifies the buffer size of event channel
+	BufferSize = 10
+)
+
+// Handler specifies a function to apply on a Config for a given event type
+type Handler func(model.Config, model.Event)
+
+// Monitor provides methods of manipulating changes in the config store
+type Monitor interface {
+	Start(<-chan struct{})
+	AppendEventHandler(string, Handler)
+	ScheduleProcessEvent(ConfigEvent)
+}
+
+// ConfigEvent defines the event to be processed
+type ConfigEvent struct {
+	config model.Config
+	event  model.Event
+}
+
+type configstoreMonitor struct {
+	store    model.ConfigStore
+	handlers map[string][]Handler
+	eventCh  chan ConfigEvent
+}
+
+// NewConfigStoreMonitor returns new Monitor implementation
+func NewConfigStoreMonitor(store model.ConfigStore) Monitor {
+	handlers := make(map[string][]Handler)
+
+	for _, typ := range store.ConfigDescriptor().Types() {
+		handlers[typ] = make([]Handler, 0)
+	}
+
+	return &configstoreMonitor{
+		store:    store,
+		handlers: handlers,
+		eventCh:  make(chan ConfigEvent, BufferSize),
+	}
+}
+
+func (m *configstoreMonitor) ScheduleProcessEvent(configEvent ConfigEvent) {
+	m.eventCh <- configEvent
+}
+
+func (m *configstoreMonitor) Start(stop <-chan struct{}) {
+	m.run(stop)
+}
+
+func (m *configstoreMonitor) run(stop <-chan struct{}) {
+	for {
+		select {
+		case <-stop:
+			if _, ok := <-m.eventCh; ok {
+				close(m.eventCh)
+			}
+			return
+		case ce, ok := <-m.eventCh:
+			if ok {
+				m.processConfigEvent(ce)
+			}
+		}
+	}
+}
+
+func (m *configstoreMonitor) processConfigEvent(ce ConfigEvent) {
+	if _, exists := m.handlers[ce.config.Type]; !exists {
+		glog.Warningf("Config Type %s does not exist in config store", ce.config.Type)
+		return
+	}
+	m.applyHandlers(ce.config, ce.event)
+}
+
+func (m *configstoreMonitor) AppendEventHandler(typ string, h Handler) {
+	m.handlers[typ] = append(m.handlers[typ], h)
+}
+
+func (m *configstoreMonitor) applyHandlers(config model.Config, e model.Event) {
+	for _, f := range m.handlers[config.Type] {
+		f(config, e)
+	}
+}
