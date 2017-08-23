61cdc6ca76d2142de3fac2af9f65e8c401ff3811
Jun Mukai <jun.mukai@gmail.com>
2017-08-22 17:35:14 -0700

    gazelle fix (#1124)
    Automatic merge from submit-queue
    
    gazelle fix
    
    - now it processes BUILD.bazel files too
    - fix fmt.sh to support BUILD.bazel
    - rerun ./bin/gazelle, some dependencies are cleaned up
    
    **Release note**:
    
    ```release-note
    none
    ```
    
    Former-commit-id: da3569fab0e2bf314ae4d17724988a46120ba422

--- mixer/bin/fmt.sh
+++ mixer/bin/fmt.sh
@@ -23,7 +23,7 @@ for fl in ${GO_FILES}; do
 done
 gofmt -s -w ${GO_FILES}
 goimports -w -local istio.io ${GO_FILES}
-buildifier -mode=fix $(find . -name BUILD -type f)
+buildifier -mode=fix $(find . \( -name BUILD -o -name BUILD.bazel \) -type f)
 buildifier -mode=fix ./*.bzl
 buildifier -mode=fix ./BUILD.ubuntu
 buildifier -mode=fix ./WORKSPACE
