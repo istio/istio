6628f9a63efbf951b0383d6f311c8863e5791b3d
Yang Guan <yangguan@google.com>
2017-08-24 11:18:51 -0700

    Use megacheck instead of running staticcheck, gosimple and unsued (#245)
    Automatic merge from submit-queue
    
    Use megacheck instead of running staticcheck, gosimple and unsued
    
    Fixed the following linter warning:
    ```
    WARNING: staticcheck, gosimple and unused are all set, using megacheck instead
    ```
    
    **Release note**:
    
    ```NONE
    ```

--- security/bin/linters.sh
+++ security/bin/linters.sh
@@ -10,16 +10,14 @@ gometalinter --concurrency=4 --enable-gc --deadline=300s --disable-all\
   --enable=gofmt\
   --enable=goimports\
   --enable=golint --exclude=.pb.go\
-  --enable=gosimple\
   --enable=gotype\
   --enable=ineffassign\
   --enable=interfacer\
   --enable=lll --line-length=120\
+  --enable=megacheck\
   --enable=misspell\
-  --enable=staticcheck\
   --enable=structcheck\
   --enable=unconvert\
-  --enable=unused\
   --enable=varcheck\
   --enable=vet\
   --enable=vetshadow\
