432346bee7bd79c49e9b1b2bdc8294832b24f6ce
Charles Xu <chx@google.com>
2017-08-24 14:17:10 -0700

    Fast-forward stable branch once passing postsubmit (#238)
    Automatic merge from submit-queue
    
    Fast-forward stable branch once passing postsubmit
    
    **Release note**:
    
    ```release-note
    None
    ```

--- security/prow/auth-postsubmit.sh
+++ security/prow/auth-postsubmit.sh
@@ -63,3 +63,18 @@ fi
 
 echo "=== Running e2e Tests ==="
 bin/e2e.sh --tag ${GIT_SHA} --hub 'gcr.io/istio-testing'
+
+if [ "${CI:-}" == 'bootstrap' ]; then
+    echo "=== Building githubctl ==="
+    bazel build @com_github_istio_test_infra//toolbox/githubctl:githubctl
+
+    echo "=== Fast-forwarding stable branch to new SHA ==="
+    bazel-bin/external/com_github_istio_test_infra/toolbox/githubctl/githubctl \
+        --token_file=/etc/github/oauth \
+        --op=fastForward \
+        --repo=auth \
+        --base_branch=stable \
+        --ref_sha=$GIT_SHA
+else
+    echo "Not in bootstrap environment, skipping stable branch promotion"
+fi
