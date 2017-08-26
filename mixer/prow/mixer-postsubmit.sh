98e979eef0582e5410602da2b463731504b2264d
Charles Xu <chx@google.com>
2017-08-26 00:38:39 -0700

    fast forward stable branch after postsubmit (#1127)
    Former-commit-id: 0f907b5dc4f389aa23007123789d640d02912473

--- mixer/prow/mixer-postsubmit.sh
+++ mixer/prow/mixer-postsubmit.sh
@@ -62,3 +62,18 @@ fi
 
 echo "=== Running e2e Tests ==="
 ./bin/publish-docker-images.sh -t ${GIT_SHA} -h 'gcr.io/istio-testing'
+
+if [ "${CI:-}" == 'bootstrap' ]; then
+    echo "=== Building githubctl ==="
+    bazel build @com_github_istio_test_infra//toolbox/githubctl:githubctl
+
+    echo "=== Fast-forwarding stable branch to new SHA ==="
+    bazel-bin/external/com_github_istio_test_infra/toolbox/githubctl/githubctl \
+        --token_file=/etc/github/oauth \
+        --op=fastForward \
+        --repo=mixer \
+        --base_branch=stable \
+        --ref_sha=$GIT_SHA
+else
+    echo "Not in bootstrap environment, skipping stable branch promotion"
+fi
