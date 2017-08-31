837a797afa168aeb790deb1b6830ba03e6488995
Laurent Demailly <ldemailly@gmail.com>
2017-08-30 17:32:30 -0700

    adding missing create db

--- samples/apps/bookinfo/src/mysql/mysqldb-init.sql
+++ samples/apps/bookinfo/src/mysql/mysqldb-init.sql
@@ -2,6 +2,7 @@
  * Initialize a mysql db with a 'test' db and be able test productpage with it.
  * mysql -h 127.0.0.1 -ppassword < mysqldb-init.sql
  */
+CREATE DATABASE test;
 USE test;
 CREATE TABLE price (name VARCHAR(20), price INT);
 INSERT INTO price VALUES('comedy', 20);
