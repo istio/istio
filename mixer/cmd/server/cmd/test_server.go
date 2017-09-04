be73c3a20c0aecfd895298c4994cae58d2da4030
Manlin Li <30706057+manlinl@users.noreply.github.com>
2017-09-04 09:32:40 -0700

    Refactor mixer server to expose a common setup method for test env (#1190)
    Automatic merge from submit-queue
    
    Refactor mixer server to expose a common setup method for test env
    
    **Release note**:
    
    ```release-note
    ```
    
    Former-commit-id: 49ad083ba35943880f57a16a475c50b1e037dde8

---
+++ mixer/cmd/server/cmd/test_server.go
@@ -0,0 +1,56 @@
+// Copyright 2017 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"istio.io/mixer/cmd/shared"
+	"istio.io/mixer/pkg/expr"
+	"istio.io/mixer/pkg/handler"
+	"istio.io/mixer/pkg/template"
+)
+
+var defaultSeverArgs = serverArgs{
+	maxMessageSize:                1024 * 1024,
+	maxConcurrentStreams:          256,
+	apiWorkerPoolSize:             256,
+	adapterWorkerPoolSize:         256,
+	expressionEvalCacheSize:       expr.DefaultCacheSize,
+	configAPIPort:                 0,
+	monitoringPort:                0,
+	singleThreaded:                false,
+	compressedPayload:             false,
+	traceOutput:                   "",
+	serverCertFile:                "",
+	serverKeyFile:                 "",
+	clientCertFiles:               "",
+	configStoreURL:                "",
+	configStore2URL:               "",
+	configDefaultNamespace:        "",
+	configFetchIntervalSec:        3,
+	configIdentityAttribute:       "target.service",
+	configIdentityAttributeDomain: "",
+	useAst: false,
+}
+
+// SetupTestServer sets up a test server environment
+func SetupTestServer(info map[string]template.Info, adapters []handler.InfoFn, configStoreURL string, configStore2URL string,
+	configDefaultNamespace string, configIdentityAttributeDomain string) *ServerContext {
+	sa := defaultSeverArgs
+	sa.configStoreURL = configStoreURL
+	sa.configStore2URL = configStore2URL
+	sa.configDefaultNamespace = configDefaultNamespace
+	sa.configIdentityAttributeDomain = configIdentityAttributeDomain
+	return setupServer(&sa, info, adapters, shared.Printf, shared.Fatalf)
+}
