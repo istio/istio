4517483f5ad30fe8d948b2a594ff0d69c9e8f9b0
Andra Cismaru <andracis@google.com>
2017-09-10 08:24:06 -0700

    Remove logs creating error

--- tests/e2e/tests/mixer/mixer_test.go
+++ tests/e2e/tests/mixer/mixer_test.go
@@ -182,8 +182,6 @@ func podLogs(labelSelector string, container string) {
 	if err != nil {
 		return
 	}
-
-	_, _ = util.Shell("kubectl --namespace %s logs %s -c %s --tail=40 -p", tc.Kube.Namespace, pod, container)
 	_, _ = util.Shell("kubectl --namespace %s logs %s -c %s --tail=40", tc.Kube.Namespace, pod, container)
 }
 
