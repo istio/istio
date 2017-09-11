87d67fb1379544509832acb0acabb1ab039d8b28
Greg Hanson <gihanson@us.ibm.com>
2017-09-11 14:34:33 -0500

    switched consul rules to use destination.service.  resolved instances where apps were using istio user instead of istio-proxy

--- samples/apps/bookinfo/src/productpage/start_service.sh
+++ samples/apps/bookinfo/src/productpage/start_service.sh
@@ -1,4 +1,4 @@
 
 /opt/istio/prepare_proxy.sh -p 15001 -u 1337
 python /opt/microservices/productpage.py 9080 &
-su istio -c "/opt/istio/pilot-agent proxy -v 2 --serviceregistry Consul > /tmp/envoy.log"
+su istio-proxy -c "/opt/istio/pilot-agent proxy -v 2 --serviceregistry Consul > /tmp/envoy.log"
