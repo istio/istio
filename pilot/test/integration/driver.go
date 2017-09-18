81e7df249e08dee099f4454799948e848319f398
Vadim Eisenberg <vadime@il.ibm.com>
2017-09-19 01:04:14 +0300

    Egress rules integration tests (#1301)
    * added e2e egress rules tests
    
    * renamed egress_rules into egressRules, according to lint
    
    * replaced errors.New(fmt.Sprintf...)) with fmt.Errorf, according to lint
    
    * changed https to http to port 443 in a test

--- pilot/test/integration/driver.go
+++ pilot/test/integration/driver.go
@@ -203,6 +203,7 @@ func runTests(envs ...infra) {
 			&headless{infra: &istio},
 			&ingress{infra: &istio},
 			&egress{infra: &istio},
+			&egressRules{infra: &istio},
 			&routing{infra: &istio},
 			&zipkin{infra: &istio},
 		}
