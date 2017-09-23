72c9328db2b31aaf900579d9457465bce38b3e1a
Laurent Demailly <ldemailly@google.com>
2017-09-22 17:48:46 -0700

    copy downloadIstio.sh into release/downloadIstioCandidate.sh

---
+++ release/downloadIstioCandidate.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+#
+# Early version of a downloader/installer for Istio
+#
+# This file will be fetched as: curl -L https://git.io/getIstio | sh -
+# so it should be pure bourne shell, not bash (and not references other scripts)
+#
+# The script fetches the latest stable Istio release and untars it.
+
+# DO NOT UPDATE THIS VERSION OR SCRIPT LIGHTLY - THIS IS THE "STABLE" VERSION
+ISTIO_VERSION=0.1.6
+
+NAME="istio-$ISTIO_VERSION"
+OS="$(uname)"
+if [ "x${OS}" = "xDarwin" ] ; then
+  OSEXT="osx"
+else
+  # TODO we should check more/complain if not likely to work, etc...
+  OSEXT="linux"
+fi
+URL="https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio-${ISTIO_VERSION}-${OSEXT}.tar.gz"
+echo "Downloading $NAME from $URL ..."
+curl -L "$URL" | tar xz
+# TODO: change this so the version is in the tgz/directory name (users trying multiple versions)
+echo "Downloaded into $NAME:"
+ls $NAME
+BINDIR="$(cd $NAME/bin; pwd)"
+echo "Add $BINDIR to your path; e.g copy paste in your shell and/or ~/.profile:"
+echo "export PATH=\"\$PATH:$BINDIR\""
