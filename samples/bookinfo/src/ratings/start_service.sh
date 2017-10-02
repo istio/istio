5cfe6a51f832006e4b98c72881988b91235ed040
Shriram Rajagopalan <rshriram@users.noreply.github.com>
2017-10-02 15:53:51 -0400

    add zipkin to consul and templatize, update consul bookinfo images (#1000)
    * update bookinfo image versions and automate consul template gen
    
    * missing file
    
    * nits

--- samples/bookinfo/src/ratings/start_service.sh
+++ samples/bookinfo/src/ratings/start_service.sh
@@ -1,4 +1,4 @@
 
 /opt/istio/prepare_proxy.sh -p 15001 -u 1337
 node /opt/microservices/ratings.js 9080 &
-su istio-proxy -c "/opt/istio/pilot-agent proxy -v 2 --serviceregistry Consul > /tmp/envoy.log"
+su istio-proxy -c "/opt/istio/pilot-agent proxy -v 2 --serviceregistry Consul  --serviceCluster ratings-v1 --zipkinAddress zipkin:9411 > /tmp/envoy.log"
