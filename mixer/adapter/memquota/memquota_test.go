6b8ed1979fcec1d6fa102444ceb450d28de4328f
Sunny Gupta <guptasu@google.com>
2017-10-13 11:46:46 -0700

    Rename memquota2 -> memquota (#1330)
    Automatic merge from submit-queue.
    
    Rename memquota2 -> memquota
    
    Simple rename for new adapter to the right name, now since the legacy one is deleted.
    
    **Release note**:
    
    ```release-note
    release-note-none
    ```
    
    Former-commit-id: 2b1dfc7e89ffb98dd1170e9ea9108affe1185cb9

--- mixer/adapter/memquota/memquota_test.go
+++ mixer/adapter/memquota/memquota_test.go
@@ -21,7 +21,7 @@ import (
 	"testing"
 	"time"
 
-	"istio.io/mixer/adapter/memquota2/config"
+	"istio.io/mixer/adapter/memquota/config"
 	"istio.io/mixer/pkg/adapter"
 	"istio.io/mixer/pkg/adapter/test"
 	"istio.io/mixer/template/quota"
