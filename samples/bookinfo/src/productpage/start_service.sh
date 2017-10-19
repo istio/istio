4324901f01d9197182d0e136ae40869d99b24682
Isaiah Snell-Feikema <ijsnellf@us.ibm.com>
2017-10-19 15:24:22 -0500

    removed unnecessary files

--- samples/bookinfo/src/productpage/start_service.sh
+++
@@ -1,4 +0,0 @@
-
-/opt/istio/prepare_proxy.sh -p 15001 -u 1337
-python /opt/microservices/productpage.py 9080 &
-su istio-proxy -c "/opt/istio/pilot-agent proxy -v 2 --serviceregistry Consul --serviceCluster productpage-v1 --zipkinAddress zipkin:9411 > /tmp/envoy.log"
