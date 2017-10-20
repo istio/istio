7adc57b08449ea2c6fd2a6a1cbe82f0c9912b3fd
Shriram Rajagopalan <rshriram@users.noreply.github.com>
2017-10-20 10:46:23 -0400

    Use pods in docker compose (#1186)
    Automatic merge from submit-queue.
    
    Use pods in docker compose
    
    **What this PR does / why we need it**:
    Simple bookinfo (same with or without istio) and a separate set of containers hosting the sidecars. Overtime, these containers can be generated using istioctl docker-inject style commands programmatically. Tested with Consul, and assuming it works similarly in Eureka.
    
    **Which issue this PR fixes** *(optional, in `fixes #<issue number>(, fixes #<issue_number>, ...)` format, will close that issue when PR gets merged)*: fixes #
    
    **Special notes for your reviewer**:
    
    **Release note**:
    
    ```release-note
    POD style construct in docker compose
    ```
    
    Former-commit-id: 4a2e9244987f341bd1d73ae25040b6ac6c4e6f0c

--- samples/bookinfo/src/reviews/reviews-wlpcfg/start_service.sh
+++
@@ -1,4 +0,0 @@
-
-/opt/istio/prepare_proxy.sh -p 15001 -u 1337
-/opt/ibm/wlp/bin/server run defaultServer &
-su istio-proxy -c "/opt/istio/pilot-agent proxy -v 2 --serviceregistry Consul --serviceCluster reviews-${SERVICE_VERSION} --zipkinAddress zipkin:9411 > /tmp/envoy.log"
