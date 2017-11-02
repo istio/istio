6adde9eb7fbd874be8b88beeb5f750b10fdbd903
Mandar U Jog <mjog@google.com>
2017-11-02 14:33:36 +0000

    fix istio location

--- prow/istio-presubmit.sh
+++ prow/istio-presubmit.sh
@@ -32,7 +32,7 @@ set -x
 if [ "${CI:-}" == 'bootstrap' ]; then
   # Test harness will checkout code to directory $GOPATH/src/github.com/istio/istio
   # but we depend on being at path $GOPATH/src/istio.io/istio for imports
-  ln -sf ${GOPATH}/src/github.com/istio ${GOPATH}/src/istio.io
+  mv ${GOPATH}/src/github.com/istio ${GOPATH}/src/istio.io
   cd ${GOPATH}/src/istio.io/istio
 
   # Use the provided pull head sha, from prow.
