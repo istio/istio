ce52c4518b927862fbeeff678441f2e03b4e2939
Mandar U Jog <mjog@google.com>
2017-11-02 18:30:06 +0000

    set ROOT to symlink in bootstrap

--- prow/istio-presubmit.sh
+++ prow/istio-presubmit.sh
@@ -32,7 +32,8 @@ set -x
 if [ "${CI:-}" == 'bootstrap' ]; then
   # Test harness will checkout code to directory $GOPATH/src/github.com/istio/istio
   # but we depend on being at path $GOPATH/src/istio.io/istio for imports
-  mv ${GOPATH}/src/github.com/istio ${GOPATH}/src/istio.io
+  ln -sf ${GOPATH}/src/github.com/istio ${GOPATH}/src/istio.io
+  ROOT=${GOPATH}/src/istio.io/istio
   cd ${GOPATH}/src/istio.io/istio
 
   # Use the provided pull head sha, from prow.
