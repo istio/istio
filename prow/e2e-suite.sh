bc0d9a827d5bce361a01a047607602db0cda9f07
Mandar Jog <mandarjog@gmail.com>
2017-11-03 16:58:37 -0700

    fix env var

--- prow/e2e-suite.sh
+++ prow/e2e-suite.sh
@@ -47,7 +47,7 @@ echo 'Running Integration Tests'
   --mixer_tag "${PULL_PULL_SHA}"\
   --pilot_tag "${PULL_PULL_SHA}"\
   --ca_tag "${PULL_PULL_SHA}"\
-  --istioctl_url "https://storage.googleapis.com/istio-artifacts/pilot/${PULL_PULL_URL}/artifacts/istioctl"\
-  --auth_debian_url "https://storage.googleapis.com/istio-artifacts/auth/${PULL_PULL_URL}/artifacts/debs"\
-  --pilot_debian_url "https://storage.googleapis.com/istio-artifacts/pilot/${PULL_PULL_URL}/artifacts/debs"
+  --istioctl_url "https://storage.googleapis.com/istio-artifacts/pilot/${PULL_PULL_SHA}/artifacts/istioctl"\
+  --auth_debian_url "https://storage.googleapis.com/istio-artifacts/auth/${PULL_PULL_SHA}/artifacts/debs"\
+  --pilot_debian_url "https://storage.googleapis.com/istio-artifacts/pilot/${PULL_PULL_SHA}/artifacts/debs"
 
