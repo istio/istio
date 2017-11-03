844a3f3ee16b8eedf5eb98d6cf66a7d44d80384e
ozben <ozben@google.com>
2017-11-03 10:53:51 -0700

    Generify the function call interface.

--- mixer/pkg/il/compiler/compiler.go
+++ mixer/pkg/il/compiler/compiler.go
@@ -208,19 +208,11 @@ func (g *generator) generateFunction(f *expr.Function, depth int, mode nilMode, 
 		g.generateIndex(f, depth, mode, valueJmpLabel)
 	case "OR":
 		g.generateOr(f, depth, mode, valueJmpLabel)
-	case "ip":
-		g.generate(f.Args[0], depth+1, nmNone, "")
-		g.builder.Call("ip")
-	case "timestamp":
-		g.generate(f.Args[0], depth+1, nmNone, "")
-		g.builder.Call("timestamp")
-	case "match":
-		g.generate(f.Args[0], depth+1, nmNone, "")
-		g.generate(f.Args[1], depth+1, nmNone, "")
-		g.builder.Call("match")
 	default:
-		// TODO: generalize "ip" and "match" case to iterate over Args and append Call
-		g.internalError("function not yet implemented: %s", f.Name)
+		for _, arg := range f.Args {
+			g.generate(arg, depth, nmNone, "")
+		}
+		g.builder.Call(f.Name)
 	}
 }
 
