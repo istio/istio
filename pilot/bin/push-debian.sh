bb67e1cbbe3f3f7cb12b1d27169ab0ddc75a6851
mandarjog <mandarjog@gmail.com>
2017-11-03 21:23:19 -0700

    Monorepo: push and use latest images for testing (#1405)
    * push images
    
    * remove manual go builds temporarily
    
    * fix env var
    
    * remove debian flags
    
    * also use HUB

--- pilot/bin/push-debian.sh
+++ pilot/bin/push-debian.sh
@@ -10,8 +10,8 @@
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
 VERSION_FILE="${ROOT}/tools/deb/version"
 BAZEL_ARGS=()
-BAZEL_TARGET='//tools/deb:istio-agent'
-BAZEL_BINARY="${ROOT}/bazel-bin/tools/deb/istio-agent"
+BAZEL_TARGET='//pilot/tools/deb:istio-agent'
+BAZEL_BINARY="${ROOT}/../bazel-bin/pilot/tools/deb/istio-agent"
 
 set -ex
 
