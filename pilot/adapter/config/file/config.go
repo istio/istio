istio/istio

commit ac16f02d3574b7c21d08dd3fc71e2cf13056ebda

[pilot] Adding file-based ConfigStore (#1601)
* Adding file-based ConfigStore

This is to support integration testing of Pilot.

* addressing comments.

* Adding file-based ConfigStore

This is to support integration testing of Pilot.

* addressing comments

pilot/adapter/config/file/config.go
@@ -1,0 +1,90 @@
+// Copyright 2017 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package file
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"istio.io/istio/pilot/model"
+)
+
+const (
+	defaultNamespace = "default"
+	defaultDomain    = "cluster.local"
+)
+
+var (
+	// A function that applies a default namespace and domain to new ConfigRef instances
+	Defaults = WithDefaults(defaultNamespace, defaultDomain)
+)
+
+// Information for a single element of configuration stored in a file.
+type ConfigRef struct {
+	Meta     *model.ConfigMeta
+	FilePath string
+}
+
+// Returns a function that applies the provided namespace and domain to ConfigRef instances.
+func WithDefaults(namespace, domain string) func(*ConfigRef) *ConfigRef {
+	return func(c *ConfigRef) *ConfigRef {
+		c.Meta.Namespace = namespace
+		c.Meta.Domain = domain
+		return c
+	}
+}
+
+// A decorator around another ConfigStore that adds support for loading configuration elements from files.
+type ConfigStore interface {
+	model.ConfigStore
+
+	// Create a new configuration element from the specified file.
+	CreateFromFile(config ConfigRef) error
+}
+
+type configStore struct {
+	model.ConfigStore
+}
+
+// Creates a new file-based config store.
+func NewConfigStore(store model.ConfigStore) ConfigStore {
+	return &configStore{store}
+}
+
+func (store *configStore) CreateFromFile(config ConfigRef) error {
+	schema, ok := model.IstioConfigTypes.GetByType(config.Meta.Type)
+	if !ok {
+		return fmt.Errorf("missing schema for %q", config.Meta.Type)
+	}
+	content, err := ioutil.ReadFile(config.FilePath)
+	if err != nil {
+		return err
+	}
+	spec, err := schema.FromYAML(string(content))
+	if err != nil {
+		return err
+	}
+	out := model.Config{
+		ConfigMeta: *config.Meta,
+		Spec:       spec,
+	}
+
+	_, err = store.Create(out)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}

pilot/adapter/config/file/config_test.go
@@ -1,0 +1,64 @@
+// Copyright 2017 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package file_test
+
+import (
+	"testing"
+
+	"istio.io/istio/pilot/adapter/config/file"
+	"istio.io/istio/pilot/adapter/config/memory"
+	"istio.io/istio/pilot/model"
+)
+
+func newConfig(schemaType string, name string, filePath string) *file.ConfigRef {
+	return file.Defaults(&file.ConfigRef{
+		Meta: &model.ConfigMeta{
+			Name: name,
+			Type: schemaType,
+		},
+		FilePath: filePath,
+	})
+}
+
+func TestAllConfigs(t *testing.T) {
+	cases := []*file.ConfigRef{
+		newConfig(model.DestinationPolicy.Type, "circuit-breaker", "testdata/cb-policy.yaml.golden"),
+		newConfig(model.RouteRule.Type, "timeout", "testdata/timeout-route-rule.yaml.golden"),
+		newConfig(model.RouteRule.Type, "weighted", "testdata/weighted-route.yaml.golden"),
+		newConfig(model.RouteRule.Type, "fault", "testdata/fault-route.yaml.golden"),
+		newConfig(model.RouteRule.Type, "redirect", "testdata/redirect-route.yaml.golden"),
+		newConfig(model.RouteRule.Type, "rewrite", "testdata/rewrite-route.yaml.golden"),
+		newConfig(model.RouteRule.Type, "websocket", "testdata/websocket-route.yaml.golden"),
+		newConfig(model.EgressRule.Type, "google", "testdata/egress-rule.yaml.golden"),
+		newConfig(model.DestinationPolicy.Type, "egress-circuit-breaker", "testdata/egress-rule-cb-policy.yaml.golden"),
+		newConfig(model.RouteRule.Type, "egress-timeout", "testdata/egress-rule-timeout-route-rule.yaml.golden"),
+		newConfig(model.IngressRule.Type, "world", "testdata/ingress-route-world.yaml.golden"),
+		newConfig(model.IngressRule.Type, "foo", "testdata/ingress-route-foo.yaml.golden"),
+	}
+
+	for _, input := range cases {
+		t.Run("test case name", func(t *testing.T) {
+			mockStore := memory.Make(model.IstioConfigTypes)
+			configStore := file.NewConfigStore(mockStore)
+			inputMeta := input.Meta
+			if err := configStore.CreateFromFile(*input); err != nil {
+				t.Fatalf("failed creating config ", input)
+			} else if _, exists := configStore.Get(inputMeta.Type, inputMeta.Name, inputMeta.Namespace); !exists {
+				t.Fatalf("missing config ", input)
+			}
+		})
+		// TODO(nmittler): Do we care about doing a deep comparison?
+	}
+}

pilot/adapter/config/file/BUILD
@@ -1,0 +1,27 @@
+load("@io_bazel_rules_go//go:def.bzl", "go_library", "go_test")
+
+go_library(
+    name = "go_default_library",
+    srcs = [
+        "config.go",
+    ],
+    visibility = ["//visibility:public"],
+    deps = [
+        "//pilot/model:go_default_library",
+        "@com_github_golang_glog//:go_default_library",
+    ],
+)
+
+go_test(
+    name = "go_default_xtest",
+    size = "small",
+    srcs = [
+        "config_test.go",
+    ],
+    data = glob(["testdata/*.golden"]),
+    deps = [
+        ":go_default_library",
+        "//pilot/adapter/config/memory:go_default_library",
+        "//pilot/model:go_default_library",
+    ],
+)

pilot/adapter/config/file/testdata/cb-policy.yaml.golden
@@ -1,0 +1,20 @@
+source:
+  name: hello
+  labels:
+    version: v0
+destination:
+  name: world
+  labels:
+    version: v0
+circuitBreaker:
+  simpleCb:
+    maxConnections: 100
+    sleepWindow: 15.5s
+    httpMaxRequests: 100
+    httpMaxRequestsPerConnection: 100
+    httpMaxPendingRequests: 100
+    httpConsecutiveErrors: 10
+    httpDetectionInterval: 30s
+    httpMaxEjectionPercent: 100
+loadBalancing:
+  name: RANDOM

pilot/adapter/config/file/testdata/egress-rule-cb-policy.yaml.golden
@@ -1,0 +1,18 @@
+source:
+  name: hello
+  labels:
+    version: v0
+destination:
+  service: "*.google.com"
+circuitBreaker:
+  simpleCb:
+    maxConnections: 9
+    sleepWindow: 9s
+    httpMaxRequests: 9
+    httpMaxRequestsPerConnection: 9
+    httpMaxPendingRequests: 9
+    httpConsecutiveErrors: 9
+    httpDetectionInterval: 9s
+    httpMaxEjectionPercent: 9
+loadBalancing:
+  name: RANDOM

pilot/adapter/config/file/testdata/egress-rule-timeout-route-rule.yaml.golden
@@ -1,0 +1,9 @@
+destination:
+  service: "*.google.com"
+httpReqTimeout:
+  simpleTimeout:
+    timeout: 30s
+httpReqRetries:
+  simple_retry:
+    attempts: 1
+    perTryTimeout: 5s

pilot/adapter/config/file/testdata/egress-rule.yaml.golden
@@ -1,0 +1,9 @@
+destination:
+  service: "*.google.com"
+ports:
+- port: 80
+  protocol: http
+- port: 443
+  protocol: https
+- port: 8080
+  protocol: http2

pilot/adapter/config/file/testdata/fault-route.yaml.golden
@@ -1,0 +1,25 @@
+destination:
+  name: world
+match:
+  source:
+    name: hello
+    labels:
+      version: v0
+  request:
+    headers:
+      scooby:
+        exact: doo
+      animal:
+        prefix: dog.cat
+      name:
+        regex: "sco+do+"
+route:
+  - labels:
+       version: v1
+httpFault:
+  delay:
+    percent: 100
+    fixed_delay: 5s
+  abort:
+    percent: 100
+    http_status: 503

commit 1ceb205d9d06c3ad9423bc6809e26b4cf0ea4022

[pilot] Fixing linter errors in new file config store
**What this PR does / why we need it**:
Linter errors were introduced by commit: ac16f02d3574b7c21d08dd3fc71e2cf13056ebda.

pilot/adapter/config/file/config.go
@@ -27,17 +27,17 @@ const (
 )
 
 var (
-	// A function that applies a default namespace and domain to new ConfigRef instances
+	// Defaults is a function that applies a default namespace and domain to new ConfigRef instances
 	Defaults = WithDefaults(defaultNamespace, defaultDomain)
 )
 
-// Information for a single element of configuration stored in a file.
+// ConfigRef provides information for a single element of configuration stored in a file.
 type ConfigRef struct {
 	Meta     *model.ConfigMeta
 	FilePath string
 }
 
-// Returns a function that applies the provided namespace and domain to ConfigRef instances.
+// WithDefaults applies the provided namespace and domain to ConfigRef instances.
 func WithDefaults(namespace, domain string) func(*ConfigRef) *ConfigRef {
 	return func(c *ConfigRef) *ConfigRef {
 		c.Meta.Namespace = namespace
@@ -46,11 +46,11 @@ func WithDefaults(namespace, domain string) func(*ConfigRef) *ConfigRef {
 	}
 }
 
-// A decorator around another ConfigStore that adds support for loading configuration elements from files.
+// ConfigStore is a decorator around another config store that adds support for loading configuration elements from files.
 type ConfigStore interface {
 	model.ConfigStore
 
-	// Create a new configuration element from the specified file.
+	// CreateFromFile create a new configuration element from the specified file.
 	CreateFromFile(config ConfigRef) error
 }
 
@@ -58,11 +58,12 @@ type configStore struct {
 	model.ConfigStore
 }
 
-// Creates a new file-based config store.
+// NewConfigStore creates a new file-based config store.
 func NewConfigStore(store model.ConfigStore) ConfigStore {
 	return &configStore{store}
 }
 
+// CreateFromFile create a new configuration element from the specified file.
 func (store *configStore) CreateFromFile(config ConfigRef) error {
 	schema, ok := model.IstioConfigTypes.GetByType(config.Meta.Type)
 	if !ok {
@@ -82,9 +83,5 @@ func (store *configStore) CreateFromFile(config ConfigRef) error {
 	}
 
 	_, err = store.Create(out)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return err
 }

pilot/adapter/config/file/config_test.go
@@ -54,9 +54,9 @@ func TestAllConfigs(t *testing.T) {
 			configStore := file.NewConfigStore(mockStore)
 			inputMeta := input.Meta
 			if err := configStore.CreateFromFile(*input); err != nil {
-				t.Fatalf("failed creating config ", input)
+				t.Fatalf("failed creating config %v", input)
 			} else if _, exists := configStore.Get(inputMeta.Type, inputMeta.Name, inputMeta.Namespace); !exists {
-				t.Fatalf("missing config ", input)
+				t.Fatalf("missing config %v", input)
 			}
 		})
 		// TODO(nmittler): Do we care about doing a deep comparison?