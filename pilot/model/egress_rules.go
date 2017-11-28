eaf2c342592680d647abfba00300ced264df397f
Vadim Eisenberg <vadime@il.ibm.com>
2017-11-28 11:11:04 +0200

    fixed a typo

--- pilot/model/egress_rules.go
+++ pilot/model/egress_rules.go
@@ -23,7 +23,7 @@ import (
 )
 
 // RejectConflictingEgressRules rejects rules that have the destination which is equal to
-// the destionation of some other rule.
+// the destination of some other rule.
 // According to Envoy's virtual host specification, no virtual hosts can share the same domain.
 // The following code rejects conflicting rules deterministically, by a lexicographical order -
 // a rule with a smaller key lexicographically wins.
