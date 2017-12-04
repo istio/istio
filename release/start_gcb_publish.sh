e83e9c50a659eeaea48d34aa1574875aee1dcb6d
Matt Delco <delco@google.com>
2017-12-03 18:35:13 -0800

    fix template name

--- release/start_gcb_publish.sh
+++ release/start_gcb_publish.sh
@@ -126,7 +126,7 @@ echo "  \"substitutions\": {
     \"_REPO\": \"${REL_REPO}\",
   }" >> "${SUBS_FILE}"
 
-run_build "${REPO}" "${REPO_FILE}" "${REPO_FILE_VER}" "cloud_tag.template.json" \
+run_build "${REPO}" "${REPO_FILE}" "${REPO_FILE_VER}" "cloud_publish.template.json" \
   "${SUBS_FILE}" "${PROJECT_ID}" "${SVC_ACCT}" "${KEY_FILE_PATH}" "${WAIT_FOR_RESULT}"
 
 # cleanup
