8aa7c68911763cda74954d369d6833c306ff2df7
Matt Delco <delco@google.com>
2017-12-04 13:53:08 -0800

    get latest ver from git

--- release/downloadIstioCandidate.sh
+++ release/downloadIstioCandidate.sh
@@ -6,15 +6,13 @@
 # so it should be pure bourne shell, not bash (and not reference other scripts)
 #
 # The script fetches the latest Istio release candidate and untars it.
-# It's a copy of ../downloadIstio.sh which is for stable releases but lets
+# It's derived from ../downloadIstio.sh which is for stable releases but lets
 # users do curl -L https://git.io/getLatestIstio | ISTIO_VERSION=0.3.6 sh -
 # for instance to change the version fetched.
 
 # This is the latest release candidate (matches ../istio.VERSION after basic
 # sanity checks)
-ISTIO_VERSION=${ISTIO_VERSION:-0.3.0}
 
-NAME="istio-$ISTIO_VERSION"
 OS="$(uname)"
 if [ "x${OS}" = "xDarwin" ] ; then
   OSEXT="osx"
@@ -22,7 +20,18 @@ else
   # TODO we should check more/complain if not likely to work, etc...
   OSEXT="linux"
 fi
-URL="https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio-${ISTIO_VERSION}-${OSEXT}.tar.gz"
+
+if [ "x${ISTIO_VERSION}" = "x" ] ; then
+  TEMP_FILE="$(mktemp /tmp/istio.latest.XXXX)"
+  curl -L -s -o "$TEMP_FILE" https://api.github.com/repos/istio/istio/releases/latest
+  NAME=istio-$(grep tag_name "$TEMP_FILE" | sed "s/ *\"tag_name\": *\"\(.*\)\",*/\1/")
+  URL=$(grep browser_download_url "$TEMP_FILE" | grep $OSEXT | sed "s/ *\"browser_download_url\": *\"\(.*\)\",*/\1/")
+  rm "$TEMP_FILE"
+else
+  NAME="istio-$ISTIO_VERSION"
+  URL="https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio-${ISTIO_VERSION}-${OSEXT}.tar.gz"
+fi
+
 echo "Downloading $NAME from $URL ..."
 curl -L "$URL" | tar xz
 # TODO: change this so the version is in the tgz/directory name (users trying multiple versions)
