a9768aeafbd0e124ee4cd87a29635c842e22f9fe
guptasu <guptasu@google.com>
2017-12-08 00:28:46 -0800

    d

--- mixer/adapter/kubernetes/kubernetes.go
+++ mixer/adapter/kubernetes/kubernetes.go
@@ -215,7 +215,7 @@ func (*builder) ValidateConfig(c adapter.Config) (ce *adapter.ConfigErrors) {
 }
 
 func (b *builder) BuildAttributesGenerator(env adapter.Env, c adapter.Config) (adapter.AttributesGenerator, error) {
-	env.Logger().Warningf("guptasu22233 ** trying local changes...")
+	env.Logger().Warningf("kub-guptasu22233 ** trying local changes...")
 
 	paramsProto := c.(*config.Params)
 	b.Lock()
