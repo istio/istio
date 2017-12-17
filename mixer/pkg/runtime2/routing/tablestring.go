bc37ae9e1ad50a3af5cd050102e06f80067f7407
ozben <ozben@google.com>
2017-12-16 18:34:25 -0800

    Stable sorting for testing.

--- mixer/pkg/runtime2/routing/tablestring.go
+++ mixer/pkg/runtime2/routing/tablestring.go
@@ -21,6 +21,7 @@ import (
 	"strings"
 
 	"istio.io/api/mixer/v1/template"
+	"istio.io/istio/mixer/pkg/template"
 )
 
 func (t *Table) String() string {
@@ -92,14 +93,24 @@ func (d *DestinationSet) write(b *bytes.Buffer, indent int, debugInfo *tableDebu
 
 		indent++
 		idnt := strings.Repeat("  ", indent)
-		for i, input := range e.Inputs {
+
+		inputs := e.Inputs
+		if debugInfo != nil {
+			// sort entries by condition string for stable ordering. This helps with tests.
+			s := inputSetSorter{
+				inputs: inputs,
+				debugInfo: debugInfo,
+			}
+			sort.Stable(&s)
+		}
+
+		for i, input := range inputs {
 			fmt.Fprintf(b, "%s[#%d]", idnt, i)
 			fmt.Fprintln(b)
-			input.write(b, indent + 1, debugInfo)
+			input.write(b, indent+1, debugInfo)
 		}
 	}
 }
-
 func (d *DestinationSet) String() string {
 	var b bytes.Buffer
 	d.write(&b, 0, nil)
@@ -121,12 +132,25 @@ func (i *InputSet) write(b *bytes.Buffer, indent int, debugInfo *tableDebugInfo)
 	}
 	fmt.Fprintln(b)
 
-	for i, bld := range i.Builders {
-		fmt.Fprintf(b, "%s[#%d]", idnt, i)
-		if debugInfo != nil {
-			fmt.Fprintf(b, " %s {I}", debugInfo.instanceNames[bld])
+	if debugInfo != nil {
+		s := builderSorter {
+			builders: i.Builders,
+			debugInfo: debugInfo,
 		}
-		fmt.Fprintln(b)
+		sort.Stable(&s)
+		for i, bld := range i.Builders {
+			fmt.Fprintf(b, "%s[#%d]", idnt, i)
+			if debugInfo != nil {
+				fmt.Fprintf(b, " %s {I}", debugInfo.instanceNames[bld])
+			}
+			fmt.Fprintln(b)
+		}
+
+	} else {
+		for i, bld := range i.Builders {
+			fmt.Fprintf(b, "%s[#%d] %v", idnt, i, bld)
+			fmt.Fprintln(b)
+		}
 	}
 }
 
@@ -134,4 +158,51 @@ func (i *InputSet) String() string {
 	var b bytes.Buffer
 	i.write(&b, 0, nil)
 	return b.String()
+}
+
+
+type inputSetSorter struct {
+	inputs []*InputSet
+	debugInfo *tableDebugInfo
+}
+
+// Len is part of sort.Interface.
+func (s *inputSetSorter) Len() int {
+	return len(s.inputs)
+}
+
+// Swap is part of sort.Interface.
+func (s *inputSetSorter) Swap(i, j int) {
+	s.inputs[i], s.inputs[j] = s.inputs[j], s.inputs[i]
+}
+
+// Less is part of sort.Interface.
+func (s *inputSetSorter) Less(i, j int) bool {
+	if s.inputs[j].Condition == nil {
+		return false
+	}
+	if s.inputs[i].Condition == nil {
+		return true
+	}
+	return s.debugInfo.matchConditions[s.inputs[i].Condition] < s.debugInfo.matchConditions[s.inputs[j].Condition]
+}
+
+type builderSorter struct {
+	builders []template.InstanceBuilder
+	debugInfo *tableDebugInfo
+}
+
+// Len is part of sort.Interface.
+func (s *builderSorter) Len() int {
+	return len(s.builders)
+}
+
+// Swap is part of sort.Interface.
+func (s *builderSorter) Swap(i, j int) {
+	s.builders[i], s.builders[j] = s.builders[j], s.builders[i]
+}
+
+// Less is part of sort.Interface.
+func (s *builderSorter) Less(i, j int) bool {
+	return s.debugInfo.instanceNames[s.builders[i]] < s.debugInfo.instanceNames[s.builders[j]]
 }
