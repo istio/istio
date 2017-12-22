c8bccac9cdbbc11713bb250566f6b617efc680f4
Costin Manolache <costin@gmail.com>
2017-12-22 11:58:11 -0800

    Found the bug - test was looking for error string, grpc changed the error message

--- security/cmd/node_agent/na/nodeagent_test.go
+++ security/cmd/node_agent/na/nodeagent_test.go
@@ -32,6 +32,7 @@ import (
 	mockutil "istio.io/istio/security/pkg/util/mock"
 	"istio.io/istio/security/pkg/workload"
 	pb "istio.io/istio/security/proto"
+	"strings"
 )
 
 const (
@@ -261,7 +262,7 @@ func TestSendCSRAgainstLocalInstance(t *testing.T) {
 			}, "", "service1", "", []byte{}, "", true},
 			res:         defaultServerResponse,
 			cAClient:    &cAGrpcClientImpl{},
-			expectedErr: "CSR request failed rpc error: code = Unavailable desc = grpc: the connection is unavailable",
+			expectedErr: "CSR request failed rpc error: code = Unavailable",
 		},
 		"Without Insecure option": {
 			config: &Config{
@@ -324,7 +325,7 @@ func TestSendCSRAgainstLocalInstance(t *testing.T) {
 		if len(c.expectedErr) > 0 {
 			if err == nil {
 				t.Errorf("Error expected: %v", c.expectedErr)
-			} else if err.Error() != c.expectedErr {
+			} else if ! strings.Contains(err.Error(), c.expectedErr) {
 				t.Errorf("%s: incorrect error message: got [%s] VS want [%s]", id, err.Error(), c.expectedErr)
 			}
 		} else {
