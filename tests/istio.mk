0fa1d859a3795dfcc16a2aca4589d7ecb1404fa3
Costin Manolache <costin@gmail.com>
2018-01-03 00:44:14 -0800

    Add timeout to the test itself

--- tests/istio.mk
+++ tests/istio.mk
@@ -58,16 +58,16 @@ e2e: istioctl
 # Simple e2e test using fortio, approx 2 min
 e2e_simple: istioctl
 	@echo "=== E2E testing with ${TAG} and ${HUB}"
-	go test  -v ${TEST_ARGS:-} ./tests/e2e/tests/simple -args ${E2E_ARGS} --mixer_tag ${TAG} --pilot_tag ${TAG} --ca_tag ${TAG} \
+	go test  -timeout 20m -v ${TEST_ARGS:-} ./tests/e2e/tests/simple -args ${E2E_ARGS} --mixer_tag ${TAG} --pilot_tag ${TAG} --ca_tag ${TAG} \
              --mixer_hub ${HUB} --pilot_hub ${HUB} --ca_hub ${HUB}
 
 
 e2e_mixer: istioctl
-	go test  -v ${TEST_ARGS:-} ./tests/e2e/tests/mixer -args ${E2E_ARGS} --mixer_tag ${TAG} --pilot_tag ${TAG} --ca_tag ${TAG} \
+	go test  -timeout 20m -v ${TEST_ARGS:-} ./tests/e2e/tests/mixer -args ${E2E_ARGS} --mixer_tag ${TAG} --pilot_tag ${TAG} --ca_tag ${TAG} \
                           --mixer_hub ${HUB} --pilot_hub ${HUB} --ca_hub ${HUB}
 
 e2e_bookinfo: istioctl
-	go test -v ${TEST_ARGS:-} ./tests/e2e/tests/bookinfo -args ${E2E_ARGS} --mixer_tag ${TAG} --pilot_tag ${TAG} --ca_tag ${TAG} \
+	go test -timeout 20m -v ${TEST_ARGS:-} ./tests/e2e/tests/bookinfo -args ${E2E_ARGS} --mixer_tag ${TAG} --pilot_tag ${TAG} --ca_tag ${TAG} \
                          --mixer_hub ${HUB} --pilot_hub ${HUB} --ca_hub ${HUB}
 
 e2e_all: e2e_simple e2e_mixer e2e_bookinfo
