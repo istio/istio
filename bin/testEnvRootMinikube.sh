1890ab7c482ff99d3ce7a9994e933fe01b99f14e
Costin Manolache <costin@gmail.com>
2018-01-04 14:18:08 -0800

    Make the script consistent with original in-line

--- bin/testEnvRootMinikube.sh
+++ bin/testEnvRootMinikube.sh
@@ -1,43 +1,41 @@
 #!/usr/bin/env bash
 
-if [ ! -f $GOPATH/bin/minikube ]; then
-    curl -Lo $GOPATH/bin/minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x $GOPATH/bin/minikube
+if [ ! -f /usr/local/bin/minikube ]; then
+   curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.22.3/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
 fi
-if [ ! -f $GOPATH/bin/kubectl ]; then
-    curl -Lo $GOPATH/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x $GOPATH/bin/kubectl
+if [ ! -f /usr/local/bin/kubectl ]; then
+   curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.7.4/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 fi
 
-export MINIKUBE_WANTUPDATENOTIFICATION=false
-export MINIKUBE_WANTREPORTERRORPROMPT=false
-export MINIKUBE_HOME=$HOME
-export CHANGE_MINIKUBE_NONE_USER=true
 
 export KUBECONFIG=${KUBECONFIG:-$GOPATH/minikube.conf}
-export PATH=$GOPATH/bin:$PATH
 
 function waitMinikube() {
     set +e
-    $GOPATH/bin/kubectl cluster-info
+    kubectl cluster-info
     # this for loop waits until kubectl can access the api server that Minikube has created
     for i in {1..150}; do # timeout for 5 minutes
-       $GOPATH/bin/kubectl get po &> /dev/null
+       kubectl get po &> /dev/null
        if [ $? -ne 1 ]; then
           break
       fi
       sleep 2
     done
-    cat $KUBECONFIG
-    $GOPATH/bin/kubectl get svc --all-namespaces
+    kubectl get svc --all-namespaces
     echo "Minikube is running"
 }
 
 
 # Requires sudo ! Start real kubernetes minikube with none driver
 function startMinikubeNone() {
-    sudo -E $GOPATH/bin/minikube start \
+    export MINIKUBE_WANTUPDATENOTIFICATION=false
+    export MINIKUBE_WANTREPORTERRORPROMPT=false
+    export MINIKUBE_HOME=$HOME
+    export CHANGE_MINIKUBE_NONE_USER=true
+    sudo -E minikube start \
             --extra-config=apiserver.Admission.PluginNames="Initializers,NamespaceLifecycle,LimitRanger,ServiceAccount,DefaultStorageClass,GenericAdmissionWebhook,ResourceQuota" \
             --kubernetes-version=v1.7.5 --vm-driver=none
-    sudo -E $GOPATH/bin/minikube update-context
+    sudo -E minikube update-context
     sudo chown -R $(id -u) $KUBECONFIG $HOME/.minikube
 }
 
