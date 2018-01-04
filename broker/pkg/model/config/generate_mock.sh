d001459abbf6b1aac25ad4bc986ec741444b4e06
Douglas Reid <douglas-reid@users.noreply.github.com>
2018-01-04 14:30:34 -0800

    No more mock

--- broker/pkg/model/config/generate_mock.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-pushd $GOPATH/src/istio.io/istio/vendor/github.com/golang/mock/mockgen
-go build
-popd
-
-$GOPATH/src/istio.io/istio/vendor/github.com/golang/mock/mockgen/mockgen -source store.go -destination mock_store_test.go -package config
--- broker/pkg/model/config/store.go
+++ broker/pkg/model/config/store.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:generate sh -c ./generate_mock.sh
-
 package config
 
 import (
