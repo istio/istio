493c71425faa84711f91d0b53fe39ca5a9deba30
ozben <ozben@google.com>
2018-01-05 14:04:30 -0800

    Add a standard AttributeDescriptorFinder implementation in expr.

---
+++ mixer/pkg/expr/finder.go
@@ -0,0 +1,71 @@
+// Copyright 2018 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package expr
+
+import (
+	"fmt"
+	"sort"
+
+	configpb "istio.io/istio/mixer/pkg/config/proto"
+	"istio.io/istio/mixer/pkg/pool"
+)
+
+// EmptyFinder is a AttributeDescriptorFinder with no attributes
+var EmptyFinder AttributeDescriptorFinder = finder{
+	attributes: make(map[string]*configpb.AttributeManifest_AttributeInfo, 0),
+}
+
+// NewFinder returns a new AttributeDescriptorFinder instances, based on the given attributes
+func NewFinder(attributes map[string]*configpb.AttributeManifest_AttributeInfo) AttributeDescriptorFinder {
+	return finder{
+		attributes: attributes,
+	}
+}
+
+// finder exposes expr.AttributeDescriptorFinder
+type finder struct {
+	attributes map[string]*configpb.AttributeManifest_AttributeInfo
+}
+
+var _ AttributeDescriptorFinder = finder{}
+
+// GetAttribute finds an attribute by Name.
+// This function is only called when a new Handler is instantiated.
+func (a finder) GetAttribute(name string) *configpb.AttributeManifest_AttributeInfo {
+	return a.attributes[name]
+}
+
+func (a finder) String() string {
+	b := pool.GetBuffer()
+
+	// Sort by attribute names for stable ordering.
+	i := 0
+	names := make([]string, len(a.attributes))
+	for name := range a.attributes {
+		names[i] = name
+		i++
+	}
+	sort.Strings(names)
+
+	fmt.Fprintln(b, "Attributes:")
+	for _, n := range names {
+		fmt.Fprintf(b, "  %s: %s", n, a.attributes[n].ValueType.String())
+		fmt.Fprintln(b)
+	}
+
+	s := b.String()
+	pool.PutBuffer(b)
+	return s
+}
