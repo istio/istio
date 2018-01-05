6d77d1a1a29c849c7009fd8cb599bef7f68e9d94
David Gageot <david@gageot.net>
2018-01-05 10:13:29 +0100

    Fix #2389 add missing jinja files to install folder
    Signed-off-by: David Gageot <david@gageot.net>

--- release/create_release_archives.sh
+++ release/create_release_archives.sh
@@ -106,7 +106,13 @@ function create_windows_archive() {
 
 pushd "${OUTPUT_PATH}"
 ${CP} istio.VERSION LICENSE README.md "${COMMON_FILES_DIR}"/
-find samples install -type f \( -name "*.yaml" -o -name "cleanup*" -o -name "*.md" -o -name "kubeconfig" \) \
+find samples install -type f \( \
+  -name "*.yaml" \
+  -o -name "cleanup*" \
+  -o -name "*.md" \
+  -o -name "kubeconfig" \
+  -o -name "*.jinja*" \
+  \) \
   -exec ${CP} --parents {} "${COMMON_FILES_DIR}" \;
 find install/tools -type f -exec ${CP} --parents {} "${COMMON_FILES_DIR}" \;
 popd
