446a1486f3286a1b9801cd864ec78636737bfc61
Laurent Demailly <ldemailly@gmail.com>
2018-01-06 14:56:28 -0800

    Restore curl -L https://git.io/getIstio | sh - (#2462)
    Deleted erroneously in #2442

---
+++ downloadIstio.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+#
+# Early version of a downloader/installer for Istio
+#
+# This file will be fetched as: curl -L https://git.io/getIstio | sh -
+# so it should be pure bourne shell, not bash (and not reference other scripts)
+#
+# The script fetches the latest STABLE Istio release and untars it.
+#
+# release/downloadIstioCandidate.sh lets users override the ISTIO_VERSION
+# and is updated more often.
+
+# DO NOT UPDATE THIS VERSION OR SCRIPT LIGHTLY - THIS IS THE "STABLE" VERSION
+ISTIO_VERSION="0.2.12"
+
+NAME="istio-$ISTIO_VERSION"
+OS="$(uname)"
+if [ "x${OS}" = "xDarwin" ] ; then
+  OSEXT="osx"
+else
+  # TODO we should check more/complain if not likely to work, etc...
+  OSEXT="linux"
+fi
+URL="https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio-${ISTIO_VERSION}-${OSEXT}.tar.gz"
+echo "Downloading $NAME from $URL ..."
+curl -L "$URL" | tar xz
+# TODO: change this so the version is in the tgz/directory name (users trying multiple versions)
+echo "Downloaded into $NAME:"
+ls $NAME
+BINDIR="$(cd $NAME/bin; pwd)"
+echo "Add $BINDIR to your path; e.g copy paste in your shell and/or ~/.profile:"
+echo "export PATH=\"\$PATH:$BINDIR\""
