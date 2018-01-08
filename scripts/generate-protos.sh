0a387cd5a52df56f5272e6532eb9ef628b68ca09
Shriram Rajagopalan <rshriram@users.noreply.github.com>
2018-01-08 18:03:31 -0500

    Circleci tweaks and remove unnecessary bazel build in presubmit (#287)
    * circleci config
    
    * nits
    
    * updates
    
    * no bazel build
    
    * test
    
    * undo test

--- scripts/generate-protos.sh
+++ scripts/generate-protos.sh
@@ -4,23 +4,3 @@ set -o errexit
 set -o nounset
 
 make clean generate
-
-bazel build //...
-
-genfiles=$(bazel info bazel-genfiles)
-files=$(find -L ${genfiles} -name "*.pb.go")
-
-for src in ${files}; do
-    dst=${src##${genfiles}/}
-    if [[ "${dst}" == "mixer*" ]]; then
-        continue
-    fi
-    echo "copying $src to $dst ..."
-    if [ -d "$(dirname ${dst})" ]; then
-        install -m 0640 ${src} ${dst}
-    fi
-done
-
-if [ -e mixer/v1/config/cfg.pb.go ]; then
-    rm mixer/v1/config/cfg.pb.go
-fi 
