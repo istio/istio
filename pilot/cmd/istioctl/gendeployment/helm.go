db19305ba670ebef797a6d4d82b4653a6fd3871c
Yusuo <yusuo@google.com>
2018-01-08 18:27:50 -0800

    Fix a typo in values.yaml template. (#2481)
    Automatic merge from submit-queue.
    
    Fix a typo in values.yaml template.
    
    Fix a typo: The yaml file generated by gen-deploy cannot be parsed by Helm.

--- pilot/cmd/istioctl/gendeployment/helm.go
+++ pilot/cmd/istioctl/gendeployment/helm.go
@@ -20,7 +20,7 @@ import (
 )
 
 var valuesTemplate = template.Must(template.New("helm").Parse(
-	`Istio:
+	`istio:
    deploy_base_config: true
    initializer_enabled: {{ .Initializer }}
    mixer_enabled: {{ .Mixer }}
