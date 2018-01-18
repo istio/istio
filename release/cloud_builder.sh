3abd0d48ac2e1b2aaf31eddb1d13360b6f2fb39c
Matt Delco <delco@google.com>
2018-01-17 22:16:00 -0800

    always set version

--- release/cloud_builder.sh
+++ release/cloud_builder.sh
@@ -87,11 +87,11 @@ if [ "${BUILD_DEBIAN}" == "true" ]; then
 fi
 
 mkdir -p "${OUTPUT_PATH}/istioctl"
-ISTIO_DOCKER_HUB=${REL_DOCKER_HUB} make istioctl-all
+VERBOSE=1 ISTIO_DOCKER_HUB=${REL_DOCKER_HUB} VERSION=$ISTIO_VERSION TAG=$ISTIO_VERSION make istioctl-all
 cp ${GOPATH}/out/istioctl-* ${OUTPUT_PATH}/istioctl
 if [[ -n "${TEST_DOCKER_HUB}" ]]; then
   mkdir -p "${OUTPUT_PATH}/istioctl-stage"
-  ISTIO_DOCKER_HUB=${TEST_DOCKER_HUB} make istioctl-all
+  VERBOSE=1 ISTIO_DOCKER_HUB=${TEST_DOCKER_HUB} VERSION=$ISTIO_VERSION TAG=$ISTIO_VERSION make istioctl-all
   cp ${GOPATH}/out/istioctl-* ${OUTPUT_PATH}/istioctl-stage
 fi
 
