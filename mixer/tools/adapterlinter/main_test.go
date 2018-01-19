ff11ea759d106e3a69f7bcce0102274cd186c10c
guptasu <guptasu@google.com>
2018-01-18 16:37:40 -0800

    fix test flackiness

--- mixer/tools/adapterlinter/main_test.go
+++ mixer/tools/adapterlinter/main_test.go
@@ -16,25 +16,27 @@ package main
 
 import (
 	"reflect"
+	"sort"
 	"testing"
 )
 
 func TestDoAllDirs(t *testing.T) {
 	got := doAllDirs([]string{"testdata/bad"})
 
+	sort.Strings(got)
 	want := []string{
-		"testdata/bad/gorutn_logimprt.go:5:2:\"log\" import is not recommended; Adapters must instead use " +
-			"env.Logger for logging during execution. This logger understands which adapter is running and routes " +
-			"the data to the place where the operator wants to see it.",
 		"testdata/bad/gorutn_logimprt.go:14:2:Adapters must use env.ScheduleWork or env.ScheduleDaemon " +
 			"in order to dispatch goroutines. This ensures all adapter goroutines are prevented from crashing Mixer " +
 			"as a whole by catching any panics they produce.",
-		"testdata/bad/gorutn_logimprt2.go:6:2:\"github.com/golang/glog\" import is not recommended; Adapters must instead use " +
+		"testdata/bad/gorutn_logimprt.go:5:2:\"log\" import is not recommended; Adapters must instead use " +
 			"env.Logger for logging during execution. This logger understands which adapter is running and routes " +
 			"the data to the place where the operator wants to see it.",
 		"testdata/bad/gorutn_logimprt2.go:15:2:Adapters must use env.ScheduleWork or env.ScheduleDaemon " +
 			"in order to dispatch goroutines. This ensures all adapter goroutines are prevented from crashing Mixer " +
 			"as a whole by catching any panics they produce.",
+		"testdata/bad/gorutn_logimprt2.go:6:2:\"github.com/golang/glog\" import is not recommended; Adapters must instead use " +
+			"env.Logger for logging during execution. This logger understands which adapter is running and routes " +
+			"the data to the place where the operator wants to see it.",
 	}
 	if !reflect.DeepEqual(want, got) {
 		t.Errorf("errors dont match\nwant:%v\ngot :%v", want, got)
