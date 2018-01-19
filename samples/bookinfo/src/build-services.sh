b4b6e6ef5570635005021386b99e7ca855b5e14b
Vadim Eisenberg <vadime@il.ibm.com>
2018-01-20 00:21:07 +0200

    Add a script to build, push bookinfo images and update yamls (#2678)
    * set images version passed as a parameter
    
    to prevent chicken-and-egg problem: the bookinfo sample yaml files should be updated with the
    latest istio version that should be updated after the yaml files are updated.
    
    * added a script to update the version of sample images
    
    * add a script to build, push and update images in yamls
    
    * replaced hard-coded 0.3.0 with $VERSION
    
    * use double quotes instead of single quotes with sed
    
    to apply $VERSION variable
    
    * remove checking ISTIO_VERSION
    
    * verify the number of scripts parameters
    
    * move update_images_in_yamls.sh into build_push_update_images.sh

--- samples/bookinfo/src/build-services.sh
+++ samples/bookinfo/src/build-services.sh
@@ -16,18 +16,24 @@
 
 set -o errexit
 
+if [ "$#" -ne 1 ]; then
+    echo Missing version parameter
+    echo Usage: build-services.sh \<version\>
+    exit 1
+fi
+
+VERSION=$1
 SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-ISTIO_VERSION=$(cat $SCRIPTDIR/../../../istio.RELEASE)
 
 pushd $SCRIPTDIR/productpage
-  docker build -t istio/examples-bookinfo-productpage-v1:${ISTIO_VERSION} .
+  docker build -t istio/examples-bookinfo-productpage-v1:${VERSION} .
 popd
 
 pushd $SCRIPTDIR/details
   #plain build -- no calling external book service to fetch topics
-  docker build -t istio/examples-bookinfo-details-v1:${ISTIO_VERSION} --build-arg service_version=v1 .
+  docker build -t istio/examples-bookinfo-details-v1:${VERSION} --build-arg service_version=v1 .
   #with calling external book service to fetch topic for the book
-  docker build -t istio/examples-bookinfo-details-v2:${ISTIO_VERSION} --build-arg service_version=v2 \
+  docker build -t istio/examples-bookinfo-details-v2:${VERSION} --build-arg service_version=v2 \
 	 --build-arg enable_external_book_service=true .
 popd
 
@@ -36,25 +42,25 @@ pushd $SCRIPTDIR/reviews
   docker run --rm -v `pwd`:/usr/bin/app:rw niaquinto/gradle clean build
   pushd reviews-wlpcfg
     #plain build -- no ratings
-    docker build -t istio/examples-bookinfo-reviews-v1:${ISTIO_VERSION} --build-arg service_version=v1 .
+    docker build -t istio/examples-bookinfo-reviews-v1:${VERSION} --build-arg service_version=v1 .
     #with ratings black stars
-    docker build -t istio/examples-bookinfo-reviews-v2:${ISTIO_VERSION} --build-arg service_version=v2 \
+    docker build -t istio/examples-bookinfo-reviews-v2:${VERSION} --build-arg service_version=v2 \
 	   --build-arg enable_ratings=true .
     #with ratings red stars
-    docker build -t istio/examples-bookinfo-reviews-v3:${ISTIO_VERSION} --build-arg service_version=v3 \
+    docker build -t istio/examples-bookinfo-reviews-v3:${VERSION} --build-arg service_version=v3 \
 	   --build-arg enable_ratings=true --build-arg star_color=red .
   popd
 popd
 
 pushd $SCRIPTDIR/ratings
-  docker build -t istio/examples-bookinfo-ratings-v1:${ISTIO_VERSION} --build-arg service_version=v1 .
-  docker build -t istio/examples-bookinfo-ratings-v2:${ISTIO_VERSION} --build-arg service_version=v2 .
+  docker build -t istio/examples-bookinfo-ratings-v1:${VERSION} --build-arg service_version=v1 .
+  docker build -t istio/examples-bookinfo-ratings-v2:${VERSION} --build-arg service_version=v2 .
 popd
 
 pushd $SCRIPTDIR/mysql
-  docker build -t istio/examples-bookinfo-mysqldb:${ISTIO_VERSION} .
+  docker build -t istio/examples-bookinfo-mysqldb:${VERSION} .
 popd
 
 pushd $SCRIPTDIR/mongodb
-  docker build -t istio/examples-bookinfo-mongodb:${ISTIO_VERSION} .
+  docker build -t istio/examples-bookinfo-mongodb:${VERSION} .
 popd
