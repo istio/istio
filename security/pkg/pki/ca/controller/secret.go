2cb11ff94cd9dbde0117f7c53d993d010b20dad8
Oliver Liu <yonggangl@google.com>
2018-01-22 19:34:24 -0800

    Allow auth node agent to specify TTL (#2712)
    * Allow node agent to specify TTL for certificates.

--- security/pkg/pki/ca/controller/secret.go
+++ security/pkg/pki/ca/controller/secret.go
@@ -59,8 +59,9 @@ const (
 
 // SecretController manages the service accounts' secrets that contains Istio keys and certificates.
 type SecretController struct {
-	ca   ca.CertificateAuthority
-	core corev1.CoreV1Interface
+	ca      ca.CertificateAuthority
+	certTTL time.Duration
+	core    corev1.CoreV1Interface
 
 	// Controller and store for service account objects.
 	saController cache.Controller
@@ -72,12 +73,13 @@ type SecretController struct {
 }
 
 // NewSecretController returns a pointer to a newly constructed SecretController instance.
-func NewSecretController(ca ca.CertificateAuthority, core corev1.CoreV1Interface,
+func NewSecretController(ca ca.CertificateAuthority, certTTL time.Duration, core corev1.CoreV1Interface,
 	namespace string) *SecretController {
 
 	c := &SecretController{
-		ca:   ca,
-		core: core,
+		ca:      ca,
+		certTTL: certTTL,
+		core:    core,
 	}
 
 	saLW := &cache.ListWatch{
@@ -237,7 +239,7 @@ func (sc *SecretController) generateKeyAndCert(saName string, saNamespace string
 		return nil, nil, err
 	}
 
-	certPEM, err := sc.ca.Sign(csrPEM)
+	certPEM, err := sc.ca.Sign(csrPEM, sc.certTTL)
 	if err != nil {
 		return nil, nil, err
 	}
