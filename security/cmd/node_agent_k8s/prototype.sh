c93908d66b5c818af390d3ddae22bae1b9b34dcb
Tao Li <lita@google.com>
2018-01-23 16:12:07 -0800

    Add the first version of flexvolume driver work

---
+++ security/cmd/node_agent_k8s/prototype.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -o errexit
+set -o pipefail
+
+VENDOR=k8s
+DRIVER=dummy
+
+# Assuming the single driver file is located at /$DRIVER inside the DaemonSet image.
+
+driver_dir=$VENDOR${VENDOR:+"~"}${DRIVER}
+if [ ! -d "/flexmnt/$driver_dir" ]; then
+  mkdir "/flexmnt/$driver_dir"
+fi
+
+cp "/$DRIVER" "/flexmnt/$driver_dir/.$DRIVER"
+mv -f "/flexmnt/$driver_dir/.$DRIVER" "/flexmnt/$driver_dir/$DRIVER"
+
+while : ; do
+  sleep 3600
+done
