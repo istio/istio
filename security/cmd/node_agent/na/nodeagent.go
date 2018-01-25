cc231d245c51004673daca8a0190ceb966691be2
Tao Li <lita@google.com>
2018-01-25 11:32:45 -0800

    Rename the ca_service protobuf to it scalable

--- security/cmd/node_agent/na/nodeagent.go
+++ security/cmd/node_agent/na/nodeagent.go
@@ -116,7 +116,7 @@ func (na *nodeAgentInternal) Start() error {
 	}
 }
 
-func (na *nodeAgentInternal) createRequest() ([]byte, *pb.Request, error) {
+func (na *nodeAgentInternal) createRequest() ([]byte, *pb.CsrRequest, error) {
 	csr, privKey, err := ca.GenCSR(ca.CertOptions{
 		Host:       na.identity,
 		Org:        na.config.ServiceIdentityOrg,
@@ -131,7 +131,7 @@ func (na *nodeAgentInternal) createRequest() ([]byte, *pb.Request, error) {
 		return nil, nil, fmt.Errorf("request creation fails on getting agent credential (%v)", err)
 	}
 
-	return privKey, &pb.Request{
+	return privKey, &pb.CsrRequest{
 		CsrPem:              csr,
 		NodeAgentCredential: cred,
 		CredentialType:      na.pc.GetCredentialType(),
