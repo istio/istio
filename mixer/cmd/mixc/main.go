8993bb564f2de8da523986fa96a1058f4f194767
Shriram Rajagopalan <shriram@us.ibm.com>
2018-01-26 18:45:48 -0500

    undo

--- mixer/cmd/mixc/main.go
+++ mixer/cmd/mixc/main.go
@@ -1,4 +1,4 @@
-foobarbaz// Copyright 2016 Istio Authors
+// Copyright 2016 Istio Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
