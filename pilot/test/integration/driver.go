7f9d4c2503938c43a9f710723d4d842963817f5c
Vadim Eisenberg <vadime@il.ibm.com>
2018-01-27 08:12:08 +0200

    Add integration tests for kubernetes ExternalName services (#2871)
    * added deploying of Kubernetes ExternalName service for httpbin.org
    
    * added a new line to a yaml
    
    * added initial tests for Kubernetes ExternalName services
    
    * fixed passing Host header in kubernetes_external_name_services test
    
    * used wikipedia.org instead of httpbin.org
    
    wikipedia.org returns 200 when unknown hostname is sent to its IP
    in our case we send "externalwikipedia" hostname
    
    * added 2018 to the copyright of the changed go files
    
    * removed the path "/status/200" remained from the test for httpbin.org

--- pilot/test/integration/driver.go
+++ pilot/test/integration/driver.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Istio Authors
+// Copyright 2017,2018 Istio Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -218,6 +218,7 @@ func runTests(envs ...infra) {
 			&routingToEgress{infra: &istio},
 			&zipkin{infra: &istio},
 			&authExclusion{infra: &istio},
+			&kubernetesExternalNameServices{infra: &istio},
 		}
 
 		for _, test := range tests {
--- pilot/test/integration/infra.go
+++ pilot/test/integration/infra.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Istio Authors
+// Copyright 2017,2018 Istio Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -262,7 +262,9 @@ func (infra *infra) setup() error {
 			return err
 		}
 	}
-
+	if err := deploy("external-wikipedia.yaml.tmpl", infra.Namespace); err != nil {
+		return err
+	}
 	return nil
 }
 
