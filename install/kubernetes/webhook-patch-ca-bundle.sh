aefd9679baf2634ac62e76cf4c39d261ea51547a
Jason Young <jasonyoung@google.com>
2018-01-27 22:46:30 -0800

    switch back to base64 and use `tr` to remove newlines

--- install/kubernetes/webhook-patch-ca-bundle.sh
+++ install/kubernetes/webhook-patch-ca-bundle.sh
@@ -6,7 +6,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-export CA_BUNDLE=$(kubectl get configmap -n kube-system extension-apiserver-authentication -o=jsonpath='{.data.client-ca-file}' | openssl base64 )
+export CA_BUNDLE=$(kubectl get configmap -n kube-system extension-apiserver-authentication -o=jsonpath='{.data.client-ca-file}' | base64 | tr -d '\n')
 
 if command -v envsubst >/dev/null 2>&1; then
     envsubst
