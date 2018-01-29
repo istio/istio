e3143e306857c88d891f6152d054d6b0ad4e7a1e
Oliver Liu <yonggangl@google.com>
2018-01-29 09:31:14 -0800

    Refactor PKI to sign CSR for CA certs. (#2919)

--- security/pkg/pki/ca/generate_cert_test.go
+++ security/pkg/pki/ca/generate_cert_test.go
@@ -25,60 +25,7 @@ import (
 
 var now = time.Now().Round(time.Second).UTC()
 
-func TestLoadSignerCredsFromFiles(t *testing.T) {
-	testCases := map[string]struct {
-		certFile    string
-		keyFile     string
-		expectedErr string
-	}{
-		"Good certificates": {
-			certFile:    "testdata/cert.pem",
-			keyFile:     "testdata/key.pem",
-			expectedErr: "",
-		},
-		"Missing cert files": {
-			certFile:    "testdata/cert-not-exist.pem",
-			keyFile:     "testdata/key.pem",
-			expectedErr: "certificate file reading failure (open testdata/cert-not-exist.pem: no such file or directory)",
-		},
-		"Missing key files": {
-			certFile:    "testdata/cert.pem",
-			keyFile:     "testdata/key-not-exist.pem",
-			expectedErr: "private key file reading failure (open testdata/key-not-exist.pem: no such file or directory)",
-		},
-		"Bad cert files": {
-			certFile:    "testdata/cert-bad.pem",
-			keyFile:     "testdata/key.pem",
-			expectedErr: "invalid PEM encoded certificate",
-		},
-		"Bad key files": {
-			certFile:    "testdata/cert.pem",
-			keyFile:     "testdata/key-bad.pem",
-			expectedErr: "invalid PEM-encoded key",
-		},
-	}
-
-	for id, tc := range testCases {
-		cert, key, err := LoadSignerCredsFromFiles(tc.certFile, tc.keyFile)
-		if len(tc.expectedErr) > 0 {
-			if err == nil {
-				t.Errorf("%s: Succeeded. Error expected: %v", id, err)
-			} else if err.Error() != tc.expectedErr {
-				t.Errorf("%s: incorrect error message: %s VS %s",
-					id, err.Error(), tc.expectedErr)
-			}
-			continue
-		} else if err != nil {
-			t.Fatalf("%s: Unexpected Error: %v", id, err)
-		}
-
-		if cert == nil || key == nil {
-			t.Errorf("%v: Faild to load signer credeitials from files: %v, %v", id, tc.certFile, tc.keyFile)
-		}
-	}
-}
-
-func TestGenCert(t *testing.T) {
+func TestGenCertKeyFromOptions(t *testing.T) {
 	// set "notBefore" to be one hour ago, this ensures the issued certifiate to
 	// be valid as of now.
 	caCertNotBefore := now.Add(-time.Hour)
@@ -99,7 +46,7 @@ func TestGenCert(t *testing.T) {
 		RSAKeySize:   512,
 	}
 
-	caCertPem, caPrivPem, err := GenCert(caCertOptions)
+	caCertPem, caPrivPem, err := GenCertKeyFromOptions(caCertOptions)
 	if err != nil {
 		t.Error(err)
 	}
@@ -129,12 +76,13 @@ func TestGenCert(t *testing.T) {
 	notBefore := now.Add(-5 * time.Minute)
 	ttl := time.Hour
 	cases := []struct {
+		name         string
 		certOptions  CertOptions
 		verifyFields *tu.VerifyFields
 	}{
 		// These certs are signed by the CA cert
 		{
-			// server cert with DNS as SAN
+			name: "Server cert with DNS SAN",
 			certOptions: CertOptions{
 				Host:         "test_server.com",
 				NotBefore:    notBefore,
@@ -158,7 +106,7 @@ func TestGenCert(t *testing.T) {
 			},
 		},
 		{
-			// client cert with DNS as SAN
+			name: "Server and client cert with DNS SAN",
 			certOptions: CertOptions{
 				Host:         "test_client.com",
 				NotBefore:    notBefore,
@@ -182,7 +130,7 @@ func TestGenCert(t *testing.T) {
 			},
 		},
 		{
-			// server cert with IP as SAN
+			name: "Server cert with IP SAN",
 			certOptions: CertOptions{
 				Host:         "1.2.3.4",
 				NotBefore:    notBefore,
@@ -206,7 +154,7 @@ func TestGenCert(t *testing.T) {
 			},
 		},
 		{
-			// client cert with service account as SAN
+			name: "Client cert with URI SAN",
 			certOptions: CertOptions{
 				Host:         "spiffe://domain/ns/bar/sa/foo",
 				NotBefore:    notBefore,
@@ -229,64 +177,71 @@ func TestGenCert(t *testing.T) {
 				Org:         "MyOrg",
 			},
 		},
-		{
-			// server cert with service account as SAN
-			certOptions: CertOptions{
-				Host:         "spiffe://domain/ns/bar/sa/foo",
-				NotBefore:    notBefore,
-				TTL:          ttl,
-				SignerCert:   caCert,
-				SignerPriv:   caPriv,
-				Org:          "",
-				IsCA:         false,
-				IsSelfSigned: false,
-				IsClient:     false,
-				IsServer:     true,
-				RSAKeySize:   512,
-			},
-			verifyFields: &tu.VerifyFields{
-				ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
-				IsCA:        false,
-				KeyUsage:    x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
-				NotBefore:   notBefore,
-				TTL:         ttl,
-				Org:         "MyOrg",
-			},
-		},
-		{
-			// a cert that can only be used as client-side cert
-			certOptions: CertOptions{
-				Host:         "spiffe://domain/ns/bar/sa/foo",
-				NotBefore:    notBefore,
-				TTL:          ttl,
-				SignerCert:   caCert,
-				SignerPriv:   caPriv,
-				Org:          "",
-				IsCA:         false,
-				IsSelfSigned: false,
-				IsClient:     true,
-				IsServer:     false,
-				RSAKeySize:   512,
-			},
-			verifyFields: &tu.VerifyFields{
-				ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
-				IsCA:        false,
-				KeyUsage:    x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
-				NotBefore:   notBefore,
-				TTL:         ttl,
-				Org:         "MyOrg",
-			},
-		},
 	}
 
 	for _, c := range cases {
 		certOptions := c.certOptions
-		certPem, privPem, err := GenCert(certOptions)
+		certPem, privPem, err := GenCertKeyFromOptions(certOptions)
 		if err != nil {
-			t.Error(err)
+			t.Errorf("[%s] cert/key generation error: %v", c.name, err)
 		}
-		if e := tu.VerifyCertificate(privPem, certPem, caCertPem, certOptions.Host, c.verifyFields); e != nil {
-			t.Error(e)
+		if err := tu.VerifyCertificate(privPem, certPem, caCertPem, certOptions.Host, c.verifyFields); err != nil {
+			t.Errorf("[%s] cert verification error: %v", c.name, err)
+		}
+	}
+}
+
+// TODO(myidpt): Add test cases for GenCertFromCSR.
+
+func TestLoadSignerCredsFromFiles(t *testing.T) {
+	testCases := map[string]struct {
+		certFile    string
+		keyFile     string
+		expectedErr string
+	}{
+		"Good certificates": {
+			certFile:    "testdata/cert.pem",
+			keyFile:     "testdata/key.pem",
+			expectedErr: "",
+		},
+		"Missing cert files": {
+			certFile:    "testdata/cert-not-exist.pem",
+			keyFile:     "testdata/key.pem",
+			expectedErr: "certificate file reading failure (open testdata/cert-not-exist.pem: no such file or directory)",
+		},
+		"Missing key files": {
+			certFile:    "testdata/cert.pem",
+			keyFile:     "testdata/key-not-exist.pem",
+			expectedErr: "private key file reading failure (open testdata/key-not-exist.pem: no such file or directory)",
+		},
+		"Bad cert files": {
+			certFile:    "testdata/cert-bad.pem",
+			keyFile:     "testdata/key.pem",
+			expectedErr: "pem encoded cert parsing failure (invalid PEM encoded certificate)",
+		},
+		"Bad key files": {
+			certFile:    "testdata/cert.pem",
+			keyFile:     "testdata/key-bad.pem",
+			expectedErr: "pem encoded key parsing failure (invalid PEM-encoded key)",
+		},
+	}
+
+	for id, tc := range testCases {
+		cert, key, err := LoadSignerCredsFromFiles(tc.certFile, tc.keyFile)
+		if len(tc.expectedErr) > 0 {
+			if err == nil {
+				t.Errorf("[%s] Succeeded. Error expected: %v", id, err)
+			} else if err.Error() != tc.expectedErr {
+				t.Errorf("[%s] incorrect error message: %s VS (expected) %s",
+					id, err.Error(), tc.expectedErr)
+			}
+			continue
+		} else if err != nil {
+			t.Fatalf("[%s] Unexpected Error: %v", id, err)
+		}
+
+		if cert == nil || key == nil {
+			t.Errorf("[%s] Faild to load signer credeitials from files: %v, %v", id, tc.certFile, tc.keyFile)
 		}
 	}
 }
