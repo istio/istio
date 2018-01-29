7991274e21992516ba0354129987bf054c08fe89
Vadim Eisenberg <vadime@il.ibm.com>
2018-01-29 18:09:11 +0200

    added a test for Kubernetes ExternalName service for httpbin.org (#2913)
    * added a test for Kubernetes ExternalName service for httpbin.org
    
    the test requires a HTTP rewrite route rule to rewrite the authority to be
    httpbin.org
    
    * added import of log
    
    * set hostname header when source is without Istio proxy
    
    * run go fmt
    
    * run goimports

--- pilot/test/integration/kubernetes_external_name_services.go
+++ pilot/test/integration/kubernetes_external_name_services.go
@@ -16,6 +16,8 @@ package main
 
 import (
 	"fmt"
+
+	"istio.io/istio/pkg/log"
 )
 
 type kubernetesExternalNameServices struct {
@@ -27,25 +29,43 @@ func (t *kubernetesExternalNameServices) String() string {
 }
 
 func (t *kubernetesExternalNameServices) setup() error {
+	if err := t.applyConfig("rule-rewrite-authority-externalbin.yaml.tmpl", nil); err != nil {
+		return err
+	}
 	return nil
 }
 
 func (t *kubernetesExternalNameServices) teardown() {
+	log.Info("Cleaning up route rules...")
+	if err := t.deleteAllConfigs(); err != nil {
+		log.Warna(err)
+	}
 }
 
 func (t *kubernetesExternalNameServices) run() error {
-	srcPods := []string{"a", "b", "t"}
-	dstServices := []string{"externalwikipedia"}
+
+	// map of source pods to test, to boolean that is true if the pod has Istio proxy
+	srcPods := map[string]bool{"a": true, "b": true, "t": false}
+
+	// map of destination external name services to their external hosts
+	dstServices := map[string]string{
+		"externalwikipedia": "wikipedia.org",
+		"externalbin":       "httpbin.org",
+	}
 
 	funcs := make(map[string]func() status)
-	for _, src := range srcPods {
-		for _, dst := range dstServices {
+	for src, withIstioProxy := range srcPods {
+		for dst, externalHost := range dstServices {
 			for _, domain := range []string{"", "." + t.Namespace} {
 				name := fmt.Sprintf("HTTP connection from %s to %s%s", src, dst, domain)
 				funcs[name] = (func(src, dst, domain string) func() status {
 					url := fmt.Sprintf("http://%s%s", dst, domain)
+					extra := ""
+					if !withIstioProxy {
+						extra = "-key Host -val " + externalHost
+					}
 					return func() status {
-						resp := t.clientRequest(src, url, 1, "")
+						resp := t.clientRequest(src, url, 1, extra)
 						if len(resp.code) > 0 && resp.code[0] == httpOk {
 							return nil
 						}
