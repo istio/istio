e841588047a32ee60a84139eb94ee5a3044e083d
mattdelco <32583120+mattdelco@users.noreply.github.com>
2018-01-31 16:10:23 -0800

    Handle non-Linux systems, build docker outside of source tree, replace & delete per-component Makefiles and push scripts (#2831)
    * initial change
    
    * add certs dir
    
    * remove test
    
    * multios
    
    * subdirs
    
    * switch to out
    
    * fix path
    
    * use rel path
    
    * factor
    
    * del parens
    
    * bin->out
    
    * rename dir
    
    * add my other changes
    
    * fix path
    
    * use make more
    
    * cleanup
    
    * faster re-init
    
    * do more
    
    * del old
    
    * add grafana
    
    * cleanup
    
    * merge fix
    
    * merge fix
    
    * del tab
    
    * merge fix
    
    * set ISTIO_OUT
    
    * workaround init.sh callers
    
    * test workarounds
    
    * stop ignoring
    
    * use gcloud for push
    
    * move block up
    
    * docker to separate mk
    
    * misc fixes
    
    * lookup dir
    
    * merge update
    
    * bring back generate_yaml
    
    * more explanation
    
    * del config
    
    * fix prebuilt
    
    * fix pilot test launch
    
    * tag but not push
    
    * add rshriram PR
    
    * del docker.prebuilt
    
    * target name changes
    
    * go-test is now test
    
    * mixer to true
    
    * add mixer-dashboard.json to makefile
    
    * fix minikube.conf path
    
    * default to rel, place init sentinel file in out so envoy downloaded when build type changes
    
    * del unused file
    
    * mixer to false
    
    * mixer back to true
    
    * TESTOPS is TESTOPTS and del unused --istioctl

--- tools/deb/istio.mk
+++ tools/deb/istio.mk
@@ -15,27 +15,29 @@ deb/build-in-docker:
 # This target uses a locally installed 'fpm' - use 'docker.sidecar.deb' to use
 # the builder image.
 # TODO: consistent layout, possibly /opt/istio-VER/...
-sidecar.deb: ${OUT}/istio-sidecar.deb
+sidecar.deb: ${ISTIO_OUT}/istio-sidecar.deb
 
-deb: ${OUT}/istio-sidecar.deb
-
-ISTIO_DEB_SRC:=tools/deb/istio-start.sh \
-			  tools/deb/istio-iptables.sh \
-			  tools/deb/istio.service \
-			  tools/deb/istio-auth-node-agent.service \
-			  tools/deb/sidecar.env \
-			  tools/deb/envoy.json
-
-ISTIO_DEB_DEPS=${ISTIO_BIN}/envoy \
-			   ${ISTIO_BIN}/pilot-agent \
-			   ${ISTIO_BIN}/pilot-discovery \
-			   ${ISTIO_BIN}/node_agent \
-			   ${ISTIO_BIN}/istioctl \
-			   ${ISTIO_BIN}/mixs \
-			   ${ISTIO_BIN}/istio_ca
+deb: ${ISTIO_OUT}/istio-sidecar.deb
 
 # Base directory for istio binaries. Likely to change !
 ISTIO_DEB_BIN=/usr/local/bin
+
+ISTIO_DEB_DEPS:=envoy pilot-agent pilot-discovery node_agent istioctl mixs istio_ca
+SIDECAR_FILES:=
+# subst is used to turn an absolute path into the relative path that fpm seems to expect
+$(foreach DEP,$(ISTIO_DEB_DEPS),\
+        $(eval ${ISTIO_OUT}/istio-sidecar.deb: $(ISTIO_OUT)/$(DEP)) \
+        $(eval SIDECAR_FILES+=$(subst $(GO_TOP)/,,$(ISTIO_OUT))/$(DEP)=$(ISTIO_DEB_BIN)/$(DEP)) )
+
+ISTIO_DEB_DEST:=${ISTIO_DEB_BIN}/istio-start.sh \
+		${ISTIO_DEB_BIN}/istio-iptables.sh \
+		/lib/systemd/system/istio.service \
+		/lib/systemd/system/istio-auth-node-agent.service \
+		/var/lib/istio/envoy/sidecar.env \
+		/var/lib/istio/envoy/envoy.json
+$(foreach DEST,$(ISTIO_DEB_DEST),\
+        $(eval ${ISTIO_OUT}/istio-sidecar.deb:   tools/deb/$(notdir $(DEST))) \
+        $(eval SIDECAR_FILES+=src/istio.io/istio/tools/deb/$(notdir $(DEST))=$(DEST)))
 
 # original name used in 0.2 - will be updated to 'istio.deb' since it now includes all istio binaries.
 ISTIO_DEB_NAME ?= istio-sidecar
@@ -46,10 +48,9 @@ ISTIO_DEB_NAME ?= istio-sidecar
 # a /etc/systemd/system/multi-user.target.wants/istio.service and auto-start. Currently not used
 # since we need configuration.
 # --iteration 1 adds a "-1" suffix to the version that didn't exist before
-${OUT}/istio-sidecar.deb: ${ISTIO_DEB_DEPS} ${ISTIO_DEB_SRC}
-	mkdir -p ${OUT}
-	rm -f ${OUT}/istio-sidecar.deb
-	fpm -s dir -t deb -n ${ISTIO_DEB_NAME} -p ${OUT}/istio-sidecar.deb --version ${VERSION} -C ${GO_TOP} -f \
+${ISTIO_OUT}/istio-sidecar.deb: | ${ISTIO_OUT}
+	rm -f ${ISTIO_OUT}/istio-sidecar.deb
+	fpm -s dir -t deb -n ${ISTIO_DEB_NAME} -p ${ISTIO_OUT}/istio-sidecar.deb --version ${VERSION} -C ${GO_TOP} -f \
 		--url http://istio.io  \
 		--license Apache \
 		--vendor istio.io \
@@ -58,29 +59,16 @@ ${OUT}/istio-sidecar.deb: ${ISTIO_DEB_DEPS} ${ISTIO_DEB_SRC}
 		--config-files /var/lib/istio/envoy/sidecar.env \
 		--config-files /var/lib/istio/envoy/envoy.json \
 		--description "Istio" \
-		src/istio.io/istio/tools/deb/istio-start.sh=${ISTIO_DEB_BIN}/istio-start.sh \
-		src/istio.io/istio/tools/deb/istio-iptables.sh=${ISTIO_DEB_BIN}/istio-iptables.sh \
-		src/istio.io/istio/tools/deb/istio.service=/lib/systemd/system/istio.service \
-		src/istio.io/istio/tools/deb/istio-auth-node-agent.service=/lib/systemd/system/istio-auth-node-agent.service \
-		bin/envoy=${ISTIO_DEB_BIN}/envoy \
-		bin/pilot-agent=${ISTIO_DEB_BIN}/pilot-agent \
-		bin/node_agent=${ISTIO_DEB_BIN}/node_agent \
-		bin/istioctl=${ISTIO_DEB_BIN}/istioctl \
-		bin/mixs=${ISTIO_DEB_BIN}/mixs \
-		bin/istio_ca=${ISTIO_DEB_BIN}/istio_ca \
-		bin/pilot-discovery=${ISTIO_DEB_BIN}/pilot-discovery \
-		src/istio.io/istio/tools/deb/sidecar.env=/var/lib/istio/envoy/sidecar.env \
-		src/istio.io/istio/tools/deb/envoy.json=/var/lib/istio/envoy/envoy.json
+		$(SIDECAR_FILES)
 
 .PHONY: deb/docker
 
 # Install the deb in a docker image, for testing.
 deb/docker:
-	mkdir -p ${OUT}/deb
-	cp tools/deb/Dockerfile ${OUT}/deb
-	cp ${OUT}/istio-sidecar.deb ${OUT}/deb/istio.deb
-	docker build -t istio_deb -f ${OUT}/deb/Dockerfile ${OUT}/deb/
-
+	mkdir -p ${ISTIO_OUT}/deb
+	cp tools/deb/Dockerfile ${ISTIO_OUT}/deb
+	cp ${ISTIO_OUT}/istio-sidecar.deb ${ISTIO_OUT}/deb/istio.deb
+	docker build -t istio_deb -f ${ISTIO_OUT}/deb/Dockerfile ${ISTIO_OUT}/deb/
 
 deb/test: deb-docker tools/deb/deb_test.sh
 	docker run --cap-add=NET_ADMIN --rm -v ${ISTIO_GO}/tools/deb/deb_test.sh:/tmp/deb_test.sh istio_deb /tmp/deb_test.sh
