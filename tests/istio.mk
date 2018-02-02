b296aeed330f4ca82ea93ecd2ca680a1ef8fb8e7
Costin Manolache <costin@gmail.com>
2018-02-01 16:54:58 -0800

    Update location of kubeconfig

--- tests/istio.mk
+++ tests/istio.mk
@@ -4,7 +4,7 @@ ifeq (${TEST_ENV},minikube)
 # In minikube env we don't need to push the images to dockerhub or gcr, it is all local,
 # but we need to use the minikube's docker env.
 # Note that tests simply use go/out, so going up 3 dirs from the os/arch/debug path
-export KUBECONFIG=${ISTIO_OUT}/../../../minikube.conf
+export KUBECONFIG=${GO_TOP}/out/minikube.conf
 export TEST_ENV=minikube
 MINIKUBE_FLAGS=-use_local_cluster -cluster_wide
 .PHONY: minikube
@@ -22,7 +22,7 @@ else ifeq (${TEST_ENV},minikube-none)
 # In minikube env we don't need to push the images to dockerhub or gcr, it is all local,
 # but we need to use the minikube's docker env.
 # Note that tests simply use go/out, so going up 3 dirs from the os/arch/debug path
-export KUBECONFIG=${ISTIO_OUT}/../../../minikube.conf
+export KUBECONFIG=${GO_TOP}/out/minikube.conf
 export TEST_ENV=minikube-none
 MINIKUBE_FLAGS=-use_local_cluster -cluster_wide
 .PHONY: minikube
