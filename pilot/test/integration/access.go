8af5587eed1b4db8f96d60b35070fd4abfa9c506
Andra Cismaru <andracis@google.com>
2018-02-02 17:09:06 -0800

    similar change

--- pilot/test/integration/access.go
+++ pilot/test/integration/access.go
@@ -85,15 +85,16 @@ func (a *accessLogs) check(infra *infra) error {
 				case "ingress":
 					ns = infra.IstioNamespace
 				}
+				util.CopyPodFiles(container, pod, ns, model.ConfigPathDir, infra.coreFilesDir+"/"+pod+"."+ns)
 				logs := util.FetchLogs(client, pod, ns, container)
 
 				if strings.Contains(logs, "segmentation fault") {
-					util.CopyPodFiles("istio-proxy", pod, ns, model.ConfigPathDir, infra.coreFilesDir+"/"+pod+"."+ns)
+					util.CopyPodFiles(container, pod, ns, model.ConfigPathDir, infra.coreFilesDir+"/"+pod+"."+ns)
 					return fmt.Errorf("segmentation fault %s log: %s", pod, logs)
 				}
 
 				if strings.Contains(logs, "assert failure") {
-					util.CopyPodFiles("istio-proxy", pod, ns, model.ConfigPathDir, infra.coreFilesDir)
+					util.CopyPodFiles(container, pod, ns, model.ConfigPathDir, infra.coreFilesDir+"/"+pod+"."+ns)
 					return fmt.Errorf("assert failure in %s log: %s", pod, logs)
 				}
 
