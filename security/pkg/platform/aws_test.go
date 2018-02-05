60ae0cee10059ba70340639c346fc369884baab5
Costin Manolache <costin@gmail.com>
2018-02-05 11:04:18 -0800

    Skip tests, issue filled

--- security/pkg/platform/aws_test.go
+++ security/pkg/platform/aws_test.go
@@ -69,6 +69,7 @@ func TestNewAwsClientImpl(t *testing.T) {
 }
 
 func TestAwsGetInstanceIdentityDocument(t *testing.T) {
+	t.Skip("https://github.com/istio/istio/issues/3177")
 	testCases := map[string]struct {
 		sigFile              string
 		requestPath          string
@@ -176,6 +177,7 @@ func TestAwsGetServiceIdentity(t *testing.T) {
 }
 
 func TestGetGetAgentCredential(t *testing.T) {
+	t.Skip("https://github.com/istio/istio/issues/3177")
 	testCases := map[string]struct {
 		sigFile            string
 		requestPath        string
