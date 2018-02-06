cc0e7ed70dd1cef254929e619769abb63995d5b8
Martin Taillefer <geeknoid@users.noreply.github.com>
2018-02-06 06:56:25 -0800

    Generate docs when compiling protos (#3136)

--- mixer/template/checknothing/template_handler.gen.go
+++ mixer/template/checknothing/template_handler.gen.go
@@ -22,6 +22,9 @@ import (
 	"istio.io/istio/mixer/pkg/adapter"
 )
 
+// The `checknothing` template represents an empty block of data, which can useful
+// in different testing scenarios.
+
 // Fully qualified name of the template
 const TemplateName = "checknothing"
 
@@ -32,7 +35,7 @@ const TemplateName = "checknothing"
 // scenarios.
 //
 // Example config:
-// ```
+// ```yaml
 // apiVersion: "config.istio.io/v1alpha2"
 // kind: checknothing
 // metadata:
