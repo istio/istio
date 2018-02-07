190f945ba2c5cc85e3c68468a3bcdc470f7941f5
Shriram Rajagopalan <shriram@us.ibm.com>
2018-02-07 17:29:45 -0500

    fix

--- pilot/pkg/model/context.go
+++ pilot/pkg/model/context.go
@@ -180,7 +180,7 @@ func DefaultMeshConfig() meshconfig.MeshConfig {
 	return meshconfig.MeshConfig{
 		MixerCheckServer:      "",
 		MixerReportServer:     "",
-		DisablePolicyChecks:   true,
+		DisablePolicyChecks:   false,
 		ProxyListenPort:       15001,
 		ConnectTimeout:        ptypes.DurationProto(1 * time.Second),
 		IngressClass:          "istio",
