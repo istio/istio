edff94b0fa08f0efcd1d7a58aa65b45a895c5e99
Charles Xu <chx@google.com>
2018-02-21 11:58:29 -0800

    Show pkg name and url when installing istio on VM (#3629)
    * Show pkg name and url when installing istio on VM
    
    * Fix echo

--- install/tools/setupIstioVM.sh
+++ install/tools/setupIstioVM.sh
@@ -58,7 +58,7 @@ function istioInstall() {
   echo "*** Fetching istio packages..."
   # Current URL for the debian files artifacts. Will be replaced by a proper apt repo.
   rm -f istio-sidecar.deb
-  echo curl -f -L ${PILOT_DEBIAN_URL}/istio-sidecar.deb > ${ISTIO_STAGING}/istio-sidecar.deb
+  echo "curl -f -L ${PILOT_DEBIAN_URL}/istio-sidecar.deb > ${ISTIO_STAGING}/istio-sidecar.deb"
   curl -f -L ${PILOT_DEBIAN_URL}/istio-sidecar.deb > ${ISTIO_STAGING}/istio-sidecar.deb
 
   # Install istio binaries
