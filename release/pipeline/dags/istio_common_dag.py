767cdc0618cf5343ef838ce7010768eb7c457b88
Andy Lai <hklai@google.com>
2018-02-22 12:17:11 -0800

    Fix typo

--- release/pipeline/dags/istio_common_dag.py
+++ release/pipeline/dags/istio_common_dag.py
@@ -1,4 +1,4 @@
-"""Airfow DAG and helpers used in one or more istio release pipline."""
+"""Airfow DAG and helpers used in one or more istio release pipeline."""
 """Copyright 2017 Istio Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
