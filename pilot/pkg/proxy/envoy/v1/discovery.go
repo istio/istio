0be7e8cbd1bf3549417769bd157f3919ff44a14a
Costin Manolache <costin@gmail.com>
2018-02-21 16:18:19 -0800

    Fmt

--- pilot/pkg/proxy/envoy/v1/discovery.go
+++ pilot/pkg/proxy/envoy/v1/discovery.go
@@ -514,10 +514,10 @@ func (ds *DiscoveryService) clearCache() {
 	clearCacheMutex.Lock()
 	defer clearCacheMutex.Unlock()
 
-	if time.Since(lastClearCache) < time.Duration(clearCacheTime) * time.Second {
+	if time.Since(lastClearCache) < time.Duration(clearCacheTime)*time.Second {
 		if !clearCacheTimerSet {
 			clearCacheTimerSet = true
-			time.AfterFunc(time.Duration(clearCacheTime) * time.Second, func() {
+			time.AfterFunc(time.Duration(clearCacheTime)*time.Second, func() {
 				clearCacheTimerSet = false
 				log.Infof("Squashed update")
 				ds.clearCache() // it's after time - so will clear the cache
