8314602aed7d9850fe8ef76a447578ce106cfc46
Sebastien Vas <sebastienvas@google.com>
2018-02-23 10:20:51 -0800

    Getting code coverage to work

--- bin/codecov.sh
+++ bin/codecov.sh
@@ -1,29 +1,77 @@
 #!/usr/bin/env bash
 set -e
 set -u
-SCRIPTPATH=$( cd "$(dirname "$0")" ; pwd -P )
-ROOTDIR=$(dirname $SCRIPTPATH)
-cd $ROOTDIR
 
-echo "Code coverage test"
+SCRIPTPATH="$(cd "$(dirname "$0")" ; pwd -P)"
+ROOTDIR="$(dirname ${SCRIPTPATH})"
+DIR="./..."
 
-OUT=coverage.txt
-PROF=profile.out
+if [ "${1:-}" != "" ]; then
+    DIR="./$1/..."
+fi
 
-echo "" > ${OUT}
-for d in $(go list ./... | grep -v vendor); do
-    #FIXME remove mixer tools exclusions after tests can be run without bazel
-    if [[ $d == "istio.io/istio/tests"* || \
-      $d == "istio.io/istio/mixer/tools/codegen"* ]];then
-      echo "Skipped $d"
+COVERAGEDIR="$(mktemp -d /tmp/XXXXX.coverage)"
+mkdir -p $COVERAGEDIR
+
+# coverage test needs to run one package per command.
+# This script runs nproc/2 in parallel.
+# Script fails if any one of the tests fail.
+
+# half the number of cpus seem to saturate
+if [[ -z ${MAXPROCS:-} ]];then
+  MAXPROCS=$[$(getconf _NPROCESSORS_ONLN)/2]
+fi
+PIDS=()
+
+declare -a PKGS
+
+function code_coverage() {
+  local filename="$(echo ${1} | tr '/' '-')"
+    go test -coverprofile=${COVERAGEDIR}/${filename}.txt ${1} &
+    local pid=$!
+    PKGS[${pid}]=${1}
+    PIDS+=(${pid})
+}
+
+function wait_for_proc() {
+    local num=$(jobs -p | wc -l)
+    while [ ${num} -gt ${MAXPROCS} ]; do
+      sleep 2
+      num=$(jobs -p|wc -l)
+    done
+}
+
+function join_procs() {
+  local ret=0
+  local p
+  for p in ${PIDS}; do
+      if ! wait ${p}; then
+          echo "${PKGS[${p}]} failed"
+          ret=1
+      fi
+  done
+  wait
+  return ${ret}
+}
+
+cd "${ROOTDIR}"
+
+echo "Code coverage test (concurrency ${MAXPROCS})"
+for P in $(go list ${DIR} | grep -v vendor); do
+    #FIXME remove mixer tools exclusion after tests can be run without bazel
+    if [[ ${P} == "istio.io/istio/tests"* || \
+      ${P} == "istio.io/istio/mixer/tools/codegen"* ]];then
+      echo "Skipped ${P}"
       continue
     fi
-
-    go test -coverprofile=${PROF} $d
-
-    if [ -f ${PROF} ]; then
-        cat ${PROF} >> ${OUT}
-        rm ${PROF}
-    fi
+    code_coverage "${P}"
+    wait_for_proc
 done
 
+EXIT_CODE=0
+join_procs || EXIT_CODE=$?
+
+touch "${COVERAGEDIR}/empty"
+cat "${COVERAGEDIR}"/* > coverage.txt
+
+exit ${EXIT_CODE}
