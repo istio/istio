36f81d545b0a1040d7d87c8b52a84cb6faa44a43
Matt Delco <delco@google.com>
2018-02-23 12:10:37 -0800

    add warning

--- release/pipline/dags/istio_common_dag.py
+++ release/pipline/dags/istio_common_dag.py
@@ -221,6 +221,7 @@ def MakeCommonDag(name='istio_daily_flow_test',
     -t "{{ m_commit }}" -m "{{ settings.MFEST_FILE }}" \
     -a {{ settings.SVC_ACCT }}
     """
+  # NOTE: if you add commands to build_template after start_gcb_build.sh then take care to preserve its return value
 
   build = BashOperator(
       task_id='run_cloud_builder', bash_command=build_template, dag=common_dag)
