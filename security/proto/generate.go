e348de6ec8d700027e35ad7287ae113cd99eeb4b
Saurabh Mohan <saurabh+github@tigera.io>
2018-02-26 15:03:58 -0800

    Remove the nodeagent service protobuf: not needed anymore.

--- security/proto/generate.go
+++ security/proto/generate.go
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 //go:generate $GOPATH/src/istio.io/istio/bin/mixer_codegen.sh -f security/proto/ca_service.proto
-//go:generate $GOPATH/src/istio.io/istio/bin/mixer_codegen.sh -f security/proto/nodeagent_service.proto
 //go:generate $GOPATH/src/istio.io/istio/bin/mixer_codegen.sh -f security/proto/workload_service.proto
 // nolint
 package istio_v1_auth
