7f1c9d350983f4677b9f5d01bded1d47904eb1b4
xiaolanz <xiaolan@google.com>
2018-02-28 11:54:43 -0800

    fmt

--- security/pkg/workload/server.go
+++ security/pkg/workload/server.go
@@ -24,12 +24,10 @@ import (
 	sds "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v2"
 	"github.com/gogo/protobuf/proto"
 	"github.com/gogo/protobuf/types"
-
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-
 	"istio.io/istio/pkg/log"
 )
 
