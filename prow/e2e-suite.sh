ce0625e480a4ff8e1d741dec1e821494a7361a98
Charles Xu <chx@google.com>
2018-03-05 14:27:35 -0800

    Conform to Testgrid Regex on Junit Reports (#3974)

--- prow/e2e-suite.sh
+++ prow/e2e-suite.sh
@@ -106,6 +106,6 @@ else
     echo "Executing e2e test suite"
     time ISTIO_DOCKER_HUB=$HUB \
       E2E_ARGS="${E2E_ARGS[@]}" \
-      JUNIT_E2E_XML="${ARTIFACTS_DIR}/junit_e2e_all.xml" \
+      JUNIT_E2E_XML="${ARTIFACTS_DIR}/junit_e2e-all.xml" \
       make e2e_all
 fi
