26b22966e69b2d4e8177a1918c722ea1fbe261a5
Yangmin Zhu <ymzhu@google.com>
2018-03-06 15:32:40 -0800

    Add more unit test for the SAN util. (#3951)

--- security/pkg/pki/util/san_test.go
+++ security/pkg/pki/util/san_test.go
@@ -17,11 +17,55 @@ package util
 import (
 	"crypto/x509/pkix"
 	"encoding/asn1"
+	"net"
 	"reflect"
 	"testing"
 )
 
-// TODO(myidpt): Add more unit tests.
+func getSANExtension(identites []Identity, t *testing.T) *pkix.Extension {
+	ext, err := BuildSANExtension(identites)
+	if err != nil {
+		t.Errorf("A unexpected error has been encountered (error: %v)", err)
+	}
+	return ext
+}
+
+func TestBuildSubjectAltNameExtension(t *testing.T) {
+	uriIdentity := Identity{Type: TypeURI, Value: []byte("spiffe://test.domain.com/ns/default/sa/default")}
+	ipIdentity := Identity{Type: TypeIP, Value: net.ParseIP("10.0.0.1").To4()}
+	dnsIdentity := Identity{Type: TypeDNS, Value: []byte("test.domain.com")}
+
+	testCases := map[string]struct {
+		hosts       string
+		expectedExt *pkix.Extension
+	}{
+		"URI host": {
+			hosts:       "spiffe://test.domain.com/ns/default/sa/default",
+			expectedExt: getSANExtension([]Identity{uriIdentity}, t),
+		},
+		"IP host": {
+			hosts:       "10.0.0.1",
+			expectedExt: getSANExtension([]Identity{ipIdentity}, t),
+		},
+		"DNS host": {
+			hosts:       "test.domain.com",
+			expectedExt: getSANExtension([]Identity{dnsIdentity}, t),
+		},
+		"URI, IP and DNS hosts": {
+			hosts:       "spiffe://test.domain.com/ns/default/sa/default,10.0.0.1,test.domain.com",
+			expectedExt: getSANExtension([]Identity{uriIdentity, ipIdentity, dnsIdentity}, t),
+		},
+	}
+
+	for id, tc := range testCases {
+		if ext, err := BuildSubjectAltNameExtension(tc.hosts); err != nil {
+			t.Errorf("Case %q: a unexpected error has been encountered (error: %v)", id, err)
+		} else if !reflect.DeepEqual(ext, tc.expectedExt) {
+			t.Errorf("Case %q: unexpected extension returned: want %v but got %v", id, tc.expectedExt, ext)
+		}
+	}
+}
+
 func TestBuildAndExtractIdentities(t *testing.T) {
 	ids := []Identity{
 		{Type: TypeDNS, Value: []byte("test.domain.com")},
@@ -46,7 +90,7 @@ func TestBuildAndExtractIdentities(t *testing.T) {
 func TestBuildSANExtensionWithError(t *testing.T) {
 	id := Identity{Type: 10}
 	if _, err := BuildSANExtension([]Identity{id}); err == nil {
-		t.Error("Expecting error to be returned by got nil")
+		t.Error("Expecting error to be returned but got nil")
 	}
 }
 
@@ -69,7 +113,7 @@ func TestExtractIDsFromSANWithError(t *testing.T) {
 
 	for id, tc := range testCases {
 		if _, err := ExtractIDsFromSAN(tc.ext); err == nil {
-			t.Errorf("%v: Expecting error to be returned by got nil", id)
+			t.Errorf("%v: Expecting error to be returned but got nil", id)
 		}
 	}
 }
@@ -81,7 +125,7 @@ func TestExtractIDsFromSANWithBadEncoding(t *testing.T) {
 	}
 
 	if _, err := ExtractIDsFromSAN(ext); err == nil {
-		t.Error("Expecting error to be returned by got nil")
+		t.Error("Expecting error to be returned but got nil")
 	}
 }
 
