afa7cec14f1c12093375890490a67e3c0a02e022
Diem Vu <diemvu@google.com>
2018-03-07 18:27:05 -0800

    Add more test for serviceaccount_test

--- security/pkg/registry/kube/serviceaccount_test.go
+++ security/pkg/registry/kube/serviceaccount_test.go
@@ -39,37 +39,132 @@ type serviceAccountPair struct {
 	newSvcAcct *v1.ServiceAccount
 }
 
+func TestSpiffeID(t *testing.T) {
+	testCases := []struct {
+		name      string
+		namespace string
+		expected  string
+	}{
+		{
+			name:      "foo",
+			namespace: "bar",
+			expected:  "spiffe://cluster.local/ns/bar/sa/foo",
+		},
+		{
+			name:      "foo",
+			namespace: "",
+			expected:  "spiffe://cluster.local/ns//sa/foo",
+		},
+		{
+			name:      "",
+			namespace: "bar",
+			expected:  "spiffe://cluster.local/ns/bar/sa/",
+		},
+		{
+			name:      "",
+			namespace: "",
+			expected:  "spiffe://cluster.local/ns//sa/",
+		},
+		{
+			name:      "svc@test.serviceaccount.com",
+			namespace: "default",
+			expected:  "spiffe://cluster.local/ns/default/sa/svc@test.serviceaccount.com",
+		},
+	}
+	for _, c := range testCases {
+		if id := getSpiffeID(createServiceAccount(c.name, c.namespace)); id != c.expected {
+			t.Errorf("getSpiffeID(%s, %s): expected %s, got %s", c.name, c.namespace, c.expected, id)
+		}
+	}
+}
+
 func TestServiceAccountController(t *testing.T) {
 	namespace := "test-ns"
 	sa := createServiceAccount("svc@test.serviceaccount.com", namespace)
 	sa1 := createServiceAccount("svc1@test.serviceaccount.com", namespace)
 	sa2 := createServiceAccount("svc2@test.serviceaccount.com", namespace)
+	sa3 := createServiceAccount("svc@test.serviceaccount.com", "on-another-galaxy")
 
 	testCases := map[string]struct {
-		toAdd    *v1.ServiceAccount
-		toDelete *v1.ServiceAccount
+		toAdd    []*v1.ServiceAccount
+		toDelete []*v1.ServiceAccount
 		toUpdate *serviceAccountPair
 		mapping  map[string]string
 	}{
 		"add k8s service account": {
-			toAdd: sa,
+			toAdd: []*v1.ServiceAccount{sa},
 			mapping: map[string]string{
 				getSpiffeID(sa): getSpiffeID(sa),
 			},
 		},
 		"add and delete k8s service account": {
-			toAdd:    sa,
-			toDelete: sa,
+			toAdd:    []*v1.ServiceAccount{sa},
+			toDelete: []*v1.ServiceAccount{sa},
 			mapping:  map[string]string{},
 		},
 		"add and update k8s service account": {
-			toAdd: sa1,
+			toAdd: []*v1.ServiceAccount{sa1},
 			toUpdate: &serviceAccountPair{
 				oldSvcAcct: sa1,
 				newSvcAcct: sa2,
 			},
 			mapping: map[string]string{
 				getSpiffeID(sa2): getSpiffeID(sa2),
+			},
+		},
+		"multiple add of different k8s service account": {
+			toAdd: []*v1.ServiceAccount{sa, sa1, sa2},
+			mapping: map[string]string{
+				getSpiffeID(sa):  getSpiffeID(sa),
+				getSpiffeID(sa1): getSpiffeID(sa1),
+				getSpiffeID(sa2): getSpiffeID(sa2),
+			},
+		},
+		"multiple add of the same k8s service account": {
+			toAdd: []*v1.ServiceAccount{sa, sa},
+			mapping: map[string]string{
+				getSpiffeID(sa): getSpiffeID(sa),
+			},
+		},
+		"multiple delete of the same k8s service account": {
+			toAdd:    []*v1.ServiceAccount{sa},
+			toDelete: []*v1.ServiceAccount{sa, sa1, sa},
+			mapping:  map[string]string{},
+		},
+		"update none existing": {
+			toUpdate: &serviceAccountPair{
+				oldSvcAcct: sa1,
+				newSvcAcct: sa2,
+			},
+			mapping: map[string]string{
+				getSpiffeID(sa2): getSpiffeID(sa2),
+			},
+		},
+		"update with same svc account": {
+			toAdd: []*v1.ServiceAccount{sa1},
+			toUpdate: &serviceAccountPair{
+				oldSvcAcct: sa1,
+				newSvcAcct: sa1,
+			},
+			mapping: map[string]string{
+				getSpiffeID(sa1): getSpiffeID(sa1),
+			},
+		},
+		"update with same non-exist svc account": {
+			toUpdate: &serviceAccountPair{
+				oldSvcAcct: sa1,
+				newSvcAcct: sa1,
+			},
+			mapping: map[string]string{},
+		},
+		"update with sva from different namespace": {
+			toAdd: []*v1.ServiceAccount{sa},
+			toUpdate: &serviceAccountPair{
+				oldSvcAcct: sa,
+				newSvcAcct: sa3,
+			},
+			mapping: map[string]string{
+				getSpiffeID(sa3): getSpiffeID(sa3),
 			},
 		},
 	}
@@ -81,11 +176,11 @@ func TestServiceAccountController(t *testing.T) {
 		}
 		controller := NewServiceAccountController(client.CoreV1(), "test-ns", reg)
 
-		if c.toAdd != nil {
-			controller.serviceAccountAdded(c.toAdd)
+		for _, svcAcc := range c.toAdd {
+			controller.serviceAccountAdded(svcAcc)
 		}
-		if c.toDelete != nil {
-			controller.serviceAccountDeleted(c.toDelete)
+		for _, svcAcc := range c.toDelete {
+			controller.serviceAccountDeleted(svcAcc)
 		}
 		if c.toUpdate != nil {
 			controller.serviceAccountUpdated(c.toUpdate.oldSvcAcct, c.toUpdate.newSvcAcct)
