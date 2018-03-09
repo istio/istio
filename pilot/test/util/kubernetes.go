da970cb63c8045158e9fb8356e49e0a8e5f71e6c
Zack Butcher <zbutcher@google.com>
2018-03-08 17:33:00 -0800

    add function name to helper function failure message to aid debugging

--- pilot/test/util/kubernetes.go
+++ pilot/test/util/kubernetes.go
@@ -212,7 +212,7 @@ func FetchLogs(cl kubernetes.Interface, name, namespace string, container string
 }
 
 // Eventually retries until f() returns true, or it times out in error
-func Eventually(f func() bool, t *testing.T) {
+func Eventually(name string, f func() bool, t *testing.T) {
 	interval := 64 * time.Millisecond
 	for i := 0; i < 10; i++ {
 		if f() {
@@ -222,5 +222,5 @@ func Eventually(f func() bool, t *testing.T) {
 		time.Sleep(interval)
 		interval = 2 * interval
 	}
-	t.Errorf("Failed to satisfy function")
+	t.Errorf("Failed to satisfy function %q", name)
 }
