97b82cc0020fab5383791571fc9b31e4da9c94d0
Oliver Liu <yonggangl@google.com>
2018-03-12 15:47:18 -0700

    Mandate certs in Istio control plane components when auth is on.

--- install/updateVersion.sh
+++ install/updateVersion.sh
@@ -177,6 +177,7 @@ function merge_files() {
   cat $SRC/istio-ca.yaml.tmpl >> $ISTIO
 
   cp $ISTIO $ISTIO_AUTH
+  execute_sed "s/optional: true # FALSE WHEN AUTH IS ENABLED/optional: false/" $ISTIO_AUTH
   execute_sed "s/discoveryAddress: istio-pilot.${ISTIO_NAMESPACE}:15007/discoveryAddress: istio-pilot.${ISTIO_NAMESPACE}:15005/" $ISTIO_AUTH
   execute_sed "s/- istio-pilot:15007/- istio-pilot:15005/" $ISTIO_AUTH
   execute_sed "s/# authPolicy: MUTUAL_TLS/authPolicy: MUTUAL_TLS/" $ISTIO_AUTH
@@ -191,6 +192,7 @@ function merge_files() {
   cat $SRC/istio-ca-one-namespace.yaml.tmpl >> $ISTIO_ONE_NAMESPACE
 
   cp $ISTIO_ONE_NAMESPACE $ISTIO_ONE_NAMESPACE_AUTH
+  execute_sed "s/optional: true # FALSE WHEN AUTH IS ENABLED/optional: false/" $ISTIO_ONE_NAMESPACE_AUTH
   execute_sed "s/discoveryAddress: istio-pilot.${ISTIO_NAMESPACE}:15007/discoveryAddress: istio-pilot.${ISTIO_NAMESPACE}:15005/" $ISTIO_ONE_NAMESPACE_AUTH
   execute_sed "s/- istio-pilot:15007/- istio-pilot:15005/" $ISTIO_ONE_NAMESPACE_AUTH
   execute_sed "s/# authPolicy: MUTUAL_TLS/authPolicy: MUTUAL_TLS/" $ISTIO_ONE_NAMESPACE_AUTH
