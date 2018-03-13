6fc6fbeb9236e5faae32c308e3265ab245325ad5
Yusuo Hu <yusuo@google.com>
2018-03-12 17:17:50 -0700

    Self-registration in webhook.

--- pilot/cmd/sidecar-injector/main.go
+++ pilot/cmd/sidecar-injector/main.go
@@ -17,19 +17,29 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"time"
 
 	multierror "github.com/hashicorp/go-multierror"
 	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
-
 	"istio.io/istio/pilot/cmd"
 	"istio.io/istio/pilot/pkg/kube/inject"
 	"istio.io/istio/pkg/collateral"
 	"istio.io/istio/pkg/log"
 	"istio.io/istio/pkg/probe"
+	"istio.io/istio/pkg/util"
 	"istio.io/istio/pkg/version"
+	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+const (
+	sidecarWebhookConfigName = "istio-sidecar-injector"
+	sidecarWebhookName       = "sidecar-injector.istio.io"
 )
 
 var (
@@ -40,10 +50,12 @@ var (
 		injectConfigFile    string
 		certFile            string
 		privateKeyFile      string
+		caCertFile          string
 		port                int
 		healthCheckInterval time.Duration
 		healthCheckFile     string
 		probeOptions        probe.Options
+		kubeConfigFile      string
 	}{
 		loggingOptions: log.DefaultOptions(),
 	}
@@ -72,6 +84,10 @@ var (
 				return multierror.Prefix(err, "failed to create injection webhook")
 			}
 
+			if err := patchCert(); err != nil {
+				return multierror.Prefix(err, "failed to patch webhook config")
+			}
+
 			stop := make(chan struct{})
 			go wh.Run(stop)
 			cmd.WaitSignal(stop)
@@ -95,22 +111,63 @@ var (
 	}
 )
 
+func patchCert() error {
+	client, err := createClientset(flags.kubeConfigFile)
+	if err != nil {
+		return err
+	}
+	caCertPem, err := ioutil.ReadFile(flags.caCertFile)
+	if err != nil {
+		return err
+	}
+	err = util.PatchMutatingWebhookConfig(client.AdmissionregistrationV1beta1().MutatingWebhookConfigurations(),
+		sidecarWebhookConfigName, sidecarWebhookName, caCertPem)
+	for err != nil {
+		log.Errorf("Register webhook failed: %s. Retrying...", err)
+		time.Sleep(time.Second * 10)
+		err = util.PatchMutatingWebhookConfig(client.AdmissionregistrationV1beta1().MutatingWebhookConfigurations(),
+			sidecarWebhookConfigName, sidecarWebhookName, caCertPem)
+	}
+	return nil
+}
+
+func createClientset(kubeConfigFile string) (*kubernetes.Clientset, error) {
+	var err error
+	var c *rest.Config
+	if kubeConfigFile != "" {
+		c, err = clientcmd.BuildConfigFromFlags("", kubeConfigFile)
+	} else {
+		c, err = rest.InClusterConfig()
+	}
+	if err != nil {
+		return nil, err
+	}
+	cs, err := kubernetes.NewForConfig(c)
+	if err != nil {
+		return nil, err
+	}
+	return cs, nil
+}
+
 func init() {
 	rootCmd.PersistentFlags().StringVar(&flags.meshconfig, "meshConfig", "/etc/istio/config/mesh",
 		"File containing the Istio mesh configuration")
 	rootCmd.PersistentFlags().StringVar(&flags.injectConfigFile, "injectConfig", "/etc/istio/inject/config",
 		"File containing the Istio sidecar injection configuration and template")
-	rootCmd.PersistentFlags().StringVar(&flags.certFile, "tlsCertFile", "/etc/istio/certs/cert.pem",
+	rootCmd.PersistentFlags().StringVar(&flags.certFile, "tlsCertFile", "/etc/istio/certs/cert-chain.pem",
 		"File containing the x509 Certificate for HTTPS.")
 	rootCmd.PersistentFlags().StringVar(&flags.privateKeyFile, "tlsKeyFile", "/etc/istio/certs/key.pem",
 		"File containing the x509 private key matching --tlsCertFile.")
+	rootCmd.PersistentFlags().StringVar(&flags.caCertFile, "caCertFile", "/etc/istio/certs/root-cert.pem",
+		"File containing the x509 Certificate for HTTPS.")
 	rootCmd.PersistentFlags().IntVar(&flags.port, "port", 443, "Webhook port")
 
 	rootCmd.PersistentFlags().DurationVar(&flags.healthCheckInterval, "healthCheckInterval", 0,
 		"Configure how frequently the health check file specified by --healhCheckFile should be updated")
 	rootCmd.PersistentFlags().StringVar(&flags.healthCheckFile, "healthCheckFile", "",
 		"File that should be periodically updated if health checking is enabled")
-
+	rootCmd.PersistentFlags().StringVar(&flags.kubeConfigFile, "kube-config", "",
+		"Specifies path to kubeconfig file. This must be specified when not running inside a Kubernetes pod.")
 	// Attach the Istio logging options to the command.
 	flags.loggingOptions.AttachCobraFlags(rootCmd)
 
