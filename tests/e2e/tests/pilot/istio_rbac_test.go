64c8a9f8e4753b0c8e53194e91723f56364c026d
Yangmin Zhu <ymzhu@google.com>
2018-03-22 14:18:08 -0700

    Change copyright to 2018.

--- tests/e2e/tests/pilot/istio_rbac_test.go
+++ tests/e2e/tests/pilot/istio_rbac_test.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Istio Authors
+// Copyright 2018 Istio Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@ package pilot
 
 import (
 	"fmt"
+
 	"istio.io/istio/pkg/log"
 	tutil "istio.io/istio/tests/e2e/tests/pilot/util"
 )
