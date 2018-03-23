bae417adbfb7c43606bee713e369f0a9444a3c6c
xiaolanz <xiaolan@google.com>
2018-03-23 11:29:28 -0700

    Revert "remove fmt.sh (#4425)"
    This reverts commit ca381be569331c02df9344edd0e7fb2e7b093b87.

---
+++ bin/fmt.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Applies requisite code formatters to the source tree
+
+set -e
+SCRIPTPATH=$( cd "$(dirname "$0")" ; pwd -P )
+
+ROOTDIR=$SCRIPTPATH/..
+cd $ROOTDIR
+
+export GOPATH=$(cd $ROOTDIR/../../..; pwd)
+export PATH=$GOPATH/bin:$PATH
+
+if which goimports; then
+  goimports=`which goimports`
+else
+  go get golang.org/x/tools/cmd/goimports
+  goimports=${GOPATH}/bin/goimports
+fi
+
+PKGS=${PKGS:-"."}
+if [[ -z ${GO_FILES} ]];then
+  GO_FILES=$(find ${PKGS} -type f -name '*.go' ! -name '*.gen.go' ! -name '*.pb.go' ! -name '*mock*.go' | grep -v ./vendor)
+fi
+
+UX=$(uname)
+
+#remove blank lines so gofmt / goimports can do their job
+for fl in ${GO_FILES}; do
+  if [[ ${UX} == "Darwin" ]];then
+    sed -i '' -e "/^import[[:space:]]*(/,/)/{ /^\s*$/d;}" $fl
+  else
+    sed -i -e "/^import[[:space:]]*(/,/)/{ /^\s*$/d;}" $fl
+fi
+done
+
+gofmt -s -w ${GO_FILES}
+$goimports -w -local istio.io ${GO_FILES}
