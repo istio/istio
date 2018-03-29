65ac779644387a01eb4c065dea5713f3bd9ee560
Shriram Rajagopalan <shriramr@vmware.com>
2018-03-29 14:04:41 -0400

    more code

--- pilot/pkg/networking/plugins/authn/authentication.go
+++ pilot/pkg/networking/plugins/authn/authentication.go
@@ -16,14 +16,14 @@ package authn
 
 import (
 	xdsapi "github.com/envoyproxy/go-control-plane/envoy/api/v2"
-	http_conn "github.com/envoyproxy/go-control-plane/envoy/config/filter/network/http_connection_manager/v2"
 	"github.com/envoyproxy/go-control-plane/envoy/api/v2/auth"
 	"github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
+	http_conn "github.com/envoyproxy/go-control-plane/envoy/config/filter/network/http_connection_manager/v2"
 
 	authn "istio.io/api/authentication/v1alpha1"
 	"istio.io/istio/pilot/pkg/model"
+	"istio.io/istio/pilot/pkg/networking"
 	"istio.io/istio/pilot/pkg/networking/util"
-	"crypto/tls"
 )
 
 const (
@@ -32,6 +32,12 @@ const (
 	// https://github.com/istio/proxy/blob/master/src/envoy/http/jwt_auth/http_filter_factory.cc#L50
 	jwtFilterName = "jwt-auth"
 )
+
+func NewAuthnPlugin() *networking.PluginCallbacks {
+	return &networking.PluginCallbacks{
+		OnOutboundCluster: HandleOutboundCluster,
+	}
+}
 
 // BuildJwtFilter returns a Jwt filter for all Jwt specs in the policy.
 func BuildJwtFilter(policy *authn.Policy) *http_conn.HttpFilter {
@@ -46,44 +52,58 @@ func BuildJwtFilter(policy *authn.Policy) *http_conn.HttpFilter {
 }
 
 // ApplyOutboundIstioAuth adds mTLS authN settings for outbound clusters
-func ApplyOutboundIstioAuth(policy *authn.Policy, cluster *xdsapi.Cluster) {
+func HandleOutboundCluster(env model.Environment, node *model.Proxy, service *model.Service, servicePort *model.Port, cluster *xdsapi.Cluster) {
+
+	mesh := env.Mesh
+	config := env.IstioConfigStore
 
 	// Original DST cluster are used to route to services outside the mesh
 	// where Istio auth does not apply.
-	//if cluster.Type == xdsapi.Cluster_ORIGINAL_DST { return }
-	//	if !isDestinationExcludedForMTLS(cluster.ServiceName, mesh.MtlsExcludedServices) &&
-	//		model.RequireTLS(model.GetConsolidateAuthenticationPolicy(mesh, config, cluster.Hostname, cluster.Port)) {
-	//		// apply auth policies
-	//		ports := model.PortList{cluster.Port}.GetNames()
-	//		serviceAccounts := accounts.GetIstioServiceAccounts(cluster.Hostname, ports)
-	//		cluster.SSLContext = buildClusterSSLContext(model.AuthCertsPath, serviceAccounts)
-	//	}
-	//
-	//cluster.TlsContext = &auth.UpstreamTlsContext{
-	//	CommonTlsContext: &auth.CommonTlsContext{
-	//		TlsCertificates: []*auth.TlsCertificate{
-	//			{
-	//				CertificateChain: &core.DataSource{
-	//					Specifier: &core.DataSource_Filename{
-	//						Filename: tls.ClientCertificate,
-	//					},
-	//				},
-	//				PrivateKey: &core.DataSource{
-	//					Specifier: &core.DataSource_Filename{
-	//						Filename: tls.PrivateKey,
-	//					},
-	//				},
-	//			},
-	//		},
-	//		ValidationContext: &auth.CertificateValidationContext{
-	//			TrustedCa: &core.DataSource{
-	//				Specifier: &core.DataSource_Filename{
-	//					Filename: tls.CaCertificates,
-	//				},
-	//			},
-	//			VerifySubjectAltName: tls.SubjectAltNames,
-	//		},
-	//	},
-	//	Sni: tls.Sni,
-	//}
+	if cluster.Type == xdsapi.Cluster_ORIGINAL_DST {
+		return
+	}
+
+	if isDestinationExcludedForMTLS(service.Hostname, mesh.MtlsExcludedServices) &&
+		!model.RequireTLS(model.GetConsolidateAuthenticationPolicy(mesh, config, service.Hostname, servicePort)) {
+		return
+	}
+
+	// apply auth policies
+	serviceAccounts := env.ServiceAccounts.GetIstioServiceAccounts(service.Hostname, []string{servicePort.Name})
+
+	cluster.TlsContext = &auth.UpstreamTlsContext{
+		CommonTlsContext: &auth.CommonTlsContext{
+			TlsCertificates: []*auth.TlsCertificate{
+				{
+					CertificateChain: &core.DataSource{
+						Specifier: &core.DataSource_Filename{
+							Filename: model.AuthCertsPath + model.CertChainFilename,
+						},
+					},
+					PrivateKey: &core.DataSource{
+						Specifier: &core.DataSource_Filename{
+							Filename: model.AuthCertsPath + model.KeyFilename,
+						},
+					},
+				},
+			},
+			ValidationContext: &auth.CertificateValidationContext{
+				TrustedCa: &core.DataSource{
+					Specifier: &core.DataSource_Filename{
+						Filename: model.AuthCertsPath + model.RootCertFilename,
+					},
+				},
+				VerifySubjectAltName: serviceAccounts,
+			},
+		},
+	}
+}
+
+func isDestinationExcludedForMTLS(destService string, mtlsExcludedServices []string) bool {
+	for _, serviceName := range mtlsExcludedServices {
+		if destService == serviceName {
+			return true
+		}
+	}
+	return false
 }
