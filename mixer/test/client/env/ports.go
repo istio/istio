24cf25a72978092d066e77f76c76b9f14147cb12
Yinjie Chen <28548492+JimmyCYJ@users.noreply.github.com>
2018-03-30 16:48:52 -0700

    Large post (#4643)
    Automatic merge from submit-queue.
    
    Large post
    
    Add test that verifies PR#[1318](https://github.com/istio/proxy/pull/1318) fixes #[1315](https://github.com/istio/proxy/issues/1315).

--- mixer/test/client/env/ports.go
+++ mixer/test/client/env/ports.go
@@ -24,10 +24,11 @@ import (
 
 // All tests should be listed here to get their test ids
 const (
-	CheckCacheTest uint16 = iota
-	CheckCacheHitTest
+	CheckCacheHitTest uint16 = iota
+	CheckCacheTest
 	CheckReportAttributesTest
 	CheckReportDisableTest
+	CheckReportLargePostRequestTest
 	DisableCheckCacheTest
 	DisableTCPCheckCallsTest
 	FailedRequestTest
@@ -35,11 +36,11 @@ const (
 	JWTAuthTest
 	MixerInternalFailTest
 	NetworkFailureTest
-	ReportBatchTest
-	TCPMixerFilterTest
 	QuotaCacheTest
 	QuotaCallTest
+	ReportBatchTest
 	TCPMixerFilterPeriodicalReportTest
+	TCPMixerFilterTest
 	TCPMixerFilterV1ConfigTest
 	XDSTest
 
