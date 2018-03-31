fcf237677d4b3c0cd9eb8ebd5f25d95be16a7572
Shriram Rajagopalan <rshriram@users.noreply.github.com>
2018-03-30 20:24:46 -0400

    LDSv2 bug fix (#4665)
    * Starting to swap alpha3 in the helm-test deployment for load/scale testing
    
    * Increase load, to test v2. Add a tag to make it easy to reload
    
    * Adjust the test templates
    
    * Adjust the max streams and circuit breakers.
    
    https://github.com/istio/istio/issues/4593
    
    * Add the golden files
    
    * Fix syntax
    
    * Mixer filter in the wrong place
    
    * Add another test service, should generate 2 virtual hosts on 80
    
    * per vip listener
    
    * route config bug fix
    
    Signed-off-by: Shriram Rajagopalan <shriramr@vmware.com>
    
    * sync
    
    * new files
    
    * bug fix: weighted cluster default weight to 100 to match spec
    
    * lint fix
    
    * lint fix
    
    * more fixes
    
    Signed-off-by: Shriram Rajagopalan <shriramr@vmware.com>
    
    * bug fix, two mock services with same IP created
    
    * sync

--- pilot/pkg/proxy/envoy/v2/lds_test.go
+++ pilot/pkg/proxy/envoy/v2/lds_test.go
@@ -72,9 +72,8 @@ func TestLDS(t *testing.T) {
 		}
 	})
 
-	// TODO need mock ingress resource or gateway resource to test this.
-	t.Run("ingress", func(t *testing.T) {
-		ldsr := connectLDS(util.MockPilotGrpcAddr, ingressId(ingressIP), t)
+	t.Run("gateway", func(t *testing.T) {
+		ldsr := connectLDS(util.MockPilotGrpcAddr, gatewayId(gatewayIP), t)
 
 		res, err := ldsr.Recv()
 		if err != nil {
@@ -84,7 +83,7 @@ func TestLDS(t *testing.T) {
 
 		strResponse, _ := model.ToJSONWithIndent(res, " ")
 
-		_ = ioutil.WriteFile(util.IstioOut+"/ldsv2_ingress.json", []byte(strResponse), 0644)
+		_ = ioutil.WriteFile(util.IstioOut+"/ldsv2_gateway.json", []byte(strResponse), 0644)
 
 		t.Log("LDS response ingress", strResponse)
 		if len(res.Resources) == 0 {
