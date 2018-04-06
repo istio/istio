aa9df890a5ee55c583d88c727cdf8bf09fda642a
Jianfei Hu <jianfeih@google.com>
2018-04-06 15:04:51 -0700

    remove empty line

--- security/cmd/istio_ca/main.go
+++ security/cmd/istio_ca/main.go
@@ -109,7 +109,6 @@ type cliOptions struct { // nolint: maligned
 	grpcHostname string
 	// Comma separated string containing all possible host name that clients may use to connect to.
 	grpcHosts string
-
 	grpcPort int
 
 	multiclusterCA bool
