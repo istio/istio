c3ea98117f8dcce0d86b6be00ff468210d097eda
Oliver Liu <yonggangl@google.com>
2018-04-09 12:30:07 -0700

    Add dumb Vault adapter complying with CA interface. (#4772)
    Automatic merge from submit-queue.
    
    Add dumb Vault adapter complying with CA interface.
    
    This PR is a kick start for Istio CA to integrate with HashiCorp Vault server.

---
+++ security/pkg/adapter/vault/ca.go
@@ -0,0 +1,49 @@
+// Copyright 2018 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package vault provides adapter to connect to vault server.
+package vault
+
+import (
+	"fmt"
+	"time"
+
+	"istio.io/istio/security/pkg/pki/util"
+)
+
+// CA connects to Vault to sign certificates.
+type CA struct {
+}
+
+// New returns a new CA instance.
+func New() (*CA, error) {
+	return &CA{}, nil
+}
+
+// Sign takes a PEM-encoded CSR and returns a signed certificate. If the CA is a multicluster CA,
+// the signed certificate is a CA certificate (CA:TRUE in X509v3 Basic Constraints), otherwise, it is a workload
+// certificate.
+func (v *CA) Sign(csrPEM []byte, ttl time.Duration) ([]byte, error) {
+	return nil, fmt.Errorf("not implemented")
+}
+
+// SignCAServerCert signs the certificate for the Istio CA server (to serve the CSR, etc).
+func (v *CA) SignCAServerCert(csrPEM []byte, ttl time.Duration) ([]byte, error) {
+	return nil, fmt.Errorf("not implemented")
+}
+
+// GetKeyCertBundle returns the KeyCertBundle for the CA.
+func (v *CA) GetKeyCertBundle() util.KeyCertBundle {
+	return nil
+}
