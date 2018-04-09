5aa39ac1bf6e87a2815dfeed6f7fe5f01ff360b0
Steven Dake <stdake@cisco.com>
2018-04-09 07:43:31 -0700

    Generate multicluster yaml files for gate testing

--- install/updateVersion.sh
+++ install/updateVersion.sh
@@ -56,7 +56,7 @@ function gen_file() {
 }
 
 
-for target in istio.yaml istio-auth.yaml istio-one-namespace.yaml istio-one-namespace-auth.yaml;do
+for target in istio.yaml istio-auth.yaml istio-one-namespace.yaml istio-one-namespace-auth.yaml istio-multicluster.yaml istio-auth-multicluster.yaml;do
     gen_file $target ${DEST_DIR}
 done
 
