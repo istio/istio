e0aa9fa5822964f0a4c10d5706c8838af52cbbe2
Shriram Rajagopalan <rshriram@users.noreply.github.com>
2018-04-11 07:38:31 -0400

    fix alt virtual host generation  (#4846)
    * use port number instead of names uniformly
    
    Signed-off-by: Shriram Rajagopalan <shriramr@vmware.com>
    
    * bug fix for alt virtual hosts
    
    Signed-off-by: Shriram Rajagopalan <shriramr@vmware.com>
    
    * undo port name to number
    
    Signed-off-by: Shriram Rajagopalan <shriramr@vmware.com>
    
    * tests
    
    Signed-off-by: Shriram Rajagopalan <shriramr@vmware.com>
    
    * nits
    
    Signed-off-by: Shriram Rajagopalan <shriramr@vmware.com>

--- pilot/pkg/networking/core/v1alpha3/httproute.go
+++ pilot/pkg/networking/core/v1alpha3/httproute.go
@@ -24,8 +24,8 @@ import (
 	"github.com/gogo/protobuf/types"
 
 	"istio.io/istio/pilot/pkg/model"
+	istio_route "istio.io/istio/pilot/pkg/networking/core/v1alpha3/route"
 	"istio.io/istio/pilot/pkg/networking/plugin"
-	istio_route "istio.io/istio/pilot/pkg/networking/route"
 	"istio.io/istio/pilot/pkg/networking/util"
 )
 
@@ -120,21 +120,9 @@ func (configgen *ConfigGeneratorImpl) BuildSidecarOutboundHTTPRouteConfig(env mo
 		}
 
 		for _, svc := range guardedHost.Services {
-			domains := []string{svc.Hostname, fmt.Sprintf("%s:%d", svc.Hostname, guardedHost.Port)}
-			if !svc.MeshExternal {
-				domains = append(domains, generateAltVirtualHosts(svc.Hostname, guardedHost.Port)...)
-			}
-			if len(svc.Address) > 0 {
-				// add a vhost match for the IP (if its non CIDR)
-				cidr := util.ConvertAddressToCidr(svc.Address)
-				if cidr.PrefixLen.Value == 32 {
-					domains = append(domains, svc.Address)
-					domains = append(domains, fmt.Sprintf("%s:%d", svc.Address, guardedHost.Port))
-				}
-			}
 			virtualHosts = append(virtualHosts, route.VirtualHost{
 				Name:    fmt.Sprintf("%s:%d", svc.Hostname, guardedHost.Port),
-				Domains: domains,
+				Domains: buildVirtualHostDomains(svc, guardedHost.Port, node),
 				Routes:  guardedHost.Routes,
 			})
 		}
@@ -168,15 +156,57 @@ func (configgen *ConfigGeneratorImpl) BuildSidecarOutboundHTTPRouteConfig(env mo
 	return out
 }
 
+// buildVirtualHostDomains generates the set of domain matches for a service being accessed from
+// a proxy node
+func buildVirtualHostDomains(service *model.Service, port int, node model.Proxy) []string {
+	domains := []string{service.Hostname, fmt.Sprintf("%s:%d", service.Hostname, port)}
+	domains = append(domains, generateAltVirtualHosts(service.Hostname, port, node.Domain)...)
+
+	if len(service.Address) > 0 {
+		// add a vhost match for the IP (if its non CIDR)
+		cidr := util.ConvertAddressToCidr(service.Address)
+		if cidr.PrefixLen.Value == 32 {
+			domains = append(domains, service.Address)
+			domains = append(domains, fmt.Sprintf("%s:%d", service.Address, port))
+		}
+	}
+	return domains
+}
+
 // Given a service, and a port, this function generates all possible HTTP Host headers.
-// For example, a service of the form foo.local.campus.net on port 80 could be accessed as
-// http://foo:80 within the .local network, as http://foo.local:80 (by other clients in the campus.net domain),
-// as http://foo.local.campus:80, etc.
-func generateAltVirtualHosts(hostname string, port int) []string {
+// For example, a service of the form foo.local.campus.net on port 80, with local domain "local.campus.net"
+// could be accessed as http://foo:80 within the .local network, as http://foo.local:80 (by other clients
+// in the campus.net domain), as http://foo.local.campus:80, etc.
+// NOTE: When a sidecar in remote.campus.net domain is talking to foo.local.campus.net,
+// we should only generate foo.local, foo.local.campus, etc (and never just "foo").
+//
+// - Given foo.local.campus.net on proxy domain local.campus.net, this function generates
+// foo:80, foo.local:80, foo.local.campus:80, with and without ports. It will not generate
+// foo.local.campus.net (full hostname) since its already added elsewhere.
+//
+// - Given foo.local.campus.net on proxy domain remote.campus.net, this function generates
+// foo.local:80, foo.local.campus:80
+//
+// - Given foo.local.campus.net on proxy domain "" or proxy domain example.com, this
+// function returns nil
+func generateAltVirtualHosts(hostname string, port int, proxyDomain string) []string {
 	var vhosts []string
-	for i := len(hostname) - 1; i >= 0; i-- {
-		if hostname[i] == '.' {
-			variant := hostname[:i]
+	uniqHostname, sharedDNSDomain := getUniqueAndSharedDNSDomain(hostname, proxyDomain)
+
+	// If there is no shared DNS name (e.g., foobar.com service on local.net proxy domain)
+	// do not generate any alternate virtual host representations
+	if len(sharedDNSDomain) == 0 {
+		return nil
+	}
+
+	// adds the uniq piece foo, foo:80
+	vhosts = append(vhosts, uniqHostname)
+	vhosts = append(vhosts, fmt.Sprintf("%s:%d", uniqHostname, port))
+
+	// adds all the other variants (foo.local, foo.local:80)
+	for i := len(sharedDNSDomain) - 1; i > 0; i-- {
+		if sharedDNSDomain[i] == '.' {
+			variant := fmt.Sprintf("%s.%s", uniqHostname, sharedDNSDomain[:i])
 			variantWithPort := fmt.Sprintf("%s:%d", variant, port)
 			vhosts = append(vhosts, variant)
 			vhosts = append(vhosts, variantWithPort)
@@ -208,4 +238,56 @@ func mergeAllVirtualHosts(vHostPortMap map[int][]route.VirtualHost) []route.Virt
 		}
 	}
 	return virtualHosts
+}
+
+// reverseArray returns its argument string array reversed
+func reverseArray(r []string) []string {
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return r
+}
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// getUniqueAndSharedDNSDomain computes the unique and shared DNS suffix from a FQDN service name and
+// the proxy's local domain with namespace. This is especially useful in Kubernetes environments, where
+// a two services can have same name in different namespaces (e.g., foo.ns1.svc.cluster.local,
+// foo.ns2.svc.cluster.local). In this case, if the proxy is in ns2.svc.cluster.local, then while
+// generating alt virtual hosts for service foo.ns1 for the sidecars in ns2 namespace, we should generate
+// foo.ns1, foo.ns1.svc, foo.ns1.svc.cluster.local and should not generate a virtual host called "foo" for
+// foo.ns1 service.
+// So given foo.ns1.svc.cluster.local and ns2.svc.cluster.local, this function will return
+// foo.ns1, and svc.cluster.local.
+// When given foo.ns2.svc.cluster.local and ns2.svc.cluster.local, this function will return
+// foo, ns2.svc.cluster.local.
+func getUniqueAndSharedDNSDomain(fqdnHostname, proxyDomain string) (string, string) {
+	// split them by the dot and reverse the arrays, so that we can
+	// start collecting the shared bits of DNS suffix.
+	// E.g., foo.ns1.svc.cluster.local -> local,cluster,svc,ns1,foo
+	//       ns2.svc.cluster.local -> local,cluster,svc,ns2
+	partsFQDN := reverseArray(strings.Split(fqdnHostname, "."))
+	partsProxyDomain := reverseArray(strings.Split(proxyDomain, "."))
+	var sharedSuffixesInReverse []string // pieces shared between proxy and svc. e.g., local,cluster,svc
+
+	for i := 0; i < min(len(partsFQDN), len(partsProxyDomain)); i++ {
+		if partsFQDN[i] == partsProxyDomain[i] {
+			sharedSuffixesInReverse = append(sharedSuffixesInReverse, partsFQDN[i])
+		} else {
+			break
+		}
+	}
+
+	if len(sharedSuffixesInReverse) == 0 {
+		return fqdnHostname, ""
+	}
+
+	// get the non shared pieces (ns1, foo) and reverse Array
+	uniqHostame := strings.Join(reverseArray(partsFQDN[len(sharedSuffixesInReverse):]), ".")
+	sharedSuffixes := strings.Join(reverseArray(sharedSuffixesInReverse), ".")
+	return uniqHostame, sharedSuffixes
 }
