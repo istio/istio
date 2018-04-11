7cb369bca5910eea1fa283dcd6a4f16ee1e72bc4
Pengyuan Bian <bianpengyuan@google.com>
2018-04-11 11:47:35 -0700

    let stackdriver adapter figure out project metadata itself.

--- mixer/adapter/stackdriver/helper/common_test.go
+++ mixer/adapter/stackdriver/helper/common_test.go
@@ -23,7 +23,6 @@ import (
 	gapiopts "google.golang.org/api/option"
 
 	"istio.io/istio/mixer/adapter/stackdriver/config"
-	"istio.io/istio/mixer/pkg/adapter"
 )
 
 func TestToOpts(t *testing.T) {
@@ -71,27 +70,106 @@ func TestToOpts(t *testing.T) {
 	}
 }
 
-func TestFillProjectID(t *testing.T) {
+func TestMetadata(t *testing.T) {
 	tests := []struct {
-		name       string
-		shouldFill shouldFillFn
-		projectID  projectIDFn
-		in         adapter.Config
-		res        string
+		name          string
+		shouldFill    shouldFillFn
+		projectIDFn   metadataFn
+		locationFn    metadataFn
+		clusterNameFn metadataFn
+		want          Metadata
 	}{
-		{"has project id", func(*config.Params) bool { return false }, func() (string, error) { return "", nil }, &config.Params{ProjectId: "id"}, "id"},
-		{"should not fill", func(*config.Params) bool { return false }, func() (string, error) { return "", nil }, &config.Params{}, ""},
-		{"fail to get project id", func(*config.Params) bool { return true }, func() (string, error) { return "", errors.New("error") }, &config.Params{}, ""},
-		{"get project id", func(*config.Params) bool { return true }, func() (string, error) { return "id", nil }, &config.Params{}, "id"},
+		{
+			"should not fill",
+			func() bool { return false },
+			func() (string, error) { return "pid", nil },
+			func() (string, error) { return "location", nil },
+			func() (string, error) { return "cluster", nil },
+			Metadata{ProjectID: "", Location: "", ClusterName: ""},
+		},
+		{
+			"should fill",
+			func() bool { return true },
+			func() (string, error) { return "pid", nil },
+			func() (string, error) { return "location", nil },
+			func() (string, error) { return "cluster", nil },
+			Metadata{ProjectID: "pid", Location: "location", ClusterName: "cluster"},
+		},
+		{
+			"project id error",
+			func() bool { return true },
+			func() (string, error) { return "", errors.New("error") },
+			func() (string, error) { return "location", nil },
+			func() (string, error) { return "cluster", nil },
+			Metadata{ProjectID: "", Location: "location", ClusterName: "cluster"},
+		},
+		{
+			"location error",
+			func() bool { return true },
+			func() (string, error) { return "pid", nil },
+			func() (string, error) { return "location", errors.New("error") },
+			func() (string, error) { return "cluster", nil },
+			Metadata{ProjectID: "pid", Location: "", ClusterName: "cluster"},
+		},
+		{
+			"cluster name error",
+			func() bool { return true },
+			func() (string, error) { return "pid", nil },
+			func() (string, error) { return "location", nil },
+			func() (string, error) { return "cluster", errors.New("error") },
+			Metadata{ProjectID: "pid", Location: "location", ClusterName: ""},
+		},
 	}
 
 	for idx, tt := range tests {
 		t.Run(fmt.Sprintf("[%d] %s", idx, tt.name), func(t *testing.T) {
-			fp := NewProjectIDFiller(tt.shouldFill, tt.projectID)
-			fp.FillProjectID(tt.in)
-			cfg := tt.in.(*config.Params)
-			if cfg.ProjectId != tt.res {
-				t.Errorf("project ID %v is wrong, expected project ID %v.", cfg.ProjectId, tt.res)
+			mg := NewMetadataGenerator(tt.shouldFill, tt.projectIDFn, tt.locationFn, tt.clusterNameFn)
+			got := mg.GenerateMetadata()
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Unexpected generated metadata: want %v got %v", tt.want, got)
+			}
+		})
+	}
+}
+
+func TestFillProjectMetadata(t *testing.T) {
+	tests := []struct {
+		name string
+		md   Metadata
+		in   map[string]string
+		want map[string]string
+	}{
+		{
+			"empty metadata",
+			Metadata{ProjectID: "", Location: "", ClusterName: ""},
+			map[string]string{"project": "pid", "location": "location", "cluster_name": ""},
+			map[string]string{"project": "pid", "location": "location", "cluster_name": ""},
+		},
+		{
+			"fill metadata",
+			Metadata{ProjectID: "pid", Location: "location", ClusterName: "cluster"},
+			map[string]string{"project": "", "location": "location", "cluster_name": ""},
+			map[string]string{"project": "pid", "location": "location", "cluster_name": "cluster"},
+		},
+		{
+			"do not override",
+			Metadata{ProjectID: "pid", Location: "location", ClusterName: "cluster"},
+			map[string]string{"project": "id", "location": "l", "cluster_name": "c"},
+			map[string]string{"project": "id", "location": "l", "cluster_name": "c"},
+		},
+		{
+			"unrelated field",
+			Metadata{ProjectID: "", Location: "", ClusterName: "cluster"},
+			map[string]string{"project": "pid", "location": "location", "cluster": ""},
+			map[string]string{"project": "pid", "location": "location", "cluster": ""},
+		},
+	}
+
+	for idx, tt := range tests {
+		t.Run(fmt.Sprintf("[%d] %s", idx, tt.name), func(t *testing.T) {
+			tt.md.FillProjectMetadata(tt.in)
+			if !reflect.DeepEqual(tt.in, tt.want) {
+				t.Errorf("Unexpected map value, want %v got %v", tt.want, tt.in)
 			}
 		})
 	}
