42c7f3b18fb3c97e73947a9d4372ff96d0d233d6
ronenschafferibm <ronen.schaffer@ibm.com>
2018-04-12 18:11:17 +0300

    Add fault injection to ExternalService test

--- tests/e2e/tests/pilot/routing_to_external_service_test.go
+++ tests/e2e/tests/pilot/routing_to_external_service_test.go
@@ -16,9 +16,114 @@ package pilot
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 	"testing"
+
+	"istio.io/istio/tests/util"
 )
+
+func TestExternalServiceRouteFaultInjection(t *testing.T) {
+	// egress rules are v1alpha1
+	if !tc.V1alpha1 {
+		t.Skipf("Skipping %s: v1alpha1=false", t.Name())
+	}
+
+	var cfgs *deployableConfig
+	applyRuleFunc := func(t *testing.T, yamlFiles []string) {
+		// Delete the previous rule if there was one. No delay on the teardown, since we're going to apply
+		// a delay when we push the new config.
+		if cfgs != nil {
+			if err := cfgs.TeardownNoDelay(); err != nil {
+				t.Fatal(err)
+			}
+			cfgs = nil
+		}
+
+		// Apply the new rule
+		cfgs = &deployableConfig{
+			Namespace:  tc.Kube.Namespace,
+			YamlFiles:  yamlFiles,
+			kubeconfig: tc.Kube.KubeConfig,
+		}
+		if err := cfgs.Setup(); err != nil {
+			t.Fatal(err)
+		}
+	}
+	// Upon function exit, delete the active rule.
+	defer func() {
+		if cfgs != nil {
+			_ = cfgs.Teardown()
+		}
+	}()
+
+	cases := []struct {
+		testName              string
+		externalServiceConfig string
+		routingTemplate       string
+		url                   string
+		routingParams         map[string]string
+	}{
+		// Fault Injection
+		{
+			testName:              "httpbin",
+			externalServiceConfig: "testdata/v1alpha3/external-service-httpbin.yaml",
+			routingTemplate:       "testdata/v1alpha3/rule-fault-injection-to-external-service.yaml.tmpl",
+			routingParams: map[string]string{
+				"host": "httpbin.org",
+			},
+			url: "http://httpbin.org",
+		},
+		{
+			testName:              "*.httpbin",
+			externalServiceConfig: "testdata/v1alpha3/external-service-httpbin.yaml",
+			routingTemplate:       "testdata/v1alpha3/rule-fault-injection-to-external-service.yaml.tmpl",
+			routingParams: map[string]string{
+				"host": "*.httpbin.org",
+			},
+			url: "http://www.httpbin.org",
+		},
+		{
+			testName:              "google",
+			externalServiceConfig: "testdata/v1alpha3/external-service-google.yaml",
+			routingTemplate:       "testdata/v1alpha3/rule-fault-injection-to-external-service.yaml.tmpl",
+			routingParams: map[string]string{
+				"host": "*google.com",
+			},
+			url: "http://www.google.com:443",
+		},
+	}
+
+	for _, c := range cases {
+		// Run case in a function to scope the configuration changes.
+		func() {
+			// Fill out the routing template
+			routingYaml, err := util.CreateAndFill(tc.Info.TempDir, c.routingTemplate, c.routingParams)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// Push all of the configs
+			applyRuleFunc(t, []string{c.externalServiceConfig, routingYaml})
+
+			runRetriableTest(t, c.testName, 3, func() error {
+				resp := ClientRequest("a", c.url, 1, "")
+
+				statusCode := ""
+				if len(resp.Code) > 0 {
+					statusCode = resp.Code[0]
+				}
+
+				expectedRespCode := 418
+				if strconv.Itoa(expectedRespCode) != statusCode {
+					return fmt.Errorf("fault injection verification failed: status code %s, expected status code %d",
+						statusCode, expectedRespCode)
+				}
+				return nil
+			})
+		}()
+	}
+}
 
 func TestExternalServiceAppendHeaders(t *testing.T) {
 	if !tc.V1alpha3 {
