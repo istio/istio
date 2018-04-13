dd5aa4046fd1acffbbc64c99219093bc66cc93d2
Ozben Evren <ozevren@users.noreply.github.com>
2018-04-13 15:02:13 -0700

    Introduce the custom resource synchronizer (#4645)
    Automatic merge from submit-queue.
    
    Introduce the custom resource synchronizer
    
    This PR introduces a custom resource synchronizer that can synchronize resource from a source resource/apigroup/version to a destination one.

---
+++ galley/pkg/resource/inject.go
@@ -0,0 +1,29 @@
+//  Copyright 2018 Istio Authors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package resource
+
+import (
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+var newDynamicClient = func(cfg *rest.Config) (dynamic.Interface, error) {
+	return dynamic.NewClient(cfg)
+}
+
+var newKubernetesClient = func(cfg *rest.Config) (kubernetes.Interface, error) {
+	return kubernetes.NewForConfig(cfg)
+}
