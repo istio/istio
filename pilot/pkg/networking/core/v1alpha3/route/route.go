31bfa70f411a66e5eba621be02b517cea60937eb
Zack <zbutcher@google.com>
2018-04-16 15:15:25 -0700

    fix linter complaint about format arg

--- pilot/pkg/networking/core/v1alpha3/route/route.go
+++ pilot/pkg/networking/core/v1alpha3/route/route.go
@@ -268,7 +268,7 @@ func translateRoute(in *networking.HTTPRoute,
 
 	// Match by the destination port specified in the match condition
 	if match != nil && match.Port != nil && match.Port.GetNumber() != uint32(port) {
-		return nil, fmt.Errorf("no port match: expected %d have %d", match.Port.GetName(), port)
+		return nil, fmt.Errorf("no port match: expected port %q to have number %d", match.Port.GetName(), port)
 	}
 
 	out := &route.Route{
