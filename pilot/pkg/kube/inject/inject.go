cb743bb4836bc65c8b4c1edfa9c815d6f957229d
Guang Ya Liu <gyliu513@gmail.com>
2018-04-19 02:00:46 +0800

    Removed duplicate core v1 import. (#4957)
    Automatic merge from submit-queue.
    
    Removed duplicate core v1 import.

--- pilot/pkg/kube/inject/inject.go
+++ pilot/pkg/kube/inject/inject.go
@@ -33,7 +33,6 @@ import (
 	"github.com/golang/protobuf/ptypes"
 	"github.com/golang/protobuf/ptypes/duration"
 	"k8s.io/api/batch/v2alpha1"
-	"k8s.io/api/core/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -93,17 +92,17 @@ const (
 // SidecarInjectionSpec collects all container types and volumes for
 // sidecar mesh injection
 type SidecarInjectionSpec struct {
-	InitContainers   []v1.Container            `yaml:"initContainers"`
-	Containers       []v1.Container            `yaml:"containers"`
-	Volumes          []v1.Volume               `yaml:"volumes"`
-	ImagePullSecrets []v1.LocalObjectReference `yaml:"imagePullSecrets"`
+	InitContainers   []corev1.Container            `yaml:"initContainers"`
+	Containers       []corev1.Container            `yaml:"containers"`
+	Volumes          []corev1.Volume               `yaml:"volumes"`
+	ImagePullSecrets []corev1.LocalObjectReference `yaml:"imagePullSecrets"`
 }
 
 // SidecarTemplateData is the data object to which the templated
 // version of `SidecarInjectionSpec` is applied.
 type SidecarTemplateData struct {
 	ObjectMeta  *metav1.ObjectMeta
-	Spec        *v1.PodSpec
+	Spec        *corev1.PodSpec
 	ProxyConfig *meshconfig.ProxyConfig
 	MeshConfig  *meshconfig.MeshConfig
 }
@@ -351,7 +350,7 @@ func validateAnnotations(metadata *metav1.ObjectMeta) error {
 	return validateAnnotation(annotations, sidecarAnnotationExcludeInboundPortsPolicyKey, ValidateExcludeInboundPorts)
 }
 
-func injectionData(sidecarTemplate, version string, spec *v1.PodSpec, metadata *metav1.ObjectMeta, proxyConfig *meshconfig.ProxyConfig, meshConfig *meshconfig.MeshConfig) (*SidecarInjectionSpec, string, error) { // nolint: lll
+func injectionData(sidecarTemplate, version string, spec *corev1.PodSpec, metadata *metav1.ObjectMeta, proxyConfig *meshconfig.ProxyConfig, meshConfig *meshconfig.MeshConfig) (*SidecarInjectionSpec, string, error) { // nolint: lll
 	if err := validateAnnotations(metadata); err != nil {
 		return nil, "", err
 	}
@@ -463,10 +462,10 @@ func intoObject(sidecarTemplate string, meshconfig *meshconfig.MeshConfig, in ru
 	out := in.DeepCopyObject()
 
 	var metadata *metav1.ObjectMeta
-	var podSpec *v1.PodSpec
+	var podSpec *corev1.PodSpec
 
 	// Handle Lists
-	if list, ok := out.(*v1.List); ok {
+	if list, ok := out.(*corev1.List); ok {
 		result := list
 
 		for i, item := range list.Items {
@@ -506,7 +505,7 @@ func intoObject(sidecarTemplate string, meshconfig *meshconfig.MeshConfig, in ru
 			templateValue = templateValue.Elem()
 		}
 		metadata = templateValue.FieldByName("ObjectMeta").Addr().Interface().(*metav1.ObjectMeta)
-		podSpec = templateValue.FieldByName("Spec").Addr().Interface().(*v1.PodSpec)
+		podSpec = templateValue.FieldByName("Spec").Addr().Interface().(*corev1.PodSpec)
 	}
 
 	// Skip injection when host networking is enabled. The problem is
