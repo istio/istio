8ac033e77a8bc0b3c8975aed21e20c9ff3926da5
Costin Manolache <costin@gmail.com>
2018-04-22 16:23:40 -0700

    Revert version matching fix

--- pilot/pkg/config/monitor/monitor.go
+++ pilot/pkg/config/monitor/monitor.go
@@ -89,7 +89,8 @@ func (m *Monitor) checkAndUpdate() {
 			m.createConfig(newConfig)
 			newIndex++
 		} else {
-			newConfig.ConfigMeta.ResourceVersion = oldConfig.ConfigMeta.ResourceVersion
+			// version may change without content changing
+			oldConfig.ConfigMeta.ResourceVersion = newConfig.ConfigMeta.ResourceVersion
 			if !reflect.DeepEqual(oldConfig, newConfig) {
 				m.updateConfig(newConfig)
 			}
