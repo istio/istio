cd1bdcd67251ddb9c4f446e4b072ecfe6e81f3fb
gargnupur <37600866+gargnupur@users.noreply.github.com>
2018-04-23 10:50:54 -0700

    Update setup_kubectl_host.sh

--- vagrant-kubernetes-istio/RunTestOnHost/setup_kubectl_host.sh
+++ vagrant-kubernetes-istio/RunTestOnHost/setup_kubectl_host.sh
@@ -1,6 +1,5 @@
 # Setting up kubectl on host to talk to kubernetest cluster on Vagrant VM.
 echo "your old ~/.kube/config file can be found at ~/.kube/config_old"
 cp ~/.kube/config ~/.kube/config_old
-vagrant ssh -c "cp /etc/kubeconfig.yml ~/.kube/config"
 vagrant ssh -c "cat ~/.kube/config" > ~/.kube/config
 sed -i '/Connection to 127.0.0.1 closed/d' ~/.kube/config
