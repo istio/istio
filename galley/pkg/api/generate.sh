7d0974a86b2a2d6d8aef737836de233d873b86bf
xiaolanz <xiaolan@google.com>
2018-04-24 16:34:09 -0700

    gen succeed

--- galley/pkg/api/generate.sh
+++ galley/pkg/api/generate.sh
@@ -1,44 +1,68 @@
 #!/bin/sh
 
+set -ex
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT="${DIR}/../../../../.."
 
 USE_GOGO=true
 
 pushd ${ROOT}
-if [ ${USE_GOGO} == true ]; then
 
     docker run \
-        --rm -v /Users/ozben/go/src/istio.io/istio/galley/pkg/api:/src/istio.io/istio/galley/pkg/api \
+        --rm -v ${ROOT}/istio.io/istio/galley/pkg/api:/src/istio.io/istio/galley/pkg/api \
         -w /src \
         gcr.io/istio-testing/protoc:2018-03-03 \
         -Iistio.io/istio/galley/pkg/api\
         --gogo_out=plugins=grpc,Mgogoproto/gogo.proto=github.com/gogo/protobuf/gogoproto,Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/descriptor.proto=github.com/gogo/protobuf/protoc-gen-gogo/descriptor,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types,Mgoogle/rpc/status.proto=github.com/gogo/googleapis/google/rpc,Mgoogle/rpc/code.proto=github.com/gogo/googleapis/google/rpc,Mgoogle/rpc/error_details.proto=github.com/gogo/googleapis/google/rpc,:. \
-        serviceconfig.proto
+        networking/v1alpha3/destination_rule.proto
 
     docker run \
-        --rm -v /Users/ozben/go/src/istio.io/istio/galley/pkg/api:/src/istio.io/istio/galley/pkg/api \
+        --rm -v ${ROOT}/istio.io/istio/galley/pkg/api:/src/istio.io/istio/galley/pkg/api \
+        -w /src \
+        gcr.io/istio-testing/protoc:2018-03-03 \
+        -Iistio.io/istio/galley/pkg/api\
+        --gogo_out=plugins=grpc,Mgogoproto/gogo.proto=github.com/gogo/protobuf/gogoproto,Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/descriptor.proto=github.com/gogo/protobuf/protoc-gen-gogo/descriptor,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types,Mgoogle/rpc/status.proto=github.com/gogo/googleapis/google/rpc,Mgoogle/rpc/code.proto=github.com/gogo/googleapis/google/rpc,Mgoogle/rpc/error_details.proto=github.com/gogo/googleapis/google/rpc,:. \
+        networking/v1alpha3/virtual_service.proto
+
+    docker run \
+        --rm -v ${ROOT}/istio.io/istio/galley/pkg/api:/src/istio.io/istio/galley/pkg/api \
+        -w /src \
+        gcr.io/istio-testing/protoc:2018-03-03 \
+        -Iistio.io/istio/galley/pkg/api\
+        --gogo_out=plugins=grpc,Mgogoproto/gogo.proto=github.com/gogo/protobuf/gogoproto,Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/descriptor.proto=github.com/gogo/protobuf/protoc-gen-gogo/descriptor,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types,Mgoogle/rpc/status.proto=github.com/gogo/googleapis/google/rpc,Mgoogle/rpc/code.proto=github.com/gogo/googleapis/google/rpc,Mgoogle/rpc/error_details.proto=github.com/gogo/googleapis/google/rpc,:. \
+        networking/v1alpha3/gateway.proto
+
+    docker run \
+        --rm -v ${ROOT}/istio.io/istio/galley/pkg/api:/src/istio.io/istio/galley/pkg/api \
+        -w /src \
+        gcr.io/istio-testing/protoc:2018-03-03 \
+        -Iistio.io/istio/galley/pkg/api\
+        --gogo_out=plugins=grpc,Mgogoproto/gogo.proto=github.com/gogo/protobuf/gogoproto,Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/descriptor.proto=github.com/gogo/protobuf/protoc-gen-gogo/descriptor,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types,Mgoogle/rpc/status.proto=github.com/gogo/googleapis/google/rpc,Mgoogle/rpc/code.proto=github.com/gogo/googleapis/google/rpc,Mgoogle/rpc/error_details.proto=github.com/gogo/googleapis/google/rpc,:. \
+        networking/v1alpha3/external_service.proto
+
+    docker run \
+        --rm -v ${ROOT}/istio.io/istio/galley/pkg/api:/src/istio.io/istio/galley/pkg/api \
         -w /src \
         gcr.io/istio-testing/protoc:2018-03-03 \
         -Iistio.io/istio/galley/pkg/api\
         --gogo_out=plugins=grpc,Mgogoproto/gogo.proto=github.com/gogo/protobuf/gogoproto,Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/descriptor.proto=github.com/gogo/protobuf/protoc-gen-gogo/descriptor,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types,Mgoogle/rpc/status.proto=github.com/gogo/googleapis/google/rpc,Mgoogle/rpc/code.proto=github.com/gogo/googleapis/google/rpc,Mgoogle/rpc/error_details.proto=github.com/gogo/googleapis/google/rpc,:. \
         distrib/mixerconfig.proto
-else
-    OUT="--go_out=."
 
-    API_PROTOS=$(find "istio.io/istio/galley/pkg/api" -type f -name '*.proto' -d 1 | sort)
-    protoc -I"istio.io/istio" ${OUT} ${API_PROTOS}
+    docker run \
+        --rm -v ${ROOT}/istio.io/istio/galley/pkg/api:/src/istio.io/istio/galley/pkg/api \
+        -w /src \
+        gcr.io/istio-testing/protoc:2018-03-03 \
+        -Iistio.io/istio/galley/pkg/api\
+        --gogo_out=plugins=grpc,Mgogoproto/gogo.proto=github.com/gogo/protobuf/gogoproto,Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/descriptor.proto=github.com/gogo/protobuf/protoc-gen-gogo/descriptor,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types,Mgoogle/rpc/status.proto=github.com/gogo/googleapis/google/rpc,Mgoogle/rpc/code.proto=github.com/gogo/googleapis/google/rpc,Mgoogle/rpc/error_details.proto=github.com/gogo/googleapis/google/rpc,:. \
+        service/dev/service_config.proto
 
-    DISTRIB_PROTOS=$(find "istio.io/istio/galley/pkg/api/distrib" -type f -name '*.proto' -d 1 | sort)
-    protoc -I"istio.io/istio" ${OUT} ${DISTRIB_PROTOS}
-fi
-
-    if [ -f "istio.io/istio/galley/pkg/api/serviceconfig.pb.go" ]; then
+    if [ -f "istio.io/istio/galley/pkg/api/service/dev/service_config.pb.go" ]; then
         sed -e 's/*google_protobuf.Struct/interface{}/g' \
             -e 's/ValueType_VALUE_TYPE_UNSPECIFIED/VALUE_TYPE_UNSPECIFIED/g' \
-            -e 's/istio_policy_v1beta1\.//g' istio.io/istio/galley/pkg/api/serviceconfig.pb.go \
-            | grep -v "google_protobuf" | grep -v "import istio_policy_v1beta1" >istio.io/istio/galley/pkg/api/serviceconfig_fixed.pb.go
-        rm -f istio.io/istio/galley/pkg/api/serviceconfig.pb.go
+            -e 's/istio_policy_v1beta1\.//g' istio.io/istio/galley/pkg/api/service/dev/service_config.pb.go \
+            | grep -v "google_protobuf" | grep -v "import istio_policy_v1beta1" >istio.io/istio/galley/pkg/api/service/dev/service_config_fixed.pb.go
+        rm -f istio.io/istio/galley/pkg/api/service/dev/serviceconfig.pb.go
     fi
 
     if [ -f "istio.io/istio/galley/pkg/api/distrib/mixerconfig.pb.go" ]; then
