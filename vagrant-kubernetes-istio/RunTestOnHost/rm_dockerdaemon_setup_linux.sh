f9e7caaa05ed1f5b22d4e08911b8c8fc4e83a474
JimmyCYJ <jimmychen.0102@gmail.com>
2018-04-23 17:55:54 -0700

    Revert unrelated files.

--- vagrant-kubernetes-istio/RunTestOnHost/rm_dockerdaemon_setup_linux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Cleanup Setup on host to talk to insecure registry on VM.
-sudo cp /lib/systemd/system/docker.service_old /lib/systemd/system/docker.service
-sudo rm -rf /lib/systemd/system/docker.service_old
-sudo systemctl daemon-reload
-sudo systemctl restart docker
