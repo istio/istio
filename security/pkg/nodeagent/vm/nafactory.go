0b8173730bfb349c1298f66a6582318a5074cbca
Jianfei Hu <jianfeih@google.com>
2018-04-25 14:50:10 -0700

    Add package level comments.

--- security/pkg/nodeagent/vm/nafactory.go
+++ security/pkg/nodeagent/vm/nafactory.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// package vm provides functionality when node agent is used on VM or bare metal machines.
 package vm
 
 import (
