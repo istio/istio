689d9eebfcc08ee2c41caa7afaaa19d479427f81
ozben <ozben@google.com>
2018-04-25 13:38:51 -0700

    Adding mixer test showcase (doesn't build)

--- tests/e2e/tests/showcase/pilot_showcase_test.go
+++ tests/e2e/tests/showcase/pilot_showcase_test.go
@@ -5,8 +5,8 @@ import (
 	"istio.io/istio/pkg/test"
 )
 
-func TestMTLSReachability(t *testing.T) {
-	test.Requires(t, pilot.Apps, environment.MTLS)
+func TestHTTPWithMTLS(t *testing.T) {
+	test.Requires(t, environment.Apps, environment.MTLS)
 
 	a := pilot.Apps.Get("a")
 	t := pilot.Apps.Get("t")
@@ -15,6 +15,34 @@ func TestMTLSReachability(t *testing.T) {
 
 	// Verify that the request was received by t
 	if err := t.Expect(reqInfo); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestHTTPWithMTLSAndMixer(t *testing.T) {
+	test.Requires(t, environment.Mixer, environment.Pilot, environment.Apps, environment.MTLS)
+
+	env := environment.Get()
+	env.Configure(cfg)
+
+	p := env.GetPilot()
+
+	m := env.GetMixer()
+
+	a := env.GetApp("a")
+	t := env.GetApp("t")
+
+	reqInfo := a.Call(t)
+
+	// Verify that the request was received by t
+	if err := t.Expect(reqInfo); err != nil {
+		t.Fatal(err)
+	}
+
+	err := m.Expect(`
+CHECK: "a" ....
+`)
+	if err != nil {
 		t.Fatal(err)
 	}
 }
