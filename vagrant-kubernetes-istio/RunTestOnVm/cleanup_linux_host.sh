1ddb8ebcfe55d5e631b0d28cb0c032221e5e2524
JimmyCYJ <jimmychen.0102@gmail.com>
2018-04-25 11:27:58 -0700

    Move files out of RunTestOnHost and remove RunTestOnVm.

--- vagrant-kubernetes-istio/RunTestOnVm/cleanup_linux_host.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Destroy vagrant vm
-vagrant destroy
-
-# Cleanup Setup on host to talk to insecure registry on VM.
-sudo cp /lib/systemd/system/docker.service_old /lib/systemd/system/docker.service
-sudo rm -rf /lib/systemd/system/docker.service_old
-sudo systemctl daemon-reload
-sudo systemctl restart docker
