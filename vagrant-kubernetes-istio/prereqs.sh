2728cab75d459d0abc47d18dc2b6fa886a2ea49b
Zijun Wang <zijun.wang@ibm.com>
2018-04-26 00:09:14 -0400

    update prereqs scripts for macos

--- vagrant-kubernetes-istio/prereqs.sh
+++ vagrant-kubernetes-istio/prereqs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 case "$OSTYPE" in
-  darwin*)  sh  mac_prereqs.sh;; 
+  darwin*)  sh  macos_prereqs.sh;; 
   linux*)   sh  linux_prereqs.sh;; 
   *)        echo "unsupported: $OSTYPE" ;;
 esac
