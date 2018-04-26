0e19c809def459d00b46250099ee1f8ef50e752d
Zijun Wang <zijun.wang@ibm.com>
2018-04-26 18:49:06 -0400

    update

--- vagrant-kubernetes-istio/setup_host.sh
+++ vagrant-kubernetes-istio/setup_host.sh
@@ -4,7 +4,7 @@ case "$OSTYPE" in
   darwin*)  sh  setup_kubectl_config_host.sh
 	;;
   linux*)   sh  setup_dockerdaemon_linux.sh
-	        sh  setup_kubectl_config_host.sh
+	    sh  setup_kubectl_config_host.sh
 	;;
   *)        echo "unsupported: $OSTYPE" 
 	;;
