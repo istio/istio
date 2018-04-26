26a01914ce8d0798cc6f4dfc8570469733611efa
gargnupur <37600866+gargnupur@users.noreply.github.com>
2018-04-26 15:18:59 -0700

    Update setup_kubelet_config_host.sh

--- vagrant-kubernetes-istio/setup_kubelet_config_host.sh
+++ vagrant-kubernetes-istio/setup_kubelet_config_host.sh
@@ -3,5 +3,12 @@ echo "your old ~/.kube/config file can be found at ~/.kube/config_old"
 ls ~/.kube/config_old
 if [ $? -ne 0 ]; then
     cp ~/.kube/config ~/.kube/config_old
+else
+    echo "There is an old ~/.kube/config_old file on your system."
+    read -p "If you believe it's outdated, we can update it[default: no]: " update
+    overrwriteExisting=${update:-"no"}
+    if [[ $overrwriteExisting = *"y"* ]] || [[ $overrwriteExisting = *"Y"* ]]; then
+        cp ~/.kube/config ~/.kube/config_old
+    fi
 fi
 vagrant ssh -c "cat ~/.kube/config" > ~/.kube/config
