e990d3f89158e3ec4c397d81a5b2fd6d007cb8d6
gargnupur <37600866+gargnupur@users.noreply.github.com>
2018-04-26 10:57:36 -0700

    Protect Copy to ~/.kube/config_old

--- vagrant-kubernetes-istio/setup_kubelet_config_host.sh
+++ vagrant-kubernetes-istio/setup_kubelet_config_host.sh
@@ -1,4 +1,7 @@
 # Setting up kubectl on host to talk to kubernetest cluster on Vagrant VM.
 echo "your old ~/.kube/config file can be found at ~/.kube/config_old"
-cp ~/.kube/config ~/.kube/config_old
+ls ~/.kube/config_old
+if [ $? -ne 0 ]; then
+    cp ~/.kube/config ~/.kube/config_old
+fi
 vagrant ssh -c "cat ~/.kube/config" > ~/.kube/config
