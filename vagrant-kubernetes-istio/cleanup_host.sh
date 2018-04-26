d71cd2660a11df1b5576026c554fc71b07100216
Zijun Wang <zijun.wang@ibm.com>
2018-04-26 18:30:41 -0400

    update scripts

--- vagrant-kubernetes-istio/cleanup_host.sh
+++ vagrant-kubernetes-istio/cleanup_host.sh
@@ -4,7 +4,7 @@ case "$OSTYPE" in
   darwin*)  sh  revert_kubectl_config_host.sh
 	;;
   linux*)   sh  revert_dockerdaemon_linux.sh
-	        sh  revert_kubectl_config_host.sh
+	    sh  revert_kubectl_config_host.sh
 	;;
   *)        echo "unsupported: $OSTYPE" 
 	;;
