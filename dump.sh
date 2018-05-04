43928992aaac7ae428a4b0de6a4c53e59a8e705f
Tyler Berry <tyler@berryac.com>
2018-05-04 12:32:53 -0700

    Also dumps previous logs

--- dump.sh
+++ dump.sh
@@ -28,8 +28,11 @@ dump_logs() {
       CONTAINERS=$(kubectl get --namespace="${namespace}" pod "${pod}" -o=jsonpath='{.spec.containers[*].name}')
       for container in ${CONTAINERS}; do
         mkdir -p "${LOG_DIR}/${namespace}/${pod}"
-        LOG_FILE="${LOG_DIR}/${namespace}/${pod}/${container}.log"
+        LOG_FILE_HEAD="${LOG_DIR}/${namespace}/${pod}/${container}"
+        LOG_FILE="${LOG_FILE_HEAD}.log"
+        LOG_FILE_PREVIOUS="${LOG_FILE_HEAD}_previous.log"
         kubectl logs --namespace="${namespace}" "${pod}" "${container}" > "${LOG_FILE}"
+        kubectl logs --namespace="${namespace}" --previous "${pod}" "${container}" > "${LOG_FILE_PREVIOUS}" 2> /dev/null
       done
     done
   done
