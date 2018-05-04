4eac2c3d96dcbe2b56006e6bdfa643d160695da8
Frank Budinsky <frankb@ca.ibm.com>
2018-05-04 14:06:23 -0400

    Gateway hosts are required (#470)

--- networking/v1alpha3/gateway.pb.go
+++ networking/v1alpha3/gateway.pb.go
@@ -91,11 +91,14 @@ func (Server_TLSOptions_TLSmode) EnumDescriptor() ([]byte, []int) {
 //           number: 9080
 //           name: http-wildcard
 //           protocol: HTTP
-//         # no hosts implies wildcard match
+//         hosts:
+//         - "*"
 //       - port:
-//           number: 2379 #to expose internal service via external port 2379
+//           number: 2379 # to expose internal service via external port 2379
 //           name: mongo
 //           protocol: MONGO
+//         hosts:
+//         - "*"
 //
 // The Gateway specification above describes the L4-L6 properties of a load
 // balancer. A `VirtualService` can then be bound to a gateway to control
@@ -212,6 +215,8 @@ func (m *Gateway) GetSelector() map[string]string {
 //           number: 80
 //           name: http2
 //           protocol: HTTP2
+//         hosts:
+//         - "*"
 //
 // Another example
 //
@@ -227,6 +232,8 @@ func (m *Gateway) GetSelector() map[string]string {
 //           number: 27018
 //           name: mongo
 //           protocol: MONGO
+//         hosts:
+//         - "*"
 //
 // The following is an example of TLS configuration for port 443
 //
@@ -242,6 +249,8 @@ func (m *Gateway) GetSelector() map[string]string {
 //           number: 443
 //           name: https
 //           protocol: HTTPS
+//         hosts:
+//         - "*"
 //         tls:
 //           mode: SIMPLE
 //           serverCertificate: /etc/certs/server.pem
@@ -251,7 +260,8 @@ type Server struct {
 	// REQUIRED: The Port on which the proxy should listen for incoming
 	// connections
 	Port *Port `protobuf:"bytes,1,opt,name=port" json:"port,omitempty"`
-	// A list of hosts exposed by this gateway. While typically applicable to
+	// REQUIRED. A list of hosts exposed by this gateway. At least one
+	// host is required. While typically applicable to
 	// HTTP services, it can also be used for TCP services using TLS with
 	// SNI. Standard DNS wildcard prefix syntax is permitted.
 	//
