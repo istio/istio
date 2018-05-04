1f5d44c6164b368f8b7ccc50776828bbaf476c91
lei-tang <32078630+lei-tang@users.noreply.github.com>
2018-05-03 18:53:45 -0700

    Add mixer tests for the Istio authn filter (#5267)
    Add the following Mixer tests for Istio authn filter
    
    - Test when requiring JWT for peer and binding to peer, the authn attributes in the actual check and report calls match those in the expected check call
    - Test when requiring JWT for peer and binding to origin, but no method specified in origin policy, the request will be rejected by Istio authn filter.
    - Test when when requiring JWT for origin and binding to origin, the authn attributes in the actual check and report calls match those in the expected check call.
    - Test when requiring JWT for origin and no binding to origin, the authn attributes in the actual check and report calls match those in the expected check call.
    - Test when the HTTP request is rejected by the Istio authn filter for peer JWT authentication, the response code and the response message is as expected.
    - Test when the HTTP request is rejected by the Istio authn filter for origin JWT authentication, the response code and the response message is as expected.
    - Test when the Istio authn filter requires mTLS for peer connection, the non mTLS connection is rejected and the response code and the response message are as expected.
    - Test when the Istio authn filter requires TLS for peer connection, the non TLS connection is rejected and the response code and the response message are as expected.

--- mixer/test/client/env/setup.go
+++ mixer/test/client/env/setup.go
@@ -34,14 +34,14 @@ type TestSetup struct {
 	mfConf *MixerFilterConf
 	ports  *Ports
 
-	envoy         *Envoy
-	mixer         *MixerServer
-	backend       *HTTPServer
-	testName      uint16
-	stress        bool
-	faultInject   bool
-	noMixer       bool
-	mfConfVersion string
+	envoy              *Envoy
+	mixer              *MixerServer
+	backend            *HTTPServer
+	testName           uint16
+	stress             bool
+	filtersBeforeMixer string
+	noMixer            bool
+	mfConfVersion      string
 
 	// EnvoyTemplate is the bootstrap config used by envoy.
 	EnvoyTemplate string
@@ -143,15 +143,15 @@ func (s *TestSetup) SetNoMixer(no bool) {
 	s.noMixer = no
 }
 
-// SetFaultInject set FaultInject flag
-func (s *TestSetup) SetFaultInject(f bool) {
-	s.faultInject = f
+// SetFiltersBeforeMixer set the configurations of the filters before the Mixer filter
+func (s *TestSetup) SetFiltersBeforeMixer(filters string) {
+	s.filtersBeforeMixer = filters
 }
 
 // SetUp setups Envoy, Mixer, and Backend server for test.
 func (s *TestSetup) SetUp() error {
 	var err error
-	s.envoy, err = s.NewEnvoy(s.stress, s.faultInject, s.mfConf, s.ports, s.epoch, s.mfConfVersion)
+	s.envoy, err = s.NewEnvoy(s.stress, s.filtersBeforeMixer, s.mfConf, s.ports, s.epoch, s.mfConfVersion)
 	if err != nil {
 		log.Printf("unable to create Envoy %v", err)
 	}
@@ -195,7 +195,7 @@ func (s *TestSetup) ReStartEnvoy() {
 	log.Printf("new allocated ports are %v:", s.ports)
 	var err error
 	s.epoch++
-	s.envoy, err = s.NewEnvoy(s.stress, s.faultInject, s.mfConf, s.ports, s.epoch, s.mfConfVersion)
+	s.envoy, err = s.NewEnvoy(s.stress, s.filtersBeforeMixer, s.mfConf, s.ports, s.epoch, s.mfConfVersion)
 	if err != nil {
 		s.t.Errorf("unable to re-start Envoy %v", err)
 	} else {
