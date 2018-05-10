267e7eadf95152aab220dea1bd40f9c3825f3b00
Martin Ostrowski <mostrowski@google.com>
2018-05-10 16:19:54 -0700

    Fix download path for kubectl on mac (#5553)

--- vagrant-kubernetes-istio/install_prereqs_macos.sh
+++ vagrant-kubernetes-istio/install_prereqs_macos.sh
@@ -84,7 +84,7 @@ kubectl --help > /dev/null
 if [ $? -ne 0 ]; 
 then
     echo "kubectl is not installed. Installing the lastest stable release..."
-    curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+    curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl
     chmod +x ./kubectl
     sudo mv ./kubectl /usr/local/bin/kubectl
     echo "Done."
