5c3f339a582c049711a5327358a8ef3740121d87
Mandar U Jog <mjog@google.com>
2018-05-10 11:46:43 -0700

    enable rbac test for auth/e2e_pilotv2

--- tests/istio.mk
+++ tests/istio.mk
@@ -180,7 +180,7 @@ test/local/noauth/e2e_pilotv2: generate_yaml-envoyv2_transition
 test/local/auth/e2e_pilotv2: generate_yaml-envoyv2_transition_auth
 	@mkdir -p ${OUT_DIR}/logs
 	set -o pipefail; ISTIO_PROXY_IMAGE=proxyv2 go test -v -timeout 20m ./tests/e2e/tests/pilot \
- 	--skip_cleanup --auth_enable=true --v1alpha3=true --egress=false --ingress=false --rbac_enable=false --v1alpha1=false --cluster_wide \
+	--skip_cleanup --auth_enable=true --v1alpha3=true --egress=false --ingress=false --rbac_enable=true --v1alpha1=false --cluster_wide \
 	${E2E_ARGS} ${T} ${EXTRA_E2E_ARGS} \
 		${CAPTURE_LOG}
 	# Run the pilot controller tests
