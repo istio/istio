cb0ed01d854c2aef909df1ab08e240e68994219f
Sebastien Vas <sebastienvas@users.noreply.github.com>
2018-05-10 11:57:04 -0700

    Separate debian linux distribution (#5515)
    * Separate debian linux distribution
    
    * add default case

--- vagrant-kubernetes-istio/install_prereqs.sh
+++ vagrant-kubernetes-istio/install_prereqs.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-case "$OSTYPE" in
-  darwin*)  sh  install_prereqs_macos.sh;; 
-  linux*)   sh  install_prereqs_linux.sh;; 
-  *)        echo "unsupported: $OSTYPE" ;;
+case "${OSTYPE}" in
+  darwin*) sh install_prereqs_macos.sh;;
+  linux*)
+    DISTRO="$(lsb_release -i -s)"
+    case "${DISTRO}" in
+      Debian|Ubuntu)
+        sh install_prereqs_debian.sh;;
+      *) echo "unsupported distro: ${DISTRO}" ;;
+    esac;;
+  *) echo "unsupported: ${OSTYPE}" ;;
 esac
