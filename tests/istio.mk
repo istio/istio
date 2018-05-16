ad371662c52e062947bff6f48606fa46359c3776
Vadim Eisenberg <vadime@il.ibm.com>
2018-05-16 10:47:10 +0300

    remove generate_yaml-envoyv2_transition_auth, use generate_yaml-envoyv2_transition (#5602)

--- tests/istio.mk
+++ tests/istio.mk
@@ -174,7 +174,7 @@ test/local/noauth/e2e_pilotv2: out_dir generate_yaml-envoyv2_transition
 	set -o pipefail; go test -v -timeout 20m ./tests/e2e/tests/controller ${CAPTURE_LOG}
 
 # v1alpha3+envoyv2 test with MTLS
-test/local/auth/e2e_pilotv2: out_dir generate_yaml-envoyv2_transition_auth
+test/local/auth/e2e_pilotv2: out_dir generate_yaml-envoyv2_transition
 	set -o pipefail; ISTIO_PROXY_IMAGE=proxyv2 go test -v -timeout 20m ./tests/e2e/tests/pilot \
 		--skip_cleanup --auth_enable=true --v1alpha3=true --egress=false --ingress=false --rbac_enable=true --v1alpha1=false --cluster_wide \
 		${E2E_ARGS} ${T} ${EXTRA_E2E_ARGS} ${CAPTURE_LOG}
