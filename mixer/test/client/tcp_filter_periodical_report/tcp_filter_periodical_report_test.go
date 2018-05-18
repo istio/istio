1c34ff51ce517fdb29f33f258efa65dd9bee2422
JimmyCYJ <jimmychen.0102@gmail.com>
2018-05-17 19:10:29 -0700

    Fix lint errors.

--- mixer/test/client/tcp_filter_periodical_report/tcp_filter_periodical_report_test.go
+++ mixer/test/client/tcp_filter_periodical_report/tcp_filter_periodical_report_test.go
@@ -82,7 +82,7 @@ var expectedStats = map[string]int{
 
 func TestTCPMixerFilterPeriodicalReport(t *testing.T) {
 	// https://github.com/istio/istio/issues/5696 skip all TCP tests.
-	return
+	t.Skip("issue https://github.com/istio/istio/issues/5696")
 
 	s := env.NewTestSetup(env.TCPMixerFilterPeriodicalReportTest, t)
 	env.SetTCPReportInterval(s.MfConfig().TCPServerConf, 2)
