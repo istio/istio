e2f6a14b3f261e194cc2e076812200329eb178fc
Ed Snible <snible@us.ibm.com>
2018-05-22 13:43:40 -0400

    Send all output through buffers

--- istioctl/cmd/istioctl/istioctl_test.go
+++ istioctl/cmd/istioctl/istioctl_test.go
@@ -17,14 +17,10 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"io"
-	"os"
 	"regexp"
 	"sort"
 	"strings"
 	"testing"
-
-	multierror "github.com/hashicorp/go-multierror"
 
 	networking "istio.io/api/networking/v1alpha3"
 	"istio.io/api/routing/v1alpha1"
@@ -36,6 +32,17 @@ import (
 // a stable List() which helps with testing `istioctl get` output.
 type sortedConfigStore struct {
 	store model.ConfigStore
+}
+
+type testCase struct {
+	configs []model.Config
+	args    []string
+
+	// Typically use one or the other
+	expectedOutput string         // Expected constant output
+	expectedRegexp *regexp.Regexp // Expected regexp output
+
+	wantException bool
 }
 
 var (
@@ -177,19 +184,13 @@ var (
 )
 
 func TestGet(t *testing.T) {
-	cases := []struct {
-		configs       []model.Config
-		args          []string
-		wantOutput    string
-		wantError     *regexp.Regexp
-		wantException bool
-	}{
+	cases := []testCase{
 		{ // case 0
 			[]model.Config{},
 			strings.Split("get routerules", " "),
 			`No resources found.
 `,
-			regexp.MustCompile("^$"),
+			nil,
 			false,
 		},
 		{ // case 1
@@ -201,7 +202,7 @@ b         RouteRule.config.v1alpha2   default
 c         RouteRule.config.v1alpha2   istio-system
 d         RouteRule.config.v1alpha2   default
 `,
-			regexp.MustCompile("^$"),
+			nil,
 			false,
 		},
 		{ // case 2
@@ -210,7 +211,7 @@ d         RouteRule.config.v1alpha2   default
 			`NAME               KIND                          NAMESPACE
 bookinfo-gateway   Gateway.networking.v1alpha3   default
 `,
-			regexp.MustCompile("^$"),
+			nil,
 			false,
 		},
 		{ // case 3
@@ -219,7 +220,7 @@ bookinfo-gateway   Gateway.networking.v1alpha3   default
 			`NAME       KIND                                 NAMESPACE
 bookinfo   VirtualService.networking.v1alpha3   default
 `,
-			regexp.MustCompile("^$"),
+			nil,
 			false,
 		},
 		{
@@ -232,125 +233,35 @@ bookinfo   VirtualService.networking.v1alpha3   default
 	}
 
 	for i, c := range cases {
-		// Override the client factory used by main.go
-		clientFactory = mockClientFactoryGenerator(c.configs)
-
-		// run getCmd capturing output
-		var out bytes.Buffer
-		rootCmd.SetOutput(&out)
-		rootCmd.SetArgs(c.args)
-
-		file = "" // Clear, because we re-use
-
-		stdOutput, fErr := captureStdout(
-			func() error {
-				rootCmd.SetArgs(c.args)
-				err := rootCmd.Execute()
-				if err != nil {
-					return multierror.Prefix(err, fmt.Sprintf("Could not run command %v", c.args))
-				}
-				return nil
-			})
-		errOutput := out.String()
-
-		if c.wantOutput != stdOutput {
-			t.Errorf("Stdout didn't match for case %d \"istioctl get %v\": Expected %q, got %q.  Stderr was %q",
-				i, strings.Join(c.args, " "),
-				c.wantOutput, stdOutput, errOutput)
-		}
-
-		if !c.wantError.MatchString(errOutput) {
-			t.Errorf("Stderr didn't match for case %d %v: Expected %v, got %q", i, c.args, c.wantError, errOutput)
-		}
-
-		if c.wantException {
-			if fErr == nil {
-				t.Errorf("Wanted an exception for case %d %v, didn't get one, output was %q", i, c.args, stdOutput)
-			}
-		} else {
-			if fErr != nil {
-				t.Errorf("Unwanted exception for case %d %v: %v", i, c.args, fErr)
-			}
-		}
+		verifyOutput(t, fmt.Sprintf("TestGet case %d", i), c)
 	}
 }
 
 func TestCreate(t *testing.T) {
-	cases := []struct {
-		configs       []model.Config
-		args          []string
-		wantOutput    *regexp.Regexp
-		wantError     *regexp.Regexp
-		wantException bool
-	}{
+	cases := []testCase{
 		{ // case 0 -- invalid doesn't provide -f filename
 			[]model.Config{},
 			strings.Split("create routerules", " "),
-			regexp.MustCompile("^$"),
+			"",
 			regexp.MustCompile("^Usage:.*"),
 			true,
 		},
 		{ // case 1
 			[]model.Config{},
 			strings.Split("create -f convert/testdata/v1alpha1/route-rule-80-20.yaml", " "), // todo add -f
+			"",
 			regexp.MustCompile("^Created config route-rule/default/route-rule-80-20.*"),
-			regexp.MustCompile("^$"),
 			false,
 		},
 	}
 
 	for i, c := range cases {
-		// Override the client factory used by main.go
-		clientFactory = mockClientFactoryGenerator(c.configs)
-
-		// run postCmd capturing output
-		var out bytes.Buffer
-		rootCmd.SetOutput(&out)
-		rootCmd.SetArgs(c.args)
-
-		file = "" // Clear, because we re-use
-
-		stdOutput, fErr := captureStdout(
-			func() error {
-				rootCmd.SetArgs(c.args)
-				err := rootCmd.Execute()
-				if err != nil {
-					return multierror.Prefix(err, fmt.Sprintf("Could not run command %v", c.args))
-				}
-				return nil
-			})
-		errOutput := out.String()
-
-		if !c.wantOutput.MatchString(stdOutput) {
-			t.Errorf("Stdout didn't match for create case %d \"istioctl create %v\": Expected %v, got %q.  Stderr was %q",
-				i, strings.Join(c.args, " "),
-				c.wantOutput, stdOutput, errOutput)
-		}
-
-		if !c.wantError.MatchString(errOutput) {
-			t.Errorf("Stderr didn't match for create case %d %v: Expected %v, got %q", i, c.args, c.wantError, errOutput)
-		}
-
-		if c.wantException {
-			if fErr == nil {
-				t.Errorf("Wanted an exception for case %d %v, didn't get one, output was %q", i, c.args, stdOutput)
-			}
-		} else {
-			if fErr != nil {
-				t.Errorf("Unwanted exception for case %d %v: %v", i, c.args, fErr)
-			}
-		}
+		verifyOutput(t, fmt.Sprintf("TestGet case %d", i), c)
 	}
 }
 
 func TestReplace(t *testing.T) {
-	cases := []struct {
-		configs       []model.Config
-		args          []string
-		wantOutput    string
-		wantError     *regexp.Regexp
-		wantException bool
-	}{
+	cases := []testCase{
 		{ // case 0 -- invalid doesn't provide -f
 			[]model.Config{},
 			strings.Split("replace routerules", " "),
@@ -361,58 +272,12 @@ func TestReplace(t *testing.T) {
 	}
 
 	for i, c := range cases {
-		// Override the client factory used by main.go
-		clientFactory = mockClientFactoryGenerator(c.configs)
-
-		// run putCmd capturing output
-		var out bytes.Buffer
-		rootCmd.SetOutput(&out)
-		rootCmd.SetArgs(c.args)
-		// fErr := rootCmd.Execute()
-
-		file = "" // Clear, because we re-use
-
-		stdOutput, fErr := captureStdout(
-			func() error {
-				rootCmd.SetArgs(c.args)
-				err := rootCmd.Execute()
-				if err != nil {
-					return multierror.Prefix(err, fmt.Sprintf("Could not run command %v", c.args))
-				}
-				return nil
-			})
-		errOutput := out.String()
-
-		if c.wantOutput != stdOutput {
-			t.Errorf("Stdout didn't match for case %d \"istioctl replace %v\": Expected %q, got %q.  Stderr was %q",
-				i, strings.Join(c.args, " "),
-				c.wantOutput, stdOutput, errOutput)
-		}
-
-		if !c.wantError.MatchString(errOutput) {
-			t.Errorf("Stderr didn't match for case %d %v: Expected %v, got %q", i, c.args, c.wantError, errOutput)
-		}
-
-		if c.wantException {
-			if fErr == nil {
-				t.Errorf("Wanted an exception for case %d %v, didn't get one, output was %q", i, c.args, stdOutput)
-			}
-		} else {
-			if fErr != nil {
-				t.Errorf("Unwanted exception for case %d %v: %v", i, c.args, fErr)
-			}
-		}
+		verifyOutput(t, fmt.Sprintf("TestGet case %d", i), c)
 	}
 }
 
 func TestDelete(t *testing.T) {
-	cases := []struct {
-		configs       []model.Config
-		args          []string
-		wantOutput    string
-		wantError     *regexp.Regexp
-		wantException bool
-	}{
+	cases := []testCase{
 		{ // case 0
 			[]model.Config{},
 			strings.Split("delete routerule unknown", " "),
@@ -425,7 +290,7 @@ func TestDelete(t *testing.T) {
 			strings.Split("delete routerule a", " "),
 			`Deleted config: routerule a
 `,
-			regexp.MustCompile("^$"),
+			nil,
 			false,
 		},
 		{ // case 1
@@ -434,7 +299,7 @@ func TestDelete(t *testing.T) {
 			`Deleted config: routerule a
 Deleted config: routerule b
 `,
-			regexp.MustCompile("^$"),
+			nil,
 			false,
 		},
 		{ // case 3 - delete by filename of istio config which doesn't exist
@@ -447,47 +312,7 @@ Deleted config: routerule b
 	}
 
 	for i, c := range cases {
-		// Override the client factory used by main.go
-		clientFactory = mockClientFactoryGenerator(c.configs)
-
-		// run deleteCmd capturing output
-		var out bytes.Buffer
-		rootCmd.SetOutput(&out)
-		rootCmd.SetArgs(c.args)
-		// fErr := rootCmd.Execute()
-
-		file = "" // Clear, because we re-use
-
-		stdOutput, fErr := captureStdout(
-			func() error {
-				rootCmd.SetArgs(c.args)
-				err := rootCmd.Execute()
-				if err != nil {
-					return multierror.Prefix(err, fmt.Sprintf("Could not run command %v", c.args))
-				}
-				return nil
-			})
-		errOutput := out.String()
-
-		if c.wantOutput != stdOutput {
-			t.Errorf("Stdout didn't match for delete case %d \"istioctl delete %v\": Expected %q, got %q.  Stderr was %q",
-				i, strings.Join(c.args, " "),
-				c.wantOutput, stdOutput, errOutput)
-		}
-
-		if !c.wantError.MatchString(errOutput) {
-			t.Errorf("Stderr didn't match for delete case %d %v: Expected %v, got %q", i, c.args, c.wantError, errOutput)
-		}
-
-		if c.wantException {
-			if fErr == nil {
-				t.Errorf("Wanted an exception for case %d %v, didn't get one, output was %q", i, c.args, stdOutput)
-			}
-		} else {
-			if fErr != nil {
-				t.Errorf("Unwanted exception for case %d %v: %v", i, c.args, fErr)
-			}
-		}
+		verifyOutput(t, fmt.Sprintf("TestGet case %d", i), c)
 	}
 }
 
@@ -513,28 +338,6 @@ func mockClientFactoryGenerator(configs []model.Config) func() (model.ConfigStor
 	}
 
 	return outFactory
-}
-
-// captureStdout invokes f capturing the output sent to stderr and stdout
-func captureStdout(f func() error) (string, error) {
-	origStdout := os.Stdout
-	rOut, wOut, _ := os.Pipe()
-	os.Stdout = wOut
-
-	errF := f()
-
-	outChannel := make(chan string)
-	go func() {
-		var buf bytes.Buffer
-		io.Copy(&buf, rOut)
-		outChannel <- buf.String()
-	}()
-
-	wOut.Close()
-	os.Stdout = origStdout
-	capturedOutput := <-outChannel
-
-	return capturedOutput, errF
 }
 
 func (cs sortedConfigStore) Create(config model.Config) (string, error) {
@@ -574,4 +377,40 @@ func (cs sortedConfigStore) List(typ, namespace string) ([]model.Config, error) 
 	})
 
 	return out, nil
+}
+
+func verifyOutput(t *testing.T, label string, c testCase) {
+	// Override the client factory used by main.go
+	clientFactory = mockClientFactoryGenerator(c.configs)
+
+	var out bytes.Buffer
+	rootCmd.SetOutput(&out)
+	rootCmd.SetArgs(c.args)
+
+	file = "" // Clear, because we re-use
+
+	fErr := rootCmd.Execute()
+	output := out.String()
+
+	if c.expectedOutput != "" && c.expectedOutput != output {
+		t.Errorf("Unexpected output for %s %s: Expected\n%q,got\n%q",
+			label, strings.Join(c.args, " "),
+			c.expectedOutput, output)
+	}
+
+	if c.expectedRegexp != nil && !c.expectedRegexp.MatchString(output) {
+		t.Errorf("Output didn't match for %s %s: Expected\n%v,got\n%q",
+			label, strings.Join(c.args, " "),
+			c.expectedRegexp, output)
+	}
+
+	if c.wantException {
+		if fErr == nil {
+			t.Errorf("Wanted an exception for %s %s, didn't get one, output was %q", label, c.args, output)
+		}
+	} else {
+		if fErr != nil {
+			t.Errorf("Unwanted exception for %s %s: %v", label, c.args, fErr)
+		}
+	}
 }
