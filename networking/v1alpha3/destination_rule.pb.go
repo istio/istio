e1e4b2ec3c66fe5410df26c254df2810a83543b6
Andy Lai <31747472+hklai@users.noreply.github.com>
2018-05-22 16:34:53 -0700

    Update destination_rule.pb.go

--- networking/v1alpha3/destination_rule.pb.go
+++ networking/v1alpha3/destination_rule.pb.go
@@ -882,17 +882,50 @@ func (m *OutlierDetection_HTTPSettings) GetMaxEjectionPercent() int32 {
 	return 0
 }
 
-// ```yaml
-// apiVersion: networking.istio.io/v1alpha3
-// kind: DestinationRule
-// metadata:
-//   name: ratings-istio-mtls
-// spec:
-//   host: ratings.prod.svc.cluster.local
-//   trafficPolicy:
-//     tls:
-//       mode: ISTIO_MUTUAL
-// ```
+// SSL/TLS related settings for upstream connections. See Envoy's [TLS
+// context](https://www.envoyproxy.io/docs/envoy/latest/api-v1/cluster_manager/cluster_ssl.html#config-cluster-manager-cluster-ssl)
+// for more details. These settings are common to both HTTP and TCP upstreams.
+//
+// For example, the following rule configures a client to use mutual TLS
+// for connections to upstream database cluster.
+//
+//     apiVersion: networking.istio.io/v1alpha3
+//     kind: DestinationRule
+//     metadata:
+//       name: db-mtls
+//     spec:
+//       host: mydbserver.prod.svc.cluster.local
+//       trafficPolicy:
+//         tls:
+//           mode: MUTUAL
+//           clientCertificate: /etc/certs/myclientcert.pem
+//           privateKey: /etc/certs/client_private_key.pem
+//           caCertificates: /etc/certs/rootcacerts.pem
+//
+// The following rule configures a client to use TLS when talking to a
+// foreign service whose domain matches *.foo.com.
+//
+//     apiVersion: networking.istio.io/v1alpha3
+//     kind: DestinationRule
+//     metadata:
+//       name: tls-foo
+//     spec:
+//       host: "*.foo.com"
+//       trafficPolicy:
+//         tls:
+//           mode: SIMPLE
+//
+// The following rule configures a client to use Istio mutual TLS when talking
+// to rating services.
+//     apiVersion: networking.istio.io/v1alpha3
+//     kind: DestinationRule
+//     metadata:
+//       name: ratings-istio-mtls
+//     spec:
+//       host: ratings.prod.svc.cluster.local
+//       trafficPolicy:
+//         tls:
+//           mode: ISTIO_MUTUAL
 type TLSSettings struct {
 	// REQUIRED: Indicates whether connections to this port should be secured
 	// using TLS. The value of this field determines how TLS is enforced.
