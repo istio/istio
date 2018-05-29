67a7674f0c67e11b1660898e9084ae5a36f75f40
Tyler Berry <tyler@berryac.com>
2018-05-29 10:07:25 -0700

    Fix check_license.sh

--- bin/check_license.sh
+++ bin/check_license.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
-SCRIPTPATH=$( cd "$(dirname "$0")" ; pwd -P )
-ROOTDIR=$SCRIPTPATH/..
-cd $ROOTDIR
+
+SCRIPTPATH=$(cd "$(dirname "$0")" && pwd)
+ROOTDIR=$(cd "$(dirname "${SCRIPTPATH}")" && pwd)
+pushd "$ROOTDIR"
 
 ret=0
-for fn in $(find ${ROOTDIR} -name '*.go' | grep -v vendor | grep -v testdata); do
-  if [[ $fn == *.pb.go ]];then
+for fn in $(find "${ROOTDIR}" -name '*.go' | grep -v vendor | grep -v testdata); do
+  if [[ "$fn" == *.pb.go ]];then
     continue
   fi
 
-  head -20 $fn | grep "auto\-generated" > /dev/null
-  if [[ $? -eq 0 ]]; then
-          continue
+  if head -20 "$fn" | grep "auto\-generated" > /dev/null; then
+    continue
   fi
 
-  head -20 $fn | grep "DO NOT EDIT" > /dev/null
-  if [[ $? -eq 0 ]]; then
-          continue
+  if head -20 "$fn" | grep "DO NOT EDIT" > /dev/null; then
+    continue
   fi
 
-  head -20 $fn | grep "Apache License, Version 2" > /dev/null
-  if [[ $? -ne 0 ]]; then
+  if ! head -20 "$fn" | grep "Apache License, Version 2" > /dev/null; then
     echo "${fn} missing license"
-    ret=$(($ret+1))
+    ret=$((ret+1))
   fi
 
-  head -20 $fn | grep Copyright > /dev/null
-  if [[ $? -ne 0 ]]; then
+  if ! head -20 "$fn" | grep Copyright > /dev/null; then
     echo "${fn} missing Copyright"
-    ret=$(($ret+1))
+    ret=$((ret+1))
   fi
 done
 
-exit $ret
+popd
+exit "$ret"
