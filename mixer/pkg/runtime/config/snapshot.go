7828345df4a23fe2c0a00f75607815869ac56334
Sunny Gupta <guptasu@google.com>
2018-05-31 11:10:53 -0700

    Snapshot contract between the config builder and the runtime. (#5917)
    * Contract between the config builder and the runtime.
    
    Add new configuration data to the snapshot. Ephemeral.go builds the snapshot. Snapshot is used by the runtime dispatch table.
    
    * address Oz's comments.
    
    * Renames old style config as legacy.

--- mixer/pkg/runtime/config/snapshot.go
+++ mixer/pkg/runtime/config/snapshot.go
@@ -16,9 +16,12 @@ package config
 
 import (
 	"github.com/gogo/protobuf/proto"
+	"github.com/gogo/protobuf/protoc-gen-gogo/descriptor"
 
+	"istio.io/api/policy/v1beta1"
 	"istio.io/istio/mixer/pkg/adapter"
 	"istio.io/istio/mixer/pkg/lang/ast"
+	"istio.io/istio/mixer/pkg/protobuf/yaml/dynamic"
 	"istio.io/istio/mixer/pkg/template"
 )
 
@@ -35,14 +38,18 @@ type (
 		// Config store based information
 		Attributes ast.AttributeDescriptorFinder
 
+		HandlersLegacy  map[string]*HandlerLegacy
+		InstancesLegacy map[string]*InstanceLegacy
+		RulesLegacy     []*RuleLegacy
+
+		//  TemplateMetadatas contains template descriptors loaded from the store
+		TemplateMetadatas map[string]*TemplateMetadata
+		//  AdapterMetadatas contains adapter metadata loaded from the store
+		AdapterMetadatas map[string]*AdapterMetadata
+
 		Handlers  map[string]*Handler
 		Instances map[string]*Instance
 		Rules     []*Rule
-
-		//  Templates2 contains template descriptors loaded from the store
-		Templates2 map[string]*TemplateMetadata
-		//  Adapters2 contains adapter metadata loaded from the store
-		Adapters2 map[string]*AdapterMetadata
 
 		// Perf Counters relevant to configuration.
 		Counters Counters
@@ -50,6 +57,19 @@ type (
 
 	// Handler configuration. Fully resolved.
 	Handler struct {
+		Name string
+
+		Adapter *Adapter
+
+		// parameters used to construct the Handler.
+		Params []byte
+
+		// Connection information for the handler.
+		Connection *v1beta1.Connection
+	}
+
+	// HandlerLegacy configuration. Fully resolved.
+	HandlerLegacy struct {
 
 		// Name of the Handler. Fully qualified.
 		Name string
@@ -63,6 +83,16 @@ type (
 
 	// Instance configuration. Fully resolved.
 	Instance struct {
+		Name string
+
+		Template *Template
+
+		// Encoder to create request instance bytes from attributes
+		Encoder *dynamic.Encoder
+	}
+
+	// InstanceLegacy configuration. Fully resolved.
+	InstanceLegacy struct {
 		// Name of the instance. Fully qualified.
 		Name string
 
@@ -75,6 +105,15 @@ type (
 
 	// Rule configuration. Fully resolved.
 	Rule struct {
+		Name         string
+		Namespace    string
+		Match        string
+		Actions      []*Action
+		ResourceType ResourceType
+	}
+
+	// RuleLegacy configuration. Fully resolved.
+	RuleLegacy struct {
 		// Name of the rule
 		Name string
 
@@ -84,7 +123,7 @@ type (
 		// Match condition
 		Match string
 
-		Actions []*Action
+		Actions []*ActionLegacy
 
 		ResourceType ResourceType
 	}
@@ -93,17 +132,62 @@ type (
 	Action struct {
 		// Handler that this action is resolved to.
 		Handler *Handler
-
 		// Instances that should be generated as part of invoking action.
 		Instances []*Instance
+	}
+
+	// ActionLegacy configuration. Fully resolved.
+	ActionLegacy struct {
+		// Handler that this action is resolved to.
+		Handler *HandlerLegacy
+
+		// Instances that should be generated as part of invoking action.
+		Instances []*InstanceLegacy
+	}
+
+	// Template contains info about a template
+	Template struct {
+		// Name of the template.
+		//
+		// Note this is the template's resource name and not the template's internal name that adapter developer
+		// uses to implement adapter service.
+		Name string
+
+		// InternalPackageDerivedName is the name of the template from adapter developer point of view.
+		// The service and functions implemented by the adapter is based on this name
+		// NOTE: This name derived from template proto package and not the resource name.
+		InternalPackageDerivedName string
+
+		// Template's file descriptor set.
+		FileDescSet *descriptor.FileDescriptorSet
+
+		// package name of the `Template` message
+		PackageName string
+	}
+
+	// Adapter contains info about an adapter
+	Adapter struct {
+		Name string
+
+		// Adapter's file descriptor set.
+		ConfigDescSet *descriptor.FileDescriptorSet
+
+		// package name of the `Params` message
+		PackageName string
+
+		SupportedTemplates []Template
+
+		SessionBased bool
+
+		Description string
 	}
 )
 
 // Empty returns a new, empty configuration snapshot.
 func Empty() *Snapshot {
 	return &Snapshot{
-		ID:       -1,
-		Rules:    []*Rule{},
-		Counters: newCounters(-1),
+		ID:          -1,
+		RulesLegacy: []*RuleLegacy{},
+		Counters:    newCounters(-1),
 	}
 }
