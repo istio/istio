427490754948127db3e6e0988943eebff392f9f7
Gary Brown <gary@brownuk.com>
2018-06-01 15:52:08 +0100

    Split test into separate for checking tracing enabled and that mixer is traced

--- tests/e2e/tests/pilot/a_tracing_test.go
+++ tests/e2e/tests/pilot/a_tracing_test.go
@@ -26,67 +26,103 @@ import (
 
 const (
 	traceHeader         = "X-Client-Trace-Id"
-	numTraces           = 5
 	mixerCheckOperation = "\"name\":\"/istio.mixer.v1.mixer/check\""
 	checkOperation      = "\"name\":\"check\""
 	traceIDField        = "\"traceId\""
 )
 
-func TestTracing(t *testing.T) {
-	for i := 0; i < numTraces; i++ {
-		testName := fmt.Sprintf("index_%d", i)
-		traceSent := false
-		var id string
+func TestTracingMixer(t *testing.T) {
+	traceSent := false
+	var id string
 
-		runRetriableTest(t, primaryCluster, testName, defaultRetryBudget, func() error {
-			if !traceSent {
-				// Send a request with a trace header.
-				id = uuid.NewV4().String()
-				response := ClientRequest(primaryCluster, "a", "http://b", 1,
-					fmt.Sprintf("-key %v -val %v", traceHeader, id))
-				if !response.IsHTTPOk() {
-					// Keep retrying until we successfully send a trace request.
-					return errAgain
-				}
-
-				traceSent = true
-			}
-
-			// Check the tracing server to verify the trace was received.
-			response := ClientRequest(
-				primaryCluster,
-				"t",
-				fmt.Sprintf("http://zipkin.%s:9411/api/v1/traces?annotationQuery=guid:x-client-trace-id=%s",
-					tc.Kube.IstioSystemNamespace(), id),
-				1, "",
-			)
-
+	runRetriableTest(t, primaryCluster, "isMixerTraced", defaultRetryBudget, func() error {
+		if !traceSent {
+			// Send a request with a trace header.
+			id = uuid.NewV4().String()
+			response := ClientRequest(primaryCluster, "a", "http://b", 1,
+				fmt.Sprintf("-key %v -val %v", traceHeader, id))
 			if !response.IsHTTPOk() {
+				// Keep retrying until we successfully send a trace request.
 				return errAgain
 			}
 
-			// Check that the trace contains the id value (must occur more than once, as the
-			// response also contains the request URL with query parameter).
-			if strings.Count(response.Body, id) == 1 {
+			traceSent = true
+		}
+
+		// Check the tracing server to verify the trace was received.
+		response := ClientRequest(
+			primaryCluster,
+			"t",
+			fmt.Sprintf("http://zipkin.%s:9411/api/v1/traces?annotationQuery=guid:x-client-trace-id=%s",
+				tc.Kube.IstioSystemNamespace(), id),
+			1, "",
+		)
+
+		if !response.IsHTTPOk() {
+			return errAgain
+		}
+
+		// Check that the trace contains the id value (must occur more than once, as the
+		// response also contains the request URL with query parameter).
+		if strings.Count(response.Body, id) == 1 {
+			return errAgain
+		}
+
+		// Check that the mixer spans are also included in the trace:
+		// a) Count the number of spans - should be 4, one for the invocation of service b, and the other 3 for the
+		//		mixer check
+		// b) Check that the trace data contains the "check" operation name associated with the mixer check client span
+		// c) Check that the trace data contains the "/istio.mixer.v1.mixer/check" operation name for the server span
+		// NOTE: We are also indirectly verifying that the mixer check spans are child spans of the service invocation, as
+		// the mixer check spans can only exist in this trace as child spans. If they weren't child spans then they would be
+		// in a separate trace instance not retrieved by the query based on the single x-client-trace-id.
+		if strings.Count(response.Body, traceIDField) != 4 ||
+			!strings.Contains(response.Body, checkOperation) ||
+			!strings.Contains(response.Body, mixerCheckOperation) {
+			return errAgain
+		}
+
+		return nil
+	})
+}
+
+func TestTracingEnabled(t *testing.T) {
+	traceSent := false
+	var id string
+
+	runRetriableTest(t, primaryCluster, "checkEnabled", defaultRetryBudget, func() error {
+		if !traceSent {
+			// Send a request with a trace header.
+			id = uuid.NewV4().String()
+			response := ClientRequest(primaryCluster, "a", "http://b", 1,
+				fmt.Sprintf("-key %v -val %v", traceHeader, id))
+			if !response.IsHTTPOk() {
+				// Keep retrying until we successfully send a trace request.
 				return errAgain
 			}
 
-			// If first invocation (due to mixer check result caching), then check that the mixer
-			// span is also included in the trace
-			// a) Count the number of spans - should be 4, one for the invocation of service b, and the other 3 for the
-			//		mixer check
-			// b) Check that the trace data contains the "check" operation name associated with the mixer check client span
-			// c) Check that the trace data contains the "/istio.mixer.v1.mixer/check" operation name for the server span
-			// NOTE: We are also indirectly verifying that the mixer check spans are child spans of the service invocation, as
-			// the mixer check spans can only exist in this trace as child spans. If they weren't child spans then they would be
-			// in a separate trace instance not retrieved by the query based on the single x-client-trace-id.
-			if i == 0 && (strings.Count(response.Body, traceIDField) != 4 ||
-				!strings.Contains(response.Body, checkOperation) ||
-				!strings.Contains(response.Body, mixerCheckOperation)) {
-				return errAgain
-			}
+			traceSent = true
+		}
 
-			return nil
-		})
-	}
+		// Check the tracing server to verify the trace was received.
+		response := ClientRequest(
+			primaryCluster,
+			"t",
+			fmt.Sprintf("http://zipkin.%s:9411/api/v1/traces?annotationQuery=guid:x-client-trace-id=%s",
+				tc.Kube.IstioSystemNamespace(), id),
+			1, "",
+		)
+
+		if !response.IsHTTPOk() {
+			return errAgain
+		}
+
+		// Check that the trace contains the id value (must occur more than once, as the
+		// response also contains the request URL with query parameter).
+		if strings.Count(response.Body, id) == 1 {
+			return errAgain
+		}
+
+		return nil
+	})
 }
