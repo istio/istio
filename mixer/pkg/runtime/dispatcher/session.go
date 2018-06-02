4aaa9f20659b9c287d473382bb57d7f156487b96
Martin Taillefer <geeknoid@users.noreply.github.com>
2018-06-02 11:14:06 -0700

    Add buffering in front of REPORT adapters. (#5432)
    * Add buffering in front of REPORT adapters.
    
    The proxy calls Mixer with large batches of reports (on the order
    of 1000 items). Instead of calling adapters for each of these reports,
    Mixer buffers the generated instances and calls into the adapters a
    single time. This eliminates a fairly large amount of computation in Mixer
    proper. But more importantly, adapters can leverage these large incoming
    batches to use batches when talking to their back end, potentially reducing
    RPC overhead considerably.
    
    Also, remove the request_count and request_duration metrics. They weren't actually
    measuring the right thing and they're redundant with gRPC metrics we already have.

--- mixer/pkg/runtime/dispatcher/session.go
+++ mixer/pkg/runtime/dispatcher/session.go
@@ -15,13 +15,19 @@
 package dispatcher
 
 import (
+	"bytes"
 	"context"
-	"sync"
-	"time"
+
+	"github.com/gogo/googleapis/google/rpc"
+	multierror "github.com/hashicorp/go-multierror"
 
 	tpb "istio.io/api/mixer/adapter/model/v1beta1"
 	"istio.io/istio/mixer/pkg/adapter"
 	"istio.io/istio/mixer/pkg/attribute"
+	"istio.io/istio/mixer/pkg/pool"
+	"istio.io/istio/mixer/pkg/runtime/config"
+	"istio.io/istio/mixer/pkg/runtime/routing"
+	"istio.io/istio/mixer/pkg/status"
 	"istio.io/istio/pkg/log"
 )
 
@@ -30,15 +36,18 @@ const queueAllocSize = 64
 // session represents a call session to the Impl. It contains all the mutable state needed for handling the
 // call. It is used as temporary memory location to keep ephemeral state, thus avoiding garbage creation.
 type session struct {
+	// owner
+	impl *Impl
 
-	// start time of the session.
-	start time.Time
+	// routing context for the life of this session
+	rc *RoutingContext
 
 	// input parameters that was collected as part of the call.
-	ctx         context.Context
-	bag         attribute.Bag
-	quotaArgs   adapter.QuotaArgs
-	responseBag *attribute.MutableBag
+	ctx          context.Context
+	bag          attribute.Bag
+	quotaArgs    adapter.QuotaArgs
+	responseBag  *attribute.MutableBag
+	reportStates map[*routing.Destination]*dispatchState
 
 	// output parameters that gets collected / accumulated as result.
 	checkResult *adapter.CheckResult
@@ -53,32 +62,17 @@ type session struct {
 
 	// The variety of the operation that is being performed.
 	variety tpb.TemplateVariety
-
-	// whether to trace spans or not
-	trace bool
-}
-
-// pool of sessions
-type sessionPool struct {
-	sessions sync.Pool
-}
-
-func (s *session) ensureParallelism(minParallelism int) {
-	// Resize the channel to accommodate the parallelism, if necessary.
-	if cap(s.completed) < minParallelism {
-		allocSize := ((minParallelism / queueAllocSize) + 1) * queueAllocSize
-		s.completed = make(chan *dispatchState, allocSize)
-	}
 }
 
 func (s *session) clear() {
+	s.impl = nil
+	s.rc = nil
 	s.variety = 0
 	s.ctx = nil
 	s.bag = nil
 	s.quotaArgs = adapter.QuotaArgs{}
 	s.responseBag = nil
-
-	s.start = time.Time{}
+	s.reportStates = nil
 
 	s.activeDispatches = 0
 	s.err = nil
@@ -98,27 +92,183 @@ func (s *session) clear() {
 	}
 }
 
-// returns a new pool of sessions that uses the provided go-routine pool to execute dispatches.
-func newSessionPool(enableTracing bool) *sessionPool {
-	return &sessionPool{
-		sessions: sync.Pool{
-			New: func() interface{} {
-				return &session{
-					trace: enableTracing,
-				}
-			},
-		},
+func (s *session) ensureParallelism(minParallelism int) {
+	// Resize the channel to accommodate the parallelism, if necessary.
+	if cap(s.completed) < minParallelism {
+		allocSize := ((minParallelism / queueAllocSize) + 1) * queueAllocSize
+		s.completed = make(chan *dispatchState, allocSize)
 	}
 }
 
-// returns a session from the pool that can support the specified number of parallel executions.
-func (p *sessionPool) get() *session {
-	session := p.sessions.Get().(*session)
+func (s *session) dispatch() error {
+	// Lookup the value of the identity attribute, so that we can extract the namespace to use for route
+	// lookup.
+	identityAttributeValue, err := getIdentityAttributeValue(s.bag, s.impl.identityAttribute)
+	if err != nil {
+		// early return.
+		updateRequestCounters(0, 0)
+		log.Warnf("unable to determine identity attribute value: '%v', operation='%d'", err, s.variety)
+		return err
+	}
+	namespace := getNamespace(identityAttributeValue)
+	destinations := s.rc.Routes.GetDestinations(s.variety, namespace)
+	ctx := adapter.NewContextWithRequestData(s.ctx, &adapter.RequestData{adapter.Service{identityAttributeValue}})
 
-	return session
+	// TODO(Issue #2139): This is for old-style metadata based policy decisions. This should be eventually removed.
+	ctxProtocol, _ := s.bag.Get(config.ContextProtocolAttributeName)
+	tcp := ctxProtocol == config.ContextProtocolTCP
+
+	// Ensure that we can run dispatches to all destinations in parallel.
+	s.ensureParallelism(destinations.Count())
+
+	ninputs := 0
+	ndestinations := 0
+	for _, destination := range destinations.Entries() {
+		var state *dispatchState
+
+		if s.variety == tpb.TEMPLATE_VARIETY_REPORT {
+			// We buffer states for report calls and dispatch them later
+			state = s.reportStates[destination]
+			if state == nil {
+				state = s.impl.getDispatchState(ctx, destination)
+				s.reportStates[destination] = state
+			}
+		}
+
+		for _, group := range destination.InstanceGroups {
+			if !group.Matches(s.bag) || group.ResourceType.IsTCP() != tcp {
+				continue
+			}
+			ndestinations++
+
+			for j, input := range group.Builders {
+				var instance interface{}
+				if instance, err = input(s.bag); err != nil {
+					log.Warnf("error creating instance: destination='%v', error='%v'", destination.FriendlyName, err)
+					continue
+				}
+				ninputs++
+
+				// For report templates, accumulate instances as much as possible before commencing dispatch.
+				if s.variety == tpb.TEMPLATE_VARIETY_REPORT {
+					state.instances = append(state.instances, instance)
+					continue
+				}
+
+				// for other templates, dispatch for each instance individually.
+				state = s.impl.getDispatchState(ctx, destination)
+				state.instances = append(state.instances, instance)
+				if s.variety == tpb.TEMPLATE_VARIETY_ATTRIBUTE_GENERATOR {
+					state.mapper = group.Mappers[j]
+					state.inputBag = s.bag
+				}
+
+				// Dispatch for singleton dispatches
+				state.quotaArgs = s.quotaArgs
+				s.dispatchToHandler(state)
+			}
+		}
+	}
+
+	updateRequestCounters(ndestinations, ninputs)
+	s.waitForDispatched()
+
+	return nil
 }
 
-func (p *sessionPool) put(session *session) {
-	session.clear()
-	p.sessions.Put(session)
+func (s *session) dispatchBufferedReports() {
+	// Ensure that we can run dispatches to all destinations in parallel.
+	s.ensureParallelism(len(s.reportStates))
+
+	// dispatch the buffered dispatchStates we've got
+	for k, v := range s.reportStates {
+		s.dispatchToHandler(v)
+		delete(s.reportStates, k)
+	}
+
+	s.waitForDispatched()
+}
+
+func (s *session) dispatchToHandler(ds *dispatchState) {
+	s.activeDispatches++
+	ds.session = s
+	s.impl.gp.ScheduleWork(ds.invokeHandler, nil)
+}
+
+func (s *session) waitForDispatched() {
+	// wait on the dispatch states and accumulate results
+	var buf *bytes.Buffer
+	code := rpc.OK
+
+	var err error
+	for s.activeDispatches > 0 {
+		state := <-s.completed
+		s.activeDispatches--
+
+		// Aggregate errors
+		if state.err != nil {
+			err = multierror.Append(err, state.err)
+		}
+
+		st := rpc.Status{Code: int32(rpc.OK)}
+
+		switch s.variety {
+		case tpb.TEMPLATE_VARIETY_REPORT:
+			// Do nothing
+
+		case tpb.TEMPLATE_VARIETY_CHECK:
+			if s.checkResult == nil {
+				r := state.checkResult
+				s.checkResult = &r
+			} else {
+				if s.checkResult.ValidDuration > state.checkResult.ValidDuration {
+					s.checkResult.ValidDuration = state.checkResult.ValidDuration
+				}
+				if s.checkResult.ValidUseCount > state.checkResult.ValidUseCount {
+					s.checkResult.ValidUseCount = state.checkResult.ValidUseCount
+				}
+			}
+			st = state.checkResult.Status
+
+		case tpb.TEMPLATE_VARIETY_QUOTA:
+			if s.quotaResult == nil {
+				r := state.quotaResult
+				s.quotaResult = &r
+			} else {
+				log.Warnf("Skipping quota op result due to previous value: '%v', op: '%s'",
+					state.quotaResult, state.destination.FriendlyName)
+			}
+			st = state.quotaResult.Status
+
+		case tpb.TEMPLATE_VARIETY_ATTRIBUTE_GENERATOR:
+			if state.outputBag != nil {
+				s.responseBag.Merge(state.outputBag)
+			}
+		}
+
+		if !status.IsOK(st) {
+			if buf == nil {
+				buf = pool.GetBuffer()
+				// the first failure result's code becomes the result code for the output
+				code = rpc.Code(st.Code)
+			} else {
+				buf.WriteString(", ")
+			}
+
+			buf.WriteString(state.destination.HandlerName + ":" + st.Message)
+		}
+
+		s.impl.putDispatchState(state)
+	}
+	s.err = err
+
+	if buf != nil {
+		switch s.variety {
+		case tpb.TEMPLATE_VARIETY_CHECK:
+			s.checkResult.Status = status.WithMessage(code, buf.String())
+		case tpb.TEMPLATE_VARIETY_QUOTA:
+			s.quotaResult.Status = status.WithMessage(code, buf.String())
+		}
+		pool.PutBuffer(buf)
+	}
 }
