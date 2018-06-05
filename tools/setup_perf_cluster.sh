9830ba8ee24074988afe5b56afee1269725543a0
Tyler Berry <tyler@berryac.com>
2018-06-05 16:40:18 -0700

    Fix tools/setup_perf_cluster.sh

--- tools/setup_perf_cluster.sh
+++ tools/setup_perf_cluster.sh
@@ -35,7 +35,7 @@ function Usage() {
 }
 
 function List_functions() {
-  egrep "^function [a-z]" "${BASH_SOURCE[0]}" | sed -e 's/function \([a-z_0-9]*\).*/\1/'
+  grep -E "^function [a-z]" "${BASH_SOURCE[0]}" | sed -e 's/function \([a-z_0-9]*\).*/\1/'
 }
 
 if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
@@ -309,11 +309,11 @@ function get_json_file_name() {
 }
 
 function file_escape() {
-  FNAME=$(echo "$FNAME"|sed -e "s/ /_/g")
+  FNAME=${FNAME// /_}
 }
 
 function label_escape() {
-  LABELS=$(echo "$LABELS"|sed -e "s/ /+/g")
+  LABELS=${LABELS// /+}
 }
 
 function run_fortio_test1() {
@@ -508,7 +508,7 @@ function run_tests() {
 
 
 function check_image_versions() {
-  kubectl get pods --all-namespaces -o jsonpath="{..image}" | tr -s '[[:space:]]' '\n' | sort | uniq -c | grep -v -e google.containers
+  kubectl get pods --all-namespaces -o jsonpath="{..image}" | tr -s '[:space:]' '\n' | sort | uniq -c | grep -v -e google.containers
 }
 
 if [[ $SOURCED == 0 ]]; then
