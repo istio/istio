c7c90475818d9cc71c91f7db9ede8a2529f178c3
Sunny Gupta <guptasu@google.com>
2018-06-06 15:57:58 -0700

    Generate descriptor for the generate service proto. (#6058)
    * f
    
    * add docgen suppression option

--- mixer/pkg/protobuf/yaml/encoder_test.go
+++ mixer/pkg/protobuf/yaml/encoder_test.go
@@ -14,7 +14,7 @@
 
 // nolint
 //go:generate protoc testdata/all/types.proto  --include_imports  -otestdata/all/types.descriptor -I$GOPATH/src/istio.io/istio/vendor/istio.io/api -I.
-//go:generate $GOPATH/src/istio.io/istio/bin/mixer_codegen.sh -f mixer/pkg/protobuf/yaml/testdata/all/types.proto
+//go:generate $GOPATH/src/istio.io/istio/bin/mixer_codegen.sh  -d false -f mixer/pkg/protobuf/yaml/testdata/all/types.proto
 
 package yaml
 
