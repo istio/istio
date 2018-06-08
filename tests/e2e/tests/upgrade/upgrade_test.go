5d02596ca4ee2096fbfd3efeadd3aa100149e66d
hklai <hklai@google.com>
2018-06-08 11:31:25 -0700

    Fix errors

--- tests/e2e/tests/upgrade/upgrade_test.go
+++ tests/e2e/tests/upgrade/upgrade_test.go
@@ -332,11 +332,10 @@ func setTestConfig() error {
 }
 
 func TestMain(m *testing.M) {
-	t.Skip("https://github.com/istio/istio/issues/4937")
-
-	flag.Parse()
-	check(framework.InitLogging(), "cannot setup logging")
-	check(setTestConfig(), "could not create TestConfig")
-	tc.Cleanup.RegisterCleanable(tc)
-	os.Exit(tc.RunTest(m))
+	// https://github.com/istio/istio/issues/4937
+	// flag.Parse()
+	// check(framework.InitLogging(), "cannot setup logging")
+	// check(setTestConfig(), "could not create TestConfig")
+	// tc.Cleanup.RegisterCleanable(tc)
+	// os.Exit(tc.RunTest(m))
 }
