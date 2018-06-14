ef5ad4659269609b5e0ef32272566248bbd8d70e
Nino Kodabande <nkodabande@suse.com>
2018-06-14 10:12:31 -0700

    Use downstream protocol by default (#6158)
    * Use downstream protocol by default
    
    * Set downstream protocol only for HTTP, also
    
    set http2ProtocolOptions for HTTP2
    
    * Fixes CF servicediscovery test
    
    These tests were failing when the optional protocol was removed
    
    * Check upstream protocol based on model.Protocol
    
    * Turns out protocol field is not quite optional :b
    
    * adds case for GRPC

--- pilot/pkg/networking/core/v1alpha3/cluster.go
+++ pilot/pkg/networking/core/v1alpha3/cluster.go
@@ -475,7 +475,12 @@ func applyUpstreamTLSSettings(cluster *v2.Cluster, tls *networking.TLSSettings) 
 }
 
 func setUpstreamProtocol(cluster *v2.Cluster, port *model.Port) {
-	if port.Protocol.IsHTTP2() {
+	switch port.Protocol {
+	case model.ProtocolHTTP:
+		cluster.ProtocolSelection = v2.Cluster_USE_DOWNSTREAM_PROTOCOL
+	case model.ProtocolGRPC:
+		fallthrough
+	case model.ProtocolHTTP2:
 		cluster.Http2ProtocolOptions = &core.Http2ProtocolOptions{
 			// Envoy default value of 100 is too low for data path.
 			MaxConcurrentStreams: &types.UInt32Value{
