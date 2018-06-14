f5ae726952464ba1ff46a713560eb4ba3ddff87d
Yossi Mesika <mesika@il.ibm.com>
2018-06-14 12:18:25 +0300

    More refactoring - moved mock code to its own file

--- pilot/pkg/serviceregistry/memory/discovery.go
+++ pilot/pkg/serviceregistry/memory/discovery.go
@@ -22,44 +22,7 @@ import (
 )
 
 var (
-	PortHTTPName   = "http"
-	HelloService   = MakeService("hello.default.svc.cluster.local", "10.1.0.0")
-	WorldService   = MakeService("world.default.svc.cluster.local", "10.2.0.0")
-	ExtHTTPService = MakeExternalHTTPService("httpbin.default.svc.cluster.local",
-		"httpbin.org", "")
-	ExtHTTPSService = MakeExternalHTTPSService("httpsbin.default.svc.cluster.local",
-		"httpbin.org", "")
-	Discovery = &ServiceDiscovery{
-		services: map[model.Hostname]*model.Service{
-			HelloService.Hostname:   HelloService,
-			WorldService.Hostname:   WorldService,
-			ExtHTTPService.Hostname: ExtHTTPService,
-			// TODO external https is not currently supported - this service
-			// should NOT be in any of the .golden json files
-			ExtHTTPSService.Hostname: ExtHTTPSService,
-		},
-		versions: 2,
-	}
-	HelloInstanceV0 = MakeIP(HelloService, 0)
-	HelloInstanceV1 = MakeIP(HelloService, 1)
-	HelloProxyV0    = model.Proxy{
-		Type:      model.Sidecar,
-		IPAddress: HelloInstanceV0,
-		ID:        "v0.default",
-		Domain:    "default.svc.cluster.local",
-	}
-	HelloProxyV1 = model.Proxy{
-		Type:      model.Sidecar,
-		IPAddress: HelloInstanceV1,
-		ID:        "v1.default",
-		Domain:    "default.svc.cluster.local",
-	}
-	Ingress = model.Proxy{
-		Type:      model.Ingress,
-		IPAddress: "10.3.3.3",
-		ID:        "ingress.default",
-		Domain:    "default.svc.cluster.local",
-	}
+	portHTTPName = "http"
 )
 
 // NewDiscovery builds a memory ServiceDiscovery
@@ -77,7 +40,7 @@ func MakeService(hostname model.Hostname, address string) *model.Service {
 		Address:  address,
 		Ports: []*model.Port{
 			{
-				Name:     PortHTTPName,
+				Name:     portHTTPName,
 				Port:     80, // target port 80
 				Protocol: model.ProtocolHTTP,
 			}, {
@@ -158,7 +121,7 @@ func MakeInstance(service *model.Service, port *model.Port, version int, az stri
 // memory MakeSericve)
 func GetPortHTTP(service *model.Service) *model.Port {
 	for _, port := range service.Ports {
-		if port.Name == PortHTTPName {
+		if port.Name == portHTTPName {
 			return port
 		}
 	}
@@ -196,6 +159,7 @@ func (sd *ServiceDiscovery) ClearErrors() {
 	sd.GetProxyServiceInstancesError = nil
 }
 
+// AddService will add to the registry the provided service
 func (sd *ServiceDiscovery) AddService(name model.Hostname, svc *model.Service) {
 	sd.services[name] = svc
 }
