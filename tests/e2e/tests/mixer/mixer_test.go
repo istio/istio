b790015a81e9c3cef8274a749ea9f4d8208b0160
Kuat Yessenov <kuat@google.com>
2018-06-14 14:47:50 -0700

    make v1alpha3 default
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- tests/e2e/tests/mixer/mixer_test.go
+++ tests/e2e/tests/mixer/mixer_test.go
@@ -79,8 +79,8 @@ type testConfig struct {
 var (
 	tc        *testConfig
 	testFlags = &framework.TestFlags{
-		V1alpha1: true,  //implies envoyv1
-		V1alpha3: false, //implies envoyv2
+		V1alpha1: false, //implies envoyv1
+		V1alpha3: true,  //implies envoyv2
 		Ingress:  true,
 		Egress:   true,
 	}
