bc8058fa5d2f945f47963cefc9f21e4c32e2e171
Shriram Rajagopalan <shriramr@vmware.com>
2018-06-19 17:53:56 -0400

    final lint

--- pilot/pkg/proxy/envoy/v2/discovery.go
+++ pilot/pkg/proxy/envoy/v2/discovery.go
@@ -47,7 +47,7 @@ const (
 	ClusterType = typePrefix + "Cluster"
 	// EndpointType is used for EDS and ADS endpoint discovery. Typically second request.
 	EndpointType = typePrefix + "ClusterLoadAssignment"
-	// ListenerProtocol is sent after clusters and endpoints.
+	// ListenerType is sent after clusters and endpoints.
 	ListenerType = typePrefix + "Listener"
 	// RouteType is sent after listeners.
 	RouteType = typePrefix + "RouteConfiguration"
