4b65248952cbaed651782f151d2d65d20442567d
Piotr Sikora <piotrsikora@google.com>
2018-06-18 22:24:55 -0700

    Fix license headers and check for them in CircleCI. (#1820)
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- src/envoy/alts/transport_security_interface_wrapper.h
+++ src/envoy/alts/transport_security_interface_wrapper.h
@@ -1,3 +1,18 @@
+/* Copyright 2018 Istio Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // Some gRPC headers contains old style cast and unused parameter which doesn't
 // compile with -Werror, ignoring those compiler warning since we don't have
 // control on those source codes. This works with GCC and Clang.
--- src/istio/mixerclient/create_global_dictionary.py
+++ src/istio/mixerclient/create_global_dictionary.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 #
 # Copyright 2017 Istio Authors. All Rights Reserved.
 #
@@ -6,8 +6,8 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
-
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
