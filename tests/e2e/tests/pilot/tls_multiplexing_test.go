7c1ddcb01bf070ed6ef47c93b1ecb355fb8ba19d
Jianfei Hu <jianfeih@google.com>
2018-06-19 10:05:55 -0700

    Update istio/proxy sha to include istio/proxy/#1819 (#6394)
    * Update istio/proxy sha to include https://github.com/istio/proxy/pull/1819
    
    * Add port 7070 back in the test.

--- tests/e2e/tests/pilot/tls_multiplexing_test.go
+++ tests/e2e/tests/pilot/tls_multiplexing_test.go
@@ -48,8 +48,7 @@ func TestTLSMultiplexing(t *testing.T) {
 	// Pod t does not have sidecar, will send plain text traffic.
 	srcPods := []string{"a", "t"}
 	dstPods := []string{"d"}
-	// TODO(incfly): add port 7070 back once https://github.com/istio/istio/issues/6361 is resolved.
-	ports := []string{"80"}
+	ports := []string{"80", "7070"}
 	shouldFails := []struct {
 		src  string
 		dest string
