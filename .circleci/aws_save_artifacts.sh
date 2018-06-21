1812c0cc138094ecce5c147dbce17d3e469f2729
Brian Marshall <brian@aspenmesh.io>
2018-06-21 16:07:44 -0600

    Upload istio release tar/zip artifacts
    Upload the artifacts that will be released, into a nicely named s3 directory
    Also maintain latest veresion for each branch

---
+++ .circleci/aws_save_artifacts.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -e
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <src dir>" >&2
+  exit 1
+fi
+
+SRCDIR="$1"
+
+if [[ $CIRCLE_BUILD_NUM == "" ]]; then
+  echo "Not a circle ci build, or missing environment" >&2
+  exit 1
+fi
+
+PATH_BUILD=""
+PATH_LATEST=""
+
+if [[ $CIRCLE_TAG != "" ]]; then
+  echo "Saving artifacts from tag ${CIRCLE_TAG}"
+  PATH_BUILD="${CIRCLE_TAG}/build-${CIRCLE_BUILD_NUM}"
+  PATH_LATEST="${CIRCLE_TAG}/latest"
+
+elif [[ $CIRCLE_PR_NUMBER != "" ]]; then
+  echo "Saving artifacts from ${CIRCLE_PR_USERNAME}'s PR ${CIRCLE_PR_NUMBER}"
+  PATH_BUILD="PR/${CIRCLE_PR_NUMBER}/build-${CIRCLE_BUILD_NUM}"
+  PATH_LATEST="PR/${CIRCLE_PR_NUMBER}/latest"
+elif [[ $CIRCLE_BRANCH != "" ]]; then
+  echo "Saving artifacts from branch ${CIRCLE_BRANCH}"
+  PATH_BUILD="${CIRCLE_BRANCH}/build-${CIRCLE_BUILD_NUM}"
+  PATH_LATEST="${CIRCLE_BRANCH}/latest"
+else
+  echo "Can't determine build type" >&2
+  exit 1
+fi
+
+set +x
+
+cat > "${SRCDIR}"/build_info.txt <<EOF
+Build Log: ${CIRCLE_BUILD_URL}
+Pull Request: ${CIRCLE_PULL_REQUEST}
+Commit SHA1: ${CIRCLE_SHA1}
+Branch: ${CIRCLE_BRANCH}
+PR User: ${CIRCLE_PR_USERNAME}
+EOF
+
+
+
+aws s3 cp "${SRCDIR}" s3://aspenmesh-ci-artifacts/istio/${PATH_BUILD}/ --recursive
+aws s3 sync "${SRCDIR}" s3://aspenmesh-ci-artifacts/istio/${PATH_LATEST}
