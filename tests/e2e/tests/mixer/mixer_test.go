5b40c109986296a56321dcaede2420c92c908f4c
gargnupur <gargnupur@google.com>
2018-06-25 12:33:31 -0700

    Add an e2e test for RedisQuota Adapter
    Fix RedisQuota Unit Test and lint error in kuberntes.go
    
    Remove additional log added in redisquota adapter
    delete the route rule after test finishes in  mixer_test.go
    
    Fix vectorValue function and remove local port forwarding to redisquota server
    
    Add an e2e test for RedisQuota Adapter
    
    Fix RedisQuota Unit Test and lint error in kuberntes.go
    
    Remove additional log added in redisquota adapter
    delete the route rule after test finishes in  mixer_test.go
    
    Add an e2e test for RedisQuota Adapter
    
    Fix RedisQuota Unit Test and lint error in kuberntes.go
    
    Remove additional log added in redisquota adapter
    delete the route rule after test finishes in  mixer_test.go
    
    Fix vectorValue function and remove local port forwarding to redisquota server
    
    Add an e2e test for RedisQuota Adapter
    
    Fix RedisQuota Unit Test and lint error in kuberntes.go
    
    Remove additional log added in redisquota adapter
    delete the route rule after test finishes in  mixer_test.go
    
    Remove destination version label for quota test
    
    Make calculations more tight for RedisQuota
    
    Format test
    
    Format test

--- tests/e2e/tests/mixer/mixer_test.go
+++ tests/e2e/tests/mixer/mixer_test.go
@@ -51,6 +51,7 @@ const (
 	bookinfoRatingsv2Yaml = "bookinfo-ratings-v2"
 	bookinfoDbYaml        = "bookinfo-db"
 	sleepYaml             = "samples/sleep/sleep"
+	mixerTestDataDir      = "tests/e2e/tests/mixer/testdata"
 
 	prometheusPort   = "9090"
 	mixerMetricsPort = "42422"
@@ -77,6 +78,9 @@ const (
 	reportPath = "/istio.mixer.v1.Mixer/Report"
 
 	testRetryTimes = 5
+
+	redisInstallDir  = "stable/redis"
+	redisInstallName = "redis-release"
 )
 
 type testConfig struct {
@@ -107,6 +111,7 @@ var (
 	routeReviewsV3Rule       = "route-rule-reviews-v3"
 	tcpDbRule                = "route-rule-ratings-db"
 	bookinfoGateway          = "bookinfo-gateway"
+	redisQuotaRule           = "mixer-rule-ratings-redis-quota"
 
 	defaultRules []string
 	rules        []string
@@ -135,12 +140,17 @@ func (t *testConfig) Setup() (err error) {
 	drs := []*string{&routeAllRule, &bookinfoGateway}
 	rs := []*string{&rateLimitRule, &denialRule, &ingressDenialRule, &newTelemetryRule,
 		&kubeenvTelemetryRule, &routeReviewsVersionsRule, &routeReviewsV3Rule, &tcpDbRule}
+	rsMore := []*string{&redisQuotaRule}
 	for _, dr := range drs {
-		*dr = filepath.Join(rulesDir, *dr)
+		*dr = filepath.Join(bookinfoSampleDir, rulesDir, *dr)
 		defaultRules = append(defaultRules, *dr)
 	}
 	for _, r := range rs {
-		*r = filepath.Join(rulesDir, *r)
+		*r = filepath.Join(bookinfoSampleDir, rulesDir, *r)
+		rules = append(rules, *r)
+	}
+	for _, r := range rsMore {
+		*r = filepath.Join(mixerTestDataDir, *r)
 		rules = append(rules, *r)
 	}
 
@@ -201,7 +211,7 @@ func copyRuleToFilesystem(t *testConfig, rule string) error {
 }
 
 func getSourceRulePath(rule string) string {
-	return util.GetResourcePath(filepath.Join(bookinfoSampleDir, rule+"."+yamlExtension))
+	return util.GetResourcePath(filepath.Join(rule + "." + yamlExtension))
 }
 
 func getDestinationRulePath(t *testConfig, rule string) string {
@@ -888,6 +898,11 @@ func TestMetricsAndRateLimitAndRulesAndBookinfo(t *testing.T) {
 	if err := replaceRouteRule(routeReviewsV3Rule); err != nil {
 		fatalf(t, "Could not create replace reviews routing rule: %v", err)
 	}
+	defer func() {
+		if err := deleteRouteRule(routeReviewsV3Rule); err != nil {
+			t.Fatalf("Could not delete reviews routing rule: %v", err)
+		}
+	}()
 
 	// the rate limit rule applies a max rate limit of 1 rps to the ratings service.
 	if err := applyMixerRule(rateLimitRule); err != nil {
@@ -1002,6 +1017,143 @@ func TestMetricsAndRateLimitAndRulesAndBookinfo(t *testing.T) {
 	}
 }
 
+func TestRedisQuota(t *testing.T) {
+	if err := replaceRouteRule(routeReviewsV3Rule); err != nil {
+		fatalf(t, "Could not create replace reviews routing rule: %v", err)
+	}
+	defer func() {
+		if err := deleteRouteRule(routeReviewsV3Rule); err != nil {
+			t.Fatalf("Could not delete reviews routing rule: %v", err)
+		}
+	}()
+
+	if err := tc.Kube.DeployTiller(); err != nil {
+		fatalf(t, "Failed to deploy helm tiller.")
+	}
+
+	setValue := "--set usePassword=false,persistence.enabled=false"
+	if err := util.HelmInstall(redisInstallDir, redisInstallName, tc.Kube.Namespace, setValue); err != nil {
+		fatalf(t, "Helm install %s failed, setValue=%s", redisInstallDir, setValue)
+	}
+	defer func() {
+		if err := util.HelmDelete(redisInstallName); err != nil {
+			t.Logf("Could not delete %s: %v", redisInstallName, err)
+		}
+	}()
+
+	allowRuleSync()
+
+	// the rate limit rule applies a max rate limit of 1 rps to the ratings service.
+	if err := applyMixerRule(redisQuotaRule); err != nil {
+		fatalf(t, "could not create required mixer rule: %v", err)
+	}
+	defer func() {
+		if err := deleteMixerRule(redisQuotaRule); err != nil {
+			t.Logf("could not clear rule: %v", err)
+		}
+	}()
+
+	allowRuleSync()
+
+	// setup prometheus API
+	promAPI, err := promAPI()
+	if err != nil {
+		fatalf(t, "Could not build prometheus API client: %v", err)
+	}
+
+	// establish baseline
+	initPrior429s, _, _ := fetchRequestCount(t, promAPI, "ratings")
+
+	_ = sendTraffic(t, "Warming traffic...", 150)
+	allowPrometheusSync()
+	prior429s, prior200s, _ := fetchRequestCount(t, promAPI, "ratings")
+	// check if at least one more prior429 was reported
+	if prior429s-initPrior429s < 1 {
+		fatalf(t, "no 429 is allotted time: prior429s:%v", prior429s)
+	}
+
+	res := sendTraffic(t, "Sending traffic...", 300)
+	allowPrometheusSync()
+
+	totalReqs := res.DurationHistogram.Count
+	succReqs := float64(res.RetCodes[http.StatusOK])
+	badReqs := res.RetCodes[http.StatusBadRequest]
+	actualDuration := res.ActualDuration.Seconds() // can be a bit more than requested
+
+	log.Info("Successfully sent request(s) to /productpage; checking metrics...")
+	t.Logf("Fortio Summary: %d reqs (%f rps, %f 200s (%f rps), %d 400s - %+v)",
+		totalReqs, res.ActualQPS, succReqs, succReqs/actualDuration, badReqs, res.RetCodes)
+
+	// consider only successful requests (as recorded at productpage service)
+	callsToRatings := succReqs
+
+	// the rate-limit is 1 rps
+	want200s := 1. * actualDuration
+
+	// everything in excess of 200s should be 429s (ideally)
+	want429s := callsToRatings - want200s
+
+	t.Logf("Expected Totals: 200s: %f (%f rps), 429s: %f (%f rps)", want200s, want200s/actualDuration, want429s, want429s/actualDuration)
+
+	// if we received less traffic than the expected enforced limit to ratings
+	// then there is no way to determine if the rate limit was applied at all
+	// and for how much traffic. log all metrics and abort test.
+	if callsToRatings < want200s {
+		t.Logf("full set of prometheus metrics:\n%s", promDump(promAPI, "istio_request_count"))
+		fatalf(t, "Not enough traffic generated to exercise rate limit: ratings_reqs=%f, want200s=%f", callsToRatings, want200s)
+	}
+
+	_, _, value := fetchRequestCount(t, promAPI, "ratings")
+	log.Infof("promvalue := %s", value.String())
+
+	got, err := vectorValue(value, map[string]string{responseCodeLabel: "429"})
+	if err != nil {
+		t.Logf("prometheus values for istio_request_count:\n%s", promDump(promAPI, "istio_request_count"))
+		errorf(t, "Could not find 429s: %v", err)
+		got = 0 // want to see 200 rate even if no 429s were recorded
+	}
+
+	want := math.Floor(want429s * 0.95)
+
+	got = (got - prior429s) / 2
+
+	t.Logf("Actual 429s: %f (%f rps)", got, got/actualDuration)
+
+	// check resource exhausted
+	if got < want {
+		t.Logf("prometheus values for istio_request_count:\n%s", promDump(promAPI, "istio_request_count"))
+		errorf(t, "Bad metric value for rate-limited requests (429s): got %f, want at least %f", got, want)
+	}
+
+	got, err = vectorValue(value, map[string]string{responseCodeLabel: "200"})
+	if err != nil {
+		t.Logf("prometheus values for istio_request_count:\n%s", promDump(promAPI, "istio_request_count"))
+		errorf(t, "Could not find successes value: %v", err)
+		got = 0
+	}
+
+	got = (got - prior200s) / 2
+
+	t.Logf("Actual 200s: %f (%f rps), expecting ~1 rps", got, got/actualDuration)
+
+	// establish some baseline to protect against flakiness due to randomness in routing
+	// and to allow for leniency in actual ceiling of enforcement (if 10 is the limit, but we allow slightly
+	// less than 10, don't fail this test).
+	want = math.Floor(want200s * 0.95)
+
+	// check successes
+	if got < want {
+		t.Logf("prometheus values for istio_request_count:\n%s", promDump(promAPI, "istio_request_count"))
+		errorf(t, "Bad metric value for successful requests (200s): got %f, want at least %f", got, want)
+	}
+	// TODO: until https://github.com/istio/istio/issues/3028 is fixed, use 25% - should be only 5% or so
+	want200s = math.Ceil(want200s * 1.5)
+	if got > want200s {
+		t.Logf("prometheus values for istio_request_count:\n%s", promDump(promAPI, "istio_request_count"))
+		errorf(t, "Bad metric value for successful requests (200s): got %f, want at most %f", got, want200s)
+	}
+}
+
 func TestMixerReportingToMixer(t *testing.T) {
 	// setup prometheus API
 	promAPI, err := promAPI()
@@ -1103,6 +1255,7 @@ func vectorValue(val model.Value, labels map[string]string) (float64, error) {
 	}
 
 	value := val.(model.Vector)
+	valueCount := 0.0
 	for _, sample := range value {
 		metric := sample.Metric
 		nameCount := len(labels)
@@ -1112,8 +1265,11 @@ func vectorValue(val model.Value, labels map[string]string) (float64, error) {
 			}
 		}
 		if nameCount == 0 {
-			return float64(sample.Value), nil
+			valueCount += float64(sample.Value)
 		}
+	}
+	if valueCount > 0.0 {
+		return valueCount, nil
 	}
 	return 0, fmt.Errorf("value not found for %#v", labels)
 }
