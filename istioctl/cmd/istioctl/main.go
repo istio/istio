d1189b4ef8613a1d1dc13ab5655213a6066ffd15
Guang Ya Liu <gyliu513@gmail.com>
2018-06-28 00:30:37 +0800

    Do not count typeConfigs if it is error. (#6527)

--- istioctl/cmd/istioctl/main.go
+++ istioctl/cmd/istioctl/main.go
@@ -416,10 +416,13 @@ istioctl get virtualservice bookinfo
 			} else {
 				for _, typ := range typs {
 					typeConfigs, err := configClient.List(typ.Type, ns)
-					if err != nil && mustList[typ.Type] {
-						errs = multierror.Append(errs, multierror.Prefix(err, fmt.Sprintf("Can't list %v:", typ.Type)))
+					if err == nil {
+						configs = append(configs, typeConfigs...)
+					} else {
+						if mustList[typ.Type] {
+							errs = multierror.Append(errs, multierror.Prefix(err, fmt.Sprintf("Can't list %v:", typ.Type)))
+						}
 					}
-					configs = append(configs, typeConfigs...)
 				}
 			}
 
