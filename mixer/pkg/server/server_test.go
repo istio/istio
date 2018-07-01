bc3f925451df9bda16a587f90a0cdabe7a06ee75
Yossi Mesika <mesika@il.ibm.com>
2018-07-01 14:32:14 +0300

    Use different port on each iteration

--- mixer/pkg/server/server_test.go
+++ mixer/pkg/server/server_test.go
@@ -199,7 +199,6 @@ func TestErrors(t *testing.T) {
 
 	a = defaultTestArgs()
 	a.APIPort = 0
-	a.MonitoringPort = 0
 	a.TracingOptions.LogTraceSpans = true
 
 	// This test is designed to exercise the many failure paths in the server creation
@@ -212,6 +211,7 @@ func TestErrors(t *testing.T) {
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
 			a.ConfigStore = configStore
 			a.ConfigStoreURL = ""
+			a.MonitoringPort = 0
 			pt := newPatchTable()
 			switch i {
 			case 1:
@@ -238,10 +238,10 @@ func TestErrors(t *testing.T) {
 					return net.Listen(network, address)
 				}
 			case 6:
-				a.MonitoringPort = 1234
+				a.MonitoringPort = 1235
 				pt.listen = func(network string, address string) (net.Listener, error) {
 					// fail the net.Listen call that's for the monitoring port.
-					if address == ":1234" {
+					if address == ":1235" {
 						return nil, errors.New("BAD")
 					}
 					return net.Listen(network, address)
