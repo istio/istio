61161a06045ae5ea10dbdb45bd80aeecf516b017
Shriram Rajagopalan <shriramr@vmware.com>
2018-07-03 17:45:36 -0400

    split tests
    Signed-off-by: Shriram Rajagopalan <shriramr@vmware.com>

--- tests/e2e/tests/pilot/egressgateway_test.go
+++ tests/e2e/tests/pilot/egressgateway_test.go
@@ -29,7 +29,7 @@ import (
 //    3.a. Traffic from all sidecars (i.e. mesh gateway) goes to egress gateway svc
 //    3.b. Traffic from egress gateway goes to actual destination (in our case, its t)
 // The tests will only check for requests from a->t with host matching ext service
-func TestEgressGateway(t *testing.T) {
+func TestRouteHTTPViaEgressGateway(t *testing.T) {
 	// In authn enable test, mTLS is enabled globally, which mean all clients will use TLS
 	// to talk to egress-gateway. We need to explicitly specify the TLSMode to DISABLE in the
 	// DestinationRule to the gateway.
@@ -39,7 +39,6 @@ func TestEgressGateway(t *testing.T) {
 			"testdata/networking/v1alpha3/disable-mtls-egressgateway.yaml",
 			"testdata/networking/v1alpha3/egressgateway.yaml",
 			"testdata/networking/v1alpha3/service-entry-bookinfo.yaml",
-			"testdata/networking/v1alpha3/service-entry-google.yaml",
 			"testdata/networking/v1alpha3/rule-route-via-egressgateway.yaml"},
 		kubeconfig: tc.Kube.KubeConfig,
 	}
@@ -49,7 +48,7 @@ func TestEgressGateway(t *testing.T) {
 	defer cfgs.Teardown()
 
 	for cluster := range tc.Kube.Clusters {
-		runRetriableTest(t, cluster, "HTTPRouteViaEgressGateway", defaultRetryBudget, func() error {
+		runRetriableTest(t, cluster, "RouteHTTPViaEgressGateway", defaultRetryBudget, func() error {
 			// We use an arbitrary IP to ensure that the test fails if networking logic is implemented incorrectly
 			reqURL := fmt.Sprintf("http://1.1.1.1/bookinfo")
 			resp := ClientRequest(cluster, "a", reqURL, 100, "-key Host -val scooby.eu.bookinfo.com")
@@ -67,9 +66,35 @@ func TestEgressGateway(t *testing.T) {
 			}
 			return errAgain
 		})
+	}
+}
 
-		runRetriableTest(t, cluster, "SNIRouteViaEgressGateway", defaultRetryBudget, func() error {
-			// We use an arbitrary IP to ensure that the test fails if networking logic is implemented incorrectly
+// To route all external traffic to https://www.google.com via Istio Egress gateway
+// 1. Add service entries
+// 2. Add egress gateway
+// 3. Add virtual service for each service entry such that
+//    3.a. TLS Traffic from all sidecars (i.e. mesh gateway) goes to egress gateway svc
+//    3.b. TLS Traffic from egress gateway goes to actual destination (in our case, its google.com)
+func TestRouteSNIViaEgressGateway(t *testing.T) {
+	// In authn enable test, mTLS is enabled globally, which mean all clients will use TLS
+	// to talk to egress-gateway. We need to explicitly specify the TLSMode to DISABLE in the
+	// DestinationRule to the gateway.
+	cfgs := &deployableConfig{
+		Namespace: tc.Kube.Namespace,
+		YamlFiles: []string{
+			"testdata/networking/v1alpha3/disable-mtls-egressgateway.yaml",
+			"testdata/networking/v1alpha3/egressgateway.yaml",
+			"testdata/networking/v1alpha3/service-entry-google.yaml",
+			"testdata/networking/v1alpha3/rule-route-via-egressgateway.yaml"},
+		kubeconfig: tc.Kube.KubeConfig,
+	}
+	if err := cfgs.Setup(); err != nil {
+		t.Fatal(err)
+	}
+	defer cfgs.Teardown()
+
+	for cluster := range tc.Kube.Clusters {
+		runRetriableTest(t, cluster, "RouteSNIViaEgressGateway", defaultRetryBudget, func() error {
 			reqURL := fmt.Sprintf("https://www.google.com")
 			resp := ClientRequest(cluster, "a", reqURL, 100, "")
 			count := make(map[string]int)
