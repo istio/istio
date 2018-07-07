0e67eb4eea045fcfe76eb5f4dcc16fbe78bc22aa
Isaiah Snell-Feikema <ijsnellf@users.noreply.github.com>
2018-07-07 01:18:20 -0500

    Remove v1alpha1 -> v1alpha3 conversion tool (#6901)
    * remove conversion tool
    
    * somewhat questionable pruning of tests without replacing them

--- istioctl/cmd/istioctl/main.go
+++ istioctl/cmd/istioctl/main.go
@@ -45,7 +45,6 @@ import (
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 
 	"istio.io/api/networking/v1alpha3"
-	"istio.io/istio/istioctl/cmd/istioctl/convert"
 	"istio.io/istio/istioctl/cmd/istioctl/gendeployment"
 	"istio.io/istio/pilot/pkg/config/kube/crd"
 	"istio.io/istio/pilot/pkg/model"
@@ -658,7 +657,6 @@ func init() {
 		"If present, list the requested object(s) across all namespaces. Namespace in current "+
 			"context is ignored even if specified with --namespace.")
 
-	experimentalCmd.AddCommand(convert.Command())
 	experimentalCmd.AddCommand(Rbac())
 
 	// Attach the Istio logging options to the command.
