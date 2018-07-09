463299eebf4c66c298ae9a85e66565a848547841
Ozben Evren <ozevren@users.noreply.github.com>
2018-07-09 14:53:52 -0700

    Add builder support to InMemory snapshot. (#3)
    * Add builder support to InMemory snapshot.
    
    * Clone resurces as well.
    
    * Use proto.Clone, instead of manual copying.

--- galley/pkg/mcp/snapshot/inmemory_test.go
+++ galley/pkg/mcp/snapshot/inmemory_test.go
@@ -16,6 +16,8 @@ package snapshot
 
 import (
 	"reflect"
+	"sort"
+	"strings"
 	"testing"
 
 	"github.com/gogo/protobuf/types"
@@ -23,61 +25,252 @@ import (
 	mcp "istio.io/api/mcp/v1alpha1"
 )
 
-func TestNewInMemory(t *testing.T) {
-	sn := NewInMemory()
-	if sn.versions == nil || len(sn.versions) != 0 {
-		t.Fatalf("Unexpected initial versions: %v", sn.versions)
-	}
-	if sn.envelopes == nil || len(sn.envelopes) != 0 {
-		t.Fatalf("Unexpected initial envelopes: %v", sn.envelopes)
-	}
-}
+func TestInMemoryBuilder(t *testing.T) {
+	b := NewInMemoryBuilder()
+	sn := b.Build()
 
-func TestInMemory_Set(t *testing.T) {
-	sn := NewInMemory()
-	items := []*mcp.Envelope{{Resource: &types.Any{}, Metadata: &mcp.Metadata{Name: "foo"}}}
-	sn.Set("type", "version", items)
-
-	if v, ok := sn.versions["type"]; !ok || v != "version" {
-		t.Fatalf("Unexpected version table after set: %+v", sn.versions)
+	if len(sn.envelopes) != 0 {
+		t.Fatal("Envelopes should have been empty")
 	}
 
-	if r, ok := sn.envelopes["type"]; !ok || !reflect.DeepEqual(r, items) {
-		t.Fatalf("Unexpected resource table after set: %+v", sn.envelopes)
+	if len(sn.versions) != 0 {
+		t.Fatal("Versions should have been empty")
 	}
 }
 
-func TestInMemory_Resources(t *testing.T) {
-	sn := NewInMemory()
-	items := []*mcp.Envelope{{Resource: &types.Any{}, Metadata: &mcp.Metadata{Name: "foo"}}}
-	sn.Set("type", "version", items)
+func TestInMemoryBuilder_Set(t *testing.T) {
+	b := NewInMemoryBuilder()
 
-	r := sn.Resources("type")
-	if !reflect.DeepEqual(items, r) {
-		t.Fatalf("Unexpected envelopes: %+v, expected: %+v", r, items)
+	items := []*mcp.Envelope{{Resource: &types.Any{}, Metadata: &mcp.Metadata{Name: "foo"}}}
+	b.Set("type", "v1", items)
+	sn := b.Build()
+
+	if sn.Version("type") != "v1" {
+		t.Fatalf("Unexpected version: %v", sn.Version("type"))
+	}
+
+	actual := sn.Resources("type")
+	if !reflect.DeepEqual(items, sn.Resources("type")) {
+		t.Fatalf("Mismatch:\nGot:\n%v\nWanted:\n%v\n", actual, items)
 	}
 }
 
-func TestInMemory_Version(t *testing.T) {
-	sn := NewInMemory()
-	items := []*mcp.Envelope{{Resource: &types.Any{}, Metadata: &mcp.Metadata{Name: "foo"}}}
-	sn.Set("type", "version", items)
+func TestInMemoryBuilder_SetEntry_Add(t *testing.T) {
+	b := NewInMemoryBuilder()
 
-	v := sn.Version("type")
-	if v != "version" {
-		t.Fatalf("Unexpected version: %s, expected: 'version'", v)
+	_ = b.SetEntry("type", "foo", &types.Any{})
+
+	sn := b.Build()
+
+	expected := []*mcp.Envelope{
+		{
+			Metadata: &mcp.Metadata{Name: "foo"},
+			Resource: &types.Any{TypeUrl: "type", Value: []byte{}},
+		},
+	}
+	actual := sn.Resources("type")
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("Mismatch:\nGot:\n%v\nWanted:\n%v\n", actual, expected)
 	}
 }
 
-func TestInMemory_Freeze(t *testing.T) {
-	defer func() {
-		if r := recover(); r == nil {
-			t.Fatal("set should have panicked")
-		}
-	}()
+func TestInMemoryBuilder_SetEntry_Update(t *testing.T) {
+	b := NewInMemoryBuilder()
 
-	sn := NewInMemory()
-	sn.Freeze()
-	items := []*mcp.Envelope{{Resource: &types.Any{}, Metadata: &mcp.Metadata{Name: "foo"}}}
-	sn.Set("type", "version", items)
+	_ = b.SetEntry("type", "foo", &types.Any{})
+	_ = b.SetEntry("type", "foo", &types.Any{})
+
+	sn := b.Build()
+
+	expected := []*mcp.Envelope{
+		{
+			Metadata: &mcp.Metadata{Name: "foo"},
+			Resource: &types.Any{TypeUrl: "type", Value: []byte{}},
+		},
+	}
+	actual := sn.Resources("type")
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("Mismatch:\nGot:\n%v\nWanted:\n%v\n", actual, expected)
+	}
+}
+
+func TestInMemoryBuilder_SetEntry_Marshal_Error(t *testing.T) {
+	b := NewInMemoryBuilder()
+
+	err := b.SetEntry("type", "foo", nil)
+	if err == nil {
+		t.Fatal("expected error not found")
+	}
+}
+
+func TestInMemoryBuilder_DeleteEntry_EntryNotFound(t *testing.T) {
+	b := NewInMemoryBuilder()
+
+	_ = b.SetEntry("type", "foo", &types.Any{})
+	b.DeleteEntry("type", "bar")
+	sn := b.Build()
+
+	expected := []*mcp.Envelope{
+		{
+			Metadata: &mcp.Metadata{Name: "foo"},
+			Resource: &types.Any{TypeUrl: "type", Value: []byte{}},
+		},
+	}
+	actual := sn.Resources("type")
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("Mismatch:\nGot:\n%v\nWanted:\n%v\n", actual, expected)
+	}
+}
+
+func TestInMemoryBuilder_DeleteEntry_TypeNotFound(t *testing.T) {
+	b := NewInMemoryBuilder()
+
+	b.DeleteEntry("type", "bar")
+	sn := b.Build()
+
+	if len(sn.envelopes) != 0 {
+		t.Fatal("Envelopes should have been empty")
+	}
+
+	if len(sn.versions) != 0 {
+		t.Fatal("Versions should have been empty")
+	}
+}
+
+func TestInMemoryBuilder_DeleteEntry_Single(t *testing.T) {
+	b := NewInMemoryBuilder()
+
+	_ = b.SetEntry("type", "foo", &types.Any{})
+	b.DeleteEntry("type", "foo")
+	sn := b.Build()
+
+	if len(sn.envelopes) != 0 {
+		t.Fatal("Envelopes should have been empty")
+	}
+
+	if len(sn.versions) != 0 {
+		t.Fatal("Versions should have been empty")
+	}
+}
+
+func TestInMemoryBuilder_DeleteEntry_Multiple(t *testing.T) {
+	b := NewInMemoryBuilder()
+
+	_ = b.SetEntry("type", "foo", &types.Any{})
+	_ = b.SetEntry("type", "bar", &types.Any{})
+	b.DeleteEntry("type", "foo")
+	sn := b.Build()
+
+	expected := []*mcp.Envelope{
+		{
+			Metadata: &mcp.Metadata{Name: "bar"},
+			Resource: &types.Any{TypeUrl: "type", Value: []byte{}},
+		},
+	}
+	actual := sn.Resources("type")
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("Mismatch:\nGot:\n%v\nWanted:\n%v\n", actual, expected)
+	}
+}
+
+func TestInMemoryBuilder_SetVersion(t *testing.T) {
+	b := NewInMemoryBuilder()
+
+	_ = b.SetEntry("type", "foo", &types.Any{})
+	b.SetVersion("type", "v1")
+	sn := b.Build()
+
+	if sn.Version("type") != "v1" {
+		t.Fatalf("Unexpected version: %s", sn.Version("type"))
+	}
+}
+
+func TestInMemory_Clone(t *testing.T) {
+	b := NewInMemoryBuilder()
+
+	_ = b.SetEntry("type", "foo", &types.Any{})
+	_ = b.SetEntry("type", "bar", &types.Any{})
+	b.SetVersion("type", "v1")
+	sn := b.Build()
+
+	sn2 := sn.Clone()
+
+	expected := []*mcp.Envelope{
+		{
+			Metadata: &mcp.Metadata{Name: "bar"},
+			Resource: &types.Any{TypeUrl: "type"},
+		},
+		{
+			Metadata: &mcp.Metadata{Name: "foo"},
+			Resource: &types.Any{TypeUrl: "type"},
+		},
+	}
+
+	actual := sn2.Resources("type")
+
+	sort.Slice(actual, func(i, j int) bool {
+		return strings.Compare(
+			actual[i].Metadata.Name,
+			actual[j].Metadata.Name) < 0
+	})
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("Mismatch:\nGot:\n%v\nWanted:\n%v\n", actual, expected)
+	}
+
+	if sn2.Version("type") != "v1" {
+		t.Fatalf("Unexpected version: %s", sn2.Version("type"))
+	}
+}
+
+func TestInMemory_Builder(t *testing.T) {
+	b := NewInMemoryBuilder()
+
+	_ = b.SetEntry("type", "foo", &types.Any{})
+	_ = b.SetEntry("type", "bar", &types.Any{})
+	b.SetVersion("type", "v1")
+	sn := b.Build()
+
+	b = sn.Builder()
+
+	sn2 := b.Build()
+
+	expected := []*mcp.Envelope{
+		{
+			Metadata: &mcp.Metadata{Name: "bar"},
+			Resource: &types.Any{TypeUrl: "type"},
+		},
+		{
+			Metadata: &mcp.Metadata{Name: "foo"},
+			Resource: &types.Any{TypeUrl: "type"},
+		},
+	}
+
+	actual := sn2.Resources("type")
+
+	sort.Slice(actual, func(i, j int) bool {
+		return strings.Compare(
+			actual[i].Metadata.Name,
+			actual[j].Metadata.Name) < 0
+	})
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("Mismatch:\nGot:\n%v\nWanted:\n%v\n", actual, expected)
+	}
+
+	if sn2.Version("type") != "v1" {
+		t.Fatalf("Unexpected version: %s", sn2.Version("type"))
+	}
+}
+
+func TestInMemory_String(t *testing.T) {
+	b := NewInMemoryBuilder()
+
+	_ = b.SetEntry("type", "foo", &types.Any{})
+	_ = b.SetEntry("type", "bar", &types.Any{})
+	b.SetVersion("type", "v1")
+	sn := b.Build()
+
+	// Shouldn't crash
+	_ = sn.String()
 }
