f4926b5380369c04277fa34d331fc93eda310ad2
Nupur Garg <gargnupur@google.com>
2018-07-10 09:39:42 -0700

    Check for existence of localregistry.yaml file in setup_host script

--- tests/e2e/local/minikube/setup_host.sh
+++ tests/e2e/local/minikube/setup_host.sh
@@ -55,7 +55,8 @@ fi
 #Setup LocalRegistry
 if [ ! -f $ISTIO/istio/tests/util/localregistry/localregistry.yaml ]; then
     echo File $ISTIO/istio/tests/util/localregistry/localregistry.yaml not found!.
-    echo Please check $ISTIO path is pointing to your Istio codebase.
+    echo Please make sure $ISTIO points to your Istio codebase.
+    echo See https://github.com/istio/istio/wiki/Dev-Guide#setting-up-environment-variables
     exit
 fi
 kubectl apply -f $ISTIO/istio/tests/util/localregistry/localregistry.yaml
