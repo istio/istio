777b38f46c113d866fd3ca57b5bddc6c08ab7d95
Vadim Eisenberg <vadime@il.ibm.com>
2018-07-10 10:21:13 +0300

    fix format

--- src/istio/utils/attribute_names.cc
+++ src/istio/utils/attribute_names.cc
@@ -58,7 +58,8 @@ const char AttributeName::kConnectionSendTotalBytes[] =
     "connection.sent.bytes_total";
 const char AttributeName::kConnectionDuration[] = "connection.duration";
 const char AttributeName::kConnectionMtls[] = "connection.mtls";
-const char AttributeName::kConnectionRequestedServerName[] = "connection.requested_server_name";
+const char AttributeName::kConnectionRequestedServerName[] =
+    "connection.requested_server_name";
 
 // Downstream TCP connection id.
 const char AttributeName::kConnectionId[] = "connection.id";
