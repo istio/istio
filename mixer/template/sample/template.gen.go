9a3f5a5e20d0a7ab6c008628068a3eb3134f19af
Pengyuan Bian <34738376+bianpengyuan@users.noreply.github.com>
2018-07-11 06:16:46 -0700

    Do not return string if not set. (#6953)

--- mixer/template/sample/template.gen.go
+++ mixer/template/sample/template.gen.go
@@ -437,6 +437,9 @@ var (
 					func(name string) (value interface{}, found bool) {
 						field := strings.TrimPrefix(name, fullOutName)
 						if len(field) != len(name) {
+							if !out.WasSet(field) {
+								return nil, false
+							}
 							switch field {
 
 							case "int64Primitive":
