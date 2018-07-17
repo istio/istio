34798a4d2fe959208c386968d8a646f657a693c4
Costin Manolache <costin@gmail.com>
2018-07-17 15:18:52 -0700

    Fix broken ingress (#7144)
    * Fix ingress
    
    * Move merging ingress into VirtualService and dedup to separate PR. Add back the previously and still broken gateway generation
    
    * Another attempt to avoid duplicated vhosts and merge ingress
    
    * Missed a pointer
    
    * Remove again mergeIngress
    
    * Improved messages. Will need to be turned into metrics soon
    
    * Merge, assorted fixes
    
    * Leave the push param in gateay, it will be needed later

--- pilot/pkg/model/gateway.go
+++ pilot/pkg/model/gateway.go
@@ -95,7 +95,7 @@ func MergeGateways(gateways ...Config) *MergedGateway {
 						// WE cannot merge two HTTPS servers even if their TLS settings have same path to the keys, because we don't know if the contents
 						// of the keys are same. So we treat them as effectively different TLS settings.
 						if _, exists := rdsRouteConfigNames[rdsName]; exists {
-							log.Debugf("skipping server on gateway %s port %s.%d.%s: non unique port name for HTTPS port",
+							log.Infof("skipping server on gateway %s port %s.%d.%s: non unique port name for HTTPS port",
 								spec.Name, s.Port.Name, s.Port.Number, s.Port.Protocol)
 							continue
 						}
@@ -103,6 +103,10 @@ func MergeGateways(gateways ...Config) *MergedGateway {
 					}
 
 					// We have another TLS server on the same port. Can differentiate servers using SNI
+					if s.Tls == nil {
+						log.Warnf("TLS server without TLS options %s %s", name, s.String())
+						continue
+					}
 					tlsServers[s.Port.Number] = append(tlsServers[s.Port.Number], s)
 				}
 			} else {
