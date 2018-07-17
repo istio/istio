7e65cd9436f2d8a0cad3fc068dd46622508bc70c
Kent Rancourt <kent.rancourt@microsoft.com>
2018-07-17 17:01:05 -0400

    bookinfo fixes (#7109)
    * update bookinfo productpage service
    
    * update bookinfo reviews service
    
    * update routing rules
    
    * fix me after-- update image references
    
    * update e2e tests
    
    * fix buildinfo build bug from #6988

--- tests/e2e/tests/upgrade/upgrade_test.go
+++ tests/e2e/tests/upgrade/upgrade_test.go
@@ -34,8 +34,6 @@ import (
 )
 
 const (
-	u1              = "normal-user"
-	u2              = "test-user"
 	bookinfoYaml    = "samples/bookinfo/platform/kube/bookinfo.yaml"
 	bookinfoGateway = "bookinfo-gateway.yaml"
 	modelDir        = "tests/apps/bookinfo/output"
@@ -46,6 +44,14 @@ const (
 )
 
 var (
+	u1 = user{
+		username:      "normal-user",
+		sessionCookie: "eyJ1c2VyIjoibm9ybWFsLXVzZXIifQ.Di_G8A.9xNK4BEiyV0-vPOB3mWF6_PXKrw",
+	}
+	u2 = user{
+		username:      "test-user",
+		sessionCookie: "eyJ1c2VyIjoidGVzdC11c2VyIn0.Di_NWg.BQ9nq-xSfqKqsT2Rd-Uh2Hg4b0I",
+	}
 	tc                *testConfig
 	baseConfig        *framework.CommonConfig
 	targetConfig      *framework.CommonConfig
@@ -54,6 +60,11 @@ var (
 	flagTargetVersion = flag.String("target_version", "0.5.1", "Target version to upgrade to.")
 	flagSmoothCheck   = flag.Bool("smooth_check", false, "Whether to check the upgrade is smooth.")
 )
+
+type user struct {
+	username      string
+	sessionCookie string
+}
 
 type testConfig struct {
 	*framework.CommonConfig
@@ -72,7 +83,7 @@ func (t *testConfig) Setup() error {
 			return err
 		}
 		content := string(ori)
-		content = strings.Replace(content, "jason", u2, -1)
+		content = strings.Replace(content, "jason", u2.username, -1)
 		err = ioutil.WriteFile(dest, []byte(content), 0600)
 		if err != nil {
 			log.Errorf("Failed to write into new rule file %s", dest)
@@ -175,7 +186,7 @@ func setUpDefaultRouting() error {
 	return probeGateway(testRetryTimes)
 }
 
-func checkRoutingResponse(user, version, gateway, modelFile string) (int, error) {
+func checkRoutingResponse(user user, version, gateway, modelFile string) (int, error) {
 	startT := time.Now()
 	cookies := []http.Cookie{
 		{
@@ -183,8 +194,8 @@ func checkRoutingResponse(user, version, gateway, modelFile string) (int, error)
 			Value: "bar",
 		},
 		{
-			Name:  "user",
-			Value: user,
+			Name:  "session",
+			Value: user.sessionCookie,
 		},
 	}
 	resp, err := getWithCookie(fmt.Sprintf("%s/productpage", gateway), cookies)
