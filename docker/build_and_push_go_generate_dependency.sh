06454061955665f87ed2273a00303b65545e52fb
gargnupur <gargnupur@google.com>
2018-07-23 16:21:33 -0700

    Change mixer_codegen.sh to use protoc dockerfile
    Add a dockerfile for counterfeiter and go-bindata
    
    Add scripts for running counterfeiter and go-bindata using docker too
    
    Add GOPATH env variable in counterfeiter.sh file
    
    Updated ROOT calculation based on feedback

---
+++ docker/build_and_push_go_generate_dependency.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# This is a temporary script, only to be used until we have a better official
+# place and procedure for generation. PLEASE use with caution
+# (read: not for general usage).
+
+HUB=gcr.io/istio-testing
+VERSION=$(date +%Y-%m-%d)
+
+docker build --no-cache -t $HUB/go_generate_dependency:$VERSION -f Dockerfile.go_generate_dependency .
+
+gcloud auth configure-docker
+
+docker push $HUB/go_generate_dependency:$VERSION
