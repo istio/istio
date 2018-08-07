40d91b302567681804c6109589a72604f6624fb8
Costin Manolache <costin@gmail.com>
2018-08-07 15:28:33 -0700

    Add allcaps comments around the global push status

--- pilot/pkg/model/context.go
+++ pilot/pkg/model/context.go
@@ -47,6 +47,11 @@ type Environment struct {
 
 	// PushStatus holds informations during push generation. It is reset on config change, at the beginning
 	// of the pushAll. It will hold all errors and stats and possibly caches needed during the entire cache computation.
+	// DO NOT USE EXCEPT FOR TESTS AND HANDLING OF NEW CONNECTIONS.
+	// ALL USE DURING A PUSH SHOULD USE THE ONE CREATED AT THE
+	// START OF THE PUSH, THE GLOBAL ONE MAY CHANGE AND RELFECT A DIFFERENT
+	// CONFIG AND PUSH
+	// Deprecated - a local config for ads will be used instead
 	PushStatus *PushStatus
 }
 
