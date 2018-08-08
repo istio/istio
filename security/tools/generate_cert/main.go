66dc7fa9ebd6ba59c54a35ee5ef3b72d84043b47
Jianfei Hu <jianfeih@google.com>
2018-08-07 17:44:39 -0700

    Gencert supports generating cert signed by citadel.

--- security/tools/generate_cert/main.go
+++ security/tools/generate_cert/main.go
@@ -19,25 +19,33 @@ package main
 import (
 	"crypto"
 	"crypto/x509"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"os"
+	"log"
+	"os/exec"
 	"time"
 
-	"istio.io/istio/pkg/log"
 	"istio.io/istio/security/pkg/pki/util"
+	k8s "k8s.io/api/core/v1"
 )
 
-// Layout for parsing time
-const timeLayout = "Jan 2 15:04:05 2006"
+const (
+	// Layout for parsing time
+	timeLayout = "Jan 2 15:04:05 2006"
+
+	// modes supported by this tool.
+	selfSignedMode = "self-signed"
+	signerMode     = "signer"
+	citadelMode    = "citadel"
+)
 
 var (
 	host           = flag.String("host", "", "Comma-separated hostnames and IPs to generate a certificate for.")
 	validFrom      = flag.String("start-date", "", "Creation date in format of "+timeLayout)
 	validFor       = flag.Duration("duration", 365*24*time.Hour, "Duration that certificate is valid for.")
 	isCA           = flag.Bool("ca", false, "Whether this cert should be a Cerificate Authority.")
-	isSelfSigned   = flag.Bool("self-signed", false, "Whether this cerificate is self-signed.")
 	signerCertFile = flag.String("signer-cert", "", "Signer certificate file (PEM encoded).")
 	signerPrivFile = flag.String("signer-priv", "", "Signer private key file (PEM encoded).")
 	isClient       = flag.Bool("client", false, "Whether this certificate is for a client.")
@@ -45,40 +53,62 @@ var (
 	outCert        = flag.String("out-cert", "cert.pem", "Output certificate file.")
 	outPriv        = flag.String("out-priv", "priv.pem", "Output private key file.")
 	keySize        = flag.Int("key-size", 2048, "Size of the generated private key")
+	mode           = flag.String("mode", "self-signed", "Supported mode: self-signed, signer, citadel")
 )
-
-func fatalf(template string, args ...interface{}) {
-	log.Errorf(template, args)
-	os.Exit(-1)
-}
 
 func checkCmdLine() {
 	flag.Parse()
 
 	hasCert, hasPriv := len(*signerCertFile) != 0, len(*signerPrivFile) != 0
-	if *isSelfSigned {
+	switch *mode {
+	case selfSignedMode:
 		if hasCert || hasPriv {
-			fatalf("--self-signed is incompatible with --signer-cert or --signer-priv.")
+			log.Fatal("--self-signed is incompatible with --signer-cert or --signer-priv.")
 		}
-	} else {
-		if !hasCert && !hasPriv {
-			fatalf("Need --self-signed or --signer-cert and --signer-priv.")
-		} else if !(hasCert && hasPriv) {
-			fatalf("Missing --signer-cert or --signer-priv.")
+	case signerMode:
+		if !hasCert || !hasPriv {
+			log.Fatalf("Need --self-signed or --signer-cert and --signer-priv.")
 		}
+	case citadelMode:
+		if hasCert || hasPriv {
+			log.Fatal("--from-citadel is incompatible with --signer-cert or --signer-priv.")
+		}
+	default:
+		log.Fatalf("Unsupported mode %v", *mode)
 	}
 }
 
 func saveCreds(certPem []byte, privPem []byte) {
 	err := ioutil.WriteFile(*outCert, certPem, 0644)
 	if err != nil {
-		fatalf("Could not write output certificate: %s.", err)
+		log.Fatalf("Could not write output certificate: %s.", err)
 	}
 
 	err = ioutil.WriteFile(*outPriv, privPem, 0600)
 	if err != nil {
-		fatalf("Could not write output private key: %s.", err)
+		log.Fatalf("Could not write output private key: %s.", err)
 	}
+}
+
+func signCertFromCitadel() (*x509.Certificate, crypto.PrivateKey) {
+	args := []string{"get", "secret", "-n", "istio-system", "istio-ca-secret", "-o", "json"}
+	cmd := exec.Command("kubectl", args...)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatalf("Command failed error: %v\n, output\n%v\n", err, string(out))
+	}
+
+	var secret k8s.Secret
+	json.Unmarshal(out, &secret)
+	key, err := util.ParsePemEncodedKey(secret.Data["ca-key.pem"])
+	if err != nil {
+		log.Fatalf("Unrecogniazed key format from citadel %v", err)
+	}
+	cert, err := util.ParsePemEncodedCertificate(secret.Data["ca-cert.pem"])
+	if err != nil {
+		log.Fatalf("Unrecogniazed cert format from citadel %v", err)
+	}
+	return cert, key
 }
 
 func main() {
@@ -87,15 +117,20 @@ func main() {
 	var signerCert *x509.Certificate
 	var signerPriv crypto.PrivateKey
 	var err error
-	if !*isSelfSigned {
+	switch *mode {
+	case selfSignedMode:
+	case signerMode:
 		signerCert, signerPriv, err = util.LoadSignerCredsFromFiles(*signerCertFile, *signerPrivFile)
 		if err != nil {
-			log.Errora(err)
-			os.Exit(-1)
+			log.Fatalf("Failed to load signer key cert from file: %v\n", err)
 		}
+	case citadelMode:
+		signerCert, signerPriv = signCertFromCitadel()
+	default:
+		log.Fatalf("Unsupported mode %v", *mode)
 	}
 
-	certPem, privPem, err := util.GenCertKeyFromOptions(util.CertOptions{
+	opts := util.CertOptions{
 		Host:         *host,
 		NotBefore:    getNotBefore(),
 		TTL:          *validFor,
@@ -103,14 +138,14 @@ func main() {
 		SignerPriv:   signerPriv,
 		Org:          *org,
 		IsCA:         *isCA,
-		IsSelfSigned: *isSelfSigned,
+		IsSelfSigned: *mode == selfSignedMode,
 		IsClient:     *isClient,
 		RSAKeySize:   *keySize,
-	})
+	}
+	certPem, privPem, err := util.GenCertKeyFromOptions(opts)
 
 	if err != nil {
-		log.Errora(err)
-		os.Exit(-1)
+		log.Fatalf("Failed to generate certificate: %v\n", err)
 	}
 
 	saveCreds(certPem, privPem)
@@ -124,7 +159,7 @@ func getNotBefore() time.Time {
 
 	t, err := time.Parse(timeLayout, *validFrom)
 	if err != nil {
-		fatalf("Failed to parse the '-start-from' option as a time (error: %s)", err)
+		log.Fatalf("Failed to parse the '-start-from' option as a time (error: %s)\n", err)
 	}
 
 	return t
