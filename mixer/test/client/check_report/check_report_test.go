d2d7e6d8950790643e0c829742c12dd0ba270724
gargnupur <gargnupur@google.com>
2018-08-09 16:34:04 -0700

    Fix unit tests for check call where "connection.requested_server_name" is a new attribute we get from envoy Fix gomatelinter error in adapter_test.go

--- mixer/test/client/check_report/check_report_test.go
+++ mixer/test/client/check_report/check_report_test.go
@@ -122,6 +122,7 @@ const checkAttributesOkPost = `
   "target.uid": "POD222",
   "target.namespace": "XYZ222",
   "connection.mtls": false,
+  "connection.requested_server_name": "",
   "origin.ip": "[127 0 0 1]",
   "request.headers": {
      ":method": "POST",
--- mixer/test/client/check_report_large_post_request/check_report_large_post_request_test.go
+++ mixer/test/client/check_report_large_post_request/check_report_large_post_request_test.go
@@ -38,6 +38,7 @@ const checkAttributesOkPost = `
   "target.uid": "POD222",
   "target.namespace": "XYZ222",
   "connection.mtls": false,
+  "connection.requested_server_name": "",
   "origin.ip": "[127 0 0 1]",
   "request.headers": {
      ":method": "POST",
