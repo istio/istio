d0db86196a341babdf07e9a6ba62aebc664cd0f7
Nino Kodabande <nkodabande@suse.com>
2018-08-09 16:04:58 -0700

    Rename missing comment
    Co-authored-by: Utako Ueda <uueda@pivotal.io>

--- pkg/mcp/client/client.go
+++ pkg/mcp/client/client.go
@@ -56,7 +56,7 @@ type Change struct {
 
 // Updater provides configuration changes in batches of the same protobuf message type.
 type Updater interface {
-	// Update is invoked when the client receives new configuration updates
+	// Apply is invoked when the client receives new configuration updates
 	// from the server. The caller should return an error if any of the provided
 	// configuration resources are invalid or cannot be applied. The client will
 	// propagate errors back to the server accordingly.
