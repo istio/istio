aebc5565608e1fb765b16ab23191723684291d6e
gargnupur <gargnupur@google.com>
2018-08-10 11:14:35 -0700

    Fix quoting error with mixer_codegen.sh as per review

--- bin/mixer_codegen.sh
+++ bin/mixer_codegen.sh
@@ -6,7 +6,7 @@ die () {
 }
 
 WD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ROOT=$(dirname $WD)
+ROOT="$(dirname $WD)"
 
 if [ ! -e "$ROOT/Gopkg.lock" ]; then
   echo "Please run 'dep ensure' first"
@@ -183,7 +183,8 @@ if [ "$opttemplate" = true ]; then
     die "template generation failure: $err";
   fi
 
-  go run $GOPATH/src/istio.io/istio/mixer/tools/mixgen/main.go api -t $templateDS --go_out $templateHG --proto_out $templateHSP $TMPL_GEN_MAP
+  IFS=" " read -r -a TMPL_GEN_MAP_ARRAY <<< "$TMPL_GEN_MAP"
+  go run "$GOPATH/src/istio.io/istio/mixer/tools/mixgen/main.go" api -t "$templateDS" --go_out "$templateHG" --proto_out "$templateHSP" "${TMPL_GEN_MAP_ARRAY[@]}"
 
   err=$($protoc "$IMPORTS" "$TMPL_PLUGIN" "$templateHSP")
   if [ ! -z "$err" ]; then
@@ -198,7 +199,7 @@ if [ "$opttemplate" = true ]; then
   fi
 
   templateYaml=${template/.proto/.yaml}
-  go run $GOPATH/src/istio.io/istio/mixer/tools/mixgen/main.go template -d $templateSDS -o $templateYaml -n $(basename "$(dirname "${template}")")
+  go run "$GOPATH/src/istio.io/istio/mixer/tools/mixgen/main.go" template -d "$templateSDS" -o "$templateYaml" -n "$(basename "$(dirname "${template}")")"
 
   rm "$templatePG"
 
@@ -222,7 +223,8 @@ if [ "$optadapter" = true ]; then
   die "config generation failure: $err";
   fi
 
-  go run $GOPATH/src/istio.io/istio/mixer/tools/mixgen/main.go adapter -c $adapteCfdDS -o $(dirname "${file}") ${extraflags}
+  IFS=" " read -r -a extraflags_array <<< "$extraflags"
+  go run "$GOPATH/src/istio.io/istio/mixer/tools/mixgen/main.go" adapter -c "$adapteCfdDS" -o "$(dirname "${file}")" "${extraflags_array[@]}"
 
   exit 0
 fi
