914b0471845c37c78465bf8c4197866366d074ac
gargnupur <gargnupur@google.com>
2018-08-13 13:21:28 -0700

    Fix shellcheck errors

--- bin/counterfeiter.sh
+++ bin/counterfeiter.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 WD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ROOT=$(dirname $WD)
+ROOT="$(dirname "$WD")"
 
 # Ensure expected GOPATH setup
-if [ $ROOT != "${GOPATH-$HOME/go}/src/istio.io/istio" ]; then
+if [ "$ROOT" != "${GOPATH-$HOME/go}/src/istio.io/istio" ]; then
   die "Istio not found in GOPATH/src/istio.io/"
 fi
 
 gen_img=gcr.io/istio-testing/go_generate_dependency:2018-07-26
 
-docker run  -i -e GOPATH=/go:$GOPATH --rm --entrypoint counterfeiter -v $ROOT:$ROOT -w $(pwd) $gen_img $*
+docker run  -i -e GOPATH=/go:$GOPATH --rm --entrypoint counterfeiter -v "$ROOT":"$ROOT" -w "$(pwd)" "$gen_img" $*
+
+
--- bin/go-bindata.sh
+++ bin/go-bindata.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
 WD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ROOT=$(dirname $WD)
+ROOT="$(dirname "$WD")"
 
 # Ensure expected GOPATH setup
-if [ $ROOT != "${GOPATH-$HOME/go}/src/istio.io/istio" ]; then
+if [ "$ROOT" != "${GOPATH-$HOME/go}/src/istio.io/istio" ]; then
   die "Istio not found in GOPATH/src/istio.io/"
 fi
 
 gen_img=gcr.io/istio-testing/go_generate_dependency:2018-07-26
 
-docker run  -i  --rm --entrypoint go-bindata -v $ROOT:$ROOT -w $(pwd) $gen_img $*
+docker run  -i  --rm --entrypoint go-bindata -v "$ROOT":"$ROOT" -w "$(pwd)" "$gen_img" $*
+
+
+
+
