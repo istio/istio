091f1c0d92817b2cd7009e709244320dfc180e03
Tao Li <lita@google.com>
2018-08-13 16:57:57 -0700

    Fix error

--- tests/e2e/tests/pilot/authn_policy_test.go
+++ tests/e2e/tests/pilot/authn_policy_test.go
@@ -27,15 +27,31 @@ func TestMTlsWithAuthNPolicy(t *testing.T) {
 		// The whole authn test suites should be rewritten after PR #TBD for better consistency.
 		t.Skipf("Skipping %s: authn=true", t.Name())
 	}
-	// This policy enable mTLS for c and d:80.
+	// Define the default permissive global mesh resource. 
+	globalPermissive := &resource{
+	    Kind: "MeshPolicy",
+	    Name: "default",
+	}
+	// This policy will remove the permissive policy and enable mTLS mesh policy.	
+	globalCfg := &deployableConfig{	
+		Namespace:  "", // Use blank for cluster CRD.		
+ 		YamlFiles:  []string{"testdata/authn/v1alpha1/global-mtls.yaml.tmpl"},
+ 		Removes: []resource{globalPermissive},		
+ 		kubeconfig: tc.Kube.KubeConfig,		
+	}
+	// This policy disable mTLS for c and d:80.	
 	cfgs := &deployableConfig{
 		Namespace:  tc.Kube.Namespace,
 		YamlFiles:  []string{"testdata/authn/v1alpha1/authn-policy.yaml.tmpl", "testdata/authn/destination-rule.yaml.tmpl"},
 		kubeconfig: tc.Kube.KubeConfig,
 	}
+	if err := globalCfg.Setup(); err != nil {		
+ 		t.Fatal(err)		
+ 	}	
 	if err := cfgs.Setup(); err != nil {
 		t.Fatal(err)
 	}
+	defer globalCfg.Teardown()
 	defer cfgs.Teardown()
 
 	srcPods := []string{"a", "t"}
@@ -53,9 +69,9 @@ func TestMTlsWithAuthNPolicy(t *testing.T) {
 							runRetriableTest(t, cluster, testName, 15, func() error {
 								reqURL := fmt.Sprintf("http://%s%s:%s/%s", dst, domain, port, src)
 								resp := ClientRequest(cluster, src, reqURL, 1, "")
-								if src == "t" && (dst == "c" || dst == "d") {
+								if src == "t" && (dst == "b" || (dst == "d" && port == "8080")) {
 									if len(resp.ID) == 0 {
-										// t cannot talk to c nor d
+										// t cannot talk to b nor d:8080
 										return nil
 									}
 									return errAgain
