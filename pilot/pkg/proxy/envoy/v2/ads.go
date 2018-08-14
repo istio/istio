2078720b0beb4b820fd8311527c91f8281b530b3
Zhonghu Xu <xuzhonghu@huawei.com>
2018-08-14 21:57:09 +0800

    xds: fix invalid comments (#7892)
    * fix invalid comments
    
    * address comments

--- pilot/pkg/proxy/envoy/v2/ads.go
+++ pilot/pkg/proxy/envoy/v2/ads.go
@@ -178,7 +178,7 @@ type XdsConnection struct {
 
 	modelNode *model.Proxy
 
-	// Sending on this channel results in  push. We may also make it a channel of objects so
+	// Sending on this channel results in a push. We may also make it a channel of objects so
 	// same info can be sent to all clients, without recomputing.
 	pushChannel chan *XdsEvent
 
@@ -392,7 +392,7 @@ func (s *DiscoveryServer) StreamAggregatedResources(stream ads.AggregatedDiscove
 	go receiveThread(con, reqChannel, &receiveError)
 
 	for {
-		// Block until either a request is received or the ticker ticks
+		// Block until either a request is received or a push is triggered.
 		select {
 		case discReq, ok = <-reqChannel:
 			if !ok {
