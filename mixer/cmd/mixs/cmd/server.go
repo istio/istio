ce9a12761b03b68371815b171bd57bffaa761637
Jason Young <jasonyoung@google.com>
2018-08-17 10:12:54 -0700

    fix lint

--- mixer/cmd/mixs/cmd/server.go
+++ mixer/cmd/mixs/cmd/server.go
@@ -57,7 +57,8 @@ func serverCmd(info map[string]template.Info, adapters []adapter.InfoFn, printf,
 		"Max number of entries in the check result cache")
 
 	serverCmd.PersistentFlags().StringVarP(&sa.ConfigStoreURL, "configStoreURL", "", sa.ConfigStoreURL,
-		"URL of the config store. Use k8s://path_to_kubeconfig, fs:// for file system, or mcp://<address> for MCP/Galley. If path_to_kubeconfig is empty, in-cluster kubeconfig is used.")
+		"URL of the config store. Use k8s://path_to_kubeconfig, fs:// for file system, or mcp://<address> for MCP/Galley. "+
+			"If path_to_kubeconfig is empty, in-cluster kubeconfig is used.")
 
 	serverCmd.PersistentFlags().StringVarP(&sa.ConfigDefaultNamespace, "configDefaultNamespace", "", sa.ConfigDefaultNamespace,
 		"Namespace used to store mesh wide configuration.")
