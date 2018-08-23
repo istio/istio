b76260f7412d463465af7af74b544baee9d5e7e0
m1093782566 <dujun5@huawei.com>
2018-08-23 19:08:16 +0800

    move some common util functions to an individual file

--- tests/e2e/local/vagrant/install_prereqs_macos.sh
+++ tests/e2e/local/vagrant/install_prereqs_macos.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Check if homebrew is installed
-if ! brew --help > /dev/null; then
-    echo "Homebrew is not installed. Please go to https://docs.brew.sh/Installation to install Homebrew."
-    exit 1
-fi
+source ../common_macos.sh
+
+check_homebrew
 
 echo "Update homebrew..."
 brew update
@@ -22,18 +20,7 @@ else
     echo "curl exists."
 fi
 
-echo "Checking docker..."
-if ! docker --help > /dev/null; then
-    echo "docker is not installed. Install it from homebrew cask."
-    if ! brew cask install docker; then
-    	echo "Installation from brew fails. Please install it manually."
-        exit 1
-    else
-    	echo "Done."
-    fi
-else
-    echo "docker exists. Please make sure to update it to latest version."
-fi
+install_docker
 
 echo "Checking vitualbox..."
 if ! virtualbox --help > /dev/null; then
@@ -68,17 +55,6 @@ else
     vagrant version
 fi
 
-echo "Checking kubectl..."
-if ! kubectl --help > /dev/null; then
-    echo "kubectl is not installed. Installing the lastest stable release..."
-    if ! brew install kubectl; then
-    	echo "Installation from brew fails. Please install it manually."
-        exit 1
-    else
-    	echo "Done."
-    fi
-else
-    echo "kubectl exists. Please make sure to update it to latest version."
-fi
+install_kubectl
 
 echo "Prerequisite check and installation process finishes."
