9c9962424bd2c2c732e202b9c129e11b9cf0038f
Quanjie Lin <quanlin@google.com>
2018-08-23 12:08:24 -0700

    make pilotplugin_mtls_test.TestPilotPlugin test pass

--- mixer/test/client/pilotplugin_mtls/pilotplugin_mtls_test.go
+++ mixer/test/client/pilotplugin_mtls/pilotplugin_mtls_test.go
@@ -153,6 +153,7 @@ static_resources:
   "destination.service.name": "svc",
   "destination.service.namespace": "ns3",
   "destination.service.uid": "istio://ns3/services/svc",
+  "source.principal": "cluster.local/ns/default/sa/client",
   "source.uid": "kubernetes://pod2.ns2",
   "request.headers": {
      ":method": "GET",
@@ -219,7 +220,6 @@ static_resources:
 }`
 
 	// See issue https://github.com/istio/proxy/issues/1910
-	// "source.principal": "cluster.local/ns/default/sa/client",
 	// "source.user": "cluster.local/ns/default/sa/client",
 	reportAttributesOkInbound = `
 {
@@ -268,7 +268,8 @@ static_resources:
      "server": "envoy"
   },
   "response.total_size": "*",
-  "request.url_path": "/echo"
+  "request.url_path": "/echo",
+  "source.principal": "cluster.local/ns/default/sa/client"
 }`
 )
 
