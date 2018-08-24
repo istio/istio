1e2256aa439a39c0167633f3d6d3c85297dc1618
Zhonghu Xu <xuzhonghu@huawei.com>
2018-08-24 19:56:09 +0800

    pilot: agent sidecar-injector print flags (#8127)
    * sidecar-injector print flags
    
    * pilot-agent pilot-discovery request print flags

--- pilot/cmd/pilot-discovery/request.go
+++ pilot/cmd/pilot-discovery/request.go
@@ -20,6 +20,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"istio.io/istio/pilot/pkg/request"
+	"istio.io/istio/pkg/cmd"
 )
 
 var (
@@ -28,6 +29,7 @@ var (
 		Short: "Makes an HTTP request to Pilot metrics/debug endpoint",
 		Args:  cobra.MinimumNArgs(2),
 		RunE: func(c *cobra.Command, args []string) error {
+			cmd.PrintFlags(c.Flags())
 			command := &request.Command{
 				Address: "127.0.0.1:9093",
 				Client:  &http.Client{},
