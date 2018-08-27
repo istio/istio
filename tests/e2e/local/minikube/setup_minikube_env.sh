1a5b36e3abbd550032a9037d592db6b5bd4be706
JimmyCYJ <jimmychen.0102@gmail.com>
2018-08-27 16:37:52 -0700

    Remove local registry from local minikube VM environment.

--- tests/e2e/local/minikube/setup_minikube_env.sh
+++ tests/e2e/local/minikube/setup_minikube_env.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Check if port forwarding is setup
-# TODO: This ps | grep | grep is pretty hacky. Rewrite it.
-# shellcheck disable=SC2009
-if ! ps -eaf | grep "kubectl port-forward" | grep "kube-registry" | grep "5000:5000" > /dev/null; then
-    echo "Port Forwarding not setup.Trying to set it up."
-    POD=$(kubectl get po -n kube-system | grep kube-registry-v0 | awk '{print $1;}')
-    # if ... &; then ... fi is a syntax error. Dropping the ';' is correct.
-    if ! kubectl port-forward --namespace kube-system "$POD" 5000:5000 & then
-        echo "Could not set up Port Forwarding. Something wrong with Minikube setup. Please check your setup and try again."
-        exit 1
-    fi
-fi
-echo "Port Forwarding is Setup"
-
 # Check if minikube docker environment is setup.
 if ! docker info | grep "localhost:5000" > /dev/null; then
     echo "Minikube docker environment not setup. Trying to set it up."
@@ -23,3 +9,4 @@ if ! docker info | grep "localhost:5000" > /dev/null; then
     fi
 fi
 echo "Minikube docker environment is setup for this shell"
+echo "To verify that localhost:5000 is added as insecure registry, please run docker info"
