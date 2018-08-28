67838446b7772c2b2bc5dce4735f7fc9781da613
Piotr Sikora <piotrsikora@google.com>
2018-08-28 05:24:58 -0700

    Pass ProxyConfig.Concurrency to pilot-agent. (#8287)
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- pilot/pkg/kube/inject/mesh.go
+++ pilot/pkg/kube/inject/mesh.go
@@ -102,6 +102,10 @@ containers:
   - [[ .ProxyConfig.StatsdUdpAddress ]]
   - --proxyAdminPort
   - [[ .ProxyConfig.ProxyAdminPort ]]
+  [[ if gt .ProxyConfig.Concurrency 0 -]]
+  - --concurrency
+  - [[ .ProxyConfig.Concurrency ]]
+  [[ end -]]
   - --controlPlaneAuthPolicy
   - [[ .ProxyConfig.ControlPlaneAuthPolicy ]]
   env:
