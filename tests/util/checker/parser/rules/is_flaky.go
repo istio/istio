d8d14fc7c2ec710a478da0eb4f50f0063f042c24
JimmyCYJ <jimmychen.0102@gmail.com>
2018-08-28 18:35:02 -0700

    Add parser

---
+++ tests/util/checker/parser/rules/is_flaky.go
@@ -0,0 +1,54 @@
+// Copyright 2018 Istio Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rules
+
+import (
+	"go/ast"
+	"go/token"
+
+	"istio.io/istio/tests/util/checker"
+)
+
+// IsFlaky detects if annotation.isFlakyTest() is called.
+type IsFlaky struct{}
+
+// NewIsFlaky creates and returns a IsFlaky object.
+func NewIsFlaky() *IsFlaky {
+	return &IsFlaky{}
+}
+
+// GetID returns is_flaky.
+func (lr *IsFlaky) GetID() string {
+	return "is_flaky"
+}
+
+// Check verifies if aNode is not time.Sleep. If verification fails lrp creates a new report.
+func (lr *IsFlaky) Check(aNode ast.Node, fs *token.FileSet, lrp *checker.Report) {
+	if ce, ok := aNode.(*ast.CallExpr); ok {
+		if lr.matchCallExpr(ce, "annotation", "IsFlaky") {
+			lrp.AddItem(fs.Position(ce.Pos()), lr.GetID(), "annotation.IsFlaky() is found.")
+		}
+	}
+}
+
+// matchCallExpr returns true if ce matches package name pn and method name mn.
+func (lr *IsFlaky)matchCallExpr(ce *ast.CallExpr, pn string, mn string) bool {
+	if sel, ok := ce.Fun.(*ast.SelectorExpr); ok {
+		if pkg, ok := sel.X.(*ast.Ident); ok {
+			return pkg.String() == pn && sel.Sel.String() == mn
+		}
+	}
+	return false
+}
---
+++ tests/util/checker/parser/rules_matcher.go
@@ -0,0 +1,48 @@
+// Copyright 2018 Istio Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"os"
+	"strings"
+
+	"istio.io/istio/tests/util/checker"
+	"istio.io/istio/tests/util/checker/parser/rules"
+)
+
+// RulesMatcher filters out test files and detects test type.
+type RulesMatcher struct {
+}
+
+// GetRules checks path absp and decides whether absp is a test file. It returns true and rules to
+// parse the file. If path absp should be skipped, it returns false.
+// If one of the following cases meet, path absp is a valid path to test file.
+// (1) e2e test file
+// .../e2e/.../*_test.go
+// (2) integration test file
+// .../integration/.../*_test.go
+// .../integration/.../*_integ_test.go
+// .../*_integ_test.go
+// (3) unit test file
+// .../*_test.go
+func (rf *RulesMatcher) GetRules(absp string, info os.FileInfo) []checker.Rule {
+	// Skip path which is not go test file or is a directory.
+	paths := strings.Split(absp, "/")
+	if len(paths) == 0 || info.IsDir() || !strings.HasSuffix(absp, "_test.go") {
+		return []checker.Rule{}
+	}
+
+	return []checker.Rule{rules.NewIsFlaky(),}
+}
