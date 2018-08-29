6904bc0061d3cf9d590a6430d065ae75ded4074e
Martin Ostrowski <mostrowski@google.com>
2018-08-28 19:15:58 -0700

    Change coredump path, limit number of core files and add integration test (#8043)
    * Change coredump path and add integration test
    
    * Modify API with maxNumCoreDumps
    
    * Update test goldens
    
    * Review comments
    
    * readOnlyRootFilesystem true only if coredump disabled
    
    * Review comments
    
    * Update unit test goldens
    
    * Go back to original proposal of one core file, default disabled
    
    * Change core path dir to /var/lib/istio
    
    * Enable coredump for e2e pilot test
    
    * Review comments, add example

--- pilot/pkg/kube/inject/mesh.go
+++ pilot/pkg/kube/inject/mesh.go
@@ -60,7 +60,7 @@ initContainers:
 {{ if eq .EnableCoreDump true -}}
 - args:
   - -c
-  - sysctl -w kernel.core_pattern=/etc/istio/proxy/core.%e.%p.%t && ulimit -c unlimited
+  - sysctl -w kernel.core_pattern=/var/lib/istio/core.proxy && ulimit -c unlimited
   command:
   - /bin/sh
   image: {{ .InitImage }}
