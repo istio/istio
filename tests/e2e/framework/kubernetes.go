01bb928d5ebd8f0c1b56adccc74fb97922bf4454
Yossi Mesika <mesika@il.ibm.com>
2018-08-30 23:01:30 +0300

    Review comments addressed

--- tests/e2e/framework/kubernetes.go
+++ tests/e2e/framework/kubernetes.go
@@ -88,10 +88,10 @@ var (
 	authEnable         = flag.Bool("auth_enable", false, "Enable auth")
 	rbacEnable         = flag.Bool("rbac_enable", true, "Enable rbac")
 	localCluster       = flag.Bool("use_local_cluster", false,
-		"Whether the cluster is local or not (i.e. the test is running within the cluster). If running on minikube, this should be set to true.")
+		"Whether the test is running within the cluster. Will use NodePort for the Ingress. If running on minikube, this should be set to true.")
 	skipSetup                = flag.Bool("skip_setup", false, "Skip namespace creation and istio cluster setup")
 	sidecarInjectorFile      = flag.String("sidecar_injector_file", defaultSidecarInjectorFile, "Sidecar injector yaml file")
-	clusterWide              = flag.Bool("cluster_wide", false, "Run cluster wide tests")
+	clusterWide              = flag.Bool("cluster_wide", false, "Run tests in an istio-system namespace")
 	imagePullPolicy          = flag.String("image_pull_policy", "", "Specifies an override for the Docker image pull policy to be used")
 	multiClusterDir          = flag.String("cluster_registry_dir", "", "Directory name for the cluster registry config")
 	useGalleyConfigValidator = flag.Bool("use_galley_config_validator", false, "Use galley configuration validation webhook")
