0c1498c1138b5cc814fdf75d6e85c0118d1fe0dd
Weibin Lin <linweibin1@huawei.com>
2018-09-01 03:37:42 +0800

    make Host a field of VerifyFields (#7921)

--- security/pkg/pki/util/verify_cert.go
+++ security/pkg/pki/util/verify_cert.go
@@ -33,14 +33,13 @@ type VerifyFields struct {
 	IsCA        bool
 	Org         string
 	CommonName  string
+	Host        string
 }
 
 // VerifyCertificate verifies a given PEM encoded certificate by
 // - building one or more chains from the certificate to a root certificate;
 // - checking fields are set as expected.
-// TODO(incfly): make host a field of VerifyFields.
-func VerifyCertificate(privPem []byte, certChainPem []byte, rootCertPem []byte,
-	host string, expectedFields *VerifyFields) error {
+func VerifyCertificate(privPem []byte, certChainPem []byte, rootCertPem []byte, expectedFields *VerifyFields) error {
 
 	roots := x509.NewCertPool()
 	if rootCertPem != nil {
@@ -59,6 +58,7 @@ func VerifyCertificate(privPem []byte, certChainPem []byte, rootCertPem []byte,
 		return err
 	}
 
+	host := expectedFields.Host
 	san := host
 	// uri scheme is currently not supported in go VerifyOptions. We verify
 	// this uri at the end as a special case.
