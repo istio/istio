b640825cd3b64d2c040ef93c6169850e56967474
Oliver Liu <yonggangl@google.com>
2018-08-31 12:42:44 -0700

    Small fix.

--- security/pkg/server/ca/authenticator.go
+++ security/pkg/server/ca/authenticator.go
@@ -90,9 +90,6 @@ type idTokenAuthenticator struct {
 	verifier *oidc.IDTokenVerifier
 }
 
-// Temporarily disable ID token authenticator.
-// [TODO](myidpt): enable ID token authenticator when the CSR API authz can work correctly.
-/*
 func newIDTokenAuthenticator(aud string) (*idTokenAuthenticator, error) {
 	provider, err := oidc.NewProvider(context.Background(), idTokenIssuer)
 	if err != nil {
@@ -102,7 +99,6 @@ func newIDTokenAuthenticator(aud string) (*idTokenAuthenticator, error) {
 	verifier := provider.Verifier(&oidc.Config{ClientID: aud})
 	return &idTokenAuthenticator{verifier}, nil
 }
-*/
 
 func (ja *idTokenAuthenticator) authenticate(ctx context.Context) (*caller, error) {
 	bearerToken, err := extractBearerToken(ctx)
