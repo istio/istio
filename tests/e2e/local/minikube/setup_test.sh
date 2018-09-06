18f0730a4498ba8e990e283f366b7df57a9a65a5
Yangmin Zhu <ymzhu@google.com>
2018-09-06 11:31:33 -0700

    Fix the minikube e2e test broken by Error: ImagePullBackOff.

--- tests/e2e/local/minikube/setup_test.sh
+++ tests/e2e/local/minikube/setup_test.sh
@@ -5,15 +5,15 @@
 . ./setup_minikube_env.sh
 
 # Remove old images.
-read -p "Do you want to delete old docker images tagged localhost:5000/*:latest[default: no]: " -r update
+read -p "Do you want to delete old docker images tagged localhost:5000/*:e2e[default: no]: " -r update
 delete_images=${update:-"no"}
 if [[ $delete_images = *"y"* ]] || [[ $delete_images = *"Y"* ]]; then
-  docker images localhost:5000/*:latest -q | xargs docker rmi -f
+  docker images localhost:5000/*:e2e -q | xargs docker rmi -f
 fi
 
 # Make and Push images to insecure local registry on VM.
 # Set GOOS=linux to make sure linux binaries are built on macOS
 cd "$ISTIO/istio" || exit
-GOOS=linux make docker HUB=localhost:5000 TAG=latest
+GOOS=linux make docker HUB=localhost:5000 TAG=e2e
 
 echo "Setup done."
