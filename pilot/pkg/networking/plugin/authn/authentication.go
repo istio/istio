216a54e3d3e115130a1d8df73dc4a1a66792b7d3
Quanjie Lin <32855694+quanjielin@users.noreply.github.com>
2018-09-12 16:56:44 -0700

    merge sds change in authN listener change into master (#8629)
    * merge sds change in authN listener change into master
    
    * address comments

--- pilot/pkg/networking/plugin/authn/authentication.go
+++ pilot/pkg/networking/plugin/authn/authentication.go
@@ -89,7 +89,7 @@ func GetMutualTLS(policy *authn.Policy, proxyType model.NodeType) *authn.MutualT
 }
 
 // setupFilterChains sets up filter chains based on authentication policy.
-func setupFilterChains(authnPolicy *authn.Policy) []plugin.FilterChain {
+func setupFilterChains(authnPolicy *authn.Policy, serviceAccount string, sdsUdsPath string, sdsRefreshDelay *types.Duration) []plugin.FilterChain {
 	if authnPolicy == nil || len(authnPolicy.Peers) == 0 {
 		return nil
 	}
@@ -98,29 +98,6 @@ func setupFilterChains(authnPolicy *authn.Policy) []plugin.FilterChain {
 	}
 	tls := &auth.DownstreamTlsContext{
 		CommonTlsContext: &auth.CommonTlsContext{
-			TlsCertificates: []*auth.TlsCertificate{
-				{
-					CertificateChain: &core.DataSource{
-						Specifier: &core.DataSource_Filename{
-							Filename: model.AuthCertsPath + model.CertChainFilename,
-						},
-					},
-					PrivateKey: &core.DataSource{
-						Specifier: &core.DataSource_Filename{
-							Filename: model.AuthCertsPath + model.KeyFilename,
-						},
-					},
-				},
-			},
-			ValidationContextType: &auth.CommonTlsContext_ValidationContext{
-				ValidationContext: &auth.CertificateValidationContext{
-					TrustedCa: &core.DataSource{
-						Specifier: &core.DataSource_Filename{
-							Filename: model.AuthCertsPath + model.RootCertFilename,
-						},
-					},
-				},
-			},
 			// TODO(incfly): should this be {"istio", "http1.1", "h2"}?
 			// Currently it works: when server is in permissive mode, client sidecar can send tls traffic.
 			AlpnProtocols: util.ALPNHttp,
@@ -128,6 +105,29 @@ func setupFilterChains(authnPolicy *authn.Policy) []plugin.FilterChain {
 		RequireClientCertificate: &types.BoolValue{
 			Value: true,
 		},
+	}
+	if sdsUdsPath == "" {
+		tls.CommonTlsContext.ValidationContextType = model.ConstructValidationContext(model.AuthCertsPath+model.RootCertFilename, []string{} /*subjectAltNames*/)
+		tls.CommonTlsContext.TlsCertificates = []*auth.TlsCertificate{
+			{
+				CertificateChain: &core.DataSource{
+					Specifier: &core.DataSource_Filename{
+						Filename: model.AuthCertsPath + model.CertChainFilename,
+					},
+				},
+				PrivateKey: &core.DataSource{
+					Specifier: &core.DataSource_Filename{
+						Filename: model.AuthCertsPath + model.KeyFilename,
+					},
+				},
+			},
+		}
+	} else {
+		tls.CommonTlsContext.ValidationContextType = model.ConstructValidationContext(model.CARootCertPath, []string{} /*subjectAltNames*/)
+		refreshDuration, _ := types.DurationFromProto(sdsRefreshDelay)
+		tls.CommonTlsContext.TlsCertificateSdsSecretConfigs = []*auth.SdsSecretConfig{
+			model.ConstructSdsSecretConfig(serviceAccount, &refreshDuration, sdsUdsPath),
+		}
 	}
 	mtls := GetMutualTLS(authnPolicy, model.Sidecar)
 	if mtls == nil {
@@ -165,7 +165,7 @@ func setupFilterChains(authnPolicy *authn.Policy) []plugin.FilterChain {
 func (Plugin) OnInboundFilterChains(in *plugin.InputParams) []plugin.FilterChain {
 	port := in.ServiceInstance.Endpoint.ServicePort
 	authnPolicy := model.GetConsolidateAuthenticationPolicy(in.Env.IstioConfigStore, in.ServiceInstance.Service, port)
-	return setupFilterChains(authnPolicy)
+	return setupFilterChains(authnPolicy, in.ServiceInstance.ServiceAccount, in.Env.Mesh.SdsUdsPath, in.Env.Mesh.SdsRefreshDelay)
 }
 
 // CollectJwtSpecs returns a list of all JWT specs (pointers) defined the policy. This
