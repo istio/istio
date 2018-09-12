295b34667381870ba314cb5d239031d47fe8aa0f
Jonh Wendell <jonh.wendell@redhat.com>
2018-09-12 13:15:47 -0300

    Prints server info on istioctl version (#7760)
    Similar to what `kubectl version` does, this patch adds the ability
    to show server's info on the output.
    
    The default behavior is to just show client info. The flag `--remote`
    was added to show server information. Fetching server info might take
    some time, so, it's an opt-in feature rather than a default behavior.
    
    This patch also modifies the output to match the `kubectl` behavior, i.e.:
     - Dumps the version structure
     - Adds JSON and YAML output formats
    
    This is useful for debugging purposes, where one can easily find
    the version of each Istio component in the control plane that's
    actually deployed.
    
    Examples of usage:
     - mixs version -o yaml
     - galley version -o json
     - istioctl version --remote
     - istioctl version --remote --short
     - istioctl version --remote -o yaml
    
    Closes #2491.

--- pkg/version/version_test.go
+++ pkg/version/version_test.go
@@ -14,18 +14,70 @@
 
 package version
 
-import "fmt"
-import "runtime"
-import "testing"
+import (
+	"fmt"
+	"runtime"
+	"testing"
+)
+
+func TestNewBuildInfoFromOldString(t *testing.T) {
+	cases := []struct {
+		name       string
+		in         string
+		expectFail bool
+		want       BuildInfo
+	}{
+		{
+			"Correct input 1",
+			`Version: 1.0.0
+GitRevision: 3a136c90ec5e308f236e0d7ebb5c4c5e405217f4
+User: root@71a9470ea93c
+Hub: docker.io/istio
+GolangVersion: go1.10.1
+BuildStatus: Clean
+`,
+			false,
+			BuildInfo{Version: "1.0.0",
+				GitRevision:   "3a136c90ec5e308f236e0d7ebb5c4c5e405217f4",
+				User:          "root@71a9470ea93c",
+				DockerHub:     "docker.io/istio",
+				GolangVersion: "go1.10.1",
+				BuildStatus:   "Clean"},
+		},
+		{
+			"Invalid input 1",
+			"Xuxa",
+			true,
+			BuildInfo{},
+		},
+		{
+			"Invalid input 2",
+			"Xuxa:Xuxo",
+			true,
+			BuildInfo{},
+		},
+	}
+
+	for _, v := range cases {
+		t.Run(v.name, func(t *testing.T) {
+			got, err := NewBuildInfoFromOldString(v.in)
+			if v.expectFail && err == nil {
+				t.Errorf("Expected failure, got success")
+			}
+			if !v.expectFail && err != nil {
+				t.Errorf("Got %v, expected success", err)
+			}
+
+			if got != v.want {
+				t.Errorf("Got %v, expected %v", got, v.want)
+			}
+		})
+	}
+}
 
 func TestBuildInfo(t *testing.T) {
-	versionedString := fmt.Sprintf(`Version: unknown
-GitRevision: unknown
-User: unknown@unknown
-Hub: unknown
-GolangVersion: %v
-BuildStatus: unknown
-`,
+	versionedString := fmt.Sprintf(`version.BuildInfo{Version:"unknown", GitRevision:"unknown", User:"unknown", `+
+		`Host:"unknown", GolangVersion:"%s", DockerHub:"unknown", BuildStatus:"unknown"}`,
 		runtime.Version())
 
 	cases := []struct {
@@ -42,13 +94,8 @@ BuildStatus: unknown
 			DockerHub:     "DH",
 			User:          "USER",
 			BuildStatus:   "STATUS"},
-			"USER@HOST-DH-VER-GITREV-STATUS", `Version: VER
-GitRevision: GITREV
-User: USER@HOST
-Hub: DH
-GolangVersion: GOLANGVER
-BuildStatus: STATUS
-`},
+			"USER@HOST-DH-VER-GITREV-STATUS",
+			`version.BuildInfo{Version:"VER", GitRevision:"GITREV", User:"USER", Host:"HOST", GolangVersion:"GOLANGVER", DockerHub:"DH", BuildStatus:"STATUS"}`},
 
 		{"init", Info, "unknown@unknown-unknown-unknown-unknown-unknown", versionedString}}
 
