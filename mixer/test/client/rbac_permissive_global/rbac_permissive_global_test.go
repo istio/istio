f6a5517c23ec07dfd86fdf8896ca9472e91b8646
Quanjie Lin <quanlin@google.com>
2018-09-14 13:30:07 -0700

    address comment

--- mixer/test/client/rbac_permissive_global/rbac_permissive_global_test.go
+++ mixer/test/client/rbac_permissive_global/rbac_permissive_global_test.go
@@ -24,52 +24,52 @@ import (
 // Report attributes from a GET request for Rbac global permissive mode.
 const reportAttributes = `
 {
-    "check.cache_hit": false,
-    "connection.mtls": false,
-    "context.protocol": "http",
-    "context.proxy_error_code": "*",
-    "destination.ip": "[127 0 0 1]",
-    "destination.port": "*",
-    "mesh1.ip": "*",
-    "mesh2.ip": "*",
-    "mesh3.ip": "*",
-    "origin.ip": "[127 0 0 1]",
-    "quota.cache_hit": false,
-    "request.headers": {
-        ":method": "GET",
-        ":path": "/echo",
-        ":authority": "*",
-        "x-forwarded-proto": "http",
-        "x-istio-attributes": "-",
-        "x-request-id": "*"
-    },
-    "request.host": "*",
-    "request.method": "GET",
-    "request.path": "/echo",
-    "request.scheme": "http",
-    "request.size": 0,
-    "request.time": "*",
-    "request.total_size": "*",
-    "request.url_path": "/echo",
-    "request.useragent": "Go-http-client/1.1",
-    "response.code": 200,
-    "response.duration": "*",
-    "response.headers": {
-        "date": "*",
-        "content-length": "0",
-        ":status": "200",
-        "server": "envoy"
-    },
-    "response.size": 0,
-    "response.time": "*",
-    "response.total_size": "*",
-    "source.namespace": "XYZ11",
-    "source.uid": "POD11",
-    "target.name": "target-name",
-    "target.namespace": "XYZ222",
-    "target.uid": "POD222",
-    "target.user": "target-user",
-    "rbac.permissive.response_code": "403"
+  "check.cache_hit": false,
+  "connection.mtls": false,
+  "context.protocol": "http",
+  "context.proxy_error_code": "*",
+  "destination.ip": "[127 0 0 1]",
+  "destination.port": "*",
+  "mesh1.ip": "*",
+  "mesh2.ip": "*",
+  "mesh3.ip": "*",
+  "origin.ip": "[127 0 0 1]",
+  "quota.cache_hit": false,
+  "request.headers": {
+      ":method": "GET",
+      ":path": "/echo",
+      ":authority": "*",
+      "x-forwarded-proto": "http",
+      "x-istio-attributes": "-",
+      "x-request-id": "*"
+  },
+  "request.host": "*",
+  "request.method": "GET",
+  "request.path": "/echo",
+  "request.scheme": "http",
+  "request.size": 0,
+  "request.time": "*",
+  "request.total_size": "*",
+  "request.url_path": "/echo",
+  "request.useragent": "Go-http-client/1.1",
+  "response.code": 200,
+  "response.duration": "*",
+  "response.headers": {
+      "date": "*",
+      "content-length": "0",
+      ":status": "200",
+      "server": "envoy"
+  },
+  "response.size": 0,
+  "response.time": "*",
+  "response.total_size": "*",
+  "source.namespace": "XYZ11",
+  "source.uid": "POD11",
+  "target.name": "target-name",
+  "target.namespace": "XYZ222",
+  "target.uid": "POD222",
+  "target.user": "target-user",
+  "rbac.permissive.response_code": "403"
 }
 `
 
@@ -80,6 +80,8 @@ const rbacGlobalPermissiveFilter = `
       policies: {}
 `
 
+// Test senario that turn on RBAC from 0 to 1.
+// by setting permissive mode at RbacConfig level.
 func TestRbacGlobalPermissive(t *testing.T) {
 	s := env.NewTestSetup(env.RbacGlobalPermissiveTest, t)
 	s.SetFiltersBeforeMixer(rbacGlobalPermissiveFilter)
