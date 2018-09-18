082e0e790732580f472379599e9291786fe901c0
m1093782566 <dujun5@huawei.com>
2018-09-18 15:12:17 +0800

    fix shell check

--- tests/e2e/local/minikube/install_prereqs.sh
+++ tests/e2e/local/minikube/install_prereqs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPTPATH="$(cd "$(dirname "$0")" ; pwd -P)"
+SCRIPTPATH="$(cd "$(dirname "$0")" || exit ; pwd -P)"
 ROOTDIR="$(dirname "${SCRIPTPATH}")"
 # shellcheck source=tests/e2e/local/common.sh
 source "${ROOTDIR}/common.sh"
@@ -8,7 +8,7 @@ source "${ROOTDIR}/common.sh"
 case "${OSTYPE}" in
   darwin*)
     echo "We are going to install/update curl, homebrew, docker, kubectl, hyperkit, minikube in your system."
-    echo "And will do `homebrew update` to fetch the latest packages."
+    echo "And will do homebrew update to fetch the latest packages."
     if read_input_y; then
       ./install_prereqs_macos.sh
     fi;;
@@ -21,13 +21,13 @@ case "${OSTYPE}" in
     case "${DISTRO}" in
       Debian|Ubuntu)
         echo "We are going to install/update curl, apt-get, dpkg, docker, kubectl, kvm2, minikube in your system."
-        echo "And will do `apt-get update` to fetch the latest packages."
+        echo "And will do apt-get update to fetch the latest packages."
         if read_input_y; then
           ./install_prereqs_debian.sh
         fi;;
       CentOS)
         echo "We are going to install/update curl, yum, rpm, docker, kubectl, kvm2, minikube in your system."
-        echo "And will do `yum update` to fetch the latest packages."
+        echo "And will do yum update to fetch the latest packages."
         if read_input_y; then
           ./install_prereqs_centos.sh
         fi;;
