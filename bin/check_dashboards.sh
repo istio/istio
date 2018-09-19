70f64fdaa81a09ff5aae71473805c06f9275d9ab
Douglas Reid <douglas-reid@users.noreply.github.com>
2018-09-19 14:29:59 -0700

    Fix up bad merge from cherry-picks

--- bin/check_dashboards.sh
+++ bin/check_dashboards.sh
@@ -3,12 +3,7 @@ SCRIPTPATH=$( cd "$(dirname "$0")" ; pwd -P )
 ROOTDIR=$SCRIPTPATH/..
 pushd $ROOTDIR
 
-<<<<<<< HEAD
-git diff --quiet addons/grafana/dashboards/
-if [[ $? -ne 0 ]]; then
-=======
 if ! git diff --quiet install/kubernetes/helm/istio/charts/grafana/dashboards/; then
->>>>>>> 3f5810940... Use stock grafana images instead of custom docker build (#8189)
     echo "Grafana dashboards have unstaged changes, please stage before linting."
     popd
     exit 1
@@ -16,12 +11,7 @@ fi
 
 install/kubernetes/helm/istio/charts/grafana/fix_datasources.sh
 
-<<<<<<< HEAD
-git diff --quiet addons/grafana/dashboards/
-if [[ $? -ne 0 ]]; then
-=======
 if ! git diff --quiet install/kubernetes/helm/istio/charts/grafana/dashboards/; then
->>>>>>> 3f5810940... Use stock grafana images instead of custom docker build (#8189)
     echo "Grafana dashboards' datasources fixed, please add to commit."
     popd
     exit 1
