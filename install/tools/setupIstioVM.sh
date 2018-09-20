istio/istio

commit 6ece5e5fa90dc97c00bb4e7f93922d5c0eaa1ccc

ShellCheck: Enable and fix quoting related rules (#7694)
* Enable and fix SC2016 by disabling instances of it

* Enable and fix SC2046

* Enable and fix SC2068

* Enable and fix SC2191

* Enable and fix SC2206

* Enable and fix SC2086

* Use an array for GOBUILDFLAGS

* Use arrays for cluster info

* Extract join_lines_by_comma function

install/tools/setupIstioVM.sh
@@ -24,8 +24,8 @@ ISTIO_STAGING=${ISTIO_STAGING:-.}
 
 function istioVersionSource() {
   echo "Sourced ${ISTIO_STAGING}/istio.VERSION"
-  cat ${ISTIO_STAGING}/istio.VERSION
-  source ${ISTIO_STAGING}/istio.VERSION
+  cat "${ISTIO_STAGING}/istio.VERSION"
+  source "${ISTIO_STAGING}/istio.VERSION"
 }
 
 # Configure network for istio use, using DNSMasq.
@@ -39,8 +39,8 @@ function istioNetworkInit() {
   fi
 
   # Copy config files for DNS
-  chmod go+r ${ISTIO_STAGING}/kubedns
-  cp ${ISTIO_STAGING}/kubedns /etc/dnsmasq.d
+  chmod go+r "${ISTIO_STAGING}/kubedns"
+  cp "${ISTIO_STAGING}/kubedns" /etc/dnsmasq.d
   systemctl restart dnsmasq
 
   # Update DHCP - if needed
@@ -58,17 +58,17 @@ function istioInstall() {
   # Current URL for the debian files artifacts. Will be replaced by a proper apt repo.
   rm -f istio-sidecar.deb
   echo "curl -f -L ${PILOT_DEBIAN_URL}/istio-sidecar.deb > ${ISTIO_STAGING}/istio-sidecar.deb"
-  curl -f -L ${PILOT_DEBIAN_URL}/istio-sidecar.deb > ${ISTIO_STAGING}/istio-sidecar.deb
+  curl -f -L "${PILOT_DEBIAN_URL}/istio-sidecar.deb" > "${ISTIO_STAGING}/istio-sidecar.deb"
 
   # Install istio binaries
-  dpkg -i ${ISTIO_STAGING}/istio-sidecar.deb
+  dpkg -i "${ISTIO_STAGING}/istio-sidecar.deb"
 
   mkdir -p /etc/certs
 
-  cp ${ISTIO_STAGING}/*.pem /etc/certs
+  cp "${ISTIO_STAGING}/*.pem" /etc/certs
 
   # Cluster settings - the CIDR in particular.
-  cp ${ISTIO_STAGING}/cluster.env /var/lib/istio/envoy
+  cp "${ISTIO_STAGING}/cluster.env" /var/lib/istio/envoy
 
   chown -R istio-proxy /etc/certs
   chown -R istio-proxy /var/lib/istio/envoy

install/tools/setupMeshEx.sh
@@ -49,13 +49,13 @@ function istioDnsmasq() {
   local NS=${ISTIO_NAMESPACE:-istio-system}
   # Multiple tries, it may take some time until the controllers generate the IPs
   for _ in {1..20}; do
-    PILOT_IP=$(kubectl get -n $NS service istio-pilot-ilb -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    PILOT_IP=$(kubectl get -n "$NS" service istio-pilot-ilb -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
     ISTIO_DNS=$(kubectl get -n kube-system service dns-ilb -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-    MIXER_IP=$(kubectl get -n $NS service mixer-ilb -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-    CITADEL_IP=$(kubectl get -n $NS service citadel-ilb -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    MIXER_IP=$(kubectl get -n "$NS" service mixer-ilb -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    CITADEL_IP=$(kubectl get -n "$NS" service citadel-ilb -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
     if [ "${PILOT_IP}" == "" ] || [  "${ISTIO_DNS}" == "" ] || [ "${MIXER_IP}" == "" ] || [ "${CITADEL_IP}" == "" ] ; then
-      echo "Waiting for ILBs, pilot=$PILOT_IP, MIXER_IP=$MIXER_IP, CITADEL_IP=$CITADEL_IP, DNS=$ISTIO_DNS - kubectl get -n $NS service: $(kubectl get -n $NS service)"
+      echo "Waiting for ILBs, pilot=$PILOT_IP, MIXER_IP=$MIXER_IP, CITADEL_IP=$CITADEL_IP, DNS=$ISTIO_DNS - kubectl get -n $NS service: $(kubectl get -n "$NS" service)"
       sleep 30
     else
       break
@@ -97,7 +97,7 @@ function istioClusterEnv() {
   local CP_AUTH_POLICY=${CONTROL_PLANE_AUTH_POLICY:-MUTUAL_TLS}
 
   # TODO: parse it all from $(kubectl config current-context)
-  CIDR=$(gcloud container clusters describe ${K8S_CLUSTER} ${GCP_OPTS:-} --format "value(servicesIpv4Cidr)")
+  CIDR=$(gcloud container clusters describe "${K8S_CLUSTER}" "${GCP_OPTS:-}" --format "value(servicesIpv4Cidr)")
   echo "ISTIO_SERVICE_CIDR=$CIDR" > cluster.env
   echo "ISTIO_SYSTEM_NAMESPACE=$ISTIO_NS" >> cluster.env
   echo "ISTIO_CP_AUTH=$CP_AUTH_POLICY" >> cluster.env
@@ -122,11 +122,11 @@ function istio_provision_certs() {
     NS="-n $NS"
   fi
   local B64_DECODE=${BASE64_DECODE:-base64 --decode}
-  kubectl get $NS secret $CERT_NAME -o jsonpath='{.data.root-cert\.pem}' | $B64_DECODE   > root-cert.pem
+  kubectl get "$NS" secret "$CERT_NAME" -o jsonpath='{.data.root-cert\.pem}' | $B64_DECODE   > root-cert.pem
   echo "Generated root-cert.pem. It should be installed on /etc/certs"
   if [ "$ALL" == "all" ] ; then
-    kubectl get $NS secret $CERT_NAME -o jsonpath='{.data.cert-chain\.pem}' | $B64_DECODE  > cert-chain.pem
-    kubectl get $NS secret $CERT_NAME -o jsonpath='{.data.key\.pem}' | $B64_DECODE   > key.pem
+    kubectl get "$NS" secret "$CERT_NAME" -o jsonpath='{.data.cert-chain\.pem}' | $B64_DECODE  > cert-chain.pem
+    kubectl get "$NS" secret "$CERT_NAME" -o jsonpath='{.data.key\.pem}' | $B64_DECODE   > key.pem
     echo "Generated cert-chain.pem and key.pem. It should be installed on /etc/certs"
   fi
 
@@ -153,16 +153,16 @@ function istioBootstrapGCE() {
   DEFAULT_SCRIPT="install/tools/setupIstioVM.sh"
   SETUP_ISTIO_VM_SCRIPT=${SETUP_ISTIO_VM_SCRIPT:-${DEFAULT_SCRIPT}}
   echo "Making certs for service account $SA (namespace $NS)"
-  istio_provision_certs $SA $NS "root-cert-only"
+  istio_provision_certs "$SA" "$NS" "root-cert-only"
 
   for _ in {1..10}; do
     # Copy deb, helper and config files
-    if ! istioCopy $DESTINATION \
+    if ! istioCopy "$DESTINATION" \
         kubedns \
         ./*.pem \
         cluster.env \
         istio.VERSION \
-        ${SETUP_ISTIO_VM_SCRIPT}; then
+        "${SETUP_ISTIO_VM_SCRIPT}"; then
       echo "scp failed, retry in 10 sec"
       sleep 10
     else
@@ -171,10 +171,10 @@ function istioBootstrapGCE() {
     fi
   done
 
-  istioRun $DESTINATION "ls -a"
+  istioRun "$DESTINATION" "ls -a"
 
   # Run the setup script.
-  istioRun $DESTINATION "sudo bash -c -x ./setupIstioVM.sh"
+  istioRun "$DESTINATION" "sudo bash -c -x ./setupIstioVM.sh"
 }
 
 # Install required files on a VM and run the setup script.
@@ -196,16 +196,16 @@ function istioBootstrapVM() {
   DEFAULT_SCRIPT="install/tools/setupIstioVM.sh"
   SETUP_ISTIO_VM_SCRIPT=${SETUP_ISTIO_VM_SCRIPT:-${DEFAULT_SCRIPT}}
   echo "Making certs for service account $SA (namespace $NS)"
-  istio_provision_certs $SA $NS "all"
+  istio_provision_certs "$SA" "$NS" "all"
 
   for _ in {1..10}; do
     # Copy deb, helper and config files
-    if ! istioCopy $DESTINATION \
+    if ! istioCopy "$DESTINATION" \
         kubedns \
         ./*.pem \
         cluster.env \
         istio.VERSION \
-        ${SETUP_ISTIO_VM_SCRIPT}; then
+        "${SETUP_ISTIO_VM_SCRIPT}"; then
       echo "scp failed, retry in 10 sec"
       sleep 10
     else
@@ -214,10 +214,10 @@ function istioBootstrapVM() {
     fi
   done
 
-  istioRun $DESTINATION "ls -a"
+  istioRun "$DESTINATION" "ls -a"
 
   # Run the setup script.
-  istioRun $DESTINATION "sudo bash -c -x ./setupIstioVM.sh"
+  istioRun "$DESTINATION" "sudo bash -c -x ./setupIstioVM.sh"
 }
 
 
@@ -233,7 +233,7 @@ function istioCopy() {
   shift
   local FILES=$*
 
-  ${ISTIO_CP:-gcloud compute scp --recurse ${GCP_OPTS:-}} $FILES ${NAME}:
+  ${ISTIO_CP:-gcloud compute scp --recurse ${GCP_OPTS:-}} "$FILES" "${NAME}:"
 }
 
 # Run a command in a VM.
@@ -243,23 +243,23 @@ function istioRun() {
   local NAME=$1
   local CMD=$2
 
-  ${ISTIO_RUN:-gcloud compute ssh ${GCP_OPTS:-}} $NAME --command "$CMD"
+  ${ISTIO_RUN:-gcloud compute ssh ${GCP_OPTS:-}} "$NAME" --command "$CMD"
 }
 
 if [[ ${1:-} == "generateDnsmasq" ]] ; then
   istioDnsmasq
 elif [[ ${1:-} == "generateClusterEnv" ]] ; then
   shift
-  istioClusterEnv $1
+  istioClusterEnv "$1"
 elif [[ ${1:-} == "machineCerts" ]] ; then
   shift
-  istio_provision_certs $1 $2 $3
+  istio_provision_certs "$1" "$2" "$3"
 elif [[ ${1:-} == "machineSetup" ]] ; then
   shift
-  istioBootstrapVM $1
+  istioBootstrapVM "$1"
 elif [[ ${1:-} == "gceMachineSetup" ]] ; then
   shift
-  istioBootstrapGCE $1
+  istioBootstrapGCE "$1"
 else
   echo "$0 generateDnsmasq: Generate dnsmasq config files (one time)"
   echo "GCP_OPTS=\"--project P --zone Z\" $0 generateClusterEnv K8S_CLUSTER_NAME: Generate cluster range config files (one time)"

commit 5b53106999d1a199823f3fded7f82769b9272e78

Fix helper script for extending the mesh to VMs
It was broken by #7694

install/tools/setupIstioVM.sh
@@ -66,7 +66,8 @@ function istioInstall() {
 
   mkdir -p /etc/certs
 
-  cp "${ISTIO_STAGING}/*.pem" /etc/certs
+  # shellcheck disable=SC2086
+  cp ${ISTIO_STAGING}/*.pem /etc/certs
 
   # Cluster settings - the CIDR in particular.
   cp "${ISTIO_STAGING}/cluster.env" /var/lib/istio/envoy

install/tools/setupMeshEx.sh
@@ -97,7 +97,8 @@ function istioClusterEnv() {
   local CP_AUTH_POLICY=${CONTROL_PLANE_AUTH_POLICY:-MUTUAL_TLS}
 
   # TODO: parse it all from $(kubectl config current-context)
-  CIDR=$(gcloud container clusters describe "${K8S_CLUSTER}" "${GCP_OPTS:-}" --format "value(servicesIpv4Cidr)")
+  # shellcheck disable=SC2086
+  CIDR=$(gcloud container clusters describe "${K8S_CLUSTER}" ${GCP_OPTS:-} --format "value(servicesIpv4Cidr)")
   echo "ISTIO_SERVICE_CIDR=$CIDR" > cluster.env
   echo "ISTIO_SYSTEM_NAMESPACE=$ISTIO_NS" >> cluster.env
   echo "ISTIO_CP_AUTH=$CP_AUTH_POLICY" >> cluster.env
@@ -122,11 +123,14 @@ function istio_provision_certs() {
     NS="-n $NS"
   fi
   local B64_DECODE=${BASE64_DECODE:-base64 --decode}
-  kubectl get "$NS" secret "$CERT_NAME" -o jsonpath='{.data.root-cert\.pem}' | $B64_DECODE   > root-cert.pem
+  # shellcheck disable=SC2086
+  kubectl get $NS secret "$CERT_NAME" -o jsonpath='{.data.root-cert\.pem}' | $B64_DECODE   > root-cert.pem
   echo "Generated root-cert.pem. It should be installed on /etc/certs"
   if [ "$ALL" == "all" ] ; then
-    kubectl get "$NS" secret "$CERT_NAME" -o jsonpath='{.data.cert-chain\.pem}' | $B64_DECODE  > cert-chain.pem
-    kubectl get "$NS" secret "$CERT_NAME" -o jsonpath='{.data.key\.pem}' | $B64_DECODE   > key.pem
+    # shellcheck disable=SC2086
+    kubectl get $NS secret "$CERT_NAME" -o jsonpath='{.data.cert-chain\.pem}' | $B64_DECODE  > cert-chain.pem
+    # shellcheck disable=SC2086
+    kubectl get $NS secret "$CERT_NAME" -o jsonpath='{.data.key\.pem}' | $B64_DECODE   > key.pem
     echo "Generated cert-chain.pem and key.pem. It should be installed on /etc/certs"
   fi
 
@@ -233,7 +237,8 @@ function istioCopy() {
   shift
   local FILES=$*
 
-  ${ISTIO_CP:-gcloud compute scp --recurse ${GCP_OPTS:-}} "$FILES" "${NAME}:"
+  # shellcheck disable=SC2086
+  ${ISTIO_CP:-gcloud compute scp --recurse ${GCP_OPTS:-}} $FILES "${NAME}:"
 }
 
 # Run a command in a VM.