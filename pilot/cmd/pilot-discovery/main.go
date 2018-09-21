c55c375527c32edf222e571b6bf86b63a54beca2
Kevin Dillane <kdillane@amazon.com>
2018-09-20 22:19:26 -0700

    Run fmt.sh against main.go

--- pilot/cmd/pilot-discovery/main.go
+++ pilot/cmd/pilot-discovery/main.go
@@ -108,8 +108,8 @@ func init() {
 
 	// Config Controller options
 	discoveryCmd.PersistentFlags().BoolVar(&serverArgs.Config.DisableInstallCRDs, "disable-install-crds", false,
-		"Disable discovery service from verifying the existence of CRDs at startup and then installing if not detected.  " +
-		"It is recommended to be disable for highly available setups.")
+		"Disable discovery service from verifying the existence of CRDs at startup and then installing if not detected.  "+
+			"It is recommended to be disable for highly available setups.")
 	discoveryCmd.PersistentFlags().StringVar(&serverArgs.Config.FileDir, "configDir", "",
 		"Directory to watch for updates to config yaml files. If specified, the files will be used as the source of config, rather than a CRD client.")
 	discoveryCmd.PersistentFlags().StringVarP(&serverArgs.Config.ControllerOptions.WatchedNamespace, "appNamespace",
