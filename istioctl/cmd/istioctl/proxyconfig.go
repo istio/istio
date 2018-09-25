33b016fdff28bb0c30a9d1ec2522ec2996541578
fisherxu <xufei40@huawei.com>
2018-09-25 20:21:51 +0800

    fix some err return and Move from using a regex to match the entire error text

--- istioctl/cmd/istioctl/proxyconfig.go
+++ istioctl/cmd/istioctl/proxyconfig.go
@@ -241,12 +241,12 @@ func handleNamespace() string {
 func setupConfigdumpEnvoyConfigWriter(podName, podNamespace string, out io.Writer) (*configdump.ConfigWriter, error) {
 	kubeClient, err := clientExecFactory(kubeconfig, configContext)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to create k8s client: %v", err)
 	}
 	path := "config_dump"
 	debug, err := kubeClient.EnvoyDo(podName, podNamespace, "GET", path, nil)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to execute command on envoy: %v", err)
 	}
 	cw := &configdump.ConfigWriter{Stdout: out}
 	err = cw.Prime(debug)
@@ -261,12 +261,12 @@ func setupConfigdumpEnvoyConfigWriter(podName, podNamespace string, out io.Write
 func setupClustersEnvoyConfigWriter(podName, podNamespace string, out io.Writer) (*clusters.ConfigWriter, error) {
 	kubeClient, err := clientExecFactory(kubeconfig, configContext)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to create k8s client: %v", err)
 	}
 	path := "clusters?format=json"
 	debug, err := kubeClient.EnvoyDo(podName, podNamespace, "GET", path, nil)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to execute command on envoy: %v", err)
 	}
 	cw := &clusters.ConfigWriter{Stdout: out}
 	err = cw.Prime(debug)
