79076a78d70f40252d615cf7878ae97d55ab3b07
lichuqiang <lichuqiang@huawei.com>
2018-09-26 09:34:32 +0800

    Refactor status syncer to remove ingress internal dependency (#8798)
    * Refactor status syncer to remove ingress internal dependency
    
    * remove ingress dependency

--- pilot/pkg/config/kube/ingress/status_test.go
+++ pilot/pkg/config/kube/ingress/status_test.go
@@ -15,14 +15,27 @@
 package ingress
 
 import (
+	"os"
 	"testing"
+	"time"
 
+	"k8s.io/api/core/v1"
 	extensions "k8s.io/api/extensions/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/ingress/core/pkg/ingress/annotations/class"
+	"k8s.io/client-go/kubernetes/fake"
 
 	meshconfig "istio.io/api/mesh/v1alpha1"
+	"istio.io/istio/pilot/pkg/model"
 	"istio.io/istio/pilot/pkg/serviceregistry/kube"
+)
+
+var (
+	pod           = "test"
+	serviceIP     = "1.2.3.4"
+	hostname      = "foo.bar.com"
+	nodeIP        = "10.0.0.2"
+	testNamespace = "test"
+	resync        = time.Second
 )
 
 func makeAnnotatedIngress(annotation string) *extensions.Ingress {
@@ -37,6 +50,98 @@ func makeAnnotatedIngress(annotation string) *extensions.Ingress {
 			},
 		},
 	}
+}
+
+func makeFakeClient() *fake.Clientset {
+	return fake.NewSimpleClientset(
+		&v1.PodList{Items: []v1.Pod{
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test",
+					Namespace: testNamespace,
+					Labels: map[string]string{
+						"lable_sig": "test",
+					},
+				},
+				Spec: v1.PodSpec{
+					NodeName: "foo_node",
+				},
+				Status: v1.PodStatus{
+					Phase: v1.PodRunning,
+				},
+			},
+		}},
+		&v1.ServiceList{Items: []v1.Service{
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "istio-ingress",
+					Namespace: testNamespace,
+				},
+				Status: v1.ServiceStatus{
+					LoadBalancer: v1.LoadBalancerStatus{
+						Ingress: []v1.LoadBalancerIngress{{
+							IP: serviceIP,
+						}},
+					},
+				},
+			},
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "istio-ingress-hostname",
+					Namespace: testNamespace,
+				},
+				Status: v1.ServiceStatus{
+					LoadBalancer: v1.LoadBalancerStatus{
+						Ingress: []v1.LoadBalancerIngress{{
+							Hostname: hostname,
+						}},
+					},
+				},
+			},
+		}},
+		&v1.NodeList{Items: []v1.Node{
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "foo_node",
+				},
+				Status: v1.NodeStatus{
+					Addresses: []v1.NodeAddress{
+						{
+							Type:    v1.NodeExternalIP,
+							Address: nodeIP,
+						},
+					},
+				},
+			},
+		}},
+	)
+}
+
+func makeStatusSyncer(t *testing.T, client *fake.Clientset) (*StatusSyncer, error) {
+	mesh := model.DefaultMeshConfig()
+	mesh.IngressService = "istio-ingress"
+
+	oldEnvs := setAndRestoreEnv(t, map[string]string{"POD_NAME": pod, "POD_NAMESPACE": testNamespace})
+	// Restore env settings
+	defer setAndRestoreEnv(t, oldEnvs)
+
+	return NewStatusSyncer(&mesh, client, testNamespace, kube.ControllerOptions{
+		WatchedNamespace: testNamespace,
+		ResyncPeriod:     resync,
+	})
+}
+
+// setAndRestoreEnv set the envs with given value, and return the old setting.
+func setAndRestoreEnv(t *testing.T, inputs map[string]string) map[string]string {
+	oldEnvs := map[string]string{}
+	for k, v := range inputs {
+		oldEnvs[k] = os.Getenv(k)
+		if err := os.Setenv(k, v); err != nil {
+			t.Error(err)
+		}
+	}
+
+	return oldEnvs
 }
 
 // TestConvertIngressControllerMode ensures that ingress controller mode is converted to the k8s ingress status syncer's
@@ -83,9 +188,64 @@ func TestConvertIngressControllerMode(t *testing.T) {
 		ingressClass, defaultIngressClass := convertIngressControllerMode(c.Mode, "istio")
 
 		ing := makeAnnotatedIngress(c.Annotation)
-		if ignore := class.IsValid(ing, ingressClass, defaultIngressClass); ignore != c.Ignore {
+		if ignore := classIsValid(ing, ingressClass, defaultIngressClass); ignore != c.Ignore {
 			t.Errorf("convertIngressControllerMode(%q, %q), with Ingress annotation %q => "+
 				"Got ignore %v, want %v", c.Mode, "istio", c.Annotation, c.Ignore, ignore)
 		}
+	}
+}
+
+func TestRunningAddressesWithService(t *testing.T) {
+	client := makeFakeClient()
+	syncer, err := makeStatusSyncer(t, client)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	address, err := syncer.runningAddresses()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(address) != 1 || address[0] != serviceIP {
+		t.Errorf("Address is not correctly set to service ip")
+	}
+}
+
+func TestRunningAddressesWithHostname(t *testing.T) {
+	client := makeFakeClient()
+	syncer, err := makeStatusSyncer(t, client)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	syncer.ingressService = "istio-ingress-hostname"
+
+	address, err := syncer.runningAddresses()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(address) != 1 || address[0] != hostname {
+		t.Errorf("Address is not correctly set to hostname")
+	}
+}
+
+func TestRunningAddressesWithPod(t *testing.T) {
+	client := makeFakeClient()
+	syncer, err := makeStatusSyncer(t, client)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	syncer.ingressService = ""
+
+	address, err := syncer.runningAddresses()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(address) != 1 || address[0] != nodeIP {
+		t.Errorf("Address is not correctly set to node ip")
 	}
 }
