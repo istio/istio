9a6d9a88cb8d158975fecbb6bc57119164ebec90
stewart-yu <yuchuanjian@huawei.com>
2018-09-28 08:48:06 +0800

    fix boilerplate error in /bin and /prow

--- downloadIstio.sh
+++ downloadIstio.sh
@@ -1,14 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+
+# Copyright 2018 Istio Authors
 #
-# Early version of a downloader/installer for Istio
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# This file will be fetched as: curl -L https://git.io/getIstio | sh -
-# so it should be pure bourne shell, not bash (and not reference other scripts)
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
-# The script fetches the latest STABLE Istio release and untars it.
-#
-# release/downloadIstioCandidate.sh lets users override the ISTIO_VERSION
-# and is updated more often.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # DO NOT UPDATE THIS VERSION OR SCRIPT LIGHTLY - THIS IS THE "STABLE" VERSION
 ISTIO_VERSION="1.0.2"
