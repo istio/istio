5a6a1b8a9bd259463e7b0d1237546f939bfdd352
Rich Curran <rcurran@cisco.com>
2018-10-05 15:23:40 -0400

    Multi-Cluster: Mixer kubernetes support (#8536)

--- mixer/adapter/kubernetesenv/cache_test.go
+++ mixer/adapter/kubernetesenv/cache_test.go
@@ -48,8 +48,8 @@ func TestClusterInfoCache_Pod(t *testing.T) {
 
 	for _, v := range tests {
 		t.Run(v.name, func(tt *testing.T) {
-			c := newCacheController(clientset, 0, test.NewEnv(t))
 			stopCh := make(chan struct{})
+			c := newCacheController(clientset, 0, test.NewEnv(t), stopCh)
 			defer close(stopCh)
 			go c.Run(stopCh)
 			if !cache.WaitForCacheSync(stopCh, c.HasSynced) {
