b412d596a1dcadba0a67fc7c76b9eca79b12ebf8
Jianfei Hu <jianfeih@google.com>
2018-10-15 22:07:44 -0700

    Pilot agent changes to redirect Kubernetes liveness/readiness check. (#9204)
    * Complete redirection in pilot-agent.
    
    * Protoype work by manual injecting.
    
    * Wrap up the changes for flag definition.
    
    * fix lint.
    
    * use one cmd argument.
    
    * Add server_test.go for unit test.
    
    * add license in the server_test.go
    
    * fix lint.
    
    * Complete redirection in pilot-agent.
    
    * Protoype work by manual injecting.
    
    * Wrap up the changes for flag definition.
    
    * fix lint.
    
    * use one cmd argument.
    
    * Add server_test.go for unit test.
    
    * add license in the server_test.go
    
    * fix lint.
    
    * doc clarify and var rename.
    
    * lint fixing.

--- pilot/cmd/pilot-agent/status/server.go
+++ pilot/cmd/pilot-agent/status/server.go
@@ -15,33 +15,54 @@
 package status
 
 import (
+	"context"
 	"fmt"
 	"net"
 	"net/http"
-
-	"context"
 	"os"
+	"strconv"
+	"strings"
 	"sync"
+	"time"
 
 	"istio.io/istio/pilot/cmd/pilot-agent/status/ready"
 	"istio.io/istio/pkg/log"
 )
 
 const (
+	// readyPath is for the pilot agent readiness itself.
 	readyPath = "/healthz/ready"
+	// appReadinessPath is the path handled by pilot agent for application's readiness probe.
+	appReadinessPath = "/app/ready"
+	// appLivenessPath is the path handled by pilot agent for application's liveness probe.
+	appLivenessPath = "/app/live"
 )
+
+// AppProbeInfo defines the information for Pilot agent to take over application probing.
+type AppProbeInfo struct {
+	Path string
+	Port uint16
+}
 
 // Config for the status server.
 type Config struct {
 	StatusPort       uint16
 	AdminPort        uint16
 	ApplicationPorts []uint16
+	// AppReadinessURL specifies the path, including the port to take over Kubernetes readiness probe.
+	// This allows Kubernetes probing to work even mTLS is turned on for the workload.
+	AppReadinessURL string
+	// AppLivenessURL specifies the path, including the port to take over Kubernetes liveness probe.
+	// This allows Kubernetes probing to work even mTLS is turned on for the workload.
+	AppLivenessURL string
 }
 
 // Server provides an endpoint for handling status probes.
 type Server struct {
 	statusPort          uint16
 	ready               *ready.Probe
+	appLiveURL          string
+	appReadyURL         string
 	mutex               sync.Mutex
 	lastProbeSuccessful bool
 }
@@ -49,7 +70,9 @@ type Server struct {
 // NewServer creates a new status server.
 func NewServer(config Config) *Server {
 	return &Server{
-		statusPort: config.StatusPort,
+		statusPort:  config.StatusPort,
+		appLiveURL:  config.AppLivenessURL,
+		appReadyURL: config.AppReadinessURL,
 		ready: &ready.Probe{
 			AdminPort:        config.AdminPort,
 			ApplicationPorts: config.ApplicationPorts,
@@ -64,10 +87,27 @@ func (s *Server) Run(ctx context.Context) {
 	// Add the handler for ready probes.
 	http.HandleFunc(readyPath, s.handleReadyProbe)
 
+	// TODO: we require non empty url to take over the health check. Make sure this is consistent in injector.
+	if s.appReadyURL != "" {
+		log.Infof("Pilot agent takes over readiness probe, path %v", s.appReadyURL)
+		http.HandleFunc(appReadinessPath, s.handleAppReadinessProbe)
+	}
+
+	if s.appLiveURL != "" {
+		log.Infof("Pilot agent takes over liveness probe, path %v", s.appLiveURL)
+		http.HandleFunc(appLivenessPath, s.handleAppLivenessProbe)
+	}
+
 	l, err := net.Listen("tcp", fmt.Sprintf(":%d", s.statusPort))
 	if err != nil {
 		log.Errorf("Error listening on status port: %v", err.Error())
 		return
+	}
+	// for testing.
+	if s.statusPort == 0 {
+		addrs := strings.Split(l.Addr().String(), ":")
+		allocatedPort, _ := strconv.Atoi(addrs[len(addrs)-1])
+		s.statusPort = uint16(allocatedPort)
 	}
 	defer l.Close()
 
@@ -100,4 +140,39 @@ func (s *Server) handleReadyProbe(w http.ResponseWriter, _ *http.Request) {
 		}
 		s.lastProbeSuccessful = true
 	}
+}
+
+func (s *Server) handleAppReadinessProbe(w http.ResponseWriter, req *http.Request) {
+	requestStatusCode(fmt.Sprintf("http://127.0.0.1%s", s.appReadyURL), w, req)
+}
+
+func (s *Server) handleAppLivenessProbe(w http.ResponseWriter, req *http.Request) {
+	requestStatusCode(fmt.Sprintf("http://127.0.0.1%s", s.appLiveURL), w, req)
+}
+
+func requestStatusCode(appURL string, w http.ResponseWriter, req *http.Request) {
+	httpClient := &http.Client{
+		// TODO: figure out the appropriate timeout?
+		Timeout: 10 * time.Second,
+	}
+
+	appReq, err := http.NewRequest(req.Method, appURL, req.Body)
+	for key, value := range req.Header {
+		appReq.Header[key] = value
+	}
+
+	if err != nil {
+		log.Errorf("Failed to copy request to probe app %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	response, err := httpClient.Do(appReq)
+	if err != nil {
+		log.Errorf("Request to probe app failed: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// We only write the status code to the response.
+	w.WriteHeader(response.StatusCode)
 }
