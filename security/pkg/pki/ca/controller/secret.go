0ae5e7beb29ccbcea7ac2752331daa77a3a02769
Oliver Liu <yonggangl@google.com>
2018-10-19 08:31:42 -0700

    Fix recreation of the secret.

--- security/pkg/pki/ca/controller/secret.go
+++ security/pkg/pki/ca/controller/secret.go
@@ -286,8 +286,8 @@ func (sc *SecretController) scrtDeleted(obj interface{}) {
 	}
 
 	saName := scrt.Annotations[ServiceAccountNameAnnotationKey]
-	if sa, _ := sc.core.ServiceAccounts(scrt.GetNamespace()).Get(saName, metav1.GetOptions{}); sa != nil {
-		log.Errorf("Re-create deleted Istio secret for existing service account.")
+	if sa, error := sc.core.ServiceAccounts(scrt.GetNamespace()).Get(saName, metav1.GetOptions{}); error == nil {
+		log.Errorf("Re-create deleted Istio secret for existing service account %s", sa.Name)
 		sc.upsertSecret(saName, scrt.GetNamespace())
 		sc.monitoring.SecretDeletion.Inc()
 	}
