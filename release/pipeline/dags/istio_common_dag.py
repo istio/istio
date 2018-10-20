9b37e4bbb51b57a1bc81e103344e313a833d91ed
Krishna Pagadala <rkrishnap@google.com>
2018-10-20 00:28:45 +0000

    add comment on how to avoid future incompatibility

--- release/pipeline/dags/istio_common_dag.py
+++ release/pipeline/dags/istio_common_dag.py
@@ -102,6 +102,9 @@ def getBashSettingsTemplate(extra_param_lst=[]):
                 gsutil -q cp "gs://${CB_GCS_RELEASE_TOOLS_PATH}/gcb_env.sh" "/tmp/gcb_env.sh"
                 source    "/tmp/gcb_env.sh"
                 gsutil -q cp "/tmp/gcb_env.sh" "gs://${CB_GCS_RELEASE_TOOLS_PATH}/"
+                # cloning master allows us to use master code to do builds for all releases, if this needs to
+                # be changed because of future incompatible changes
+                # we just need to clone the compatible SHA here and in get_commit.template.json
                 git clone "https://github.com/${CB_GITHUB_ORG}/istio.git" "istio-code" -b "master" --depth 1
                 # use code from branch
                 cp istio-code/release/pipeline/*sh istio-code/release/gcb/json_parse_shared.sh istio-code/release/gcb/*json .
