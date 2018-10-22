d6af89421bf990fa9526eee4adf30b422459b528
Andy Lai <hklai@google.com>
2018-10-22 16:52:38 -0700

    Simplify modify_values.sh

--- release/gcb/modify_values.sh
+++ release/gcb/modify_values.sh
@@ -12,17 +12,16 @@ source "/workspace/gcb_env.sh"
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$ROOT"
 
-function fix_values_yaml_worker() {
-  local unzip_cmd
-  unzip_cmd="$1"
-  local zip_cmd
-  zip_cmd="$2"
-  local folder_name
-  folder_name="$3"
-  local tarball_name
-  tarball_name="$4"
-  local gcs_folder_path
-  gcs_folder_path="$5"
+function fix_values_yaml() {
+  local gcs_folder_path="gs://${CB_GCS_BUILD_PATH}"
+  local tarball_name="$1"
+  if [[ ${tarball_name} == *.zip ]]; then
+    local unzip_cmd="unzip -q"
+    local zip_cmd="zip -q -r"
+  else
+    local unzip_cmd="tar -zxf"
+    local zip_cmd="tar -zcf"
+  fi
 
   gsutil -q cp "${gcs_folder_path}/${tarball_name}" .
   eval    "$unzip_cmd"     "${tarball_name}"
@@ -33,18 +32,12 @@ function fix_values_yaml_worker() {
 
   eval "$zip_cmd" "${tarball_name}" "${folder_name}"
   sha256sum       "${tarball_name}" > "${tarball_name}.sha256"
-  rm -rf                            "${folder_name}"
+  # Save the modified files for later use.
+  mv  "${folder_name}"   $(echo $tarball_name | cut -f1 -d".")
 
   gsutil -q cp "${tarball_name}"        "${gcs_folder_path}/${tarball_name}"
   gsutil -q cp "${tarball_name}.sha256" "${gcs_folder_path}/${tarball_name}.sha256"
   echo "DONE fixing  ${gcs_folder_path}/${tarball_name} with hub: ${CB_DOCKER_HUB} tag: ${CB_VERSION}"
-}
-
-function fix_values_yaml() {
-  # called with params as shown below
-  # fix_values_yaml unzip_cmd zip_cmd folder_name tarball_name
-
-  fix_values_yaml_worker "$1" "$2" "$3" "$4" "gs://${CB_GCS_BUILD_PATH}"
 }
 
 mkdir modification-tmp
@@ -54,11 +47,11 @@ pwd
 
 folder_name="istio-${CB_VERSION}"
 # Linux
-fix_values_yaml     "tar -zxf" "tar -zcf" "${folder_name}" "${folder_name}-linux.tar.gz"
+fix_values_yaml  "${folder_name}-linux.tar.gz"
 # Mac
-fix_values_yaml     "tar -zxf" "tar -zcf" "${folder_name}" "${folder_name}-osx.tar.gz"
+fix_values_yaml  "${folder_name}-osx.tar.gz"
 # Windows
-fix_values_yaml    "unzip -q" "zip -q -r" "${folder_name}" "${folder_name}-win.zip"
+fix_values_yaml  "${folder_name}-win.zip"
 
 #filename | sha256 hash
 #-------- | -----------
