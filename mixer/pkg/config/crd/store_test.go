7a7a90802b4833e3ff28d2a76dad9d4a7ab88481
Yossi Mesika <mesika@il.ibm.com>
2018-10-29 21:09:33 +0200

    Fix a data race accessing the callCount (#9497) (#9580)

--- mixer/pkg/config/crd/store_test.go
+++ mixer/pkg/config/crd/store_test.go
@@ -499,8 +499,11 @@ func TestCrdsRetryAsynchronouslyStoreClose(t *testing.T) {
 		},
 	}
 	callCount := 0
+	mutex := sync.RWMutex{}
 	fakeDiscovery.AddReactor("get", "resource", func(k8stesting.Action) (bool, runtime.Object, error) {
+		mutex.Lock()
 		callCount++
+		mutex.Unlock()
 		return true, nil, nil
 	})
 
@@ -516,7 +519,9 @@ func TestCrdsRetryAsynchronouslyStoreClose(t *testing.T) {
 	time.Sleep(30 * time.Millisecond)
 	s.Stop()
 	time.Sleep(30 * time.Millisecond)
+	mutex.RLock()
 	if callCount > 4 {
 		t.Errorf("got %v, want no more than 4 calls", callCount)
 	}
+	mutex.RUnlock()
 }
