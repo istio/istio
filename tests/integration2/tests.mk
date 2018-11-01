f2aeeb9ee8acece0a31d70b8e11f2ff8885d7e76
Andy Lai <hklai@google.com>
2018-10-31 20:03:12 -0700

    Update makefile.

--- tests/integration2/tests.mk
+++ tests/integration2/tests.mk
@@ -30,8 +30,22 @@ ifneq ($(KUBECONFIG),)
 endif
 
 # This is a useful debugging target for testing everything.
-.PHONY: test.integration.all
+#.PHONY: test.integration.all
 test.integration.all: test.integration test.integration.kube
+
+# Generate integration test targets for local environment.
+test.integration.%:
+	$(GO) test -p 1 ${T} ./tests/integration2/$*/... --istio.test.env local
+
+# Generate integration test targets for kubernetes environment.
+test.integration.%.kube:
+	$(GO) test -p 1 ${T} ./tests/integration2/$*/... ${_INTEGRATION_TEST_WORKDIR_FLAG} ${_INTEGRATION_TEST_LOGGING_FLAG} \
+	--istio.test.env kubernetes \
+	--istio.test.kube.config ${INTEGRATION_TEST_KUBECONFIG} \
+	--istio.test.kube.deploy \
+	--istio.test.kube.helm.values global.hub=${HUB},global.tag=${TAG} \
+	${_INTEGRATION_TEST_INGRESS_FLAG}
+
 
 JUNIT_UNIT_TEST_XML ?= $(ISTIO_OUT)/junit_unit-tests.xml
 JUNIT_REPORT = $(shell which go-junit-report 2> /dev/null || echo "${ISTIO_BIN}/go-junit-report")
@@ -40,6 +54,7 @@ JUNIT_REPORT = $(shell which go-junit-report 2> /dev/null || echo "${ISTIO_BIN}/
 TEST_PACKAGES = $(shell go list ./tests/integration2/... | grep -v /qualification | grep -v /examples)
 
 # All integration tests targeting local environment.
+.PHONY: test.integration
 test.integration: | $(JUNIT_REPORT)
 	mkdir -p $(dir $(JUNIT_UNIT_TEST_XML))
 	set -o pipefail; \
@@ -47,6 +62,7 @@ test.integration: | $(JUNIT_REPORT)
 	2>&1 | tee >($(JUNIT_REPORT) > $(JUNIT_UNIT_TEST_XML))
 
 # All integration tests targeting Kubernetes environment.
+.PHONY: test.integration.kube
 test.integration.kube: | $(JUNIT_REPORT)
 	mkdir -p $(dir $(JUNIT_UNIT_TEST_XML))
 	set -o pipefail; \
