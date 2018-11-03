9ccc2944abf4b3bddea5c9ea7cbd83b734612850
Jason Young <jasonyoung@google.com>
2018-11-02 18:56:34 -0700

    wait for supported resource types to be ready before starting galley (#9702)
    * wait for CRD to be ready before starting galley
    
    fixes https://github.com/istio/istio/issues/9491
    
    * add --disableCRDReadyCheck flag
    
    * fix printf format
    
    * dep ensure
    
    * refactor to check generic k8s resources
    
    * fix DisableResourceReadyCheck flag
    
    * remove crd from var names

--- galley/cmd/galley/cmd/root.go
+++ galley/cmd/galley/cmd/root.go
@@ -142,6 +142,8 @@ func GetRootCmd(args []string, printf, fatalf shared.FormatFn) *cobra.Command {
 		"Path to the mesh config file")
 	rootCmd.PersistentFlags().StringVar(&serverArgs.DomainSuffix, "domain", serverArgs.DomainSuffix,
 		"DNS domain suffix")
+	rootCmd.PersistentFlags().BoolVar(&serverArgs.DisableResourceReadyCheck, "disableResourceReadyCheck", serverArgs.DisableResourceReadyCheck,
+		"Disable resource readiness checks. This allows Galley to start if not all resource types are supported")
 
 	serverArgs.IntrospectionOptions.AttachCobraFlags(rootCmd)
 
