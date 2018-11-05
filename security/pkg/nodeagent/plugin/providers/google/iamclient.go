94e8a1f999bf4c94324888d8599e8b2422287636
Quanjie Lin <quanlin@google.com>
2018-11-05 10:11:01 -0800

    add comment

--- security/pkg/nodeagent/plugin/providers/google/iamclient.go
+++ security/pkg/nodeagent/plugin/providers/google/iamclient.go
@@ -70,6 +70,8 @@ func NewPlugin() plugin.Plugin {
 
 // ExchangeToken exchange oauth access token from trusted domain and k8s sa jwt.
 func (p Plugin) ExchangeToken(ctx context.Context, trustedDomain, k8sSAjwt string) (string /*access token*/, time.Time /*expireTime*/, error) {
+	// The resource name of the service account for which the credentials are requested, in the following format: projects/-/serviceAccounts/{ACCOUNT_EMAIL_OR_UNIQUEID}
+	// Similar to https://cloud.google.com/iam/credentials/reference/rest/v1/projects.serviceAccounts/generateAccessToken
 	req := &iam.GenerateIdentityBindingAccessTokenRequest{
 		Name:  fmt.Sprintf("projects/-/serviceAccounts/%s", trustedDomain),
 		Scope: scope,
