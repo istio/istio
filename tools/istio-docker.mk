6982138a72cabe9e646fb79fbd7821a7202716c5
Quanjie Lin <quanlin@google.com>
2018-11-05 11:55:51 -0800

    add nodeagent to docker_targets

--- tools/istio-docker.mk
+++ tools/istio-docker.mk
@@ -22,7 +22,7 @@
 docker: build test-bins docker.all
 
 DOCKER_TARGETS:=docker.pilot docker.proxy_debug docker.proxytproxy docker.proxyv2 docker.app docker.test_policybackend \
-	docker.proxy_init docker.mixer docker.citadel docker.galley docker.sidecar_injector docker.kubectl
+	docker.proxy_init docker.mixer docker.citadel docker.galley docker.sidecar_injector docker.kubectl docker.node-agent-k8s
 
 $(ISTIO_DOCKER) $(ISTIO_DOCKER_TAR):
 	mkdir -p $@
