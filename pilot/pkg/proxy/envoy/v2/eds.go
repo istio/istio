cbb1fcf6c88cef67cbea3a01676782d230d113fd
Zhonghu Xu <xuzhonghu@huawei.com>
2018-11-07 04:00:52 +0800

    set endpoint.LocalityLbEndpoints LoadBalancingWeight (#9627)
    * set endpoint.LocalityLbEndpoints LoadBalancingWeight
    
    * fix ut

--- pilot/pkg/proxy/envoy/v2/eds.go
+++ pilot/pkg/proxy/envoy/v2/eds.go
@@ -254,6 +254,9 @@ func (s *DiscoveryServer) updateClusterInc(push *model.PushContext, clusterName 
 	}
 	locEps := make([]endpoint.LocalityLbEndpoints, 0, len(localityEpMap))
 	for _, locLbEps := range localityEpMap {
+		locLbEps.LoadBalancingWeight = &types.UInt32Value{
+			Value: uint32(len(locLbEps.LbEndpoints)),
+		}
 		locEps = append(locEps, *locLbEps)
 	}
 
@@ -580,6 +583,9 @@ func localityLbEndpointsFromInstances(instances []*model.ServiceInstance) []endp
 	}
 	out := make([]endpoint.LocalityLbEndpoints, 0, len(localityEpMap))
 	for _, locLbEps := range localityEpMap {
+		locLbEps.LoadBalancingWeight = &types.UInt32Value{
+			Value: uint32(len(locLbEps.LbEndpoints)),
+		}
 		out = append(out, *locLbEps)
 	}
 	return out
