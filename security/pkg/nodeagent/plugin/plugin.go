8905f672c43328e5fd67c37c5654c2aad56fe046
Quanjie Lin <32855694+quanjielin@users.noreply.github.com>
2018-11-05 18:59:36 -0800

    thread authn provider specific plugins into nodeagent (#9757)
    * thread plugins into nodeagent
    
    * rename

--- security/pkg/nodeagent/plugin/plugin.go
+++ security/pkg/nodeagent/plugin/plugin.go
@@ -19,7 +19,12 @@ import (
 	"time"
 )
 
-// Plugin provides common interfaces so that authN providers could choose to implement their specific logic.
+const (
+	// GoogleIAM is the name of the google IAM plugin.
+	GoogleIAM = "GoogleIAM"
+)
+
+// Plugin provides common interfaces so that authentication providers could choose to implement their specific logic.
 type Plugin interface {
 	ExchangeToken(context.Context, string, string) (string, time.Time, error)
 }
