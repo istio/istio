799d16be8dac8a9c8adbfccd6ba67f2bcc882e30
ozben <ozben@google.com>
2018-11-09 14:58:30 -0800

    Move Galley validation integration test to its own folder.

--- tests/integration2/galley/validation/data.gen.go
+++ tests/integration2/galley/validation/data.gen.go
@@ -12,6 +12,6 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-package galley
+package validation
 
 //go:generate go-bindata --nocompress --nometadata --pkg galley -o data.gen_test.go testdata/...
--- tests/integration2/galley/validation/main_test.go
+++ tests/integration2/galley/validation/main_test.go
@@ -12,7 +12,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-package galley
+package validation
 
 import (
 	"testing"
@@ -21,5 +21,5 @@ import (
 )
 
 func TestMain(m *testing.M) {
-	framework.Run("galley_test", m)
+	framework.Run("galley_validation_test", m)
 }
