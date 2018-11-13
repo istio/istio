72adc699dd906981ef30468bb6fe05a7132d535c
Pengyuan Bian <bianpengyuan@google.com>
2018-11-13 13:44:35 -0800

    Fix excessive mixer cpu usage in release 1.0 (#9922)
    * avoid pipe read.
    
    * update
    
    * lint

--- mixer/pkg/config/crd/store.go
+++ mixer/pkg/config/crd/store.go
@@ -201,20 +201,20 @@ func (s *Store) retryCreateCache(
 	lwBuilder listerWatcherBuilderInterface,
 	kinds []string) {
 	remaining := kinds
-	tick := time.Tick(s.bgRetryInterval)
+	ticker := time.NewTicker(s.bgRetryInterval)
+	defer ticker.Stop()
 	stopRetry := false
 
 	for len(remaining) != 0 && !stopRetry {
 		select {
 		case <-s.donec:
 			stopRetry = true
-		case <-tick:
+		case <-ticker.C:
 			rm := s.checkAndCreateCaches(d, lwBuilder, remaining)
 			if len(rm) < len(remaining) {
 				log.Debugf("discovered %v new kinds, remaining undiscovered kinds: %v", len(remaining)-len(rm), rm)
 			}
 			remaining = rm
-		default:
 		}
 	}
 }
