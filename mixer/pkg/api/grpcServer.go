921163be32d73424acffcc6ed4ae742cfb22cac7
Jinming Yue <jmyue@hotmail.com>
2018-11-14 08:04:00 +0800

    Use pool for protobags (#9603)
    * use pool for protobags
    
    * fix comments

--- mixer/pkg/api/grpcServer.go
+++ mixer/pkg/api/grpcServer.go
@@ -25,7 +25,6 @@ import (
 	otlog "github.com/opentracing/opentracing-go/log"
 	"google.golang.org/grpc/codes"
 	grpc "google.golang.org/grpc/status"
-
 	mixerpb "istio.io/api/mixer/v1"
 	"istio.io/istio/mixer/pkg/attribute"
 	"istio.io/istio/mixer/pkg/checkcache"
@@ -79,7 +78,7 @@ func (s *grpcServer) Check(ctx context.Context, req *mixerpb.CheckRequest) (*mix
 	}
 
 	// bag around the input proto that keeps track of reference attributes
-	protoBag := attribute.NewProtoBag(&req.Attributes, s.globalDict, s.globalWordList)
+	protoBag := attribute.GetProtoBag(&req.Attributes, s.globalDict, s.globalWordList)
 
 	if s.cache != nil {
 		if value, ok := s.cache.Get(protoBag); ok {
@@ -237,7 +236,7 @@ func (s *grpcServer) Report(ctx context.Context, req *mixerpb.ReportRequest) (*m
 	}
 
 	// bag around the input proto that keeps track of reference attributes
-	protoBag := attribute.NewProtoBag(&req.Attributes[0], s.globalDict, s.globalWordList)
+	protoBag := attribute.GetProtoBag(&req.Attributes[0], s.globalDict, s.globalWordList)
 
 	// This tracks the delta attributes encoded in the individual report entries
 	accumBag := attribute.GetMutableBag(protoBag)
