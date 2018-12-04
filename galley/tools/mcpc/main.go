5a2c82eea0b9b1607e380ebd80cdedfb7e5fc854
Jason Young <jasonyoung@google.com>
2018-12-03 16:47:14 -0800

    add short and jsonpath printing to mcpc tool

--- galley/tools/mcpc/main.go
+++ galley/tools/mcpc/main.go
@@ -15,14 +15,21 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
+	"text/tabwriter"
+	"time"
 
+	"github.com/gogo/protobuf/jsonpb"
+	"github.com/gogo/protobuf/types"
 	"google.golang.org/grpc"
+	"k8s.io/client-go/util/jsonpath"
 
 	mcp "istio.io/api/mcp/v1alpha1"
 	"istio.io/istio/pkg/mcp/client"
@@ -34,17 +41,32 @@ import (
 
 var (
 	serverAddr = flag.String("server", "127.0.0.1:9901", "The server address")
-	types      = flag.String("types", "", "The fully qualified type URLs of resources to deploy")
+	typeURLs   = flag.String("types", "", "The fully qualified type URLs of resources to deploy")
 	id         = flag.String("id", "", "The node id for the client")
+	output     = flag.String("output", "short", "output format. One of: long|short|jsonpath=<template>")
+)
+
+var (
+	shortHeader        = strings.Join([]string{"TYPE_URL", "INDEX", "KEY", "NAMESPACE", "NAME", "VERSION", "AGE"}, "\t")
+	jsonpathHeader     = strings.Join([]string{"TYPE_URL", "INDEX", "KEY", "NAMESPACE", "NAME", "VERSION", "AGE", "JSONPATH"}, "\t")
+	outputFormatWriter = tabwriter.NewWriter(os.Stdout, 4, 2, 4, ' ', 0)
 )
 
 type updater struct {
+	jp *jsonpath.JSONPath
 }
 
-// Update interface method implementation.
-func (u *updater) Apply(ch *client.Change) error {
-	fmt.Printf("Incoming change: %v\n", ch.TypeURL)
+// TODO - remove once namespace is part of MCP metadata
+func asNamespaceAndName(key string) (string, string) {
+	parts := strings.SplitN(key, "/", 2)
+	if len(parts) == 1 {
+		return "", parts[0]
+	}
+	return parts[0], parts[1]
+}
 
+func (u *updater) printLongChange(ch *client.Change) {
+	fmt.Printf("Incoming change: %v\n", ch.TypeURL)
 	for i, o := range ch.Objects {
 		fmt.Printf("%s[%d]\n", ch.TypeURL, i)
 
@@ -57,15 +79,114 @@ func (u *updater) Apply(ch *client.Change) error {
 
 		fmt.Printf("===============\n")
 	}
+}
+
+func (u *updater) printShortChange(ch *client.Change) {
+	now := time.Now()
+
+	fmt.Fprintln(outputFormatWriter, shortHeader)
+	for i, o := range ch.Objects {
+		age := ""
+		if then, err := types.TimestampFromProto(o.Metadata.CreateTime); err == nil {
+			age = now.Sub(then).Round(time.Millisecond).String()
+		}
+		namespace, name := asNamespaceAndName(o.Metadata.Name)
+
+		parts := []string{
+			o.TypeURL,
+			strconv.Itoa(i),
+			o.Metadata.Name,
+			namespace, name,
+			o.Metadata.Version,
+			age,
+		}
+
+		fmt.Fprintln(outputFormatWriter, strings.Join(parts, "\t"))
+	}
+	outputFormatWriter.Flush()
+}
+
+func (u *updater) printJsonpathChange(ch *client.Change) {
+	now := time.Now()
+
+	fmt.Fprintln(outputFormatWriter, jsonpathHeader)
+	for i, o := range ch.Objects {
+		age := ""
+		if then, err := types.TimestampFromProto(o.Metadata.CreateTime); err == nil {
+			age = now.Sub(then).Round(time.Millisecond).String()
+		}
+
+		namespace, name := asNamespaceAndName(o.Metadata.Name)
+
+		output := ""
+		m := jsonpb.Marshaler{}
+		resourceStr, err := m.MarshalToString(o.Resource)
+		if err == nil {
+			queryObj := map[string]interface{}{}
+			if err := json.Unmarshal([]byte(resourceStr), &queryObj); err == nil {
+				var b bytes.Buffer
+				if err := u.jp.Execute(&b, queryObj); err == nil {
+					output = b.String()
+				}
+			}
+		}
+
+		parts := []string{
+			o.TypeURL,
+			strconv.Itoa(i),
+			o.Metadata.Name,
+			namespace, name,
+			o.Metadata.Version,
+			age,
+			output,
+		}
+
+		fmt.Fprintln(outputFormatWriter, strings.Join(parts, "\t"))
+	}
+	outputFormatWriter.Flush()
+}
+
+// Update interface method implementation.
+func (u *updater) Apply(ch *client.Change) error {
+	switch *output {
+	case "long":
+		u.printLongChange(ch)
+	case "short":
+		u.printShortChange(ch)
+	default: // jsonpath
+		u.printJsonpathChange(ch)
+	}
 	return nil
 }
 
 func main() {
 	flag.Parse()
 
-	typeNames := strings.Split(*types, ",")
-
 	u := &updater{}
+
+	switch *output {
+	case "long":
+	case "short":
+	default:
+		parts := strings.Split(*output, "=")
+		if len(parts) != 2 {
+			fmt.Printf("unknown output option %q:\n", *output)
+			os.Exit(-1)
+		}
+
+		if parts[0] != "jsonpath" {
+			fmt.Printf("unknown output %q:\n", *output)
+			os.Exit(-1)
+		}
+
+		u.jp = jsonpath.New("default")
+		if err := u.jp.Parse(parts[1]); err != nil {
+			fmt.Printf("Error parsing jsonpath: %v\n", err)
+			os.Exit(-1)
+		}
+	}
+
+	typeNames := strings.Split(*typeURLs, ",")
 
 	conn, err := grpc.Dial(*serverAddr, grpc.WithInsecure())
 	if err != nil {
