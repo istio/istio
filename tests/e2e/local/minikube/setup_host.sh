a2612c61e06b1b1084004b4157a21f160d4a4075
Itay Shakury <itay@itaysk.com>
2018-12-05 17:41:26 +0200

    remove localkube paths (#10265)

--- tests/e2e/local/minikube/setup_host.sh
+++ tests/e2e/local/minikube/setup_host.sh
@@ -31,8 +31,6 @@ if [[ "${VM_DRIVER}" == "none" ]]; then
 fi
 
 $SUDO_PREFIX minikube start \
-    --extra-config=controller-manager.cluster-signing-cert-file="/var/lib/localkube/certs/ca.crt" \
-    --extra-config=controller-manager.cluster-signing-key-file="/var/lib/localkube/certs/ca.key" \
     --extra-config=apiserver.admission-control="NamespaceLifecycle,LimitRanger,ServiceAccount,PersistentVolumeLabel,DefaultStorageClass,DefaultTolerationSeconds,MutatingAdmissionWebhook,ValidatingAdmissionWebhook,ResourceQuota" \
     --kubernetes-version=v1.10.0 \
     --insecure-registry="localhost:5000" \
