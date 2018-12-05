24996d74f6a0dfc7e240300310a76c9ad4a3c2aa
Quanjie Lin <quanlin@google.com>
2018-12-05 10:08:07 -0800

    fix unit test

--- pilot/pkg/networking/core/v1alpha3/fakes/fake_service_accounts.go
+++ pilot/pkg/networking/core/v1alpha3/fakes/fake_service_accounts.go
@@ -24,7 +24,7 @@ type ServiceAccounts struct {
 	invocationsMutex sync.RWMutex
 }
 
-func (fake *ServiceAccounts) GetIstioServiceAccounts(hostname model.Hostname, ports []int) []string {
+func (fake *ServiceAccounts) GetIstioServiceAccounts(hostname model.Hostname, ports []int, trustDomain string) []string {
 	var portsCopy []int
 	if ports != nil {
 		portsCopy = make([]int, len(ports))
