2ceb718fe0de6854f7b1750d3d9de4a7808bc675
Kevin Conner <kconner@redhat.com>
2018-12-07 05:49:26 -0800

    Handle envoy RDS protocol error response - 1.1 (#10108)
    * Handle envoy RDS protocol error response
    
    * Respond to DiscoveryRequest asking for an update of route resources

--- pilot/pkg/proxy/envoy/v2/init_test.go
+++ pilot/pkg/proxy/envoy/v2/init_test.go
@@ -229,9 +229,9 @@ func sendLDSNack(node string, ldsstr ads.AggregatedDiscoveryService_StreamAggreg
 	return nil
 }
 
-func sendRDSReq(node string, routes []string, rdsstr ads.AggregatedDiscoveryService_StreamAggregatedResourcesClient) error {
+func sendRDSReq(node string, routes []string, nonce string, rdsstr ads.AggregatedDiscoveryService_StreamAggregatedResourcesClient) error {
 	err := rdsstr.Send(&xdsapi.DiscoveryRequest{
-		ResponseNonce: time.Now().String(),
+		ResponseNonce: nonce,
 		Node: &core.Node{
 			Id:       node,
 			Metadata: nodeMetadata,
@@ -245,9 +245,9 @@ func sendRDSReq(node string, routes []string, rdsstr ads.AggregatedDiscoveryServ
 	return nil
 }
 
-func sendRDSNack(node string, _ []string, rdsstr ads.AggregatedDiscoveryService_StreamAggregatedResourcesClient) error {
+func sendRDSNack(node string, routes []string, nonce string, rdsstr ads.AggregatedDiscoveryService_StreamAggregatedResourcesClient) error {
 	err := rdsstr.Send(&xdsapi.DiscoveryRequest{
-		ResponseNonce: time.Now().String(),
+		ResponseNonce: nonce,
 		Node: &core.Node{
 			Id:       node,
 			Metadata: nodeMetadata,
