912ed074595041736018c43256970d55d414e9ac
Costin Manolache <costin@gmail.com>
2018-12-10 19:28:41 -0800

    Enable the new processing, for testing - can be turned off by default in case of problems

--- pkg/features/pilot/pilot.go
+++ pkg/features/pilot/pilot.go
@@ -76,13 +76,13 @@ var (
 
 	// SingleThreadPerNode eliminates 1/2 of the pilot go-routines and optimizes the push handling.
 	// This is a temporary safety flag.
-	SingleThreadPerNode = os.Getenv("THREAD_PER_NODE") == "1"
+	SingleThreadPerNode = os.Getenv("THREAD_PER_NODE") != "0" // on by default for initial testing
 
 	// WaitAck waits for an ACK/NACK after pushing any config. If a nack is received the other configs will not
 	// be pushed, but connection will not be closed. This may slow down the push for a node - the EDS will wait for
 	// CDS to be acked for example, however it is safer.
 	// This is a temporary safety flag.
-	WaitAck = os.Getenv("WAIT_ACK") == "1"
+	WaitAck = os.Getenv("WAIT_ACK") != "0" // on by default for testing
 )
 
 const (
