29ca0b7db91dcb15e24296398fc9b42b9b7190ee
Ulrich Kramer <u.kramer@sap.com>
2018-12-12 13:20:06 +0100

    Fix lint errors

--- pilot/pkg/serviceregistry/kube/conversion.go
+++ pilot/pkg/serviceregistry/kube/conversion.go
@@ -23,7 +23,7 @@ import (
 	multierror "github.com/hashicorp/go-multierror"
 	"istio.io/istio/pkg/spiffe"
 
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 
