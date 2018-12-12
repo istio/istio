05a830c392b1a6aeb67eebec057faef2bd614ee8
Martin Ostrowski <mostrowski@google.com>
2018-12-12 09:53:13 -0800

    Move metric registration to init

--- pilot/pkg/bootstrap/server.go
+++ pilot/pkg/bootstrap/server.go
@@ -36,6 +36,7 @@ import (
 	middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
 	"github.com/hashicorp/go-multierror"
+	prom "github.com/prometheus/client_golang/prometheus"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/keepalive"
@@ -43,7 +44,6 @@ import (
 	"k8s.io/apimachinery/pkg/api/errors"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	prom "github.com/prometheus/client_golang/prometheus"
 
 	mcpapi "istio.io/api/mcp/v1alpha1"
 	meshconfig "istio.io/api/mesh/v1alpha1"
@@ -120,6 +120,14 @@ func init() {
 	// get the grpc server wired up
 	// This should only be set before any RPCs are sent or received by this program.
 	grpc.EnableTracing = true
+
+	// Export pilot version as metric for fleet analytics.
+	pilotVersion := prom.NewGaugeVec(prom.GaugeOpts{
+		Name: "pilot_info",
+		Help: "Pilot version and build information.",
+	}, []string{"version"})
+	prom.MustRegister(pilotVersion)
+	pilotVersion.With(prom.Labels{"version": version.Info.String()}).Set(1)
 }
 
 // MeshArgs provide configuration options for the mesh. If ConfigFile is provided, an attempt will be made to
@@ -256,14 +264,6 @@ func NewServer(args PilotArgs) (*Server, error) {
 	if args.CtrlZOptions != nil {
 		_, _ = ctrlz.Run(args.CtrlZOptions, nil)
 	}
-
-	// Export pilot version as metric for fleet analytics.
-	pilotVersion := prom.NewGaugeVec(prom.GaugeOpts{
-		Name: "pilot_info",
-		Help: "Pilot version and build information.",
-	}, []string{"version"})
-	prom.MustRegister(pilotVersion)
-	pilotVersion.With(prom.Labels{"version": version.Info.String()}).Set(1)
 
 	return s, nil
 }
@@ -888,11 +888,11 @@ func (s *Server) initServiceControllers(args *PilotArgs) error {
 func (s *Server) initMemoryRegistry(serviceControllers *aggregate.Controller) {
 	// MemServiceDiscovery implementation
 	discovery1 := srmemory.NewDiscovery(
-		map[model.Hostname]*model.Service{// srmemory.HelloService.Hostname: srmemory.HelloService,
+		map[model.Hostname]*model.Service{ // srmemory.HelloService.Hostname: srmemory.HelloService,
 		}, 2)
 
 	discovery2 := srmemory.NewDiscovery(
-		map[model.Hostname]*model.Service{// srmemory.WorldService.Hostname: srmemory.WorldService,
+		map[model.Hostname]*model.Service{ // srmemory.WorldService.Hostname: srmemory.WorldService,
 		}, 2)
 
 	registry1 := aggregate.Registry{
