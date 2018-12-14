c366a788a5c7c455b5f7e15137716ea14e89b9fc
Shriram Rajagopalan <shriramr@vmware.com>
2018-12-14 11:49:33 -0800

    lint
    Signed-off-by: Shriram Rajagopalan <shriramr@vmware.com>

--- pilot/pkg/networking/plugin/mixer/mixer.go
+++ pilot/pkg/networking/plugin/mixer/mixer.go
@@ -33,8 +33,8 @@ import (
 	"istio.io/istio/pilot/pkg/model"
 	"istio.io/istio/pilot/pkg/networking/plugin"
 	"istio.io/istio/pilot/pkg/networking/util"
-	"istio.io/istio/pkg/log"
 	featureflags "istio.io/istio/pkg/features/pilot"
+	"istio.io/istio/pkg/log"
 )
 
 type mixerplugin struct{}
