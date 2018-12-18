2f38e95d7fb0955d7e57feca80ab23500c42b973
Ram Vennam <rvennam@us.ibm.com>
2018-12-17 23:15:45 -0500

    manually set version when api call to get latest fails

--- release/downloadIstioCandidate.sh
+++ release/downloadIstioCandidate.sh
@@ -26,6 +26,11 @@ if [ "x${ISTIO_VERSION}" = "x" ] ; then
                   grep tag_name | sed "s/ *\"tag_name\": *\"\\(.*\\)\",*/\\1/")
 fi
 
+if [ "x${ISTIO_VERSION}" = "x" ] ; then
+  echo "Unable to get latest Istio version. Set ISTIO_VERSION env var and re-run. (export ISTIO_VERSION=1.0.4)"
+  exit;
+fi
+
 NAME="istio-$ISTIO_VERSION"
 URL="https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio-${ISTIO_VERSION}-${OSEXT}.tar.gz"
 echo "Downloading $NAME from $URL ..."
