6e2bab38bf5d57e8cf4db1f83e2b694929755c5c
qfel <qfel@google.com>
2018-12-20 13:01:03 -0800

    Improve ctrlz_test - Check for errors - Use a shorter timeout when waiting for test callback - Split it as it was two tests

--- pkg/ctrlz/ctrlz_test.go
+++ pkg/ctrlz/ctrlz_test.go
@@ -15,23 +15,37 @@
 package ctrlz
 
 import (
-	"sync"
 	"testing"
+	"time"
 )
 
 func TestStartStop(t *testing.T) {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	listeningTestProbe = wg.Done
-	defer func() { listeningTestProbe = nil }()
-	o := DefaultOptions()
-	s, _ := Run(o, nil)
-	wg.Wait()
-	s.Close()
+	t.Run("Enabled", func(t *testing.T) {
+		done := make(chan struct{})
+		listeningTestProbe = func() { close(done) }
+		defer func() { listeningTestProbe = nil }()
+		o := DefaultOptions()
+		// TODO: Pick an unused port in o.Port.
+		s, err := Run(o, nil)
+		if err != nil {
+			t.Fatalf("Run: %v", err)
+		}
+		defer s.Close()
+		select {
+		case <-done:
+		case <-time.After(30 * time.Second):
+			t.Fatal("Timed out waiting for listeningTestProbe to be called")
+		}
+	})
 
-	listeningTestProbe = nil
-	o = DefaultOptions()
-	o.Port = 0
-	s, _ = Run(o, nil)
-	s.Close()
+	t.Run("Disabled", func(t *testing.T) {
+		listeningTestProbe = nil
+		o := DefaultOptions()
+		o.Port = 0
+		s, err := Run(o, nil)
+		if err != nil {
+			t.Fatalf("Run: %v", err)
+		}
+		s.Close()
+	})
 }
