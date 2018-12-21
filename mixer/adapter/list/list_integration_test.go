70e330cc7b55e767768739a3e06bf4e4a95515c6
Nupur Garg <37600866+gargnupur@users.noreply.github.com>
2018-12-21 13:13:39 -0800

    White/BlackListing: Change NOT_FOUND return code for a value not whitelisted to PERMISSION_DENIED similar to blacklisted case. (#10444)
    Added a test case for using IP_ADDRESS for whitelisting using X-Forwarded-for request header
    
    Change list type to v1beta1.value as compared to string
    
    Fixed based on reviews
    
    Fixed broken tests and took care of rest of the comments
    
    Made IP test case to use a subnet
    
    Fix lint error
    
    Fix based on reviews
    
    Fix based on reviews
    
    Making ipList.go file, same as before
    
    Making ipList.go file, same as before
    
    Making list.go file, same as before
    
    minor fix in list_integration_test.go

--- mixer/adapter/list/list_integration_test.go
+++ mixer/adapter/list/list_integration_test.go
@@ -15,6 +15,7 @@
 package list
 
 import (
+	"net"
 	"testing"
 
 	adapter_integration "istio.io/istio/mixer/pkg/adapter/test"
@@ -30,6 +31,16 @@ metadata:
 spec:
   overrides: ["src1", "src2"]
   blacklist: false
+`
+	h1BlacklistOverrideSrc1Src2 = `
+apiVersion: "config.istio.io/v1alpha2"
+kind: listchecker
+metadata:
+  name: staticversion
+  namespace: istio-system
+spec:
+  overrides: ["src1", "src2"]
+  blacklist: true
 `
 	i1ValSrcNameAttr = `
 apiVersion: "config.istio.io/v1alpha2"
@@ -51,6 +62,47 @@ spec:
   - handler: staticversion.listchecker
     instances:
     - appversion.listentry
+`
+	h1OverrideIP = `
+apiVersion: "config.istio.io/v1alpha2"
+kind: listchecker
+metadata:
+  name: ipoveride
+  namespace: istio-system
+spec:
+  overrides: ["10.57.0.0/16"]
+  blacklist: false
+  entryType: IP_ADDRESSES
+`
+	i1ValIPAttr = `
+apiVersion: "config.istio.io/v1alpha2"
+kind: listentry
+metadata:
+  name: ipwhitelist
+  namespace: istio-system
+spec:
+  value: source.ip
+`
+	r1IPI1 = `
+apiVersion: "config.istio.io/v1alpha2"
+kind: rule
+metadata:
+  name: checkip
+  namespace: istio-system
+spec:
+  actions:
+  - handler: ipoveride.listchecker
+    instances:
+    - ipwhitelist.listentry
+`
+	i1ValIPAsStringAttr = `
+apiVersion: "config.istio.io/v1alpha2"
+kind: listentry
+metadata:
+  name: ipwhitelist
+  namespace: istio-system
+spec:
+  value: request.headers["x-forwarded-for"]
 `
 )
 
@@ -86,11 +138,164 @@ func TestReport(t *testing.T) {
              {
               "Check": {
                "Status": {
-                "code": 5,
+                "code": 7,
                 "message": "staticversion.listchecker.istio-system: is not whitelisted"
                },
                "ValidDuration": 300000000000,
                "ValidUseCount": 10000
+              }
+             }
+            ]
+            }`,
+		},
+	)
+}
+
+func TestReportBlacklist(t *testing.T) {
+	adapter_integration.RunTest(
+		t,
+		GetInfo,
+		adapter_integration.Scenario{
+			ParallelCalls: []adapter_integration.Call{
+				{
+					CallKind: adapter_integration.CHECK,
+					Attrs:    map[string]interface{}{"source.name": "src1"},
+				},
+				{
+					CallKind: adapter_integration.CHECK,
+				},
+			},
+			Configs: []string{
+				h1BlacklistOverrideSrc1Src2,
+				r1H1I1,
+				i1ValSrcNameAttr,
+			},
+			Want: `{
+            "AdapterState": null,
+            "Returns": [
+             {
+              "Check": {
+                "Status": {
+                "code": 7,
+                "message": "staticversion.listchecker.istio-system:src1 is blacklisted"
+               },
+               "ValidDuration": 300000000000,
+               "ValidUseCount": 10000
+              }
+             },
+             {
+              "Check": {
+                "Status": {},
+                "ValidDuration": 300000000000,
+                "ValidUseCount": 10000
+              }
+             }
+            ]
+            }`,
+		},
+	)
+}
+
+func TestReportIpWhitelist(t *testing.T) {
+	adapter_integration.RunTest(
+		t,
+		GetInfo,
+		adapter_integration.Scenario{
+			ParallelCalls: []adapter_integration.Call{
+				{
+					CallKind: adapter_integration.CHECK,
+					Attrs: map[string]interface{}{"source.ip": []byte(net.ParseIP("10.56.0.1")),
+						"request.headers": map[string]string{
+							"x-forwarded-for": "10.56.0.1",
+						},
+					},
+				},
+				{
+					CallKind: adapter_integration.CHECK,
+					Attrs: map[string]interface{}{"source.ip": []byte(net.ParseIP("10.57.0.1")),
+						"request.headers": map[string]string{
+							"x-forwarded-for": "10.57.0.1",
+						},
+					},
+				},
+			},
+			Configs: []string{
+				h1OverrideIP,
+				i1ValIPAttr,
+				r1IPI1,
+			},
+			Want: `{
+            "AdapterState": null,
+            "Returns": [
+             {
+              "Check": {
+                "Status": {
+                "code": 7,
+                "message": "ipoveride.listchecker.istio-system:10.56.0.1 is not whitelisted"
+               },
+               "ValidDuration": 300000000000,
+               "ValidUseCount": 10000
+              }
+             },
+             {
+              "Check": {
+                "Status": {},
+                "ValidDuration": 300000000000,
+                "ValidUseCount": 10000
+              }
+             }
+            ]
+            }`,
+		},
+	)
+}
+
+func TestReportIpAsStringWhitelist(t *testing.T) {
+	adapter_integration.RunTest(
+		t,
+		GetInfo,
+		adapter_integration.Scenario{
+			ParallelCalls: []adapter_integration.Call{
+				{
+					CallKind: adapter_integration.CHECK,
+					Attrs: map[string]interface{}{"source.ip": []byte(net.ParseIP("10.56.0.1")),
+						"request.headers": map[string]string{
+							"x-forwarded-for": "10.56.0.1",
+						},
+					},
+				},
+				{
+					CallKind: adapter_integration.CHECK,
+					Attrs: map[string]interface{}{"source.ip": []byte(net.ParseIP("10.57.0.1")),
+						"request.headers": map[string]string{
+							"x-forwarded-for": "10.57.0.1",
+						},
+					},
+				},
+			},
+			Configs: []string{
+				h1OverrideIP,
+				i1ValIPAsStringAttr,
+				r1IPI1,
+			},
+			Want: `{
+            "AdapterState": null,
+            "Returns": [
+             {
+              "Check": {
+                "Status": {
+                "code": 7,
+                "message": "ipoveride.listchecker.istio-system:10.56.0.1 is not whitelisted"
+               },
+               "ValidDuration": 300000000000,
+               "ValidUseCount": 10000
+              }
+             },
+             {
+              "Check": {
+                "Status": {},
+                "ValidDuration": 300000000000,
+                "ValidUseCount": 10000
               }
              }
             ]
