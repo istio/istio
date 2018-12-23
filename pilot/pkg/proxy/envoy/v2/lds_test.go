9976cffb2648cfb2edfaa7c906868df43794e817
Costin Manolache <costin@gmail.com>
2018-12-23 07:46:43 -0800

    Add 'interception=none' test.
    This also covers the basic isolation testing.

--- pilot/pkg/proxy/envoy/v2/lds_test.go
+++ pilot/pkg/proxy/envoy/v2/lds_test.go
@@ -14,13 +14,79 @@
 package v2_test
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"testing"
+	"time"
 
 	"istio.io/istio/pilot/pkg/model"
+	"istio.io/istio/pilot/pkg/proxy/envoy/v2"
+	"istio.io/istio/pkg/adsc"
+	"istio.io/istio/pkg/features/pilot"
 	"istio.io/istio/pkg/test/env"
 	"istio.io/istio/tests/util"
+
+	xdsapi "github.com/envoyproxy/go-control-plane/envoy/api/v2"
 )
+
+
+// TestLDS using isolated namespaces
+func TestLDSIsolated(t *testing.T) {
+
+	_, tearDown := initLocalPilotTestEnv(t)
+	defer tearDown()
+
+	// Sidecar in 'none' mode
+	t.Run("sidecar_none", func(t *testing.T) {
+		// TODO: add a Service with EDS resolution in the none ns.
+		// The ServiceEntry only allows STATIC - both STATIC and EDS should generated TCP listeners on :port
+		// while DNS and NONE should generate old-style bind ports.
+		// Right now 'STATIC' and 'EDS' result in ClientSideLB in the internal object, so listener test is valid.
+
+		ldsr, err := adsc.Dial(util.MockPilotGrpcAddr, "", &adsc.Config{
+			Meta: map[string]string{
+				pilot.InterceptionMode: pilot.InterceptionModeNone,
+			},
+			IP: "10.11.0.1", // matches none.yaml s1tcp.none
+			Namespace: "none",
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer ldsr.Close()
+
+		ldsr.Send(&xdsapi.DiscoveryRequest{
+			TypeUrl: v2.ListenerType})
+
+		_, err = ldsr.Wait("lds", 50000 * time.Second)
+		if err != nil {
+			t.Fatal("Failed to receive LDS", err)
+			return
+		}
+
+		strResponse, _ := json.MarshalIndent(ldsr.TCPListeners, "  ", "  ")
+		_ = ioutil.WriteFile(env.IstioOut+"/ldsv2_sidecar_none_tcp.json", strResponse, 0644)
+		strResponse, _ = json.MarshalIndent(ldsr.HTTPListeners, "  ", "  ")
+		_ = ioutil.WriteFile(env.IstioOut+"/ldsv2_sidecar_none_http.json", strResponse, 0644)
+
+		// s1http - inbound HTTP on 7071 (forwarding to app on 30000 + 7071 - or custom port)
+		// All outbound on http proxy
+		if len(ldsr.HTTPListeners) != 2 {
+			t.Error("HTTP listeners, expecting 2 got ", len(ldsr.HTTPListeners), ldsr.HTTPListeners)
+		}
+
+		// s1tcp:2000 outbound, bind=true (to reach other instances of the service)
+		// s1:5005 outbound, bind=true
+		// :443 - https external, bind=false
+		// 10.11.0.1_7070, bind=true -> inbound|2000|s1 - on port 7070, fwd to 37070
+		// virtual
+		if len(ldsr.TCPListeners) == 0 {
+			t.Fatal("No response")
+		}
+		// TODO: check bind==true
+	})
+
+}
 
 // TestLDS is running LDSv2 tests.
 func TestLDS(t *testing.T) {
