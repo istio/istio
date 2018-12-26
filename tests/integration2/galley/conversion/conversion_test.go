2a881accdf3c4ea392a600f07dcceefce47b0ef0
ozben <ozben@google.com>
2018-12-26 09:36:41 -0800

    Enhance Galley TestData model
    + Add multi-phase support for Galley test data, allowing multiple
    back-to-back application of test data in stages, each with its own
    validation mode.
    + Add support for skipping/ignoring tests via signal-files.

--- tests/integration2/galley/conversion/conversion_test.go
+++ tests/integration2/galley/conversion/conversion_test.go
@@ -15,6 +15,7 @@
 package conversion
 
 import (
+	"fmt"
 	"testing"
 
 	"istio.io/istio/pkg/test/framework"
@@ -50,35 +51,52 @@ func TestConversion(t *testing.T) {
 
 			gal := components.GetGalley(ctx, t)
 
-			input, err := d.LoadInputFile()
-			if err != nil {
-				t.Fatalf("Unable to load input test data: %v", err)
-			}
-
-			if d.HasMeshConfigFile() {
-				mc, err := d.LoadMeshConfigFile()
-				if err != nil {
-					t.Fatalf("Error loading Mesh config file: %v", err)
+			for i, fset := range d.FileSets() {
+				testName := d.TestName()
+				if len(d.FileSets()) != 1 {
+					runTest(t, fset, gal)
+					testName = fmt.Sprintf("%s_%d", d.TestName(), i)
 				}
-				if err = gal.SetMeshConfig(string(mc)); err != nil {
-					t.Fatalf("Error setting Mesh config file: %v", err)
-				}
-			}
-
-			expected, err := d.LoadExpectedResources()
-			if err != nil {
-				t.Fatalf("unable to load expected resources: %v", err)
-			}
-
-			if err = gal.ApplyConfig(string(input)); err != nil {
-				t.Fatalf("unable to apply config to Galley: %v", err)
-			}
-
-			for typeURL, e := range expected {
-				if err = gal.WaitForSnapshot(typeURL, e...); err != nil {
-					t.Errorf("Error waiting for %s:\n%v\n", typeURL, err)
-				}
+				t.Run(testName, func(t *testing.T) {
+					runTest(t, fset, gal)
+				})
 			}
 		})
+	}
+}
+
+func runTest(t *testing.T, fset *testdata.FileSet, gal components.Galley) {
+	input, err := fset.LoadInputFile()
+	if err != nil {
+		t.Fatalf("Unable to load input test data: %v", err)
+	}
+
+	if fset.HasMeshConfigFile() {
+		mc, err := fset.LoadMeshConfigFile()
+		if err != nil {
+			t.Fatalf("Error loading Mesh config file: %v", err)
+		}
+		if err = gal.SetMeshConfig(string(mc)); err != nil {
+			t.Fatalf("Error setting Mesh config file: %v", err)
+		}
+	}
+
+	expected, err := fset.LoadExpectedResources()
+	if err != nil {
+		t.Fatalf("unable to load expected resources: %v", err)
+	}
+
+	if err = gal.ClearConfig(); err != nil {
+		t.Fatalf("unable to clear config from Galley: %v", err)
+	}
+
+	if err = gal.ApplyConfig(string(input)); err != nil {
+		t.Fatalf("unable to apply config to Galley: %v", err)
+	}
+
+	for typeURL, e := range expected {
+		if err = gal.WaitForSnapshot(typeURL, e...); err != nil {
+			t.Errorf("Error waiting for %s:\n%v\n", typeURL, err)
+		}
 	}
 }
