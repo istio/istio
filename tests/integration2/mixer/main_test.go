e25340f32faf595c0af80dfef81de1223280fbc3
Andy Lai <31747472+hklai@users.noreply.github.com>
2019-01-14 15:00:48 -0800

    Update integration test job (#10888)
    * Fix integration test scripts
    
    * Making TestMain exit with the proper return code
    
    * Update local env references to native
    
    * Fix linter errors
    
    * Skipping integration tests in codecov since they fail

--- tests/integration2/mixer/main_test.go
+++ tests/integration2/mixer/main_test.go
@@ -15,11 +15,13 @@
 package mixer
 
 import (
+	"os"
 	"testing"
 
 	"istio.io/istio/pkg/test/framework"
 )
 
 func TestMain(m *testing.M) {
-	framework.Run("mixer_test", m)
+	rt, _ := framework.Run("mixer_test", m)
+	os.Exit(rt)
 }
