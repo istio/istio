f747e70ad42693a899a6f71ea702c2027e600e6b
Etai Lev Ran <etail@il.ibm.com>
2019-01-15 14:51:38 +0200

    added tests for default values and setting via command line

---
+++ pkg/keepalive/options_test.go
@@ -0,0 +1,48 @@
+package keepalive_test
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/spf13/cobra"
+
+	"istio.io/istio/pkg/keepalive"
+)
+
+// Test default maximum connection age is set to infinite, preserving previous
+// unbounded lifetime behavior.
+func TestAgeDefaultsToInfinite(t *testing.T) {
+	ko := keepalive.DefaultOption()
+
+	if ko.MaxServerConnectionAge != keepalive.Infinity {
+		t.Errorf("%s maximum connection age %v", t.Name(), ko.MaxServerConnectionAge)
+	} else if ko.MaxServerConnectionAgeGrace != keepalive.Infinity {
+		t.Errorf("%s maximum connection age grace %v", t.Name(), ko.MaxServerConnectionAgeGrace)
+	}
+}
+
+// Confirm maximum connection age parameters can be set from the command line.
+func TestSetConnectionAgeCommandlineOptions(t *testing.T) {
+	ko := keepalive.DefaultOption()
+	cmd := &cobra.Command{}
+	ko.AttachCobraFlags(cmd)
+
+	buf := new(bytes.Buffer)
+	cmd.SetOutput(buf)
+	sec := time.Duration(1 * time.Second)
+	cmd.SetArgs([]string{
+		fmt.Sprintf("--keepaliveMaxServerConnectionAge=%v", sec),
+		fmt.Sprintf("--keepaliveMaxServerConnectionAgeGrace=%v", sec),
+	})
+
+	if err := cmd.Execute(); err != nil {
+		t.Errorf("%s %s", t.Name(), err.Error())
+	}
+	if ko.MaxServerConnectionAge != sec {
+		t.Errorf("%s maximum connection age %v", t.Name(), ko.MaxServerConnectionAge)
+	} else if ko.MaxServerConnectionAgeGrace != sec {
+		t.Errorf("%s maximum connection age grace %v", t.Name(), ko.MaxServerConnectionAgeGrace)
+	}
+}
