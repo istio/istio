e45e22a56a3e66b493edace45b9c772b451dd6a4
xuzhonghu <xuzhonghu@huawei.com>
2019-01-21 17:20:13 +0800

    fix lint

--- tests/e2e/tests/pilot/pod_churn_test.go
+++ tests/e2e/tests/pilot/pod_churn_test.go
@@ -54,8 +54,7 @@ const (
 // to ensure that no 503s are received by the application. This verifies that Envoy is properly retrying disconnected
 // endpoints.
 func TestPodChurn(t *testing.T) {
-	// TODO: remove this once https://github.com/istio/istio/issues/11115 fixed
-	t.Skip()
+	t.Skip("https://github.com/istio/istio/issues/11115")
 	// Deploy the churn app
 	app := newChurnApp(t)
 	defer app.stop()
