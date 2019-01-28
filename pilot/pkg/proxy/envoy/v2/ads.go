da43734c32e50dbc1a0707b86fb3c74b8add7cd6
Zhonghu Xu <xuzhonghu@huawei.com>
2019-01-28 09:22:26 +0800

    Incremental EDS only need updated service names (#11117)

--- pilot/pkg/proxy/envoy/v2/ads.go
+++ pilot/pkg/proxy/envoy/v2/ads.go
@@ -318,7 +318,7 @@ func (s *DiscoveryServer) configDump(conn *XdsConnection) (*adminapi.ConfigDump,
 type XdsEvent struct {
 	// If not empty, it is used to indicate the event is caused by a change in the clusters.
 	// Only EDS for the listed clusters will be sent.
-	edsUpdatedServices map[string]*EndpointShards
+	edsUpdatedServices map[string]struct{}
 
 	push *model.PushContext
 
@@ -459,7 +459,7 @@ func (s *DiscoveryServer) StreamAggregatedResources(stream ads.AggregatedDiscove
 				// too verbose - sent immediately after EDS response is received
 				adsLog.Debugf("ADS:LDS: REQ %s %v", con.ConID, peerAddr)
 				con.LDSWatch = true
-				err := s.pushLds(con, s.globalPushContext(), true, versionInfo())
+				err := s.pushLds(con, s.globalPushContext(), versionInfo())
 				if err != nil {
 					return err
 				}
@@ -569,7 +569,7 @@ func (s *DiscoveryServer) StreamAggregatedResources(stream ads.AggregatedDiscove
 
 				con.Clusters = clusters
 				adsLog.Debugf("ADS:EDS: REQ %s %s clusters: %d", peerAddr, con.ConID, len(con.Clusters))
-				err := s.pushEds(s.globalPushContext(), con, true, nil)
+				err := s.pushEds(s.globalPushContext(), con, nil)
 				if err != nil {
 					return err
 				}
@@ -663,7 +663,7 @@ func (s *DiscoveryServer) pushConnection(con *XdsConnection, pushEv *XdsEvent) e
 		// Push only EDS. This is indexed already - push immediately
 		// (may need a throttle)
 		if len(con.Clusters) > 0 {
-			if err := s.pushEds(pushEv.push, con, false, pushEv.edsUpdatedServices); err != nil {
+			if err := s.pushEds(pushEv.push, con, pushEv.edsUpdatedServices); err != nil {
 				return err
 			}
 		}
@@ -712,13 +712,13 @@ func (s *DiscoveryServer) pushConnection(con *XdsConnection, pushEv *XdsEvent) e
 	}
 
 	if len(con.Clusters) > 0 {
-		err := s.pushEds(pushEv.push, con, true, nil)
+		err := s.pushEds(pushEv.push, con, nil)
 		if err != nil {
 			return err
 		}
 	}
 	if con.LDSWatch {
-		err := s.pushLds(con, pushEv.push, false, pushEv.version)
+		err := s.pushLds(con, pushEv.push, pushEv.version)
 		if err != nil {
 			return err
 		}
@@ -749,7 +749,7 @@ func AdsPushAll(s *DiscoveryServer) {
 // Primary code path is from v1 discoveryService.clearCache(), which is added as a handler
 // to the model ConfigStorageCache and Controller.
 func (s *DiscoveryServer) AdsPushAll(version string, push *model.PushContext,
-	full bool, edsUpdates map[string]*EndpointShards) {
+	full bool, edsUpdates map[string]struct{}) {
 	if !full {
 		s.edsIncremental(version, push, edsUpdates)
 		return
@@ -790,7 +790,7 @@ func (s *DiscoveryServer) AdsPushAll(version string, push *model.PushContext,
 
 // Send a signal to all connections, with a push event.
 func (s *DiscoveryServer) startPush(version string, push *model.PushContext, full bool,
-	edsUpdates map[string]*EndpointShards) {
+	edsUpdates map[string]struct{}) {
 
 	// Push config changes, iterating over connected envoys. This cover ADS and EDS(0.7), both share
 	// the same connection table
