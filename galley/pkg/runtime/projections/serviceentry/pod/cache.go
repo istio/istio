69386c1dd0861cf414c1db94ebdb4f50b5a46857
nmittler <nmittler@gmail.com>
2019-01-29 07:10:38 -0800

    [Galley] Adding ServiceEntry synthesis
    Added a new custom projection that is subscribed to events for k8s Pods, Nodes, Services and Endpoints. These events are absorbed and do not become part of the snapshot. Instead, synthetic ServiceEntry resources are generated and become part of the snapshot.
    
    Partially addresses #10497 and #10589

---
+++ galley/pkg/runtime/projections/serviceentry/pod/cache.go
@@ -0,0 +1,109 @@
+// Copyright 2019 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package pod
+
+import (
+	"istio.io/istio/galley/pkg/metadata"
+	"istio.io/istio/galley/pkg/runtime/processing"
+	"istio.io/istio/galley/pkg/runtime/resource"
+	"istio.io/istio/pkg/spiffe"
+
+	coreV1 "k8s.io/api/core/v1"
+)
+
+var _ Cache = cacheImpl{}
+var _ processing.Handler = cacheImpl{}
+
+// Info for a Pod.
+type Info struct {
+	FullName resource.FullName
+	NodeName string
+	Labels   map[string]string
+
+	// ServiceAccountName the Spiffe name for the Pod service account.
+	ServiceAccountName string
+}
+
+// Cache for pod Info.
+type Cache interface {
+	GetPodByIP(ip string) (Info, bool)
+}
+
+// NewCache creates a cache and its update handler
+func NewCache() (Cache, processing.Handler) {
+	c := make(cacheImpl)
+	return c, c
+}
+
+type cacheImpl map[string]Info
+
+func (pc cacheImpl) GetPodByIP(ip string) (Info, bool) {
+	pod, ok := pc[ip]
+	return pod, ok
+}
+
+func (pc cacheImpl) Handle(event resource.Event) {
+	if event.Entry.ID.Collection != metadata.K8sCoreV1Pods.Collection {
+		return
+	}
+
+	switch event.Kind {
+	case resource.Added, resource.Updated:
+		pod := event.Entry.Item.(*coreV1.Pod)
+
+		ip := pod.Status.PodIP
+		if ip == "" {
+			// PodIP will be empty when pod is just created, but before the IP is assigned
+			// via UpdateStatus.
+			return
+		}
+
+		switch pod.Status.Phase {
+		case coreV1.PodPending, coreV1.PodRunning:
+			// add to cache if the pod is running or pending
+			pc[ip] = Info{
+				FullName:           event.Entry.ID.FullName,
+				NodeName:           pod.Spec.NodeName,
+				Labels:             pod.Labels,
+				ServiceAccountName: kubeToIstioServiceAccount(pod.Spec.ServiceAccountName, pod.Namespace),
+			}
+		default:
+			// delete if the pod switched to other states and is in the cache
+			delete(pc, ip)
+		}
+	case resource.Deleted:
+		var ip string
+		pod, ok := event.Entry.Item.(*coreV1.Pod)
+		if ok {
+			ip = pod.Status.PodIP
+		} else {
+			// The resource was either not available or failed parsing. Look it up by brute force.
+			for infoIP, info := range pc {
+				if info.FullName == event.Entry.ID.FullName {
+					ip = infoIP
+					break
+				}
+			}
+		}
+
+		// delete only if this pod was in the cache
+		delete(pc, ip)
+	}
+}
+
+// kubeToIstioServiceAccount converts a K8s service account to an Istio service account
+func kubeToIstioServiceAccount(saname string, ns string) string {
+	return spiffe.MustGenSpiffeURI(ns, saname)
+}
--- galley/pkg/source/kube/dynamic/converter/converter_test.go
+++ galley/pkg/source/kube/dynamic/converter/converter_test.go
@@ -22,7 +22,6 @@ import (
 
 	"github.com/gogo/protobuf/proto"
 	"github.com/gogo/protobuf/types"
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/api/extensions/v1beta1"
 	extensions "k8s.io/api/extensions/v1beta1"
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -31,7 +30,6 @@ import (
 
 	authn "istio.io/api/authentication/v1alpha1"
 	meshcfg "istio.io/api/mesh/v1alpha1"
-	networking "istio.io/api/networking/v1alpha3"
 	"istio.io/istio/galley/pkg/meshconfig"
 	"istio.io/istio/galley/pkg/runtime/resource"
 	"istio.io/istio/pilot/pkg/model"
@@ -570,115 +568,5 @@ func TestShouldProcessIngress(t *testing.T) {
 			t.Errorf("shouldProcessIngress(<ingress of class '%s'>) => %v, want %v for mesh config %v",
 				c.ingressClass, !c.shouldProcess, c.shouldProcess, c.ingressMode)
 		}
-	}
-}
-
-func TestKubeServiceResource(t *testing.T) {
-	cases := []struct {
-		name string
-		from corev1.Service
-		want Entry
-	}{
-		{
-			name: "Simple",
-			from: corev1.Service{
-				ObjectMeta: metaV1.ObjectMeta{
-					Name:              "reviews",
-					Namespace:         "default",
-					CreationTimestamp: metaV1.Time{Time: fakeCreateTime},
-					Annotations: map[string]string{
-						"a1_key": "a1_value",
-						"a2_key": "a2_value",
-					},
-					Labels: map[string]string{
-						"l1_key": "l1_value",
-						"l2_key": "l2_value",
-					},
-				},
-				Spec: corev1.ServiceSpec{
-					ClusterIP: "10.39.241.161",
-					Ports: []corev1.ServicePort{
-						{
-							Name:       "http",
-							Protocol:   "TCP",
-							Port:       9080,
-							TargetPort: intstr.FromInt(9080),
-						},
-						{
-							Name:       "https-web",
-							Protocol:   "TCP",
-							Port:       9081,
-							TargetPort: intstr.FromInt(9081),
-						},
-						{
-							Name:       "ssh",
-							Protocol:   "TCP",
-							Port:       9082,
-							TargetPort: intstr.FromInt(9082),
-						},
-					},
-				},
-			},
-			want: Entry{
-				Key: resource.FullNameFromNamespaceAndName("default", "reviews"),
-				Metadata: resource.Metadata{
-					Annotations: map[string]string{
-						"a1_key": "a1_value",
-						"a2_key": "a2_value",
-					},
-					Labels: map[string]string{
-						"l1_key": "l1_value",
-						"l2_key": "l2_value",
-					},
-					CreateTime: fakeCreateTime.Local(),
-				},
-				Resource: &networking.ServiceEntry{
-					Hosts:      []string{"reviews.default.svc.cluster.local"},
-					Addresses:  []string{"10.39.241.161"},
-					Resolution: networking.ServiceEntry_STATIC,
-					Location:   networking.ServiceEntry_MESH_INTERNAL,
-					Ports: []*networking.Port{
-						{
-							Name:     "http",
-							Number:   9080,
-							Protocol: "HTTP",
-						},
-						{
-							Name:     "https-web",
-							Number:   9081,
-							Protocol: "HTTPS",
-						},
-						{
-							Name:     "ssh",
-							Number:   9082,
-							Protocol: "TCP",
-						},
-					},
-				},
-			},
-		},
-	}
-
-	for _, c := range cases {
-		t.Run(c.name, func(t *testing.T) {
-			var u unstructured.Unstructured
-			u.Object = make(map[string]interface{})
-			if err := convertJSON(&c.from, &u.Object); err != nil {
-				t.Fatalf("Internal test error: %v", err)
-			}
-			entries, err := kubeServiceResource(&Config{DomainSuffix: "cluster.local"}, resource.Info{}, c.want.Key, "kind", &u)
-			if err != nil {
-				t.Fatalf("Unexpected error: %v", err)
-			}
-			if len(entries) != 1 {
-				t.Fatalf("Expected one entry: %v", entries)
-			}
-
-			got := entries[0]
-
-			if !reflect.DeepEqual(got, c.want) {
-				t.Fatalf("Mismatch:\nGot:\n%v\nWanted:\n%v\n", got, c.want)
-			}
-		})
 	}
 }
