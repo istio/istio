82d0ad43915eb9ea0c828c59942c5b27a9f47aa3
Quanjie Lin <32855694+quanjielin@users.noreply.github.com>
2019-01-29 10:07:28 -0800

    incorporate google CA's merge APIs change in nodeagent  (#11341)
    * merge api
    
    * remove extra line

--- security/pkg/nodeagent/caclient/providers/google/client.go
+++ security/pkg/nodeagent/caclient/providers/google/client.go
@@ -19,8 +19,6 @@ import (
 	"crypto/x509"
 	"errors"
 	"fmt"
-	"os"
-	"strconv"
 	"strings"
 
 	"google.golang.org/grpc"
@@ -34,16 +32,12 @@ import (
 
 var usePodDefaultFlag = false
 
-const (
-	podIdentityFlag   = "POD_IDENTITY"
-	bearerTokenPrefix = "Bearer "
-)
+const bearerTokenPrefix = "Bearer "
 
 type googleCAClient struct {
-	caEndpoint     string
-	enableTLS      bool
-	client         gcapb.IstioCertificateServiceClient
-	usePodIdentity bool
+	caEndpoint string
+	enableTLS  bool
+	client     gcapb.IstioCertificateServiceClient
 }
 
 // NewGoogleCAClient create a CA client for Google CA.
@@ -53,13 +47,8 @@ func NewGoogleCAClient(endpoint string, tls bool) (caClientInterface.Client, err
 		enableTLS:  tls,
 	}
 
-	c.usePodIdentity = usePodDefaultFlag
-	b, err := strconv.ParseBool(os.Getenv(podIdentityFlag))
-	if err == nil && b == true {
-		c.usePodIdentity = true
-	}
-
 	var opts grpc.DialOption
+	var err error
 	if tls {
 		opts, err = c.getTLSDialOption()
 		if err != nil {
@@ -93,14 +82,7 @@ func (cl *googleCAClient) CSRSign(ctx context.Context, csrPEM []byte, token stri
 	}
 
 	ctx = metadata.NewOutgoingContext(ctx, metadata.Pairs("Authorization", token))
-
-	var resp *gcapb.IstioCertificateResponse
-	var err error
-	if cl.usePodIdentity {
-		resp, err = cl.client.CreatePodCertificate(ctx, req)
-	} else {
-		resp, err = cl.client.CreateCertificate(ctx, req)
-	}
+	resp, err := cl.client.CreateCertificate(ctx, req)
 	if err != nil {
 		log.Errorf("Failed to create certificate: %v", err)
 		return nil, err
