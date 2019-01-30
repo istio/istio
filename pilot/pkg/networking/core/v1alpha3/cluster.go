dc4f83c38a73d0b7285fc25d4c7564f3603917ca
xuzhonghu <xuzhonghu@huawei.com>
2019-01-30 11:17:36 +0800

    move load balancer setting to a separate pkg

--- pilot/pkg/networking/core/v1alpha3/cluster.go
+++ pilot/pkg/networking/core/v1alpha3/cluster.go
@@ -16,9 +16,7 @@ package v1alpha3
 
 import (
 	"fmt"
-	"math"
 	"path"
-	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -28,12 +26,12 @@ import (
 	v2Cluster "github.com/envoyproxy/go-control-plane/envoy/api/v2/cluster"
 	"github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
 	"github.com/envoyproxy/go-control-plane/envoy/api/v2/endpoint"
-	envoy_type "github.com/envoyproxy/go-control-plane/envoy/type"
+	"github.com/envoyproxy/go-control-plane/envoy/type"
 	"github.com/gogo/protobuf/types"
 
-	meshconfig "istio.io/api/mesh/v1alpha1"
 	networking "istio.io/api/networking/v1alpha3"
 	"istio.io/istio/pilot/pkg/model"
+	"istio.io/istio/pilot/pkg/networking/core/v1alpha3/loadbalancer"
 	"istio.io/istio/pilot/pkg/networking/plugin"
 	"istio.io/istio/pilot/pkg/networking/util"
 	"istio.io/istio/pkg/log"
@@ -109,7 +107,7 @@ func (configgen *ConfigGeneratorImpl) BuildClusters(env *model.Environment, prox
 						// outlier detection settings
 						if cluster.LoadAssignment != nil && cluster.OutlierDetection != nil {
 							clone := util.CloneCluster(cluster)
-							ApplyLocalityLBSetting(locality, clone.LoadAssignment, env.Mesh.LocalityLbSetting)
+							loadbalancer.ApplyLocalityLBSetting(locality, clone.LoadAssignment, env.Mesh.LocalityLbSetting)
 							clusters[i] = &clone
 						}
 					}
@@ -143,7 +141,7 @@ func (configgen *ConfigGeneratorImpl) BuildClusters(env *model.Environment, prox
 							// outlier detection settings
 							if cluster.LoadAssignment != nil && cluster.OutlierDetection != nil {
 								clone := util.CloneCluster(cluster)
-								ApplyLocalityLBSetting(locality, clone.LoadAssignment, env.Mesh.LocalityLbSetting)
+								loadbalancer.ApplyLocalityLBSetting(locality, clone.LoadAssignment, env.Mesh.LocalityLbSetting)
 								clusters[i] = &clone
 							}
 						}
@@ -826,123 +824,6 @@ func applyLoadBalancer(cluster *apiv2.Cluster, lb *networking.LoadBalancerSettin
 			},
 		}
 	}
-}
-
-func ApplyLocalityLBSetting(
-	locality *core.Locality,
-	loadAssignment *apiv2.ClusterLoadAssignment,
-	localityLB *meshconfig.LocalityLoadBalancerSetting,
-) {
-	if locality == nil || loadAssignment == nil {
-		return
-	}
-
-	// one of Distribute or Failover settings can be applied.
-	if localityLB.GetDistribute() != nil {
-		applyLocalityWeight(locality, loadAssignment, localityLB.GetDistribute())
-	} else if localityLB.GetFailover() != nil {
-		applyLocalityFailover(locality, loadAssignment, localityLB.GetFailover())
-	}
-}
-
-// set locality loadbalancing weight
-func applyLocalityWeight(
-	locality *core.Locality,
-	loadAssignment *apiv2.ClusterLoadAssignment,
-	distribute []*meshconfig.LocalityLoadBalancerSetting_Distribute) {
-	if distribute == nil {
-		return
-	}
-
-	// Support Locality weighted load balancing
-	// (https://www.envoyproxy.io/docs/envoy/latest/intro/arch_overview/load_balancing/locality_weight.html)
-	// by providing weights in LocalityLbEndpoints via load_balancing_weight.
-	// By setting weights across different localities, it can allow
-	// Envoy to weight assignments across different zones and geographical locations.
-	for _, localityWeightSetting := range distribute {
-		if localityWeightSetting != nil &&
-			util.LocalityMatch(locality, localityWeightSetting.From) {
-			misMatched := map[int]struct{}{}
-			for i := range loadAssignment.Endpoints {
-				misMatched[i] = struct{}{}
-			}
-			for locality, weight := range localityWeightSetting.To {
-				// index -> original weight
-				destLocMap := map[int]uint32{}
-				totalWeight := uint32(0)
-				for i, ep := range loadAssignment.Endpoints {
-					if _, exist := misMatched[i]; exist {
-						if util.LocalityMatch(ep.Locality, locality) {
-							delete(misMatched, i)
-							destLocMap[i] = ep.LoadBalancingWeight.Value
-							totalWeight += destLocMap[i]
-						}
-					}
-				}
-				// in case wildcard dest matching multi groups of endpoints
-				// the load balancing weight for a locality is divided by the sum of the weights of all localities
-				for index, originalWeight := range destLocMap {
-					weight := float64(originalWeight*weight) / float64(totalWeight)
-					loadAssignment.Endpoints[index].LoadBalancingWeight = &types.UInt32Value{
-						Value: uint32(math.Ceil(weight)),
-					}
-				}
-			}
-
-			// remove groups of endpoints in a locality that miss matched
-			for i := range misMatched {
-				loadAssignment.Endpoints[i].LbEndpoints = nil
-			}
-			break
-		}
-	}
-}
-
-// set locality loadbalancing priority
-func applyLocalityFailover(
-	locality *core.Locality,
-	loadAssignment *apiv2.ClusterLoadAssignment,
-	failover []*meshconfig.LocalityLoadBalancerSetting_Failover) {
-	// key is priority, value is the index of the LocalityLbEndpoints in ClusterLoadAssignment
-	priorityMap := map[int][]int{}
-
-	// 1. calculate the LocalityLbEndpoints.Priority compared with proxy locality
-	for i, localityEndpoint := range loadAssignment.Endpoints {
-		priority := util.LbPriority(locality, localityEndpoint.Locality)
-		// region not match, apply failover settings
-		if priority == 3 {
-			for _, failoverSetting := range failover {
-				if failoverSetting.From == locality.Region {
-					if localityEndpoint.Locality.Region != failoverSetting.To {
-						priority = 4
-					}
-					break
-				}
-			}
-		}
-		loadAssignment.Endpoints[i].Priority = uint32(priority)
-		priorityMap[priority] = append(priorityMap[priority], i)
-	}
-
-	// since Priorities should range from 0 (highest) to N (lowest) without skipping.
-	// 2. adjust the priorities in order
-	// 2.1 sort all priorities in increasing order.
-	priorities := []int{}
-	for priority := range priorityMap {
-		priorities = append(priorities, priority)
-	}
-	sort.Ints(priorities)
-	// 2.2 adjust LocalityLbEndpoints priority
-	// if the index and value of priorities array is not equal.
-	for i, priority := range priorities {
-		if i != priority {
-			// the LocalityLbEndpoints index in ClusterLoadAssignment.Endpoints
-			for index := range priorityMap[priority] {
-				loadAssignment.Endpoints[index].Priority = uint32(i)
-			}
-		}
-	}
-
 }
 
 func applyUpstreamTLSSettings(env *model.Environment, cluster *apiv2.Cluster, tls *networking.TLSSettings) {
