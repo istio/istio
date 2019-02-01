istio/istio

commit 5249a5fd3bb321bb51b306f52813367895334e53

Randomize Galley ports for integration testing (#11285)
* Randomize Galley port for code-coverage runs.

* Remove runaway empty test.

galley/pkg/server/server.go
@@ -252,6 +252,14 @@ func (s *Server) Run() {
 	}
 }
 
+// Address returns the Address of the MCP service.
+func (s *Server) Address() net.Addr {
+	if s.listener == nil {
+		return nil
+	}
+	return s.listener.Addr()
+}
+
 // Close cleans up resources used by the server.
 func (s *Server) Close() error {
 	if s.stopCh != nil {
@@ -273,6 +281,7 @@ func (s *Server) Close() error {
 
 	if s.listener != nil {
 		_ = s.listener.Close()
+		s.listener = nil
 	}
 
 	if s.reporter != nil {

codecov.skip
@@ -15,7 +15,6 @@ istio.io/istio/tests/codecov
 istio.io/istio/tests/e2e
 istio.io/istio/tests/integration2/examples
 istio.io/istio/tests/integration2/qualification
-istio.io/istio/tests/integration2
 istio.io/istio/tests/integration_old
 istio.io/istio/tests/local
 istio.io/istio/tests/util

galley/pkg/server/server_test.go
@@ -147,6 +147,12 @@ func TestServer_Basic(t *testing.T) {
 		t.Fatalf("Unexpected error creating service: %v", err)
 	}
 
+	// ensure that it does not crash
+	addr := s.Address()
+	if addr == nil {
+		t.Fatalf("expected address not found")
+	}
+
 	var wg sync.WaitGroup
 	wg.Add(1)
 
@@ -157,4 +163,10 @@ func TestServer_Basic(t *testing.T) {
 
 	wg.Wait()
 	_ = s.Close()
+
+	// ensure that it does not crash
+	addr = s.Address()
+	if addr != nil {
+		t.Fatalf("unexpected address found")
+	}
 }

pkg/test/framework/runtime/components/galley/native.go
@@ -16,24 +16,22 @@ package galley
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"path"
-
-	"io"
 	"time"
 
 	multierror "github.com/hashicorp/go-multierror"
 
 	"istio.io/istio/galley/pkg/server"
-	"istio.io/istio/pkg/test/scopes"
-
 	"istio.io/istio/pkg/test/framework/api/component"
 	"istio.io/istio/pkg/test/framework/api/components"
 	"istio.io/istio/pkg/test/framework/api/context"
 	"istio.io/istio/pkg/test/framework/api/descriptors"
 	"istio.io/istio/pkg/test/framework/api/lifecycle"
 	"istio.io/istio/pkg/test/framework/runtime/api"
+	"istio.io/istio/pkg/test/scopes"
 )
 
 var (
@@ -176,7 +174,13 @@ func (c *nativeComponent) restart() error {
 	a.DisableResourceReadyCheck = true
 	a.ConfigPath = c.configDir
 	a.MeshConfigFile = c.meshConfigFile
+	// To prevent ctrlZ port collision between galley/pilot&mixer
+	a.IntrospectionOptions.Port = 0
 	a.ExcludedResourceKinds = make([]string, 0)
+
+	// Bind to an arbitrary port.
+	a.APIAddress = "tcp://0.0.0.0:0"
+
 	s, err := server.New(a)
 	if err != nil {
 		scopes.Framework.Errorf("Error starting Galley: %v", err)
@@ -188,7 +192,7 @@ func (c *nativeComponent) restart() error {
 	go s.Run()
 
 	c.client = &client{
-		address: a.APIAddress,
+		address: fmt.Sprintf("tcp://%s", s.Address().String()),
 		ctx:     c.ctx,
 	}
 

commit 5840139c1d6b593de04eb43452964daf8ac8db16

Fixing race condition in Galley Server.Close()
The issue was introduced by #11285

It causes a race with the startup of the gRPC server, which leads to a segfault.  From prow logs:

```
=== RUN TestServer_Basic 2019-02-01T20:33:05.867746Z	info	ControlZ available at 10.44.58.28:9876 2019-02-01T20:33:05.867968Z	info ControlZ terminated 2019-02-01T20:33:05.867987Z	info	runtime Stopping processor... 2019-02-01T20:33:05.868000Z	warn	runtime Processor has already stopped 2019-02-01T20:33:05.867798Z	info runtime	Starting processor... panic: runtime error: invalid memory address or nil pointer dereference [signal SIGSEGV: segmentation violation code=0x1 addr=0x28 pc=0x9e4bc8] goroutine 148 [running]: istio.io/istio/vendor/google.golang.org/grpc.(*Server).Serve(0xc42046d080, 0x0, 0x0, 0x0, 0x0) /home/prow/go/src/istio.io/istio/vendor/google.golang.org/grpc/server.go:522 +0x748 istio.io/istio/galley/pkg/server.(*Server).Run.func1(0xc4202d9490) /home/prow/go/src/istio.io/istio/galley/pkg/server/server.go:242 +0xfb created by istio.io/istio/galley/pkg/server.(*Server).Run /home/prow/go/src/istio.io/istio/galley/pkg/server/server.go:233 +0x5c FAIL	istio.io/istio/galley/pkg/server 0.383s
```

galley/pkg/server/server.go
@@ -50,16 +50,17 @@ var scope = log.RegisterScope("server", "Galley server debugging", 0)
 
 // Server is the main entry point into the Galley code.
 type Server struct {
-	serveWG    sync.WaitGroup
-	grpcServer *grpc.Server
-	processor  *runtime.Processor
-	mcp        *server.Server
-	mcpSource  *source.Server
-	reporter   monitoring.Reporter
-	listener   net.Listener
-	controlZ   *ctrlz.Server
-	stopCh     chan struct{}
-	callOut    *callout
+	serveWG       sync.WaitGroup
+	grpcServer    *grpc.Server
+	processor     *runtime.Processor
+	mcp           *server.Server
+	mcpSource     *source.Server
+	reporter      monitoring.Reporter
+	listenerMutex sync.Mutex
+	listener      net.Listener
+	controlZ      *ctrlz.Server
+	stopCh        chan struct{}
+	callOut       *callout
 }
 
 type patchTable struct {
@@ -237,10 +238,13 @@ func (s *Server) Run() {
 			return
 		}
 
-		// start serving
-		err = s.grpcServer.Serve(s.listener)
-		if err != nil {
-			scope.Errorf("Galley Server unexpectedly terminated: %v", err)
+		l := s.getListener()
+		if l != nil {
+			// start serving
+			err = s.grpcServer.Serve(l)
+			if err != nil {
+				scope.Errorf("Galley Server unexpectedly terminated: %v", err)
+			}
 		}
 	}()
 	if s.callOut != nil {
@@ -252,12 +256,19 @@ func (s *Server) Run() {
 	}
 }
 
+func (s *Server) getListener() net.Listener {
+	s.listenerMutex.Lock()
+	defer s.listenerMutex.Unlock()
+	return s.listener
+}
+
 // Address returns the Address of the MCP service.
 func (s *Server) Address() net.Addr {
-	if s.listener == nil {
+	l := s.getListener()
+	if l == nil {
 		return nil
 	}
-	return s.listener.Addr()
+	return l.Addr()
 }
 
 // Close cleans up resources used by the server.
@@ -279,10 +290,12 @@ func (s *Server) Close() error {
 		s.processor.Stop()
 	}
 
+	s.listenerMutex.Lock()
 	if s.listener != nil {
 		_ = s.listener.Close()
 		s.listener = nil
 	}
+	s.listenerMutex.Unlock()
 
 	if s.reporter != nil {
 		_ = s.reporter.Close()