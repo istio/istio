5128d7923db5977777fb9257238569de1822e171
ozben <ozben@google.com>
2019-01-31 21:56:54 -0800

    Add Observable interface.

---
+++ galley/pkg/runtime/processing/observable.go
@@ -0,0 +1,22 @@
+//  Copyright 2019 Istio Authors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package processing
+
+// Observable notifies provided listeners about changes in collections. When SetListener gets called multiple
+// times, only the last set listener will be used by the implementation. Calling with a nil value removes
+// the existing listener.
+type Observable interface {
+	SetListener(l Listener)
+}
