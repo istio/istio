b767691e3b6c7627c98df9457cbcdf77467e2b31
Yossi Mesika <mesika@il.ibm.com>
2019-02-04 16:51:53 +0200

    Skip spybackend test when in racetest (#11497)
    * Workaround to make racetest skip this test due to low memory
    
    * Lint

--- mixer/test/spybackend/nosession_integration_test.go
+++ mixer/test/spybackend/nosession_integration_test.go
@@ -17,6 +17,7 @@ package spybackend
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 	"testing"
 	"time"
@@ -252,6 +253,12 @@ spec:
 )
 
 func TestNoSessionBackend(t *testing.T) {
+
+	// We skip this test if it is being run as part of the racetest because
+	// it is intensive on memory and will cause the test to fail with no error
+	if len(os.Getenv("RACE_TEST")) > 0 {
+		t.Skip()
+	}
 
 	testdata := []struct {
 		name   string
