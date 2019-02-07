b85d05513ec7f143571a62960ac608149cf0187a
Martin Taillefer <geeknoid@users.noreply.github.com>
2019-02-07 15:11:36 -0800

    Fix a few doc issues. (#11596)

--- mixer/adapter/zipkin/zipkin.go
+++ mixer/adapter/zipkin/zipkin.go
@@ -1,4 +1,4 @@
-// Copyright 2018 the Istio Authors.
+// Copyright 2019 the Istio Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
