cde958c25d5d86c69196b2e769e2e6f52cd5992c
Steven Dake <steven.dake@gmail.com>
2019-02-08 12:58:23 -0700

    Fix routing when DNS is resolved (#11522)
    The DNSDomain variable needs to be enhanced to include more
    then one DNS entry.  Change DNSDomain to DNSDomains as a meta
    and add the dnsConfig in the meta.  As now DNSDomain is a slice
    of strings instead of a string, the variable needs consolidation.

--- pilot/pkg/networking/core/v1alpha3/listener_test.go
+++ pilot/pkg/networking/core/v1alpha3/listener_test.go
@@ -39,7 +39,7 @@ var (
 		Type:        model.SidecarProxy,
 		IPAddresses: []string{"1.1.1.1"},
 		ID:          "v0.default",
-		DNSDomain:   "default.example.org",
+		DNSDomains:  []string{"default.example.org"},
 		Metadata: map[string]string{
 			model.NodeMetadataConfigNamespace: "not-default",
 			"ISTIO_PROXY_VERSION":             "1.1",
