3118100db82df79b26a0b4feb589c1fd5282e18b
Steven Dake <steven.dake@gmail.com>
2019-02-08 12:58:23 -0700

    Fix routing when DNS is resolved (#11522)
    The DNSDomain variable needs to be enhanced to include more
    then one DNS entry.  Change DNSDomain to DNSDomains as a meta
    and add the dnsConfig in the meta.  As now DNSDomain is a slice
    of strings instead of a string, the variable needs consolidation.

--- pilot/pkg/networking/core/v1alpha3/cluster_test.go
+++ pilot/pkg/networking/core/v1alpha3/cluster_test.go
@@ -169,7 +169,7 @@ func buildTestClustersWithProxyMetadata(serviceHostname string, nodeType model.N
 			ClusterID:   "some-cluster-id",
 			Type:        model.SidecarProxy,
 			IPAddresses: []string{"6.6.6.6"},
-			DNSDomain:   "com",
+			DNSDomains:  []string{"com"},
 			Metadata:    meta,
 		}
 	case model.Router:
@@ -177,7 +177,7 @@ func buildTestClustersWithProxyMetadata(serviceHostname string, nodeType model.N
 			ClusterID:   "some-cluster-id",
 			Type:        model.Router,
 			IPAddresses: []string{"6.6.6.6"},
-			DNSDomain:   "default.example.org",
+			DNSDomains:  []string{"default.example.org"},
 			Metadata:    meta,
 		}
 	default:
