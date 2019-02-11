70146f75ed80797598590c7af96d63f8f027c749
Ed Snible <snible@us.ibm.com>
2019-02-11 13:02:47 -0500

    Revise wording of error msg

--- istioctl/cmd/istioctl/kubeinject.go
+++ istioctl/cmd/istioctl/kubeinject.go
@@ -72,7 +72,7 @@ func getMeshConfigFromConfigMap(kubeconfig string) (*meshconfig.MeshConfig, erro
 	}
 	cfg, err := model.ApplyMeshConfigDefaults(configYaml)
 	if err != nil {
-		err = multierr.Append(fmt.Errorf("istioctl version %s cannot parse mesh config.  Check Istio control plane version",
+		err = multierr.Append(fmt.Errorf("istioctl version %s cannot parse mesh config.  Install istioctl from the latest Istio release",
 			version.Info.Version), err)
 	}
 	return cfg, err
