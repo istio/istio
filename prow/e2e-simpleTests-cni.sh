dddd996dbf631d4ac0d3b1a9b536b6cf15f8fbc8
john-a-joyce <joycej@cisco.com>
2019-02-20 12:49:57 +0000

    Move daemon start after cluster setup
    THe daemon start was before the cluster start.

--- prow/e2e-simpleTests-cni.sh
+++ prow/e2e-simpleTests-cni.sh
@@ -30,25 +30,8 @@ set -x
 
 echo 'Running e2e_simple test with rbac, auth Tests and CNI enabled'
 
-echo 'Run the CNI deamon set'
-CNI_HUB=${CNI_HUB:-gcr.io/istio-release}
-CNI_TAG=${CNI_TAG:-master-latest-daily}
-
-
-chartdir=$(pwd)/charts
-mkdir ${chartdir}
-helm init --client-only
-helm repo add istio.io https://storage.googleapis.com/istio-release/releases/1.1.0-snapshot.6/charts
-helm fetch --untar --untardir ${chartdir} istio.io/istio-cni
-
-helm template --values ${chartdir}/istio-cni/values.yaml --name=istio-cni --namespace=istio-system --set "excludeNamespaces={}" --set hub=${CNI_HUB} --set tag=${CNI_TAG} --set pullPolicy=IfNotPresent --set logLevel=${CNI_LOGLVL:-debug}  ${chartdir}/istio-cni > istio-cni_install.yaml
-
-kubectl apply -f istio-cni_install.yaml
-
 export ENABLE_ISTIO_CNI=true
-# cniBinDir setting is appropriate for GKE environments
-# HUB and TAG for the cni image will be based on the defaults checked in the cni repo
-#export EXTRA_HELM_SETTINGS="--set istio-cni.excludeNamespaces={} --set istio-cni.cniBinDir=/home/kubernetes/bin"
+
 # only gke-e2e-test-latest is enabled for Networkpolicy and Calico
 export RESOURCE_TYPE="gke-e2e-test-latest"
 # TODO - When the inline kube inject code defaults to using the configmap this setting can be removed.
