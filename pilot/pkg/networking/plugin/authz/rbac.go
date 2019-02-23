e565a6ced73591bb9c9af62397d55d526726ecff
Phillip Le <philliple@google.com>
2019-02-22 17:14:58 -0800

    WIP AuthorizationPolicy with selector

--- pilot/pkg/networking/plugin/authz/rbac.go
+++ pilot/pkg/networking/plugin/authz/rbac.go
@@ -177,6 +177,19 @@ func (service serviceMetadata) match(rule *rbacproto.AccessRule) bool {
 	return true
 }
 
+// matchV2 checks if the calling service is matched with the selector in the AuthorizationPolicy if
+// present.
+func (service serviceMetadata) matchV2(labels map[string]string) bool {
+	// Check if labels with |key| and |value| exist in service.
+	for key, value := range labels {
+		actualValue, present := service.labels[key]
+		if !present || actualValue != value {
+			return false
+		}
+	}
+	return true
+}
+
 // attributesFromAuthN returns true if the given attribute is generated from AuthN filter. This implies
 // the attribute should be enforced by dynamic metadata.
 func attributesFromAuthN(k string) bool {
@@ -453,7 +466,8 @@ func buildTCPFilter(service *serviceMetadata, option rbacOption, is11 bool) *lis
 // service which is co-located with the sidecar proxy.
 func buildHTTPFilter(service *serviceMetadata, option rbacOption, is11 bool) *http_conn.HttpFilter {
 	option.forTCPFilter = false
-	config := convertRbacRulesToFilterConfig(service, option)
+	//config := convertRbacRulesToFilterConfig(service, option)
+	config := convertRbacRulesToFilterConfigV2(service, option)
 	rbacLog.Debugf("generated http filter config: %v", *config)
 	out := &http_conn.HttpFilter{
 		Name: rbacHTTPFilterName,
@@ -466,6 +480,118 @@ func buildHTTPFilter(service *serviceMetadata, option rbacOption, is11 bool) *ht
 	}
 
 	return out
+}
+
+func convertRbacRulesToFilterConfigV2(service *serviceMetadata, option rbacOption) *http_config.RBAC {
+	rbac := &policyproto.RBAC{
+		Action:   policyproto.RBAC_ALLOW,
+		Policies: map[string]*policyproto.Policy{},
+	}
+	namespace := service.attributes[attrDestNamespace]
+	// Check to make sure we are not dereferencing null pointers.
+	if option.authzPolicies == nil || option.authzPolicies.NamespaceToAuthorizationConfigV2 == nil {
+		return &http_config.RBAC{Rules: rbac}
+	}
+	if _, present := option.authzPolicies.NamespaceToAuthorizationConfigV2[namespace]; !present {
+		return &http_config.RBAC{Rules: rbac}
+	}
+
+	// Get all AuthorizationPolicy Istio config from this namespace.
+	allAuthzPolicies := option.authzPolicies.NamespaceToAuthorizationConfigV2[namespace].AuthzPolicies
+	// Get all key-value pairs of AuthorizationPolicy name to list of Subjects/ServiceRoleBindings in this namespace.
+	authzPolicyToSubjects := option.authzPolicies.AuthzPolicyToAllowSubjects(namespace)
+	for _, authzPolicy := range allAuthzPolicies {
+		// If a WorkloadSelector is used in the AuthorizationPolicy config and does not match this service, skip this
+		// AuthorizationPolicy.
+		if authzPolicy.Policy.WorkloadSelector != nil && !service.matchV2(authzPolicy.Policy.WorkloadSelector.Labels) {
+			continue
+		}
+		for i, subject := range authzPolicyToSubjects[authzPolicy.Name] {
+			// Convert role.
+			role := option.authzPolicies.GetServiceRoleFromName(namespace, subject.RoleRef.Name)
+			permission := &policyproto.Permission{}
+			rule := role.Rules[0]
+
+			// Check to make sure that the current service (caller) is the one this rule is applying to.
+			if !service.match(rule) {
+				continue
+			}
+			// Also check if we have converted this rule before.
+			if !hasConvertedPermission(rbac, subject.RoleRef.Name) {
+				rbacLog.Debugf("rules[%d] matched", i)
+				if option.forTCPFilter {
+					// TODO(yangminzhu): Move the validate logic to push context and add metrics.
+					if err := validateRuleForTCPFilter(rule); err != nil {
+						// It's a user misconfiguration if a HTTP rule is specified to a TCP service.
+						// For safety consideration, we ignore the whole rule which means no access is opened to
+						// the TCP service in this case.
+						rbacLog.Debugf("rules[%d] ignored, found HTTP only rule for a TCP service: %v", i, err)
+						continue
+					}
+				}
+				// Generate the policy if the service is matched and validated to the services specified in
+				// ServiceRole.
+				permission = convertToPermission(rule)
+				if permission.String() == "" {
+					rbacLog.Debugf("role %s skipped for no rule matched", subject.RoleRef.Name)
+					continue
+				}
+			}
+
+			if option.forTCPFilter {
+				if err := validateBindingsForTCPFilter([]*rbacproto.ServiceRoleBinding{subject}); err != nil {
+					rbacLog.Debugf("role %s skipped, found HTTP only binding for a TCP service: %v", subject.RoleRef.Name, err)
+					continue
+				}
+			}
+			// Convert binding.
+			enforcedPrincipals, _ := convertToPrincipals([]*rbacproto.ServiceRoleBinding{subject}, option.forTCPFilter)
+
+			if len(enforcedPrincipals) == 0 {
+				rbacLog.Debugf("role %s skipped for no principals found", subject.RoleRef.Name)
+				continue
+			}
+
+			if len(enforcedPrincipals) != 0 {
+				if rbac.Policies[subject.RoleRef.Name] == nil {
+					rbac.Policies[subject.RoleRef.Name] = &policyproto.Policy{
+						Permissions: []*policyproto.Permission{permission},
+						Principals:  enforcedPrincipals,
+					}
+				} else {
+					allPrincipals := rbac.Policies[subject.RoleRef.Name].Principals
+					// Check if enforcedPrincipals has existed in the allPrincipals.
+					if !isPrincipalInPrincipals(enforcedPrincipals, allPrincipals) {
+						rbac.Policies[subject.RoleRef.Name].Principals = append(allPrincipals, enforcedPrincipals...)
+					}
+				}
+			}
+		}
+	}
+	return &http_config.RBAC{Rules: rbac}
+}
+
+// isPrincipalInPrincipals returns True if the |principal| has already existed in the |principals|,
+// False otherwise.
+// This is to make sure we don't have duplicated principals for the same ServiceRole.
+// This can happen when we have a service foo that has more than one exact Subjects (in one or many
+// AuthorizationPolicy) that bind to the same ServiceRole.
+func isPrincipalInPrincipals(principal []*policyproto.Principal, principals []*policyproto.Principal) bool {
+	for _, currPrincipal := range principals {
+		if currPrincipal.String() == principal[0].String() {
+			return true
+		}
+	}
+	return false
+}
+
+// hasConvertedPermission returns True if there is already a |roleName| ServiceRole for |rbac|, False
+// otherwise.
+func hasConvertedPermission(rbac *policyproto.RBAC, roleName string) bool {
+	if rbac.Policies[roleName] == nil {
+		return false
+	}
+	return rbac.Policies[roleName].Permissions != nil
 }
 
 // convertRbacRulesToFilterConfig converts the current RBAC rules (ServiceRole and ServiceRoleBindings)
--- pilot/pkg/networking/plugin/authz/rbac_test.go
+++ pilot/pkg/networking/plugin/authz/rbac_test.go
@@ -263,10 +263,14 @@ func TestBuildHTTPFilter(t *testing.T) {
 	}
 	bindingCfg := model.Config{
 		ConfigMeta: model.ConfigMeta{
-			Type: model.ServiceRoleBinding.Type, Name: "test-binding-1", Namespace: "default"},
-		Spec: &rbacproto.ServiceRoleBinding{
-			Subjects: []*rbacproto.Subject{{User: "test-user-1"}},
-			RoleRef:  &rbacproto.RoleRef{Kind: "ServiceRole", Name: "test-role-1"},
+			Type: model.AuthorizationPolicy.Type, Name: "test-binding-1", Namespace: "default"},
+		Spec: &rbacproto.AuthorizationPolicy{
+			Allow: []*rbacproto.ServiceRoleBinding{
+				{
+					Subjects: []*rbacproto.Subject{{User: "test-user-1"}},
+					RoleRef:  &rbacproto.RoleRef{Kind: "ServiceRole", Name: "test-role-1"},
+				},
+			},
 		},
 	}
 	option := rbacOption{
@@ -323,6 +327,297 @@ func TestBuildHTTPFilter(t *testing.T) {
 				} else if _, ok := rbac.Policies[tc.Policy]; !ok {
 					t.Errorf("%s: expecting policy %s but got %v", tc.Name, tc.Policy, rbac.Policies)
 				}
+			}
+		}
+	}
+}
+
+func TestConvertRbacRulesToFilterConfigV2(t *testing.T) {
+	roles := []model.Config{
+		{
+			ConfigMeta: model.ConfigMeta{Name: "service-role-1"},
+			Spec: &rbacproto.ServiceRole{
+				Rules: []*rbacproto.AccessRule{
+					{
+						Services:   []string{"backup_service"},
+						NotMethods: []string{"DELETE"},
+					},
+				},
+			},
+		},
+		{
+			ConfigMeta: model.ConfigMeta{Name: "service-role-2"},
+			Spec: &rbacproto.ServiceRole{
+				Rules: []*rbacproto.AccessRule{
+					{
+						Services: []string{"service-2"},
+					},
+				},
+			},
+		},
+		{
+			ConfigMeta: model.ConfigMeta{Name: "service-role-3"},
+			Spec: &rbacproto.ServiceRole{
+				Rules: []*rbacproto.AccessRule{
+					{
+						Services: []string{"productpage"},
+					},
+				},
+			},
+		},
+		{
+			ConfigMeta: model.ConfigMeta{Name: "service-role-4"},
+			Spec: &rbacproto.ServiceRole{
+				Rules: []*rbacproto.AccessRule{
+					{
+						Services: []string{"secret-service"},
+					},
+				},
+			},
+		},
+	}
+	bindings := []model.Config{
+		{
+			ConfigMeta: model.ConfigMeta{Name: "authz-policy-multiple-bindings"},
+			Spec: &rbacproto.AuthorizationPolicy{
+				Allow: []*rbacproto.ServiceRoleBinding{
+					{
+						Subjects: []*rbacproto.Subject{
+							{
+								NotNames: []string{"cluster.local/ns/testing/sa/unstable-service"},
+							},
+						},
+						RoleRef: &rbacproto.RoleRef{
+							Kind: "ServiceRole",
+							Name: "service-role-3",
+						},
+					},
+					{
+						Subjects: []*rbacproto.Subject{
+							{
+								Namespaces: []string{"*"},
+							},
+						},
+						RoleRef: &rbacproto.RoleRef{
+							Kind: "ServiceRole",
+							Name: "service-role-1",
+						},
+					},
+				},
+			},
+		},
+		{
+			ConfigMeta: model.ConfigMeta{Name: "authz-policy-single-binding"},
+			Spec: &rbacproto.AuthorizationPolicy{
+				Allow: []*rbacproto.ServiceRoleBinding{
+					{
+						Subjects: []*rbacproto.Subject{
+							{
+								NotNames: []string{"cluster.local/ns/testing/sa/unstable-service"},
+							},
+						},
+						RoleRef: &rbacproto.RoleRef{
+							Kind: "ServiceRole",
+							Name: "service-role-2",
+						},
+					},
+				},
+			},
+		},
+		{
+			ConfigMeta: model.ConfigMeta{Name: "authz-policy-with-selector"},
+			Spec: &rbacproto.AuthorizationPolicy{
+				WorkloadSelector: &rbacproto.WorkloadSelector{
+					Labels: map[string]string{
+						"app": "productpage",
+					},
+				},
+				Allow: []*rbacproto.ServiceRoleBinding{
+					{
+						Subjects: []*rbacproto.Subject{
+							{
+								NotNames: []string{"cluster.local/ns/testing/sa/unstable-service"},
+							},
+						},
+						RoleRef: &rbacproto.RoleRef{
+							Kind: "ServiceRole",
+							Name: "service-role-3",
+						},
+					},
+				},
+			},
+		},
+		{
+			ConfigMeta: model.ConfigMeta{Name: "authz-policy-with-selector-no-match"},
+			Spec: &rbacproto.AuthorizationPolicy{
+				WorkloadSelector: &rbacproto.WorkloadSelector{
+					Labels: map[string]string{
+						"app": "notfound",
+					},
+				},
+				Allow: []*rbacproto.ServiceRoleBinding{
+					{
+						Subjects: []*rbacproto.Subject{
+							{
+								NotNames: []string{"cluster.local/ns/testing/sa/unstable-service"},
+							},
+						},
+						RoleRef: &rbacproto.RoleRef{
+							Kind: "ServiceRole",
+							Name: "service-role-4",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	policy1 := &policy.Policy{
+		Permissions: []*policy.Permission{
+			{
+				Rule: &policy.Permission_AndRules{
+					AndRules: &policy.Permission_Set{
+						Rules: []*policy.Permission{
+							{
+								Rule: &policy.Permission_NotRule{
+									NotRule: &policy.Permission{
+										Rule: generateHeaderRule([]*route.HeaderMatcher{
+											{Name: methodHeader, HeaderMatchSpecifier: &route.HeaderMatcher_ExactMatch{ExactMatch: "DELETE"}},
+										}),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		Principals: []*policy.Principal{{
+			Identifier: &policy.Principal_AndIds{
+				AndIds: &policy.Principal_Set{
+					Ids: []*policy.Principal{
+						{
+							Identifier: &policy.Principal_OrIds{
+								OrIds: &policy.Principal_Set{
+									Ids: []*policy.Principal{
+										{
+											Identifier: &policy.Principal_Metadata{
+												Metadata: generateMetadataStringMatcher(
+													attrSrcPrincipal, &metadata.StringMatcher{
+														MatchPattern: &metadata.StringMatcher_Regex{
+															Regex: ".*/ns/.*/.*",
+														}},
+													authn.AuthnFilterName),
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}},
+	}
+	policy2 := &policy.Policy{
+		Permissions: []*policy.Permission{{
+			Rule: &policy.Permission_AndRules{
+				AndRules: &policy.Permission_Set{
+					Rules: []*policy.Permission{
+						{
+							Rule: &policy.Permission_Any{
+								Any: true,
+							},
+						},
+					},
+				},
+			},
+		},
+		},
+		Principals: []*policy.Principal{{
+			Identifier: &policy.Principal_AndIds{
+				AndIds: &policy.Principal_Set{
+					Ids: []*policy.Principal{
+						{
+							Identifier: &policy.Principal_NotId{
+								NotId: &policy.Principal{
+									Identifier: &policy.Principal_OrIds{
+										OrIds: &policy.Principal_Set{
+											Ids: []*policy.Principal{
+												{
+													Identifier: &policy.Principal_Metadata{
+														Metadata: generateMetadataStringMatcher(
+															attrSrcPrincipal, &metadata.StringMatcher{
+																MatchPattern: &metadata.StringMatcher_Exact{Exact: "cluster.local/ns/testing/sa/unstable-service"}}, authn.AuthnFilterName),
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}},
+	}
+	policy3 := &policy.Policy{}
+
+	expectRbac1 := generateExpectRBACForSinglePolicy("service-role-1", policy1)
+	expectRbac2 := generateExpectRBACForSinglePolicy("service-role-2", policy2)
+	expectRbac3 := generateExpectRBACForSinglePolicy("service-role-3", policy2)
+	expectRbac4 := generateExpectRBACForSinglePolicy("" /*serviceRoleName*/, policy3)
+
+	authzPolicies := newAuthzPoliciesWithRolesAndBindings(roles, bindings)
+	option := rbacOption{authzPolicies: authzPolicies}
+	testCases := []struct {
+		name    string
+		service *serviceMetadata
+		rbac    *policy.RBAC
+		option  rbacOption
+	}{
+		{
+			name: "multiple bindings",
+			service: &serviceMetadata{
+				name: "backup_service",
+			},
+			rbac:   expectRbac1,
+			option: option,
+		},
+		{
+			name: "single binding",
+			service: &serviceMetadata{
+				name: "service-2",
+			},
+			rbac:   expectRbac2,
+			option: option,
+		},
+		{
+			name: "with workload selector",
+			service: &serviceMetadata{
+				name:   "productpage",
+				labels: map[string]string{"app": "productpage"},
+			},
+			rbac:   expectRbac3,
+			option: option,
+		},
+		{
+			name: "with workload selector but no match",
+			service: &serviceMetadata{
+				name:   "secret-service",
+				labels: map[string]string{"app": "productpage"},
+			},
+			rbac:   expectRbac4,
+			option: option,
+		},
+	}
+
+	for _, tc := range testCases {
+		if tc.name == "with workload selector" {
+			rbac := convertRbacRulesToFilterConfigV2(tc.service, tc.option)
+			if !reflect.DeepEqual(*tc.rbac, *rbac.Rules) {
+				t.Errorf("%s want:\n%v\nbut got:\n%v", tc.name, *tc.rbac, *rbac.Rules)
 			}
 		}
 	}
