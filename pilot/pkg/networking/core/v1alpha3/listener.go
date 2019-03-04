49f19a90bdc9762a155c0af4c694e650fca2e54d
Serguei Bezverkhi <sbezverk@cisco.com>
2019-03-04 08:22:51 -0500

    Reverting debug related changes
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>

--- pilot/pkg/networking/core/v1alpha3/listener.go
+++ pilot/pkg/networking/core/v1alpha3/listener.go
@@ -219,10 +219,9 @@ func (configgen *ConfigGeneratorImpl) buildSidecarListeners(env *model.Environme
 		}
 
 		// add an extra listener that binds to the port that is the recipient of the iptables redirect
-		// ><SB> Changing WildcardAddress
 		listeners = append(listeners, &xdsapi.Listener{
 			Name:           VirtualListenerName,
-			Address:        util.BuildAddress("::" /*WildcardAddress*/, uint32(mesh.ProxyListenPort)),
+			Address:        util.BuildAddress(WildcardAddress, uint32(mesh.ProxyListenPort)),
 			Transparent:    transparent,
 			UseOriginalDst: proto.BoolTrue,
 			FilterChains: []listener.FilterChain{
@@ -231,7 +230,6 @@ func (configgen *ConfigGeneratorImpl) buildSidecarListeners(env *model.Environme
 				},
 			},
 		})
-		log.Warnf("><SB> Listeners for a sidecar proxy: %+v", listeners)
 	}
 
 	httpProxyPort := mesh.ProxyHttpPort
