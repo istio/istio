860d0d3d189d878c22bef8004296d4e88a717a4b
Zhonghu Xu <xuzhonghu@huawei.com>
2019-03-07 02:04:55 +0800

    wait for service and config cache sync before serving http and grpc (#12278)
    * wait for service and config cache sync before serving http and grpc
    
    * wait for cache sync before running queue
    
    * fix
    
    * Add PILOT_ENABLE_WAIT_CACHE_SYNC env to control if pilot should wait for cache sync
    
    * fix comments

--- pilot/pkg/serviceregistry/kube/controller.go
+++ pilot/pkg/serviceregistry/kube/controller.go
@@ -35,6 +35,7 @@ import (
 
 	meshconfig "istio.io/api/mesh/v1alpha1"
 	"istio.io/istio/pilot/pkg/model"
+	"istio.io/istio/pkg/features/pilot"
 	"istio.io/istio/pkg/log"
 )
 
@@ -256,7 +257,12 @@ func (c *Controller) HasSynced() bool {
 
 // Run all controllers until a signal is received
 func (c *Controller) Run(stop <-chan struct{}) {
-	go c.queue.Run(stop)
+	go func() {
+		if pilot.EnableWaitCacheSync {
+			cache.WaitForCacheSync(stop, c.HasSynced)
+		}
+		c.queue.Run(stop)
+	}()
 
 	go c.services.informer.Run(stop)
 	go c.pods.informer.Run(stop)
