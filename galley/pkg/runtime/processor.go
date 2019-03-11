3e8d0e5986435724634aebf0c7e07731374c39fb
Nathan Mittler <nathanmittler@google.com>
2019-03-11 09:59:33 -0700

    [Galley] Introduce processing.Listener (#12167)
    This is a break-out PR from #11293. This changes the runtime
    processor and state to use a listener callback for detecting when
    a collection has changed.

--- galley/pkg/runtime/processor.go
+++ galley/pkg/runtime/processor.go
@@ -17,6 +17,7 @@ package runtime
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 
 	"istio.io/istio/galley/pkg/metadata"
@@ -37,13 +38,19 @@ type Processor struct {
 	// source interface for retrieving the events from.
 	source Source
 
+	distribute bool
+
+	// eventCh channel that was obtained from source
+	eventCh chan resource.Event
+
 	// handler for events.
 	handler processing.Handler
 
-	eventCh chan resource.Event
-
 	// The current in-memory configuration State
-	state *State
+	state         *State
+	stateStrategy *publish.Strategy
+
+	distributor publish.Distributor
 
 	// hook that gets called after each event processing. Useful for testing.
 	postProcessHook postProcessHookFn
@@ -51,32 +58,51 @@ type Processor struct {
 	// lastEventTime records the last time an event was received.
 	lastEventTime time.Time
 
+	// worker handles the lifecycle of the processing worker thread.
 	worker *util.Worker
+
+	// Condition used to notify callers of AwaitFullSync that the full sync has occurred.
+	fullSyncCond *sync.Cond
 }
 
 type postProcessHookFn func()
 
 // NewProcessor returns a new instance of a Processor
 func NewProcessor(src Source, distributor publish.Distributor, cfg *Config) *Processor {
-	state := newState(groups.Default, metadata.Types, cfg, publish.NewStrategyWithDefaults(), distributor)
-	return newProcessor(state, src, nil)
+	stateStrategy := publish.NewStrategyWithDefaults()
+
+	return newProcessor(src, cfg, metadata.Types, stateStrategy, distributor, nil)
 }
 
 func newProcessor(
-	state *State,
 	src Source,
+	cfg *Config,
+	schema *resource.Schema,
+	stateStrategy *publish.Strategy,
+	distributor publish.Distributor,
 	postProcessHook postProcessHookFn) *Processor {
-
 	now := time.Now()
-	return &Processor{
-		handler:         buildDispatcher(state),
-		state:           state,
+
+	p := &Processor{
+		stateStrategy:   stateStrategy,
+		distributor:     distributor,
 		source:          src,
 		eventCh:         make(chan resource.Event, 1024),
 		postProcessHook: postProcessHook,
 		worker:          util.NewWorker("runtime processor", scope),
 		lastEventTime:   now,
+		fullSyncCond:    sync.NewCond(&sync.Mutex{}),
 	}
+	stateListener := processing.ListenerFromFn(func(c resource.Collection) {
+		// When the state indicates a change occurred, update the publishing strategy
+		if p.distribute {
+			stateStrategy.OnChange()
+		}
+	})
+	p.state = newState(schema, cfg, stateListener)
+
+	p.handler = buildDispatcher(p.state)
+	return p
 }
 
 // Start the processor. This will cause processor to listen to incoming events from the provider
@@ -97,7 +123,7 @@ func (p *Processor) Start() error {
 		defer func() {
 			scope.Debugf("Process.process: Exiting worker thread")
 			close(p.eventCh)
-			p.state.close()
+			p.stateStrategy.Reset()
 		}()
 
 		defer p.source.Stop()
@@ -112,9 +138,10 @@ func (p *Processor) Start() error {
 				return
 			case e := <-p.eventCh:
 				p.processEvent(e)
-			case <-p.state.strategy.Publish:
+			case <-p.stateStrategy.Publish:
 				scope.Debug("Processor.process: publish")
-				p.state.publish()
+				s := p.state.buildSnapshot()
+				p.distributor.SetSnapshot(groups.Default, s)
 			}
 
 			if p.postProcessHook != nil {
@@ -132,6 +159,16 @@ func (p *Processor) Stop() {
 	p.worker.Stop()
 }
 
+// AwaitFullSync waits until the full sync event is received from the source. For testing purposes only.
+func (p *Processor) AwaitFullSync() {
+	p.fullSyncCond.L.Lock()
+	defer p.fullSyncCond.L.Unlock()
+
+	if !p.distribute {
+		p.fullSyncCond.Wait()
+	}
+}
+
 func (p *Processor) processEvent(e resource.Event) {
 	if scope.DebugEnabled() {
 		scope.Debugf("Incoming source event: %v", e)
@@ -140,7 +177,13 @@ func (p *Processor) processEvent(e resource.Event) {
 
 	if e.Kind == resource.FullSync {
 		scope.Infof("Synchronization is complete, starting distribution.")
-		p.state.onFullSync()
+
+		p.fullSyncCond.L.Lock()
+		p.distribute = true
+		p.fullSyncCond.Broadcast()
+		p.fullSyncCond.L.Unlock()
+
+		p.stateStrategy.OnChange()
 		return
 	}
 
@@ -153,12 +196,12 @@ func (p *Processor) recordEvent() {
 	p.lastEventTime = now
 }
 
-func buildDispatcher(states ...*State) *processing.Dispatcher {
+func buildDispatcher(state *State) *processing.Dispatcher {
 	b := processing.NewDispatcherBuilder()
-	for _, state := range states {
-		for _, spec := range state.schema.All() {
-			b.Add(spec.Collection, state)
-		}
+
+	for _, spec := range state.schema.All() {
+		b.Add(spec.Collection, state)
 	}
+
 	return b.Build()
 }
