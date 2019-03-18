d0160c7d91bc6b2eaf65b62d2808d0938bf447d7
Zhonghu Xu <xuzhonghu@huawei.com>
2019-03-18 15:46:38 +0800

    Revert "Allow no hosts in the ServiceEntry validation (#12497)" (#12559)
    This reverts commit ffce6df70376079e4ee04d6e7e6476c5bf6f7275.

--- pilot/pkg/model/validation.go
+++ pilot/pkg/model/validation.go
@@ -2145,10 +2145,9 @@ func ValidateServiceEntry(name, namespace string, config proto.Message) (errs er
 		return fmt.Errorf("cannot cast to service entry")
 	}
 
-	if len(serviceEntry.Hosts) == 0 && len(serviceEntry.Addresses) == 0 {
-		errs = appendErrors(errs, fmt.Errorf("addresses and/or hosts must be specified"))
+	if len(serviceEntry.Hosts) == 0 {
+		errs = appendErrors(errs, fmt.Errorf("service entry must have at least one host"))
 	}
-
 	for _, host := range serviceEntry.Hosts {
 		// Full wildcard is not allowed in the service entry.
 		if host == "*" {
