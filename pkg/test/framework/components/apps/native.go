8bdba1abe3e485a616d0338bf37142c86bd174df
Oz Evren <ozben@google.com>
2019-03-18 21:10:11 -0700

    Fix lint errors.

--- pkg/test/framework/components/apps/native.go
+++ pkg/test/framework/components/apps/native.go
@@ -27,6 +27,7 @@ import (
 	xdsapi "github.com/envoyproxy/go-control-plane/envoy/api/v2"
 	"github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
 	"github.com/hashicorp/go-multierror"
+
 	"istio.io/istio/pkg/test/framework/resource"
 
 	"istio.io/istio/pilot/pkg/model"
