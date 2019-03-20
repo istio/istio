3a73cb6f3d06735052010e37bfa8b93ff50f0e38
icygalz <angwar@google.com>
2019-03-20 11:29:48 -0700

    Update install_prereqs_debian.sh

--- tests/e2e/local/kind/install_prereqs_debian.sh
+++ tests/e2e/local/kind/install_prereqs_debian.sh
@@ -18,9 +18,7 @@ function check_and_install_golang() {
 function check_and_install_kind() {
 	echo "Checking KinD is installed..."
 	if ! kind --help > /dev/null; then
-		if ! (curl -Lo kind https://github.com/kubernetes-sigs/kind/releases/download/0.0.1/kind-linux-amd64 && \
-			chmod +x kind && \
-			sudo mv kind /usr/local/bin/); then
+		if ! (go get sigs.k8s.io/kind); then
 			echo "Looks like KinD installation failed."
 			echo "Please install it manually then run this script again."
 			exit 1
