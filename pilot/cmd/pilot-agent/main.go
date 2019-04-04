a788675c8cc25aa49a0c6aa80bbd49152b45045b
Dan Richelson <drichelson@users.noreply.github.com>
2019-04-04 07:49:01 -0700

    Cherry pick cert file config from master to release-1.1 (#12707)
    * Cherry pick from master: Configuration:  no longer hardcode mesh certs (#12189)
    
    * Configuration: Pilot-Agent: no longer hardcode certs to watch. Pilot-Discovery: no longer hardcode Envoy listener cert paths.
    
    * Address demands of golangcibot overlord
    
    * Change usages of github.com/stretchr/testify/require to github.com/stretchr/testify/assert
    
    * Address code style violation
    
    * Revert temporary api changes. Set cert paths in envoy node metadata and use them when setting up listeners
    
    * Use envoy node metadata cert paths (if available) when constructing clusters
    
    * Rename constants to make golint happy
    
    * Fix imports
    
    * Ignore ordering in test
    
    * Pass around proxy instead of proxy.Metadata
    
    (cherry picked from commit 7c342741df9bd4e313420b4d17e279089d8956da)
    
    * goimports file

--- pilot/cmd/pilot-agent/main.go
+++ pilot/cmd/pilot-agent/main.go
@@ -22,6 +22,7 @@ import (
 	"io/ioutil"
 	"net"
 	"os"
+	"path"
 	"strconv"
 	"strings"
 	"sync"
@@ -77,6 +78,13 @@ var (
 	concurrency                int
 	templateFile               string
 	disableInternalTelemetry   bool
+	tlsServerCertChain         string
+	tlsServerKey               string
+	tlsServerRootCert          string
+	tlsClientCertChain         string
+	tlsClientKey               string
+	tlsClientRootCert          string
+	tlsCertsToWatch            []string
 	loggingOptions             = log.DefaultOptions()
 
 	wg sync.WaitGroup
@@ -140,6 +148,40 @@ var (
 			spiffe.SetTrustDomain(spiffe.DetermineTrustDomain(role.TrustDomain, true))
 			role.TrustDomain = spiffe.GetTrustDomain()
 			log.Infof("Proxy role: %#v", role)
+
+			// Add cert paths as node metadata only if they differ from defaults
+			if tlsServerCertChain != model.DefaultCertChain {
+				role.Metadata[model.NodeMetadataTLSServerCertChain] = tlsServerCertChain
+			}
+			if tlsServerKey != model.DefaultKey {
+				role.Metadata[model.NodeMetadataTLSServerKey] = tlsServerKey
+			}
+			if tlsServerRootCert != model.DefaultRootCert {
+				role.Metadata[model.NodeMetadataTLSServerRootCert] = tlsServerRootCert
+			}
+
+			if tlsClientCertChain != model.DefaultCertChain {
+				role.Metadata[model.NodeMetadataTLSClientCertChain] = tlsClientCertChain
+			}
+			if tlsClientKey != model.DefaultKey {
+				role.Metadata[model.NodeMetadataTLSClientKey] = tlsClientKey
+			}
+			if tlsClientRootCert != model.DefaultRootCert {
+				role.Metadata[model.NodeMetadataTLSClientRootCert] = tlsClientRootCert
+			}
+
+			tlsCertsToWatch = []string{
+				tlsServerCertChain, tlsServerKey, tlsServerRootCert,
+				tlsClientCertChain, tlsClientKey, tlsClientCertChain,
+			}
+
+			if role.Type == model.Ingress {
+				tlsCertsToWatch = append(tlsCertsToWatch, path.Join(model.IngressCertsPath, model.IngressCertFilename))
+				tlsCertsToWatch = append(tlsCertsToWatch, path.Join(model.IngressCertsPath, model.IngressKeyFilename))
+			}
+
+			// dedupe cert paths so we don't set up 2 watchers for the same file:
+			tlsCertsToWatch = dedupeStrings(tlsCertsToWatch)
 
 			proxyConfig := model.DefaultProxyConfig()
 
@@ -234,21 +276,13 @@ var (
 				log.Infof("Effective config: %s", out)
 			}
 
-			certs := []envoy.CertSource{
-				{
-					Directory: model.AuthCertsPath,
-					Files:     []string{model.CertChainFilename, model.KeyFilename, model.RootCertFilename},
-				},
+			log.Infof("Monitored certs: %#v", tlsCertsToWatch)
+			// since Envoy needs the certs for mTLS, we wait for them to become available before starting it
+			if controlPlaneAuthPolicy == meshconfig.AuthenticationPolicy_MUTUAL_TLS.String() {
+				for _, cert := range tlsCertsToWatch {
+					waitForCerts(cert, 2*time.Minute)
+				}
 			}
-
-			if role.Type == model.Ingress {
-				certs = append(certs, envoy.CertSource{
-					Directory: model.IngressCertsPath,
-					Files:     []string{model.IngressCertFilename, model.IngressKeyFilename},
-				})
-			}
-
-			log.Infof("Monitored certs: %#v", certs)
 
 			// TODO: change Mixer and Pilot to use standard template and deprecate this custom bootstrap parser
 			if controlPlaneBootstrap {
@@ -318,7 +352,7 @@ var (
 
 			envoyProxy := envoy.NewProxy(proxyConfig, role.ServiceNode(), proxyLogLevel, pilotSAN, role.IPAddresses)
 			agent := proxy.NewAgent(envoyProxy, proxy.DefaultRetry, pilot.TerminationDrainDuration())
-			watcher := envoy.NewWatcher(certs, agent.ConfigCh())
+			watcher := envoy.NewWatcher(tlsCertsToWatch, agent.ConfigCh())
 
 			go waitForCompletion(ctx, agent.Run)
 			go waitForCompletion(ctx, watcher.Run)
@@ -328,6 +362,18 @@ var (
 		},
 	}
 )
+
+func dedupeStrings(in []string) []string {
+	stringMap := map[string]bool{}
+	for _, c := range in {
+		stringMap[c] = true
+	}
+	unique := make([]string, 0)
+	for c := range stringMap {
+		unique = append(unique, c)
+	}
+	return unique
+}
 
 func waitForCompletion(ctx context.Context, fn func(context.Context)) {
 	wg.Add(1)
@@ -463,6 +509,22 @@ func init() {
 	proxyCmd.PersistentFlags().BoolVar(&controlPlaneBootstrap, "controlPlaneBootstrap", true,
 		"Process bootstrap provided via templateFile to be used by control plane components.")
 
+	// server certs
+	proxyCmd.PersistentFlags().StringVar(&tlsServerCertChain, "tlsServerCertChain",
+		model.DefaultCertChain, "Absolute path to server cert-chain file used for istio mTLS")
+	proxyCmd.PersistentFlags().StringVar(&tlsServerKey, "tlsServerKey",
+		model.DefaultKey, "Absolute path to server private key file used for istio mTLS")
+	proxyCmd.PersistentFlags().StringVar(&tlsServerRootCert, "tlsServerRootCert",
+		model.DefaultRootCert, "Absolute path to server root cert file used for istio mTLS")
+
+	// client certs
+	proxyCmd.PersistentFlags().StringVar(&tlsClientCertChain, "tlsClientCertChain",
+		model.DefaultCertChain, "Absolute path to client cert-chain file used for istio mTLS")
+	proxyCmd.PersistentFlags().StringVar(&tlsClientKey, "tlsSClientKey",
+		model.DefaultKey, "Absolute path to client key file used for istio mTLS")
+	proxyCmd.PersistentFlags().StringVar(&tlsClientRootCert, "tlsClientRootCert",
+		model.DefaultRootCert, "Absolute path to client root cert file used for istio mTLS")
+
 	// Attach the Istio logging options to the command.
 	loggingOptions.AttachCobraFlags(rootCmd)
 
@@ -476,6 +538,37 @@ func init() {
 		Section: "pilot-agent CLI",
 		Manual:  "Istio Pilot Agent",
 	}))
+}
+
+func waitForCerts(fname string, maxWait time.Duration) {
+	log.Infof("waiting %v for %s", maxWait, fname)
+
+	logDelay := 1 * time.Second
+	nextLog := time.Now().Add(logDelay)
+	endWait := time.Now().Add(maxWait)
+
+	for {
+		_, err := os.Stat(fname)
+		if err == nil {
+			return
+		}
+		if !os.IsNotExist(err) { // another error (e.g., permission) - likely no point in waiting longer
+			log.Errora("error while waiting for certificates", err.Error())
+			return
+		}
+
+		now := time.Now()
+		if now.After(endWait) {
+			log.Warna("certificates still not available after", maxWait)
+			break
+		}
+		if now.After(nextLog) {
+			log.Infof("waiting for certificates")
+			logDelay = logDelay * 2
+			nextLog.Add(logDelay)
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
 }
 
 func main() {
