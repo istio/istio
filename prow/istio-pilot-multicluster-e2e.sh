35732956b20cd36df593451a219ba9902d7b8ce3
Vadim Eisenberg <vadime@il.ibm.com>
2019-04-07 16:24:48 +0300

    remove the flat network multicluster test until it will be fixed

--- prow/istio-pilot-multicluster-e2e.sh
+++ prow/istio-pilot-multicluster-e2e.sh
@@ -22,8 +22,5 @@ export SETUP_CLUSTERREG="True"
 CLUSTERREG_DIR="${CLUSTERREG_DIR:-$(mktemp -d /tmp/clusterregXXX)}"
 export CLUSTERREG_DIR
 
-#echo 'Running pilot multi-cluster e2e tests (v1alpha1, noauth)'
-./prow/e2e-suite.sh --timeout 50 --cluster_registry_dir="$CLUSTERREG_DIR" --single_test e2e_pilotv2_v1alpha3 "$@"
-
 #echo 'Running pilot multi-cluster split horizon e2e tests'
 ./prow/e2e-suite.sh --timeout 50 --cluster_registry_dir="$CLUSTERREG_DIR" --single_test e2e_multicluster_split_horizon "$@"
