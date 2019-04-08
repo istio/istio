c75ba6295bb7fdc32c8524a82516e6f14527ec2a
Vadim Eisenberg <vadime@il.ibm.com>
2019-04-08 11:02:40 +0300

    add split horizon pilot tests
    Running the regular pilot tests on two clusters, with Split Horizon EDS.
    The apps are deployed in the both clusters identically.

--- prow/istio-pilot-multicluster-e2e.sh
+++ prow/istio-pilot-multicluster-e2e.sh
@@ -24,3 +24,6 @@ export CLUSTERREG_DIR
 
 #echo 'Running pilot multi-cluster split horizon e2e tests'
 ./prow/e2e-suite.sh --timeout 50 --cluster_registry_dir="$CLUSTERREG_DIR" --single_test e2e_multicluster_split_horizon "$@"
+
+#echo 'Running pilot multi-cluster e2e tests with split horizon (v1alpha1, auth)'
+./prow/e2e-suite.sh --timeout 50 --cluster_registry_dir="$CLUSTERREG_DIR" --single_test e2e_pilotv2_v1alpha3_auth "$@"
