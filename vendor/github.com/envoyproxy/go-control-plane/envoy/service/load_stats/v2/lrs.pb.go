fe0a151c0ee3b331c575a21aaa7eda4f772b9cce
Kuat <kyessenov@users.noreply.github.com>
2019-04-08 19:32:26 -0700

    update go-control-plane (#13154)
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- vendor/github.com/envoyproxy/go-control-plane/envoy/service/load_stats/v2/lrs.pb.go
+++ vendor/github.com/envoyproxy/go-control-plane/envoy/service/load_stats/v2/lrs.pb.go
@@ -9,9 +9,9 @@ import (
 	io "io"
 	math "math"
 
+	_ "github.com/envoyproxy/protoc-gen-validate/validate"
 	proto "github.com/gogo/protobuf/proto"
 	types "github.com/gogo/protobuf/types"
-	_ "github.com/lyft/protoc-gen-validate/validate"
 	grpc "google.golang.org/grpc"
 
 	core "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
