04cf6338aeaa456ce6f2b224dafdb8e8a6ed7c6a
Ralf Pannemans <ralf.pannemans@sap.com>
2019-04-09 12:38:34 +0200

    Fix trap handling
    Co-authored-by: Ulrich Kramer <u.kramer@sap.com>
    Signed-off-by: Ulrich Kramer <u.kramer@sap.com>
    Signed-off-by: Ralf Pannemans <ralf.pannemans@sap.com>

--- .concourse/bookinfo-watch.sh
+++ .concourse/bookinfo-watch.sh
@@ -5,10 +5,11 @@
 CMD=$*
 # Run command in background
 touch command_running
-( set +e ; $CMD ; RTC=$?; rm -f  command_running; exit $RTC ) &
-PID=$!
+bash -cxe "trap 'rm -f  command_running' EXIT; $CMD" &
+CMD_PID=$!
+GPID=$(ps -o pgid= $CMD_PID)
 # In case of CTRL-C this will also kill the background process
-trap 'kill $PID; exit' SIGINT
+trap "set -x; pkill -g $GPID; exit 1" SIGINT
 
 # Test if bookinfo is working
 export INGRESS_HOST=$(kubectl -n istio-ingress get service ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
@@ -16,14 +17,16 @@ export INGRESS_PORT=$(kubectl -n istio-ingress get service ingressgateway -o jso
 export PRODUCTPAGE_URL=http://${INGRESS_HOST}:${INGRESS_PORT}/productpage
 while [ -f command_running ]
 do
-    RESULT=$(curl -s -o /dev/null -w "%{http_code}" $PRODUCTPAGE_URL)
-    if [ $RESULT -ne "200"  ]; then
+    RESULT=$(curl -v -s -o /tmp/out -w "%{http_code}" $PRODUCTPAGE_URL 2> /tmp/err)
+    if [ "$RESULT" -ne "200"  ]; then
         echo "Got $RESULT when curl-ing $PRODUCTPAGE_URL"
+        cat /tmp/err
+        cat /tmp/out
         exit 1
     fi
 done
 
-wait $PID
+wait $CMD_PID
 RTC=$?
 echo "bookinfo-watch exited with $RTC"
 exit $RTC
