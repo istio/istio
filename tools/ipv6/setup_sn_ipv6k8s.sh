39b1a85cea17d06f35a1336ae7177e092dfaf19a
Serguei Bezverkhi <sbezverk@cisco.com>
2019-04-09 22:26:12 -0400

    Factoring the script to bring up single node ipv6 k8s
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>

---
+++ tools/ipv6/setup_sn_ipv6k8s.sh
@@ -0,0 +1,53 @@
+#!/bin/bash -e
+
+sudo curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo bash -c 'echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list'
+sudo apt-get update
+sudo apt-get install -y docker.io kubeadm kubelet kubectl kubernetes-cni
+
+cat <<'EOF' | sudo tee /etc/docker/daemon.json
+{
+  "ipv6": true,
+  "fixed-cidr-v6": "2001:db8:1::/64"
+}
+EOF
+
+cat <<'EOF' | sudo tee /etc/hosts
+2001:470:b16e:84::11    istio-ipv6  istio-ipv6.cluster.local
+EOF
+
+sudo bash -c 'cat << EOT >> /etc/sysctl.conf
+net.ipv6.conf.all.forwarding=1
+net.bridge.bridge-nf-call-ip6tables=1
+net.bridge.bridge-nf-call-iptables=1
+net.ipv6.conf.all.disable_ipv6 = 0
+net.ipv6.conf.default.disable_ipv6 = 0
+net.ipv6.conf.lo.disable_ipv6 = 0
+EOT'
+sudo sysctl -p /etc/sysctl.conf
+
+sudo bash -c 'echo 1 > /proc/sys/net/netfilter/nf_log_all_netns'
+sudo modprobe br_netfilter || true
+sudo bash -c 'echo 1 > /proc/sys/net/bridge/bridge-nf-call-iptables'
+
+sudo systemctl daemon-reload
+sudo systemctl start docker
+sudo systemctl restart kubelet
+
+sudo ip -6 addr add 2001:470:b16e:84::11 dev docker0 || true
+sudo kubeadm init --config config-ipv6.yaml | tee /tmp/kubeout
+
+# Check if client config has been generated
+if [ ! -f "/etc/kubernetes/admin.conf" ]; then
+    echo "kubeconfig file not found, exiting..."
+    exit -1
+fi
+
+sudo cp /etc/kubernetes/admin.conf /tmp/kube.conf
+export KUBECONFIG=/tmp/kube.conf
+#
+# Wait for control plane to compe up completely, timeout 180 seconds
+#
+wait_for_control_plane.sh
+
+kubectl get pods -n kube-system
---
+++ tools/ipv6/wait_for_control_plane.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -e
+# Default wait timeout is 180 seconds
+set +x
+count=0
+
+for x in etcd kube-apiserver kube-controller-manager kube-scheduler; do
+   echo $x
+   pod_name=""
+   while [ "x$pod_name" == "x" ]; do
+      pod_name=$(kubectl get pod -n kube-system | grep -G ^$x | awk '{print $1}')
+      if [ "x$pod_name" != "x" ]; then
+         while true; do
+            pod_status=$(kubectl get pod -n kube-system $pod_name -o jsonpath='{.status.phase}')
+            echo "Pod name: $pod_name status: $pod_status"
+            if [ "x$pod_status" != "xRunning" ]; then
+               if [ $count -gt 180 ]; then
+                  echo "Kubernetes cluster control plane failed to come up"
+                  exit -1
+               fi
+               count=$((count+1))
+               sleep 1
+            else
+               break
+            fi
+         done
+      else
+         if [ $count -gt 180 ]; then
+            echo "Kubernetes cluster control plane failed to come up"
+            exit -1
+         fi
+         count=$((count+1))
+         sleep 1
+      fi
+   done
+done
