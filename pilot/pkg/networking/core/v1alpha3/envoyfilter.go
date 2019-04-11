ba409ef4f2892e200b0999fa7c15f2476c38de29
Robert Panzer <Robert.Panzer.PB@googlemail.com>
2019-04-11 18:04:40 +0200

    Fix #11818 fix workloadSelector for Sidecars (#12666)
    * Fix test error in mixer/adapter/bypass
    
    * Fixes #11818. Extend ServiceRegistries to return workload labels independent of Services
    
    * Added test for getting workload labels from consul registry
    
    * Removed expected errors and results for now from MemoryRegistry.GetProxyWorkloadLabels()
    
    * Added LDS test for consumer without Service and workload specific Sidecar
    
    * Removed unnecessary fake for service_accounts
    
    * Fix #12957. Match EnvoyFilter.workloadSelector against Pod labels, instead of Service labels
    
    * Don't dump config in EnvoyFilter LDSTest
    
    * Added missing test data
    
    * Implemented review comments.
    
    * Added test for generation of inbound listeners for proxies without services.
    
    * Add ingress to Sidecar configuration for consumer-only Sidecar.workloadSelector test
    
    * Formatted imports based review comments
    
    * Only log at debug level if ServiceRegistries fail at determining workload labels

--- pilot/pkg/networking/core/v1alpha3/envoyfilter.go
+++ pilot/pkg/networking/core/v1alpha3/envoyfilter.go
@@ -115,15 +115,8 @@ func insertUserFilters(in *plugin.InputParams, listener *xdsapi.Listener,
 // is undefined.
 func getUserFiltersForWorkload(in *plugin.InputParams) *networking.EnvoyFilter {
 	env := in.Env
-	// collect workload labels
-	workloadInstances := in.ProxyInstances
 
-	var workloadLabels model.LabelsCollection
-	for _, w := range workloadInstances {
-		workloadLabels = append(workloadLabels, w.Labels)
-	}
-
-	f := env.EnvoyFilter(workloadLabels)
+	f := env.EnvoyFilter(in.Node.WorkloadLabels)
 	if f != nil {
 		return f.Spec.(*networking.EnvoyFilter)
 	}
