299546dfca89c207345091d746677c02a255a48b
Lei Tang <32078630+lei-tang@users.noreply.github.com>
2019-04-12 14:46:48 -0700

    Refactor integration tests of Citadel
    - Citadel is a security component -> organize Citadel integration tests
    under the security integration tests folder.
    - The common utility functions are refactored into the util folder.

--- pkg/test/util/secret/secret.go
+++ pkg/test/util/secret/secret.go
@@ -1,4 +1,4 @@
-//  Copyright 2018 Istio Authors
+//  Copyright 2019 Istio Authors
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-package citadel
+package secret
 
 import (
 	"crypto/x509"
