a7bf9e3badc3022d4994260c94519a6af3034ba5
Serguei Bezverkhi <sbezverk@cisco.com>
2019-04-12 16:34:09 -0400

    fixing ci failures #2
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>

--- tools/deb/istio-iptables.sh
+++ tools/deb/istio-iptables.sh
@@ -209,9 +209,9 @@ else
         r=${range%$pl}
         if isValidIP "$r"; then
             if isIPv4 "$r"; then
-                ipv4_ranges_exclude+=("$range,")
+                ipv4_ranges_exclude+=("$range")
             else
-                ipv6_ranges_exclude+=("$range,")
+                ipv6_ranges_exclude+=("$range")
             fi
         fi
     done
