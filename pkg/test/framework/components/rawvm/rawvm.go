4d16f1ea8f9fb507650413fa1d361554d0f8eedd
Jianfei Hu <jianfeih@google.com>
2019-04-15 23:33:56 -0700

    initial setup for component and fake test.

---
+++ pkg/test/framework/components/rawvm/rawvm.go
@@ -0,0 +1,42 @@
+// Package rawvm provides component for Istio mesh expansion.
+package rawvm
+
+import (
+	"fmt"
+
+	"istio.io/istio/pkg/test/framework/resource"
+)
+
+// Instance represents a VM instance running the workload via Istio
+// mesh expansion.
+type Instance interface {
+	// Execute executes a command in the VM instance, returns the output and the error.
+	Execute(args []string) (string, error)
+
+	// Close is invoked when VM instance is cleaned up.
+	Close() error
+}
+
+// Type for the VM workload.
+type Type string
+
+const (
+	// Native type VM Type.
+	Native Type = "native"
+	// GCE type VM Type.
+	GCE Type = "gce"
+)
+
+// Config for VM instance.
+type Config struct {
+	Type Type
+}
+
+// New returns a VMInstance.
+func New(ctx resource.Context, cfg Config) (i Instance, err error) {
+	// Currently only implements GCE VM.
+	if cfg.Type != GCE {
+		return nil, fmt.Errorf("only GCE typed VM are supported")
+	}
+	return NewGCE(ctx, cfg)
+}
