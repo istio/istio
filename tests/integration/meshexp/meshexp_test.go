bf495f1b887ed80e5879322efb43cd53bb629c0e
Jianfei Hu <jianfeih@google.com>
2019-04-24 14:06:52 -0700

    add license comment.

--- tests/integration/meshexp/meshexp_test.go
+++ tests/integration/meshexp/meshexp_test.go
@@ -1,4 +1,26 @@
+//  Copyright 2019 Istio Authors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
 // Package meshexp contains test suite for mesh expansion.
+// How to run this test suite locally
+// go test -v ./tests/integration/meshexp   \
+// -istio.test.env  kube -istio.test.hub "gcr.io/istio-release" \
+// -istio.test.tag "master-latest-daily" \
+// --project_number=895429144602  --project_id=jianfeih-test  \
+// --log_output_level=tf:debug,CI:debug  --zone=us-central1-a \
+// --deb_url=https://storage.googleapis.com/istio-release/releases/1.1.3/deb  \
+// --cluster_name=istio-dev --istio.test.nocleanup
 package meshexp
 
 import (
@@ -32,14 +54,6 @@ const (
 	serviceName = ""
 )
 
-// How to run this test suite locally
-// go test -v ./tests/integration/meshexp   \
-// -istio.test.env  kube -istio.test.hub "gcr.io/istio-release" \
-// -istio.test.tag "master-latest-daily" \
-// --project_number=895429144602  --project_id=jianfeih-test  \
-// --log_output_level=tf:debug,CI:debug  --zone=us-central1-a \
-// --deb_url=https://storage.googleapis.com/istio-release/releases/1.1.3/deb  \
-// --cluster_name=istio-dev --istio.test.nocleanup
 func TestMain(m *testing.M) {
 	framework.
 		NewSuite("meshexp_test", m).
