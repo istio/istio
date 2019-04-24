029398cec8881ded07d71aa79a9ff1af10c5afbb
Maria Skidanova <skidam@google.com>
2019-04-24 14:34:47 -0700

    cni

--- release/create_release_archives.sh
+++ release/create_release_archives.sh
@@ -170,6 +170,12 @@ rm -rf "${COMMON_FILES_DIR}/install/kubernetes/helm/istio/test-values/"
 
 ls -l  "${COMMON_FILES_DIR}/install/kubernetes/helm/istio"
 
+TEMP_DIR=$(mktemp -d)
+pushd ${TEMP_DIR}
+  git clone -b master https://github.com/istio/cni.git
+  cp -r cni/deployments/kubernetes/install/helm/istio-cni/ "${COMMON_FILES_DIR}/install/kubernetes/helm"
+popd
+
 # Changing dir such that tar and zip files are
 # created with right hiereachy
 pushd "${COMMON_FILES_DIR}/.."
