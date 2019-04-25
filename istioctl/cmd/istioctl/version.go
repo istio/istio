1b41480130655939a674dc3817ff86195ce4b2e7
irisdingbj <irisdingbj@gmail.com>
2019-04-25 13:17:14 +0800

    fix lint

--- istioctl/cmd/istioctl/version.go
+++ istioctl/cmd/istioctl/version.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
+
 	istioVersion "istio.io/istio/pkg/version"
 )
 
@@ -10,10 +14,16 @@ func newVersionCommand() *cobra.Command {
 	versionCmd := istioVersion.CobraCommandWithOptions(istioVersion.CobraOptions{GetRemoteVersion: getRemoteInfo})
 	versionCmd.Flags().VisitAll(func(flag *pflag.Flag) {
 		if flag.Name == "short" {
-			flag.Value.Set("true")
+			err := flag.Value.Set("true")
+			if err != nil {
+				fmt.Fprint(os.Stdout, fmt.Sprintf("set flag %q as true failed due to error %v", flag.Name, err))
+			}
 		}
 		if flag.Name == "remote" {
-			flag.Value.Set("true")
+			err := flag.Value.Set("true")
+			if err != nil {
+				fmt.Fprint(os.Stdout, fmt.Sprintf("set flag %q as true failed due to error %v", flag.Name, err))
+			}
 		}
 	})
 	return versionCmd
