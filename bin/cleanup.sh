3a544ecd54bd0cde64d68597f1ab60fdfe7a8a92
Ralf Pannemans <ralf.pannemans@sap.com>
2019-04-26 01:00:30 +0200

    cleanup.sh: remove istio-env label from all namespaces (#51)
    * cleanup.sh: remove istio-env label from all namespaces
    
    Co-authored-by: Philipp Stehle <philipp.stehle@sap.com>
    Signed-off-by: Ralf Pannemans <ralf.pannemans@sap.com>
    
    * test.sh: don't fail if no istio-env is set
    
    Co-authored-by: Philipp Stehle <philipp.stehle@sap.com>
    Signed-off-by: Ralf Pannemans <ralf.pannemans@sap.com>

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -4,3 +4,8 @@
 for namespace in istio-system istio-control istio-control-master istio-ingress istio-telemetry; do
     kubectl delete namespace $namespace --wait --ignore-not-found
 done
+
+ACTIVE_NAMESPACES=$(kubectl get namespaces --no-headers -l istio-env -o=custom-columns=NAME:.metadata.name)
+for ns in $ACTIVE_NAMESPACES; do
+    kubectl label namespaces ${ns} istio-env-
+done
