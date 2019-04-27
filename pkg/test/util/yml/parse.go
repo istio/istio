b9237898a98b6d97b6ac38ae65e298eaabbed24e
Oz Evren <ozben@google.com>
2019-04-26 17:20:30 -0700

    Add a new Yaml resource tracker utility to yml package.

---
+++ pkg/test/util/yml/parse.go
@@ -0,0 +1,89 @@
+// Copyright 2019 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package yml
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/ghodss/yaml"
+)
+
+// Metadata metadata for a kubernetes resource.
+type Metadata struct {
+	Name      string `json:"name"`
+	Namespace string `json:"namespace"`
+}
+
+// Descriptor a descriptor for a kubernetes resource.
+type Descriptor struct {
+	Kind       string   `json:"kind"`
+	Group      string   `json:"group"`
+	APIVersion string   `json:"apiVersion"`
+	Metadata   Metadata `json:"metadata"`
+}
+
+// Part is a single-part yaml source, along with its descriptor.
+type Part struct {
+	Contents   string
+	Descriptor Descriptor
+}
+
+// Parse parses the given multi-part yaml text, and returns as Parts.
+func Parse(yamlText string) ([]Part, error) {
+	splitContent := SplitString(yamlText)
+	parts := make([]Part, 0, len(splitContent))
+	for _, part := range splitContent {
+		if len(part) > 0 {
+			descriptor, err := ParseDescriptor(part)
+			if err != nil {
+				return nil, err
+			}
+
+			parts = append(parts, Part{
+				Contents:   part,
+				Descriptor: descriptor,
+			})
+		}
+	}
+	return parts, nil
+}
+
+// ParseDescriptor parses the given single-part yaml and generates the descriptor.
+func ParseDescriptor(yamlText string) (Descriptor, error) {
+	d := Descriptor{}
+	jsonText, err := yaml.YAMLToJSON([]byte(yamlText))
+	if err != nil {
+		return Descriptor{}, fmt.Errorf("failed converting YAML to JSON: %v", err)
+	}
+
+	if err := json.Unmarshal(jsonText, &d); err != nil {
+		return Descriptor{}, fmt.Errorf("failed parsing descriptor: %v", err)
+	}
+
+	parts := strings.Split(d.APIVersion, "/")
+	switch len(parts) {
+	case 1:
+		d.APIVersion = parts[0]
+	case 2:
+		d.Group = parts[0]
+		d.APIVersion = parts[1]
+	default:
+		return Descriptor{}, fmt.Errorf("unexpected apiGroup: %q", d.APIVersion)
+	}
+
+	return d, nil
+}
---
+++ pkg/test/util/yml/tracker_test.go
@@ -0,0 +1,356 @@
+// Copyright 2019 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package yml
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
+	"testing"
+
+	. "github.com/onsi/gomega"
+)
+
+var (
+	gateway = `
+apiVersion: networking.istio.io/v1alpha3
+kind: Gateway
+metadata:
+  name: some-ingress
+spec:
+  servers:
+  - port:
+      number: 80
+      name: http
+      protocol: http
+    hosts:
+    - "*.example.com"
+`
+
+	updatedGateway = `
+apiVersion: networking.istio.io/v1alpha3
+kind: Gateway
+metadata:
+  name: some-ingress
+spec:
+  servers:
+  - port:
+      number: 8080
+      name: https
+      protocol: https
+    hosts:
+    - "*.example.com"
+`
+
+	virtualService = `
+apiVersion: networking.istio.io/v1alpha3
+kind: VirtualService
+metadata:
+  name: route-for-myapp
+spec:
+  hosts:
+  - some.example.com
+  gateways:
+  - some-ingress
+  http:
+  - route:
+    - destination:
+        host: some.example.internal
+`
+)
+
+func TestTracker_Apply_Basic(t *testing.T) {
+	g := NewGomegaWithT(t)
+	d, err := ioutil.TempDir(os.TempDir(), t.Name())
+	g.Expect(err).To(BeNil())
+	t.Logf("Test Dir: %q", d)
+
+	tr := NewTracker(d)
+
+	keys, err := tr.Apply("ctx1", gateway)
+	g.Expect(err).To(BeNil())
+	g.Expect(keys).To(HaveLen(1))
+	expected := TrackerKey{
+		group:     "networking.istio.io",
+		kind:      "Gateway",
+		namespace: "",
+		name:      "some-ingress",
+	}
+	g.Expect(keys[0]).To(Equal(expected))
+
+	file := tr.GetFileFor(keys[0])
+	by, err := ioutil.ReadFile(file)
+	g.Expect(err).To(BeNil())
+	g.Expect(strings.TrimSpace(string(by))).To(Equal(strings.TrimSpace(gateway)))
+
+	keys, err = tr.Apply("ctx2", virtualService)
+	g.Expect(err).To(BeNil())
+	g.Expect(keys).To(HaveLen(1))
+	expected = TrackerKey{
+		group:     "networking.istio.io",
+		kind:      "VirtualService",
+		namespace: "",
+		name:      "route-for-myapp",
+	}
+	g.Expect(keys[0]).To(Equal(expected))
+
+	file = tr.GetFileFor(keys[0])
+	by, err = ioutil.ReadFile(file)
+	g.Expect(err).To(BeNil())
+	g.Expect(strings.TrimSpace(string(by))).To(Equal(strings.TrimSpace(virtualService)))
+
+	items, err := ioutil.ReadDir(d)
+	g.Expect(err).To(BeNil())
+	g.Expect(items).To(HaveLen(2))
+}
+
+func TestTracker_Apply_MultiPart(t *testing.T) {
+	g := NewGomegaWithT(t)
+	d, err := ioutil.TempDir(os.TempDir(), t.Name())
+	g.Expect(err).To(BeNil())
+	t.Logf("Test Dir: %q", d)
+
+	tr := NewTracker(d)
+
+	keys, err := tr.Apply("ctx1", JoinString(gateway, virtualService))
+	g.Expect(err).To(BeNil())
+	g.Expect(keys).To(HaveLen(2))
+	expected := TrackerKey{
+		group:     "networking.istio.io",
+		kind:      "Gateway",
+		namespace: "",
+		name:      "some-ingress",
+	}
+	g.Expect(keys[0]).To(Equal(expected))
+
+	file := tr.GetFileFor(keys[0])
+	by, err := ioutil.ReadFile(file)
+	g.Expect(err).To(BeNil())
+	g.Expect(strings.TrimSpace(string(by))).To(Equal(strings.TrimSpace(gateway)))
+
+	expected = TrackerKey{
+		group:     "networking.istio.io",
+		kind:      "VirtualService",
+		namespace: "",
+		name:      "route-for-myapp",
+	}
+	g.Expect(keys[1]).To(Equal(expected))
+
+	file = tr.GetFileFor(keys[1])
+	by, err = ioutil.ReadFile(file)
+	g.Expect(err).To(BeNil())
+	g.Expect(strings.TrimSpace(string(by))).To(Equal(strings.TrimSpace(virtualService)))
+
+	items, err := ioutil.ReadDir(d)
+	g.Expect(err).To(BeNil())
+	g.Expect(items).To(HaveLen(2))
+}
+
+func TestTracker_Apply_Add_Delete(t *testing.T) {
+	g := NewGomegaWithT(t)
+	d, err := ioutil.TempDir(os.TempDir(), t.Name())
+	g.Expect(err).To(BeNil())
+	t.Logf("Test Dir: %q", d)
+
+	tr := NewTracker(d)
+
+	keys, err := tr.Apply("ctx1", gateway)
+	g.Expect(err).To(BeNil())
+
+	file := tr.GetFileFor(keys[0])
+	by, err := ioutil.ReadFile(file)
+	g.Expect(err).To(BeNil())
+	g.Expect(strings.TrimSpace(string(by))).To(Equal(strings.TrimSpace(gateway)))
+
+	keys, err = tr.Apply("ctx1", virtualService)
+	g.Expect(err).To(BeNil())
+
+	items, err := ioutil.ReadDir(d)
+	g.Expect(err).To(BeNil())
+	g.Expect(items).To(HaveLen(1))
+}
+
+func TestTracker_Apply_Add_Update(t *testing.T) {
+	g := NewGomegaWithT(t)
+	d, err := ioutil.TempDir(os.TempDir(), t.Name())
+	g.Expect(err).To(BeNil())
+	t.Logf("Test Dir: %q", d)
+
+	tr := NewTracker(d)
+
+	keys, err := tr.Apply("ctx1", gateway)
+	g.Expect(err).To(BeNil())
+
+	file := tr.GetFileFor(keys[0])
+	by, err := ioutil.ReadFile(file)
+	g.Expect(err).To(BeNil())
+	g.Expect(strings.TrimSpace(string(by))).To(Equal(strings.TrimSpace(gateway)))
+
+	keys, err = tr.Apply("ctx1", updatedGateway)
+	g.Expect(err).To(BeNil())
+	file = tr.GetFileFor(keys[0])
+	by, err = ioutil.ReadFile(file)
+	g.Expect(err).To(BeNil())
+	g.Expect(strings.TrimSpace(string(by))).To(Equal(strings.TrimSpace(updatedGateway)))
+
+	items, err := ioutil.ReadDir(d)
+	g.Expect(err).To(BeNil())
+	g.Expect(items).To(HaveLen(1))
+}
+
+func TestTracker_Apply_SameContent_DifferentContexts(t *testing.T) {
+	g := NewGomegaWithT(t)
+	d, err := ioutil.TempDir(os.TempDir(), t.Name())
+	g.Expect(err).To(BeNil())
+	t.Logf("Test Dir: %q", d)
+
+	tr := NewTracker(d)
+
+	_, err = tr.Apply("ctx1", gateway)
+	g.Expect(err).To(BeNil())
+
+	_, err = tr.Apply("ctx2", gateway)
+	g.Expect(err).To(BeNil())
+
+	items, err := ioutil.ReadDir(d)
+	g.Expect(err).To(BeNil())
+	g.Expect(items).To(HaveLen(1))
+}
+
+func TestTracker_RemoveContext(t *testing.T) {
+	g := NewGomegaWithT(t)
+	d, err := ioutil.TempDir(os.TempDir(), t.Name())
+	g.Expect(err).To(BeNil())
+	t.Logf("Test Dir: %q", d)
+
+	tr := NewTracker(d)
+
+	_, err = tr.Apply("ctx1", gateway)
+	g.Expect(err).To(BeNil())
+
+	_, err = tr.Apply("ctx2", virtualService)
+	g.Expect(err).To(BeNil())
+
+	err = tr.RemoveContext("ctx1")
+	g.Expect(err).To(BeNil())
+
+	items, err := ioutil.ReadDir(d)
+	g.Expect(err).To(BeNil())
+	g.Expect(items).To(HaveLen(1))
+
+	by, err := ioutil.ReadFile(path.Join(d, items[0].Name()))
+	g.Expect(err).To(BeNil())
+	g.Expect(strings.TrimSpace(string(by))).To(Equal(strings.TrimSpace(virtualService)))
+}
+
+func TestTracker_RemoveContext_NotFound(t *testing.T) {
+	g := NewGomegaWithT(t)
+	d, err := ioutil.TempDir(os.TempDir(), t.Name())
+	g.Expect(err).To(BeNil())
+	t.Logf("Test Dir: %q", d)
+
+	tr := NewTracker(d)
+	err = tr.RemoveContext("ctx1")
+	g.Expect(err).To(BeNil())
+
+	items, err := ioutil.ReadDir(d)
+	g.Expect(err).To(BeNil())
+	g.Expect(items).To(HaveLen(0))
+}
+
+func TestTracker_Clear(t *testing.T) {
+	g := NewGomegaWithT(t)
+	d, err := ioutil.TempDir(os.TempDir(), t.Name())
+	g.Expect(err).To(BeNil())
+	t.Logf("Test Dir: %q", d)
+
+	tr := NewTracker(d)
+
+	_, err = tr.Apply("ctx1", gateway)
+	g.Expect(err).To(BeNil())
+
+	_, err = tr.Apply("ctx2", virtualService)
+	g.Expect(err).To(BeNil())
+
+	err = tr.Clear()
+	g.Expect(err).To(BeNil())
+
+	items, err := ioutil.ReadDir(d)
+	g.Expect(err).To(BeNil())
+	g.Expect(items).To(HaveLen(0))
+}
+
+func TestTracker_GetFileFor_Empty(t *testing.T) {
+	g := NewGomegaWithT(t)
+	d, err := ioutil.TempDir(os.TempDir(), t.Name())
+	g.Expect(err).To(BeNil())
+	t.Logf("Test Dir: %q", d)
+
+	tr := NewTracker(d)
+
+	f := tr.GetFileFor(TrackerKey{})
+	g.Expect(f).To(BeEmpty())
+}
+
+func TestTracker_Delete(t *testing.T) {
+	g := NewGomegaWithT(t)
+	d, err := ioutil.TempDir(os.TempDir(), t.Name())
+	g.Expect(err).To(BeNil())
+	t.Logf("Test Dir: %q", d)
+
+	tr := NewTracker(d)
+
+	_, err = tr.Apply("ctx1", gateway)
+	g.Expect(err).To(BeNil())
+
+	_, err = tr.Apply("ctx2", virtualService)
+	g.Expect(err).To(BeNil())
+
+	err = tr.Delete(gateway)
+	g.Expect(err).To(BeNil())
+
+	items, err := ioutil.ReadDir(d)
+	g.Expect(err).To(BeNil())
+	g.Expect(items).To(HaveLen(1))
+
+	by, err := ioutil.ReadFile(path.Join(d, items[0].Name()))
+	g.Expect(err).To(BeNil())
+	g.Expect(strings.TrimSpace(string(by))).To(Equal(strings.TrimSpace(virtualService)))
+}
+
+
+func TestTracker_Delete_Missing(t *testing.T) {
+	g := NewGomegaWithT(t)
+	d, err := ioutil.TempDir(os.TempDir(), t.Name())
+	g.Expect(err).To(BeNil())
+	t.Logf("Test Dir: %q", d)
+
+	tr := NewTracker(d)
+
+	_, err = tr.Apply("ctx1", gateway)
+	g.Expect(err).To(BeNil())
+
+	err = tr.Delete(virtualService)
+	g.Expect(err).To(BeNil())
+
+	items, err := ioutil.ReadDir(d)
+	g.Expect(err).To(BeNil())
+	g.Expect(items).To(HaveLen(1))
+
+	by, err := ioutil.ReadFile(path.Join(d, items[0].Name()))
+	g.Expect(err).To(BeNil())
+	g.Expect(strings.TrimSpace(string(by))).To(Equal(strings.TrimSpace(gateway)))
+}
