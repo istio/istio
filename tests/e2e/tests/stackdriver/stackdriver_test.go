39049cb2f7e02a6babd325898dfe37846bcbe50e
Kuat Yessenov <kuat@google.com>
2019-05-01 16:05:38 -0700

    update stackdriver test to share sample
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- tests/e2e/tests/stackdriver/stackdriver_test.go
+++ tests/e2e/tests/stackdriver/stackdriver_test.go
@@ -39,7 +39,7 @@ import (
 
 const (
 	fortioYaml  = "tests/e2e/tests/stackdriver/fortio-rules.yaml"
-	adapterYaml = "tests/e2e/tests/stackdriver/adapter.yaml"
+	adapterYaml = "samples/fortio/stackdriver.yaml"
 )
 
 type (
