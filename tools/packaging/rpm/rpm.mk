36dadd1ca95375fc38feb6cfb12c80f2f3f76c35
Jianfei Hu <jianfeih@google.com>
2019-05-02 23:13:47 -0700

    remove tty

--- tools/packaging/rpm/rpm.mk
+++ tools/packaging/rpm/rpm.mk
@@ -1,7 +1,7 @@
 rpm: rpm/builder-image rpm/istio rpm/proxy
 
 rpm/istio:
-	docker run --rm -it \
+	docker run --rm \
 				-v ${GO_TOP}:${GO_TOP} \
         -w ${PWD} \
         -e USER=${USER} \
@@ -15,7 +15,7 @@ rpm/istio:
 				tools/packaging/rpm/build-istio-rpm.sh
 
 rpm/proxy:
-	docker run --rm -it \
+	docker run --rm \
         -v ${GO_TOP}:${GO_TOP} \
 				-w /builder \
         -e USER=${USER} \
