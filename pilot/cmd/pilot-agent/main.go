c697428e0154d65ff55fa02d84605674220a58cd
John Howard <howardjohn@google.com>
2019-05-06 09:30:56 -0700

    Configure dnsRefreshRate in bootstrap config
    We previously added configuration for dns_refresh_rate, which applies to
    STRICT_DNS clusters for ServiceEntries. However, this didn't get passed
    through to the bootstrap config. This is especially critical as we plan
    to change the default dns_refresh_rate, so we need to also have this
    apply to the bootstrap.

--- pilot/cmd/pilot-agent/main.go
+++ pilot/cmd/pilot-agent/main.go
@@ -76,6 +76,7 @@ var (
 	customConfigFile           string
 	proxyLogLevel              string
 	proxyComponentLogLevel     string
+	dnsRefreshRate             string
 	concurrency                int
 	templateFile               string
 	disableInternalTelemetry   bool
@@ -385,7 +386,7 @@ var (
 
 			log.Infof("PilotSAN %#v", pilotSAN)
 
-			envoyProxy := envoy.NewProxy(proxyConfig, role.ServiceNode(), proxyLogLevel, proxyComponentLogLevel, pilotSAN, role.IPAddresses)
+			envoyProxy := envoy.NewProxy(proxyConfig, role.ServiceNode(), proxyLogLevel, proxyComponentLogLevel, pilotSAN, role.IPAddresses, dnsRefreshRate)
 			agent := proxy.NewAgent(envoyProxy, proxy.DefaultRetry, pilot.TerminationDrainDuration())
 			watcher := envoy.NewWatcher(tlsCertsToWatch, agent.ConfigCh())
 
@@ -558,6 +559,8 @@ func init() {
 	// See https://www.envoyproxy.io/docs/envoy/latest/operations/cli#cmdoption-component-log-level
 	proxyCmd.PersistentFlags().StringVar(&proxyComponentLogLevel, "proxyComponentLogLevel", "misc:error",
 		"The component log level used to start the Envoy proxy")
+	proxyCmd.PersistentFlags().StringVar(&dnsRefreshRate, "dnsRefreshRate", "5m",
+		"The dns_refresh_rate for bootstrap STRICT_DNS clusters")
 	proxyCmd.PersistentFlags().IntVar(&concurrency, "concurrency", int(values.Concurrency),
 		"number of worker threads to run")
 	proxyCmd.PersistentFlags().StringVar(&templateFile, "templateFile", "",
