9be035a9449425bcc3a2f96e7665020bbb04f2d5
Oz Evren <ozben@google.com>
2019-05-08 09:35:23 -0700

    Redo the old script refactoring.

--- prow/istio-integ-local-tests.sh
+++ prow/istio-integ-local-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2018 Istio Authors
+# Copyright 2019 Istio Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,10 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-WD=$(dirname "$0")
-WD=$(cd "$WD"; pwd)
-ROOT=$(dirname "$WD")
-
 # Exit immediately for non zero status
 set -e
 # Check unset variables
@@ -25,14 +21,8 @@ set -u
 # Print commands
 set -x
 
-# shellcheck source=prow/lib.sh
-source "${ROOT}/prow/lib.sh"
-setup_and_export_git_sha
+WD=$(dirname "$0")
+WD=$(cd "$WD"; pwd)
+ROOT=$(dirname "$WD")
 
-cd "${ROOT}"
-
-make sync
-
-JUNIT_UNIT_TEST_XML="${ARTIFACTS_DIR}/junit_unit-tests.xml" \
-T="-v" \
-make test.integration.local
+"${ROOT}/prow/integ-suite-k8s.sh" test.integration.local
