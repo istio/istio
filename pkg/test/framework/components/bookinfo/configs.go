eb4338866cc3f20b868c409885df1eced915da40
Kuat <kyessenov@users.noreply.github.com>
2019-05-13 16:03:57 -0700

    consolidate ratelimit test samples (#13763)
    * consolidate test samples
    
    Signed-off-by: Kuat Yessenov <kuat@google.com>
    
    * update stackdriver test to share sample
    
    Signed-off-by: Kuat Yessenov <kuat@google.com>
    
    * try reusing configs
    
    Signed-off-by: Kuat Yessenov <kuat@google.com>
    
    * fix configs
    
    Signed-off-by: Kuat Yessenov <kuat@google.com>
    
    * remove skip
    
    Signed-off-by: Kuat Yessenov <kuat@google.com>
    
    * fix linter error
    
    Signed-off-by: Kuat Yessenov <kuat@google.com>
    
    * fix a config
    
    Signed-off-by: Kuat Yessenov <kuat@google.com>
    
    * merge fix
    
    Signed-off-by: Kuat Yessenov <kuat@google.com>
    
    * fix build error
    
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- pkg/test/framework/components/bookinfo/configs.go
+++ pkg/test/framework/components/bookinfo/configs.go
@@ -49,6 +49,15 @@ const (
 	// NetworkingReviewsV3Rule uses "networking/virtual-service-reviews-v3"
 	NetworkingReviewsV3Rule ConfigFile = "networking/virtual-service-reviews-v3.yaml"
 
+	// RatingsRedisRateLimitFixed uses Mixer configs
+	RatingsRedisRateLimitFixed ConfigFile = "policy/mixer-rule-ratings-redis-quota-fixed-window.yaml"
+
+	// RatingsRedisRateLimitRolling uses Mixer configs
+	RatingsRedisRateLimitRolling ConfigFile = "policy/mixer-rule-ratings-redis-quota-rolling-window.yaml"
+
+	// ProductPageRedisRateLimit uses Mixer configs
+	ProductPageRedisRateLimit ConfigFile = "policy/mixer-rule-productpage-redis-quota-fixed-window.yaml"
+
 	// PolicyDenyIPRule uses "policy/mixer-rule-deny-ip.yaml"
 	PolicyDenyIPRule ConfigFile = "policy/mixer-rule-deny-ip.yaml"
 )
