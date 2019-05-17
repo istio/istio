34c54a559218ac660ab70082e8ccd01da4a228bf
Ralf Pannemans <ralf.pannemans@sap.com>
2019-05-17 16:10:52 +0200

    Add tests that configStore.List returns instances

--- pilot/pkg/config/memory/config_test.go
+++ pilot/pkg/config/memory/config_test.go
@@ -15,11 +15,15 @@
 package memory_test
 
 import (
+	"istio.io/istio/pilot/pkg/model/test"
+	"strconv"
 	"testing"
 
 	"istio.io/istio/pilot/pkg/config/memory"
 	"istio.io/istio/pilot/pkg/model"
 	"istio.io/istio/pilot/test/mock"
+
+	. "github.com/onsi/gomega"
 )
 
 func TestStoreInvariant(t *testing.T) {
@@ -30,4 +34,72 @@ func TestStoreInvariant(t *testing.T) {
 func TestIstioConfig(t *testing.T) {
 	store := memory.Make(model.IstioConfigTypes)
 	mock.CheckIstioConfigTypes(store, "some-namespace", t)
+}
+
+func TestStoreCreateConfig(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	store := memory.Make(mock.Types)
+	name := "dummy"
+	config := createDummyConfig(name)
+
+	_, err := store.Create(config)
+	g.Expect(err).NotTo(HaveOccurred())
+
+	retrievedConfig := store.Get(model.MockConfig.Type, "dummy", "")
+	g.Expect(retrievedConfig.Spec).To(Equal(config.Spec))
+}
+
+func TestStoreGetReturnsCopies(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	store := memory.Make(mock.Types)
+	name := "dummy"
+	config := createDummyConfig(name)
+	store.Create(config)
+
+	copiedConfig := store.Get(model.MockConfig.Type, "dummy", "")
+
+	config.Spec.(*test.MockConfig).Key = "xxx"
+	g.Expect(copiedConfig.Spec).NotTo(Equal(config.Spec))
+}
+
+func TestStoreListReturnsCopy(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	store := memory.Make(mock.Types)
+	name := "dummy"
+	config := createDummyConfig(name)
+	store.Create(config)
+
+	storedConfigs, err := store.List(model.MockConfig.Type, "")
+
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(storedConfigs).To(HaveLen(1))
+
+	config.Spec.(*test.MockConfig).Key = "xxx"
+	g.Expect(storedConfigs[0].Spec).NotTo(Equal(config.Spec))
+}
+
+func createDummyConfig(name string) model.Config {
+	config := model.Config{
+		ConfigMeta: model.ConfigMeta{
+			Type: model.MockConfig.Type,
+			Name: name,
+			Labels: map[string]string{
+				"key": name,
+			},
+			Annotations: map[string]string{
+				"annotationkey": name,
+			},
+		},
+		Spec: &test.MockConfig{
+			Key: "dummy",
+			Pairs: []*test.ConfigPair{{
+				Key:   "dummy",
+				Value: strconv.Itoa(42),
+			}},
+		},
+	}
+	return config
 }
