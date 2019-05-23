653a065062f088127a34cae336f4124576f75ee0
John Howard <howardjohn@google.com>
2019-05-23 11:08:41 -0700

    Add pod locality in updateEDS
    Without this change, new endpoints added in this method will be missing
    the locality. This generally means only the first replica will get a
    locality assigned to it. This means if locality LB is enabled, only one
    replica will recieve traffic.

--- pilot/pkg/serviceregistry/kube/controller.go
+++ pilot/pkg/serviceregistry/kube/controller.go
@@ -804,6 +804,7 @@ func (c *Controller) updateEDS(ep *v1.Endpoints, event model.Event) {
 						UID:             uid,
 						ServiceAccount:  secureNamingSAN(pod),
 						Network:         c.endpointNetwork(ea.IP),
+						Locality:        c.GetPodLocality(pod),
 					})
 				}
 			}
