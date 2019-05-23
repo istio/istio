425a4d0da96efa99f2ad5ba5a97c7bf82875cc1c
Jimmy Chen <28548492+JimmyCYJ@users.noreply.github.com>
2019-05-22 17:34:27 -0700

    Cherry pick #14043 (#14305)
    * cherry pick #14043
    
    * format
    
    * Remove flag
    
    * fix lint

--- security/pkg/nodeagent/sds/server.go
+++ security/pkg/nodeagent/sds/server.go
@@ -83,6 +83,9 @@ type Options struct {
 	EnableIngressGatewaySDS bool
 	// AlwaysValidTokenFlag is set to true for if token used is always valid(ex, normal k8s JWT)
 	AlwaysValidTokenFlag bool
+
+	// Recycle job running interval (to clean up staled sds client connections).
+	RecycleInterval time.Duration
 }
 
 // Server is the gPRC server that exposes SDS through UDS.
@@ -100,8 +103,8 @@ type Server struct {
 // NewServer creates and starts the Grpc server for SDS.
 func NewServer(options Options, workloadSecretCache, gatewaySecretCache cache.SecretManager) (*Server, error) {
 	s := &Server{
-		workloadSds: newSDSService(workloadSecretCache, false),
-		gatewaySds:  newSDSService(gatewaySecretCache, true),
+		workloadSds: newSDSService(workloadSecretCache, false, options.RecycleInterval),
+		gatewaySds:  newSDSService(gatewaySecretCache, true, options.RecycleInterval),
 	}
 	if options.EnableWorkloadSDS {
 		if err := s.initWorkloadSdsService(&options); err != nil {
@@ -138,9 +141,11 @@ func (s *Server) Stop() {
 	}
 
 	if s.grpcWorkloadServer != nil {
+		s.workloadSds.Stop()
 		s.grpcWorkloadServer.Stop()
 	}
 	if s.grpcGatewayServer != nil {
+		s.gatewaySds.Stop()
 		s.grpcGatewayServer.Stop()
 	}
 }
