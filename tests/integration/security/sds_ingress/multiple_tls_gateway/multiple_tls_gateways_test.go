2abdc5d3ed14b20eacbbcea22bf01d9cdba3f497
Jimmy Chen <jimmychen.0102@gmail.com>
2019-05-23 16:19:14 -0700

    uncomment

--- tests/integration/security/sds_ingress/multiple_tls_gateway/multiple_tls_gateways_test.go
+++ tests/integration/security/sds_ingress/multiple_tls_gateway/multiple_tls_gateways_test.go
@@ -16,9 +16,15 @@ package multiple_tls_gateway
 
 import (
 	"istio.io/istio/pkg/test/framework"
+	"istio.io/istio/pkg/test/framework/components/bookinfo"
 	"istio.io/istio/pkg/test/framework/components/environment"
 	"istio.io/istio/pkg/test/framework/components/environment/kube"
-	//ingressutil "istio.io/istio/tests/integration/security/sds_ingress/util"
+	"istio.io/istio/pkg/test/framework/components/ingress"
+	"istio.io/istio/pkg/test/framework/components/namespace"
+	"istio.io/istio/pkg/test/util/file"
+	"time"
+
+	ingressutil "istio.io/istio/tests/integration/security/sds_ingress/util"
 
 	"testing"
 )
@@ -35,30 +41,30 @@ func testMultiTlsGateways(t *testing.T, ctx framework.TestContext) { // nolint:i
 	if ctx.Environment().(*kube.Environment).Settings().Minikube {
 		t.Skip("https://github.com/istio/istio/issues/14180")
 	}
-	//
-	//bookinfoNs, err := namespace.New(ctx, "istio-bookinfo", true)
-	//if err != nil {
-	//	t.Fatalf("Could not create istio-bookinfo Namespace; err:%v", err)
-	//}
-	//bookinfo.DeployOrFail(t, ctx, bookinfo.Config{Namespace: bookinfoNs, Cfg: bookinfo.BookInfo})
-	//
-	//// Apply the policy to the system namespace.
-	//bGatewayDeployment := file.AsStringOrFail(t, "testdata/bookinfo-multiple-gateways.yaml")
-	//g.ApplyConfigOrFail(t, bookinfoNs, bGatewayDeployment)
-	//defer g.DeleteConfigOrFail(t, bookinfoNs, bGatewayDeployment)
-	//
-	//bVirtualServiceDeployment := file.AsStringOrFail(t, "testdata/bookinfo-multiple-virtualservices.yaml")
-	//g.ApplyConfigOrFail(t, bookinfoNs, bVirtualServiceDeployment)
-	//defer g.DeleteConfigOrFail(t, bookinfoNs, bVirtualServiceDeployment)
-	//
-	//ingressutil.CreateIngressKubeSecret(t, ctx, credNames)
-	//ing := ingress.NewOrFail(t, ctx, ingress.Config{Istio: inst, IngressType: ingress.Tls, CaCert: ingressutil.CaCert})
-	//
-	//// Warm up
-	//err = ingressutil.VisitProductPage(ing, 30*time.Second, 200, t)
-	//if err != nil {
-	//	t.Fatalf("unable to retrieve 200 from product page: %v", err)
-	//}
+
+	bookinfoNs, err := namespace.New(ctx, "istio-bookinfo", true)
+	if err != nil {
+		t.Fatalf("Could not create istio-bookinfo Namespace; err:%v", err)
+	}
+	bookinfo.DeployOrFail(t, ctx, bookinfo.Config{Namespace: bookinfoNs, Cfg: bookinfo.BookInfo})
+
+	// Apply the policy to the system namespace.
+	bGatewayDeployment := file.AsStringOrFail(t, "testdata/bookinfo-multiple-gateways.yaml")
+	g.ApplyConfigOrFail(t, bookinfoNs, bGatewayDeployment)
+	defer g.DeleteConfigOrFail(t, bookinfoNs, bGatewayDeployment)
+
+	bVirtualServiceDeployment := file.AsStringOrFail(t, "testdata/bookinfo-multiple-virtualservices.yaml")
+	g.ApplyConfigOrFail(t, bookinfoNs, bVirtualServiceDeployment)
+	defer g.DeleteConfigOrFail(t, bookinfoNs, bVirtualServiceDeployment)
+
+	ingressutil.CreateIngressKubeSecret(t, ctx, credNames)
+	ing := ingress.NewOrFail(t, ctx, ingress.Config{Istio: inst, IngressType: ingress.Tls, CaCert: ingressutil.CaCert})
+
+	// Warm up
+	err = ingressutil.VisitProductPage(ing, 30*time.Second, 200, t)
+	if err != nil {
+		t.Fatalf("unable to retrieve 200 from product page: %v", err)
+	}
 }
 
 func TestTlsGateways(t *testing.T) {
