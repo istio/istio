4316c5fe8e7a994cf1e31f605a0a0fe69d228e00
John Howard <howardjohn@google.com>
2019-05-24 02:51:56 -0700

    Add pod locality in updateEDS (#14343)
    * Add pod locality in updateEDS
    
    Without this change, new endpoints added in this method will be missing
    the locality. This generally means only the first replica will get a
    locality assigned to it. This means if locality LB is enabled, only one
    replica will recieve traffic.
    
    * Correctly apply locality label override

--- pilot/pkg/model/service.go
+++ pilot/pkg/model/service.go
@@ -387,15 +387,21 @@ type ServiceInstance struct {
 //
 // This is used by CDS/EDS to group the endpoints by locality.
 func (si *ServiceInstance) GetLocality() string {
-	if si.Labels != nil && si.Labels[LocalityLabel] != "" {
+	return GetLocalityOrDefault(si.Endpoint.Locality, si.Labels)
+}
+
+// Gets the locality from the labels, or falls back to to a default locality if not found
+// Because Kubernetes labels don't support `/`, we replace "." with "/" as a workaround
+func GetLocalityOrDefault(defaultLocality string, labels map[string]string) string {
+	if labels != nil && labels[LocalityLabel] != "" {
 		// if there are /'s present we don't need to replace
-		if strings.Contains(si.Labels[LocalityLabel], "/") {
-			return si.Labels[LocalityLabel]
+		if strings.Contains(labels[LocalityLabel], "/") {
+			return labels[LocalityLabel]
 		}
 		// replace "." with "/"
-		return strings.Replace(si.Labels[LocalityLabel], k8sSeparator, "/", -1)
+		return strings.Replace(labels[LocalityLabel], k8sSeparator, "/", -1)
 	}
-	return si.Endpoint.Locality
+	return defaultLocality
 }
 
 // IstioEndpoint has the information about a single address+port for a specific
