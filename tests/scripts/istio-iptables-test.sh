90943d1a91112ef1467fe3f57047d930aef69faa
Ralf Pannemans <ralf.pannemans@sap.com>
2019-05-24 07:13:45 +0200

    Show failing tests in a cleaner wway
    Co-authored-by: Philipp Stehle <philipp.stehle@sap.com>

--- tests/scripts/istio-iptables-test.sh
+++ tests/scripts/istio-iptables-test.sh
@@ -19,13 +19,13 @@
 set -e
 
 function assert_equals() {
-    if [ "$1" != "$2" ]; then
-        echo "Expected result "
-        echo $1
-        echo "doesn't match current result"
+    if [ "$2" != "$3" ]; then
+        echo "FAIL: Expected result "
         echo $2
-        diff -u <(echo "$1") <(echo "$2")
-        exit 1
+        echo "doesn't match current result"
+        echo $3
+        diff -u <(echo "$2") <(echo "$3") || true
+        FAILED+=("$1")
     fi
 }
 
@@ -34,6 +34,7 @@ FILE_UNDER_TEST=./tools/packaging/common/istio-iptables.sh
 export PATH="${PWD}/tests/scripts/stubs:${PATH}"
 
 declare -A TESTS
+declare -a FAILED
 TESTS[mode_redirect]="-p 12345 -u 4321 -g 4444 -m REDIRECT -b 5555,6666 -d 7777,8888  -i 1.1.1.0/16 -x 9.9.9.0/16  -k eth1,eth2"
 TESTS[mode_tproxy]="-p 12345 -u 4321 -g 4444 -m TPROXY -b 5555,6666 -d 7777,8888  -i 1.1.1.0/16 -x 9.9.9.0/16  -k eth1,eth2"
 TESTS[empty_parameter]=""
@@ -47,7 +48,16 @@ do
   # shellcheck disable=SC2086
   OUTPUT=$($FILE_UNDER_TEST $TEST_ARGS  2>/dev/null)
   EXPECTED_OUTPUT=$(cat "tests/scripts/testdata/${TEST_NAME}_golden.txt")
-  assert_equals "$OUTPUT" "$EXPECTED_OUTPUT"
+  assert_equals "$TEST_NAME" "$OUTPUT" "$EXPECTED_OUTPUT"
 done
 
-echo "Tests were successful"
+if [[ ${#FAILED[@]} -eq 0 ]] ; then
+    echo -e "\nAll tests were successful"
+else
+    echo -e "\nThe following tests failed:"
+    for TEST_NAME in "${FAILED[@]}"
+    do
+        echo "  - $TEST_NAME"
+    done
+    exit 1
+fi
