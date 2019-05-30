8ed9242c3a09c3d8cfc9b840dddeb05269f5c828
llcao <llcao@cn.ibm.com>
2019-05-30 10:19:45 +0800

    recursive replace release version.

--- release/gcb/helm_values.sh
+++ release/gcb/helm_values.sh
@@ -35,8 +35,12 @@ function fix_values_yaml() {
   sed -i "s|tag: .*-latest-daily|tag: ${CB_VERSION}|g"         ./"istio-${CB_VERSION}"/install/kubernetes/helm/istio*/values.yaml
 
   current_tag=$(grep "appVersion" ./"istio-${CB_VERSION}"/install/kubernetes/helm/istio/Chart.yaml  | cut -d ' ' -f2)
-  sed -i "s/version: ${current_tag}/version: ${CB_VERSION}/g"  ./"istio-${CB_VERSION}"/install/kubernetes/helm/istio*/Chart.yaml
-  sed -i "s/appVersion: ${current_tag}/appVersion: ${CB_VERSION}/g"  ./"istio-${CB_VERSION}"/install/kubernetes/helm/istio*/Chart.yaml
+  if [ "${current_tag}" != "${CB_VERSION}" ]; then
+    find . -type f -exec sed -i "s/tag: ${current_tag}/tag: ${CB_VERSION}/g" {} \;
+    find . -type f -exec sed -i "s/version: ${current_tag}/version: ${CB_VERSION}/g" {} \;
+    find . -type f -exec sed -i "s/appVersion: ${current_tag}/appVersion: ${CB_VERSION}/g" {} \;
+    find . -type f -exec sed -i "s/istio-release\/releases\/${current_tag}/istio-release\/releases\/${CB_VERSION}/g" {} \;
+  fi
 
   # replace prerelease with release location for istio.io repo
   if [ "${CB_PIPELINE_TYPE}" = "monthly" ]; then
