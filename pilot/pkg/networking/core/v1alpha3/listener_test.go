ccc3295d5eec05d9e33ff2358b56ffd55a32699c
Gao Hongtao <hanahmily@gmail.com>
2019-06-05 01:02:34 +0800

    Enabling Envoy gRPC Access Log Service (ALS) (#13638)
    * Enabling Envoy gRPC Access Log Service (ALS)
    
    * Adding als to envoy.http_connection_manager
    * Adding als to envoy.tcp_proxy
    
    * Fixed some nits
    
    * Polish codes
    
    * Adding test case for als
    
    * vendor
    
    * go mod
    
    * Replacing envoy_google_grpc with envoy_grpc
    
    * Update all.proto

--- pilot/pkg/networking/core/v1alpha3/listener_test.go
+++ pilot/pkg/networking/core/v1alpha3/listener_test.go
@@ -29,6 +29,7 @@ import (
 	"github.com/gogo/protobuf/types"
 
 	networking "istio.io/api/networking/v1alpha3"
+
 	"istio.io/istio/pilot/pkg/model"
 	"istio.io/istio/pilot/pkg/networking/core/v1alpha3/fakes"
 	"istio.io/istio/pilot/pkg/networking/plugin"
@@ -819,6 +820,7 @@ func buildListenerEnv(services []*model.Service) model.Environment {
 	}
 
 	mesh := model.DefaultMeshConfig()
+	mesh.EnableEnvoyAccessLogService = true
 	env := model.Environment{
 		PushContext:      model.NewPushContext(),
 		ServiceDiscovery: serviceDiscovery,
