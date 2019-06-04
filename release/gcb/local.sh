58794f24ccc62bde32f5b47d75649506627acf0b
Maria Skidanova <skidam@google.com>
2019-06-04 10:42:51 -0700

    few fixes

--- release/gcb/local.sh
+++ release/gcb/local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 
- # shellcheck disable=SC1091
+# shellcheck disable=SC1091
 source gcb_lib.sh
 ROOT=$( cd $(git rev-parse --show-cdup); pwd)
 artifacts=(~/output/local)
@@ -10,16 +10,16 @@ export DOCKER_HUB='utka/testing_local'
 export GOPATH=$(cd "$ROOT/../../.." && pwd)
 echo "gopath is $GOPATH"
 
- export CB_VERIFY_CONSISTENCY=true
+export CB_VERIFY_CONSISTENCY=true
 
- echo "Delete old builds"
+echo "Delete old builds"
 rm -r "${artifacts}" || echo
 mkdir -p "${artifacts}"
 
- pushd "${ROOT}/../tools"
+pushd "${ROOT}/../tools"
   TOOLS_HEAD_SHA=$(git rev-parse HEAD)
 popd
 
- pushd "$ROOT"
+pushd "$ROOT"
   create_manifest_check_consistency "${artifacts}/manifest.txt"
 popd
