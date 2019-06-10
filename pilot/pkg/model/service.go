bff85801a180a5c6c7f7dc2a1c1efa2dd6aa233c
John Howard <howardjohn@google.com>
2019-06-10 08:33:30 -0700

    Define constants

--- pilot/pkg/model/service.go
+++ pilot/pkg/model/service.go
@@ -202,6 +202,11 @@ const (
 	TrafficDirectionInbound TrafficDirection = "inbound"
 	// TrafficDirectionOutbound indicates outbound traffic
 	TrafficDirectionOutbound TrafficDirection = "outbound"
+
+	// TrafficDirectionOutboundSrvPrefix the prefix for a DNS SRV type subset key
+	TrafficDirectionOutboundSrvPrefix string = string(TrafficDirectionOutbound) + "_"
+	// TrafficDirectionInboundSrvPrefix the prefix for a DNS SRV type subset key
+	TrafficDirectionInboundSrvPrefix string = string(TrafficDirectionInbound) + "_"
 )
 
 // Visibility defines whether a given config or service is exported to local namespace, all namespaces or none
@@ -944,8 +949,8 @@ func ParseSubsetKey(s string) (direction TrafficDirection, subsetName string, ho
 	// This could be the DNS srv form of the cluster that uses outbound_.port_.subset_.hostname
 	// Since we dont want every callsite to implement the logic to differentiate between the two forms
 	// we add an alternate parser here.
-	if strings.HasPrefix(s, string(TrafficDirectionOutbound)+"_") ||
-		strings.HasPrefix(s, string(TrafficDirectionInbound)+"_") {
+	if strings.HasPrefix(s, TrafficDirectionOutboundSrvPrefix) ||
+		strings.HasPrefix(s, TrafficDirectionInboundSrvPrefix) {
 		parts = strings.SplitN(s, ".", 4)
 		dnsSrvMode = true
 	} else {
