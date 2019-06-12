8b654865d5692a6c026f911a551290b622a26533
Quanjie Lin <quanlin@google.com>
2019-06-12 11:13:50 -0700

    add comment

--- security/pkg/nodeagent/sds/sdsservice.go
+++ security/pkg/nodeagent/sds/sdsservice.go
@@ -492,6 +492,7 @@ func receiveThread(con *sdsConnection, reqChannel chan *xdsapi.DiscoveryRequest,
 	for {
 		req, err := con.stream.Recv()
 		if err != nil {
+			// Add read lock to avoid race condition with set con.conID in StreamSecrets.
 			con.mutex.RLock()
 			conID := con.conID
 			con.mutex.RUnlock()
