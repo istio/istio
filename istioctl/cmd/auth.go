9eab338ad7166305c8dd670fb32af1fe0f084e8d
Phillip Quy Le <pitlv2109@gmail.com>
2019-06-15 09:23:58 -0700

    Hide istioctl upgrade command for RBAC (#14849)

--- istioctl/cmd/auth.go
+++ istioctl/cmd/auth.go
@@ -93,8 +93,9 @@ THIS COMMAND IS STILL UNDER ACTIVE DEVELOPMENT AND NOT READY FOR PRODUCTION USE.
 	}
 
 	upgradeCmd = &cobra.Command{
-		Use:   "upgrade",
-		Short: "Upgrade Istio Authorization Policy from version v1 to v2",
+		Hidden: true,
+		Use:    "upgrade",
+		Short:  "Upgrade Istio Authorization Policy from version v1 to v2",
 		Long: `Upgrade converts Istio authorization policy from version v1 to v2. It requires access to Kubernetes
 service definition in order to translate the service name specified in the ServiceRole to the corresponding
 workload labels in the AuthorizationPolicy. The service definition could be provided either from the current
