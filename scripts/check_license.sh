db32a6947e50cdf9dadaffac588965e7a6ccb259
Martin Taillefer <geeknoid@users.noreply.github.com>
2019-06-17 09:35:01 -0700

    Update common files. (#238)

--- scripts/check_license.sh
+++ scripts/check_license.sh
@@ -4,8 +4,8 @@
 #
 # The original version of this file is located in the https://github.com/istio/common-files repo.
 # If you're looking at this file in a different repo and want to make a change, please go to the
-# common-files repo, make the change there and check it in. Then come back to this repo and run the
-# scripts/updatecommonfiles.sh script.
+# common-files repo, make the change there and check it in. Then come back to this repo and run
+# "make updatecommon".
 
 # Copyright 2018 Istio Authors
 #
