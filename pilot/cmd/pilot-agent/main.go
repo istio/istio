011e2ecd237a9cd0484e09e73817ab8b34b81b9a
xuzhonghu <xuzhonghu@huawei.com>
2019-06-18 11:34:44 +0800

    lint

--- pilot/cmd/pilot-agent/main.go
+++ pilot/cmd/pilot-agent/main.go
@@ -138,7 +138,7 @@ var (
 			// Obtain all the IPs from the node
 			if ipAddrs, ok := proxy.GetPrivateIPs(context.Background()); ok {
 				log.Infof("Obtained private IP %v", ipAddrs)
-					role.IPAddresses = append(role.IPAddresses, ipAddrs...)
+				role.IPAddresses = append(role.IPAddresses, ipAddrs...)
 			}
 
 			// No IP addresses provided, append 127.0.0.1 for ipv4 and ::1 for ipv6
