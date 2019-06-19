598b0194698bcf48e040fce6650aee017beb81c4
Maria Skidanova <skidam@google.com>
2019-06-19 13:00:36 -0700

    Local build

--- release/gcb/gcb_lib.sh
+++ release/gcb/gcb_lib.sh
@@ -47,7 +47,6 @@ function create_manifest_check_consistency() {
     echo "ISTIO_REPO_SHA:$ISTIO_REPO_SHA API_REPO_SHA:$API_REPO_SHA PROXY_REPO_SHA:$PROXY_REPO_SHA CNI_REPO_SHA:$CNI_REPO_SHA some shas not found"
     exit 8
   fi
-
   pushd ../api || exit
     API_SHA=$(git rev-parse "${API_REPO_SHA}")
   popd || exit
@@ -70,3 +69,81 @@ EOF
      fi
   fi
 }
+
+function make_istio() {
+  # Functiona expects to be called from istio/istio repo.
+  local OUTPUT_PATH=$1
+  local DOCKER_HUB=$2
+  local REL_DOCKER_HUB=$3
+  local TAG=$4
+  local BRANCH=$5
+  local ISTIO_OUT=$(make DEBUG=0 where-is-out)
+    
+  MAKE_TARGETS=(istio-archive)
+  MAKE_TARGETS+=(sidecar.deb)
+  
+  CB_BRANCH=${BRANCH} VERBOSE=1 DEBUG=0 ISTIO_DOCKER_HUB="${DOCKER_HUB}" HUB="${DOCKER_HUB}" VERSION="${TAG}" TAG="${TAG}" make "${MAKE_TARGETS[@]}"
+  mkdir -p "${OUTPUT_PATH}/deb"
+  sha256sum "${ISTIO_OUT}/istio-sidecar.deb" > "${OUTPUT_PATH}/deb/istio-sidecar.deb.sha256"
+  cp        "${ISTIO_OUT}/istio-sidecar.deb"   "${OUTPUT_PATH}/deb/"
+  cp        "${ISTIO_OUT}"/archive/istio-*z*   "${OUTPUT_PATH}/"
+  
+  rm -r "${ISTIO_OUT}/docker" || true
+  BUILD_DOCKER_TARGETS=(docker.save)
+
+  CB_BRANCH=${BRANCH} VERBOSE=1 DEBUG=0 ISTIO_DOCKER_HUB=${REL_DOCKER_HUB} HUB=${REL_DOCKER_HUB} VERSION="${TAG}" TAG="${TAG}" make "${BUILD_DOCKER_TARGETS[@]}"
+
+  # preserve the source from the root of the code
+  pushd "${ROOT}/../../../.."
+    pwd
+    # tar the source code
+    if [ -z "${LOCAL_BUILD+x}" ]; then
+      tar -czf "${OUTPUT_PATH}/source.tar.gz" go src --exclude go/out --exclude go/bin 
+    else
+      tar -cvzf "${OUTPUT_PATH}/source.tar.gz" go/src/istio.io/istio go/src/istio.io/api go/src/istio.io/cni go/src/istio.io/proxy
+    fi
+  popd
+
+  cp -r "${ISTIO_OUT}/docker" "${OUTPUT_PATH}/"
+  go run tools/license/get_dep_licenses.go --branch "${BRANCH}" > LICENSES.txt
+
+  # Add extra artifacts for legal compliance. The caller of this script can
+  # optionally set their EXTRA_ARTIFACTS environment variable to an arbitrarily
+  # long list of space-delimited filepaths --- and each artifact would get
+  # injected into the Docker image.
+  add_extra_artifacts_to_tar_images \
+    "${DOCKER_HUB}" \
+    "${TAG}" \
+    "${OUTPUT_PATH}" \
+    "${DOCKER_HUB}" \
+    "${EXTRA_ARTIFACTS:-$PWD/LICENSES.txt}"
+
+  # log where git thinks the build might be dirty
+  git status
+}
+
+function make_cni() {
+  #Handle CNI artifacts. Expects to be called from istio/cni repo.
+  local OUTPUT_PATH=$1
+  local DOCKER_HUB=$2
+  local REL_DOCKER_HUB=$3
+  local TAG=$4
+  local BRANCH=$5
+  CNI_OUT=$(make DEBUG=0 where-is-out)
+  rm -r "${CNI_OUT}/docker" || true
+  # CNI version strategy is to have CNI run lock step with Istio i.e. CB_VERSION
+  CB_BRANCH=${BRANCH} VERBOSE=1 DEBUG=0 ISTIO_DOCKER_HUB="${DOCKER_HUB}" HUB="${DOCKER_HUB}" VERSION="${TAG}" TAG="${TAG}" make build
+  
+  CB_BRANCH=${BRANCH} VERBOSE=1 DEBUG=0 ISTIO_DOCKER_HUB=${REL_DOCKER_HUB} HUB=${REL_DOCKER_HUB} VERSION="${TAG}" TAG="${TAG}" make docker.save || exit 1
+
+  cp -r "${CNI_OUT}/docker" "${OUTPUT_PATH}/"
+  go run ../istio/tools/license/get_dep_licenses.go --branch "${BRANCH}" > LICENSES.txt
+  add_extra_artifacts_to_tar_images \
+      "${DOCKER_HUB}" \
+      "${TAG}" \
+      "${OUTPUT_PATH}" \
+      "${DOCKER_HUB}" \
+      "${EXTRA_ARTIFACTS_CNI:-$PWD/LICENSES.txt}"
+  git status
+}
+
