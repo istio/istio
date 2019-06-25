3a0bdd13f63e9fe55082ed0fb215e48282b12eab
Douglas Reid <douglas-reid@users.noreply.github.com>
2019-06-25 10:57:02 -0700

    fix(docs): add copyright to file

--- pilot/pkg/monitoring/monitoring.go
+++ pilot/pkg/monitoring/monitoring.go
@@ -1,3 +1,17 @@
+// Copyright 2019 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package monitoring
 
 import (
@@ -108,7 +122,7 @@ func (f *float64Metric) Increment() {
 }
 
 func (f *float64Metric) Name() string {
-	return f.Name()
+	return f.Float64Measure.Name()
 }
 
 func (f *float64Metric) Record(value float64) {
