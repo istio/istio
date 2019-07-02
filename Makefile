istio/istio

commit 1620478d1a6aa57f831861fbf0e3558318764b34

Build distroless proxy_init image with go version of istio-iptables (#14985)
* Build distroless proxy_init image with go version of istio-iptables

Co-authored-by: Philipp Stehle <philipp.stehle@sap.com>

* Renamed istio-iptables.sh to istio-iptables for distroless

Makefile
@@ -443,8 +443,9 @@ citadel: istio_ca
 pilot: pilot-discovery
 
 .PHONY: istio-iptables
-istio-iptables:
-	bin/gobuild.sh ${ISTIO_OUT}/$@ ./tools/istio-iptables
+istio-iptables: ${ISTIO_OUT}/istio-iptables
+${ISTIO_OUT}/istio-iptables:
+	bin/gobuild.sh $@ ./tools/istio-iptables
 
 # istioctl-all makes all of the non-static istioctl executables for each supported OS
 .PHONY: istioctl-all

pilot/docker/Dockerfile.proxy_init
@@ -1,4 +1,8 @@
-FROM ubuntu:xenial
+# BASE_DISTRIBUTION is used to switch between the old base distribution and distroless base images
+ARG BASE_DISTRIBUTION=default
+
+# The following section is used as base image if BASE_DISTRIBUTION=default
+FROM ubuntu:xenial as default
 RUN apt-get update && apt-get upgrade -y && apt-get install -y \
     iproute2 \
     iptables \
@@ -6,3 +10,17 @@ RUN apt-get update && apt-get upgrade -y && apt-get install -y \
 
 ADD istio-iptables.sh /usr/local/bin/
 ENTRYPOINT ["/usr/local/bin/istio-iptables.sh"]
+
+# The following section is used as base image if BASE_DISTRIBUTION=distroless
+FROM gcr.io/distroless/cc as distroless
+
+COPY --from=default /sbin/xtables-multi /sbin/iptables* /sbin/ip6tables* /sbin/ip /sbin/
+COPY --from=default /lib/xtables/ /lib/xtables
+COPY --from=default /lib/x86_64-linux-gnu/ /lib/x86_64-linux-gnu
+COPY --from=default /etc/iproute2 /etc/iproute2
+
+ADD istio-iptables /usr/local/bin/istio-iptables
+ENTRYPOINT ["/usr/local/bin/istio-iptables"]
+
+# This will build the final image based on either default or distroless from above
+FROM ${BASE_DISTRIBUTION}

tools/istio-docker.mk
@@ -48,7 +48,7 @@ $(ISTIO_DOCKER)/node_agent.crt $(ISTIO_DOCKER)/node_agent.key: ${GEN_CERT} $(IST
 # 	cp $(ISTIO_OUT_LINUX)/$FILE $(ISTIO_DOCKER)/($FILE)
 DOCKER_FILES_FROM_ISTIO_OUT:=pkg-test-echo-cmd-client pkg-test-echo-cmd-server \
                              pilot-discovery pilot-agent sidecar-injector mixs mixgen \
-                             istio_ca node_agent node_agent_k8s galley
+                             istio_ca node_agent node_agent_k8s galley istio-iptables
 $(foreach FILE,$(DOCKER_FILES_FROM_ISTIO_OUT), \
         $(eval $(ISTIO_DOCKER)/$(FILE): $(ISTIO_OUT_LINUX)/$(FILE) | $(ISTIO_DOCKER); cp $(ISTIO_OUT_LINUX)/$(FILE) $(ISTIO_DOCKER)/$(FILE)))
 
@@ -81,6 +81,7 @@ $(foreach FILE,$(DOCKER_FILES_FROM_ISTIO_BIN), \
 
 docker.proxy_init: pilot/docker/Dockerfile.proxy_init
 docker.proxy_init: $(ISTIO_DOCKER)/istio-iptables.sh
+docker.proxy_init: $(ISTIO_DOCKER)/istio-iptables
 	# Ensure ubuntu:xenial, the base image for proxy_init, is present so build doesn't fail on network hiccup
 	if [[ "$(docker images -q ubuntu:xenial 2> /dev/null)" == "" ]]; then \
 		docker pull ubuntu:xenial || (sleep 15 ; docker pull ubuntu:xenial) || (sleep 45 ; docker pull ubuntu:xenial) \

commit a6b9c11269632a9897d5810f43799ad7b91d92d1

Fixing macos docker build
This was broken by #14985

Makefile
@@ -288,9 +288,12 @@ ${ISTIO_ENVOY_MACOS_RELEASE_PATH}: init
 depend: init | $(ISTIO_OUT)
 
 OUTPUT_DIRS = $(ISTIO_OUT) $(ISTIO_BIN)
+DIRS_TO_CLEAN+=${ISTIO_OUT}
 ifneq ($(ISTIO_OUT),$(ISTIO_OUT_LINUX))
   OUTPUT_DIRS += $(ISTIO_OUT_LINUX)
+  DIRS_TO_CLEAN += $(ISTIO_OUT_LINUX)
 endif
+
 $(OUTPUT_DIRS):
 	@mkdir -p $@
 
@@ -370,15 +373,11 @@ endif
 
 endef
 
-# Build targets for Pilot
-PILOT_GO_BINS:=pilot-discovery pilot-agent sidecar-injector
-$(foreach ITEM,$(PILOT_GO_BINS),$(eval $(call genTargetsForNativeAndDocker,$(ITEM),./pilot/cmd/$(ITEM),$(RELEASE_LDFLAGS))))
-
-# Builds targets for istioctl
+# Build targets for istioctl
 ISTIOCTL_BINS:=istioctl
 $(foreach ITEM,$(ISTIOCTL_BINS),$(eval $(call genTargetsForNativeAndDocker,$(ITEM),./istioctl/cmd/$(ITEM),$(RELEASE_LDFLAGS))))
 
-# Non-static istioctls. These are typically a build artifact.
+# Non-static istioctl targets. These are typically a build artifact.
 ${ISTIO_OUT}/istioctl-linux: depend
 	STATIC=0 GOOS=linux LDFLAGS=$(RELEASE_LDFLAGS) bin/gobuild.sh $@ ./istioctl/cmd/istioctl
 ${ISTIO_OUT}/istioctl-osx: depend
@@ -395,27 +394,35 @@ ${ISTIO_OUT}/_istioctl: istioctl
 	${ISTIO_OUT}/istioctl collateral --zsh && \
 	mv _istioctl ${ISTIO_OUT}/_istioctl
 
-# Build targets for Mixer
-MIXER_GO_BINS:=mixs mixc
-$(foreach ITEM,$(MIXER_GO_BINS),$(eval $(call genTargetsForNativeAndDocker,$(ITEM),./mixer/cmd/$(ITEM),$(RELEASE_LDFLAGS))))
+# Build targets for apps under ./pilot/cmd
+PILOT_BINS:=pilot-discovery pilot-agent sidecar-injector
+$(foreach ITEM,$(PILOT_BINS),$(eval $(call genTargetsForNativeAndDocker,$(ITEM),./pilot/cmd/$(ITEM),$(RELEASE_LDFLAGS))))
 
-# Build targets for Mixgen
-MIXGEN_GO_BINS:=mixgen
-$(foreach ITEM,$(MIXGEN_GO_BINS),$(eval $(call genTargetsForNativeAndDocker,$(ITEM),./mixer/tools/$(ITEM),$(RELEASE_LDFLAGS))))
+# Build targets for apps under ./mixer/cmd
+MIXER_BINS:=mixs mixc
+$(foreach ITEM,$(MIXER_BINS),$(eval $(call genTargetsForNativeAndDocker,$(ITEM),./mixer/cmd/$(ITEM),$(RELEASE_LDFLAGS))))
 
-# Build targets for Galley
-GALLEY_GO_BINS:=galley
-$(foreach ITEM,$(GALLEY_GO_BINS),$(eval $(call genTargetsForNativeAndDocker,$(ITEM),./galley/cmd/$(ITEM),$(RELEASE_LDFLAGS))))
+# Build targets for apps under ./mixer/tools
+MIXER_TOOLS_BINS:=mixgen
+$(foreach ITEM,$(MIXER_TOOLS_BINS),$(eval $(call genTargetsForNativeAndDocker,$(ITEM),./mixer/tools/$(ITEM),$(RELEASE_LDFLAGS))))
 
-# Build targets for security
-SECURITY_GO_BINS:=node_agent node_agent_k8s istio_ca
-$(foreach ITEM,$(SECURITY_GO_BINS),$(eval $(call genTargetsForNativeAndDocker,$(ITEM),./security/cmd/$(ITEM),$(RELEASE_LDFLAGS))))
+# Build targets for apps under ./galley/cmd
+GALLEY_BINS:=galley
+$(foreach ITEM,$(GALLEY_BINS),$(eval $(call genTargetsForNativeAndDocker,$(ITEM),./galley/cmd/$(ITEM),$(RELEASE_LDFLAGS))))
 
-# Build targets for security tools
-SECURITY_TOOLS_GO_BINS:=sdsclient
-$(foreach ITEM,$(SECURITY_TOOLS_GO_BINS),$(eval $(call genTargetsForNativeAndDocker,$(ITEM),./security/tools/$(ITEM),$(RELEASE_LDFLAGS))))
+# Build targets for apps under ./security/cmd
+SECURITY_BINS:=node_agent node_agent_k8s istio_ca
+$(foreach ITEM,$(SECURITY_BINS),$(eval $(call genTargetsForNativeAndDocker,$(ITEM),./security/cmd/$(ITEM),$(RELEASE_LDFLAGS))))
 
-BUILD_BINS:=$(PILOT_GO_BINS) mixc mixs mixgen node_agent node_agent_k8s istio_ca istioctl galley sdsclient
+# Build targets for apps under ./security/tools
+SECURITY_TOOLS_BINS:=sdsclient
+$(foreach ITEM,$(SECURITY_TOOLS_BINS),$(eval $(call genTargetsForNativeAndDocker,$(ITEM),./security/tools/$(ITEM),$(RELEASE_LDFLAGS))))
+
+# Build targets for apps under ./tools
+ISTIO_TOOLS_BINS:=hyperistio istio-iptables
+$(foreach ITEM,$(ISTIO_TOOLS_BINS),$(eval $(call genTargetsForNativeAndDocker,$(ITEM),./tools/$(ITEM),$(DEBUG_LDFLAGS))))
+
+BUILD_BINS:=$(PILOT_BINS) mixc mixs mixgen node_agent node_agent_k8s istio_ca istioctl galley sdsclient
 LINUX_BUILD_BINS:=$(foreach buildBin,$(BUILD_BINS),$(ISTIO_OUT_LINUX)/$(buildBin))
 
 .PHONY: build
@@ -428,7 +435,7 @@ build-linux: depend $(LINUX_BUILD_BINS)
 .PHONY: version-test
 # Do not run istioctl since is different (connects to kubernetes)
 version-test:
-	go test ./tests/version/... -v --base-dir ${ISTIO_OUT} --binaries="$(PILOT_GO_BINS) mixc mixs mixgen node_agent node_agent_k8s istio_ca galley sdsclient"
+	go test ./tests/version/... -v --base-dir ${ISTIO_OUT} --binaries="$(PILOT_BINS) mixc mixs mixgen node_agent node_agent_k8s istio_ca galley sdsclient"
 
 # The following are convenience aliases for most of the go targets
 # The first block is for aliases that are the same as the actual binary,
@@ -441,11 +448,6 @@ citadel: istio_ca
 
 .PHONY: pilot
 pilot: pilot-discovery
-
-.PHONY: istio-iptables
-istio-iptables: ${ISTIO_OUT}/istio-iptables
-${ISTIO_OUT}/istio-iptables:
-	bin/gobuild.sh $@ ./tools/istio-iptables
 
 # istioctl-all makes all of the non-static istioctl executables for each supported OS
 .PHONY: istioctl-all
@@ -515,9 +517,6 @@ $(foreach ITEM,$(TEST_APP_BINS),$(eval $(call genTargetsForNativeAndDocker,pkg-t
 
 MIXER_TEST_BINS:=policybackend
 $(foreach ITEM,$(MIXER_TEST_BINS),$(eval $(call genTargetsForNativeAndDocker,mixer-test-$(ITEM),./mixer/test/$(ITEM),$(DEBUG_LDFLAGS))))
-
-ISTIO_TOOLS_BINS:=hyperistio
-$(foreach ITEM,$(ISTIO_TOOLS_BINS),$(eval $(call genTargetsForNativeAndDocker,$(ITEM),./tools/$(ITEM),$(DEBUG_LDFLAGS))))
 
 TEST_BINS:=$(foreach ITEM,$(TEST_APP_BINS),$(ISTIO_OUT)/pkg-test-echo-cmd-$(ITEM)) $(foreach ITEM,$(MIXER_TEST_BINS),$(ISTIO_OUT)/mixer-test-$(ITEM))
 LINUX_TEST_BINS:=$(foreach ITEM,$(TEST_APP_BINS),$(ISTIO_OUT_LINUX)/pkg-test-echo-cmd-$(ITEM)) $(foreach ITEM,$(MIXER_TEST_BINS),$(ISTIO_OUT_LINUX)/mixer-test-$(ITEM))
@@ -646,8 +645,6 @@ common-racetest:
 # Target: clean
 #-----------------------------------------------------------------------------
 .PHONY: clean clean.go
-
-DIRS_TO_CLEAN+=${ISTIO_OUT}
 
 clean: clean.go
 	rm -rf $(DIRS_TO_CLEAN)