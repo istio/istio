9dcd0f8d47a4c853feb6e9f1cbd3fb478477e962
John Howard <howardjohn@google.com>
2019-07-08 14:31:31 -0700

    Standardize and increase E2E timeout (#15295)

--- prow/istio-pilot-multicluster-e2e.sh
+++ prow/istio-pilot-multicluster-e2e.sh
@@ -24,4 +24,4 @@ export CLUSTERREG_DIR
 
 echo 'Running pilot multi-cluster e2e tests'
 date
-./prow/e2e-suite.sh --timeout 50 --cluster_registry_dir="$CLUSTERREG_DIR" --single_test e2e_pilotv2_v1alpha3 "$@"
+./prow/e2e-suite.sh --timeout 90m --cluster_registry_dir="$CLUSTERREG_DIR" --single_test e2e_pilotv2_v1alpha3 "$@"
