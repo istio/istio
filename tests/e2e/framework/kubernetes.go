8efd9869e978881aabb1f6efee6101e1ec7a7b5a
Zhonghu Xu <xuzhonghu@huawei.com>
2019-07-09 17:38:25 +0800

    try to fix TestJobComplete flake (#15398)
    * try to fix TestJobComplete flake
    
    * makes curl in a loop
    
    * fix sh loop

--- tests/e2e/framework/kubernetes.go
+++ tests/e2e/framework/kubernetes.go
@@ -655,9 +655,9 @@ func (k *KubeInfo) GetAppPods(cluster string) map[string][]string {
 // CheckJobSucceeded checks whether the job succeeded.
 func (k *KubeInfo) CheckJobSucceeded(cluster, jobName string) error {
 	retry := util.Retrier{
-		BaseDelay: 5 * time.Second,
-		MaxDelay:  5 * time.Second,
-		Retries:   5,
+		BaseDelay: 1 * time.Second,
+		MaxDelay:  1 * time.Second,
+		Retries:   15,
 	}
 
 	retryFn := func(_ context.Context, i int) error {
