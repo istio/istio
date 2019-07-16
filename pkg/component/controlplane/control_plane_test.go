9c1401db5f9dbdfe7763cda84e56d9491e257f02
Xinnan Wen <iamwen@google.com>
2019-07-16 14:25:28 -0700

    iop command to compare manifests (#88)
    * initial implementation
    
    * add logic
    
    * Address comment

--- pkg/component/controlplane/control_plane_test.go
+++ pkg/component/controlplane/control_plane_test.go
@@ -19,13 +19,11 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strings"
 	"testing"
 
 	"github.com/ghodss/yaml"
 	"github.com/gogo/protobuf/proto"
 	"github.com/golang/protobuf/jsonpb"
-	"github.com/kylelemons/godebug/diff"
 
 	"istio.io/operator/pkg/apis/istio/v1alpha2"
 	"istio.io/operator/pkg/manifest"
@@ -191,7 +189,7 @@ trafficManagement:
 			if err != nil {
 				t.Fatal(err)
 			}
-			diff, err := ManifestDiff(manifestMapToStr(got), want)
+			diff, err := manifest.ManifestsDiff(manifestMapToStr(got), want)
 			if err != nil {
 				t.Fatal(err)
 			}
@@ -228,69 +226,6 @@ func unmarshalWithJSONPB(y string, out proto.Message) error {
 func readFile(path string) (string, error) {
 	b, err := ioutil.ReadFile(filepath.Join(testDataDir, path))
 	return string(b), err
-}
-
-func YAMLDiff(a, b string) string {
-	ao, bo := make(map[string]interface{}), make(map[string]interface{})
-	if err := yaml.Unmarshal([]byte(a), &ao); err != nil {
-		return err.Error()
-	}
-	if err := yaml.Unmarshal([]byte(b), &bo); err != nil {
-		return err.Error()
-	}
-
-	ay, err := yaml.Marshal(ao)
-	if err != nil {
-		return err.Error()
-	}
-	by, err := yaml.Marshal(bo)
-	if err != nil {
-		return err.Error()
-	}
-
-	return diff.Diff(string(ay), string(by))
-}
-
-func ManifestDiff(a, b string) (string, error) {
-	ao, err := manifest.ParseK8sObjectsFromYAMLManifest(a)
-	if err != nil {
-		return "", err
-	}
-	bo, err := manifest.ParseK8sObjectsFromYAMLManifest(b)
-	if err != nil {
-		return "", err
-	}
-	aom, bom := ao.ToMap(), bo.ToMap()
-	var sb strings.Builder
-	for ak, av := range aom {
-		ay, err := av.YAML()
-		if err != nil {
-			return "", err
-		}
-		by, err := bom[ak].YAML()
-		if err != nil {
-			return "", err
-		}
-		diff := YAMLDiff(string(ay), string(by))
-		if diff != "" {
-			sb.WriteString("\n\nObject " + ak + " has diffs:\n\n")
-			sb.WriteString(diff)
-		}
-	}
-	for bk, bv := range bom {
-		if aom[bk] == nil {
-			by, err := bv.YAML()
-			if err != nil {
-				return "", err
-			}
-			diff := YAMLDiff(string(by), "")
-			if diff != "" {
-				sb.WriteString("\n\nObject " + bk + " is missing:\n\n")
-				sb.WriteString(diff)
-			}
-		}
-	}
-	return sb.String(), err
 }
 
 /*func ObjectsInManifest(mstr string) string {
