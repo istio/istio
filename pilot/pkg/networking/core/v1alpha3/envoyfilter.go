3ddd0581a290a6a5b62899ecb1fa818ef3ada2b6
Shriram Rajagopalan <rshriram@tetrate.io>
2019-07-16 23:56:24 -0400

    nit
    Signed-off-by: Shriram Rajagopalan <rshriram@tetrate.io>

--- pilot/pkg/networking/core/v1alpha3/envoyfilter.go
+++ pilot/pkg/networking/core/v1alpha3/envoyfilter.go
@@ -394,7 +394,6 @@ func applyClusterPatches(env *model.Environment, proxy *model.Proxy,
 				clusters = append(clusters, cp.Value.(*xdsapi.Cluster))
 			}
 		}
-		clusters = trimmedClusters
 	}
 
 	if clustersRemoved {
