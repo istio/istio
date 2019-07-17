8d6b6a04c316f2c4902d832be9ff36b9a4be6515
Oz Evren <ozben@google.com>
2019-07-17 02:28:57 -0700

    Add basic metadata.

---
+++ galley/pkg/config/processor/metadata/collections.gen.go
@@ -0,0 +1,233 @@
+// GENERATED FILE -- DO NOT EDIT
+//
+
+package metadata
+
+import (
+	"istio.io/istio/galley/pkg/config/collection"
+)
+
+var (
+
+	// IstioAuthenticationV1Alpha1Meshpolicies is the name of collection istio/authentication/v1alpha1/meshpolicies
+	IstioAuthenticationV1Alpha1Meshpolicies = collection.NewName("istio/authentication/v1alpha1/meshpolicies")
+
+	// IstioAuthenticationV1Alpha1Policies is the name of collection istio/authentication/v1alpha1/policies
+	IstioAuthenticationV1Alpha1Policies = collection.NewName("istio/authentication/v1alpha1/policies")
+
+	// IstioConfigV1Alpha2Adapters is the name of collection istio/config/v1alpha2/adapters
+	IstioConfigV1Alpha2Adapters = collection.NewName("istio/config/v1alpha2/adapters")
+
+	// IstioConfigV1Alpha2Httpapispecbindings is the name of collection istio/config/v1alpha2/httpapispecbindings
+	IstioConfigV1Alpha2Httpapispecbindings = collection.NewName("istio/config/v1alpha2/httpapispecbindings")
+
+	// IstioConfigV1Alpha2Httpapispecs is the name of collection istio/config/v1alpha2/httpapispecs
+	IstioConfigV1Alpha2Httpapispecs = collection.NewName("istio/config/v1alpha2/httpapispecs")
+
+	// IstioConfigV1Alpha2Templates is the name of collection istio/config/v1alpha2/templates
+	IstioConfigV1Alpha2Templates = collection.NewName("istio/config/v1alpha2/templates")
+
+	// IstioMeshV1Alpha1MeshConfig is the name of collection istio/mesh/v1alpha1/MeshConfig
+	IstioMeshV1Alpha1MeshConfig = collection.NewName("istio/mesh/v1alpha1/MeshConfig")
+
+	// IstioMixerV1ConfigClientQuotaspecbindings is the name of collection istio/mixer/v1/config/client/quotaspecbindings
+	IstioMixerV1ConfigClientQuotaspecbindings = collection.NewName("istio/mixer/v1/config/client/quotaspecbindings")
+
+	// IstioMixerV1ConfigClientQuotaspecs is the name of collection istio/mixer/v1/config/client/quotaspecs
+	IstioMixerV1ConfigClientQuotaspecs = collection.NewName("istio/mixer/v1/config/client/quotaspecs")
+
+	// IstioNetworkingV1Alpha3Destinationrules is the name of collection istio/networking/v1alpha3/destinationrules
+	IstioNetworkingV1Alpha3Destinationrules = collection.NewName("istio/networking/v1alpha3/destinationrules")
+
+	// IstioNetworkingV1Alpha3Envoyfilters is the name of collection istio/networking/v1alpha3/envoyfilters
+	IstioNetworkingV1Alpha3Envoyfilters = collection.NewName("istio/networking/v1alpha3/envoyfilters")
+
+	// IstioNetworkingV1Alpha3Gateways is the name of collection istio/networking/v1alpha3/gateways
+	IstioNetworkingV1Alpha3Gateways = collection.NewName("istio/networking/v1alpha3/gateways")
+
+	// IstioNetworkingV1Alpha3Serviceentries is the name of collection istio/networking/v1alpha3/serviceentries
+	IstioNetworkingV1Alpha3Serviceentries = collection.NewName("istio/networking/v1alpha3/serviceentries")
+
+	// IstioNetworkingV1Alpha3Sidecars is the name of collection istio/networking/v1alpha3/sidecars
+	IstioNetworkingV1Alpha3Sidecars = collection.NewName("istio/networking/v1alpha3/sidecars")
+
+	// IstioNetworkingV1Alpha3SyntheticServiceentries is the name of collection istio/networking/v1alpha3/synthetic/serviceentries
+	IstioNetworkingV1Alpha3SyntheticServiceentries = collection.NewName("istio/networking/v1alpha3/synthetic/serviceentries")
+
+	// IstioNetworkingV1Alpha3Virtualservices is the name of collection istio/networking/v1alpha3/virtualservices
+	IstioNetworkingV1Alpha3Virtualservices = collection.NewName("istio/networking/v1alpha3/virtualservices")
+
+	// IstioPolicyV1Beta1Attributemanifests is the name of collection istio/policy/v1beta1/attributemanifests
+	IstioPolicyV1Beta1Attributemanifests = collection.NewName("istio/policy/v1beta1/attributemanifests")
+
+	// IstioPolicyV1Beta1Handlers is the name of collection istio/policy/v1beta1/handlers
+	IstioPolicyV1Beta1Handlers = collection.NewName("istio/policy/v1beta1/handlers")
+
+	// IstioPolicyV1Beta1Instances is the name of collection istio/policy/v1beta1/instances
+	IstioPolicyV1Beta1Instances = collection.NewName("istio/policy/v1beta1/instances")
+
+	// IstioPolicyV1Beta1Rules is the name of collection istio/policy/v1beta1/rules
+	IstioPolicyV1Beta1Rules = collection.NewName("istio/policy/v1beta1/rules")
+
+	// IstioRbacV1Alpha1Authorizationpolicies is the name of collection istio/rbac/v1alpha1/authorizationpolicies
+	IstioRbacV1Alpha1Authorizationpolicies = collection.NewName("istio/rbac/v1alpha1/authorizationpolicies")
+
+	// IstioRbacV1Alpha1Clusterrbacconfigs is the name of collection istio/rbac/v1alpha1/clusterrbacconfigs
+	IstioRbacV1Alpha1Clusterrbacconfigs = collection.NewName("istio/rbac/v1alpha1/clusterrbacconfigs")
+
+	// IstioRbacV1Alpha1Rbacconfigs is the name of collection istio/rbac/v1alpha1/rbacconfigs
+	IstioRbacV1Alpha1Rbacconfigs = collection.NewName("istio/rbac/v1alpha1/rbacconfigs")
+
+	// IstioRbacV1Alpha1Servicerolebindings is the name of collection istio/rbac/v1alpha1/servicerolebindings
+	IstioRbacV1Alpha1Servicerolebindings = collection.NewName("istio/rbac/v1alpha1/servicerolebindings")
+
+	// IstioRbacV1Alpha1Serviceroles is the name of collection istio/rbac/v1alpha1/serviceroles
+	IstioRbacV1Alpha1Serviceroles = collection.NewName("istio/rbac/v1alpha1/serviceroles")
+
+	// K8SAuthenticationIstioIoV1Alpha1Meshpolicies is the name of collection k8s/authentication.istio.io/v1alpha1/meshpolicies
+	K8SAuthenticationIstioIoV1Alpha1Meshpolicies = collection.NewName("k8s/authentication.istio.io/v1alpha1/meshpolicies")
+
+	// K8SAuthenticationIstioIoV1Alpha1Policies is the name of collection k8s/authentication.istio.io/v1alpha1/policies
+	K8SAuthenticationIstioIoV1Alpha1Policies = collection.NewName("k8s/authentication.istio.io/v1alpha1/policies")
+
+	// K8SConfigIstioIoV1Alpha2Adapters is the name of collection k8s/config.istio.io/v1alpha2/adapters
+	K8SConfigIstioIoV1Alpha2Adapters = collection.NewName("k8s/config.istio.io/v1alpha2/adapters")
+
+	// K8SConfigIstioIoV1Alpha2Attributemanifests is the name of collection k8s/config.istio.io/v1alpha2/attributemanifests
+	K8SConfigIstioIoV1Alpha2Attributemanifests = collection.NewName("k8s/config.istio.io/v1alpha2/attributemanifests")
+
+	// K8SConfigIstioIoV1Alpha2Handlers is the name of collection k8s/config.istio.io/v1alpha2/handlers
+	K8SConfigIstioIoV1Alpha2Handlers = collection.NewName("k8s/config.istio.io/v1alpha2/handlers")
+
+	// K8SConfigIstioIoV1Alpha2Httpapispecbindings is the name of collection k8s/config.istio.io/v1alpha2/httpapispecbindings
+	K8SConfigIstioIoV1Alpha2Httpapispecbindings = collection.NewName("k8s/config.istio.io/v1alpha2/httpapispecbindings")
+
+	// K8SConfigIstioIoV1Alpha2Httpapispecs is the name of collection k8s/config.istio.io/v1alpha2/httpapispecs
+	K8SConfigIstioIoV1Alpha2Httpapispecs = collection.NewName("k8s/config.istio.io/v1alpha2/httpapispecs")
+
+	// K8SConfigIstioIoV1Alpha2Instances is the name of collection k8s/config.istio.io/v1alpha2/instances
+	K8SConfigIstioIoV1Alpha2Instances = collection.NewName("k8s/config.istio.io/v1alpha2/instances")
+
+	// K8SConfigIstioIoV1Alpha2Quotaspecbindings is the name of collection k8s/config.istio.io/v1alpha2/quotaspecbindings
+	K8SConfigIstioIoV1Alpha2Quotaspecbindings = collection.NewName("k8s/config.istio.io/v1alpha2/quotaspecbindings")
+
+	// K8SConfigIstioIoV1Alpha2Quotaspecs is the name of collection k8s/config.istio.io/v1alpha2/quotaspecs
+	K8SConfigIstioIoV1Alpha2Quotaspecs = collection.NewName("k8s/config.istio.io/v1alpha2/quotaspecs")
+
+	// K8SConfigIstioIoV1Alpha2Rules is the name of collection k8s/config.istio.io/v1alpha2/rules
+	K8SConfigIstioIoV1Alpha2Rules = collection.NewName("k8s/config.istio.io/v1alpha2/rules")
+
+	// K8SConfigIstioIoV1Alpha2Templates is the name of collection k8s/config.istio.io/v1alpha2/templates
+	K8SConfigIstioIoV1Alpha2Templates = collection.NewName("k8s/config.istio.io/v1alpha2/templates")
+
+	// K8SCoreV1Endpoints is the name of collection k8s/core/v1/endpoints
+	K8SCoreV1Endpoints = collection.NewName("k8s/core/v1/endpoints")
+
+	// K8SCoreV1Namespaces is the name of collection k8s/core/v1/namespaces
+	K8SCoreV1Namespaces = collection.NewName("k8s/core/v1/namespaces")
+
+	// K8SCoreV1Nodes is the name of collection k8s/core/v1/nodes
+	K8SCoreV1Nodes = collection.NewName("k8s/core/v1/nodes")
+
+	// K8SCoreV1Pods is the name of collection k8s/core/v1/pods
+	K8SCoreV1Pods = collection.NewName("k8s/core/v1/pods")
+
+	// K8SCoreV1Services is the name of collection k8s/core/v1/services
+	K8SCoreV1Services = collection.NewName("k8s/core/v1/services")
+
+	// K8SExtensionsV1Beta1Ingresses is the name of collection k8s/extensions/v1beta1/ingresses
+	K8SExtensionsV1Beta1Ingresses = collection.NewName("k8s/extensions/v1beta1/ingresses")
+
+	// K8SNetworkingIstioIoV1Alpha3Destinationrules is the name of collection k8s/networking.istio.io/v1alpha3/destinationrules
+	K8SNetworkingIstioIoV1Alpha3Destinationrules = collection.NewName("k8s/networking.istio.io/v1alpha3/destinationrules")
+
+	// K8SNetworkingIstioIoV1Alpha3Envoyfilters is the name of collection k8s/networking.istio.io/v1alpha3/envoyfilters
+	K8SNetworkingIstioIoV1Alpha3Envoyfilters = collection.NewName("k8s/networking.istio.io/v1alpha3/envoyfilters")
+
+	// K8SNetworkingIstioIoV1Alpha3Gateways is the name of collection k8s/networking.istio.io/v1alpha3/gateways
+	K8SNetworkingIstioIoV1Alpha3Gateways = collection.NewName("k8s/networking.istio.io/v1alpha3/gateways")
+
+	// K8SNetworkingIstioIoV1Alpha3Serviceentries is the name of collection k8s/networking.istio.io/v1alpha3/serviceentries
+	K8SNetworkingIstioIoV1Alpha3Serviceentries = collection.NewName("k8s/networking.istio.io/v1alpha3/serviceentries")
+
+	// K8SNetworkingIstioIoV1Alpha3Sidecars is the name of collection k8s/networking.istio.io/v1alpha3/sidecars
+	K8SNetworkingIstioIoV1Alpha3Sidecars = collection.NewName("k8s/networking.istio.io/v1alpha3/sidecars")
+
+	// K8SNetworkingIstioIoV1Alpha3Virtualservices is the name of collection k8s/networking.istio.io/v1alpha3/virtualservices
+	K8SNetworkingIstioIoV1Alpha3Virtualservices = collection.NewName("k8s/networking.istio.io/v1alpha3/virtualservices")
+
+	// K8SRbacIstioIoV1Alpha1Authorizationpolicies is the name of collection k8s/rbac.istio.io/v1alpha1/authorizationpolicies
+	K8SRbacIstioIoV1Alpha1Authorizationpolicies = collection.NewName("k8s/rbac.istio.io/v1alpha1/authorizationpolicies")
+
+	// K8SRbacIstioIoV1Alpha1Clusterrbacconfigs is the name of collection k8s/rbac.istio.io/v1alpha1/clusterrbacconfigs
+	K8SRbacIstioIoV1Alpha1Clusterrbacconfigs = collection.NewName("k8s/rbac.istio.io/v1alpha1/clusterrbacconfigs")
+
+	// K8SRbacIstioIoV1Alpha1Policy is the name of collection k8s/rbac.istio.io/v1alpha1/policy
+	K8SRbacIstioIoV1Alpha1Policy = collection.NewName("k8s/rbac.istio.io/v1alpha1/policy")
+
+	// K8SRbacIstioIoV1Alpha1Rbacconfigs is the name of collection k8s/rbac.istio.io/v1alpha1/rbacconfigs
+	K8SRbacIstioIoV1Alpha1Rbacconfigs = collection.NewName("k8s/rbac.istio.io/v1alpha1/rbacconfigs")
+
+	// K8SRbacIstioIoV1Alpha1Serviceroles is the name of collection k8s/rbac.istio.io/v1alpha1/serviceroles
+	K8SRbacIstioIoV1Alpha1Serviceroles = collection.NewName("k8s/rbac.istio.io/v1alpha1/serviceroles")
+)
+
+// CollectionNames returns the collection names declared in this package.
+func CollectionNames() []collection.Name {
+	return []collection.Name{
+		IstioAuthenticationV1Alpha1Meshpolicies,
+		IstioAuthenticationV1Alpha1Policies,
+		IstioConfigV1Alpha2Adapters,
+		IstioConfigV1Alpha2Httpapispecbindings,
+		IstioConfigV1Alpha2Httpapispecs,
+		IstioConfigV1Alpha2Templates,
+		IstioMeshV1Alpha1MeshConfig,
+		IstioMixerV1ConfigClientQuotaspecbindings,
+		IstioMixerV1ConfigClientQuotaspecs,
+		IstioNetworkingV1Alpha3Destinationrules,
+		IstioNetworkingV1Alpha3Envoyfilters,
+		IstioNetworkingV1Alpha3Gateways,
+		IstioNetworkingV1Alpha3Serviceentries,
+		IstioNetworkingV1Alpha3Sidecars,
+		IstioNetworkingV1Alpha3SyntheticServiceentries,
+		IstioNetworkingV1Alpha3Virtualservices,
+		IstioPolicyV1Beta1Attributemanifests,
+		IstioPolicyV1Beta1Handlers,
+		IstioPolicyV1Beta1Instances,
+		IstioPolicyV1Beta1Rules,
+		IstioRbacV1Alpha1Authorizationpolicies,
+		IstioRbacV1Alpha1Clusterrbacconfigs,
+		IstioRbacV1Alpha1Rbacconfigs,
+		IstioRbacV1Alpha1Servicerolebindings,
+		IstioRbacV1Alpha1Serviceroles,
+		K8SAuthenticationIstioIoV1Alpha1Meshpolicies,
+		K8SAuthenticationIstioIoV1Alpha1Policies,
+		K8SConfigIstioIoV1Alpha2Adapters,
+		K8SConfigIstioIoV1Alpha2Attributemanifests,
+		K8SConfigIstioIoV1Alpha2Handlers,
+		K8SConfigIstioIoV1Alpha2Httpapispecbindings,
+		K8SConfigIstioIoV1Alpha2Httpapispecs,
+		K8SConfigIstioIoV1Alpha2Instances,
+		K8SConfigIstioIoV1Alpha2Quotaspecbindings,
+		K8SConfigIstioIoV1Alpha2Quotaspecs,
+		K8SConfigIstioIoV1Alpha2Rules,
+		K8SConfigIstioIoV1Alpha2Templates,
+		K8SCoreV1Endpoints,
+		K8SCoreV1Namespaces,
+		K8SCoreV1Nodes,
+		K8SCoreV1Pods,
+		K8SCoreV1Services,
+		K8SExtensionsV1Beta1Ingresses,
+		K8SNetworkingIstioIoV1Alpha3Destinationrules,
+		K8SNetworkingIstioIoV1Alpha3Envoyfilters,
+		K8SNetworkingIstioIoV1Alpha3Gateways,
+		K8SNetworkingIstioIoV1Alpha3Serviceentries,
+		K8SNetworkingIstioIoV1Alpha3Sidecars,
+		K8SNetworkingIstioIoV1Alpha3Virtualservices,
+		K8SRbacIstioIoV1Alpha1Authorizationpolicies,
+		K8SRbacIstioIoV1Alpha1Clusterrbacconfigs,
+		K8SRbacIstioIoV1Alpha1Policy,
+		K8SRbacIstioIoV1Alpha1Rbacconfigs,
+		K8SRbacIstioIoV1Alpha1Serviceroles,
+	}
+}
