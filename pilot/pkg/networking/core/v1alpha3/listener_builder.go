8fc63cfdfc01c6a08dd52044017e8700b1f8629a
Shriram Rajagopalan <rshriram@tetrate.io>
2019-07-27 10:19:08 -0400

    lint
    Signed-off-by: Shriram Rajagopalan <rshriram@tetrate.io>

--- pilot/pkg/networking/core/v1alpha3/listener_builder.go
+++ pilot/pkg/networking/core/v1alpha3/listener_builder.go
@@ -22,10 +22,10 @@ import (
 	xdsutil "github.com/envoyproxy/go-control-plane/pkg/util"
 	google_protobuf "github.com/gogo/protobuf/types"
 
-	"istio.io/istio/pilot/pkg/features"
-	"istio.io/istio/pilot/pkg/networking/core/v1alpha3/envoyfilter"
 	networking "istio.io/api/networking/v1alpha3"
+	"istio.io/istio/pilot/pkg/features"
 	"istio.io/istio/pilot/pkg/model"
+	"istio.io/istio/pilot/pkg/networking/core/v1alpha3/envoyfilter"
 	"istio.io/istio/pilot/pkg/networking/util"
 	"istio.io/istio/pkg/proto"
 	"istio.io/pkg/log"
