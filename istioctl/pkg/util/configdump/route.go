1e5d1c39d8081e4e2363dafe02e1e46f80183276
hzxuzhonghu <xuzhonghu@huawei.com>
2019-07-31 21:39:43 +0800

    fix

--- istioctl/pkg/util/configdump/route.go
+++ istioctl/pkg/util/configdump/route.go
@@ -71,17 +71,17 @@ func (w *Wrapper) GetDynamicRouteDump(stripVersions bool) (*adminapi.RoutesConfi
 func (w *Wrapper) GetRouteConfigDump() (*adminapi.RoutesConfigDump, error) {
 	// The route dump is no longer the 4th one;
 	// now envoy.admin.v2alpha.ScopedRoutesConfigDump may be 4th.
-	var routeDumpAny *proto.Any
+	var routeDumpAny proto.Any
 	for _, conf := range w.Configs {
 		if conf.TypeUrl == "type.googleapis.com/envoy.admin.v2alpha.RoutesConfigDump" {
-			routeDumpAny = conf
+			routeDumpAny = *conf
 		}
 	}
 	if routeDumpAny.TypeUrl == "" {
 		return nil, fmt.Errorf("config dump has no route dump")
 	}
 	routeDump := &adminapi.RoutesConfigDump{}
-	err := proto.UnmarshalAny(routeDumpAny, routeDump)
+	err := proto.UnmarshalAny(&routeDumpAny, routeDump)
 	if err != nil {
 		return nil, err
 	}
