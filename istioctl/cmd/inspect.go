bdb3652f18a4ac69b20cdb022f6d4cd544beea84
Ed Snible <snible@us.ibm.com>
2019-07-31 13:02:09 -0400

    Work around for goimports bug

--- istioctl/cmd/inspect.go
+++ istioctl/cmd/inspect.go
@@ -27,7 +27,7 @@ import (
 	gogo_types "github.com/gogo/protobuf/types"
 	"github.com/spf13/cobra"
 
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	k8s_labels "k8s.io/apimachinery/pkg/labels"
 
