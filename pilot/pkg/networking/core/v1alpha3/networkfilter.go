2f5df776067ac6708291b5e94446d616acd80880
Shriram Rajagopalan <rshriram@tetrate.io>
2019-07-31 21:48:11 -0400

    fixes
    Signed-off-by: Shriram Rajagopalan <rshriram@tetrate.io>

--- pilot/pkg/networking/core/v1alpha3/networkfilter.go
+++ pilot/pkg/networking/core/v1alpha3/networkfilter.go
@@ -186,14 +186,14 @@ func buildNetworkFiltersStack(node *model.Proxy, port *model.Port, tcpFilter *li
 	case protocol.Mongo:
 		filterstack = append(filterstack, buildMongoFilter(statPrefix, util.IsXDSMarshalingToAnyEnabled(node)), tcpFilter)
 	case protocol.Redis:
-		if util.IsProxyVersionGE11(node) && features.EnableRedisFilter.Get() {
+		if features.EnableRedisFilter.Get() {
 			// redis filter has route config, it is a terminating filter, no need append tcp filter.
 			filterstack = append(filterstack, buildRedisFilter(statPrefix, clusterName, util.IsXDSMarshalingToAnyEnabled(node)))
 		} else {
 			filterstack = append(filterstack, tcpFilter)
 		}
 	case protocol.MySQL:
-		if util.IsProxyVersionGE11(node) && features.EnableMysqlFilter.Get() {
+		if features.EnableMysqlFilter.Get() {
 			filterstack = append(filterstack, buildMySQLFilter(statPrefix, util.IsXDSMarshalingToAnyEnabled(node)))
 		}
 		filterstack = append(filterstack, tcpFilter)
