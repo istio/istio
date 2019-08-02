78505e775e3e80c059e5cdc3a2b3a441f3e0c201
Martin Taillefer <geeknoid@users.noreply.github.com>
2019-08-02 08:30:43 -0700

    Grab latest istio.io/pkg, istio.io/api, and istio.io/common-files (#16018)
    This required a simple change in Galley to stop depending on the
    non-existing pkg/annotations package anymore.

--- scripts/run_gobindata.sh
+++ scripts/run_gobindata.sh
@@ -26,6 +26,6 @@ set -e
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOTDIR=$(dirname "${SCRIPTPATH}")
 
-img=gcr.io/istio-testing/api-build-tools:2019-07-31
+img=gcr.io/istio-testing/api-build-tools:2019-07-30
 
 docker run -i --sig-proxy=true --rm --entrypoint go-bindata --user "$(id -u)" -v /etc/passwd:/etc/passwd:ro -v "${ROOTDIR}:${ROOTDIR}" -w "$(pwd)" ${img} "$@"
