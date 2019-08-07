206216c3261c590a6cbbc601812ce75dde164945
Brian Avery <bavery@redhat.com>
2019-08-07 19:31:26 -0400

    Started README and moved examples

---
+++ pkg/test/istio.io/README.md
@@ -0,0 +1,10 @@
+# Istio.io Example Tests
+
+## Overview
+This folder contains a test framework as well as tests for the different examples
+on Istio.io. The framework relies on
+[istio.io/istio/pkg/test/framework](https://github.com/istio/istio/tree/master/pkg/test/framework)
+to set up an environment to run the tests
+
+## Writing Tests
+
--- pkg/test/istio.io/examples/example.go
+++ pkg/test/istio.io/examples/example.go
@@ -22,11 +22,11 @@ import (
 type outputType string
 
 const (
-	//TextOutput describes a test which returns text output
+	// TextOutput describes a test which returns text output
 	TextOutput outputType = "text"
-	//YamlOutput describes a test which returns yaml output
+	// YamlOutput describes a test which returns yaml output
 	YamlOutput outputType = "yaml"
-	//JSONOutput describes a test which returns json output
+	// JSONOutput describes a test which returns json output
 	JSONOutput outputType = "json"
 )
 
@@ -34,8 +34,15 @@ const (
 	istioPath = "istio.io/istio/"
 )
 
-//CreateExample returns an instance of an example test
-func CreateExample(t *testing.T, name string) Example {
+// Example manages the steps in a test, executes them, and records the output
+type Example struct {
+	name  string
+	steps []testStep
+	t     *testing.T
+}
+
+// New returns an instance of an example test
+func New(t *testing.T, name string) Example {
 	return Example{
 		name:  name,
 		steps: make([]testStep, 0),
@@ -43,31 +50,24 @@ func CreateExample(t *testing.T, name string) Example {
 	}
 }
 
-//Example manages the steps in a test, executes them, and records the output
-type Example struct {
-	name  string
-	steps []testStep
-	t     *testing.T
-}
-
-//AddScript adds a directive to run a script
+// AddScript adds a directive to run a script
 func (example *Example) AddScript(script string, output outputType) {
 	//fullPath := getFullPath(istioPath + script)
 	example.steps = append(example.steps, newStepScript("./"+script, output))
 }
 
-//AddFile adds an existing file
+// AddFile adds an existing file
 func (example *Example) AddFile(path string) {
 	fullPath := getFullPath(istioPath + path)
 	example.steps = append(example.steps, newStepFile(fullPath))
 	panic("Not yet implemneted")
 }
 
-//todo: get last script run output
+// todo: get last script run output
 type testFunc func(t *testing.T)
 
-//Exec registers a callback to be invoked synchronously. This is typically used for
-//validation logic to ensure command-lines worked as intended
+// Exec registers a callback to be invoked synchronously. This is typically used for
+// validation logic to ensure command-lines worked as intended
 func (example *Example) Exec(testFunction testFunc) {
 	example.steps = append(example.steps, newStepFunction(testFunction))
 }
@@ -80,8 +80,8 @@ func getFullPath(path string) string {
 
 }
 
-//Run runs the scripts and capture output
-//Note that this overrides os.Stdout/os.Stderr and is not thread-safe
+// Run runs the scripts and capture output
+// Note that this overrides os.Stdout/os.Stderr and is not thread-safe
 func (example *Example) Run() {
 
 	//override stdout and stderr for test. Is there a better way of doing this?
