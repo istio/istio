14d52c591671e51b04e92c7eb83e1efe92d36e7e
crazyxy <yxyan@google.com>
2019-08-12 17:19:50 -0700

    fix

--- pilot/pkg/features/pilot.go
+++ pilot/pkg/features/pilot.go
@@ -174,7 +174,7 @@ var (
 		"PILOT_ENABLE_PROTOCOL_SNIFFING",
 		false,
 		"If enabled, protocol sniffing will be used on ports whose port protocol is not specified or unknown."+
-			"The listener will detect http vs tcp automatically. Protocol sniffing won't work on ports which are "+
+			"The listener will detect http vs tcp automatically. Protocol sniffing will be disabled on ports which are "+
 			"used by MySQL and MongoDB. Protocol sniffing are not supported for ports using HTTP2 and gRPC.",
 	)
 
