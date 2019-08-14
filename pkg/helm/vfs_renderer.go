f3f867d41f8063b756e0c046cbe5228d0f0c6d6f
Martin Ostrowski <mostrowski@google.com>
2019-08-13 21:32:38 -0700

    Revert "Eliminate use of vfsgen (#180)" (#191)
    This reverts commit 81504f6ec6931a0b9595968f9950072eaa6b9179.

--- pkg/helm/vfs_renderer.go
+++ pkg/helm/vfs_renderer.go
@@ -23,7 +23,8 @@ import (
 	"k8s.io/helm/pkg/proto/hapi/chart"
 
 	"istio.io/operator/pkg/util"
-	"istio.io/operator/pkg/vfs"
+	"istio.io/operator/pkg/vfsgen"
+
 	"istio.io/pkg/log"
 )
 
@@ -31,8 +32,8 @@ const (
 	// DefaultProfileFilename is the name of the default profile yaml file.
 	DefaultProfileFilename = "default.yaml"
 
-	chartsRoot   = "charts"
-	profilesRoot = "profiles"
+	chartsRoot   = "/charts"
+	profilesRoot = "/profiles"
 )
 
 var (
@@ -41,7 +42,7 @@ var (
 )
 
 func init() {
-	profilePaths, err := vfs.ReadDir(profilesRoot)
+	profilePaths, err := vfsgen.ReadDir(profilesRoot)
 	if err != nil {
 		panic(err)
 	}
@@ -94,7 +95,7 @@ func (h *VFSRenderer) RenderManifest(values string) (string, error) {
 func LoadValuesVFS(profileName string) (string, error) {
 	path := filepath.Join(profilesRoot, BuiltinProfileToFilename(profileName))
 	log.Infof("Loading values from compiled in VFS at path %s", path)
-	b, err := vfs.ReadFile(path)
+	b, err := vfsgen.ReadFile(path)
 	return string(b), err
 }
 
@@ -108,13 +109,13 @@ func isBuiltinProfileName(name string) bool {
 // loadChart implements the TemplateRenderer interface.
 func (h *VFSRenderer) loadChart() error {
 	prefix := filepath.Join(chartsRoot, h.helmChartDirPath)
-	fnames, err := vfs.GetFilesRecursive(prefix)
+	fnames, err := vfsgen.GetFilesRecursive(prefix)
 	if err != nil {
 		return err
 	}
 	var bfs []*chartutil.BufferedFile
 	for _, fname := range fnames {
-		b, err := vfs.ReadFile(fname)
+		b, err := vfsgen.ReadFile(fname)
 		if err != nil {
 			return err
 		}
