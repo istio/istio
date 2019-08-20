31df23ef150f18dbe9c959a6eccc14e5176b19dc
Jimmy Chen <jimmychen.0102@gmail.com>
2019-08-19 17:03:34 -0700

    support control plane SDS

--- pilot/cmd/pilot-agent/main.go
+++ pilot/cmd/pilot-agent/main.go
@@ -295,7 +295,7 @@ var (
 			}
 
 			controlPlaneAuthEnabled := controlPlaneAuthPolicy == meshconfig.AuthenticationPolicy_MUTUAL_TLS.String()
-			sdsEnabled, sdsTokenPath := detectSds(controlPlaneBootstrap, controlPlaneAuthEnabled, sdsUdsPathVar.Get(), trustworthyJWTPath)
+			sdsEnabled, sdsTokenPath := detectSds(sdsUdsPathVar.Get(), trustworthyJWTPath)
 
 			// since Envoy needs the certs for mTLS, we wait for them to become available before starting it
 			// skip waiting cert if sds is enabled, otherwise it takes long time for pod to start.
@@ -308,7 +308,7 @@ var (
 
 			opts := make(map[string]interface{})
 			if sdsEnabled {
-				opts["sds_uds_path"] = sdsUdsPathVar.Get()
+				opts["sds_uds_path"] = "unix:" + sdsUdsPathVar.Get()
 				opts["sds_token_path"] = sdsTokenPath
 			}
 
@@ -478,27 +478,8 @@ func getDNSDomain(domain string) string {
 
 // check if SDS UDS path and token path exist, if both exist, requests key/cert
 // using SDS instead of secret mount.
-func detectSds(controlPlaneBootstrap, controlPlaneAuthEnabled bool, udspath, trustworthyJWTPath string) (bool, string) {
+func detectSds(udspath, trustworthyJWTPath string) (bool, string) {
 	if !sdsEnabledVar.Get() {
-		return false, ""
-	}
-
-	if !controlPlaneBootstrap {
-		// workload sidecar
-		// treat sds as disabled if uds path isn't set.
-		if _, err := os.Stat(udspath); err != nil {
-			return false, ""
-		}
-		if _, err := os.Stat(trustworthyJWTPath); err == nil {
-			return true, trustworthyJWTPath
-		}
-
-		return false, ""
-	}
-
-	// for controlplane sidecar, if controlplanesecurity isn't enabled
-	// doens't matter what to return since sds won't be used.
-	if !controlPlaneAuthEnabled {
 		return false, ""
 	}
 
