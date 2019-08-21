c1a8fcbc56b032f20ae8c96914df29b1ad455d23
John Howard <howardjohn@google.com>
2019-08-21 09:15:19 -0700

    WIP 12/fix build (#16412)
    * mixer: update test certificates (#16206)
    
    * mixer: update test certificates
    
    Signed-off-by: Kuat Yessenov <kuat@google.com>
    
    * update to 5 years
    
    Signed-off-by: Kuat Yessenov <kuat@google.com>
    
    * Update docker image
    
    * Fix broken kubectl stuff
    
    * Fix jwks test
    
    * Update circleci image
    
    * Fix cloudfoundry
    
    * Update dockerfile
    
    * Attempt vm image
    
    * typo
    
    * Revert "typo"
    
    This reverts commit 2cafc72fbde67532de0f7f4b46d88c22ba4cc766.
    
    * Revert "Attempt vm image"
    
    This reverts commit 9c0c8f0083014540843d4e150938ba1d7ff7be76.
    
    * Install libc at runtime
    
    * Fix variables

--- mixer/test/client/pilotplugin_mtls/testdata/generate.sh
+++ mixer/test/client/pilotplugin_mtls/testdata/generate.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 openssl genrsa -out root.key 2048
-openssl req -x509 -new -nodes -key root.key -sha256 -days 1825 -out root.pem
+openssl req -x509 -new -nodes -key root.key -sha256 -days 1825 -out root.cert
+
 # generate mTLS cert for client as follows:
-go run security/tools/generate_cert/main.go -host="spiffe://cluster.local/ns/default/sa/client" -signer-priv=root.key -signer-cert=root.cert
+go run security/tools/generate_cert/main.go -host="spiffe://cluster.local/ns/default/sa/client" -signer-priv=mixer/test/client/pilotplugin_mtls/testdata/root.key -signer-cert=mixer/test/client/pilotplugin_mtls/testdata/root.cert --mode=signer
--- tests/istio.mk
+++ tests/istio.mk
@@ -204,8 +204,6 @@ test/local/auth/e2e_sds_pilotv2: out_dir generate_e2e_yaml_coredump
 	set -o pipefail; go test -v -timeout ${E2E_TIMEOUT}m ./tests/e2e/tests/controller ${CAPTURE_LOG}
 
 test/local/cloudfoundry/e2e_pilotv2: out_dir
-	sudo apt update
-	sudo apt install -y iptables
 	sudo iptables -t nat -A OUTPUT -d 127.1.1.1/32 -p tcp -j REDIRECT --to-port 15001
 	set -o pipefail; go test -v -timeout 25m ./tests/e2e/tests/pilot/cloudfoundry ${T} \
 		${CAPTURE_LOG}
