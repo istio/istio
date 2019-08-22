9ad7e241a64ccf25c39913ce6fc7281c9ffb9898
Yuchen Dai <silentdai@gmail.com>
2019-08-22 16:42:27 -0700

    agent: rewrite readiness probe (#16336)
    * pilot-agent: support filter chain inbound listener
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * apply and add test
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * lint
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * improve test
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * lint again
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * apply and add test
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * Address comment: 1.3 pilot agent assumes virtual inbound listener using filter chains
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * remove env var
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * not check inbound for sidecar proxy
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * retore check virtual inbound
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * check inbound listener only for router type envoy
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>

--- pilot/cmd/pilot-agent/status/util/listeners.go
+++ pilot/cmd/pilot-agent/status/util/listeners.go
@@ -21,7 +21,7 @@ import (
 
 	admin "github.com/envoyproxy/go-control-plane/envoy/admin/v2alpha"
 	"github.com/gogo/protobuf/jsonpb"
-	multierror "github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/go-multierror"
 
 	"istio.io/istio/pilot/pkg/model"
 )
@@ -67,6 +67,23 @@ func GetInboundListeningPorts(localHostAddr string, adminPort uint16, nodeType m
 	}
 
 	return ports, buf.String(), nil
+}
+
+func HasListenerName(localHostAddr string, adminPort uint16, name string) error {
+	buf, err := doHTTPGet(fmt.Sprintf("http://%s:%d/listeners?format=json", localHostAddr, adminPort))
+	if err != nil {
+		return multierror.Prefix(err, "failed retrieving Envoy listeners:")
+	}
+	listeners := &admin.Listeners{}
+	if err := jsonpb.Unmarshal(buf, listeners); err != nil {
+		return fmt.Errorf("failed parsing Envoy listeners %s: %s", buf, err)
+	}
+	for _, l := range listeners.ListenerStatuses {
+		if l.Name == name {
+			return nil
+		}
+	}
+	return fmt.Errorf("no such listener name %s", name)
 }
 
 func isLocalListener(l string) bool {
