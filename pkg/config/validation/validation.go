61c24682f112ca6f1aed273b458b05bc12b86709
Yan Xue <yxyan@google.com>
2019-08-23 13:29:44 -0700

    fix

--- pkg/config/validation/validation.go
+++ pkg/config/validation/validation.go
@@ -752,6 +752,9 @@ func validateSidecarEgressPortBindAndCaptureMode(port *networking.Port, bind str
 	// Handle Unix domain sockets
 	if port.Number == 0 {
 		// require bind to be a unix domain socket
+		errs = appendErrors(errs,
+			validateProtocol(port.Protocol))
+
 		if !strings.HasPrefix(bind, UnixAddressPrefix) {
 			errs = appendErrors(errs, fmt.Errorf("sidecar: ports with 0 value must have a unix domain socket bind address"))
 		} else {
@@ -763,6 +766,7 @@ func validateSidecarEgressPortBindAndCaptureMode(port *networking.Port, bind str
 		}
 	} else {
 		errs = appendErrors(errs,
+			validateProtocol(port.Protocol),
 			ValidatePort(int(port.Number)))
 
 		if len(bind) != 0 {
@@ -781,6 +785,7 @@ func validateSidecarIngressPortAndBind(port *networking.Port, bind string) (errs
 	}
 
 	errs = appendErrors(errs,
+		validateProtocol(port.Protocol),
 		ValidatePort(int(port.Number)))
 
 	if len(bind) != 0 {
@@ -2453,6 +2458,7 @@ func ValidateServiceEntry(_, _ string, config proto.Message) (errs error) {
 	for _, port := range serviceEntry.Ports {
 		errs = appendErrors(errs,
 			validatePortName(port.Name),
+			validateProtocol(port.Protocol),
 			ValidatePort(int(port.Number)))
 	}
 
@@ -2463,6 +2469,14 @@ func ValidateServiceEntry(_, _ string, config proto.Message) (errs error) {
 func validatePortName(name string) error {
 	if !labels.IsDNS1123Label(name) {
 		return fmt.Errorf("invalid port name: %s", name)
+	}
+	return nil
+}
+
+func validateProtocol(protocolStr string) error {
+	// Empty string is used for protocol sniffing.
+	if protocolStr != "" && protocol.Parse(protocolStr) == protocol.Unsupported {
+		return fmt.Errorf("unsupported protocol: %s", protocolStr)
 	}
 	return nil
 }
