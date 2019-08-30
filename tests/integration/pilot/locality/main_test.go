cc532dbb6745c817f681fb7b5508a0eafbb65699
Johannes M. Scheuermann <joh.scheuer@gmail.com>
2019-08-30 14:47:01 +0200

    Fix InstancePort in locality tests

--- tests/integration/pilot/locality/main_test.go
+++ tests/integration/pilot/locality/main_test.go
@@ -158,6 +158,8 @@ func echoConfig(ns namespace.Instance, name string) echo.Config {
 				Name:        "http",
 				Protocol:    protocol.HTTP,
 				ServicePort: 80,
+				// We use a port > 1024 to not require root
+				InstancePort: 8090,
 			},
 		},
 		Galley: g,
