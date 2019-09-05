20af44ae792574131854126e2567ccf597e8b5c1
Sam Naser <samnaser@google.com>
2019-09-05 10:10:09 -0700

    Add test for warming secrets

--- istioctl/cmd/proxystatus_test.go
+++ istioctl/cmd/proxystatus_test.go
@@ -57,8 +57,9 @@ Routes Match
 			args:             strings.Split("proxy-status details-v1-5b7f94f9bc-wp5tb --sds", " "),
 			wantException:    false,
 			// nolint: lll
-			expectedString: `RESOURCE NAME     TYPE           VALID CERT     NODE AGENT     PROXY      SERIAL NUMBER                               EXPIRES                  NOT BEFORE
-default           Cert Chain     true                          ACTIVE     172326788211665918318952701714288464978     2019-08-28T17:19:57Z     2019-08-27T17:19:57Z`,
+			expectedString: `RESOURCE NAME     TYPE           VALID CERT     NODE AGENT     PROXY       SERIAL NUMBER                               EXPIRES                  NOT BEFORE
+default           Cert Chain     true                          WARMING     102248101821513494474081488414108563796     2019-09-05T21:18:20Z     2019-09-04T21:18:20Z
+default           Cert Chain     true                          ACTIVE      172326788211665918318952701714288464978     2019-08-28T17:19:57Z     2019-08-27T17:19:57Z`,
 		},
 		{ // case 5: supplying nonexistent pod name should result in error with --sds flag
 			args:          strings.Split("proxy-status random-gibberish-podname-61789237418234", " "),
