32d8f6c68a497ae97bcf4a76e1f1fa922acbdd2a
Yan Xue <yxyan@google.com>
2019-09-06 13:15:22 -0700

    fix

--- pilot/pkg/networking/core/v1alpha3/httproute.go
+++ pilot/pkg/networking/core/v1alpha3/httproute.go
@@ -227,6 +227,10 @@ func (configgen *ConfigGeneratorImpl) buildSidecarOutboundHTTPRouteConfig(env *m
 				}
 			}
 		}
+
+		if len(virtualHosts) == 0 {
+			virtualHosts = tmpVirtualHosts
+		}
 	} else {
 		virtualHosts = tmpVirtualHosts
 	}
