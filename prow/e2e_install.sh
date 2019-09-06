8d0f29aa180674e151cb9326b03c8be93fe0aee5
Xinnan Wen <iamwen@google.com>
2019-09-06 14:19:43 -0700

    Add kind setup back (#246)
    * Add kind setup back
    
    * Remove hardcode node image version
    
    * Fix ns and gopath

--- prow/e2e_install.sh
+++ prow/e2e_install.sh
@@ -20,50 +20,59 @@ set -x
 WD=$(dirname "$0")
 WD=$(cd "$WD"; pwd)
 ROOT=$(dirname "$WD")
-export GO111MODULE=on
-
+export GOPATH=$(cd ../../../; pwd)
 cd "${ROOT}"
+
+ISTIO_DIR="${GOPATH}/src/istio.io/istio"
+if [[ ! -d "${ISTIO_DIR}" ]]
+then
+    git clone https://github.com/istio/istio.git "${ISTIO_DIR}"
+fi
+
+ISTIO_NS=istio-system
+MODE=permissive
+SIMPLE_AUTH=false
+E2E_ARGS="--skip_setup=true --use_local_cluster=true --istio_namespace=${ISTIO_NS}"
+TMPDIR=/tmp
+export GO111MODULE=on
+export IstioTop=${ISTIO_DIR}/../../..
+
+#kind and istioctl setup
+pushd ${ISTIO_DIR}
+go install ./istioctl/cmd/istioctl
+export ISTIOCTL_BIN=${GOPATH}/bin/istioctl
+source "./prow/lib.sh"
+setup_kind_cluster ${NODE_IMAGE}
+popd
+
 echo "installing istio with operator CLI"
 go run ./cmd/mesh.go manifest apply
 
-ISTIO_DIR=$(mktemp -d)/src/istio.io/istio
-ISTIO_CONTROL_NS=istio-system
-MODE=permissive
-ISTIOCTL_BIN=${ISTIOCTL_BIN:-/usr/local/bin/istioctl}
-SIMPLE_AUTH=false
-E2E_ARGS="--skip_setup=true --use_local_cluster=true --istio_namespace=${ISTIO_CONTROL_NS}"
-TMPDIR=/tmp
-export IstioTop=${ISTIO_DIR}/../../..
-
-git clone https://github.com/istio/istio.git "${ISTIO_DIR}"
-
 function run-simple-base() {
-    kubectl delete ns ${NS} || true
-  	kubectl create ns ${NS} || true
-  	kubectl -n ${NS} apply -f prow/k8s/mtls_${MODE}.yaml
-  	kubectl -n ${NS} apply -f prow/k8s/sidecar-local.yaml
-  	kubectl label ns ${NS} istio-injection=disabled --overwrite
-  	(cd ${ISTIO_DIR}; make e2e_simple_run ${TEST_FLAGS} \
+    kubectl create ns ${NS} || true
+    kubectl -n ${NS} apply -f prow/k8s/mtls_${MODE}.yaml
+    kubectl -n ${NS} apply -f prow/k8s/sidecar-local.yaml
+    kubectl label ns ${NS} istio-injection=disabled --overwrite
+    (cd ${ISTIO_DIR}; make e2e_simple_run ${TEST_FLAGS} \
     E2E_ARGS="${E2E_ARGS} --auth_enable=${SIMPLE_AUTH} --namespace=${NS}")
 }
 function run-simple() {
-	ISTIO_CONTROL_NS=istio-system MODE=permissive NS=simple run-simple-base 
+    ISTIO_NS=istio-system MODE=permissive NS=simple run-simple-base
 }
 # Simple test, strict mode
 function run-simple-strict() {
-	MODE=strict ISTIO_CONTROL_NS=istio-system NS=simple-strict SIMPLE_AUTH=true run-simple-base 
+    MODE=strict ISTIO_NS=istio-system NS=simple-strict SIMPLE_AUTH=true run-simple-base
 }
 
 function run-bookinfo-demo() {
-	kubectl create ns bookinfo-demo || true
-	kubectl -n bookinfo-demo apply -f prow/k8s/mtls_permissive.yaml
-	kubectl -n bookinfo-demo apply -f prow/k8s/sidecar-local.yaml
-	(cd ${ISTIO_DIR}; make e2e_bookinfo_run ${TEST_FLAGS} \
-		E2E_ARGS="${E2E_ARGS} --namespace=bookinfo-demo")
+    kubectl create ns bookinfo-demo || true
+    kubectl -n bookinfo-demo apply -f prow/k8s/mtls_permissive.yaml
+    kubectl -n bookinfo-demo apply -f prow/k8s/sidecar-local.yaml
+    (cd ${ISTIO_DIR}; make e2e_bookinfo_run ${TEST_FLAGS} \
+      E2E_ARGS="${E2E_ARGS} --namespace=bookinfo-demo")
 }
 
 echo "start e2e testing"
 run-simple
 run-simple-strict
 run-bookinfo-demo
-
