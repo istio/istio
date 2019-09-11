85c5db40ad23e3970389d77341de05325fad7d09
Xinnan Wen <iamwen@google.com>
2019-09-10 18:52:12 -0700

    update the perf test script

---
+++ prow/perf_benchmark_test.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+WD=$(dirname "$0")
+WD=$(cd "$WD"; pwd)
+ROOT=$(dirname "$WD")
+
+# Exit immediately for non zero status
+set -e
+# Check unset variables
+set -u
+# Print commands
+set -x
+
+# Check https://github.com/istio/test-infra/blob/master/boskos/configs.yaml
+# for existing resources types
+export RESOURCE_TYPE="${RESOURCE_TYPE:-gke-perf-preset}"
+export OWNER="${OWNER:-perf-tests}"
+export PILOT_CLUSTER="${PILOT_CLUSTER:-}"
+export USE_MASON_RESOURCE="${USE_MASON_RESOURCE:-True}"
+export CLEAN_CLUSTERS="${CLEAN_CLUSTERS:-True}"
+# This is config for postsubmit cluster under istio/tools/perf/istio.
+export VALUES="${VALUES:-values-istio-postsubmit.yaml}"
+export DNS_DOMAIN="fake-dns.org"
+export CMD=""
+export DELETE=""
+export FORTIO_CLIENT_URL=""
+# shellcheck source=prow/lib.sh
+source "${ROOT}/prow/lib.sh"
+ setup_e2e_cluster
+helm init --client-only
+
+echo "Get istio release: $TAG"
+pushd "${GOPATH}/src/istio.io/tools/perf/istio-install"
+   ./setup_istio.sh "${TAG}"
+popd
+
+function collect_metrics() {
+  local GENERATE_GRAPH=$1
+  FORTIO_CLIENT_URL=http://$(kubectl get services -n twopods fortioclient -o jsonpath="{.status.loadBalancer.ingress[0].ip}"):8080
+  if [ -z "$FORTIO_CLIENT_URL" ];then
+    kubectl -n twopods port-forward svc/fortioclient 8080:8080 &
+    export FORTIO_CLIENT_URL=http://localhost:8080
+  fi
+  export PROMETHEUS_URL=http://localhost:9090
+  kubectl -n istio-prometheus port-forward svc/istio-prometheus 9090:9090 &>/dev/null &
+
+  CSV_OUTPUT="$(mktemp /tmp/benchmark_XXXX.csv)"
+  pipenv install
+  pipenv run python fortio.py $FORTIO_CLIENT_URL --csv_output=$CSV_OUTPUT --prometheus=$PROMETHEUS_URL \
+   --csv StartTime,ActualDuration,Labels,NumThreads,ActualQPS,p50,p90,p99,cpu_mili_avg_telemetry_mixer,cpu_mili_max_telemetry_mixer,\
+mem_MB_max_telemetry_mixer,cpu_mili_avg_fortioserver_deployment_proxy,cpu_mili_max_fortioserver_deployment_proxy,\
+mem_MB_max_fortioserver_deployment_proxy,cpu_mili_avg_ingressgateway_proxy,cpu_mili_max_ingressgateway_proxy,mem_MB_max_ingressgateway_proxy
+
+  if [ "$GENERATE_GRAPH" = true ];then
+    METRICS=(p99 mem cpu)
+    for metric in "${METRICS[@]}"
+    do
+      BENCHMARK_GRAPH="$(mktemp /tmp/benchmark_graph_XXXX.html)"
+      pipenv run python graph.py "${CSV_OUTPUT}" "${metric}" "${BENCHMARK_GRAPH}"
+      gsutil -q cp "${BENCHMARK_GRAPH}" "gs://$CB_GCS_BUILD_PATH/${BENCHMARK_GRAPH}"
+    done
+  fi
+}
+
+cd "${GOPATH}/src/istio.io/tools/perf/benchmark/runner"
+pipenv install
+
+EXTRA_ARGS="--serversidecar --baseline"
+CONN=16,64
+QPS=500,1000
+DURATION=300
+pushd "../"
+./setup_test.sh
+popd
+
+echo "Start running perf benchmark test."
+# run towards all combinations of the parameter
+pipenv run python runner.py ${CONN} ${QPS} ${DURATION} ${EXTRA_ARGS}
+collect_metrics true
+
+echo "perf benchmark test is done."
