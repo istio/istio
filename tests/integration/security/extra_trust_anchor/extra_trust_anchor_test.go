66d1b70e710295348ecc8dba01009caba790a1fd
Jason Young <jasonyoung@google.com>
2019-09-19 09:43:17 -0700

    fix linter

--- tests/integration/security/extra_trust_anchor/extra_trust_anchor_test.go
+++ tests/integration/security/extra_trust_anchor/extra_trust_anchor_test.go
@@ -12,7 +12,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-package extra_trust_anchor
+package extratrustanchor
 
 import (
 	"crypto/x509"
