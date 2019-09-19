c7033bde859eb719df45a6e463f0d0aa5216450a
Kuat Yessenov <kuat@google.com>
2019-09-18 20:02:08 -0700

    allow unknown fields
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- pilot/cmd/pilot-agent/status/util/server_info.go
+++ pilot/cmd/pilot-agent/status/util/server_info.go
@@ -28,7 +28,8 @@ func GetServerInfo(localHostAddr string, adminPort uint16) (*admin.ServerInfo, e
 		return nil, multierror.Prefix(err, "failed retrieving Envoy stats:")
 	}
 	info := &admin.ServerInfo{}
-	if err := jsonpb.Unmarshal(input, info); err != nil {
+	jspb := jsonpb.Unmarshaler{AllowUnknownFields: true}
+	if err := jspb.Unmarshal(input, info); err != nil {
 		return &admin.ServerInfo{}, err
 	}
 
