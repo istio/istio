69d9f0b366839da1b618b2a1ebe550ab3bd5c86e
Daniel Grimm <dgrimm@redhat.com>
2019-09-24 14:09:47 +0200

    TestController_GetIstioServiceAccounts: wait for services to be processed by Controller

--- pilot/pkg/serviceregistry/kube/controller/controller_test.go
+++ pilot/pkg/serviceregistry/kube/controller/controller_test.go
@@ -639,6 +639,7 @@ func TestController_GetIstioServiceAccounts(t *testing.T) {
 			annotation.AlphaKubernetesServiceAccounts.Name: k8sSaOnVM,
 			annotation.AlphaCanonicalServiceAccounts.Name:  canonicalSaOnVM},
 		[]int32{8080}, map[string]string{"app": "prod-app"}, t)
+	<-fx.Events
 	createService(controller, "svc2", "nsA", nil, []int32{8080}, map[string]string{"app": "staging-app"}, t)
 	<-fx.Events
 
