8b213b612475218c37ed18d701006a5e35fcaf4c
Martin Taillefer <mtail@google.com>
2019-09-27 11:45:27 -0700

    Fix a few items that were breaking container-based build.

--- bin/init_helm.sh
+++ bin/init_helm.sh
@@ -22,11 +22,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# TODO(nmittler): Remove these variables and require that this script be run from the Makefile
-
-# Set GOPATH to match the expected layout
-GO_TOP=$(cd "$(dirname "$0")"/../../../..; pwd)
-
+export GO_TOP=${GO_TOP:-$(echo "${GOPATH}" | cut -d ':' -f1)}
 export OUT_DIR=${OUT_DIR:-${GO_TOP}/out}
 
 HELM_VER=${HELM_VER:-v2.10.0}
