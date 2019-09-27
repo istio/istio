6a7f2ae61e1c7afc49a62a52cc82e79aca678758
John Howard <howardjohn@google.com>
2019-09-27 13:52:54 -0700

    wip

--- prow/release-test.sh
+++ prow/release-test.sh
@@ -29,27 +29,45 @@ set -u
 # Print commands
 set -x
 
-# shellcheck source=prow/lib.sh
-source "${ROOT}/prow/lib.sh"
-setup_and_export_git_sha
+set -eux
 
-cd "$ROOT"
+# Reference to the next minor version of Istio
+# This will create a version like 1.4.0-alpha.20191001
+NEXT_VERSION=1.4.0
+DATE=$(date '+%Y%m%d')
+VERSION="${NEXT_VERSION}-alpha.${DATE}"
 
-function create_gcb_env() {
-  mkdir /workspace
-  mkdir /output
-  mkdir "$ROOT/../../../../src"
-  touch /workspace/manifest.txt
+# In CI we want to store the outputs to artifacts, which will preserve the build
+# If not specified, we can just create a temporary directory
+WORK_DIR="${ARTIFACTS:-$(mktemp -d)}/build"
+mkdir -p "${WORK_DIR}"
 
-cat << EOF > "/workspace/gcb_env.sh"
-export CB_BRANCH="${GIT_BRANCH}"
-export CB_VERSION="${GIT_SHA}"
-export CB_ISTIOCTL_DOCKER_HUB="docker.io/istio"
-export CB_PIPELINE_TYPE=daily
+# TODO pin version of CNI
+
+MANIFEST=$(cat <<EOF
+version: ${VERSION}
+docker: docker.io/istio
+directory: ${WORK_DIR}
+dependencies:
+  - org: istio
+    repo: istio
+    localpath: ${ROOT}
+  - org: istio
+    repo: cni
+    branch: master
+outputs:
+- archive
 EOF
-}
+)
 
-create_gcb_env
-clone_cni
+echo "${MANIFEST}"
 
-time ./release/gcb/cloud_builder.sh
+# tmp hacks
+export PATH=${GOPATH}/bin:${PATH}
+export GOSUMDB=sum.golang.org
+
+# cd to not impact go.mod
+(cd /tmp; go get istio.io/release-builder)
+
+release-builder build --manifest <(echo "${MANIFEST}")
+#go run "${ROOT}/main.go" publish --release "${WORK_DIR}/out" --gcsbucket howardjohn/release --dockerhub "gcr.io/howardjohn-istio"
