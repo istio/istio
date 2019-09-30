a20724f3ba659cfca6de5e73f37ef9594695fe75
Rama Chavali <rama.rao@salesforce.com>
2019-09-30 20:11:51 +0530

    warn if proxy version does not exist
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/model/context.go
+++ pilot/pkg/model/context.go
@@ -588,7 +588,11 @@ func ParseServiceNodeWithMetadata(s string, metadata *NodeMetadata) (*Proxy, err
 
 	out.ID = parts[2]
 	out.DNSDomain = parts[3]
-	out.IstioVersion = ParseIstioVersion(metadata.IstioVersion)
+	if len(metadata.IstioVersion) == 0 {
+		log.Warnf("Istio Version is not found in metadata, which may have undesirable side effects")
+	} else {
+		out.IstioVersion = ParseIstioVersion(metadata.IstioVersion)
+	}
 
 	if len(metadata.Labels) > 0 {
 		out.WorkloadLabels = labels.Collection{metadata.Labels}
