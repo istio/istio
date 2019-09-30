89e86ef10efc4e936f415ead9e5b7503b3ac1da2
Rama Chavali <rama.rao@salesforce.com>
2019-09-30 21:02:42 +0530

    fix condition
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/model/context.go
+++ pilot/pkg/model/context.go
@@ -590,9 +590,9 @@ func ParseServiceNodeWithMetadata(s string, metadata *NodeMetadata) (*Proxy, err
 	out.DNSDomain = parts[3]
 	if len(metadata.IstioVersion) == 0 {
 		log.Warnf("Istio Version is not found in metadata, which may have undesirable side effects")
-	} else {
-		out.IstioVersion = ParseIstioVersion(metadata.IstioVersion)
 	}
+	out.IstioVersion = ParseIstioVersion(metadata.IstioVersion)
+	
 
 	if len(metadata.Labels) > 0 {
 		out.WorkloadLabels = labels.Collection{metadata.Labels}
