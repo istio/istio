617c9693a0626381e9666d3c4be8a19897a63848
Martin Taillefer <geeknoid@users.noreply.github.com>
2019-10-02 11:22:50 -0700

    Build update (#17508)
    - Update common files & common protos
    
    - Introduce license file linting.
    
    - Introduce the gen-check rule which will be used by a new CI rule to
    ensure all generated files are up to date before allowing a PR through.
    
    - Regenerate go sources, given latest gogo.

--- mixer/adapter/redisquota/config/config.pb.go
+++ mixer/adapter/redisquota/config/config.pb.go
@@ -34,7 +34,7 @@ var _ = time.Kitchen
 // is compatible with the proto package it is being compiled against.
 // A compilation error at this line likely means your copy of the
 // proto package needs to be updated.
-const _ = proto.GoGoProtoPackageIsVersion2 // please upgrade the proto package
+const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 // Algorithms for rate-limiting:
 type Params_QuotaAlgorithm int32
