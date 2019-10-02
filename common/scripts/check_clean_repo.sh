469743f121f7fdf264724bf879ead51ae465b543
Martin Taillefer <geeknoid@users.noreply.github.com>
2019-10-02 10:01:26 -0700

    Add a gen-check target. (#314)

---
+++ common/scripts/check_clean_repo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Copyright 2019 Istio Authors
+
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+if [[ -n $(git status --porcelain) ]]; then
+    git status
+  	echo "ERROR: Some files need to be updated, please run make and include any changed files in your PR"
+  	exit 1
+fi
