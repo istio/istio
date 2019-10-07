b2f795567814e7e946533ad4ff11a5eef808a6c8
Joe Selman <jsselman@google.com>
2019-10-07 15:51:07 -0700

    gofmt -s

--- galley/pkg/config/analysis/analyzers/injection/injection-version.go
+++ galley/pkg/config/analysis/analyzers/injection/injection-version.go
@@ -22,8 +22,8 @@ import (
 	"istio.io/istio/galley/pkg/config/analysis"
 	"istio.io/istio/galley/pkg/config/analysis/msg"
 	"istio.io/istio/galley/pkg/config/meta/metadata"
-	"istio.io/istio/galley/pkg/config/resource"
 	"istio.io/istio/galley/pkg/config/meta/schema/collection"
+	"istio.io/istio/galley/pkg/config/resource"
 )
 
 // VersionAnalyzer checks the version of auto-injection configured with the running proxies on pods.
