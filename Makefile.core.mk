8448c82066aa74ecb78817ea4d979e063cd53d34
John Howard <howardjohn@google.com>
2019-10-10 14:12:49 -0700

    Add standalone istioctl to the release and update to use new dev builds (#17762)
    * Add standalone istioctl to release
    
    * Update to use dev builds
    
    * Add operator

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -33,9 +33,9 @@ ISTIO_DOCKER_HUB ?= docker.io/istio
 export ISTIO_DOCKER_HUB
 ISTIO_GCS ?= istio-release/releases/$(VERSION)
 ISTIO_URL ?= https://storage.googleapis.com/$(ISTIO_GCS)
-ISTIO_CNI_HUB ?= gcr.io/istio-release
+ISTIO_CNI_HUB ?= gcr.io/istio-testing
 export ISTIO_CNI_HUB
-ISTIO_CNI_TAG ?= master-latest-daily
+ISTIO_CNI_TAG ?= latest
 export ISTIO_CNI_TAG
 
 # cumulatively track the directories/files to delete after a clean
