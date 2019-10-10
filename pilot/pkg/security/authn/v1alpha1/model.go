b26686cf78b9c271454871b4ea9ad75d781bcbe1
Diem Vu <25132401+diemtvu@users.noreply.github.com>
2019-10-10 10:03:50 -0700

    Remove obsolete AuthenticationPolicyForWorkload in config store (#17691)
    * Remove obsolete functions
    
    * Lint: Remove unuse import

--- pilot/pkg/security/authn/v1alpha1/model.go
+++ pilot/pkg/security/authn/v1alpha1/model.go
@@ -16,27 +16,7 @@ package v1alpha1
 
 import (
 	authn "istio.io/api/authentication/v1alpha1"
-	"istio.io/istio/pilot/pkg/model"
 )
-
-// GetConsolidateAuthenticationPolicy returns the v1alpha1 authentication policy for workload specified by
-// hostname (or label selector if specified) and port, if defined.
-// It also tries to resolve JWKS URI if necessary.
-func GetConsolidateAuthenticationPolicy(store model.IstioConfigStore, serviceInstance *model.ServiceInstance) *authn.Policy {
-	service := serviceInstance.Service
-	port := serviceInstance.Endpoint.ServicePort
-	labels := serviceInstance.Labels
-
-	config := store.AuthenticationPolicyForWorkload(service, labels, port)
-	if config != nil {
-		policy := config.Spec.(*authn.Policy)
-		if err := model.JwtKeyResolver.SetAuthenticationPolicyJwksURIs(policy); err == nil {
-			return policy
-		}
-	}
-
-	return nil
-}
 
 // MutualTLSMode is the mutule TLS mode specified by authentication policy.
 type MutualTLSMode int
