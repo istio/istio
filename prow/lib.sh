abd3e88cb36d0295f24392dfc764e346539f5663
Joshua Blatt <jblatt@google.com>
2019-10-11 16:10:46 -0700

    Update lib.sh

--- prow/lib.sh
+++ prow/lib.sh
@@ -46,9 +46,9 @@ function setup_and_export_git_sha() {
     # Use the current commit.
     GIT_SHA="$(git rev-parse --verify HEAD)"
     export GIT_SHA
-    GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-    export GIT_BRANCH
   fi
+  GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+  export GIT_BRANCH
   gcloud auth configure-docker -q
 }
 
