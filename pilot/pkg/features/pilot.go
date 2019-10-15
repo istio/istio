7a9a996f6641024298e6010c6f1d7e87a83a48dc
Daniel Grimm <dgn@users.noreply.github.com>
2019-10-15 03:19:20 +0200

    Migrate to SafeRegex in route generation, add feature flag for old behaviour (#17490)

--- pilot/pkg/features/pilot.go
+++ pilot/pkg/features/pilot.go
@@ -232,6 +232,14 @@ var (
 		"If enabled, any HTTP services will be blocked on HTTPS port (443). If this is disabled, any "+
 			"HTTP service on port 443 could block all external traffic",
 	).Get()
+
+	EnableUnsafeRegex = env.RegisterBoolVar(
+		"PILOT_ENABLE_UNSAFE_REGEX",
+		false,
+		"If enabled, pilot will generate Envoy configuration that does not use safe_regex "+
+			"but the older, deprecated regex field. This should only be enabled to support "+
+			"legacy deployments that have not yet been migrated to the new safe regular expressions.",
+	)
 )
 
 var (
