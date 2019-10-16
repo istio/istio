227b06049704e2a827d319500dfa44a1a38c402e
Costin Manolache <costin@gmail.com>
2019-10-16 14:46:21 -0700

    Add boilerplate

--- pkg/istiod/galley.go
+++ pkg/istiod/galley.go
@@ -1,4 +1,4 @@
-// Copyright 2018 Istio Authors
+// Copyright 2019 Istio Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 package istiod
 
 import (
--- pkg/istiod/k8s/certs_test.go
+++ pkg/istiod/k8s/certs_test.go
@@ -1,3 +1,17 @@
+// Copyright 2019 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package k8s
 
 import (
@@ -11,7 +25,7 @@ func TestCerts(t *testing.T) {
 	t0 := time.Now()
 	client, kcfg, err := CreateClientset("", "")
 	if err != nil {
-		t.Fatal("Missing K8S", err)
+		t.Skip("Missing K8S", err)
 	}
 
 	certChain, keyPEM, err := GenKeyCertK8sCA(client.CertificatesV1beta1(), "istio-system", "istio-pilot.istio-system")
@@ -35,7 +49,7 @@ func BenchmarkCerts(t *testing.B) {
 
 	client, kcfg, err := CreateClientset("", "")
 	if err != nil {
-		t.Fatal("Missing K8S", err)
+		t.Skip("Missing K8S", err)
 	}
 
 	certChain, _, err := GenKeyCertK8sCA(client.CertificatesV1beta1(), "istio-system", "istio-pilot.istio-system")
