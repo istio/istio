03920f0be566586c47ad3de357b068d4ef637125
Iris <irisdingbj@gmail.com>
2019-10-16 16:53:38 +0800

    Add more switch Profile Test (#387)

--- pkg/controller/istiocontrolplane/istiocontrolplane_controller_test.go
+++ pkg/controller/istiocontrolplane/istiocontrolplane_controller_test.go
@@ -17,10 +17,10 @@ package istiocontrolplane
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"testing"
 
 	"istio.io/operator/pkg/apis/istio/v1alpha2"
-
 	"istio.io/operator/pkg/helmreconciler"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -32,103 +32,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
-// TestICPController runs ReconcileIstioControlPlane.Reconcile() against a
-// fake client that tracks an IstioControlPlane object.
-func TestICPController_SwitchProfile(t *testing.T) {
-	name := "example-istiocontrolplane"
-	namespace := "istio-system"
-	icp := &v1alpha2.IstioControlPlane{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      name,
-			Namespace: namespace,
-		},
-		Spec: &v1alpha2.IstioControlPlaneSpec{
-			Profile: "minimal",
-		},
-	}
-	objs := []runtime.Object{
-		icp,
-	}
-
-	s := scheme.Scheme
-	s.AddKnownTypes(v1alpha2.SchemeGroupVersion, icp)
-	cl := fake.NewFakeClientWithScheme(s, objs...)
-	factory := &helmreconciler.Factory{CustomizerFactory: &IstioRenderingCustomizerFactory{}}
-	r := &ReconcileIstioControlPlane{client: cl, scheme: s, factory: factory}
-
-	req := reconcile.Request{
-		NamespacedName: types.NamespacedName{
-			Name:      name,
-			Namespace: namespace,
-		},
-	}
-	res, err := r.Reconcile(req)
-	if err != nil {
-		t.Fatalf("reconcile: (%v)", err)
-	}
-	// check ICP status
-	succeed, err := checkICPStatus(cl, req.NamespacedName, "minimal")
-	if !succeed || err != nil {
-		t.Fatalf("failed to get expected IstioControlPlane status: (%v)", err)
-	}
-
-	//update IstioControlPlane : switch profile from minimal to default and reconcile
-	err = switchIstioControlPlaneProfile(cl, req.NamespacedName, "default")
-	if err != nil {
-		t.Fatalf("failed to update IstioControlPlane: (%v)", err)
-	}
-	res, err = r.Reconcile(req)
-	if err != nil {
-		t.Fatalf("reconcile: (%v)", err)
-	}
-	if res.Requeue {
-		t.Error("reconcile requeue which is not expected")
-	}
-	// check ICP status
-	succeed, err = checkICPStatus(cl, req.NamespacedName, "default")
-	if !succeed || err != nil {
-		t.Fatalf("failed to get expected IstioControlPlane status: (%v)", err)
-	}
-
-	//update IstioControlPlane : switch profile from default to minimal again and reconcile
-	err = switchIstioControlPlaneProfile(cl, req.NamespacedName, "minimal")
-	if err != nil {
-		t.Fatalf("failed to update IstioControlPlane: (%v)", err)
-	}
-	res, err = r.Reconcile(req)
-	if err != nil {
-		t.Fatalf("reconcile: (%v)", err)
-	}
-	// Check the result of reconciliation to make sure it has the desired state.
-	if res.Requeue {
-		t.Error("reconcile requeue which is not expected")
-	}
-	// check ICP status
-	succeed, err = checkICPStatus(cl, req.NamespacedName, "minimal")
-	if !succeed || err != nil {
-		t.Fatalf("failed to get expected IstioControlPlane status: (%v)", err)
-	}
-
-}
-func statusExpected(s1, s2 *v1alpha2.InstallStatus_VersionStatus) bool {
-	return s1.Status.String() == s2.Status.String()
-}
-
-func switchIstioControlPlaneProfile(cl client.Client, key client.ObjectKey, profile string) error {
-	instance := &v1alpha2.IstioControlPlane{}
-	err := cl.Get(context.TODO(), key, instance)
-	if err != nil {
-		return err
-	}
-	instance.Spec.Profile = profile
-	err = cl.Update(context.TODO(), instance)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-func checkICPStatus(cl client.Client, key client.ObjectKey, profile string) (bool, error) {
-	minimalStatus := map[string]*v1alpha2.InstallStatus_VersionStatus{
+var (
+	minimalStatus = map[string]*v1alpha2.InstallStatus_VersionStatus{
 		"Pilot": {
 			Status: v1alpha2.InstallStatus_HEALTHY,
 		},
@@ -136,7 +41,7 @@ func checkICPStatus(cl client.Client, key client.ObjectKey, profile string) (boo
 			Status: v1alpha2.InstallStatus_HEALTHY,
 		},
 	}
-	defaultStatus := map[string]*v1alpha2.InstallStatus_VersionStatus{
+	defaultStatus = map[string]*v1alpha2.InstallStatus_VersionStatus{
 		"crds": {
 			Status: v1alpha2.InstallStatus_HEALTHY,
 		},
@@ -165,6 +70,199 @@ func checkICPStatus(cl client.Client, key client.ObjectKey, profile string) (boo
 			Status: v1alpha2.InstallStatus_HEALTHY,
 		},
 	}
+	demoStatus = map[string]*v1alpha2.InstallStatus_VersionStatus{
+		"crds": {
+			Status: v1alpha2.InstallStatus_HEALTHY,
+		},
+		"Pilot": {
+			Status: v1alpha2.InstallStatus_HEALTHY,
+		},
+		"Policy": {
+			Status: v1alpha2.InstallStatus_HEALTHY,
+		},
+		"Telemetry": {
+			Status: v1alpha2.InstallStatus_HEALTHY,
+		},
+		"Injector": {
+			Status: v1alpha2.InstallStatus_HEALTHY,
+		},
+		"Citadel": {
+			Status: v1alpha2.InstallStatus_HEALTHY,
+		},
+		"Galley": {
+			Status: v1alpha2.InstallStatus_HEALTHY,
+		},
+		"Prometheus": {
+			Status: v1alpha2.InstallStatus_HEALTHY,
+		},
+		"IngressGateway": {
+			Status: v1alpha2.InstallStatus_HEALTHY,
+		},
+		"Grafana": {
+			Status: v1alpha2.InstallStatus_HEALTHY,
+		},
+		"Kiali": {
+			Status: v1alpha2.InstallStatus_HEALTHY,
+		},
+		"Tracing": {
+			Status: v1alpha2.InstallStatus_HEALTHY,
+		},
+		"EgressGateway": {
+			Status: v1alpha2.InstallStatus_HEALTHY,
+		},
+	}
+	sdsStatus = map[string]*v1alpha2.InstallStatus_VersionStatus{
+		"crds": {
+			Status: v1alpha2.InstallStatus_HEALTHY,
+		},
+		"Pilot": {
+			Status: v1alpha2.InstallStatus_HEALTHY,
+		},
+		"Policy": {
+			Status: v1alpha2.InstallStatus_HEALTHY,
+		},
+		"Telemetry": {
+			Status: v1alpha2.InstallStatus_HEALTHY,
+		},
+		"Injector": {
+			Status: v1alpha2.InstallStatus_HEALTHY,
+		},
+		"Citadel": {
+			Status: v1alpha2.InstallStatus_HEALTHY,
+		},
+		"Galley": {
+			Status: v1alpha2.InstallStatus_HEALTHY,
+		},
+		"Prometheus": {
+			Status: v1alpha2.InstallStatus_HEALTHY,
+		},
+		"IngressGateway": {
+			Status: v1alpha2.InstallStatus_HEALTHY,
+		},
+		"NodeAgent": {
+			Status: v1alpha2.InstallStatus_HEALTHY,
+		},
+	}
+)
+
+type testCase struct {
+	description    string
+	initialProfile string
+	targetProfile  string
+}
+
+// TestICPController_SwitchProfile
+func TestICPController_SwitchProfile(t *testing.T) {
+	cases := []testCase{
+		{
+			description:    "switch profile from minimal to default",
+			initialProfile: "minimal",
+			targetProfile:  "default",
+		},
+		{
+			description:    "switch profile from default to minimal",
+			initialProfile: "default",
+			targetProfile:  "minimal",
+		},
+		{
+			description:    "switch profile from default to demo",
+			initialProfile: "default",
+			targetProfile:  "demo",
+		},
+		{
+			description:    "switch profile from demo to sds",
+			initialProfile: "demo",
+			targetProfile:  "sds",
+		},
+		{
+			description:    "switch profile from sds to default",
+			initialProfile: "sds",
+			targetProfile:  "default",
+		},
+	}
+	for i, c := range cases {
+		t.Run(strconv.Itoa(i)+":"+c.description, func(t *testing.T) {
+			testSwitchProfile(t, c)
+		})
+	}
+}
+func testSwitchProfile(t *testing.T, c testCase) {
+	t.Helper()
+	name := "example-istiocontrolplane"
+	namespace := "istio-system"
+	icp := &v1alpha2.IstioControlPlane{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: &v1alpha2.IstioControlPlaneSpec{
+			Profile: c.initialProfile,
+		},
+	}
+	objs := []runtime.Object{
+		icp,
+	}
+
+	s := scheme.Scheme
+	s.AddKnownTypes(v1alpha2.SchemeGroupVersion, icp)
+	cl := fake.NewFakeClientWithScheme(s, objs...)
+	factory := &helmreconciler.Factory{CustomizerFactory: &IstioRenderingCustomizerFactory{}}
+	r := &ReconcileIstioControlPlane{client: cl, scheme: s, factory: factory}
+
+	req := reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      name,
+			Namespace: namespace,
+		},
+	}
+	res, err := r.Reconcile(req)
+	if err != nil {
+		t.Fatalf("reconcile: (%v)", err)
+	}
+	// check ICP status
+	succeed, err := checkICPStatus(cl, req.NamespacedName, c.initialProfile)
+	if !succeed || err != nil {
+		t.Fatalf("failed to get expected IstioControlPlane status: (%v)", err)
+	}
+
+	//update IstioControlPlane : switch profile from minimal to default and reconcile
+	err = switchIstioControlPlaneProfile(cl, req.NamespacedName, c.targetProfile)
+	if err != nil {
+		t.Fatalf("failed to update IstioControlPlane: (%v)", err)
+	}
+	res, err = r.Reconcile(req)
+	if err != nil {
+		t.Fatalf("reconcile: (%v)", err)
+	}
+	if res.Requeue {
+		t.Error("reconcile requeue which is not expected")
+	}
+	// check ICP status
+	succeed, err = checkICPStatus(cl, req.NamespacedName, c.targetProfile)
+	if !succeed || err != nil {
+		t.Fatalf("failed to get expected IstioControlPlane status: (%v)", err)
+	}
+}
+
+func statusExpected(s1, s2 *v1alpha2.InstallStatus_VersionStatus) bool {
+	return s1.Status.String() == s2.Status.String()
+}
+
+func switchIstioControlPlaneProfile(cl client.Client, key client.ObjectKey, profile string) error {
+	instance := &v1alpha2.IstioControlPlane{}
+	err := cl.Get(context.TODO(), key, instance)
+	if err != nil {
+		return err
+	}
+	instance.Spec.Profile = profile
+	err = cl.Update(context.TODO(), instance)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func checkICPStatus(cl client.Client, key client.ObjectKey, profile string) (bool, error) {
+
 	instance := &v1alpha2.IstioControlPlane{}
 	err := cl.Get(context.TODO(), key, instance)
 	if err != nil {
@@ -176,6 +274,10 @@ func checkICPStatus(cl client.Client, key client.ObjectKey, profile string) (boo
 		status = minimalStatus
 	case "default":
 		status = defaultStatus
+	case "sds":
+		status = sdsStatus
+	case "demo":
+		status = demoStatus
 	}
 	installStatus := instance.GetStatus()
 	size := len(installStatus.Status)
