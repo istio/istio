ea3a6193a5c67f393fc3c61538804238d441c1c0
Xinnan Wen <iamwen@google.com>
2019-10-15 21:36:38 -0700

    Turn on values validation and update values schema (#383)
    * Turn on values validation and update values schema, sync profiles,
    update goldens
    
    * Fix test
    
    * update golden test files

--- pkg/util/yaml.go
+++ pkg/util/yaml.go
@@ -102,7 +102,7 @@ func UnmarshalValuesWithJSONPB(y string, out proto.Message) error {
 	if err != nil {
 		return err
 	}
-	u := jsonpb2.Unmarshaler{AllowUnknownFields: true}
+	u := jsonpb2.Unmarshaler{AllowUnknownFields: false}
 	err = u.Unmarshal(bytes.NewReader(jb), out)
 	if err != nil {
 		return err
