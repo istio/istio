baef607e1616f35fca3b6a39ddbd0d4a200a0d88
Tao HE <1579288+elfinhe@users.noreply.github.com>
2019-10-17 16:40:56 -0700

    [release-1.4] Add upgarde subcommand from operator to istioctl (#18000)
    * [release-1.4] Add upgarde subcommand from operator to istioctl
    
    * Fix gencheck_istio check.

--- istioctl/cmd/root.go
+++ istioctl/cmd/root.go
@@ -154,6 +154,8 @@ debug and diagnose their Istio mesh.
 	hideInheritedFlags(profileCmd, "namespace", "istioNamespace")
 	experimentalCmd.AddCommand(profileCmd)
 
+	experimentalCmd.AddCommand(mesh.UpgradeCmd())
+
 	experimentalCmd.AddCommand(multicluster.NewCreateRemoteSecretCommand(&kubeconfig, &configContext, &istioNamespace))
 
 	rootCmd.AddCommand(collateral.CobraCommand(rootCmd, &doc.GenManHeader{
