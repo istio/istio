659365b4c14f639f2116340c456b761de4cabc62
JimmyCYJ <jimmychen.0102@gmail.com>
2019-10-20 11:14:34 -0700

    add test

--- security/pkg/k8s/configmap/configmap_test.go
+++ security/pkg/k8s/configmap/configmap_test.go
@@ -19,6 +19,9 @@ import (
 	"testing"
 	"time"
 
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -35,30 +38,17 @@ func TestInsertCATLSRootCert(t *testing.T) {
 		namespace         string
 		existingConfigMap *v1.ConfigMap
 		certToAdd         string
-		expectedActionsA  []ktesting.Action
-		expectedActionsB  []ktesting.Action
+		expectedActions   []ktesting.Action
 		expectedErr       string
+		client            *fake.Clientset
 	}{
 		"Non-existing ConfigMap": {
 			existingConfigMap: nil,
 			certToAdd:         "ABCD",
-			expectedActionsA: []ktesting.Action{
+			expectedActions: []ktesting.Action{
 				ktesting.NewGetAction(gvr, "test-ns", istioSecurityConfigMapName),
 				ktesting.NewCreateAction(gvr, "test-ns", createConfigMap("test-ns", map[string]string{
 					"key1": "data1", caTLSRootCertName: "ABCD"})),
-			},
-			expectedActionsB: []ktesting.Action{
-				ktesting.NewGetAction(gvr, "test-ns", istioSecurityConfigMapName),
-				ktesting.NewCreateAction(gvr, "test-ns", createConfigMap("test-ns", map[string]string{
-					"key1": "data1", caTLSRootCertName: "ABCD"})),
-				ktesting.NewGetAction(gvr, "test-ns", istioSecurityConfigMapName),
-				ktesting.NewUpdateAction(gvr, "test-ns", &v1.ConfigMap{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      istioSecurityConfigMapName,
-						Namespace: "test-ns",
-					},
-					Data: map[string]string{},
-				}),
 			},
 			expectedErr: "",
 		},
@@ -66,15 +56,7 @@ func TestInsertCATLSRootCert(t *testing.T) {
 			namespace:         "test-ns",
 			existingConfigMap: createConfigMap("test-ns", map[string]string{"key1": "data1"}),
 			certToAdd:         "ABCD",
-			expectedActionsA: []ktesting.Action{
-				ktesting.NewGetAction(gvr, "test-ns", istioSecurityConfigMapName),
-				ktesting.NewUpdateAction(gvr, "test-ns", createConfigMap("test-ns", map[string]string{
-					"key1": "data1", caTLSRootCertName: "ABCD"})),
-			},
-			expectedActionsB: []ktesting.Action{
-				ktesting.NewGetAction(gvr, "test-ns", istioSecurityConfigMapName),
-				ktesting.NewUpdateAction(gvr, "test-ns", createConfigMap("test-ns", map[string]string{
-					"key1": "data1", caTLSRootCertName: "ABCD"})),
+			expectedActions: []ktesting.Action{
 				ktesting.NewGetAction(gvr, "test-ns", istioSecurityConfigMapName),
 				ktesting.NewUpdateAction(gvr, "test-ns", createConfigMap("test-ns", map[string]string{
 					"key1": "data1", caTLSRootCertName: "ABCD"})),
@@ -85,25 +67,33 @@ func TestInsertCATLSRootCert(t *testing.T) {
 			namespace:         "",
 			existingConfigMap: createConfigMap("", map[string]string{"key1": "data1"}),
 			certToAdd:         "ABCD",
-			expectedActionsA: []ktesting.Action{
-				ktesting.NewGetAction(gvr, "test-ns", istioSecurityConfigMapName),
-				ktesting.NewUpdateAction(gvr, "test-ns", createConfigMap("", map[string]string{
-					"key1": "data1", caTLSRootCertName: "ABCD"})),
-			},
-			expectedActionsB: []ktesting.Action{
-				ktesting.NewGetAction(gvr, "test-ns", istioSecurityConfigMapName),
-				ktesting.NewUpdateAction(gvr, "test-ns", createConfigMap("", map[string]string{
-					"key1": "data1", caTLSRootCertName: "ABCD"})),
+			expectedActions: []ktesting.Action{
 				ktesting.NewGetAction(gvr, "test-ns", istioSecurityConfigMapName),
 				ktesting.NewUpdateAction(gvr, "test-ns", createConfigMap("", map[string]string{
 					"key1": "data1", caTLSRootCertName: "ABCD"})),
 			},
 			expectedErr: "",
 		},
+		"ConfigMap creation failure": {
+			existingConfigMap: nil,
+			certToAdd:         "ABCD",
+			expectedActions: []ktesting.Action{
+				ktesting.NewGetAction(gvr, "test-ns", istioSecurityConfigMapName),
+				ktesting.NewCreateAction(gvr, "test-ns", createConfigMap("test-ns", map[string]string{
+					"key1": "data1", caTLSRootCertName: "ABCD"})),
+			},
+			expectedErr: "failed to insert CA TLS root cert: no permission to create configmap",
+			client:      creatConfigMapDisabledClient(),
+		},
 	}
 
 	for id, tc := range testCases {
-		client := fake.NewSimpleClientset()
+		var client *fake.Clientset
+		if tc.client == nil {
+			client = fake.NewSimpleClientset()
+		} else {
+			client = tc.client
+		}
 		if tc.existingConfigMap != nil {
 			if _, err := client.CoreV1().ConfigMaps(tc.namespace).Create(tc.existingConfigMap); err != nil {
 				t.Errorf("Test case [%s]: Failed to update configmap %v", id, err)
@@ -122,12 +112,99 @@ func TestInsertCATLSRootCert(t *testing.T) {
 		if err == nil {
 			if tc.expectedErr != "" {
 				t.Errorf("Test case [%s]: Expecting error %s but got no error", id, tc.expectedErr)
-			} else if err := checkActions(client.Actions(), tc.expectedActionsA); err != nil {
+			} else if err := checkActions(client.Actions(), tc.expectedActions); err != nil {
 				t.Errorf("Test case [%s]: %v", id, err)
 			}
 		}
+	}
+}
 
-		err = controller.InsertCATLSRootCertWithRetry(tc.certToAdd, 1*time.Second, 2*time.Second)
+func creatConfigMapDisabledClient() *fake.Clientset {
+	client := &fake.Clientset{}
+	client.AddReactor("get", "configmaps", func(action ktesting.Action) (bool, runtime.Object, error) {
+		return true, &v1.ConfigMap{}, errors.NewNotFound(v1.Resource("configmaps"), istioSecurityConfigMapName)
+	})
+	client.AddReactor("create", "configmaps", func(action ktesting.Action) (bool, runtime.Object, error) {
+		return true, &v1.ConfigMap{}, errors.NewUnauthorized("no permission to create configmap")
+	})
+	return client
+}
+
+func TestInsertCATLSRootCertWithRetry(t *testing.T) {
+	gvr := schema.GroupVersionResource{
+		Resource: "configmaps",
+		Version:  "v1",
+	}
+	testCases := map[string]struct {
+		namespace         string
+		existingConfigMap *v1.ConfigMap
+		certToAdd         string
+		expectedActions   []ktesting.Action
+		expectedErr       string
+		client            *fake.Clientset
+	}{
+		"Non-existing ConfigMap": {
+			existingConfigMap: nil,
+			certToAdd:         "ABCD",
+			expectedActions: []ktesting.Action{
+				ktesting.NewGetAction(gvr, "test-ns", istioSecurityConfigMapName),
+				ktesting.NewCreateAction(gvr, "test-ns", createConfigMap("test-ns", map[string]string{
+					"key1": "data1", caTLSRootCertName: "ABCD"})),
+			},
+			expectedErr: "",
+		},
+		"Existing ConfigMap": {
+			namespace:         "test-ns",
+			existingConfigMap: createConfigMap("test-ns", map[string]string{"key1": "data1"}),
+			certToAdd:         "ABCD",
+			expectedActions: []ktesting.Action{
+				ktesting.NewGetAction(gvr, "test-ns", istioSecurityConfigMapName),
+				ktesting.NewUpdateAction(gvr, "test-ns", createConfigMap("test-ns", map[string]string{
+					"key1": "data1", caTLSRootCertName: "ABCD"})),
+			},
+			expectedErr: "",
+		},
+		"Namespace not specified": {
+			namespace:         "",
+			existingConfigMap: createConfigMap("", map[string]string{"key1": "data1"}),
+			certToAdd:         "ABCD",
+			expectedActions: []ktesting.Action{
+				ktesting.NewGetAction(gvr, "test-ns", istioSecurityConfigMapName),
+				ktesting.NewUpdateAction(gvr, "test-ns", createConfigMap("", map[string]string{
+					"key1": "data1", caTLSRootCertName: "ABCD"})),
+			},
+			expectedErr: "",
+		},
+		"ConfigMap creation failure": {
+			existingConfigMap: nil,
+			certToAdd:         "ABCD",
+			expectedActions: []ktesting.Action{
+				ktesting.NewGetAction(gvr, "test-ns", istioSecurityConfigMapName),
+				ktesting.NewCreateAction(gvr, "test-ns", createConfigMap("test-ns", map[string]string{
+					"key1": "data1", caTLSRootCertName: "ABCD"})),
+			},
+			expectedErr: "failed to insert CA TLS root cert: no permission to create configmap",
+			client:      creatConfigMapDisabledClient(),
+		},
+	}
+
+	for id, tc := range testCases {
+		var client *fake.Clientset
+		if tc.client == nil {
+			client = fake.NewSimpleClientset()
+		} else {
+			client = tc.client
+		}
+		if tc.existingConfigMap != nil {
+			if _, err := client.CoreV1().ConfigMaps(tc.namespace).Create(tc.existingConfigMap); err != nil {
+				t.Errorf("Test case [%s]: Failed to update configmap %v", id, err)
+			}
+		}
+
+		client.ClearActions()
+		controller := NewController(tc.namespace, client.CoreV1())
+
+		err := controller.InsertCATLSRootCertWithRetry(tc.certToAdd, 1*time.Second, 2*time.Second)
 		if err != nil && err.Error() != tc.expectedErr {
 			t.Errorf("Test case [%s]: Get error (%s) different from expected error (%s).",
 				id, err.Error(), tc.expectedErr)
@@ -135,7 +212,7 @@ func TestInsertCATLSRootCert(t *testing.T) {
 		if err == nil {
 			if tc.expectedErr != "" {
 				t.Errorf("Test case [%s]: Expecting error %s but got no error", id, tc.expectedErr)
-			} else if err := checkActions(client.Actions(), tc.expectedActionsB); err != nil {
+			} else if err := checkActions(client.Actions(), tc.expectedActions); err != nil {
 				t.Errorf("Test case [%s]: %v", id, err)
 			}
 		}
