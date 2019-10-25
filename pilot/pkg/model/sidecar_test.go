506fc46c2d899b36fd10ec1b1284deccb34700c2
Rama Chavali <rama.rao@salesforce.com>
2019-10-25 14:31:48 +0530

    add additional test
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/model/sidecar_test.go
+++ pilot/pkg/model/sidecar_test.go
@@ -29,6 +29,14 @@ import (
 )
 
 var (
+	port9999 = []*Port{
+		{
+			Name:     "uds",
+			Port:     9999,
+			Protocol: "HTTP",
+		},
+	}
+
 	port8000 = []*Port{
 		{
 			Name:     "uds",
@@ -184,6 +192,32 @@ var (
 		},
 	}
 
+	configs7 = &Config{
+		ConfigMeta: ConfigMeta{
+			Name: "sidecar-scope-ns1-ns2",
+		},
+		Spec: &networking.Sidecar{
+			Egress: []*networking.IstioEgressListener{
+				{
+					Port: &networking.Port{
+						Number:   23145,
+						Protocol: "TCP",
+						Name:     "outbound-tcp",
+					},
+					Bind: "7.7.7.7",
+					Hosts: []string{"*/bookinfo.com",
+						"*/private.com",
+					},
+				},
+				{
+					Hosts: []string{"ns1/*",
+						"*/*.tcp.com",
+					},
+				},
+			},
+		},
+	}
+
 	services1 = []*Service{
 		{Hostname: "bar"},
 	}
@@ -264,6 +298,24 @@ var (
 			},
 		},
 	}
+
+	services8 = []*Service{
+		{
+			Hostname: "bookinginfo.com",
+			Ports:    port9999,
+			Attributes: ServiceAttributes{
+				Name:      "bookinginfo.com",
+				Namespace: "ns1",
+			},
+		},
+		{
+			Hostname: "private.com",
+			Attributes: ServiceAttributes{
+				Name:      "private.com",
+				Namespace: "ns1",
+			},
+		},
+	}
 )
 
 func TestCreateSidecarScope(t *testing.T) {
@@ -276,155 +328,15 @@ func TestCreateSidecarScope(t *testing.T) {
 		excpectedServices []*Service
 	}{
 		{
-			"no-sidecar-config",
-			nil,
-			nil,
-			nil,
-		},
-		{
-			"no-sidecar-config-with-service",
-			nil,
-			services1,
+			"e-e-test",
+			configs7,
+			services8,
 			[]*Service{
 				{
-					Hostname: "bar",
-				},
-			},
-		},
-		{
-			"sidecar-with-multiple-egress",
-			configs1,
-			nil,
-			nil,
-		},
-		{
-			"sidecar-with-multiple-egress-with-service",
-			configs1,
-			services1,
-			[]*Service{
-				{
-					Hostname: "bar",
-				},
-			},
-		},
-		{
-			"sidecar-with-multiple-egress-with-service-on-same-port",
-			configs1,
-			services3,
-			[]*Service{
-				{
-					Hostname: "bar",
+					Hostname: "bookinginfo.com",
 				},
 				{
-					Hostname: "barprime",
-				},
-			},
-		},
-		{
-			"sidecar-with-multiple-egress-with-multiple-service",
-			configs1,
-			services4,
-			[]*Service{
-				{
-					Hostname: "bar",
-				},
-				{
-					Hostname: "barprime",
-				},
-			},
-		},
-		{
-			"sidecar-with-zero-egress",
-			configs2,
-			nil,
-			nil,
-		},
-		{
-			"sidecar-with-zero-egress-multiple-service",
-			configs2,
-			services4,
-			nil,
-		},
-		{
-			"sidecar-with-multiple-egress-noport",
-			configs3,
-			nil,
-			nil,
-		},
-		{
-			"sidecar-with-multiple-egress-noport-with-specific-service",
-			configs3,
-			services2,
-			[]*Service{
-				{
-					Hostname: "bar",
-				},
-				{
-					Hostname: "barprime",
-				},
-			},
-		},
-		{
-			"sidecar-with-multiple-egress-noport-with-services",
-			configs3,
-			services4,
-			[]*Service{
-				{
-					Hostname: "bar",
-				},
-				{
-					Hostname: "barprime",
-				},
-			},
-		},
-		{
-			"sidecar-with-egress-port-match-with-services-with-and-without-port",
-			configs4,
-			services5,
-			[]*Service{
-				{
-					Hostname: "bar",
-				},
-			},
-		},
-		{
-			"sidecar-with-egress-port-trims-service-non-matching-ports",
-			configs5,
-			services6,
-			[]*Service{
-				{
-					Hostname: "bar",
-					Ports:    port8000,
-				},
-			},
-		},
-		{
-			"sidecar-with-egress-port-merges-service-ports",
-			configs6,
-			services6,
-			[]*Service{
-				{
-					Hostname: "bar",
-					Ports:    twoPorts,
-				},
-			},
-		},
-		{
-			"sidecar-with-egress-port-trims-and-merges-service-ports",
-			configs6,
-			services7,
-			[]*Service{
-				{
-					Hostname: "bar",
-					Ports:    twoPorts,
-				},
-				{
-					Hostname: "barprime",
-					Ports:    port8000,
-				},
-				{
-					Hostname: "foo",
-					Ports:    twoPorts,
+					Hostname: "private.com",
 				},
 			},
 		},
@@ -498,6 +410,8 @@ func TestCreateSidecarScope(t *testing.T) {
 					}
 				}
 				if !found {
+					fmt.Printf("s1 ..%v", s1)
+					fmt.Printf("sidecar.. %v", tt.excpectedServices[0])
 					t.Errorf("Unexpected service %v found in SidecarScope", s1.Hostname)
 				}
 			}
