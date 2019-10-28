e169b24d97b6f6892e170353f2b448242532b0f9
Xavier Canal i Masjuan <xeviknal@gmail.com>
2019-10-28 16:24:42 +0100

    make gen files

--- galley/pkg/config/analysis/msg/messages.gen.go
+++ galley/pkg/config/analysis/msg/messages.gen.go
@@ -60,11 +60,10 @@ var (
 	// ConflictingMeshGatewayVirtualServiceHosts defines a diag.MessageType for message "ConflictingMeshGatewayVirtualServiceHosts".
 	// Description: Conflicting hosts on VirtualServices associated with mesh gateway
 	ConflictingMeshGatewayVirtualServiceHosts = diag.NewMessageType(diag.Error, "IST0109", "The VirtualServices %s associated with mesh gateway define the same host %s which can lead to undefined behavior. This can be fixed by merging the conflicting VirtualServices into a single resource.")
-	
+
 	// NoResourcesNotFoundForNamespace defines a diag.MessageType for message "NoResourcesNotFoundForNamespace".
 	// Description: There are no resources in the namespace
 	NoResourcesNotFoundForNamespace = diag.NewMessageType(diag.Error, "IST0110", "No %s found in %s namespace")
-	
 )
 
 // NewInternalError returns a new diag.Message based on InternalError.
