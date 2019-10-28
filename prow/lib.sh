fd02b1c980e741e63084fbd1918150ac2c172a5b
Jonh Wendell <jonh.wendell@redhat.com>
2019-10-28 12:07:59 -0400

    Do not fail if gcloud is not found (#18382)
    It's not needed to run prow scripts locally.

--- prow/lib.sh
+++ prow/lib.sh
@@ -14,6 +14,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+function setup_gcloud_credentials() {
+  if [[ $(command -v gcloud) ]]; then
+    gcloud auth configure-docker -q
+  elif [[ $(command -v docker-credential-gcr) ]]; then
+    docker-credential-gcr configure-docker
+  else
+    echo "No credential helpers found, push to docker may not function properly"
+  fi
+}
+
 function setup_and_export_git_sha() {
   if [[ -n "${CI:-}" ]]; then
 
@@ -35,7 +46,7 @@ function setup_and_export_git_sha() {
   fi
   GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
   export GIT_BRANCH
-  gcloud auth configure-docker -q
+  setup_gcloud_credentials
 }
 
 # Download and unpack istio release artifacts.
