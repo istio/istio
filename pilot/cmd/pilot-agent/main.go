istio/istio

commit c55ac78a09bc619aa417ff31e78c7c8a5b74f51b

Strongly type bootstrap interface (#17478)
The bootstrap package is loosely typed and it's not clear what values are settable or what their types are.

This PR changes the bootstrap package to clearly indicate what parameters it supports as well as their types.

This is a step toward a broader refactoring of the pilot agent code.

pilot/cmd/pilot-agent/main.go
@@ -17,7 +17,6 @@ package main
 import (
 	"bytes"
 	"context"
-	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -46,6 +45,7 @@ import (
 	"istio.io/istio/pilot/pkg/proxy"
 	envoyDiscovery "istio.io/istio/pilot/pkg/proxy/envoy"
 	"istio.io/istio/pilot/pkg/serviceregistry"
+	"istio.io/istio/pkg/bootstrap/option"
 	"istio.io/istio/pkg/cmd"
 	"istio.io/istio/pkg/config/constants"
 	"istio.io/istio/pkg/config/mesh"
@@ -135,6 +135,12 @@ var (
 			if err := log.Configure(loggingOptions); err != nil {
 				return err
 			}
+
+			// Extract pod variables.
+			podName := podNameVar.Get()
+			podNamespace := podNamespaceVar.Get()
+			podIP := net.ParseIP(instanceIPVar.Get()) // protobuf encoding of IP_ADDRESS type
+
 			log.Infof("Version %s", version.Info.String())
 			role.Type = model.SidecarProxy
 			if len(args) > 0 {
@@ -148,11 +154,8 @@ var (
 			//Do we need to get IP from the command line or environment?
 			if len(proxyIP) != 0 {
 				role.IPAddresses = append(role.IPAddresses, proxyIP)
-			} else {
-				envIP := instanceIPVar.Get()
-				if len(envIP) > 0 {
-					role.IPAddresses = append(role.IPAddresses, envIP)
-				}
+			} else if podIP != nil {
+				role.IPAddresses = append(role.IPAddresses, podIP.String())
 			}
 
 			// Obtain all the IPs from the node
@@ -166,12 +169,13 @@ var (
 				role.IPAddresses = append(role.IPAddresses, "127.0.0.1")
 				role.IPAddresses = append(role.IPAddresses, "::1")
 			}
+
 			// Check if proxy runs in ipv4 or ipv6 environment to set Envoy's
 			// operational parameters correctly.
 			proxyIPv6 := isIPv6Proxy(role.IPAddresses)
 			if len(role.ID) == 0 {
 				if registry == serviceregistry.KubernetesRegistry {
-					role.ID = podNameVar.Get() + "." + podNamespaceVar.Get()
+					role.ID = podName + "." + podNamespace
 				} else if registry == serviceregistry.ConsulRegistry {
 					role.ID = role.IPAddresses[0] + ".service.consul"
 				} else {
@@ -216,11 +220,13 @@ var (
 			proxyConfig.Concurrency = int32(concurrency)
 
 			var pilotSAN []string
+			controlPlaneAuthEnabled := false
 			ns := ""
 			switch controlPlaneAuthPolicy {
 			case meshconfig.AuthenticationPolicy_NONE.String():
 				proxyConfig.ControlPlaneAuthPolicy = meshconfig.AuthenticationPolicy_NONE
 			case meshconfig.AuthenticationPolicy_MUTUAL_TLS.String():
+				controlPlaneAuthEnabled = true
 				proxyConfig.ControlPlaneAuthPolicy = meshconfig.AuthenticationPolicy_MUTUAL_TLS
 				if registry == serviceregistry.KubernetesRegistry {
 					partDiscoveryAddress := strings.Split(discoveryAddress, ":")
@@ -229,7 +235,7 @@ var (
 					if len(parts) == 1 {
 						// namespace of pilot is not part of discovery address use
 						// pod namespace e.g. istio-pilot:15005
-						ns = podNamespaceVar.Get()
+						ns = podNamespace
 					} else if len(parts) == 2 {
 						// namespace is found in the discovery address
 						// e.g. istio-pilot.istio-system:15005
@@ -244,12 +250,14 @@ var (
 					}
 				}
 			}
-			role.DNSDomain = getDNSDomain(role.DNSDomain)
-			setSpiffeTrustDomain(role.DNSDomain)
+			role.DNSDomain = getDNSDomain(podNamespace, role.DNSDomain)
+			setSpiffeTrustDomain(podNamespace, role.DNSDomain)
 
-			// Obtain the SAN to later create a Envoy proxy.
+			// Obtain the Pilot and Mixer SANs. Used below to create a Envoy proxy.
 			pilotSAN = getSAN(ns, envoyDiscovery.PilotSvcAccName, pilotIdentity)
 			log.Infof("PilotSAN %#v", pilotSAN)
+			mixerSAN := getSAN(ns, envoyDiscovery.MixerSvcAccName, mixerIdentity)
+			log.Infof("MixerSAN %#v", mixerSAN)
 
 			// resolve statsd address
 			if proxyConfig.StatsdUdpAddress != "" {
@@ -304,9 +312,8 @@ var (
 				log.Infof("Effective config: %s", out)
 			}
 
-			controlPlaneAuthEnabled := controlPlaneAuthPolicy == meshconfig.AuthenticationPolicy_MUTUAL_TLS.String()
-			sdsEnabled, sdsTokenPath := detectSds(controlPlaneBootstrap, sdsUdsPathVar.Get(), trustworthyJWTPath)
-
+			sdsUDSPath := sdsUdsPathVar.Get()
+			sdsEnabled, sdsTokenPath := detectSds(controlPlaneBootstrap, sdsUDSPath, trustworthyJWTPath)
 			// dedupe cert paths so we don't set up 2 watchers for the same file:
 			tlsCertsToWatch = dedupeStrings(tlsCertsToWatch)
 
@@ -320,43 +327,40 @@ var (
 				}
 			}
 
-			opts := make(map[string]interface{})
-			// If control plane auth is mTLS and global SDS flag is turned on, set UDS path and token path
+			// If control plane auth is not mTLS or global SDS flag is turned off, unset UDS path and token path
 			// for control plane SDS.
-			if controlPlaneAuthEnabled && sdsEnabled {
-				opts["sds_uds_path"] = sdsUdsPathVar.Get()
-				opts["sds_token_path"] = sdsTokenPath
+			if !controlPlaneAuthEnabled || !sdsEnabled {
+				sdsUDSPath = ""
+				sdsTokenPath = ""
 			}
 
 			// TODO: change Mixer and Pilot to use standard template and deprecate this custom bootstrap parser
 			if controlPlaneBootstrap {
 				if templateFile != "" && proxyConfig.CustomConfigFile == "" {
-					opts["PodName"] = podNameVar.Get()
-					opts["PodNamespace"] = podNamespaceVar.Get()
-					// Setting default to ipv4 local host, wildcard and dns policy
-					opts["localhost"] = "127.0.0.1"
-					opts["wildcard"] = "0.0.0.0"
-					opts["dns_lookup_family"] = "V4_ONLY"
+					// Generate a custom bootstrap template.
+					opts := []option.Instance{
+						option.PodName(podName),
+						option.PodNamespace(podNamespace),
+						option.MixerSubjectAltName(mixerSAN),
+						option.PodIP(podIP),
+						option.ControlPlaneAuth(controlPlaneAuthEnabled),
+						option.DisableReportCalls(disableInternalTelemetry),
+					}
+
 					// Check if nodeIP carries IPv4 or IPv6 and set up proxy accordingly
 					if proxyIPv6 {
-						opts["localhost"] = "::1"
-						opts["wildcard"] = "::"
-						opts["dns_lookup_family"] = "AUTO"
-					}
-					mixerSAN := getSAN(ns, envoyDiscovery.MixerSvcAccName, mixerIdentity)
-					log.Infof("MixerSAN %#v", mixerSAN)
-					if len(mixerSAN) > 1 {
-						opts["MixerSubjectAltName"] = mixerSAN[0]
+						opts = append(opts, option.Localhost(option.LocalhostIPv6),
+							option.Wildcard(option.WildcardIPv6),
+							option.DNSLookupFamily(option.DNSLookupFamilyIPv6))
+					} else {
+						opts = append(opts, option.Localhost(option.LocalhostIPv4),
+							option.Wildcard(option.WildcardIPv4),
+							option.DNSLookupFamily(option.DNSLookupFamilyIPv4))
 					}
 
-					// protobuf encoding of IP_ADDRESS type
-					opts["PodIP"] = base64.StdEncoding.EncodeToString(net.ParseIP(instanceIPVar.Get()))
-
-					if proxyConfig.ControlPlaneAuthPolicy == meshconfig.AuthenticationPolicy_MUTUAL_TLS {
-						opts["ControlPlaneAuth"] = "enable"
-					}
-					if disableInternalTelemetry {
-						opts["DisableReportCalls"] = "true"
+					params, err := option.NewTemplateParams(opts...)
+					if err != nil {
+						return err
 					}
 
 					tmpl, err := template.ParseFiles(templateFile)
@@ -364,7 +368,7 @@ var (
 						return err
 					}
 					var buffer bytes.Buffer
-					err = tmpl.Execute(&buffer, opts)
+					err = tmpl.Execute(&buffer, params)
 					if err != nil {
 						return err
 					}
@@ -413,7 +417,23 @@ var (
 
 			log.Infof("PilotSAN %#v", pilotSAN)
 
-			envoyProxy := envoy.NewProxy(proxyConfig, role.ServiceNode(), proxyLogLevel, proxyComponentLogLevel, pilotSAN, role.IPAddresses, dnsRefreshRate, opts)
+			envoyProxy := envoy.NewProxy(envoy.ProxyConfig{
+				Config:              proxyConfig,
+				Node:                role.ServiceNode(),
+				LogLevel:            proxyLogLevel,
+				ComponentLogLevel:   proxyComponentLogLevel,
+				PilotSubjectAltName: pilotSAN,
+				MixerSubjectAltName: mixerSAN,
+				NodeIPs:             role.IPAddresses,
+				DNSRefreshRate:      dnsRefreshRate,
+				PodName:             podName,
+				PodNamespace:        podNamespace,
+				PodIP:               podIP,
+				SDSUDSPath:          sdsUDSPath,
+				SDSTokenPath:        sdsTokenPath,
+				ControlPlaneAuth:    controlPlaneAuthEnabled,
+				DisableReportCalls:  disableInternalTelemetry,
+			})
 			agent := envoy.NewAgent(envoyProxy, envoy.DefaultRetry, features.TerminationDrainDuration())
 			watcher := envoy.NewWatcher(tlsCertsToWatch, agent.ConfigCh())
 
@@ -449,12 +469,12 @@ func waitForCompletion(ctx context.Context, fn func(context.Context)) {
 
 //explicitly setting the trustdomain so the pilot and mixer SAN will have same trustdomain
 //and the initialization of the spiffe pkg isn't linked to generating pilot's SAN first
-func setSpiffeTrustDomain(domain string) {
+func setSpiffeTrustDomain(podNamespace string, domain string) {
 	if controlPlaneAuthPolicy == meshconfig.AuthenticationPolicy_MUTUAL_TLS.String() {
 		pilotTrustDomain := trustDomain
 		if len(pilotTrustDomain) == 0 {
 			if registry == serviceregistry.KubernetesRegistry &&
-				(domain == podNamespaceVar.Get()+".svc.cluster.local" || domain == "") {
+				(domain == podNamespace+".svc.cluster.local" || domain == "") {
 				pilotTrustDomain = "cluster.local"
 			} else if registry == serviceregistry.ConsulRegistry &&
 				(domain == "service.consul" || domain == "") {
@@ -481,10 +501,10 @@ func getSAN(ns string, defaultSA string, overrideIdentity string) []string {
 	return san
 }
 
-func getDNSDomain(domain string) string {
+func getDNSDomain(podNamespace, domain string) string {
 	if len(domain) == 0 {
 		if registry == serviceregistry.KubernetesRegistry {
-			domain = podNamespaceVar.Get() + ".svc.cluster.local"
+			domain = podNamespace + ".svc.cluster.local"
 		} else if registry == serviceregistry.ConsulRegistry {
 			domain = "service.consul"
 		} else {

pilot/cmd/pilot-agent/main_test.go
@@ -36,7 +36,7 @@ func TestNoPilotSanIfAuthenticationNone(t *testing.T) {
 	role.DNSDomain = ""
 	controlPlaneAuthPolicy = meshconfig.AuthenticationPolicy_NONE.String()
 
-	setSpiffeTrustDomain(role.DNSDomain)
+	setSpiffeTrustDomain("", role.DNSDomain)
 	pilotSAN := getSAN("anything", envoy.PilotSvcAccName, pilotIdentity)
 
 	g.Expect(pilotSAN).To(gomega.BeNil())
@@ -49,7 +49,7 @@ func TestPilotSanIfAuthenticationMutualDomainEmptyKubernetes(t *testing.T) {
 	registry = serviceregistry.KubernetesRegistry
 	controlPlaneAuthPolicy = meshconfig.AuthenticationPolicy_MUTUAL_TLS.String()
 
-	setSpiffeTrustDomain(role.DNSDomain)
+	setSpiffeTrustDomain("", role.DNSDomain)
 	pilotSAN := getSAN("anything", envoy.PilotSvcAccName, pilotIdentity)
 
 	g.Expect(pilotSAN).To(gomega.Equal([]string{"spiffe://cluster.local/ns/anything/sa/istio-pilot-service-account"}))
@@ -63,7 +63,7 @@ func TestPilotSanIfAuthenticationMutualDomainNotEmptyKubernetes(t *testing.T) {
 	registry = serviceregistry.KubernetesRegistry
 	controlPlaneAuthPolicy = meshconfig.AuthenticationPolicy_MUTUAL_TLS.String()
 
-	setSpiffeTrustDomain(role.DNSDomain)
+	setSpiffeTrustDomain("", role.DNSDomain)
 	pilotSAN := getSAN("anything", envoy.PilotSvcAccName, pilotIdentity)
 
 	g.Expect(pilotSAN).To(gomega.Equal([]string{"spiffe://my.domain/ns/anything/sa/istio-pilot-service-account"}))
@@ -77,7 +77,7 @@ func TestPilotSanIfAuthenticationMutualDomainEmptyConsul(t *testing.T) {
 	registry = serviceregistry.ConsulRegistry
 	controlPlaneAuthPolicy = meshconfig.AuthenticationPolicy_MUTUAL_TLS.String()
 
-	setSpiffeTrustDomain(role.DNSDomain)
+	setSpiffeTrustDomain("", role.DNSDomain)
 	pilotSAN := getSAN("anything", envoy.PilotSvcAccName, pilotIdentity)
 
 	g.Expect(pilotSAN).To(gomega.Equal([]string{"spiffe:///ns/anything/sa/istio-pilot-service-account"}))
@@ -94,7 +94,7 @@ func TestPilotSanIfAuthenticationMutualTrustDomain(t *testing.T) {
 	registry = serviceregistry.KubernetesRegistry
 	controlPlaneAuthPolicy = meshconfig.AuthenticationPolicy_MUTUAL_TLS.String()
 
-	setSpiffeTrustDomain(role.DNSDomain)
+	setSpiffeTrustDomain("", role.DNSDomain)
 	pilotSAN := getSAN("anything", envoy.PilotSvcAccName, pilotIdentity)
 
 	g.Expect(pilotSAN).To(gomega.Equal([]string{"spiffe://secured/ns/anything/sa/istio-pilot-service-account"}))
@@ -111,7 +111,7 @@ func TestPilotSanIfAuthenticationMutualTrustDomainAndDomain(t *testing.T) {
 	registry = serviceregistry.KubernetesRegistry
 	controlPlaneAuthPolicy = meshconfig.AuthenticationPolicy_MUTUAL_TLS.String()
 
-	setSpiffeTrustDomain(role.DNSDomain)
+	setSpiffeTrustDomain("", role.DNSDomain)
 	pilotSAN := getSAN("anything", envoy.PilotSvcAccName, pilotIdentity)
 
 	g.Expect(pilotSAN).To(gomega.Equal([]string{"spiffe://secured/ns/anything/sa/istio-pilot-service-account"}))
@@ -122,12 +122,10 @@ func TestPilotDefaultDomainKubernetes(t *testing.T) {
 	role = &model.Proxy{}
 	role.DNSDomain = ""
 	registry = serviceregistry.KubernetesRegistry
-	_ = os.Setenv("POD_NAMESPACE", "default")
 
-	domain := getDNSDomain(role.DNSDomain)
+	domain := getDNSDomain("default", role.DNSDomain)
 
 	g.Expect(domain).To(gomega.Equal("default.svc.cluster.local"))
-	_ = os.Unsetenv("POD_NAMESPACE")
 }
 
 func TestDetectSds(t *testing.T) {
@@ -220,7 +218,7 @@ func TestPilotDefaultDomainConsul(t *testing.T) {
 	role.DNSDomain = ""
 	registry = serviceregistry.ConsulRegistry
 
-	domain := getDNSDomain(role.DNSDomain)
+	domain := getDNSDomain("", role.DNSDomain)
 
 	g.Expect(domain).To(gomega.Equal("service.consul"))
 }
@@ -231,7 +229,7 @@ func TestPilotDefaultDomainOthers(t *testing.T) {
 	role.DNSDomain = ""
 	registry = serviceregistry.MockRegistry
 
-	domain := getDNSDomain(role.DNSDomain)
+	domain := getDNSDomain("", role.DNSDomain)
 
 	g.Expect(domain).To(gomega.Equal(""))
 }
@@ -241,7 +239,7 @@ func TestPilotDomain(t *testing.T) {
 	role.DNSDomain = "my.domain"
 	registry = serviceregistry.MockRegistry
 
-	domain := getDNSDomain(role.DNSDomain)
+	domain := getDNSDomain("", role.DNSDomain)
 
 	g.Expect(domain).To(gomega.Equal("my.domain"))
 }
@@ -253,7 +251,7 @@ func TestPilotSanIfAuthenticationMutualStdDomainKubernetes(t *testing.T) {
 	registry = serviceregistry.KubernetesRegistry
 	controlPlaneAuthPolicy = meshconfig.AuthenticationPolicy_MUTUAL_TLS.String()
 
-	setSpiffeTrustDomain(role.DNSDomain)
+	setSpiffeTrustDomain("", role.DNSDomain)
 	pilotSAN := getSAN("anything", envoy.PilotSvcAccName, pilotIdentity)
 
 	g.Expect(pilotSAN).To(gomega.Equal([]string{"spiffe://cluster.local/ns/anything/sa/istio-pilot-service-account"}))
@@ -269,7 +267,7 @@ func TestPilotSanIfAuthenticationMutualStdDomainConsul(t *testing.T) {
 	registry = serviceregistry.ConsulRegistry
 	controlPlaneAuthPolicy = meshconfig.AuthenticationPolicy_MUTUAL_TLS.String()
 
-	setSpiffeTrustDomain(role.DNSDomain)
+	setSpiffeTrustDomain("", role.DNSDomain)
 	pilotSAN := getSAN("anything", envoy.PilotSvcAccName, pilotIdentity)
 
 	g.Expect(pilotSAN).To(gomega.Equal([]string{"spiffe:///ns/anything/sa/istio-pilot-service-account"}))
@@ -283,7 +281,7 @@ func TestCustomPilotSanIfAuthenticationMutualDomainKubernetesNoTrustDomain(t *te
 	registry = serviceregistry.KubernetesRegistry
 	controlPlaneAuthPolicy = meshconfig.AuthenticationPolicy_MUTUAL_TLS.String()
 
-	setSpiffeTrustDomain(role.DNSDomain)
+	setSpiffeTrustDomain("", role.DNSDomain)
 	pilotSAN := getSAN("anything", envoy.PilotSvcAccName, pilotIdentity)
 
 	g.Expect(pilotSAN).To(gomega.Equal([]string{"spiffe://cluster.local/pilot-identity"}))
@@ -298,7 +296,7 @@ func TestCustomPilotSanIfAuthenticationMutualDomainKubernetes(t *testing.T) {
 	registry = serviceregistry.KubernetesRegistry
 	controlPlaneAuthPolicy = meshconfig.AuthenticationPolicy_MUTUAL_TLS.String()
 
-	setSpiffeTrustDomain(role.DNSDomain)
+	setSpiffeTrustDomain("", role.DNSDomain)
 	pilotSAN := getSAN("anything", envoy.PilotSvcAccName, pilotIdentity)
 
 	g.Expect(pilotSAN).To(gomega.Equal([]string{"spiffe://mesh.com/pilot-identity"}))
@@ -313,7 +311,7 @@ func TestCustomMixerSanIfAuthenticationMutualDomainKubernetes(t *testing.T) {
 	registry = serviceregistry.KubernetesRegistry
 	controlPlaneAuthPolicy = meshconfig.AuthenticationPolicy_MUTUAL_TLS.String()
 
-	setSpiffeTrustDomain(role.DNSDomain)
+	setSpiffeTrustDomain("", role.DNSDomain)
 	mixerSAN := envoy.GetSAN("", mixerIdentity)
 
 	g.Expect(mixerSAN).To(gomega.Equal("spiffe://mesh.com/mixer-identity"))

commit db7601b8cffb761e201d09608ec05197826d0541

Fix sds info in control plane bootstrap
This was a regression cause in (#17478). Previously, we set these two
fields outside of the `if controlPlaneBootstrap` section, but in that
change we removed it without adding it here.

pilot/cmd/pilot-agent/main.go
@@ -372,6 +372,8 @@ var (
 						option.PodIP(podIP),
 						option.ControlPlaneAuth(controlPlaneAuthEnabled),
 						option.DisableReportCalls(disableInternalTelemetry),
+						option.SDSTokenPath(sdsTokenPath),
+						option.SDSUDSPath(sdsUDSPath),
 					}
 
 					// Check if nodeIP carries IPv4 or IPv6 and set up proxy accordingly