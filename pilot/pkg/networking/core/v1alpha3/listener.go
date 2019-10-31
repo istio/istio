4c58b3d2f32c715f2188f227774256e2f81d865b
Yan Xue <3491507+yxue@users.noreply.github.com>
2019-10-31 14:49:03 -0700

    disable protocol detection timeout for gateway (#18463) (#18527)
    * disable protocol detection timeout for gateway
    
    * address comment
    
    * address comment
    
    * resolve comment
    
    * more
    
    * tests
    
    * fix

--- pilot/pkg/networking/core/v1alpha3/listener.go
+++ pilot/pkg/networking/core/v1alpha3/listener.go
@@ -36,8 +36,6 @@ import (
 	"github.com/golang/protobuf/ptypes"
 	structpb "github.com/golang/protobuf/ptypes/struct"
 
-	"istio.io/istio/pkg/util/gogo"
-
 	meshconfig "istio.io/api/mesh/v1alpha1"
 	networking "istio.io/api/networking/v1alpha3"
 	"istio.io/pkg/log"
@@ -54,6 +52,7 @@ import (
 	"istio.io/istio/pkg/config/labels"
 	"istio.io/istio/pkg/config/protocol"
 	"istio.io/istio/pkg/proto"
+	"istio.io/istio/pkg/util/gogo"
 	"istio.io/pkg/monitoring"
 )
 
@@ -1881,8 +1880,9 @@ func buildListener(opts buildListenerOpts) *xdsapi.Listener {
 		DeprecatedV1:    deprecatedV1,
 	}
 
-	if util.IsIstioVersionGE13(opts.proxy) {
+	if util.IsIstioVersionGE13(opts.proxy) && opts.proxy.Type != model.Router {
 		listener.ListenerFiltersTimeout = gogo.DurationToProtoDuration(opts.env.Mesh.ProtocolDetectionTimeout)
+
 		if listener.ListenerFiltersTimeout != nil {
 			listener.ContinueOnListenerFiltersTimeout = true
 		}
