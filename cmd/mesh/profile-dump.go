f739927b127fd58c9e3a5b19cb38dc87e0ccc2b8
Xinnan Wen <iamwen@google.com>
2019-10-31 11:31:37 -0700

    use force flag for iscp (#492)

--- cmd/mesh/profile-dump.go
+++ cmd/mesh/profile-dump.go
@@ -106,7 +106,7 @@ func genICPS(inFilename, profile, setOverlayYAML string, force bool, l *logger) 
 		if err != nil {
 			return "", nil, fmt.Errorf("could not read values from file %s: %s", inFilename, err)
 		}
-		overlayICPS, overlayYAML, err = unmarshalAndValidateICP(string(b))
+		overlayICPS, overlayYAML, err = unmarshalAndValidateICP(string(b), force)
 		if err != nil {
 			return "", nil, err
 		}
@@ -135,7 +135,7 @@ func genICPS(inFilename, profile, setOverlayYAML string, force bool, l *logger) 
 		}
 	}
 
-	_, baseYAML, err := unmarshalAndValidateICP(baseCRYAML)
+	_, baseYAML, err := unmarshalAndValidateICP(baseCRYAML, force)
 	if err != nil {
 		return "", nil, err
 	}
@@ -203,7 +203,7 @@ func genProfile(helmValues bool, inFilename, profile, setOverlayYAML, configPath
 	return finalYAML, err
 }
 
-func unmarshalAndValidateICP(crYAML string) (*v1alpha2.IstioControlPlaneSpec, string, error) {
+func unmarshalAndValidateICP(crYAML string, force bool) (*v1alpha2.IstioControlPlaneSpec, string, error) {
 	// TODO: add GVK handling as appropriate.
 	if crYAML == "" {
 		return &v1alpha2.IstioControlPlaneSpec{}, "", nil
@@ -213,7 +213,9 @@ func unmarshalAndValidateICP(crYAML string) (*v1alpha2.IstioControlPlaneSpec, st
 		return nil, "", fmt.Errorf("could not unmarshal the overlay file: %s\n\nOriginal YAML:\n%s", err, crYAML)
 	}
 	if errs := validate.CheckIstioControlPlaneSpec(icps, false); len(errs) != 0 {
-		return nil, "", fmt.Errorf("input file failed validation with the following errors: %s\n\nOriginal YAML:\n%s", errs, crYAML)
+		if !force {
+			return nil, "", fmt.Errorf("input file failed validation with the following errors: %s\n\nOriginal YAML:\n%s", errs, crYAML)
+		}
 	}
 	icpsYAML, err := util.MarshalWithJSONPB(icps)
 	if err != nil {
