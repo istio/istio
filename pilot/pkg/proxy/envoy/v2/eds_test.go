8accfa2c36c383cc9940b8a8a6d9298903a1c182
Rama Chavali <rama.rao@salesforce.com>
2019-11-06 10:26:13 +0530

    eds: do not trigger continuous full pushes when a pod is in crash loop (#18574)
    * wip
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * add test cases
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * revert controller change
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * add debug error
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * try deleting completely
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * trigger full push for ss3 config update
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * change sse to trigger full push
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * lint
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * review comments
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * split delete in to two functions
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * address comments
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * clean up ports
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/proxy/envoy/v2/eds_test.go
+++ pilot/pkg/proxy/envoy/v2/eds_test.go
@@ -163,7 +163,7 @@ func TestEDSServiceResolutionUpdate(t *testing.T) {
 	defer tearDown()
 
 	// add a eds type of cluster with static end points.
-	addEdsClusterWithStaticEndpoints(server)
+	addEdsCluster(server, "edsdns.svc.cluster.local", "http", "10.0.0.53", 8080)
 
 	adscConn := adsConnectAndWait(t, 0x0a0a0a0a)
 	defer adscConn.Close()
@@ -180,6 +180,89 @@ func TestEDSServiceResolutionUpdate(t *testing.T) {
 	lbe := adscConn.GetEndpoints()["outbound|8080||edsdns.svc.cluster.local"]
 	if lbe != nil && len(lbe.Endpoints) > 0 {
 		t.Fatalf("endpoints not expected for  %s,  but got %v", "edsdns.svc.cluster.local", adscConn.EndpointsJSON())
+	}
+}
+
+// Validate that when endpoints of a service flipflop between 1 and 0 does not trigger a full push.
+func TestEndpointFlipFlops(t *testing.T) {
+
+	server, tearDown := initLocalPilotTestEnv(t)
+	defer tearDown()
+
+	// add a eds type of cluster with static end points.
+	addEdsCluster(server, "flipflop.com", "http", "10.0.0.53", 8080)
+
+	adscConn := adsConnectAndWait(t, 0x0a0a0a0a)
+	defer adscConn.Close()
+
+	// Validate that endpoints are pushed correctly.
+	testEndpoints("10.0.0.53", "outbound|8080||flipflop.com", adscConn, t)
+
+	// Clear the endpoint and validate it does not trigger a full push.
+	server.EnvoyXdsServer.MemRegistry.SetEndpoints("flipflop.com", "", []*model.IstioEndpoint{})
+
+	upd, _ := adscConn.Wait(5*time.Second, "cds")
+
+	if contains(upd, "cds") {
+		t.Fatalf("Expecting only EDS update as part of a partial push. But received CDS also %v", upd)
+	}
+
+	if len(upd) > 0 && !contains(upd, "eds") {
+		t.Fatalf("Expecting EDS push as part of a partial push. But did not receive %v", upd)
+	}
+
+	lbe := adscConn.GetEndpoints()["outbound|8080||flipflop.com"]
+	if len(lbe.Endpoints) != 0 {
+		t.Fatalf("There should be no endpoints for outbound|8080||flipflop.com. Endpoints:\n%v", adscConn.EndpointsJSON())
+	}
+
+	// Validate that keys in service still exist in EndpointShardsByService - this prevents full push.
+	if len(server.EnvoyXdsServer.EndpointShardsByService["flipflop.com"]) == 0 {
+		t.Fatalf("Expected service key %s to be present in EndpointShardsByService. But missing %v", "flipflop.com", server.EnvoyXdsServer.EndpointShardsByService)
+	}
+
+	// Set the endpoints again and validate it does not trigger full push.
+	server.EnvoyXdsServer.MemRegistry.SetEndpoints("flipflop.com", "",
+		[]*model.IstioEndpoint{
+			{
+				Address:         "10.10.1.1",
+				ServicePortName: "http",
+				EndpointPort:    8080,
+			}})
+
+	upd, _ = adscConn.Wait(5*time.Second, "cds")
+
+	if contains(upd, "cds") {
+		t.Fatal("Expecting only EDS update as part of a partial push. But received CDS also +v", upd)
+	}
+
+	if len(upd) > 0 && !contains(upd, "eds") {
+		t.Fatal("Expecting EDS push as part of a partial push. But did not receive +v", upd)
+	}
+
+	testEndpoints("10.10.1.1", "outbound|8080||flipflop.com", adscConn, t)
+}
+
+// Validate that deleting a service clears entries from EndpointShardsByService.
+func TestDeleteService(t *testing.T) {
+
+	server, tearDown := initLocalPilotTestEnv(t)
+	defer tearDown()
+
+	// add a eds type of cluster with static end points.
+	addEdsCluster(server, "removeservice.com", "http", "10.0.0.53", 8080)
+
+	adscConn := adsConnectAndWait(t, 0x0a0a0a0a)
+	defer adscConn.Close()
+
+	// Validate that endpoints are pushed correctly.
+	testEndpoints("10.0.0.53", "outbound|8080||removeservice.com", adscConn, t)
+
+	server.EnvoyXdsServer.MemRegistry.RemoveService("removeservice.com")
+
+	if len(server.EnvoyXdsServer.EndpointShardsByService["removeservice.com"]) != 0 {
+		t.Fatalf("Expected service key %s to be deleted in EndpointShardsByService. But is still there %v",
+			"removeservice.com", server.EnvoyXdsServer.EndpointShardsByService)
 	}
 }
 
@@ -655,25 +738,25 @@ func addLocalityEndpoints(server *bootstrap.Server, hostname host.Name) {
 	server.EnvoyXdsServer.Push(&model.PushRequest{Full: true})
 }
 
-func addEdsClusterWithStaticEndpoints(server *bootstrap.Server) {
-	server.EnvoyXdsServer.MemRegistry.AddService("edsdns.svc.cluster.local", &model.Service{
-		Hostname: "edsdns.svc.cluster.local",
+func addEdsCluster(server *bootstrap.Server, hostName string, portName string, address string, port int) {
+	server.EnvoyXdsServer.MemRegistry.AddService(host.Name(hostName), &model.Service{
+		Hostname: host.Name(hostName),
 		Ports: model.PortList{
 			{
-				Name:     "http",
-				Port:     8080,
+				Name:     portName,
+				Port:     port,
 				Protocol: protocol.HTTP,
 			},
 		},
 	})
 
-	server.EnvoyXdsServer.MemRegistry.AddInstance("edsdns.svc.cluster.local", &model.ServiceInstance{
+	server.EnvoyXdsServer.MemRegistry.AddInstance(host.Name(hostName), &model.ServiceInstance{
 		Endpoint: model.NetworkEndpoint{
-			Address: "10.0.0.53",
-			Port:    8080,
+			Address: address,
+			Port:    port,
 			ServicePort: &model.Port{
-				Name:     "http",
-				Port:     8080,
+				Name:     portName,
+				Port:     port,
 				Protocol: protocol.HTTP,
 			},
 		},
@@ -758,4 +841,13 @@ func testEdsz(t *testing.T) {
 	if !strings.Contains(statusStr, "\"outbound|8080||eds.test.svc.cluster.local\"") {
 		t.Fatal("Mock eds service not found ", statusStr)
 	}
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
