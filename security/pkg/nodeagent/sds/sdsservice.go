3bb1c4569b43e69dd9c60c48587891f7ee8a9519
Costin Manolache <costin@gmail.com>
2019-11-07 17:37:51 -0800

    Istiod agent (#18314)
    * Incremental changes:
    
    - remove the SDS startup in pilot - the feedback is to start with SDS in
    agent, pilot will only sign certs.
    - cleanup a bit the startup
    - use port 8080 for http - istioctl expects it.
    - add the original Dockerfile - while it is getting integrated into the
    istio build system ( can be useful after as well, it's very easy)
    
    * Add a cloudbuilder for istiod, add missing files
    
    * More cleanup, remove controversial dockerfile
    
    * Use upstream galley server, reuse kube
    
    * Make format
    
    * Fix lint
    
    * Add missing trust domain
    
    * Post merge fixes
    
    * Lint
    
    * Fix nil check for interface
    
    * Lint
    
    * Initial SDS agent and support
    
    * Improved startup config, use injection
    
    * Tested SDS with Istiod and google plugin
    
    * Format and lint
    
    * More lint fighting, cleanup
    
    * Change the template to use K8S-signed cert with local SDS
    
    * More fixes, better debug
    
    * Lint and fixes
    
    * More lint
    
    * Simpler/safer code. Istiod will not support citadel agent
    
    * Restore previous code to disable sds
    
    * Another attempt to format code and fight linter
    
    * Indent and fix sds default
    
    * Fix another crazy startup issue.
    
    The watcher is not only watching - but also kicks the first start.
    This code needs to be mostly replaced once SDS and istiod are enabled.
    
    Also add a simple way to patch galley options for fine tunning. Like
    values.yaml subset needed by injection, it's part of the transition to
    mesh.yaml
    
    * Add explicit option to use local JWT
    
    * format
    
    * Review feedback, renames and more comments
    
    * Test failure
    
    * Signature change in security
    
    * Fixes for crash and canary mode
    
    * Remove dep on bootstrap

--- security/pkg/nodeagent/sds/sdsservice.go
+++ security/pkg/nodeagent/sds/sdsservice.go
@@ -20,6 +20,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"strconv"
 	"sync"
 	"sync/atomic"
@@ -115,7 +116,8 @@ type sdsservice struct {
 	tickerInterval time.Duration
 
 	// close channel.
-	closing chan bool
+	closing  chan bool
+	localJWT bool
 }
 
 // ClientDebug represents a single SDS connection to the ndoe agent
@@ -137,7 +139,7 @@ type Debug struct {
 }
 
 // newSDSService creates Secret Discovery Service which implements envoy v2 SDS API.
-func newSDSService(st cache.SecretManager, skipTokenVerification bool, recycleInterval time.Duration) *sdsservice {
+func newSDSService(st cache.SecretManager, skipTokenVerification, localJWT bool, recycleInterval time.Duration) *sdsservice {
 	if st == nil {
 		return nil
 	}
@@ -147,6 +149,7 @@ func newSDSService(st cache.SecretManager, skipTokenVerification bool, recycleIn
 		skipToken:      skipTokenVerification,
 		tickerInterval: recycleInterval,
 		closing:        make(chan bool),
+		localJWT:       localJWT,
 	}
 
 	go ret.clearStaledClientsJob()
@@ -202,7 +205,7 @@ func (s *sdsservice) DeltaSecrets(stream sds.SecretDiscoveryService_DeltaSecrets
 // StreamSecrets serves SDS discovery requests and SDS push requests
 func (s *sdsservice) StreamSecrets(stream sds.SecretDiscoveryService_StreamSecretsServer) error {
 	token := ""
-	var ctx context.Context
+	ctx := context.Background()
 
 	var receiveError error
 	reqChannel := make(chan *xdsapi.DiscoveryRequest, 1)
@@ -260,7 +263,15 @@ func (s *sdsservice) StreamSecrets(stream sds.SecretDiscoveryService_StreamSecre
 			defer recycleConnection(conID, resourceName)
 
 			conIDresourceNamePrefix := sdsLogPrefix(conID, resourceName)
-			if !s.skipToken {
+			if s.localJWT {
+				// Running in-process, no need to pass the token from envoy to agent as in-context - use the file
+				tok, err := ioutil.ReadFile("./var/run/secrets/tokens/istio-token")
+				if err != nil {
+					sdsServiceLog.Errorf("Failed to get credential token: %v", err)
+					return err
+				}
+				token = string(tok)
+			} else if !s.skipToken {
 				ctx = stream.Context()
 				t, err := getCredentialToken(ctx)
 				if err != nil {
@@ -360,7 +371,15 @@ func (s *sdsservice) StreamSecrets(stream sds.SecretDiscoveryService_StreamSecre
 // FetchSecrets generates and returns secret from SecretManager in response to DiscoveryRequest
 func (s *sdsservice) FetchSecrets(ctx context.Context, discReq *xdsapi.DiscoveryRequest) (*xdsapi.DiscoveryResponse, error) {
 	token := ""
-	if !s.skipToken {
+	if s.localJWT {
+		// Running in-process, no need to pass the token from envoy to agent as in-context - use the file
+		tok, err := ioutil.ReadFile("./var/run/secrets/tokens/istio-token")
+		if err != nil {
+			sdsServiceLog.Errorf("Failed to get credential token: %v", err)
+			return nil, err
+		}
+		token = string(tok)
+	} else if !s.skipToken {
 		t, err := getCredentialToken(ctx)
 		if err != nil {
 			sdsServiceLog.Errorf("Failed to get credential token: %v", err)
