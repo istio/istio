457907f2212531e3c5cca236a09d0d2c3e863ad1
John Howard <howardjohn@google.com>
2019-11-10 20:49:43 -0800

    Remove misleading push error metric
    Fixes https://github.com/istio/istio/issues/18842

--- pilot/pkg/proxy/envoy/v2/monitoring.go
+++ pilot/pkg/proxy/envoy/v2/monitoring.go
@@ -158,9 +158,10 @@ var (
 
 func recordSendError(metric monitoring.Metric, err error) {
 	s, ok := status.FromError(err)
-	// Unavailable code will be sent when a connection is closing down. This is very normal,
+	// Unavailable or canceled code will be sent when a connection is closing down. This is very normal,
 	// due to the XDS connection being dropped every 30 minutes, or a pod shutting down.
-	if !ok || s.Code() != codes.Unavailable {
+	isError := s.Code() != codes.Unavailable && s.Code() != codes.Canceled
+	if !ok || isError {
 		metric.Increment()
 	}
 }
