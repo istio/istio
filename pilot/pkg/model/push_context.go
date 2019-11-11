74dd1ebc6f082d50a5ca5c9569517ed73c145429
Costin Manolache <costin@gmail.com>
2019-11-11 13:34:14 -0800

    Revert hacky fix

--- pilot/pkg/model/push_context.go
+++ pilot/pkg/model/push_context.go
@@ -796,10 +796,6 @@ func (ps *PushContext) updateContext(
 			authzChanged = true
 		case schemas.AuthenticationPolicy.Type, schemas.AuthenticationMeshPolicy.Type:
 			authnChanged = true
-		case "":
-			// Special case(s): synthetic events can update more than one object. Currently ingress.
-			virtualServicesChanged = true
-			gatewayChanged = true
 		}
 	}
 
