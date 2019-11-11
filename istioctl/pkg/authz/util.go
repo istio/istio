2bd0587afb0956bbb9f0b110f0ecc6c448d651ed
Yangmin Zhu <ymzhu@google.com>
2019-11-11 13:42:54 -0800

    Update authz convert to support converting policies applied in the current cluster (#18678)
    * update authz convert tool to support the internal authzPolicy from Pilot
    
    * address comments
    
    * fix lint
    
    * fix build
    
    * fix name

--- istioctl/pkg/authz/util.go
+++ istioctl/pkg/authz/util.go
@@ -15,11 +15,8 @@
 package authz
 
 import (
-	"fmt"
-	"io/ioutil"
 	"strings"
 
-	"istio.io/istio/pilot/pkg/config/kube/crd"
 	"istio.io/istio/pilot/pkg/model"
 
 	envoy_auth "github.com/envoyproxy/go-control-plane/envoy/api/v2/auth"
@@ -27,26 +24,6 @@ import (
 
 // PolicyTypeToConfigs maps policy type (e.g. service-role) to a list of its config.
 type PolicyTypeToConfigs map[string][]model.Config
-
-// getConfigsFromFiles returns a list of model.Configs from the given files.
-func getConfigsFromFiles(fileNames []string) (PolicyTypeToConfigs, error) {
-	policyTypeToConfigs := make(PolicyTypeToConfigs)
-	for _, fileName := range fileNames {
-		fileBuf, err := ioutil.ReadFile(fileName)
-		if err != nil {
-			return nil, fmt.Errorf("failed to read file %s", fileName)
-		}
-		configsFromFile, _, err := crd.ParseInputs(string(fileBuf))
-		if err != nil {
-			return nil, err
-		}
-		for _, config := range configsFromFile {
-			configType := config.Type
-			policyTypeToConfigs[configType] = append(policyTypeToConfigs[configType], config)
-		}
-	}
-	return policyTypeToConfigs, nil
-}
 
 func getCertificate(ctx *envoy_auth.CommonTlsContext) string {
 	cert := "none"
@@ -79,22 +56,4 @@ func getCertificate(ctx *envoy_auth.CommonTlsContext) string {
 		cert += "SDS: " + strings.Join(sdsConfigs, ",")
 	}
 	return cert
-}
-
-func getValidate(ctx *envoy_auth.CommonTlsContext) string {
-	var ret string
-	switch v := ctx.ValidationContextType.(type) {
-	case *envoy_auth.CommonTlsContext_ValidationContext:
-		ret = strings.Join(v.ValidationContext.VerifySubjectAltName, ",")
-	case *envoy_auth.CommonTlsContext_ValidationContextSdsSecretConfig:
-		ret = fmt.Sprintf("SDS: %s", v.ValidationContextSdsSecretConfig.Name)
-	case *envoy_auth.CommonTlsContext_CombinedValidationContext:
-		san := strings.Join(v.CombinedValidationContext.GetDefaultValidationContext().GetVerifySubjectAltName(), ",")
-		sds := fmt.Sprintf("SDS: %s", v.CombinedValidationContext.GetValidationContextSdsSecretConfig().GetName())
-		ret = fmt.Sprintf("[%s] + [%s]", san, sds)
-	}
-	if ret == "" {
-		return "none"
-	}
-	return ret
 }
