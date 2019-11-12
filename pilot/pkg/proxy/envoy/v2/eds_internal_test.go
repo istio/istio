e2ccc37ba048fb76543b07b63d6dc4438aeb7012
xuzhonghu <xuzhonghu@huawei.com>
2019-11-12 15:53:01 +0800

    Add test case

---
+++ pilot/pkg/proxy/envoy/v2/eds_internal_test.go
@@ -0,0 +1,107 @@
+// Copyright 2019 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v2
+
+import (
+	"testing"
+	"time"
+
+	networking "istio.io/api/networking/v1alpha3"
+	"istio.io/istio/pilot/pkg/config/memory"
+	"istio.io/istio/pilot/pkg/model"
+	"istio.io/istio/pilot/pkg/networking/core/v1alpha3"
+	"istio.io/istio/pilot/pkg/networking/plugin"
+	"istio.io/istio/pilot/pkg/serviceregistry/aggregate"
+	"istio.io/istio/pilot/pkg/serviceregistry/external"
+	"istio.io/istio/pkg/config/mesh"
+	"istio.io/istio/pkg/config/schemas"
+)
+
+// newDiscoveryServer creates a DiscoveryServer with the provided configs using the mem registry
+func newDiscoveryServer(cfgs ...model.Config) (*DiscoveryServer, model.ConfigStoreCache, error) {
+	m := mesh.DefaultMeshConfig()
+	store := memory.Make(schemas.Istio)
+	configController := memory.NewController(store)
+	istioConfigStore := model.MakeIstioStore(configController)
+	serviceControllers := aggregate.NewController()
+	serviceEntryStore := external.NewServiceDiscovery(configController, istioConfigStore)
+	go configController.Run(make(chan struct{}))
+	serviceEntryRegistry := aggregate.Registry{
+		Name:             "ServiceEntries",
+		Controller:       serviceEntryStore,
+		ServiceDiscovery: serviceEntryStore,
+	}
+	serviceControllers.AddRegistry(serviceEntryRegistry)
+
+	env := &model.Environment{
+		Mesh:             &m,
+		MeshNetworks:     nil,
+		IstioConfigStore: istioConfigStore,
+		ServiceDiscovery: serviceControllers,
+		PushContext:      model.NewPushContext(),
+	}
+	for _, cfg := range cfgs {
+		if _, err := configController.Create(cfg); err != nil {
+			return nil, nil, err
+		}
+	}
+	if err := env.PushContext.InitContext(env, env.PushContext, nil); err != nil {
+		return nil, nil, err
+	}
+	s := NewDiscoveryServer(env, v1alpha3.NewConfigGenerator([]plugin.Plugin{}))
+	return s, configController, nil
+}
+
+func TestEndpointShardsMemoryLeak(t *testing.T) {
+	configs := createEndpoints(2, 5)
+	server, configStore, err := newDiscoveryServer(configs...)
+	if err != nil {
+		t.Errorf("Failed creating discovery server: %v", err)
+		return
+	}
+
+	if err := server.updateServiceShards(server.globalPushContext()); err != nil {
+		t.Errorf("Failed updateServiceShards: %v", err)
+	}
+
+	for _, se := range configs {
+		endpointShardsByNamespace, exist := server.EndpointShardsByService[se.Spec.(*networking.ServiceEntry).Hosts[0]]
+		if !exist {
+			t.Errorf("Service %s not exist", se.Spec.(*networking.ServiceEntry).Hosts[0])
+		}
+		endpointShards, exist := endpointShardsByNamespace[se.Namespace]
+		if !exist {
+			t.Errorf("Service %s not exist", se.Spec.(*networking.ServiceEntry).Hosts[0])
+		}
+		endpointShards.mutex.RLock()
+		epNum := len(endpointShards.Shards[""])
+		endpointShards.mutex.RUnlock()
+		if epNum != 2 {
+			t.Errorf("Service %s endpoint does not match", se.Spec.(*networking.ServiceEntry).Hosts[0])
+		}
+
+		configStore.Delete(se.Type, se.Name, se.Namespace)
+
+		// TODO: figure out a way to notify
+		time.Sleep(100 * time.Millisecond)
+
+		_, exist = server.EndpointShardsByService[se.Spec.(*networking.ServiceEntry).Hosts[0]]
+		if exist {
+			t.Errorf("EndpointShards of service %s should be deleted", se.Spec.(*networking.ServiceEntry).Hosts[0])
+		}
+
+	}
+
+}
