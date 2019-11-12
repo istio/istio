7f7dd8feed2958f64ff10ef9a33c734206638b1e
xuzhonghu <xuzhonghu@huawei.com>
2019-11-12 16:33:00 +0800

    revert istiod

--- pkg/istiod/pilotstart.go
+++ pkg/istiod/pilotstart.go
@@ -674,8 +674,8 @@ func (s *Server) initGrpcServer(options *istiokeepalive.Options) {
 // initEventHandlers sets up event handlers for config and service updates
 func (s *Server) initEventHandlers() error {
 	// Flush cached discovery responses whenever services configuration change.
-	serviceHandler := func(clusterID string, svc *model.Service, event model.Event) {
-		s.EnvoyXdsServer.SvcUpdate("", string(svc.Hostname), svc.Attributes.Namespace, event)
+	serviceHandler := func(svc *model.Service, event model.Event) {
+		s.EnvoyXdsServer.SvcUpdate(svc.Attributes.ClusterID, string(svc.Hostname), svc.Attributes.Namespace, event)
 		pushReq := &model.PushRequest{
 			Full:               true,
 			NamespacesUpdated:  map[string]struct{}{svc.Attributes.Namespace: {}},
