ef2ff350a05747b5175255586a65105d1a9c2b5f
John Howard <howardjohn@google.com>
2019-11-15 15:26:00 -0800

    Update common files (#555)

--- common/Makefile.common.mk
+++ common/Makefile.common.mk
@@ -104,7 +104,7 @@ update-common-protos:
 	@git clone -q --depth 1 --single-branch --branch $(UPDATE_BRANCH) https://github.com/istio/common-files $(TMP)/common-files
 	@cd $(TMP)/common-files ; git rev-parse HEAD > common-protos/.commonfiles.sha
 	@rm -fr common-protos
-	@cp -ar $(TMP)/common-files/common-protos/* $(shell pwd)/common-protos
+	@cp -ar $(TMP)/common-files/common-protos $(shell pwd)
 	@rm -fr $(TMP)/common-files
 
 check-clean-repo:
