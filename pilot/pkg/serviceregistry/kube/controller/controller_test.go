0f7dcaf6be17f57bdc965f2ddac448878f3112a4
Rama Chavali <rama.rao@salesforce.com>
2019-11-18 19:43:12 +0530

    remove unnecessary comment
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/serviceregistry/kube/controller/controller_test.go
+++ pilot/pkg/serviceregistry/kube/controller/controller_test.go
@@ -325,7 +325,7 @@ func makeService(n, ns string, cl kubernetes.Interface, t *testing.T) {
 }
 
 func TestController_GetPodLocality(t *testing.T) {
-	//	t.Parallel()
+	t.Parallel()
 	pod1 := generatePod("128.0.1.1", "pod1", "nsA", "", "node1", map[string]string{"app": "prod-app"}, map[string]string{})
 	pod2 := generatePod("128.0.1.2", "pod2", "nsB", "", "node2", map[string]string{"app": "prod-app"}, map[string]string{})
 	podOverride := generatePod("128.0.1.2", "pod2", "nsB", "",
