5ab8f4990709919bb513a1369bfdd9bfe1b4cff9
Joe Selman <jsselman@google.com>
2019-11-19 14:16:29 -0800

    Build a docker image for istioctl
    This makes it easier for users wanting to use `istioctl analyze` as part of their CI system.

--- tools/istio-docker.mk
+++ tools/istio-docker.mk
@@ -23,8 +23,8 @@ docker: build-linux docker.all
 
 # Add new docker targets to the end of the DOCKER_TARGETS list.
 DOCKER_TARGETS:=docker.pilot docker.istiod docker.proxytproxy docker.proxyv2 docker.app docker.app_sidecar docker.test_policybackend \
-	docker.mixer docker.mixer_codegen docker.citadel docker.galley docker.sidecar_injector docker.kubectl docker.node-agent-k8s
-
+	docker.mixer docker.mixer_codegen docker.citadel docker.galley docker.sidecar_injector docker.kubectl docker.node-agent-k8s \
+	docker.istioctl
 $(ISTIO_DOCKER) $(ISTIO_DOCKER_TAR):
 	mkdir -p $@
 
@@ -49,7 +49,7 @@ $(ISTIO_DOCKER)/node_agent.crt $(ISTIO_DOCKER)/node_agent.key: ${GEN_CERT} $(IST
 # 	cp $(ISTIO_OUT_LINUX)/$FILE $(ISTIO_DOCKER)/($FILE)
 DOCKER_FILES_FROM_ISTIO_OUT_LINUX:=client server \
                              pilot-discovery pilot-agent sidecar-injector mixs mixgen \
-                             istio_ca node_agent node_agent_k8s galley istio-iptables istio-clean-iptables
+                             istio_ca node_agent node_agent_k8s galley istio-iptables istio-clean-iptables istioctl
 $(foreach FILE,$(DOCKER_FILES_FROM_ISTIO_OUT_LINUX), \
         $(eval $(ISTIO_DOCKER)/$(FILE): $(ISTIO_OUT_LINUX)/$(FILE) | $(ISTIO_DOCKER); cp $(ISTIO_OUT_LINUX)/$(FILE) $(ISTIO_DOCKER)/$(FILE)))
 
@@ -176,6 +176,11 @@ docker.test_policybackend: $(ISTIO_OUT_LINUX)/policybackend
 
 docker.kubectl: BUILD_ARGS=--build-arg BASE_VERSION=${BASE_VERSION}
 docker.kubectl: docker/Dockerfile$$(suffix $$@)
+	$(DOCKER_RULE)
+
+docker.istioctl: BUILD_ARGS=--build-arg BASE_VERSION=${BASE_VERSION}
+docker.istioctl: istioctl/docker/Dockerfile.istioctl
+docker.istioctl: $(ISTIO_OUT_LINUX)/istioctl
 	$(DOCKER_RULE)
 
 # mixer docker images
