a9f73709361cc170ffcbc59583470651bcae9ddb
Chris Wilson <crwilson@google.com>
2019-11-20 21:39:34 +0000

    Use documentation_url as field name

--- galley/pkg/config/analysis/diag/message.go
+++ galley/pkg/config/analysis/diag/message.go
@@ -65,7 +65,7 @@ func (m *Message) Unstructured(includeOrigin bool) map[string]interface{} {
 		result["origin"] = m.Origin.FriendlyName()
 	}
 	result["message"] = fmt.Sprintf(m.Type.Template(), m.Parameters...)
-	result["doc"] = fmt.Sprintf("%s/%s", DocPrefix, m.Type.Code())
+	result["documentation_url"] = fmt.Sprintf("%s/%s", DocPrefix, m.Type.Code())
 
 	return result
 }
