631145d0d8141430444f47ff735f276f01b1a6fc
ckcd <curtis@mail.ustc.edu.cn>
2019-11-21 17:22:41 +0800

    support outlierDetection logging

--- pkg/envoy/proxy.go
+++ pkg/envoy/proxy.go
@@ -59,6 +59,7 @@ type ProxyConfig struct {
 	SDSTokenPath        string
 	ControlPlaneAuth    bool
 	DisableReportCalls  bool
+	OutlierLogPath      string
 }
 
 // NewProxy creates an instance of the proxy control commands
@@ -164,6 +165,7 @@ func (e *envoy) Run(config interface{}, epoch int, abort <-chan error) error {
 			SDSTokenPath:        e.SDSTokenPath,
 			ControlPlaneAuth:    e.ControlPlaneAuth,
 			DisableReportCalls:  e.DisableReportCalls,
+			OutlierLogPath:      e.OutlierLogPath,
 		}).CreateFileForEpoch(epoch)
 		if err != nil {
 			log.Errora("Failed to generate bootstrap config: ", err)
