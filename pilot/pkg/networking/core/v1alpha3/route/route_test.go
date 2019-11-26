aab8397f6ce51b45b9c8f7c84a6718a170589d67
Rama Chavali <rama.rao@salesforce.com>
2019-11-26 09:19:42 +0530

    remove safe regex validation and default program size
    Signed-off-by: Julian Nodorp <jnodorp@salesforce.com>

--- pilot/pkg/networking/core/v1alpha3/route/route_test.go
+++ pilot/pkg/networking/core/v1alpha3/route/route_test.go
@@ -76,6 +76,8 @@ func TestBuildHTTPRoutes(t *testing.T) {
 		g.Expect(err).NotTo(gomega.HaveOccurred())
 		g.Expect(len(routes)).To(gomega.Equal(1))
 		g.Expect(routes[0].GetMatch().GetSafeRegex().GetRegex()).To(gomega.Equal("\\/(.?)\\/status"))
+		g.Expect(routes[0].GetMatch().GetSafeRegex().GetGoogleRe2().GetMaxProgramSize().GetValue()).To(gomega.Equal(uint32(1024)))
+
 	})
 
 	t.Run("for virtual service with unsafe regex matching on URI", func(t *testing.T) {
