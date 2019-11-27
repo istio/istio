cc2b6e2653bc56bf6a1ddcc7672252f41515027f
Kuat Yessenov <kuat@google.com>
2019-11-27 11:48:19 -0800

    udpa: add typed struct
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- pilot/pkg/proxy/envoy/v2/discovery.go
+++ pilot/pkg/proxy/envoy/v2/discovery.go
@@ -19,6 +19,7 @@ import (
 	"sync"
 	"time"
 
+	udpa "github.com/cncf/udpa/go/udpa/type/v1"
 	ads "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v2"
 	"github.com/google/uuid"
 	"go.uber.org/atomic"
@@ -50,6 +51,8 @@ var (
 	// while debouncing. Defaults to 10 seconds. If events keep
 	// showing up with no break for this time, we'll trigger a push.
 	DebounceMax time.Duration
+
+	_ = udpa.TypedStruct{}
 )
 
 const (
