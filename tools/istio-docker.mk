349e21741f172253dacaff72de767eee7d1b6bd8
Costin Manolache <costin@gmail.com>
2019-12-03 09:40:42 -0800

    Revert future work

--- tools/istio-docker.mk
+++ tools/istio-docker.mk
@@ -126,9 +126,6 @@ docker.pilot: BUILD_PRE=; chmod 755 pilot-discovery cacert.pem
 docker.pilot: BUILD_ARGS=--build-arg BASE_VERSION=${BASE_VERSION}
 docker.pilot: $(ISTIO_OUT_LINUX)/pilot-discovery
 docker.pilot: tests/testdata/certs/cacert.pem
-docker.pilot: sidecar-injector/inject/config
-docker.pilot: sidecar-injector/inject/values
-docker.pilot: pilot/docker/mesh
 docker.pilot: pilot/docker/Dockerfile.pilot
 	$(DOCKER_RULE)
 
