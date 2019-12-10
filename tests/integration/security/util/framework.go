553ed130c14488aca6bf2454918d272fb1fefa53
xuzhonghu <xuzhonghu@huawei.com>
2019-12-10 17:15:29 +0800

    enable headless service, and make sure the port and targetport are equal

--- tests/integration/security/util/framework.go
+++ tests/integration/security/util/framework.go
@@ -23,7 +23,7 @@ import (
 )
 
 func EchoConfig(name string, ns namespace.Instance, headless bool, annos echo.Annotations, g galley.Instance, p pilot.Instance) echo.Config {
-	return echo.Config{
+	out := echo.Config{
 		Service:        name,
 		Namespace:      ns,
 		ServiceAccount: true,
@@ -48,4 +48,10 @@ func EchoConfig(name string, ns namespace.Instance, headless bool, annos echo.An
 		Galley: g,
 		Pilot:  p,
 	}
+
+	// for headless service, the port and target port must be equal
+	if headless {
+		out.Ports[0].ServicePort = 8090
+	}
+	return out
 }
