b3a0826663976e01edb96b877e4411d109cd7e11
John Howard <howardjohn@google.com>
2019-12-17 14:47:26 -0800

    Update dependencies

--- prow/release-commit.sh
+++ prow/release-commit.sh
@@ -32,7 +32,7 @@ DOCKER_HUB=${DOCKER_HUB:-gcr.io/istio-testing}
 GCS_BUCKET=${GCS_BUCKET:-istio-build/dev}
 
 # Use a pinned version in case breaking changes are needed
-BUILDER_SHA=1e62ccee94b4080dd334f8ee4e412f3d6e77b787
+BUILDER_SHA=230d197b96bb0166c65636b9b389e759dd506d9c
 
 # Reference to the next minor version of Istio
 # This will create a version like 1.4-alpha.sha
