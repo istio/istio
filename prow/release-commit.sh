87a6376046dd38b09df1330d02cc6a6725dbf408
Steven Dake <sdake@ibm.com>
2019-12-19 09:31:44 -0700

    Update release builder version

--- prow/release-commit.sh
+++ prow/release-commit.sh
@@ -32,7 +32,7 @@ DOCKER_HUB=${DOCKER_HUB:-gcr.io/istio-testing}
 GCS_BUCKET=${GCS_BUCKET:-istio-build/dev}
 
 # Use a pinned version in case breaking changes are needed
-BUILDER_SHA=1e62ccee94b4080dd334f8ee4e412f3d6e77b787
+BUILDER_SHA=56f21736b7956ef451a98516f8f6919b4b3b6a49
 
 # Reference to the next minor version of Istio
 # This will create a version like 1.4-alpha.sha
