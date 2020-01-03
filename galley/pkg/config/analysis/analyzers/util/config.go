1954191dd58de0fe7e391d0767fc5146e1c8f5d4
Chris Wilson <crwilson@google.com>
2020-01-03 18:48:16 +0000

    Update analyzer IsIstioControlPlane filter to also look for release=istio

--- galley/pkg/config/analysis/analyzers/util/config.go
+++ galley/pkg/config/analysis/analyzers/util/config.go
@@ -36,11 +36,18 @@ func MeshConfig(ctx analysis.Context) *v1alpha1.MeshConfig {
 	return mc
 }
 
+// IsSystemNamespace returns true for system namespaces
 func IsSystemNamespace(ns resource.Namespace) bool {
 	return ns == "kube-system" || ns == "kube-public"
 }
 
+// IsIstioControlPlane returns true for resources that are part of the Istio control plane
 func IsIstioControlPlane(r *resource.Instance) bool {
-	_, ok := r.Metadata.Labels["istio"]
-	return ok
+	if _, ok := r.Metadata.Labels["istio"]; ok {
+		return true
+	}
+	if r.Metadata.Labels["release"] == "istio" {
+		return true
+	}
+	return false
 }
