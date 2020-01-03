984594f2984dbb1573c3c48a38a4b722d8b7d750
Pengyuan Bian <bianpengyuan@google.com>
2020-01-03 10:07:18 -0800

    only retry deadline exceed and unavailable.

--- mixer/adapter/stackdriver/metric/bufferedClient.go
+++ mixer/adapter/stackdriver/metric/bufferedClient.go
@@ -16,6 +16,9 @@ package metric
 
 import (
 	"context"
+	"encoding/binary"
+	"fmt"
+	"hash/fnv"
 	"io"
 	"strings"
 	"sync"
@@ -23,6 +26,7 @@ import (
 
 	monitoringpb "google.golang.org/genproto/googleapis/monitoring/v3"
 
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
 	"istio.io/istio/mixer/pkg/adapter"
@@ -52,7 +56,14 @@ type buffered struct {
 	mergeTrigger int
 	mergedTS     map[uint64]*monitoringpb.TimeSeries
 
+	// retryBuffer holds timeseries that fail in push.
+	retryBuffer []*monitoringpb.TimeSeries
+	// retryCounter maps a timeseries to the attempts that it has been retried.
+	// Key is the hash of timeseries' metric name, monitored resource and start time.
+	retryCounter map[uint64]int
+
 	timeSeriesBatchSize int
+	retryLimit          int
 	pushInterval        time.Duration
 
 	env adapter.Env
@@ -122,7 +133,7 @@ func (b *buffered) mergeTimeSeries() {
 
 func (b *buffered) Send() {
 	b.tsm.Lock()
-	if len(b.mergedTS) == 0 {
+	if len(b.mergedTS) == 0 && len(b.retryBuffer) == 0 {
 		b.tsm.Unlock()
 		b.l.Debugf("No data to send to Stackdriver.")
 		return
@@ -136,7 +147,9 @@ func (b *buffered) Send() {
 	for _, v := range tmpBuffer {
 		toSend = append(toSend, v)
 	}
-	batches := batchTimeSeries(toSend, b.timeSeriesBatchSize)
+	merged := append(b.retryBuffer, toSend...)
+	b.retryBuffer = make([]*monitoringpb.TimeSeries, 0, len(b.retryBuffer))
+	batches := batchTimeSeries(merged, b.timeSeriesBatchSize)
 	// Spread monitoring API calls evenly over the push interval.
 	ns := b.pushInterval.Nanoseconds() / int64(len(batches))
 	t := time.NewTicker(time.Duration(ns))
@@ -152,6 +165,9 @@ func (b *buffered) Send() {
 		// We need to build framework level support for these kinds of async tasks. Perhaps a generic batching adapter
 		// can handle some of this complexity?
 		if err != nil {
+			if isRetryable(status.Code(err)) {
+				b.updateRetryBuffer(timeSeries)
+			}
 			b.l.Errorf("%d time series was sent and Stackdriver returned: %v\n", len(timeSeries), err) // nolint: errcheck
 			if isOutOfOrderError(err) {
 				b.l.Debugf("Given data: %v", timeSeries)
@@ -175,6 +191,36 @@ func (b *buffered) Close() error {
 	return b.closeMe.Close()
 }
 
+func (b *buffered) updateRetryBuffer(errorTS []*monitoringpb.TimeSeries) {
+	retryCounter := map[uint64]int{}
+	for _, ts := range errorTS {
+		k, err := toRetryKey(ts)
+		if err != nil {
+			b.l.Debugf("cannot generate retry key for timeseries %v: %v", ts, err)
+			continue
+		}
+		if c, ok := b.retryCounter[k]; ok {
+			attempt := c + 1
+			if attempt >= b.retryLimit {
+				continue
+			}
+			retryCounter[k] = attempt
+		} else {
+			retryCounter[k] = 0
+		}
+		b.retryBuffer = append(b.retryBuffer, ts)
+	}
+	b.retryCounter = retryCounter
+}
+
+func isRetryable(c codes.Code) bool {
+	switch c {
+	case codes.DeadlineExceeded, codes.Unavailable:
+		return true
+	}
+	return false
+}
+
 func isOutOfOrderError(err error) bool {
 	if s, ok := status.FromError(err); ok {
 		if strings.Contains(strings.ToLower(s.Message()), "order") {
@@ -182,4 +228,21 @@ func isOutOfOrderError(err error) bool {
 		}
 	}
 	return false
+}
+
+func toRetryKey(ts *monitoringpb.TimeSeries) (uint64, error) {
+	hash := fnv.New64()
+	if len(ts.Points) != 1 {
+		return 0, fmt.Errorf("timeseries has to contain exactly one point")
+	}
+
+	buf := make([]byte, 8)
+	mKey := toKey(ts.Metric, ts.Resource)
+	binary.BigEndian.PutUint64(buf, mKey)
+	_, _ = hash.Write(buf)
+
+	s := ts.Points[0].Interval.StartTime.GetSeconds()
+	binary.BigEndian.PutUint64(buf, uint64(s))
+	_, _ = hash.Write(buf)
+	return hash.Sum64(), nil
 }
--- mixer/adapter/stackdriver/metric/bufferedClient_test.go
+++ mixer/adapter/stackdriver/metric/bufferedClient_test.go
@@ -21,9 +21,13 @@ import (
 	"testing"
 	"time"
 
+	"github.com/gogo/protobuf/proto"
 	gax "github.com/googleapis/gax-go/v2"
 	xcontext "golang.org/x/net/context"
 	monitoring "google.golang.org/genproto/googleapis/monitoring/v3"
+	grpcstatus "google.golang.org/genproto/googleapis/rpc/status"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"istio.io/istio/mixer/pkg/adapter/test"
 )
@@ -47,8 +51,10 @@ func TestBuffered_Send(t *testing.T) {
 	b := buffered{
 		l:                   env,
 		timeSeriesBatchSize: 100,
+		retryLimit:          1,
 		mergeTrigger:        1000,
 		buffer:              []*monitoring.TimeSeries{},
+		retryBuffer:         []*monitoring.TimeSeries{},
 		pushInterval:        100 * time.Millisecond,
 		mergedTS:            make(map[uint64]*monitoring.TimeSeries),
 	}
@@ -93,8 +99,10 @@ func TestBuffered_Send(t *testing.T) {
 				l:                   env,
 				pushMetrics:         tt.fn,
 				timeSeriesBatchSize: 100,
+				retryLimit:          1,
 				mergeTrigger:        1000,
 				buffer:              []*monitoring.TimeSeries{},
+				retryBuffer:         []*monitoring.TimeSeries{},
 				pushInterval:        100 * time.Millisecond,
 				mergedTS:            make(map[uint64]*monitoring.TimeSeries),
 			}
@@ -163,6 +171,7 @@ func TestBuffered_SpreadPush(t *testing.T) {
 		l:                   l,
 		pushMetrics:         pushFunc,
 		timeSeriesBatchSize: 1,
+		retryBuffer:         []*monitoring.TimeSeries{},
 		mergeTrigger:        1000,
 		pushInterval:        1 * time.Second,
 		mergedTS:            make(map[uint64]*monitoring.TimeSeries),
@@ -198,5 +207,174 @@ func TestBuffered_Close(t *testing.T) {
 	}
 	if !closeMe.closed {
 		t.Fatalf("buffered.Close() did not call Close() on buffered.closeMe.")
+	}
+}
+
+func createRetryPushFn(pushCount *int, expReqTS [][]*monitoring.TimeSeries, withError []bool, failedTS [][]*monitoring.TimeSeries,
+	errorCode [][]codes.Code, overallCode codes.Code, t *testing.T) pushFunc {
+	retryPushFn := func(ctx xcontext.Context, req *monitoring.CreateTimeSeriesRequest, opts ...gax.CallOption) error {
+		if len(expReqTS) != len(withError) || len(expReqTS) != len(failedTS) || len(expReqTS) <= *pushCount {
+			t.Errorf("args size does not match or potential out of bound. abort push operation %v %v %v %v",
+				len(expReqTS), len(withError), len(failedTS), *pushCount)
+			return nil
+		}
+		defer func(pc *int) {
+			(*pc)++
+		}(pushCount)
+
+		// Verify time series in request match the given ones
+		if len(expReqTS[*pushCount]) != len(req.TimeSeries) {
+			t.Errorf("push %v - number of time series in CreateTimeSeriesRequest is not expected: got %+v want %+v",
+				*pushCount, req.TimeSeries, expReqTS[*pushCount])
+			return nil
+		}
+		for _, ets := range expReqTS[*pushCount] {
+			found := false
+			for _, ts := range req.TimeSeries {
+				if proto.Equal(ts, ets) {
+					found = true
+					break
+				}
+			}
+			if !found {
+				t.Errorf("got %+v, want %+v in push request but cannot find", req.TimeSeries, ets)
+				return nil
+			}
+		}
+
+		// If return with error, check if specific time series should be included in error response
+		if !withError[*pushCount] {
+			return nil
+		}
+
+		return status.ErrorProto(&grpcstatus.Status{
+			Code:    int32(overallCode),
+			Message: "A subset of time series had errors.",
+		})
+	}
+	return retryPushFn
+}
+
+func TestBuffered_Retry(t *testing.T) {
+	in := []*monitoring.TimeSeries{makeTS(m1, mr1, 1, 1), makeTS(m2, mr2, 2, 1), makeTS(m3, mr3, 3, 1)}
+	tests := []struct {
+		name        string
+		pushCount   int
+		requestTS   [][]*monitoring.TimeSeries
+		withError   []bool
+		overallCode codes.Code
+		failedTS    [][]*monitoring.TimeSeries
+	}{
+		{
+			name:        "retry",
+			pushCount:   2,
+			requestTS:   [][]*monitoring.TimeSeries{in, in},
+			withError:   []bool{true, false},
+			overallCode: codes.Unavailable,
+			failedTS:    [][]*monitoring.TimeSeries{{}, {}},
+		},
+		{
+			name:        "do not retry",
+			pushCount:   1,
+			requestTS:   [][]*monitoring.TimeSeries{in},
+			withError:   []bool{true},
+			overallCode: codes.InvalidArgument,
+			failedTS:    [][]*monitoring.TimeSeries{{}},
+		},
+	}
+
+	for idx, tt := range tests {
+		t.Run(fmt.Sprintf("[%d] %s", idx, tt.name), func(t *testing.T) {
+			pushCount := 0
+			env := test.NewEnv(t)
+			b := buffered{
+				l:                   env,
+				pushMetrics:         createRetryPushFn(&pushCount, tt.requestTS, tt.withError, tt.failedTS, nil, tt.overallCode, t),
+				timeSeriesBatchSize: 100,
+				retryLimit:          5,
+				retryBuffer:         []*monitoring.TimeSeries{},
+				retryCounter:        map[uint64]int{},
+				mergeTrigger:        1000,
+				pushInterval:        100 * time.Millisecond,
+				mergedTS:            make(map[uint64]*monitoring.TimeSeries),
+			}
+			b.Record(in)
+			b.mergeTimeSeries()
+			b.Send()
+
+			// call send again without TS in normal buffer. It should retry failed TS.
+			b.Send()
+			if pushCount != tt.pushCount {
+				t.Errorf("push call count is not expected, got %v want %v", pushCount, tt.pushCount)
+			}
+		})
+	}
+}
+
+func TestBuffered_RetryCombine(t *testing.T) {
+	in := []*monitoring.TimeSeries{makeTS(m1, mr1, 1, 1), makeTS(m2, mr2, 1, 1), makeTS(m3, mr3, 1, 1)}
+	in2 := []*monitoring.TimeSeries{makeTS(m1, mr1, 2, 1), makeTS(m2, mr2, 2, 1)}
+	pushCount := 0
+	requestTS := [][]*monitoring.TimeSeries{in, append(in, in2...)}
+	failedTS := [][]*monitoring.TimeSeries{in, {}}
+	// Make first push fail. Retry timeSeries should be combined with newer timeseries.
+	withError := []bool{true, false}
+	env := test.NewEnv(t)
+	b := buffered{
+		l:                   env,
+		pushMetrics:         createRetryPushFn(&pushCount, requestTS, withError, failedTS, nil, codes.Unavailable, t),
+		timeSeriesBatchSize: 100,
+		retryLimit:          5,
+		retryBuffer:         []*monitoring.TimeSeries{},
+		retryCounter:        map[uint64]int{},
+		pushInterval:        100 * time.Millisecond,
+		mergeTrigger:        1000,
+		mergedTS:            make(map[uint64]*monitoring.TimeSeries),
+	}
+	b.Record(in)
+	b.mergeTimeSeries()
+	b.Send()
+
+	// Call record and send again with some newer timeseries. Retry buffer should be combined with normal buffer.
+	b.Record(in2)
+	b.mergeTimeSeries()
+	b.Send()
+	if pushCount != 2 {
+		t.Errorf("push call count is not expected, got %v want 2", pushCount)
+	}
+}
+
+func TestBuffered_RetryMaxAttempt(t *testing.T) {
+	in := []*monitoring.TimeSeries{makeTS(m1, mr1, 1, 1), makeTS(m2, mr2, 1, 1), makeTS(m3, mr3, 1, 1)}
+	pushCount := 0
+	l := test.NewEnv(t).Logger()
+	requestTS := make([][]*monitoring.TimeSeries, 0, 31)
+	failedTS := make([][]*monitoring.TimeSeries, 0, 31)
+	withError := make([]bool, 0, 31)
+	for i := 0; i < 15; i++ {
+		requestTS = append(requestTS, in)
+		failedTS = append(failedTS, []*monitoring.TimeSeries{})
+		withError = append(withError, true)
+	}
+	b := buffered{
+		l:                   l,
+		pushMetrics:         createRetryPushFn(&pushCount, requestTS, withError, failedTS, nil, codes.Unavailable, t),
+		timeSeriesBatchSize: 100,
+		retryLimit:          2,
+		retryBuffer:         []*monitoring.TimeSeries{},
+		retryCounter:        map[uint64]int{},
+		pushInterval:        100 * time.Millisecond,
+		mergeTrigger:        1000,
+		mergedTS:            make(map[uint64]*monitoring.TimeSeries),
+	}
+	b.Record(in)
+	b.mergeTimeSeries()
+	// Call Send() for lots of times. Push should just be called with limited times.
+	// In this case, push was called 3 times.
+	for i := 0; i < 15; i++ {
+		b.Send()
+	}
+	if pushCount != 3 {
+		t.Errorf("push call count is not expected, got %v want 3", pushCount)
 	}
 }
