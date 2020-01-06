5faa037c55c3ec4f3e64855871b80e7f2aeb5a7a
Steven Dake <sdake@ibm.com>
2020-01-06 04:05:15 -0700

    Place istio/istio on BUILD_WITH_CONTAINER=1 plan

--- Makefile.overrides.mk
+++ Makefile.overrides.mk
@@ -14,13 +14,11 @@
 
 .DEFAULT_GOAL := default
 
-# this repo is not yet on the container plan by default.
-#
 # This repository has been enabled for BUILD_WITH_CONTAINER=1. Some
 # test cases fail within Docker, and Mac + Docker isn't quite perfect.
 # For more information see: https://github.com/istio/istio/pull/19322/
 
-BUILD_WITH_CONTAINER ?= 0
+BUILD_WITH_CONTAINER ?= 1
 
 ifeq ($(BUILD_WITH_CONTAINER),1)
 # create phony targets for the top-level items in the repo
