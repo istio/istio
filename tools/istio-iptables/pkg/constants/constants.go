5d5b1093f9bd08d2370f7fbe96cb736742dc02f2
Yuchen Dai <silentdai@gmail.com>
2020-01-10 18:13:29 -0800

    add a iptables failure detector, default off (#19534)
    * cni validation
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    stash
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    add iptables for test
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    update v6 and host addr
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    working get origin dst ipv4 and ipv6
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    refactor server
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    add default validator to iptables command
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    iptables failure detector in preinit
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    replace native byte order detection
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    add tests
    skip validation by default
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * fix rootcmd flag
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * lint
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * go import
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * more lints
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * lintlintlint
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * go.mod go.sum by make gen
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * stash
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * stash2
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * formatter
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * use simulate outbound server
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * ipv4,v6; server and client, both proxyport and inboundport
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * exit code
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * revert debug
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * address comment
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * gofmt -w -s
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * fix double run
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * revert RunOrFail
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * make gen
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * make gen make lint
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * resolve an error where it cannot be error
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * syscall reuseaddr
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * minor fix
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * save 1 second in iptable validation
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    (cherry picked from commit ae817fccf6798e66ed70c4e1ad2ee06a834886d8)

--- tools/istio-iptables/pkg/constants/constants.go
+++ tools/istio-iptables/pkg/constants/constants.go
@@ -18,7 +18,25 @@ package constants
 const (
 	MANGLE = "mangle"
 	NAT    = "nat"
+	FILTER = "filter"
 )
+
+// Built-in iptables chains
+const (
+	INPUT       = "INPUT"
+	OUTPUT      = "OUTPUT"
+	FORWARD     = "FORWARD"
+	PREROUTING  = "PREROUTING"
+	POSTROUTING = "POSTROUTING"
+)
+
+var BuiltInChainsMap = map[string]struct{}{
+	INPUT:       {},
+	OUTPUT:      {},
+	FORWARD:     {},
+	PREROUTING:  {},
+	POSTROUTING: {},
+}
 
 // Constants used for generating iptables commands
 const (
@@ -62,4 +80,43 @@ const (
 	KubeVirtInterfaces        = "kube-virt-interfaces"
 	DryRun                    = "dry-run"
 	Clean                     = "clean"
+	RestoreFormat             = "restore-format"
+	SkipRuleApply             = "skip-rule-apply"
+	RunValidation             = "run-validation"
+	IptablesProbePort         = "iptables-probe-port"
+)
+
+const (
+	DefaultProxyUID = "1337"
+)
+
+// Constants used in environment variables
+const (
+	DisableRedirectionOnLocalLoopback = "DISABLE_REDIRECTION_ON_LOCAL_LOOPBACK"
+	EnvoyUser                         = "ENVOY_USER"
+)
+
+// Constants for iptables commands
+const (
+	IPTABLES         = "iptables"
+	IPTABLESRESTORE  = "iptables-restore"
+	IPTABLESSAVE     = "iptables-save"
+	IP6TABLES        = "ip6tables"
+	IP6TABLESRESTORE = "ip6tables-restore"
+	IP6TABLESSAVE    = "ip6tables-save"
+	IP               = "ip"
+)
+
+// Constants for syscall
+const (
+	// sys/socket.h
+	SoOriginalDst = 80
+)
+
+const (
+	DefaultIptablesProbePort = 15002
+)
+
+const (
+	ValidationErrorCode = 126
 )
