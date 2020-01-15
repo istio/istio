63103d458035647de316135ec344d4242a193b35
Martin Ostrowski <mostrowski@google.com>
2020-01-15 15:15:03 -0800

    Clean up makefile

--- operator/cmd/mesh/manifest_shared_test.go
+++ operator/cmd/mesh/manifest_shared_test.go
@@ -18,7 +18,6 @@ import (
 	"bytes"
 	"io/ioutil"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"strings"
 )
@@ -34,10 +33,6 @@ func init() {
 		panic(err)
 	}
 	repoRootDir = filepath.Join(wd, "../..")
-
-	if err := syncCharts(); err != nil {
-		panic(err)
-	}
 }
 
 func runCommand(command string) (string, error) {
@@ -49,11 +44,6 @@ func runCommand(command string) (string, error) {
 		return "", err
 	}
 	return out.String(), nil
-}
-
-func syncCharts() error {
-	cmd := exec.Command(filepath.Join(repoRootDir, "scripts/run_update_charts.sh"))
-	return cmd.Run()
 }
 
 func readFile(path string) (string, error) {
