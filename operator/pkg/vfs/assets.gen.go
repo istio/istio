119cbb197c798d5d3fac3ffe3984d95a5fadb3bb
Xinnan Wen <iamwen@google.com>
2020-01-17 12:47:17 -0800

    add k8s service path to operator (#20233)
    * add k8s service path
    
    * fix test
    
    * update gen

--- operator/pkg/vfs/assets.gen.go
+++ operator/pkg/vfs/assets.gen.go
@@ -40928,6 +40928,8 @@ kubernetesMapping:
     outPath: "[{{.ResourceType}}:{{.ResourceName}}].spec.strategy"
   "{{.FeatureName}}.Components.{{.ComponentName}}.K8S.Tolerations":
     outPath: "[{{.ResourceType}}:{{.ResourceName}}].spec.template.spec.tolerations"
+  "{{.FeatureName}}.Components.{{.ComponentName}}.K8S.Service":
+    outPath: "[Service:{{.ResourceName}}].spec"
 toFeature:
     Base:               Base
     Pilot:              TrafficManagement
@@ -41157,6 +41159,8 @@ kubernetesMapping:
     outPath: "[{{.ResourceType}}:{{.ResourceName}}].spec.template.spec.tolerations"
   "Components.{{.ComponentName}}.K8S.ServiceAnnotations":
     outPath: "[Service:{{.ResourceName}}].metadata.annotations"
+  "Components.{{.ComponentName}}.K8S.Service":
+    outPath: "[Service:{{.ResourceName}}].spec"
 globalNamespaces:
   Pilot:      "istioNamespace"
   Galley:     "configNamespace"
