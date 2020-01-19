cec15c12db242e63fe0fc9642920345b2b247065
xuanyin.zy <xuanyin.zy@alibaba-inc.com>
2020-01-19 13:43:04 +0000

    go fmt

--- pilot/pkg/features/pilot.go
+++ pilot/pkg/features/pilot.go
@@ -51,7 +51,7 @@ var (
 	// MaxRecvMsgSize The max receive buffer size of gRPC received channel of Pilot in bytes.
 	MaxRecvMsgSize = env.RegisterIntVar(
 		"ISTIO_GPRC_MAXRECVMSGSIZE",
-		4 * 1024 * 1024,
+		4*1024*1024,
 		"Sets the max receive buffer size of gRPC stream in bytes.",
 	).Get()
 
