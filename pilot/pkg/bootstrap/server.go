8326c692f1cb655d30bec2348a11f48b7a522339
Rama Chavali <rama.rao@salesforce.com>
2020-01-20 22:11:19 +0530

    server: refresh push context after cache sync (#20323)
    * refresh push context after cache sync
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * pass env
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * call push after cache sync
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/bootstrap/server.go
+++ pilot/pkg/bootstrap/server.go
@@ -292,9 +292,10 @@ func (s *Server) Start(stop <-chan struct{}) error {
 		return fmt.Errorf("failed to sync cache")
 	}
 
-	// Notify Server readiness to Discovery Server, so that it can start accepting connections.
-	log.Infof("All caches have been synced up, notifying discovery server")
-	s.EnvoyXdsServer.OnServerReady()
+	// Trigger a push, so that the global push context is updated with the new config and Pilot's local Envoy
+	// also is updated with new config.
+	log.Infof("All caches have been synced up, triggering a push")
+	s.EnvoyXdsServer.Push(&model.PushRequest{Full: true})
 
 	// At this point we are ready - start Http Listener so that it can respond to readiness events.
 	go func() {
