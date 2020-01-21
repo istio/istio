6e2412ebbde27c219a958e0b51dc21911dc20b87
Bryant Hagadorn <4451303+blhagadorn@users.noreply.github.com>
2020-01-20 22:31:43 -0500

    Update root.go

--- operator/cmd/mesh/root.go
+++ operator/cmd/mesh/root.go
@@ -25,7 +25,7 @@ import (
 
 const (
 	SetFlagHelpStr = `Override an IstioOperator value, e.g. to choose a profile
-(--set profile=demo), enable or disable components (--set policy.enabled=true), or override Istio 
+(--set profile=demo), enable or disable components (--set components.policy.enabled=true), or override Istio 
 settings (--set values.global.mtls.enabled=true). See documentation for more info: 
 https://istio.io/docs/reference/config/istio.operator.v1alpha12.pb/#IstioControlPlaneSpec`
 	skipConfirmationFlagHelpStr = `skipConfirmation determines whether the user is prompted for confirmation. 
