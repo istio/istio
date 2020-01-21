9269798d5551675fc6763c2e9ed6b397cbbb6d2a
Christopher Manzi <chris.manzi@invitae.com>
2020-01-21 17:11:08 -0600

    Completed unit tests for createCA function.

--- security/cmd/istio_ca/main_test.go
+++ security/cmd/istio_ca/main_test.go
@@ -15,47 +15,110 @@
 package main
 
 import (
-	"github.com/spf13/cobra"
+	"io/ioutil"
 	"reflect"
 	"testing"
+	"time"
 
-	"istio.io/istio/security/pkg/pki/ca"
+	"istio.io/istio/pkg/spiffe"
 	"k8s.io/client-go/kubernetes/fake"
 	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 )
 
 func Test_createCA(t *testing.T) {
+	certPem, err := ioutil.ReadFile("../../pkg/pki/testdata/multilevelpki/int2-cert.pem")
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
 	client := fake.NewSimpleClientset()
 	type args struct {
 		client corev1.CoreV1Interface
 	}
 	tests := []struct {
-		name     string
-		args     args
-		commands []*cobra.Command
-		want     *ca.IstioCA
+		name            string
+		args            args
+		opts            cliOptions
+		wantCert        []byte
+		wantTrustDomain string
 	}{
 		{
-			name: "Given a client with no TrustDomain specified",
+			name: "Given a client with no TrustDomain specified (self-signed)",
 			args: args{
 				client: client.CoreV1(),
 			},
-			want: &ca.IstioCA{},
+			opts: cliOptions{
+				selfSignedCA:        true,
+				readSigningCertOnly: false,
+				selfSignedCACertTTL: time.Hour * 24 * 365,
+				workloadCertTTL:     time.Hour * 24,
+				maxWorkloadCertTTL:  time.Hour * 24 * 7,
+				dualUse:             false,
+			},
+			wantTrustDomain: "cluster.local",
 		},
 		{
-			name: "Given a client with a custom TrustDomain specified",
+			name: "Given a client with a custom TrustDomain specified (self-signed)",
 			args: args{
 				client: client.CoreV1(),
 			},
-			want: &ca.IstioCA{},
+			opts: cliOptions{
+				selfSignedCA:        true,
+				readSigningCertOnly: false,
+				selfSignedCACertTTL: time.Hour * 24 * 365,
+				workloadCertTTL:     time.Hour * 24,
+				maxWorkloadCertTTL:  time.Hour * 24 * 7,
+				dualUse:             false,
+				trustDomain:         "my.domain.com",
+			},
+			wantTrustDomain: "my.domain.com",
+		},
+		{
+			name: "Given a client with a no TrustDomain specified (not self-signed)",
+			args: args{
+				client: client.CoreV1(),
+			},
+			opts: cliOptions{
+				selfSignedCA:       false,
+				rootCertFile:       "../../pkg/pki/testdata/multilevelpki/root-cert.pem",
+				certChainFile:      "../../pkg/pki/testdata/multilevelpki/int2-cert-chain.pem",
+				signingCertFile:    "../../pkg/pki/testdata/multilevelpki/int2-cert.pem",
+				signingKeyFile:     "../../pkg/pki/testdata/multilevelpki/int2-key.pem",
+				workloadCertTTL:    time.Hour * 24,
+				maxWorkloadCertTTL: time.Hour * 24 * 7,
+			},
+			wantCert:        certPem,
+			wantTrustDomain: "my.domain.com",
+		},
+		{
+			name: "Given a client with a custom TrustDomain specified (not self-signed)",
+			args: args{
+				client: client.CoreV1(),
+			},
+			opts: cliOptions{
+				selfSignedCA:       false,
+				rootCertFile:       "../../pkg/pki/testdata/multilevelpki/root-cert.pem",
+				certChainFile:      "../../pkg/pki/testdata/multilevelpki/int2-cert-chain.pem",
+				signingCertFile:    "../../pkg/pki/testdata/multilevelpki/int2-cert.pem",
+				signingKeyFile:     "../../pkg/pki/testdata/multilevelpki/int2-key.pem",
+				workloadCertTTL:    time.Hour * 24,
+				maxWorkloadCertTTL: time.Hour * 24 * 7,
+				trustDomain:        "my.domain.com",
+			},
+			wantCert:        certPem,
+			wantTrustDomain: "my.domain.com",
 		},
 	}
 	for _, tt := range tests {
-		rootCmd.ResetCommands()
-		rootCmd.AddCommand(tt.commands...)
+		opts = tt.opts
 		t.Run(tt.name, func(t *testing.T) {
-			if got := createCA(tt.args.client); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("createCA() = %+v, want %+v", got, tt.want)
+			got := createCA(tt.args.client)
+			gotCert, _, _, _ := got.GetCAKeyCertBundle().GetAllPem()
+			if !reflect.DeepEqual(gotCert, tt.wantCert) && !tt.opts.selfSignedCA {
+				t.Errorf("cert PEM got = %v, want %v", string(gotCert), string(tt.wantCert))
+			}
+			if !reflect.DeepEqual(spiffe.GetTrustDomain(), tt.wantTrustDomain) {
+				t.Errorf("spiffe trustDomain got = %v, want %v", string(spiffe.GetTrustDomain()), string(tt.wantTrustDomain))
 			}
 		})
 	}
