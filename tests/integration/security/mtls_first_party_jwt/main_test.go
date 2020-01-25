532648e163b2997433ae9ff9cf38a275c4236e01
Lei Tang <32078630+lei-tang@users.noreply.github.com>
2020-01-24 20:06:26 -0800

    Fix lint error

--- tests/integration/security/mtls_first_party_jwt/main_test.go
+++ tests/integration/security/mtls_first_party_jwt/main_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package mtls_first_party_jwt
+package mtlsfirstpartyjwt
 
 import (
 	"testing"
