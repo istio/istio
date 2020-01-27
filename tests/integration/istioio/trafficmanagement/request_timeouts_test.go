a547e329c003c6ab057f329071d821bffea9beff
shamsher31 <shaansar@redhat.com>
2020-01-27 16:25:46 +0530

    Improve handling of ingress host/port

--- tests/integration/istioio/trafficmanagement/request_timeouts_test.go
+++ tests/integration/istioio/trafficmanagement/request_timeouts_test.go
@@ -18,32 +18,62 @@ import (
 	"testing"
 
 	"istio.io/istio/pkg/test/framework"
+	"istio.io/istio/pkg/test/framework/components/environment/kube"
 	"istio.io/istio/pkg/test/istioio"
+)
+
+const (
+	ingressHostCommand = `$(kubectl -n istio-system get service istio-ingressgateway \
+-o jsonpath='{.status.loadBalancer.ingress[0].ip}')`
+	ingressPortCommand = `$(kubectl -n istio-system get service istio-ingressgateway \
+-o jsonpath='{.spec.ports[?(@.name=="tcp")].port}')`
+	minikubeIngressHostCommand = `$(kubectl -n istio-system get pod -l istio=ingressgateway \
+-o jsonpath='{.items[0].status.hostIP}')`
+	minikubeIngressPortCommand = `$(kubectl -n istio-system get service istio-ingressgateway \
+-o jsonpath='{.spec.ports[?(@.name=="tcp")].nodePort}')`
 )
 
 func TestRequestTimeouts(t *testing.T) {
 	framework.
 		NewTest(t).
-		Run(istioio.NewBuilder("tasks__traffic_management__request_timeouts").
-			Add(istioio.Script{
-				Input: istioio.Path("../common/scripts/bookinfo.txt"),
-			}).
-			Add(istioio.Script{
-				Input: istioio.Path("scripts/request_timeouts.txt"),
-			}).
-			Add(istioio.Script{
-				Input: istioio.Path("scripts/request_timeouts_delay.txt"),
-			}).
-			Defer(istioio.Script{
-				Input: istioio.Inline{
-					FileName: "cleanup.sh",
-					Value: `
+		Run(func(ctx framework.TestContext) {
+			istioio.NewBuilder("tasks__traffic_management__request_timeouts").
+				Add(istioio.Script{Input: istioio.Path("../common/scripts/bookinfo.txt")}).
+				Add(script(ctx, "request_timeouts.txt")).
+				Add(script(ctx, "request_timeouts_delay.txt")).
+				Defer(istioio.Script{
+					Input: istioio.Inline{
+						FileName: "cleanup.sh",
+						Value: `
 kubectl delete -n default -f samples/bookinfo/platform/kube/bookinfo.yaml || true
 kubectl delete -f samples/bookinfo/networking/destination-rule-all.yaml || true
 kubectl delete -f samples/bookinfo/networking/virtual-service-all-v1.yaml || true
 kubectl delete -f samples/bookinfo/networking/bookinfo-gateway.yaml || true
 kubectl delete -f samples/sleep/sleep.yaml || true`,
-				},
-			}).
-			Build())
+					},
+				}).
+				BuildAndRun(ctx)
+		})
+}
+
+func script(ctx framework.TestContext, filename string) istioio.Script {
+	e := ctx.Environment().(*kube.Environment)
+	runtimeIngressHostCommand := ingressHostCommand
+	runtimeIngressPortCommand := ingressPortCommand
+	if e.Settings().Minikube {
+		runtimeIngressHostCommand = minikubeIngressHostCommand
+		runtimeIngressPortCommand = minikubeIngressPortCommand
+	}
+	return istioio.Script{
+		Input: istioio.Evaluate(istioio.Path("scripts/"+filename), map[string]interface{}{
+			"isSnippet":          false,
+			"ingressHostCommand": runtimeIngressHostCommand,
+			"ingressPortCommand": runtimeIngressPortCommand,
+		}),
+		SnippetInput: istioio.Evaluate(istioio.Path("scripts/"+filename), map[string]interface{}{
+			"isSnippet":          true,
+			"ingressHostCommand": ingressHostCommand,
+			"ingressPortCommand": ingressPortCommand,
+		}),
+	}
 }
