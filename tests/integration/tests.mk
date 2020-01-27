62e01a4636fd5c4fc5bd39b9f1f7667bc31d6b3d
John Howard <howardjohn@google.com>
2020-01-27 10:01:28 -0800

    Fix istio.io test missing escape

--- tests/integration/tests.mk
+++ tests/integration/tests.mk
@@ -83,7 +83,7 @@ test.integration.%.kube.presubmit: istioctl | $(JUNIT_REPORT)
 test.integration.istioio.kube.presubmit: istioctl | $(JUNIT_REPORT)
 	PATH=${PATH}:${ISTIO_OUT} $(GO) test -p 1 ${T} ./tests/integration/istioio/... ${_INTEGRATION_TEST_WORKDIR_FLAG} ${_INTEGRATION_TEST_CIMODE_FLAG} -timeout 30m \
 	--istio.test.select -postsubmit,-flaky \
-	--istio.test.kube.operator=false
+	--istio.test.kube.operator=false \
 	--istio.test.env kube \
 	--istio.test.kube.config ${INTEGRATION_TEST_KUBECONFIG} \
 	--istio.test.hub=${HUB} \
