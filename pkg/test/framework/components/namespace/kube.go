47f35048c418dc361f299425638e77300621fcb8
John Howard <howardjohn@google.com>
2020-01-27 13:53:02 -0800

    Capture init container logs for dumped pods
    This would allow us to debug failures like
    https://prow.istio.io/view/gcs/istio-prow/logs/integ-pilot-k8s-tests_istio_postsubmit/1253
    where the pod fails during init. We only dump on errors, so this won't
    negatively impact test time or anything like that

--- pkg/test/framework/components/namespace/kube.go
+++ pkg/test/framework/components/namespace/kube.go
@@ -61,7 +61,8 @@ func (n *kubeNamespace) Dump() {
 	}
 
 	for _, pod := range pods {
-		for _, container := range pod.Spec.Containers {
+		containers := append(pod.Spec.Containers, pod.Spec.InitContainers...)
+		for _, container := range containers {
 			l, err := n.a.Logs(pod.Namespace, pod.Name, container.Name, false /* previousLog */)
 			if err != nil {
 				scopes.CI.Errorf("Unable to get logs for pod/container: %s/%s/%s", pod.Namespace, pod.Name, container.Name)
