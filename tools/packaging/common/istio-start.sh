97cc01d5cc5543e83792c6f068551efeeb43d494
John Howard <howardjohn@google.com>
2020-01-27 13:09:28 -0800

    [1.4] Fix version for VMs

--- tools/packaging/common/istio-start.sh
+++ tools/packaging/common/istio-start.sh
@@ -21,7 +21,7 @@
 set -e
 
 # Match pilot/docker/Dockerfile.proxyv2
-export ISTIO_META_ISTIO_VERSION="1.3.0"
+export ISTIO_META_ISTIO_VERSION="1.4.4"
 
 # Load optional config variables
 ISTIO_SIDECAR_CONFIG=${ISTIO_SIDECAR_CONFIG:-/var/lib/istio/envoy/sidecar.env}
