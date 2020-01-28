799d257fb1e9c2c25c16fa411b56f038392f325e
Eric Van Norman <ericvn@us.ibm.com>
2020-01-27 21:06:27 -0600

    Add KUBECONFIG back into tests (#20558)
    * Add KUBECONFIG back in for tests
    
    * Add back tests kubeconfig file

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -413,6 +413,7 @@ else
        TEST_OBJ = selected-pkg-test
 endif
 test: | $(JUNIT_REPORT)
+	KUBECONFIG="$${KUBECONFIG:-$${REPO_ROOT}/tests/util/kubeconfig}" \
 	$(MAKE) -e -f Makefile.core.mk --keep-going $(TEST_OBJ) \
 	2>&1 | tee >($(JUNIT_REPORT) > $(JUNIT_OUT))
 
