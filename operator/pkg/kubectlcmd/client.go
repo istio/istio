308fb59bf5208463db9791d923a20dd9bb52e937
Tao HE <1579288+elfinhe@users.noreply.github.com>
2020-01-29 14:25:15 -0800

    Check k8s version before kubectl apply (#20391)

--- operator/pkg/kubectlcmd/client.go
+++ operator/pkg/kubectlcmd/client.go
@@ -108,6 +108,12 @@ func (c *Client) GetConfigMap(name string, opts *Options) (string, string, error
 	return c.kubectl([]string{"get", "cm", name}, opts)
 }
 
+// Version runs the `kubectl version` and return stdout and stderr
+func (c *Client) Version(opts *Options) (string, string, error) {
+	opts.Output = "yaml"
+	return c.kubectl([]string{"version"}, opts)
+}
+
 // kubectl runs the `kubectl` command by specifying subcommands in subcmds with opts.
 func (c *Client) kubectl(subcmds []string, opts *Options) (string, string, error) {
 	hasStdin := strings.TrimSpace(opts.Stdin) != ""
