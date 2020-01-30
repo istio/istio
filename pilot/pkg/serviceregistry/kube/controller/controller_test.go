afb7fa1d29249c16d74789f68d9f1c761371673b
John Howard <howardjohn@google.com>
2020-01-30 08:23:34 -0800

    Fix pod IP re-use bug in kube controller
    Fixes https://github.com/istio/istio/issues/20676
    
    See issue for details

--- pilot/pkg/serviceregistry/kube/controller/controller_test.go
+++ pilot/pkg/serviceregistry/kube/controller/controller_test.go
@@ -1444,6 +1444,20 @@ func deleteExternalNameService(controller *Controller, name, namespace string, t
 	}
 }
 
+func updatePods(t *testing.T, controller *Controller, pods ...*coreV1.Pod) {
+	for _, pod := range pods {
+		newPod, err := controller.client.CoreV1().Pods(pod.Namespace).Update(pod)
+		if err != nil {
+			t.Fatalf("Cannot update %s in namespace %s (error: %v)", pod.ObjectMeta.Name, pod.ObjectMeta.Namespace, err)
+		}
+		// Apiserver doesn't allow Create/Update to modify the pod status. Creating doesn't result in
+		// events - since PodIP will be "".
+		newPod.Status.PodIP = pod.Status.PodIP
+		newPod.Status.Phase = coreV1.PodRunning
+		_, _ = controller.client.CoreV1().Pods(pod.Namespace).UpdateStatus(newPod)
+	}
+}
+
 func addPods(t *testing.T, controller *Controller, pods ...*coreV1.Pod) {
 	for _, pod := range pods {
 		newPod, err := controller.client.CoreV1().Pods(pod.Namespace).Create(pod)
