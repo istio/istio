d8b4f2fc6053c9fd6a3ff8171029ab364bc9ef12
Jason Wang <jasonwzm@gmail.com>
2020-02-03 13:00:35 -0800

    run make gen

--- mixer/test/spyAdapter/template/checkoutput/tmpl_handler_service.pb.go
+++ mixer/test/spyAdapter/template/checkoutput/tmpl_handler_service.pb.go
@@ -1677,6 +1677,7 @@ func (m *InstanceParam) Unmarshal(dAtA []byte) error {
 func skipTmplHandlerService(dAtA []byte) (n int, err error) {
 	l := len(dAtA)
 	iNdEx := 0
+	depth := 0
 	for iNdEx < l {
 		var wire uint64
 		for shift := uint(0); ; shift += 7 {
@@ -1708,10 +1709,8 @@ func skipTmplHandlerService(dAtA []byte) (n int, err error) {
 					break
 				}
 			}
-			return iNdEx, nil
 		case 1:
 			iNdEx += 8
-			return iNdEx, nil
 		case 2:
 			var length int
 			for shift := uint(0); ; shift += 7 {
@@ -1732,55 +1731,30 @@ func skipTmplHandlerService(dAtA []byte) (n int, err error) {
 				return 0, ErrInvalidLengthTmplHandlerService
 			}
 			iNdEx += length
-			if iNdEx < 0 {
-				return 0, ErrInvalidLengthTmplHandlerService
-			}
-			return iNdEx, nil
 		case 3:
-			for {
-				var innerWire uint64
-				var start int = iNdEx
-				for shift := uint(0); ; shift += 7 {
-					if shift >= 64 {
-						return 0, ErrIntOverflowTmplHandlerService
-					}
-					if iNdEx >= l {
-						return 0, io.ErrUnexpectedEOF
-					}
-					b := dAtA[iNdEx]
-					iNdEx++
-					innerWire |= (uint64(b) & 0x7F) << shift
-					if b < 0x80 {
-						break
-					}
-				}
-				innerWireType := int(innerWire & 0x7)
-				if innerWireType == 4 {
-					break
-				}
-				next, err := skipTmplHandlerService(dAtA[start:])
-				if err != nil {
-					return 0, err
-				}
-				iNdEx = start + next
-				if iNdEx < 0 {
-					return 0, ErrInvalidLengthTmplHandlerService
-				}
-			}
-			return iNdEx, nil
+			depth++
 		case 4:
-			return iNdEx, nil
+			if depth == 0 {
+				return 0, ErrUnexpectedEndOfGroupTmplHandlerService
+			}
+			depth--
 		case 5:
 			iNdEx += 4
-			return iNdEx, nil
 		default:
 			return 0, fmt.Errorf("proto: illegal wireType %d", wireType)
 		}
+		if iNdEx < 0 {
+			return 0, ErrInvalidLengthTmplHandlerService
+		}
+		if depth == 0 {
+			return iNdEx, nil
+		}
 	}
-	panic("unreachable")
+	return 0, io.ErrUnexpectedEOF
 }
 
 var (
-	ErrInvalidLengthTmplHandlerService = fmt.Errorf("proto: negative length found during unmarshaling")
-	ErrIntOverflowTmplHandlerService   = fmt.Errorf("proto: integer overflow")
+	ErrInvalidLengthTmplHandlerService        = fmt.Errorf("proto: negative length found during unmarshaling")
+	ErrIntOverflowTmplHandlerService          = fmt.Errorf("proto: integer overflow")
+	ErrUnexpectedEndOfGroupTmplHandlerService = fmt.Errorf("proto: unexpected end of group")
 )
