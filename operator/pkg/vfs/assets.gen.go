857e7e0776ed08c468bb03552bf3768bf5e1106e
Xinnan Wen <iamwen@google.com>
2020-02-07 12:44:02 -0800

    update default profile (#20947)
    *  update default profile
    
    * fix gen

--- operator/pkg/vfs/assets.gen.go
+++ operator/pkg/vfs/assets.gen.go
@@ -37718,6 +37718,18 @@ spec:
   addonComponents:
     prometheus:
       enabled: true
+      k8s:
+        replicaCount: 1
+    kiali:
+      enabled: false
+      k8s:
+        replicaCount: 1
+    grafana:
+      enabled: false
+      k8s:
+        replicaCount: 1
+    tracing:
+      enabled: false
 
   # Global values passed through to helm global.yaml.
   # Please keep this in sync with manifests/global.yaml
@@ -38051,8 +38063,6 @@ spec:
         autoInject: true
 
     prometheus:
-      enabled: true
-      replicaCount: 1
       hub: docker.io/prom
       tag: v2.15.1
       retention: 6h
@@ -38072,8 +38082,6 @@ spec:
       podAntiAffinityTermLabelSelector: []
 
     grafana:
-      enabled: false
-      replicaCount: 1
       image:
         repository: grafana/grafana
         tag: 6.5.2
@@ -38122,7 +38130,6 @@ spec:
       envSecrets: {}
 
     tracing:
-      enabled: false
       provider: jaeger
       nodeSelector: {}
       podAntiAffinityLabelSelector: []
@@ -38182,8 +38189,6 @@ spec:
       coreDNSPluginImage: istio/coredns-plugin:0.2-istio-1.1
 
     kiali:
-      enabled: false
-      replicaCount: 1
       hub: quay.io/kiali
       tag: v1.9
       contextPath: /kiali
