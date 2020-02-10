de18ea6a776328bcbc82a2c5ed5a99637f1bcd7f
Istio Automation <istio.testing@gmail.com>
2020-02-10 12:50:00 -0800

    [release-1.5] Make leader election a top level pilot component, use it for singleton components (#20939)
    * Add initial leader election
    
    * Cleanup
    
    * Add unit tests
    
    * user leader election for ns controller, validation controller
    
    * Log about ingress
    
    * Add graceful shutdown
    
    * Fix tests, add injector controller
    
    * Fix lint
    
    * Add renewal when lease is droppped, failing test
    
    * wip
    
    * Get everything working
    
    * Fix build
    
    * lint
    
    * Skip lint
    
    Co-authored-by: John Howard <howardjohn@google.com>

---
+++ pilot/pkg/leaderelection/leaderelection.go
@@ -0,0 +1,122 @@
+// Copyright 2020 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package leaderelection
+
+import (
+	"context"
+	"time"
+
+	"go.uber.org/atomic"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/leaderelection"
+	"k8s.io/client-go/tools/leaderelection/resourcelock"
+
+	"istio.io/pkg/log"
+)
+
+const (
+	electionID = "istio-leader"
+)
+
+type LeaderElection struct {
+	namespace string
+	name      string
+	runFns    []func(stop <-chan struct{})
+	client    kubernetes.Interface
+	ttl       time.Duration
+
+	// Records which "cycle" the election is on. This is incremented each time an election is won and then lost
+	// This is mostly just for testing
+	cycle *atomic.Int32
+}
+
+// Run will start leader election, calling all runFns when we become the leader.
+func (l *LeaderElection) Run(stop <-chan struct{}) {
+	for {
+		le, err := l.create()
+		if err != nil {
+			// This should never happen; errors are only from invalid input and the input is not user modifiable
+			panic("leaderelection creation failed: " + err.Error())
+		}
+		l.cycle.Inc()
+		ctx, cancel := context.WithCancel(context.Background())
+		go func() {
+			<-stop
+			cancel()
+		}()
+		le.Run(ctx)
+		select {
+		case <-stop:
+			// We were told to stop explicitly. Exit now
+			return
+		default:
+			// Otherwise, we may have lost our lock. In practice, this is extremely rare; we need to have the lock, then lose it
+			// Typically this means something went wrong, such as API server downtime, etc
+			// If this does happen, we will start the cycle over again
+			log.Errorf("Leader election cycle %v lost. Trying again", l.cycle.Load())
+		}
+	}
+}
+
+func (l *LeaderElection) create() (*leaderelection.LeaderElector, error) {
+	callbacks := leaderelection.LeaderCallbacks{
+		OnStartedLeading: func(ctx context.Context) {
+			for _, f := range l.runFns {
+				go f(ctx.Done())
+			}
+		},
+		OnStoppedLeading: func() {
+			log.Infof("leader election lock lost")
+
+		},
+	}
+	lock := resourcelock.ConfigMapLock{
+		ConfigMapMeta: metaV1.ObjectMeta{Namespace: l.namespace, Name: electionID},
+		Client:        l.client.CoreV1(),
+		LockConfig: resourcelock.ResourceLockConfig{
+			Identity: l.name,
+		},
+	}
+	return leaderelection.NewLeaderElector(leaderelection.LeaderElectionConfig{
+		Lock:          &lock,
+		LeaseDuration: l.ttl,
+		RenewDeadline: l.ttl / 2,
+		RetryPeriod:   l.ttl / 4,
+		Callbacks:     callbacks,
+		// When Pilot exits, the lease will be dropped. This is more likely to lead to a case where
+		// to instances are both considered the leaders. As such, if this is intended to be use for mission-critical
+		// usages (rather than avoiding duplication of work), this may need to be re-evaluated.
+		// TODO this should be true once https://github.com/kubernetes/kubernetes/issues/87800 is fixed
+		ReleaseOnCancel: false,
+	})
+}
+
+// AddRunFunction registers a function to run when we are the leader. These will be run asynchronously.
+// To avoid running when not a leader, functions should respect the stop channel.
+func (l *LeaderElection) AddRunFunction(f func(stop <-chan struct{})) {
+	l.runFns = append(l.runFns, f)
+}
+
+func NewLeaderElection(namespace, name string, client kubernetes.Interface) *LeaderElection {
+	return &LeaderElection{
+		namespace: namespace,
+		name:      name,
+		client:    client,
+		// Default to a 30s ttl. Overridable for tests
+		ttl:   time.Second * 30,
+		cycle: atomic.NewInt32(0),
+	}
+}
