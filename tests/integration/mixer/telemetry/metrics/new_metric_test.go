454dcb0b08b0d65b2cf324f0b7a2cdd070ff0cf3
John Howard <howardjohn@google.com>
2020-02-11 15:17:55 -0800

    Add NewMetricTest to mixer integration tests
    For https://github.com/istio/istio/issues/20441
    
    This migrates the last mixer e2e test without existing coverage. Once
    this change is merged (and we verify I am correct that this is the last
    one) we should be able to delete helm.

---
+++ tests/integration/mixer/telemetry/metrics/new_metric_test.go
@@ -0,0 +1,48 @@
+//  Copyright 2020 Istio Authors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package metrics
+
+import (
+	"fmt"
+	"testing"
+
+	"istio.io/istio/pkg/test/framework"
+	"istio.io/istio/pkg/test/framework/components/bookinfo"
+	"istio.io/istio/pkg/test/framework/components/environment"
+	util "istio.io/istio/tests/integration/mixer"
+)
+
+func TestNewMetric(t *testing.T) {
+	framework.
+		NewTest(t).
+		RequiresEnvironment(environment.Kube).
+		Run(func(ctx framework.TestContext) {
+			g.ApplyConfigOrFail(ctx, nil,
+				bookinfo.DoubleMetric.LoadOrFail(ctx))
+			defer g.DeleteConfigOrFail(ctx, nil,
+				bookinfo.DoubleMetric.LoadOrFail(ctx))
+
+			util.AllowRuleSync(t)
+
+			addr := ing.HTTPAddress()
+			url := fmt.Sprintf("http://%s/productpage", addr.String())
+			res := util.SendTraffic(ing, t, "Sending traffic", url, "", 10)
+			if res.RetCodes[200] < 1 {
+				t.Fatalf("unable to retrieve 200 from product page: %v", res.RetCodes)
+			}
+
+			util.ValidateMetric(t, prom, "sum(istio_double_request_count{message=\"twice the fun!\"})", "istio_double_request_count", 1)
+		})
+}
