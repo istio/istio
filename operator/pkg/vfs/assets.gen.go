3e06475057cbcd5feae175494059f57e7f05a2bb
Martin Ostrowski <mostrowski@google.com>
2020-02-12 13:37:52 -0800

    Use JSON merge to update objects

--- operator/pkg/vfs/assets.gen.go
+++ operator/pkg/vfs/assets.gen.go
@@ -39324,7 +39324,7 @@ componentMaps:
     SkipReverseTranslate: true
   Pilot:
     ResourceType:         "Deployment"
-    ResourceName:         "istio-pilot"
+    ResourceName:         "istiod"
     ContainerName:        "discovery"
     HelmSubdir:           "istio-control/istio-discovery"
     ToHelmValuesTreeRoot: "pilot"
