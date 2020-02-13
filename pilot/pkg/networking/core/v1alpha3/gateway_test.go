ba13ab354986be7689d15ccddbe3ee422636f77d
John Howard <howardjohn@google.com>
2020-02-13 09:07:59 -0800

    Use a single shared SDS cluster
    Split out of https://github.com/istio/istio/pull/21038 because that PR
    got a bit too complicated

--- pilot/pkg/networking/core/v1alpha3/gateway_test.go
+++ pilot/pkg/networking/core/v1alpha3/gateway_test.go
@@ -108,18 +108,8 @@ func TestBuildGatewayListenerTlsContext(t *testing.T) {
 										ApiType: core.ApiConfigSource_GRPC,
 										GrpcServices: []*core.GrpcService{
 											{
-												TargetSpecifier: &core.GrpcService_GoogleGrpc_{
-													GoogleGrpc: &core.GrpcService_GoogleGrpc{
-														TargetUri:  "unix:/var/run/sds/uds_path",
-														StatPrefix: model.SDSStatPrefix,
-														ChannelCredentials: &core.GrpcService_GoogleGrpc_ChannelCredentials{
-															CredentialSpecifier: &core.GrpcService_GoogleGrpc_ChannelCredentials_LocalCredentials{
-																LocalCredentials: &core.GrpcService_GoogleGrpc_GoogleLocalCredentials{},
-															},
-														},
-														CallCredentials:        model.ConstructgRPCCallCredentials(model.K8sSATrustworthyJwtFileName, model.K8sSAJwtTokenHeaderKey),
-														CredentialsFactoryName: model.FileBasedMetadataPlugName,
-													},
+												TargetSpecifier: &core.GrpcService_EnvoyGrpc_{
+													EnvoyGrpc: &core.GrpcService_EnvoyGrpc{ClusterName: model.SDSClusterName},
 												},
 											},
 										},
@@ -140,18 +130,8 @@ func TestBuildGatewayListenerTlsContext(t *testing.T) {
 											ApiType: core.ApiConfigSource_GRPC,
 											GrpcServices: []*core.GrpcService{
 												{
-													TargetSpecifier: &core.GrpcService_GoogleGrpc_{
-														GoogleGrpc: &core.GrpcService_GoogleGrpc{
-															TargetUri:  "unix:/var/run/sds/uds_path",
-															StatPrefix: model.SDSStatPrefix,
-															ChannelCredentials: &core.GrpcService_GoogleGrpc_ChannelCredentials{
-																CredentialSpecifier: &core.GrpcService_GoogleGrpc_ChannelCredentials_LocalCredentials{
-																	LocalCredentials: &core.GrpcService_GoogleGrpc_GoogleLocalCredentials{},
-																},
-															},
-															CallCredentials:        model.ConstructgRPCCallCredentials(model.K8sSATrustworthyJwtFileName, model.K8sSAJwtTokenHeaderKey),
-															CredentialsFactoryName: model.FileBasedMetadataPlugName,
-														},
+													TargetSpecifier: &core.GrpcService_EnvoyGrpc_{
+														EnvoyGrpc: &core.GrpcService_EnvoyGrpc{ClusterName: model.SDSClusterName},
 													},
 												},
 											},
