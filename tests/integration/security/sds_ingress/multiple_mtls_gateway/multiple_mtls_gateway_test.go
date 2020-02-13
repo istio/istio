a586faaae31568655aaed37ec5d527a78c9940c6
Lei Tang <32078630+lei-tang@users.noreply.github.com>
2020-02-13 11:11:20 +0800

    Make functions shared between tests

--- tests/integration/security/sds_ingress/multiple_mtls_gateway/multiple_mtls_gateway_test.go
+++ tests/integration/security/sds_ingress/multiple_mtls_gateway/multiple_mtls_gateway_test.go
@@ -15,66 +15,18 @@
 package multiplemtlsgateway
 
 import (
-	"time"
-
 	"istio.io/istio/pkg/test/framework"
 	"istio.io/istio/pkg/test/framework/components/environment"
-	"istio.io/istio/pkg/test/framework/components/ingress"
-	ingressutil "istio.io/istio/tests/integration/security/sds_ingress/util"
+	"istio.io/istio/tests/integration/security/sds_ingress/util"
 
 	"testing"
 )
-
-var (
-	credNames = []string{"bookinfo-credential-1", "bookinfo-credential-2", "bookinfo-credential-3",
-		"bookinfo-credential-4", "bookinfo-credential-5"}
-	hosts = []string{"bookinfo1.example.com", "bookinfo2.example.com", "bookinfo3.example.com",
-		"bookinfo4.example.com", "bookinfo5.example.com"}
-)
-
-// testMultiMtlsGateways deploys multiple mTLS gateways with SDS enabled, and creates kubernetes that store
-// private key, server certificate and CA certificate for each mTLS gateway. Verifies that all gateways are able to terminate
-// mTLS connections successfully.
-func testMultiMtlsGateways(t *testing.T, ctx framework.TestContext) { // nolint:interfacer
-	t.Helper()
-
-	ingressutil.CreateIngressKubeSecret(t, ctx, credNames, ingress.Mtls, ingressutil.IngressCredentialA)
-	ingressutil.DeployBookinfo(t, ctx, g, ingressutil.MultiMTLSGateway)
-
-	ing := ingress.NewOrFail(t, ctx, ingress.Config{
-		Istio: inst,
-	})
-	// Expect 2 SDS updates for each listener, one for server key/cert, and one for CA cert.
-	err := ingressutil.WaitUntilGatewaySdsStatsGE(t, ing, 2*len(credNames), 30*time.Second)
-	if err != nil {
-		t.Errorf("sds update stats does not match: %v", err)
-	}
-	// Expect 2 active listeners, one listens on 443 and the other listens on 15090
-	err = ingressutil.WaitUntilGatewayActiveListenerStatsGE(t, ing, 2, 60*time.Second)
-	if err != nil {
-		t.Errorf("total active listener stats does not match: %v", err)
-	}
-	tlsContext := ingressutil.TLSContext{
-		CaCert:     ingressutil.CaCertA,
-		PrivateKey: ingressutil.TLSClientKeyA,
-		Cert:       ingressutil.TLSClientCertA,
-	}
-	callType := ingress.Mtls
-
-	for _, h := range hosts {
-		err := ingressutil.VisitProductPage(ing, h, callType, tlsContext, 30*time.Second,
-			ingressutil.ExpectedResponse{ResponseCode: 200, ErrorMessage: ""}, t)
-		if err != nil {
-			t.Errorf("unable to retrieve 200 from product page at host %s: %v", h, err)
-		}
-	}
-}
 
 func TestMtlsGateways(t *testing.T) {
 	framework.
 		NewTest(t).
 		RequiresEnvironment(environment.Kube).
 		Run(func(ctx framework.TestContext) {
-			testMultiMtlsGateways(t, ctx)
+			util.RunTestMultiMtlsGateways(t, ctx, inst, g)
 		})
 }
--- tests/integration/security/sds_ingress_k8sca/multiple_tls_gateway/main_test.go
+++ tests/integration/security/sds_ingress_k8sca/multiple_tls_gateway/main_test.go
@@ -39,7 +39,7 @@ func TestMain(m *testing.M) {
 		NewSuite("sds_ingress_multiple_tls_gateways_k8sca_test", m).
 		Label(label.CustomSetup).
 		Label(label.Flaky).
-		SetupOnEnv(environment.Kube, istio.Setup(&inst, setupConfig)).
+		SetupOnEnv(environment.Kube, istio.Setup(&inst, nil)).
 		Setup(func(ctx resource.Context) (err error) {
 			if g, err = galley.New(ctx, galley.Config{}); err != nil {
 				return err
@@ -53,11 +53,4 @@ func TestMain(m *testing.M) {
 		}).
 		Run()
 
-}
-
-func setupConfig(cfg *istio.Config) {
-	if cfg == nil {
-		return
-	}
-	cfg.Values["global.pilotCertProvider"] = "kubernetes"
 }
