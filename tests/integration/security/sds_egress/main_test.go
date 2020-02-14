10ba91244e262700c85df779354eca42f5c2bc4e
Daniel Grimm <dgn@users.noreply.github.com>
2020-02-14 17:48:11 +0100

    tests: stop using ValuesFile, use ControlPlaneValues (#21065)

--- tests/integration/security/sds_egress/main_test.go
+++ tests/integration/security/sds_egress/main_test.go
@@ -65,7 +65,34 @@ func setupConfig(cfg *istio.Config) {
 	if cfg == nil {
 		return
 	}
-	cfg.ValuesFile = "values-istio-sds-auth.yaml"
-	cfg.Values["gateways.istio-egressgateway.enabled"] = "true"
-	cfg.Values["prometheus.enabled"] = "true"
+	cfg.ControlPlaneValues = `
+components:
+  gateways:
+    istio-egressgateway:
+      enabled: true
+addonComponents:
+  prometheus:
+    enabled: true
+values:
+  global:
+    controlPlaneSecurityEnabled: true
+
+    mtls:
+      # Default setting for service-to-service mtls. Can be set explicitly using
+      # destination rules or service annotations.
+      enabled: true
+
+    sds:
+      enabled: true
+      udsPath: "unix:/var/run/sds/uds_path"
+      token:
+        aud: "istio-ca"
+  nodeagent:
+    enabled: true
+    image: node-agent-k8s
+    env:
+      CA_PROVIDER: "Citadel"
+      CA_ADDR: "istio-citadel:8060"
+      VALID_TOKEN: true
+`
 }
