d97d52d3bd0f3ad82e176d069919766a268c1ae8
John Howard <howardjohn@google.com>
2020-02-14 11:43:41 -0800

    Optimize namespace controller in Pilot (#20915)
    * Optimize namespace controller
    
    Right now the ns controller runs at a rate of 2.5 namespaces/s due to
    kube client rate limiting. We also resync every 30s. This means that if
    you have over 75 namespaces, you will be constantly writing config map.
    Even worse, this limit is shared with the rest of istiod.
    
    There is no real reason to resync every 30s, its just excessive. We can
    just turn this off. Note - Galley has never had a resync period and I
    have never heard of a "missed event" issue as a result. Kubernetes
    provides the same guidance.
    
    * fix lint
    
    * fix test
    
    * fix type
    
    * clean up per comments
    
    * improve error handling
    
    * fix nits
    
    * fix flake
    
    (cherry picked from commit 7bcc71e51b87098bbd3da1523dcf6e9a94dbb5a3)

--- pilot/pkg/bootstrap/istio_ca.go
+++ pilot/pkg/bootstrap/istio_ca.go
@@ -25,6 +25,8 @@ import (
 	"strings"
 	"time"
 
+	"istio.io/istio/pkg/config/constants"
+
 	oidc "github.com/coreos/go-oidc"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
@@ -150,7 +152,7 @@ func (s *Server) EnableCA() bool {
 // Protected by installer options: the CA will be started only if the JWT token in /var/run/secrets
 // is mounted. If it is missing - for example old versions of K8S that don't support such tokens -
 // we will not start the cert-signing server, since pods will have no way to authenticate.
-func (s *Server) RunCA(grpc *grpc.Server, ca *ca.IstioCA, opts *CAOptions, stopCh <-chan struct{}) {
+func (s *Server) RunCA(grpc *grpc.Server, ca caserver.CertificateAuthority, opts *CAOptions, stopCh <-chan struct{}) {
 	if !s.EnableCA() {
 		return
 	}
@@ -210,13 +212,16 @@ func (s *Server) RunCA(grpc *grpc.Server, ca *ca.IstioCA, opts *CAOptions, stopC
 	}
 	log.Info("Istiod CA has started")
 
-	nc, err := NewNamespaceController(ca, s.kubeClient.CoreV1())
+	nc, err := NewNamespaceController(func() map[string]string {
+		return map[string]string{
+			constants.CACertNamespaceConfigMapDataName: string(ca.GetCAKeyCertBundle().GetRootCertPem()),
+		}
+	}, s.kubeClient.CoreV1())
 	if err != nil {
 		log.Warnf("failed to start istiod namespace controller, error: %v", err)
 	} else {
 		s.leaderElection.AddRunFunction(func(stop <-chan struct{}) {
-			log.Infof("Starting namespace controller")
-			nc.namespaceController.Run(stop)
+			nc.Run(stop)
 		})
 	}
 }
