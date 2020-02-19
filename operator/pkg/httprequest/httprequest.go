18f22d6e9fce20be7ef0ada54e3b96cc12dddb21
Martin Ostrowski <mostrowski@google.com>
2020-02-18 18:01:13 -0800

    Make urlfetcher work with package URLs

--- operator/pkg/httprequest/httprequest.go
+++ operator/pkg/httprequest/httprequest.go
@@ -15,36 +15,26 @@
 package httprequest
 
 import (
-	"bytes"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"net/http"
 )
 
 // Get sends an HTTP GET request and returns the result.
 func Get(url string) ([]byte, error) {
-	buf := bytes.NewBuffer(nil)
-
-	req, err := http.NewRequest("GET", url, nil)
+	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
-
-	client := &http.Client{
-		Transport: &http.Transport{
-			DisableCompression: true,
-			Proxy:              http.ProxyFromEnvironment,
-		}}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
 	if resp.StatusCode != 200 {
 		return nil, fmt.Errorf("failed to fetch URL %s : %s", url, resp.Status)
 	}
-
-	_, err = io.Copy(buf, resp.Body)
-	return buf.Bytes(), err
+	ret, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	if err := resp.Body.Close(); err != nil {
+		return nil, err
+	}
+	return ret, nil
 }
