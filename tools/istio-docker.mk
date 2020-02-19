c7deac967336edfa42ccf8c4ad86e4f03ce99e36
John Howard <howardjohn@google.com>
2020-02-19 07:54:51 -0800

    Remove legacy kubectl image

--- tools/istio-docker.mk
+++ tools/istio-docker.mk
@@ -24,7 +24,7 @@ docker: docker.all
 # Add new docker targets to the end of the DOCKER_TARGETS list.
 
 DOCKER_TARGETS ?= docker.pilot docker.proxyv2 docker.app docker.app_sidecar docker.test_policybackend \
-	docker.mixer docker.mixer_codegen docker.citadel docker.galley docker.sidecar_injector docker.kubectl \
+	docker.mixer docker.mixer_codegen docker.citadel docker.galley docker.sidecar_injector \
 	docker.istioctl docker.operator
 
 $(ISTIO_DOCKER) $(ISTIO_DOCKER_TAR):
@@ -65,15 +65,6 @@ $(ISTIO_DOCKER)/certs:
 DOCKER_FILES_FROM_SOURCE:=tests/testdata/certs/cert.crt tests/testdata/certs/cert.key tests/testdata/certs/cacert.pem
 $(foreach FILE,$(DOCKER_FILES_FROM_SOURCE), \
         $(eval $(ISTIO_DOCKER)/$(notdir $(FILE)): $(FILE) | $(ISTIO_DOCKER); cp $(FILE) $$(@D)))
-
-
-# tell make which files are copied from ISTIO_BIN and generate rules to copy them to the proper location:
-# generates rules like the following:
-# $(ISTIO_DOCKER)/kubectl: $(ISTIO_BIN)/kubectl | $(ISTIO_DOCKER)
-# 	cp $(ISTIO_BIN)/kubectl $(ISTIO_DOCKER)/kubectl
-DOCKER_FILES_FROM_ISTIO_BIN:=kubectl
-$(foreach FILE,$(DOCKER_FILES_FROM_ISTIO_BIN), \
-        $(eval $(ISTIO_DOCKER)/$(FILE): $(ISTIO_BIN)/$(FILE) | $(ISTIO_DOCKER); cp $(ISTIO_BIN)/$(FILE) $(ISTIO_DOCKER)/$(FILE)))
 
 docker.sidecar_injector: BUILD_PRE=&& chmod 755 sidecar-injector
 docker.sidecar_injector: BUILD_ARGS=--build-arg BASE_VERSION=${BASE_VERSION}
@@ -155,10 +146,6 @@ docker.app_sidecar: $(ISTIO_DOCKER)/istio-clean-iptables
 docker.test_policybackend: BUILD_ARGS=--build-arg BASE_VERSION=${BASE_VERSION}
 docker.test_policybackend: mixer/docker/Dockerfile.test_policybackend
 docker.test_policybackend: $(ISTIO_OUT_LINUX)/policybackend
-	$(DOCKER_RULE)
-
-docker.kubectl: BUILD_ARGS=--build-arg BASE_VERSION=${BASE_VERSION}
-docker.kubectl: docker/Dockerfile.kubectl
 	$(DOCKER_RULE)
 
 docker.istioctl: BUILD_ARGS=--build-arg BASE_VERSION=${BASE_VERSION}
