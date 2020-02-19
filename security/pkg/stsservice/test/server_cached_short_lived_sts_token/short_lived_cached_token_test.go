cdc925da4b5adf12cd25fa112dcbaaaa32b3299e
Jimmy Chen <yinjie@google.com>
2020-02-18 16:24:30 -0800

    revise

--- security/pkg/stsservice/test/server_cached_short_lived_sts_token/short_lived_cached_token_test.go
+++ security/pkg/stsservice/test/server_cached_short_lived_sts_token/short_lived_cached_token_test.go
@@ -28,6 +28,9 @@ import (
 // proxy calls STS server to fetch token. If the original token is going to expire,
 // STS server fetches a new token for the proxy.
 func TestServerShortLivedCachedToken(t *testing.T) {
+	// Enable this test when gRPC fix is picked by Istio Proxy
+	// https://github.com/grpc/grpc/pull/21641
+	t.Skip("https://github.com/istio/istio/issues/20133")
 	// Sets up callback that verifies token on new XDS stream.
 	cb := xdsService.CreateXdsCallback(t)
 	// Start all test servers and proxy
