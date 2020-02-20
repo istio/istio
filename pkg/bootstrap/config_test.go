7f953a01ea2d0ff406be772e35bcb0dcc7449021
John Howard <howardjohn@google.com>
2020-02-19 16:53:16 -0800

    Read labels and annotations from downward API (#21184)
    * Read labels and annotations from downward API
    
    There are a few motivations for this change:
    * Use standard kubernetes features, rather than relying on
    Helm/injection complexity. This allows people to use standard tools like
    kustomize to add labels without having things break because they need to
    change things in two places.
    * In a future PR (blocked by some other changes), I will change the
    agent to support overriding mesh config settings by annotations. For
    example, currently in the injection template we have this:
    
    ```
        - --discoveryAddress
        - "{{ annotation .ObjectMeta `sidecar.istio.io/discoveryAddress` .ProxyConfig.DiscoveryAddress }}"
    ```
    
    This is not great, because we have to have a flag just for this setting,
    rather than read it directly from mesh config, and it doesn't even work
    on gateways, so there is inconsistency. By reading the annotations in
    the code, we can instead completely remove this flag and have
    consistency between gateway and sidecar, while making both templates
    much simpler in the process.
    
    * Update goldens
    
    * update goldens
    
    * add copyright
    
    * Fix lint

---
+++ pkg/bootstrap/config_test.go
@@ -0,0 +1,64 @@
+// Copyright 2020 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bootstrap
+
+import (
+	"reflect"
+	"testing"
+
+	"k8s.io/kubectl/pkg/util/fieldpath"
+)
+
+func TestParseDownwardApi(t *testing.T) {
+	cases := []struct {
+		name string
+		m    map[string]string
+	}{
+		{
+			"empty",
+			map[string]string{},
+		},
+		{
+			"single",
+			map[string]string{"foo": "bar"},
+		},
+		{
+			"multi line",
+			map[string]string{
+				"app":               "istio-ingressgateway",
+				"chart":             "gateways",
+				"heritage":          "Tiller",
+				"istio":             "ingressgateway",
+				"pod-template-hash": "54756dbcf9",
+			},
+		},
+		{
+			"weird values",
+			map[string]string{
+				"foo": `a1_-.as1`,
+			},
+		},
+	}
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			// Using the function kubernetes actually uses to write this, we do a round trip of
+			// map -> file -> map and ensure the input and output are the same
+			got, err := ParseDownwardAPI(fieldpath.FormatMap(tt.m))
+			if !reflect.DeepEqual(got, tt.m) {
+				t.Fatalf("expected %v, got %v with err: %v", tt.m, got, err)
+			}
+		})
+	}
+}
