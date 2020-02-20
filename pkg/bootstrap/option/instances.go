b9de19129e0b680c9c498226217100c753b4b013
Jimmy Chen <yinjie@google.com>
2020-02-20 11:40:35 -0800

    restore changes

--- pkg/bootstrap/option/instances.go
+++ pkg/bootstrap/option/instances.go
@@ -243,3 +243,11 @@ func SDSTokenPath(value string) Instance {
 func PilotCertProvider(value string) Instance {
 	return newOption("pilot_cert_provider", value)
 }
+
+func STSPort(value int) Instance {
+	return newOption("sts_port", value)
+}
+
+func STSEnabled(value bool) Instance {
+	return newOption("sts", value)
+}
