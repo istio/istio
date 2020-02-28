cbb2d2cbf86b38dbc59c1877842f6f56aa655fb7
John Howard <howardjohn@google.com>
2020-02-27 17:16:30 -0800

    Make jwt check resiliant to errors (#21609)
    Fixes https://github.com/istio/istio/issues/20946
    
    (cherry picked from commit 44bbe614da1455d12433de5d270b1faef5865da0)

--- operator/cmd/mesh/profile-common.go
+++ operator/cmd/mesh/profile-common.go
@@ -23,6 +23,7 @@ import (
 
 	"istio.io/istio/operator/pkg/translate"
 
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/rest"
 
@@ -294,8 +295,14 @@ func getJwtTypeOverlay(config *rest.Config, l *Logger) (string, error) {
 		return "", fmt.Errorf("failed to determine JWT policy support. Use the --force flag to ignore this: %v", err)
 	}
 	_, s, err := d.ServerGroupsAndResources()
+	// This may fail if any api service is down. We should only fail if the specific API we care about failed
 	if err != nil {
-		return "", fmt.Errorf("failed to determine JWT policy support. Use the --force flag to ignore this: %v", err)
+		if discovery.IsGroupDiscoveryFailedError(err) {
+			derr := err.(*discovery.ErrGroupDiscoveryFailed)
+			if _, f := derr.Groups[schema.GroupVersion{Group: "authentication.k8s.io", Version: "v1"}]; f {
+				return "", fmt.Errorf("failed to determine JWT policy support. Use the --force flag to ignore this: %v", err)
+			}
+		}
 	}
 	for _, res := range s {
 		for _, api := range res.APIResources {
