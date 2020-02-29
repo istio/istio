2b671a4d6b7403042fe43d3c0effe634154c995b
John Howard <howardjohn@google.com>
2020-02-29 09:22:42 -0800

    Some improvements to integration test speeds

--- pkg/test/framework/test.go
+++ pkg/test/framework/test.go
@@ -183,8 +183,13 @@ func (t *Test) doRun(ctx *testContext, fn func(ctx TestContext), parallel bool) 
 	scopes.CI.Infof("=== BEGIN: Test: '%s[%s]' ===", rt.suiteContext().Settings().TestID, t.goTest.Name())
 	defer func() {
 		doneFn := func() {
+			message := "passed"
+			if t.goTest.Failed() {
+				message = "failed"
+			}
 			end := time.Now()
-			scopes.CI.Infof("=== DONE:  Test: '%s[%s] (%v)' ===",
+			scopes.CI.Infof("=== DONE (%s):  Test: '%s[%s] (%v)' ===",
+				message,
 				rt.suiteContext().Settings().TestID,
 				t.goTest.Name(),
 				end.Sub(start))
