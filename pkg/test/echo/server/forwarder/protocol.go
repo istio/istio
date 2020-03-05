c360514b6882cd1875036ed152321b4b3d5b05cc
Yan Xue <yxyan@google.com>
2020-03-04 21:37:56 -0800

    TCP test framework

--- pkg/test/echo/server/forwarder/protocol.go
+++ pkg/test/echo/server/forwarder/protocol.go
@@ -20,11 +20,12 @@ import (
 	"context"
 	"crypto/tls"
 	"fmt"
-	"log"
 	"net"
 	"net/http"
 	"net/url"
 	"time"
+
+	"istio.io/pkg/log"
 
 	"github.com/gorilla/websocket"
 
@@ -127,6 +128,21 @@ func newProtocol(cfg Config) (protocol, error) {
 		}
 		return &websocketProtocol{
 			dialer: dialer,
+		}, nil
+	case scheme.TCP:
+		dialer := net.Dialer{
+			Timeout: timeout,
+		}
+		ctx, cancel := context.WithTimeout(context.Background(), common.ConnectionTimeout)
+		defer cancel()
+		address := rawURL[len(u.Scheme+"://"):]
+
+		tcpConn, err := cfg.Dialer.TCP(dialer, ctx, address)
+		if err != nil {
+			return nil, err
+		}
+		return &tcpProtocol{
+			conn: tcpConn,
 		}, nil
 	}
 
