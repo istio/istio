d6fd5f7012687aba954137f81cbc48fe42a1346f
John Howard <howardjohn@google.com>
2020-03-05 14:52:06 -0800

    Revert "Fix meshconfig"
    This reverts commit 7f53a4ee5c1882c1cdc7281c1357d3e416066cf3.

--- galley/testdatasets/conversion/dataset.gen.go
+++ galley/testdatasets/conversion/dataset.gen.go
@@ -987,8 +987,7 @@ var _datasetMeshIstioIoV1alpha1Meshconfig_expectedJson = []byte(`{
                   },
                   "proxy_admin_port": 15000,
                   "service_cluster": "istio-proxy",
-                  "stat_name_length": 189,
-                  "status_port": 15020
+                  "stat_name_length": 189
                 },
                 "default_destination_rule_export_to": [
                   "*"
