65294555fe575709f300e8eee4fde8aa428f4bcd
istio-testing <istio.testing@gmail.com>
2020-03-07 00:38:00 +0000

    Automator: update common-files@master in istio/istio@master

---
+++ common/scripts/setup_env.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# WARNING: DO NOT EDIT, THIS FILE IS PROBABLY A COPY
+#
+# The original version of this file is located in the https://github.com/istio/common-files repo.
+# If you're looking at this file in a different repo and want to make a change, please go to the
+# common-files repo, make the change there and check it in. Then come back to this repo and run
+# "make update-common".
+
+# Copyright Istio Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+LOCAL_ARCH=$(uname -m)
+export LOCAL_ARCH
+if [[ ${LOCAL_ARCH} == x86_64 ]]; then
+    export TARGET_ARCH=amd64
+elif [[ ${LOCAL_ARCH} == armv8* ]]; then
+    export TARGET_ARCH=arm64
+elif [[ ${LOCAL_ARCH} == aarch64* ]]; then
+    export TARGET_ARCH=arm64
+elif [[ ${LOCAL_ARCH} == armv* ]]; then
+    export TARGET_ARCH=arm
+else
+    echo "This system's architecture, ${LOCAL_ARCH}, isn't supported"
+    exit 1
+fi
+
+LOCAL_OS=$(uname)
+export LOCAL_OS
+if [[ $LOCAL_OS == Linux ]]; then
+    export TARGET_OS=linux
+    readlink_flags="-f"
+elif [[ $LOCAL_OS == Darwin ]]; then
+    export TARGET_OS=darwin
+    readlink_flags=""
+else
+    echo "This system's OS, $LOCAL_OS, isn't supported"
+    exit 1
+fi
+
+# Build image to use
+export IMAGE_VERSION=master-2020-03-05T18-27-04
+
+export UID
+DOCKER_GID=$(grep '^docker:' /etc/group | cut -f3 -d:)
+export DOCKER_GID
+
+TIMEZONE=$(readlink $readlink_flags /etc/localtime | sed -e 's/^.*zoneinfo\///')
+export TIMEZONE
+
+export TARGET_OUT="${TARGET_OUT:-${PWD}/out/${TARGET_OS}_${TARGET_ARCH}}"
+export TARGET_OUT_LINUX="${TARGET_OUT_LINUX:-${PWD}/out/linux_amd64}"
+
+export CONTAINER_TARGET_OUT="${CONTAINER_TARGET_OUT:-/work/out/${TARGET_OS}_${TARGET_ARCH}}"
+export CONTAINER_TARGET_OUT_LINUX="${CONTAINER_TARGET_OUT_LINUX:-/work/out/linux_amd64}"
+
+export IMG="${IMG:-gcr.io/istio-testing/build-tools:${IMAGE_VERSION}}"
+
+export CONTAINER_CLI="${CONTAINER_CLI:-docker}"
+
+export ENV_BLOCKLIST="${ENV_BLOCKLIST:-^_\|PATH\|SHELL\|EDITOR\|TMUX\|USER\|HOME\|PWD\|TERM\|GO\|rvm\|SSH}"
+
+# Set up conditional host mounts for docker and kubernetes config
+export CONDITIONAL_HOST_MOUNTS=${CONDITIONAL_HOST_MOUNTS:-}
+if [[ -d "${HOME}/.docker" ]]; then
+  CONDITIONAL_HOST_MOUNTS+="--mount type=bind,source=${HOME}/.docker,destination=/config/.docker,readonly "
+fi
+if [[ -d "${HOME}/.config/gcloud" ]]; then
+  CONDITIONAL_HOST_MOUNTS+="--mount type=bind,source=${HOME}/.config/gcloud,destination=/config/.config/gcloud,readonly "
+fi
+if [[ -d "${HOME}/.kube" ]]; then
+  CONDITIONAL_HOST_MOUNTS+="--mount type=bind,source=${HOME}/.kube,destination=/home/.kube "
+fi
+
+export REPO_ROOT=/work
