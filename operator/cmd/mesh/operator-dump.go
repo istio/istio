8f1ed2691567f31c7f0e3c73350efc94d7282372
Marshall Ford <inbox@marshallford.me>
2020-03-07 11:43:10 -0600

    istioctl operator 'dump' sub command
    Signed-off-by: Marshall Ford <inbox@marshallford.me>

---
+++ operator/cmd/mesh/operator-dump.go
@@ -0,0 +1,62 @@
+// Copyright 2019 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mesh
+
+import (
+	"github.com/spf13/cobra"
+	buildversion "istio.io/pkg/version"
+)
+
+type operatorDumpArgs struct {
+	// common is shared operator args
+	common operatorCommonArgs
+}
+
+func addOperatorDumpFlags(cmd *cobra.Command, args *operatorDumpArgs) {
+	hub, tag := buildversion.DockerInfo.Hub, buildversion.DockerInfo.Tag
+	if hub == "" {
+		hub = "gcr.io/istio-testing"
+	}
+	if tag == "" {
+		tag = "latest"
+	}
+	cmd.PersistentFlags().StringVar(&args.common.hub, "hub", hub, "The hub for the operator controller image")
+	cmd.PersistentFlags().StringVar(&args.common.tag, "tag", tag, "The tag for the operator controller image")
+	cmd.PersistentFlags().StringVar(&args.common.operatorNamespace, "operatorNamespace", "istio-operator",
+		"The namespace the operator controller is installed into")
+	cmd.PersistentFlags().StringVar(&args.common.istioNamespace, "istioNamespace", "istio-system",
+		"The namespace Istio is installed into")
+}
+
+func operatorDumpCmd(rootArgs *rootArgs, odArgs *operatorDumpArgs) *cobra.Command {
+	return &cobra.Command{
+		Use:   "dump",
+		Short: "Dumps the Istio operator controller manifest.",
+		Long:  "The dump subcommand dumps the Istio operator controller manifest.",
+		Args:  cobra.ExactArgs(0),
+		Run: func(cmd *cobra.Command, args []string) {
+			l := NewLogger(rootArgs.logToStdErr, cmd.OutOrStdout(), cmd.ErrOrStderr())
+			operatorDump(rootArgs, odArgs, l)
+		}}
+}
+
+func operatorDump(args *rootArgs, odArgs *operatorDumpArgs, l *Logger) {
+	_, mstr, err := renderOperatorManifest(args, &odArgs.common, l)
+	if err != nil {
+		l.logAndFatal(err)
+	}
+
+	l.print(mstr)
+}
