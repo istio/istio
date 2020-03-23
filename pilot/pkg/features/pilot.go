9f467677d8cd1e72301f1521c252cbaaa3652c2f
John Howard <howardjohn@google.com>
2020-03-23 12:30:49 -0700

    Revert "Add default address"
    This reverts commit 6abd5411cb4ce3263ba429b4e0a9ed6746d8c45c.

--- pilot/pkg/features/pilot.go
+++ pilot/pkg/features/pilot.go
@@ -263,7 +263,7 @@ var (
 	// imposed by K8S - that's how the names for webhooks are defined, based on webhook service (which will be
 	// istio-pilot or istiod) plus namespace and .svc.
 	// The 15010 port is used with plain text, 15011 with Spiffee certs - we need a different port for DNS cert.
-	IstiodService = env.RegisterStringVar("ISTIOD_ADDR", "istiod.istio-system.svc:15012",
+	IstiodService = env.RegisterStringVar("ISTIOD_ADDR", "",
 		"Service name of istiod. If empty the istiod listener, certs will be disabled.")
 
 	PilotCertProvider = env.RegisterStringVar("PILOT_CERT_PROVIDER", "istiod",
