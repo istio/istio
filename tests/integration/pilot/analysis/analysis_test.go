2082b0b17327a7fabcd558492907339e0a198195
therealmitchconnors <mitchconnors@gmail.com>
2020-03-24 11:40:25 -0700

    First pass at test.  Does not work yet.

---
+++ tests/integration/pilot/analysis/analysis_test.go
@@ -0,0 +1,79 @@
+// Copyright 2019 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package analysis
+
+import (
+	"istio.io/istio/pkg/test/framework"
+	"istio.io/istio/pkg/test/framework/components/bookinfo"
+	"istio.io/istio/pkg/test/framework/components/deployment"
+	"istio.io/istio/pkg/test/framework/components/environment/kube"
+	"istio.io/istio/pkg/test/framework/components/namespace"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"strings"
+	"testing"
+)
+
+func TestAnalysisWritesStatus(t *testing.T) {
+	framework.NewTest(t).Run(func(ctx framework.TestContext) {
+		ns := setupModifiedBookinfo(t, ctx)
+		doTest(t, ctx, ns)
+	})
+}
+
+func setupModifiedBookinfo(t *testing.T, ctx framework.TestContext) namespace.Instance {
+	ns := namespace.NewOrFail(t, ctx, namespace.Config{
+		Prefix:   "default",
+		Inject:   true,
+		Revision: "",
+		Labels:   nil,
+	})
+	bookinfo.DeployOrFail(t, ctx, bookinfo.Config{
+		Namespace: ns,
+		Cfg:       bookinfo.BookinfoRatingsv2,
+	})
+	gatewayYaml := bookinfo.NetworkingBookinfoGateway.LoadGatewayFileWithNamespaceOrFail(t, ns.Name())
+	_, err := deployment.New(ctx, deployment.Config{
+		Name:      "bookinfo-gateway",
+		Namespace: ns,
+		Yaml:      gatewayYaml,
+	})
+	if err != nil {
+		t.Fatalf("TODO: make a better failure message: %v", err)
+	}
+	vsYaml := bookinfo.NetworkingVirtualServiceAllV1.LoadWithNamespaceOrFail(t, ns.Name())
+	vsYaml = strings.Replace(vsYaml, "old", "new", -1)
+	_, err = deployment.New(ctx, deployment.Config{
+		Name:      "bookinfo-virtualservice",
+		Namespace: ns,
+		Yaml:      vsYaml,
+	})
+	return ns
+}
+
+func doTest(t *testing.T, ctx framework.TestContext, ns namespace.Instance){
+	x, err := kube.ClusterOrDefault(nil, ctx.Environment()).GetUnstructured(schema.GroupVersionResource{
+		Group:    "networking.istio.io",
+		Version:  "v1alpha3",
+		Resource: "virtualservices",
+	}, ns.Name(), "reviews")
+	if err != nil {
+		t.Fatalf("TODO: another error message here: %v", err)
+	}
+
+	if x.Object["status"] == nil {
+		t.Fatalf("Object is missing expected status field.  Actual object is: %v", x)
+	}
+}
+
