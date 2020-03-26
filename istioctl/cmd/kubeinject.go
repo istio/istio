a4cd507a96f22b8e9d94870f0c135d52d2750519
Ed Snible <snible@us.ibm.com>
2020-03-26 10:31:13 -0400

    Error message refers to incorrect CLI option

--- istioctl/cmd/kubeinject.go
+++ istioctl/cmd/kubeinject.go
@@ -89,7 +89,7 @@ func getValuesFromConfigMap(kubeconfig string) (string, error) {
 	meshConfigMap, err := client.CoreV1().ConfigMaps(istioNamespace).Get(injectConfigMapName, metav1.GetOptions{})
 	if err != nil {
 		return "", fmt.Errorf("could not find valid configmap %q from namespace  %q: %v - "+
-			"Use --injectConfigFile or re-run kube-inject with `-i <istioSystemNamespace> and ensure istio-inject configmap exists",
+			"Use --valuesFile or re-run kube-inject with `-i <istioSystemNamespace> and ensure istio-inject configmap exists",
 			injectConfigMapName, istioNamespace, err)
 	}
 
