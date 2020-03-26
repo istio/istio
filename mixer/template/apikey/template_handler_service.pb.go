702025db0bf66e5392d5087f5d9fe369188f4234
Francois Pesce <fpesce@google.com>
2020-03-26 13:26:41 -0700

    make gen

--- mixer/template/apikey/template_handler_service.pb.go
+++ mixer/template/apikey/template_handler_service.pb.go
@@ -1404,6 +1404,7 @@ func (m *InstanceParam) Unmarshal(dAtA []byte) error {
 func skipTemplateHandlerService(dAtA []byte) (n int, err error) {
 	l := len(dAtA)
 	iNdEx := 0
+	depth := 0
 	for iNdEx < l {
 		var wire uint64
 		for shift := uint(0); ; shift += 7 {
@@ -1435,10 +1436,8 @@ func skipTemplateHandlerService(dAtA []byte) (n int, err error) {
 					break
 				}
 			}
-			return iNdEx, nil
 		case 1:
 			iNdEx += 8
-			return iNdEx, nil
 		case 2:
 			var length int
 			for shift := uint(0); ; shift += 7 {
@@ -1459,55 +1458,30 @@ func skipTemplateHandlerService(dAtA []byte) (n int, err error) {
 				return 0, ErrInvalidLengthTemplateHandlerService
 			}
 			iNdEx += length
-			if iNdEx < 0 {
-				return 0, ErrInvalidLengthTemplateHandlerService
-			}
-			return iNdEx, nil
 		case 3:
-			for {
-				var innerWire uint64
-				var start int = iNdEx
-				for shift := uint(0); ; shift += 7 {
-					if shift >= 64 {
-						return 0, ErrIntOverflowTemplateHandlerService
-					}
-					if iNdEx >= l {
-						return 0, io.ErrUnexpectedEOF
-					}
-					b := dAtA[iNdEx]
-					iNdEx++
-					innerWire |= (uint64(b) & 0x7F) << shift
-					if b < 0x80 {
-						break
-					}
-				}
-				innerWireType := int(innerWire & 0x7)
-				if innerWireType == 4 {
-					break
-				}
-				next, err := skipTemplateHandlerService(dAtA[start:])
-				if err != nil {
-					return 0, err
-				}
-				iNdEx = start + next
-				if iNdEx < 0 {
-					return 0, ErrInvalidLengthTemplateHandlerService
-				}
-			}
-			return iNdEx, nil
+			depth++
 		case 4:
-			return iNdEx, nil
+			if depth == 0 {
+				return 0, ErrUnexpectedEndOfGroupTemplateHandlerService
+			}
+			depth--
 		case 5:
 			iNdEx += 4
-			return iNdEx, nil
 		default:
 			return 0, fmt.Errorf("proto: illegal wireType %d", wireType)
 		}
+		if iNdEx < 0 {
+			return 0, ErrInvalidLengthTemplateHandlerService
+		}
+		if depth == 0 {
+			return iNdEx, nil
+		}
 	}
-	panic("unreachable")
+	return 0, io.ErrUnexpectedEOF
 }
 
 var (
-	ErrInvalidLengthTemplateHandlerService = fmt.Errorf("proto: negative length found during unmarshaling")
-	ErrIntOverflowTemplateHandlerService   = fmt.Errorf("proto: integer overflow")
+	ErrInvalidLengthTemplateHandlerService        = fmt.Errorf("proto: negative length found during unmarshaling")
+	ErrIntOverflowTemplateHandlerService          = fmt.Errorf("proto: integer overflow")
+	ErrUnexpectedEndOfGroupTemplateHandlerService = fmt.Errorf("proto: unexpected end of group")
 )
--- mixer/template/sample/check/CheckTesterTemplate_handler_service.pb.go
+++ mixer/template/sample/check/CheckTesterTemplate_handler_service.pb.go
@@ -5217,6 +5217,7 @@ func (m *Res2InstanceParam) Unmarshal(dAtA []byte) error {
 func skipCheckTesterTemplateHandlerService(dAtA []byte) (n int, err error) {
 	l := len(dAtA)
 	iNdEx := 0
+	depth := 0
 	for iNdEx < l {
 		var wire uint64
 		for shift := uint(0); ; shift += 7 {
@@ -5248,10 +5249,8 @@ func skipCheckTesterTemplateHandlerService(dAtA []byte) (n int, err error) {
 					break
 				}
 			}
-			return iNdEx, nil
 		case 1:
 			iNdEx += 8
-			return iNdEx, nil
 		case 2:
 			var length int
 			for shift := uint(0); ; shift += 7 {
@@ -5272,55 +5271,30 @@ func skipCheckTesterTemplateHandlerService(dAtA []byte) (n int, err error) {
 				return 0, ErrInvalidLengthCheckTesterTemplateHandlerService
 			}
 			iNdEx += length
-			if iNdEx < 0 {
-				return 0, ErrInvalidLengthCheckTesterTemplateHandlerService
-			}
-			return iNdEx, nil
 		case 3:
-			for {
-				var innerWire uint64
-				var start int = iNdEx
-				for shift := uint(0); ; shift += 7 {
-					if shift >= 64 {
-						return 0, ErrIntOverflowCheckTesterTemplateHandlerService
-					}
-					if iNdEx >= l {
-						return 0, io.ErrUnexpectedEOF
-					}
-					b := dAtA[iNdEx]
-					iNdEx++
-					innerWire |= (uint64(b) & 0x7F) << shift
-					if b < 0x80 {
-						break
-					}
-				}
-				innerWireType := int(innerWire & 0x7)
-				if innerWireType == 4 {
-					break
-				}
-				next, err := skipCheckTesterTemplateHandlerService(dAtA[start:])
-				if err != nil {
-					return 0, err
-				}
-				iNdEx = start + next
-				if iNdEx < 0 {
-					return 0, ErrInvalidLengthCheckTesterTemplateHandlerService
-				}
-			}
-			return iNdEx, nil
+			depth++
 		case 4:
-			return iNdEx, nil
+			if depth == 0 {
+				return 0, ErrUnexpectedEndOfGroupCheckTesterTemplateHandlerService
+			}
+			depth--
 		case 5:
 			iNdEx += 4
-			return iNdEx, nil
 		default:
 			return 0, fmt.Errorf("proto: illegal wireType %d", wireType)
 		}
+		if iNdEx < 0 {
+			return 0, ErrInvalidLengthCheckTesterTemplateHandlerService
+		}
+		if depth == 0 {
+			return iNdEx, nil
+		}
 	}
-	panic("unreachable")
+	return 0, io.ErrUnexpectedEOF
 }
 
 var (
-	ErrInvalidLengthCheckTesterTemplateHandlerService = fmt.Errorf("proto: negative length found during unmarshaling")
-	ErrIntOverflowCheckTesterTemplateHandlerService   = fmt.Errorf("proto: integer overflow")
+	ErrInvalidLengthCheckTesterTemplateHandlerService        = fmt.Errorf("proto: negative length found during unmarshaling")
+	ErrIntOverflowCheckTesterTemplateHandlerService          = fmt.Errorf("proto: integer overflow")
+	ErrUnexpectedEndOfGroupCheckTesterTemplateHandlerService = fmt.Errorf("proto: unexpected end of group")
 )
