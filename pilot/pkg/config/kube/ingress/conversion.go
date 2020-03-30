ba3fcb1844c2dea5fcc020fd223f263a8f23066d
John Howard <howardjohn@google.com>
2020-03-30 14:49:44 -0700

    Support new Ingress pathType

--- pilot/pkg/config/kube/ingress/conversion.go
+++ pilot/pkg/config/kube/ingress/conversion.go
@@ -144,8 +144,29 @@ func ConvertIngressVirtualService(ingress v1beta1.Ingress, domainSuffix string, 
 
 		httpRoutes := make([]*networking.HTTPRoute, 0)
 		for _, httpPath := range rule.HTTP.Paths {
-			httpMatch := &networking.HTTPMatchRequest{
-				Uri: createStringMatch(httpPath.Path),
+			httpMatch := &networking.HTTPMatchRequest{}
+			if httpPath.PathType != nil {
+				switch *httpPath.PathType {
+				case v1beta1.PathTypeExact:
+					httpMatch.Uri = &networking.StringMatch{
+						MatchType: &networking.StringMatch_Exact{Exact: httpPath.Path},
+					}
+				case v1beta1.PathTypePrefix:
+					// From the spec: /foo/bar matches /foo/bar/baz, but does not match /foo/barbaz
+					// Envoy prefix match behaves differently, so insert a / if we don't have one
+					path := httpPath.Path
+					if !strings.HasSuffix(path, "/") {
+						path += "/"
+					}
+					httpMatch.Uri = &networking.StringMatch{
+						MatchType: &networking.StringMatch_Prefix{Prefix: path},
+					}
+				default:
+					// Fallback to the legacy string matching
+					httpMatch.Uri = createFallbackStringMatch(httpPath.Path)
+				}
+			} else {
+				httpMatch.Uri = createFallbackStringMatch(httpPath.Path)
 			}
 
 			httpRoute := ingressBackendToHTTPRoute(&httpPath.Backend, ingress.Namespace, domainSuffix)
@@ -248,7 +269,7 @@ func shouldProcessIngress(mesh *meshconfig.MeshConfig, ingress *v1beta1.Ingress)
 	}
 }
 
-func createStringMatch(s string) *networking.StringMatch {
+func createFallbackStringMatch(s string) *networking.StringMatch {
 	if s == "" {
 		return nil
 	}
