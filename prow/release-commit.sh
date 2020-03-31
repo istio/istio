c7ac67d1d69e7ac6e13c11043c8df414cd2ef8ef
John Howard <howardjohn@google.com>
2020-03-31 14:45:57 -0700

    Pull in dependency updates
    This pulls in fixes in release-builder and CNI

--- prow/release-commit.sh
+++ prow/release-commit.sh
@@ -32,7 +32,7 @@ DOCKER_HUB=${DOCKER_HUB:-gcr.io/istio-testing}
 GCS_BUCKET=${GCS_BUCKET:-istio-build/dev}
 
 # Use a pinned version in case breaking changes are needed
-BUILDER_SHA=39ec4ecf1691512a37d26dde0b12f6dc4d65dbb7
+BUILDER_SHA=91bfbef1e80a6726c771902dd80ea926f070d2d7
 
 # Reference to the next minor version of Istio
 # This will create a version like 1.4-alpha.sha
