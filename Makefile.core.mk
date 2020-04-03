8a5ce743060f638802e4389d8c624b7894da435d
dave93cab <davejonthomas@gmail.com>
2020-04-02 23:19:31 -0500

    ARM additions cleanup: removal of istioctl-linux (#22567)

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -383,10 +383,6 @@ ${ISTIO_OUT}/release/istioctl-osx: depend
 ${ISTIO_OUT}/release/istioctl-win.exe: depend
 	STATIC=0 GOOS=windows LDFLAGS=$(RELEASE_LDFLAGS) common/scripts/gobuild.sh $@ ./istioctl/cmd/istioctl
 
-# legacy istioctl-linux (duplicate of istioctl-linux-amd64) remaining until istio/release-builder is updated, at which point it can be removed
-${ISTIO_OUT}/release/istioctl-linux: depend
-	STATIC=0 GOOS=linux GOARCH=amd64 LDFLAGS=$(RELEASE_LDFLAGS) common/scripts/gobuild.sh $@ ./istioctl/cmd/istioctl
-
 # generate the istioctl completion files
 ${ISTIO_OUT}/release/istioctl.bash: istioctl
 	${LOCAL_OUT}/istioctl collateral --bash && \
@@ -401,12 +397,10 @@ binaries-test:
 	go test ${GOBUILDFLAGS} ./tests/binary/... -v --base-dir ${ISTIO_OUT} --binaries="$(RELEASE_BINARIES)"
 
 # istioctl-all makes all of the non-static istioctl executables for each supported OS
-# legacy istioctl-linux (duplicate of istioctl-linux-amd64) remaining until istio/release-builder is updated, at which point it can be removed
 .PHONY: istioctl-all
 istioctl-all: ${ISTIO_OUT}/release/istioctl-linux-amd64 ${ISTIO_OUT}/release/istioctl-linux-armv7 ${ISTIO_OUT}/release/istioctl-linux-arm64 \
 	${ISTIO_OUT}/release/istioctl-osx \
-	${ISTIO_OUT}/release/istioctl-win.exe \
-	${ISTIO_OUT}/release/istioctl-linux
+	${ISTIO_OUT}/release/istioctl-win.exe
 
 .PHONY: istioctl.completion
 istioctl.completion: ${ISTIO_OUT}/release/istioctl.bash ${ISTIO_OUT}/release/_istioctl
