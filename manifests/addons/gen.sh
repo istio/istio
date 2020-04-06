c137b411f0d0a3df88789b8132889b7f19171060
John Howard <howardjohn@google.com>
2020-04-06 15:25:16 -0700

    Initial prometheus

---
+++ manifests/addons/gen.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+# Copyright Istio Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+WD=$(dirname "$0")
+WD=$(cd "$WD"; pwd)
+
+set -eux
+
+ADDONS="${WD}/../../samples/addons"
+DASHBOARDS="${WD}/../charts/istio-telemetry/grafana/dashboards"
+mkdir -p "${ADDONS}"
+
+helm3 template prometheus stable/prometheus \
+  --namespace istio-system \
+  --version 11.0.2 \
+  -f "${WD}/values-prometheus.yaml" \
+  > "${ADDONS}/prometheus.yaml"
+
+helm3 template grafana stable/grafana \
+  --namespace istio-system \
+  --version 5.0.7 \
+  -f "${WD}/values-grafana.yaml" \
+  > "${ADDONS}/grafana.yaml"
+
+# Set up grafana dashboards. Split into 2 to avoid Kubernetes size limits
+echo -e "\n---\n" >> "${ADDONS}/grafana.yaml"
+kubectl create configmap istio-grafana-dashboards \
+  --dry-run=client -oyaml \
+  --from-file=pilot-dashboard.json="${DASHBOARDS}/pilot-dashboard.json" \
+  --from-file=mixer-dashboard.json="${DASHBOARDS}/mixer-dashboard.json" \
+  --from-file=istio-performance-dashboard.json="${DASHBOARDS}/istio-performance-dashboard.json" \
+  >> "${ADDONS}/grafana.yaml"
+echo -e "\n---\n" >> "${ADDONS}/grafana.yaml"
+kubectl create configmap istio-services-grafana-dashboards \
+  --dry-run=client -oyaml \
+  --from-file=istio-workload-dashboard.json="${DASHBOARDS}/istio-workload-dashboard.json" \
+  --from-file=istio-service-dashboard.json="${DASHBOARDS}/istio-service-dashboard.json" \
+  --from-file=istio-mesh-dashboard.json="${DASHBOARDS}/istio-mesh-dashboard.json" \
+  >> "${ADDONS}/grafana.yaml"
+
+# Zipkin does not have a helm chart, but the deployment is trivial
+cp "${WD}/zipkin.yaml" "${ADDONS}/zipkin.yaml"
