c0ec054b29d7d6b90b37cd0a9c24804a953c4a1a
Nupur Garg <37600866+gargnupur@users.noreply.github.com>
2020-04-06 20:27:47 -0700

    Apply ratelimiting to external services (#21437)
    * Ratelimit External Requests
    
    Fix unit tests
    
    Fix based on reviews
    
    Fix ci errors
    
    Make test less flaky
    
    Fixed golang error
    
    looks like pods are using http/2.. changing test to reflect that when verifying status returned
    
    fix flakiness
    
    Fixed govet error
    
    Fixed golang error
    
    Fix fmt
    
    nit: correct a comment on yaml file
    
    Fix lint error
    
    Fix golang error
    
    Fix test
    
    Fix golang errors
    
    Fix istio config store
    
    Fixed based on feedback
    
    Fix build error
    
    Fix formatting
    
    * Fix tests
    
    Signed-off-by: gargnupur <gargnupur@google.com>
    
    * Run make gen
    
    Signed-off-by: gargnupur <gargnupur@google.com>
    
    * Fixed based on feedback
    
    Signed-off-by: gargnupur <gargnupur@google.com>
    
    * Changing back to using "istio.io/istio/pilot/pkg/networking/plugin/mixer/client"  as compared  to "istio.io/api/mixer/v1/config/client" as mixer was changed to use above in PR https://github.com/istio/istio/pull/17132/
    
    Signed-off-by: gargnupur <gargnupur@google.com>
    
    * Remove extra {}
    
    Signed-off-by: gargnupur <gargnupur@google.com>
    
    * Fix goland formatting!
    
    Signed-off-by: gargnupur <gargnupur@google.com>
    
    * Fix goland formatting!
    
    Signed-off-by: gargnupur <gargnupur@google.com>

--- pilot/pkg/model/config_test.go
+++ pilot/pkg/model/config_test.go
@@ -23,7 +23,6 @@ import (
 	"github.com/davecgh/go-spew/spew"
 	"github.com/gogo/protobuf/proto"
 
-	mccpb "istio.io/api/mixer/v1/config/client"
 	networking "istio.io/api/networking/v1alpha3"
 	rbacproto "istio.io/api/rbac/v1alpha1"
 	authz "istio.io/api/security/v1beta1"
@@ -31,6 +30,7 @@ import (
 
 	"istio.io/istio/pilot/pkg/config/memory"
 	"istio.io/istio/pilot/pkg/model"
+	mccpb "istio.io/istio/pilot/pkg/networking/plugin/mixer/client"
 	mock_config "istio.io/istio/pilot/test/mock"
 	"istio.io/istio/pkg/config/constants"
 	"istio.io/istio/pkg/config/host"
@@ -629,11 +629,7 @@ func TestIstioConfigStore_QuotaSpecByDestination(t *testing.T) {
 		},
 	}
 	ii := model.MakeIstioStore(l)
-	cfgs := ii.QuotaSpecByDestination(&model.ServiceInstance{
-		Service: &model.Service{
-			Hostname: host.Name("a." + ns + ".svc.cluster.local"),
-		},
-	})
+	cfgs := ii.QuotaSpecByDestination(host.Name("a." + ns + ".svc.cluster.local"))
 
 	if len(cfgs) != 1 {
 		t.Fatalf("did not find 1 matched quota")
