8414bd35b18834d9b503e43fba9eae4bc871fba5
John Howard <howardjohn@google.com>
2020-04-09 17:07:40 -0700

    Use jsonpb for validation

--- operator/cmd/mesh/manifest-generate_test.go
+++ operator/cmd/mesh/manifest-generate_test.go
@@ -187,6 +187,8 @@ func TestManifestGeneratePilot(t *testing.T) {
 			desc:       "pilot_override_kubernetes",
 			diffSelect: "Deployment:*:istiod, Service:*:istiod",
 		},
+		// TODO https://github.com/istio/istio/issues/22347 this is broken for overriding things to default value
+		// This can be seen from REGISTRY_ONLY not applying
 		{
 			desc:       "pilot_merge_meshconfig",
 			diffSelect: "ConfigMap:*:istio$",
