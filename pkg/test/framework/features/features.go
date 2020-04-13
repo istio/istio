81be7b4d3a9c6f8dcf5ddaa1fddd76d42e0d2457
therealmitchconnors <mitchconnors@gmail.com>
2020-04-13 10:20:53 -0700

    abandon stringer generation, and add copyright

--- pkg/test/framework/features/features.go
+++ pkg/test/framework/features/features.go
@@ -1,3 +1,17 @@
+// Copyright 2019 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package features
 
 // TODO: this file should be generated from YAML to make it more easy to modify.
@@ -5,11 +19,9 @@ package features
 // WARNING: changes to existing elements in this file will cause corruption of test coverage data.
 // don't change existing entries unless absolutely necessary
 
-//go:generate stringer -type=Feature
-
-type Feature int
+type Feature string
 
 const (
-	USABILITY_OBSERVABILITY_STATUS Feature = iota
-	USABILITY_OBSERVABILITY_STATUS_DEFAULTEXISTS
+	USABILITY_OBSERVABILITY_STATUS Feature = "Usability.Observability.Status"
+	USABILITY_OBSERVABILITY_STATUS_DEFAULTEXISTS = "Usability.Observability.Status.DefaultExists"
 )
