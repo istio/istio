004b1e35d9cff8563844887c5a819c0ba37312fb
Xinnan Wen <iamwen@google.com>
2020-04-13 19:02:32 -0700

    support for setting k8s overlay from values (#22662)
    * support for setting k8s overlay from values
    
    * add warning for gateways, add test
    
    * fix test
    
    * address comment
    
    * fix test for reconciling, add reverse translate logic for controller

--- operator/pkg/translate/translate_value_test.go
+++ operator/pkg/translate/translate_value_test.go
@@ -140,6 +140,7 @@ values:
     telemetryNamespace: istio-telemetry
   pilot:
     image: pilot
+    autoscaleEnabled: true
     traceSampling: 1
     podAntiAffinityLabelSelector:
     - key: istio
@@ -270,7 +271,7 @@ values:
 				ms := jsonpb.Marshaler{}
 				gotString, err := ms.MarshalToString(gotSpec)
 				if err != nil {
-					t.Errorf("error when marshal translated IstioOperatorSpec: %s", err)
+					t.Errorf("failed to marshal translated IstioOperatorSpec: %s", err)
 				}
 				cpYaml, _ := yaml.JSONToYAML([]byte(gotString))
 				if want := tt.want; !util.IsYAMLEqual(gotString, want) {
