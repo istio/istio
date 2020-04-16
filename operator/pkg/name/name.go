7f6a6b9748034402474829884241eb026cdd60f0
Martin Ostrowski <mostrowski@google.com>
2020-04-16 10:24:38 -0700

    Make missing charts error message more helpful

--- operator/pkg/name/name.go
+++ operator/pkg/name/name.go
@@ -191,6 +191,12 @@ var onceErr error
 
 func ScanBundledAddonComponents(chartsRootDir string) error {
 	scanAddons.Do(func() {
+		if chartsRootDir == "" {
+			if onceErr = helm.CheckCompiledInCharts(); onceErr != nil {
+				return
+			}
+		}
+
 		var addonComponentNames []string
 		addonComponentNames, onceErr = helm.GetAddonNamesFromCharts(chartsRootDir, true)
 		if onceErr != nil {
