1f043b0447e3d1c4f55c4edcd43f850bfbdbfe67
John Howard <howardjohn@google.com>
2020-04-16 12:01:38 -0700

    Use a distinct lock id for Istiod (#22905)
    * Individual locks
    
    * Update controller.go

--- pilot/pkg/leaderelection/leaderelection.go
+++ pilot/pkg/leaderelection/leaderelection.go
@@ -27,8 +27,13 @@ import (
 	"istio.io/pkg/log"
 )
 
+// Various locks used throughout the code
 const (
-	electionID = "istio-leader"
+	NamespaceController  = "istio-namespace-controller-election"
+	ValidationController = "istio-validation-controller-election"
+	// This holds the legacy name to not conflict with older control plane deployments which are just
+	// doing the ingress syncing.
+	IngressController = "istio-leader"
 )
 
 type LeaderElection struct {
@@ -40,7 +45,8 @@ type LeaderElection struct {
 
 	// Records which "cycle" the election is on. This is incremented each time an election is won and then lost
 	// This is mostly just for testing
-	cycle *atomic.Int32
+	cycle      *atomic.Int32
+	electionID string
 }
 
 // Run will start leader election, calling all runFns when we become the leader.
@@ -84,7 +90,7 @@ func (l *LeaderElection) create() (*leaderelection.LeaderElector, error) {
 		},
 	}
 	lock := resourcelock.ConfigMapLock{
-		ConfigMapMeta: metaV1.ObjectMeta{Namespace: l.namespace, Name: electionID},
+		ConfigMapMeta: metaV1.ObjectMeta{Namespace: l.namespace, Name: l.electionID},
 		Client:        l.client.CoreV1(),
 		LockConfig: resourcelock.ResourceLockConfig{
 			Identity: l.name,
@@ -107,15 +113,17 @@ func (l *LeaderElection) create() (*leaderelection.LeaderElector, error) {
 
 // AddRunFunction registers a function to run when we are the leader. These will be run asynchronously.
 // To avoid running when not a leader, functions should respect the stop channel.
-func (l *LeaderElection) AddRunFunction(f func(stop <-chan struct{})) {
+func (l *LeaderElection) AddRunFunction(f func(stop <-chan struct{})) *LeaderElection {
 	l.runFns = append(l.runFns, f)
+	return l
 }
 
-func NewLeaderElection(namespace, name string, client kubernetes.Interface) *LeaderElection {
+func NewLeaderElection(namespace, name, electionID string, client kubernetes.Interface) *LeaderElection {
 	return &LeaderElection{
-		namespace: namespace,
-		name:      name,
-		client:    client,
+		namespace:  namespace,
+		name:       name,
+		electionID: electionID,
+		client:     client,
 		// Default to a 30s ttl. Overridable for tests
 		ttl:   time.Second * 30,
 		cycle: atomic.NewInt32(0),
