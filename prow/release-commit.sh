5f83dc0bc4d6895d913db0beb730387353eb0c2a
Eric Van Norman <ericvn@us.ibm.com>
2020-04-19 09:07:34 -0500

    Update with another cni change

--- prow/release-commit.sh
+++ prow/release-commit.sh
@@ -37,7 +37,7 @@ DOCKER_HUB=${DOCKER_HUB:-gcr.io/istio-testing}
 GCS_BUCKET=${GCS_BUCKET:-istio-build/dev}
 
 # Use a pinned version in case breaking changes are needed
-BUILDER_SHA=ce7f73c0b188386fbaec3b8651f7acaabef1fbed
+BUILDER_SHA=226efab93b38fe2573eccecdedcbaa5cea51a3be
 
 # Reference to the next minor version of Istio
 # This will create a version like 1.4-alpha.sha
