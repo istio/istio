99ef666d0edb56afecca3111ae5e280ee5784b39
Tao HE <1579288+elfinhe@users.noreply.github.com>
2020-04-20 13:14:25 -0700

    Fix undefined “IOPS” wording and confusing validation in upgrade (#22908)
    * Fix undefined “IOPS” wording and a validation in upgrade
    
    * Fix lint

--- operator/cmd/mesh/upgrade.go
+++ operator/cmd/mesh/upgrade.go
@@ -134,7 +134,7 @@ func upgrade(rootArgs *rootArgs, args *upgradeArgs, l *Logger) (err error) {
 	// Generate IOPS parseObjectSetFromManifest
 	targetIOPSYaml, targetIOPS, err := GenerateConfig(args.inFilenames, ysf, args.force, nil, l)
 	if err != nil {
-		return fmt.Errorf("failed to generate IOPS from file %s, error: %s", args.inFilenames, err)
+		return fmt.Errorf("failed to generate Istio configs from file %s, error: %s", args.inFilenames, err)
 	}
 
 	// Get the target version from the tag in the IOPS
@@ -194,7 +194,7 @@ func upgrade(rootArgs *rootArgs, args *upgradeArgs, l *Logger) (err error) {
 	}
 	currentProfileIOPSYaml, _, err := GenerateConfig(nil, ysf, args.force, nil, l)
 	if err != nil {
-		return fmt.Errorf("failed to generate IOPS from file: %s for the current version: %s, error: %v",
+		return fmt.Errorf("failed to generate Istio configs from file %s for the current version: %s, error: %v",
 			args.inFilenames, currentVersion, err)
 	}
 	checkUpgradeIOPS(currentProfileIOPSYaml, targetIOPSYaml, overrideIOPSYaml, l)
