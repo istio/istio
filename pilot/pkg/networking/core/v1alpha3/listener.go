bd564af3706c0f6275a0f6ec7ed6287479b41442
Jacob Delgado <jacob.delgado@volunteers.acasi.info>
2020-04-20 13:56:15 -0600

    Support Envoy max_path_tag_length used in tracing
    Fixes https://github.com/envoyproxy/envoy/pull/8095
    
    When the user has specified a max_path_tag_length in MeshConfig,
    propagate that setting to the http_connection_manager filter for
    proxies.

--- pilot/pkg/networking/core/v1alpha3/listener.go
+++ pilot/pkg/networking/core/v1alpha3/listener.go
@@ -39,6 +39,7 @@ import (
 	"github.com/envoyproxy/go-control-plane/pkg/wellknown"
 	"github.com/golang/protobuf/ptypes"
 	structpb "github.com/golang/protobuf/ptypes/struct"
+	"github.com/golang/protobuf/ptypes/wrappers"
 
 	meshconfig "istio.io/api/mesh/v1alpha1"
 	networking "istio.io/api/networking/v1alpha3"
@@ -2006,10 +2007,22 @@ func buildTracingConfig(config *meshconfig.ProxyConfig, proxyType model.NodeType
 		},
 	}
 
-	// custom tags should only be used for sidecar proxies and should not include
-	// gateways due to client requests from outside of the mesh
-	if config.Tracing != nil && len(config.Tracing.CustomTags) != 0 && proxyType == model.SidecarProxy {
-		tracingCfg.CustomTags = buildCustomTags(config.Tracing.CustomTags)
+	if config.Tracing != nil {
+		// only specify a MaxPathTagLength if meshconfig has specified one
+		// otherwise, rely on upstream envoy defaults
+		if config.Tracing.MaxPathTagLength != 0 {
+			tracingCfg.MaxPathTagLength =
+				&wrappers.UInt32Value{
+					Value: config.Tracing.MaxPathTagLength,
+				}
+		}
+
+		// custom tags should only be used for sidecar proxies and should not include
+		// gateways due to client requests from outside of the mesh
+		if len(config.Tracing.CustomTags) != 0 && proxyType == model.SidecarProxy {
+			tracingCfg.CustomTags = buildCustomTags(config.Tracing.CustomTags)
+
+		}
 	}
 
 	return tracingCfg
