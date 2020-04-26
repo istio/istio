9226687f8517036f8d97d14b3c011b7d50199a48
Rama Chavali <rama.rao@salesforce.com>
2020-04-26 11:18:21 +0530

    eds: reduce locks during eds updates (#23266)
    * reduce locks during eds updates
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * lint
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * fix race
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * rename method
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/proxy/envoy/v2/discovery.go
+++ pilot/pkg/proxy/envoy/v2/discovery.go
@@ -27,6 +27,7 @@ import (
 	"istio.io/istio/pilot/pkg/features"
 	"istio.io/istio/pilot/pkg/model"
 	"istio.io/istio/pilot/pkg/networking/core"
+	"istio.io/istio/pilot/pkg/util/sets"
 )
 
 var (
@@ -143,7 +144,7 @@ type EndpointShards struct {
 	// current list, a full push will be forced, to trigger a secure naming update.
 	// Due to the larger time, it is still possible that connection errors will occur while
 	// CDS is updated.
-	ServiceAccounts map[string]bool
+	ServiceAccounts sets.Set
 }
 
 // NewDiscoveryServer creates DiscoveryServer that sources data from Pilot's internal mesh data structures
