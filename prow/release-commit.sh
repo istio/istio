14c5b6d79a85912c191338a2188c2df716749b1e
Pengyuan Bian <bianpengyuan@google.com>
2020-04-28 03:06:26 +0000

    update istio deps

--- prow/release-commit.sh
+++ prow/release-commit.sh
@@ -32,7 +32,7 @@ DOCKER_HUB=${DOCKER_HUB:-gcr.io/istio-testing}
 GCS_BUCKET=${GCS_BUCKET:-istio-build/dev}
 
 # Use a pinned version in case breaking changes are needed
-BUILDER_SHA=da65893f8a333034dc6ff0bee835a5cc79dd58c3
+BUILDER_SHA=6e92a3075f2abb7c03b23f90a9835dfdd430ee87
 
 # Reference to the next minor version of Istio
 # This will create a version like 1.4-alpha.sha
