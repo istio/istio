e65cbcd5dd1f8521d1765b54bff8067f29e2b90b
John Howard <howardjohn@google.com>
2020-04-29 13:27:37 -0700

    Improvements to operator pruning (#23314)
    * Improvements to operator pruning
    
    Currently the pruning is very slow and offers no feedback to the user.
    This results in a ~15s period of nothing happening, which is confusing.
    This PR improves this:
    
    * Install time 32s -> 20s on my GKE cluster
    * New logging for prune
    
    This is done by reducing the number of list calls. Previously this was
    O(config types * components), now its just O(config types).
    Additionally, we can reduce the number of config types by only looking
    at resources we actually are producing in our installation.
    
    * Fix lint
    
    (cherry picked from commit 364a0d56f7e5218f96f2d7dd8cf62a535e90d192)

--- operator/pkg/util/progress_test.go
+++ operator/pkg/util/progress_test.go
@@ -60,4 +60,10 @@ func TestProgressLog(t *testing.T) {
 
 	foo.ReportFinished()
 	expect(`✔ Component foo installed`)
+
+	p.SetState(StatePruning)
+	expect(`- Pruning removed resources`)
+
+	p.SetState(StateComplete)
+	expect(`✔ Installation complete`)
 }
