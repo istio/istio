c5f47fa5f976a4dcfa1cb629c514553316b0ee96
Steven Landow <landow@google.com>
2020-04-28 18:47:25 -0700

    prow: run metallb on kind in multicluster topology

--- prow/lib.sh
+++ prow/lib.sh
@@ -22,6 +22,10 @@ export CLUSTER3_NAME=${CLUSTER3_NAME:-"cluster3"}
 export CLUSTER_NAMES=("${CLUSTER1_NAME}" "${CLUSTER2_NAME}" "${CLUSTER3_NAME}")
 export CLUSTER_POD_SUBNETS=(10.10.0.0/16 10.20.0.0/16 10.30.0.0/16)
 
+# Take IPs from the end of the docker bridge network subnet to use for Metal LB IPs
+export DOCKER_BRIDGE_SUBNET=$(docker inspect bridge | jq .[0].IPAM.Config[0].Subnet -r)
+export METALLB_IPS=($(nmap -sL $DOCKER_BRIDGE_SUBNET | awk '/Nmap scan report/{print $NF}' | tail -n 100))
+
 function setup_gcloud_credentials() {
   if [[ $(command -v gcloud) ]]; then
     gcloud auth configure-docker -q
@@ -236,6 +240,7 @@ EOF
           connect_kind_clusters "${CLUSTERI_NAME}" "${CLUSTERI_KUBECONFIG}" "${CLUSTERJ_NAME}" "${CLUSTERJ_KUBECONFIG}"
         fi
       done
+      install_metallb $CLUSTERI_KUBECONFIG
     done
   else
     # Connect clusters 1 and 2, but leave cluster 3 on a separate network.
@@ -259,6 +264,27 @@ function connect_kind_clusters() {
   C2_POD_CIDR=$(KUBECONFIG="${C2_KUBECONFIG}" kubectl get node -ojsonpath='{.items[0].spec.podCIDR}')
   docker exec "${C1_NODE}" ip route add "${C2_POD_CIDR}" via "${C2_DOCKER_IP}"
   docker exec "${C2_NODE}" ip route add "${C1_POD_CIDR}" via "${C1_DOCKER_IP}"
+}
+
+function install_metallb() {
+  KUBECONFIG="${1}"
+  kubectl apply --kubeconfig=$KUBECONFIG -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+  kubectl apply --kubeconfig=$KUBECONFIG -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+  # allocate 10 IPs form METALLB_IPS array to this clusters MetalLB IP Pool
+  RANGE="${METALLB_IPS[0]}-${METALLB_IPS[9]}"
+  export METALLB_IPS=(${METALLB_IPS[@]:10})
+  echo 'apiVersion: v1
+kind: ConfigMap
+metadata:
+  namespace: metallb-system
+  name: config
+data:
+  config: |
+    address-pools:
+    - name: default
+      protocol: layer2
+      addresses:
+      - '$RANGE | kubectl apply --kubeconfig=$KUBECONFIG -f -
 }
 
 function cni_run_daemon_kind() {
