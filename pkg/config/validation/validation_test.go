62316b915ad7d32316300f2cf66202046f039d4c
John Howard <howardjohn@google.com>
2020-04-29 09:07:14 -0700

    Change validation test

--- pkg/config/validation/validation_test.go
+++ pkg/config/validation/validation_test.go
@@ -3165,7 +3165,7 @@ func TestValidateEnvoyFilter(t *testing.T) {
 						Operation: networking.EnvoyFilter_Patch_ADD,
 						Value: &types.Struct{
 							Fields: map[string]*types.Value{
-								"foo": {
+								"hosts": {
 									Kind: &types.Value_BoolValue{BoolValue: false},
 								},
 							},
@@ -3173,7 +3173,7 @@ func TestValidateEnvoyFilter(t *testing.T) {
 					},
 				},
 			},
-		}, error: `Envoy filter: unknown field "foo" in envoy_api_v2.Cluster`},
+		}, error: `Envoy filter: json: cannot unmarshal bool into Go value of type []json.RawMessage`},
 		{name: "happy config", in: &networking.EnvoyFilter{
 			ConfigPatches: []*networking.EnvoyFilter_EnvoyConfigObjectPatch{
 				{
