1520f76fe53d5aad81a059f24f2dd80eb2105e89
Shakti Das <shaktiprakash.das@salesforce.com>
2020-04-29 10:25:41 +0530

    Add command args to configure Isitod with externally managed certificates (#23116)
    * Read sidecar-injector certificates default values from env variables if it exists
    
    * rename vars
    
    * Add prefix INJECTION_WEBHOOK_
    
    * Add optional defaults for Istiod servers TLS certs
    
    * Fix lint checks
    
    * Add command args to configure Isitod with externally managed certificates
    
    * Handler for failed CA read

--- pilot/pkg/bootstrap/validation.go
+++ pilot/pkg/bootstrap/validation.go
@@ -18,6 +18,8 @@ import (
 	"path/filepath"
 	"strings"
 
+	"istio.io/istio/pilot/pkg/model"
+
 	"k8s.io/client-go/dynamic"
 
 	"istio.io/pkg/env"
@@ -55,8 +57,8 @@ func (s *Server) initConfigValidation(args *PilotArgs) error {
 		MixerValidator: validate.NewDefaultValidator(false),
 		Schemas:        collections.Istio,
 		DomainSuffix:   args.Config.ControllerOptions.DomainSuffix,
-		CertFile:       filepath.Join(dnsCertDir, "cert-chain.pem"),
-		KeyFile:        filepath.Join(dnsCertDir, "key.pem"),
+		CertFile:       model.GetOrDefault(args.TLSOptions.CertFile, filepath.Join(dnsCertDir, "cert-chain.pem")),
+		KeyFile:        model.GetOrDefault(args.TLSOptions.KeyFile, filepath.Join(dnsCertDir, "key.pem")),
 		Mux:            s.httpsMux,
 	}
 	whServer, err := server.New(params)
