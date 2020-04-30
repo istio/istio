155b85d90f76b840416abf5674889f318b1c2e34
John Howard <howardjohn@google.com>
2020-04-30 15:34:26 -0700

    Comments and fix kiali

--- manifests/addons/gen.sh
+++ manifests/addons/gen.sh
@@ -19,17 +19,23 @@ WD=$(cd "$WD"; pwd)
 
 set -eux
 
+# This script sets up the plain text rendered deployments for addons
+# See samples/addons/README.md for more information
+
 ADDONS="${WD}/../../samples/addons"
 DASHBOARDS="${WD}/../charts/istio-telemetry/grafana/dashboards"
 mkdir -p "${ADDONS}"
 
+# Set up Kiali. The upstream only has an operator, so we will deploy that
+# We set up anonymous authentication as this is for demos
 KIALI_SRC=$(mktemp -d)
 KIALI_VERSION=1.17.0
 pushd "${KIALI_SRC}"
 curl -s -L https://github.com/kiali/kiali-operator/archive/v${KIALI_VERSION}.tar.gz | tar xz
 OPERATOR_ROLE_CREATE="- create" OPERATOR_ROLE_DELETE="- delete" OPERATOR_ROLE_PATCH="- patch" ./kiali-operator-${KIALI_VERSION}/deploy/merge-operator-yaml.sh \
   -f "${ADDONS}/kiali.yaml" --operator-image-version v${KIALI_VERSION} --operator-namespace istio-system
-cat <<EOF > "${ADDONS}/kiali.yaml"
+cat <<EOF >> "${ADDONS}/kiali.yaml"
+---
 apiVersion: kiali.io/v1alpha1
 kind: Kiali
 metadata:
@@ -41,34 +47,38 @@ spec:
 EOF
 popd
 
+# Set up prometheus
 helm3 template prometheus stable/prometheus \
   --namespace istio-system \
   --version 11.0.2 \
   -f "${WD}/values-prometheus.yaml" \
   > "${ADDONS}/prometheus.yaml"
 
-helm3 template grafana stable/grafana \
-  --namespace istio-system \
-  --version 5.0.7 \
-  -f "${WD}/values-grafana.yaml" \
-  > "${ADDONS}/grafana.yaml"
+# Set up grafana
+{
+  helm3 template grafana stable/grafana \
+    --namespace istio-system \
+    --version 5.0.7 \
+    -f "${WD}/values-grafana.yaml"
 
-# Set up grafana dashboards. Split into 2 to avoid Kubernetes size limits
-echo -e "\n---\n" >> "${ADDONS}/grafana.yaml"
-kubectl create configmap istio-grafana-dashboards \
-  --dry-run=client -oyaml \
-  --from-file=pilot-dashboard.json="${DASHBOARDS}/pilot-dashboard.json" \
-  --from-file=mixer-dashboard.json="${DASHBOARDS}/mixer-dashboard.json" \
-  --from-file=istio-performance-dashboard.json="${DASHBOARDS}/istio-performance-dashboard.json" \
-  >> "${ADDONS}/grafana.yaml"
-echo -e "\n---\n" >> "${ADDONS}/grafana.yaml"
-kubectl create configmap istio-services-grafana-dashboards \
-  --dry-run=client -oyaml \
-  --from-file=istio-workload-dashboard.json="${DASHBOARDS}/istio-workload-dashboard.json" \
-  --from-file=istio-service-dashboard.json="${DASHBOARDS}/istio-service-dashboard.json" \
-  --from-file=istio-mesh-dashboard.json="${DASHBOARDS}/istio-mesh-dashboard.json" \
-  >> "${ADDONS}/grafana.yaml"
+  # Set up grafana dashboards. Split into 2 to avoid Kubernetes size limits
+  echo -e "\n---\n"
+  kubectl create configmap istio-grafana-dashboards \
+    --dry-run=client -oyaml \
+    --from-file=pilot-dashboard.json="${DASHBOARDS}/pilot-dashboard.json" \
+    --from-file=mixer-dashboard.json="${DASHBOARDS}/mixer-dashboard.json" \
+    --from-file=istio-performance-dashboard.json="${DASHBOARDS}/istio-performance-dashboard.json"
+
+  echo -e "\n---\n"
+  kubectl create configmap istio-services-grafana-dashboards \
+    --dry-run=client -oyaml \
+    --from-file=istio-workload-dashboard.json="${DASHBOARDS}/istio-workload-dashboard.json" \
+    --from-file=istio-service-dashboard.json="${DASHBOARDS}/istio-service-dashboard.json" \
+    --from-file=istio-mesh-dashboard.json="${DASHBOARDS}/istio-mesh-dashboard.json"
+} > "${ADDONS}/grafana.yaml"
+
 
 # Zipkin does not have a helm chart, but the deployment is trivial
 cp "${WD}/zipkin.yaml" "${ADDONS}/zipkin.yaml"
+# Jaeger does not have a helm chart for in memory option. To keep things simple we use plain deployment
 cp "${WD}/jaeger.yaml" "${ADDONS}/jaeger.yaml"
