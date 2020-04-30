1e8f7a2eb650de4da228c486da5fc75d7a64a474
Istio Automation <istio-testing-bot@google.com>
2020-04-29 19:01:05 -0700

    [release-1.6] Remove helm type dependency (#23405)
    * Remove helm type dependency
    
    * Lint
    
    Co-authored-by: Martin Ostrowski <mostrowski@google.com>

--- operator/pkg/helmreconciler/reconciler.go
+++ operator/pkg/helmreconciler/reconciler.go
@@ -21,7 +21,6 @@ import (
 	"sync"
 	"time"
 
-	"helm.sh/helm/v3/pkg/releaseutil"
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -141,8 +140,8 @@ func (h *HelmReconciler) processRecursive(manifests name.ManifestMap) *v1alpha1.
 			status := v1alpha1.InstallStatus_NONE
 			var err error
 			if len(ms) != 0 {
-				m := releaseutil.Manifest{
-					Name:    string(c),
+				m := name.Manifest{
+					Name:    c,
 					Content: name.MergeManifestSlices(ms),
 				}
 				if processedObjs, _, err = h.ApplyManifest(m, len(ComponentDependencies[c]) > 0); err != nil {
