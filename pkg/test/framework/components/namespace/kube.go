015ba9f08a7d19e450e4c1b7eaaa83322cf5cbfd
Steven Landow <landow@google.com>
2020-05-01 15:19:36 -0700

    test framework: dump envoy proxy configs (#23468)
    * test framework: dump envoy proxy configs
    
    * use pilot-agent request instead of curl

--- pkg/test/framework/components/namespace/kube.go
+++ pkg/test/framework/components/namespace/kube.go
@@ -77,6 +77,17 @@ func (n *kubeNamespace) Dump() {
 				if err = ioutil.WriteFile(fname, []byte(l), os.ModePerm); err != nil {
 					scopes.CI.Errorf("Unable to write logs for pod/container: %s/%s/%s", pod.Namespace, pod.Name, container.Name)
 				}
+
+				if container.Name == "istio-proxy" {
+					if cfgDump, err := cluster.Exec(pod.Namespace, pod.Name, container.Name, "pilot-agent request GET config_dump"); err == nil {
+						fname := path.Join(d, fmt.Sprintf("%s-%s.config.json", pod.Name, container.Name))
+						if err = ioutil.WriteFile(fname, []byte(cfgDump), os.ModePerm); err != nil {
+							scopes.CI.Errorf("Unable to write logs for pod/container: %s/%s/%s", pod.Namespace, pod.Name, container.Name)
+						}
+					} else {
+						scopes.CI.Errorf("Unable to get istio-proxy config dump for pod: %s/%s", pod.Namespace, pod.Name)
+					}
+				}
 			}
 		}
 	}
