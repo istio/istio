6b1ddc5a05e54d6628c68db1589cbb31a0ca9c9a
John Howard <howardjohn@google.com>
2020-05-01 17:00:55 -0700

    Remove node_agent binary (#23437)
    This is no longer used in the new model

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -275,7 +275,6 @@ BINARIES:=./istioctl/cmd/istioctl \
   ./mixer/cmd/mixs \
   ./mixer/cmd/mixc \
   ./mixer/tools/mixgen \
-  ./security/cmd/node_agent \
   ./security/tools/sdsclient \
   ./pkg/test/echo/cmd/client \
   ./pkg/test/echo/cmd/server \
@@ -283,7 +282,7 @@ BINARIES:=./istioctl/cmd/istioctl \
   ./operator/cmd/operator
 
 # List of binaries included in releases
-RELEASE_BINARIES:=pilot-discovery pilot-agent mixc mixs mixgen node_agent istioctl sdsclient
+RELEASE_BINARIES:=pilot-discovery pilot-agent mixc mixs mixgen istioctl sdsclient
 
 .PHONY: build
 build: depend
