2bb6b0c0994d94a9c657a855a95f96f5f18dc09b
John Howard <howardjohn@google.com>
2020-05-05 13:06:22 -0700

    Fully support installPackagePath (#23520)
    * Fully support installPackagePath
    
    Currently the profiles are always grabbed from the VFS in some cases.
    This is especially broken in development, as the assets.gen.go is not
    updated. This changes the last few areas to properly support
    installPackagePath. As a result, we no longer check in the profile
    to assets.gen.go to github (it is added at release time).
    
    * gen

--- operator/pkg/helm/helm.go
+++ operator/pkg/helm/helm.go
@@ -77,18 +77,13 @@ func NewHelmRenderer(operatorDataDir, helmSubdir, componentName, namespace strin
 
 // ReadProfileYAML reads the YAML values associated with the given profile. It uses an appropriate reader for the
 // profile format (compiled-in, file, HTTP, etc.).
-func ReadProfileYAML(profile string) (string, error) {
+func ReadProfileYAML(profile, chartsDir string) (string, error) {
 	var err error
 	var globalValues string
-	if profile == "" {
-		scope.Infof("ReadProfileYAML for profile name: [Empty]")
-	} else {
-		scope.Infof("ReadProfileYAML for profile name: %s", profile)
-	}
 
 	// Get global values from profile.
 	switch {
-	case IsBuiltinProfileName(profile):
+	case chartsDir == "":
 		if globalValues, err = LoadValuesVFS(profile); err != nil {
 			return "", err
 		}
@@ -98,7 +93,9 @@ func ReadProfileYAML(profile string) (string, error) {
 			return "", err
 		}
 	default:
-		return "", fmt.Errorf("unsupported Profile type: %s", profile)
+		if globalValues, err = LoadValues(profile, chartsDir); err != nil {
+			return "", fmt.Errorf("failed to read profile %v from %v: %v", profile, chartsDir, err)
+		}
 	}
 
 	return globalValues, nil
@@ -197,15 +194,12 @@ func renderTemplate(tmpl string, ts interface{}) (string, error) {
 }
 
 // DefaultFilenameForProfile returns the profile name of the default profile for the given profile.
-func DefaultFilenameForProfile(profile string) (string, error) {
+func DefaultFilenameForProfile(profile string) string {
 	switch {
 	case util.IsFilePath(profile):
-		return filepath.Join(filepath.Dir(profile), DefaultProfileFilename), nil
+		return filepath.Join(filepath.Dir(profile), DefaultProfileFilename)
 	default:
-		if _, ok := ProfileNames[profile]; ok || profile == "" {
-			return DefaultProfileString, nil
-		}
-		return "", fmt.Errorf("bad profile string %s", profile)
+		return DefaultProfileString
 	}
 }
 
@@ -308,23 +302,24 @@ func GetProfileYAML(installPackagePath, profileOrPath string) (string, error) {
 	if profileOrPath == "" {
 		profileOrPath = "default"
 	}
+	profiles, err := readProfiles(installPackagePath)
+	if err != nil {
+		return "", fmt.Errorf("failed to read profiles: %v", err)
+	}
 	// If charts are a file path and profile is a name like default, transform it to the file path.
-	if installPackagePath != "" && IsBuiltinProfileName(profileOrPath) {
+	if profiles[profileOrPath] {
 		profileOrPath = filepath.Join(installPackagePath, "profiles", profileOrPath+".yaml")
 	}
 	// This contains the IstioOperator CR.
-	baseCRYAML, err := ReadProfileYAML(profileOrPath)
+	baseCRYAML, err := ReadProfileYAML(profileOrPath, installPackagePath)
 	if err != nil {
 		return "", err
 	}
 
 	if !IsDefaultProfile(profileOrPath) {
 		// Profile definitions are relative to the default profileOrPath, so read that first.
-		dfn, err := DefaultFilenameForProfile(profileOrPath)
-		if err != nil {
-			return "", err
-		}
-		defaultYAML, err := ReadProfileYAML(dfn)
+		dfn := DefaultFilenameForProfile(profileOrPath)
+		defaultYAML, err := ReadProfileYAML(dfn, installPackagePath)
 		if err != nil {
 			return "", err
 		}
