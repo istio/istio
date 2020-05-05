e6f36832bfbf1526b6ea675ac8ebeae5813dfcfd
John Howard <howardjohn@google.com>
2020-05-05 13:05:12 -0700

    wip

--- pkg/test/framework/resource/cluster.go
+++ pkg/test/framework/resource/cluster.go
@@ -16,10 +16,34 @@ package resource
 
 import (
 	"fmt"
+
+	"istio.io/istio/pkg/test"
+	"istio.io/istio/pkg/test/framework/components/namespace"
 )
 
 // ClusterIndex is the index of a cluster within the Environment
 type ClusterIndex int
+
+type ConfigManager interface {
+	// ApplyConfig applies the given config yaml text via Galley.
+	ApplyConfig(ns namespace.Instance, yamlText ...string) error
+
+	// ApplyConfigOrFail applies the given config yaml text via Galley.
+	ApplyConfigOrFail(t test.Failer, ns namespace.Instance, yamlText ...string)
+
+	// DeleteConfig deletes the given config yaml text via Galley.
+	DeleteConfig(ns namespace.Instance, yamlText ...string) error
+
+	// DeleteConfigOrFail deletes the given config yaml text via Galley.
+	DeleteConfigOrFail(t test.Failer, ns namespace.Instance, yamlText ...string)
+
+	// ApplyConfigDir recursively applies all the config files in the specified directory
+	ApplyConfigDir(ns namespace.Instance, configDir string) error
+
+	// DeleteConfigDir recursively deletes all the config files in the specified directory
+	DeleteConfigDir(ns namespace.Instance, configDir string) error
+
+}
 
 // Cluster in a multicluster environment.
 type Cluster interface {
