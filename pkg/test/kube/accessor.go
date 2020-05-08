a78f3674647f39ed2139b0d3d6421de7eb5d3b10
Steven Landow <landow@google.com>
2020-05-08 15:10:49 -0700

    remote extraneous change

--- pkg/test/kube/accessor.go
+++ pkg/test/kube/accessor.go
@@ -497,7 +497,8 @@ func (a *Accessor) WaitForSecretToExistOrFail(t test.Failer, namespace, name str
 
 // DeleteSecret deletes secret by name in namespace.
 func (a *Accessor) DeleteSecret(namespace, name string) (err error) {
-	err = a.set.CoreV1().Secrets(namespace).Delete(context.TODO(), name, *deleteOptionsForeground())
+	var immediate int64
+	err = a.set.CoreV1().Secrets(namespace).Delete(context.TODO(), name, kubeApiMeta.DeleteOptions{GracePeriodSeconds: &immediate})
 	return err
 }
 
