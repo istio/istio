istio/istio

commit 621a185fcab5a476d92978d0bcd5071fa51a5ee0

create istio-ca-root-cert cm for each remote ns (#23173)
* create istio-ca-root-cert cm for each remote ns

* merge code

* fix lint

pilot/pkg/bootstrap/server.go
@@ -159,7 +159,6 @@ func NewServer(args *PilotArgs) (*Server, error) {
 	e := &model.Environment{
 		ServiceDiscovery: aggregate.NewController(),
 		PushContext:      model.NewPushContext(),
-		DomainSuffix:     args.Config.ControllerOptions.DomainSuffix,
 	}
 
 	s := &Server{
@@ -258,10 +257,15 @@ func NewServer(args *PilotArgs) (*Server, error) {
 	if err := s.initMonitor(args.DiscoveryOptions.MonitoringAddr); err != nil {
 		return nil, fmt.Errorf("error initializing monitor: %v", err)
 	}
+	args.Config.ControllerOptions.CAROOT = ""
+	if features.CentralIstioD {
+		if s.ca != nil && s.ca.GetCAKeyCertBundle() != nil {
+			args.Config.ControllerOptions.CAROOT = string(s.ca.GetCAKeyCertBundle().GetRootCertPem())
+		}
+	}
 	if err := s.initClusterRegistries(args); err != nil {
 		return nil, fmt.Errorf("error initializing cluster registries: %v", err)
 	}
-
 	if dns.DNSAddr.Get() != "" {
 		if err := s.initDNSTLSListener(dns.DNSAddr.Get()); err != nil {
 			log.Warna("error initializing DNS-over-TLS listener ", err)
@@ -299,7 +303,7 @@ func NewServer(args *PilotArgs) (*Server, error) {
 					NewLeaderElection(args.Namespace, args.PodName, leaderelection.NamespaceController, s.kubeClient).
 					AddRunFunction(func(stop <-chan struct{}) {
 						log.Infof("Starting namespace controller")
-						nc := NewNamespaceController(fetchData, args.Config.ControllerOptions, s.kubeClient)
+						nc := kubecontroller.NewNamespaceController(fetchData, args.Config.ControllerOptions, s.kubeClient)
 						nc.Run(stop)
 					}).
 					Run(stop)
@@ -323,7 +327,6 @@ func getClusterID(args *PilotArgs) string {
 			clusterID = string(serviceregistry.Kubernetes)
 		}
 	}
-
 	return clusterID
 }
 

manifests/charts/base/files/gen-istio-cluster.yaml
@@ -5084,14 +5084,11 @@ rules:
     resources: ["*"]
     verbs: ["get", "list", "watch"]
   - apiGroups: [""]
-    resources: ["endpoints", "pods", "services", "nodes", "replicationcontrollers"]
+    resources: ["endpoints", "pods", "services", "nodes", "replicationcontrollers", "namespaces"]
     verbs: ["get", "list", "watch"]
   - apiGroups: ["apps"]
     resources: ["replicasets"]
     verbs: ["get", "list", "watch"]
-  - apiGroups: ["authentication.k8s.io"]
-    resources: ["tokenreviews"]
-    verbs: ["create"]
 ---
 # Source: base/templates/clusterrolebinding.yaml
 apiVersion: rbac.authorization.k8s.io/v1

manifests/charts/base/templates/clusterrole.yaml
@@ -119,13 +119,18 @@ rules:
     resources: ["*"]
     verbs: ["get", "list", "watch"]
   - apiGroups: [""]
-    resources: ["endpoints", "pods", "services", "nodes", "replicationcontrollers"]
+    resources: ["endpoints", "pods", "services", "nodes", "replicationcontrollers", "namespaces"]
     verbs: ["get", "list", "watch"]
   - apiGroups: ["apps"]
     resources: ["replicasets"]
     verbs: ["get", "list", "watch"]
+{{- if .Values.global.centralIstiod }}
   - apiGroups: ["authentication.k8s.io"]
     resources: ["tokenreviews"]
     verbs: ["create"]
+  - apiGroups: [""]
+    resources: ["configmaps"]
+    verbs: ["create", "get", "list", "watch", "update"]
+{{- end}}
 ---
 

manifests/charts/global.yaml
@@ -54,6 +54,9 @@ global:
     enabled: true
     enableAnalysis: false
 
+  # One central istiod controls all remote clusters: disabled by default
+  centralIstiod: false
+
   proxy:
     image: proxyv2
 

manifests/charts/istio-control/istio-discovery/files/gen-istio.yaml
@@ -91,6 +91,7 @@ data:
           "s390x": 2
         },
         "caAddress": "",
+        "centralIstiod": false,
         "certificates": [],
         "configRootNamespace": "istio-system",
         "configValidation": true,
@@ -848,6 +849,8 @@ spec:
             value: "false"
           - name: CLUSTER_ID
             value: "Kubernetes"
+          - name: CENTRAL_ISTIOD
+            value: "false"
           resources:
             requests:
               cpu: 500m

manifests/charts/istio-control/istio-discovery/templates/deployment.yaml
@@ -148,6 +148,8 @@ spec:
             value: "{{ .Values.global.istiod.enableAnalysis }}"
           - name: CLUSTER_ID
             value: "{{ $.Values.global.multiCluster.clusterName | default `Kubernetes` }}"
+          - name: CENTRAL_ISTIOD
+            value: "{{ $.Values.global.centralIstiod | default "false" }}"
           resources:
 {{- if .Values.pilot.resources }}
 {{ toYaml .Values.pilot.resources | trim | indent 12 }}

operator/pkg/apis/istio/v1alpha1/v1alpha1.pb.html
@@ -1706,6 +1706,17 @@ No
 No
 </td>
 </tr>
+<tr id="GlobalConfig-centralIstiod">
+<td><code>centralIstiod</code></td>
+<td><code><a href="https://developers.google.com/protocol-buffers/docs/reference/google.protobuf#boolvalue">BoolValue</a></code></td>
+<td>
+<p>Controls whether one central istiod is enabled.</p>
+
+</td>
+<td>
+No
+</td>
+</tr>
 <tr id="GlobalConfig-defaultNodeSelector" class="deprecated ">
 <td><code>defaultNodeSelector</code></td>
 <td><code><a href="#TypeMapStringInterface">TypeMapStringInterface</a></code></td>

commit 0f8fc517e1f27a3bc332e6680349c796ec865abd

Fix broken cluster-local domain override
This was broken by https://github.com/istio/istio/pull/23173

Restored the setting of the domain and added a test.

Also declaring a common constant for the default domain.

galley/pkg/config/analysis/analyzers/util/constants.go
@@ -16,10 +16,12 @@ package util
 
 import (
 	"regexp"
+
+	"istio.io/istio/pkg/config/constants"
 )
 
 const (
-	DefaultKubernetesDomain = "svc.cluster.local"
+	DefaultKubernetesDomain = "svc." + constants.DefaultKubernetesDomain
 	MeshGateway             = "mesh"
 	ExportToNamespaceLocal  = "."
 	ExportToAllNamespaces   = "*"

galley/pkg/config/analysis/local/analyze.go
@@ -42,6 +42,7 @@ import (
 	"istio.io/istio/galley/pkg/config/source/kube/apiserver"
 	"istio.io/istio/galley/pkg/config/source/kube/inmemory"
 	"istio.io/istio/galley/pkg/config/util/kuberesource"
+	"istio.io/istio/pkg/config/constants"
 	"istio.io/istio/pkg/config/mesh"
 	"istio.io/istio/pkg/config/resource"
 	"istio.io/istio/pkg/config/schema"
@@ -51,7 +52,6 @@ import (
 )
 
 const (
-	domainSuffix       = "cluster.local"
 	meshConfigMapKey   = "mesh"
 	meshConfigMapName  = "istio"
 	meshNetworksMapKey = "meshNetworks"
@@ -209,7 +209,7 @@ func (sa *SourceAnalyzer) Analyze(cancel chan struct{}) (AnalysisResult, error) 
 
 	processorSettings := processor.Settings{
 		Metadata:           sa.m,
-		DomainSuffix:       domainSuffix,
+		DomainSuffix:       constants.DefaultKubernetesDomain,
 		Source:             newPrecedenceSource(sa.sources),
 		TransformProviders: sa.transformerProviders,
 		Distributor:        distributor,

galley/pkg/server/settings/args.go
@@ -28,6 +28,7 @@ import (
 	"istio.io/pkg/probe"
 
 	"istio.io/istio/galley/pkg/config/util/kuberesource"
+	"istio.io/istio/pkg/config/constants"
 	"istio.io/istio/pkg/config/schema/snapshots"
 	"istio.io/istio/pkg/keepalive"
 	"istio.io/istio/pkg/mcp/creds"
@@ -44,7 +45,6 @@ const (
 	defaultMeshConfigFolder = "/etc/mesh-config/"
 	defaultAccessListFile   = defaultConfigMapFolder + "accesslist.yaml"
 	defaultMeshConfigFile   = defaultMeshConfigFolder + "mesh"
-	defaultDomainSuffix     = "cluster.local"
 )
 
 // Args contains the startup arguments to instantiate Galley.
@@ -181,7 +181,7 @@ func DefaultArgs() *Args {
 		EnableServer:                    true,
 		CredentialOptions:               creds.DefaultOptions(),
 		ConfigPath:                      "",
-		DomainSuffix:                    defaultDomainSuffix,
+		DomainSuffix:                    constants.DefaultKubernetesDomain,
 		DisableResourceReadyCheck:       false,
 		ExcludedResourceKinds:           kuberesource.DefaultExcludedResourceKinds(),
 		SinkMeta:                        make([]string, 0),

istioctl/cmd/add-to-mesh.go
@@ -42,6 +42,7 @@ import (
 	"istio.io/istio/istioctl/pkg/util/handlers"
 	"istio.io/istio/pilot/pkg/model"
 	kube_registry "istio.io/istio/pilot/pkg/serviceregistry/kube"
+	"istio.io/istio/pkg/config/constants"
 	"istio.io/istio/pkg/config/mesh"
 	istioProtocol "istio.io/istio/pkg/config/protocol"
 	"istio.io/istio/pkg/config/schema/collections"
@@ -492,7 +493,7 @@ func generateServiceEntry(u *unstructured.Unstructured, o *vmServiceOpts) error 
 			Labels:  o.Labels,
 		})
 	}
-	host := fmt.Sprintf("%v.%v.svc.cluster.local", o.Name, o.Namespace)
+	host := fmt.Sprintf("%v.%v.svc.%s", o.Name, o.Namespace, constants.DefaultKubernetesDomain)
 	spec := &v1alpha3.ServiceEntry{
 		Hosts:      []string{host},
 		Ports:      ports,

istioctl/cmd/describe.go
@@ -49,6 +49,7 @@ import (
 	"istio.io/istio/pilot/pkg/model"
 	authz_model "istio.io/istio/pilot/pkg/security/authz/model"
 	pilotcontroller "istio.io/istio/pilot/pkg/serviceregistry/kube/controller"
+	"istio.io/istio/pkg/config/constants"
 	"istio.io/istio/pkg/config/host"
 	"istio.io/istio/pkg/config/protocol"
 	"istio.io/istio/pkg/config/schema/collections"
@@ -60,7 +61,7 @@ type myProtoValue struct {
 }
 
 const (
-	k8sSuffix         = ".svc.cluster.local"
+	k8sSuffix         = ".svc." + constants.DefaultKubernetesDomain
 	noVersionMetadata = "no-metadata"
 )
 

istioctl/pkg/convert/ingress.go
@@ -21,6 +21,7 @@ import (
 
 	"istio.io/istio/pilot/pkg/config/kube/ingress"
 	"istio.io/istio/pilot/pkg/model"
+	"istio.io/istio/pkg/config/constants"
 )
 
 // IstioIngresses converts K8s extensions/v1beta1 Ingresses with Istio rules to v1alpha3 gateway and virtual service
@@ -30,7 +31,7 @@ func IstioIngresses(ingresses []*v1beta1.Ingress, domainSuffix string) ([]model.
 		return make([]model.Config, 0), nil
 	}
 	if len(domainSuffix) == 0 {
-		domainSuffix = "cluster.local"
+		domainSuffix = constants.DefaultKubernetesDomain
 	}
 
 	ingressByHost := map[string]*model.Config{}

pilot/cmd/pilot-agent/main.go
@@ -359,8 +359,8 @@ func setSpiffeTrustDomain(podNamespace string, domain string) {
 	pilotTrustDomain := trustDomain
 	if len(pilotTrustDomain) == 0 {
 		if registryID == serviceregistry.Kubernetes &&
-			(domain == podNamespace+".svc.cluster.local" || domain == "") {
-			pilotTrustDomain = "cluster.local"
+			(domain == podNamespace+".svc."+constants.DefaultKubernetesDomain || domain == "") {
+			pilotTrustDomain = constants.DefaultKubernetesDomain
 		} else if registryID == serviceregistry.Consul &&
 			(domain == "service.consul" || domain == "") {
 			pilotTrustDomain = ""
@@ -385,7 +385,7 @@ func getSAN(ns string, defaultSA string, overrideIdentity string) []string {
 func getDNSDomain(podNamespace, domain string) string {
 	if len(domain) == 0 {
 		if registryID == serviceregistry.Kubernetes {
-			domain = podNamespace + ".svc.cluster.local"
+			domain = podNamespace + ".svc." + constants.DefaultKubernetesDomain
 		} else if registryID == serviceregistry.Consul {
 			domain = "service.consul"
 		} else {

pilot/cmd/pilot-discovery/main.go
@@ -32,6 +32,7 @@ import (
 	"istio.io/istio/pilot/pkg/features"
 	"istio.io/istio/pilot/pkg/serviceregistry"
 	"istio.io/istio/pkg/cmd"
+	"istio.io/istio/pkg/config/constants"
 	"istio.io/istio/pkg/spiffe"
 )
 
@@ -148,7 +149,7 @@ func init() {
 		"Restrict the applications namespace the controller manages; if not set, controller watches all namespaces")
 	discoveryCmd.PersistentFlags().DurationVar(&serverArgs.Config.ControllerOptions.ResyncPeriod, "resync", 60*time.Second,
 		"Controller resync interval")
-	discoveryCmd.PersistentFlags().StringVar(&serverArgs.Config.ControllerOptions.DomainSuffix, "domain", "cluster.local",
+	discoveryCmd.PersistentFlags().StringVar(&serverArgs.Config.ControllerOptions.DomainSuffix, "domain", constants.DefaultKubernetesDomain,
 		"DNS domain suffix")
 	discoveryCmd.PersistentFlags().StringVar(&serverArgs.Config.ControllerOptions.ClusterID, "clusterID", features.ClusterName,
 		"The ID of the cluster that this Istiod instance resides")

pilot/pkg/bootstrap/server.go
@@ -167,6 +167,7 @@ func NewServer(args *PilotArgs) (*Server, error) {
 	e := &model.Environment{
 		ServiceDiscovery: aggregate.NewController(),
 		PushContext:      model.NewPushContext(),
+		DomainSuffix:     args.Config.ControllerOptions.DomainSuffix,
 	}
 
 	s := &Server{

pilot/pkg/config/kube/gateway/conversion.go
@@ -171,7 +171,7 @@ func createRoute(action *k8s.HTTPRouteAction, ns string) []*istio.HTTPRouteDesti
 		Destination: &istio.Destination{
 			// TODO cluster.local hardcode
 			// TODO is this the right format?
-			Host: fmt.Sprintf("%s.%s.svc.cluster.local", action.ForwardTo.Name, ns),
+			Host: action.ForwardTo.Name + "." + ns + ".svc." + constants.DefaultKubernetesDomain,
 		},
 	}}
 

pilot/pkg/model/context.go
@@ -34,12 +34,9 @@ import (
 	meshconfig "istio.io/api/mesh/v1alpha1"
 	"istio.io/pkg/monitoring"
 
+	"istio.io/istio/pkg/config/constants"
 	"istio.io/istio/pkg/config/labels"
 	"istio.io/istio/pkg/config/mesh"
-)
-
-const (
-	defaultDomainSuffix = "cluster.local"
 )
 
 var _ mesh.Holder = &Environment{}
@@ -79,7 +76,7 @@ func (e *Environment) GetDomainSuffix() string {
 	if len(e.DomainSuffix) > 0 {
 		return e.DomainSuffix
 	}
-	return defaultDomainSuffix
+	return constants.DefaultKubernetesDomain
 }
 
 func (e *Environment) Mesh() *meshconfig.MeshConfig {

pilot/pkg/security/trustdomain/bundle.go
@@ -19,6 +19,8 @@ import (
 	"strings"
 
 	istiolog "istio.io/pkg/log"
+
+	"istio.io/istio/pkg/config/constants"
 )
 
 var (
@@ -68,7 +70,7 @@ func (t Bundle) ReplaceTrustDomainAliases(principals []string) []string {
 		// Only generate configuration if the extracted trust domain from the policy is part of the trust domain list,
 		// or if the extracted/existing trust domain is "cluster.local", which is a pointer to the local trust domain
 		// and its aliases.
-		if stringMatch(trustDomainFromPrincipal, t.TrustDomains) || trustDomainFromPrincipal == "cluster.local" {
+		if stringMatch(trustDomainFromPrincipal, t.TrustDomains) || trustDomainFromPrincipal == constants.DefaultKubernetesDomain {
 			// Generate configuration for trust domain and trust domain aliases.
 			principalsIncludingAliases = append(principalsIncludingAliases, t.replaceTrustDomains(principal, trustDomainFromPrincipal)...)
 		} else {

pkg/config/constants/constants.go
@@ -59,6 +59,9 @@ const (
 	// IstioIngressNamespace is the namespace where Istio ingress controller is deployed
 	IstioIngressNamespace = "istio-system"
 
+	// DefaultKubernetesDomain the default service domain suffix for Kubernetes, if not overridden in config.
+	DefaultKubernetesDomain = "cluster.local"
+
 	// IstioLabel indicates that a workload is part of a named Istio system component.
 	IstioLabel = "istio"
 

pkg/dns/dns.go
@@ -28,6 +28,8 @@ import (
 
 	"istio.io/pkg/env"
 	"istio.io/pkg/log"
+
+	"istio.io/istio/pkg/config/constants"
 )
 
 // Based on istio-ecosystem/istio-coredns-plugin
@@ -167,7 +169,7 @@ func InitDNSAgent(discoveryAddress string, domain string, cert []byte, suffixes 
 	}
 
 	dnsDomainL := strings.Split(domain, ".")
-	clusterLocal := "cluster.local"
+	clusterLocal := constants.DefaultKubernetesDomain
 	if len(dnsDomainL) > 3 {
 		clusterLocal = strings.Join(dnsDomainL[2:], ".")
 	}

pkg/spiffe/spiffe.go
@@ -20,6 +20,8 @@ import (
 	"sync"
 
 	"istio.io/pkg/log"
+
+	"istio.io/istio/pkg/config/constants"
 )
 
 const (
@@ -28,7 +30,7 @@ const (
 	URIPrefix = Scheme + "://"
 
 	// The default SPIFFE URL value for trust domain
-	defaultTrustDomain = "cluster.local"
+	defaultTrustDomain = constants.DefaultKubernetesDomain
 )
 
 var (

galley/pkg/server/settings/args_test.go
@@ -14,7 +14,11 @@
 
 package settings
 
-import "testing"
+import (
+	"testing"
+
+	"istio.io/istio/pkg/config/constants"
+)
 
 func TestDefaultArgs(t *testing.T) {
 	a := DefaultArgs()
@@ -36,7 +40,7 @@ func TestDefaultArgs(t *testing.T) {
 		t.Fatalf("unexpected MeshConfigFile: %s", a.MeshConfigFile)
 	}
 
-	if a.DomainSuffix != defaultDomainSuffix {
+	if a.DomainSuffix != constants.DefaultKubernetesDomain {
 		t.Fatalf("unexpected DomainSuffix: %s", a.DomainSuffix)
 	}
 

pilot/pkg/bootstrap/server_test.go
@@ -21,10 +21,14 @@ import (
 	"path/filepath"
 	"testing"
 
-	"istio.io/istio/pkg/testcerts"
+	. "github.com/onsi/gomega"
+
 	"istio.io/pkg/filewatcher"
 
-	"github.com/onsi/gomega"
+	kubecontroller "istio.io/istio/pilot/pkg/serviceregistry/kube/controller"
+	"istio.io/istio/pkg/config/constants"
+	"istio.io/istio/pkg/config/mesh"
+	"istio.io/istio/pkg/testcerts"
 )
 
 func TestReloadIstiodCert(t *testing.T) {
@@ -36,8 +40,8 @@ func TestReloadIstiodCert(t *testing.T) {
 
 	defer func() {
 		close(stop)
-		s.fileWatcher.Close()
-		_ = os.RemoveAll(dir) // nolint: errcheck
+		_ = s.fileWatcher.Close()
+		_ = os.RemoveAll(dir)
 	}()
 	if err != nil {
 		t.Fatalf("TempDir() failed: %v", err)
@@ -82,12 +86,88 @@ func TestReloadIstiodCert(t *testing.T) {
 		t.Fatalf("WriteFile(%v) failed: %v", tlsOptions.KeyFile, err)
 	}
 
-	g := gomega.NewGomegaWithT(t)
+	g := NewGomegaWithT(t)
 
 	// Validate that istiod cert is updated.
 	g.Eventually(func() bool {
 		return checkCert(t, s, testcerts.RotatedCert, testcerts.RotatedKey)
-	}, "10s", "100ms").Should(gomega.BeTrue())
+	}, "10s", "100ms").Should(BeTrue())
+}
+
+func TestNewServer(t *testing.T) {
+	// All of the settings to apply and verify. Currently just testing domain suffix,
+	// but we should expand this list.
+	cases := []struct {
+		name           string
+		domain         string
+		expectedDomain string
+	}{
+		{
+			name:           "default domain",
+			domain:         "",
+			expectedDomain: constants.DefaultKubernetesDomain,
+		},
+		{
+			name:           "override domain",
+			domain:         "mydomain.com",
+			expectedDomain: "mydomain.com",
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			configDir, err := ioutil.TempDir("", "TestNewServer")
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			defer func() {
+				_ = os.RemoveAll(configDir)
+			}()
+
+			args := NewPilotArgs(func(p *PilotArgs) {
+				p.Namespace = "istio-system"
+				p.DiscoveryOptions = DiscoveryServiceOptions{
+					// Dynamically assign all ports.
+					HTTPAddr:       ":0",
+					MonitoringAddr: ":0",
+					GrpcAddr:       ":0",
+				}
+				p.Config = ConfigArgs{
+					ControllerOptions: kubecontroller.Options{
+						DomainSuffix: c.domain,
+					},
+					FileDir: configDir,
+				}
+
+				meshCfg := mesh.DefaultMeshConfig()
+				p.MeshConfig = &meshCfg
+
+				// Use the config store for service entries as well.
+				p.Service = ServiceArgs{
+					// A ServiceEntry registry is added by default, which is what we want. Don't include any other registries.
+					Registries: []string{},
+				}
+
+				// Include all of the default plugins for integration with Mixer, etc.
+				p.Plugins = DefaultPlugins
+				p.ForceStop = true
+			})
+
+			g := NewGomegaWithT(t)
+			s, err := NewServer(args)
+			g.Expect(err).To(Succeed())
+
+			stop := make(chan struct{})
+			g.Expect(s.Start(stop)).To(Succeed())
+			defer func() {
+				close(stop)
+				s.WaitUntilCompletion()
+			}()
+
+			g.Expect(s.environment.GetDomainSuffix()).To(Equal(c.expectedDomain))
+		})
+	}
 }
 
 func checkCert(t *testing.T, s *Server, cert, key []byte) bool {

pkg/test/framework/components/echo/kube/instance.go
@@ -22,6 +22,7 @@ import (
 
 	"github.com/hashicorp/go-multierror"
 
+	"istio.io/istio/pkg/config/constants"
 	"istio.io/istio/pkg/config/protocol"
 	"istio.io/istio/pkg/test"
 	appEcho "istio.io/istio/pkg/test/echo/client"
@@ -37,7 +38,7 @@ import (
 const (
 	tcpHealthPort     = 3333
 	httpReadinessPort = 8080
-	defaultDomain     = "cluster.local"
+	defaultDomain     = constants.DefaultKubernetesDomain
 )
 
 var (

pkg/test/framework/components/environment/native/native.go
@@ -21,6 +21,7 @@ import (
 	"github.com/docker/docker/client"
 	"github.com/hashicorp/go-multierror"
 
+	"istio.io/istio/pkg/config/constants"
 	"istio.io/istio/pkg/spiffe"
 	"istio.io/istio/pkg/test/docker"
 	"istio.io/istio/pkg/test/framework/resource"
@@ -29,7 +30,7 @@ import (
 
 const (
 	systemNamespace = "istio-system"
-	domain          = "cluster.local"
+	domain          = constants.DefaultKubernetesDomain
 
 	networkLabelKey   = "app"
 	networkLabelValue = "istio-test"