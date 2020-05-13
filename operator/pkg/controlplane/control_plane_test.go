53b4d79c47757063b8fde976801fa120c83380d3
Lin Sun <linsun@us.ibm.com>
2020-05-12 22:29:18 -0400

    make gen and fix test

--- operator/pkg/controlplane/control_plane_test.go
+++ operator/pkg/controlplane/control_plane_test.go
@@ -115,6 +115,12 @@ func TestNewIstioOperator(t *testing.T) {
 							Options:       coreComponentOptions,
 						},
 					},
+					&component.IstiodRemoteComponent{
+						CommonComponentFields: &component.CommonComponentFields{
+							ComponentName: name.IstiodRemoteComponentName,
+							Options:       coreComponentOptions,
+						},
+					},
 				},
 			},
 		},
