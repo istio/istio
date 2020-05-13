a7db79a9a1ae3c45ecde1d435e97f9fb7c304031
Lin Sun <linsun@us.ibm.com>
2020-05-12 21:32:28 -0400

    add associated operator code for istiodremote

--- operator/pkg/component/component.go
+++ operator/pkg/component/component.go
@@ -105,6 +105,8 @@ func NewCoreComponent(cn name.ComponentName, opts *Options) IstioComponent {
 		component = NewTelemetryComponent(opts)
 	case name.CNIComponentName:
 		component = NewCNIComponent(opts)
+	case name.IstiodRemoteComponentName:
+		component = NewIstiodRemoteComponent(opts)
 	default:
 		panic("Unknown component componentName: " + string(cn))
 	}
@@ -295,12 +297,12 @@ func (c *TelemetryComponent) Enabled() bool {
 	return isCoreComponentEnabled(c.CommonComponentFields)
 }
 
-// CNIComponent is the egress gateway component.
+// CNIComponent is the istio cni component.
 type CNIComponent struct {
 	*CommonComponentFields
 }
 
-// NewCNIComponent creates a new IngressComponent and returns a pointer to it.
+// NewCNIComponent creates a new NewCNIComponent and returns a pointer to it.
 func NewCNIComponent(opts *Options) *CNIComponent {
 	cn := name.CNIComponentName
 	return &CNIComponent{
@@ -338,6 +340,52 @@ func (c *CNIComponent) Namespace() string {
 
 // Enabled implements the IstioComponent interface.
 func (c *CNIComponent) Enabled() bool {
+	return isCoreComponentEnabled(c.CommonComponentFields)
+}
+
+// IstiodRemoteComponent is the istiod remote component.
+type IstiodRemoteComponent struct {
+	*CommonComponentFields
+}
+
+// NewIstiodRemoteComponent creates a new NewIstiodRemoteComponent and returns a pointer to it.
+func NewIstiodRemoteComponent(opts *Options) *IstiodRemoteComponent {
+	cn := name.IstiodRemoteComponentName
+	return &IstiodRemoteComponent{
+		&CommonComponentFields{
+			Options:       opts,
+			ComponentName: cn,
+		},
+	}
+}
+
+// Run implements the IstioComponent interface.
+func (c *IstiodRemoteComponent) Run() error {
+	return runComponent(c.CommonComponentFields)
+}
+
+// RenderManifest implements the IstioComponent interface.
+func (c *IstiodRemoteComponent) RenderManifest() (string, error) {
+	return renderManifest(c, c.CommonComponentFields)
+}
+
+// ComponentName implements the IstioComponent interface.
+func (c *IstiodRemoteComponent) ComponentName() name.ComponentName {
+	return c.CommonComponentFields.ComponentName
+}
+
+// ResourceName implements the IstioComponent interface.
+func (c *IstiodRemoteComponent) ResourceName() string {
+	return c.CommonComponentFields.ResourceName
+}
+
+// Namespace implements the IstioComponent interface.
+func (c *IstiodRemoteComponent) Namespace() string {
+	return c.CommonComponentFields.Namespace
+}
+
+// Enabled implements the IstioComponent interface.
+func (c *IstiodRemoteComponent) Enabled() bool {
 	return isCoreComponentEnabled(c.CommonComponentFields)
 }
 
