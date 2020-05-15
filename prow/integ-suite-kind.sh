60516717243aa55c2abd9045db2bae1671c107c3
Steven Landow <landow@google.com>
2020-05-15 13:18:57 -0700

    change INTEGRATION_TEST_NETWORKS to be string

--- prow/integ-suite-kind.sh
+++ prow/integ-suite-kind.sh
@@ -109,7 +109,7 @@ if [[ -z "${SKIP_SETUP:-}" ]]; then
 
     # Set the kube configs to point to the clusters.
     export INTEGRATION_TEST_KUBECONFIG="${CLUSTER1_KUBECONFIG},${CLUSTER2_KUBECONFIG},${CLUSTER3_KUBECONFIG}"
-    export INTEGRATION_TEST_NETWORKS+=("0:test-network-0,1:test-network-0,2:test-network-1")
+    export INTEGRATION_TEST_NETWORKS="0:test-network-0,1:test-network-0,2:test-network-1"
   fi
 fi
 
