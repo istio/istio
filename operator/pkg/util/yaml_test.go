f7e3da1eacf76cff73a93c4263b8572f76b49ba9
Aditya Prerepa <adiprerepa@gmail.com>
2020-05-21 15:43:01 -0700

    add tests

--- operator/pkg/util/yaml_test.go
+++ operator/pkg/util/yaml_test.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"errors"
 	"github.com/gogo/protobuf/proto"
 	"istio.io/istio/operator/pkg/apis/istio/v1alpha1"
 	"reflect"
@@ -80,12 +81,12 @@ metadata:
 }
 
 func TestOverlayTrees(t *testing.T) {
-	tests := []struct{
-		desc string
-		inBase map[string]interface{}
-		inOverlays map[string]interface{}
+	tests := []struct {
+		desc            string
+		inBase          map[string]interface{}
+		inOverlays      map[string]interface{}
 		expectedOverlay map[string]interface{}
-		expectedErr error
+		expectedErr     error
 	}{
 		{
 			desc: "overlay-valid",
@@ -103,7 +104,7 @@ func TestOverlayTrees(t *testing.T) {
 			expectedErr: nil,
 		},
 		{
-			desc : "overlay-key-does-not-exist",
+			desc: "overlay-key-does-not-exist",
 			inBase: map[string]interface{}{
 				"foo": "bar",
 				"baz": "naz",
@@ -112,8 +113,8 @@ func TestOverlayTrees(t *testing.T) {
 				"i-dont-exist": "i-really-dont-exist",
 			},
 			expectedOverlay: map[string]interface{}{
-				"baz": "naz",
-				"foo": "bar",
+				"baz":          "naz",
+				"foo":          "bar",
 				"i-dont-exist": "i-really-dont-exist",
 			},
 			expectedErr: nil,
@@ -127,7 +128,7 @@ func TestOverlayTrees(t *testing.T) {
 				"foo": nil,
 			},
 			expectedOverlay: map[string]interface{}{},
-			expectedErr: nil,
+			expectedErr:     nil,
 		},
 	}
 	for _, tt := range tests {
@@ -136,6 +137,187 @@ func TestOverlayTrees(t *testing.T) {
 				((err != nil && tt.expectedErr == nil) || (err == nil && tt.expectedErr != nil)) {
 				t.Errorf("%s: expected overlay & err %v %v got %v %v", tt.desc, tt.expectedOverlay, tt.expectedErr,
 					gotOverlays, err)
+			}
+		})
+	}
+}
+func TestOverlayYAML(t *testing.T) {
+	tests := []struct{
+		desc string
+		base string
+		overlay string
+		expect string
+		err error
+	}{
+		{
+			desc: "overlay-yaml",
+			base: `foo: bar
+yo: lo
+`,
+			overlay: `yo: go`,
+			expect: `foo: bar
+yo: go
+`,
+			err: nil,
+		},
+		{
+			desc: "combine-yaml",
+			base: `foo: bar`,
+			overlay: `baz: razmatazz`,
+			expect: `baz: razmatazz
+foo: bar
+`,
+			err: nil,
+		},
+		{
+			desc: "blank",
+			base: `R#)*J#FN`,
+			overlay: `FM#)M#F(*#M`,
+			expect: "",
+			err: errors.New("invalid json"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			if got, err := OverlayYAML(tt.base, tt.overlay); got != tt.expect || ((tt.err != nil && err == nil) || (tt.err == nil && err != nil)) {
+				t.Errorf("%s: expected overlay&err %v %v got %v %v", tt.desc, tt.expect, tt.err, got, err)
+			}
+		})
+	}
+}
+
+func TestYAMLDiff(t *testing.T) {
+	tests := []struct{
+		desc string
+		diff1 string
+		diff2 string
+		expect string
+	}{
+		{
+			desc: "1-line-diff",
+			diff1: `hola: yo
+foo: bar
+goo: tar
+`,
+			diff2: `hola: yo
+foo: bar
+notgoo: nottar
+`,
+			expect: ` foo: bar
+-goo: tar
+ hola: yo
++notgoo: nottar
+ `,
+		},
+		{
+			desc: "no-diff",
+			diff1: `foo: bar`,
+			diff2: `foo: bar`,
+			expect: ``,
+		},
+		{
+			desc: "invalid-yaml",
+			diff1: `Ij#**#f#`,
+			diff2: `fm*##)n`,
+			expect: "error unmarshaling JSON: while decoding JSON: json: cannot unmarshal string into Go value of type map[string]interface {}",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			if got := YAMLDiff(tt.diff1, tt.diff2); got != tt.expect {
+				t.Errorf("%s: expect %v got %v", tt.desc, tt.expect, got)
+			}
+		})
+	}
+}
+
+func TestIsYAMLEqual(t *testing.T) {
+	tests := []struct{
+		desc string
+		in1 string
+		in2 string
+		expect bool
+	}{
+		{
+			desc: "yaml-equal",
+			in1: `foo: bar`,
+			in2: `foo: bar`,
+			expect: true,
+		},
+		{
+			desc: "bad-yaml-1",
+			in1: "O#JF*()#",
+			in2: `foo: bar`,
+			expect: false,
+		},
+		{
+			desc: "bad-yaml-2",
+			in1: `foo: bar`,
+			in2: "#OHJ*#()F",
+			expect: false,
+		},
+		{
+			desc: "yaml-not-equal",
+			in1: `zinc: iron
+stoichiometry: avagadro
+`,
+			in2: `i-swear: i-am
+definitely-not: in1
+`,
+			expect: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			if got := IsYAMLEqual(tt.in1, tt.in2); got != tt.expect {
+				t.Errorf("%v: got %v want %v", tt.desc, got, tt.expect)
+			}
+		})
+	}
+}
+
+func TestIsYAMLEmpty(t *testing.T) {
+	tests := []struct{
+		desc string
+		in string
+		expect bool
+	}{
+		{
+			desc: "completely-empty",
+			in: "",
+			expect: true,
+		},
+		{
+			desc: "comment-logically-empty",
+			in: `# this is a comment
+# this is another comment that serves no purpose
+# (like all comments usually do)
+`,
+			expect: true,
+		},
+		{
+			desc: "start-yaml",
+			in: `--- I dont mean anything`,
+			expect: true,
+		},
+		{
+			desc: "combine-comments-and-yaml",
+			in: `#this is another comment
+foo: bar
+# ^ that serves purpose
+`,
+			expect: false,
+		},
+		{
+			desc: "yaml-not-empty",
+			in: `foo: bar`,
+			expect: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			if got := IsYAMLEmpty(tt.in); got != tt.expect {
+				t.Errorf("%v: expect %v got %v", tt.desc, tt.expect, got)
 			}
 		})
 	}
