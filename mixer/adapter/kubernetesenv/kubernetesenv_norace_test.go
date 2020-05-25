1031d04540a96a49d22c59cede1fe7d659d00c11
John Howard <howardjohn@google.com>
2020-05-25 05:17:15 -0700

    Fix Test_KubeSecretController (#23968)
    This fails locally due to dependency on KUBECONFIG
    
    Fixes https://github.com/istio/istio/issues/23966

--- mixer/adapter/kubernetesenv/kubernetesenv_norace_test.go
+++ mixer/adapter/kubernetesenv/kubernetesenv_norace_test.go
@@ -34,6 +34,8 @@ func Test_KubeSecretController(t *testing.T) {
 	secretcontroller.LoadKubeConfig = mockLoadKubeConfig
 	secretcontroller.ValidateClientConfig = mockValidateClientConfig
 	secretcontroller.CreateInterfaceFromClusterConfig = mockCreateInterfaceFromClusterConfig
+	secretcontroller.CreateMetadataInterfaceFromClusterConfig = mockCreateMetaInterfaceFromClusterConfig
+	secretcontroller.CreateDynamicInterfaceFromClusterConfig = mockCreateDynamicInterfaceFromClusterConfig
 
 	clientset := fake.NewSimpleClientset()
 	b := newBuilder(func(string, adapter.Env) (kubernetes.Interface, error) {
