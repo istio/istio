2a2c4d3eebe59e3e29aee6f347755d3c420f7749
John Howard <howardjohn@google.com>
2020-05-29 13:42:01 -0700

    Stop putting year in the copyright notice (#23958)
    * Stop putting year in the copyright notice
    
    This was already done on a few files by Martin, so I assume he got legal
    approval here.
    
    The motivation with this is to end bikeshedding on PRs about the
    copyright year.
    
    * Fix tests
    
    * fix gen

--- pkg/test/framework/tools/featuresgen/cmd/root.go
+++ pkg/test/framework/tools/featuresgen/cmd/root.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Istio Authors
+// Copyright Istio Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -28,7 +28,7 @@ import (
 )
 
 const (
-	Copyright = `// Copyright 2020 Istio Authors
+	Copyright = `// Copyright Istio Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
