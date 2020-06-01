5a7d8a777bca5d4c892f4e32c708261a655a5f9d
gargnupur <gargnupur@google.com>
2020-06-01 14:09:06 -0700

    fix lint error
    Signed-off-by: gargnupur <gargnupur@google.com>

--- tests/integration/mixer/policy/ratelimiting_test.go
+++ tests/integration/mixer/policy/ratelimiting_test.go
@@ -94,7 +94,7 @@ func testRedisQuota(t *testing.T, config bookinfo.ConfigFile, destinationService
 		defer deleteConfigOrFail(t, config, ctx)
 		util.AllowRuleSync(t)
 
-		_ = util.SendTraffic(ing, t, "Sending traffic...", "", "", 300)
+		res := util.SendTraffic(ing, t, "Sending traffic...", "", "", 300)
 		_, _ = util.FetchRequestCount(t, prom, destinationService, "",
 			bookInfoNameSpaceStr, 300)
 
