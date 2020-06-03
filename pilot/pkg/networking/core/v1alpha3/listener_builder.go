591d4b8807af88ad8982c021962789309f9244df
John Howard <howardjohn@google.com>
2020-06-03 14:51:41 -0700

    Fix condition

--- pilot/pkg/networking/core/v1alpha3/listener_builder.go
+++ pilot/pkg/networking/core/v1alpha3/listener_builder.go
@@ -81,7 +81,7 @@ func amendFilterChainMatchFromInboundListener(chain *listener.FilterChain, l *li
 				log.Debugf("Intercepted inbound listener %s have neither 0 or 1 prefix ranges. Actual:  %d",
 					l.Name, len(chain.FilterChainMatch.PrefixRanges))
 			}
-			if sockAddr.Address == WildcardAddress || sockAddr.Address == WildcardIPv6Address {
+			if sockAddr.Address != WildcardAddress && sockAddr.Address != WildcardIPv6Address {
 				chain.FilterChainMatch.PrefixRanges = []*core.CidrRange{util.ConvertAddressToCidr(sockAddr.GetAddress())}
 			}
 		}
