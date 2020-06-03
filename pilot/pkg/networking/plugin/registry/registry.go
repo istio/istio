2de4805a50fbe62bbf557b2fc8ac6b102ed02063
John Howard <howardjohn@google.com>
2020-06-02 17:54:50 -0700

    Remove health plugin

--- pilot/pkg/networking/plugin/registry/registry.go
+++ pilot/pkg/networking/plugin/registry/registry.go
@@ -21,14 +21,12 @@ import (
 	"istio.io/istio/pilot/pkg/networking/plugin"
 	"istio.io/istio/pilot/pkg/networking/plugin/authn"
 	"istio.io/istio/pilot/pkg/networking/plugin/authz"
-	"istio.io/istio/pilot/pkg/networking/plugin/health"
 	"istio.io/istio/pilot/pkg/networking/plugin/mixer"
 )
 
 var availablePlugins = map[string]plugin.Plugin{
 	plugin.Authn:  authn.NewPlugin(),
 	plugin.Authz:  authz.NewPlugin(),
-	plugin.Health: health.NewPlugin(),
 	plugin.Mixer:  mixer.NewPlugin(),
 }
 
