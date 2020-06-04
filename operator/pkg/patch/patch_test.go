ca7e72b036b388eeaa87d039c1dc57f98a5837a6
Shakti Das <shaktiprakash.das@salesforce.com>
2020-06-05 01:33:11 +0530

    Support appending to list via k8s component patches (#24369)
    Signed-off-by: Shakti <shaktiprakash.das@salesforce.com>

--- operator/pkg/patch/patch_test.go
+++ operator/pkg/patch/patch_test.go
@@ -196,6 +196,29 @@ a:
     d: n3
 `,
 		},
+		{
+			desc:  "AppendToListEntry",
+			path:  `a.b.[name:n2].list.[3]`,
+			value: `v4`,
+			want: `
+apiVersion: extensions/v1beta1
+kind: Deployment
+metadata:
+  name: istio-citadel
+  namespace: istio-system
+a:
+  b:
+  - name: n1
+    value: v1
+  - list:
+    - v1
+    - v2
+    - v3_regex
+    - v4
+    name: n2
+  c:
+`,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.desc, func(t *testing.T) {
