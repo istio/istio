16d502105a0f028abd37aac807ba87205a541773
Aditya Prerepa <adiprerepa@gmail.com>
2020-06-04 10:58:37 -0700

    Add istio MTLS tests

--- pkg/bootstrap/option/convert_test.go
+++ pkg/bootstrap/option/convert_test.go
@@ -116,14 +116,106 @@ func TestTlsContextConvert(t *testing.T) {
 				Sni: "bar",
 			},
 		},
+		{
+			desc: "tls-istio-mutual-no-certs",
+			tls: &networkingAPI.ClientTLSSettings{
+				Mode: networkingAPI.ClientTLSSettings_ISTIO_MUTUAL,
+			},
+			sni: "i-should-be-sni",
+			meta: &model.NodeMetadata{},
+			expectTlsCtx: &auth.UpstreamTLSContext{
+				CommonTLSContext: &auth.CommonTLSContext{
+					TLSCertificates: []*auth.TLSCertificate{
+						{
+							CertificateChain: &auth.DataSource{
+								Filename: "/etc/certs/root-cert.pem",
+							},
+							PrivateKey: &auth.DataSource{
+								Filename: "/etc/certs/key.pem",
+							},
+						},
+					},
+					ValidationContext: &auth.CertificateValidationContext{
+						TrustedCa: &auth.DataSource{
+							Filename: "/etc/certs/cert-chain.pem",
+						},
+					},
+					AlpnProtocols: []string{"istio", "h2"},
+				},
+				Sni: "i-should-be-sni",
+			},
+		},
+		{
+			desc: "tls-istio-mutual-provide-certs",
+			tls: &networkingAPI.ClientTLSSettings{
+				Mode: networkingAPI.ClientTLSSettings_ISTIO_MUTUAL,
+				ClientCertificate: "foo.pem",
+				PrivateKey: "bar.pem",
+			},
+			sni: "i-should-be-sni",
+			meta: &model.NodeMetadata{},
+			expectTlsCtx: &auth.UpstreamTLSContext{
+				CommonTLSContext: &auth.CommonTLSContext{
+					TLSCertificates: []*auth.TLSCertificate{
+						{
+							CertificateChain: &auth.DataSource{
+								Filename: "foo.pem",
+							},
+							PrivateKey: &auth.DataSource{
+								Filename: "bar.pem",
+							},
+						},
+					},
+					ValidationContext: &auth.CertificateValidationContext{
+						TrustedCa: &auth.DataSource{
+							Filename: "/etc/certs/cert-chain.pem",
+						},
+					},
+					AlpnProtocols: []string{"istio", "h2"},
+				},
+				Sni: "i-should-be-sni",
+			},
+		},
+		{
+			desc: "tls-istio-mutual-meta-certs",
+			tls: &networkingAPI.ClientTLSSettings{
+				Mode: networkingAPI.ClientTLSSettings_ISTIO_MUTUAL,
+				ClientCertificate: "foo.pem",
+				PrivateKey: "bar.pem",
+			},
+			sni: "i-should-be-sni",
+			meta: &model.NodeMetadata{
+				TLSClientCertChain: "better-foo.pem",
+				TLSClientKey: "better-bar.pem",
+			},
+			expectTlsCtx: &auth.UpstreamTLSContext{
+				CommonTLSContext: &auth.CommonTLSContext{
+					TLSCertificates: []*auth.TLSCertificate{
+						{
+							CertificateChain: &auth.DataSource{
+								Filename: "better-foo.pem",
+							},
+							PrivateKey: &auth.DataSource{
+								Filename: "better-bar.pem",
+							},
+						},
+					},
+					ValidationContext: &auth.CertificateValidationContext{
+						TrustedCa: &auth.DataSource{
+							Filename: "/etc/certs/cert-chain.pem",
+						},
+					},
+					AlpnProtocols: []string{"istio", "h2"},
+				},
+				Sni: "i-should-be-sni",
+			},
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.desc, func(t *testing.T) {
 			if got := tlsContextConvert(tt.tls, tt.sni, tt.meta); !reflect.DeepEqual(tt.expectTlsCtx, got) {
 				t.Errorf("%s: expected TLS ctx %v got %v", tt.desc, tt.expectTlsCtx, got)
-				//g, _ := json.Marshal(got)
-				//log.Println(string(g))
 			}
 		})
 	}
