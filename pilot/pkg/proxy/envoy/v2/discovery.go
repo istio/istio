cb01b4a6974373ffc56058d1d07c046ac5b4b04a
Rama Chavali <rama.rao@salesforce.com>
2020-06-06 16:15:30 +0530

    comment
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/proxy/envoy/v2/discovery.go
+++ pilot/pkg/proxy/envoy/v2/discovery.go
@@ -193,6 +193,8 @@ func (s *DiscoveryServer) Register(rpcs *grpc.Server) {
 
 func (s *DiscoveryServer) Start(stopCh <-chan struct{}) {
 	adsLog.Infof("Starting ADS server")
+	// Initialize non-k8s registries so that they can be used
+	// for updating service shards during push.
 	s.initNonK8sRegistries()
 	go s.handleUpdates(stopCh)
 	go s.periodicRefreshMetrics(stopCh)
@@ -200,7 +202,6 @@ func (s *DiscoveryServer) Start(stopCh <-chan struct{}) {
 }
 
 func (s *DiscoveryServer) initNonK8sRegistries() {
-	// Initialize non-k8s registries so that they can be used for updating service shards during push.
 	var registries []serviceregistry.Instance
 	if agg, ok := s.Env.ServiceDiscovery.(*aggregate.Controller); ok {
 		registries = agg.GetRegistries()
