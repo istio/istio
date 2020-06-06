50e85a0858239527479b4c647ac48242786389ae
Rama Chavali <rama.rao@salesforce.com>
2020-06-06 14:36:38 +0530

    cleanup more imports
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pkg/bootstrap/platform/platform.go
+++ pkg/bootstrap/platform/platform.go
@@ -15,7 +15,7 @@
 package platform
 
 import (
-	corev3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 )
 
 // Environment provides information for the platform on which the bootstrapping
@@ -29,7 +29,7 @@ type Environment interface {
 
 	// Locality returns the run location for the bootstrap transformed from the
 	// platform-specific representation into the Envoy Locality schema.
-	Locality() *corev3.Locality
+	Locality() *core.Locality
 }
 
 // Unknown provides a default platform environment for cases in which the platform
@@ -42,6 +42,6 @@ func (*Unknown) Metadata() map[string]string {
 }
 
 // Locality returns an empty core.Locality struct.
-func (*Unknown) Locality() *corev3.Locality {
-	return &corev3.Locality{}
+func (*Unknown) Locality() *core.Locality {
+	return &core.Locality{}
 }
