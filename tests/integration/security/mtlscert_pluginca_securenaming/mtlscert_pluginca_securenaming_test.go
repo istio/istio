6a3aafb86bbbbf0c48315e4065a6afe5ec1b8b3a
williamaronli <64571891+williamaronli@users.noreply.github.com>
2020-06-09 10:04:48 -0700

    add feature label for security integration test (#24346)
    * add feature label
    
    * migrate tls and mtls to ingress feature sub folder
    
    * delete useless label and refactor the label
    
    * fix ingress name
    
    * add more features
    
    * add label for webhook

--- tests/integration/security/mtlscert_pluginca_securenaming/mtlscert_pluginca_securenaming_test.go
+++ tests/integration/security/mtlscert_pluginca_securenaming/mtlscert_pluginca_securenaming_test.go
@@ -101,6 +101,7 @@ spec:
 // - Secure naming information is respected in the mTLS handshake.
 func TestMTLSCertPluginCASecureNaming(t *testing.T) {
 	framework.NewTest(t).
+		Features("security.peer.secure-naming", "security.control-plane.plugin-cert").
 		RequiresEnvironment(environment.Kube).
 		Run(func(ctx framework.TestContext) {
 			istioCfg := istio.DefaultConfigOrFail(t, ctx)
