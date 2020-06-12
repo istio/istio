6fd0f9c25c239a153639f6fc8726425f5e82e2ac
Navraj Singh Chhina <navuchhina@live.com>
2020-06-12 14:57:18 -0400

    add integration tests

---
+++ tests/integration/security/sds_gateway_egress/main_test.go
@@ -0,0 +1,67 @@
+//  Copyright Istio Authors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package sdsgatewayegress
+
+import (
+	"testing"
+
+	"istio.io/istio/pkg/test/framework"
+	"istio.io/istio/pkg/test/framework/components/istio"
+	"istio.io/istio/pkg/test/framework/components/pilot"
+	"istio.io/istio/pkg/test/framework/components/prometheus"
+	"istio.io/istio/pkg/test/framework/label"
+	"istio.io/istio/pkg/test/framework/resource"
+)
+
+var (
+	inst istio.Instance
+	p    pilot.Instance
+	prom prometheus.Instance
+)
+
+func TestMain(m *testing.M) {
+	framework.
+		NewSuite("sds_egress_egress_mtls_test", m).
+		Skip("https://github.com/istio/istio/issues/17933").
+		Label(label.CustomSetup).
+
+		// SDS requires Kubernetes 1.13
+		RequireEnvironmentVersion("1.13").
+		RequireSingleCluster().
+		Setup(istio.Setup(&inst, setupConfig)).
+		Setup(func(ctx resource.Context) (err error) {
+			if p, err = pilot.New(ctx, pilot.Config{}); err != nil {
+				return err
+			}
+			if prom, err = prometheus.New(ctx, prometheus.Config{}); err != nil {
+				return err
+			}
+			return nil
+		}).
+		Run()
+
+}
+
+func setupConfig(cfg *istio.Config) {
+	if cfg == nil {
+		return
+	}
+	cfg.ControlPlaneValues = `
+components:
+  gateways:
+    istio-egressgateway:
+      enabled: true
+`
+}
---
+++ tests/integration/security/sds_gateway_egress/sds_gateway_egress_test.go
@@ -0,0 +1,175 @@
+//  Copyright Istio Authors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package sdsgatewayegress
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"istio.io/istio/pkg/test/echo/common/response"
+	epb "istio.io/istio/pkg/test/echo/proto"
+	"istio.io/istio/pkg/test/framework"
+	"istio.io/istio/pkg/test/framework/components/echo"
+	"istio.io/istio/pkg/test/framework/components/echo/echoboot"
+	"istio.io/istio/pkg/test/framework/components/istio"
+	"istio.io/istio/pkg/test/framework/components/namespace"
+	"istio.io/istio/pkg/test/framework/components/prometheus"
+	"istio.io/istio/pkg/test/framework/resource/environment"
+	"istio.io/istio/pkg/test/util/file"
+	"istio.io/istio/tests/integration/security/util"
+)
+
+const (
+	// should be templated into YAML, and made interchangeable with other sites
+	externalURL      = "http://edition.cnn.com/politics"
+	externalReqCount = 1
+	egressName       = "istio-egressgateway"
+	// paths to test configs
+	simpleTLSGatewayConfig         = "testdata/gateway-tls-origination.yaml"
+	simpleTLSDestinationRuleConfig = "testdata/destination-rule-tls-origination.yaml"
+)
+
+// TestSdsEgressGatewayTls brings up an SDS enabled cluster and will ensure that the TLS origination at
+// egress gateway allows secure communication between the egress gateway and external workload.
+//This test brings up an egress gateway to originate TLS connection. The test will ensure that requests
+//are routed securely through the egress gateway and that the TLS origination happens at the gateway.
+func TestSdsEgressGatewayTls(t *testing.T) {
+	framework.NewTest(t).
+		Run(func(ctx framework.TestContext) {
+			ctx.RequireOrSkip(environment.Kube)
+			istioCfg := istio.DefaultConfigOrFail(t, ctx)
+
+			namespace.ClaimOrFail(t, ctx, istioCfg.SystemNamespace)
+			ns := namespace.NewOrFail(t, ctx, namespace.Config{
+				Prefix: "sds-egress-gateway-tls-origination",
+				Inject: true,
+			})
+			applySetupConfig(ctx, ns)
+
+			testCases := map[string]struct {
+				destinationRulePath string
+				configPath          string
+				response            string
+			}{
+				"SIMPLE TLS origination from egress gateway succeeds": {
+					destinationRulePath: simpleTLSDestinationRuleConfig,
+					configPath:          simpleTLSGatewayConfig,
+					response:            response.StatusCodeOK,
+				},
+			}
+
+			for name, tc := range testCases {
+				ctx.NewSubTest(name).
+					Run(func(ctx framework.TestContext) {
+						doOriginationTest(ctx, ns, tc.destinationRulePath, tc.configPath, tc.response)
+					})
+			}
+		})
+}
+
+func doOriginationTest(
+	ctx framework.TestContext, ns namespace.Instance, destinationRulePath, configPath, expectedResp string) {
+	var client echo.Instance
+	echoboot.NewBuilderOrFail(ctx, ctx).
+		With(&client, util.EchoConfig("client", ns, false, nil, p)).
+		BuildOrFail(ctx)
+	ctx.ApplyConfigOrFail(ctx, ns.Name(), file.AsStringOrFail(ctx, destinationRulePath))
+	ctx.ApplyConfigOrFail(ctx, ns.Name(), file.AsStringOrFail(ctx, configPath))
+	defer ctx.DeleteConfigOrFail(ctx, ns.Name(), file.AsStringOrFail(ctx, configPath))
+
+	// give the configuration a moment to kick in
+	time.Sleep(time.Second * 20)
+	pretestReqCount := getEgressRequestCountOrFail(ctx, ns, prom)
+
+	for i := 0; i < externalReqCount; i++ {
+		w := client.WorkloadsOrFail(ctx)[0]
+		responses, err := w.ForwardEcho(context.TODO(), &epb.ForwardEchoRequest{
+			Url:   externalURL,
+			Count: 1,
+		})
+		if err != nil {
+			ctx.Fatalf("failed to make request from echo instance to %s: %v", externalURL, err)
+		}
+		if len(responses) < 1 {
+			ctx.Fatalf("received no responses from request to %s", externalURL)
+		}
+		resp := responses[0]
+
+		if expectedResp != resp.Code {
+			ctx.Errorf("expected status %s but got %s", expectedResp, resp.Code)
+		}
+
+	}
+
+	// give prometheus some time to ingest the metrics
+	posttestReqCount := getEgressRequestCountOrFail(ctx, ns, prom)
+	newGatewayReqs := posttestReqCount - pretestReqCount
+	if newGatewayReqs != externalReqCount {
+		ctx.Errorf("expected %d requests routed through egress, got %d",
+			externalReqCount, newGatewayReqs)
+	}
+}
+
+// sets up the destination rule to route through egress, virtual service, and service entry
+func applySetupConfig(ctx framework.TestContext, ns namespace.Instance) {
+	ctx.Helper()
+
+	configFiles := []string{
+		"testdata/service-entry-cnn.yaml",
+		"testdata/rule-route-request-to-egress-cnn.yaml",
+		"testdata/destination-rule-to-gateway.yaml",
+	}
+
+	for _, c := range configFiles {
+		if err := ctx.ApplyConfig(ns.Name(), file.AsStringOrFail(ctx, c)); err != nil {
+			ctx.Fatalf("failed to apply configuration file %s; err: %v", c, err)
+		}
+	}
+}
+
+func getMetric(ctx framework.TestContext, prometheus prometheus.Instance, query string) (float64, error) {
+	ctx.Helper()
+
+	value, err := prometheus.WaitForQuiesce(query)
+	if err != nil {
+		return 0, fmt.Errorf("failed to retrieve metric from prom with err: %v", err)
+	}
+
+	metric, err := prometheus.Sum(value, nil)
+	if err != nil {
+		ctx.Logf("value: %s", value.String())
+		return 0, fmt.Errorf("could not find metric value: %v", err)
+	}
+
+	return metric, nil
+}
+
+func getEgressRequestCountOrFail(ctx framework.TestContext, ns namespace.Instance, prom prometheus.Instance) int {
+	query := fmt.Sprintf("istio_requests_total{destination_app=\"%s\",source_workload_namespace=\"%s\"}",
+		egressName, ns.Name())
+	ctx.Helper()
+
+	reqCount, err := getMetric(ctx, prom, query)
+	if err != nil {
+		// assume that if the request failed, it was because there was no metric ingested
+		// if this is not the case, the test will fail down the road regardless
+		// checking for error based on string match could lead to future failure
+		reqCount = 0
+	}
+
+	return int(reqCount)
+}
