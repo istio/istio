e72ca39ee231f8ef95adfe5f45dabb639e01578a
Venil Noronha <veniln@vmware.com>
2020-06-16 22:34:36 -0700

    Fix tests
    Signed-off-by: Venil Noronha <veniln@vmware.com>

--- mixer/adapter/kubernetesenv/cache.go
+++ mixer/adapter/kubernetesenv/cache.go
@@ -144,9 +144,8 @@ func (c *controllerImpl) Pod(podKey string) (*v1.Pod, bool) {
 		return nil, false
 	}
 	if len(objs) > 0 {
-		maxCreationTime := metav1.NewTime(time.Time{})
 		var latestPod *v1.Pod
-		for _, obj := range objs {
+		for i, obj := range objs {
 			pod, ok := obj.(*v1.Pod)
 			if !ok {
 				return nil, false
@@ -154,8 +153,7 @@ func (c *controllerImpl) Pod(podKey string) (*v1.Pod, bool) {
 			// If Pods associated with completed Jobs exist, there can be a case
 			// where more than 1 Pod is found during lookup, and we should
 			// always pick the latest created Pod out of the lot.
-			if maxCreationTime.Before(&pod.CreationTimestamp) {
-				maxCreationTime = pod.CreationTimestamp
+			if i == 0 || latestPod.CreationTimestamp.Before(&pod.CreationTimestamp) {
 				latestPod = pod
 			}
 		}
