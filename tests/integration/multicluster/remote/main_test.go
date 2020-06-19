99b67ebd08815508e352970a67e49f226eb5e971
Gregory Hanson <gihanson@us.ibm.com>
2020-06-19 12:39:27 -0400

    add features for multicluster tests (#24779)
    * add features for multicluster tests
    
    * typo
    
    * subtests inherit feature from parent tests
    
    * update multicluster tests to pass and set Features in tests

--- tests/integration/multicluster/remote/main_test.go
+++ tests/integration/multicluster/remote/main_test.go
@@ -69,13 +69,13 @@ func TestMain(m *testing.M) {
 }
 
 func TestMulticlusterReachability(t *testing.T) {
-	multicluster.ReachabilityTest(t, mcReachabilityNS, pilots)
+	multicluster.ReachabilityTest(t, mcReachabilityNS, pilots, "installation.multicluster.remote")
 }
 
 func TestClusterLocalService(t *testing.T) {
-	multicluster.ClusterLocalTest(t, clusterLocalNS, pilots)
+	multicluster.ClusterLocalTest(t, clusterLocalNS, pilots, "installation.multicluster.remote")
 }
 
 func TestTelemetry(t *testing.T) {
-	multicluster.TelemetryTest(t, mcReachabilityNS, pilots)
+	multicluster.TelemetryTest(t, mcReachabilityNS, pilots, "installation.multicluster.remote")
 }
