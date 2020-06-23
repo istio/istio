43f401d7f01c7c3f2583e36600f71237566130e5
Xinnan Wen <iamwen@google.com>
2020-06-22 21:40:25 -0700

    fix hpa scaleTargetRef

--- operator/pkg/translate/translate_value_test.go
+++ operator/pkg/translate/translate_value_test.go
@@ -109,7 +109,7 @@ components:
           scaleTargetRef:
             apiVersion: apps/v1
             kind: Deployment
-            name: istio-pilot
+            name: istiod
           metrics:
            - resource:
                name: cpu
