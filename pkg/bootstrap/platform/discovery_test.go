d61b156cc331c4a7402542afdb510885d9de8cbf
Aditya Prerepa <adiprerepa@gmail.com>
2020-06-29 13:28:41 -0700

    setup and teardown funcs

--- pkg/bootstrap/platform/discovery_test.go
+++ pkg/bootstrap/platform/discovery_test.go
@@ -18,30 +18,25 @@ import (
 	"os"
 	"testing"
 	"time"
+
+	"istio.io/pkg/env"
 )
 
 type platMetaFn func(map[string]string) bool
-
-func UnsetEnvs(t *testing.T) {
-	err := os.Unsetenv("GCP_METADATA")
-	if err != nil {
-		t.Errorf("unable to tear down: %v", err)
-	}
-}
 
 func TestDiscoverWithTimeout(t *testing.T) {
 	tests := []struct {
 		desc         string
 		timeout      time.Duration
-		platKey      string
-		platVal      string
+		envSetup     func(t *testing.T)
+		envTeardown  func(t *testing.T)
 		platExpectFn platMetaFn
 	}{
 		{
-			desc:    "no-plat",
-			timeout: 1 * time.Second,
-			platKey: "",
-			platVal: "",
+			desc:        "no-plat",
+			timeout:     1 * time.Second,
+			envSetup:    func(*testing.T) {},
+			envTeardown: func(*testing.T) {},
 			platExpectFn: func(m map[string]string) bool {
 				// unknown has no metadata
 				return len(m) == 0
@@ -53,8 +48,23 @@ func TestDiscoverWithTimeout(t *testing.T) {
 		{
 			desc:    "gcp",
 			timeout: 1 * time.Second,
-			platKey: "GCP_METADATA",
-			platVal: "FOO|BAR|BAZ|MAR",
+			envSetup: func(*testing.T) {
+				// dont want to mess with GCP_METADATA on the host
+				// it may be write-protected, and runs on GKE
+				gcpMetadataVar = env.RegisterStringVar("TEST_GCP_METADATA", "", "Pipe separted GCP metadata, schemed as PROJECT_ID|PROJECT_NUMBER|CLUSTER_NAME|CLUSTER_ZONE")
+				err := os.Setenv("TEST_GCP_METADATA", "FOO|BAR|BAZ|MAR")
+				if err != nil {
+					t.Fatalf("Unable to setup environment: %v", err)
+				}
+			},
+			envTeardown: func(t *testing.T) {
+				// re-register GCP_METADATA gcpMetadataVar
+				err := os.Unsetenv("TEST_GCP_METADATA")
+				if err != nil {
+					t.Fatalf("Unable to tear down: %v", err)
+				}
+				gcpMetadataVar = env.RegisterStringVar("GCP_METADATA", "", "Pipe separted GCP metadata, schemed as PROJECT_ID|PROJECT_NUMBER|CLUSTER_NAME|CLUSTER_ZONE")
+			},
 			platExpectFn: func(m map[string]string) bool {
 				// PROJECT_ID|PROJECT_NUMBER|CLUSTER_NAME|CLUSTER_ZONE -> FOO|BAR|BAZ|MAR
 				if proj, ok := m[GCPProject]; !ok || proj != "FOO" {
@@ -76,12 +86,8 @@ func TestDiscoverWithTimeout(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.desc, func(t *testing.T) {
-			UnsetEnvs(t)
-			err := os.Setenv(tt.platKey, tt.platVal)
-			defer UnsetEnvs(t)
-			if err != nil && tt.platKey != "" {
-				t.Errorf("unable to setup: %v", err)
-			}
+			tt.envSetup(t)
+			defer tt.envTeardown(t)
 			if got := DiscoverWithTimeout(tt.timeout); !tt.platExpectFn(got.Metadata()) {
 				t.Errorf("TestDiscoveryWithTimeout(%s) %s: %v metadata not expected", tt.timeout.String(), tt.desc, got.Metadata())
 			}
