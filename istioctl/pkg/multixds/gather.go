90b1ec969016ed51301b54c8573d678c3323f02c
Ed Snible <snible@us.ibm.com>
2020-06-29 14:17:00 -0400

    Fix typo

--- istioctl/pkg/multixds/gather.go
+++ istioctl/pkg/multixds/gather.go
@@ -1,6 +1,6 @@
-// Copyright Istio Authors
+// Copyright Istio Authors.
 //
-// Licensed under the Apache License, ~ 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
