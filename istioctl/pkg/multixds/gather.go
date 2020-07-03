0837d4dbdd33ed13fb7f501774d9de7d971eb410
Ed Snible <snible@us.ibm.com>
2020-07-03 09:13:00 -0400

    Never check cert hostname when using portforward

--- istioctl/pkg/multixds/gather.go
+++ istioctl/pkg/multixds/gather.go
@@ -74,9 +74,9 @@ func queryEachShard(dr *xdsapi.DiscoveryRequest, istioNamespace string, kubeClie
 		defer fw.Close()
 		response, err := xds.GetXdsResponse(dr, &clioptions.CentralControlPlaneOptions{
 			Xds:                fw.Address(),
+			InsecureSkipVerify: true, // (Client certs won't be good for fw.Address())
 			CertDir:            centralOpts.CertDir,
 			Timeout:            centralOpts.Timeout,
-			InsecureSkipVerify: centralOpts.InsecureSkipVerify,
 		})
 		if err != nil {
 			return nil, fmt.Errorf("could not get XDS from discovery pod %q: %v", pod.Name, err)
