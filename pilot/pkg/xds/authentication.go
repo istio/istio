da9f068d42794fd93dbaf6cfba2386f953911f7f
xuzhonghu <xuzhonghu@huawei.com>
2020-07-06 19:12:17 +0800

    address comments

--- pilot/pkg/xds/authentication.go
+++ pilot/pkg/xds/authentication.go
@@ -29,35 +29,32 @@ import (
 // If no authenticators are configured, or if the request is on a non-secure
 // stream ( 15010 ) - returns an empty list of principals and no errors.
 func (s *DiscoveryServer) authenticate(ctx context.Context) ([]string, error) {
+	if len(s.Authenticators) == 0 {
+		return nil, nil
+	}
+
 	// Authenticate - currently just checks that request has a certificate signed with the our key.
 	// Protected by flag to avoid breaking upgrades - should be enabled in multi-cluster/meshexpansion where
 	// XDS is exposed.
-	if len(s.Authenticators) > 0 {
-		peerInfo, ok := peer.FromContext(ctx)
-		if !ok {
-			return nil, errors.New("invalid context")
-		}
-		// Not a TLS connection, we will not authentication
-		// TODO: add a flag to prevent unauthenticated requests ( 15010 )
-		// request not over TLS ( on the insecure port
-		if _, ok := peerInfo.AuthInfo.(credentials.TLSInfo); !ok {
-			return nil, nil
-		}
-		var authenticatedID *authenticate.Caller
-		for _, authn := range s.Authenticators {
-			u, err := authn.Authenticate(ctx)
-			if u != nil && err == nil {
-				authenticatedID = u
-				break
-			}
-		}
-
-		if authenticatedID == nil {
-			adsLog.Errora("Failed to authenticate client ", peerInfo)
-			return nil, errors.New("authentication failure")
-		}
-
-		return authenticatedID.Identities, nil
+	peerInfo, ok := peer.FromContext(ctx)
+	if !ok {
+		return nil, errors.New("invalid context")
 	}
-	return nil, nil
+	// Not a TLS connection, we will not perform authentication
+	// TODO: add a flag to prevent unauthenticated requests ( 15010 )
+	// request not over TLS on the insecure port
+	if _, ok := peerInfo.AuthInfo.(credentials.TLSInfo); !ok {
+		return nil, nil
+	}
+	var authenticatedID *authenticate.Caller
+	for _, authn := range s.Authenticators {
+		u, err := authn.Authenticate(ctx)
+		// If one authenticator passes, return
+		if u != nil && err == nil {
+			return authenticatedID.Identities, nil
+		}
+	}
+
+	adsLog.Errora("Failed to authenticate client ", peerInfo)
+	return nil, errors.New("authentication failure")
 }
