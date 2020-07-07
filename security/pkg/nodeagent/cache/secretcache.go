8755c8c945a72414a5a1306fc8f658c9a8c975a9
williamaronli <fengxiangli@google.com>
2020-07-06 21:34:56 -0700

    add authorization test

--- security/pkg/nodeagent/cache/secretcache.go
+++ security/pkg/nodeagent/cache/secretcache.go
@@ -1066,7 +1066,7 @@ func (sc *SecretCache) shouldRotate(secret *model.SecretItem) bool {
 func (sc *SecretCache) isTokenExpired(secret *model.SecretItem) bool {
 	// skip check if the token passed from envoy is always valid (ex, normal k8s sa JWT).
 	if sc.configOptions.AlwaysValidTokenFlag {
-		sc.configOptions.AlwaysValidTokenFlag = false
+		//sc.configOptions.AlwaysValidTokenFlag = false
 		return false
 	} else {
 		return true
