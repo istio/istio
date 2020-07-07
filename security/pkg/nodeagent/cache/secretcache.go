2c10562da4541cd57b43c1d6894e60efca86ebb8
williamaronli <fengxiangli@google.com>
2020-07-06 21:37:48 -0700

    add authorization test

--- security/pkg/nodeagent/cache/secretcache.go
+++ security/pkg/nodeagent/cache/secretcache.go
@@ -662,6 +662,9 @@ func (sc *SecretCache) rotate(updateRootFlag bool) {
 					cacheLog.Errorf("%s failed to rotate secret: %v", logPrefix, err)
 					return
 				}
+				cacheLog.Infof("kkkkjjkjkjkjkjkjkjkjkkllllloooo\n")
+				cacheLog.Infof("%+v", sc.configOptions.OutputKeyCertToDir)
+				cacheLog.Infof("%ggggggggggggg")
 				// Output the key and cert to dir to make sure key and cert are rotated.
 				if err = nodeagentutil.OutputKeyCertToDir(sc.configOptions.OutputKeyCertToDir, ns.PrivateKey,
 					ns.CertificateChain, ns.RootCert); err != nil {
