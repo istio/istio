d1ca08a8efad423aed35cf36ea97cf55f8dd30cd
williamaronli <fengxiangli@google.com>
2020-07-09 00:06:15 -0700

    add authorization test

--- pkg/istio-agent/sds-agent.go
+++ pkg/istio-agent/sds-agent.go
@@ -527,7 +527,7 @@ func applyEnvVars() {
 	workloadSdsCacheOptions.InitialBackoffInMilliSec = int64(initialBackoffInMilliSecEnv)
 	// Disable the secret eviction for istio agent.
 	workloadSdsCacheOptions.EvictionDuration = 0
-	if citadel.ProvCert != "" && citadel.ProvCert != "/etc/certs" {
+	if citadel.ProvCert != "" {
 		workloadSdsCacheOptions.AlwaysValidTokenFlag = true
 	}
 	workloadSdsCacheOptions.OutputKeyCertToDir = serverOptions.OutputKeyCertToDir
