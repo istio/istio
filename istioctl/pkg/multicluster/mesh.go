d49afea601ce63f1b5d67ca46337c0876f624304
nmittler <nmittler@gmail.com>
2020-07-08 19:54:26 -0700

    Expose istioctl multicluster as a library
    This is part of a larger effort to remove the istioctl test component altogether in favor of just using the istioctl code.

--- istioctl/pkg/multicluster/mesh.go
+++ istioctl/pkg/multicluster/mesh.go
@@ -19,98 +19,93 @@ import (
 	"sort"
 	"strings"
 
-	"github.com/ghodss/yaml"
 	"github.com/spf13/cobra"
+
+	"istio.io/istio/pkg/kube"
 )
 
-// MeshDesc describes the topology of a multi-cluster mesh. The clustersByContext in the mesh reference the active
-// Kubeconfig file as described by https://kubernetes.io/docs/concepts/configuration/organize-cluster-access-kubeconfig.
-type MeshDesc struct {
-	// Mesh Identifier.
-	MeshID string `json:"mesh_id,omitempty"`
-
-	// Collection of clusters in the multi-cluster mesh. Clusters are indexed by context name and
-	// reference clusters defined in the Kubeconfig following kubectl precedence rules.
-	Clusters map[string]ClusterDesc `json:"contexts,omitempty"`
+// Mesh contains the clusters within the Istio mesh.
+type Mesh struct {
+	id             string
+	clustersByName map[string]*Cluster
+	subject        *Cluster
 }
 
-// ClusterDesc describes attributes of a cluster and the desired state of joining the mesh.
-type ClusterDesc struct {
-	// Name of the cluster's network
-	Network string `json:"network,omitempty"`
-
-	// Optional Namespace override of the Istio control plane. `istio-system` if not set.
-	Namespace string `json:"namespace,omitempty"`
-
-	// Optional service account to use for cross-cluster authentication. `istio-multi` if not set.
-	ServiceAccountReader string `json:"serviceAccountReader"`
-
-	// When true, disables linking the service registry of this cluster with other clustersByContext in the mesh.
-	DisableRegistryJoin bool `json:"disableRegistryJoin,omitempty"`
+// ID returns the unique identifier of this Mesh.
+func (m *Mesh) ID() string {
+	return m.id
 }
 
-func (m *Mesh) addCluster(c *Cluster) {
-	m.clustersByContext[c.Context] = c
-	m.clustersByClusterName[c.clusterName] = c
+// Subject returns the cluster that is the subject of the current command.
+func (m *Mesh) Subject() *Cluster {
+	return m.subject
 }
 
+// GetCluster by name.
+func (m *Mesh) GetCluster(name string) *Cluster {
+	return m.clustersByName[name]
+}
+
+// AddCluster to this Mesh.
+func (m *Mesh) AddCluster(c *Cluster) {
+	m.clustersByName[c.Name] = c
+}
+
+// SortedCluster returns the list of Cluster instances sorted by name.
 func (m *Mesh) SortedClusters() []*Cluster {
-	sortedClusters := make([]*Cluster, 0, len(m.clustersByContext))
-	for _, other := range m.clustersByContext {
+	sortedClusters := make([]*Cluster, 0, len(m.clustersByName))
+	for _, other := range m.clustersByName {
 		sortedClusters = append(sortedClusters, other)
 	}
 	sort.Slice(sortedClusters, func(i, j int) bool {
-		return strings.Compare(sortedClusters[i].clusterName, sortedClusters[j].clusterName) < 0
+		return strings.Compare(sortedClusters[i].Name, sortedClusters[j].Name) < 0
 	})
 	return sortedClusters
 }
 
-type Mesh struct {
-	meshID                string
-	clustersByContext     map[string]*Cluster // by Context
-	clustersByClusterName map[string]*Cluster
-}
-
-func LoadMeshDesc(filename string, env Environment) (*MeshDesc, error) {
-	out, err := env.ReadFile(filename)
-	if err != nil {
-		return nil, fmt.Errorf("cannot read %v: %v", filename, err)
-	}
-	md := &MeshDesc{}
-	if err := yaml.Unmarshal(out, md); err != nil {
-		return nil, err
-	}
-	return md, nil
-}
-
-func NewMesh(md *MeshDesc, clusters ...*Cluster) *Mesh {
+// NewMesh creates a new Mesh instance from the given clusters.
+func NewMesh(md *MeshDesc, subject *Cluster, clusters ...*Cluster) *Mesh {
 	mesh := &Mesh{
-		meshID:                md.MeshID,
-		clustersByContext:     make(map[string]*Cluster),
-		clustersByClusterName: make(map[string]*Cluster),
+		id:             md.MeshID,
+		subject:        subject,
+		clustersByName: make(map[string]*Cluster),
 	}
 	for _, cluster := range clusters {
-		mesh.addCluster(cluster)
+		mesh.AddCluster(cluster)
 	}
 	return mesh
 }
 
-func meshFromFileDesc(filename string, env Environment) (*Mesh, error) {
-	md, err := LoadMeshDesc(filename, env)
+func meshFromFileDesc(filename, kubeconfig, subjectContext string, cfact ClientFactory, printer Printer) (*Mesh, error) {
+	md, err := LoadMeshDesc(filename)
 	if err != nil {
 		return nil, err
 	}
 
+	var subjectCluster *Cluster
 	clusters := make([]*Cluster, 0, len(md.Clusters))
 	for context, clusterDesc := range md.Clusters {
-		cluster, err := NewCluster(context, clusterDesc, env)
+		client, err := cfact.NewClient(kube.BuildClientCmd(kubeconfig, subjectContext))
+		if err != nil {
+			return nil, err
+		}
+
+		cluster, err := NewCluster(clusterDesc, context, client, printer)
 		if err != nil {
 			return nil, fmt.Errorf("error discovering %v: %v", context, err)
 		}
 		clusters = append(clusters, cluster)
+
+		if context == subjectContext {
+			subjectCluster = cluster
+		}
 	}
 
-	return NewMesh(md, clusters...), nil
+	if subjectCluster == nil {
+		return nil, fmt.Errorf("unable to locate subject cluster for context %s", subjectCluster)
+	}
+
+	return NewMesh(md, subjectCluster, clusters...), nil
 }
 
 func NewMulticlusterCommand() *cobra.Command {
