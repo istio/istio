8819e134fb5f1ab6e0072c30e4979e41fae20050
John Howard <howardjohn@google.com>
2020-07-09 10:52:10 -0700

    Handle tombstone in ledger updates

--- pilot/pkg/config/kube/crdclient/ledger.go
+++ pilot/pkg/config/kube/crdclient/ledger.go
@@ -15,7 +15,11 @@
 package crdclient
 
 import (
+	"fmt"
+
+	"istio.io/pkg/log"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/cache"
 
 	"istio.io/pkg/ledger"
 
@@ -39,8 +43,27 @@ func (cl *Client) SetLedger(l ledger.Ledger) error {
 	return nil
 }
 
+func castToObject(obj interface{}) (metav1.Object, error) {
+	iobj, ok := obj.(metav1.Object)
+	if !ok {
+		tombstone, ok := obj.(cache.DeletedFinalStateUnknown)
+		if !ok {
+			return nil, fmt.Errorf("couldn't get object from tombstone %#v", obj)
+		}
+		iobj, ok = tombstone.Obj.(metav1.Object)
+		if !ok {
+			return nil, fmt.Errorf("tombstone contained object that is not a object %#v", obj)
+		}
+	}
+	return iobj, nil
+}
+
 func (cl *Client) tryLedgerPut(obj interface{}, kind string) {
-	iobj := obj.(metav1.Object)
+	iobj, err := castToObject(obj)
+	if err != nil {
+		log.Errora(err)
+		return
+	}
 	key := model.Key(kind, iobj.GetName(), iobj.GetNamespace())
 	if _, err := cl.configLedger.Put(key, iobj.GetResourceVersion()); err != nil {
 		scope.Errorf("Failed to update %s in ledger, status will be out of date.", key)
@@ -48,7 +71,11 @@ func (cl *Client) tryLedgerPut(obj interface{}, kind string) {
 }
 
 func (cl *Client) tryLedgerDelete(obj interface{}, kind string) {
-	iobj := obj.(metav1.Object)
+	iobj, err := castToObject(obj)
+	if err != nil {
+		log.Errora(err)
+		return
+	}
 	key := model.Key(kind, iobj.GetName(), iobj.GetNamespace())
 	if err := cl.configLedger.Delete(key); err != nil {
 		scope.Errorf("Failed to delete %s in ledger, status will be out of date.", key)
