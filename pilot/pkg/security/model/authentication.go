53f5840521fc14b9fbad29348a07ccda08d4779b
Aditya Prerepa <adiprerepa@gmail.com>
2020-07-09 16:26:16 -0700

    init

--- pilot/pkg/security/model/authentication.go
+++ pilot/pkg/security/model/authentication.go
@@ -16,6 +16,7 @@ package model
 
 import (
 	"sync"
+	"time"
 
 	core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	envoy_config_grpc_credential "github.com/envoyproxy/go-control-plane/envoy/config/grpc_credential/v3"
@@ -183,7 +184,7 @@ func ConstructSdsSecretConfig(name, requestedType string) *tls.SdsSecretConfig {
 					},
 				},
 			},
-			InitialFetchTimeout: features.InitialFetchTimeout,
+			InitialFetchTimeout: ptypes.DurationProto(time.Second * 0),
 		},
 	}
 	if useV3 {
