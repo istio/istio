206c5b574b351fa56000704788a1fb3328b00ca8
Istio Automation <istio-testing-bot@google.com>
2020-07-10 10:36:32 -0700

    [release-1.6] Add version label to `pilot_xds`  metric (#25284)
    * Add version label to metric
    
    Signed-off-by: Liam White <liam@tetrate.io>
    
    * back to gauge
    
    Signed-off-by: Liam White <liam@tetrate.io>
    
    Co-authored-by: Liam White <liam@tetrate.io>

--- pilot/pkg/proxy/envoy/v2/ads.go
+++ pilot/pkg/proxy/envoy/v2/ads.go
@@ -769,21 +769,20 @@ func (s *DiscoveryServer) addCon(conID string, con *XdsConnection) {
 	s.adsClientsMutex.Lock()
 	defer s.adsClientsMutex.Unlock()
 	s.adsClients[conID] = con
-	xdsClients.Record(float64(len(s.adsClients)))
+	recordXDSClients(con.node.Metadata.IstioVersion, 1)
 }
 
 func (s *DiscoveryServer) removeCon(conID string) {
 	s.adsClientsMutex.Lock()
 	defer s.adsClientsMutex.Unlock()
 
-	if _, exist := s.adsClients[conID]; !exist {
+	if con, exist := s.adsClients[conID]; !exist {
 		adsLog.Errorf("ADS: Removing connection for non-existing node:%v.", conID)
 		totalXDSInternalErrors.Increment()
 	} else {
 		delete(s.adsClients, conID)
+		recordXDSClients(con.node.Metadata.IstioVersion, -1)
 	}
-
-	xdsClients.Record(float64(len(s.adsClients)))
 	if s.StatusReporter != nil {
 		go s.StatusReporter.RegisterDisconnect(conID, []string{ClusterType, ListenerType, RouteType, EndpointType})
 	}
--- pilot/pkg/proxy/envoy/v2/monitoring.go
+++ pilot/pkg/proxy/envoy/v2/monitoring.go
@@ -14,6 +14,8 @@
 package v2
 
 import (
+	"sync"
+
 	"google.golang.org/grpc/codes"
 
 	"istio.io/istio/pilot/pkg/model"
@@ -27,6 +29,7 @@ var (
 	clusterTag = monitoring.MustCreateLabel("cluster")
 	nodeTag    = monitoring.MustCreateLabel("node")
 	typeTag    = monitoring.MustCreateLabel("type")
+	versionTag = monitoring.MustCreateLabel("version")
 
 	cdsReject = monitoring.NewGauge(
 		"pilot_xds_cds_reject",
@@ -84,7 +87,10 @@ var (
 	xdsClients = monitoring.NewGauge(
 		"pilot_xds",
 		"Number of endpoints connected to this pilot using XDS.",
+		monitoring.WithLabels(versionTag),
 	)
+	xdsClientTrackerMutex                    = &sync.Mutex{}
+	xdsClientTracker      map[string]float64 = make(map[string]float64)
 
 	xdsResponseWriteTimeouts = monitoring.NewSum(
 		"pilot_xds_write_timeout",
@@ -163,6 +169,13 @@ var (
 	inboundServiceUpdates = inboundUpdates.With(typeTag.Value("svc"))
 	inboundServiceDeletes = inboundUpdates.With(typeTag.Value("svcdelete"))
 )
+
+func recordXDSClients(version string, delta float64) {
+	xdsClientTrackerMutex.Lock()
+	defer xdsClientTrackerMutex.Unlock()
+	xdsClientTracker[version] += delta
+	xdsClients.With(versionTag.Value(version)).Record(xdsClientTracker[version])
+}
 
 func recordPushTriggers(reasons ...model.TriggerReason) {
 	for _, r := range reasons {
