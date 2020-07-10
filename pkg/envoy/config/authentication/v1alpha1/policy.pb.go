6795d8e09fcee3e66457cc88eef8ef60f2190572
nmittler <nmittler@gmail.com>
2020-07-10 11:34:51 -0700

    Moving security envoy filters to pkg/envoy
    Also adding the istio-specific filters to the generated import list.

--- pkg/envoy/config/authentication/v1alpha1/policy.pb.go
+++ pkg/envoy/config/authentication/v1alpha1/policy.pb.go
@@ -7,8 +7,9 @@ package v1alpha1
 
 import (
 	fmt "fmt"
-	proto "github.com/golang/protobuf/proto"
 	math "math"
+
+	proto "github.com/golang/protobuf/proto"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
