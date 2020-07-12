istio/istio

commit 5ef2bdd531c54e61e4e75689b234af2d02face0d

Expose more of istioctl as library (#25384)
This is another step toward removing the istioctl test component.

operator/cmd/mesh/manifest-apply.go
@@ -22,6 +22,8 @@ import (
 	"github.com/spf13/cobra"
 
 	"istio.io/api/operator/v1alpha1"
+	"istio.io/pkg/log"
+
 	iopv1alpha1 "istio.io/istio/operator/pkg/apis/istio/v1alpha1"
 	"istio.io/istio/operator/pkg/cache"
 	"istio.io/istio/operator/pkg/helmreconciler"
@@ -29,7 +31,7 @@ import (
 	"istio.io/istio/operator/pkg/translate"
 	"istio.io/istio/operator/pkg/util/clog"
 	"istio.io/istio/operator/pkg/util/progress"
-	"istio.io/pkg/log"
+	"istio.io/istio/pkg/kube"
 )
 
 const (
@@ -144,8 +146,12 @@ func runApplyCmd(cmd *cobra.Command, rootArgs *rootArgs, maArgs *manifestApplyAr
 	if err := configLogs(logOpts); err != nil {
 		return fmt.Errorf("could not configure logs: %s", err)
 	}
-	if err := ApplyManifests(applyFlagAliases(maArgs.set, maArgs.manifestsPath, maArgs.revision), maArgs.inFilenames, maArgs.force, rootArgs.dryRun,
-		maArgs.kubeConfigPath, maArgs.context, maArgs.readinessTimeout, l); err != nil {
+	client, err := k8sClient(maArgs.kubeConfigPath, maArgs.context)
+	if err != nil {
+		return err
+	}
+	if err := ApplyManifests(applyFlagAliases(maArgs.set, maArgs.manifestsPath, maArgs.revision), maArgs.inFilenames,
+		maArgs.force, rootArgs.dryRun, client, maArgs.readinessTimeout, l); err != nil {
 		return fmt.Errorf("failed to apply manifests: %v", err)
 	}
 
@@ -156,14 +162,10 @@ func runApplyCmd(cmd *cobra.Command, rootArgs *rootArgs, maArgs *manifestApplyAr
 // cluster. See GenManifests for more description of the manifest generation process.
 //  force   validation warnings are written to logger but command is not aborted
 //  dryRun  all operations are done but nothing is written
-func ApplyManifests(setOverlay []string, inFilenames []string, force bool, dryRun bool,
-	kubeConfigPath string, context string, waitTimeout time.Duration, l clog.Logger) error {
+func ApplyManifests(inFilenames, setOverlay []string, force bool, dryRun bool,
+	client kube.Client, waitTimeout time.Duration, l clog.Logger) error {
 
-	restConfig, clientset, client, err := K8sConfig(kubeConfigPath, context)
-	if err != nil {
-		return err
-	}
-	_, iops, err := GenerateConfig(inFilenames, setOverlay, force, restConfig, l)
+	_, iops, err := GenerateConfig(inFilenames, setOverlay, force, client.RESTConfig(), l)
 	if err != nil {
 		return err
 	}
@@ -177,7 +179,7 @@ func ApplyManifests(setOverlay []string, inFilenames []string, force bool, dryRu
 		return err
 	}
 
-	if err := createNamespace(clientset, iop.Namespace); err != nil {
+	if err := createNamespace(client.Kube(), iop.Namespace); err != nil {
 		return err
 	}
 
@@ -185,7 +187,7 @@ func ApplyManifests(setOverlay []string, inFilenames []string, force bool, dryRu
 	cache.FlushObjectCaches()
 	opts := &helmreconciler.Options{DryRun: dryRun, Log: l, WaitTimeout: waitTimeout, ProgressLog: progress.NewLog(),
 		Force: force}
-	reconciler, err := helmreconciler.NewHelmReconciler(client, restConfig, iop, opts)
+	reconciler, err := helmreconciler.NewHelmReconciler(client.Controller(), client.RESTConfig(), iop, opts)
 	if err != nil {
 		return err
 	}

operator/cmd/mesh/manifest-generate.go
@@ -127,9 +127,9 @@ func manifestGenerate(args *rootArgs, mgArgs *manifestGenerateArgs, logopts *log
 // representation of path-values passed through the --set flag.
 // If force is set, validation errors will not cause processing to abort but will result in warnings going to the
 // supplied logger.
-func GenManifests(inFilename []string, setFlags []string, force bool,
+func GenManifests(inFilename []string, setOverlay []string, force bool,
 	kubeConfig *rest.Config, l clog.Logger) (name.ManifestMap, *v1alpha1.IstioOperatorSpec, error) {
-	mergedYAML, _, err := GenerateConfig(inFilename, setFlags, force, kubeConfig, l)
+	mergedYAML, _, err := GenerateConfig(inFilename, setOverlay, force, kubeConfig, l)
 	if err != nil {
 		return nil, nil, err
 	}

operator/cmd/mesh/profile-common.go
@@ -47,18 +47,18 @@ import (
 // Otherwise it will be the compiled in profile YAMLs.
 // In step 3, the remaining fields in the same user overlay are applied on the resulting profile base.
 // The force flag causes validation errors not to abort but only emit log/console warnings.
-func GenerateConfig(inFilenames []string, setFlags []string, force bool, kubeConfig *rest.Config,
+func GenerateConfig(inFilenames, setOverlay []string, force bool, kubeConfig *rest.Config,
 	l clog.Logger) (string, *v1alpha1.IstioOperatorSpec, error) {
-	if err := validateSetFlags(setFlags); err != nil {
+	if err := validateSetFlags(setOverlay); err != nil {
 		return "", nil, err
 	}
 
-	fy, profile, err := readYamlProfile(inFilenames, setFlags, force, l)
+	fy, profile, err := readYamlProfile(inFilenames, setOverlay, force, l)
 	if err != nil {
 		return "", nil, err
 	}
 
-	iopsString, iops, err := genIOPSFromProfile(profile, fy, setFlags, force, kubeConfig, l)
+	iopsString, iops, err := genIOPSFromProfile(profile, fy, setOverlay, force, kubeConfig, l)
 	if err != nil {
 		return "", nil, err
 	}

operator/cmd/mesh/shared.go
@@ -28,12 +28,11 @@ import (
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	v12 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"istio.io/pkg/log"
 
 	"istio.io/istio/operator/pkg/apis/istio/v1alpha1"
 	"istio.io/istio/operator/pkg/cache"
@@ -41,7 +40,7 @@ import (
 	"istio.io/istio/operator/pkg/name"
 	"istio.io/istio/operator/pkg/util"
 	"istio.io/istio/operator/pkg/util/clog"
-	"istio.io/pkg/log"
+	"istio.io/istio/pkg/kube"
 )
 
 var (
@@ -101,7 +100,7 @@ func readLayeredYAMLs(filenames []string, stdinReader io.Reader) (string, error)
 
 // confirm waits for a user to confirm with the supplied message.
 func confirm(msg string, writer io.Writer) bool {
-	fmt.Fprintf(writer, "%s ", msg)
+	_, _ = fmt.Fprintf(writer, "%s ", msg)
 
 	var response string
 	_, err := fmt.Scanln(&response)
@@ -116,77 +115,15 @@ func confirm(msg string, writer io.Writer) bool {
 	return false
 }
 
-// K8sConfig creates a rest.Config, Clientset and controller runtime Client from the given kubeconfig path and context.
-func K8sConfig(kubeConfigPath string, context string) (*rest.Config, *kubernetes.Clientset, client.Client, error) {
-	restConfig, clientset, err := InitK8SRestClient(kubeConfigPath, context)
-	if err != nil {
-		return nil, nil, nil, err
-	}
-	// We are running a one-off command locally, so we don't need to worry too much about rate limitting
-	// Bumping this up greatly decreases install time
-	restConfig.QPS = 50
-	restConfig.Burst = 100
-	client, err := client.New(restConfig, client.Options{Scheme: scheme.Scheme})
-	if err != nil {
-		return nil, nil, nil, err
-	}
-	return restConfig, clientset, client, nil
-}
-
-// InitK8SRestClient creates a rest.Config qne Clientset from the given kubeconfig path and context.
-func InitK8SRestClient(kubeconfig, kubeContext string) (*rest.Config, *kubernetes.Clientset, error) {
-	restConfig, err := defaultRestConfig(kubeconfig, kubeContext)
-	if err != nil {
-		return nil, nil, err
-	}
-	clientset, err := kubernetes.NewForConfig(restConfig)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return restConfig, clientset, nil
-}
-
-func defaultRestConfig(kubeconfig, kubeContext string) (*rest.Config, error) {
-	config, err := BuildClientConfig(kubeconfig, kubeContext)
+func k8sClient(kubeConfigPath string, context string) (kube.Client, error) {
+	restCfg, err := kube.DefaultRestConfig(kubeConfigPath, context, func(config *rest.Config) {
+		config.QPS = 50
+		config.Burst = 100
+	})
 	if err != nil {
 		return nil, err
 	}
-	config.APIPath = "/api"
-	config.GroupVersion = &v1.SchemeGroupVersion
-	config.NegotiatedSerializer = serializer.WithoutConversionCodecFactory{CodecFactory: scheme.Codecs}
-	return config, nil
-}
-
-// BuildClientConfig is a helper function that builds client config from a kubeconfig filepath.
-// It overrides the current context with the one provided (empty to use default).
-//
-// This is a modified version of k8s.io/client-go/tools/clientcmd/BuildConfigFromFlags with the
-// difference that it loads default configs if not running in-cluster.
-func BuildClientConfig(kubeconfig, context string) (*rest.Config, error) {
-	if kubeconfig != "" {
-		info, err := os.Stat(kubeconfig)
-		if err != nil || info.Size() == 0 {
-			// If the specified kubeconfig doesn't exists / empty file / any other error
-			// from file stat, fall back to default
-			kubeconfig = ""
-		}
-	}
-
-	//Config loading rules:
-	// 1. kubeconfig if it not empty string
-	// 2. In cluster config if running in-cluster
-	// 3. Config(s) in KUBECONFIG environment variable
-	// 4. Use $HOME/.kube/config
-	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
-	loadingRules.DefaultClientConfig = &clientcmd.DefaultClientConfig
-	loadingRules.ExplicitPath = kubeconfig
-	configOverrides := &clientcmd.ConfigOverrides{
-		ClusterDefaults: clientcmd.ClusterDefaults,
-		CurrentContext:  context,
-	}
-
-	return clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, configOverrides).ClientConfig()
+	return kube.NewClient(kube.NewClientConfigForRestConfig(restCfg))
 }
 
 // applyOptions contains the startup options for applying the manifest.

operator/cmd/mesh/upgrade.go
@@ -37,6 +37,8 @@ import (
 	"istio.io/istio/operator/pkg/util"
 	"istio.io/istio/operator/pkg/util/clog"
 	pkgversion "istio.io/istio/operator/pkg/version"
+	"istio.io/istio/pkg/kube"
+
 	"istio.io/pkg/log"
 )
 
@@ -128,7 +130,7 @@ func UpgradeCmd() *cobra.Command {
 // upgrade is the main function for Upgrade command
 func upgrade(rootArgs *rootArgs, args *upgradeArgs, l clog.Logger) (err error) {
 	// Create a kube client from args.kubeConfigPath and  args.context
-	kubeClient, err := NewClient(args.kubeConfigPath, args.context)
+	client, err := k8sClient(args.kubeConfigPath, args.context)
 	if err != nil {
 		return fmt.Errorf("failed to connect Kubernetes API server, error: %v", err)
 	}
@@ -154,13 +156,13 @@ func upgrade(rootArgs *rootArgs, args *upgradeArgs, l clog.Logger) (err error) {
 	istioNamespace := iop.Namespace(targetIOPS)
 
 	// Read the current Istio version from the the cluster
-	currentVersion, err := retrieveControlPlaneVersion(kubeClient, istioNamespace, l)
+	currentVersion, err := retrieveControlPlaneVersion(client, istioNamespace, l)
 	if err != nil && !args.force {
 		return fmt.Errorf("failed to read the current Istio version, error: %v", err)
 	}
 
 	// Check if the upgrade currentVersion -> targetVersion is supported
-	err = checkSupportedVersions(kubeClient, currentVersion)
+	err = checkSupportedVersions(client, currentVersion)
 	if err != nil && !args.force {
 		return fmt.Errorf("upgrade version check failed: %v -> %v. Error: %v",
 			currentVersion, targetVersion, err)
@@ -205,7 +207,7 @@ func upgrade(rootArgs *rootArgs, args *upgradeArgs, l clog.Logger) (err error) {
 
 	// Apply the Istio Control Plane specs reading from inFilenames to the cluster
 	err = ApplyManifests(applyFlagAliases(args.set, args.manifestsPath, ""), args.inFilenames, args.force, rootArgs.dryRun,
-		args.kubeConfigPath, args.context, args.readinessTimeout, l)
+		client, args.readinessTimeout, l)
 	if err != nil {
 		return fmt.Errorf("failed to apply the Istio Control Plane specs. Error: %v", err)
 	}
@@ -213,13 +215,13 @@ func upgrade(rootArgs *rootArgs, args *upgradeArgs, l clog.Logger) (err error) {
 	if !rootArgs.dryRun {
 		// Waits for the upgrade to complete by periodically comparing the each
 		// component version to the target version.
-		err = waitUpgradeComplete(kubeClient, istioNamespace, targetVersion, l)
+		err = waitUpgradeComplete(client, istioNamespace, targetVersion, l)
 		if err != nil {
 			return fmt.Errorf("failed to wait for the upgrade to complete. Error: %v", err)
 		}
 
 		// Read the upgraded Istio version from the the cluster
-		upgradeVer, err := retrieveControlPlaneVersion(kubeClient, istioNamespace, l)
+		upgradeVer, err := retrieveControlPlaneVersion(client, istioNamespace, l)
 		if err != nil {
 			return fmt.Errorf("failed to read the upgraded Istio version. Error: %v", err)
 		}
@@ -263,7 +265,7 @@ func waitForConfirmation(skipConfirmation bool, l clog.Logger) {
 
 var SupportedIstioVersions, _ = goversion.NewConstraint(">=1.6.0, <1.8")
 
-func checkSupportedVersions(kubeClient *Client, currentVersion string) error {
+func checkSupportedVersions(client kube.Client, currentVersion string) error {
 	curGoVersion, err := goversion.NewVersion(currentVersion)
 	if err != nil {
 		return fmt.Errorf("failed to parse the current version %q: %v", currentVersion, err)
@@ -273,12 +275,12 @@ func checkSupportedVersions(kubeClient *Client, currentVersion string) error {
 		return fmt.Errorf("upgrade is currently not supported from version: %v", currentVersion)
 	}
 
-	return kubeClient.CheckUnsupportedAlphaSecurityCRD()
+	return CheckUnsupportedAlphaSecurityCRD(client)
 }
 
 // retrieveControlPlaneVersion retrieves the version number from the Istio control plane
-func retrieveControlPlaneVersion(kubeClient ExecClient, istioNamespace string, l clog.Logger) (string, error) {
-	cv, e := kubeClient.GetIstioVersions(istioNamespace)
+func retrieveControlPlaneVersion(client kube.Client, istioNamespace string, l clog.Logger) (string, error) {
+	cv, e := GetIstioVersions(client.REST(), istioNamespace)
 	if e != nil {
 		return "", fmt.Errorf("failed to retrieve Istio control plane version, error: %v", e)
 	}
@@ -301,10 +303,10 @@ func retrieveControlPlaneVersion(kubeClient ExecClient, istioNamespace string, l
 
 // waitUpgradeComplete waits for the upgrade to complete by periodically comparing the current component version
 // to the target version.
-func waitUpgradeComplete(kubeClient ExecClient, istioNamespace string, targetVer string, l clog.Logger) error {
+func waitUpgradeComplete(client kube.Client, istioNamespace string, targetVer string, l clog.Logger) error {
 	for i := 1; i <= upgradeWaitCheckVerMaxAttempts; i++ {
 		sleepSeconds(upgradeWaitSecCheckVerPerLoop)
-		cv, e := kubeClient.GetIstioVersions(istioNamespace)
+		cv, e := GetIstioVersions(client.REST(), istioNamespace)
 		if e != nil {
 			l.LogAndPrintf("Failed to retrieve Istio control plane version, error: %v", e)
 			continue
@@ -359,12 +361,6 @@ func identicalVersions(cv []ComponentVersion) bool {
 	return true
 }
 
-// Client is a helper wrapper around the Kube RESTClient for istioctl -> Pilot/Envoy/Mesh related things
-type Client struct {
-	Config *rest.Config
-	*rest.RESTClient
-}
-
 // ComponentVersion is a pair of component name and version
 type ComponentVersion struct {
 	Component string
@@ -377,30 +373,9 @@ func (cv ComponentVersion) String() string {
 		cv.Component, cv.Pod.GetName(), cv.Version)
 }
 
-// ExecClient is an interface for remote execution
-type ExecClient interface {
-	GetIstioVersions(namespace string) ([]ComponentVersion, error)
-	GetPods(namespace string, params map[string]string) (*v1.PodList, error)
-	PodsForSelector(namespace, labelSelector string) (*v1.PodList, error)
-	ConfigMapForSelector(namespace, labelSelector string) (*v1.ConfigMapList, error)
-}
-
-// NewClient is the constructor for the client wrapper
-func NewClient(kubeconfig, configContext string) (*Client, error) {
-	config, err := defaultRestConfig(kubeconfig, configContext)
-	if err != nil {
-		return nil, err
-	}
-	restClient, err := rest.RESTClientFor(config)
-	if err != nil {
-		return nil, err
-	}
-	return &Client{config, restClient}, nil
-}
-
 // GetIstioVersions gets the version for each Istio component
-func (client *Client) GetIstioVersions(namespace string) ([]ComponentVersion, error) {
-	pods, err := client.GetPods(namespace, map[string]string{
+func GetIstioVersions(client rest.Interface, namespace string) ([]ComponentVersion, error) {
+	pods, err := GetPods(client, namespace, map[string]string{
 		"labelSelector": "istio",
 		"fieldSelector": "status.phase=Running",
 	})
@@ -466,18 +441,8 @@ func parseTag(image string) (string, error) {
 	}
 }
 
-func (client *Client) PodsForSelector(namespace, labelSelector string) (*v1.PodList, error) {
-	pods, err := client.GetPods(namespace, map[string]string{
-		"labelSelector": labelSelector,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("failed to retrieve pods, error: %v", err)
-	}
-	return pods, nil
-}
-
 // GetPods retrieves the pod objects for Istio deployments
-func (client *Client) GetPods(namespace string, params map[string]string) (*v1.PodList, error) {
+func GetPods(client rest.Interface, namespace string, params map[string]string) (*v1.PodList, error) {
 	req := client.Get().
 		Resource("pods").
 		Namespace(namespace)
@@ -496,17 +461,8 @@ func (client *Client) GetPods(namespace string, params map[string]string) (*v1.P
 	return list, nil
 }
 
-func (client *Client) ConfigMapForSelector(namespace, labelSelector string) (*v1.ConfigMapList, error) {
-	cmGet := client.Get().Resource("configmaps").Namespace(namespace).Param("labelSelector", labelSelector)
-	obj, err := cmGet.Do(context.TODO()).Get()
-	if err != nil {
-		return nil, fmt.Errorf("failed retrieving configmap: %v", err)
-	}
-	return obj.(*v1.ConfigMapList), nil
-}
-
-func (client *Client) CheckUnsupportedAlphaSecurityCRD() error {
-	c, err := client_v1beta1.NewForConfig(client.Config)
+func CheckUnsupportedAlphaSecurityCRD(client kube.Client) error {
+	c, err := client_v1beta1.NewForConfig(client.RESTConfig())
 	if err != nil {
 		return err
 	}
@@ -540,7 +496,7 @@ func (client *Client) CheckUnsupportedAlphaSecurityCRD() error {
 		}
 
 		parts := strings.Split(crd, ".")
-		cmd := client.Get().AbsPath("apis", strings.Join(parts[1:], "."), "v1alpha1", parts[0])
+		cmd := client.REST().Get().AbsPath("apis", strings.Join(parts[1:], "."), "v1alpha1", parts[0])
 		obj, err := cmd.DoRaw(context.TODO())
 		if err != nil {
 			log.Errorf("failed to get resources for crd %s: %v", crd, err)

commit 714ee5a9867974cb4b99b9f95107adaaa6363c66

Fix breakage in istioctl manifest apply
This was introduced by #25384. The issue was that the parameter order was swapped in order to be consistent with GenerateConfig (and a few other places). There was apparently no test coverage for this beyond the integration test framework's usage when installing istio. Unfortunately, we don't want to use the commands directly there because the use of flags interferes with the ability to install multiple control planes in parallel.

Fixing the issue by swapping to a structure for the parameter to avoid similar breakage in the future.

Fixes #25447

operator/cmd/mesh/manifest-apply.go
@@ -64,6 +64,21 @@ type manifestApplyArgs struct {
 	revision string
 }
 
+func (args manifestApplyArgs) toOptions(rootArgs *rootArgs) (ApplyManifestsOptions, error) {
+	client, err := k8sClient(args.kubeConfigPath, args.context)
+	if err != nil {
+		return ApplyManifestsOptions{}, err
+	}
+	return ApplyManifestsOptions{
+		Client:      client,
+		InFilenames: args.inFilenames,
+		SetOverlay:  applyFlagAliases(args.set, args.manifestsPath, args.revision),
+		Force:       args.force,
+		DryRun:      rootArgs.dryRun,
+		WaitTimeout: args.readinessTimeout,
+	}, nil
+}
+
 func addManifestApplyFlags(cmd *cobra.Command, args *manifestApplyArgs) {
 	cmd.PersistentFlags().StringSliceVarP(&args.inFilenames, "filename", "f", nil, filenameFlagHelpStr)
 	cmd.PersistentFlags().StringVarP(&args.kubeConfigPath, "kubeconfig", "c", "", "Path to kube config.")
@@ -146,26 +161,40 @@ func runApplyCmd(cmd *cobra.Command, rootArgs *rootArgs, maArgs *manifestApplyAr
 	if err := configLogs(logOpts); err != nil {
 		return fmt.Errorf("could not configure logs: %s", err)
 	}
-	client, err := k8sClient(maArgs.kubeConfigPath, maArgs.context)
+
+	opts, err := maArgs.toOptions(rootArgs)
 	if err != nil {
 		return err
 	}
-	if err := ApplyManifests(applyFlagAliases(maArgs.set, maArgs.manifestsPath, maArgs.revision), maArgs.inFilenames,
-		maArgs.force, rootArgs.dryRun, client, maArgs.readinessTimeout, l); err != nil {
+
+	if err := ApplyManifests(opts, l); err != nil {
 		return fmt.Errorf("failed to apply manifests: %v", err)
 	}
 
 	return nil
 }
 
+// ApplyManifestOptions options for the apply manifests command.
+type ApplyManifestsOptions struct {
+	Client      kube.Client
+	InFilenames []string
+	SetOverlay  []string
+	Force       bool
+	DryRun      bool
+	WaitTimeout time.Duration
+}
+
 // ApplyManifests generates manifests from the given input files and --set flag overlays and applies them to the
 // cluster. See GenManifests for more description of the manifest generation process.
 //  force   validation warnings are written to logger but command is not aborted
 //  dryRun  all operations are done but nothing is written
-func ApplyManifests(inFilenames, setOverlay []string, force bool, dryRun bool,
-	client kube.Client, waitTimeout time.Duration, l clog.Logger) error {
-
-	_, iops, err := GenerateConfig(inFilenames, setOverlay, force, client.RESTConfig(), l)
+func ApplyManifests(opts ApplyManifestsOptions, l clog.Logger) error {
+	_, iops, err := GenerateConfig(GenerateConfigOptions{
+		InFilenames: opts.InFilenames,
+		SetOverlay:  opts.SetOverlay,
+		Force:       opts.Force,
+		KubeConfig:  opts.Client.RESTConfig(),
+	}, l)
 	if err != nil {
 		return err
 	}
@@ -179,15 +208,20 @@ func ApplyManifests(inFilenames, setOverlay []string, force bool, dryRun bool,
 		return err
 	}
 
-	if err := createNamespace(client.Kube(), iop.Namespace); err != nil {
+	if err := createNamespace(opts.Client.Kube(), iop.Namespace); err != nil {
 		return err
 	}
 
 	// Needed in case we are running a test through this path that doesn't start a new process.
 	cache.FlushObjectCaches()
-	opts := &helmreconciler.Options{DryRun: dryRun, Log: l, WaitTimeout: waitTimeout, ProgressLog: progress.NewLog(),
-		Force: force}
-	reconciler, err := helmreconciler.NewHelmReconciler(client.Controller(), client.RESTConfig(), iop, opts)
+	helmOpts := &helmreconciler.Options{
+		DryRun:      opts.DryRun,
+		Log:         l,
+		WaitTimeout: opts.WaitTimeout,
+		ProgressLog: progress.NewLog(),
+		Force:       opts.Force,
+	}
+	reconciler, err := helmreconciler.NewHelmReconciler(opts.Client.Controller(), opts.Client.RESTConfig(), iop, helmOpts)
 	if err != nil {
 		return err
 	}
@@ -199,7 +233,7 @@ func ApplyManifests(inFilenames, setOverlay []string, force bool, dryRun bool,
 		return fmt.Errorf("errors occurred during operation")
 	}
 
-	opts.ProgressLog.SetState(progress.StateComplete)
+	helmOpts.ProgressLog.SetState(progress.StateComplete)
 
 	// Save state to cluster in IstioOperator CR.
 	iopStr, err := translate.IOPStoIOPstr(iops, crName, iopv1alpha1.Namespace(iops))

operator/cmd/mesh/manifest-generate.go
@@ -129,7 +129,12 @@ func manifestGenerate(args *rootArgs, mgArgs *manifestGenerateArgs, logopts *log
 // supplied logger.
 func GenManifests(inFilename []string, setOverlay []string, force bool,
 	kubeConfig *rest.Config, l clog.Logger) (name.ManifestMap, *v1alpha1.IstioOperatorSpec, error) {
-	mergedYAML, _, err := GenerateConfig(inFilename, setOverlay, force, kubeConfig, l)
+	mergedYAML, _, err := GenerateConfig(GenerateConfigOptions{
+		InFilenames: inFilename,
+		SetOverlay:  setOverlay,
+		Force:       force,
+		KubeConfig:  kubeConfig,
+	}, l)
 	if err != nil {
 		return nil, nil, err
 	}

operator/cmd/mesh/profile-common.go
@@ -36,6 +36,14 @@ import (
 	pkgversion "istio.io/pkg/version"
 )
 
+// GenerateConfigOptions provides options for the generate command.
+type GenerateConfigOptions struct {
+	InFilenames []string
+	SetOverlay  []string
+	Force       bool
+	KubeConfig  *rest.Config
+}
+
 // GenerateConfig creates an IstioOperatorSpec from the following sources, overlaid sequentially:
 // 1. Compiled in base, or optionally base from paths pointing to one or multiple ICP/IOP files at inFilenames.
 // 2. Profile overlay, if non-default overlay is selected. This also comes either from compiled in or path specified in IOP contained in inFilenames.
@@ -47,18 +55,17 @@ import (
 // Otherwise it will be the compiled in profile YAMLs.
 // In step 3, the remaining fields in the same user overlay are applied on the resulting profile base.
 // The force flag causes validation errors not to abort but only emit log/console warnings.
-func GenerateConfig(inFilenames, setOverlay []string, force bool, kubeConfig *rest.Config,
-	l clog.Logger) (string, *v1alpha1.IstioOperatorSpec, error) {
-	if err := validateSetFlags(setOverlay); err != nil {
+func GenerateConfig(opts GenerateConfigOptions, l clog.Logger) (string, *v1alpha1.IstioOperatorSpec, error) {
+	if err := validateSetFlags(opts.SetOverlay); err != nil {
 		return "", nil, err
 	}
 
-	fy, profile, err := readYamlProfile(inFilenames, setOverlay, force, l)
+	fy, profile, err := readYamlProfile(opts.InFilenames, opts.SetOverlay, opts.Force, l)
 	if err != nil {
 		return "", nil, err
 	}
 
-	iopsString, iops, err := genIOPSFromProfile(profile, fy, setOverlay, force, kubeConfig, l)
+	iopsString, iops, err := genIOPSFromProfile(profile, fy, opts.SetOverlay, opts.Force, opts.KubeConfig, l)
 	if err != nil {
 		return "", nil, err
 	}

operator/cmd/mesh/profile-dump.go
@@ -129,8 +129,11 @@ func profileDump(args []string, rootArgs *rootArgs, pdArgs *profileDumpArgs, l c
 	if len(args) == 1 {
 		setFlags = append(setFlags, "profile="+args[0])
 	}
-
-	y, _, err := GenerateConfig(pdArgs.inFilenames, setFlags, true, nil, l)
+	y, _, err := GenerateConfig(GenerateConfigOptions{
+		InFilenames: pdArgs.inFilenames,
+		SetOverlay:  setFlags,
+		Force:       true,
+	}, l)
 	if err != nil {
 		return err
 	}

operator/cmd/mesh/shared.go
@@ -161,7 +161,9 @@ func getCRAndNamespaceFromFile(filePath string, l clog.Logger) (customResource s
 		return "", "", nil
 	}
 
-	_, mergedIOPS, err := GenerateConfig([]string{filePath}, nil, false, nil, l)
+	_, mergedIOPS, err := GenerateConfig(GenerateConfigOptions{
+		InFilenames: []string{filePath},
+	}, l)
 	if err != nil {
 		return "", "", err
 	}

operator/cmd/mesh/upgrade.go
@@ -136,7 +136,11 @@ func upgrade(rootArgs *rootArgs, args *upgradeArgs, l clog.Logger) (err error) {
 	}
 	setFlags := applyFlagAliases(args.set, args.manifestsPath, "")
 	// Generate IOPS parseObjectSetFromManifest
-	targetIOPSYaml, targetIOPS, err := GenerateConfig(args.inFilenames, setFlags, args.force, nil, l)
+	targetIOPSYaml, targetIOPS, err := GenerateConfig(GenerateConfigOptions{
+		InFilenames: args.inFilenames,
+		SetOverlay:  setFlags,
+		Force:       args.force,
+	}, l)
 	if err != nil {
 		return fmt.Errorf("failed to generate Istio configs from file %s, error: %s", args.inFilenames, err)
 	}
@@ -206,8 +210,14 @@ func upgrade(rootArgs *rootArgs, args *upgradeArgs, l clog.Logger) (err error) {
 	waitForConfirmation(args.skipConfirmation, l)
 
 	// Apply the Istio Control Plane specs reading from inFilenames to the cluster
-	err = ApplyManifests(applyFlagAliases(args.set, args.manifestsPath, ""), args.inFilenames, args.force, rootArgs.dryRun,
-		client, args.readinessTimeout, l)
+	err = ApplyManifests(ApplyManifestsOptions{
+		Client:      nil,
+		InFilenames: args.inFilenames,
+		SetOverlay:  applyFlagAliases(args.set, args.manifestsPath, ""),
+		Force:       args.force,
+		DryRun:      rootArgs.dryRun,
+		WaitTimeout: args.readinessTimeout,
+	}, l)
 	if err != nil {
 		return fmt.Errorf("failed to apply the Istio Control Plane specs. Error: %v", err)
 	}

operator/cmd/mesh/manifest-generate_test.go
@@ -650,7 +650,10 @@ func TestLDFlags(t *testing.T) {
 	version.DockerInfo.Hub = "testHub"
 	version.DockerInfo.Tag = "testTag"
 	l := clog.NewConsoleLogger(os.Stdout, os.Stderr, installerScope)
-	_, iops, err := GenerateConfig(nil, []string{"installPackagePath=" + string(liveCharts)}, true, nil, l)
+	_, iops, err := GenerateConfig(GenerateConfigOptions{
+		SetOverlay: []string{"installPackagePath=" + string(liveCharts)},
+		Force:      true,
+	}, l)
 	if err != nil {
 		t.Fatal(err)
 	}

pkg/test/framework/components/istio/operator.go
@@ -498,7 +498,14 @@ func applyManifest(ctx resource.Context, inFiles, setOverlay []string, cluster r
 	}
 
 	scopes.Framework.Infof("Running istio control plane on cluster %s", cluster.Name())
-	err = mesh.ApplyManifests(inFiles, setOverlay, false, false, cluster, 300*time.Second, l)
+	err = mesh.ApplyManifests(mesh.ApplyManifestsOptions{
+		Client:      cluster,
+		InFilenames: inFiles,
+		SetOverlay:  setOverlay,
+		Force:       false,
+		DryRun:      false,
+		WaitTimeout: 300 * time.Second,
+	}, l)
 	if err != nil {
 		return fmt.Errorf("manifest apply failed: %v", err)
 	}