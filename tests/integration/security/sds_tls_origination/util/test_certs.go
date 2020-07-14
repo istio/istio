4c02f620b4b281347775d0bbdf37b5bd9306b831
nschhina <navuchhina@live.com>
2020-07-14 19:03:20 -0400

    add copyrights

--- tests/integration/security/sds_tls_origination/util/test_certs.go
+++ tests/integration/security/sds_tls_origination/util/test_certs.go
@@ -1,3 +1,18 @@
+// Copyright Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
 package sdstlsutil
 
 const (
@@ -106,5 +121,4 @@ gAuXCFMGogCroMnB4mL9y6UBU9jw1fuC63NHv5uNEdP6FEisV240thQ9RBmI8HSI
 MEhMqHAGwqTSFzSeMuHI1fhlEJ/TPhACjhYm8ag87zMK7dOKJgyNPmjJLpwbo3y8
 sH2Vo/2b
 -----END CERTIFICATE-----`
-
 )
--- tests/integration/security/sds_tls_origination/util/util.go
+++ tests/integration/security/sds_tls_origination/util/util.go
@@ -1,3 +1,18 @@
+// Copyright Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
 package sdstlsutil
 
 import (
@@ -221,7 +236,7 @@ func SetupEcho(t *testing.T, ctx resource.Context, p *pilot.Instance) (echo.Inst
 				Hostname: "server.default.svc",
 			},
 			Subsets: []echo.SubsetConfig{{
-				Version: "v1",
+				Version:     "v1",
 				Annotations: echo.NewAnnotations().SetBool(echo.SidecarInject, false),
 			}},
 		}).
