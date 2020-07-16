06a3b6ac299824d37798d0f809f9aa137c935661
Yuchen Dai <silentdai@gmail.com>
2020-07-16 23:42:07 +0000

    fix dns rules on v4 only system
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>

--- tools/istio-iptables/pkg/cmd/run.go
+++ tools/istio-iptables/pkg/cmd/run.go
@@ -382,8 +382,10 @@ func (iptConfigurator *IptablesConfigurator) run() {
 	if redirectDNS {
 		iptConfigurator.iptables.AppendRuleV4(
 			constants.ISTIOREDIRECT, constants.NAT, "-p", constants.UDP, "-j", constants.REDIRECT, "--to-ports", dnsTargetPort)
-		iptConfigurator.iptables.AppendRuleV6(
-			constants.ISTIOREDIRECT, constants.NAT, "-p", constants.UDP, "-j", constants.REDIRECT, "--to-ports", dnsTargetPort)
+		if iptConfigurator.cfg.EnableInboundIPv6 {
+			iptConfigurator.iptables.AppendRuleV6(
+				constants.ISTIOREDIRECT, constants.NAT, "-p", constants.UDP, "-j", constants.REDIRECT, "--to-ports", dnsTargetPort)
+		}
 	}
 
 	// Use this chain also for redirecting inbound traffic to the common Envoy port
