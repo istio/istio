14e4479cceee822c5867b5e25c2142b9153cbf00
Aditya Prerepa <adiprerepa@gmail.com>
2020-07-17 13:54:12 -0700

    init

--- pilot/pkg/security/model/authentication.go
+++ pilot/pkg/security/model/authentication.go
@@ -16,6 +16,7 @@ package model
 
 import (
 	"sync"
+	"time"
 
 	core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	envoy_config_grpc_credential "github.com/envoyproxy/go-control-plane/envoy/config/grpc_credential/v3"
@@ -100,7 +101,10 @@ func ConstructSdsSecretConfigWithCustomUds(name, sdsUdsPath, requestedType strin
 					},
 				},
 			},
-			InitialFetchTimeout: features.InitialFetchTimeout,
+			// set the fetch timeout to 0 here because workload certs are
+			// guaranteed to exist. while others like gateway certs may not
+			// exist.
+			InitialFetchTimeout: ptypes.DurationProto(time.Second * 0),
 		},
 	}
 
