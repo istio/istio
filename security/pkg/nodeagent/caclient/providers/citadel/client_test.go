cf1852e2c272933f2ce85d804fd7c18e92349cb8
williamaronli <64571891+williamaronli@users.noreply.github.com>
2020-07-18 15:59:21 -0700

    Enable the workload cert rotate automatically  (#25526)
    * add rorate feature
    
    * add certificate for response
    
    * add add reconnect features
    
    * refactor to use interface
    
    (cherry picked from commit 3055ad8cfd79d8ba1ba1da76746f0e39e5b8115e)
    
    * reset GetClientCertificate logic
    
    * ret the public type code
    
    * fix lint
    
    * address comment
    
    * fix when key,cert not loaded successfully
    
    * refactor and remove interface logic to simplify code
    
    * remove useless function
    
    * fix err
    
    (cherry picked from commit ad1c4c8bf70378b6361bd11f0a5342873a9303c0)
    
    * refactor to use reconnect logic
    
    * remove useless releaseResourceCode
    
    * fix lint
    
    * add unit test
    
    * fix lint and address comments
    
    * add release note
    
    * address comments
    
    * address comments
    
    * add retry logic
    
    * address comments
    
    * address comment
    
    * address comment
    
    * fix lint

--- security/pkg/nodeagent/caclient/providers/citadel/client_test.go
+++ security/pkg/nodeagent/caclient/providers/citadel/client_test.go
@@ -19,19 +19,27 @@ import (
 	"fmt"
 	"net"
 	"reflect"
+	"strings"
 	"testing"
 	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+
+	"istio.io/istio/pkg/test/util/retry"
 
 	pb "istio.io/istio/security/proto"
 )
 
-const mockServerAddress = "localhost:0"
+const (
+	mockServerAddress = "localhost:0"
+)
 
 var (
-	fakeCert  = []string{"foo", "bar"}
-	fakeToken = "Bearer fakeToken"
+	fakeCert          = []string{"foo", "bar"}
+	fakeToken         = "Bearer fakeToken"
+	validToken        = "Bearer validToken"
+	authorizationMeta = "authorization"
 )
 
 type mockCAServer struct {
@@ -105,5 +113,110 @@ func TestCitadelClient(t *testing.T) {
 				t.Errorf("Test case [%s]: resp: got %+v, expected %v", id, resp, tc.expectedCert)
 			}
 		}
+	}
+}
+
+type mockTokenCAServer struct {
+	Certs []string
+}
+
+func (ca *mockTokenCAServer) CreateCertificate(ctx context.Context, in *pb.IstioCertificateRequest) (*pb.IstioCertificateResponse, error) {
+	targetJWT, err := extractBearerToken(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if targetJWT != validToken {
+		return nil, fmt.Errorf("token is not valid")
+	}
+	return &pb.IstioCertificateResponse{CertChain: ca.Certs}, nil
+}
+
+func extractBearerToken(ctx context.Context) (string, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return "", fmt.Errorf("no metadata is attached")
+	}
+
+	authHeader, exists := md[authorizationMeta]
+	if !exists {
+		return "", fmt.Errorf("no HTTP authorization header exists")
+	}
+
+	for _, value := range authHeader {
+		if strings.HasPrefix(value, bearerTokenPrefix) {
+			return strings.TrimPrefix(value, bearerTokenPrefix), nil
+		}
+	}
+
+	return "", fmt.Errorf("no bearer token exists in HTTP authorization header")
+}
+
+// this test is to test whether the server side receive the correct token when
+// we build the CSR sign request
+func TestCitadelClientWithDifferentTypeToken(t *testing.T) {
+	testCases := map[string]struct {
+		server       mockTokenCAServer
+		expectedCert []string
+		expectedErr  string
+		token        string
+	}{
+		"Valid Token": {
+			server:       mockTokenCAServer{Certs: fakeCert},
+			expectedCert: fakeCert,
+			expectedErr:  "",
+			token:        validToken,
+		},
+		"Empty Token": {
+			server:       mockTokenCAServer{Certs: nil},
+			expectedCert: nil,
+			expectedErr:  "rpc error: code = Unknown desc = no HTTP authorization header exists",
+			token:        "",
+		},
+		"InValid Token": {
+			server:       mockTokenCAServer{Certs: []string{}},
+			expectedCert: nil,
+			expectedErr:  "rpc error: code = Unknown desc = token is not valid",
+			token:        fakeToken,
+		},
+	}
+
+	for id, tc := range testCases {
+		t.Run(id, func(t *testing.T) {
+			s := grpc.NewServer()
+			defer s.Stop()
+			lis, err := net.Listen("tcp", mockServerAddress)
+			if err != nil {
+				t.Fatalf("test case [%s]: failed to listen: %v", id, err)
+			}
+			go func() {
+				pb.RegisterIstioCertificateServiceServer(s, &tc.server)
+				if err := s.Serve(lis); err != nil {
+					t.Logf("Test case [%s]: failed to serve: %v", id, err)
+				}
+			}()
+
+			err = retry.UntilSuccess(func() error {
+				cli, err := NewCitadelClient(lis.Addr().String(), false, nil, "Kubernetes")
+				if err != nil {
+					return fmt.Errorf("test case [%s]: failed to create ca client: %v", id, err)
+				}
+				resp, err := cli.CSRSign(context.Background(), "12345678-1234-1234-1234-123456789012", []byte{01}, tc.token, 1)
+				if err != nil {
+					if err.Error() != tc.expectedErr {
+						return fmt.Errorf("test case [%s]: error (%s) does not match expected error (%s)", id, err.Error(), tc.expectedErr)
+					}
+				} else {
+					if tc.expectedErr != "" {
+						return fmt.Errorf("test case [%s]: expect error: %s but got no error", id, tc.expectedErr)
+					} else if !reflect.DeepEqual(resp, tc.expectedCert) {
+						return fmt.Errorf("test case [%s]: resp: got %+v, expected %v", id, resp, tc.expectedCert)
+					}
+				}
+				return nil
+			}, retry.Timeout(20*time.Second), retry.Delay(2*time.Second))
+			if err != nil {
+				t.Fatalf("test failed error is： %+v", err)
+			}
+		})
 	}
 }
