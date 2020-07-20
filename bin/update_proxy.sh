49d25f10fe2fd24b75388c342d91e640e81ab24b
Eric Van Norman <ericvn@us.ibm.com>
2020-07-20 10:35:50 -0500

    Add wait for proxy to be built to upadte_proxy.sh (#25651)

--- bin/update_proxy.sh
+++ bin/update_proxy.sh
@@ -23,4 +23,16 @@ SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOTDIR=$(dirname "${SCRIPTPATH}")
 cd "${ROOTDIR}"
 
+# Wait for the proxy to become available
+ISTIO_ENVOY_VERSION=${ISTIO_ENVOY_VERSION:-$1}
+ISTIO_ENVOY_LINUX_VERSION=${ISTIO_ENVOY_LINUX_VERSION:-${ISTIO_ENVOY_VERSION}}
+ISTIO_ENVOY_BASE_URL=${ISTIO_ENVOY_BASE_URL:-https://storage.googleapis.com/istio-build/proxy}
+ISTIO_ENVOY_RELEASE_URL=${ISTIO_ENVOY_RELEASE_URL:-${ISTIO_ENVOY_BASE_URL}/envoy-alpha-${ISTIO_ENVOY_LINUX_VERSION}.tar.gz}
+
+until curl --output /dev/null --silent --head --fail "$ISTIO_ENVOY_RELEASE_URL"; do
+    printf '.'
+    sleep 3
+done
+
+# Update the dependency in istio.deps
 sed -i '/PROXY_REPO_SHA/,/lastStableSHA/ { s/"lastStableSHA":.*/"lastStableSHA": "'"$1"'"/  }' istio.deps
