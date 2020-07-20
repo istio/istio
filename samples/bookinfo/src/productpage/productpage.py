eb2736e68cc73fcbd551fb3b74c5682f28499f42
Shamsher Ansari <shaansar@redhat.com>
2020-07-21 01:16:43 +0530

    Update the base image versions to latest for bookinfo sample (#24967) (#25645)
    * Update the base image versions to latest for bookinfo sample
    
    * use IPv4 address instead of IPv6 address

--- samples/bookinfo/src/productpage/productpage.py
+++ samples/bookinfo/src/productpage/productpage.py
@@ -382,4 +382,6 @@ if __name__ == '__main__':
 
     p = int(sys.argv[1])
     logging.info("start at port %s" % (p))
-    app.run(host='::', port=p, debug=True, threaded=True)
+    # Python does not work on an IPv6 only host
+    # https://bugs.python.org/issue10414
+    app.run(host='0.0.0.0', port=p, debug=True, threaded=True)
