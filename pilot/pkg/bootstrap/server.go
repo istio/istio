8af01c71d3c7243c458f23e8be25b3e5ded52bbb
nirvanagit <anubhavaeron@gmail.com>
2020-07-20 19:36:32 -0700

    fix options

--- pilot/pkg/bootstrap/server.go
+++ pilot/pkg/bootstrap/server.go
@@ -410,8 +410,8 @@ func (s *Server) initKubeClient(args *PilotArgs) error {
 		var err error
 		// Used by validation
 		s.kubeRestConfig, err = kubelib.DefaultRestConfig(args.RegistryOptions.KubeConfig, "", func(config *rest.Config) {
-			config.QPS = args.Config.ControllerOptions.KubernetesAPIQPS
-			config.Burst = args.Config.ControllerOptions.KubernetesAPIBurst
+			config.QPS = args.RegistryOptions.KubeOptions.KubernetesAPIQPS
+			config.Burst = args.RegistryOptions.KubeOptions.KubernetesAPIBurst
 		})
 		if err != nil {
 			return fmt.Errorf("failed creating kube config: %v", err)
