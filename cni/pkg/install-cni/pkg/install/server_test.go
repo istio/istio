17a4e4c223c7dca0339287bf81d86d304d512a05
Brian Cheung <bscheung@google.com>
2020-07-22 12:04:57 -0500

    Add liveness and readiness endpoints for the install CNI process (#25670)
    * Add liveness and readiness endpoints and probes
    
    * Remove unused constant

---
+++ cni/pkg/install-cni/pkg/install/server_test.go
@@ -0,0 +1,61 @@
+// Copyright Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package install
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"istio.io/istio/cni/pkg/install-cni/pkg/constants"
+)
+
+func TestServer(t *testing.T) {
+	router := http.NewServeMux()
+	isReady := initRouter(router)
+
+	assert.Falsef(t, isReady.Load().(bool), "isReady should be initialized to false")
+
+	server := httptest.NewServer(router)
+	defer server.Close()
+
+	makeReq(t, server.URL, constants.LivenessEndpoint, http.StatusOK)
+	makeReq(t, server.URL, constants.ReadinessEndpoint, http.StatusServiceUnavailable)
+
+	SetReady(isReady)
+	assert.Truef(t, isReady.Load().(bool), "isReady should be set to true")
+
+	makeReq(t, server.URL, constants.LivenessEndpoint, http.StatusOK)
+	makeReq(t, server.URL, constants.ReadinessEndpoint, http.StatusOK)
+
+	SetNotReady(isReady)
+	assert.Falsef(t, isReady.Load().(bool), "isReady should be set to false")
+
+	makeReq(t, server.URL, constants.LivenessEndpoint, http.StatusOK)
+	makeReq(t, server.URL, constants.ReadinessEndpoint, http.StatusServiceUnavailable)
+}
+
+func makeReq(t *testing.T, url, endpoint string, expectedStatusCode int) {
+	t.Helper()
+	res, err := http.Get(url + endpoint)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.StatusCode != expectedStatusCode {
+		t.Fatalf("expected status code from %s: %d, got: %d", endpoint, expectedStatusCode, res.StatusCode)
+	}
+}
