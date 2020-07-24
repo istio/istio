1bedf8907541b066938553e6119c37f5c5025258
John Howard <howardjohn@google.com>
2020-07-23 17:57:09 -0700

    Merge vm/ test suite with main test suite (#25754)
    * Merge vm/ test suite with main test suite
    
    As part of ongoing efforts to minimize test time, we are moving pilot
    tests (and soon others) to share a common set of deployments. This
    merges the VM tests into this suite. The same tests are ran (traffic
    shifting, DNS tests, pod <-> vm, and extra OSes), just in a more
    performant manner
    
    * some improvements
    
    * Fix build
    
    * fix tests
    
    * move readme
    
    * Add requires single cluster

---
+++ tests/integration/pilot/vm_test.go
@@ -0,0 +1,62 @@
+// Copyright Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package pilot
+
+import (
+	"strings"
+	"testing"
+
+	"istio.io/istio/pkg/test/framework"
+	"istio.io/istio/pkg/test/framework/components/echo"
+	"istio.io/istio/pkg/test/framework/components/echo/echoboot"
+	"istio.io/istio/pkg/test/framework/label"
+)
+
+func GetAdditionVMImages() []string {
+	// Note - bionic is not here as its the default
+	return []string{"app_sidecar_ubuntu_xenial", "app_sidecar_ubuntu_focal",
+		"app_sidecar_debian_9", "app_sidecar_debian_10", "app_sidecar_centos_8"}
+}
+
+func TestVmOSPost(t *testing.T) {
+	framework.
+		NewTest(t).
+		RequiresSingleCluster().
+		Features("traffic.reachability").
+		Label(label.Postsubmit).
+		Run(func(ctx framework.TestContext) {
+			b := echoboot.NewBuilder(ctx)
+			images := GetAdditionVMImages()
+			instances := make([]echo.Instance, len(images))
+			for i, image := range images {
+				b = b.With(&instances[i], echo.Config{
+					Service:    "vm-" + strings.ReplaceAll(image, "_", "-"),
+					Namespace:  apps.namespace,
+					Ports:      echoPorts,
+					DeployAsVM: true,
+					Subsets:    []echo.SubsetConfig{{}},
+				})
+			}
+			b.BuildOrFail(ctx)
+
+			for i, image := range images {
+				ctx.NewSubTest(image).Run(func(ctx framework.TestContext) {
+					for _, tt := range vmTestCases(instances[i]) {
+						ExecuteTrafficTest(ctx, tt)
+					}
+				})
+			}
+		})
+}
