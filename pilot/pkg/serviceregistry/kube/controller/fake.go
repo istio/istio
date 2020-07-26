12c4e51d5827cd10224965f3c1ad1771be3383ea
costin <costin@gmail.com>
2020-07-26 08:25:38 -0700

    merged

---
+++ pilot/pkg/serviceregistry/kube/controller/fake.go
@@ -0,0 +1,200 @@
+// Copyright Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package controller
+
+import (
+	"errors"
+	"time"
+
+	klabels "k8s.io/apimachinery/pkg/labels"
+	listerv1 "k8s.io/client-go/listers/core/v1"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/cache"
+
+	kubelib "istio.io/istio/pkg/kube"
+
+	"istio.io/istio/pilot/pkg/model"
+	"istio.io/istio/pkg/config/mesh"
+)
+
+const (
+	defaultFakeDomainSuffix = "company.com"
+)
+
+// FakeXdsUpdater is used to test the registry.
+type FakeXdsUpdater struct {
+	// Events tracks notifications received by the updater
+	Events chan FakeXdsEvent
+}
+
+func (fx *FakeXdsUpdater) ConfigUpdate(*model.PushRequest) {
+	select {
+	case fx.Events <- FakeXdsEvent{Type: "xds"}:
+	default:
+	}
+}
+
+func (fx *FakeXdsUpdater) ProxyUpdate(_, _ string) {
+	select {
+	case fx.Events <- FakeXdsEvent{Type: "proxy"}:
+	default:
+	}
+}
+
+// FakeXdsEvent is used to watch XdsEvents
+type FakeXdsEvent struct {
+	// Type of the event
+	Type string
+
+	// The id of the event
+	ID string
+
+	// The endpoints associated with an EDS push if any
+	Endpoints []*model.IstioEndpoint
+}
+
+// NewFakeXDS creates a XdsUpdater reporting events via a channel.
+func NewFakeXDS() *FakeXdsUpdater {
+	return &FakeXdsUpdater{
+		Events: make(chan FakeXdsEvent, 100),
+	}
+}
+
+func (fx *FakeXdsUpdater) EDSUpdate(_, hostname string, _ string, entry []*model.IstioEndpoint) error {
+	if len(entry) > 0 {
+		select {
+		case fx.Events <- FakeXdsEvent{Type: "eds", ID: hostname, Endpoints: entry}:
+		default:
+		}
+
+	}
+	return nil
+}
+
+// SvcUpdate is called when a service port mapping definition is updated.
+// This interface is WIP - labels, annotations and other changes to service may be
+// updated to force a EDS and CDS recomputation and incremental push, as it doesn't affect
+// LDS/RDS.
+func (fx *FakeXdsUpdater) SvcUpdate(_, hostname string, _ string, _ model.Event) {
+	select {
+	case fx.Events <- FakeXdsEvent{Type: "service", ID: hostname}:
+	default:
+	}
+}
+
+func (fx *FakeXdsUpdater) Wait(et string) *FakeXdsEvent {
+	for {
+		select {
+		case e := <-fx.Events:
+			if e.Type == et {
+				return &e
+			}
+			continue
+		case <-time.After(5 * time.Second):
+			return nil
+		}
+	}
+}
+
+// Clear any pending event
+func (fx *FakeXdsUpdater) Clear() {
+	wait := true
+	for wait {
+		select {
+		case <-fx.Events:
+		default:
+			wait = false
+		}
+	}
+}
+
+type FakeControllerOptions struct {
+	Objects           []runtime.Object
+	NetworksWatcher   mesh.NetworksWatcher
+	ServiceHandler    func(service *model.Service, event model.Event)
+	InstanceHandler   func(instance *model.ServiceInstance, event model.Event)
+	Mode              EndpointMode
+	ClusterID         string
+	WatchedNamespaces string
+	DomainSuffix      string
+	XDSUpdater        model.XDSUpdater
+}
+
+type FakeController struct {
+	*Controller
+}
+
+func (f *FakeController) ResyncEndpoints() error {
+	e, ok := f.endpoints.(*endpointsController)
+	if !ok {
+		return errors.New("cannot run ResyncEndpoints; EndpointsMode must be EndpointsOnly")
+	}
+	eps, err := listerv1.NewEndpointsLister(e.informer.GetIndexer()).List(klabels.Everything())
+	if err != nil {
+		return err
+	}
+	// endpoint processing may beat services
+	for _, ep := range eps {
+		err = f.endpoints.onEvent(ep, model.EventAdd)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func NewFakeControllerWithOptions(opts FakeControllerOptions) (*FakeController, *FakeXdsUpdater) {
+	xdsUpdater := opts.XDSUpdater
+	if xdsUpdater == nil {
+		xdsUpdater = NewFakeXDS()
+	}
+
+	domainSuffix := defaultFakeDomainSuffix
+	if opts.DomainSuffix != "" {
+		domainSuffix = opts.DomainSuffix
+	}
+	clients := kubelib.NewFakeClient(opts.Objects...)
+	options := Options{
+		WatchedNamespaces: opts.WatchedNamespaces, // default is all namespaces
+		ResyncPeriod:      1 * time.Second,
+		DomainSuffix:      domainSuffix,
+		XDSUpdater:        xdsUpdater,
+		Metrics:           &model.Environment{},
+		NetworksWatcher:   opts.NetworksWatcher,
+		EndpointMode:      opts.Mode,
+		ClusterID:         opts.ClusterID,
+	}
+	c := NewController(clients, options)
+	if opts.InstanceHandler != nil {
+		_ = c.AppendInstanceHandler(opts.InstanceHandler)
+	}
+	if opts.ServiceHandler != nil {
+		_ = c.AppendServiceHandler(opts.ServiceHandler)
+	}
+	c.stop = make(chan struct{})
+	// Run in initiation to prevent calling each test
+	// TODO: fix it, so we can remove `stop` channel
+	go c.Run(c.stop)
+	clients.RunAndWait(c.stop)
+	// Wait for the caches to sync, otherwise we may hit race conditions where events are dropped
+	cache.WaitForCacheSync(c.stop, c.pods.informer.HasSynced, c.serviceInformer.HasSynced, c.endpoints.HasSynced)
+
+	var fx *FakeXdsUpdater
+	if x, ok := xdsUpdater.(*FakeXdsUpdater); ok {
+		fx = x
+	}
+	return &FakeController{c}, fx
+}
---
+++ tests/integration/pilot/ingress_test.go
@@ -0,0 +1,222 @@
+// Copyright Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package pilot
+
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+	"time"
+
+	ingressutil "istio.io/istio/tests/integration/security/sds_ingress/util"
+
+	"istio.io/istio/pkg/test/framework"
+	"istio.io/istio/pkg/test/framework/components/ingress"
+	"istio.io/istio/pkg/test/util/retry"
+)
+
+func TestGateway(t *testing.T) {
+	framework.
+		NewTest(t).
+		Run(func(ctx framework.TestContext) {
+			crd, err := ioutil.ReadFile("testdata/service-apis-crd.yaml")
+			if err != nil {
+				ctx.Fatal(err)
+			}
+			ctx.Config().ApplyYAMLOrFail(ctx, "", string(crd))
+
+			ctx.Config().ApplyYAMLOrFail(ctx, apps.namespace.Name(), `
+apiVersion: networking.x-k8s.io/v1alpha1
+kind: GatewayClass
+metadata:
+  name: istio
+spec:
+  controller: istio.io/gateway-controller
+---
+apiVersion: networking.x-k8s.io/v1alpha1
+kind: Gateway
+metadata:
+  name: gateway
+spec:
+  class: istio
+  listeners:
+  - hostname:
+      match: Domain
+      name: domain.example
+    port: 80
+    protocol: HTTP
+    routes: {}
+---
+apiVersion: networking.x-k8s.io/v1alpha1
+kind: HTTPRoute
+metadata:
+  name: http
+spec:
+  hosts:
+  - hostnames: ["my.domain.example"]
+    rules:
+    - match:
+        pathType: Prefix
+        path: /get
+      action:
+        forwardTo:
+          targetRef:
+            name: b
+            group: ""
+            resource: ""`)
+
+			if err := retry.UntilSuccess(func() error {
+				resp, err := ingr.Call(ingress.CallOptions{
+					Host:     "my.domain.example",
+					Path:     "/get",
+					CallType: ingress.PlainText,
+					Address:  ingr.HTTPAddress(),
+				})
+				if err != nil {
+					return err
+				}
+				if resp.Code != 200 {
+					return fmt.Errorf("got invalid response code %v: %v", resp.Code, resp.Body)
+				}
+				return nil
+			}); err != nil {
+				t.Fatal(err)
+			}
+		})
+}
+
+func skipIfIngressClassUnsupported(ctx framework.TestContext) {
+	ver, err := ctx.Clusters().Default().GetKubernetesVersion()
+	if err != nil {
+		ctx.Fatalf("failed to get Kubernetes version: %v", err)
+	}
+	serverVersion := fmt.Sprintf("%s.%s", ver.Major, ver.Minor)
+	if serverVersion < "1.18" {
+		ctx.Skip("IngressClass not supported")
+	}
+}
+
+// TestIngress tests that we can route using standard Kubernetes Ingress objects.
+func TestIngress(t *testing.T) {
+	framework.
+		NewTest(t).
+		Run(func(ctx framework.TestContext) {
+			skipIfIngressClassUnsupported(ctx)
+			// Set up secret contain some TLS certs for *.example.com
+			// we will define one for foo.example.com and one for bar.example.com, to ensure both can co-exist
+			credName := "k8s-ingress-secret-foo"
+			ingressutil.CreateIngressKubeSecret(t, ctx, []string{credName}, ingress.TLS, ingressutil.IngressCredentialA, false)
+			defer ingressutil.DeleteKubeSecret(t, ctx, []string{credName})
+			credName2 := "k8s-ingress-secret-bar"
+			ingressutil.CreateIngressKubeSecret(t, ctx, []string{credName2}, ingress.TLS, ingressutil.IngressCredentialB, false)
+			defer ingressutil.DeleteKubeSecret(t, ctx, []string{credName2})
+
+			if err := ctx.Config().ApplyYAML(apps.namespace.Name(), `
+apiVersion: networking.k8s.io/v1beta1
+kind: IngressClass
+metadata:
+  name: istio-test
+spec:
+  controller: istio.io/ingress-controller`, `
+apiVersion: networking.k8s.io/v1beta1
+kind: Ingress
+metadata:
+  name: ingress
+spec:
+  ingressClassName: istio-test
+  tls:
+  - hosts: ["foo.example.com"]
+    secretName: k8s-ingress-secret-foo
+  - hosts: ["bar.example.com"]
+    secretName: k8s-ingress-secret-bar
+  rules:
+    - http:
+        paths:
+          - path: /test/namedport
+            backend:
+              serviceName: b
+              servicePort: http
+          - path: /test
+            backend:
+              serviceName: b
+              servicePort: 80`,
+			); err != nil {
+				t.Fatal(err)
+			}
+
+			cases := []struct {
+				name string
+				call ingress.CallOptions
+			}{
+				{
+					// Basic HTTP call
+					name: "http",
+					call: ingress.CallOptions{
+						Host:     "server",
+						Path:     "/test",
+						CallType: ingress.PlainText,
+						Address:  ingr.HTTPAddress(),
+					},
+				},
+				{
+					// Basic HTTPS call for foo. CaCert matches the secret
+					name: "https-foo",
+					call: ingress.CallOptions{
+						Host:     "foo.example.com",
+						Path:     "/test",
+						CallType: ingress.TLS,
+						Address:  ingr.HTTPSAddress(),
+						CaCert:   ingressutil.IngressCredentialA.CaCert,
+					},
+				},
+				{
+					// Basic HTTPS call for bar. CaCert matches the secret
+					name: "https-bar",
+					call: ingress.CallOptions{
+						Host:     "bar.example.com",
+						Path:     "/test",
+						CallType: ingress.TLS,
+						Address:  ingr.HTTPSAddress(),
+						CaCert:   ingressutil.IngressCredentialB.CaCert,
+					},
+				},
+				{
+					// HTTPS call for bar with namedport route. CaCert matches the secret
+					name: "https-namedport",
+					call: ingress.CallOptions{
+						Host:     "bar.example.com",
+						Path:     "/test/namedport",
+						CallType: ingress.TLS,
+						Address:  ingr.HTTPSAddress(),
+						CaCert:   ingressutil.IngressCredentialB.CaCert,
+					},
+				},
+			}
+			for _, tt := range cases {
+				ctx.NewSubTest(tt.name).Run(func(t framework.TestContext) {
+					retry.UntilSuccessOrFail(t, func() error {
+						resp, err := ingr.Call(tt.call)
+						if err != nil {
+							return err
+						}
+						if resp.Code != 200 {
+							return fmt.Errorf("got invalid response code %v: %v", resp.Code, resp.Body)
+						}
+						return nil
+					}, retry.Delay(time.Millisecond*100), retry.Timeout(time.Minute*2))
+				})
+			}
+		})
+}
