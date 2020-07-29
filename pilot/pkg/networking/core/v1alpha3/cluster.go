c593f076b81e4375065e038e8be7946ecf1c7a8a
Rama Chavali <rama.rao@salesforce.com>
2020-07-29 09:14:34 +0530

    cluster: refactor auto mtls settings (#25910)
    * refactor conditionally convert
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * rename methods
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * change method
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * test change
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/networking/core/v1alpha3/cluster.go
+++ pilot/pkg/networking/core/v1alpha3/cluster.go
@@ -447,10 +447,10 @@ const (
 	autoDetected
 )
 
-// conditionallyConvertToIstioMtls fills key cert fields for all TLSSettings when the mode is `ISTIO_MUTUAL`.
+// buildAutoMtlsSettings fills key cert fields for all TLSSettings when the mode is `ISTIO_MUTUAL`.
 // If the (input) TLS setting is nil (i.e not set), *and* the service mTLS mode is STRICT, it also
 // creates and populates the config as if they are set as ISTIO_MUTUAL.
-func conditionallyConvertToIstioMtls(
+func buildAutoMtlsSettings(
 	tls *networking.ClientTLSSettings,
 	serviceAccounts []string,
 	sni string,
@@ -459,26 +459,14 @@ func conditionallyConvertToIstioMtls(
 	meshExternal bool,
 	serviceMTLSMode model.MutualTLSMode,
 	clusterDiscoveryType cluster.Cluster_DiscoveryType) (*networking.ClientTLSSettings, mtlsContextType) {
-	mtlsCtx := userSupplied
-	if tls == nil {
-		if meshExternal || !autoMTLSEnabled || serviceMTLSMode == model.MTLSUnknown || serviceMTLSMode == model.MTLSDisable {
-			return nil, mtlsCtx
-		}
-		// Do not enable auto mtls when cluster type is `Cluster_ORIGINAL_DST`
-		// We don't know whether headless service instance has sidecar injected or not.
-		if clusterDiscoveryType == cluster.Cluster_ORIGINAL_DST {
-			return nil, mtlsCtx
+	if tls != nil {
+		if tls.Mode != networking.ClientTLSSettings_ISTIO_MUTUAL {
+			return tls, userSupplied
 		}
 
-		mtlsCtx = autoDetected
-		// we will setup transport sockets later
-		tls = &networking.ClientTLSSettings{
-			Mode: networking.ClientTLSSettings_ISTIO_MUTUAL,
-		}
-	}
-	if tls.Mode == networking.ClientTLSSettings_ISTIO_MUTUAL {
+		// Update TLS settings for ISTIO_MUTUAL.
 		// Use client provided SNI if set. Otherwise, overwrite with the auto generated SNI
-		// user specified SNIs in the istio mtls settings are useful when routing via gateways
+		// user specified SNIs in the istio mtls settings are useful when routing via gateways.
 		sniToUse := tls.Sni
 		if len(sniToUse) == 0 {
 			sniToUse = sni
@@ -487,9 +475,19 @@ func conditionallyConvertToIstioMtls(
 		if len(subjectAltNamesToUse) == 0 {
 			subjectAltNamesToUse = serviceAccounts
 		}
-		return buildIstioMutualTLS(subjectAltNamesToUse, sniToUse, proxy), mtlsCtx
+		return buildIstioMutualTLS(subjectAltNamesToUse, sniToUse, proxy), userSupplied
 	}
-	return tls, mtlsCtx
+
+	if meshExternal || !autoMTLSEnabled || serviceMTLSMode == model.MTLSUnknown || serviceMTLSMode == model.MTLSDisable {
+		return nil, userSupplied
+	}
+	// Do not enable auto mtls when cluster type is `Cluster_ORIGINAL_DST`
+	// We don't know whether headless service instance has sidecar injected or not.
+	if clusterDiscoveryType == cluster.Cluster_ORIGINAL_DST {
+		return nil, userSupplied
+	}
+	// Build settings for auto MTLS.
+	return buildIstioMutualTLS(serviceAccounts, sni, proxy), autoDetected
 }
 
 // buildIstioMutualTLS returns a `TLSSettings` for ISTIO_MUTUAL mode.
@@ -629,7 +627,7 @@ func applyTrafficPolicy(opts buildClusterOpts) {
 	if opts.clusterMode != SniDnatClusterMode && opts.direction != model.TrafficDirectionInbound {
 		autoMTLSEnabled := opts.push.Mesh.GetEnableAutoMtls().Value
 		var mtlsCtxType mtlsContextType
-		tls, mtlsCtxType = conditionallyConvertToIstioMtls(tls, opts.serviceAccounts, opts.istioMtlsSni, opts.proxy,
+		tls, mtlsCtxType = buildAutoMtlsSettings(tls, opts.serviceAccounts, opts.istioMtlsSni, opts.proxy,
 			autoMTLSEnabled, opts.meshExternal, opts.serviceMTLSMode, opts.cluster.GetType())
 		applyUpstreamTLSSettings(&opts, tls, mtlsCtxType, opts.proxy)
 	}
