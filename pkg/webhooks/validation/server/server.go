d1f7050cbeffbe810080fbd58ce1d61cccedda04
Ed Snible <snible@us.ibm.com>
2020-07-30 09:57:43 -0400

    [release-1.7] Manual cherry pick 25927 (#25957)
    * Use encoding/json to decode JSON
    
    * Use encoding/json to decode mixer JSON

--- pkg/webhooks/validation/server/server.go
+++ pkg/webhooks/validation/server/server.go
@@ -22,7 +22,6 @@ import (
 	"io/ioutil"
 	"net/http"
 
-	"github.com/ghodss/yaml"
 	"github.com/hashicorp/go-multierror"
 	kubeApiAdmission "k8s.io/api/admission/v1"
 	kubeApiApps "k8s.io/api/apps/v1beta1"
@@ -247,7 +246,7 @@ func (wh *Webhook) admitPilot(request *kubeApiAdmission.AdmissionRequest) *kubeA
 	}
 
 	var obj crd.IstioKind
-	if err := yaml.Unmarshal(request.Object.Raw, &obj); err != nil {
+	if err := json.Unmarshal(request.Object.Raw, &obj); err != nil {
 		scope.Infof("cannot decode configuration: %v", err)
 		reportValidationFailed(request, reasonYamlDecodeError)
 		return toAdmissionResponse(fmt.Errorf("cannot decode configuration: %v", err))
@@ -300,7 +299,7 @@ func (wh *Webhook) admitMixer(request *kubeApiAdmission.AdmissionRequest) *kubeA
 	case kubeApiAdmission.Create, kubeApiAdmission.Update:
 		ev.Type = store.Update
 		var obj crd.IstioKind
-		if err := yaml.Unmarshal(request.Object.Raw, &obj); err != nil {
+		if err := json.Unmarshal(request.Object.Raw, &obj); err != nil {
 			reportValidationFailed(request, reasonYamlDecodeError)
 			return toAdmissionResponse(fmt.Errorf("cannot decode configuration: %v", err))
 		}
@@ -349,7 +348,7 @@ func (wh *Webhook) admitMixer(request *kubeApiAdmission.AdmissionRequest) *kubeA
 
 func checkFields(raw []byte, kind string, namespace string, name string) (string, error) {
 	trial := make(map[string]json.RawMessage)
-	if err := yaml.Unmarshal(raw, &trial); err != nil {
+	if err := json.Unmarshal(raw, &trial); err != nil {
 		scope.Infof("cannot decode configuration fields: %v", err)
 		return reasonYamlDecodeError, fmt.Errorf("cannot decode configuration fields: %v", err)
 	}
