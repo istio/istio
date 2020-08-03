018cd4a6b3e16ce472526fac10fd59f03604784a
Istio Automation <istio-testing-bot@google.com>
2020-08-03 10:15:54 -0700

    [release-1.7] properly drain gateway listeners (#26054)
    * drain all listeners for gateway
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * lint
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    Co-authored-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/cmd/pilot-agent/main.go
+++ pilot/cmd/pilot-agent/main.go
@@ -401,6 +401,7 @@ var (
 				OutlierLogPath:      outlierLogPath,
 				PilotCertProvider:   pilotCertProvider,
 				ProvCert:            citadel.ProvCert,
+				Sidecar:             role.Type == model.SidecarProxy,
 			})
 
 			drainDuration, _ := types.DurationFromProto(proxyConfig.TerminationDrainDuration)
