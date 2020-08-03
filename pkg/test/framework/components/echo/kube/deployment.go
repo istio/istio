26a4d5202d57870bf1e15dc54d58761723739e04
Shriram Rajagopalan <rshriram@tetrate.io>
2020-08-03 09:15:54 -0400

    no ndots
    Signed-off-by: Shriram Rajagopalan <rshriram@tetrate.io>

--- pkg/test/framework/components/echo/kube/deployment.go
+++ pkg/test/framework/components/echo/kube/deployment.go
@@ -212,18 +212,13 @@ spec:
     spec:
       # Disable kube-dns, to mirror VM
       # we set policy to none and explicitly provide a set of invalid values
-      # for nameservers, search namespaces, etc. ndots is set to 1 so that
-      # the application will first try to resolve the hostname (a, a.ns, etc.) as is
-      # before attempting to add the search namespaces.
+      # for nameservers, search namespaces, etc.
       dnsPolicy: None
       dnsConfig:
         nameservers:
         - "8.8.8.8"
         searches:
         - "com"
-        options:
-        - name: "ndots"
-          value: "1"
       # Disable service account mount, to mirror VM
       automountServiceAccountToken: false
       containers:
