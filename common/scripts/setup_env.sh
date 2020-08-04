5c9ab452bb1e00fee79e99375b0f85b7eb4be8e9
Istio Automation <istio-testing-bot@google.com>
2020-08-04 08:09:25 -0700

    Automator: update common-files@master in istio/istio@master (#26040)

--- common/scripts/setup_env.sh
+++ common/scripts/setup_env.sh
@@ -59,7 +59,7 @@ fi
 
 # Build image to use
 if [[ "${IMAGE_VERSION:-}" == "" ]]; then
-  export IMAGE_VERSION=master-2020-07-22T15-36-22
+  export IMAGE_VERSION=master-2020-08-03T16-23-11
 fi
 if [[ "${IMAGE_NAME:-}" == "" ]]; then
   export IMAGE_NAME=build-tools
