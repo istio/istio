391422da0ba95ce70f83653b721901c90c39bfbb
Jonathan Kogan <jonathankogan@google.com>
2020-08-12 19:30:40 +0000

    test new metric

--- tests/integration/mixer/envoymixer/logs/accesslog_test.go
+++ tests/integration/mixer/envoymixer/logs/accesslog_test.go
@@ -139,9 +139,6 @@ func validateLog(content string) error {
 	if !strings.Contains(content, "newlog") {
 		return fmt.Errorf("accesslog doesn't contain newlog instance. Log %v", content)
 	}
-	// TODO: destination in test comes back as unknown.
-	// It seems to be since destination app comes back "" and may be because of a configuration  with parameters
-	// involving destination.
 	for _, expected := range []string{"details", "reviews", "productpage"} {
 		if !strings.Contains(content, fmt.Sprintf("\"destination\":\"%s\"", expected)) {
 			return fmt.Errorf("accesslog doesn't contain %s destination. Log %v", expected, content)
@@ -156,9 +153,8 @@ func validateLog(content string) error {
 	if !strings.Contains(content, "\"source\":\"productpage\"") {
 		return fmt.Errorf("accesslog doesn't contain productpage source. Log %v", content)
 	}
-	// TODO: This metric fails and isn't found. newlog is created from the accesslog and must be using a failing metric
-	// if  !strings.Contains(content, "\"source\":\"istio-ingressgateway\"") {
-	//	return fmt.Errorf("accesslog doesn't contain istio-ingressgateway source. Log %v", content)
-	// }
+	if  !strings.Contains(content, "\"source\":\"istio-ingressgateway\"") {
+		return fmt.Errorf("accesslog doesn't contain istio-ingressgateway source. Log %v END CONTENT", content)
+	}
 	return nil
 }
