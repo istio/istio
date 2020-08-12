f474a57186d40d7c997687553ed50d2d8cb36815
Nathan Mittler <nmittler@gmail.com>
2020-08-12 10:43:37 -0700

    Cherry-pick 1.7: Fix remote clusters when caAddress is not specified (#26334) (#26421)
    The installation of remote clusters now requires manually setting `caAddress`. This breaks our docs and is a general regression WRT multi-cluster installation.
    
    This change manually sets `CA_ADDR` correctly based on the existence of `caAddress`. It also reverts changes to tests to manually specify `caAddress`, so that the tests are more closely aligned with what we're telling users to do.
    
    Fixes #26325

--- pkg/test/framework/components/istio/operator.go
+++ pkg/test/framework/components/istio/operator.go
@@ -479,9 +479,7 @@ func deployControlPlane(c *operatorComponent, cfg Config, cluster resource.Clust
 				return fmt.Errorf("failed getting the istiod address for cluster %s: %v", controlPlaneCluster.Name(), err)
 			}
 			installSettings = append(installSettings,
-				"--set", "values.global.remotePilotAddress="+remoteIstiodAddress.IP.String(),
-				// Use the local Istiod for CA
-				"--set", "values.global.caAddress="+"istiod.istio-system.svc:15012")
+				"--set", "values.global.remotePilotAddress="+remoteIstiodAddress.IP.String())
 
 			if isCentralIstio(c.environment, cfg) {
 				installSettings = append(installSettings,
