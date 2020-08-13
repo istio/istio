afc15bdf0f55b2c2d5163e575cdbe0a3a83e6864
Chi Zhang <chizhg@google.com>
2020-08-13 20:45:39 +0000

    Support multiproject multicluster for GKE on GCP for running the ASM integration tests, and add the README file.
    Note:
    
    1. This solution is a bit hacky because we have to use the projects created in the `google3/configs/cloud/gong/org_hierarchy/google.com/teams/asm/dev`, which Yuan got exemption to be able to enable the shared VPC in https://buganizer.corp.google.com/issues/162463500.
    In the long term I will check if we can get rid of this hack by either getting exemption for the common boskos projects, or configure another boskos project pool with a different folder.
    
    2. Now you can type `/test multiproject-multicluster-test` to manually test the workflow, and an example is in https://prow-gob.gcpnode.com/view/gs/gob-prow/pr-logs/pull/gke-internal-review.googlesource.com_istio_istio/160874/multiproject-multicluster-test/1295448540310409216.
    After the rest (control plane setup and test execution) is implemented, we can convert the Prow job to be postsubmit, and configure testgrid for visualization and alerting.
    
    
    Change-Id: I44d67d23da09800e0f4d5c8748f5e1f0b1c27673

--- prow/asm/integ-suite-kubetest2.sh
+++ prow/asm/integ-suite-kubetest2.sh
@@ -14,6 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# This script is used as the entrypoint for running Prow jobs.
+# It brings up the Kubernetes clusters based on the input flags, and then invokes
+# integ-run-tests.sh which will setup the SUT and run the tests.
+
 # Usage:   ./integ-suite-kubetest2.sh --deployer [deployer_name]
 #             --deployer-flags [deployer_flag1 deployer_flag2 ...] \
 #             --test-flags [test_flag1 test_flag2 ...]
@@ -31,11 +35,14 @@ set -u
 # Print commands
 set -x
 
+source "${WD}/lib.sh"
+
 echo "Installing kubetest2..."
 # Switch to a temp directory to run `go get` which avoids touching the go.mod file.
 temp_dir="$(mktemp -d)"
 # Swallow the output as we are returning the stdout in the end.
 pushd "${temp_dir}" > /dev/null 2>&1 || exit 1
+GO111MODULE=on go get -u sigs.k8s.io/boskos/cmd/boskosctl
 GO111MODULE=on go get -u sigs.k8s.io/kubetest2
 GO111MODULE=on go get -u sigs.k8s.io/kubetest2/kubetest2-gke
 GO111MODULE=on go get -u sigs.k8s.io/kubetest2/kubetest2-kind
@@ -43,11 +50,15 @@ GO111MODULE=on go get -u sigs.k8s.io/kubetest2/kubetest2-tester-exec
 # TODO(chizhg): install kubetest2-tailorbird
 popd > /dev/null 2>&1 || exit 1
 
-deployer_flags=( "--up" "--down" )
+deployer_flags=(
+  "--up" "--create-command=beta container clusters create --quiet" "--ignore-gcp-ssh-key=true"
+  "--gcp-service-account=${GOOGLE_APPLICATION_CREDENTIALS}"
+)
 
 DEPLOYER=""
 EXTRA_DEPLOYER_FLAGS=""
 TEST_FLAGS=""
+TOPOLOGY=SINGLE_CLUSTER
 
 while (( "$#" )); do
   case "$1" in
@@ -65,17 +76,57 @@ while (( "$#" )); do
     --test-flags)
       TEST_FLAGS=$2
       shift 2
+    ;;
+    --topology)
+      case $2 in
+        MULTICLUSTER | MULTIPROJECT_MULTICLUSTER )
+          TOPOLOGY=$2
+          echo "Running with topology ${TOPOLOGY}"
+          ;;
+        *)
+          echo "Error: Unsupported topology ${TOPOLOGY}" >&2
+          exit 1
+          ;;
+      esac
+      shift 2
+    ;;
+    *)
+      echo "Error: unknown option $1"
+      exit 1
+      ;;
   esac
 done
+
+# Activate the service account with the key file.
+# gcloud auth activate-service-account --key-file="${GOOGLE_APPLICATION_CREDENTIALS}"
 
 readonly DEPLOYER
 readonly EXTRA_DEPLOYER_FLAGS
 readonly TEST_FLAGS
+readonly TOPOLOGY
+export DEPLOYER
+export TOPOLOGY
 
 IFS=' ' read -r -a extra_deployer_flags <<< "$EXTRA_DEPLOYER_FLAGS"
 deployer_flags+=( "${extra_deployer_flags[@]}" )
 
 IFS=' ' read -r -a test_flags <<< "$TEST_FLAGS"
 
+if [[ "${DEPLOYER}" == "gke" ]]; then
+  if [[ "${TOPOLOGY}" == "MULTICLUSTER"  ]]; then
+    deployer_flags+=("--cluster-name=test1,test2" "--machine-type=e2-standard-4" "--num-nodes=3" "--region=us-central1")
+  elif [[ "${TOPOLOGY}" == "MULTIPROJECT_MULTICLUSTER" ]]; then
+    # A slightly hacky step to setup the environment, see the comments on the
+    # function signature.
+    multiproject_multicluster_setup
+    multi_cluster_deployer_flags=("--cluster-name=prow-test1:1,prow-test2:2" "--machine-type=e2-standard-4" "--num-nodes=1" "--region=us-central1")
+    multi_cluster_deployer_flags+=("--network=test-network" "--subnetwork-ranges=172.16.4.0/22 172.16.16.0/20 172.20.0.0/14,10.0.4.0/22 10.0.32.0/20 10.4.0.0/14" )
+    # These projects are mananged by the boskos project rental pool in
+    # https://gke-internal.googlesource.com/istio/test-infra-internal/+/refs/heads/master/boskos/config/resources.yaml#105
+    multi_cluster_deployer_flags+=("--project=engprod-test-host,engprod-test-service-1,engprod-test-service-2")
+    deployer_flags+=( "${multi_cluster_deployer_flags[@]}" )
+  fi
+fi
+
 # Run kubetest2 to start running the test workflow.
-kubetest2 "${DEPLOYER}" "${deployer_flags[@]}" --test=exec -- "{WD}"/integ-run-tests.sh "${test_flags[@]}" || exit 1
+kubetest2 "${DEPLOYER}" "${deployer_flags[@]}" --test=exec -- "${WD}"/integ-run-tests.sh "${test_flags[@]}" || exit 1
