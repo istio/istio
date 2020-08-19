cd8b01b6ea8f16e5d728020a394a9078f5a840ba
Nikolay Pshenichnyy <nikolay.pshenichny@gmail.com>
2020-08-19 14:29:00 -0700

    fix indentation in update_crds

--- bin/update_crds.sh
+++ bin/update_crds.sh
@@ -46,8 +46,8 @@ fi
 git clone  "https://${REPO}" "${API_TMP}" && cd "${API_TMP}"
 git checkout "${SHA}"
 if [ ! -f "${API_TMP}/kubernetes/customresourcedefinitions.gen.yaml" ]; then
- echo "Generated Custom Resource Definitions file does not exist in the commit SHA ${SHA}. Not updating the CRD file."
- exit
+  echo "Generated Custom Resource Definitions file does not exist in the commit SHA ${SHA}. Not updating the CRD file."
+  exit
 fi
 rm -f "${ROOTDIR}/manifests/charts/base/crds/crd-all.gen.yaml"
 cp "${API_TMP}/kubernetes/customresourcedefinitions.gen.yaml" "${ROOTDIR}/manifests/charts/base/crds/crd-all.gen.yaml"
