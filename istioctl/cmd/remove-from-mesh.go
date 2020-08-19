83ffb4e8d01da6b87a9be1fad7a68e03acd4fb1d
Tariq Ibrahim <tariq181290@gmail.com>
2020-08-19 15:21:16 -0700

    remove-from-mesh should also restore rewritten app probes

--- istioctl/cmd/remove-from-mesh.go
+++ istioctl/cmd/remove-from-mesh.go
@@ -16,11 +16,12 @@ package cmd
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"strings"
 
-	"github.com/hashicorp/go-multierror"
+	multierror "github.com/hashicorp/go-multierror"
 	"github.com/spf13/cobra"
 	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -30,6 +31,7 @@ import (
 
 	"istio.io/api/annotation"
 	"istio.io/istio/istioctl/pkg/util/handlers"
+	istioStatus "istio.io/istio/pilot/cmd/pilot-agent/status"
 	"istio.io/istio/pkg/config/schema/collections"
 	"istio.io/pkg/log"
 )
@@ -200,6 +202,17 @@ func unInjectSideCarFromDeployment(client kubernetes.Interface, deps []appsv1.De
 				continue
 			}
 		}
+
+		var appProbe istioStatus.KubeAppProbers
+		appProbeStr := retrieveAppProbe(podSpec.Containers)
+		if appProbeStr != "" {
+			err := json.Unmarshal([]byte(appProbeStr), &appProbe)
+			errs = multierror.Append(errs, err)
+		}
+		if appProbe != nil {
+			podSpec.Containers = restoreAppProbes(podSpec.Containers, appProbe)
+		}
+
 		podSpec.InitContainers = removeInjectedContainers(podSpec.InitContainers, initContainerName)
 		podSpec.InitContainers = removeInjectedContainers(podSpec.InitContainers, enableCoreDumpContainerName)
 		podSpec.Containers = removeInjectedContainers(podSpec.Containers, proxyContainerName)
