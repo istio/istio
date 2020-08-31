601077e81bafd02cf9f2bf2f4325b2b1ea9e6040
Shriram Rajagopalan <rshriram@users.noreply.github.com>
2020-08-31 13:27:33 -0400

    Add tests for name table (#26927)
    * NDS tests
    
    * missing files
    
    Signed-off-by: Shriram Rajagopalan <rshriram@tetrate.io>
    
    * remove cruft
    
    Signed-off-by: Shriram Rajagopalan <rshriram@tetrate.io>
    
    * lint
    
    Signed-off-by: Shriram Rajagopalan <rshriram@tetrate.io>

--- pilot/pkg/networking/core/v1alpha3/listener_builder.go
+++ pilot/pkg/networking/core/v1alpha3/listener_builder.go
@@ -63,8 +63,6 @@ type ListenerBuilder struct {
 	httpProxyListener       *listener.Listener
 	virtualOutboundListener *listener.Listener
 	virtualInboundListener  *listener.Listener
-	// UDP listener for local dns resolution in Envoy
-	dnsListener *listener.Listener
 }
 
 // Setup the filter chain match so that the match should work under both
@@ -283,7 +281,6 @@ func (lb *ListenerBuilder) patchListeners() {
 	}
 
 	lb.virtualOutboundListener = lb.patchOneListener(lb.virtualOutboundListener, networking.EnvoyFilter_SIDECAR_OUTBOUND)
-	lb.dnsListener = lb.patchOneListener(lb.dnsListener, networking.EnvoyFilter_SIDECAR_OUTBOUND)
 	lb.virtualInboundListener = lb.patchOneListener(lb.virtualInboundListener, networking.EnvoyFilter_SIDECAR_INBOUND)
 	lb.inboundListeners = envoyfilter.ApplyListenerPatches(networking.EnvoyFilter_SIDECAR_INBOUND, lb.node,
 		lb.push, lb.inboundListeners, false)
@@ -294,7 +291,7 @@ func (lb *ListenerBuilder) patchListeners() {
 func (lb *ListenerBuilder) getListeners() []*listener.Listener {
 	if lb.node.Type == model.SidecarProxy {
 		nInbound, nOutbound := len(lb.inboundListeners), len(lb.outboundListeners)
-		nHTTPProxy, nVirtual, nVirtualInbound, nDNS := 0, 0, 0, 0
+		nHTTPProxy, nVirtual, nVirtualInbound := 0, 0, 0
 		if lb.httpProxyListener != nil {
 			nHTTPProxy = 1
 		}
@@ -304,11 +301,8 @@ func (lb *ListenerBuilder) getListeners() []*listener.Listener {
 		if lb.virtualInboundListener != nil {
 			nVirtualInbound = 1
 		}
-		if lb.dnsListener != nil {
-			nDNS = 1
-		}
 
-		nListener := nInbound + nOutbound + nHTTPProxy + nVirtual + nVirtualInbound + nDNS
+		nListener := nInbound + nOutbound + nHTTPProxy + nVirtual + nVirtualInbound
 
 		listeners := make([]*listener.Listener, 0, nListener)
 		listeners = append(listeners, lb.inboundListeners...)
@@ -322,19 +316,15 @@ func (lb *ListenerBuilder) getListeners() []*listener.Listener {
 		if lb.virtualInboundListener != nil {
 			listeners = append(listeners, lb.virtualInboundListener)
 		}
-		if lb.dnsListener != nil {
-			listeners = append(listeners, lb.dnsListener)
-		}
 
 		log.Debugf("Build %d listeners for node %s including %d outbound, %d http proxy, "+
-			"%d virtual outbound and %d virtual inbound listeners, and %d DNS listener",
+			"%d virtual outbound and %d virtual inbound listeners",
 			nListener,
 			lb.node.ID,
 			nOutbound,
 			nHTTPProxy,
 			nVirtual,
-			nVirtualInbound,
-			nDNS)
+			nVirtualInbound)
 		return listeners
 	}
 
