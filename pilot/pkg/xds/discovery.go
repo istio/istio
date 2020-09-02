40a3201c49d8d781352c5c3121ea368bd2dd73c7
Steven Landow <steven@stlcomputerservices.com>
2020-09-02 11:44:06 -0700

    implement lru cache with metrics (#26944)
    * implement lru cache with metrics
    
    * fix defer
    
    * add knobs and move locks
    
    * only use one set of locks
    
    * rename cache vars
    
    * init monitoring
    
    * reading is a write operation in lru
    
    * spelling
    
    * allow configuring inMemoryCache cache
    
    * allow configuring inMemoryCache cache
    
    * lint
    
    * lint

--- pilot/pkg/xds/discovery.go
+++ pilot/pkg/xds/discovery.go
@@ -174,7 +174,7 @@ func NewDiscoveryServer(env *model.Environment, plugins []string) *DiscoveryServ
 
 	out.initGenerators()
 
-	if features.EnableEDSCaching {
+	if features.EnableXDSCaching {
 		out.Cache = model.NewXdsCache()
 	}
 
