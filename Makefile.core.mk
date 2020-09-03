2303da7ba19e8d78b9eb45c44216a799d07a4512
John Howard <howardjohn@google.com>
2020-09-03 07:33:55 -0700

    Test flakes

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -420,7 +420,7 @@ BENCH_TARGETS ?= ./pilot/...
 
 .PHONY: racetest
 racetest: $(JUNIT_REPORT)
-	go test ${GOBUILDFLAGS} ${T} -race ./... 2>&1 | tee >($(JUNIT_REPORT) > $(JUNIT_OUT))
+	go test ${GOBUILDFLAGS} ${T} -count=2 -race ./... 2>&1 | tee >($(JUNIT_REPORT) > $(JUNIT_OUT))
 
 .PHONY: benchtest
 benchtest: $(JUNIT_REPORT) ## Runs all benchmarks
