5db9aa7ab8bda95baf985ca8d6a72d76d41ce225
Chi Zhang <chizhg@google.com>
2020-09-10 02:54:10 +0000

    Support provisioning dynamic GCP projects
    Now we have 5 host projects and 15 service projects, as defined in
    https://gke-internal.googlesource.com/istio/test-infra-internal/+/refs/heads/master/boskos/config/resources.yaml#111,
    so we support at most 5 Prow jobs running in parallel.
    
    Change-Id: I6f9ef77e96d4342e6ec7dfe07316764638d2414c
    Bug: 167461713

--- prow/asm/lib.sh
+++ prow/asm/lib.sh
@@ -14,18 +14,39 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-readonly MULTIPROJECT_MULTICLUSTER_BOSKOS_RESOURCE="mm-gke-project"
+readonly SHARED_VPC_HOST_BOSKOS_RESOURCE="shared-vpc-host-gke-project"
+readonly SHARED_VPC_SVC_BOSKOS_RESOURCE="shared-vpc-svc-gke-project"
 
 # The network and firewall rule resources are very likely leaked if we
 # teardown them with `kubetest2 --down`, so we leverage boskos-janitor here
 # since it can make sure the projects can be back to the sanity state.
 # TODO(chizhg): find a cleaner and less hacky way to handle this.
 function multiproject_multicluster_setup() {
-  # Acquire 3 projects for multiproject multicluster tests.
-  for ((i=0;i<3;i++));
-  do
-    boskos_acquire "${MULTIPROJECT_MULTICLUSTER_BOSKOS_RESOURCE}"
-  done
+  # Acquire a host project from the project rental pool.
+  local host_project
+  host_project=$(boskos_acquire "${SHARED_VPC_HOST_BOSKOS_RESOURCE}")
+  # Remove all projects that are currently associated with this host project.
+  local associated_projects
+  associated_projects=$(gcloud beta compute shared-vpc associated-projects list "${host_project}" --format="value(RESOURCE_ID)")
+  if [ -n "${associated_projects}" ]; then
+    while read -r svc_project
+    do
+      gcloud beta compute shared-vpc associated-projects remove "${svc_project}" --host-project "${host_project}"
+    done <<< "$associated_projects"
+  fi
+
+  # Acquire two service projects from the project rental pool.
+  local service_project1
+  service_project1=$(boskos_acquire "${SHARED_VPC_SVC_BOSKOS_RESOURCE}")
+  local service_project2
+  service_project2=$(boskos_acquire "${SHARED_VPC_SVC_BOSKOS_RESOURCE}")
+  # Associate the service projects with the host project.
+  # TODO(chizhg): remove this once
+  # https://github.com/kubernetes-sigs/kubetest2/pull/50 is merged.
+  gcloud beta compute shared-vpc associated-projects add "${service_project1}" --host-project "${host_project}"
+  gcloud beta compute shared-vpc associated-projects add "${service_project2}" --host-project "${host_project}"
+  
+  echo "${host_project},${service_project1},${service_project2}"
 }
 
 #####################################################################
