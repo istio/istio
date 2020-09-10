b0fbe73106e9cc31e4f73fde91b8697186ebc1c3
Martin Ostrowski <mostrowski@google.com>
2020-09-10 14:49:40 -0700

    Output progress to console, capture log, create archive, cleanup (#27025)
    * Output progress to console, capture log, create archive, cleanup
    
    * Fix binary params

--- istioctl/cmd/root.go
+++ istioctl/cmd/root.go
@@ -264,7 +264,7 @@ debug and diagnose their Istio mesh.
 	hideInheritedFlags(upgradeCmd, "namespace", "istioNamespace")
 	rootCmd.AddCommand(upgradeCmd)
 
-	bugReportCmd := bugreport.Cmd()
+	bugReportCmd := bugreport.Cmd(loggingOptions)
 	hideInheritedFlags(bugReportCmd, "namespace", "istioNamespace")
 	rootCmd.AddCommand(bugReportCmd)
 
