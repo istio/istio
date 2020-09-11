3c642f405dbbe8fdf00fbd609bd9ceb6fa1840ad
nmittler <nmittler@gmail.com>
2020-09-11 10:16:51 -0700

    Use dedicated east-west gateway for multicluster samples.

---
+++ samples/multicluster/gen-eastwest-gateway.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+# Copyright Istio Authors
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+set -euo pipefail
+
+if [[ -n "${NETWORK:-}" ]]; then
+  echo The NETWORK environment variable must be set in order to specify the local network for the gateway.
+  exit 1
+fi
+
+# Generate the YAML for the east-west gateway.
+istioctl manifest generate -f - <<EOF
+apiVersion: install.istio.io/v1alpha1
+kind: IstioOperator
+spec:
+  profile: empty # Only generate a gateway component defined below.
+  components:
+    ingressGateways:
+      - name: istio-eastwestgateway
+        label:
+          istio: eastwestgateway
+          app: istio-eastwestgateway
+        enabled: true
+        k8s:
+          env:
+            # traffic through this gateway should be routed inside the network
+            - name: ISTIO_META_REQUESTED_NETWORK_VIEW
+              value: ${NETWORK}
+          service:
+            ports:
+              - name: status-port
+                port: 15021
+                targetPort: 15021
+              - name: mtls
+                port: 15443
+                targetPort: 15443
+              - name: tcp-istiod
+                port: 15012
+                targetPort: 15012
+              - name: tcp-webhook
+                port: 15017
+                targetPort: 15017
+              - name: tcp-dns-tls
+                port: 853
+                targetPort: 8853
+EOF
