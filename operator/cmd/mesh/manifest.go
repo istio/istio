86c8ba7581fdb201b0d9e99a20df2a24a292fb7d
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2020-09-14 16:07:26 +0900

    Add deprecated message in root code

--- operator/cmd/mesh/manifest.go
+++ operator/cmd/mesh/manifest.go
@@ -35,8 +35,6 @@ func ManifestCmd(logOpts *log.Options) *cobra.Command {
 
 	mgc := manifestGenerateCmd(args, mgcArgs, logOpts)
 	mdc := manifestDiffCmd(args, mdcArgs)
-	ic := InstallCmd(logOpts)
-	ic.Short += " " + "[Deprecated, use \"istioctl install\". it will be removed in Istio 1.9]"
 
 	addFlags(mc, args)
 	addFlags(mgc, args)
@@ -47,7 +45,6 @@ func ManifestCmd(logOpts *log.Options) *cobra.Command {
 
 	mc.AddCommand(mgc)
 	mc.AddCommand(mdc)
-	mc.AddCommand(ic)
 
 	return mc
 }
