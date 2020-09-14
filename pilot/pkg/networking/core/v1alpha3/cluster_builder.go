istio/istio

commit 9d12499c33fde7dba45b1b371a41ad3b312112f6

cluster: remove dns lookup family (#26409)
* remove conflicting dns settings

Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

* revert dns refresh rate change

Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

pilot/pkg/networking/core/v1alpha3/cluster_builder.go
@@ -199,7 +199,6 @@ func (cb *ClusterBuilder) buildDefaultCluster(name string, discoveryType cluster
 
 	switch discoveryType {
 	case cluster.Cluster_STRICT_DNS:
-		c.DnsLookupFamily = cluster.Cluster_V4_ONLY
 		dnsRate := gogo.DurationToProtoDuration(cb.push.Mesh.DnsRefreshRate)
 		c.DnsRefreshRate = dnsRate
 		c.RespectDnsTtl = true

commit 3f071971d4a25cb571ffba30207e623bd036f481

Fix regression in ipv4 only clusters with DNS
Broke in https://github.com/istio/istio/pull/26409/files

pilot/pkg/networking/core/v1alpha3/cluster_builder.go
@@ -196,9 +196,14 @@ func (cb *ClusterBuilder) buildDefaultCluster(name string, discoveryType cluster
 		Name:                 name,
 		ClusterDiscoveryType: &cluster.Cluster_Type{Type: discoveryType},
 	}
-
 	switch discoveryType {
 	case cluster.Cluster_STRICT_DNS:
+		// By default, Envoy will use AUTO, which will use IPv6 if the DNS response returns IPv6.
+		// This will break if the proxy does not support IPv6. In these cases, we should explicitly
+		// request on v4.
+		if !cb.proxy.SupportsIPv6() {
+			c.DnsLookupFamily = cluster.Cluster_V4_ONLY
+		}
 		dnsRate := gogo.DurationToProtoDuration(cb.push.Mesh.DnsRefreshRate)
 		c.DnsRefreshRate = dnsRate
 		c.RespectDnsTtl = true