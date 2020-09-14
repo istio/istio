06ea740b45fc090cfd0087740f94314b7d9e0ac0
Rama Chavali <rama.rao@salesforce.com>
2020-09-15 00:24:33 +0530

    do not apply locality load balancer settings for inbound clusters (#27295)
    * do not apply locality lb settings for inbound clusters
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * add release notes
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * fix release notes
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * lint
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/networking/core/v1alpha3/cluster.go
+++ pilot/pkg/networking/core/v1alpha3/cluster.go
@@ -573,10 +573,13 @@ func setH2Options(cluster *cluster.Cluster) {
 
 func applyTrafficPolicy(opts buildClusterOpts) {
 	connectionPool, outlierDetection, loadBalancer, tls := selectTrafficPolicyComponents(opts.policy)
-	applyH2Upgrade(opts, connectionPool)
+	// Connection pool settings are applicable for both inbound and outbound clusters.
 	applyConnectionPool(opts.mesh, opts.cluster, connectionPool)
-	applyOutlierDetection(opts.cluster, outlierDetection)
-	applyLoadBalancer(opts.cluster, loadBalancer, opts.port, opts.proxy, opts.mesh)
+	if opts.direction != model.TrafficDirectionInbound {
+		applyH2Upgrade(opts, connectionPool)
+		applyOutlierDetection(opts.cluster, outlierDetection)
+		applyLoadBalancer(opts.cluster, loadBalancer, opts.port, opts.proxy, opts.mesh)
+	}
 
 	if opts.clusterMode != SniDnatClusterMode && opts.direction != model.TrafficDirectionInbound {
 		autoMTLSEnabled := opts.mesh.GetEnableAutoMtls().Value
