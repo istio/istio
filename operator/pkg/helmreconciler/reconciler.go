ddbe75046cb25246f3ec7023af3140dadfc79e2e
morvencao <morvencao@gmail.com>
2020-09-16 08:43:56 -0700

    fix inconsistent cr hash.

--- operator/pkg/helmreconciler/reconciler.go
+++ operator/pkg/helmreconciler/reconciler.go
@@ -415,11 +415,7 @@ func (h *HelmReconciler) getCRHash(componentName string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	var host string
-	if h.restConfig != nil {
-		host = h.restConfig.Host
-	}
-	return strings.Join([]string{crName, crNamespace, componentName, host}, "-"), nil
+	return strings.Join([]string{crName, crNamespace, componentName}, "-"), nil
 }
 
 // getCRNamespace returns the namespace of the CR associated with h.
