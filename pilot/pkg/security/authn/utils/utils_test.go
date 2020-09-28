174178fb9812c483164425555cd4c4ce15731e3e
Rama Chavali <rama.rao@salesforce.com>
2020-09-28 22:00:12 +0530

    default min TLS version and configure cipher suites (#27500)
    * default min tls version and cipher suites
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * fix ut
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * lint
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * add release notes
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * add release notes
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * fix release notes
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * order cipher suites
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * fix unit tests
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * change to positive predicate and remove unnecessary word
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * fix ut
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/security/authn/utils/utils_test.go
+++ pilot/pkg/security/authn/utils/utils_test.go
@@ -25,6 +25,7 @@ import (
 	"github.com/google/go-cmp/cmp"
 	"google.golang.org/protobuf/testing/protocmp"
 
+	"istio.io/istio/pilot/pkg/features"
 	"istio.io/istio/pilot/pkg/model"
 	"istio.io/istio/pilot/pkg/networking"
 	authn_model "istio.io/istio/pilot/pkg/security/model"
@@ -39,6 +40,7 @@ func TestBuildInboundFilterChain(t *testing.T) {
 		node             *model.Proxy
 		listenerProtocol networking.ListenerProtocol
 		trustDomains     []string
+		tlsV2Enabled     bool
 	}
 	tests := []struct {
 		name string
@@ -53,6 +55,7 @@ func TestBuildInboundFilterChain(t *testing.T) {
 					Metadata: &model.NodeMetadata{},
 				},
 				listenerProtocol: networking.ListenerProtocolAuto,
+				tlsV2Enabled:     false,
 			},
 			// No need to set up filter chain, default one is okay.
 			want: nil,
@@ -65,6 +68,7 @@ func TestBuildInboundFilterChain(t *testing.T) {
 					Metadata: &model.NodeMetadata{},
 				},
 				listenerProtocol: networking.ListenerProtocolAuto,
+				tlsV2Enabled:     false,
 			},
 			want: nil,
 		},
@@ -77,6 +81,7 @@ func TestBuildInboundFilterChain(t *testing.T) {
 					Metadata: &model.NodeMetadata{},
 				},
 				listenerProtocol: networking.ListenerProtocolHTTP,
+				tlsV2Enabled:     true,
 			},
 			want: []networking.FilterChain{
 				{
@@ -130,6 +135,17 @@ func TestBuildInboundFilterChain(t *testing.T) {
 								},
 							},
 							AlpnProtocols: []string{"h2", "http/1.1"},
+							TlsParams: &auth.TlsParameters{
+								TlsMinimumProtocolVersion: auth.TlsParameters_TLSv1_2,
+								CipherSuites: []string{
+									"ECDHE-ECDSA-AES256-GCM-SHA384",
+									"ECDHE-RSA-AES256-GCM-SHA384",
+									"ECDHE-ECDSA-AES128-GCM-SHA256",
+									"ECDHE-RSA-AES128-GCM-SHA256",
+									"AES256-GCM-SHA384",
+									"AES128-GCM-SHA256",
+								},
+							},
 						},
 						RequireClientCertificate: protovalue.BoolTrue,
 					},
@@ -146,6 +162,90 @@ func TestBuildInboundFilterChain(t *testing.T) {
 				},
 				listenerProtocol: networking.ListenerProtocolHTTP,
 				trustDomains:     []string{"cluster.local"},
+				tlsV2Enabled:     true,
+			},
+			want: []networking.FilterChain{
+				{
+					TLSContext: &auth.DownstreamTlsContext{
+						CommonTlsContext: &auth.CommonTlsContext{
+							TlsCertificateSdsSecretConfigs: []*auth.SdsSecretConfig{
+								{
+									Name: "default",
+									SdsConfig: &core.ConfigSource{
+										InitialFetchTimeout: ptypes.DurationProto(0 * time.Second),
+										ResourceApiVersion:  core.ApiVersion_V3,
+										ConfigSourceSpecifier: &core.ConfigSource_ApiConfigSource{
+											ApiConfigSource: &core.ApiConfigSource{
+												ApiType:             core.ApiConfigSource_GRPC,
+												TransportApiVersion: core.ApiVersion_V3,
+												GrpcServices: []*core.GrpcService{
+													{
+														TargetSpecifier: &core.GrpcService_EnvoyGrpc_{
+															EnvoyGrpc: &core.GrpcService_EnvoyGrpc{ClusterName: authn_model.SDSClusterName},
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+							ValidationContextType: &auth.CommonTlsContext_CombinedValidationContext{
+								CombinedValidationContext: &auth.CommonTlsContext_CombinedCertificateValidationContext{
+									DefaultValidationContext: &auth.CertificateValidationContext{MatchSubjectAltNames: []*matcher.StringMatcher{
+										{MatchPattern: &matcher.StringMatcher_Prefix{Prefix: spiffe.URIPrefix + spiffe.GetTrustDomain() + "/"}},
+									}},
+									ValidationContextSdsSecretConfig: &auth.SdsSecretConfig{
+										Name: "ROOTCA",
+										SdsConfig: &core.ConfigSource{
+											InitialFetchTimeout: ptypes.DurationProto(0 * time.Second),
+											ResourceApiVersion:  core.ApiVersion_V3,
+											ConfigSourceSpecifier: &core.ConfigSource_ApiConfigSource{
+												ApiConfigSource: &core.ApiConfigSource{
+													ApiType:             core.ApiConfigSource_GRPC,
+													TransportApiVersion: core.ApiVersion_V3,
+													GrpcServices: []*core.GrpcService{
+														{
+															TargetSpecifier: &core.GrpcService_EnvoyGrpc_{
+																EnvoyGrpc: &core.GrpcService_EnvoyGrpc{ClusterName: authn_model.SDSClusterName},
+															},
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+							AlpnProtocols: []string{"h2", "http/1.1"},
+							TlsParams: &auth.TlsParameters{
+								TlsMinimumProtocolVersion: auth.TlsParameters_TLSv1_2,
+								CipherSuites: []string{
+									"ECDHE-ECDSA-AES256-GCM-SHA384",
+									"ECDHE-RSA-AES256-GCM-SHA384",
+									"ECDHE-ECDSA-AES128-GCM-SHA256",
+									"ECDHE-RSA-AES128-GCM-SHA256",
+									"AES256-GCM-SHA384",
+									"AES128-GCM-SHA256",
+								},
+							},
+						},
+						RequireClientCertificate: protovalue.BoolTrue,
+					},
+				},
+			},
+		},
+		{
+			name: "MTLSStrict using SDS with local trust domain and TLSv2 feature disabled",
+			args: args{
+				mTLSMode:   model.MTLSStrict,
+				sdsUdsPath: "/tmp/sdsuds.sock",
+				node: &model.Proxy{
+					Metadata: &model.NodeMetadata{},
+				},
+				listenerProtocol: networking.ListenerProtocolHTTP,
+				trustDomains:     []string{"cluster.local"},
+				tlsV2Enabled:     false,
 			},
 			want: []networking.FilterChain{
 				{
@@ -210,6 +310,11 @@ func TestBuildInboundFilterChain(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			defaultValue := features.EnableTLSv2OnInboundPath
+			features.EnableTLSv2OnInboundPath = tt.args.tlsV2Enabled
+			defer func() {
+				features.EnableTLSv2OnInboundPath = defaultValue
+			}()
 			got := BuildInboundFilterChain(tt.args.mTLSMode, tt.args.sdsUdsPath, tt.args.node, tt.args.listenerProtocol, tt.args.trustDomains)
 			if diff := cmp.Diff(got, tt.want, protocmp.Transform()); diff != "" {
 				t.Errorf("BuildInboundFilterChain() = %v", diff)
