b3583c08922ac3a2d4df1860aa3170debdbe81df
Nikolay Pshenichnyy <nikolay.pshenichny@gmail.com>
2020-10-04 15:13:36 -0700

    filebased mtls: Initial

---
+++ tests/integration/security/file_mounted_certs/main_test.go
@@ -0,0 +1,206 @@
+// +build integ
+//  Copyright Istio Authors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package filemountedcerts
+
+import (
+	"context"
+
+	"io/ioutil"
+	"path"
+	"strings"
+	"testing"
+
+	"istio.io/istio/pkg/test/framework"
+	"istio.io/istio/pkg/test/framework/components/istio"
+	"istio.io/istio/pkg/test/framework/label"
+	"istio.io/istio/pkg/test/framework/resource"
+
+	"istio.io/istio/pkg/test/env"
+	"istio.io/istio/pkg/test/framework/components/environment/kube"
+	"istio.io/istio/pkg/test/framework/components/namespace"
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var (
+	inst istio.Instance
+)
+
+const (
+	ProxyMetadataJson = `
+	{
+      "JWT_POLICY": "third-party-jwt",
+      "PILOT_CERT_PROVIDER": "mycopki",
+      "FILE_MOUNTED_CERTS": "true",
+      "ISTIO_META_TLS_CLIENT_CERT_CHAIN": "/client-certs/cert.pem",
+      "ISTIO_META_TLS_CLIENT_KEY": "/client-certs/key.pem",
+      "ISTIO_META_TLS_CLIENT_ROOT_CERT": "/client-certs/ca.pem",
+      "ISTIO_META_TLS_SERVER_CERT_CHAIN": "/server-certs/cert.pem",
+      "ISTIO_META_TLS_SERVER_KEY": "/server-certs/key.pem",
+      "ISTIO_META_TLS_SERVER_ROOT_CERT": "/server-certs/ca.pem",
+      "ISTIO_META_PROXY_XDS_VIA_AGENT": "false",
+      "ISTIO_METAJSON_METRICS_INCLUSIONS": '{\"sidecar.istio.io/statsInclusionPrefixes\": \"access_log_file,cluster,cluster_manager,control_plane,http,http2,http_mixer_filter,listener,listener_manager,redis,runtime,server,stats,tcp,tcp_mixer_filter,tracing\"}',
+      "ISTIO_META_DNS_CAPTURE": "false",
+	}
+`
+)
+
+func TestMain(m *testing.M) {
+	framework.
+		NewSuite(m).
+		Label(label.CustomSetup).
+
+		// SDS requires Kubernetes 1.13
+		RequireEnvironmentVersion("1.13").
+		Label("CustomSetup").
+		Setup(istio.Setup(&inst, setupConfig, CreateCustomIstiodSecret)).
+		Run()
+}
+
+
+func setupConfig(_ resource.Context, cfg *istio.Config) {
+	if cfg == nil {
+		return
+	}
+
+	cfg.ControlPlaneValues = `
+components:
+  egressGateways:
+  - enabled: false
+    name: istio-egressgateway
+  ingressGateways:
+  - enabled: false
+    name: istio-ingressgateway
+  pilot:
+    enabled: true
+    k8s:
+      overlays:
+        - kind: Deployment
+          name: istiod
+          patches:
+            - path: spec.template.spec.containers.[name:discovery].args
+              value: [
+                "discovery",
+                "--monitoringAddr=:15014",
+                "--log_output_level=default:info",
+                "--domain",
+                "cluster.local",
+                "--trust-domain=cluster.local",
+                "--keepaliveMaxServerConnectionAge",
+                "30m",
+                "--resync",
+                "5m",
+                "--caCertFile",
+                "/server-certs/ca.pem",
+                "--tlsCertFile",
+                "/server-certs/cert.pem",
+                "--tlsKeyFile",
+                "/server-certs/key.pem"
+              ]
+            - path: spec.template.spec.volumes[8]
+              value: |-
+                name: server-certs
+                secret:
+                  secretName: test-istiod-server-cred
+                  defaultMode: 420
+            - path: spec.template.spec.containers[name:discovery].volumeMounts[7]
+              value: |-
+                name: server-certs
+                mountPath: /server-certs
+
+meshConfig:
+  defaultConfig:
+    controlPlaneAuthPolicy: "NONE"
+    discoveryAddress: istiod.istio-system:15010
+    proxyMetadata: ` + strings.Replace(ProxyMetadataJson, "\n", "", -1) +
+`
+values:
+  global:
+    pilotCertProvider: "mycopki"
+    logging:
+      level: "default:debug"
+    proxy:
+      logLevel: debug
+      componentLogLevel: "misc:debug"
+  pilot:
+    env:
+      PILOT_SKIP_VALIDATE_TRUST_DOMAIN: "true"
+      PILOT_SIDECAR_USE_REMOTE_ADDRESS: "true"
+      PILOT_ENABLE_PROTOCOL_SNIFFING_FOR_OUTBOUND: "true"
+      PILOT_ENABLE_PROTOCOL_SNIFFING_FOR_INBOUND: "true"
+      XDS_AUTH: "false"
+      PILOT_ENABLE_CRD_VALIDATION: "true"
+      DNS_ADDR: ""
+      ENABLE_CA_SERVER: "false"
+`
+}
+
+func CreateCustomIstiodSecret(ctx resource.Context) error {
+	systemNs, err := istio.ClaimSystemNamespace(ctx)
+	if err != nil {
+		return err
+	}
+
+
+	CreateCustomSecret(ctx, "test-istiod-server-cred", systemNs, "tests/testdata/certs/pilot")
+	return nil
+}
+
+func CreateCustomSecret(ctx resource.Context, name string, namespace namespace.Instance, certsPath string) error {
+	if certsPath == "" {
+		certsPath = "tests/testdata/certs/dns"
+	}
+
+	var privateKey, clientCert, caCert []byte
+	var err error
+	if privateKey, err = ReadCustomCertFromFile(certsPath,"key.pem"); err != nil {
+		return err
+	}
+	if clientCert, err = ReadCustomCertFromFile(certsPath, "cert-chain.pem"); err != nil {
+		return err
+	}
+	if caCert, err = ReadCustomCertFromFile(certsPath, "root-cert.pem"); err != nil {
+		return err
+	}
+
+	kubeAccessor := ctx.Environment().(*kube.Environment).KubeClusters[0]
+	secret := &v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace.Name(),
+		},
+		Data: map[string][]byte{
+			"key.pem":     privateKey,
+			"cert.pem":    clientCert,
+			"ca.pem":      caCert,
+		},
+	}
+
+	_, err = kubeAccessor.CoreV1().Secrets(namespace.Name()).Create(context.TODO(), secret, metav1.CreateOptions{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ReadCustomCertFromFile(certsPath string, f string) ([]byte, error) {
+	b, err := ioutil.ReadFile(path.Join(env.IstioSrc, certsPath, f))
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
---
+++ tests/integration/security/file_mounted_certs/p2p_mtls_test.go
@@ -0,0 +1,397 @@
+// +build integ
+//  Copyright Istio Authors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package filemountedcerts
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"path"
+	"reflect"
+	"strings"
+	"testing"
+	"time"
+
+	dto "github.com/prometheus/client_model/go"
+	"istio.io/istio/pkg/config/protocol"
+	"istio.io/istio/pkg/test/echo/common/response"
+	"istio.io/istio/pkg/test/echo/common/scheme"
+	"istio.io/istio/pkg/test/env"
+	"istio.io/istio/pkg/test/framework"
+	"istio.io/istio/pkg/test/framework/components/echo"
+	"istio.io/istio/pkg/test/framework/components/echo/echoboot"
+	"istio.io/istio/pkg/test/framework/components/namespace"
+	"istio.io/istio/pkg/test/framework/resource"
+	"istio.io/istio/pkg/test/util/retry"
+	"istio.io/istio/pkg/test/util/structpath"
+)
+
+
+
+var (
+	vmEnv     map[string]string
+)
+
+
+
+func mustReadCert(t *testing.T, f string) string {
+	b, err := ioutil.ReadFile(path.Join(env.IstioSrc, "tests/testdata/certs/dns", f))
+	if err != nil {
+		t.Fatalf("failed to read %v: %v", f, err)
+	}
+	return string(b)
+}
+
+func TestClientToServiceTls(t *testing.T) {
+	framework.NewTest(t).
+		Features("security.peer.file-mounted-certs").
+		Run(func(ctx framework.TestContext) {
+
+			echoClient, echoServer, serviceNamespace := setupEcho(t, ctx)
+
+			bufDestinationRule := createObject(t, ctx, serviceNamespace.Name(), DestinationRuleConfigMutual)
+			defer ctx.Config().DeleteYAMLOrFail(ctx, serviceNamespace.Name(), bufDestinationRule.String())
+
+			bufVirtualService := createObject(t, ctx,serviceNamespace.Name(), VirtualServiceConfigMutual)
+			defer ctx.Config().DeleteYAMLOrFail(ctx, serviceNamespace.Name(), bufVirtualService.String())
+
+			bufPeerAuthentication := createObject(t, ctx, "istio-system", PeerAuthenticationConfig)
+			defer ctx.Config().DeleteYAMLOrFail(ctx, "istio-system", bufPeerAuthentication.String())
+
+			bufEnvoyFilter := createObject(t, ctx, serviceNamespace.Name(), EnvoyFilterConfig)
+			defer ctx.Config().DeleteYAMLOrFail(ctx, serviceNamespace.Name(), bufEnvoyFilter.String())
+
+			retry.UntilSuccessOrFail(t, func() error {
+				resp, err := echoClient.Call(echo.CallOptions{
+					Target:   echoServer,
+					PortName: "http",
+					Scheme:   scheme.HTTP,
+				})
+
+				if err != nil {
+					return fmt.Errorf("request failed: %v", err)
+				}
+
+				codes := make([]string, 0, len(resp))
+				for _, r := range resp {
+					codes = append(codes, r.Code)
+				}
+				if !reflect.DeepEqual(codes, []string{response.StatusCodeOK}) {
+					return fmt.Errorf("got codes %q, expected %q", codes, []string{response.StatusCodeOK})
+				}
+				for _, r := range resp {
+					if _, f := r.RawResponse["X-Forwarded-Client-Cert"]; !f {
+						return fmt.Errorf("expected to be see XFCC header, but none found. response: %+v", r.RawResponse)
+					}
+				}
+				return nil
+			}, retry.Delay(5*time.Second), retry.Timeout(1*time.Minute))
+
+			verifyUsingFileBasedCerts(t, echoClient, echoServer)
+			verifyStats(t, echoClient, echoServer)
+	})
+}
+
+const (
+	DestinationRuleConfigMutual = `
+apiVersion: networking.istio.io/v1alpha3
+kind: DestinationRule
+metadata:
+  name: server
+  namespace: {{.AppNamespace}}
+spec:
+  host: "server.{{.AppNamespace}}.svc.cluster.local"
+  trafficPolicy:
+    tls:
+      mode: MUTUAL
+      sni: server.istio-fd-sds-1-94047.svc.cluster.local
+      caCertificates: /client-certs/ca.pem
+      clientCertificate: /client-certs/cert.pem
+      privateKey: /client-certs/key.pem
+      subjectAltNames:
+        - server.default.svc
+
+`
+	VirtualServiceConfigMutual = `
+apiVersion: networking.istio.io/v1beta1
+kind: VirtualService
+metadata:
+  name: server
+  namespace: {{.AppNamespace}}
+spec:
+  hosts:
+  - server.{{.AppNamespace}}.svc.cluster.local
+  http:
+  - retries:
+      attempts: 5
+      retryOn: cancelled,connect-failure,gateway-error,refused-stream,resource-exhausted,unavailable,retriable-status-codes
+    route:
+    - destination:
+        host: server.{{.AppNamespace}}.svc.cluster.local
+    timeout: 15s
+`
+
+
+	PeerAuthenticationConfig = `
+apiVersion: security.istio.io/v1beta1
+kind: PeerAuthentication
+metadata:
+  name: default
+  namespace: "istio-system"
+spec:
+  mtls:
+    mode: STRICT
+`
+
+	EnvoyFilterConfig = `
+apiVersion: networking.istio.io/v1alpha3
+kind: EnvoyFilter
+metadata:
+  name: test
+  namespace: {{.AppNamespace}}
+spec:
+  workloadSelector:
+    labels:
+      app: server
+  configPatches:
+  - applyTo: HTTP_FILTER
+    match:
+      context: SIDECAR_INBOUND
+      listener:
+        filterChain:
+          filter:
+            name: "envoy.filters.network.http_connection_manager"
+            subFilter:
+              name: "istio_authn"
+    patch:
+      operation: REMOVE
+`
+)
+
+
+func createObject(t *testing.T, ctx framework.TestContext, serviceNamespace string, yamlManifest string) bytes.Buffer {
+	var destinationRuleToParse string
+	destinationRuleToParse = DestinationRuleConfigMutual
+
+	tmpl, err := template.New("DestinationRule").Parse(destinationRuleToParse)
+	if err != nil {
+		t.Errorf("failed to create template: %v", err)
+	}
+
+	var buf bytes.Buffer
+	if err := tmpl.Execute(&buf, map[string]string{"AppNamespace": serviceNamespace}); err != nil {
+		t.Fatalf("failed to create template: %v", err)
+	}
+
+	ctx.Config().ApplyYAMLOrFail(ctx, serviceNamespace, buf.String())
+
+	return buf
+}
+
+// setupEcho creates an `istio-fd-sds` namespace and brings up two echo instances server and
+// client in that namespace.
+func setupEcho(t *testing.T, ctx resource.Context) (echo.Instance, echo.Instance, namespace.Instance) {
+
+	appsNamespace := namespace.NewOrFail(t, ctx, namespace.Config{
+		Prefix: "istio-fd-sds",
+		Inject: true,
+	})
+
+	credNameGeneric := "test-cred"
+	CreateCustomSecret(ctx, credNameGeneric, appsNamespace, "")
+
+	var internalClient, internalServer echo.Instance
+
+	echoboot.NewBuilder(ctx).
+		With(&internalClient, echo.Config{
+			Service:   "client",
+			Namespace: appsNamespace,
+			Ports:     []echo.Port{},
+			Subsets: []echo.SubsetConfig{{
+				Version: "v1",
+				// Set up custom annotations to mount the certs.
+				Annotations: echo.NewAnnotations().
+					Set(echo.SidecarVolume, `{"server-certs":{"secret":{"secretName":"test-cred"}},"client-certs":{"secret":{"secretName":"test-cred"}}}`).
+					Set(echo.SidecarVolumeMount, `{"server-certs":{"mountPath":"/server-certs"},"client-certs":{"mountPath":"/client-certs"}}`).
+					Set(echo.SidecarConfig, `{"controlPlaneAuthPolicy":"NONE","proxyMetadata":` + strings.Replace(ProxyMetadataJson, "\n", "", -1) + `}`),
+			}},
+		}).
+		With(&internalServer, echo.Config{
+			Service:   "server",
+			Namespace: appsNamespace,
+			Ports: []echo.Port{
+				{
+					Name:         "http",
+					Protocol:     protocol.HTTP,
+					ServicePort:  8443,
+					InstancePort: 8443,
+					TLS:          false,
+				},
+			},
+			Subsets: []echo.SubsetConfig{{
+				Version:     "v1",
+				// Set up custom annotations to mount the certs.
+				Annotations: echo.NewAnnotations().
+					Set(echo.SidecarVolume, `{"server-certs":{"secret":{"secretName":"test-cred"}},"client-certs":{"secret":{"secretName":"test-cred"}}}`).
+					Set(echo.SidecarVolumeMount, `{"server-certs":{"mountPath":"/server-certs"},"client-certs":{"mountPath":"/client-certs"}}`).
+					Set(echo.SidecarConfig, `{"controlPlaneAuthPolicy":"NONE","proxyMetadata":` + strings.Replace(ProxyMetadataJson, "\n", "", -1) + `}`),
+			}},
+		}).
+		BuildOrFail(t)
+
+	return internalClient, internalServer, appsNamespace
+}
+
+
+func clusterName(target echo.Instance, port echo.Port) string {
+	cfg := target.Config()
+	return fmt.Sprintf("outbound|%d||%s.%s.svc.%s", port.ServicePort, cfg.Service, cfg.Namespace.Name(), cfg.Domain)
+}
+
+
+func getMetricForCluster(metrics []*dto.Metric, clusterName string) *dto.Metric {
+	for _, metric := range metrics {
+		for _, label := range metric.Label {
+			if (*label.Name == "cluster_name" && *label.Value == clusterName) {
+				return metric
+			}
+		}
+	}
+	return nil
+}
+
+func getMetricForListener(metrics []*dto.Metric, listenerName string) *dto.Metric {
+	for _, metric := range metrics {
+		for _, label := range metric.Label {
+			if (*label.Name == "listener_address" && *label.Value == listenerName) {
+				return metric
+			}
+		}
+	}
+	return nil
+}
+
+
+func verifyUsingFileBasedCerts(t *testing.T, client echo.Instance, server echo.Instance) {
+	// Verify client side configs
+	clientWorkloads, _ := client.Workloads()
+	if clientWorkloads[0].Sidecar() == nil {
+		t.Fatalf("Sidcecar is expected, but was not found")
+	}
+
+	configDump, err := clientWorkloads[0].Sidecar().Config()
+	if err != nil {
+		t.Fatalf("Unable to retrieve config_dump from the client. %v", err)
+	}
+
+	clusterName := clusterName(server, echo.Port{ServicePort: 8443})
+
+	validator := structpath.ForProto(configDump)
+	destCluster := validator.Select("{.configs[*].dynamicActiveClusters[?(@.cluster.name == '%s')]}", clusterName)
+	// Ensure that the Destination/Server cluster is using file based tls configs
+	err = destCluster.Exists("{.cluster.transportSocket.typedConfig.commonTlsContext.tlsCertificateSdsSecretConfigs[?(@.name == 'file-cert:/client-certs/cert.pem~/client-certs/key.pem')]}}").Check()
+	if err != nil {
+		t.Fatalf("Destination cluster on the Client side is expected to use FileBased SDS configs. %v", err)
+	}
+
+
+	// Verify server side configs
+	serverWorkloads, _ := server.Workloads()
+	if serverWorkloads[0].Sidecar() == nil {
+		t.Fatalf("Sidcecar is expected, but was not found")
+	}
+
+	configDump, err = serverWorkloads[0].Sidecar().Config()
+	if err != nil {
+		t.Fatalf("Unable to retrieve config_dump from the server. %v", err)
+	}
+
+	validator = structpath.ForProto(configDump)
+	virtualInboundListener := validator.Select("{.configs[*].dynamicListeners[?(@.name == 'virtualInbound')]}")
+	err = virtualInboundListener.Select("{.activeState.listener.filterChains[*].transportSocket.typedConfig.commonTlsContext.tlsCertificateSdsSecretConfigs[?(@.name == 'file-cert:/server-certs/cert.pem~/server-certs/key.pem')]}").Check()
+	if err != nil {
+		t.Fatalf("Server is expected to use FileBased SDS configs with DownstreamTlsContext. %v", err)
+	}
+}
+
+func verifyStats(t *testing.T, client echo.Instance, server echo.Instance) {
+	// Verify client side stats
+	clientWorkloads, _ := client.Workloads()
+	if clientWorkloads[0].Sidecar() == nil {
+		t.Fatalf("Sidcecar is expected, but was not found")
+	}
+
+	stats := clientWorkloads[0].Sidecar().StatsOrFail(t)
+
+	clusterName := clusterName(server, echo.Port{ServicePort: 8443})
+	var metric *dto.Metric
+
+	clientTestCases := []struct {
+		metricName string
+		expectedValue float64
+	}{
+		{
+			metricName: "envoy_cluster_ssl_handshake",
+			expectedValue: 1.0,
+		},
+		{
+			metricName: "envoy_cluster_upstream_rq_200",
+			expectedValue: 1.0,
+		},
+	}
+
+	for _, tc := range clientTestCases {
+		metric = getMetricForCluster(stats[tc.metricName].Metric, clusterName)
+		if *metric.Counter.Value < tc.expectedValue {
+			t.Fatalf("%s expected, but not found. Expected - %v, Got - %v", tc.metricName,  tc.expectedValue, metric.Counter.Value)
+		}
+	}
+
+
+	// Verify server side stats
+	serverWorkloads, _ := server.Workloads()
+	if serverWorkloads[0].Sidecar() == nil {
+		t.Fatalf("Sidcecar is expected, but was not found")
+	}
+
+	stats = serverWorkloads[0].Sidecar().StatsOrFail(t)
+
+	serverTestCases := []struct {
+		metricName string
+		expectedValue float64
+	}{
+		{
+			metricName: "envoy_listener_ssl_handshake",
+			expectedValue: 1.0,
+		},
+		{
+			metricName: "envoy_listener_ssl_versions_TLSv1_2",
+			expectedValue: 1.0,
+		},
+		{
+			metricName: "envoy_listener_http_inbound_0_0_0_0_8443_downstream_rq_completed",
+			expectedValue: 1.0,
+		},
+	}
+
+	for _, tc := range serverTestCases {
+		metric = getMetricForListener(stats[tc.metricName].Metric, "0.0.0.0_15006") // Check metrics on VirtualInbound listener
+		if *metric.Counter.Value < tc.expectedValue {
+			t.Fatalf("%s expected, but not found. Expected - %v, Got - %v", tc.metricName,  tc.expectedValue, metric.Counter.Value)
+		}
+	}
+}
