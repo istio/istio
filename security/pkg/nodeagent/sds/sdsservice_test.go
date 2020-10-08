c237c233e4439b978633e915889b121c240b1ee9
williamaronli <fengxiangli@google.com>
2020-10-08 13:50:01 -0700

    verify empty token
    Change-Id: Ife54e65c57b0f4e0caf85c128c088a28e2f3b67f

--- security/pkg/nodeagent/sds/sdsservice_test.go
+++ security/pkg/nodeagent/sds/sdsservice_test.go
@@ -422,7 +422,7 @@ func verifyResponseForInvalidResourceNames(err error) bool {
 
 func verifyResponseForEmptyToken(err error) bool {
 	fmt.Printf("%v\n",err)
-	s := fmt.Sprintf("the token is empty the error is: [%s]", )
+	s := fmt.Sprintf("the token is empty the error is: [%s]", err)
 	return strings.Contains(err.Error(), s)
 }
 
