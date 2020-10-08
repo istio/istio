d863b497c113da01027e212ea48c5c6968d88d70
williamaronli <fengxiangli@google.com>
2020-10-08 14:09:07 -0700

    verify empty token
    Change-Id: I5f451242eac733c62579bc2155c9247f2db285cc

--- security/pkg/nodeagent/sds/sdsservice_test.go
+++ security/pkg/nodeagent/sds/sdsservice_test.go
@@ -395,6 +395,8 @@ func sendRequestAndVerifyResponseWithCredentialFetcher(t *testing.T, cb secretCa
 		time.Sleep(wait)
 		// Try to call the server
 		resp, err := cb(socket, req)
+		t.Logf("%v",resp)
+		t.Logf("sssllllllsssllll")
 		if testEmptyToken {
 			if ok := verifyResponseForEmptyToken(err); ok {
 				return
