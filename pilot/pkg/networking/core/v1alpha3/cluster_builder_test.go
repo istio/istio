beeba3384a4eea06ce824ca6ca9324e8facf39c6
Pengyuan Bian <bianpengyuan@google.com>
2020-10-14 11:26:58 -0700

    Add cluster metadata for telemetry (#27823)
    * add cluster metadata for telemetry
    
    * fix lint
    
    * add outbound and fix inbound
    
    * fix
    
    * fix
    
    * fix test
    
    * make cluster telemetry struct with a function and add comment
    
    * fix
    
    * comment

--- pilot/pkg/networking/core/v1alpha3/cluster_builder_test.go
+++ pilot/pkg/networking/core/v1alpha3/cluster_builder_test.go
@@ -614,6 +614,31 @@ func TestBuildDefaultCluster(t *testing.T) {
 				CircuitBreakers: &cluster.CircuitBreakers{
 					Thresholds: []*cluster.CircuitBreakers_Thresholds{&defaultCircuitBreakerThresholds},
 				},
+				Metadata: &core.Metadata{
+					FilterMetadata: map[string]*structpb.Struct{
+						util.IstioMetadataKey: {Fields: map[string]*structpb.Value{
+							"services": {Kind: &structpb.Value_ListValue{ListValue: &structpb.ListValue{Values: []*structpb.Value{
+								{Kind: &structpb.Value_StructValue{StructValue: &structpb.Struct{Fields: map[string]*structpb.Value{
+									"host": {
+										Kind: &structpb.Value_StringValue{
+											StringValue: "host",
+										},
+									},
+									"name": {
+										Kind: &structpb.Value_StringValue{
+											StringValue: "svc",
+										},
+									},
+									"namespace": {
+										Kind: &structpb.Value_StringValue{
+											StringValue: "default",
+										},
+									},
+								}}}},
+							}}}},
+						}},
+					},
+				},
 			},
 		},
 		{
@@ -678,6 +703,31 @@ func TestBuildDefaultCluster(t *testing.T) {
 				CircuitBreakers: &cluster.CircuitBreakers{
 					Thresholds: []*cluster.CircuitBreakers_Thresholds{&defaultCircuitBreakerThresholds},
 				},
+				Metadata: &core.Metadata{
+					FilterMetadata: map[string]*structpb.Struct{
+						util.IstioMetadataKey: {Fields: map[string]*structpb.Value{
+							"services": {Kind: &structpb.Value_ListValue{ListValue: &structpb.ListValue{Values: []*structpb.Value{
+								{Kind: &structpb.Value_StructValue{StructValue: &structpb.Struct{Fields: map[string]*structpb.Value{
+									"host": {
+										Kind: &structpb.Value_StringValue{
+											StringValue: "host",
+										},
+									},
+									"name": {
+										Kind: &structpb.Value_StringValue{
+											StringValue: "svc",
+										},
+									},
+									"namespace": {
+										Kind: &structpb.Value_StringValue{
+											StringValue: "default",
+										},
+									},
+								}}}},
+							}}}},
+						}},
+					},
+				},
 			},
 		},
 	}
@@ -688,7 +738,8 @@ func TestBuildDefaultCluster(t *testing.T) {
 			cb := NewClusterBuilder(cg.SetupProxy(nil), cg.PushContext())
 
 			defaultCluster := cb.buildDefaultCluster(tt.clusterName, tt.discovery,
-				tt.endpoints, tt.direction, servicePort, tt.external)
+				tt.endpoints, tt.direction, servicePort,
+				&model.Service{Hostname: "host", MeshExternal: false, Attributes: model.ServiceAttributes{Name: "svc", Namespace: "default"}})
 
 			if diff := cmp.Diff(defaultCluster, tt.expectedCluster, protocmp.Transform()); diff != "" {
 				t.Errorf("Unexpected default cluster, diff: %v", diff)
