6166065f7eef4effc787aa24c8f8d0f277c324d8
Steven Landow <landow@google.com>
2020-10-17 19:33:20 -0700

    lookup cm by name

--- pkg/kube/meta.go
+++ pkg/kube/meta.go
@@ -2,8 +2,8 @@ package kube
 
 import (
 	"context"
-
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 
 	"istio.io/pkg/log"
@@ -16,21 +16,12 @@ type ClusterMeta struct {
 
 // ClusterMetaFromConfigMap attempts to load the istio multicluster config to get overrides for cluster and network names.
 func ClusterMetaFromConfigMap(client kubernetes.Interface, namespace string) *ClusterMeta {
-	// TODO fix circular import for label const
 	log.Infof("looking for istio-cluster vm in namespace %s", namespace)
-	res, err := client.CoreV1().ConfigMaps(namespace).List(context.TODO(), v1.ListOptions{LabelSelector: "istio/multiCluster=true"})
+	cm, err := client.CoreV1().ConfigMaps(namespace).Get(context.TODO(), "istio-cluster", metaV1.GetOptions{})
 	if err != nil {
-		log.Errorf("failed fetching cluster meta configmap: %v", err)
+		log.Errorf("error fetching istio-cluster configmap: %v", err)
 		return nil
 	}
-	if len(res.Items) == 0 {
-		log.Errorf("no matching configmap: %v", err)
-		return nil
-	}
-	if len(res.Items) > 1 {
-		log.Warnf("multiple ConfigMaps with istio/multiCluster=true; using %s", res.Items[0].Name)
-	}
-	cm := res.Items[0]
 
 	return &ClusterMeta{ID: cm.Data["cluster"], Network: cm.Data["network"]}
 }
