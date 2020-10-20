24cd75ab98cadf4e8e6248cb10bae93ca252860b
Steven Landow <landow@google.com>
2020-10-20 10:26:23 -0700

    fix multicluster vm test

--- tests/integration/pilot/common/apps.go
+++ tests/integration/pilot/common/apps.go
@@ -188,7 +188,7 @@ func SetupApps(ctx resource.Context, i istio.Instance, apps *EchoDeployments) er
 			Namespace:      apps.Namespace,
 			Ports:          EchoPorts,
 			DeployAsVM:     true,
-			AutoRegisterVM: true,
+			AutoRegisterVM: false, // TODO support auto-registration with multi-primary
 			Subsets:        []echo.SubsetConfig{{}},
 			Cluster:        c[0],
 		})
