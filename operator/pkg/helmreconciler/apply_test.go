f3a8c20d96fb30f60c2568c53e67f29bf6e4f521
Xinnan Wen <iamwen@google.com>
2020-10-20 14:12:42 -0700

    resolve conflict

--- operator/pkg/helmreconciler/apply_test.go
+++ operator/pkg/helmreconciler/apply_test.go
@@ -23,8 +23,8 @@ import (
 
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
