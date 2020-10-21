4632951768fb8e3ea74aa6bc5f8b42c153b7fff3
Chi Zhang <chizhg@google.com>
2020-10-21 05:00:34 +0000

    Add a helper bash function to upgrade GKE clusters to a given version
    Change-Id: I73854de20820850960309cde25eb50ccc9962d35

--- prow/asm/lib.sh
+++ prow/asm/lib.sh
@@ -92,6 +92,26 @@ function boskos_release() {
   boskos_cmd release --name "${resource_name}" --target-state dirty
 }
 
+# Upgrade the GKE clusters to a given version.
+# Parameters: $1 - the Kubernetes release version to which to upgrade the cluster
+#             $2 - array of k8s contexts
+function upgrade_gke_clusters() {
+  local target_cluster_version=$1; shift
+  local contexts=("${@}")
+
+  for i in "${!contexts[@]}"; do
+    IFS="_" read -r -a vals <<< "${contexts[$i]}"
+    local project_id="${vals[1]}"
+    local region="${vals[2]}"
+    local cluster="${vals[3]}"
+    # Run the gcloud upgrade command in the background, for now only upgrade GKE master.
+    gcloud container clusters upgrade "${cluster}" --cluster-version="${target_cluster_version}" \
+      --project="${project_id}" --region="${region}" --master --quiet &
+  done
+  # Wait until all the upgrades finishes.
+  wait
+}
+
 #####################################################################
 #                    Functions used for ASM                         #
 #####################################################################
