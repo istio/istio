6b728c6e64126076977077ffbc4714ce2f9ace90
Marco Li <liwenhao@google.com>
2020-10-25 22:05:36 -0400

    refactor based on comments

--- tests/integration/security/ca_custom_root/trust_domain_alias_secure_naming_test.go
+++ tests/integration/security/ca_custom_root/trust_domain_alias_secure_naming_test.go
@@ -42,24 +42,33 @@ spec:
 `
 )
 
-// TestTrustDomainAliasClient scope:
+// TestTrustDomainAliasSecureNaming scope:
 // The client side mTLS connection should validate the trust domain alias during secure naming validation.
-// - When server side has trust domain which defined as trust domain alias, the mTLS connection should expect success
-// - When server side has trust domain which not defined as trust domain alias, the mTLS connection should expect fail
-func TestTrustDomainAliasClient(t *testing.T) {
+//
+// Setup:
+// 1. Setup Istio with custom CA cert, to make sure all workloads have same root cert because of #3
+// 2. One client workload with istio-proxy sidecar injected
+// 3. Two naked server workloads with custom certs in two different trust domain reflected in the SAN (spiffe)
+// 4. PeerAuthentication with strict mtls, and DestinaitonRule to the server workloads with tls ISTIO_MUTUAL mode
+// 5. MeshConfig.TrustDomainAliases contains one of the trust domain in #3
+//
+// Expectation:
+// 1. When the trust domain of server TLS certificate is in the list of MeshConfig.TrustDomainAliases,
+//    the connection from client to server should succeed.
+// 2. When the trust domain of server TLS certificate is not in the list of MeshConfig.TrustDomainAliases,
+//    then the connection from client to server should fail.
+func TestTrustDomainAliasSecureNaming(t *testing.T) {
 	framework.NewTest(t).
-		Features("security.peer.trust-domain-validation-alias-client").
+		Features("security.peer.trust-domain-alias-secure-naming").
 		Run(func(ctx framework.TestContext) {
 			testNS := namespace.NewOrFail(t, ctx, namespace.Config{
-				Prefix: "trust-domain-alias-client-validation",
+				Prefix: "trust-domain-alias",
 				Inject: true,
 			})
 
 			// Create testing certs using runtime namespace
-			//generateCerts(ctx, testNS.Name())
-			generateCerts(ctx, testNS.Name(), "foo", "server-naked-foo")
-			generateCerts(ctx, testNS.Name(), "bar", "server-naked-bar")
-			defer cleanCerts(ctx)
+			cleanup := generateCerts(ctx, testNS.Name())
+			defer cleanup()
 
 			// Deploy 3 workloads:
 			// client: echo app with istio-proxy sidecar injected, holds default trust domain cluster.local
@@ -123,11 +132,11 @@ func TestTrustDomainAliasClient(t *testing.T) {
 
 			ctx.Config().ApplyYAMLOrFail(ctx, testNS.Name(), fmt.Sprintf(POLICY))
 
-			verify := func(t *testing.T, src echo.Instance, dest echo.Instance, s scheme.Instance, allow bool) {
+			verify := func(t *testing.T, src echo.Instance, dest echo.Instance, s scheme.Instance, success bool) {
 				t.Helper()
-				want := "allow"
-				if !allow {
-					want = "deny"
+				want := "success"
+				if !success {
+					want = "fail"
 				}
 				name := fmt.Sprintf("server:%s[%s]", dest.Config().Service, want)
 				t.Run(name, func(t *testing.T) {
@@ -141,14 +150,32 @@ func TestTrustDomainAliasClient(t *testing.T) {
 					checker := connection.Checker{
 						From:          src,
 						Options:       opt,
-						ExpectSuccess: allow,
+						ExpectSuccess: success,
 					}
 					checker.CheckOrFail(ctx)
 				})
 			}
 
-			verify(t, client, serverNakedFoo, scheme.HTTP, true)
-			verify(t, client, serverNakedBar, scheme.HTTP, false)
+			cases := []struct {
+				src echo.Instance
+				dest echo.Instance
+				expect bool
+			}{
+				{
+					src: client,
+					dest: serverNakedFoo,
+					expect: true,
+				},
+				{
+					src: client,
+					dest: serverNakedBar,
+					expect: false,
+				},
+			}
+
+			for _, tc := range cases {
+				verify(t, tc.src, tc.dest, scheme.HTTP, tc.expect)
+			}
 		})
 }
 
@@ -160,23 +187,40 @@ func loadCert(t test.Failer, name string) string {
 	return string(data)
 }
 
-func generateCerts(ctx framework.TestContext, ns string, td string, sa string) {
+func generateCerts(t test.Failer, ns string) func() {
 	workDir := path.Join(env.IstioSrc, "samples/certs")
 	script := path.Join(workDir, "generate-workload.sh")
-	command := exec.Cmd{
-		Path:   script,
-		Args:   []string{script, td, ns, sa, workDir, "tmp"},
-		Stdout: os.Stdout,
-		Stderr: os.Stdout,
-	}
-	if err := command.Run(); err != nil {
-		ctx.Logf("Failed to create testing certificates: %s", err)
-	}
-}
 
-func cleanCerts(ctx framework.TestContext) {
-	err := os.RemoveAll(path.Join(env.IstioSrc, "samples/certs/tmp"))
-	if err != nil {
-		ctx.Logf("Failed to clean testing certificates: %s", err)
+	crts := []struct {
+		td string
+		sa string
+	}{
+		{
+			td: "foo",
+			sa: "server-naked-foo",
+		},
+		{
+			td: "bar",
+			sa: "server-naked-bar",
+		},
+	}
+
+	for _, crt := range crts {
+		command := exec.Cmd{
+			Path:   script,
+			Args:   []string{script, crt.td, ns, crt.sa, workDir, "tmp"},
+			Stdout: os.Stdout,
+			Stderr: os.Stdout,
+		}
+		if err := command.Run(); err != nil {
+			t.Fatal("Failed to create testing certificates: %s", err)
+		}
+	}
+
+	return func() {
+		err := os.RemoveAll(path.Join(env.IstioSrc, "samples/certs/tmp"))
+		if err != nil {
+			t.Fatal("Failed to clean testing certificates: %s", err)
+		}
 	}
 }
