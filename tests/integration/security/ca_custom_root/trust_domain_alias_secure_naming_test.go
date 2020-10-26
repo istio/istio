9be98ecbafedc725838570c1a3f1fba2cc5d82ce
Marco Li <liwenhao@google.com>
2020-10-26 17:35:43 -0400

    add build constraints

--- tests/integration/security/ca_custom_root/trust_domain_alias_secure_naming_test.go
+++ tests/integration/security/ca_custom_root/trust_domain_alias_secure_naming_test.go
@@ -1,3 +1,18 @@
+// +build integ
+// Copyright Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package cacustomroot
 
 import (
@@ -158,18 +173,18 @@ func TestTrustDomainAliasSecureNaming(t *testing.T) {
 			}
 
 			cases := []struct {
-				src echo.Instance
-				dest echo.Instance
+				src    echo.Instance
+				dest   echo.Instance
 				expect bool
 			}{
 				{
-					src: client,
-					dest: serverNakedFoo,
+					src:    client,
+					dest:   serverNakedFoo,
 					expect: true,
 				},
 				{
-					src: client,
-					dest: serverNakedBar,
+					src:    client,
+					dest:   serverNakedBar,
 					expect: false,
 				},
 			}
