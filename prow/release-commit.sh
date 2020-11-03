c0d560cd9e307467d9ef2cbff3d36ed9059af55d
Ram Vennam <rvennam@us.ibm.com>
2020-11-02 20:50:19 -0500

    update release sha (#28470)

--- prow/release-commit.sh
+++ prow/release-commit.sh
@@ -32,7 +32,7 @@ DOCKER_HUB=${DOCKER_HUB:-gcr.io/istio-testing}
 GCS_BUCKET=${GCS_BUCKET:-istio-build/dev}
 
 # Use a pinned version in case breaking changes are needed
-BUILDER_SHA=5293d6348817781b890d368476a008de7cadce34
+BUILDER_SHA=c05aea345076f3a14bd10ee69d91b4c5f83d129b
 
 # Reference to the next minor version of Istio
 # This will create a version like 1.4-alpha.sha
