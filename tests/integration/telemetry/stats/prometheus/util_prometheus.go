d8445769cafbc29186df1c0b5da98a1ff15faa2d
gargnupur <gargnupur@google.com>
2020-11-04 13:51:02 -0800

    fix build fail
    Signed-off-by: gargnupur <gargnupur@google.com>

--- tests/integration/telemetry/stats/prometheus/util_prometheus.go
+++ tests/integration/telemetry/stats/prometheus/util_prometheus.go
@@ -28,7 +28,7 @@ import (
 )
 
 // QueryPrometheus queries prometheus and returns the result once the query stabilizes
-func QueryPrometheus(t *testing.T, cluster resource.Cluster, query string, promInst prometheus.Instance) (string, error) {
+func QueryPrometheus(t *testing.T, cluster resource.Cluster, query string, promInst prometheus.Instance) error {
 	t.Logf("query prometheus with: %v", query)
 	val, err := promInst.WaitForQuiesceForCluster(cluster, query)
 	if err != nil {
