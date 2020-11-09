e2a425427b5df5965e2cf175020667fe9607871f
John Howard <howardjohn@google.com>
2020-11-09 08:52:18 -0800

    Remove staticinit gen (#28692)
    I am not sure what changed between this being added and now, but I am
    fairly sure this is not needed know, as its not possible to import
    collections without importing the packages.
    
    This is not strictly neccesary, but allows splitting the kubernetes
    imports out for https://github.com/istio/istio/issues/26232 much more
    easily.

--- galley/pkg/config/testing/basicmeta/generate.go
+++ galley/pkg/config/testing/basicmeta/generate.go
@@ -17,11 +17,7 @@ package basicmeta
 // Embed the core metadata file containing the collections as a resource
 //go:generate go-bindata --nocompress --nometadata --pkg basicmeta -o basicmeta.gen.go basicmeta.yaml basicmeta2.yaml
 
-// Create static initializers file
-//go:generate go run $REPO_ROOT/pkg/config/schema/codegen/tools/staticinit.main.go basicmeta basicmeta.yaml staticinit.gen.go
-
 // Create collection constants
 //go:generate go run $REPO_ROOT/pkg/config/schema/codegen/tools/collections.main.go basicmeta basicmeta.yaml collections.gen.go
 
 //go:generate goimports -w -local istio.io "$REPO_ROOT/galley/pkg/config/testing/basicmeta/collections.gen.go"
-//go:generate goimports -w -local istio.io "$REPO_ROOT/galley/pkg/config/testing/basicmeta/staticinit.gen.go"
