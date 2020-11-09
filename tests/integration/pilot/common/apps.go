7511d5177b0af81a3307667fde851980128ec997
Rui Gu <ruigu@google.com>
2020-11-09 15:26:52 -0800

    Skip VM related parts for TestTraffic
    1. Add test input config argument to disable VM related parts in
    TestTraffic.
    2. Skip all the VM related parts based on the input argument.

--- tests/integration/pilot/common/apps.go
+++ tests/integration/pilot/common/apps.go
@@ -181,17 +181,18 @@ func SetupApps(ctx resource.Context, i istio.Instance, apps *EchoDeployments) er
 				Cluster: c,
 			})
 	}
-
-	for _, c := range ctx.Clusters().ByNetwork() {
-		builder.With(nil, echo.Config{
-			Service:        VMSvc,
-			Namespace:      apps.Namespace,
-			Ports:          EchoPorts,
-			DeployAsVM:     true,
-			AutoRegisterVM: false, // TODO support auto-registration with multi-primary
-			Subsets:        []echo.SubsetConfig{{}},
-			Cluster:        c[0],
-		})
+	if !ctx.Settings().SkipVM {
+		for _, c := range ctx.Clusters().ByNetwork() {
+			builder.With(nil, echo.Config{
+				Service:        VMSvc,
+				Namespace:      apps.Namespace,
+				Ports:          EchoPorts,
+				DeployAsVM:     true,
+				AutoRegisterVM: false, // TODO support auto-registration with multi-primary
+				Subsets:        []echo.SubsetConfig{{}},
+				Cluster:        c[0],
+			})
+		}
 	}
 
 	echos, err := builder.Build()
@@ -205,7 +206,9 @@ func SetupApps(ctx resource.Context, i istio.Instance, apps *EchoDeployments) er
 	apps.Headless = echos.Match(echo.Service(HeadlessSvc))
 	apps.Naked = echos.Match(echo.Service(NakedSvc))
 	apps.External = echos.Match(echo.Service(ExternalSvc))
-	apps.VM = echos.Match(echo.Service(VMSvc))
+	if !ctx.Settings().SkipVM {
+		apps.VM = echos.Match(echo.Service(VMSvc))
+	}
 
 	if err := ctx.Config().ApplyYAML(apps.Namespace.Name(), `
 apiVersion: networking.istio.io/v1alpha3
