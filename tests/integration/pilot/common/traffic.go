6f0704ba2bbf2e9c1071466e9105430d0995cadc
Rui Gu <ruigu@google.com>
2020-11-09 16:25:17 -0800

    Skip VM related parts for TestTraffic (#28729)
    1. Add test input config argument to disable VM related parts in
    TestTraffic.
    2. Skip all the VM related parts based on the input argument.

--- tests/integration/pilot/common/traffic.go
+++ tests/integration/pilot/common/traffic.go
@@ -91,7 +91,9 @@ func RunAllTrafficTests(ctx framework.TestContext, apps *EchoDeployments) {
 	cases["serverfirst"] = serverFirstTestCases(apps)
 	cases["gateway"] = gatewayCases(apps)
 	cases["loop"] = trafficLoopCases(apps)
-	cases["vm"] = VMTestCases(apps.VM, apps)
+	if !ctx.Settings().SkipVM {
+		cases["vm"] = VMTestCases(apps.VM, apps)
+	}
 	for name, tts := range cases {
 		ctx.NewSubTest(name).Run(func(ctx framework.TestContext) {
 			for _, tt := range tts {
