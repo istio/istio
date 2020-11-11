500dcf6a229d0570bbc37199c33ddce205dcf551
jtrbs <tairan@google.com>
2020-11-11 12:09:02 -0800

    convert TestMtlsStrictK8sCA into multicluster support (#28720)

--- tests/integration/security/util/framework.go
+++ tests/integration/security/util/framework.go
@@ -31,7 +31,6 @@ const (
 	DSvc             = "d"
 	ESvc             = "e"
 	FSvc             = "f"
-	XSvc             = "x"
 	MultiversionSvc  = "multiversion"
 	VMSvc            = "vm"
 	HeadlessSvc      = "headless"
@@ -45,16 +44,14 @@ const (
 type EchoDeployments struct {
 	// TODO: Consolidate the echo config and reduce/reuse echo instances (https://github.com/istio/istio/issues/28599)
 	// Namespace1 is used as the default namespace for reachability tests and other tests which share the same config
-	Namespace1 namespace.Instance
-	// Namespace2 is used for echos used by TestPassThroughFilterChain
-	Namespace2          namespace.Instance
-	A, B, C, D, E, F, X echo.Instances
-	Multiversion        echo.Instances
-	Headless            echo.Instances
-	Naked               echo.Instances
-	VM                  echo.Instances
-	HeadlessNaked       echo.Instances
-	All                 echo.Instances
+	Namespace1       namespace.Instance
+	A, B, C, D, E, F echo.Instances
+	Multiversion     echo.Instances
+	Headless         echo.Instances
+	Naked            echo.Instances
+	VM               echo.Instances
+	HeadlessNaked    echo.Instances
+	All              echo.Instances
 }
 
 func EchoConfig(name string, ns namespace.Instance, headless bool, annos echo.Annotations, cluster resource.Cluster) echo.Config {
@@ -85,6 +82,26 @@ func EchoConfig(name string, ns namespace.Instance, headless bool, annos echo.An
 				Protocol: protocol.GRPC,
 			},
 		},
+		// Workload Ports needed by TestPassThroughFilterChain
+		// The port 8085,8086,8087,8088 will be defined only in the workload and not in the k8s service.
+		WorkloadOnlyPorts: []echo.WorkloadPort{
+			{
+				Port:     8085,
+				Protocol: protocol.HTTP,
+			},
+			{
+				Port:     8086,
+				Protocol: protocol.HTTP,
+			},
+			{
+				Port:     8087,
+				Protocol: protocol.TCP,
+			},
+			{
+				Port:     8088,
+				Protocol: protocol.TCP,
+			},
+		},
 		Cluster: cluster,
 	}
 
@@ -100,13 +117,6 @@ func SetupApps(ctx resource.Context, i istio.Instance, apps *EchoDeployments, bu
 	var err error
 	apps.Namespace1, err = namespace.New(ctx, namespace.Config{
 		Prefix: "test-ns1",
-		Inject: true,
-	})
-	if err != nil {
-		return err
-	}
-	apps.Namespace2, err = namespace.New(ctx, namespace.Config{
-		Prefix: "test-ns2",
 		Inject: true,
 	})
 	if err != nil {
@@ -148,47 +158,6 @@ func SetupApps(ctx resource.Context, i istio.Instance, apps *EchoDeployments, bu
 		builder.With(nil, EchoConfig(HeadlessNakedSvc, apps.Namespace1, true, echo.NewAnnotations().
 			SetBool(echo.SidecarInject, false), c[0]))
 	}
-	newEchoConfigForTestPassThroughFilterChain := func(service string, ns namespace.Instance, cluster resource.Cluster) echo.Config {
-		return echo.Config{
-			Service:   service,
-			Namespace: ns,
-			Subsets:   []echo.SubsetConfig{{}},
-			Ports: []echo.Port{
-				{
-					Name:     "grpc",
-					Protocol: protocol.GRPC,
-				},
-			},
-			Cluster: cluster,
-			// The port 8085,8086,8087,8088 will be defined only in the workload and not in the k8s service.
-			WorkloadOnlyPorts: []echo.WorkloadPort{
-				{
-					Port:     8085,
-					Protocol: protocol.HTTP,
-				},
-				{
-					Port:     8086,
-					Protocol: protocol.HTTP,
-				},
-				{
-					Port:     8087,
-					Protocol: protocol.TCP,
-				},
-				{
-					Port:     8088,
-					Protocol: protocol.TCP,
-				},
-			},
-		}
-	}
-	for _, cluster := range ctx.Clusters() {
-		builder.
-			With(nil, newEchoConfigForTestPassThroughFilterChain(ASvc, apps.Namespace2, cluster)).
-			With(nil, newEchoConfigForTestPassThroughFilterChain(BSvc, apps.Namespace2, cluster)).
-			With(nil, newEchoConfigForTestPassThroughFilterChain(CSvc, apps.Namespace2, cluster)).
-			With(nil, newEchoConfigForTestPassThroughFilterChain(DSvc, apps.Namespace2, cluster)).
-			With(nil, newEchoConfigForTestPassThroughFilterChain(XSvc, apps.Namespace2, cluster))
-	}
 
 	echos, err := builder.Build()
 	if err != nil {
@@ -201,7 +170,6 @@ func SetupApps(ctx resource.Context, i istio.Instance, apps *EchoDeployments, bu
 	apps.D = echos.Match(echo.Service(DSvc))
 	apps.E = echos.Match(echo.Service(ESvc))
 	apps.F = echos.Match(echo.Service(FSvc))
-	apps.X = echos.Match(echo.Service(XSvc))
 	apps.Multiversion = echos.Match(echo.Service(MultiversionSvc))
 	apps.Headless = echos.Match(echo.Service(HeadlessSvc))
 	apps.Naked = echos.Match(echo.Service(NakedSvc))
