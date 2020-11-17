9debcf778c6a2c228aa6c16f32db6e43cab822b0
John Howard <howardjohn@google.com>
2020-11-16 17:42:02 -0800

    Improve speed of YAML apply/echo init (#28904)
    * Improve speed of YAML apply/echo init
    
    Currently every call to ApplyYAML will reinitialize discovery, which can
    take O(seconds) on slow clusters. This likely doesn't impact CI much, which
    runs in kind, but on GKE I saw echo deployment times go from 30s+ to 5s.
    
    * attempt to fix race
    
    * attempt deepcopy
    
    * fix nil
    
    * fix cpy
    
    * revert parallel

--- pkg/test/framework/components/echo/kube/instance.go
+++ pkg/test/framework/components/echo/kube/instance.go
@@ -62,7 +62,8 @@ type instance struct {
 	cluster   resource.Cluster
 }
 
-func newInstance(ctx resource.Context, cfg echo.Config) (out *instance, err error) {
+func newInstance(ctx resource.Context, originalCfg echo.Config) (out *instance, err error) {
+	cfg := originalCfg.DeepCopy()
 	// Fill in defaults for any missing values.
 	common.AddPortIfMissing(&cfg, protocol.GRPC)
 	if err = common.FillInDefaults(ctx, defaultDomain, &cfg); err != nil {
