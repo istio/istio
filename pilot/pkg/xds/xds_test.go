248108885b1457405f497ecafe40aac657166b6f
Weibo He <weibo.he@airbnb.com>
2020-12-01 17:45:20 -0800

    xds: fix broken unit test
    Fix broken unit test:
    https://prow.istio.io/view/gs/istio-prow/pr-logs/pull/istio_istio/29316/unit-tests_istio_release-1.7/1333945380173254656
    
    Change-Id: I23c283e4151ff78b64a5e41d79b740325aff20a7
    Reviewed-on: https://gerrit.musta.ch/c/public/istio/+/845
    Reviewed-by: Jungho Ahn <jungho.ahn@airbnb.com>
    Reviewed-by: Brian Wolfe <brian.wolfe@airbnb.com>

--- pilot/pkg/xds/xds_test.go
+++ pilot/pkg/xds/xds_test.go
@@ -221,7 +221,7 @@ func TestServiceScoping(t *testing.T) {
 		proxy := s.SetupProxy(p)
 
 		endpoints := ExtractClusterEndpoints(s.Clusters(proxy))
-		eps := endpoints["inbound|9080|custom-http|sidecar.app"]
+		eps := endpoints["inbound|9080||localhost"]
 		if !listEqualUnordered(eps, []string{"/var/run/someuds.sock"}) {
 			t.Fatalf("expected /var/run/someuds.sock, got %v", eps)
 		}
