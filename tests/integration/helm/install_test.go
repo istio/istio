6340485618d1eb548472b3470dbfc0501913938d
Jacob Delgado <jacob.delgado@volunteers.acasi.info>
2020-12-02 20:50:12 -0700

    Fix linting error

--- tests/integration/helm/install_test.go
+++ tests/integration/helm/install_test.go
@@ -24,7 +24,6 @@ import (
 	"testing"
 	"time"
 
-	"istio.io/pkg/log"
 	v1 "k8s.io/api/core/v1"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -39,6 +38,7 @@ import (
 	"istio.io/istio/pkg/test/scopes"
 	"istio.io/istio/pkg/test/util/retry"
 	"istio.io/istio/tests/util/sanitycheck"
+	"istio.io/pkg/log"
 )
 
 const (
