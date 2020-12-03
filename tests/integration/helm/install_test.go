3ef9e483d4ec831f78d1c02de5152bed8eb0ee8c
Jacob Delgado <jacob.delgado@volunteers.acasi.info>
2020-12-02 19:08:24 -0700

    Fix flaky integration test - helm/TestInstallWithFirstPartyJwt
    Attempt to fix https://github.com/istio/istio/issues/29167
    based on data from https://storage.googleapis.com/istio-prow/logs/integ-k8s-115_istio_postsubmit/1331309815770648576/build-log.txt
    
    There's reason to believe that the test doing a helm delete and an
    immediate helm install was causing issues between test runs. Post-merge
    this will still need to be monitored.
    
    * Add --timeout flag to helm install command with a default of
      2 minutes, up from 30s.
    * Delete the istio-system namespace in-between tests and wait for it to
      be removed before proceeding.

--- tests/integration/helm/install_test.go
+++ tests/integration/helm/install_test.go
@@ -24,6 +24,7 @@ import (
 	"testing"
 	"time"
 
+	"istio.io/pkg/log"
 	v1 "k8s.io/api/core/v1"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -38,7 +39,6 @@ import (
 	"istio.io/istio/pkg/test/scopes"
 	"istio.io/istio/pkg/test/util/retry"
 	"istio.io/istio/tests/util/sanitycheck"
-	"istio.io/pkg/log"
 )
 
 const (
@@ -56,6 +56,7 @@ const (
 	GatewayChartsDir    = "gateways"
 	retryDelay          = 2 * time.Second
 	retryTimeOut        = 5 * time.Minute
+	helmTimeout = 2 * time.Minute
 )
 
 var (
@@ -155,28 +156,28 @@ func installIstio(t *testing.T, ctx resource.Context, cs resource.Cluster,
 
 	// Install base chart
 	err := h.InstallChart(BaseReleaseName, BaseChart,
-		IstioNamespace, overrideValuesFile)
+		IstioNamespace, overrideValuesFile, helmTimeout)
 	if err != nil {
 		t.Errorf("failed to install istio %s chart", BaseChart)
 	}
 
 	// Install discovery chart
 	err = h.InstallChart(IstiodReleaseName, filepath.Join(ControlChartsDir, DiscoveryChart),
-		IstioNamespace, overrideValuesFile)
+		IstioNamespace, overrideValuesFile, helmTimeout)
 	if err != nil {
 		t.Errorf("failed to install istio %s chart", DiscoveryChart)
 	}
 
 	// Install ingress gateway chart
 	err = h.InstallChart(IngressReleaseName, filepath.Join(GatewayChartsDir, IngressGatewayChart),
-		IstioNamespace, overrideValuesFile)
+		IstioNamespace, overrideValuesFile, helmTimeout)
 	if err != nil {
 		t.Errorf("failed to install istio %s chart", IngressGatewayChart)
 	}
 
 	// Install egress gateway chart
 	err = h.InstallChart(EgressReleaseName, filepath.Join(GatewayChartsDir, EgressGatewayChart),
-		IstioNamespace, overrideValuesFile)
+		IstioNamespace, overrideValuesFile, helmTimeout)
 	if err != nil {
 		t.Errorf("failed to install istio %s chart", EgressGatewayChart)
 	}
@@ -196,6 +197,12 @@ func deleteIstio(t *testing.T, cs resource.Cluster, h *helm.Helm) {
 	}
 	if err := h.DeleteChart(BaseReleaseName, IstioNamespace); err != nil {
 		t.Errorf("failed to delete %s release", BaseReleaseName)
+	}
+	if err := cs.CoreV1().Namespaces().Delete(context.TODO(), IstioNamespace, metav1.DeleteOptions{}); err != nil {
+		t.Errorf("failed to delete istio namespace: %v", err)
+	}
+	if err := kubetest.WaitForNamespaceDeletion(cs, IstioNamespace, retry.Timeout(retryTimeOut)); err != nil {
+		t.Errorf("wating for istio namespace to be deleted: %v", err)
 	}
 }
 
