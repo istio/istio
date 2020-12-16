ea3b7cf1c20eceeeb7d6afc0fa334ab88c9d7597
carolynhu <carolynprh@google.com>
2020-12-16 09:47:43 -0800

    Remove experimental multicluster istioctl cmd (#29628)
    * Remove experimental multicluster istioctl cmd
    
    * remove cluster file
    
    * remove unused stuff and add var to let test pass
    
    * remove unused stuff and add var to let test pass
    
    * run make gen

--- istioctl/pkg/multicluster/remote_secret_test.go
+++ istioctl/pkg/multicluster/remote_secret_test.go
@@ -28,6 +28,7 @@ import (
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/clientcmd/api"
 
 	"istio.io/istio/pkg/kube"
@@ -35,12 +36,21 @@ import (
 	"istio.io/istio/pkg/test/env"
 )
 
+var (
+	kubeSystemNamespaceUID = types.UID("54643f96-eca0-11e9-bb97-42010a80000a")
+	kubeSystemNamespace    = &v1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "kube-system",
+			UID:  kubeSystemNamespaceUID,
+		},
+	}
+)
+
 const (
 	testNamespace          = "istio-system-test"
 	testServiceAccountName = "test-service-account"
 	testKubeconfig         = "test-kubeconfig"
 	testContext            = "test-context"
-	testNetwork            = "test-network"
 )
 
 func makeServiceAccount(secrets ...string) *v1.ServiceAccount {
