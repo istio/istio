5e3bbec56734ac406c101a261992dc3d3282d20a
John Howard <howardjohn@google.com>
2020-12-29 08:29:40 -0800

    Add size to EDS log (#29801)
    This was present on others, but missing for EDS. Add it for consistency.
    
    End result:
    ```
    2020-12-29T16:06:02.397001Z     info    ads     CDS: PUSH for node:4c5bab2845ad.default resources:10 size:4.1kB
    2020-12-29T16:06:02.397126Z     info    ads     EDS: PUSH for node:4c5bab2845ad.default resources:5 size:981B empty:1 cached:0/5
    2020-12-29T16:06:02.397683Z     info    ads     LDS: PUSH for node:4c5bab2845ad.default resources:7 size:15.3kB
    2020-12-29T16:06:02.397902Z     info    ads     NDS: PUSH for node:4c5bab2845ad.default resources:1 size:330B
    2020-12-29T16:06:02.398067Z     info    ads     RDS: PUSH for node:4c5bab2845ad.default resources:3 size:1.9kB
    ```

--- pilot/pkg/xds/eds.go
+++ pilot/pkg/xds/eds.go
@@ -367,11 +367,11 @@ func (eds *EdsGenerator) Generate(proxy *model.Proxy, push *model.PushContext, w
 		}
 	}
 	if len(edsUpdatedServices) == 0 {
-		adsLog.Infof("EDS: PUSH for node:%s resources:%d empty:%v cached:%v/%v",
-			proxy.ID, len(resources), empty, cached, cached+regenerated)
-	} else {
-		adsLog.Debugf("EDS: PUSH INC for node:%s clusters:%d empty:%v cached:%v/%v",
-			proxy.ID, len(resources), empty, cached, cached+regenerated)
+		adsLog.Infof("EDS: PUSH for node:%s resources:%d size:%s empty:%v cached:%v/%v",
+			proxy.ID, len(resources), util.ByteCount(ResourceSize(resources)), empty, cached, cached+regenerated)
+	} else if adsLog.DebugEnabled() {
+		adsLog.Debugf("EDS: PUSH INC for node:%s clusters:%d size:%s empty:%vcached:%v/%v",
+			proxy.ID, len(resources), util.ByteCount(ResourceSize(resources)), empty, cached, cached+regenerated)
 	}
 	return resources
 }
