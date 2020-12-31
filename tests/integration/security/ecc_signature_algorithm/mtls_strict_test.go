0836de5a46503d26493f2dcd215f7250bfa4d654
jacob-delgado <jacob.delgado@volunteers.acasi.info>
2020-12-30 17:49:28 -0700

    [release-1.8] Add ECC_SIGNATURE_ALGORITHM env integration test (#28894) (#29829)
    * Add ECC_SIGNATURE_ALGORITHM env integration test (#28894)
    
    * Add integration test for ECC_SIGNATURE_ALGORITHM
    
    There was a previous regression when specifying ECC_SIGNATURE_ALGORITHM,
    see https://github.com/istio/istio/pull/25203.
    
    Setup:
    - A test namespace is created
    - A client and server Echo deployment is created in this namespace
    - The appropriate PeerAuthentication and DestinationRule resources are
    set so that mTLS is strict and used
    - The root CA is not altered and it will use a generated certificate
    using RSA. Due to https://github.com/istio/istio/pull/27239, the
    root CA no longer has to ECC to enable ECC_SIGNATURE_ALGORITHM. This has
    the benefit of additional testing as well.
    
    Test:
    - The client sends a request to the server deployment to make sure
    communication works inside of the mesh when using ECC certificates.
    - Using kubectl/openssl, the client then gets the certificate from the
    server deployment. It then verifies that the certificate is using ECC,
    notably ECDSA, algorithms.
    
    * Add license to top of file; lint errors
    
    * Run make gen
    
    * Fix linting issue
    
    * Add proper feature label
    
    * Fix name of feature
    
    * Fix compilation error

---
+++ tests/integration/security/ecc_signature_algorithm/mtls_strict_test.go
@@ -0,0 +1,108 @@
+// +build integ
+//  Copyright Istio Authors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package eccsignaturealgorithm
+
+import (
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"testing"
+
+	"istio.io/istio/pkg/test/echo/common/scheme"
+	"istio.io/istio/pkg/test/framework"
+	"istio.io/istio/pkg/test/framework/components/echo"
+	"istio.io/istio/pkg/test/util/tmpl"
+	"istio.io/istio/tests/integration/security/util"
+	"istio.io/istio/tests/integration/security/util/cert"
+)
+
+const (
+	DestinationRuleConfigIstioMutual = `
+apiVersion: networking.istio.io/v1alpha3
+kind: DestinationRule
+metadata:
+  name: server
+  namespace: {{.AppNamespace}}
+spec:
+  host: "server.{{.AppNamespace}}.svc.cluster.local"
+  trafficPolicy:
+    tls:
+      mode: ISTIO_MUTUAL
+`
+
+	PeerAuthenticationConfig = `
+apiVersion: security.istio.io/v1beta1
+kind: PeerAuthentication
+metadata:
+  name: default
+  namespace: {{.AppNamespace}}
+spec:
+  mtls:
+    mode: STRICT
+`
+)
+
+func TestStrictMTLS(t *testing.T) {
+	framework.
+		NewTest(t).
+		Features("security.peer.ecc-signature-algorithm").
+		Run(func(ctx framework.TestContext) {
+			peerTemplate := tmpl.EvaluateOrFail(ctx, PeerAuthenticationConfig, map[string]string{"AppNamespace": apps.Namespace.Name()})
+			ctx.Config().ApplyYAMLOrFail(ctx, apps.Namespace.Name(), peerTemplate)
+			ctx.WhenDone(func() error {
+				return ctx.Config().DeleteYAML(apps.Namespace.Name(), peerTemplate)
+			})
+			util.WaitForConfigWithSleep(ctx, "strict-mtls", peerTemplate, apps.Namespace)
+
+			drTemplate := tmpl.EvaluateOrFail(ctx, DestinationRuleConfigIstioMutual, map[string]string{"AppNamespace": apps.Namespace.Name()})
+			ctx.Config().ApplyYAMLOrFail(ctx, apps.Namespace.Name(), drTemplate)
+			ctx.WhenDone(func() error {
+				return ctx.Config().DeleteYAML(apps.Namespace.Name(), drTemplate)
+			})
+			util.WaitForConfigWithSleep(ctx, "strict-mtls", drTemplate, apps.Namespace)
+
+			response := apps.Client.CallOrFail(t, echo.CallOptions{
+				Target:   apps.Server,
+				PortName: "http",
+				Scheme:   scheme.HTTP,
+				Count:    1,
+			})
+
+			if err := response.CheckOK(); err != nil {
+				ctx.Fatalf("client could not reach server: %v", err)
+			}
+
+			target := fmt.Sprintf("server.%s:8091", apps.Namespace.Name())
+			certPEM, err := cert.DumpCertFromSidecar(apps.Namespace, "app=client", "istio-proxy", target)
+			if err != nil {
+				ctx.Fatalf("client could not get certificate from server: %v", err)
+			}
+			block, _ := pem.Decode([]byte(certPEM))
+			if block == nil {
+				ctx.Fatalf("failed to parse certificate PEM")
+			}
+
+			// nolint: staticcheck
+			certificate, parseErr := x509.ParseCertificate(block.Bytes)
+			if err != nil {
+				ctx.Fatalf("failed to parse certificate: %v", parseErr)
+			}
+
+			if certificate.PublicKeyAlgorithm != x509.ECDSA {
+				ctx.Fatalf("public key used in server cert is not ECDSA: %v", certificate.PublicKeyAlgorithm)
+			}
+		})
+}
