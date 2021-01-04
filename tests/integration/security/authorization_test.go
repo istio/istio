d6406143dfe9130d1a76ac16079753d3d7b177e9
Linggg <linggg@google.com>
2021-01-04 11:25:48 -0500

    update ext-authz sample app tag (#29760)
    * update ext-authz sample app tag
    
    * change source from a to x
    
    * remove instance a

--- tests/integration/security/authorization_test.go
+++ tests/integration/security/authorization_test.go
@@ -1305,24 +1305,24 @@ func TestAuthorization_Custom(t *testing.T) {
 			policy := applyYAML("testdata/authz/v1beta1-custom.yaml.tmpl", ns)
 			defer ctx.Config().DeleteYAMLOrFail(t, ns.Name(), policy...)
 
-			var a, b, c, d, e echo.Instance
+			var b, c, d, e, x echo.Instance
 			echoConfig := func(name string, includeExtAuthz bool) echo.Config {
 				cfg := util.EchoConfig(name, ns, false, nil, nil)
 				cfg.IncludeExtAuthz = includeExtAuthz
 				return cfg
 			}
 			echoboot.NewBuilder(ctx).
-				With(&a, echoConfig("a", false)).
 				With(&b, echoConfig("b", false)).
 				With(&c, echoConfig("c", false)).
 				With(&d, echoConfig("d", true)).
 				With(&e, echoConfig("e", true)).
+				With(&x, echoConfig("x", true)).
 				BuildOrFail(t)
 
 			newTestCase := func(target echo.Instance, path string, header string, expectAllowed bool) rbacUtil.TestCase {
 				return rbacUtil.TestCase{
 					Request: connection.Checker{
-						From: a,
+						From: x,
 						Options: echo.CallOptions{
 							Target:   target,
 							PortName: "http",
