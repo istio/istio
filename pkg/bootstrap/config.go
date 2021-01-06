cdf014051fb69f269f48b06680a22dea5b75ea29
Douglas Reid <dougreid@google.com>
2021-01-05 18:59:42 -0800

    Support non-GCP envs for Stackdriver trace

--- pkg/bootstrap/config.go
+++ pkg/bootstrap/config.go
@@ -16,6 +16,7 @@ package bootstrap
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net"
@@ -24,7 +25,6 @@ import (
 	"strconv"
 	"strings"
 
-	md "cloud.google.com/go/compute/metadata"
 	core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	"github.com/gogo/protobuf/types"
 
@@ -302,9 +302,14 @@ func getProxyConfigOptions(config *meshAPI.ProxyConfig, metadata *model.Bootstra
 			opts = append(opts, option.DataDogAddress(tracer.Datadog.Address))
 		case *meshAPI.Tracing_Stackdriver_:
 			var projectID string
-			var err error
-			if projectID, err = md.ProjectID(); err != nil {
-				return nil, fmt.Errorf("unable to process Stackdriver tracer: %v", err)
+			var projFound bool
+
+			if projectID, projFound = metadata.PlatformMetadata[platform.GCPProject]; !projFound {
+				projectID, projFound = metadata.PlatformMetadata[platform.GCPProjectNumber]
+			}
+
+			if !projFound {
+				return nil, errors.New("unable to process Stackdriver tracer: missing GCP Project")
 			}
 
 			opts = append(opts, option.StackDriverEnabled(true),
