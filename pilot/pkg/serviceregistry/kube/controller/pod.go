7bc85bc6eba6e4b44601d1d21afbee3fe1a223ab
Andy Huang <mail@andyh.io>
2021-01-09 09:52:56 +0900

    only push eds update when the endpoints are ready in service entry (#29602)
    * only push eds update when the endpoints are ready in service entry. Also need to remove when it becomes unready
    
    * right now only pod that has passed the readiness will be added to the service entry. That would require makePod to support pod with readiness
    
    * add tests for pods that are in transit state
    
    * add some comments to the code
    
    * fix service discovery test. By default all the testing workloads are in transit state
    
    * resolve the pkg name conflict
    
    * explain why we need podTransitionTime map
    
    * remove using inTransition flag
    
    * revert uncessary change
    
    * simplify the code with gofmt -s

--- pilot/pkg/serviceregistry/kube/controller/pod.go
+++ pilot/pkg/serviceregistry/kube/controller/pod.go
@@ -62,6 +62,42 @@ func newPodCache(c *Controller, informer coreinformers.PodInformer, queueEndpoin
 	return out
 }
 
+// copy from kubernetes/pkg/api/v1/pod/utils.go
+func IsPodReady(pod *v1.Pod) bool {
+	return IsPodReadyConditionTrue(pod.Status)
+}
+
+// IsPodReadyConditionTrue returns true if a pod is ready; false otherwise.
+func IsPodReadyConditionTrue(status v1.PodStatus) bool {
+	condition := GetPodReadyCondition(status)
+	return condition != nil && condition.Status == v1.ConditionTrue
+}
+func GetPodReadyCondition(status v1.PodStatus) *v1.PodCondition {
+	_, condition := GetPodCondition(&status, v1.PodReady)
+	return condition
+}
+
+func GetPodCondition(status *v1.PodStatus, conditionType v1.PodConditionType) (int, *v1.PodCondition) {
+	if status == nil {
+		return -1, nil
+	}
+	return GetPodConditionFromList(status.Conditions, conditionType)
+}
+
+// GetPodConditionFromList extracts the provided condition from the given list of condition and
+// returns the index of the condition and the condition. Returns -1 and nil if the condition is not present.
+func GetPodConditionFromList(conditions []v1.PodCondition, conditionType v1.PodConditionType) (int, *v1.PodCondition) {
+	if conditions == nil {
+		return -1, nil
+	}
+	for i := range conditions {
+		if conditions[i].Type == conditionType {
+			return i, &conditions[i]
+		}
+	}
+	return -1, nil
+}
+
 // onEvent updates the IP-based index (pc.podsByIP).
 func (pc *PodCache) onEvent(curr interface{}, ev model.Event) error {
 	pc.Lock()
@@ -94,6 +130,9 @@ func (pc *PodCache) onEvent(curr interface{}, ev model.Event) error {
 					// add to cache if the pod is running or pending
 					pc.update(ip, key)
 				}
+				if !IsPodReady(pod) {
+					ev = model.EventDelete
+				}
 			}
 		case model.EventUpdate:
 			if pod.DeletionTimestamp != nil {
@@ -109,12 +148,15 @@ func (pc *PodCache) onEvent(curr interface{}, ev model.Event) error {
 						// add to cache if the pod is running or pending
 						pc.update(ip, key)
 					}
-
+					if !IsPodReady(pod) {
+						ev = model.EventDelete
+					}
 				default:
 					// delete if the pod switched to other states and is in the cache
 					if pc.podsByIP[ip] == key {
 						pc.deleteIP(ip)
 					}
+					ev = model.EventDelete
 				}
 			}
 		case model.EventDelete:
