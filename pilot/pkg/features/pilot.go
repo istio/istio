74c47697be17f3ac98a2663b42a94ae9506ecf87
therealmitchconnors <mitchconnors@gmail.com>
2021-01-09 13:51:19 -0800

    address pr comments

--- pilot/pkg/features/pilot.go
+++ pilot/pkg/features/pilot.go
@@ -407,7 +407,7 @@ var (
 		"If enabled, Envoy will be configured to prevent traffic directly the the inbound/outbound "+
 			"ports (15001/15006). This prevents traffic loops. This option will be removed, and considered always enabled, in 1.9.").Get()
 
-	StatusMaxWorkers = env.RegisterIntVar("PILOT_STATUS_MAX_WORKERS", 100, "the maximum number of workers"+
+	StatusMaxWorkers = env.RegisterIntVar("PILOT_STATUS_MAX_WORKERS", 100, "The maximum number of workers"+
 		" Pilot will use to keep configuration status up to date.  Smaller numbers will result in higher status latency, "+
 		"but larger numbers may impact CPU in high scale environments.")
 )
