6098b64d60271db6fa0303da6864cdee62582391
Sam Naser <samnaser@google.com>
2021-01-13 16:10:48 -0500

    goimports ordering

--- istioctl/cmd/tag_test.go
+++ istioctl/cmd/tag_test.go
@@ -23,13 +23,11 @@ import (
 	"testing"
 
 	"github.com/davecgh/go-spew/spew"
-
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/serializer"
-
 	admit_v1 "k8s.io/api/admissionregistration/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/client-go/kubernetes/fake"
 
 	"istio.io/api/label"
