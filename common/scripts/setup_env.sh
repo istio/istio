f8c4552284d5d8066d386baa17a20acf47ca11ae
jacob-delgado <jacob.delgado@volunteers.acasi.info>
2021-01-14 13:14:05 -0700

    run UPDATE_BRANCH=release-1.8 make update-common (#30072)

--- common/scripts/setup_env.sh
+++ common/scripts/setup_env.sh
@@ -63,7 +63,7 @@ fi
 
 # Build image to use
 if [[ "${IMAGE_VERSION:-}" == "" ]]; then
-  export IMAGE_VERSION=master-2021-01-13T15-02-13
+  export IMAGE_VERSION=release-1.9-2021-01-14T17-00-42
 fi
 if [[ "${IMAGE_NAME:-}" == "" ]]; then
   export IMAGE_NAME=build-tools
