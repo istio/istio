ef62e0fd1c4d940ec7b1b973095cb93ac8496040
Sam Naser <samnaser@google.com>
2021-01-21 15:25:31 -0500

    remove commented out stub

--- pkg/webhooks/validation/controller/controller.go
+++ pkg/webhooks/validation/controller/controller.go
@@ -482,8 +482,6 @@ func (c *Controller) updateValidatingWebhookConfiguration(caBundle []byte, failu
 	return nil
 }
 
-// func () shouldReset
-
 type configError struct {
 	err    error
 	reason string
