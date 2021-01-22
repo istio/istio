824d6083c912a42bd83d76007c81dfaf60ae36b1
Mitch Connors <mitchconnors@gmail.com>
2021-01-21 16:11:13 -0800

    Update pkg (#30187)
    * Use new ledger
    
    * enable status by default for tests
    
    * fmt and fix test
    
    * fix various test failures due to bad fakes
    
    * skip starting status if no kube client
    
    * prevent nil reference on nil ledger
    
    * revert enable by defailt
    
    * cleanup hashes on init, rather than on merge
    
    * fmt
    
    * go mod
    
    * prevent status regression on newly completed resources
    
    * go mod tidy
    
    * remove dead code
    
    * increase error detail for troubleshooting
    
    * make pushContext.cleanup normal function
    
    * fix fmt issues
    
    Change-Id: I39c8dd9d594a40c470d58f9a9254cebcaaf1e098

--- pilot/pkg/status/distribution/state.go
+++ pilot/pkg/status/distribution/state.go
@@ -24,7 +24,6 @@ import (
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
