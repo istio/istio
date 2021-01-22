5c8048a94333b8cd59ed6b8d272ee42c21bb7bdd
Steven Landow <landow@google.com>
2021-01-22 12:14:38 -0800

    lint

--- pkg/test/framework/components/cluster/aggregate/factory.go
+++ pkg/test/framework/components/cluster/aggregate/factory.go
@@ -20,7 +20,9 @@ import (
 	"github.com/hashicorp/go-multierror"
 
 	"istio.io/istio/pkg/test/framework/components/cluster"
+	// imported to trigger registration
 	_ "istio.io/istio/pkg/test/framework/components/cluster/fake"
+	// imported to trigger registration
 	_ "istio.io/istio/pkg/test/framework/components/cluster/kube"
 	"istio.io/istio/pkg/test/framework/resource"
 	"istio.io/istio/pkg/test/scopes"
