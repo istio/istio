fbf9257c97daca40b241b9c7f9054606e0712fab
Steven Landow <landow@google.com>
2021-01-22 11:19:41 -0800

    banners

--- pkg/test/framework/components/cluster/fake/fake.go
+++ pkg/test/framework/components/cluster/fake/fake.go
@@ -1,12 +1,23 @@
+// Copyright Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package fake
 
 import (
-	"fmt"
-	"github.com/hashicorp/go-multierror"
 	"istio.io/istio/pkg/kube"
 	"istio.io/istio/pkg/test/framework/components/cluster"
 	"istio.io/istio/pkg/test/framework/resource"
-	"k8s.io/client-go/rest"
 )
 
 var _ resource.Cluster = Cluster{}
