6c58224d69c9e152da7d811bae3fd01b61168eb0
Rama Chavali <rama.rao@salesforce.com>
2021-01-25 16:26:22 +0530

    fix test
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- tests/integration/security/jwt_test.go
+++ tests/integration/security/jwt_test.go
@@ -250,8 +250,7 @@ func TestRequestAuthentication(t *testing.T) {
 							},
 							ExpectResponseCode: response.StatusCodeOK,
 							ExpectHeaders: map[string]string{
-								authHeaderKey:    "Bearer " + jwt.TokenIssuer1,
-								"X-Test-Payload": payload1,
+								authHeaderKey: "Bearer " + jwt.TokenIssuer1,
 							},
 						},
 						{
