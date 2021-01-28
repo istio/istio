3fddda726b20156372702d7331ecc37c71ec713c
Pengyuan Bian <bianpengyuan@google.com>
2021-01-28 10:04:15 -0800

    Add integration tests for Wasm module remote load (#30241)
    * Convert customize metrics to use remote load.
    
    * lint
    
    * feature label
    
    * fix
    
    * fix build
    
    * fix
    
    * make bad wasm remote load test single cluster
    
    * fix

--- tests/integration/telemetry/stats/prometheus/stats.go
+++ tests/integration/telemetry/stats/prometheus/stats.go
@@ -251,9 +251,10 @@ func TestSetup(ctx resource.Context) (err error) {
 // SendTraffic makes a client call to the "server" service on the http port.
 func SendTraffic(cltInstance echo.Instance) error {
 	_, err := cltInstance.Call(echo.CallOptions{
-		Target:   server[0],
-		PortName: "http",
-		Count:    util.RequestCountMultipler * len(server),
+		Target:    server[0],
+		PortName:  "http",
+		Count:     util.RequestCountMultipler * len(server),
+		Validator: echo.ExpectOK(),
 	})
 	if err != nil {
 		return err
