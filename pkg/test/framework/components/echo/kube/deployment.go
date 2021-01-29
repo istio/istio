eb126dbb60a9d414a6046b4e3724d7c97ce438fb
Steven Landow <landow@google.com>
2021-01-29 14:52:44 -0800

    both templates

--- pkg/test/framework/components/echo/kube/deployment.go
+++ pkg/test/framework/components/echo/kube/deployment.go
@@ -103,7 +103,7 @@ spec:
 {{- end }}
 {{- if $.ImagePullSecret }}
       imagePullSecrets:
-      - {{ $.ImagePullSecret }}
+      - name: {{ $.ImagePullSecret }}
 {{- end }}
       containers:
 {{- if $.IncludeExtAuthz }}
