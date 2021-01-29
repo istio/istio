d3309f5bd5bb3d954a9a10f9e40057e4a48cd0d2
Chi Zhang <chizhg@google.com>
2021-01-29 07:32:56 +0000

    A couple of fixes and improvements to Prow test script/code
    1. Add a temporary workaround to retry GKE cluster creation before
    b/162609408 can be fixed upstream. This will reduce the infra flakiness
    to some extent.
    2. Set the quota project before creating GKE clusters, which will fix b/178405946
    3. Some other some fixes and improvements to the infra code.
    
    Bug: 178405946
    Change-Id: I0ddc2f320be53bdc47a55e0be33c4ee87af362cc

--- prow/asm/infra/main.go
+++ prow/asm/infra/main.go
@@ -15,16 +15,25 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"regexp"
 	"strings"
 
 	"istio.io/istio/prow/asm/infra/exec"
 	"istio.io/istio/prow/asm/infra/gke"
+	"istio.io/istio/prow/asm/infra/tailorbird"
 
 	shell "github.com/kballard/go-shellquote"
+)
+
+const (
+	gkeDeployerName        = "gke"
+	tailorbirdDeployerName = "tailorbird"
 )
 
 var (
@@ -34,6 +43,7 @@ var (
 type options struct {
 	kubetest2WorkingDir string
 	deployerName        string
+	clusterType         string
 	extraDeployerFlags  string
 	testScript          string
 	testFlags           string
@@ -44,7 +54,8 @@ type options struct {
 func main() {
 	o := options{}
 	flag.StringVar(&o.kubetest2WorkingDir, "kubetest2-working-dir", "", "the working directory for running the kubetest2 command")
-	flag.StringVar(&o.deployerName, "deployer", "", "kubetest2 deployer name, can be gke, tailorbird or kind")
+	flag.StringVar(&o.deployerName, "deployer", "", "kubetest2 deployer name, can be gke or tailorbird. Will be deprecated, use --cluster-type instead.")
+	flag.StringVar(&o.clusterType, "cluster-type", "gke", "the cluster type, can be one of gke, gke-on-prem, bare-metal, etc")
 	flag.StringVar(&o.extraDeployerFlags, "deployer-flags", "", "extra flags corresponding to the deployer being used, supported flags can be"+
 		" checked by running `kubetest2 [deployer] --help`")
 	flag.StringVar(&o.testScript, "test-script", "", "the script to run the tests after clusters are created")
@@ -72,10 +83,6 @@ func main() {
 		}
 	}
 
-	if os.Getenv("CI") != "true" {
-		// Also tear down the clusters in non-CI environment.
-		baseDeployerFlags = append(baseDeployerFlags, "--down")
-	}
 	baseDeployerFlags = append(baseDeployerFlags, extraDeployerFlagArr...)
 	if err := o.runKubetest2(baseDeployerFlags, testFlagArr); err != nil {
 		log.Fatal("Error running the test flow with kubetest2: ", err)
@@ -83,6 +90,12 @@ func main() {
 }
 
 func (o *options) initSetup() error {
+	if o.clusterType == "gke" {
+		o.deployerName = gkeDeployerName
+	} else {
+		o.deployerName = tailorbirdDeployerName
+	}
+
 	o.setEnvVars()
 
 	if err := o.installTools(); err != nil {
@@ -102,7 +115,7 @@ func (o *options) setEnvVars() {
 }
 
 func (o *options) installTools() error {
-	if o.deployerName == "tailorbird" {
+	if o.deployerName == tailorbirdDeployerName {
 		log.Println("Installing kubetest2 tailorbird deployer...")
 		cookieFile := "/secrets/cookiefile/cookies"
 		exec.Run("git config --global http.cookiefile " + cookieFile)
@@ -127,51 +140,77 @@ func (o *options) installTools() error {
 
 func (o *options) runKubetest2(deployerFlags, testFlags []string) error {
 	switch o.deployerName {
-	case "gke":
+	case gkeDeployerName:
 		log.Println("Will run kubetest2 gke deployer to create the clusters...")
-		switch o.clusterTopology {
-		case "SINGLECLUSTER":
-			deployerFlags = append(deployerFlags, gke.SingleClusterFlags()...)
-		case "MULTICLUSTER":
-			deployerFlags = append(deployerFlags, gke.MultiClusterFlags()...)
-		case "MULTIPROJECT_MULTICLUSTER":
-			extraFlags, err := gke.MultiProjectMultiClusterFlags()
-			if err != nil {
-				return fmt.Errorf("error constructing the flags for multi-project multi-cluster setup: %w", err)
-			}
-			deployerFlags = append(deployerFlags, extraFlags...)
-		default:
-			log.Fatalf("The cluster topology %q is not supported, please double check!", o.clusterTopology)
-		}
 
-		if o.featureToTest != "" {
-			switch o.featureToTest {
-			case "VPC_SC":
-				extraFlags, err := gke.ExtraVPCSCClusterFlags()
-				if err != nil {
-					return fmt.Errorf("error getting the extra flags for testing with VPC-SC: %w", err)
-				}
-				deployerFlags = append(deployerFlags, extraFlags...)
-			default:
-				log.Fatalf("The feature %q is not supported, please double check!", o.featureToTest)
-			}
+		extraFlags, err := gke.DeployerFlags(o.clusterTopology, o.featureToTest)
+		if err != nil {
+			return fmt.Errorf("error getting deployer flags for gke: %w", err)
 		}
-	case "tailorbird":
+		deployerFlags = append(deployerFlags, extraFlags...)
+	case tailorbirdDeployerName:
 		log.Println("Will run kubetest2 tailorbird deployer to create the clusters...")
-		// Always tear down the clusters created by Tailorbird after test is finished.
-		deployerFlags = append(deployerFlags, "--down")
-		deployerFlags = append(deployerFlags, "--tbenv=int", "--verbose")
+
+		extraFlags := tailorbird.DeployerFlags(o.clusterType, o.clusterTopology, o.featureToTest)
+		deployerFlags = append(deployerFlags, extraFlags...)
 	default:
-		log.Fatalf("The deployer %q is not supported, please double check!", o.deployerName)
+		return fmt.Errorf("unsupported deployer: %q", o.deployerName)
 	}
 
 	kubetest2Flags := []string{o.deployerName}
 	kubetest2Flags = append(kubetest2Flags, deployerFlags...)
-	kubetest2Flags = append(kubetest2Flags, "--test=exec", "--", o.testScript)
-	kubetest2Flags = append(kubetest2Flags, testFlags...)
-	if err := exec.Run(fmt.Sprintf("kubetest2 %s", strings.Join(kubetest2Flags, " ")), exec.WithWorkingDir(o.kubetest2WorkingDir)); err != nil {
-		return err
+
+	if o.deployerName == gkeDeployerName {
+		var buf bytes.Buffer
+		// Retry creating the cluster in different regions to reduce the test
+		// flakniess caused by GCE_STOCKOUT and other recoverable GKE errors.
+		// This is a temporary workaround before b/162609408 is solved upstream.
+		// TODO(chizhg): remove the retry logic here after b/162609408 is solved
+		//    upstream in kubetest2.
+		for _, region := range []string{"us-central1", "us-west1", "us-east1"} {
+			newKubetest2Flags := make([]string, len(kubetest2Flags))
+			copy(newKubetest2Flags, kubetest2Flags)
+			newKubetest2Flags = append(newKubetest2Flags, "--region="+region)
+			newKubetest2Flags = append(newKubetest2Flags, "--test=exec", "--", o.testScript)
+			newKubetest2Flags = append(newKubetest2Flags, testFlags...)
+			if err := exec.Run(fmt.Sprintf("kubetest2 %s", strings.Join(newKubetest2Flags, " ")),
+				exec.WithWorkingDir(o.kubetest2WorkingDir),
+				exec.WithWriter(io.MultiWriter(os.Stdout, &buf), io.MultiWriter(os.Stderr, &buf))); err != nil {
+				if !isRetryableError(buf.String()) {
+					return err
+				}
+			} else {
+				return nil
+			}
+		}
+	} else {
+		kubetest2Flags = append(kubetest2Flags, "--test=exec", "--", o.testScript)
+		kubetest2Flags = append(kubetest2Flags, testFlags...)
+		if err := exec.Run(fmt.Sprintf("kubetest2 %s", strings.Join(kubetest2Flags, " ")), exec.WithWorkingDir(o.kubetest2WorkingDir)); err != nil {
+			return err
+		}
 	}
 
 	return nil
+}
+
+// If one of the error patterns below is matched, it would be recommended to
+// retry creating the cluster in a different region.
+// - stockout
+// - nodes fail to start
+// - component is unhealthy
+var retryableCreationErrors = []*regexp.Regexp{
+	regexp.MustCompile(`.*does not have enough resources available to fulfill.*`),
+	regexp.MustCompile(`.*only \d+ nodes out of \d+ have registered; this is likely due to Nodes failing to start correctly.*`),
+	regexp.MustCompile(`.*All cluster resources were brought up, but: component .+ from endpoint .+ is unhealthy.*`),
+}
+
+// isRetryableError checks if the error happens during cluster creation can be potentially solved by retrying or not.
+func isRetryableError(errMsg string) bool {
+	for _, regx := range retryableCreationErrors {
+		if regx.MatchString(errMsg) {
+			return true
+		}
+	}
+	return false
 }
