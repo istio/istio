64dcaf8b280508be6cc7e9cda46f8cf08a29dcd3
Rui Gu <ruigu@google.com>
2021-01-31 16:11:20 -0800

    Refactor and add test skeleton code for managed control plane
    BUG: b/178416099
    Change-Id: I0ff4264dc0f3b7ec01a8664d3d26af386a591654

--- prow/asm/integ-run-tests.sh
+++ prow/asm/integ-run-tests.sh
@@ -37,6 +37,8 @@ export BUILD_WITH_CONTAINER=0
 CA="MESHCA"
 # WIP(Workload Identity Pool) = GKE or HUB
 WIP="GKE"
+# CONTROL_PLANE = UNMANAGED or MANAGED
+CONTROL_PLANE="UNMANAGED"
 TEST_TARGET="test.integration.multicluster.kube.presubmit"
 
 while (( "$#" )); do
@@ -49,6 +51,18 @@ while (( "$#" )); do
           ;;
         *)
           echo "Error: Unsupported CA $2" >&2
+          exit 1
+          ;;
+      esac
+      ;;
+    --control-plane)
+      case $2 in
+        "UNMANAGED" | "MANAGED" )
+          CONTROL_PLANE=$2
+          shift 2
+          ;;
+        *)
+          echo "Error: Unsupported ASM Control Plane $2" >&2
           exit 1
           ;;
       esac
@@ -75,7 +89,7 @@ while (( "$#" )); do
       ;;
   esac
 done
-echo "Running with CA ${CA} and ${WIP} Workload Identity Pool"
+echo "Running with CA ${CA}, ${WIP} Workload Identity Pool, ${CONTROL_PLANE} control plane."
 
 echo "Using ${KUBECONFIG} to connect to the cluster(s)"
 if [[ -z "${KUBECONFIG}" ]]; then
@@ -122,88 +136,98 @@ if [[ "${DEPLOYER}" == "gke" ]]; then
 else
   GCR_PROJECT_ID="${CENTRAL_GCP_PROJECT}"
 fi
-export HUB="gcr.io/${GCR_PROJECT_ID}/asm"
-export TAG="BUILD_ID_${BUILD_ID}"
 
-if [[ "${DEPLOYER}" == "gke" ]]; then
-  echo "Set permissions to allow the Pods on the GKE clusters to pull images..."
-  set_gcp_permissions "${GCR_PROJECT_ID}" "${CONTEXTSTR}"
-  add_trap "remove_gcp_permissions ${GCR_PROJECT_ID} ${CONTEXTSTR}" EXIT SIGKILL SIGTERM SIGQUIT
-elif [[ "${DEPLOYER}" == "tailorbird" ]]; then
-  echo "Set permissions to allow the Pods on the multicloud clusters to pull images..."
-  # TODO: remove it if there is a general solution for b/174580152
-  set_multicloud_permissions "${GCR_PROJECT_ID}" "${CONTEXTSTR}"
+if [[ "${CLUSTER_TOPOLOGY}" == "MULTICLUSTER" || "${CLUSTER_TOPOLOGY}" == "MULTIPROJECT_MULTICLUSTER" ]]; then
+  echo "Setting up ${CLUSTER_TOPOLOGY} envrionment for test"
+
+  # TODO(ruigu): extract the common part of MANAGED and UNMANAGED when MANAGED test is added.
+  if [[ "${CONTROL_PLANE}" == "UNMANAGED" ]]; then
+    echo "Setting up ASM ${CONTROL_PLANE} control plane for test"
+
+    export HUB="gcr.io/${GCR_PROJECT_ID}/asm"
+    export TAG="BUILD_ID_${BUILD_ID}"
+
+    if [[ "${DEPLOYER}" == "gke" ]]; then
+      echo "Set permissions to allow the Pods on the GKE clusters to pull images..."
+      set_gcp_permissions "${GCR_PROJECT_ID}" "${CONTEXTSTR}"
+      add_trap "remove_gcp_permissions ${GCR_PROJECT_ID} ${CONTEXTSTR}" EXIT SIGKILL SIGTERM SIGQUIT
+    elif [[ "${DEPLOYER}" == "tailorbird" ]]; then
+      echo "Set permissions to allow the Pods on the multicloud clusters to pull images..."
+      # TODO: remove it if there is a general solution for b/174580152
+      set_multicloud_permissions "${GCR_PROJECT_ID}" "${CONTEXTSTR}"
+    fi
+
+    echo "Preparing images..."
+    prepare_images
+    add_trap "cleanup_images" EXIT SIGKILL SIGTERM SIGQUIT
+
+    if [[ "${WIP}" == "HUB" ]]; then
+      echo "Register clusters into the Hub..."
+      # Use the first project as the GKE Hub host project
+      GKEHUB_PROJECT_ID="${GCR_PROJECT_ID}"
+      register_clusters_in_hub "${GKEHUB_PROJECT_ID}" "${CONTEXTS[@]}"
+      add_trap "cleanup_hub_setup ${GKEHUB_PROJECT_ID} ${CONTEXTSTR}" EXIT SIGKILL SIGTERM SIGQUIT
+    fi
+
+    echo "Building istioctl..."
+    build_istioctl
+
+    echo "Installing ASM control plane..."
+    gcloud components install kpt
+    if [[ "${DEPLOYER}" == "gke" ]]; then
+      install_asm "${WD}/kpt-pkg" "${CA}" "${WIP}" "${CONTEXTS[@]}"
+    elif [[ "${DEPLOYER}" == "tailorbird" ]]; then
+      install_asm_on_multicloud "${WD}/pkg" "CITADEL" "${WIP}" "${CONTEXTS[@]}"
+    fi
+
+    echo "Processing kubeconfig files for running the tests..."
+    process_kubeconfigs
+
+    export KUBECONFIGINPUT="${KUBECONFIG/:/,}"
+
+    # exported GCR_PROJECT_ID_1, GCR_PROJECT_ID_2, WIP and CA values are needed for security test.
+    export GCR_PROJECT_ID_1=${GCR_PROJECT_ID}
+    IFS="_" read -r -a VALS_2 <<< "${CONTEXTS[1]}"
+    export GCR_PROJECT_ID_2=${VALS_2[1]}
+    # When HUB Workload Identity Pool is used in the case of multi projects setup, clusters in different projects
+    # will use the same WIP and P4SA of the Hub host project.
+    if [[ "${WIP}" == "HUB" ]]; then
+      export GCR_PROJECT_ID_2="${GCR_PROJECT_ID_1}"
+    fi
+    export CA
+    export WIP
+
+    # DISABLED_TESTS contains a list of all tests we skip
+    # pilot/ tests
+    DISABLED_TESTS="TestWait|TestVersion|TestProxyStatus" # UNSUPPORTED: istioctl doesn't work
+    DISABLED_TESTS+="|TestAnalysisWritesStatus" # UNSUPPORTED: require custom installation
+    # telemetry/ tests
+    DISABLED_TESTS+="|TestDashboard" # UNSUPPORTED: Relies on istiod in cluster. TODO: filter out only pilot-dashboard.json
+    DISABLED_TESTS+="|TestCustomizeMetrics|TestStatsFilter|TestTcpMetric|TestWasmStatsFilter|TestWASMTcpMetric" # UNKNOWN: b/177606974
+    # security/ tests
+
+    export INTEGRATION_TEST_FLAGS="${INTEGRATION_TEST_FLAGS:-}"
+
+    # TODO(nmittler): Remove this once we no longer run the multicluster tests.
+    export TEST_SELECT="${TEST_SELECT:-}"
+    if [[ $TEST_TARGET == "test.integration.multicluster.kube.presubmit" ]]; then
+      TEST_SELECT="+multicluster"
+    fi
+
+    # Don't deploy Istio. Instead just use the pre-installed ASM
+    INTEGRATION_TEST_FLAGS+=" --istio.test.kube.deploy=false"
+
+    # Don't run VM tests. Echo deployment requires the eastwest gateway
+    # which isn't deployed for all configurations.
+    INTEGRATION_TEST_FLAGS+=" --istio.test.skipVM"
+
+    # Skip the tests that are known to be not working.
+    INTEGRATION_TEST_FLAGS+=" --istio.test.skip=\"${DISABLED_TESTS}\""
+
+    export INTEGRATION_TEST_KUBECONFIG="${KUBECONFIGINPUT}"
+
+    echo "Running e2e test: ${TEST_TARGET}..."
+    export JUNIT_OUT="${ARTIFACTS}/junit1.xml"
+    make "${TEST_TARGET}"
+  fi
 fi
-
-echo "Preparing images..."
-prepare_images
-add_trap "cleanup_images" EXIT SIGKILL SIGTERM SIGQUIT
-
-if [[ "${WIP}" == "HUB" ]]; then
-  echo "Register clusters into the Hub..."
-  # Use the first project as the GKE Hub host project
-  GKEHUB_PROJECT_ID="${GCR_PROJECT_ID}"
-  register_clusters_in_hub "${GKEHUB_PROJECT_ID}" "${CONTEXTS[@]}"
-  add_trap "cleanup_hub_setup ${GKEHUB_PROJECT_ID} ${CONTEXTSTR}" EXIT SIGKILL SIGTERM SIGQUIT
-fi
-
-echo "Building istioctl..."
-build_istioctl
-
-echo "Installing ASM control plane..."
-gcloud components install kpt
-if [[ "${DEPLOYER}" == "gke" ]]; then
-  install_asm "${WD}/kpt-pkg" "${CA}" "${WIP}" "${CONTEXTS[@]}"
-elif [[ "${DEPLOYER}" == "tailorbird" ]]; then
-  install_asm_on_multicloud "${WD}/pkg" "CITADEL" "${WIP}" "${CONTEXTS[@]}"
-fi
-
-echo "Processing kubeconfig files for running the tests..."
-process_kubeconfigs
-
-export KUBECONFIGINPUT="${KUBECONFIG/:/,}"
-
-# exported GCR_PROJECT_ID_1, GCR_PROJECT_ID_2, WIP and CA values are needed for security test.
-export GCR_PROJECT_ID_1=${GCR_PROJECT_ID}
-IFS="_" read -r -a VALS_2 <<< "${CONTEXTS[1]}"
-export GCR_PROJECT_ID_2=${VALS_2[1]}
-# When HUB Workload Identity Pool is used in the case of multi projects setup, clusters in different projects
-# will use the same WIP and P4SA of the Hub host project.
-if [[ "${WIP}" == "HUB" ]]; then
-  export GCR_PROJECT_ID_2="${GCR_PROJECT_ID_1}"
-fi
-export CA
-export WIP
-
-# DISABLED_TESTS contains a list of all tests we skip
-# pilot/ tests
-DISABLED_TESTS="TestWait|TestVersion|TestProxyStatus" # UNSUPPORTED: istioctl doesn't work
-DISABLED_TESTS+="|TestAnalysisWritesStatus" # UNSUPPORTED: require custom installation
-# telemetry/ tests
-DISABLED_TESTS+="|TestDashboard" # UNSUPPORTED: Relies on istiod in cluster. TODO: filter out only pilot-dashboard.json
-DISABLED_TESTS+="|TestCustomizeMetrics|TestStatsFilter|TestTcpMetric|TestWasmStatsFilter|TestWASMTcpMetric" # UNKNOWN: b/177606974
-# security/ tests
-
-export INTEGRATION_TEST_FLAGS="${INTEGRATION_TEST_FLAGS:-}"
-
-# TODO(nmittler): Remove this once we no longer run the multicluster tests.
-export TEST_SELECT="${TEST_SELECT:-}"
-if [[ $TEST_TARGET == "test.integration.multicluster.kube.presubmit" ]]; then
-  TEST_SELECT="+multicluster"
-fi
-
-# Don't deploy Istio. Instead just use the pre-installed ASM
-INTEGRATION_TEST_FLAGS+=" --istio.test.kube.deploy=false"
-
-# Don't run VM tests. Echo deployment requires the eastwest gateway
-# which isn't deployed for all configurations.
-INTEGRATION_TEST_FLAGS+=" --istio.test.skipVM"
-
-# Skip the tests that are known to be not working.
-INTEGRATION_TEST_FLAGS+=" --istio.test.skip=\"${DISABLED_TESTS}\""
-
-export INTEGRATION_TEST_KUBECONFIG="${KUBECONFIGINPUT}"
-
-echo "Running e2e test: ${TEST_TARGET}..."
-export JUNIT_OUT="${ARTIFACTS}/junit1.xml"
-make "${TEST_TARGET}"
