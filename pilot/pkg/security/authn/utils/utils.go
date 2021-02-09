422841c81a9ce44efd4a22deac0ffe3cf7f640c0
Istio Automation <istio-testing-bot@google.com>
2021-02-09 14:50:52 -0800

    [release-1.9] Fix inbound TLS v2 settings (#30623)
    * Fix inbound TLS v2 settings
    
    Currently, its accidentally inside the "if protocol == HTTP" clause. I
    am pretty sure its intended to be outside of it, so it applies to all
    protocol types.
    
    * Update releasenotes/notes/tls-inbound-all-protocols.yaml
    
    Co-authored-by: Eric Van Norman <ericvn@us.ibm.com>
    
    Co-authored-by: John Howard <howardjohn@google.com>
    Co-authored-by: Eric Van Norman <ericvn@us.ibm.com>

--- pilot/pkg/security/authn/utils/utils.go
+++ pilot/pkg/security/authn/utils/utils.go
@@ -86,15 +86,14 @@ func BuildInboundFilterChain(mTLSMode model.MutualTLSMode, sdsUdsPath string, no
 			},
 			RequireClientCertificate: protovalue.BoolTrue,
 		}
+	}
 
-		if features.EnableTLSv2OnInboundPath {
-			// Set Minimum TLS version to match the default client version and allowed strong cipher suites for sidecars.
-			ctx.CommonTlsContext.TlsParams = &tls.TlsParameters{
-				TlsMinimumProtocolVersion: tls.TlsParameters_TLSv1_2,
-				CipherSuites:              SupportedCiphers,
-			}
+	if features.EnableTLSv2OnInboundPath {
+		// Set Minimum TLS version to match the default client version and allowed strong cipher suites for sidecars.
+		ctx.CommonTlsContext.TlsParams = &tls.TlsParameters{
+			TlsMinimumProtocolVersion: tls.TlsParameters_TLSv1_2,
+			CipherSuites:              SupportedCiphers,
 		}
-
 	}
 
 	authn_model.ApplyToCommonTLSContext(ctx.CommonTlsContext, node, sdsUdsPath, []string{} /*subjectAltNames*/, trustDomainAliases)
