29cc0f359b96cbdcd8e0d84bdea543cc911b5296
Steven Landow <landow@google.com>
2021-02-23 10:51:39 -0800

    expect gateways to be ignored resulting in local endpoints

--- pilot/pkg/xds/ep_filters_test.go
+++ pilot/pkg/xds/ep_filters_test.go
@@ -211,7 +211,7 @@ func TestEndpointsByNetworkFilter(t *testing.T) {
 	}
 }
 
-func TestEndpointsByNetworkFilter_SkipLBWithHostname(t *testing.T) {
+func TestEndpointsByNetworkFilter_IgnoreHostnameGateways(t *testing.T) {
 	//  - 1 IP gateway for network1
 	//  - 1 DNS gateway for network2
 	//  - 1 IP gateway for network3
@@ -227,7 +227,6 @@ func TestEndpointsByNetworkFilter_SkipLBWithHostname(t *testing.T) {
 		},
 	}})
 	serviceDiscovery.SetGatewaysForNetwork("network2", &model.Gateway{Addr: "aeiou.scooby.do", Port: 80})
-
 	env.ServiceDiscovery = serviceDiscovery
 
 	// Test endpoints creates:
@@ -257,10 +256,12 @@ func TestEndpointsByNetworkFilter_SkipLBWithHostname(t *testing.T) {
 						// 2 local endpoints
 						{address: "10.0.0.1", weight: 1},
 						{address: "10.0.0.2", weight: 1},
-						// 0 endpoint to gateway of network2 a its a dns name instead of IP
+						// non-gateway endpoint to gateway of network2 a its a dns name instead of IP
+						{address: "20.0.0.1", weight: 1},
+						// non-gateway endpoint to network4 since there is no gateway specified
 						{address: "40.0.0.1", weight: 1},
 					},
-					weight: 3,
+					weight: 4,
 				},
 			},
 		},
@@ -292,10 +293,12 @@ func TestEndpointsByNetworkFilter_SkipLBWithHostname(t *testing.T) {
 					lbEps: []LbEpInfo{
 						// 1 endpoint to gateway of network1 with weight 2 because it has 2 endpoints
 						{address: "1.1.1.1", weight: 2},
-						// 0 endpoint to gateway of network2 as its a DNS gateway
+						// non-gateway endpoint to gateway of network2 a its a dns name instead of IP
+						{address: "20.0.0.1", weight: 1},
+						// non-gateway endpoint to network4 since there is no gateway specified
 						{address: "40.0.0.1", weight: 1},
 					},
-					weight: 3,
+					weight: 4,
 				},
 			},
 		},
@@ -311,9 +314,10 @@ func TestEndpointsByNetworkFilter_SkipLBWithHostname(t *testing.T) {
 						{address: "40.0.0.1", weight: 1},
 						// 1 endpoint to gateway of network1 with weight 2 because it has 2 endpoints
 						{address: "1.1.1.1", weight: 2},
-						// 0 endpoint to gateway of network2 as its a dns gateway
+						// non-gateway endpoint to gateway of network2 a its a dns name instead of IP
+						{address: "20.0.0.1", weight: 1},
 					},
-					weight: 3,
+					weight: 4,
 				},
 			},
 		},
