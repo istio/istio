b7d55f52d2faf971981448680785a91253fdfa28
Steven Landow <landow@google.com>
2021-02-23 18:13:01 -0800

    refactor real-stackdriver tests
    Change-Id: I6cb9202fd0a33b59cb2803727807161436ee5598

--- tests/integration/multiclusterasm/telemetry_sd_test.go
+++ tests/integration/multiclusterasm/telemetry_sd_test.go
@@ -19,21 +19,23 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
+	"istio.io/istio/pkg/test/framework/components/echo/echoboot"
+	"istio.io/istio/pkg/test/framework/resource"
+	"istio.io/istio/pkg/test/util/file"
+	"istio.io/istio/pkg/test/util/tmpl"
+	"istio.io/istio/tests/integration/pilot/common"
 	"os"
 	"testing"
 	"time"
 
 	"istio.io/istio/pkg/test/framework"
 	"istio.io/istio/pkg/test/framework/components/echo"
-	"istio.io/istio/pkg/test/framework/components/istio"
+	// Side-effect import to register cmd line flags
+	_ "istio.io/istio/pkg/test/framework/components/istio"
 	"istio.io/istio/pkg/test/framework/components/namespace"
 	stackdriver "istio.io/istio/pkg/test/framework/components/stackdriverasm"
 	"istio.io/istio/pkg/test/framework/label"
-	"istio.io/istio/pkg/test/framework/resource"
-	"istio.io/istio/pkg/test/util/file"
 	"istio.io/istio/pkg/test/util/retry"
-	"istio.io/istio/pkg/test/util/tmpl"
-	"istio.io/istio/tests/integration/multicluster"
 )
 
 const (
@@ -42,11 +44,12 @@ const (
 	metricDuration         = 5 * time.Minute
 	containerResourceType  = "k8s_container"
 	auditPolicyForLogEntry = "testdata/security/v1beta1-audit-authorization-policy.yaml.tmpl"
+	requestCount           = 100
 )
 
 var (
-	ist    istio.Instance
-	appCtx multicluster.AppContext
+	apps echo.Instances
+	ns   namespace.Instance
 )
 
 func TestMain(m *testing.M) {
@@ -54,54 +57,91 @@ func TestMain(m *testing.M) {
 		NewSuite(m).
 		Label(label.Multicluster).
 		RequireMinClusters(2).
-		Setup(multicluster.Setup(&appCtx)).
-		Setup(istio.Setup(&ist, func(_ resource.Context, cfg *istio.Config) {
-			cfg.ControlPlaneValues = appCtx.ControlPlaneValues
-		})).
-		Setup(multicluster.SetupApps(&appCtx)).
+		RequireMaxClusters(2).
+		Setup(setupApps).
 		Run()
 }
 
-// TestMetrics validates that metrics and logs are collected in stackdriver.
-func TestMetrics(t *testing.T) {
-	framework.NewTest(t).
-		Features("installation.multicluster.multimaster_telemetry").
-		Run(func(ctx framework.TestContext) {
-			ctx.NewSubTest("telemetry_asm").
-				Run(func(ctx framework.TestContext) {
-					clusters := ctx.Environment().Clusters()
-					cluster1, cluster2 := clusters[0], clusters[1]
-					src := appCtx.LBEchos.GetOrFail(ctx, echo.InCluster(cluster1))
-					dest := appCtx.LBEchos.GetOrFail(ctx, echo.InCluster(cluster2))
-
-					const (
-						requestCount = 100
-					)
-
-					retry.UntilSuccessOrFail(t, func() error {
-						r, err := src.Call(echo.CallOptions{
-							Count:    requestCount,
-							Target:   dest,
-							PortName: "grpc",
-							Headers: map[string][]string{
-								"Host": {dest.Config().FQDN()},
-							},
-							Message: t.Name(),
-						})
-						if err != nil {
-							return err
-						}
-						if err := r.CheckOK(); err != nil {
-							return err
-						}
-						return nil
-					})
-					testStackdriver(t, appCtx.Namespace, src, dest)
-				})
-		})
+func setupApps(ctx resource.Context) error {
+	var err error
+	ns, err = namespace.New(ctx, namespace.Config{Prefix: "echo-stackdriver", Inject: true})
+	if err != nil {
+		return err
+	}
+	apps, err = echoboot.NewBuilder(ctx).
+		WithClusters(ctx.Clusters()...).
+		WithConfig(echo.Config{
+			Namespace: ns,
+			Service:   "app",
+			Ports:     common.EchoPorts,
+		}).
+		WithConfig(echo.Config{
+			Namespace:  ns,
+			Service:    "vm",
+			Ports:      common.EchoPorts,
+			DeployAsVM: true,
+		}).
+		Build()
+	return err
 }
 
-func validateTelemetry(t *testing.T, projectID string, sd *stackdriver.Instance, ns namespace.Instance, src, dest echo.Instance, filter string) {
+func TestStackdriver(t *testing.T) {
+	framework.NewTest(t).Run(func(ctx framework.TestContext) {
+		testMulticluster(ctx, func(ctx framework.TestContext, ns namespace.Instance, src, dest echo.Instance) {
+			st := stackdriver.NewOrFail(context.Background(), ctx)
+			projectID1 := os.Getenv("GCR_PROJECT_ID_1")
+			projectID2 := os.Getenv("GCR_PROJECT_ID_2")
+
+			// Verify metrics and logs on cluster1 in project id 1
+			ctx.Logf("Validating Telemetry for Cluster in project %v", projectID1)
+			validateTelemetry(ctx, projectID1, st, ns, src, dest, "grpc", "server-accesslog-stackdriver")
+
+			// Verify metrics and logs on cluster1 in project id 2
+			ctx.Logf("Validating Telemetry for for Cluster in project %v", projectID2)
+			validateTelemetry(ctx, projectID2, st, ns, src, dest, "http", "server-accesslog-stackdriver")
+		})
+	})
+}
+
+func TestStackdriverAudit(t *testing.T) {
+	framework.NewTest(t).Run(func(ctx framework.TestContext) {
+		testMulticluster(ctx, func(ctx framework.TestContext, ns namespace.Instance, src, dest echo.Instance) {
+			st := stackdriver.NewOrFail(context.Background(), ctx)
+
+			args := map[string]string{
+				"Namespace": ns.Name(),
+			}
+			policies := tmpl.EvaluateAllOrFail(ctx, args, file.AsStringOrFail(ctx, auditPolicyForLogEntry))
+			ctx.Config().ApplyYAMLOrFail(ctx, ns.Name(), policies...)
+
+			// TODO: audit feature supports only single project scenario
+			projectID := os.Getenv("GCR_PROJECT_ID_2")
+			ctx.Logf("Validating Telemetry for for Cluster in project %v", projectID)
+			validateTelemetry(ctx, projectID, st, ns, src, dest, "http", "server-istio-audit-log")
+		})
+	})
+}
+
+func testMulticluster(ctx framework.TestContext, doTest func(ctx framework.TestContext, ns namespace.Instance, src, dest echo.Instance)) {
+	for _, src := range apps {
+		for _, dst := range apps {
+			src, dst := src, dst
+			ctx.
+				NewSubTest(fmt.Sprintf("from %s to %s", src.Config().Cluster.Name(), dst.Config().Cluster.Name())).
+				Run(func(ctx framework.TestContext) {
+					doTest(ctx, ns, src, dst)
+				})
+		}
+	}
+}
+
+func validateTelemetry(ctx framework.TestContext,
+	projectID string,
+	sd *stackdriver.Instance, ns namespace.Instance,
+	src, dest echo.Instance,
+	protocolPort string,
+	filter string,
+) {
 	param := &stackdriver.ResourceFilterParam{
 		Namespace:     ns.Name(),
 		ContainerName: appCN,
@@ -113,105 +153,42 @@ func validateTelemetry(t *testing.T, projectID string, sd *stackdriver.Instance,
 	// Add a buffer to endTime for metrics to show up.
 	endTime := now.Add(metricDuration).Format(time.RFC3339)
 
-	retry.UntilSuccessOrFail(t, func() error {
-		if err := validateMetrics(t, projectID, sd, ns, src, dest, startTime, endTime, param); err != nil {
-			t.Logf("failed to validate metrics for %v, resending new traffics to check", err)
-			r, err := src.Call(echo.CallOptions{
-				Count:    30,
-				Target:   dest,
-				PortName: "grpc",
-				Headers: map[string][]string{
-					"Host": {dest.Config().FQDN()},
-				},
-				Message: t.Name(),
-			})
-			if err != nil {
-				return err
-			}
-			if err := r.CheckOK(); err != nil {
-				return err
-			}
+	// Get 1 successful traffic call first
+	retry.UntilSuccessOrFail(ctx, func() error {
+		return sendTraffic(ctx, src, dest, protocolPort)
+	})
+	// Then validate telemetry
+	retry.UntilSuccessOrFail(ctx, func() error {
+		if err := sendTraffic(ctx, src, dest, protocolPort); err != nil {
+			return err
+		}
+		if err := validateMetrics(ctx, projectID, sd, ns, src, dest, startTime, endTime, param); err != nil {
+			ctx.Logf("failed to validate metrics for %v, resending new traffics to check", err)
 			return fmt.Errorf("resending traffic for: %v, check for metrics again", err)
 		}
-
 		return nil
 	}, retry.Timeout(15*time.Minute))
 
-	validateLog(t, projectID, sd, src, dest, param, startTime, filter)
+	validateLog(ctx, projectID, sd, src, dest, param, startTime, filter)
 }
 
-func testStackdriver(t *testing.T, ns namespace.Instance, src, dest echo.Instance) {
-	st := stackdriver.NewOrFail(context.Background(), t)
-
-	// Verify metrics and logs on cluster1 in project id 1
-	projectID1 := os.Getenv("GCR_PROJECT_ID_1")
-	projectID2 := os.Getenv("GCR_PROJECT_ID_2")
-	t.Logf("Validating Telemetry for Cluster in project %v", projectID1)
-	validateTelemetry(t, projectID1, st, ns, src, dest, "server-accesslog-stackdriver")
-
-	// Verify metrics and logs on cluster1 in project id 2
-	t.Logf("Validating Telemetry for for Cluster in project %v", projectID2)
-	validateTelemetry(t, projectID2, st, ns, src, dest, "server-accesslog-stackdriver")
+func sendTraffic(t framework.TestContext, src, dest echo.Instance, portName string) error {
+	r, err := src.Call(echo.CallOptions{
+		Count:    30,
+		Target:   dest,
+		PortName: portName,
+		Headers: map[string][]string{
+			"Host": {dest.Config().FQDN()},
+		},
+		Message: t.Name(),
+	})
+	if err != nil {
+		return err
+	}
+	return r.CheckOK()
 }
 
-func testStackdriverAudit(t *testing.T, ns namespace.Instance, src, dest echo.Instance) {
-	st := stackdriver.NewOrFail(context.Background(), t)
-
-	// TODO: audit feature supports only single project scenario
-	projectID := os.Getenv("GCR_PROJECT_ID_2")
-	t.Logf("Validating Telemetry for for Cluster in project %v", projectID)
-	validateTelemetry(t, projectID, st, ns, src, dest, "server-istio-audit-log")
-}
-
-// TestMetricsAudit validates Audit metrics and logs are collected in stackdriver with filter server-istio-audit-log.
-func TestMetricsAudit(t *testing.T) {
-	framework.NewTest(t).
-		Features("installation.multicluster.multimaster_telemetry").
-		Run(func(ctx framework.TestContext) {
-			ctx.NewSubTest("telemetry_asm").
-				Run(func(ctx framework.TestContext) {
-					clusters := ctx.Environment().Clusters()
-					cluster1, cluster2 := clusters[0], clusters[1]
-					srcAudit := appCtx.LBEchos.GetOrFail(ctx, echo.InCluster(cluster1))
-					destAudit := appCtx.LBEchos.GetOrFail(ctx, echo.InCluster(cluster2))
-
-					ns := appCtx.Namespace.Name()
-					args := map[string]string{
-						"Namespace": ns,
-					}
-					policies := tmpl.EvaluateAllOrFail(t, args, file.AsStringOrFail(t, auditPolicyForLogEntry))
-					ctx.Config().ApplyYAMLOrFail(t, ns, policies...)
-					defer ctx.Config().DeleteYAMLOrFail(t, ns, policies...)
-					t.Logf("Audit policy deployed to namespace %v", ns)
-
-					const (
-						requestCount = 100
-					)
-
-					retry.UntilSuccessOrFail(t, func() error {
-						r, err := srcAudit.Call(echo.CallOptions{
-							Count:    requestCount,
-							Target:   destAudit,
-							PortName: "http",
-							Headers: map[string][]string{
-								"Host": {destAudit.Config().FQDN()},
-							},
-							Message: t.Name(),
-						})
-						if err != nil {
-							return err
-						}
-						if err := r.CheckOK(); err != nil {
-							return err
-						}
-						return nil
-					})
-					testStackdriverAudit(t, appCtx.Namespace, srcAudit, destAudit)
-				})
-		})
-}
-
-func validateMetrics(t *testing.T, projectID string, sd *stackdriver.Instance, ns namespace.Instance, src, dest echo.Instance,
+func validateMetrics(t framework.TestContext, projectID string, sd *stackdriver.Instance, ns namespace.Instance, src, dest echo.Instance,
 	startTime, endTime string, param *stackdriver.ResourceFilterParam) error {
 	filter := fmt.Sprintf("%s AND metric.type = %q", param, "istio.io/service/server/request_count")
 
@@ -232,10 +209,10 @@ func validateMetrics(t *testing.T, projectID string, sd *stackdriver.Instance, n
 	return err
 }
 
-func validateLog(t *testing.T, projectID string, sd *stackdriver.Instance, src, dest echo.Instance,
+func validateLog(ctx framework.TestContext, projectID string, sd *stackdriver.Instance, src, dest echo.Instance,
 	param *stackdriver.ResourceFilterParam, startTime string, filter string) {
 	lf := fmt.Sprintf("%s AND timestamp > %q AND logName=\"projects/%s/logs/%s\"", param, startTime, projectID, filter)
-	sd.CheckForLogEntry(context.Background(), t, lf, projectID, map[string]string{
+	sd.CheckForLogEntry(context.Background(), ctx, lf, projectID, map[string]string{
 		"source_workload":      fmt.Sprintf("%s-v1", src.Config().Service),
 		"destination_workload": fmt.Sprintf("%s-v1", dest.Config().Service),
 	})
