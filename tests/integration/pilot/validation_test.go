dc280ec8359b6a0f5aceea23e170ab3cfe4d88be
John Howard <howardjohn@google.com>
2021-02-26 15:19:02 -0800

    Add analyzer for webhook issues (#31069)
    * Initial generation
    
    * Add the analyzer
    
    * add test
    
    * add banner
    
    * fix test
    
    * release note and lint
    
    * fix description

--- tests/integration/pilot/validation_test.go
+++ tests/integration/pilot/validation_test.go
@@ -147,6 +147,7 @@ var ignoredCRDs = []string{
 	"/v1/Service",
 	"/v1/ConfigMap",
 	"apiextensions.k8s.io/v1/CustomResourceDefinition",
+	"admissionregistration.k8s.io/v1/MutatingWebhookConfiguration",
 	"apps/v1/Deployment",
 	"extensions/v1beta1/Ingress",
 }
