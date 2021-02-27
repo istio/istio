487a231e24716cb01a7cef494e3a32080f1fa584
John Howard <howardjohn@google.com>
2021-02-27 02:15:08 -0800

    use gRPC errors codes in Istiod (#31124)
    To give better visibility into type of errors
    
    (cherry picked from commit 28d1ba0f0bf3b23934f485217f47049ca11f7433)

--- pilot/pkg/xds/ads.go
+++ pilot/pkg/xds/ads.go
@@ -15,7 +15,6 @@
 package xds
 
 import (
-	"errors"
 	"fmt"
 	"io"
 	"strconv"
@@ -160,7 +159,7 @@ func (s *DiscoveryServer) receive(con *Connection, reqChannel chan *discovery.Di
 		if firstReq {
 			firstReq = false
 			if req.Node == nil || req.Node.Id == "" {
-				*errP = errors.New("missing node ID")
+				*errP = status.New(codes.InvalidArgument, "missing node ID").Err()
 				return
 			}
 			// TODO: We should validate that the namespace in the cert matches the claimed namespace in metadata.
@@ -241,7 +240,7 @@ func (s *DiscoveryServer) Stream(stream DiscoveryStream) error {
 	// ip tables update latencies.
 	// See https://github.com/istio/istio/issues/25495.
 	if !s.IsServerReady() {
-		return errors.New("server is not ready to serve discovery information")
+		return status.Error(codes.Unavailable, "server is not ready to serve discovery information")
 	}
 
 	ctx := stream.Context()
@@ -265,7 +264,7 @@ func (s *DiscoveryServer) Stream(stream DiscoveryStream) error {
 		// Error accessing the data - log and close, maybe a different pilot replica
 		// has more luck
 		adsLog.Warnf("Error reading config %v", err)
-		return err
+		return status.Error(codes.Unavailable, "error reading config")
 	}
 	con := newConnection(peerAddr, stream)
 	con.Identities = ids
@@ -479,7 +478,7 @@ func (s *DiscoveryServer) initConnection(node *core.Node, con *Connection) error
 		id, err := checkConnectionIdentity(con)
 		if err != nil {
 			adsLog.Warnf("Unauthorized XDS: %v with identity %v: %v", con.PeerAddr, con.Identities, err)
-			return fmt.Errorf("authorization failed: %v", err)
+			return status.Newf(codes.PermissionDenied, "authorization failed: %v", err).Err()
 		}
 		con.proxy.VerifiedIdentity = id
 	}
