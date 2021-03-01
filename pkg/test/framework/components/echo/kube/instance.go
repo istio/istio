1b5e586f04bccb61f2e87956ee154d8926de2a52
Sam Naser <samnaser@google.com>
2021-03-01 09:08:39 -0500

    send aggregate calls from echo instance

--- pkg/test/framework/components/echo/kube/instance.go
+++ pkg/test/framework/components/echo/kube/instance.go
@@ -20,6 +20,8 @@ import (
 	"io"
 	"time"
 
+	"github.com/hashicorp/go-multierror"
+
 	kubeCore "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
@@ -140,11 +142,7 @@ func (c *instance) Config() echo.Config {
 }
 
 func (c *instance) Call(opts echo.CallOptions) (appEcho.ParsedResponses, error) {
-	out, err := common.ForwardEcho(c.cfg.Service, c.firstClient, &opts, false)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
+	return c.aggregateResponses(opts, true)
 }
 
 func (c *instance) CallOrFail(t test.Failer, opts echo.CallOptions) appEcho.ParsedResponses {
@@ -158,11 +156,7 @@ func (c *instance) CallOrFail(t test.Failer, opts echo.CallOptions) appEcho.Pars
 
 func (c *instance) CallWithRetry(opts echo.CallOptions,
 	retryOptions ...retry.Option) (appEcho.ParsedResponses, error) {
-	out, err := common.ForwardEcho(c.cfg.Service, c.firstClient, &opts, true, retryOptions...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
+	return c.aggregateResponses(opts, true, retryOptions...)
 }
 
 func (c *instance) CallWithRetryOrFail(t test.Failer, opts echo.CallOptions,
@@ -204,4 +198,38 @@ func (c *instance) Restart() error {
 
 		return nil
 	}, retry.Timeout(c.cfg.ReadinessTimeout), startDelay)
+}
+
+func (c *instance) aggregateResponses(opts echo.CallOptions, retry bool, retryOptions ...retry.Option) (appEcho.ParsedResponses, error) {
+	resps := make([]*appEcho.ParsedResponse, 0)
+	var aggErr error
+	for _, w := range c.workloadMgr.workloads {
+		out, err := common.ForwardEcho(c.cfg.Service, w.Client, &opts, retry, retryOptions...)
+		if err != nil {
+			aggErr = multierror.Append(err, aggErr)
+			continue
+		}
+		for _, r := range out {
+			resps = append(resps, r)
+		}
+	}
+	if aggErr != nil {
+		return nil, aggErr
+	}
+
+	c.summarize(resps, opts)
+	return resps, nil
+}
+
+func (c *instance) summarize(responses appEcho.ParsedResponses, opts echo.CallOptions) {
+	fmt.Println("-=-=-=-=")
+	if opts.Target == nil {
+		fmt.Printf("Received: %d responses for %s->%s\n", len(responses), c.cfg.Service, opts.Target)
+	} else {
+		fmt.Printf("Received: %d responses for %s->%s\n", len(responses), c.cfg.Service, opts.Target.Address())
+	}
+	for i, r := range responses {
+		fmt.Printf("    %d: HOST->%s, RESPONSE->%s\n", i, r.Hostname, r.Code)
+	}
+	fmt.Println("-=-=-=-=")
 }
