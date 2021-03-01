e7d1a0173f4f4dcd134dfc15e97643c27bed7fb9
Sam Naser <samnaser@google.com>
2021-03-01 15:21:09 -0500

    wording improvement in min version test skip message

--- pkg/test/framework/test.go
+++ pkg/test/framework/test.go
@@ -244,7 +244,7 @@ func (t *testImpl) doRun(ctx *testContext, fn func(ctx TestContext), parallel bo
 	if t.minIstioVersion != "" {
 		if resource.Version(t.minIstioVersion).Compare(t.ctx.Settings().Versions.Minimum()) > 0 {
 			ctx.Done()
-			t.goTest.Skipf("Skipping %q: running with min Istio version %s, test requires %s",
+			t.goTest.Skipf("Skipping %q: running with min Istio version %q, test requires at least %s",
 				t.goTest.Name(), t.ctx.Settings().Versions.Minimum(), t.minIstioVersion)
 		}
 	}
