c623921f70692708a2991c799432468c41945ae8
Eric Van Norman <ericvn@us.ibm.com>
2021-03-04 13:59:14 -0600

    Change to update buildx-gen.sh

--- tools/build-base-images.sh
+++ tools/build-base-images.sh
@@ -19,7 +19,7 @@
 
 set -ex
 
-HUBS="${HUBS:?specify a comma seperated list of hubs}"
+HUBS="${HUBS:?specify a space seperated list of hubs}"
 TAG="${TAG:?specify a tag}"
 
 # For multi architecture building:
@@ -29,21 +29,4 @@ TAG="${TAG:?specify a tag}"
 # * export DOCKER_ARCHITECTURES="linux/amd64,linux/arm64"
 # Note: if you already have a container builder before running the qemu setup you will need to restart them
 
-index=0
-DOCKER_TARGETS="docker.base docker.distroless docker.app_sidecar_base_debian_9 docker.app_sidecar_base_debian_10 docker.app_sidecar_base_ubuntu_xenial docker.app_sidecar_base_ubuntu_bionic docker.app_sidecar_base_ubuntu_focal docker.app_sidecar_base_centos_7 docker.app_sidecar_base_centos_8"
-
-for hub in ${HUBS//,/ }  # Iterate over comma seperated hubs
-do
- if (( index == 0 )) ; then # For first HUB, build and push the images
-    hubBuilt=${hub}
-    HUB=${hub} BUILDX_BAKE_EXTRA_OPTIONS="--no-cache --pull" DOCKER_TARGETS=${DOCKER_TARGETS} make dockerx.pushx
-  else
-    for target in ${DOCKER_TARGETS// / } # For successive HUBs, iterate of the images, retagging for the HUB and pushing
-    do
-      image=${target#*.}
-      docker tag "${hubBuilt}"/"${image}":"${TAG}" "${hub}"/"${image}":"${TAG}"
-      docker push "${hub}"/"${image}":"${TAG}"
-    done
-  fi
-  ((index=index+1))
-done
+BUILDX_BAKE_EXTRA_OPTIONS="--no-cache --pull" DOCKER_TARGETS="docker.base docker.distroless docker.app_sidecar_base_debian_9 docker.app_sidecar_base_debian_10 docker.app_sidecar_base_ubuntu_xenial docker.app_sidecar_base_ubuntu_bionic docker.app_sidecar_base_ubuntu_focal docker.app_sidecar_base_centos_7 docker.app_sidecar_base_centos_8" make dockerx.pushx
