c197c9aca7d443233b298db2a413b9a086291809
John Howard <howardjohn@google.com>
2021-03-04 11:13:48 -0800

    Add runtime assertions to istiod (#31045)

--- pilot/pkg/features/pilot.go
+++ pilot/pkg/features/pilot.go
@@ -424,4 +424,13 @@ var (
 	StripHostPort = env.RegisterBoolVar("ISTIO_GATEWAY_STRIP_HOST_PORT", false,
 		"If enabled, Gateway will remove any port from host/authority header "+
 			"before any processing of request by HTTP filters or routing.").Get()
+
+	// EnableUnsafeAssertions enables runtime checks to test assertions in our code. This should never be enabled in
+	// production; when assertions fail Istio will panic.
+	EnableUnsafeAssertions = env.RegisterBoolVar(
+		"PILOT_ENABLE_UNSAFE_RUNTIME_ASSERTIONS",
+		false,
+		"If enabled, addition runtime asserts will be performed. "+
+			"These checks are both expensive and panic on failure. As a result, this should be used only for testing.",
+	).Get()
 )
