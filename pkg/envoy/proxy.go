f0f8f4652729b1e3a7817d377b41c7b07d477038
John Howard <howardjohn@google.com>
2021-03-08 14:06:29 -0800

    Disable envoy hot restart
    Part of https://github.com/istio/istio/issues/31138
    
    This is generally to simplify running multiple Envoy's on one pod. It
    may have the side effect of reducing the work Envoy needs to do by
    disablnig hot restart. It also drops privileges required by Envoy (write
    access to /dev/shm).
    
    We don't use hot restart today. A user with custom fork might, in which
    case they can either comment these two lines out or send a PR upstream
    to make this opt-in. Until then, I think we can just hard disable it as
    I believe no users are using this without a fork.

--- pkg/envoy/proxy.go
+++ pkg/envoy/proxy.go
@@ -117,6 +117,8 @@ func (e *envoy) args(fname string, epoch int, bootstrapConfig string) []string {
 		"--service-node", e.Node,
 		"--local-address-ip-version", proxyLocalAddressType,
 		"--bootstrap-version", "3",
+		"--use-dynamic-base-id", // Ensure we don't conflict with other Envoys in the same process namespace
+		"--disable-hot-restart", // We don't use it, so disable it to simplify Envoy's logic
 	}
 	if e.ProxyConfig.LogAsJSON {
 		startupArgs = append(startupArgs,
