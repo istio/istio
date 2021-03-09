e0b9c7108835bdc6a1c81f8760eee5c164be19e0
Sam Naser <samnaser@google.com>
2021-03-09 16:26:40 -0500

    Make test framework version-aware (#31150)
    * plumb versions through settings
    
    * send aggregate calls from echo instance
    
    * add test for versions
    
    * add test for resource versions
    
    * make tests skippable per Istio version
    
    * deploy echo instances per revision
    
    * change test framework namespace labeling injection behavior to accomodate object selectors
    
    * pass versions through to deployment generation
    
    * handle empty versions in echo deployment yaml
    
    * fix conditional in IsMultiVersion
    
    * refactor version logic into the Istio component
    
    * add licenses to version files
    
    * move versions back to avoid circular deps
    
    * disable retry for echo single Call
    
    * fix aggregateResponses client fetching behavior
    
    * make gen formatting
    
    * remove debugging functions, add comments to aggregateResponses
    
    * wording improvement in min version test skip message
    
    * naming and comment improvements
    
    * implement String for Version type
    
    * guard against setting --istio.test.revision and --istio.test.versions in same test run
    
    * refactor Version->IstioVersion and Versions->IstioVersions
    
    * refactor ToRevision to RevisionLabels
    
    * improve Versions setting comment
    
    * fix missed Version rename
    
    * refactor Versions->IstioVersions and IsCrossVersion->IsMultiVersion
    
    * Refactor --istio.test.versions to --istio.test.revVerMap
    
    * fix rendering deployment YAML with empty RevVerMap
    
    * naming fixes

--- pkg/test/framework/components/echo/kube/deployment.go
+++ pkg/test/framework/components/echo/kube/deployment.go
@@ -89,13 +89,19 @@ spec:
 `
 
 	deploymentYAML = `
+{{- $revVerMap := .IstioVersions }}
 {{- $subsets := .Subsets }}
 {{- $cluster := .Cluster }}
 {{- range $i, $subset := $subsets }}
+{{- range $revision, $version := $revVerMap }}
 apiVersion: apps/v1
 kind: Deployment
 metadata:
+{{- if $.IsMultiVersion }}
+  name: {{ $.Service }}-{{ $subset.Version }}-{{ $revision }}
+{{- else }}
   name: {{ $.Service }}-{{ $subset.Version }}
+{{- end }}
 spec:
   replicas: 1
   selector:
@@ -110,6 +116,9 @@ spec:
       labels:
         app: {{ $.Service }}
         version: {{ $subset.Version }}
+{{- if $.IsMultiVersion }}
+        istio.io/rev: {{ $revision }}
+{{- end }}
 {{- if ne $.Locality "" }}
         istio-locality: {{ $.Locality }}
 {{- end }}
@@ -234,9 +243,10 @@ spec:
       - configMap:
           name: {{ $.Service }}-certs
         name: custom-certs
-{{- end}}
+{{- end }}
 ---
-{{- end}}
+{{- end }}
+{{- end }}
 {{- if .TLSSettings }}
 apiVersion: v1
 kind: ConfigMap
@@ -444,7 +454,7 @@ func newDeployment(ctx resource.Context, cfg echo.Config) (*deployment, error) {
 		}
 	}
 
-	deploymentYAML, err := generateDeploymentYAML(cfg, nil)
+	deploymentYAML, err := generateDeploymentYAML(cfg, nil, ctx.Settings().IstioVersions)
 	if err != nil {
 		return nil, fmt.Errorf("failed generating echo deployment YAML for %s/%s",
 			cfg.Namespace.Name(),
@@ -541,8 +551,8 @@ spec:
 `, name, podIP, sa, network, service, version)
 }
 
-func generateDeploymentYAML(cfg echo.Config, settings *image.Settings) (string, error) {
-	params, err := templateParams(cfg, settings)
+func generateDeploymentYAML(cfg echo.Config, settings *image.Settings, versions resource.RevVerMap) (string, error) {
+	params, err := templateParams(cfg, settings, versions)
 	if err != nil {
 		return "", err
 	}
@@ -556,7 +566,7 @@ func generateDeploymentYAML(cfg echo.Config, settings *image.Settings) (string, 
 }
 
 func GenerateService(cfg echo.Config) (string, error) {
-	params, err := templateParams(cfg, nil)
+	params, err := templateParams(cfg, nil, resource.RevVerMap{})
 	if err != nil {
 		return "", err
 	}
@@ -566,7 +576,7 @@ func GenerateService(cfg echo.Config) (string, error) {
 
 const DefaultVMImage = "app_sidecar_ubuntu_bionic"
 
-func templateParams(cfg echo.Config, settings *image.Settings) (map[string]interface{}, error) {
+func templateParams(cfg echo.Config, settings *image.Settings, versions resource.RevVerMap) (map[string]interface{}, error) {
 	if settings == nil {
 		var err error
 		settings, err = image.SettingsFromCommandLine()
@@ -620,6 +630,8 @@ func templateParams(cfg echo.Config, settings *image.Settings) (map[string]inter
 		},
 		"StartupProbe":    supportStartupProbe,
 		"IncludeExtAuthz": cfg.IncludeExtAuthz,
+		"IstioVersions":   versions.TemplateMap(),
+		"IsMultiVersion":  versions.IsMultiVersion(),
 	}
 	return params, nil
 }
