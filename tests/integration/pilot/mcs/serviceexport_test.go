d2625e3f1562109d4ed76c07d68fcbd45d777ea6
Joe Peacock <joe.peacock.joe@gmail.com>
2021-03-09 15:19:49 -0800

    integration test iteration

--- tests/integration/pilot/mcs/serviceexport_test.go
+++ tests/integration/pilot/mcs/serviceexport_test.go
@@ -24,7 +24,7 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	mcsapisClient "sigs.k8s.io/mcs-api/pkg/client/clientset/versioned"
 
@@ -120,12 +120,15 @@ func TestServiceExports(t *testing.T) {
 				t.Fatalf("Failed during service setup with error %v", err)
 			}
 
+			log.Infof("Service Setup Done")
+
+			serviceExports, err := mcsapis.MulticlusterV1alpha1().ServiceExports("").List(context.TODO(), v1.ListOptions{})
+			log.Info("Listing All ServiceExports")
+			for export := range serviceExports.Items {
+				log.Infof("%v \n", export)
+			}
+
 			retry.UntilSuccessOrFail(t, func() error {
-				serviceExports, err := mcsapis.MulticlusterV1alpha1().ServiceExports("").List(context.TODO(), v1.ListOptions{})
-				log.Info("Listing All ServiceExports")
-				for export := range serviceExports.Items {
-					log.Infof("%v \n", export)
-				}
 
 				serviceExport, err := mcsapis.MulticlusterV1alpha1().ServiceExports("svc-namespace").Get(context.TODO(), "svc1", v1.GetOptions{})
 				if err != nil {
