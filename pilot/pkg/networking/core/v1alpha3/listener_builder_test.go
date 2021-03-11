71619cebc7c10b14c190e6cc175c37036315e6c1
John Howard <howardjohn@google.com>
2021-03-10 16:14:28 -0800

    Fix listener filter predicate
    Note: this impacts tests only
    
    The logic for "Not" was implemented wrong (and more complicated than it
    needed to be!). This doesn't impact current tests but impacts some I am
    working on and wanted to submit it in isolation.

--- pilot/pkg/networking/core/v1alpha3/listener_builder_test.go
+++ pilot/pkg/networking/core/v1alpha3/listener_builder_test.go
@@ -708,7 +708,7 @@ func TestInboundListenerFilters(t *testing.T) {
 func evaluateListenerFilterPredicates(t testing.TB, predicate *listener.ListenerFilterChainMatchPredicate, expected map[int]bool) {
 	t.Helper()
 	for port, expect := range expected {
-		got := xdstest.EvaluateListenerFilterPredicates(predicate, false, port)
+		got := xdstest.EvaluateListenerFilterPredicates(predicate, port)
 		if got != expect {
 			t.Errorf("expected port %v to have match=%v, got match=%v", port, expect, got)
 		}
