bffe4ac36b8b775cb0209e92e573b21d70fa4b1d
Pengyuan Bian <bianpengyuan@google.com>
2021-03-12 10:26:27 -0800

    Update gateway port to service port before gateway merging. (#31386)
    * Update gateway port to service port before gateway merging.
    
    * Skip if service prot and target port overlaps.
    
    * update
    
    * update
    
    * add a test case
    
    * release notes.

--- pilot/pkg/model/push_context_test.go
+++ pilot/pkg/model/push_context_test.go
@@ -1527,6 +1527,118 @@ func TestServiceWithExportTo(t *testing.T) {
 	}
 }
 
+func TestMergeGatewayServerPort(t *testing.T) {
+	cases := []struct {
+		name        string
+		gateways    []config.Config
+		services    []*ServiceInstance
+		wantedPorts map[uint32]struct{}
+	}{
+		{
+			name: "basic",
+			gateways: []config.Config{
+				makeConfig("foo", "not-default", "foo.bar.com", "name1", "http", 7, "ingressgateway"),
+			},
+			services: []*ServiceInstance{
+				{
+					Endpoint:    &IstioEndpoint{EndpointPort: 7},
+					ServicePort: &Port{Port: 8},
+				},
+			},
+			wantedPorts: map[uint32]struct{}{8: {}},
+		},
+		{
+			name: "merge",
+			gateways: []config.Config{
+				makeConfig("foo", "not-default", "foo.bar.com", "name1", "http", 7, "ingressgateway"),
+				makeConfig("foo", "not-default", "foo.bar.com", "name2", "http", 8, "ingressgateway"),
+			},
+			services: []*ServiceInstance{
+				{
+					Endpoint:    &IstioEndpoint{EndpointPort: 7},
+					ServicePort: &Port{Port: 8},
+				},
+			},
+			wantedPorts: map[uint32]struct{}{8: {}},
+		},
+		{
+			name: "skip",
+			gateways: []config.Config{
+				makeConfig("foo", "not-default", "foo.bar.com", "name1", "http", 7, "ingressgateway"),
+				makeConfig("foo", "not-default", "foo.bar.com", "name2", "http", 8, "ingressgateway"),
+			},
+			services: []*ServiceInstance{
+				{
+					Endpoint:    &IstioEndpoint{EndpointPort: 7},
+					ServicePort: &Port{Port: 8},
+				},
+				{
+					Endpoint:    &IstioEndpoint{EndpointPort: 6},
+					ServicePort: &Port{Port: 7},
+				},
+			},
+			wantedPorts: map[uint32]struct{}{8: {}, 7: {}},
+		},
+		{
+			name: "service target same",
+			gateways: []config.Config{
+				makeConfig("foo", "not-default", "foo.bar.com", "name1", "http", 7, "ingressgateway"),
+				makeConfig("foo", "not-default", "foo.bar.com", "name2", "http", 8, "ingressgateway"),
+			},
+			services: []*ServiceInstance{
+				{
+					Endpoint:    &IstioEndpoint{EndpointPort: 8},
+					ServicePort: &Port{Port: 8},
+				},
+				{
+					Endpoint:    &IstioEndpoint{EndpointPort: 7},
+					ServicePort: &Port{Port: 7},
+				},
+			},
+			wantedPorts: map[uint32]struct{}{7: {}, 8: {}},
+		},
+		{
+			name: "same service port different targets",
+			gateways: []config.Config{
+				makeConfig("foo", "not-default", "foo.bar.com", "name1", "http", 8, "ingressgateway"),
+				makeConfig("foo", "not-default", "foo.bar.com", "name2", "http", 7, "ingressgateway"),
+			},
+			services: []*ServiceInstance{
+				{
+					Endpoint:    &IstioEndpoint{EndpointPort: 9},
+					ServicePort: &Port{Port: 8},
+				},
+				{
+					Endpoint:    &IstioEndpoint{EndpointPort: 7},
+					ServicePort: &Port{Port: 8},
+				},
+			},
+			wantedPorts: map[uint32]struct{}{8: {}},
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			proxy := &Proxy{
+				Metadata:         &NodeMetadata{Labels: map[string]string{"istio": "ingressgateway"}},
+				ServiceInstances: c.services,
+			}
+			pc := &PushContext{
+				gatewayIndex: gatewayIndex{all: c.gateways},
+			}
+			mg := pc.mergeGateways(proxy)
+			if len(mg.MergedServers) != len(c.wantedPorts) {
+				t.Errorf("number of servers want %v got %v", c.wantedPorts, mg.MergedServers)
+			}
+			for p := range mg.MergedServers {
+				if _, ok := c.wantedPorts[p.Number]; !ok {
+					t.Errorf("server port want %v got %v", c.wantedPorts, p)
+					break
+				}
+			}
+		})
+	}
+}
+
 var _ ServiceDiscovery = &localServiceDiscovery{}
 
 // MockDiscovery is an in-memory ServiceDiscover with mock services
