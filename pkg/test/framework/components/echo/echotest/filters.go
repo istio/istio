5ef93935f023491803895958d74f37f8b121ff68
Steven Landow <steven@stlcomputerservices.com>
2021-03-17 15:08:22 -0700

    make virtual service tests workload agnostic (#31500)
    * make virtual service tests workload agnostic
    
    Change-Id: I7a4da60f9293b7892627385c7d8b26891a5aac97
    
    * filter extraneous test cases
    
    Change-Id: I6523722739cf1cdb163bf2d99dc0a5a996bb0b8d
    
    * template before applying namespace
    
    Change-Id: Iad947bcee30a5f4fbda64ec3f9cf34ceb7a7af1f
    
    * set workload agnostic on cors
    
    Change-Id: Ic358f44bc8e3d65bf400f417d2ded08a2c5518da
    
    * fix traffictest names for agnostic tests
    
    Change-Id: I557c1a75a673e32656e86ea39158a8e817cccc11
    
    * typo
    
    Change-Id: I48b046425c955f6d6152e6f6b1e16712e8c8124c
    
    * add default filters that test pod to different pod
    
    Change-Id: Ib7ea83ae98f2465a68773d532677a9eeba47a5ba
    
    * gen
    
    Change-Id: Id554c33aab3201661a469a89b0f1a7f7c5249aae
    
    * dont test cors to VM
    
    Change-Id: Ida6e4b586e0e043345030b32271838fa3edd1ffa
    
    * fix external svc filter
    
    Change-Id: If0d8e253fe18f0567a8d9a2a4fcb3972c0a9f71b

--- pkg/test/framework/components/echo/echotest/filters.go
+++ pkg/test/framework/components/echo/echotest/filters.go
@@ -17,16 +17,16 @@ package echotest
 import "istio.io/istio/pkg/test/framework/components/echo"
 
 type (
-	simpleFilter      func(echo.Instances) echo.Instances
-	combinationFilter func(from echo.Instance, to echo.Instances) echo.Instances
+	SimpleFilter      func(echo.Instances) echo.Instances
+	CombinationFilter func(from echo.Instance, to echo.Instances) echo.Instances
 )
 
 // From applies each of the filter functions in order to allow removing workloads from the set of clients.
 // Example:
 //     echotest.New(t, apps).
-//       From(echotest.SingleSimplePodBasedService, echotest.NoExternalServices).
+//       From(echotest.SingleSimplePodServiceAndAllSpecial, echotest.NoExternalServices).
 //       Run()
-func (t *T) From(filters ...simpleFilter) *T {
+func (t *T) From(filters ...SimpleFilter) *T {
 	for _, filter := range filters {
 		t.sources = filter(t.sources)
 	}
@@ -36,9 +36,9 @@ func (t *T) From(filters ...simpleFilter) *T {
 // To applies each of the filter functions in order to allow removing workloads from the set of destinations.
 // Example:
 //     echotest.New(t, apps).
-//       To(echotest.SingleSimplePodBasedService).
+//       To(echotest.SingleSimplePodServiceAndAllSpecial).
 //       Run()
-func (t *T) To(filters ...simpleFilter) *T {
+func (t *T) To(filters ...SimpleFilter) *T {
 	for _, filter := range filters {
 		t.destinations = filter(t.destinations)
 	}
@@ -52,9 +52,22 @@ func (t *T) To(filters ...simpleFilter) *T {
 //     echotest.New(t, apps).
 //       ConditionallyTo(echotest.ReachableDestinations).
 //       Run()
-func (t *T) ConditionallyTo(filters ...combinationFilter) *T {
+func (t *T) ConditionallyTo(filters ...CombinationFilter) *T {
 	t.destinationFilters = append(t.destinationFilters, filters...)
 	return t
+}
+
+// WithDefaultFilters applies common filters that work for most tests.
+// Example:
+//   The full set of apps is a, b, c, headless, naked, and vm (one simple pod).
+//   Only a, headless, naked and vm are used as sources.
+//   Subtests are generated only for reachable destinations.
+//   Pod a will not be in destinations, but b will (one simpe pod not in sources)
+func (t *T) WithDefaultFilters() *T {
+	return t.
+		From(SingleSimplePodServiceAndAllSpecial(), Not(ExternalServices)).
+		ConditionallyTo(ReachableDestinations).
+		To(SingleSimplePodServiceAndAllSpecial(t.sources...))
 }
 
 func (t *T) applyCombinationFilters(from echo.Instance, to echo.Instances) echo.Instances {
@@ -64,21 +77,26 @@ func (t *T) applyCombinationFilters(from echo.Instance, to echo.Instances) echo.
 	return to
 }
 
-// SingleSimplePodBasedService finds the first Pod deployment that has a sidecar and doesn't use a headless service and removes all
+// SingleSimplePodServiceAndAllSpecial finds the first Pod deployment that has a sidecar and doesn't use a headless service and removes all
 // other "regular" pods that aren't part of the same Service. Pods that are part of the same Service but are in a
 // different cluster or revision will still be included.
-func SingleSimplePodBasedService(instances echo.Instances) echo.Instances {
-	return oneRegularPod(instances)
+// Example:
+//     The full set of apps is a, b, c, headless, naked, and vm.
+//     The plain-pods are a, b and c.
+//     This filter would result in a, headless, naked and vm.
+// TODO this name is not good
+func SingleSimplePodServiceAndAllSpecial(exclude ...echo.Instance) SimpleFilter {
+	return func(instances echo.Instances) echo.Instances {
+		return oneRegularPod(instances, exclude)
+	}
 }
 
-// NoExternalServices filters out external services which are based on
-func NoExternalServices(instances echo.Instances) echo.Instances {
-	return instances.Match(echo.IsExternal().Negate())
-}
-
-func oneRegularPod(instances echo.Instances) echo.Instances {
+func oneRegularPod(instances echo.Instances, exclude echo.Instances) echo.Instances {
 	regularPods := instances.Match(isRegularPod)
 	others := instances.Match(echo.Not(isRegularPod))
+	for _, exclude := range exclude {
+		regularPods = regularPods.Match(echo.Not(echo.SameDeployment(exclude)))
+	}
 	if len(regularPods) == 0 {
 		return others
 	}
@@ -93,11 +111,32 @@ func isRegularPod(instance echo.Instance) bool {
 	return !c.IsVM() && len(c.Subsets) == 1 && !c.IsNaked() && !c.IsHeadless()
 }
 
+// Not includes all workloads that don't match the given filter
+func Not(filter SimpleFilter) SimpleFilter {
+	return func(instances echo.Instances) echo.Instances {
+		filtered := filter(instances)
+		inverted := instances.Match(func(instance echo.Instance) bool {
+			return !filtered.Contains(instance)
+		})
+		return inverted
+	}
+}
+
+// VirtualMachines includes VM deployments
+func VirtualMachines(instances echo.Instances) echo.Instances {
+	return instances.Match(echo.IsVirtualMachine())
+}
+
+// ExternalServices includes services that are based on naked pods with a custom DefaultHostHeader
+func ExternalServices(instances echo.Instances) echo.Instances {
+	return instances.Match(echo.IsExternal())
+}
+
 // ReachableDestinations filters out known-unreachable destinations given a source.
 // - from a naked pod, we can't reach cross-network endpoints or VMs
 // - we can't reach cross-cluster headless endpoints
 // - from an injected Pod, only non-naked cross-network endpoints are reachable
-var ReachableDestinations combinationFilter = func(from echo.Instance, to echo.Instances) echo.Instances {
+var ReachableDestinations CombinationFilter = func(from echo.Instance, to echo.Instances) echo.Instances {
 	return to.Match(fromNaked(from).
 		And(fromVM(from)).
 		And(toSameNetworkNaked(from)).
