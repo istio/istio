50060ca2f0dddc66a3185bf4d061948714a06125
tianqian.zyf <tianqian.zyf@alibaba-inc.com>
2021-03-22 10:32:37 +0800

    Fix spelling errors

--- pilot/pkg/networking/core/v1alpha3/envoyfilter/listener_patch.go
+++ pilot/pkg/networking/core/v1alpha3/envoyfilter/listener_patch.go
@@ -449,7 +449,7 @@ func doHTTPFilterListOperation(patchContext networking.EnvoyFilter_PatchContext,
 				hcm.HttpFilters[insertPosition] = clonedVal
 			}
 		} else if cp.Operation == networking.EnvoyFilter_Patch_INSERT_BEFORE || cp.Operation == networking.EnvoyFilter_Patch_INSERT_FIRST {
-			// insert before without a filter match is same as insert in the beginning
+			// insert before/first without a filter match is same as insert in the beginning
 			if !hasHTTPFilterMatch(cp) {
 				hcm.HttpFilters = append([]*http_conn.HttpFilter{proto.Clone(cp.Value).(*http_conn.HttpFilter)}, hcm.HttpFilters...)
 				continue
