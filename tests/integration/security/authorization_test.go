3e22c1823c5d511afad4e04ec20ac86a1ac8e083
Jimmy Chen <28548492+JimmyCYJ@users.noreply.github.com>
2021-03-23 10:42:51 -0700

    Unskip authz tests for multi-cluster (#31620)
    * unskip authz tests for multi-cluster
    
    * skip deny test

--- tests/integration/security/authorization_test.go
+++ tests/integration/security/authorization_test.go
@@ -309,7 +309,8 @@ func TestAuthorization_Deny(t *testing.T) {
 	framework.NewTest(t).
 		Features("security.authorization.deny-action").
 		Run(func(ctx framework.TestContext) {
-			// TODO: Convert into multicluster support. Currently 503 is received cross-network
+			// TODO: Convert into multicluster support. Currently reachability does
+			// not cover all clusters.
 			if ctx.Clusters().IsMulticluster() {
 				ctx.Skip()
 			}
@@ -479,10 +480,6 @@ func TestAuthorization_IngressGateway(t *testing.T) {
 	framework.NewTest(t).
 		Features("security.authorization.ingress-gateway").
 		Run(func(ctx framework.TestContext) {
-			// TODO: finish convert all authorization tests into multicluster supported
-			if ctx.Clusters().IsMulticluster() {
-				ctx.Skip()
-			}
 			ns := namespace.NewOrFail(t, ctx, namespace.Config{
 				Prefix: "v1beta1-ingress-gateway",
 				Inject: true,
@@ -621,10 +618,6 @@ func TestAuthorization_EgressGateway(t *testing.T) {
 	framework.NewTest(t).
 		Features("security.authorization.egress-gateway").
 		Run(func(ctx framework.TestContext) {
-			// TODO: Convert into multicluster support. Currently 503 is received
-			if ctx.Clusters().IsMulticluster() {
-				ctx.Skip()
-			}
 			ns := namespace.NewOrFail(t, ctx, namespace.Config{
 				Prefix: "v1beta1-egress-gateway",
 				Inject: true,
@@ -798,10 +791,6 @@ func TestAuthorization_TCP(t *testing.T) {
 	framework.NewTest(t).
 		Features("security.authorization.tcp").
 		Run(func(ctx framework.TestContext) {
-			// TODO: finish convert all authorization tests into multicluster supported
-			if ctx.Clusters().IsMulticluster() {
-				ctx.Skip()
-			}
 			ns := namespace.NewOrFail(t, ctx, namespace.Config{
 				Prefix: "v1beta1-tcp-1",
 				Inject: true,
@@ -1072,10 +1061,6 @@ func TestAuthorization_GRPC(t *testing.T) {
 	framework.NewTest(t).
 		Features("security.authorization.grpc-protocol").
 		Run(func(ctx framework.TestContext) {
-			// TODO: finish convert all authorization tests into multicluster supported
-			if ctx.Clusters().IsMulticluster() {
-				ctx.Skip()
-			}
 			ns := namespace.NewOrFail(t, ctx, namespace.Config{
 				Prefix: "v1beta1-grpc",
 				Inject: true,
