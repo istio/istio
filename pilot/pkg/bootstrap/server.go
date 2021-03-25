9c59e5170274459194ccb6e4796f3664f258edfd
Shankar Ganesan <shankgan@google.com>
2021-03-25 16:24:51 +0000

    Finalizing e2e tests
    Change-Id: I51fedfc7456493723a6b76bbcc2901476f7a451c

--- pilot/pkg/bootstrap/server.go
+++ pilot/pkg/bootstrap/server.go
@@ -25,6 +25,7 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"path"
 	"strings"
 	"sync"
 	"time"
@@ -36,6 +37,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/reflection"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	v1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
@@ -1113,10 +1115,49 @@ func (s *Server) addIstioCAToTrustBundle(args *PilotArgs) error {
 			Source:            tb.SourceIstioCA,
 		})
 		if err != nil {
-			log.Errorf("unable to add CA root from namespace %s as trustAnchor", args.Namespace)
+			log.Errorf("unable to add CA root from namespace %s as trustAnchor: %v", args.Namespace, err)
 			return err
 		}
 		return nil
+	}
+
+	// If CA is not running, derive root certificates from the configured CA secrets
+	signingKeyFile := path.Join(LocalCertDir.Get(), "ca-key.pem")
+	if _, err := os.Stat(signingKeyFile); err != nil && s.kubeClient != nil {
+		// Fetch self signed certificates
+		caSecret, err := s.kubeClient.CoreV1().Secrets(args.Namespace).
+			Get(context.TODO(), ca.CASecret, metav1.GetOptions{})
+		if err != nil {
+			log.Infof("unable to retrieve self signed CA secret: %v", err)
+			return nil
+		}
+		rootCertBytes := caSecret.Data[ca.CaCertID]
+		err = s.workloadTrustBundle.UpdateTrustAnchor(&tb.TrustAnchorUpdate{
+			TrustAnchorConfig: tb.TrustAnchorConfig{Certs: []string{string(rootCertBytes)}},
+			Source:            tb.SourceIstioCA,
+		})
+		if err != nil {
+			log.Errorf("unable to update trustbundle with self signed CA root: %v", err)
+			return err
+		}
+	} else {
+		// If NOT self signed certificates
+		rootCertFile := path.Join(LocalCertDir.Get(), "root-cert.pem")
+		if _, err := os.Stat(rootCertFile); err != nil {
+			rootCertFile = path.Join(LocalCertDir.Get(), "ca-cert.pem")
+		}
+		certBytes, err := ioutil.ReadFile(rootCertFile)
+		if err != nil {
+			return nil
+		}
+		err = s.workloadTrustBundle.UpdateTrustAnchor(&tb.TrustAnchorUpdate{
+			TrustAnchorConfig: tb.TrustAnchorConfig{Certs: []string{string(certBytes)}},
+			Source:            tb.SourceIstioCA,
+		})
+		if err != nil {
+			log.Errorf("unable to update trustbundle with plugin CA root: %v", err)
+			return err
+		}
 	}
 	return nil
 }
--- tests/integration/security/ca_migration/main_test.go
+++ tests/integration/security/ca_migration/main_test.go
@@ -16,7 +16,13 @@
 package migrationca
 
 import (
+	"context"
+	"fmt"
 	"testing"
+	"time"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"istio.io/istio/pkg/test/echo/common/scheme"
 	"istio.io/istio/pkg/test/framework"
@@ -33,42 +39,38 @@ import (
 var inst istio.Instance
 
 const (
-	ASvc = "a"
-	BSvc = "b"
+	ASvc            = "a"
+	BSvc            = "b"
+	IstioCARevision = "asm-revision-istiodca"
+	MeshCARevision  = "asm-revision-meshca"
 )
 
-func checkConnectivity(t *testing.T, ctx framework.TestContext, a echo.Instances, b echo.Instances) {
+func checkConnectivity(t *testing.T, ctx framework.TestContext, a echo.Instances, b echo.Instances, testPrefix string) {
 	t.Helper()
-
-	callOptions := echo.CallOptions{
-		Target:   b[0],
-		PortName: "http",
-		Scheme:   scheme.HTTP,
-		Count:    1,
-	}
-	checker := connection.Checker{
-		From:          a[0],
-		Options:       callOptions,
-		ExpectSuccess: true,
-		DestClusters:  b.Clusters(),
-	}
-	checker.CheckOrFail(ctx)
-
-	callOptions = echo.CallOptions{
-		Target:   a[0],
-		PortName: "http",
-		Scheme:   scheme.HTTP,
-		Count:    1,
-	}
-	checker = connection.Checker{
-		From:          b[0],
-		Options:       callOptions,
-		ExpectSuccess: true,
-		DestClusters:  a.Clusters(),
-	}
-	checker.CheckOrFail(ctx)
+	ctx.NewSubTest(testPrefix).Run(func(ctx framework.TestContext) {
+		srcList := []echo.Instance{a[0], b[0]}
+		dstList := []echo.Instance{b[0], a[0]}
+		for index := range srcList {
+			src := srcList[index]
+			dst := dstList[index]
+			callOptions := echo.CallOptions{
+				Target:   dst,
+				PortName: "http",
+				Scheme:   scheme.HTTP,
+				Count:    1,
+			}
+			checker := connection.Checker{
+				From:          src,
+				Options:       callOptions,
+				ExpectSuccess: true,
+				DestClusters:  b.Clusters(),
+			}
+			checker.CheckOrFail(ctx)
+		}
+	})
 }
 
+// TestIstiodToMeshCAMigration: test zero downtime migration from Istiod CA to Google Mesh CA
 func TestIstiodToMeshCAMigration(t *testing.T) {
 	framework.NewTest(t).
 		RequiresSingleCluster().
@@ -77,16 +79,18 @@ func TestIstiodToMeshCAMigration(t *testing.T) {
 			oldCaNamespace := namespace.NewOrFail(t, ctx, namespace.Config{
 				Prefix:   "citadel",
 				Inject:   true,
-				Revision: "asm-revision-istiodca",
+				Revision: IstioCARevision,
 			})
 
 			newCaNamespace := namespace.NewOrFail(t, ctx, namespace.Config{
 				Prefix:   "meshca",
 				Inject:   true,
-				Revision: "asm-revision-meshca",
+				Revision: MeshCARevision,
 			})
 
 			builder := echoboot.NewBuilder(ctx)
+
+			// Create workloads in namespaces served by both CA's
 			builder.
 				WithClusters(ctx.Clusters()...).
 				WithConfig(util.EchoConfig(ASvc, oldCaNamespace, false, nil)).
@@ -101,16 +105,16 @@ func TestIstiodToMeshCAMigration(t *testing.T) {
 			a := echos.Match(echo.Service(ASvc)).Match(echo.InCluster(cluster))
 			b := echos.Match(echo.Service(BSvc)).Match(echo.InCluster(cluster))
 
-			// Test Basic setup
-			checkConnectivity(t, ctx, a, b)
+			// Test Basic setup between workloads served by different CA's
+			checkConnectivity(t, ctx, a, b, "cross-ca-connectivity")
 
 			// Migration tests
 
-			// Annotate oldCANamespace with newer CA revision label.
-			err = oldCaNamespace.SetLabel("istio.io/rev", "asm-revision-meshca")
+			// Annotate oldCANamespace with newer CA control plane revision label.
+			err = oldCaNamespace.SetLabel("istio.io/rev", MeshCARevision)
 			if err != nil {
 				t.Fatalf("unable to annotate namespace %v with label %v: %v",
-					oldCaNamespace.Name(), "asm-revision-meshca", err)
+					oldCaNamespace.Name(), MeshCARevision, err)
 			}
 
 			// Restart workload A to allow mutating webhook to do its job
@@ -118,10 +122,45 @@ func TestIstiodToMeshCAMigration(t *testing.T) {
 				t.Fatalf("revisioned instance rollout failed with: %v", err)
 			}
 
-			// Check mTLS works
-			checkConnectivity(t, ctx, a, b)
+			// Check mTLS connectivity works after workload A is signed by meshca
+			checkConnectivity(t, ctx, a, b, "same-ca-connectivity")
 
-			// TODO: Handle removal of root of trust
+			// Handle removal of root of trust
+			systemNs, err := istio.ClaimSystemNamespace(ctx)
+			if err != nil {
+				t.Fatalf("unable to retrieve istio-system namespace: %v", err)
+			}
+
+			err = cluster.CoreV1().Secrets(systemNs.Name()).Delete(context.TODO(), "istio-ca-secret", metav1.DeleteOptions{})
+			if err != nil && !errors.IsNotFound(err) {
+				t.Fatalf("unable to delete secret %v from the cluster. Encountered error: %v", "istio-ca-secret", err)
+			}
+			err = cluster.CoreV1().Secrets(systemNs.Name()).Delete(context.TODO(), "cacerts", metav1.DeleteOptions{})
+			if err != nil && !errors.IsNotFound(err) {
+				t.Fatalf("unable to delete secret %v from the cluster. Encountered error: %v", "cacerts", err)
+			}
+
+			// TODO: need better way to determine that secret has been unmounted from istiod file-system in case of cacerts
+			// instead of sleeping
+			time.Sleep(10 * time.Second)
+
+			// Restart MeshCa Control plane to pick up new roots of trust
+			deploymentsClient := cluster.AppsV1().Deployments(systemNs.Name())
+			meshcaDeployment, err := deploymentsClient.Get(context.TODO(), fmt.Sprintf("istiod-%s", MeshCARevision), metav1.GetOptions{})
+			if err != nil {
+				t.Fatalf("unable to retrieve meshca control plane deployment: %v", err)
+			}
+			meshcaDeployment.Spec.Template.ObjectMeta.Annotations["restart"] = "true"
+			_, err = deploymentsClient.Update(context.TODO(), meshcaDeployment, metav1.UpdateOptions{})
+			if err != nil {
+				t.Fatalf("unable to update meshca control plane deployment: %v", err)
+			}
+			// TODO: need better way to rollout and restart meshca deployment and wait until pods are active
+			// also need way to correctly determine if Istiod root has been removed from workload trustbundle
+			time.Sleep(10 * time.Second)
+
+			// Check mTLS works after roots of trust have been removed
+			checkConnectivity(t, ctx, a, b, "post-trust-removal-connectivity")
 		})
 }
 
