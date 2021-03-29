0c5f61354c95db430e4851ebc12f1f166d3d6c16
Rama Chavali <rama.rao@salesforce.com>
2021-03-29 21:34:55 +0530

    fix comments
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/bootstrap/webhook.go
+++ pilot/pkg/bootstrap/webhook.go
@@ -32,7 +32,7 @@ const (
 // TLS handled by a proxy/gateway in front of Istiod.
 func (s *Server) initSecureWebhookServer(args *PilotArgs) {
 	// create the https server for hosting the k8s injectionWebhook handlers.
-	if s.kubeClient == nil || args.ServerOptions.HTTPSAddr == "" {
+	if args.ServerOptions.HTTPSAddr == "" {
 		s.httpsMux = s.httpMux
 		log.Info("HTTPS port is disabled, multiplexing webhooks on the httpAddr ", args.ServerOptions.HTTPAddr)
 		return
