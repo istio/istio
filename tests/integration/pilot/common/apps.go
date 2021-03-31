30a0e10731ad5120ba39b7368d701c199a71d90a
John Howard <howardjohn@google.com>
2021-03-31 09:46:38 -0700

    Fix "HTTPS" port that wasn't actually using TLS (#31792) (#31819)
    (cherry picked from commit 1ceeb7b361f8ebcac9dfcce6f2c6355298d68ff4)

--- tests/integration/pilot/common/apps.go
+++ tests/integration/pilot/common/apps.go
@@ -79,7 +79,7 @@ var EchoPorts = []echo.Port{
 	{Name: "auto-tcp-server", Protocol: protocol.TCP, ServicePort: 9093, InstancePort: 16061, ServerFirst: true},
 	{Name: "auto-http", Protocol: protocol.HTTP, ServicePort: 81, InstancePort: 18081},
 	{Name: "auto-grpc", Protocol: protocol.GRPC, ServicePort: 7071, InstancePort: 17071},
-	{Name: "auto-https", Protocol: protocol.HTTPS, ServicePort: 9443, InstancePort: 19443},
+	{Name: "auto-https", Protocol: protocol.HTTPS, ServicePort: 9443, InstancePort: 19443, TLS: true},
 }
 
 var WorkloadPorts = []echo.WorkloadPort{
