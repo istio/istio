892b143c38c1a34c76c7c8afc1f0257687a5fc44
John Howard <howardjohn@google.com>
2021-03-31 16:24:41 -0700

    do not enable delta by default

--- pilot/pkg/features/pilot.go
+++ pilot/pkg/features/pilot.go
@@ -421,7 +421,7 @@ var (
 			"These checks are both expensive and panic on failure. As a result, this should be used only for testing.",
 	).Get()
 
-	DeltaXds = env.RegisterBoolVar("ISTIO_DELTA_XDS", true,
+	DeltaXds = env.RegisterBoolVar("ISTIO_DELTA_XDS", false,
 		"If enabled, pilot will only send the delta configs as opposed to the state of the world on a "+
 			"Resource Request")
 
