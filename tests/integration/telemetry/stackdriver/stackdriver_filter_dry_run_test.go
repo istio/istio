38d1ef8d41bf0327893d7e7c9eae3ac2a8833db9
Yangmin Zhu <ymzhu@google.com>
2021-04-02 19:08:21 -0700

    add tcp test

--- tests/integration/telemetry/stackdriver/stackdriver_filter_dry_run_test.go
+++ tests/integration/telemetry/stackdriver/stackdriver_filter_dry_run_test.go
@@ -20,10 +20,11 @@ import (
 	"testing"
 
 	"golang.org/x/sync/errgroup"
+
 	"istio.io/istio/pkg/test/framework"
+	"istio.io/istio/pkg/test/framework/components/echo"
 	"istio.io/istio/pkg/test/framework/components/stackdriver"
 	telemetrypkg "istio.io/istio/pkg/test/framework/components/telemetry"
-	"istio.io/istio/pkg/test/scopes"
 	"istio.io/istio/pkg/test/util/file"
 	"istio.io/istio/pkg/test/util/retry"
 	"istio.io/istio/pkg/test/util/tmpl"
@@ -32,8 +33,10 @@ import (
 )
 
 const (
-	dryRunServerLogEntry      = "testdata/security_authz_dry_run/server_access_log.json.tmpl"
-	dryRunAuthorizationPolicy = "testdata/security_authz_dry_run/authorization_policy.yaml.tmpl"
+	dryRunServerLogEntry         = "testdata/security_authz_dry_run/server_access_log.json.tmpl"
+	dryRunTCPServerLogEntry      = "testdata/security_authz_dry_run/tcp_server_access_log.json.tmpl"
+	dryRunAuthorizationPolicy    = "testdata/security_authz_dry_run/authorization_policy.yaml.tmpl"
+	dryRunTCPAuthorizationPolicy = "testdata/security_authz_dry_run/tcp_authorization_policy.yaml.tmpl"
 )
 
 // TestStackdriverAuthzDryRun verifies that stackdriver WASM filter exports dry-run metrics with expected labels.
@@ -41,11 +44,7 @@ func TestStackdriverAuthzDryRun(t *testing.T) {
 	framework.NewTest(t).
 		Features("observability.telemetry.stackdriver").
 		Run(func(ctx framework.TestContext) {
-			ns := getEchoNamespaceInstance()
-			policies := tmpl.EvaluateAllOrFail(t, map[string]string{"Namespace": ns.Name()}, file.AsStringOrFail(t, dryRunAuthorizationPolicy))
-			ctx.Config().ApplyYAMLOrFail(t, ns.Name(), policies...)
-			util.WaitForConfig(ctx, policies[0], ns)
-
+			createDryRunPolicy(t, ctx, dryRunAuthorizationPolicy)
 			g, _ := errgroup.WithContext(context.Background())
 			for _, cltInstance := range clt {
 				cltInstance := cltInstance
@@ -54,14 +53,7 @@ func TestStackdriverAuthzDryRun(t *testing.T) {
 						if err := sendTraffic(t, cltInstance); err != nil {
 							return err
 						}
-						clName := cltInstance.Config().Cluster.Name()
-						trustDomain := telemetry.GetTrustDomain(cltInstance.Config().Cluster, ist.Settings().SystemNamespace)
-						scopes.Framework.Infof("Validating for cluster %s", clName)
-						if err := validateLogs(t, dryRunServerLogEntry, clName, trustDomain, stackdriver.ServerAccessLog); err != nil {
-							return err
-						}
-						t.Logf("logs validated")
-						return nil
+						return verifyAccessLog(t, cltInstance, dryRunServerLogEntry)
 					}, retry.Delay(telemetrypkg.RetryDelay), retry.Timeout(telemetrypkg.RetryTimeout))
 					if err != nil {
 						return err
@@ -73,4 +65,54 @@ func TestStackdriverAuthzDryRun(t *testing.T) {
 				t.Fatalf("test failed: %v", err)
 			}
 		})
+}
+
+// TestTCPStackdriverAuthzDryRun verifies that stackdriver TCP filter works.
+func TestTCPStackdriverAuthzDryRun(t *testing.T) {
+	framework.NewTest(t).
+		Features("observability.telemetry.stackdriver").
+		Run(func(ctx framework.TestContext) {
+			createDryRunPolicy(t, ctx, dryRunTCPAuthorizationPolicy)
+			g, _ := errgroup.WithContext(context.Background())
+			for _, cltInstance := range clt {
+				cltInstance := cltInstance
+				g.Go(func() error {
+					err := retry.UntilSuccess(func() error {
+						_, err := cltInstance.Call(echo.CallOptions{
+							Target:   srv[0],
+							PortName: "tcp",
+							Count:    telemetry.RequestCountMultipler * len(srv),
+						})
+						if err != nil {
+							return err
+						}
+						return verifyAccessLog(t, cltInstance, dryRunTCPServerLogEntry)
+					}, retry.Delay(telemetrypkg.RetryDelay), retry.Timeout(telemetrypkg.RetryTimeout))
+					if err != nil {
+						return err
+					}
+					return nil
+				})
+			}
+			if err := g.Wait(); err != nil {
+				t.Fatalf("test failed: %v", err)
+			}
+		})
+}
+
+func createDryRunPolicy(t *testing.T, ctx framework.TestContext, authz string) {
+	ns := getEchoNamespaceInstance()
+	policies := tmpl.EvaluateAllOrFail(t, map[string]string{"Namespace": ns.Name()}, file.AsStringOrFail(t, authz))
+	ctx.Config().ApplyYAMLOrFail(t, ns.Name(), policies...)
+	util.WaitForConfig(ctx, policies[0], ns)
+}
+
+func verifyAccessLog(t *testing.T, cltInstance echo.Instance, wantLog string) error {
+	t.Logf("Validating for cluster %v", cltInstance.Config().Cluster)
+	clName := cltInstance.Config().Cluster.Name()
+	trustDomain := telemetry.GetTrustDomain(cltInstance.Config().Cluster, ist.Settings().SystemNamespace)
+	if err := validateLogs(t, wantLog, clName, trustDomain, stackdriver.ServerAccessLog); err != nil {
+		return err
+	}
+	return nil
 }
