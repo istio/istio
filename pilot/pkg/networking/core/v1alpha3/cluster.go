31742c44c08a55a224f976ba39697946106627d4
Rama Chavali <rama.rao@salesforce.com>
2021-04-07 03:41:48 +0530

    add envoy filter stats (#31608)
    * add envoy filter stats
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * lint
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * lint again
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * fix ut
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * remove node label
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * lint
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * lint again
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * remove proxy from patcher
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * move to single metric
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * move to patch type
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * lint
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * move to loop
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * move to monitoring
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * change to nametag with flag
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * fix ut
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * address comments
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * handle nil
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * default to false
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/networking/core/v1alpha3/cluster.go
+++ pilot/pkg/networking/core/v1alpha3/cluster.go
@@ -83,20 +83,22 @@ func (configgen *ConfigGeneratorImpl) BuildClusters(proxy *model.Proxy, push *mo
 	switch proxy.Type {
 	case model.SidecarProxy:
 		// Setup outbound clusters
-		outboundPatcher := clusterPatcher{envoyFilterPatches, networking.EnvoyFilter_SIDECAR_OUTBOUND}
+		outboundPatcher := clusterPatcher{efw: envoyFilterPatches, pctx: networking.EnvoyFilter_SIDECAR_OUTBOUND}
 		clusters = append(clusters, configgen.buildOutboundClusters(cb, outboundPatcher)...)
 		// Add a blackhole and passthrough cluster for catching traffic to unresolved routes
 		clusters = outboundPatcher.conditionallyAppend(clusters, nil, cb.buildBlackHoleCluster(), cb.buildDefaultPassthroughCluster())
 		clusters = append(clusters, outboundPatcher.insertedClusters()...)
+		outboundPatcher.incrementFilterMetrics()
 
 		// Setup inbound clusters
-		inboundPatcher := clusterPatcher{envoyFilterPatches, networking.EnvoyFilter_SIDECAR_INBOUND}
+		inboundPatcher := clusterPatcher{efw: envoyFilterPatches, pctx: networking.EnvoyFilter_SIDECAR_INBOUND}
 		clusters = append(clusters, configgen.buildInboundClusters(cb, instances, inboundPatcher)...)
 		// Pass through clusters for inbound traffic. These cluster bind loopback-ish src address to access node local service.
 		clusters = inboundPatcher.conditionallyAppend(clusters, nil, cb.buildInboundPassthroughClusters()...)
 		clusters = append(clusters, inboundPatcher.insertedClusters()...)
+		inboundPatcher.incrementFilterMetrics()
 	default: // Gateways
-		patcher := clusterPatcher{envoyFilterPatches, networking.EnvoyFilter_GATEWAY}
+		patcher := clusterPatcher{efw: envoyFilterPatches, pctx: networking.EnvoyFilter_GATEWAY}
 		clusters = append(clusters, configgen.buildOutboundClusters(cb, patcher)...)
 		// Gateways do not require the default passthrough cluster as they do not have original dst listeners.
 		clusters = patcher.conditionallyAppend(clusters, nil, cb.buildBlackHoleCluster())
@@ -104,6 +106,7 @@ func (configgen *ConfigGeneratorImpl) BuildClusters(proxy *model.Proxy, push *mo
 			clusters = append(clusters, configgen.buildOutboundSniDnatClusters(proxy, push, patcher)...)
 		}
 		clusters = append(clusters, patcher.insertedClusters()...)
+		patcher.incrementFilterMetrics()
 	}
 
 	return cb.normalizeClusters(clusters)
@@ -153,17 +156,26 @@ func (configgen *ConfigGeneratorImpl) buildOutboundClusters(cb *ClusterBuilder, 
 var NilClusterPatcher = clusterPatcher{}
 
 type clusterPatcher struct {
-	efw  *model.EnvoyFilterWrapper
-	pctx networking.EnvoyFilter_PatchContext
+	efw            *model.EnvoyFilterWrapper
+	pctx           networking.EnvoyFilter_PatchContext
+	patchesApplied int
 }
 
 func (p clusterPatcher) conditionallyAppend(l []*cluster.Cluster, hosts []host.Name, clusters ...*cluster.Cluster) []*cluster.Cluster {
 	if !p.hasPatches() {
 		return append(l, clusters...)
 	}
+	var pc *cluster.Cluster
+	applied := false
 	for _, c := range clusters {
 		if envoyfilter.ShouldKeepCluster(p.pctx, p.efw, c, hosts) {
-			l = append(l, envoyfilter.ApplyClusterMerge(p.pctx, p.efw, c, hosts))
+			pc, applied = envoyfilter.ApplyClusterMerge(p.pctx, p.efw, c, hosts)
+			l = append(l, pc)
+		} else {
+			applied = true // Remove patch is applied.
+		}
+		if applied {
+			p.patchesApplied++
 		}
 	}
 	return l
@@ -171,6 +183,16 @@ func (p clusterPatcher) conditionallyAppend(l []*cluster.Cluster, hosts []host.N
 
 func (p clusterPatcher) insertedClusters() []*cluster.Cluster {
 	return envoyfilter.InsertedClusters(p.pctx, p.efw)
+}
+
+func (p clusterPatcher) incrementFilterMetrics() {
+	if p.efw != nil {
+		if len(p.efw.Patches[networking.EnvoyFilter_CLUSTER]) != p.patchesApplied {
+			envoyfilter.RecordEnvoyFilterMetric(p.efw.Key(), envoyfilter.Cluster, false,
+				float64(len(p.efw.Patches[networking.EnvoyFilter_CLUSTER])-p.patchesApplied))
+		}
+		envoyfilter.RecordEnvoyFilterMetric(p.efw.Key(), envoyfilter.Cluster, true, float64(p.patchesApplied))
+	}
 }
 
 func (p clusterPatcher) hasPatches() bool {
