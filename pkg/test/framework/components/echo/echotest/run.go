070d3c5bd62e9432ccb018b88b4232a42d4dca6d
Steven Landow <landow@google.com>
2021-04-12 09:41:27 -0700

    echotest: include namespace in subtest when service exists in multiple
    Change-Id: I7d4f087d134041d5e09a84b0ab446fed3b86e615

--- pkg/test/framework/components/echo/echotest/run.go
+++ pkg/test/framework/components/echo/echotest/run.go
@@ -89,9 +89,24 @@ func (t *T) RunToN(n int, testFn oneToNTest) {
 // fromEachDeployment enumerates subtests for deployment with the structure <src>
 // Intended to be used in combination with other helpers to enumerate subtests for destinations.
 func (t *T) fromEachDeployment(ctx framework.TestContext, testFn perDeploymentTest) {
+	duplicateShortnames := false
+	shortnames := map[string]struct{}{}
+	for _, src := range t.sources.Services() {
+		svc := src[0].Config().Service
+		if _, ok := shortnames[svc]; ok {
+			duplicateShortnames = true
+			break
+		}
+		shortnames[svc] = struct{}{}
+	}
+
 	for _, src := range t.sources.Services() {
 		src := src
-		ctx.NewSubTestf("%s", src[0].Config().Service).Run(func(ctx framework.TestContext) {
+		subtestName := src[0].Config().Service
+		if duplicateShortnames {
+			subtestName = src[0].Config().FQDN()
+		}
+		ctx.NewSubTest(subtestName).Run(func(ctx framework.TestContext) {
 			testFn(ctx, src)
 		})
 	}
