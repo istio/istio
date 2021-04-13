a9b9c2b0431c1ec395d116a3355e559e6af5e240
Steven Landow <landow@google.com>
2021-04-13 09:19:14 -0700

    include both filters
    Change-Id: If4979882822c8b3a7bd5d6bcf1defaa245dbdb7b

--- tests/integration/pilot/common/routing.go
+++ tests/integration/pilot/common/routing.go
@@ -887,6 +887,7 @@ func selfCallsCases() []TrafficTestCase {
 	sourceFilters := []echotest.Filter{
 		echotest.Not(echotest.ExternalServices),
 		echotest.Not(echotest.FilterMatch(echo.IsNaked())),
+		echotest.Not(echotest.FilterMatch(echo.IsHeadless())),
 	}
 	comboFilters := []echotest.CombinationFilter{func(from echo.Instance, to echo.Instances) echo.Instances {
 		return to.Match(echo.FQDN(from.Config().FQDN()))
@@ -912,6 +913,7 @@ func selfCallsCases() []TrafficTestCase {
 		{
 			name:             "to localhost",
 			workloadAgnostic: true,
+			sourceFilters:    sourceFilters,
 			comboFilters:     comboFilters,
 			setupOpts:        noTarget,
 			opts: echo.CallOptions{
