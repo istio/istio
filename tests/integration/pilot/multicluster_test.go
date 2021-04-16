aff317b83cd3687f8c6c42fc498891d004f6c618
Steven Landow <steven@stlcomputerservices.com>
2021-04-16 11:31:07 -0700

    add timeout for remote clusters to become ready (#31876)
    * DRY secretcontroller logic
    
    Change-Id: I91df3f2b29acdff1b082ae1fd0ae77650239755a
    
    * centralize RunAndWait and per-cluster stops
    
    Change-Id: Icdbbb085187e5dcd1d3eb1e0347bad92d6470dce
    
    * add sync timeout and share it with k8s cluster
    
    Change-Id: Ic8986c513dc032cdf4d0c0d23cd537dcb8740ff2
    
    * integration test
    
    * fix unit test
    
    Change-Id: Ib7276f4db9924eab7787a52ded4a4d59d97d6874
    
    * fix deadlock
    
    Change-Id: Ic63c071c88f4fdbbec82c47e1596dfb8d3ff778b
    
    * lint
    
    Change-Id: If8368f3789043ea1b8c46c1e424452b714fc2290
    
    * debug sa creation in integ
    
    Change-Id: Ib58a7c10561bc9eec852fb6c02103ca6a22b9a53
    
    * use unique cluster names in bad secret test
    
    Change-Id: I28abca2305b50add30143625e88ffde2c73a37c5
    
    * use context for timeout in crdcontroller init
    
    Change-Id: Ib8abd5600f3471e750742ca498fc1cf3f2b1189d
    
    * log
    
    Change-Id: I99fd6e3fbde2c1e13dd38308681935bd6dde8f6e
    
    * metric
    
    Change-Id: I33b5fbd81d5291edc43916330f28817437293d1f
    
    * test sync timeout
    
    Change-Id: Id724a7f6e68f8e70a046e1e9194575d2015b99cc
    
    * gen and release note
    
    Change-Id: I5303cddf7343d0bed4927928c5d6dc3c6e8efe0d
    
    * return err when failing to watch workloadentry
    
    Change-Id: I252ff0d316478a9460c78f7914dcbf7069a98e9a
    
    * gen
    
    Change-Id: I2f892ef7b15edeb5c5ae72a52a0167d00999034c
    
    * note
    
    Change-Id: I7dd67c5a6f7f69414bc71c338e2b80fbebb0c4d7
    
    * metric name
    
    Change-Id: I8131c18338d586bf2d85bd41ac6e240e09cca55c
    
    * address comments
    
    Change-Id: Ib025d5b6cf99eb9bed0cefe045091976e0c95e7c

--- tests/integration/pilot/multicluster_test.go
+++ tests/integration/pilot/multicluster_test.go
@@ -20,16 +20,20 @@ import (
 	"fmt"
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/go-multierror"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	meshconfig "istio.io/api/mesh/v1alpha1"
 	"istio.io/istio/pkg/test/echo/common/scheme"
 	"istio.io/istio/pkg/test/framework"
 	"istio.io/istio/pkg/test/framework/components/cluster"
 	"istio.io/istio/pkg/test/framework/components/echo"
+	"istio.io/istio/pkg/test/framework/components/istio"
 	"istio.io/istio/pkg/test/scopes"
+	"istio.io/istio/pkg/test/util/retry"
 	"istio.io/istio/pkg/util/gogoprotomarshal"
 )
 
@@ -91,6 +95,85 @@ serviceSettings:
 		})
 }
 
+func TestBadRemoteSecret(t *testing.T) {
+	framework.NewTest(t).
+		RequiresMinClusters(2).
+		Features(
+			// TODO tracking topologies as feature labels doesn't make sense
+			"installation.multicluster.multimaster",
+			"installation.multicluster.remote",
+		).
+		Run(func(t framework.TestContext) {
+			// we don't need to test this per-cluster
+			primary := t.Clusters().Configs()[0]
+			// it doesn't matter if the other cluster is a primary/remote/etc.
+			remote := t.Clusters().Exclude(primary)[0]
+
+			var (
+				ns  = i.Settings().SystemNamespace
+				sa  = "istio-reader-no-perms"
+				pod = "istiod-bad-secrets-test"
+			)
+			t.Logf("creating service account %s/%s", ns, sa)
+			if _, err := remote.CoreV1().ServiceAccounts(ns).Create(context.TODO(), &corev1.ServiceAccount{
+				ObjectMeta: metav1.ObjectMeta{Name: sa},
+			}, metav1.CreateOptions{}); err != nil {
+				t.Fatal(err)
+			}
+
+			// intentionally not doing this with subtests since it would be pretty slow
+			for _, opts := range [][]string{
+				{"--name", "unreachable", "--server", "https://255.255.255.255"},
+				{"--name", "no-permissions", "--service-account", "istio-reader-no-perms"},
+			} {
+				var secret string
+				retry.UntilSuccessOrFail(t, func() error {
+					var err error
+					secret, err = istio.CreateRemoteSecret(t, remote, i.Settings(), opts...)
+					return err
+				}, retry.Timeout(15*time.Second))
+
+				t.Config().ApplyYAMLOrFail(t, ns, secret)
+			}
+
+			// create a new istiod pod using the template from the deployment, but not managed by the deployment
+			t.Logf("creating pod %s/%s", ns, pod)
+			deps, err := primary.AppsV1().
+				Deployments(ns).List(context.TODO(), metav1.ListOptions{LabelSelector: "app=istiod"})
+			if err != nil {
+				t.Fatal(err)
+			}
+			pods := primary.CoreV1().Pods(ns)
+			podMeta := deps.Items[0].Spec.Template.ObjectMeta
+			podMeta.Name = pod
+			_, err = pods.Create(context.TODO(), &corev1.Pod{
+				ObjectMeta: podMeta,
+				Spec:       deps.Items[0].Spec.Template.Spec,
+			}, metav1.CreateOptions{})
+			if err != nil {
+				t.Fatal(err)
+			}
+			t.Cleanup(func() {
+				if err := pods.Delete(context.TODO(), pod, metav1.DeleteOptions{}); err != nil {
+					t.Logf("error cleaning up %s: %v", pod, err)
+				}
+			})
+
+			// make sure the pod comes up healthy
+			retry.UntilSuccessOrFail(t, func() error {
+				pod, err := pods.Get(context.TODO(), pod, metav1.GetOptions{})
+				if err != nil {
+					return err
+				}
+				status := pod.Status.ContainerStatuses
+				if len(status) < 1 || !status[0].Ready {
+					return fmt.Errorf("%s not ready", pod)
+				}
+				return nil
+			}, retry.Timeout(time.Minute), retry.Delay(time.Second))
+		})
+}
+
 func patchMeshConfig(t framework.TestContext, clusters cluster.Clusters, patch string) {
 	errG := multierror.Group{}
 	origCfg := map[string]string{}
@@ -100,10 +183,11 @@ func patchMeshConfig(t framework.TestContext, clusters cluster.Clusters, patch s
 	if rev := t.Settings().Revision; rev != "default" && rev != "" {
 		cmName += "-" + rev
 	}
+	namespace := i.Settings().SystemNamespace
 	for _, c := range clusters.Kube() {
 		c := c
 		errG.Go(func() error {
-			cm, err := c.CoreV1().ConfigMaps(i.Settings().SystemNamespace).Get(context.TODO(), cmName, v1.GetOptions{})
+			cm, err := c.CoreV1().ConfigMaps(namespace).Get(context.TODO(), cmName, metav1.GetOptions{})
 			if err != nil {
 				return err
 			}
@@ -125,7 +209,7 @@ func patchMeshConfig(t framework.TestContext, clusters cluster.Clusters, patch s
 			if err != nil {
 				return err
 			}
-			_, err = c.CoreV1().ConfigMaps(i.Settings().SystemNamespace).Update(context.TODO(), cm, v1.UpdateOptions{})
+			_, err = c.CoreV1().ConfigMaps(namespace).Update(context.TODO(), cm, metav1.UpdateOptions{})
 			if err != nil {
 				return err
 			}
@@ -141,12 +225,12 @@ func patchMeshConfig(t framework.TestContext, clusters cluster.Clusters, patch s
 			cn, mcYaml := cn, mcYaml
 			c := clusters.GetByName(cn)
 			errG.Go(func() error {
-				cm, err := c.CoreV1().ConfigMaps(i.Settings().SystemNamespace).Get(context.TODO(), cmName, v1.GetOptions{})
+				cm, err := c.CoreV1().ConfigMaps(namespace).Get(context.TODO(), cmName, metav1.GetOptions{})
 				if err != nil {
 					return err
 				}
 				cm.Data["mesh"] = mcYaml
-				_, err = c.CoreV1().ConfigMaps(i.Settings().SystemNamespace).Update(context.TODO(), cm, v1.UpdateOptions{})
+				_, err = c.CoreV1().ConfigMaps(namespace).Update(context.TODO(), cm, metav1.UpdateOptions{})
 				return err
 			})
 		}
