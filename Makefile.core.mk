8fa975c596c8484fb808cafdc4d841b91e718b1f
Eric Van Norman <ericvn@us.ibm.com>
2021-04-19 08:55:02 -0500

    Update BASE_VERSION to 1.10-dev.3

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -22,7 +22,7 @@ SHELL := /bin/bash -o pipefail
 export VERSION ?= 1.10-dev
 
 # Base version of Istio image to use
-BASE_VERSION ?= 1.10-dev.2
+BASE_VERSION ?= 1.10-dev.3
 
 export GO111MODULE ?= on
 export GOPROXY ?= https://proxy.golang.org
