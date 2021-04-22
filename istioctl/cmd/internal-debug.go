15a87a4f7a1f75ab5df3b7200fc423cf6a3f21db
Steve Zhang <huailong.zhang@intel.com>
2021-04-22 12:13:41 +0800

    handle the conflict for internal-debug.go on release-1.10 (#32354)

--- istioctl/cmd/internal-debug.go
+++ istioctl/cmd/internal-debug.go
@@ -17,17 +17,18 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"io"
+	"strings"
 
 	envoy_corev3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	xdsapi "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
 	"github.com/spf13/cobra"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/kubectl/pkg/polymorphichelpers"
 
 	"istio.io/istio/istioctl/pkg/clioptions"
 	"istio.io/istio/istioctl/pkg/multixds"
-	"istio.io/istio/istioctl/pkg/util/handlers"
 	"istio.io/istio/istioctl/pkg/writer/pilot"
+	"istio.io/istio/pkg/kube"
 )
 
 const (
@@ -36,6 +37,50 @@ const (
 	istiodServiceName = "istiod"
 	xdsPortName       = "https-dns"
 )
+
+func HandlerForRetrieveDebugList(kubeClient kube.ExtendedClient,
+	centralOpts *clioptions.CentralControlPlaneOptions,
+	writer io.Writer) (map[string]*xdsapi.DiscoveryResponse, error) {
+	var namespace, serviceAccount string
+	xdsRequest := xdsapi.DiscoveryRequest{
+		ResourceNames: []string{"list"},
+		Node: &envoy_corev3.Node{
+			Id: "debug~0.0.0.0~istioctl~cluster.local",
+		},
+		TypeUrl: TypeDebug,
+	}
+	xdsResponses, respErr := multixds.AllRequestAndProcessXds(&xdsRequest, centralOpts, istioNamespace,
+		namespace, serviceAccount, kubeClient)
+	if respErr != nil {
+		return xdsResponses, respErr
+	}
+	_, _ = fmt.Fprint(writer, "error: according to below command list, please check all supported internal debug commands\n")
+	return xdsResponses, nil
+}
+
+func HandlerForDebugErrors(kubeClient kube.ExtendedClient,
+	centralOpts *clioptions.CentralControlPlaneOptions,
+	writer io.Writer,
+	xdsResponses map[string]*xdsapi.DiscoveryResponse) (map[string]*xdsapi.DiscoveryResponse, error) {
+	for _, response := range xdsResponses {
+		for _, resource := range response.Resources {
+			eString := string(resource.Value)
+			switch {
+			case strings.Contains(eString, "You must provide a proxyID in the query string"):
+				return nil, fmt.Errorf(" You must provide a proxyID in the query string, e.g. [%s]",
+					"edsz?proxyID=istio-ingressgateway")
+
+			case strings.Contains(eString, "404 page not found"):
+				return HandlerForRetrieveDebugList(kubeClient, centralOpts, writer)
+
+			case strings.Contains(eString, "querystring parameter 'resource' is required"):
+				return nil, fmt.Errorf("querystring parameter 'resource' is required, e.g. [%s]",
+					"config_distribution?resource=VirtualService/default/bookinfo")
+			}
+		}
+	}
+	return nil, nil
+}
 
 func debugCommand() *cobra.Command {
 	var opts clioptions.ControlPlaneOptions
@@ -50,24 +95,18 @@ By default it will use the default serviceAccount from (istio-system) namespace 
 `,
 		Example: `  # Retrieve sync status for all Envoys in a mesh
   istioctl x internal-debug syncz
-
   # Retrieve sync diff for a single Envoy and Istiod
   istioctl x internal-debug syncz istio-egressgateway-59585c5b9c-ndc59.istio-system
-
   # SECURITY OPTIONS
-
   # Retrieve syncz debug information directly from the control plane, using token security
   # (This is the usual way to get the debug information with an out-of-cluster control plane.)
   istioctl x internal-debug syncz --xds-address istio.cloudprovider.example.com:15012
-
   # Retrieve syncz debug information via Kubernetes config, using token security
   # (This is the usual way to get the debug information with an in-cluster control plane.)
   istioctl x internal-debug syncz
-
   # Retrieve syncz debug information directly from the control plane, using RSA certificate security
   # (Certificates must be obtained before this step.  The --cert-dir flag lets istioctl bypass the Kubernetes API server.)
   istioctl x internal-debug syncz --xds-address istio.example.com:15012 --cert-dir ~/.istio-certs
-
   # Retrieve syncz information via XDS from specific control plane in multi-control plane in-cluster configuration
   # (Select a specific control plane in an in-cluster canary Istio configuration.)
   istioctl x internal-debug syncz --xds-label istio.io/rev=default
@@ -84,79 +123,87 @@ By default it will use the default serviceAccount from (istio-system) namespace 
 			}
 			var xdsRequest xdsapi.DiscoveryRequest
 			var namespace, serviceAccount string
-			if len(args) > 1 {
-				podName, ns, err := handlers.InferPodInfoFromTypedResource(args[1],
-					handlers.HandleNamespace(namespace, defaultNamespace),
-					kubeClient.UtilFactory())
-				if err != nil {
-					return err
-				}
-				pod, err := kubeClient.CoreV1().Pods(ns).Get(context.TODO(), podName, metav1.GetOptions{})
-				if err != nil {
-					return err
-				}
-				namespace = ns
-				serviceAccount = pod.Spec.ServiceAccountName
-				xdsRequest = xdsapi.DiscoveryRequest{
-					ResourceNames: []string{fmt.Sprintf("%s?proxyID=%s.%s", args[0], podName, ns)},
-					Node: &envoy_corev3.Node{
-						Id: "debug~0.0.0.0~istioctl~cluster.local",
-					},
-					TypeUrl: TypeDebug,
-				}
-			} else {
-				xdsRequest = xdsapi.DiscoveryRequest{
-					ResourceNames: []string{args[0]},
-					Node: &envoy_corev3.Node{
-						Id: "debug~0.0.0.0~istioctl~cluster.local",
-					},
-					TypeUrl: TypeDebug,
-				}
-			}
-			if centralOpts.Xds == "" {
-				svc, err := kubeClient.CoreV1().Services(istioNamespace).Get(context.Background(), istiodServiceName, metav1.GetOptions{})
-				if err != nil {
-					return fmt.Errorf("please specify %q as %v", "--xds-address", err)
-				}
-				namespace, selector, err := polymorphichelpers.SelectorsForObject(svc)
-				if err != nil {
-					return fmt.Errorf("please specify %q as we cannot attach to %T: %v", "--xds-address", svc, err)
-				}
 
-				options := metav1.ListOptions{LabelSelector: selector.String()}
-
-				podList, err := kubeClient.CoreV1().Pods(namespace).List(context.TODO(), options)
-				if err != nil {
-					return fmt.Errorf("please specify %q as %v", "--xds-address", err)
-				}
-				//  select a pod randomly to simulate current debug behavior
-				pod := podList.Items[0]
-				podPort := 15012
-				for _, v := range svc.Spec.Ports {
-					if v.Name == xdsPortName {
-						podPort = v.TargetPort.IntValue()
-					}
-				}
-				f, err := kubeClient.NewPortForwarder(pod.Name, pod.Namespace, "", 0, podPort)
-				if err != nil {
-					return fmt.Errorf("please specify %q as %v", "--xds-address", err)
-				}
-				if err := f.Start(); err != nil {
-					return fmt.Errorf("please specify %q as %v", "--xds-address", err)
-				}
-				centralOpts.Xds = f.Address()
-				defer func() {
-					f.Close()
-					f.WaitForStop()
-				}()
+			xdsRequest = xdsapi.DiscoveryRequest{
+				ResourceNames: []string{args[0]},
+				Node: &envoy_corev3.Node{
+					Id: "debug~0.0.0.0~istioctl~cluster.local",
+				},
+				TypeUrl: TypeDebug,
 			}
-			xdsResponses, err := multixds.AllRequestAndProcessXds(&xdsRequest, &centralOpts, istioNamespace,
-				namespace, serviceAccount, kubeClient)
+
+			svc, err := kubeClient.CoreV1().Services(istioNamespace).Get(context.Background(), istiodServiceName, metav1.GetOptions{})
 			if err != nil {
-				return err
+				return fmt.Errorf("please specify %q as %v", "--xds-address", err)
 			}
-			sw := pilot.XdsStatusWriter{Writer: c.OutOrStdout()}
-			return sw.PrintAll(xdsResponses)
+			podPort := 15012
+			for _, v := range svc.Spec.Ports {
+				if v.Name == xdsPortName {
+					podPort = v.TargetPort.IntValue()
+				}
+			}
+
+			labelSelector := centralOpts.XdsPodLabel
+			if labelSelector == "" {
+				labelSelector = "app=istiod"
+			}
+			podList, podErr := kubeClient.GetIstioPods(context.TODO(), istioNamespace, map[string]string{
+				"labelSelector": labelSelector,
+				"fieldSelector": "status.phase=Running",
+			})
+			if podErr != nil {
+				return podErr
+			}
+			if len(podList) == 0 {
+				return fmt.Errorf("no running Istio pods in %q", istioNamespace)
+			}
+
+			var isNull bool = false
+			var printErr error
+			// Iterate all istiod pods for retrieving debug information
+			for _, pod := range podList {
+				fmt.Println("------------------------------------------------------------------------------")
+				fmt.Println("istioctl x debug for pod: ", pod.Name+"."+pod.Namespace)
+				fmt.Println("------------------------------------------------------------------------------")
+				if centralOpts.Xds == "" {
+					isNull = true
+					f, kcerr := kubeClient.NewPortForwarder(pod.Name, pod.Namespace, "", 0, podPort)
+					if kcerr != nil {
+						return fmt.Errorf("please specify %q as %v", "--xds-address", kcerr)
+					}
+					if ferr := f.Start(); ferr != nil {
+						return fmt.Errorf("please specify %q as %v", "--xds-address", ferr)
+					}
+					centralOpts.Xds = f.Address()
+					defer func() {
+						f.Close()
+						f.WaitForStop()
+					}()
+				}
+
+				xdsResponses, respErr := multixds.AllRequestAndProcessXds(&xdsRequest, &centralOpts, istioNamespace,
+					namespace, serviceAccount, kubeClient)
+				if respErr != nil {
+					return respErr
+				}
+				sw := pilot.XdsStatusWriter{Writer: c.OutOrStdout()}
+				newResponse, hDErr := HandlerForDebugErrors(kubeClient, &centralOpts, c.OutOrStdout(), xdsResponses)
+				if newResponse != nil {
+					return sw.PrintAll(newResponse)
+				}
+				if hDErr != nil {
+					return hDErr
+				}
+				printErr = sw.PrintAll(xdsResponses)
+				if printErr != nil {
+					return printErr
+				}
+				// reset centralOpts.Xds as blank if it's originally null
+				if isNull {
+					centralOpts.Xds = ""
+				}
+			}
+			return nil
 		},
 	}
 
