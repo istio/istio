e79609b13a60150104445f8459f77d5620b9fec2
Jacob Delgado <jacob.delgado@volunteers.acasi.info>
2021-04-23 10:51:00 -0600

    Update base image

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -22,7 +22,7 @@ SHELL := /bin/bash -o pipefail
 export VERSION ?= 1.9-dev
 
 # Base version of Istio image to use
-BASE_VERSION ?= 1.9-dev.5
+BASE_VERSION ?= 1.9-dev.6
 
 export GO111MODULE ?= on
 export GOPROXY ?= https://proxy.golang.org
