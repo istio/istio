0d78897215c0d487cbf67edbfb5de21b6b530e34
Rama Chavali <rama.rao@salesforce.com>
2021-04-26 16:10:51 +0530

    fix
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/serviceregistry/kube/controller/controller.go
+++ pilot/pkg/serviceregistry/kube/controller/controller.go
@@ -16,6 +16,7 @@ package controller
 
 import (
 	"fmt"
+	"sort"
 	"sync"
 	"time"
 
@@ -710,6 +711,7 @@ func (c *Controller) Services() ([]*model.Service, error) {
 		out = append(out, svc)
 	}
 	c.RUnlock()
+	sort.Slice(out, func(i, j int) bool { return out[i].Hostname < out[j].Hostname })
 	return out, nil
 }
 
