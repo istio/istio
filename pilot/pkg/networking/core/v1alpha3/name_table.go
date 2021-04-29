2aa667da800c4d0c10633c58f6246535d88eac28
Steven Landow <landow@google.com>
2021-04-29 09:32:23 -0700

    release note
    Change-Id: I259b5522787259ab21063767d000b157151b3ed2

--- pilot/pkg/networking/core/v1alpha3/name_table.go
+++ pilot/pkg/networking/core/v1alpha3/name_table.go
@@ -19,7 +19,6 @@ import (
 	"strings"
 
 	"istio.io/istio/pilot/pkg/features"
-
 	"istio.io/istio/pilot/pkg/model"
 	nds "istio.io/istio/pilot/pkg/proto"
 	"istio.io/istio/pilot/pkg/serviceregistry"
