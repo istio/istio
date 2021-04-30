b6ec9d93ebe00f4f6cf497c8d4b0dd252166d068
John Howard <howardjohn@google.com>
2021-04-29 23:27:38 -0700

    Improve internal-debug and use it in tf Dump commands (#32492)
    * Support external istiod without XDS Address. If no pod are found for
    the revision, we will fetch the URL from the webhook as a fallback
    mechanism.
    * Improve the output. Currently, we always fetch from all istiods and
    then emit some non-json output to distinguish the requests. This is hard
    to read/parse in the case we do want all requests, and excessive
    verbosity/performance when we only care about one. Instead, expose a
    --all flag, off by default. When set, we will query all Istiod's, but
    put the response in a structured JSON blob. When unset, we just output
    the response from Istiod directly.
    * Switch Dumping or configz and ndsz in test framework to use the new
    command. This both tests the command and makes tf dumping work for
    external Istiod tests. I would also like bug-report to do it, but the PR is pretty
    big.
    * Some smaller refactorings in the istioctl code to make this PR cleaner

--- pkg/test/framework/components/istioctl/kube.go
+++ pkg/test/framework/components/istioctl/kube.go
@@ -22,29 +22,31 @@ import (
 	"istio.io/istio/istioctl/cmd"
 	"istio.io/istio/pilot/pkg/config/kube/crd"
 	"istio.io/istio/pkg/test"
-	kubecluster "istio.io/istio/pkg/test/framework/components/cluster/kube"
 	"istio.io/istio/pkg/test/framework/resource"
 )
 
 type kubeComponent struct {
-	config  Config
-	id      resource.ID
-	cluster *kubecluster.Cluster
+	config     Config
+	kubeconfig string
 }
 
-func newKube(ctx resource.Context, config Config) Instance {
-	n := &kubeComponent{
-		config:  config,
-		cluster: ctx.Clusters().GetOrDefault(config.Cluster).(*kubecluster.Cluster),
+// Filenamer is an interface to avoid importing kubecluster package, instead build our own interface
+// to extract kube context
+type Filenamer interface {
+	Filename() string
+}
+
+func newKube(ctx resource.Context, config Config) (Instance, error) {
+	fn, ok := ctx.Clusters().GetOrDefault(config.Cluster).(Filenamer)
+	if !ok {
+		return nil, fmt.Errorf("cluster does not support fetching kube config")
 	}
-	n.id = ctx.TrackResource(n)
+	n := &kubeComponent{
+		config:     config,
+		kubeconfig: fn.Filename(),
+	}
 
-	return n
-}
-
-// ID implements resource.Instance
-func (c *kubeComponent) ID() resource.ID {
-	return c.id
+	return n, nil
 }
 
 // Invoke implements WaitForConfigs
@@ -69,7 +71,7 @@ func (c *kubeComponent) WaitForConfigs(defaultNamespace string, configs string) 
 func (c *kubeComponent) Invoke(args []string) (string, string, error) {
 	cmdArgs := append([]string{
 		"--kubeconfig",
-		c.cluster.Filename(),
+		c.kubeconfig,
 	}, args...)
 
 	var out bytes.Buffer
