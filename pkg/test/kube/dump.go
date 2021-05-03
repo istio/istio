8fc2ed4e6df53279c386cb08a84b6817df28c364
Steven Landow <landow@google.com>
2021-05-03 15:40:59 -0700

    imports
    Change-Id: I5a44b02f7ce29f50b7ccaa8e3acdc714e0677aba

--- pkg/test/kube/dump.go
+++ pkg/test/kube/dump.go
@@ -25,10 +25,8 @@ import (
 	"strings"
 	"sync"
 
-	"gopkg.in/yaml.v2"
-
 	"github.com/hashicorp/go-multierror"
-
+	"gopkg.in/yaml.v2"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
