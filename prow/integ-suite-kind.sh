e7f40ec1855882b20e9068d0ce86872d85036741
Steven Landow <landow@google.com>
2021-05-03 10:50:11 -0700

    test istiodless remotes
    Change-Id: I36fefd19e35061782a46b1e0370abf9abbe04a20

--- prow/integ-suite-kind.sh
+++ prow/integ-suite-kind.sh
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+export INTEGRATION_TEST_FLAGS="--istio.test.istio.istiodlessRemotes"
 
 # Usage: ./integ-suite-kind.sh TARGET
 # Example: ./integ-suite-kind.sh test.integration.pilot.kube.presubmit
