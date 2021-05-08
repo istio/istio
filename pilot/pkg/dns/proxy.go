d34f85b7e626e72e24c0706d2f264309376f5a05
Rama Chavali <rama.rao@salesforce.com>
2021-05-08 17:08:43 +0530

    lint
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/dns/proxy.go
+++ pilot/pkg/dns/proxy.go
@@ -37,7 +37,7 @@ func newDNSProxy(protocol string, resolver *LocalDNSServer) (*dnsProxy, error) {
 		server:   &dns.Server{},
 		upstreamClient: &dns.Client{
 			Net: protocol,
-			// Set this true, so that multiple queries for the same Qname, Qtype and Qclass are supressed and
+			// Set this true, so that multiple queries for the same Qname, Qtype and Qclass are suppressed and
 			// after first response is received, use the same response for outstanding queries.
 			SingleInflight: true,
 		},
