4e3e677c1207f269f082601c3cb8c8f8049d27e1
John Howard <howardjohn@google.com>
2021-05-12 13:31:23 -0700

    Fix AUTO_PASSTHROUGH
    (cherry picked from commit 0839f02b7170a7b5237b2f588c7628247d8c3bfb)

--- pilot/pkg/model/service.go
+++ pilot/pkg/model/service.go
@@ -582,6 +582,15 @@ func IsValidSubsetKey(s string) bool {
 	return strings.Count(s, "|") == 3
 }
 
+// IsDNSSrvSubsetKey checks whether the given key is a DNSSrv key (built by BuildDNSSrvSubsetKey).
+func IsDNSSrvSubsetKey(s string) bool {
+	if strings.HasPrefix(s, trafficDirectionOutboundSrvPrefix) ||
+		strings.HasPrefix(s, trafficDirectionInboundSrvPrefix) {
+		return true
+	}
+	return false
+}
+
 // ParseSubsetKey is the inverse of the BuildSubsetKey method
 func ParseSubsetKey(s string) (direction TrafficDirection, subsetName string, hostname host.Name, port int) {
 	var parts []string
--- tests/integration/security/normalization_test.go
+++ tests/integration/security/normalization_test.go
@@ -1,3 +1,4 @@
+// +build integ
 // Copyright Istio Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
