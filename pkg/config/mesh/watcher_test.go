4052f4265c64e38fe2de979fd42b1c97b84ea06a
John Howard <howardjohn@google.com>
2021-05-17 08:55:53 -0700

    Fix flakes in TestMultiWatcherShouldNotifyHandlers (#32847)
    * Fix flakes in TestMultiWatcherShouldNotifyHandlers
    
    Basically what happens is at the end of the test, the file is removed.
    The mesh watcher responds to DELETE events, but we ignore the error that
    file is not found and call the handlers again.
    
    Instead, if we cannot process the update, we should just ignore it (as
    we did prior to recent changes).
    
    The changes in _test.go are not required, just make things a bit
    cleaner.
    
    * fix lint
    
    * lint

--- pkg/config/mesh/watcher_test.go
+++ pkg/config/mesh/watcher_test.go
@@ -15,7 +15,6 @@
 package mesh_test
 
 import (
-	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -49,7 +48,6 @@ func watcherShouldNotifyHandlers(t *testing.T, multi bool) {
 	g := NewWithT(t)
 
 	path := newTempFile(t)
-	defer removeSilent(path)
 
 	m := mesh.DefaultMeshConfig()
 	writeMessage(t, path, &m)
@@ -90,11 +88,12 @@ func newWatcher(t testing.TB, filename string, multi bool) mesh.Watcher {
 
 func newTempFile(t testing.TB) string {
 	t.Helper()
-	f, err := ioutil.TempFile("", t.Name())
+
+	f, err := ioutil.TempFile(t.TempDir(), t.Name())
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer closeSilent(f)
+	t.Cleanup(func() { _ = f.Close() })
 
 	path, err := filepath.Abs(f.Name())
 	if err != nil {
@@ -114,13 +113,9 @@ func writeMessage(t testing.TB, path string, msg proto.Message) {
 
 func writeFile(t testing.TB, path, content string) {
 	t.Helper()
-	if err := ioutil.WriteFile(path, []byte(content), 0666); err != nil {
+	if err := ioutil.WriteFile(path, []byte(content), 0o666); err != nil {
 		t.Fatal(err)
 	}
-}
-
-func closeSilent(c io.Closer) {
-	_ = c.Close()
 }
 
 func removeSilent(path string) {
