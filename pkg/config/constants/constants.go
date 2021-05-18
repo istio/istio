3f48f1855ecc433853c62155b78de083f0ed24b3
Kuat Yessenov <kuat@google.com>
2021-05-18 16:06:11 -0700

    merge fix
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- pkg/config/constants/constants.go
+++ pkg/config/constants/constants.go
@@ -93,14 +93,6 @@ const (
 	// This is typically set by the downward API
 	PodInfoAnnotationsPath = "./etc/istio/pod/annotations"
 
-	// PodInfoCPURequestsPath is the filepath that pod CPU requests will be stored
-	// This is typically set by the downward API
-	PodInfoCPURequestsPath = "./etc/istio/pod/cpu-request"
-
-	// PodInfoCPULimitsPath is the filepath that pod CPU limits will be stored
-	// This is typically set by the downward API
-	PodInfoCPULimitsPath = "./etc/istio/pod/cpu-limit"
-
 	// DefaultSdsUdsPath is the path used for SDS communication between istio-agent and proxy during
 	// mtls.
 	DefaultSdsUdsPath = "unix:./etc/istio/proxy/SDS"
