a852941166e96d94e1f63f94cf15a4b364841db9
sharathchandra <sharath.chandra.balachandra@est.tech>
2021-06-02 10:37:28 +0200

    Added test case to check ipv6 rules when outbound ports are included

--- tools/istio-iptables/pkg/cmd/run.go
+++ tools/istio-iptables/pkg/cmd/run.go
@@ -298,7 +298,8 @@ func (iptConfigurator *IptablesConfigurator) handleInboundIpv6Rules(ipv6RangesEx
 	for _, cidr := range ipv6RangesExclude.IPNets {
 		iptConfigurator.iptables.AppendRuleV6(constants.ISTIOOUTPUT, constants.NAT, "-d", cidr.String(), "-j", constants.RETURN)
 	}
-	// Apply outbound IPv6 inclusions.
+
+	// Redirect traffic to Envoy for the included outbound ports
 	if iptConfigurator.cfg.OutboundPortsInclude != "" {
 		for _, port := range split(iptConfigurator.cfg.OutboundPortsInclude) {
 			iptConfigurator.iptables.AppendRuleV6(
@@ -306,6 +307,7 @@ func (iptConfigurator *IptablesConfigurator) handleInboundIpv6Rules(ipv6RangesEx
 		}
 	}
 
+	// Apply outbound IPv6 inclusions.
 	if ipv6RangesInclude.IsWildcard {
 		// Wildcard specified. Redirect all remaining outbound traffic to Envoy.
 		iptConfigurator.iptables.AppendRuleV6(constants.ISTIOOUTPUT, constants.NAT, "-j", constants.ISTIOREDIRECT)
