16e772fe8dd7540b5897fe94ac05fb6fa66baf1b
John Howard <howardjohn@google.com>
2021-06-09 18:15:53 -0700

    Merge gateway topology test into common suite
    We can now efficiently update mesh config, no need for a new suite

--- tests/integration/pilot/gw_topology_test.go
+++ tests/integration/pilot/gw_topology_test.go
@@ -13,57 +13,28 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package gwtopology
+package pilot
 
 import (
 	"testing"
 
 	"istio.io/istio/pkg/test/framework"
 	"istio.io/istio/pkg/test/framework/components/istio"
-	"istio.io/istio/pkg/test/framework/resource"
 	"istio.io/istio/tests/integration/pilot/common"
 )
 
-var (
-	i    istio.Instance
-	apps = &common.EchoDeployments{}
-)
-
-func TestMain(m *testing.M) {
-	framework.
-		NewSuite(m).
-		Setup(istio.Setup(&i, func(t resource.Context, cfg *istio.Config) {
-			cfg.ControlPlaneValues = `
-meshConfig:
-  defaultConfig:
-    gatewayTopology:
-      numTrustedProxies: 2`
-		})).
-		Setup(func(t resource.Context) error {
-			return common.SetupApps(t, i, apps)
-		}).
-		Run()
-}
-
-func TestTraffic(t *testing.T) {
+func TestXFFGateway(t *testing.T) {
 	framework.
 		NewTest(t).
 		Features("traffic.ingress.topology").
 		Run(func(t framework.TestContext) {
-			runXFFTrafficTests(t, apps)
-		})
-}
-
-func runXFFTrafficTests(t framework.TestContext, apps *common.EchoDeployments) {
-	cases := map[string][]common.TrafficTestCase{
-		"xff": common.XFFGatewayCase(apps),
-	}
-
-	for name, tts := range cases {
-		t.NewSubTest(name).Run(func(t framework.TestContext) {
-			for _, tt := range tts {
+			istio.PatchMeshConfig(t, i.Settings().SystemNamespace, t.Clusters(), `
+meshConfig:
+  defaultConfig:
+    gatewayTopology:
+      numTrustedProxies: 2`)
+			for _, tt := range common.XFFGatewayCase(apps) {
 				tt.Run(t, apps.Namespace.Name())
 			}
 		})
-	}
 }
