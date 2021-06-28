a788777ee0782cfd2e74cfa15b85e44cc9c1a417
Douglas Reid <douglas-reid@users.noreply.github.com>
2021-06-28 14:45:55 -0700

    Add support for env var override of gcp quota project (#33670)
    * Add support for env var override of gcp quota project
    
    * expose Var for override
    
    * fmt and make gen

--- pkg/bootstrap/platform/gcp.go
+++ pkg/bootstrap/platform/gcp.go
@@ -42,9 +42,15 @@ const (
 	GCEInstanceID        = "gcp_gce_instance_id"
 	GCEInstanceTemplate  = "gcp_gce_instance_template"
 	GCEInstanceCreatedBy = "gcp_gce_instance_created_by"
+	GCPQuotaProject      = "gcp_quota_project"
 )
 
-var GCPMetadata = env.RegisterStringVar("GCP_METADATA", "", "Pipe separted GCP metadata, schemed as PROJECT_ID|PROJECT_NUMBER|CLUSTER_NAME|CLUSTER_ZONE").Get()
+var (
+	GCPMetadata = env.RegisterStringVar("GCP_METADATA", "", "Pipe separted GCP metadata, schemed as PROJECT_ID|PROJECT_NUMBER|CLUSTER_NAME|CLUSTER_ZONE").Get()
+
+	// GCPQuotaProjectVar holds the value of the `GCP_QUOTA_PROJECT` environment variable.
+	GCPQuotaProjectVar = env.RegisterStringVar("GCP_QUOTA_PROJECT", "", "Allows specification of a quota project to be used in requests to GCP APIs.").Get()
+)
 
 var (
 	shouldFillMetadata = metadata.OnGCE
@@ -162,6 +168,9 @@ func (e *gcpEnv) Metadata() map[string]string {
 		md[GCPCluster] = envCN
 	} else if cn, err := clusterNameFn(); err == nil {
 		md[GCPCluster] = cn
+	}
+	if GCPQuotaProjectVar != "" {
+		md[GCPQuotaProject] = GCPQuotaProjectVar
 	}
 	// Exit early now if not on GCE. This allows setting env var when not on GCE.
 	if !shouldFillMetadata() {
