4a40cbc18809a1c0b6981d528681dc721bbfd985
Zhonghu Xu <xuzhonghu@huawei.com>
2021-06-30 23:26:34 +0800

    Rename BuildSidecarVirtualHostWrapper (#33721)

--- pilot/pkg/networking/core/v1alpha3/route/route_test.go
+++ pilot/pkg/networking/core/v1alpha3/route/route_test.go
@@ -646,7 +646,7 @@ func TestBuildHTTPRoutes(t *testing.T) {
 				Spec: networkingDestinationRule,
 			},
 		})
-		vhosts := route.BuildSidecarVirtualHostsFromConfigAndRegistry(node, push, serviceRegistry, []config.Config{}, 8080)
+		vhosts := route.BuildSidecarVirtualHostWrapper(node, push, serviceRegistry, []config.Config{}, 8080)
 		g.Expect(vhosts[0].Routes[0].Action.(*envoyroute.Route_Route).Route.HashPolicy).NotTo(gomega.BeNil())
 	})
 	t.Run("for no virtualservice but has destinationrule with portLevel consistentHash loadbalancer", func(t *testing.T) {
@@ -665,7 +665,7 @@ func TestBuildHTTPRoutes(t *testing.T) {
 			},
 		})
 
-		vhosts := route.BuildSidecarVirtualHostsFromConfigAndRegistry(node, push, serviceRegistry, []config.Config{}, 8080)
+		vhosts := route.BuildSidecarVirtualHostWrapper(node, push, serviceRegistry, []config.Config{}, 8080)
 
 		hashPolicy := &envoyroute.RouteAction_HashPolicy{
 			PolicySpecifier: &envoyroute.RouteAction_HashPolicy_Cookie_{
