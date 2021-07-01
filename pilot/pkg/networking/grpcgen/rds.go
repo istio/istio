2b7715b39f1bcd4b56647f2983e7ad81829f15d1
Steven Landow <landow@google.com>
2021-07-01 10:35:05 -0700

    attempt re-using virthualhost gen
    Change-Id: Id84b343bfc52498f789272fb56df58526c64dab4

--- pilot/pkg/networking/grpcgen/rds.go
+++ pilot/pkg/networking/grpcgen/rds.go
@@ -15,15 +15,12 @@
 package grpcgen
 
 import (
-	"net"
-	"strconv"
-
-	"github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
+	routev3 "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
 	"github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
 
 	"istio.io/istio/pilot/pkg/model"
+	"istio.io/istio/pilot/pkg/networking/core/v1alpha3"
 	"istio.io/istio/pilot/pkg/networking/util"
-	"istio.io/istio/pkg/config/host"
 )
 
 // BuildHTTPRoutes supports per-VIP routes, as used by GRPC.
@@ -31,54 +28,29 @@ import (
 // Returns true of the request is of this type.
 func (g *GrpcConfigGenerator) BuildHTTPRoutes(node *model.Proxy, push *model.PushContext, routeNames []string) model.Resources {
 	resp := model.Resources{}
-
-	// Currently this mode is only used by GRPC, to extract Cluster for the default
-	// route.
-	for _, n := range routeNames {
-		_, _, hostname, port := model.ParseSubsetKey(n)
-		if hostname == "" || port == 0 {
-			log.Warn("Failed to parse ", n)
-			continue
-		}
-		hn := string(hostname)
-		el := node.SidecarScope.GetEgressListenerForRDS(port, "")
-		// TODO: use VirtualServices instead !
-		// Currently gRPC doesn't support matching the path.
-		svc := el.Services()
-		for _, s := range svc {
-			if s.Hostname.Matches(host.Name(hn)) {
-				// Only generate the required route for grpc. Will need to generate more
-				// as GRPC adds more features.
-				rc := &envoy_config_route_v3.RouteConfiguration{
-					Name: n,
-					VirtualHosts: []*envoy_config_route_v3.VirtualHost{
-						{
-							Name:    hn,
-							Domains: []string{hn, net.JoinHostPort(hn, strconv.Itoa(port))},
-
-							Routes: []*envoy_config_route_v3.Route{
-								{
-									Match: &envoy_config_route_v3.RouteMatch{
-										PathSpecifier: &envoy_config_route_v3.RouteMatch_Prefix{Prefix: ""},
-									},
-									Action: &envoy_config_route_v3.Route_Route{
-										Route: &envoy_config_route_v3.RouteAction{
-											ClusterSpecifier: &envoy_config_route_v3.RouteAction_Cluster{
-												Cluster: n,
-											},
-										},
-									},
-								},
-							},
-						},
-					},
-				}
-				resp = append(resp, &envoy_service_discovery_v3.Resource{
-					Name:     n,
-					Resource: util.MessageToAny(rc),
-				})
-			}
+	for _, routeName := range routeNames {
+		if rc := buildHTTPRoute(node, push, routeName); rc != nil {
+			resp = append(resp, &envoy_service_discovery_v3.Resource{
+				Name:     routeName,
+				Resource: util.MessageToAny(rc),
+			})
 		}
 	}
 	return resp
+}
+
+func buildHTTPRoute(node *model.Proxy, push *model.PushContext, routeName string) *routev3.RouteConfiguration {
+	// TODO use route-style naming instead of cluster naming
+	_, _, hostname, port := model.ParseSubsetKey(routeName)
+	if hostname == "" || port == 0 {
+		log.Warn("Failed to parse ", routeName)
+		return nil
+	}
+
+	// Only generate the required route for grpc. Will need to generate more
+	// as GRPC adds more features.
+	return &routev3.RouteConfiguration{
+		Name:         routeName,
+		VirtualHosts: v1alpha3.BuildSidecarOutboundVirtualHosts(node, push, routeName, port),
+	}
 }
