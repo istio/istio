ce4bc4fd6bd70064ec583cc0a082761d1b79a8fa
Pengyuan Bian <bianpengyuan@google.com>
2021-07-01 14:20:36 -0700

    Move CNI plugin code into plugin package. (#33771)
    * Move CNI code into plugin package.
    
    * remove unused vairable.
    
    * lint

--- cni/pkg/plugin/intercept_rule_mgr.go
+++ cni/pkg/plugin/intercept_rule_mgr.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Istio Authors
+// Copyright Istio Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
+package plugin
 
 const (
 	defInterceptRuleMgrType = "iptables"
