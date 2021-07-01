6e540a099924654373e6338845c64f4b2e9755b5
Pengyuan Bian <bianpengyuan@google.com>
2021-07-01 18:33:01 +0000

    Move CNI code into plugin package.

--- cni/pkg/plugin/redirect.go
+++ cni/pkg/plugin/redirect.go
@@ -1,4 +1,4 @@
-// Copyright 2018 Istio authors
+// Copyright Istio Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 // Defines the redirect object and operations.
-package main
+package plugin
 
 import (
 	"fmt"
