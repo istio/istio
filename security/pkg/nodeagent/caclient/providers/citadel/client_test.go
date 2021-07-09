4c8a948099cd7a0d7f375e22a78d105ebddf7f46
FENGXIANGLI <fengxiangli@google.com>
2021-07-08 17:56:44 -0700

    change unit test

--- security/pkg/nodeagent/caclient/providers/citadel/client_test.go
+++ security/pkg/nodeagent/caclient/providers/citadel/client_test.go
@@ -358,22 +358,6 @@ func TestCitadelClientWithDifferentTypeToken(t *testing.T) {
 }
 
 func TestCertExpired(t *testing.T) {
-	s := grpc.NewServer()
-	defer s.Stop()
-	lis, err := net.Listen("tcp", mockServerAddress)
-	if err != nil {
-		t.Fatalf("failed to listen: %v", err)
-	}
-	go func() {
-		pb.RegisterIstioCertificateServiceServer(s, &mockTokenCAServer{Certs: []string{}})
-		if err := s.Serve(lis); err != nil {
-			t.Logf("failed to serve: %v", err)
-		}
-	}()
-
-	opts := &security.Options{CAEndpoint: lis.Addr().String(), ClusterID: "Kubernetes", CredFetcher: plugin.CreateMockPlugin(validToken)}
-	cli, _ := NewCitadelClient(opts, false, nil)
-
 	testCases := map[string]struct {
 		filepath string
 		expected bool
@@ -388,6 +372,25 @@ func TestCertExpired(t *testing.T) {
 		},
 	}
 	for id, tc := range testCases {
+		s := grpc.NewServer()
+		defer s.Stop()
+		lis, err := net.Listen("tcp", mockServerAddress)
+		if err != nil {
+			t.Fatalf("failed to listen: %v", err)
+		}
+		go func() {
+			pb.RegisterIstioCertificateServiceServer(s, &mockTokenCAServer{Certs: []string{}})
+			if err := s.Serve(lis); err != nil {
+				t.Logf("failed to serve: %v", err)
+			}
+		}()
+
+		opts := &security.Options{CAEndpoint: lis.Addr().String(), ClusterID: "Kubernetes", CredFetcher: plugin.CreateMockPlugin(validToken)}
+		cli, err := NewCitadelClient(opts, false, nil)
+		if err != nil {
+			t.Fatalf("failed to create ca client: %v", err)
+		}
+		t.Cleanup(cli.Close)
 		t.Run(id, func(t *testing.T) {
 			certExpired, err := cli.isCertExpired(tc.filepath)
 			if err != nil {
