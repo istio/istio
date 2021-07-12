7ffad777b2c988b1b634f0db02c601a6b8ff8d8b
John Howard <howardjohn@google.com>
2021-07-12 09:00:12 -0700

    Remove proxy via agent option
    This has been stable for a number of releases now

--- pilot/cmd/pilot-agent/options/agent.go
+++ pilot/cmd/pilot-agent/options/agent.go
@@ -45,16 +45,12 @@ func NewAgentOptions(proxy *model.Proxy, cfg *meshconfig.ProxyConfig) *istioagen
 		Platform:                 platform.Discover(),
 		GRPCBootstrapPath:        grpcBootstrapEnv,
 		DisableEnvoy:             disableEnvoyEnv,
+		ProxyXDSDebugViaAgent:    proxyXDSDebugViaAgent,
+		DNSCapture:               DNSCaptureByAgent.Get(),
+		ProxyNamespace:           PodNamespaceVar.Get(),
+		ProxyDomain:              proxy.DNSDomain,
 	}
 	extractXDSHeadersFromEnv(o)
-	if proxyXDSViaAgent {
-		o.ProxyXDSViaAgent = true
-		o.ProxyXDSDebugViaAgent = proxyXDSDebugViaAgent
-		o.DNSCapture = DNSCaptureByAgent.Get()
-		o.ProxyNamespace = PodNamespaceVar.Get()
-		o.ProxyDomain = proxy.DNSDomain
-	}
-
 	return o
 }
 
