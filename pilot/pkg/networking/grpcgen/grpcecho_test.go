3ee9831395ff973794a0b5dcff274a6eea512bc0
Steven Landow <steven@stlcomputerservices.com>
2021-07-14 11:23:44 -0700

    implement Istio APIs in xDS for gRPC (#33756)
    * use istio-style xds cluster names
    
    Change-Id: I2bf1e3a2bb4442244982d771fa6ae9df0da8963b
    
    * split into separate files
    
    Change-Id: Ifb9e1bfd1d57f3ea29e4673f3688c902319194b3
    
    * fix cluster names
    
    Change-Id: Ic5e8fab6feb17efefc07822346745ba84645ead1
    
    * implement basic dr support
    
    Change-Id: If2e095040cdda1bb0eb774f4dfb5974632b4e2b9
    
    * warn for non round-robin lb
    
    Change-Id: I6d7f83bc95d8b23b20c4f87873cf2423d66b7c21
    
    * attempt re-using virthualhost gen
    
    Change-Id: Id84b343bfc52498f789272fb56df58526c64dab4
    
    * add basic unit test using echo
    
    Change-Id: I91fc99089ffd6f3ee86c396a062a89c5f96362da
    
    * proper virtualservice test
    
    Change-Id: I698a5ef38d0bf0bc727b0626e253b7f22c58a7ff
    
    * try traffic shifting integration test with proxyless
    
    Change-Id: I48c43186b1ed6b7e8927f378ea023c5e10828056
    
    * use correct targetport in test
    
    Change-Id: I48f443d34c0be6e66090e7f2fc4d8dce1ad8156c
    
    * handle subset-only cds requests
    
    Change-Id: I61d429a6b9194e9af2e64e16caf0d9328b48b403
    
    * debugability
    
    Change-Id: Id6ec3844a324528e58b4aab4dd4b64bccccebf17
    
    * ut cleanup
    
    Change-Id: I669ba6dce8d5701891dc6be26166c6d3a11fc803
    
    * fix echo deployment yaml
    
    Change-Id: Ia86e6e4ab9c1b5b63603ff50b70073affaf62120
    
    * rename
    
    Change-Id: I380ab878a3df1f5460f80f820e7cacc25b3cf5cd
    
    * use getter due to Cast DR behavior change
    
    Change-Id: Id6bd71f950ac9aa36c5397c70e1589f514dbf075
    
    * filter headless in echotest
    
    Change-Id: I2e558ff609915292102e47c1ba83e42a76669fbc
    
    * code cleanup
    
    Change-Id: I5ce5047b2ed176048ef5c5a60ac50e99994d3e3d
    
    * hack to force XDS scheme in echo req
    
    Change-Id: I5f174477284ef669c7c5bca74e01e868f952f408
    
    * more cleanup
    
    Change-Id: Icf7832a48bab72fe907087ecdfc2854c6137ec40
    
    * address comments
    
    Change-Id: I53f6bd41a584378db0025760ff0acab7c34590fd
    
    * lds cleanup
    
    Change-Id: I6fc8f3a426c2c1e2282ebd3e4b410f208dfaaafc
    
    * skip test for mac
    
    Change-Id: I038a9a2f8e84e3bda0d46067b3c00f03980ba172

---
+++ pilot/pkg/networking/grpcgen/grpcecho_test.go
@@ -0,0 +1,210 @@
+// Copyright Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package grpcgen_test
+
+import (
+	"context"
+	"fmt"
+	"math"
+	"net"
+	"runtime"
+	"testing"
+
+	"google.golang.org/grpc"
+
+	//  To install the xds resolvers and balancers.
+	_ "google.golang.org/grpc/xds"
+
+	networking "istio.io/api/networking/v1alpha3"
+	"istio.io/istio/pilot/pkg/xds"
+	"istio.io/istio/pkg/config"
+	"istio.io/istio/pkg/config/protocol"
+	"istio.io/istio/pkg/config/schema/collections"
+	"istio.io/istio/pkg/test/echo/client"
+	"istio.io/istio/pkg/test/echo/common"
+	"istio.io/istio/pkg/test/echo/proto"
+	"istio.io/istio/pkg/test/echo/server/endpoint"
+)
+
+const grpcEchoPort = 14058
+
+type echoCfg struct {
+	version   string
+	namespace string
+}
+
+type configGenTest struct {
+	*testing.T
+	endpoints []endpoint.Instance
+	ds        *xds.FakeDiscoveryServer
+}
+
+// newConfigGenTest creates a FakeDiscoveryServer that listens for gRPC on grpcXdsAddr
+// For each of the given servers, we serve echo (only supporting Echo, no ForwardEcho) and
+// create a corresponding WorkloadEntry. The WorkloadEntry will have the given format:
+//
+//    meta:
+//      name: echo-{generated portnum}-{server.version}
+//      namespace: {server.namespace or "default"}
+//      labels: {"app": "grpc", "version": "{server.version}"}
+//    spec:
+//      address: {grpcEchoHost}
+//      ports:
+//        grpc: {generated portnum}
+func newConfigGenTest(t *testing.T, discoveryOpts xds.FakeOptions, servers ...echoCfg) *configGenTest {
+	if runtime.GOOS == "darwin" {
+		// TODO always skip if this breaks anywhere else
+		t.Skip("cannot use 127.0.0.x on OSX")
+	}
+	cgt := &configGenTest{T: t}
+	for i, s := range servers {
+		// TODO this breaks without extra ifonfig aliases on OSX, and probably elsewhere
+		host := fmt.Sprintf("127.0.0.%d", i+1)
+		ep, err := endpoint.New(endpoint.Config{
+			IsServerReady: func() bool { return true },
+			Port:          &common.Port{Name: "grpc", Port: grpcEchoPort, Protocol: protocol.GRPC},
+			ListenerIP:    host,
+			Version:       s.version,
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		if err := ep.Start(func() {}); err != nil {
+			t.Fatal(err)
+		}
+		cgt.endpoints = append(cgt.endpoints, ep)
+		discoveryOpts.Configs = append(discoveryOpts.Configs, makeWE(s, host, grpcEchoPort))
+	}
+
+	discoveryOpts.ListenerBuilder = func() (net.Listener, error) {
+		return net.Listen("tcp", grpcXdsAddr)
+	}
+	cgt.ds = xds.NewFakeDiscoveryServer(t, discoveryOpts)
+	return cgt
+}
+
+func makeWE(s echoCfg, host string, port int) config.Config {
+	ns := "default"
+	if s.namespace != "" {
+		ns = s.namespace
+	}
+	return config.Config{
+		Meta: config.Meta{
+			Name:             fmt.Sprintf("echo-%d-%s", port, s.version),
+			Namespace:        ns,
+			GroupVersionKind: collections.IstioNetworkingV1Alpha3Workloadentries.Resource().GroupVersionKind(),
+			Labels: map[string]string{
+				"app":     "echo",
+				"version": s.version,
+			},
+		},
+		Spec: &networking.WorkloadEntry{
+			Address: host,
+			Ports:   map[string]uint32{"grpc": uint32(port)},
+		},
+	}
+}
+
+func (t *configGenTest) dialEcho(addr string) *client.Instance {
+	resolver := resolverForTest(t)
+	out, err := client.New(addr, nil, grpc.WithResolvers(resolver))
+	if err != nil {
+		t.Fatal(err)
+	}
+	return out
+}
+
+func TestGrpcVirtualService(t *testing.T) {
+	tt := newConfigGenTest(t, xds.FakeOptions{
+		KubernetesObjectString: `
+apiVersion: v1
+kind: Service
+metadata:
+  labels:
+    app: echo-app
+  name: echo-app
+  namespace: default
+spec:
+  clusterIP: 1.2.3.4
+  selector:
+    app: echo
+  ports:
+  - name: grpc
+    targetPort: grpc
+    port: 7070
+`,
+		ConfigString: `
+apiVersion: networking.istio.io/v1alpha3
+kind: DestinationRule
+metadata:
+  name: echo-dr
+  namespace: default
+spec:
+  host: echo-app.default.svc.cluster.local
+  subsets:
+    - name: v1
+      labels:
+        version: v1
+    - name: v2
+      labels:
+        version: v2
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: VirtualService
+metadata:
+  name: echo-vs
+  namespace: default
+spec:
+  hosts:
+  - echo-app.default.svc.cluster.local
+  http:
+  - route:
+    - destination:
+        host: echo-app.default.svc.cluster.local
+        subset: v1
+      weight: 20
+    - destination:
+        host: echo-app.default.svc.cluster.local
+        subset: v2
+      weight: 80
+
+`,
+	}, echoCfg{version: "v1"}, echoCfg{version: "v2"})
+	cw := tt.dialEcho("xds:///echo-app.default.svc.cluster.local:7070")
+
+	distribution := map[string]int{}
+
+	for i := 0; i < 100; i++ {
+		res, err := cw.Echo(context.Background(), &proto.EchoRequest{Message: "needle"})
+		if err != nil {
+			t.Fatal(err)
+		}
+		distribution[res.Version]++
+	}
+
+	if err := expectAlmost(distribution["v1"], 20); err != nil {
+		t.Fatal(err)
+	}
+	if err := expectAlmost(distribution["v2"], 80); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func expectAlmost(got, want int) error {
+	if math.Abs(float64(want-got)) > 10 {
+		return fmt.Errorf("expected ~%d but got %d", want, got)
+	}
+	return nil
+}
