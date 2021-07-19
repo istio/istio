223f48e6da54fa3db974e584d802dacd4fe093a9
gy95 <1015105054@qq.com>
2021-07-20 00:49:49 +0800

    no need to use defer to release response Body (#34124)

--- security/pkg/nodeagent/plugin/providers/google/stsclient/stsclient.go
+++ security/pkg/nodeagent/plugin/providers/google/stsclient/stsclient.go
@@ -51,7 +51,7 @@ type federatedTokenResponse struct {
 	ExpiresIn       int64  `json:"expires_in"` // Expiration time in seconds
 }
 
-// TokenExchanger for google securetoken api interaction.
+// SecureTokenServiceExchanger for google securetoken api interaction.
 type SecureTokenServiceExchanger struct {
 	httpClient  *http.Client
 	credFetcher security.CredFetcher
@@ -100,8 +100,8 @@ func (p *SecureTokenServiceExchanger) requestWithRetry(reqBytes []byte) ([]byte,
 			continue
 		}
 		if resp.StatusCode == http.StatusOK {
-			defer resp.Body.Close()
 			body, err := ioutil.ReadAll(resp.Body)
+			resp.Body.Close()
 			return body, err
 		}
 		body, _ := ioutil.ReadAll(resp.Body)
