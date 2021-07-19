b815bd916095a860d336d5839d712180f528e83f
Steven Landow <landow@google.com>
2021-07-19 16:42:47 -0700

    cleanup
    Change-Id: I9ed72ed9a8ae5e55b25de693820fb91da94e5e2b

--- istioctl/cmd/clusters.go
+++ istioctl/cmd/clusters.go
@@ -1,15 +1,30 @@
+// Copyright Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package cmd
 
 import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/spf13/cobra"
 	"io"
-	"istio.io/istio/pkg/cluster"
 	"text/tabwriter"
 
+	"github.com/spf13/cobra"
+
 	"istio.io/istio/istioctl/pkg/clioptions"
+	"istio.io/istio/pkg/cluster"
 )
 
 // TODO move to multicluster package; requires exposing some private funcs/vars in this package
