0129613e12579242005f27f4813f37a29b9a06bc
Iris <irisdingbj@gmail.com>
2021-07-20 00:49:21 +0800

    support signer from proxyConfig in istio-agent side (#33662)
    * support signer from proxyConfig in istio-agent side
    
    * fix error
    
    * change log level

--- pilot/pkg/features/pilot.go
+++ pilot/pkg/features/pilot.go
@@ -505,6 +505,8 @@ var (
 	// New behavior (true): we create listener 0.0.0.0_8080 and route http.8080. This has no conflicts; routes are 1:1 with listener.
 	UseTargetPortForGatewayRoutes = env.RegisterBoolVar("PILOT_USE_TARGET_PORT_FOR_GATEWAY_ROUTES", true,
 		"If true, routes will use the target port of the gateway service in the route name, not the service port.").Get()
+
+	CertSignerDomain = env.RegisterStringVar("CERT_SIGNER_DOMAIN", "", "The cert signer domain info").Get()
 )
 
 // UnsafeFeaturesEnabled returns true if any unsafe features are enabled.
