2f0e782376b42869bdb3a77cdadeb24b45b2547c
John Howard <howardjohn@google.com>
2021-07-20 17:22:10 -0700

    bump kind

--- prow/integ-suite-kind.sh
+++ prow/integ-suite-kind.sh
@@ -34,6 +34,9 @@ set -x
 source "${ROOT}/prow/lib.sh"
 setup_and_export_git_sha
 
+go install sigs.k8s.io/kind@v0.11.0
+kind version
+
 # shellcheck source=common/scripts/kind_provisioner.sh
 source "${ROOT}/common/scripts/kind_provisioner.sh"
 
