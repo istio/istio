b046ced51cceaec1a09492a5c06ea581883aa8c0
Jianfei Hu <jianfeih@google.com>
2021-07-23 09:46:57 -0700

    Fix race condition for auto mTLS plus workload level peer authn. (#34275)
    * Revert "Disable EnableAutomTLSCheckPolicies due to race condition (#34231)"
    
    This reverts commit 7f47cb97310085a6afd7cdc06879ce707379a330.
    
    * fix the race by copying.
    
    * fix the lint and remove dbg log.
    
    * lint fix.
    
    * wording comment fix.

--- pilot/pkg/networking/util/util.go
+++ pilot/pkg/networking/util/util.go
@@ -524,30 +524,41 @@ func BuildLbEndpointMetadata(networkID network.ID, tlsMode, workloadname, namesp
 	return metadata
 }
 
-// MaybeUpdateTLSModeLabel may or may not update the metadata for the Envoy transport socket matches for auto mTLS.
-func MaybeUpdateTLSModeLabel(metadata *core.Metadata, tlsMode string) bool {
-	if metadata == nil {
-		return false
+// MaybeApplyTLSModeLabel may or may not update the metadata for the Envoy transport socket matches for auto mTLS.
+func MaybeApplyTLSModeLabel(ep *endpoint.LbEndpoint, tlsMode string) (*endpoint.LbEndpoint, bool) {
+	if ep == nil || ep.Metadata == nil {
+		return nil, false
 	}
 	epTLSMode := ""
-	if metadata.FilterMetadata != nil {
-		if v, ok := metadata.FilterMetadata[EnvoyTransportSocketMetadataKey]; ok {
+	if ep.Metadata.FilterMetadata != nil {
+		if v, ok := ep.Metadata.FilterMetadata[EnvoyTransportSocketMetadataKey]; ok {
 			epTLSMode = v.Fields[model.TLSModeLabelShortname].GetStringValue()
 		}
 	}
-	if epTLSMode == tlsMode {
-		return false
+	// Normalize the tls label name before comparison. This ensure we won't falsely cloning
+	// the endpoint when they are "" and model.DisabledTLSModeLabel.
+	if epTLSMode == model.DisabledTLSModeLabel {
+		epTLSMode = ""
 	}
+	if tlsMode == model.DisabledTLSModeLabel {
+		tlsMode = ""
+	}
+	if epTLSMode == tlsMode {
+		return nil, false
+	}
+	// We make a copy instead of modifying on existing endpoint pointer directly to avoid data race.
+	// See https://github.com/istio/istio/issues/34227 for details.
+	newEndpoint := proto.Clone(ep).(*endpoint.LbEndpoint)
 	if tlsMode != "" && tlsMode != model.DisabledTLSModeLabel {
-		metadata.FilterMetadata[EnvoyTransportSocketMetadataKey] = &pstruct.Struct{
+		newEndpoint.Metadata.FilterMetadata[EnvoyTransportSocketMetadataKey] = &pstruct.Struct{
 			Fields: map[string]*pstruct.Value{
 				model.TLSModeLabelShortname: {Kind: &pstruct.Value_StringValue{StringValue: tlsMode}},
 			},
 		}
 	} else {
-		delete(metadata.FilterMetadata, EnvoyTransportSocketMetadataKey)
+		delete(newEndpoint.Metadata.FilterMetadata, EnvoyTransportSocketMetadataKey)
 	}
-	return true
+	return newEndpoint, true
 }
 
 func addIstioEndpointLabel(metadata *core.Metadata, key string, val *pstruct.Value) {
