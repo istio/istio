cf49a455bb78744a2276495adf62694bb115c3d0
Jonh Wendell <jonh.wendell@redhat.com>
2021-07-23 14:53:45 -0400

    Bump dependencies

--- prow/release-commit.sh
+++ prow/release-commit.sh
@@ -32,7 +32,7 @@ DOCKER_HUB=${DOCKER_HUB:-gcr.io/istio-testing}
 GCS_BUCKET=${GCS_BUCKET:-istio-build/dev}
 
 # Use a pinned version in case breaking changes are needed
-BUILDER_SHA=fb4c430f2ebaa3b55eac02730d040c508681bdf8
+BUILDER_SHA=68d16c51799c4a347f17ffaf45e5697fe65b1528
 
 # Reference to the next minor version of Istio
 # This will create a version like 1.4-alpha.sha
