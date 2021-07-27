cdb0abfb31d0169ee9209c35111935d37e85c9cf
Rama Chavali <rama.rao@salesforce.com>
2021-07-27 16:34:09 +0530

    more logging
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/bootstrap/server.go
+++ pilot/pkg/bootstrap/server.go
@@ -893,10 +893,10 @@ func (s *Server) initRegistryEventHandlers() {
 			}()
 			// For update events, trigger push only if spec has changed.
 			if event == model.EventUpdate {
-				log.Infof("config upadte event for %s, needsPush %v", prev.Key(), needsPush(prev, curr))
+				log.Infof("config upadte event for %v %v, needsPush %v", prev, curr, needsPush(prev, curr))
 			}
 			if event == model.EventUpdate && !needsPush(prev, curr) {
-				log.Debugf("skipping push for %s as spec has not changed", prev.Key())
+				log.Infof("skipping push for %s as spec has not changed", prev.Name)
 				return
 			}
 			pushReq := &model.PushRequest{
