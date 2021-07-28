e7090cc9b076bed44eadabb7a8a6f3c909eec0de
Yangmin Zhu <ymzhu@google.com>
2021-07-27 17:36:48 -0700

    fix test

--- pilot/pkg/security/authz/matcher/header_test.go
+++ pilot/pkg/security/authz/matcher/header_test.go
@@ -132,6 +132,8 @@ func TestHostMatcherWithRegex(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.Name, func(t *testing.T) {
 			actual := HostMatcherWithRegex(tc.K, tc.V)
+			// nolint: staticcheck
+			// Update to not use the deprecated fields later.
 			if re := actual.GetSafeRegexMatch().GetRegex(); re != "" {
 				_, err := regexp.Compile(re)
 				if err != nil {
