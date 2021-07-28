2b73ca536517c61db4f7b227ad8ca85aa53acd0f
Steven Landow <landow@google.com>
2021-07-28 16:17:32 -0700

    grpcgen: port-level lds filter
    Change-Id: I9bb52c689635c645f1cfc9305b00674e93b5c5f3

--- pilot/pkg/networking/grpcgen/lds.go
+++ pilot/pkg/networking/grpcgen/lds.go
@@ -15,14 +15,12 @@
 package grpcgen
 
 import (
-	"net"
-	"strconv"
-	"strings"
-
 	core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	listener "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
 	hcm "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
 	discovery "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
+	"net"
+	"strconv"
 
 	"istio.io/istio/pilot/pkg/model"
 	"istio.io/istio/pilot/pkg/networking/util"
@@ -34,29 +32,19 @@ import (
 func (g *GrpcConfigGenerator) BuildListeners(node *model.Proxy, push *model.PushContext, names []string) model.Resources {
 	resp := model.Resources{}
 
-	// TODO filter by port as well, this only filters by a host and includes every port on matching service
-	filter := map[string]struct{}{}
-	for _, name := range names {
-		if strings.Contains(name, ":") {
-			n, _, err := net.SplitHostPort(name)
-			if err == nil {
-				name = n
-			}
-		}
-		filter[name] = struct{}{}
-	}
-
+	filter := newListenerNameFilter(names)
 	for _, el := range node.SidecarScope.EgressListeners {
 		for _, sv := range el.Services() {
-			shost := string(sv.Hostname)
-			if len(filter) > 0 {
-				// DiscReq has a filter - only return services that match
-				if _, ok := filter[shost]; !ok {
-					continue
-				}
+			sHost := string(sv.Hostname)
+			if !filter.includeHostOrExactName(sHost) {
+				continue
 			}
 			for _, p := range sv.Ports {
-				hp := net.JoinHostPort(shost, strconv.Itoa(p.Port))
+				sPort := strconv.Itoa(p.Port)
+				if !filter.includeHostPort(sHost, sPort) {
+					continue
+				}
+				hp := net.JoinHostPort(sHost, sPort)
 				ll := &listener.Listener{
 					Name: hp,
 					Address: &core.Address{
@@ -79,7 +67,7 @@ func (g *GrpcConfigGenerator) BuildListeners(node *model.Proxy, push *model.Push
 											Ads: &core.AggregatedConfigSource{},
 										},
 									},
-									RouteConfigName: clusterKey(shost, p.Port),
+									RouteConfigName: clusterKey(sHost, p.Port),
 								},
 							},
 						}),
@@ -96,4 +84,60 @@ func (g *GrpcConfigGenerator) BuildListeners(node *model.Proxy, push *model.Push
 	}
 
 	return resp
+}
+
+// map[host] -> map[port] -> exists
+// if the map[port] is empty, an exact listener name was provided (non-hostport)
+type listenerNameFilter map[string]map[string]struct{}
+
+func (f listenerNameFilter) includeHostOrExactName(s string) bool {
+	if len(f) == 0 {
+		// filter is empty, include everything
+		return true
+	}
+	_, ok := f[s]
+	return ok
+}
+
+func (f listenerNameFilter) includeHostPort(host string, port string) bool {
+	if len(f) == 0 {
+		// filter is empty, include everything
+		return true
+	}
+	portMap, ok := f[host]
+	if !ok {
+		return false
+	}
+	if len(portMap) == 0 {
+		return true
+	}
+	_, ok = portMap[port]
+	return ok
+}
+
+func newListenerNameFilter(names []string) listenerNameFilter {
+	filter := make(listenerNameFilter, len(names))
+	for _, name := range names {
+		if host, port, err := net.SplitHostPort(name); err == nil {
+			var first bool
+			portMap, ok := filter[host]
+			if !ok {
+				portMap = map[string]struct{}{}
+				filter[host] = portMap
+				first = true
+			}
+			// the portMap is empty and we didn't just create it, we want to include all ports
+			if len(portMap) == 0 && !first {
+				continue
+			}
+
+			portMap[port] = struct{}{}
+		} else {
+			// if the listener name was "foo.com" and we already have "foo.com" -> {80 -> struct{}{}}, replace
+			// with an empty map to indicate we will include all ports since only the hostname was provided
+			// TODO, should we just default to some port in this case?
+			filter[name] = map[string]struct{}{}
+		}
+	}
+	return filter
 }
---
+++ pilot/pkg/networking/grpcgen/lds_test.go
@@ -0,0 +1,55 @@
+// Copyright Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package grpcgen
+
+import (
+	"istio.io/istio/pilot/pkg/model"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+var wildcardMap = map[string]struct{}{}
+
+func TestNewListenerNameFilter(t *testing.T) {
+	cases := map[string]struct {
+		in   []string
+		want listenerNameFilter
+	}{
+		"simple": {
+			in: []string{"foo.com:80", "foo.com:443", "wildcard.com"},
+			want: listenerNameFilter{
+				"foo.com":      map[string]struct{}{"80": {}, "443": {}},
+				"wildcard.com": wildcardMap,
+			},
+		},
+		"plain-host clears port-map": {
+			in:   []string{"foo.com:80", "foo.com"},
+			want: listenerNameFilter{"foo.com": wildcardMap},
+		},
+		"port-map stays clear": {
+			in:   []string{"foo.com:80", "foo.com", "foo.com:443"},
+			want: listenerNameFilter{"foo.com": wildcardMap},
+		},
+	}
+	for name, tt := range cases {
+		t.Run(name, func(t *testing.T) {
+			got := newListenerNameFilter(tt.in)
+			if diff := cmp.Diff(got, tt.want); diff != "" {
+				t.Fatal(diff)
+			}
+		})
+	}
+}
