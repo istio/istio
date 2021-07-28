6457bb6c8e7f205bcb9a4d7fd9ef18989fec4e57
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2021-07-29 08:22:49 +0900

    Fix root prefix

--- pilot/pkg/config/kube/gateway/conversion.go
+++ pilot/pkg/config/kube/gateway/conversion.go
@@ -870,7 +870,7 @@ func createURIMatch(match k8s.HTTPRouteMatch) (*istio.StringMatch, *ConfigError)
 	case k8s.PathMatchPrefix, k8s.PathMatchImplementationSpecific:
 		if dest == "/" {
 			return &istio.StringMatch{
-				MatchType: &istio.StringMatch_Regex{Regex: regexp.QuoteMeta(dest)},
+				MatchType: &istio.StringMatch_Prefix{Prefix: dest},
 			}, nil
 		}
 		return &istio.StringMatch{
