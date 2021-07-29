9ec46085e60f8415731f5d277a8f766ccf4e572b
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2021-07-30 08:20:34 +0900

    Revert "Drop double slash"
    This reverts commit 82619278e5603b0355af2d8cab1f76feb345dc10.

--- tests/integration/pilot/ingress_test.go
+++ tests/integration/pilot/ingress_test.go
@@ -402,12 +402,29 @@ spec:
 				},
 				{
 					// Prefix / should match any path
-					name: "http-root-prefix-should-match-random-path",
+					name: "http-root-prefix-should-match-random-path-continuation",
 					call: echo.CallOptions{
 						Port: &echo.Port{
 							Protocol: protocol.HTTP,
 						},
 						Path: "/testrandom",
+						Headers: map[string][]string{
+							"Host": {"server"},
+						},
+						Validator: successValidator,
+						Count:     count,
+					},
+					path:       "/test",
+					prefixPath: "/",
+				},
+				{
+					// Prefix / should match any path
+					name: "http-root-prefix-should-match-multi-paths-continuation",
+					call: echo.CallOptions{
+						Port: &echo.Port{
+							Protocol: protocol.HTTP,
+						},
+						Path: "//",
 						Headers: map[string][]string{
 							"Host": {"server"},
 						},
