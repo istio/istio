a1ae7dc33c24e8faeee6330c6235fa8203149610
xuzhonghu <xuzhonghu@huawei.com>
2021-08-05 17:43:50 +0800

    Skip push cds for sidecar when virtualService updated

--- pilot/pkg/xds/cds_test.go
+++ pilot/pkg/xds/cds_test.go
@@ -16,12 +16,126 @@ package xds_test
 import (
 	"testing"
 
+	"istio.io/istio/pilot/pkg/model"
 	"istio.io/istio/pilot/pkg/xds"
 	v3 "istio.io/istio/pilot/pkg/xds/v3"
+	"istio.io/istio/pkg/config/schema/gvk"
 )
 
 func TestCDS(t *testing.T) {
 	s := xds.NewFakeDiscoveryServer(t, xds.FakeOptions{})
 	ads := s.ConnectADS().WithType(v3.ClusterType)
 	ads.RequestResponseAck(t, nil)
+}
+
+func TestCdsNeedsPush(t *testing.T) {
+	testCases := []struct {
+		name           string
+		proxyType      model.NodeType
+		configsUpdated map[model.ConfigKey]struct{}
+		expected       bool
+	}{
+		{
+			name:      "sidecar without configs that do not influence cds",
+			proxyType: model.SidecarProxy,
+			configsUpdated: map[model.ConfigKey]struct{}{
+				{
+					Kind:      gvk.VirtualService,
+					Name:      "vs1",
+					Namespace: "test",
+				}: {},
+				{
+					Kind:      gvk.Gateway,
+					Name:      "gw1",
+					Namespace: "test",
+				}: {},
+				{
+					Kind:      gvk.AuthorizationPolicy,
+					Name:      "authz1",
+					Namespace: "test",
+				}: {},
+				{
+					Kind:      gvk.RequestAuthentication,
+					Name:      "ra1",
+					Namespace: "test",
+				}: {},
+			},
+			expected: false,
+		},
+		{
+			name:      "sidecar with configs that influence cds",
+			proxyType: model.SidecarProxy,
+			configsUpdated: map[model.ConfigKey]struct{}{
+				{
+					Kind:      gvk.VirtualService,
+					Name:      "vs1",
+					Namespace: "test",
+				}: {},
+				{
+					Kind:      gvk.Gateway,
+					Name:      "gw1",
+					Namespace: "test",
+				}: {},
+				{
+					Kind:      gvk.AuthorizationPolicy,
+					Name:      "authz1",
+					Namespace: "test",
+				}: {},
+				{
+					Kind:      gvk.RequestAuthentication,
+					Name:      "ra1",
+					Namespace: "test",
+				}: {},
+				{
+					Kind:      gvk.DestinationRule,
+					Name:      "ra1",
+					Namespace: "test",
+				}: {},
+			},
+			expected: true,
+		},
+		{
+			name:      "gateway with nil configs",
+			proxyType: model.SidecarProxy,
+			expected:  true,
+		},
+		{
+			name:      "gateway with common configs that influence cds",
+			proxyType: model.Router,
+			configsUpdated: map[model.ConfigKey]struct{}{
+				{
+					Kind:      gvk.VirtualService,
+					Name:      "vs1",
+					Namespace: "test",
+				}: {},
+			},
+			expected: true,
+		},
+		{
+			name:      "gateway with common configs that influence cds",
+			proxyType: model.Router,
+			configsUpdated: map[model.ConfigKey]struct{}{
+				{
+					Kind:      gvk.ServiceEntry,
+					Name:      "svc",
+					Namespace: "test",
+				}: {},
+			},
+			expected: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			req := &model.PushRequest{
+				Full:           true,
+				ConfigsUpdated: tc.configsUpdated,
+			}
+			proxy := &model.Proxy{Type: tc.proxyType}
+			got := xds.CdsNeedsPush(req, proxy)
+			if got != tc.expected {
+				t.Errorf("Got unexpected %v, expected %v", got, tc.expected)
+			}
+		})
+	}
 }
