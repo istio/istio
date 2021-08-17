311847134ff36fc85771e00b73603c367d405f8e
John Howard <howardjohn@google.com>
2021-08-17 11:19:05 -0700

    Run go1.17 gofmt
    In preperation for updating CI to 1.17
    
    For https://github.com/istio/istio/issues/33382

--- tests/integration/pilot/ingress_test.go
+++ tests/integration/pilot/ingress_test.go
@@ -1,4 +1,6 @@
+//go:build integ
 // +build integ
+
 // Copyright Istio Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
--- tests/integration/security/fuzz/fuzz_test.go
+++ tests/integration/security/fuzz/fuzz_test.go
@@ -1,4 +1,6 @@
+//go:build integfuzz
 // +build integfuzz
+
 // Copyright Istio Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
