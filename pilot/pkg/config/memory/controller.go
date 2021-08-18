3d3c88ac58924484adf0209e1138985c1e973c48
xuzhonghu <xuzhonghu@huawei.com>
2021-08-18 21:05:15 +0800

    Simplify memory controller has synced cd

--- pilot/pkg/config/memory/controller.go
+++ pilot/pkg/config/memory/controller.go
@@ -26,92 +26,69 @@ import (
 	"istio.io/istio/pkg/config/schema/collection"
 )
 
-var PatchHasSynced = func(hasSynced func() bool) func(model.ConfigStoreCache) {
-	return wrapPatch(func(c *controller) {
-		c.hasSynced = hasSynced
-	})
-}
-
-func wrapPatch(p func(c *controller)) func(model.ConfigStoreCache) {
-	return func(c model.ConfigStoreCache) {
-		if c == nil {
-			return
-		}
-		con, ok := c.(*controller)
-		if !ok || con == nil {
-			return
-		}
-		p(con)
-	}
-}
-
-func applyPatches(out *controller, patches ...func(model.ConfigStoreCache)) {
-	for _, p := range patches {
-		p(out)
-	}
-}
-
-type controller struct {
+// Controller is an implementation of ConfigStoreCache.
+type Controller struct {
 	monitor     Monitor
 	configStore model.ConfigStore
 	hasSynced   func() bool
 }
 
-// NewController return an implementation of model.ConfigStoreCache
+// NewController return an implementation of ConfigStoreCache
 // This is a client-side monitor that dispatches events as the changes are being
 // made on the client.
-func NewController(cs model.ConfigStore, patches ...func(model.ConfigStoreCache)) model.ConfigStoreCache {
-	out := &controller{
+func NewController(cs model.ConfigStore) *Controller {
+	out := &Controller{
 		configStore: cs,
 		monitor:     NewMonitor(cs),
 	}
-
-	applyPatches(out, patches...)
 	return out
 }
 
 // NewSyncController return an implementation of model.ConfigStoreCache which processes events synchronously
-func NewSyncController(cs model.ConfigStore, patches ...func(model.ConfigStoreCache)) model.ConfigStoreCache {
-	out := &controller{
+func NewSyncController(cs model.ConfigStore) *Controller {
+	out := &Controller{
 		configStore: cs,
 		monitor:     NewSyncMonitor(cs),
 	}
 
-	applyPatches(out, patches...)
 	return out
 }
 
-func (c *controller) RegisterEventHandler(kind config.GroupVersionKind, f model.EventHandler) {
+func (c *Controller) RegisterHasSyncedHandler(cb func() bool) {
+	c.hasSynced = cb
+}
+
+func (c *Controller) RegisterEventHandler(kind config.GroupVersionKind, f model.EventHandler) {
 	c.monitor.AppendEventHandler(kind, f)
 }
 
-func (c *controller) SetWatchErrorHandler(handler func(r *cache.Reflector, err error)) error {
+func (c *Controller) SetWatchErrorHandler(handler func(r *cache.Reflector, err error)) error {
 	return nil
 }
 
 // HasSynced return whether store has synced
 // It can be controlled externally (such as by the data source),
 // otherwise it'll always consider synced.
-func (c *controller) HasSynced() bool {
+func (c *Controller) HasSynced() bool {
 	if c.hasSynced != nil {
 		return c.hasSynced()
 	}
 	return true
 }
 
-func (c *controller) Run(stop <-chan struct{}) {
+func (c *Controller) Run(stop <-chan struct{}) {
 	c.monitor.Run(stop)
 }
 
-func (c *controller) Schemas() collection.Schemas {
+func (c *Controller) Schemas() collection.Schemas {
 	return c.configStore.Schemas()
 }
 
-func (c *controller) Get(kind config.GroupVersionKind, key, namespace string) *config.Config {
+func (c *Controller) Get(kind config.GroupVersionKind, key, namespace string) *config.Config {
 	return c.configStore.Get(kind, key, namespace)
 }
 
-func (c *controller) Create(config config.Config) (revision string, err error) {
+func (c *Controller) Create(config config.Config) (revision string, err error) {
 	if revision, err = c.configStore.Create(config); err == nil {
 		c.monitor.ScheduleProcessEvent(ConfigEvent{
 			config: config,
@@ -121,7 +98,7 @@ func (c *controller) Create(config config.Config) (revision string, err error) {
 	return
 }
 
-func (c *controller) Update(config config.Config) (newRevision string, err error) {
+func (c *Controller) Update(config config.Config) (newRevision string, err error) {
 	oldconfig := c.configStore.Get(config.GroupVersionKind, config.Name, config.Namespace)
 	if newRevision, err = c.configStore.Update(config); err == nil {
 		c.monitor.ScheduleProcessEvent(ConfigEvent{
@@ -133,7 +110,7 @@ func (c *controller) Update(config config.Config) (newRevision string, err error
 	return
 }
 
-func (c *controller) UpdateStatus(config config.Config) (newRevision string, err error) {
+func (c *Controller) UpdateStatus(config config.Config) (newRevision string, err error) {
 	oldconfig := c.configStore.Get(config.GroupVersionKind, config.Name, config.Namespace)
 	if newRevision, err = c.configStore.UpdateStatus(config); err == nil {
 		c.monitor.ScheduleProcessEvent(ConfigEvent{
@@ -145,7 +122,7 @@ func (c *controller) UpdateStatus(config config.Config) (newRevision string, err
 	return
 }
 
-func (c *controller) Patch(orig config.Config, patchFn config.PatchFunc) (newRevision string, err error) {
+func (c *Controller) Patch(orig config.Config, patchFn config.PatchFunc) (newRevision string, err error) {
 	cfg, typ := patchFn(orig.DeepCopy())
 	switch typ {
 	case types.MergePatchType:
@@ -163,7 +140,7 @@ func (c *controller) Patch(orig config.Config, patchFn config.PatchFunc) (newRev
 	return
 }
 
-func (c *controller) Delete(kind config.GroupVersionKind, key, namespace string, resourceVersion *string) (err error) {
+func (c *Controller) Delete(kind config.GroupVersionKind, key, namespace string, resourceVersion *string) (err error) {
 	if config := c.Get(kind, key, namespace); config != nil {
 		if err = c.configStore.Delete(kind, key, namespace, resourceVersion); err == nil {
 			c.monitor.ScheduleProcessEvent(ConfigEvent{
@@ -176,6 +153,6 @@ func (c *controller) Delete(kind config.GroupVersionKind, key, namespace string,
 	return errors.New("Delete failure: config" + key + "does not exist")
 }
 
-func (c *controller) List(kind config.GroupVersionKind, namespace string) ([]config.Config, error) {
+func (c *Controller) List(kind config.GroupVersionKind, namespace string) ([]config.Config, error) {
 	return c.configStore.List(kind, namespace)
 }
