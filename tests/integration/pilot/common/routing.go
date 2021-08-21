7d309bb1e31ac4fe51f28a1b687609389b918d3b
Zufar Dhiyaulhaq <zufardhiyaulhaq@gmail.com>
2021-08-21 14:44:28 +0200

    fix integration tests
    Signed-off-by: Zufar Dhiyaulhaq <zufardhiyaulhaq@gmail.com>

--- tests/integration/pilot/common/routing.go
+++ tests/integration/pilot/common/routing.go
@@ -1041,7 +1041,7 @@ spec:
 		{
 			// See https://github.com/istio/istio/issues/27315
 			// See https://github.com/istio/istio/issues/34609
-			name:             "http redirect with with x-forwarded-proto when vs port specify https",
+			name:             "http ok with with x-forwarded-proto when vs port specify https",
 			targetFilters:    singleTarget,
 			workloadAgnostic: true,
 			viaIngress:       true,
@@ -1097,7 +1097,7 @@ spec:
 					// In real world, this may be set by a downstream LB that terminates the TLS
 					"X-Forwarded-Proto": {"https"},
 				},
-				Validator: echo.ExpectCode("301"),
+				Validator: echo.ExpectOK(),
 			},
 			setupOpts: fqdnHostHeader,
 			templateVars: func(_ echo.Callers, dests echo.Instances) map[string]interface{} {
