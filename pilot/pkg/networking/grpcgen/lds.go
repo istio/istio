f083dc1409a19d2f4ebff1fb2cbf347d5977096f
Steven Landow <steven@stlcomputerservices.com>
2021-08-30 07:56:26 -1000

    grpcgen: get services from push context (#34921)
    Change-Id: I69db8350d1b5e64b5eb9690a7a4e9ab79cc9f3cf

--- pilot/pkg/networking/grpcgen/lds.go
+++ pilot/pkg/networking/grpcgen/lds.go
@@ -46,7 +46,7 @@ func (g *GrpcConfigGenerator) BuildListeners(node *model.Proxy, push *model.Push
 	log.Debugf("building lds for %s with filter:\n%v", node.ID, filter)
 
 	resp := make(model.Resources, 0, len(filter))
-	resp = append(resp, buildOutboundListeners(node, filter)...)
+	resp = append(resp, buildOutboundListeners(node, push, filter)...)
 	resp = append(resp, buildInboundListeners(node, push, filter.inboundNames())...)
 
 	return resp
@@ -161,55 +161,53 @@ func buildFilterChain(nameSuffix string, tlsContext *tls.DownstreamTlsContext) *
 	return out
 }
 
-func buildOutboundListeners(node *model.Proxy, filter listenerNames) model.Resources {
+func buildOutboundListeners(node *model.Proxy, push *model.PushContext, filter listenerNames) model.Resources {
 	out := make(model.Resources, 0, len(filter))
-	for _, el := range node.SidecarScope.EgressListeners {
-		for _, sv := range el.Services() {
-			serviceHost := string(sv.Hostname)
-			match, ok := filter.includes(serviceHost)
-			if !ok {
-				continue
-			}
-			// we must duplicate the listener for every requested host - grpc may have watches for both foo and foo.ns
-			for _, matchedHost := range match.RequestedNames.SortedList() {
-				for _, p := range sv.Ports {
-					sPort := strconv.Itoa(p.Port)
-					if !match.includesPort(sPort) {
-						continue
-					}
-					ll := &listener.Listener{
-						Name: net.JoinHostPort(matchedHost, sPort),
-						Address: &core.Address{
-							Address: &core.Address_SocketAddress{
-								SocketAddress: &core.SocketAddress{
-									Address: sv.GetServiceAddressForProxy(node),
-									PortSpecifier: &core.SocketAddress_PortValue{
-										PortValue: uint32(p.Port),
-									},
+	for _, sv := range push.Services(node) {
+		serviceHost := string(sv.Hostname)
+		match, ok := filter.includes(serviceHost)
+		if !ok {
+			continue
+		}
+		// we must duplicate the listener for every requested host - grpc may have watches for both foo and foo.ns
+		for _, matchedHost := range match.RequestedNames.SortedList() {
+			for _, p := range sv.Ports {
+				sPort := strconv.Itoa(p.Port)
+				if !match.includesPort(sPort) {
+					continue
+				}
+				ll := &listener.Listener{
+					Name: net.JoinHostPort(matchedHost, sPort),
+					Address: &core.Address{
+						Address: &core.Address_SocketAddress{
+							SocketAddress: &core.SocketAddress{
+								Address: sv.GetServiceAddressForProxy(node),
+								PortSpecifier: &core.SocketAddress_PortValue{
+									PortValue: uint32(p.Port),
 								},
 							},
 						},
-						ApiListener: &listener.ApiListener{
-							ApiListener: util.MessageToAny(&hcm.HttpConnectionManager{
-								RouteSpecifier: &hcm.HttpConnectionManager_Rds{
-									// TODO: for TCP listeners don't generate RDS, but some indication of cluster name.
-									Rds: &hcm.Rds{
-										ConfigSource: &core.ConfigSource{
-											ConfigSourceSpecifier: &core.ConfigSource_Ads{
-												Ads: &core.AggregatedConfigSource{},
-											},
+					},
+					ApiListener: &listener.ApiListener{
+						ApiListener: util.MessageToAny(&hcm.HttpConnectionManager{
+							RouteSpecifier: &hcm.HttpConnectionManager_Rds{
+								// TODO: for TCP listeners don't generate RDS, but some indication of cluster name.
+								Rds: &hcm.Rds{
+									ConfigSource: &core.ConfigSource{
+										ConfigSourceSpecifier: &core.ConfigSource_Ads{
+											Ads: &core.AggregatedConfigSource{},
 										},
-										RouteConfigName: clusterKey(serviceHost, p.Port),
 									},
+									RouteConfigName: clusterKey(serviceHost, p.Port),
 								},
-							}),
-						},
-					}
-					out = append(out, &discovery.Resource{
-						Name:     ll.Name,
-						Resource: util.MessageToAny(ll),
-					})
+							},
+						}),
+					},
 				}
+				out = append(out, &discovery.Resource{
+					Name:     ll.Name,
+					Resource: util.MessageToAny(ll),
+				})
 			}
 		}
 	}
