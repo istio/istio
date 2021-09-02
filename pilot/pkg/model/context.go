b1436f9ebce135d2e204325f75a323712222cb39
Costin Manolache <costin@gmail.com>
2021-09-02 14:17:19 -0700

    Allow http proxy to be enabled per workload instead of global. (#34774)
    * Allow http proxy to be enabled per workload instead of global.
    
    * Fix lint
    
    * Fix lint
    
    Co-authored-by: Costin Manolache <costin@google.com>

--- pilot/pkg/model/context.go
+++ pilot/pkg/model/context.go
@@ -537,6 +537,10 @@ type NodeMetadata struct {
 	// ServiceAccount specifies the service account which is running the workload.
 	ServiceAccount string `json:"SERVICE_ACCOUNT,omitempty"`
 
+	// HTTPProxyPort enables http proxy on the port for the current sidecar.
+	// Same as MeshConfig.HttpProxyPort, but with per/sidecar scope.
+	HTTPProxyPort string `json:"HTTP_PROXY_PORT,omitempty"`
+
 	// RouterMode indicates whether the proxy is functioning as a SNI-DNAT router
 	// processing the AUTO_PASSTHROUGH gateway servers
 	RouterMode string `json:"ROUTER_MODE,omitempty"`
