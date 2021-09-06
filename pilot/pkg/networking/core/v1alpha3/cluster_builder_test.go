635a21ab1609952ec7770b7d9d6232e77b6a2192
xuzhonghu <xuzhonghu@huawei.com>
2021-09-06 10:35:32 +0800

    make use of new HttpProtocolOptions.CommonHttpProtocolOptions.MaxRequestsPerConnection

--- pilot/pkg/networking/core/v1alpha3/cluster_builder_test.go
+++ pilot/pkg/networking/core/v1alpha3/cluster_builder_test.go
@@ -241,6 +241,26 @@ func TestApplyDestinationRule(t *testing.T) {
 			expectedSubsetClusters: []*cluster.Cluster{},
 		},
 		{
+			name:        "destination rule with maxRequestsPerConnection",
+			cluster:     &cluster.Cluster{Name: "foo", ClusterDiscoveryType: &cluster.Cluster_Type{Type: cluster.Cluster_EDS}},
+			clusterMode: DefaultClusterMode,
+			service:     service,
+			port:        servicePort[0],
+			networkView: map[network.ID]bool{},
+			destRule: &networking.DestinationRule{
+				Host: "foo.default.svc.cluster.local",
+				TrafficPolicy: &networking.TrafficPolicy{
+					ConnectionPool: &networking.ConnectionPoolSettings{
+						Http: &networking.ConnectionPoolSettings_HTTPSettings{
+							MaxRetries:               10,
+							MaxRequestsPerConnection: 10,
+						},
+					},
+				},
+			},
+			expectedSubsetClusters: []*cluster.Cluster{},
+		},
+		{
 			name:        "subset without labels in both",
 			cluster:     &cluster.Cluster{Name: "foo", ClusterDiscoveryType: &cluster.Cluster_Type{Type: cluster.Cluster_STRICT_DNS}},
 			clusterMode: DefaultClusterMode,
@@ -407,6 +427,21 @@ func TestApplyDestinationRule(t *testing.T) {
 					ec.httpProtocolOptions.GetUseDownstreamProtocolConfig() == nil {
 					t.Errorf("Expected cluster %s to have downstream protocol options but not found", tt.cluster.Name)
 				}
+			}
+
+			// Validate that use client protocol configures cluster correctly.
+			if tt.destRule != nil && tt.destRule.TrafficPolicy != nil && tt.destRule.TrafficPolicy.GetConnectionPool().GetHttp().MaxRequestsPerConnection > 0 {
+				if ec.httpProtocolOptions == nil {
+					t.Errorf("Expected cluster %s to have http protocol options but not found", tt.cluster.Name)
+				}
+				if ec.httpProtocolOptions.CommonHttpProtocolOptions == nil {
+					t.Errorf("Expected cluster %s to have common http protocol options but not found", tt.cluster.Name)
+				}
+				if ec.httpProtocolOptions.CommonHttpProtocolOptions.MaxRequestsPerConnection.GetValue() !=
+					uint32(tt.destRule.TrafficPolicy.GetConnectionPool().GetHttp().MaxRequestsPerConnection) {
+					t.Errorf("Unexpected max_requests_per_connection found")
+				}
+
 			}
 
 			// Validate that ORIGINAL_DST cluster does not have load assignments
