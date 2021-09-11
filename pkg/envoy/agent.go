e7f59c6c07f2db7a65884caa7931ada47c796008
Rama Chavali <rama.rao@salesforce.com>
2021-09-11 15:41:06 +0530

    lint
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pkg/envoy/agent.go
+++ pkg/envoy/agent.go
@@ -36,7 +36,6 @@ var activeConnectionCheckDelay = 1 * time.Second
 // NewAgent creates a new proxy agent for the proxy start-up and clean-up functions.
 func NewAgent(proxy Proxy, terminationDrainDuration, minDrainDuration time.Duration, localhost string,
 	adminPort, statusPort, prometheusPort int) *Agent {
-
 	knownIstioListeners := sets.NewSet(
 		fmt.Sprintf("listener.0.0.0.0_%d.downstream_cx_active", statusPort),
 		fmt.Sprintf("listener.0.0.0.0_%d.downstream_cx_active", prometheusPort),
