0f5a11ee9e62c76167d42d8e2e914f90c6e5d78f
Xiaopeng Han <hanxiaop8@outlook.com>
2021-09-14 13:45:30 +0800

    fix analyzer test and conflicting gateway analyzer (#35095)
    * fix analyzeCRD func, and fix conflict gateway analyzer if no selector exists
    
    * add type switch
    
    * revise for lint

--- galley/pkg/config/analysis/analyzers/util/find_errorline_utils.go
+++ galley/pkg/config/analysis/analyzers/util/find_errorline_utils.go
@@ -116,6 +116,9 @@ func ErrorLine(r *resource.Instance, path string) (line int, found bool) {
 // ExtractLabelFromSelectorString returns the label of the match in the k8s labels.Selector
 func ExtractLabelFromSelectorString(s string) string {
 	equalIndex := strings.Index(s, "=")
+	if equalIndex < 0 {
+		return ""
+	}
 	return s[:equalIndex]
 }
 
