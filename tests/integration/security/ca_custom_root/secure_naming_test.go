d0513f6bfcd3b539e1fc0fd026679918868fcccd
Frank Budinsky <frankb@ca.ibm.com>
2021-09-14 09:21:29 -0400

    Fix TestStrictMTLS test to work in istiodremote topology

--- tests/integration/security/ca_custom_root/secure_naming_test.go
+++ tests/integration/security/ca_custom_root/secure_naming_test.go
@@ -30,6 +30,7 @@ import (
 	"istio.io/istio/pkg/config/constants"
 	"istio.io/istio/pkg/test/echo/common/scheme"
 	"istio.io/istio/pkg/test/framework"
+	"istio.io/istio/pkg/test/framework/components/cluster/kube"
 	"istio.io/istio/pkg/test/framework/components/echo"
 	"istio.io/istio/pkg/test/framework/components/istio"
 	"istio.io/istio/pkg/test/framework/components/namespace"
@@ -133,7 +134,7 @@ func TestSecureNaming(t *testing.T) {
 							// Verify that the certificate issued to the sidecar is as expected.
 							connectTarget := fmt.Sprintf("b.%s:8095", testNamespace.Name())
 							out, err := cert.DumpCertFromSidecar(testNamespace, "app=a", "istio-proxy",
-								connectTarget)
+								(cluster.(*kube.Cluster)).Filename(), connectTarget)
 							if err != nil {
 								t.Fatalf("failed to dump certificate: %v", err)
 								return
