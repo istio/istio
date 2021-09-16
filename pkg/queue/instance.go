8f527d148029a7eb90ed7ca18acbc6c65b324601
Rama Chavali <rama.rao@salesforce.com>
2021-09-16 13:32:40 +0530

    add exponential backoff to queue

--- pkg/queue/instance.go
+++ pkg/queue/instance.go
@@ -18,11 +18,17 @@ import (
 	"sync"
 	"time"
 
+	"github.com/cenkalti/backoff"
 	"istio.io/pkg/log"
 )
 
 // Task to be performed.
 type Task func() error
+
+type BackoffTask struct {
+	task Task
+	eb   *backoff.ExponentialBackOff
+}
 
 // Instance of work tickets processed using a rate-limiting loop
 type Instance interface {
@@ -34,22 +40,39 @@ type Instance interface {
 
 type queueImpl struct {
 	delay   time.Duration
-	tasks   []Task
+	tasks   []*BackoffTask
 	cond    *sync.Cond
 	closing bool
+}
+
+func newExponentialBackOff(delay time.Duration) *backoff.ExponentialBackOff {
+	eb := backoff.NewExponentialBackOff()
+	eb.InitialInterval = delay
+	eb.MaxElapsedTime = 0                          // never elapses.
+	eb.MaxInterval = backoff.DefaultMaxElapsedTime // set max inteerval to 15 mins.
+	return eb
 }
 
 // NewQueue instantiates a queue with a processing function
 func NewQueue(errorDelay time.Duration) Instance {
 	return &queueImpl{
 		delay:   errorDelay,
-		tasks:   make([]Task, 0),
+		tasks:   make([]*BackoffTask, 0),
 		closing: false,
 		cond:    sync.NewCond(&sync.Mutex{}),
 	}
 }
 
 func (q *queueImpl) Push(item Task) {
+	q.cond.L.Lock()
+	defer q.cond.L.Unlock()
+	if !q.closing {
+		q.tasks = append(q.tasks, &BackoffTask{item, newExponentialBackOff(q.delay)})
+	}
+	q.cond.Signal()
+}
+
+func (q *queueImpl) pushRetryTask(item *BackoffTask) {
 	q.cond.L.Lock()
 	defer q.cond.L.Unlock()
 	if !q.closing {
@@ -79,17 +102,17 @@ func (q *queueImpl) Run(stop <-chan struct{}) {
 			return
 		}
 
-		task := q.tasks[0]
+		backoffTask := q.tasks[0]
 		// Slicing will not free the underlying elements of the array, so explicitly clear them out here
 		q.tasks[0] = nil
 		q.tasks = q.tasks[1:]
 
 		q.cond.L.Unlock()
 
-		if err := task(); err != nil {
+		if err := backoffTask.task(); err != nil {
 			log.Infof("Work item handle failed (%v), retry after delay %v", err, q.delay)
-			time.AfterFunc(q.delay, func() {
-				q.Push(task)
+			time.AfterFunc(backoffTask.eb.NextBackOff(), func() {
+				q.pushRetryTask(backoffTask)
 			})
 		}
 	}
