3c10504f53a68ab1497ef59104a260ce5fcd0bc8
John Howard <howardjohn@google.com>
2021-10-01 08:39:02 -0700

    bump

--- prow/release-commit.sh
+++ prow/release-commit.sh
@@ -32,7 +32,7 @@ DOCKER_HUB=${DOCKER_HUB:-gcr.io/istio-testing}
 GCS_BUCKET=${GCS_BUCKET:-istio-build/dev}
 
 # Use a pinned version in case breaking changes are needed
-BUILDER_SHA=343862e8279d9acd0308256b69a5f93fe74d5fde
+BUILDER_SHA=b84702ef5d23549007093d9e59c0597e8d3538c2
 
 # Reference to the next minor version of Istio
 # This will create a version like 1.4-alpha.sha
