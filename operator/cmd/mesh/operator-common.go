1e2f8d4b1b07c3bd385b634e1878d61132942f1e
Arko Dasgupta <arko@tetrate.io>
2021-10-06 12:17:20 -0700

    fix build
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>

--- operator/cmd/mesh/operator-common.go
+++ operator/cmd/mesh/operator-common.go
@@ -24,6 +24,7 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	"istio.io/istio/operator/pkg/helm"
+	"istio.io/istio/operator/pkg/manifest"
 	"istio.io/istio/operator/pkg/name"
 	"istio.io/istio/operator/pkg/util"
 )
@@ -65,7 +66,7 @@ func isControllerInstalled(cs kubernetes.Interface, operatorNamespace string, re
 // renderOperatorManifest renders a manifest to install the operator with the given input arguments.
 func renderOperatorManifest(_ *rootArgs, ocArgs *operatorCommonArgs) (string, string, error) {
 	// If manifestsPath is a URL, fetch and extract it and continue with the local filesystem path instead.
-	installPackagePath, _, err = rewriteURLToLocalInstallPath(ocArgs.manifestsPath, "" /*profileOrPath*/, false /*skipValidation */)
+	installPackagePath, _, err := manifest.RewriteURLToLocalInstallPath(ocArgs.manifestsPath, "" /*profileOrPath*/, false /*skipValidation */)
 	if err != nil {
 		return "", "", err
 	}
