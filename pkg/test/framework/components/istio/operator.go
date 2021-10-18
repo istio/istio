af74e59336bd4dbf2c46b2269bcb5d6938037108
Frank Budinsky <frankb@ca.ibm.com>
2021-10-18 16:30:28 -0400

    Configure istioctl for istiodremote topology

--- pkg/test/framework/components/istio/operator.go
+++ pkg/test/framework/components/istio/operator.go
@@ -36,6 +36,7 @@ import (
 
 	"istio.io/api/label"
 	opAPI "istio.io/api/operator/v1alpha1"
+	"istio.io/istio/istioctl/cmd"
 	pkgAPI "istio.io/istio/operator/pkg/apis/istio/v1alpha1"
 	"istio.io/istio/pkg/test/cert/ca"
 	testenv "istio.io/istio/pkg/test/env"
@@ -931,6 +932,13 @@ func configureRemoteClusterDiscovery(i *operatorComponent, cfg Config, c cluster
 	if err != nil {
 		return err
 	}
+
+	if !i.ctx.Clusters().IsMulticluster() {
+		os.Setenv("ISTIOCTL_PREFER_EXPERIMENTAL", "true")
+		os.Setenv("ISTIOCTL_XDS_ADDRESS", discoveryAddress.String())
+		cmd.ConfigAndEnvProcessing()
+	}
+
 	discoveryIP := discoveryAddress.IP.String()
 
 	scopes.Framework.Infof("creating endpoints and service in %s to get discovery from %s", c.Name(), discoveryIP)
