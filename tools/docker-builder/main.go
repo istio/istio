7c2977e6ada3d99b3ee7be683507a8661b55f30b
John Howard <howardjohn@google.com>
2021-10-18 13:31:22 -0700

    Core building

--- tools/docker-builder/main.go
+++ tools/docker-builder/main.go
@@ -1,16 +1,49 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"github.com/spf13/cobra"
 	"k8s.io/utils/env"
 
-	"istio.io/pkg/log"
+	"istio.io/istio/pilot/pkg/util/sets"
 	testenv "istio.io/istio/pkg/test/env"
+	"istio.io/pkg/log"
 )
+
+type Group struct {
+	Targets []string `json:"targets" hcl:"targets"`
+}
+
+type BakeFile struct {
+	Target map[string]Target `json:"target,omitempty"`
+	Group  map[string]Group `json:"group,omitempty"`
+}
+
+type Target struct {
+	Context          *string           `json:"context,omitempty" hcl:"context,optional"`
+	Dockerfile       *string           `json:"dockerfile,omitempty" hcl:"dockerfile,optional"`
+	DockerfileInline *string           `json:"dockerfile-inline,omitempty" hcl:"dockerfile-inline,optional"`
+	Args             map[string]string `json:"args,omitempty" hcl:"args,optional"`
+	Labels           map[string]string `json:"labels,omitempty" hcl:"labels,optional"`
+	Tags             []string          `json:"tags,omitempty" hcl:"tags,optional"`
+	CacheFrom        []string          `json:"cache-from,omitempty"  hcl:"cache-from,optional"`
+	CacheTo          []string          `json:"cache-to,omitempty"  hcl:"cache-to,optional"`
+	Target           *string           `json:"target,omitempty" hcl:"target,optional"`
+	Secrets          []string          `json:"secret,omitempty" hcl:"secret,optional"`
+	SSH              []string          `json:"ssh,omitempty" hcl:"ssh,optional"`
+	Platforms        []string          `json:"platforms,omitempty" hcl:"platforms,optional"`
+	Outputs          []string          `json:"output,omitempty" hcl:"output,optional"`
+	Pull             *bool             `json:"pull,omitempty" hcl:"pull,optional"`
+	NoCache          *bool             `json:"no-cache,omitempty" hcl:"no-cache,optional"`
+
+	// IMPORTANT: if you add more fields here, do not forget to update newOverrides and README.
+}
 
 // docker-builder
 // * --push
@@ -72,8 +105,48 @@ var rootCmd = &cobra.Command{
 	Short: "Builds Istio docker images",
 	Run: func(cmd *cobra.Command, _ []string) {
 		log.Infof("Args: %+v", args)
+		ConstructBakeFile(args)
 		RunMake(args, "docker.pilot2")
 	},
+}
+
+func sp(s string) *string {
+	return &s
+}
+
+func ConstructBakeFile(a Args) error {
+	targets := map[string]Target{}
+	for _, variant := range a.Variants {
+		for _, target := range a.Targets {
+			p := filepath.Join(testenv.IstioOut, "dockerx_build", fmt.Sprintf("docker.%s", target))
+			t := Target{
+				Context:    sp(p),
+				Dockerfile: sp(fmt.Sprintf("Dockerfile.%s", target)),
+				Args: map[string]string{
+					"BASE_VERSION":      "2021-10-06T19-01-15",
+					"BASE_DISTRIBUTION": "debug",
+					"proxy_version":     "istio-proxy:74393cf764c167b545f32eb895314e624186e5b6",
+					"istio_version":     "1.12-dev",
+					"VM_IMAGE_NAME":     "",
+					"VM_IMAGE_VERSION":  "",
+				},
+				Tags:      []string{fmt.Sprintf("%s/%s:%s-%s", a.Hub, target, a.Tag, variant)},
+				Platforms: []string{"linux/amd64"},
+				Outputs:   []string{"type=docker"}, // TODO push
+			}
+			targets[fmt.Sprintf("%s-%s", target, variant)] = t
+		}
+	}
+	bf := BakeFile{
+		Target: targets,
+		Group:  nil,
+	}
+	out := filepath.Join(testenv.IstioOut, "dockerx_build", "docker-bake.json")
+	j, err := json.MarshalIndent(bf, "", "  ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(out, j, 0o644)
 }
 
 // VerboseCommand runs a command, outputting stderr and stdout
@@ -87,13 +160,17 @@ func VerboseCommand(name string, arg ...string) *exec.Cmd {
 
 func StandardEnv(args Args) []string {
 	env := os.Environ()
+	if len(sets.NewSet(args.Targets...).Delete("proxyv2")) <= 1 {
+		// If we are building multiple, it is faster to build all binaries in a single invocation
+		// Otherwise, build just the single item. proxyv2 is special since it is always built separately with tag=agent.
+		// Ideally we would just always build the targets we need but our Makefile is not that smart
+		env = append(env, "BUILD_ALL=false")
+	}
 	env = append(env,
 		"BUILD_WITH_CONTAINER=0", // Build should already run in container, having multiple layers of docker causes issues
 	)
 	return env
 }
-
-
 
 // RunMake runs a make command for the repo, with standard environment variables set
 func RunMake(args Args, c ...string) error {
