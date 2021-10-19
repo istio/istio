1c82a227a4e258b0db9bb2922484dbd277fd79ac
Steven Landow <steven@stlcomputerservices.com>
2021-10-19 15:37:23 -0700

    use EndpointSlice v1 in Kubernetes 1.21+ (#35577)
    * use EndpointSlice v1 in Kubernetes 1.21+
    
    Change-Id: Ic7ba8e7aec18251fde1b67d781da53113ff82a4b
    
    * wrap v1beta1 in v1
    
    Change-Id: I273591abffa3c0b780d4f4289f362c7a0e7bd194
    
    * fix bad es list
    
    Change-Id: I0eb34b59839ad4f4257659fc8ed4468cdc3b027b
    
    * read locality from node
    
    Change-Id: Ic98c33df220a1b0b6f4b7f1eb693b1b42ecd6192
    
    * make tests use v1
    
    Change-Id: If68f4dc8d2176f0ba5d8ba79071c0fda35df9286
    
    * lint
    
    Change-Id: I34aa8c43809177ff33bbf3770fa9ad8fb6948921
    
    * mirror v1 to v1beta1 in tests
    
    Change-Id: I28b2cd7fb672d222df6715d40819ef44086b7650
    
    * refactor
    
    Change-Id: Ic68b370484ed211470d160f9fd782cf0c8978ddd

--- pilot/pkg/xds/fake.go
+++ pilot/pkg/xds/fake.go
@@ -60,6 +60,8 @@ import (
 type FakeOptions struct {
 	// If provided, sets the name of the "default" or local cluster to the similaed pilots. (Defaults to opts.DefaultClusterName)
 	DefaultClusterName cluster.ID
+	// If provided, the minor version will be overridden for calls to GetKubernetesVersion to 1.minor
+	KubernetesVersion string
 	// If provided, a service registry with the name of each map key will be created with the given objects.
 	KubernetesObjectsByCluster map[cluster.ID][]runtime.Object
 	// If provided, these objects will be used directly for the default cluster ("Kubernetes" or DefaultClusterName)
@@ -167,7 +169,7 @@ func NewFakeDiscoveryServer(t test.Failer, opts FakeOptions) *FakeDiscoveryServe
 		}
 	}
 	for k8sCluster, objs := range k8sObjects {
-		client := kubelib.NewFakeClient(objs...)
+		client := kubelib.NewFakeClientWithVersion(opts.KubernetesVersion, objs...)
 		if opts.KubeClientModifier != nil {
 			opts.KubeClientModifier(client)
 		}
