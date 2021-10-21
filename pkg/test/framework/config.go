8ee705aa6a7b3ddd8d65022b129f791074fa2d6e
Frank Budinsky <frankb@ca.ibm.com>
2021-10-21 15:44:51 -0400

    typo

--- pkg/test/framework/config.go
+++ pkg/test/framework/config.go
@@ -34,7 +34,7 @@ type configManager struct {
 
 func newConfigManager(ctx resource.Context, clusters cluster.Clusters) resource.ConfigManager {
 	if len(clusters) == 0 {
-		clusters = ctx.Clusters().Config()
+		clusters = ctx.Clusters().Configs()
 	}
 	return &configManager{
 		ctx:      ctx,
