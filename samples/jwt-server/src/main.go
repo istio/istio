9faf829c1c64d1702cb1911d590a0400eb348537
Aryan Gupta <garyan@google.com>
2021-10-25 18:21:43 +0000

    Refactored jwk https server and kubernetes deployment

--- samples/jwt-server/src/main.go
+++ samples/jwt-server/src/main.go
@@ -32,15 +32,18 @@ const (
 )
 
 var httpPort = flag.String("http", "8000", "HTTP server port")
+var httpsPort = flag.String("https", "8443", "HTTPS server port")
 
-var serverKey = flag.String("servcert", "", "Required, the name of server's private key")
-var serverCert = flag.String("servkey", "", "Required, the name of server's certificate file")
+var serverCert = flag.String("cert", "", "Optional, the name of server's certificate file")
+var serverkey = flag.String("key", "", "Optional, the name of server's private key")
 
 // JWTServer implements the sample server that serves jwt keys.
 type JWTServer struct {
 	httpServer *http.Server
 	// For test only
 	httpPort chan int
+	// For https test
+	httpsPort chan int
 }
 
 // ServeHTTP serves the JWT Keys.
@@ -80,19 +83,26 @@ func (s *JWTServer) startHTTPS(address string, wg *sync.WaitGroup) {
 		log.Fatalf("Failed to create HTTPS server: %v", err)
 	}
 	// Store the port for test only.
-	s.httpPort <- listener.Addr().(*net.TCPAddr).Port
+	s.httpsPort <- listener.Addr().(*net.TCPAddr).Port
 	s.httpServer = &http.Server{Handler: s}
 
 	log.Printf("Starting HTTPS server on port %s", address)
-	if err := s.httpServer.ServeTLS(listener, *serverKey, *serverCert); err != nil {
+	if err := s.httpServer.ServeTLS(listener, *serverCert, *serverkey); err != nil {
 		log.Fatalf("Failed to start HTTPS server: %v", err)
 	}
 }
 
-func (s *JWTServer) run(httpAddr string) {
+func (s *JWTServer) runHTTP(httpAddr string) {
 	var wg sync.WaitGroup
 	wg.Add(1)
-	go s.startHTTPS(httpAddr, &wg)
+	go s.startHTTP(httpAddr, &wg)
+	wg.Wait()
+}
+
+func (s *JWTServer) runHTTPS(httpsAddr string) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go s.startHTTPS(httpsAddr, &wg)
 	wg.Wait()
 }
 
@@ -102,14 +112,18 @@ func (s *JWTServer) stop() {
 
 func NewJwtServer() *JWTServer {
 	return &JWTServer{
-		httpPort: make(chan int, 1),
+		httpPort:  make(chan int, 1),
+		httpsPort: make(chan int, 1),
 	}
 }
 
 func main() {
 	flag.Parse()
 	s := NewJwtServer()
-	go s.run(fmt.Sprintf(":%s", *httpPort))
+	go s.runHTTP(fmt.Sprintf(":%s", *httpPort))
+	if *serverCert != "" && *serverkey != "" {
+		go s.runHTTPS(fmt.Sprintf(":%s", *httpsPort))
+	}
 	defer s.stop()
 
 	// Wait for the process to be shutdown.
