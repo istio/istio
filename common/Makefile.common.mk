d923ee03efbab152002d2092f979fe6c8a01bae6
Istio Automation <istio-testing-bot@google.com>
2021-10-28 19:49:44 -0700

    Automator: update common-files@release-1.12 in istio/istio@release-1.12 (#35790)

--- common/Makefile.common.mk
+++ common/Makefile.common.mk
@@ -97,7 +97,7 @@ mirror-licenses: mod-download-go
 	@license-lint --mirror
 
 TMP := $(shell mktemp -d -u)
-UPDATE_BRANCH ?= "release-1.12"
+UPDATE_BRANCH ?= "master"
 
 update-common:
 	@mkdir -p $(TMP)
