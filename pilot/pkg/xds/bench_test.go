ee3df6972d39cfd80a55aa0e997a56a2ac92dcda
Kenan O'Neal <kenan.oneal@volunteers.acasi.info>
2021-10-29 08:32:02 +0000

    Bump master to 1.13 (#35792)
    * Bump master to 1.13
    
    * Remove telemetryv2_1.10.yaml, run `make gen`
    
    * Add 1.13 to wellKnownVersions, default to 1.13 in proxy metadata
    
    Co-authored-by: Daniel Grimm <dgrimm@redhat.com>

--- pilot/pkg/xds/bench_test.go
+++ pilot/pkg/xds/bench_test.go
@@ -354,7 +354,7 @@ func setupTest(t testing.TB, config ConfigInput) (*FakeDiscoveryServer, *model.P
 				"istio.io/benchmark": "true",
 			},
 			ClusterID:    "Kubernetes",
-			IstioVersion: "1.12.0",
+			IstioVersion: "1.13.0",
 		},
 		ConfigNamespace:  "default",
 		VerifiedIdentity: &spiffe.Identity{Namespace: "default"},
