a4ce9465c6fa1d6b6900d61ac5c667a83945328d
Yangmin Zhu <ymzhu@google.com>
2021-11-04 14:26:18 -0700

    update JWT claim routing implementation (#35762)
    * add analyzer for JWT claim based routing
    
    * fix test
    
    * remove e2e test
    
    * address comments
    
    * update
    
    * update to @request.auth.claims
    
    * fix test

--- galley/pkg/config/analysis/msg/messages.gen.go
+++ galley/pkg/config/analysis/msg/messages.gen.go
@@ -188,6 +188,10 @@ var (
 	// NamespaceInjectionEnabledByDefault defines a diag.MessageType for message "NamespaceInjectionEnabledByDefault".
 	// Description: user namespace should be injectable if Istio is installed with enableNamespacesByDefault enabled and neither injection label is set.
 	NamespaceInjectionEnabledByDefault = diag.NewMessageType(diag.Info, "IST0148", "is enabled for Istio injection, as Istio is installed with enableNamespacesByDefault as true.")
+
+	// JwtClaimBasedRoutingWithoutRequestAuthN defines a diag.MessageType for message "JwtClaimBasedRoutingWithoutRequestAuthN".
+	// Description: Virtual service using JWT claim based routing without request authentication.
+	JwtClaimBasedRoutingWithoutRequestAuthN = diag.NewMessageType(diag.Error, "IST0149", "The virtual service uses the JWT claim based routing (key: %s) but found no request authentication for the gateway (%s) pod (%s). The request authentication must first be applied for the gateway pods to validate the JWT token and make the claims available for routing.")
 )
 
 // All returns a list of all known message types.
@@ -238,6 +242,7 @@ func All() []*diag.MessageType {
 		ImageAutoWithoutInjectionWarning,
 		ImageAutoWithoutInjectionError,
 		NamespaceInjectionEnabledByDefault,
+		JwtClaimBasedRoutingWithoutRequestAuthN,
 	}
 }
 
@@ -688,5 +693,16 @@ func NewNamespaceInjectionEnabledByDefault(r *resource.Instance) diag.Message {
 	return diag.NewMessage(
 		NamespaceInjectionEnabledByDefault,
 		r,
+	)
+}
+
+// NewJwtClaimBasedRoutingWithoutRequestAuthN returns a new diag.Message based on JwtClaimBasedRoutingWithoutRequestAuthN.
+func NewJwtClaimBasedRoutingWithoutRequestAuthN(r *resource.Instance, key string, gateway string, pod string) diag.Message {
+	return diag.NewMessage(
+		JwtClaimBasedRoutingWithoutRequestAuthN,
+		r,
+		key,
+		gateway,
+		pod,
 	)
 }
