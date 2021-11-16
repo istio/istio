d170b9f78af5ef6afecf048d7274dbd99fcc4d93
xuzhonghu <xuzhonghu@huawei.com>
2021-11-16 20:06:27 +0800

    reduce pilot-agent binary from 46M to 42M

--- pilot/pkg/serviceregistry/util/label/label.go
+++ pilot/pkg/serviceregistry/util/label/label.go
@@ -15,13 +15,19 @@
 package label
 
 import (
-	v1 "k8s.io/api/core/v1"
-
 	"istio.io/api/label"
+
 	"istio.io/istio/pilot/pkg/model"
 	"istio.io/istio/pkg/cluster"
 	"istio.io/istio/pkg/config/labels"
 	"istio.io/istio/pkg/network"
+)
+
+// copied from https://github.com/kubernetes/api/blob/master/core/v1/well_known_labels.go
+// It is to remove dependency on k8s.io/api/core/v1
+const (
+	LabelTopologyZone   = "topology.kubernetes.io/zone"
+	LabelTopologyRegion = "topology.kubernetes.io/region"
 )
 
 // AugmentLabels adds additional labels to the those provided.
@@ -34,10 +40,10 @@ func AugmentLabels(in labels.Instance, clusterID cluster.ID, locality string, ne
 
 	region, zone, subzone := model.SplitLocalityLabel(locality)
 	if len(region) > 0 {
-		out[v1.LabelTopologyRegion] = region
+		out[LabelTopologyRegion] = region
 	}
 	if len(zone) > 0 {
-		out[v1.LabelTopologyZone] = zone
+		out[LabelTopologyZone] = zone
 	}
 	if len(subzone) > 0 {
 		out[label.TopologySubzone.Name] = subzone
