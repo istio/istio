9e608c65aa86174d3e1574e62fba13040396a40b
Sam Naser <samnaser@google.com>
2021-11-22 11:02:29 -0800

    Update base image to 1.10-dev.15

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -22,7 +22,7 @@ SHELL := /bin/bash -o pipefail
 export VERSION ?= 1.10-dev
 
 # Base version of Istio image to use
-BASE_VERSION ?= 1.10-dev.13
+BASE_VERSION ?= 1.10-dev.15
 
 export GO111MODULE ?= on
 export GOPROXY ?= https://proxy.golang.org
