1d3501415daa505b9cb888b2ed3a3cd48fd0f191
John Howard <howardjohn@google.com>
2021-12-01 10:28:12 -0800

    Revert "fixed issue#36105 (#36255)" (#36324)
    This reverts commit 830d632145183db02083721b29ed723eafd4d6d2.

--- pkg/config/analysis/analyzers/util/config.go
+++ pkg/config/analysis/analyzers/util/config.go
@@ -15,8 +15,6 @@
 package util
 
 import (
-	"strings"
-
 	"istio.io/istio/pkg/config/constants"
 	"istio.io/istio/pkg/config/resource"
 )
@@ -59,15 +57,6 @@ func IsIncluded(slice []string, term string) bool {
 		if val == term {
 			return true
 		}
-	}
-	return false
-}
-
-// IsMatched check if the term can be matched in a slice of string
-func IsMatched(slice []string, term string) bool {
-	for _, val := range slice {
-		matched := strings.Contains(term, val)
-		return matched
 	}
 	return false
 }
