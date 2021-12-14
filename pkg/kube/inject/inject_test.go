efe19f7ad310c798ce9b43b40d6d51f4ee70bed2
Xiaopeng Han <hanxiaop8@outlook.com>
2021-12-15 01:26:47 +0800

    Add kind and namespace to kube-inject log (#36486)
    * add ns to injection log
    
    * add releasenote
    
    * revise based on comments

--- pkg/kube/inject/inject_test.go
+++ pkg/kube/inject/inject_test.go
@@ -53,6 +53,7 @@ func TestInjection(t *testing.T) {
 		mesh          func(m *meshapi.MeshConfig)
 		skipWebhook   bool
 		expectedError string
+		expectedLog   string
 		setup         func()
 		teardown      func()
 	}
@@ -304,6 +305,11 @@ func TestInjection(t *testing.T) {
 				features.RewriteTCPProbes = true
 			},
 		},
+		{
+			in:          "hello-host-network-with-ns.yaml",
+			want:        "hello-host-network-with-ns.yaml.injected",
+			expectedLog: "Skipping injection because Deployment \"sample/hello-host-network\" has host networking enabled",
+		},
 	}
 	// Keep track of tests we add options above
 	// We will search for all test files and skip these ones
@@ -387,7 +393,9 @@ func TestInjection(t *testing.T) {
 			// First we test kube-inject. This will run exactly what kube-inject does, and write output to the golden files
 			t.Run("kube-inject", func(t *testing.T) {
 				var got bytes.Buffer
+				logs := make([]string, 0)
 				warn := func(s string) {
+					logs = append(logs, s)
 					t.Log(s)
 				}
 				if err = IntoResourceFile(nil, sidecarTemplate.Templates, valuesConfig, "", mc, in, &got, warn); err != nil {
@@ -401,6 +409,18 @@ func TestInjection(t *testing.T) {
 				}
 				if c.expectedError != "" {
 					t.Fatalf("expected error but got none")
+				}
+				if c.expectedLog != "" {
+					hasExpectedLog := false
+					for _, log := range logs {
+						if strings.Contains(log, c.expectedLog) {
+							hasExpectedLog = true
+							break
+						}
+					}
+					if !hasExpectedLog {
+						t.Fatal("expected log but got none")
+					}
 				}
 
 				// The version string is a maintenance pain for this test. Strip the version string before comparing.
