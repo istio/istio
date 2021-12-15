40bb3e5f7078482a1198124a390d8c7f89d59a30
John Howard <howardjohn@google.com>
2021-12-15 03:35:23 -0800

    Bump addon versions (#36438)
    * Bump addon versions
    
    * Adopt to https://github.com/prometheus-community/helm-charts/commit/df8add6a684501ca655c5162890f7e15aa7afc72

--- tests/integration/telemetry/stats/prometheus/stats.go
+++ tests/integration/telemetry/stats/prometheus/stats.go
@@ -364,7 +364,7 @@ func BuildQueryCommon(labels map[string]string, ns string) (sourceQuery, destina
 	}
 	sourceQuery += "}"
 	destinationQuery += "}"
-	appQuery += `istio_echo_http_requests_total{kubernetes_namespace="` + ns + `"}`
+	appQuery += `istio_echo_http_requests_total{namespace="` + ns + `"}`
 	return
 }
 
