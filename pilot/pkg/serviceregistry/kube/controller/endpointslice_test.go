97a3d4de5ee67e2717dffc5979fbca3936545535
dwq <41563853+dddddai@users.noreply.github.com>
2021-12-21 06:02:25 +0800

    [release-1.11] Fix endpoint slice cache memory leak (#36557)
    * fix endpoint slice cache memory leak
    
    Signed-off-by: dddddai <dddwq@foxmail.com>
    
    * add unit test
    
    Signed-off-by: dddddai <dddwq@foxmail.com>
    
    * add release note
    
    Signed-off-by: dddddai <dddwq@foxmail.com>

--- pilot/pkg/serviceregistry/kube/controller/endpointslice_test.go
+++ pilot/pkg/serviceregistry/kube/controller/endpointslice_test.go
@@ -19,6 +19,8 @@ import (
 	"testing"
 
 	"istio.io/api/label"
+	"istio.io/istio/pilot/pkg/model"
+	"istio.io/istio/pkg/config/host"
 )
 
 func TestGetLocalityFromTopology(t *testing.T) {
@@ -60,4 +62,84 @@ func TestGetLocalityFromTopology(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestEndpointSliceCache(t *testing.T) {
+	cache := newEndpointSliceCache()
+	hostname := host.Name("foo")
+
+	// add a endpoint
+	ep1 := &model.IstioEndpoint{
+		Address:         "1.2.3.4",
+		ServicePortName: "http",
+	}
+	cache.Update(hostname, "slice1", []*model.IstioEndpoint{ep1})
+	if !testEndpointsEqual(cache.Get(hostname), []*model.IstioEndpoint{ep1}) {
+		t.Fatalf("unexpected endpoints")
+	}
+
+	// add a new endpoint
+	ep2 := &model.IstioEndpoint{
+		Address:         "2.3.4.5",
+		ServicePortName: "http",
+	}
+	cache.Update(hostname, "slice1", []*model.IstioEndpoint{ep1, ep2})
+	if !testEndpointsEqual(cache.Get(hostname), []*model.IstioEndpoint{ep1, ep2}) {
+		t.Fatalf("unexpected endpoints")
+	}
+
+	// change service port name
+	ep1 = &model.IstioEndpoint{
+		Address:         "1.2.3.4",
+		ServicePortName: "http2",
+	}
+	ep2 = &model.IstioEndpoint{
+		Address:         "2.3.4.5",
+		ServicePortName: "http2",
+	}
+	cache.Update(hostname, "slice1", []*model.IstioEndpoint{ep1, ep2})
+	if !testEndpointsEqual(cache.Get(hostname), []*model.IstioEndpoint{ep1, ep2}) {
+		t.Fatalf("unexpected endpoints")
+	}
+
+	// add a new slice
+	ep3 := &model.IstioEndpoint{
+		Address:         "3.4.5.6",
+		ServicePortName: "http2",
+	}
+	cache.Update(hostname, "slice2", []*model.IstioEndpoint{ep3})
+	if !testEndpointsEqual(cache.Get(hostname), []*model.IstioEndpoint{ep1, ep2, ep3}) {
+		t.Fatalf("unexpected endpoints")
+	}
+
+	// dedup when transitioning
+	cache.Update(hostname, "slice2", []*model.IstioEndpoint{ep2, ep3})
+	if !testEndpointsEqual(cache.Get(hostname), []*model.IstioEndpoint{ep1, ep2, ep3}) {
+		t.Fatalf("unexpected endpoints")
+	}
+
+	cache.Delete(hostname, "slice1")
+	if !testEndpointsEqual(cache.Get(hostname), []*model.IstioEndpoint{ep2, ep3}) {
+		t.Fatalf("unexpected endpoints")
+	}
+
+	cache.Delete(hostname, "slice2")
+	if cache.Get(hostname) != nil {
+		t.Fatalf("unexpected endpoints")
+	}
+}
+
+func testEndpointsEqual(a, b []*model.IstioEndpoint) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	m1 := make(map[endpointKey]int)
+	m2 := make(map[endpointKey]int)
+	for _, i := range a {
+		m1[endpointKey{i.Address, i.ServicePortName}]++
+	}
+	for _, i := range b {
+		m2[endpointKey{i.Address, i.ServicePortName}]++
+	}
+	return reflect.DeepEqual(m1, m2)
 }
