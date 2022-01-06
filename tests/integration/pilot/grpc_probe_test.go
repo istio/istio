5b608195a8a6ab8e258b3d38e074a30ebf8f079e
dddddai <dddwq@foxmail.com>
2022-01-06 15:45:23 +0800

    add an integration test

---
+++ tests/integration/pilot/grpc_probe_test.go
@@ -0,0 +1,101 @@
+//go:build integ
+// +build integ
+
+//  Copyright Istio Authors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package pilot
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"istio.io/istio/pkg/config/protocol"
+	"istio.io/istio/pkg/test/framework"
+	"istio.io/istio/pkg/test/framework/components/echo"
+	"istio.io/istio/pkg/test/framework/components/echo/echoboot"
+	"istio.io/istio/pkg/test/framework/components/namespace"
+)
+
+func TestGRPCProbe(t *testing.T) {
+	framework.NewTest(t).
+		Features("usability.observability.grpc-probe").
+		Run(func(t framework.TestContext) {
+			ns := namespace.NewOrFail(t, t, namespace.Config{Prefix: "grpc-probe", Inject: true})
+			// apply strict mtls
+			t.ConfigKube().ApplyYAMLOrFail(t, ns.Name(), fmt.Sprintf(`
+apiVersion: security.istio.io/v1beta1
+kind: PeerAuthentication
+metadata:
+  name: grpc-probe-mtls
+  namespace: %s
+spec:
+  mtls:
+    mode: STRICT`, ns.Name()))
+
+			for _, testCase := range []struct {
+				name     string
+				rewrite  bool
+				ready    bool
+				openPort bool
+			}{
+				{name: "norewrite-unready", rewrite: false, ready: false, openPort: true},
+				{name: "rewrite-unready", rewrite: true, ready: false, openPort: false},
+				{name: "rewrite-ready", rewrite: true, ready: true, openPort: true},
+			} {
+				t.NewSubTest(testCase.name).Run(func(t framework.TestContext) {
+					runGRPCProbeDeployment(t, ns, testCase.name, testCase.rewrite, testCase.ready, testCase.openPort)
+				})
+			}
+		})
+}
+
+func runGRPCProbeDeployment(ctx framework.TestContext, ns namespace.Instance, //nolint:interfacer
+	name string, rewrite bool, wantReady bool, openPort bool) {
+	ctx.Helper()
+
+	var grpcProbe echo.Instance
+	cfg := echo.Config{
+		Namespace:         ns,
+		Service:           name,
+		ReadinessGRPCPort: "1234",
+		Subsets: []echo.SubsetConfig{
+			{
+				Annotations: echo.NewAnnotations().SetBool(echo.SidecarRewriteAppHTTPProbers, rewrite),
+			},
+		},
+	}
+
+	if openPort {
+		cfg.Ports = []echo.Port{{
+			Name:         "readiness-grpc-port",
+			Protocol:     protocol.GRPC,
+			ServicePort:  1234,
+			InstancePort: 1234,
+		}}
+	}
+
+	// Negative test, we expect the grpc readiness check fails, so set a timeout duration.
+	if !wantReady {
+		cfg.ReadinessTimeout = time.Second * 15
+	}
+	_, err := echoboot.NewBuilder(ctx).
+		With(&grpcProbe, cfg).
+		Build()
+	gotReady := err == nil
+	if gotReady != wantReady {
+		ctx.Errorf("grpcProbe app %v, got error %v, want ready = %v", name, err, wantReady)
+	}
+}
