a9010cb19c292c25027ead013be7dd36a1c96b24
Mangaal <angommeeteimangaal@gmail.com>
2022-01-19 16:09:00 +0530

    ingressv1 no change for now

--- pilot/pkg/config/kube/ingressv1/conversion.go
+++ pilot/pkg/config/kube/ingressv1/conversion.go
@@ -81,26 +81,24 @@ func ConvertIngressV1alpha3(ingress knetworking.Ingress, mesh *meshconfig.MeshCo
 		if tls.SecretName == "" {
 			log.Infof("invalid ingress rule %s:%s for hosts %q, no secretName defined", ingress.Namespace, ingress.Name, tls.Hosts)
 			continue
-		} else {
-			gateway.Servers = append(gateway.Servers, &networking.Server{
-				Port: &networking.Port{
-					Number:   443,
-					Protocol: string(protocol.HTTPS),
-					Name:     fmt.Sprintf("https-443-ingress-%s-%s-%d", ingress.Name, ingress.Namespace, i),
-				},
-				Hosts: tls.Hosts,
-				Tls: &networking.ServerTLSSettings{
-					HttpsRedirect:  false,
-					Mode:           networking.ServerTLSSettings_SIMPLE,
-					CredentialName: tls.SecretName,
-				},
-			})
 		}
 		// TODO validation when multiple wildcard tls secrets are given
 		if len(tls.Hosts) == 0 {
 			tls.Hosts = []string{"*"}
 		}
-
+		gateway.Servers = append(gateway.Servers, &networking.Server{
+			Port: &networking.Port{
+				Number:   443,
+				Protocol: string(protocol.HTTPS),
+				Name:     fmt.Sprintf("https-443-ingress-%s-%s-%d", ingress.Name, ingress.Namespace, i),
+			},
+			Hosts: tls.Hosts,
+			Tls: &networking.ServerTLSSettings{
+				HttpsRedirect:  false,
+				Mode:           networking.ServerTLSSettings_SIMPLE,
+				CredentialName: tls.SecretName,
+			},
+		})
 	}
 
 	gateway.Servers = append(gateway.Servers, &networking.Server{
