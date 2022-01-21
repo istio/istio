0735a0a880bde286c4f2389ea646b5c8ca7746c2
Tianpeng Wang <tpwang@alauda.io>
2022-01-21 19:03:22 +0800

    fix tests
    Signed-off-by: Tianpeng Wang <tpwang@alauda.io>

--- operator/pkg/translate/translate_test.go
+++ operator/pkg/translate/translate_test.go
@@ -18,12 +18,10 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
 	"istio.io/api/operator/v1alpha1"
-	v1alpha12 "istio.io/api/operator/v1alpha1"
 	"istio.io/istio/operator/pkg/name"
 	"istio.io/istio/operator/pkg/util"
+	"istio.io/istio/pkg/test/util/assert"
 )
 
 func Test_skipReplicaCountWithAutoscaleEnabled(t *testing.T) {
@@ -99,7 +97,7 @@ components:
 		t.Run(tt.name, func(t *testing.T) {
 			var iop *v1alpha1.IstioOperatorSpec
 			if tt.values != "" {
-				iop = &v1alpha12.IstioOperatorSpec{}
+				iop = &v1alpha1.IstioOperatorSpec{}
 				if err := util.UnmarshalWithJSONPB(tt.values, iop, true); err != nil {
 					t.Fatal(err)
 				}
