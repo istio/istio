f14ea9f7ff4f36fb70a85a78307686f909218159
jacob-delgado <jacob.delgado@volunteers.acasi.info>
2022-02-01 15:19:02 -0700

    Run bin/update_deps.sh (#37099)
    * Run bin/update_deps.sh
    
    * istio/proxy cannot be merged; remove those dependencies

--- prow/release-commit.sh
+++ prow/release-commit.sh
@@ -32,7 +32,7 @@ DOCKER_HUB=${DOCKER_HUB:-gcr.io/istio-testing}
 GCS_BUCKET=${GCS_BUCKET:-istio-build/dev}
 
 # Use a pinned version in case breaking changes are needed
-BUILDER_SHA=5c13ba8b2b7c5507cd3bd6fa60856dc5c0381694
+BUILDER_SHA=48a3d9a351c40a7afd4da2767c305861f50f5f43
 
 # Reference to the next minor version of Istio
 # This will create a version like 1.4-alpha.sha
