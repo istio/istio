470d84acac06180c8ff1f29c0c1ab99923c505c9
John Howard <howardjohn@google.com>
2022-02-02 09:10:09 -0800

    Merge into main suite

--- tests/integration/security/egress_gateway_origination_test.go
+++ tests/integration/security/egress_gateway_origination_test.go
@@ -15,20 +15,30 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package sdstlsorigination
+package security
 
 import (
+	"fmt"
+	"os"
+	"path"
 	"strings"
 	"testing"
 
+	"istio.io/istio/pkg/test"
+	"istio.io/istio/pkg/test/echo/client"
 	"istio.io/istio/pkg/test/echo/common/response"
+	"istio.io/istio/pkg/test/echo/common/scheme"
+	"istio.io/istio/pkg/test/env"
 	"istio.io/istio/pkg/test/framework"
 	"istio.io/istio/pkg/test/framework/components/echo"
 	"istio.io/istio/pkg/test/framework/components/echo/echotest"
 	"istio.io/istio/pkg/test/framework/components/istio"
 	"istio.io/istio/pkg/test/framework/components/namespace"
+	"istio.io/istio/pkg/test/framework/resource"
+	"istio.io/istio/pkg/test/util/tmpl"
 	ingressutil "istio.io/istio/tests/integration/security/sds_ingress/util"
 	sdstlsutil "istio.io/istio/tests/integration/security/sds_tls_origination/util"
+	"istio.io/istio/tests/integration/security/util"
 )
 
 // TestSimpleTlsOrigination test SIMPLE TLS mode with TLS origination happening at Gateway proxy
@@ -44,7 +54,7 @@ func TestSimpleTlsOrigination(t *testing.T) {
 			)
 
 			credentialA := ingressutil.IngressCredential{
-				CaCert: sdstlsutil.MustReadCert(t, "root-cert.pem"),
+				CaCert: MustReadCert(t, "root-cert.pem"),
 			}
 			CredentialB := ingressutil.IngressCredential{
 				CaCert: sdstlsutil.FakeRoot,
@@ -55,13 +65,10 @@ func TestSimpleTlsOrigination(t *testing.T) {
 			// Add kubernetes secret to provision key/cert for gateway.
 			ingressutil.CreateIngressKubeSecret(t, fakeCredName, ingressutil.TLS, CredentialB, false)
 
-			apps := &sdstlsutil.EchoDeployments{}
-			sdstlsutil.SetupEcho(t, t, apps)
-
 			// Set up Host Namespace
-			host := sdstlsutil.ServerSvc + "." + apps.ServerNamespace.Name() + ".svc.cluster.local"
+			host := util.ExternalSvc + "." + apps.Namespace1.Name() + ".svc.cluster.local"
 
-			testCases := []sdstlsutil.TestCase{
+			testCases := []TLSTestCase{
 				// Use CA certificate stored as k8s secret with the same issuing CA as server's CA.
 				// This root certificate can validate the server cert presented by the echoboot server instance.
 				{
@@ -89,38 +96,16 @@ func TestSimpleTlsOrigination(t *testing.T) {
 				},
 			}
 
+			CreateGateway(t, t, apps.Namespace1, apps.Namespace1)
 			for _, tc := range testCases {
 				t.NewSubTest(tc.Name).Run(func(t framework.TestContext) {
+					CreateDestinationRule(t, apps.Namespace1, "SIMPLE", tc.CredentialToUse)
 					echotest.New(t, apps.All).
-						SetupForDestination(func(t framework.TestContext, dst echo.Instances) error {
-							bufDestinationRule := sdstlsutil.CreateDestinationRule(t, apps.ServerNamespace, "SIMPLE", tc.CredentialToUse)
-
-							// Get namespace for gateway pod.
-							istioCfg := istio.DefaultConfigOrFail(t, t)
-							systemNS := namespace.ClaimOrFail(t, t, istioCfg.SystemNamespace)
-
-							t.ConfigKube(t.Clusters().Default()).ApplyYAMLOrFail(t, systemNS.Name(), bufDestinationRule.String())
-							return nil
-						}).
-						From(
-							echotest.Not(func(instances echo.Instances) echo.Instances {
-								return instances.Match(echo.Service(sdstlsutil.ServerSvc))
-							}),
-							func(instances echo.Instances) echo.Instances {
-								return instances.Match(echo.InCluster(t.Clusters().Default()))
-							},
-						).
-						ConditionallyTo(echotest.ReachableDestinations).
-						To(
-							func(instances echo.Instances) echo.Instances {
-								return instances.Match(echo.Service(sdstlsutil.ServerSvc))
-							},
-							func(instances echo.Instances) echo.Instances {
-								return instances.Match(echo.InCluster(t.Clusters().Default()))
-							},
-						).
-						Run(func(t framework.TestContext, src echo.Instance, _ echo.Instances) {
-							callOpt := sdstlsutil.CallOpts(apps.Server, host, tc.Name, tc)
+						WithDefaultFilters().
+						From(echotest.Not(echotest.FilterMatch(echo.IsNaked()))).
+						To(echotest.FilterMatch(echo.Service(util.ExternalSvc))).
+						Run(func(t framework.TestContext, src echo.Instance, dst echo.Instances) {
+							callOpt := CallOpts(dst[0], host, tc)
 							src.CallWithRetryOrFail(t, callOpt, echo.DefaultCallRetryOptions()...)
 						})
 				})
@@ -146,42 +131,39 @@ func TestMutualTlsOrigination(t *testing.T) {
 			// Add kubernetes secret to provision key/cert for gateway.
 
 			ingressutil.CreateIngressKubeSecret(t, credNameGeneric, ingressutil.Mtls, ingressutil.IngressCredential{
-				Certificate: sdstlsutil.MustReadCert(t, "cert-chain.pem"),
-				PrivateKey:  sdstlsutil.MustReadCert(t, "key.pem"),
-				CaCert:      sdstlsutil.MustReadCert(t, "root-cert.pem"),
+				Certificate: MustReadCert(t, "cert-chain.pem"),
+				PrivateKey:  MustReadCert(t, "key.pem"),
+				CaCert:      MustReadCert(t, "root-cert.pem"),
 			}, false)
 
 			ingressutil.CreateIngressKubeSecret(t, credNameNotGeneric, ingressutil.Mtls, ingressutil.IngressCredential{
-				Certificate: sdstlsutil.MustReadCert(t, "cert-chain.pem"),
-				PrivateKey:  sdstlsutil.MustReadCert(t, "key.pem"),
-				CaCert:      sdstlsutil.MustReadCert(t, "root-cert.pem"),
+				Certificate: MustReadCert(t, "cert-chain.pem"),
+				PrivateKey:  MustReadCert(t, "key.pem"),
+				CaCert:      MustReadCert(t, "root-cert.pem"),
 			}, true)
 
 			// Configured with an invalid ClientCert
 			ingressutil.CreateIngressKubeSecret(t, fakeCredNameA, ingressutil.Mtls, ingressutil.IngressCredential{
 				Certificate: sdstlsutil.FakeCert,
-				PrivateKey:  sdstlsutil.MustReadCert(t, "key.pem"),
-				CaCert:      sdstlsutil.MustReadCert(t, "root-cert.pem"),
+				PrivateKey:  MustReadCert(t, "key.pem"),
+				CaCert:      MustReadCert(t, "root-cert.pem"),
 			}, false)
 
 			// Configured with an invalid ClientCert and PrivateKey
 			ingressutil.CreateIngressKubeSecret(t, fakeCredNameB, ingressutil.Mtls, ingressutil.IngressCredential{
 				Certificate: sdstlsutil.FakeCert,
 				PrivateKey:  sdstlsutil.FakeKey,
-				CaCert:      sdstlsutil.MustReadCert(t, "root-cert.pem"),
+				CaCert:      MustReadCert(t, "root-cert.pem"),
 			}, false)
 
 			ingressutil.CreateIngressKubeSecret(t, simpleCredName, ingressutil.TLS, ingressutil.IngressCredential{
-				CaCert: sdstlsutil.MustReadCert(t, "root-cert.pem"),
+				CaCert: MustReadCert(t, "root-cert.pem"),
 			}, false)
 
-			apps := &sdstlsutil.EchoDeployments{}
-			sdstlsutil.SetupEcho(t, t, apps)
-
 			// Set up Host Namespace
-			host := sdstlsutil.ServerSvc + "." + apps.ServerNamespace.Name() + ".svc.cluster.local"
+			host := util.ExternalSvc + "." + apps.Namespace1.Name() + ".svc.cluster.local"
 
-			testCases := []sdstlsutil.TestCase{
+			testCases := []TLSTestCase{
 				// Use CA certificate and client certs stored as k8s secret with the same issuing CA as server's CA.
 				// This root certificate can validate the server cert presented by the echoboot server instance and server CA can
 				// validate the client cert. Secret is of type generic.
@@ -226,41 +208,184 @@ func TestMutualTlsOrigination(t *testing.T) {
 				},
 			}
 
+			CreateGateway(t, t, apps.Namespace1, apps.Namespace1)
 			for _, tc := range testCases {
 				t.NewSubTest(tc.Name).Run(func(t framework.TestContext) {
+					CreateDestinationRule(t, apps.Namespace1, "MUTUAL", tc.CredentialToUse)
 					echotest.New(t, apps.All).
-						SetupForDestination(func(t framework.TestContext, dst echo.Instances) error {
-							bufDestinationRule := sdstlsutil.CreateDestinationRule(t, apps.ServerNamespace, "MUTUAL", tc.CredentialToUse)
-
-							// Get namespace for gateway pod.
-							istioCfg := istio.DefaultConfigOrFail(t, t)
-							systemNS := namespace.ClaimOrFail(t, t, istioCfg.SystemNamespace)
-
-							t.ConfigKube(t.Clusters().Default()).ApplyYAMLOrFail(t, systemNS.Name(), bufDestinationRule.String())
-							return nil
-						}).
-						From(
-							echotest.Not(func(instances echo.Instances) echo.Instances {
-								return instances.Match(echo.Service(sdstlsutil.ServerSvc))
-							}),
-							func(instances echo.Instances) echo.Instances {
-								return instances.Match(echo.InCluster(t.Clusters().Default()))
-							},
-						).
-						ConditionallyTo(echotest.ReachableDestinations).
-						To(
-							func(instances echo.Instances) echo.Instances {
-								return instances.Match(echo.Service(sdstlsutil.ServerSvc))
-							},
-							func(instances echo.Instances) echo.Instances {
-								return instances.Match(echo.InCluster(t.Clusters().Default()))
-							},
-						).
-						Run(func(t framework.TestContext, src echo.Instance, _ echo.Instances) {
-							callOpt := sdstlsutil.CallOpts(apps.Server, host, tc.Name, tc)
+						WithDefaultFilters().
+						From(echotest.Not(echotest.FilterMatch(echo.IsNaked()))).
+						To(echotest.FilterMatch(echo.Service(util.ExternalSvc))).
+						Run(func(t framework.TestContext, src echo.Instance, dst echo.Instances) {
+							callOpt := CallOpts(dst[0], host, tc)
 							src.CallWithRetryOrFail(t, callOpt, echo.DefaultCallRetryOptions()...)
 						})
 				})
 			}
 		})
+}
+
+const (
+	Gateway = `
+apiVersion: networking.istio.io/v1alpha3
+kind: Gateway
+metadata:
+  name: istio-egressgateway-sds
+spec:
+  selector:
+    istio: egressgateway
+  servers:
+    - port:
+        number: 443
+        name: https-sds
+        protocol: HTTPS
+      hosts:
+      - external.{{.ServerNamespace}}.svc.cluster.local
+      tls:
+        mode: ISTIO_MUTUAL
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: DestinationRule
+metadata:
+  name: egressgateway-for-server-sds
+spec:
+  host: istio-egressgateway.istio-system.svc.cluster.local
+  subsets:
+  - name: server
+    trafficPolicy:
+      portLevelSettings:
+      - port:
+          number: 443
+        tls:
+          mode: ISTIO_MUTUAL
+          sni: external.{{.ServerNamespace}}.svc.cluster.local
+`
+	VirtualService = `
+apiVersion: networking.istio.io/v1alpha3
+kind: VirtualService
+metadata:
+  name: route-via-egressgateway-sds
+spec:
+  hosts:
+    - external.{{.ServerNamespace}}.svc.cluster.local
+  gateways:
+    - istio-egressgateway-sds
+    - mesh
+  http:
+    - match:
+        - gateways:
+            - mesh # from sidecars, route to egress gateway service
+          port: 80
+      route:
+        - destination:
+            host: istio-egressgateway.istio-system.svc.cluster.local
+            subset: server
+            port:
+              number: 443
+          weight: 100
+    - match:
+        - gateways:
+            - istio-egressgateway-sds
+          port: 443
+      route:
+        - destination:
+            host: external.{{.ServerNamespace}}.svc.cluster.local
+            port:
+              number: 443
+          weight: 100
+      headers:
+        request:
+          add:
+            handled-by-egress-gateway: "true"
+`
+)
+
+// We want to test out TLS origination at Gateway, to do so traffic from client in client namespace is first
+// routed to egress-gateway service in istio-system namespace and then from egress-gateway to server in server namespace.
+// TLS origination at Gateway happens using DestinationRule with CredentialName reading k8s secret at the gateway proxy.
+func CreateGateway(t test.Failer, ctx resource.Context, clientNamespace namespace.Instance, serverNamespace namespace.Instance) {
+	gw := tmpl.EvaluateOrFail(t, Gateway, map[string]string{"ServerNamespace": serverNamespace.Name()})
+	ctx.ConfigIstio().ApplyYAMLOrFail(t, clientNamespace.Name(), gw)
+
+	vs := tmpl.EvaluateOrFail(t, VirtualService, map[string]string{"ServerNamespace": serverNamespace.Name()})
+	ctx.ConfigIstio().ApplyYAMLOrFail(t, clientNamespace.Name(), vs)
+}
+
+const (
+	// Destination Rule configs
+	DestinationRuleConfig = `
+apiVersion: networking.istio.io/v1alpha3
+kind: DestinationRule
+metadata:
+  name: originate-tls-for-server-sds-{{.CredentialName}}
+spec:
+  host: "external.{{.ServerNamespace}}.svc.cluster.local"
+  trafficPolicy:
+    portLevelSettings:
+      - port:
+          number: 443
+        tls:
+          mode: {{.Mode}}
+          credentialName: {{.CredentialName}}
+          sni: external.{{.ServerNamespace}}.svc.cluster.local
+`
+)
+
+// Create the DestinationRule for TLS origination at Gateway by reading secret in istio-system namespace.
+func CreateDestinationRule(t framework.TestContext, serverNamespace namespace.Instance,
+	destinationRuleMode string, credentialName string) {
+	dr := tmpl.EvaluateOrFail(t, DestinationRuleConfig, map[string]string{
+		"ServerNamespace": serverNamespace.Name(),
+		"Mode":            destinationRuleMode, "CredentialName": credentialName,
+	})
+
+	// Get namespace for gateway pod.
+	istioCfg := istio.DefaultConfigOrFail(t, t)
+	systemNS := namespace.ClaimOrFail(t, t, istioCfg.SystemNamespace)
+
+	t.ConfigKube(t.Clusters().Default()).ApplyYAMLOrFail(t, systemNS.Name(), dr)
+}
+
+type TLSTestCase struct {
+	Name            string
+	Response        string
+	CredentialToUse string
+	Gateway         bool // true if the request is expected to be routed through gateway
+}
+
+func CallOpts(dest echo.Instance, host string, tc TLSTestCase) echo.CallOptions {
+	return echo.CallOptions{
+		Target:   dest,
+		Count:    util.CallsPerCluster,
+		PortName: "http",
+		Scheme:   scheme.HTTP,
+		Headers: map[string][]string{
+			"Host": {host},
+		},
+		Validator: echo.And(echo.ValidatorFunc(
+			func(responses client.ParsedResponses, err error) error {
+				if err != nil {
+					return fmt.Errorf("request failed: %v", err)
+				}
+				for _, r := range responses {
+					if r.Code != tc.Response {
+						return fmt.Errorf("got code %s, expected %s", r.Code, tc.Response)
+					}
+				}
+				for _, r := range responses {
+					if _, f := r.RawResponse["Handled-By-Egress-Gateway"]; tc.Gateway && !f {
+						return fmt.Errorf("expected to be handled by gateway. response: %+v", r.RawResponse)
+					}
+				}
+				return nil
+			})),
+	}
+}
+
+func MustReadCert(t test.Failer, f string) string {
+	b, err := os.ReadFile(path.Join(env.IstioSrc, "tests/testdata/certs/dns", f))
+	if err != nil {
+		t.Fatalf("failed to read %v: %v", f, err)
+	}
+	return string(b)
 }
