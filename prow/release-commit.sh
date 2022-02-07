2f6cc5c2b92dce03f3dd158afe53a236344f381d
Eric Van Norman <ericvn@us.ibm.com>
2022-02-07 08:20:45 -0700

    Pick up latest release-builder

--- prow/release-commit.sh
+++ prow/release-commit.sh
@@ -32,7 +32,7 @@ DOCKER_HUB=${DOCKER_HUB:-gcr.io/istio-testing}
 GCS_BUCKET=${GCS_BUCKET:-istio-build/dev}
 
 # Use a pinned version in case breaking changes are needed
-BUILDER_SHA=1e0e0905de03fd56959c71e4b6ec9b4676ca33f3
+BUILDER_SHA=601aab5c9e10c618362a80a14c521fbc1537c0db
 
 # Reference to the next minor version of Istio
 # This will create a version like 1.4-alpha.sha
