177aa315ad01ecbec313f08ce596275b9f80e7fa
John Howard <howardjohn@google.com>
2022-02-10 09:04:03 -0800

    Implement `runtimeValues` in proxy config (#37236)
    * Implement `runtimeValues` from proxy config
    
    * Rerun golden
    
    * add note
    
    * rename

--- pkg/bootstrap/option/instances.go
+++ pkg/bootstrap/option/instances.go
@@ -84,6 +84,10 @@ func NodeMetadata(meta *model.BootstrapNodeMetadata, rawMeta map[string]interfac
 	return newOptionOrSkipIfZero("meta_json_str", meta).withConvert(nodeMetadataConverter(meta, rawMeta))
 }
 
+func RuntimeFlags(flags map[string]string) Instance {
+	return newOptionOrSkipIfZero("runtime_flags", flags).withConvert(jsonConverter(flags))
+}
+
 func DiscoveryAddress(value string) Instance {
 	return newOption("discovery_address", value)
 }
