025c212dc63e30c0ac8e6525b37a707532fb92ff
Zhonghu Xu <xuzhonghu@huawei.com>
2022-02-16 02:40:09 +0800

    Validate certificate before sending back to proxy (#37040)
    * Validate certificate before sending back to proxy
    
    * update
    
    * update
    
    * lint
    
    * Added VERIFY_SDS_CERTIFICATE env to control whether to verify certificate provided by users
    
    * use x509.ParseCertificate
    
    * turn on for debug
    
    * remove base6d decode
    
    * Update test

--- pilot/pkg/features/pilot.go
+++ pilot/pkg/features/pilot.go
@@ -600,6 +600,9 @@ var (
 				"`clientKey`, `clientCertificate`, `tokenFile`, and `exec`.").Get()
 		return sets.NewSet(strings.Split(v, ",")...)
 	}()
+
+	VerifySDSCertificate = env.RegisterBoolVar("VERIFY_SDS_CERTIFICATE", true,
+		"If enabled, certificates fetched from SDS server will be verified before sending back to proxy.").Get()
 )
 
 // EnableEndpointSliceController returns the value of the feature flag and whether it was actually specified.
