c76347f379c49a3069c10e0ae0dec28bae4c2aa6
Jacob Delgado <jacob.delgado@volunteers.acasi.info>
2022-02-16 18:36:24 -0700

    Image version should point to release-1.13 image

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -22,7 +22,7 @@ SHELL := /bin/bash -o pipefail
 export VERSION ?= 1.13-dev
 
 # Base version of Istio image to use
-BASE_VERSION ?= master-2022-02-09T22-12-56
+BASE_VERSION ?= release-1.13-2022-02-11T18-19-22
 
 export GO111MODULE ?= on
 export GOPROXY ?= https://proxy.golang.org
