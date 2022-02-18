11a4d16c22a204e3dc99e6202d152be121e327a5
Pengyuan Bian <bianpengyuan@google.com>
2022-02-18 22:21:41 +0000

    update to redirect.

--- pkg/test/fakes/imageregistry/main.go
+++ pkg/test/fakes/imageregistry/main.go
@@ -15,35 +15,29 @@
 package main
 
 import (
-	"crypto/tls"
 	"encoding/base64"
 	"flag"
 	"fmt"
 	"net/http"
-	"strings"
 
 	"istio.io/pkg/log"
-
-	"github.com/google/go-containerregistry/pkg/authn"
-	"github.com/google/go-containerregistry/pkg/name"
-	containerregistry "github.com/google/go-containerregistry/pkg/registry"
-	"github.com/google/go-containerregistry/pkg/v1/remote"
 )
 
 var port = flag.Int("port", 1338, "port to run registry on")
-var images = flag.String("images", "", "comma separated list of images that will be preloaded and served")
-var registry = flag.String("registry", "localhost:1338", "name of registry to push and pull image")
+var registry = flag.String("registry", "", "name of registry to redirect registry request to")
 
 const (
 	User   = "user"
 	Passwd = "passwd"
 )
 
-type Handler struct {
-	registry http.Handler
-}
+type Handler struct{}
 
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/ready" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
 	encoded := base64.StdEncoding.EncodeToString([]byte("user:passwd"))
 	authHdr := r.Header.Get("Authorization")
 	wantHdr := fmt.Sprintf("Basic %s", encoded)
@@ -54,60 +48,18 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
 		return
 	}
-	h.registry.ServeHTTP(w, r)
+	rurl := fmt.Sprintf("https://%v%v", registry, r.URL.Path)
+	log.Infof("Get %v, send redirect to %v", r.URL, rurl)
+	http.Redirect(w, r, rurl, http.StatusMovedPermanently)
 }
 
 func main() {
 	flag.Parse()
 	s := &http.Server{
-		Addr: fmt.Sprintf(":%d", *port),
-		Handler: &Handler{
-			registry: containerregistry.New(),
-		},
+		Addr:    fmt.Sprintf(":%d", *port),
+		Handler: &Handler{},
 	}
-	done := make(chan bool, 1)
-	go func() {
-		if err := s.ListenAndServe(); err != nil {
-			log.Error(err)
-		}
-		done <- true
-	}()
-	parts := strings.Split(*images, ",")
-	for _, originImg := range parts {
-		if originImg == "" {
-			continue
-		}
-		log.Infof("Pulling image %v", originImg)
-		// Get the new image name from the original image.
-		originImageParts := strings.Split(originImg, "/")
-		originImageParts[0] = *registry
-		newImage := strings.Join(originImageParts, "/")
-
-		// Pull the origin image.
-		ref, err := name.ParseReference(originImg)
-		if err != nil {
-			log.Errorf("Could not parse url in image reference: %v", err)
-			continue
-		}
-		t := remote.DefaultTransport.Clone()
-		t.TLSClientConfig = &tls.Config{
-			InsecureSkipVerify: true, //nolint: gosec
-		}
-		img, err := remote.Image(ref, remote.WithTransport(t))
-		if err != nil {
-			log.Errorf("Could not fetch image %v", err)
-			continue
-		}
-
-		log.Infof("Pusing image %v", newImage)
-		// Push the image with new tag.
-		tag, err := name.ParseReference(newImage)
-		options := []remote.Option{
-			remote.WithAuth(&authn.Basic{Username: User, Password: Passwd}),
-		}
-		if err := remote.Write(tag, img, options...); err != nil {
-			log.Errorf("Failed to push image %v: %v", tag, err)
-		}
+	if err := s.ListenAndServe(); err != nil {
+		log.Error(err)
 	}
-	<-done
 }
