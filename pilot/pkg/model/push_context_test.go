8d579c26b0ec642bd62642890073b678c9d04e6b
Rama Chavali <rama.rao@salesforce.com>
2022-02-19 11:37:52 +0530

    retain DestinationRule func name in push context (#37430)
    * retain DestinationRule func name in push context
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * move to private
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/model/push_context_test.go
+++ pilot/pkg/model/push_context_test.go
@@ -1343,7 +1343,7 @@ func TestSetDestinationRuleInheritance(t *testing.T) {
 	ps.SetDestinationRules([]config.Config{meshDestinationRule, nsDestinationRule, svcDestinationRule, destinationRuleNamespace2})
 
 	for _, tt := range testCases {
-		mergedConfig := ps.destinationRuleForSidecarScope(&Proxy{ConfigNamespace: tt.proxyNs},
+		mergedConfig := ps.destinationRule(&Proxy{ConfigNamespace: tt.proxyNs},
 			&Service{
 				Hostname: host.Name(tt.serviceHostname),
 				Attributes: ServiceAttributes{
@@ -1622,7 +1622,7 @@ func TestSetDestinationRuleWithExportTo(t *testing.T) {
 	}
 	for _, tt := range cases {
 		t.Run(fmt.Sprintf("%s-%s", tt.proxyNs, tt.serviceNs), func(t *testing.T) {
-			destRuleConfig := ps.destinationRuleForSidecarScope(&Proxy{ConfigNamespace: tt.proxyNs},
+			destRuleConfig := ps.destinationRule(&Proxy{ConfigNamespace: tt.proxyNs},
 				&Service{
 					Hostname: host.Name(tt.host),
 					Attributes: ServiceAttributes{
