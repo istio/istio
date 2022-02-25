740afb01913e6381359f09f0193200d7678f94f2
Yaroslav Skopets <yaroslav@tetrate.io>
2022-02-25 16:43:38 +0100

    istio-iptables: support constraints on groups outgoing traffic from which should be redirected to Envoy (#37057)
    * istio-iptables: support constraints on users/groups traffic from which should be redirected to Envoy
    
    Signed-off-by: Yaroslav Skopets <yaroslav@tetrate.io>
    
    * fix lint issues
    
    Signed-off-by: Yaroslav Skopets <yaroslav@tetrate.io>
    
    * code review: remove filtering by UID
    
    Signed-off-by: Yaroslav Skopets <yaroslav@tetrate.io>
    
    * code review: use only environment variables
    
    Signed-off-by: Yaroslav Skopets <yaroslav@tetrate.io>
    
    * code review: put a limit on number of owner groups
    
    Signed-off-by: Yaroslav Skopets <yaroslav@tetrate.io>
    
    * fix lint issues
    
    Signed-off-by: Yaroslav Skopets <yaroslav@tetrate.io>
    
    * add unit tests for ISTIO_OUTBOUND_OWNER_GROUPS and ISTIO_OUTBOUND_OWNER_GROUPS_EXCLUDE
    
    Signed-off-by: Yaroslav Skopets <yaroslav@tetrate.io>
    
    * use `assert` package
    
    Signed-off-by: Yaroslav Skopets <yaroslav@tetrate.io>
    
    * move `maintest` package
    
    Signed-off-by: Yaroslav Skopets <yaroslav@tetrate.io>
    
    * add release notes
    
    Signed-off-by: Yaroslav Skopets <yaroslav@tetrate.io>

---
+++ tools/istio-iptables/pkg/config/validation.go
@@ -0,0 +1,43 @@
+// Copyright Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import (
+	"fmt"
+)
+
+const (
+	// Due to implementation constraints, we have to impose a limit on the
+	// number of owner groups whose outgoing traffic should be redirected
+	// to Envoy.
+	//
+	// Since all included groups will be translated into a single Iptables
+	// rule that combines N match expressions `-m owner ! --gid-owner <GID>`,
+	// we need to be sure it won't be too long.
+	//
+	// Most common Linux distributions allow no more than 128-1200
+	// match expressions per rule.
+	maxOwnerGroupsInclude = 64
+)
+
+func ValidateOwnerGroups(include, exclude string) error {
+	filter := ParseInterceptFilter(include, exclude)
+	if !filter.Except && len(filter.Values) > maxOwnerGroupsInclude {
+		return fmt.Errorf("number of owner groups whose outgoing traffic "+
+			"should be redirected to Envoy cannot exceed %d, got %d: %v",
+			maxOwnerGroupsInclude, len(filter.Values), filter.Values)
+	}
+	return nil
+}
