ba1b98fbc0b4870f45b09736335d176a09b8921c
Zhonghu Xu <xuzhonghu@huawei.com>
2022-02-26 00:38:34 +0800

    Revert (#37539)

--- pilot/pkg/serviceregistry/serviceentry/servicediscovery_test.go
+++ pilot/pkg/serviceregistry/serviceentry/servicediscovery_test.go
@@ -1036,17 +1036,6 @@ func expectProxyInstances(t testing.TB, sd *ServiceEntryStore, expected []*model
 		instances := sd.GetProxyServiceInstances(&model.Proxy{IPAddresses: []string{ip}, Metadata: &model.NodeMetadata{}})
 		sortServiceInstances(instances)
 		sortServiceInstances(expected)
-		sd.mutex.RLock()
-		allServices := sd.services.getAllServices()
-		sd.mutex.RUnlock()
-		for _, inst := range expected {
-			for _, asvc := range allServices {
-				if inst.Service.Hostname == asvc.Hostname {
-					inst.Service.AutoAllocatedAddress = asvc.AutoAllocatedAddress
-					break
-				}
-			}
-		}
 		if err := compare(t, instances, expected); err != nil {
 			return err
 		}
@@ -1107,17 +1096,6 @@ func expectServiceInstances(t testing.TB, sd *ServiceEntryStore, cfg *config.Con
 			instances := sd.InstancesByPort(svc, port, nil)
 			sortServiceInstances(instances)
 			sortServiceInstances(expected[i])
-			sd.mutex.RLock()
-			allServices := sd.services.getAllServices()
-			sd.mutex.RUnlock()
-			for _, inst := range expected[i] {
-				for _, asvc := range allServices {
-					if inst.Service.Hostname == asvc.Hostname {
-						inst.Service.AutoAllocatedAddress = asvc.AutoAllocatedAddress
-						break
-					}
-				}
-			}
 			if err := compare(t, instances, expected[i]); err != nil {
 				return fmt.Errorf("%d: %v", i, err)
 			}
