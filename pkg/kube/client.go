fb7c6b46fe1ffe6c95c5c4adf466ba8f80be9bb1
Istio Automation <istio-testing-bot@google.com>
2022-02-25 16:42:51 -0800

    Add api extensions v1 to IstioSchemas (#37389)
    Change-Id: I448be35602110c43281dde8f2e87a6263825c46f
    
    Co-authored-by: therealmitchconnors <mitchconnors@gmail.com>

--- pkg/kube/client.go
+++ pkg/kube/client.go
@@ -32,6 +32,7 @@ import (
 	"go.uber.org/atomic"
 	"google.golang.org/grpc/credentials"
 	v1 "k8s.io/api/core/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	kubeExtClient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	extfake "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/fake"
 	"k8s.io/apimachinery/pkg/api/meta"
@@ -1076,6 +1077,7 @@ func istioScheme() *runtime.Scheme {
 	utilruntime.Must(clientextensions.AddToScheme(scheme))
 	utilruntime.Must(gatewayapi.AddToScheme(scheme))
 	utilruntime.Must(apis.AddToScheme(scheme))
+	utilruntime.Must(apiextensionsv1.AddToScheme(scheme))
 	return scheme
 }
 
