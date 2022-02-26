ad1a173c505035e0d932650d17fc8007d6505478
John Howard <howardjohn@google.com>
2022-02-25 17:03:45 -0800

    tests no parallel

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -470,7 +470,7 @@ BENCH_TARGETS ?= ./pilot/...
 
 .PHONY: racetest
 racetest: $(JUNIT_REPORT)
-	go test ${GOBUILDFLAGS} ${T} -race ./... 2>&1 | tee >($(JUNIT_REPORT) > $(JUNIT_OUT))
+	go test ${GOBUILDFLAGS} ${T} -p 1 -race ./... 2>&1 | tee >($(JUNIT_REPORT) > $(JUNIT_OUT))
 
 .PHONY: benchtest
 benchtest: $(JUNIT_REPORT) ## Runs all benchmarks
