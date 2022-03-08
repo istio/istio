6f544de89d68009f1b1ba3b74cf87d5a50d9db46
TrevorTaoARM <trevor.tao@arm.com>
2022-03-08 15:01:30 +0800

    Don't add credHelper if there's no GCP credentials
    As we know, gcloud auth configure-docker adds the Docker credHelper
    entry to Docker's configuration file, or creates the file
    if it doesn't exist.
    But if there's no GCP credential file(defined by env
    GOOGLE_APPLICATION_CREDENTIALS), which is often met in internal CI,
    it would lead to a failure when doing
    the normal docker operation:
    "
    ERROR: error getting credentials - err: exit status 1, out:
     `You do not currently have an active account selected.
    See https://cloud.google.com/sdk/docs/authorizing for more information
    "
    
    So just ignore the credHelper adding operation if there's no
    GCP credentials.
    
    Signed-off-by: TrevorTaoARM <trevor.tao@arm.com>

--- prow/lib.sh
+++ prow/lib.sh
@@ -41,12 +41,14 @@ function trace() {
 }
 
 function setup_gcloud_credentials() {
-  if [[ $(command -v gcloud) ]]; then
-    gcloud auth configure-docker -q
-  elif [[ $(command -v docker-credential-gcr) ]]; then
-    docker-credential-gcr configure-docker
-  else
-    echo "No credential helpers found, push to docker may not function properly"
+  if [[ -n "${GOOGLE_APPLICATION_CREDENTIALS:-}" ]]; then
+    if [[ $(command -v gcloud) ]]; then
+      gcloud auth configure-docker -q
+    elif [[ $(command -v docker-credential-gcr) ]]; then
+      docker-credential-gcr configure-docker
+    else
+      echo "No credential helpers found, push to docker may not function properly"
+    fi
   fi
 }
 
