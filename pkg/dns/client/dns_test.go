5d5cf32ef87c0ecc16f6e1a59e17234d65007ea3
Rama Chavali <rama.rao@salesforce.com>
2022-03-09 21:11:58 +0530

    add test
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pkg/dns/client/dns_test.go
+++ pkg/dns/client/dns_test.go
@@ -185,6 +185,11 @@ func TestDNS(t *testing.T) {
 			expected: a("foo.foons.svc.mesh.company.net.", []net.IP{net.ParseIP("10.1.2.3").To4()}),
 		},
 		{
+			name:     "success: wild card with search domain returns A record correctly",
+			host:     "mj-splunk.svc.mesh.company.net.ns1.svc.cluster.local.",
+			expected: a("foo.svc.mesh.company.net.", []net.IP{net.ParseIP("10.1.2.3").To4()}),
+		},
+		{
 			name:      "success: TypeAAAA query returns AAAA records only",
 			host:      "dual.localhost.",
 			queryAAAA: true,
