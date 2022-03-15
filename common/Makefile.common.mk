fcbd46609eac6e96cf38a03f52d313a9f915b866
Istio Automation <istio-testing-bot@google.com>
2022-03-15 12:15:41 -0700

    Automator: update common-files@master in istio/istio@master (#37940)

--- common/Makefile.common.mk
+++ common/Makefile.common.mk
@@ -69,13 +69,13 @@ lint-licenses:
 lint-all: lint-dockerfiles lint-scripts lint-yaml lint-helm lint-copyright-banner lint-go lint-python lint-markdown lint-sass lint-typescript lint-protos lint-licenses
 
 tidy-go:
-	@go mod tidy
+	@find -name go.mod -execdir go mod tidy \;
 
 mod-download-go:
-	@-GOFLAGS="-mod=readonly" go mod download
+	@-GOFLAGS="-mod=readonly" find -name go.mod -execdir go mod download \;
 # go mod tidy is needed with Golang 1.16+ as go mod download affects go.sum
 # https://github.com/golang/go/issues/43994
-	@go mod tidy
+	@find -name go.mod -execdir go mod tidy \;
 
 format-go: tidy-go
 	@${FINDFILES} -name '*.go' \( ! \( -name '*.gen.go' -o -name '*.pb.go' \) \) -print0 | ${XARGS} common/scripts/format_go.sh
