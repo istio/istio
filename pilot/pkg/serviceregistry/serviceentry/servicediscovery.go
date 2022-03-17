170013afbda1221c8a129e927183b8586718f36d
Rama Chavali <rama.rao@salesforce.com>
2022-03-17 23:27:52 +0530

    clarify some comments in service discovery (#37980)
    * clarify some comments in service discovery
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * more
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * comments
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/serviceregistry/serviceentry/servicediscovery.go
+++ pilot/pkg/serviceregistry/serviceentry/servicediscovery.go
@@ -347,15 +347,17 @@ func (s *ServiceEntryStore) serviceEntryHandler(_, curr config.Config, event mod
 		s.XdsUpdater.SvcUpdate(shard, string(svc.Hostname), svc.Attributes.Namespace, model.EventUpdate)
 		configsUpdated[makeConfigKey(svc)] = struct{}{}
 	}
-
-	// If service entry is deleted, cleanup endpoint shards for services.
+	// If service entry is deleted, call SvcUpdate to cleanup endpoint shards for services.
 	for _, svc := range deletedSvcs {
 		s.XdsUpdater.SvcUpdate(shard, string(svc.Hostname), svc.Attributes.Namespace, model.EventDelete)
 		configsUpdated[makeConfigKey(svc)] = struct{}{}
 	}
 
+	// If a service is updated and is not part of updatedSvcs, that means its endpoints might have changed.
+	// If this service entry had endpoints with IPs (i.e. resolution STATIC), then we do EDS update.
+	// If the service entry had endpoints with FQDNs (i.e. resolution DNS), then we need to do
+	// full push (as fqdn endpoints go via strict_dns clusters in cds).
 	if len(unchangedSvcs) > 0 {
-		// Trigger full push for DNS resolution ServiceEntry in case endpoint changes.
 		if currentServiceEntry.Resolution == networking.ServiceEntry_DNS || currentServiceEntry.Resolution == networking.ServiceEntry_DNS_ROUND_ROBIN {
 			for _, svc := range unchangedSvcs {
 				configsUpdated[makeConfigKey(svc)] = struct{}{}
@@ -363,7 +365,7 @@ func (s *ServiceEntryStore) serviceEntryHandler(_, curr config.Config, event mod
 		}
 	}
 
-	serviceInstancesByConfig, serviceInstances := s.buildServiceInstancesForSE(curr, cs)
+	serviceInstancesByConfig, serviceInstances := s.buildServiceInstances(curr, cs)
 	oldInstances := s.serviceInstances.getServiceEntryInstances(key)
 	for configKey, old := range oldInstances {
 		s.serviceInstances.deleteInstances(configKey, old)
@@ -859,7 +861,7 @@ func parseHealthAnnotation(s string) bool {
 	return p
 }
 
-func (s *ServiceEntryStore) buildServiceInstancesForSE(
+func (s *ServiceEntryStore) buildServiceInstances(
 	curr config.Config,
 	services []*model.Service,
 ) (map[configKey][]*model.ServiceInstance, []*model.ServiceInstance) {
