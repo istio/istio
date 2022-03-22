63e1611699146d78b0715f7e09ae67a66242af2f
Lawrence Gadban <lawrence.gadban@solo.io>
2022-03-22 14:43:39 -0500

    add custom tag validation for Telemetry

--- pkg/config/validation/validation_test.go
+++ pkg/config/validation/validation_test.go
@@ -6875,7 +6875,7 @@ func TestValidateTelemetry(t *testing.T) {
 	tests := []struct {
 		name    string
 		in      proto.Message
-		out     string
+		err     string
 		warning string
 	}{
 		{"empty", &telemetry.Telemetry{}, "", ""},
@@ -6907,6 +6907,34 @@ func TestValidateTelemetry(t *testing.T) {
 				}},
 			},
 			"randomSamplingPercentage", "",
+		},
+		{
+			"tracing with a good custom tag",
+			&telemetry.Telemetry{
+				Tracing: []*telemetry.Tracing{{
+					CustomTags: map[string]*telemetry.Tracing_CustomTag{
+						"clusterID": {
+							Type: &telemetry.Tracing_CustomTag_Environment{
+								Environment: &telemetry.Tracing_Environment{
+									Name: "FOO",
+								},
+							},
+						},
+					},
+				}},
+			},
+			"", "",
+		},
+		{
+			"tracing with a nil custom tag",
+			&telemetry.Telemetry{
+				Tracing: []*telemetry.Tracing{{
+					CustomTags: map[string]*telemetry.Tracing_CustomTag{
+						"clusterID": nil,
+					},
+				}},
+			},
+			"tag 'clusterID' may not have a nil value", "",
 		},
 		{
 			"bad metrics operation",
@@ -6980,7 +7008,7 @@ func TestValidateProxyConfig(t *testing.T) {
 				},
 				Spec: tt.in,
 			})
-			checkValidationMessage(t, warn, err, tt.warning, tt.out)
+			checkValidationMessage(t, warn, err, tt.warning, tt.err)
 		})
 	}
 }
