d54b41da5158681846b741f4e65d43d321ebf7b9
John Howard <howardjohn@google.com>
2022-03-22 15:02:28 -0700

    eds: correct (and test) always pushing (#38072)

--- pilot/pkg/xds/eds.go
+++ pilot/pkg/xds/eds.go
@@ -200,7 +200,7 @@ func (s *DiscoveryServer) edsCacheUpdate(shard model.ShardKey, hostname string, 
 		// Next, check for endpoints that were in old but no longer exist. If there are any, there is a
 		// removal so we need to push an update.
 		for _, oie := range oldIstioEndpoints {
-			if _, f := nmap[oie.Address]; f {
+			if _, f := nmap[oie.Address]; !f {
 				needPush = true
 			}
 		}
