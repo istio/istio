ece74924d45ba65007ce2826d5b40f87df243366
Zhengzhe Yang <zhengzhey@google.com>
2022-03-28 17:18:56 +0000

    add enable install env var to be able to skip installation entirely
    Change-Id: I9e303287bbdb82c22b40590fef9092e77b7d9df4

--- cni/pkg/install/install_test.go
+++ cni/pkg/install/install_test.go
@@ -36,6 +36,7 @@ func TestCheckInstall(t *testing.T) {
 		cniConfigFilename string
 		cniConfName       string
 		chainedCNIPlugin  bool
+		skipInstall       bool
 		existingConfFiles map[string]string // {srcFilename: targetFilename, ...}
 	}{
 		{
@@ -63,6 +64,12 @@ func TestCheckInstall(t *testing.T) {
 		{
 			name:              "CNI config file removed",
 			expectedFailure:   true,
+			cniConfigFilename: "file-removed.conflist",
+		},
+		{
+			name:              "CNI config file non-existent but install skipped",
+			expectedFailure:   false,
+			skipInstall:       true,
 			cniConfigFilename: "file-removed.conflist",
 		},
 		{
@@ -115,6 +122,7 @@ func TestCheckInstall(t *testing.T) {
 				MountedCNINetDir: tempDir,
 				CNIConfName:      c.cniConfName,
 				ChainedCNIPlugin: c.chainedCNIPlugin,
+				CNIEnableInstall: !c.skipInstall,
 			}
 			err = checkInstall(cfg, filepath.Join(tempDir, c.cniConfigFilename))
 			if (c.expectedFailure && err == nil) || (!c.expectedFailure && err != nil) {
@@ -165,6 +173,7 @@ func TestSleepCheckInstall(t *testing.T) {
 			cfg := &config.InstallConfig{
 				MountedCNINetDir: tempDir,
 				ChainedCNIPlugin: c.chainedCNIPlugin,
+				CNIEnableInstall: true,
 			}
 			cniConfigFilepath := filepath.Join(tempDir, c.cniConfigFilename)
 			isReady := &atomic.Value{}
