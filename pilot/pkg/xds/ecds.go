dad43a496fc57bfaf81de372d993f7ef3e37096b
Pengyuan Bian <bianpengyuan@google.com>
2022-03-30 22:12:49 +0000

    fix

--- pilot/pkg/xds/ecds.go
+++ pilot/pkg/xds/ecds.go
@@ -154,7 +154,7 @@ func referencedSecrets(proxy *model.Proxy, push *model.PushContext, resourceName
 	// * Secret are referenced by a WasmPlugin which applies to this proxy.
 	// TODO: we get the WasmPlugins here to get the secrets reference in order to decide whether ECDS push is needed,
 	//       and we will get it again at extension config build. Avoid getting it twice if this becomes a problem.
-	watched := sets.NewWith(resourceNames...)
+	watched := sets.New(resourceNames...)
 	wasmPlugins := push.WasmPlugins(proxy)
 	referencedSecrets := sets.Set{}
 	for _, wps := range wasmPlugins {
