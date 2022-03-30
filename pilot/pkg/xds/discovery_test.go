597fa4fc80488cb68d8eab7f699a77d2e92ebd66
Rama Chavali <rama.rao@salesforce.com>
2022-03-30 20:26:57 +0530

    refactor xds code (#38141)
    * refactor xds code
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * move delta back
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * revert authenticate
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * revert delta
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/xds/discovery_test.go
+++ pilot/pkg/xds/discovery_test.go
@@ -36,7 +36,7 @@ func createProxies(n int) []*Connection {
 	proxies := make([]*Connection, 0, n)
 	for p := 0; p < n; p++ {
 		proxies = append(proxies, &Connection{
-			ConID:       fmt.Sprintf("proxy-%v", p),
+			conID:       fmt.Sprintf("proxy-%v", p),
 			pushChannel: make(chan *Event),
 			stream:      &fakeStream{},
 		})
@@ -80,7 +80,7 @@ func TestSendPushesManyPushes(t *testing.T) {
 				case p := <-proxy.pushChannel:
 					p.done()
 					pushesMu.Lock()
-					pushes[proxy.ConID]++
+					pushes[proxy.conID]++
 					pushesMu.Unlock()
 				case <-stopCh:
 					return
@@ -133,7 +133,7 @@ func TestSendPushesSinglePush(t *testing.T) {
 				case p := <-proxy.pushChannel:
 					p.done()
 					pushesMu.Lock()
-					pushes[proxy.ConID]++
+					pushes[proxy.conID]++
 					pushesMu.Unlock()
 					wg.Done()
 				case <-stopCh:
--- pilot/pkg/xds/xdsgen.go
+++ pilot/pkg/xds/xdsgen.go
@@ -117,7 +117,7 @@ func (s *DiscoveryServer) pushXds(con *Connection, w *model.WatchedResource, req
 	if err != nil || res == nil {
 		// If we have nothing to send, report that we got an ACK for this version.
 		if s.StatusReporter != nil {
-			s.StatusReporter.RegisterEvent(con.ConID, w.TypeUrl, req.Push.LedgerVersion)
+			s.StatusReporter.RegisterEvent(con.conID, w.TypeUrl, req.Push.LedgerVersion)
 		}
 		return err
 	}
