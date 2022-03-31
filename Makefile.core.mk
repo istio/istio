97422856cdf3905d08dc800e88d35b2c894f1fb6
John Howard <howardjohn@google.com>
2022-03-31 14:56:36 -0700

    vm tests: update and deprcated OS's tested (#38182)
    * vm tests: update and deprcated OS's tested
    
    * fix
    
    * fix docker.xyz
    
    * cleanup

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -22,7 +22,7 @@ SHELL := /bin/bash -o pipefail
 export VERSION ?= 1.14-dev
 
 # Base version of Istio image to use
-BASE_VERSION ?= master-2022-03-16T19-01-24
+BASE_VERSION ?= master-2022-03-30T19-01-25
 
 export GO111MODULE ?= on
 export GOPROXY ?= https://proxy.golang.org
