73fa3c15f6cd4f85f8b837f397e26cffa9428b6a
John Howard <howardjohn@google.com>
2022-04-01 10:09:52 -0700

    Skip CentOS 8 tests for VMs (#38225)
    See https://github.com/istio/istio/issues/38224

--- prow/lib.sh
+++ prow/lib.sh
@@ -119,7 +119,9 @@ function build_images() {
   nonDistrolessTargets="docker.app docker.app_sidecar_ubuntu_jammy "
   if [[ "${JOB_TYPE:-presubmit}" == "postsubmit" ]]; then
     # We run tests across all VM types only in postsubmit
-    nonDistrolessTargets+="docker.app_sidecar_ubuntu_xenial docker.app_sidecar_debian_11  docker.app_sidecar_centos_7 docker.app_sidecar_rockylinux_8 "
+    nonDistrolessTargets+="docker.app_sidecar_ubuntu_xenial docker.app_sidecar_debian_11  docker.app_sidecar_centos_7 "
+    # TODO(https://github.com/istio/istio/issues/38224)
+#    nonDistrolessTargets+="docker.app_sidecar_rockylinux_8 "
   fi
   if [[ "${SELECT_TEST}" == "test.integration.operator.kube" || "${SELECT_TEST}" == "test.integration.kube" || "${JOB_TYPE:-postsubmit}" == "postsubmit" ]]; then
     targets+="docker.operator "
