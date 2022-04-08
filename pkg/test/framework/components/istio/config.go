6833ea30629e7978c0234edb309ec399c840991d
Frank Budinsky <frankb@ca.ibm.com>
2022-04-08 12:11:38 -0400

    test framework istiodless false by default

--- pkg/test/framework/components/istio/config.go
+++ pkg/test/framework/components/istio/config.go
@@ -85,7 +85,7 @@ var (
 		BaseIOPFile:             BaseIOP,
 		DeployEastWestGW:        true,
 		DumpKubernetesManifests: false,
-		IstiodlessRemotes:       true,
+		IstiodlessRemotes:       false,
 		EnableCNI:               false,
 	}
 )
