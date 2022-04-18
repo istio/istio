b892f4ddc97ca9ef4b53b240213b134cc65c4ce6
Rama Chavali <rama.rao@salesforce.com>
2022-04-18 18:50:13 +0530

    add description for gateway config env var (#38423)
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/features/pilot.go
+++ pilot/pkg/features/pilot.go
@@ -81,7 +81,8 @@ var (
 	// FilterGatewayClusterConfig controls if a subset of clusters(only those required) should be pushed to gateways
 	// TODO enable by default once https://github.com/istio/istio/issues/28315 is resolved
 	// Currently this may cause a bug when we go from N clusters -> 0 clusters -> N clusters
-	FilterGatewayClusterConfig = env.RegisterBoolVar("PILOT_FILTER_GATEWAY_CLUSTER_CONFIG", false, "").Get()
+	FilterGatewayClusterConfig = env.RegisterBoolVar("PILOT_FILTER_GATEWAY_CLUSTER_CONFIG", false,
+		"If enabled, Pilot will send only clusters that referenced in gateway virtual services attached to gateway").Get()
 
 	DebounceAfter = env.RegisterDurationVar(
 		"PILOT_DEBOUNCE_AFTER",
