e929ac356ce143816709c21745a09203aa296ca8
vince-0202 <w.vince.0202@gmail.com>
2022-04-21 10:52:23 +0800

    Uniform pilot-agent and pilot-discover 's code style

--- pilot/cmd/pilot-agent/app/cmd_test.go
+++ pilot/cmd/pilot-agent/app/cmd_test.go
@@ -11,14 +11,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package main
+package app
 
 import (
 	"testing"
 
 	"github.com/onsi/gomega"
-
 	"istio.io/istio/pilot/pkg/model"
 )
 
--- pilot/cmd/pilot-agent/app/wait.go
+++ pilot/cmd/pilot-agent/app/wait.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
+package app
 
 import (
 	"fmt"
@@ -81,6 +81,4 @@ func init() {
 	waitCmd.PersistentFlags().IntVar(&requestTimeoutMillis, "requestTimeoutMillis", 500, "number of milliseconds to wait for response")
 	waitCmd.PersistentFlags().IntVar(&periodMillis, "periodMillis", 500, "number of milliseconds to wait between attempts")
 	waitCmd.PersistentFlags().StringVar(&url, "url", "http://localhost:15021/healthz/ready", "URL to use in requests")
-
-	rootCmd.AddCommand(waitCmd)
 }
