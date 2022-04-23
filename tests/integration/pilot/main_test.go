102afac222c0f29638684cb72a60b31722249493
Nathan Mittler <nmittler@gmail.com>
2022-04-23 09:02:21 -0700

    [tf] New authz server component (#38536)
    Moving the authz server creation to a new test component that can be easily reused.
    
    This is supporting the work on https://github.com/istio/istio/pull/37914.

--- tests/integration/pilot/main_test.go
+++ tests/integration/pilot/main_test.go
@@ -54,7 +54,7 @@ func TestMain(m *testing.M) {
 	framework.
 		NewSuite(m).
 		Setup(istio.Setup(&i, nil)).
-		Setup(deployment.SetupSingleNamespace(&apps)).
+		Setup(deployment.SetupSingleNamespace(&apps, deployment.Config{})).
 		Setup(func(t resource.Context) error {
 			gatewayConformanceInputs.Client = t.Clusters().Default()
 			gatewayConformanceInputs.Cleanup = !t.Settings().NoCleanup
