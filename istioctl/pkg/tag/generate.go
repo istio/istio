119ece011c518471c2f7831e1c6c8221ef6da128
Istio Automation <istio-testing-bot@google.com>
2022-04-25 20:47:04 -0700

    [release-1.13] istiod-default-validator not respect custom istioNamespace (#38586)
    * istiod-default-validator not respect custom istioNamespace when running istioctl instal
    
    * update ut
    
    * remove duplicate whSvcNamespace
    
    Co-authored-by: hejianpeng <hejianpeng2@huawei.com>

--- istioctl/pkg/tag/generate.go
+++ istioctl/pkg/tag/generate.go
@@ -159,10 +159,12 @@ func generateValidatingWebhook(config *tagWebhookConfig, chartPath string) (stri
 	}
 
 	values := fmt.Sprintf(`
+global:
+  istioNamespace: %s
 revision: %q
 base:
   validationURL: %s
-`, config.Revision, config.URL)
+`, config.IstioNamespace, config.Revision, config.URL)
 
 	validatingWebhookYAML, err := r.RenderManifestFiltered(values, func(tmplName string) bool {
 		return strings.Contains(tmplName, vwhTemplateName)
