a9e6beb76843c4e193f23d82fc480ae40b808efe
Ingwon Song <102102227+ingwonsong@users.noreply.github.com>
2022-05-05 09:25:00 -0700

    Giving tag modification capability to the faked container registry component (#38717)
    * Modify the tag to another tag or sha to deal with more complex test scenarios
    
    * Revert unintended commenting out, and bump up the version

--- pkg/test/fakes/imageregistry/main.go
+++ pkg/test/fakes/imageregistry/main.go
@@ -19,6 +19,7 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"regexp"
 
 	"istio.io/pkg/log"
 )
@@ -33,13 +34,75 @@ const (
 	Passwd = "passwd"
 )
 
-type Handler struct{}
+type Handler struct {
+	// Mapping table for support conversion of tag.
+	// The key is combination of the image name and tag with `:` separator,
+	// and the value is the target tag or sha.
+	// For example,
+	//  1) Convert from a tag to a digest
+	//     istio-testing/awesomedocker:latest -> sha256:abcedf0123456789
+	//  2) Convert from a tag to another tag
+	//     istio-testing/awesomedocker:v1.0.0 -> v1.0.1
+	tagMap map[string]string
+}
+
+var re = regexp.MustCompile(`(?P<Prefix>/v\d+)?/(?P<ImageName>.+)/manifests/(?P<Tag>[^:]*)$`)
+
+// Convert tag based on the tag map.
+// If the given path does not have tagged form or the image name and tag is not the registered one,
+// just return the path without modification.
+func (h *Handler) convertTag(path string) string {
+	matches := re.FindStringSubmatch(path)
+	if matches == nil {
+		return path
+	}
+
+	prefix := matches[re.SubexpIndex("Prefix")]
+	imageName := matches[re.SubexpIndex("ImageName")]
+	tag := matches[re.SubexpIndex("Tag")]
+	key := imageName + ":" + tag
+
+	log.Infof("key: %v", key)
+
+	if converted, found := h.tagMap[imageName+":"+tag]; found {
+		return prefix + "/" + imageName + "/manifests/" + converted
+	}
+	return path
+}
 
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/ready" {
 		w.WriteHeader(http.StatusOK)
 		return
 	}
+
+	// convert the requested tag to the other tag or sha of the real registry
+	if r.URL.Path == "/admin/v1/tagmap" {
+		switch r.Method {
+		case http.MethodPost:
+			err := r.ParseForm()
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			h.tagMap = make(map[string]string)
+			for k, v := range r.PostForm {
+				if len(v) > 0 {
+					h.tagMap[k] = v[0]
+				}
+			}
+			w.WriteHeader(http.StatusOK)
+		case http.MethodGet:
+			w.WriteHeader(http.StatusOK)
+			for k, v := range h.tagMap {
+				fmt.Fprintf(w, "%s -> %s", k, v)
+			}
+		default:
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		return
+	}
+
 	encoded := base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%v:%v", User, Passwd)))
 	authHdr := r.Header.Get("Authorization")
 	wantHdr := fmt.Sprintf("Basic %s", encoded)
@@ -50,7 +113,7 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
 		return
 	}
-	rurl := fmt.Sprintf("https://%v%v", *registry, r.URL.Path)
+	rurl := fmt.Sprintf("https://%v%v", *registry, h.convertTag(r.URL.Path))
 	log.Infof("Get %q, send redirect to %q", r.URL, rurl)
 	http.Redirect(w, r, rurl, http.StatusMovedPermanently)
 }
@@ -58,9 +121,12 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	flag.Parse()
 	s := &http.Server{
-		Addr:    fmt.Sprintf(":%d", *port),
-		Handler: &Handler{},
+		Addr: fmt.Sprintf(":%d", *port),
+		Handler: &Handler{
+			tagMap: make(map[string]string),
+		},
 	}
+	log.Infof("Fake containerregistry server is starting at %d", *port)
 	if err := s.ListenAndServe(); err != nil {
 		log.Error(err)
 	}
