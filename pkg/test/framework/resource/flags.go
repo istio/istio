f610d21f4efb9e5e27b1b2221ad4c347c5799c94
Menghan Li <menghanl@google.com>
2022-05-05 17:46:57 -0700

    tests: update deployment.yaml for gRPC C++ PSM tests (#38763)
    * update deployment.yaml for C++ tests
    
    C++'s flag parsing library only supports `--a=b`.
    
    * new env
    
    * readiness probe based on image
    
    * s/TEST_/
    
    * fix test golden file
    
    * more golden files
    
    * quotes

--- pkg/test/framework/resource/flags.go
+++ pkg/test/framework/resource/flags.go
@@ -71,6 +71,10 @@ func SettingsFromCommandLine(testID string) (*Settings, error) {
 		s.Image.PullPolicy = env.PULL_POLICY.ValueOrDefault("Always")
 	}
 
+	if s.EchoImage == "" {
+		s.EchoImage = env.ECHO_IMAGE.ValueOrDefault("")
+	}
+
 	if err = validate(s); err != nil {
 		return nil, err
 	}
