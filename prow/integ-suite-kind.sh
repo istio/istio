8d2fc69fea282168fe646089482993d56b163dc3
Steven Landow <landow@google.com>
2022-05-12 10:53:05 -0700

    prow: update default image to 1.24.0
    Change-Id: I5b8732367af80a948f857a71d14102ca0549436b

--- prow/integ-suite-kind.sh
+++ prow/integ-suite-kind.sh
@@ -37,10 +37,11 @@ setup_and_export_git_sha
 source "${ROOT}/common/scripts/kind_provisioner.sh"
 
 TOPOLOGY=SINGLE_CLUSTER
-NODE_IMAGE="gcr.io/istio-testing/kind-node:v1.23.0"
+NODE_IMAGE="gcr.io/istio-testing/kind-node:v1.24.0-0.13.0"
 KIND_CONFIG=""
 CLUSTER_TOPOLOGY_CONFIG_FILE="${ROOT}/prow/config/topology/multicluster.json"
-
+go install sigs.k8s.io/kind@v0.13.0
+kind version
 export FAST_VM_BUILDS=true
 export ISTIO_DOCKER_BUILDER=crane
 
