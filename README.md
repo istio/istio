e9bc849d17ebe8b66a0342218800205ea1426d65
Yuval Kohavi <yuval.kohavi@gmail.com>
2022-05-12 16:07:15 -0400

    test using latest kind (#72)
    * test using latest kind
    
    * more notes to readme
    
    * update readme for legacy

--- README.md
+++ README.md
@@ -5,10 +5,10 @@
 ```shell
 ./local-test-utils/kind-registry.sh
 
-export ISTIO_ENVOY_BASE_URL ?= https://storage.googleapis.com/solo-istio-build/proxy
+export ISTIO_ENVOY_BASE_URL=https://storage.googleapis.com/solo-istio-build/proxy
 HUB=localhost:5000 # local registry in docker (helps avoid permissions issues from private registry)
-TAG=ambient-oss
-tools/docker --targets=pilot,proxyv2 --hub=$HUB --tag=$TAG --push
+TAG=ambient-oss # note, if you change the tag here, also change it in the daemonset yaml below.
+tools/docker --targets=pilot,proxyv2 --hub=$HUB --tag=$TAG --push --builder=crane
 ```
 
 ## Cluster setup (kind)
@@ -19,8 +19,21 @@ tools/docker --targets=pilot,proxyv2 --hub=$HUB --tag=$TAG --push
 # Configure cluster to use the local registry
 ./local-test-utils/kind-registry.sh
 
-# Move images from your remote registry to the local one (if needed)
+# Move images from your remote registry to the local one (if needed) - not needed if building and pushing images to localhost.
 ./local-test-utils/refresh-istio-images.sh
+```
+
+Check iptables version
+
+```shell
+docker exec -ti ambient-worker iptables-nft-save
+```
+
+If you see and empty list and message saying: "Warning: iptables-legacy tables present, use iptables-legacy-save to see them" run the following command:
+
+
+```shell
+export IPTABLES=iptables-legacy
 ```
 
 ## Install
@@ -61,4 +74,26 @@ k exec -it $(k get po -lapp=sleep -ojsonpath='{.items[0].metadata.name}') -- sh
 
 # (From the client pod) Send traffic
 curl helloworld:5000/hello
+```
+
+# Debugging
+
+Turning on debug logs
+
+```shell
+WORKER1=$(kubectl -n istio-system get pods --field-selector spec.nodeName==ambient-worker -lapp=uproxy -o custom-columns=:.metadata.name --no-headers)
+WORKER2=$(kubectl -n istio-system get pods --field-selector spec.nodeName==ambient-worker2 -lapp=uproxy -o custom-columns=:.metadata.name --no-headers)
+
+kubectl -n istio-system port-forward $WORKER1 15000:15000&
+kubectl -n istio-system port-forward $WORKER2 15001:15000&
+
+curl -XPOST "localhost:15000/logging?level=debug"
+curl -XPOST "localhost:15001/logging?level=debug"
+
+kubectl -n istio-system logs -lapp=uproxy -f
+# Or,
+kubectl -n istio-system logs $WORKER1 -f
+kubectl -n istio-system logs $WORKER2 -f
+
+curl "localhost:15000/config_dump"
 ```
--- redirect.sh
+++ redirect.sh
@@ -42,20 +42,25 @@ INCOMING_INTERFACE=eth0
 # a route table number number we can use to send traffic to envoy (should be unused).
 ROUTE_TABLE=100
 
+# if iptables var not set, use iptables-nft
+if [ -z "${IPTABLES}" ]; then
+  IPTABLES=iptables-nft
+fi
+
 if [[ "${2:-}" == clean ]]; then
   # clean up previous chains
   for node in $(kind get nodes --name "${1:-kind}" | grep worker); do
     docker exec -i "$node" sh -x <<EOF
-    iptables-nft -t nat -F uproxy-PREROUTING
-    iptables-nft -t mangle -F uproxy-PREROUTING
-    iptables-nft -t mangle -F uproxy-POSTROUTING
+    $IPTABLES -t nat -F uproxy-PREROUTING
+    $IPTABLES -t mangle -F uproxy-PREROUTING
+    $IPTABLES -t mangle -F uproxy-POSTROUTING
     # we flush because sometimes -X doesn't work.
-    iptables-nft -t mangle -D POSTROUTING -j uproxy-POSTROUTING
-    iptables-nft -t mangle -X uproxy-POSTROUTING
-    iptables-nft -t mangle -D PREROUTING -j uproxy-PREROUTING
-    iptables-nft -t mangle -X uproxy-PREROUTING
-    iptables-nft -t nat -D PREROUTING -j uproxy-PREROUTING
-    iptables-nft -t nat -X uproxy-PREROUTING
+    $IPTABLES -t mangle -D POSTROUTING -j uproxy-POSTROUTING
+    $IPTABLES -t mangle -X uproxy-POSTROUTING
+    $IPTABLES -t mangle -D PREROUTING -j uproxy-PREROUTING
+    $IPTABLES -t mangle -X uproxy-PREROUTING
+    $IPTABLES -t nat -D PREROUTING -j uproxy-PREROUTING
+    $IPTABLES -t nat -X uproxy-PREROUTING
     ip route del local 0.0.0.0/0 dev lo table $ROUTE_TABLE
     ipset destroy uproxy-pods-ips
 EOF
@@ -78,17 +83,17 @@ done
 for node in $(kind get nodes --name "${1:-kind}" | grep worker); do
 
 docker exec -i "$node" sh -x <<EOF
-if iptables-nft -t nat -C PREROUTING -j uproxy-PREROUTING; then
-  iptables-nft -t nat -F uproxy-PREROUTING
-  iptables-nft -t mangle -F uproxy-PREROUTING
-  iptables-nft -t mangle -F uproxy-POSTROUTING
+if $IPTABLES -t nat -C PREROUTING -j uproxy-PREROUTING; then
+  $IPTABLES -t nat -F uproxy-PREROUTING
+  $IPTABLES -t mangle -F uproxy-PREROUTING
+  $IPTABLES -t mangle -F uproxy-POSTROUTING
 else 
-  iptables-nft -t nat -N uproxy-PREROUTING
-  iptables-nft -t nat -I PREROUTING -j uproxy-PREROUTING
-  iptables-nft -t mangle -N uproxy-PREROUTING
-  iptables-nft -t mangle -I PREROUTING -j uproxy-PREROUTING
-  iptables-nft -t mangle -N uproxy-POSTROUTING
-  iptables-nft -t mangle -I POSTROUTING -j uproxy-POSTROUTING
+  $IPTABLES -t nat -N uproxy-PREROUTING
+  $IPTABLES -t nat -I PREROUTING -j uproxy-PREROUTING
+  $IPTABLES -t mangle -N uproxy-PREROUTING
+  $IPTABLES -t mangle -I PREROUTING -j uproxy-PREROUTING
+  $IPTABLES -t mangle -N uproxy-POSTROUTING
+  $IPTABLES -t mangle -I POSTROUTING -j uproxy-POSTROUTING
 fi 
 EOF
 done
@@ -116,39 +121,45 @@ ip route add local 0.0.0.0/0 dev lo table $ROUTE_TABLE
 
 # Route packets with tproxy mark locally so they end up with envoy.
 # TODO: double check if this is needed, we maybe be able to just use tproxy rule.
+ip rule del fwmark $MARK lookup $ROUTE_TABLE # potentially delete previous rule
 ip rule add fwmark $MARK lookup $ROUTE_TABLE
 
 # In the NAT table, accept stuff with our tproxy mark. This is done to we skip k8s NAT rules.
 # Obviously needs to be inserted before k8s rules.
-iptables-nft -t nat -I uproxy-PREROUTING 1 -m mark --mark $MARK -j ACCEPT
+$IPTABLES -t nat -I uproxy-PREROUTING 1 -m mark --mark $MARK -j ACCEPT
 
-## Original SRC setup (optional if original src is not desired).
 
-# OUTMARK is set by envoy when doing original src. if we see packets with that mark, save it to the connection mark,
-# so we can mark returning packets, as we need to divert them back to envoy.
-# (we can also do this on the output chain).
-iptables-nft -t mangle -A uproxy-POSTROUTING -o "${INTERFACE_PREFIX}+" -m mark --mark $OUTMARK -j CONNMARK --save-mark --nfmask $OUTMARK_MASK --ctmask $OUTMARK_MASK
-
-# Alternativly, mark outgoing connections from envoy. we do need to exclude XDS if we do this.
-# this has the advantage that no mark needs to be configured in envoy, and pilot and uproxy do not need to agree on a mark.
-# TODO: make sure NEW doesn't include syn,ack.
-# iptables-nft -t mangle -A uproxy-POSTROUTING -o "${INTERFACE_PREFIX}+" -m owner --uid-owner $ENVOY_UID -m conntrack --ctstate NEW -j CONNMARK --set-mark $OUTMARK --nfmask $OUTMARK_MASK --ctmask $OUTMARK_MASK
 
 # move hack from init container to here, so all iptables are in one place. and use the same iptables (nft vs legacy)
 # TODO(@yuval-k): @stevenctl should this be in the init container?
 # we can't use tproxy here because it's on the output chain and tproxy only works in prerouting.
 # we don't want to use REDIRECT, as it doesn't work with all CNIs. 
 # so hopefully we will fix this soon by either using SNI, or even better, making this hop internal to envoy.
-iptables-nft -t nat -I OUTPUT 1 -p tcp -o "${INTERFACE_PREFIX}+" --dport 15088 -j REDIRECT --to-port $POD_INBOUND
+$IPTABLES -t nat -I OUTPUT 1 -p tcp -o "${INTERFACE_PREFIX}+" --dport 15088 -j REDIRECT --to-port $POD_INBOUND
+
+
+## Original SRC setup (optional if original src is not desired).
+
+# OUTMARK is set by envoy when doing original src. if we see packets with that mark, save it to the connection mark,
+# so we can mark returning packets, as we need to divert them back to envoy.
+# (we can also do this on the output chain).
+$IPTABLES -t mangle -A uproxy-POSTROUTING -m mark --mark $OUTMARK -j CONNMARK --save-mark --nfmask $OUTMARK_MASK --ctmask $OUTMARK_MASK
+
+# Alternativly, mark outgoing connections from envoy. we do need to exclude XDS if we do this.
+# this has the advantage that no mark needs to be configured in envoy, and pilot and uproxy do not need to agree on a mark.
+# TODO: make sure NEW doesn't include syn,ack.
+# $IPTABLES -t mangle -A uproxy-POSTROUTING -m owner --uid-owner $ENVOY_UID -m conntrack --ctstate NEW -j CONNMARK --set-mark $OUTMARK --nfmask $OUTMARK_MASK --ctmask $OUTMARK_MASK
 
 # If we see the connmark in the PREROUTING, this is a packet coming back and should be directed to envoy. mark it with $OUTMARK_RET and send it to envoy with an ip rule.
 # We set a different mark on return path, as we only want these packages to go back to localhost, the outgoing packet needs to be routed to the local network card that belongs to the destination.
 ip rule add fwmark $OUTMARK_RET lookup $ROUTE_TABLE
-iptables-nft -t mangle -A uproxy-PREROUTING -i "${INTERFACE_PREFIX}+" -m connmark --mark $OUTMARK -j MARK --set-mark $OUTMARK_RET --nfmask $OUTMARK_RET_MASK --ctmask $OUTMARK_MASK
+$IPTABLES -t mangle -A uproxy-PREROUTING -m connmark --mark $OUTMARK -j MARK --set-mark $OUTMARK_RET
 # if a packet has the return mark, accept it (as no futher processing is needed, and ip rule above will send it to envoy)
-iptables-nft -t mangle -A uproxy-PREROUTING -i "${INTERFACE_PREFIX}+" -m mark --mark $OUTMARK_RET -j ACCEPT
+$IPTABLES -t mangle -A uproxy-PREROUTING -m mark --mark $OUTMARK_RET -j ACCEPT
 # return here, so filter network policy will potentially be applied. TODO: test if it is really needed.
-iptables-nft -t mangle -A uproxy-PREROUTING -i "${INTERFACE_PREFIX}+" -m mark --mark $OUTMARK -j RETURN
+$IPTABLES -t mangle -A uproxy-PREROUTING -m mark --mark $OUTMARK -j RETURN
+
+## Pod membership and tproxy setup.
 
 ipset create uproxy-pods-ips hash:ip
 # once we have a CNI the first tproxy rule can be changed to use an interface set. this has the advantage
@@ -156,22 +167,22 @@ ipset create uproxy-pods-ips hash:ip
 # ipset create uproxy-pods-ifaces hash:iface
 
 # note sure if needed. right now as the proxy sends traffic to itself if pod is on the same node
-# iptables-nft -t mangle -A uproxy-PREROUTING -p tcp -d 127.0.0.1 --dport $POD_INBOUND -j ACCEPT
+# $IPTABLES -t mangle -A uproxy-PREROUTING -p tcp -d 127.0.0.1 --dport $POD_INBOUND -j ACCEPT
 # disable rule above for now, as we have the nat rule in the init container
 
 # TODO: once we sort out the pod in the same node communication, we can remove the " ! --dport 15088" part here.
 
 # tproxy outbound connections from "injected" pods.
-iptables-nft -t mangle -A uproxy-PREROUTING -p tcp -i "${INTERFACE_PREFIX}+" ! --dport 15088 -m set --match-set uproxy-pods-ips src -j TPROXY --tproxy-mark $MARK --on-port $POD_OUTBOUND --on-ip 127.0.0.1
+$IPTABLES -t mangle -A uproxy-PREROUTING -p tcp -i "${INTERFACE_PREFIX}+" ! --dport 15088 -m set --match-set uproxy-pods-ips src -j TPROXY --tproxy-mark $MARK --on-port $POD_OUTBOUND --on-ip 127.0.0.1
 
 # if we got here, this is not an outbound connection from pod in the mesh.
 # it could be an inbound connection from pod not in the mesh to a pod in the mesh.
 # tproxy connections to "injected" pods (that can come from pods not in the mesh, but on the same node).
-iptables-nft -t mangle -A uproxy-PREROUTING -p tcp -i "${INTERFACE_PREFIX}+" ! --dport 15088 -m set --match-set uproxy-pods-ips dst -j TPROXY --tproxy-mark $MARK --on-port $POD_INBOUND --on-ip 127.0.0.1
+$IPTABLES -t mangle -A uproxy-PREROUTING -p tcp -i "${INTERFACE_PREFIX}+" ! --dport 15088 -m set --match-set uproxy-pods-ips dst -j TPROXY --tproxy-mark $MARK --on-port $POD_INBOUND --on-ip 127.0.0.1
 
 # external traffic to pod
 # tproxy connections coming from outside the node to "injected" pods.
-iptables-nft -t mangle -A uproxy-PREROUTING -p tcp -i "${INCOMING_INTERFACE}" ! --dport 15088 -m set --match-set uproxy-pods-ips dst -j TPROXY --tproxy-mark $MARK --on-port $POD_INBOUND --on-ip 127.0.0.1
+$IPTABLES -t mangle -A uproxy-PREROUTING -p tcp -i "${INCOMING_INTERFACE}" ! --dport 15088 -m set --match-set uproxy-pods-ips dst -j TPROXY --tproxy-mark $MARK --on-port $POD_INBOUND --on-ip 127.0.0.1
 
 EOF
 done
