c1079d8221feb8f7d55e5c52a9c4bbfee0d7dfb0
Ingwon Song <102102227+ingwonsong@users.noreply.github.com>
2022-05-19 12:55:27 -0700

    Allow all the insecure servers when WASM_INSECURE_REGISTRIES has a "*" (#39024)
    * Allow all the insecure servers when the environment variable WASM_INSECURE_REGISTRIES has a "*"
    
    * Fix the flakiness

--- pkg/wasm/cache.go
+++ pkg/wasm/cache.go
@@ -81,9 +81,10 @@ type LocalFileCache struct {
 	mux sync.Mutex
 
 	// Duration for stale Wasm module purging.
-	purgeInterval      time.Duration
-	wasmModuleExpiry   time.Duration
-	insecureRegistries sets.Set
+	purgeInterval              time.Duration
+	wasmModuleExpiry           time.Duration
+	insecureRegistries         sets.Set
+	allowAllInsecureRegistries bool
 
 	// stopChan currently is only used by test
 	stopChan chan struct{}
@@ -127,6 +128,7 @@ type cacheEntry struct {
 
 // NewLocalFileCache create a new Wasm module cache which downloads and stores Wasm module files locally.
 func NewLocalFileCache(dir string, purgeInterval, moduleExpiry time.Duration, insecureRegistries []string) *LocalFileCache {
+	ir := sets.New(insecureRegistries...)
 	cache := &LocalFileCache{
 		httpFetcher:        NewHTTPFetcher(DefaultWasmHTTPRequestTimeout),
 		modules:            make(map[moduleKey]*cacheEntry),
@@ -135,8 +137,11 @@ func NewLocalFileCache(dir string, purgeInterval, moduleExpiry time.Duration, in
 		purgeInterval:      purgeInterval,
 		wasmModuleExpiry:   moduleExpiry,
 		stopChan:           make(chan struct{}),
-		insecureRegistries: sets.New(insecureRegistries...),
+		insecureRegistries: ir,
+		// If the set of the given insecure registries contains "*", then allow all the insecure registries.
+		allowAllInsecureRegistries: ir.Contains("*"),
 	}
+
 	go func() {
 		cache.purge()
 	}()
@@ -203,7 +208,7 @@ func (c *LocalFileCache) Get(
 	// Hex-Encoded sha256 checksum of binary.
 	var dChecksum string
 	var binaryFetcher func() ([]byte, error)
-	insecure := c.insecureRegistries.Contains(u.Host)
+	insecure := c.allowAllInsecureRegistries || c.insecureRegistries.Contains(u.Host)
 
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
