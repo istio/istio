43fa2a2c5616f322dd75488485744f550aa48916
Aryan Gupta <garyan@google.com>
2022-05-20 23:06:26 +0000

    enabled gateway for config cluster

--- tests/integration/security/external_ca/main_test.go
+++ tests/integration/security/external_ca/main_test.go
@@ -118,10 +118,10 @@ values:
 components:
   ingressGateways:
   - name: istio-ingressgateway
-    enabled: false
+    enabled: true
   egressGateways:
   - name: istio-egressgateway
-    enabled: false
+    enabled: true
   istiodRemote:
     k8s:
       overlays:
