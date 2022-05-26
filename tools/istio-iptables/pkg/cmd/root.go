78b1d92c046fa66b585a1efa09ae4bda4990e7db
zhlsunshine <huailong.zhang@intel.com>
2022-05-26 17:44:34 +0800

    Enable both IPv4 and IPv6 iptables rule to support dual stack.
    Co-authored-by: jacob-delgado <jacob.delgado@volunteers.acasi.info>
    Co-authored-by: irisdingbj <irisdingbj@gmail.com>
    Co-authored-by: ycai-aspen <yingchun.cai@volunteers.acasi.info>

--- tools/istio-iptables/pkg/cmd/root.go
+++ tools/istio-iptables/pkg/cmd/root.go
@@ -78,15 +78,17 @@ var rootCmd = &cobra.Command{
 			}
 		}
 		if cfg.RunValidation {
-			hostIP, err := getLocalIP()
+			hostIPs, err := getLocalIP()
 			if err != nil {
 				// Assume it is not handled by istio-cni and won't reuse the ValidationErrorCode
 				panic(err)
 			}
-			validator := validation.NewValidator(cfg, hostIP)
 
-			if err := validator.Run(); err != nil {
-				handleErrorWithCode(err, constants.ValidationErrorCode)
+			for _, hostIP := range hostIPs {
+				validator := validation.NewValidator(cfg, hostIP)
+				if err := validator.Run(); err != nil {
+					handleErrorWithCode(err, constants.ValidationErrorCode)
+				}
 			}
 		}
 	},
@@ -162,11 +164,17 @@ func constructConfig() *config.Config {
 	}
 
 	// Detect whether IPv6 is enabled by checking if the pod's IP address is IPv4 or IPv6.
-	podIP, err := getLocalIP()
+	podIPs, err := getLocalIP()
 	if err != nil {
 		panic(err)
 	}
-	cfg.EnableInboundIPv6 = podIP.To4() == nil
+
+	for _, podIP := range podIPs {
+		if podIP.To4() == nil {
+			cfg.EnableInboundIPv6 = true
+			break
+		}
+	}
 
 	// Lookup DNS nameservers. We only do this if DNS is enabled in case of some obscure theoretical
 	// case where reading /etc/resolv.conf could fail.
@@ -183,18 +191,22 @@ func constructConfig() *config.Config {
 }
 
 // getLocalIP returns the local IP address
-func getLocalIP() (net.IP, error) {
+func getLocalIP() ([]net.IP, error) {
 	addrs, err := LocalIPAddrs()
 	if err != nil {
 		return nil, err
 	}
 
+	var netip []net.IP
 	for _, a := range addrs {
 		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && !ipnet.IP.IsLinkLocalUnicast() && !ipnet.IP.IsLinkLocalMulticast() {
-			return ipnet.IP, nil
+			netip = append(netip, ipnet.IP)
 		}
 	}
-	return nil, fmt.Errorf("no valid local IP address found")
+	if len(netip) != 0 {
+		return netip, nil
+	}
+	return netip, fmt.Errorf("no valid local IP address found")
 }
 
 func handleError(err error) {
