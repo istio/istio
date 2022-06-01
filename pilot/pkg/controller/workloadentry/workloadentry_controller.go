34f1f464334ccd018402a7832bfc3775a5bb6b53
Nicole LiHui <nicole.li@daocloud.io>
2022-06-02 00:47:11 +0800

    [Release-1.12] Fix pilot panic with vm workload entry: assignment to entry in nil map  (#39217)
    * > fix pilot panic with vm workload entry: assignment to entry in nil map
    
    * add releasenotes

--- pilot/pkg/controller/workloadentry/workloadentry_controller.go
+++ pilot/pkg/controller/workloadentry/workloadentry_controller.go
@@ -249,6 +249,9 @@ func (c *Controller) processNextWorkItem() bool {
 }
 
 func setConnectMeta(c *config.Config, controller string, conTime time.Time) {
+	if c.Annotations == nil {
+		c.Annotations = map[string]string{}
+	}
 	c.Annotations[WorkloadControllerAnnotation] = controller
 	c.Annotations[ConnectedAtAnnotation] = conTime.Format(timeFormat)
 	delete(c.Annotations, DisconnectedAtAnnotation)
