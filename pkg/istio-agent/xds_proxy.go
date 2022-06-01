a45b0b5126b049339c38dd37a846cd889a5ea00f
Ingwon Song <102102227+ingwonsong@users.noreply.github.com>
2022-06-01 13:09:11 -0700

    Use the same thread for grpcstream.SendMsg (#39230)

--- pkg/istio-agent/xds_proxy.go
+++ pkg/istio-agent/xds_proxy.go
@@ -533,7 +533,6 @@ func (p *XdsProxy) handleUpstreamResponse(con *ProxyConnection) {
 						// to prevent concurrent access to forwardToEnvoy
 						select {
 						case forwardEnvoyCh <- resp:
-							proxyLog.Debugf("wasm send response: %v", resp.TypeUrl)
 						case <-con.stopChan:
 						}
 					})
