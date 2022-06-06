55f0fb7f24ad933ddddefc58d04b27c31cfdfd97
nmittler <nmittler@gmail.com>
2022-06-06 12:58:45 -0700

    [tf] re-enable authz tests except for egress
    Based on [recent flakes](https://github.com/istio/istio/issues/39255) the egress test appears to be very flaky.
    
    As a temporary fix, we accidentally [disabled](https://github.com/istio/istio/pull/39280) all authz tests.
    
    This PR re-enables the authz test suite, but disables only the egress test.

--- tests/integration/security/authz/main_test.go
+++ tests/integration/security/authz/main_test.go
@@ -37,7 +37,6 @@ var (
 func TestMain(m *testing.M) {
 	framework.
 		NewSuite(m).
-		Skip("https://github.com/istio/istio/issues/39255").
 		Setup(istio.Setup(nil, func(_ resource.Context, cfg *istio.Config) {
 			cfg.ControlPlaneValues = `
 values:
