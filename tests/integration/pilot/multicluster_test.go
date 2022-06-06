4ede111c96d21089ad4ca6972fb9c775c8dec752
Steven Landow <landow@google.com>
2022-06-06 15:56:17 -0700

    TestBadRemoteSecret: wait for containers to ready before status check
    Change-Id: I4e4cdbe36d9dc78ada48f8286611c2fd5b38d05d

--- tests/integration/pilot/multicluster_test.go
+++ tests/integration/pilot/multicluster_test.go
@@ -20,6 +20,7 @@ package pilot
 import (
 	"context"
 	"fmt"
+	"sigs.k8s.io/yaml"
 	"testing"
 	"time"
 
@@ -265,6 +266,19 @@ stringData:
 				}
 			})
 
+			retry.UntilSuccessOrFail(t, func() error {
+				pod, err := pods.Get(context.TODO(), pod, metav1.GetOptions{})
+				if err != nil {
+					return err
+				}
+				for _, status := range pod.Status.ContainerStatuses {
+					if status.Started != nil && !*status.Started {
+						return fmt.Errorf("container %s in %s is not started", status.Name, pod)
+					}
+				}
+				return nil
+			}, retry.Timeout(5*time.Minute), retry.Delay(time.Second))
+
 			// make sure the pod comes up healthy
 			retry.UntilSuccessOrFail(t, func() error {
 				pod, err := pods.Get(context.TODO(), pod, metav1.GetOptions{})
@@ -273,7 +287,8 @@ stringData:
 				}
 				status := pod.Status.ContainerStatuses
 				if len(status) < 1 || !status[0].Ready {
-					return fmt.Errorf("%s not ready", pod)
+					conditions, _ := yaml.Marshal(pod.Status.ContainerStatuses)
+					return fmt.Errorf("%s not ready: %s", conditions)
 				}
 				return nil
 			}, retry.Timeout(time.Minute), retry.Delay(time.Second))
