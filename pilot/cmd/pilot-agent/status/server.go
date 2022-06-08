96484b703ecc6df7dacad3f265b2880e1a12cddc
Steve Zhang <huailong.zhang@intel.com>
2022-06-08 12:40:36 +0800

    [Dual Stack] Fixed bug for istio-agent status server (#39340)
    Fixed bug for istio-agent status server

--- pilot/cmd/pilot-agent/status/server.go
+++ pilot/cmd/pilot-agent/status/server.go
@@ -164,6 +164,14 @@ func NewServer(config Options) (*Server, error) {
 	if config.IPv6 {
 		localhost = localHostIPv6
 		upstreamLocalAddress = UpstreamLocalAddressIPv6
+	} else {
+		// if not ipv6-only, it can be ipv4-only or dual-stack
+		// let InstanceIP decide the localhost
+		netIP := net.ParseIP(config.PodIP)
+		if netIP.To4() == nil && netIP.To16() != nil && !netIP.IsLinkLocalUnicast() {
+			localhost = localHostIPv6
+			upstreamLocalAddress = UpstreamLocalAddressIPv6
+		}
 	}
 	probes := make([]ready.Prober, 0)
 	if !config.NoEnvoy {
