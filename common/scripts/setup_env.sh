9d23daa5df20f6cb2bf235ee33c8423695627f69
istio-testing <istio-testing-bot@google.com>
2022-06-09 13:24:45 +0000

    Automator: update common-files@release-1.14 in istio/istio@release-1.14

--- common/scripts/setup_env.sh
+++ common/scripts/setup_env.sh
@@ -73,7 +73,7 @@ fi
 
 # Build image to use
 if [[ "${IMAGE_VERSION:-}" == "" ]]; then
-  IMAGE_VERSION=release-1.14-2022-05-12T14-42-16
+  IMAGE_VERSION=release-1.14-2022-06-09T02-18-17
 fi
 if [[ "${IMAGE_NAME:-}" == "" ]]; then
   IMAGE_NAME=build-tools
