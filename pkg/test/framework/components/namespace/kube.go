578b10280a12af2a21afc32dfea5c2a2d722bb6e
Aryan Gupta <garyan@google.com>
2022-06-09 21:35:22 +0000

    add istio-system ns creation for external cp cluster

--- pkg/test/framework/components/namespace/kube.go
+++ pkg/test/framework/components/namespace/kube.go
@@ -239,7 +239,7 @@ func (n *kubeNamespace) createInCluster(c cluster.Cluster, cfg Config) error {
 
 func (n *kubeNamespace) forEachCluster(fn func(i int, c cluster.Cluster) error) error {
 	errG := multierror.Group{}
-	for i, c := range n.ctx.Clusters().Kube() {
+	for i, c := range n.ctx.AllClusters().Kube() {
 		i, c := i, c
 		errG.Go(func() error {
 			return fn(i, c)
