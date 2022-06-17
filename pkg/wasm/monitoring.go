896c11332e6fa8c70e17efc54e3b50fd4324ca45
Rama Chavali <rama.rao@salesforce.com>
2022-06-17 15:24:15 +0530

    add more debug logging to wasm image fetcher
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pkg/wasm/monitoring.go
+++ pkg/wasm/monitoring.go
@@ -21,6 +21,7 @@ const (
 	// For remote fetch metric.
 	fetchSuccess     = "success"
 	downloadFailure  = "download_failure"
+	manifestFailure  = "mainfest_failure"
 	checksumMismatch = "checksum_mismatched"
 
 	// For Wasm conversion metric.
