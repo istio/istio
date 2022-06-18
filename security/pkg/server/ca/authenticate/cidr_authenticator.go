4d8e065cf376e4cff0cf58e18fa5be2b5232563e
Rama Chavali <rama.rao@salesforce.com>
2022-06-18 14:36:16 +0530

    add cidr authenticator
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

---
+++ security/pkg/server/ca/authenticate/cidr_authenticator.go
@@ -0,0 +1,86 @@
+// Copyright 2017 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package authenticate
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"strings"
+
+	"google.golang.org/grpc/peer"
+	"istio.io/istio/pilot/pkg/features"
+	"istio.io/istio/pkg/security"
+)
+
+const (
+	CidrAuthenticatorType = "CidrAuthenticator"
+)
+
+// CidrAuthenticator extracts identities from connected ip if it is part of trusted cidr ranges.
+type CidrAuthenticator struct{}
+
+var _ security.Authenticator = &CidrAuthenticator{}
+
+func (c *CidrAuthenticator) AuthenticatorType() string {
+	return CidrAuthenticatorType
+}
+
+// Authenticate extracts identities from trusted cidr ranges.
+func (c *CidrAuthenticator) Authenticate(ctx security.AuthContext) (*security.Caller, error) {
+	peerInfo, _ := peer.FromContext(ctx.RequestContext)
+	if !isAuthenticated(peerInfo.Addr.String()) {
+		return nil, fmt.Errorf("")
+	}
+	ctx.AddDelegatedAuthenticator(XfccAuthenticator{})
+	return &security.Caller{Identities: []string{peerInfo.Addr.String()}}, nil
+}
+
+func isAuthenticated(addr string) bool {
+	if len(features.TrustedCIDRRanges) > 0 {
+		cidrs := strings.Split(features.TrustedCIDRRanges, ",")
+		for _, cidr := range cidrs {
+			if isInRange(addr, cidr) {
+				return true
+			}
+		}
+	}
+	// Always trust local host addresses.
+	if net.ParseIP(addr).IsLoopback() {
+		return true
+	}
+	return false
+}
+
+func isInRange(addr, cidr string) bool {
+	if strings.Contains(cidr, "/") {
+		ip, ipnet, err := net.ParseCIDR(cidr)
+		if err != nil {
+			return false
+		}
+		if ip.To4() == nil && ip.To16() == nil {
+			return false
+		}
+		return ipnet.Contains(net.ParseIP(addr))
+	}
+	return false
+}
+
+func (c *CidrAuthenticator) AuthenticateRequest(req *http.Request) (*security.Caller, error) {
+	if isAuthenticated(req.RemoteAddr) {
+		return &security.Caller{Identities: []string{req.RemoteAddr}}, nil
+	}
+	return nil, nil
+}
---
+++ security/pkg/server/ca/authenticate/cidr_authenticator_test.go
@@ -0,0 +1,83 @@
+// Copyright 2017 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package authenticate
+
+import (
+	"net"
+	"reflect"
+	"testing"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc/peer"
+
+	"istio.io/istio/pilot/pkg/features"
+	"istio.io/istio/pkg/security"
+	"istio.io/istio/pkg/test"
+)
+
+func TestCidrAuthenticator(t *testing.T) {
+	cases := []struct {
+		name   string
+		cidr   string
+		caller *security.Caller
+		peer   string
+	}{
+		{
+			name: "localhost client",
+			cidr: "",
+			peer: "127.0.0.1",
+			caller: &security.Caller{
+				AuthSource: security.AuthSourceClientCertificate,
+				Identities: []string{
+					"127.0.0.1",
+				},
+			},
+		},
+		{
+			name: "cidr in range",
+			cidr: "172.17.0.0/16,192.17.0.0/16",
+			peer: "172.17.0.2",
+			caller: &security.Caller{
+				AuthSource: security.AuthSourceClientCertificate,
+				Identities: []string{
+					"172.17.0.2",
+				},
+			},
+		},
+		{
+			name:   "cidr outside range",
+			cidr:   "172.17.0.0/16,172.17.0.0/16",
+			peer:   "110.17.0.2",
+			caller: nil,
+		},
+	}
+
+	auth := &CidrAuthenticator{}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			if len(tt.cidr) > 0 {
+				test.SetStringForTest(t, &features.TrustedCIDRRanges, tt.cidr)
+			}
+			ctx := peer.NewContext(context.Background(), &peer.Peer{Addr: &net.IPAddr{IP: net.ParseIP(tt.peer).To4()}})
+			result, _ := auth.Authenticate(security.NewAuthContext(ctx))
+
+			if !reflect.DeepEqual(tt.caller, result) {
+				t.Errorf("Unexpected authentication result: want %v but got %v",
+					tt.caller, result)
+			}
+		})
+	}
+}
