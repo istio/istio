ab63b39c7cf4657284e4dca21761f1433de4cc67
Akshay J Nambiar <akshayjnambiar@users.noreply.github.com>
2022-07-01 11:25:48 +0530

    [tf] Storing deployment templates in files (#39647)
    * [tf] Storing deployment templates in files
    
    The yaml templates for service, echo deployment and vm deployment are copied to files in the template directory. This will allow adding new templates and using it by specifying the DEPLOYMENT_TEMPLATE environment variable.
    
    * use flags to get template file paths and rename template files
    
    * use helper methods to parse templates and move flags to new flags.go file
    
    * add copyright banner to new file
    
    * reviews

--- pkg/test/framework/components/echo/kube/deployment.go
+++ pkg/test/framework/components/echo/kube/deployment.go
@@ -15,22 +15,25 @@
 package kube
 
 import (
-	"bufio"
 	"context"
 	"fmt"
 	"net"
 	"os"
 	"path"
+	"path/filepath"
 	"strings"
 	"text/template"
 	"time"
 
-	"github.com/Masterminds/sprig/v3"
 	"github.com/hashicorp/go-multierror"
 	kubeCore "k8s.io/api/core/v1"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+
+	"istio.io/istio/pkg/test/util/file"
+
+	"istio.io/istio/pkg/test/env"
 
 	"istio.io/api/label"
 	meshconfig "istio.io/api/mesh/v1alpha1"
@@ -58,469 +61,34 @@ const (
 	grpcMagicPort = 17171
 	// for non-Go implementations of gRPC echo, this is the port used to forward non-gRPC requests to the Go server
 	grpcFallbackPort = 17777
-
-	serviceYAML = `
-{{- if .ServiceAccount }}
-apiVersion: v1
-kind: ServiceAccount
-metadata:
-  name: {{ .Service }}
----
-{{- end }}
-apiVersion: v1
-kind: Service
-metadata:
-  name: {{ .Service }}
-  labels:
-    app: {{ .Service }}
-{{- if .ServiceAnnotations }}
-  annotations:
-{{- range $name, $value := .ServiceAnnotations }}
-    {{ $name.Name }}: {{ printf "%q" $value.Value }}
-{{- end }}
-{{- end }}
-spec:
-{{- if .IPFamilies }}
-  ipFamilies: [ {{ .IPFamilies }} ]
-{{- end }}
-{{- if .IPFamilyPolicy }}
-  ipFamilyPolicy: {{ .IPFamilyPolicy }}
-{{- end }}
-{{- if .Headless }}
-  clusterIP: None
-{{- end }}
-  ports:
-{{- range $i, $p := .ServicePorts }}
-  - name: {{ $p.Name }}
-    port: {{ $p.ServicePort }}
-    targetPort: {{ $p.WorkloadPort }}
-{{- end }}
-  selector:
-    app: {{ .Service }}
-`
-
-	deploymentYAML = `
-{{- $revVerMap := .Revisions }}
-{{- $subsets := .Subsets }}
-{{- $cluster := .Cluster }}
-{{- range $i, $subset := $subsets }}
-{{- range $revision, $version := $revVerMap }}
-apiVersion: apps/v1
-{{- if $.StatefulSet }}
-kind: StatefulSet
-{{- else }}
-kind: Deployment
-{{- end }}
-metadata:
-{{- if $.Compatibility }}
-  name: {{ $.Service }}-{{ $subset.Version }}-{{ $revision }}
-{{- else }}
-  name: {{ $.Service }}-{{ $subset.Version }}
-{{- end }}
-spec:
-  {{- if $.StatefulSet }}
-  serviceName: {{ $.Service }}
-  {{- end }}
-  replicas: 1
-  selector:
-    matchLabels:
-      app: {{ $.Service }}
-      version: {{ $subset.Version }}
-{{- if ne $.Locality "" }}
-      istio-locality: {{ $.Locality }}
-{{- end }}
-  template:
-    metadata:
-      labels:
-        app: {{ $.Service }}
-        version: {{ $subset.Version }}
-        test.istio.io/class: {{ $.WorkloadClass }}
-{{- if $.Compatibility }}
-        istio.io/rev: {{ $revision }}
-{{- end }}
-{{- if ne $.Locality "" }}
-        istio-locality: {{ $.Locality }}
-{{- end }}
-      annotations:
-        prometheus.io/scrape: "true"
-        prometheus.io/port: "15014"
-{{- range $name, $value := $subset.Annotations }}
-        {{ $name.Name }}: {{ printf "%q" $value.Value }}
-{{- end }}
-{{- if $.ProxylessGRPC }}
-        proxy.istio.io/config: '{"holdApplicationUntilProxyStarts": true}'
-{{- end }}
-    spec:
-{{- if $.ServiceAccount }}
-      serviceAccountName: {{ $.Service }}
-{{- end }}
-{{- if ne $.ImagePullSecretName "" }}
-      imagePullSecrets:
-      - name: {{ $.ImagePullSecretName }}
-{{- end }}
-      containers:
-{{- if and
-  (ne ($subset.Annotations.GetByName "sidecar.istio.io/inject") "false")
-  (ne ($subset.Annotations.GetByName "inject.istio.io/templates") "grpc")
-  ($.OverlayIstioProxy)
-}}
-      - name: istio-proxy
-        image: auto
-        imagePullPolicy: {{ $.ImagePullPolicy }}
-        securityContext: # to allow core dumps
-          readOnlyRootFilesystem: false
-{{- end }}
-{{- if $.IncludeExtAuthz }}
-      - name: ext-authz
-        image: {{ $.ImageHub }}/ext-authz:{{ $.ImageTag }}
-        imagePullPolicy: {{ $.ImagePullPolicy }}
-        ports:
-        - containerPort: 8000
-        - containerPort: 9000
-{{- end }}
-{{- range $i, $appContainer := $.AppContainers }}
-      - name: {{ $appContainer.Name }}
-{{- if $appContainer.ImageFullPath }}
-        image: {{ $appContainer.ImageFullPath }}
-{{- else }}
-        image: {{ $.ImageHub }}/app:{{ $.ImageTag }}
-{{- end }}
-        imagePullPolicy: {{ $.ImagePullPolicy }}
-        securityContext:
-          runAsUser: 1338
-          runAsGroup: 1338
-        args:
-{{- if $appContainer.FallbackPort }}
-          - --forwarding_address=0.0.0.0:{{ $appContainer.FallbackPort }}
-{{- end }}
-          - --metrics=15014
-          - --cluster={{ $cluster }}
-{{- range $i, $p := $appContainer.ContainerPorts }}
-{{- if and $p.XDSServer (eq .Protocol "GRPC") }}
-          - --xds-grpc-server={{ $p.Port }}
-{{- else if eq .Protocol "GRPC" }}
-          - --grpc={{ $p.Port }}
-{{- else if eq .Protocol "TCP" }}
-          - --tcp={{ $p.Port }}
-{{- else }}
-          - --port={{ $p.Port }}
-{{- end }}
-{{- if $p.TLS }}
-          - --tls={{ $p.Port }}
-{{- end }}
-{{- if $p.ServerFirst }}
-          - --server-first={{ $p.Port }}
-{{- end }}
-{{- if $p.InstanceIP }}
-          - --bind-ip={{ $p.Port }}
-{{- end }}
-{{- if $p.LocalhostIP }}
-          - --bind-localhost={{ $p.Port }}
-{{- end }}
-{{- end }}
-          - --version={{ $subset.Version }}
-          - --istio-version={{ $version }}
-{{- if $.TLSSettings }}
-          - --crt=/etc/certs/custom/cert-chain.pem
-          - --key=/etc/certs/custom/key.pem
-{{- if $.TLSSettings.AcceptAnyALPN}}
-          - --disable-alpn
-{{- end }}
-{{- else }}
-          - --crt=/cert.crt
-          - --key=/cert.key
-{{- end }}
-        ports:
-{{- range $i, $p := $appContainer.ContainerPorts }}
-        - containerPort: {{ $p.Port }}
-{{- if eq .Port 3333 }}
-          name: tcp-health-port
-{{- else if and ($appContainer.ImageFullPath) (eq .Port 17171) }}
-          name: tcp-health-port
-{{- end }}
-{{- end }}
-        env:
-        - name: INSTANCE_IP
-          valueFrom:
-            fieldRef:
-              fieldPath: status.podIP
-{{- if $.ProxylessGRPC }}
-        - name: EXPOSE_GRPC_ADMIN
-          value: "true"
-{{- end }}
-        readinessProbe:
-{{- if $.ReadinessTCPPort }}
-          tcpSocket:
-            port: {{ $.ReadinessTCPPort }}
-{{- else if $.ReadinessGRPCPort }}
-          grpc:
-            port: {{ $.ReadinessGRPCPort }}			
-{{- else if $appContainer.ImageFullPath }}
-          tcpSocket:
-            port: tcp-health-port
-{{- else }}
-          httpGet:
-            path: /
-            port: 8080
-{{- end }}
-          initialDelaySeconds: 1
-          periodSeconds: 2
-          failureThreshold: 10
-        livenessProbe:
-          tcpSocket:
-            port: tcp-health-port
-          initialDelaySeconds: 10
-          periodSeconds: 10
-          failureThreshold: 10
-{{- if $.StartupProbe }}
-        startupProbe:
-          tcpSocket:
-            port: tcp-health-port
-          periodSeconds: 1
-          failureThreshold: 10
-{{- end }}
-{{- if $.TLSSettings }}
-        volumeMounts:
-        - mountPath: /etc/certs/custom
-          name: custom-certs
-{{- end }}
-{{- end }}
-{{- if $.TLSSettings }}
-      volumes:
-{{- if $.TLSSettings.ProxyProvision }}
-      - emptyDir:
-          medium: Memory
-{{- else }}
-      - configMap:
-          name: {{ $.Service }}-certs
-{{- end }}
-        name: custom-certs
-{{- end }}
----
-{{- end }}
-{{- end }}
-{{- if .TLSSettings}}{{if not .TLSSettings.ProxyProvision }}
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: {{ $.Service }}-certs
-data:
-  root-cert.pem: |
-{{ .TLSSettings.RootCert | indent 4 }}
-  cert-chain.pem: |
-{{ .TLSSettings.ClientCert | indent 4 }}
-  key.pem: |
-{{.TLSSettings.Key | indent 4}}
----
-{{- end}}{{- end}}
-`
-
-	// vmDeploymentYaml aims to simulate a VM, but instead of managing the complex test setup of spinning up a VM,
-	// connecting, etc we run it inside a pod. The pod has pretty much all Kubernetes features disabled (DNS and SA token mount)
-	// such that we can adequately simulate a VM and DIY the bootstrapping.
-	vmDeploymentYaml = `
-{{- $subsets := .Subsets }}
-{{- $cluster := .Cluster }}
-{{- range $i, $subset := $subsets }}
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: {{ $.Service }}-{{ $subset.Version }}
-spec:
-  replicas: 1
-  selector:
-    matchLabels:
-      istio.io/test-vm: {{ $.Service }}
-      istio.io/test-vm-version: {{ $subset.Version }}
-  template:
-    metadata:
-      annotations:
-        # Sidecar is inside the pod to simulate VMs - do not inject
-        sidecar.istio.io/inject: "false"
-      labels:
-        # Label should not be selected. We will create a workload entry instead
-        istio.io/test-vm: {{ $.Service }}
-        istio.io/test-vm-version: {{ $subset.Version }}
-    spec:
-      # Disable kube-dns, to mirror VM
-      # we set policy to none and explicitly provide a set of invalid values
-      # for nameservers, search namespaces, etc. ndots is set to 1 so that
-      # the application will first try to resolve the hostname (a, a.ns, etc.) as is
-      # before attempting to add the search namespaces.
-      dnsPolicy: None
-      dnsConfig:
-        nameservers:
-        - "8.8.8.8"
-        searches:
-        - "com"
-        options:
-        - name: "ndots"
-          value: "1"
-{{- if $.VM.IstioHost }}
-      # Override the istiod host to force traffic through east-west gateway. 
-      hostAliases:
-      - ip: {{ $.VM.IstioIP }}
-        hostnames:
-        - {{ $.VM.IstioHost }}
-{{- end }}
-      # Disable service account mount, to mirror VM
-      automountServiceAccountToken: false
-      {{- if $.ImagePullSecretName }}
-      imagePullSecrets:
-      - name: {{ $.ImagePullSecretName }}
-      {{- end }}
-      containers:
-      - name: istio-proxy
-        image: {{ $.ImageHub }}/{{ $.VM.Image }}:{{ $.ImageTag }}
-        imagePullPolicy: {{ $.ImagePullPolicy }}
-        securityContext:
-          capabilities:
-            add:
-            - NET_ADMIN
-          runAsUser: 1338
-          runAsGroup: 1338
-        command:
-        - bash
-        - -c
-        - |-
-          # To support image builders which cannot do RUN, do the run commands at startup.
-          # This exploits the fact the images remove the installer once its installed.
-          # This is a horrible idea for production images, but these are just for tests.
-          [[ -f /tmp/istio-sidecar-centos-7.rpm ]] && sudo rpm -vi /tmp/istio-sidecar-centos-7.rpm && sudo rm /tmp/istio-sidecar-centos-7.rpm
-          [[ -f /tmp/istio-sidecar.rpm ]] && sudo rpm -vi /tmp/istio-sidecar.rpm && sudo rm /tmp/istio-sidecar.rpm
-          [[ -f /tmp/istio-sidecar.deb ]] && sudo dpkg -i /tmp/istio-sidecar.deb && sudo rm /tmp/istio-sidecar.deb
-
-          # Read root cert from and place signed certs here (can't mount directly or the dir would be unwritable)
-          sudo mkdir -p /var/run/secrets/istio
-
-          # hack: remove certs that are bundled in the image
-          sudo rm /var/run/secrets/istio/cert-chain.pem
-          sudo rm /var/run/secrets/istio/key.pem
-          sudo chown -R istio-proxy /var/run/secrets
-
-          # place mounted bootstrap files (token is mounted directly to the correct location)
-          sudo cp /var/run/secrets/istio/bootstrap/root-cert.pem /var/run/secrets/istio/root-cert.pem
-          sudo cp /var/run/secrets/istio/bootstrap/*.env /var/lib/istio/envoy/
-          sudo cp /var/run/secrets/istio/bootstrap/mesh.yaml /etc/istio/config/mesh
-
-          # don't overwrite /etc/hosts since it's managed by kubeproxy
-          #sudo sh -c 'cat /var/run/secrets/istio/bootstrap/hosts >> /etc/hosts'
-
-          # since we're not overwriting /etc/hosts on k8s, verify that istiod hostname in /etc/hosts
-          # matches the value generated by istioctl
-          echo "checking istio host"
-          SYSTEM_HOST=$(cat /etc/hosts | grep istiod)
-          ISTIOCTL_HOST=$(cat /var/run/secrets/istio/bootstrap/hosts | grep istiod)
-          if [ "$(echo "$SYSTEM_HOST" | tr -d '[:space:]')" != "$(echo "$ISTIOCTL_HOST" | tr -d '[:space:]')" ]; then
-            echo "istiod host in /etc/hosts does not match value generated by istioctl"
-            echo "/etc/hosts: $SYSTEM_HOST"
-            echo "/var/run/secrets/istio/bootstrap/hosts: $ISTIOCTL_HOST"
-            exit 1
-          fi
-          echo "istiod host ok"
-
-          # read certs from correct directory
-          sudo sh -c 'echo PROV_CERT=/var/run/secrets/istio >> /var/lib/istio/envoy/cluster.env'
-          sudo sh -c 'echo OUTPUT_CERTS=/var/run/secrets/istio >> /var/lib/istio/envoy/cluster.env'
-
-          # su will mess with the limits set on the process we run. This may lead to quickly exhausting the file limits
-          # We will get the host limit and set it in the child as well.
-          # TODO(https://superuser.com/questions/1645513/why-does-executing-a-command-in-su-change-limits) can we do better?
-          currentLimit=$(ulimit -n)
-
-          # Run the pilot agent and Envoy
-          # TODO: run with systemctl?
-          export ISTIO_AGENT_FLAGS="--concurrency 2 --proxyLogLevel warning,misc:error,rbac:debug,jwt:debug"
-          sudo -E -s /bin/bash -c "ulimit -n ${currentLimit}; exec /usr/local/bin/istio-start.sh&"
-
-          /usr/local/bin/server --cluster "{{ $cluster }}" --version "{{ $subset.Version }}" \
-{{- range $i, $p := $.ContainerPorts }}
-{{- if eq .Protocol "GRPC" }}
-             --grpc \
-{{- else if eq .Protocol "TCP" }}
-             --tcp \
-{{- else }}
-             --port \
-{{- end }}
-             "{{ $p.Port }}" \
-{{- if $p.ServerFirst }}
-             --server-first={{ $p.Port }} \
-{{- end }}
-{{- if $p.TLS }}
-             --tls={{ $p.Port }} \
-{{- end }}
-{{- if $p.InstanceIP }}
-             --bind-ip={{ $p.Port }} \
-{{- end }}
-{{- if $p.LocalhostIP }}
-             --bind-localhost={{ $p.Port }} \
-{{- end }}
-{{- end }}
-             --crt=/var/lib/istio/cert.crt \
-             --key=/var/lib/istio/cert.key
-        env:
-        - name: INSTANCE_IP
-          valueFrom:
-            fieldRef:
-              fieldPath: status.podIP
-        volumeMounts:
-        - mountPath: /var/run/secrets/tokens
-          name: {{ $.Service }}-istio-token
-        - mountPath: /var/run/secrets/istio/bootstrap
-          name: istio-vm-bootstrap
-        {{- range $name, $value := $subset.Annotations }}
-        {{- if eq $name.Name "sidecar.istio.io/bootstrapOverride" }}
-        - mountPath: /etc/istio-custom-bootstrap
-          name: custom-bootstrap-volume
-        {{- end }}
-        {{- end }}
-{{- if $.IncludeExtAuthz }}
-      - name: ext-authz
-        image: {{ $.ImageHub }}/ext-authz:{{ $.ImageTag }}
-        imagePullPolicy: {{ $.ImagePullPolicy }}
-        ports:
-        - containerPort: 8000
-        - containerPort: 9000
-{{- end }}
-      volumes:
-      - secret:
-          secretName: {{ $.Service }}-istio-token
-        name: {{ $.Service }}-istio-token
-      - configMap:
-          name: {{ $.Service }}-{{ $subset.Version }}-vm-bootstrap
-        name: istio-vm-bootstrap
-      {{- range $name, $value := $subset.Annotations }}
-      {{- if eq $name.Name "sidecar.istio.io/bootstrapOverride" }}
-      - name: custom-bootstrap-volume
-        configMap:
-          name: {{ $value.Value }}
-      {{- end }}
-      {{- end }}
-{{- end}}
-`
 )
 
 var (
 	serviceTemplate      *template.Template
 	deploymentTemplate   *template.Template
 	vmDeploymentTemplate *template.Template
+
+	echoKubeTemplatesDir = path.Join(env.IstioSrc, "pkg/test/framework/components/echo/kube/templates")
 )
 
 func init() {
-	serviceTemplate = template.New("echo_service")
-	if _, err := serviceTemplate.Funcs(sprig.TxtFuncMap()).Parse(serviceYAML); err != nil {
-		panic(fmt.Sprintf("unable to parse echo service template: %v", err))
+	serviceYAMLPath := path.Join(echoKubeTemplatesDir, serviceTemplateFile)
+	if filepath.IsAbs(serviceTemplateFile) {
+		serviceYAMLPath = serviceTemplateFile
 	}
+	serviceTemplate = tmpl.MustParse(file.MustAsString(serviceYAMLPath))
 
-	deploymentTemplate = template.New("echo_deployment")
-	if _, err := deploymentTemplate.Funcs(sprig.TxtFuncMap()).Parse(deploymentYAML); err != nil {
-		panic(fmt.Sprintf("unable to parse echo deployment template: %v", err))
+	deploymentYAMLPath := path.Join(echoKubeTemplatesDir, deploymentTemplateFile)
+	if filepath.IsAbs(deploymentTemplateFile) {
+		deploymentYAMLPath = deploymentTemplateFile
 	}
+	deploymentTemplate = tmpl.MustParse(file.MustAsString(deploymentYAMLPath))
 
-	vmDeploymentTemplate = template.New("echo_vm_deployment")
-	if _, err := vmDeploymentTemplate.Funcs(sprig.TxtFuncMap()).Funcs(template.FuncMap{"Lines": lines}).Parse(vmDeploymentYaml); err != nil {
-		panic(fmt.Sprintf("unable to parse echo vm deployment template: %v", err))
+	vmDeploymentYAMLPath := path.Join(echoKubeTemplatesDir, vmDeploymentTemplateFile)
+	if filepath.IsAbs(vmDeploymentTemplateFile) {
+		vmDeploymentYAMLPath = vmDeploymentTemplateFile
 	}
+	vmDeploymentTemplate = tmpl.MustParse(file.MustAsString(vmDeploymentYAMLPath))
 }
 
 var _ workloadHandler = &deployment{}
@@ -825,15 +393,6 @@ func serviceParams(cfg echo.Config) map[string]interface{} {
 		"IPFamilies":         cfg.IPFamilies,
 		"IPFamilyPolicy":     cfg.IPFamilyPolicy,
 	}
-}
-
-func lines(input string) []string {
-	out := make([]string, 0)
-	scanner := bufio.NewScanner(strings.NewReader(input))
-	for scanner.Scan() {
-		out = append(out, scanner.Text())
-	}
-	return out
 }
 
 // createVMConfig sets up a Service account,
