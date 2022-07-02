6c451db70c4769c5f7592ac8eaf15530082a4676
Rama Chavali <rama.rao@salesforce.com>
2022-07-02 14:08:07 +0530

    fix compile
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/xds/xds_cache_test.go
+++ pilot/pkg/xds/xds_cache_test.go
@@ -290,7 +290,7 @@ func TestXdsCacheEvict(t *testing.T) {
 			dependents := key.DependentConfigs()
 			for _, config := range dependents {
 				if lruCache.GetKeysByConfigKey(config) != nil {
-					return fmt.Errorf("config %s should be evicted", config)
+					return fmt.Errorf("config %v should be evicted", config)
 				}
 			}
 		}
