38c42183a71b90229d34a4823b61e070beca5339
Zhonghu Xu <xuzhonghu@huawei.com>
2022-07-04 16:58:55 +0800

    Not really evict a key that is added later after LRU evict bt before the evict handler run by istio (#39764)
    * Donot evict keys that are added again after evict
    
    * Add test

--- pilot/pkg/model/xds_cache.go
+++ pilot/pkg/model/xds_cache.go
@@ -229,6 +229,13 @@ func (l *LruCache) clearEvicted() {
 	if len(l.evictedKeys) < 100 {
 		return
 	}
+	// Do not evict keys that are added again after evicted
+	for key := range l.evictedKeys {
+		_, ok := l.store.Get(key)
+		if ok {
+			delete(l.evictedKeys, key)
+		}
+	}
 
 	for configKey, index := range l.configIndex {
 		evicted := index.Intersection(l.evictedKeys)
