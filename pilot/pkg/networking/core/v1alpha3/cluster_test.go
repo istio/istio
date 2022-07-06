2db21adf36bec11cb83d68ed1a6bec2fe303b237
Rama Chavali <rama.rao@salesforce.com>
2022-07-06 07:14:28 +0530

    minor refactor in cluster builder (#39757)
    * fix cluster nack for strict dns clusters
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * add comments and tests
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * add additional condition
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * rearrange
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/networking/core/v1alpha3/cluster_test.go
+++ pilot/pkg/networking/core/v1alpha3/cluster_test.go
@@ -25,6 +25,7 @@ import (
 
 	cluster "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
 	core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	endpoint "github.com/envoyproxy/go-control-plane/envoy/config/endpoint/v3"
 	http "github.com/envoyproxy/go-control-plane/envoy/extensions/upstreams/http/v3"
 	"github.com/google/go-cmp/cmp"
 	. "github.com/onsi/gomega"
@@ -1708,12 +1709,13 @@ func TestAutoMTLSClusterIgnoreWorkloadLevelPeerAuthn(t *testing.T) {
 
 func TestApplyLoadBalancer(t *testing.T) {
 	testcases := []struct {
-		name                           string
-		lbSettings                     *networking.LoadBalancerSettings
-		discoveryType                  cluster.Cluster_DiscoveryType
-		port                           *model.Port
-		expectedLbPolicy               cluster.Cluster_LbPolicy
-		expectedLocalityWeightedConfig bool
+		name                               string
+		lbSettings                         *networking.LoadBalancerSettings
+		discoveryType                      cluster.Cluster_DiscoveryType
+		port                               *model.Port
+		expectedLbPolicy                   cluster.Cluster_LbPolicy
+		expectedLocalityWeightedConfig     bool
+		expectClusterLoadAssignmenttoBeNil bool
 	}{
 		{
 			name:             "ORIGINAL_DST discovery type is a no op",
@@ -1748,6 +1750,15 @@ func TestApplyLoadBalancer(t *testing.T) {
 			expectedLbPolicy:               defaultLBAlgorithm(),
 			expectedLocalityWeightedConfig: true,
 		},
+		{
+			name:          "DNS cluster with PASSTHROUGH in DR",
+			discoveryType: cluster.Cluster_STRICT_DNS,
+			lbSettings: &networking.LoadBalancerSettings{
+				LbPolicy: &networking.LoadBalancerSettings_Simple{Simple: networking.LoadBalancerSettings_PASSTHROUGH},
+			},
+			expectedLbPolicy:                   cluster.Cluster_CLUSTER_PROVIDED,
+			expectClusterLoadAssignmenttoBeNil: true,
+		},
 		// TODO: add more to cover all cases
 	}
 
@@ -1761,6 +1772,7 @@ func TestApplyLoadBalancer(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			c := &cluster.Cluster{
 				ClusterDiscoveryType: &cluster.Cluster_Type{Type: tt.discoveryType},
+				LoadAssignment:       &endpoint.ClusterLoadAssignment{},
 			}
 
 			if tt.discoveryType == cluster.Cluster_ORIGINAL_DST {
@@ -1779,6 +1791,9 @@ func TestApplyLoadBalancer(t *testing.T) {
 
 			if tt.expectedLocalityWeightedConfig && c.CommonLbConfig.GetLocalityWeightedLbConfig() == nil {
 				t.Errorf("cluster expected to have weighed config, but is nil")
+			}
+			if tt.expectClusterLoadAssignmenttoBeNil && c.LoadAssignment != nil {
+				t.Errorf("cluster expected not to have load assignmentset, but is present")
 			}
 		})
 	}
