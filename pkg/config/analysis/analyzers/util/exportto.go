1585ebdb0cb306eeaa68ef2240985239a5b37c6e
Xiaopeng Han <hanxiaop8@outlook.com>
2022-07-08 23:13:29 +0800

    Fix analyze conflicting mesh gateway with exportTo (#39729)
    * fix exportto not work in analyze
    
    * fix
    
    * add releasenotes
    
    * Update releasenotes/notes/39729.yaml
    
    Co-authored-by: Eric Van Norman <ericvn@us.ibm.com>
    
    Co-authored-by: Eric Van Norman <ericvn@us.ibm.com>

--- pkg/config/analysis/analyzers/util/exportto.go
+++ pkg/config/analysis/analyzers/util/exportto.go
@@ -17,15 +17,16 @@ package util
 // IsExportToAllNamespaces returns true if export to applies to all namespaces
 // and false if it is set to namespace local.
 func IsExportToAllNamespaces(exportTos []string) bool {
-	exportedToAll := true
+	exportedToAll := false
 	for _, e := range exportTos {
-		if e == ExportToNamespaceLocal {
-			exportedToAll = false
-		} else if e == ExportToAllNamespaces {
+		if e == ExportToAllNamespaces {
 			// give preference to "*" and stop iterating
 			exportedToAll = true
 			break
 		}
+	}
+	if len(exportTos) == 0 {
+		exportedToAll = true
 	}
 	return exportedToAll
 }
