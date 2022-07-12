d5596f73ec94b754f5717447abf947522b65426c
Sebastian Schepens <sebastian.schepens@mercadolibre.com>
2022-07-12 16:18:18 -0300

    add size warning and error

--- pkg/config/validation/validation.go
+++ pkg/config/validation/validation.go
@@ -83,6 +83,8 @@ const (
 )
 
 const (
+	kb              = 1024
+	mb              = 1024 * kb
 	regionIndex int = iota
 	zoneIndex
 	subZoneIndex
@@ -3024,12 +3026,29 @@ func validateHTTPRedirect(redirect *networking.HTTPRedirect) error {
 	return nil
 }
 
-func validateHTTPDirectResponse(directResponse *networking.HTTPDirectResponse) error {
+func validateHTTPDirectResponse(directResponse *networking.HTTPDirectResponse) (errs Validation) {
 	if directResponse == nil {
-		return nil
+		return
 	}
 
-	return validateHTTPStatus(int32(directResponse.Status))
+	if directResponse.Body != nil {
+		size := 0
+		switch op := directResponse.Body.Specifier.(type) {
+		case *networking.HTTPBody_String_:
+			size = len(op.String_)
+		case *networking.HTTPBody_Bytes:
+			size = len(op.Bytes)
+		}
+
+		if size > 1*mb {
+			errs = appendValidation(errs, WrapError(fmt.Errorf("large direct_responses may impact control plane performance, must be less than 1MB")))
+		} else if size > 100*kb {
+			errs = appendValidation(errs, WrapWarning(fmt.Errorf("large direct_responses may impact control plane performance")))
+		}
+	}
+
+	errs = appendValidation(errs, WrapError(validateHTTPStatus(int32(directResponse.Status))))
+	return
 }
 
 func validateHTTPRewrite(rewrite *networking.HTTPRewrite) error {
--- pkg/config/validation/validation_test.go
+++ pkg/config/validation/validation_test.go
@@ -2158,6 +2158,7 @@ func TestValidateHTTPDirectResponse(t *testing.T) {
 		name           string
 		directResponse *networking.HTTPDirectResponse
 		valid          bool
+		warning        bool
 	}{
 		{
 			name:           "nil redirect",
@@ -2196,6 +2197,27 @@ func TestValidateHTTPDirectResponse(t *testing.T) {
 			valid: true,
 		},
 		{
+			name: "with string body over 100kb",
+			directResponse: &networking.HTTPDirectResponse{
+				Status: 200,
+				Body: &networking.HTTPBody{
+					Specifier: &networking.HTTPBody_String_{String_: strings.Repeat("a", 101*kb)},
+				},
+			},
+			valid:   true,
+			warning: true,
+		},
+		{
+			name: "with string body over 1mb",
+			directResponse: &networking.HTTPDirectResponse{
+				Status: 200,
+				Body: &networking.HTTPBody{
+					Specifier: &networking.HTTPBody_String_{String_: strings.Repeat("a", 2*mb)},
+				},
+			},
+			valid: false,
+		},
+		{
 			name: "with bytes body",
 			directResponse: &networking.HTTPDirectResponse{
 				Status: 200,
@@ -2205,12 +2227,36 @@ func TestValidateHTTPDirectResponse(t *testing.T) {
 			},
 			valid: true,
 		},
+		{
+			name: "with bytes body over 100kb",
+			directResponse: &networking.HTTPDirectResponse{
+				Status: 200,
+				Body: &networking.HTTPBody{
+					Specifier: &networking.HTTPBody_Bytes{Bytes: []byte(strings.Repeat("a", (100*kb)+1))},
+				},
+			},
+			valid:   true,
+			warning: true,
+		},
+		{
+			name: "with bytes body over 1mb",
+			directResponse: &networking.HTTPDirectResponse{
+				Status: 200,
+				Body: &networking.HTTPBody{
+					Specifier: &networking.HTTPBody_Bytes{Bytes: []byte(strings.Repeat("a", (1*mb)+1))},
+				},
+			},
+			valid: false,
+		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			if err := validateHTTPDirectResponse(tc.directResponse); (err == nil) != tc.valid {
-				t.Fatalf("got valid=%v but wanted valid=%v: %v", err == nil, tc.valid, err)
+			if err := validateHTTPDirectResponse(tc.directResponse); (err.Err == nil) != tc.valid {
+				t.Fatalf("got valid=%v but wanted valid=%v: %v", err.Err == nil, tc.valid, err)
+			}
+			if err := validateHTTPDirectResponse(tc.directResponse); (err.Warning != nil) != tc.warning {
+				t.Fatalf("got valid=%v but wanted valid=%v: %v", err.Warning != nil, tc.warning, err)
 			}
 		})
 	}
