d2a28611c639bdf0e015bdd2cbeb4ceeaf45b626
hejianpeng <hejianpeng2@huawei.com>
2022-07-13 15:14:31 +0800

    add tests

--- pilot/pkg/networking/core/v1alpha3/accesslog_test.go
+++ pilot/pkg/networking/core/v1alpha3/accesslog_test.go
@@ -18,6 +18,9 @@ import (
 	"testing"
 
 	accesslog "github.com/envoyproxy/go-control-plane/envoy/config/accesslog/v3"
+	core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	listener "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
+	fileaccesslog "github.com/envoyproxy/go-control-plane/envoy/extensions/access_loggers/file/v3"
 	httppb "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
 	tcp "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/tcp_proxy/v3"
 	"github.com/envoyproxy/go-control-plane/pkg/conversion"
@@ -26,10 +29,21 @@ import (
 	"google.golang.org/protobuf/testing/protocmp"
 
 	meshconfig "istio.io/api/mesh/v1alpha1"
+	tpb "istio.io/api/telemetry/v1alpha1"
+	"istio.io/api/type/v1beta1"
+	"istio.io/istio/pilot/pkg/config/memory"
 	"istio.io/istio/pilot/pkg/model"
+	"istio.io/istio/pilot/pkg/networking"
 	"istio.io/istio/pilot/pkg/networking/util"
+	memregistry "istio.io/istio/pilot/pkg/serviceregistry/memory"
+	"istio.io/istio/pilot/pkg/util/protoconv"
 	"istio.io/istio/pilot/test/xdstest"
+	"istio.io/istio/pkg/config"
 	"istio.io/istio/pkg/config/mesh"
+	"istio.io/istio/pkg/config/protocol"
+	"istio.io/istio/pkg/config/schema/collections"
+	"istio.io/istio/pkg/config/schema/gvk"
+	"istio.io/istio/pkg/test/util/assert"
 	"istio.io/istio/pkg/util/protomarshal"
 )
 
@@ -211,4 +225,267 @@ spec:
 	}
 }
 
-// TODO: add tests for filters
+func newTestEnviroment() *model.Environment {
+	serviceDiscovery := memregistry.NewServiceDiscovery(&model.Service{
+		Hostname:       "test.example.org",
+		DefaultAddress: "1.1.1.1",
+		Ports: model.PortList{
+			&model.Port{
+				Name:     "default",
+				Port:     8080,
+				Protocol: protocol.HTTP,
+			},
+		},
+	})
+
+	meshConfig := mesh.DefaultMeshConfig()
+	meshConfig.AccessLogFile = model.DevStdout
+	meshConfig.ExtensionProviders = append(meshConfig.ExtensionProviders, &meshconfig.MeshConfig_ExtensionProvider{
+		Name: "envoy-json",
+		Provider: &meshconfig.MeshConfig_ExtensionProvider_EnvoyFileAccessLog{
+			EnvoyFileAccessLog: &meshconfig.MeshConfig_ExtensionProvider_EnvoyFileAccessLogProvider{
+				Path: model.DevStdout,
+				LogFormat: &meshconfig.MeshConfig_ExtensionProvider_EnvoyFileAccessLogProvider_LogFormat{
+					LogFormat: &meshconfig.MeshConfig_ExtensionProvider_EnvoyFileAccessLogProvider_LogFormat_Labels{},
+				},
+			},
+		},
+	})
+
+	configStore := model.MakeIstioStore(memory.Make(collections.Pilot))
+	configStore.Create(config.Config{
+		Meta: config.Meta{
+			Name:             "test",
+			Namespace:        "default",
+			GroupVersionKind: gvk.Telemetry,
+		},
+		Spec: &tpb.Telemetry{
+			Selector: &v1beta1.WorkloadSelector{
+				MatchLabels: map[string]string{
+					"app": "test",
+				},
+			},
+			AccessLogging: []*tpb.AccessLogging{
+				{
+					Providers: []*tpb.ProviderRef{
+						{
+							Name: "envoy-json",
+						},
+					},
+				},
+			},
+		},
+	})
+
+	env := model.NewEnvironment()
+	env.ServiceDiscovery = serviceDiscovery
+	env.ConfigStore = configStore
+	env.Watcher = mesh.NewFixedWatcher(meshConfig)
+
+	env.PushContext = model.NewPushContext()
+	env.Init()
+	_ = env.PushContext.InitContext(env, nil, nil)
+
+	return env
+}
+
+var (
+	defaultJSONLabelsOut = &fileaccesslog.FileAccessLog{
+		Path: model.DevStdout,
+		AccessLogFormat: &fileaccesslog.FileAccessLog_LogFormat{
+			LogFormat: &core.SubstitutionFormatString{
+				Format: &core.SubstitutionFormatString_JsonFormat{
+					JsonFormat: EnvoyJSONLogFormatIstio,
+				},
+			},
+		},
+	}
+
+	defaultOut = &fileaccesslog.FileAccessLog{
+		Path: model.DevStdout,
+		AccessLogFormat: &fileaccesslog.FileAccessLog_LogFormat{
+			LogFormat: &core.SubstitutionFormatString{
+				Format: &core.SubstitutionFormatString_TextFormatSource{
+					TextFormatSource: &core.DataSource{
+						Specifier: &core.DataSource_InlineString{
+							InlineString: model.EnvoyTextLogFormat,
+						},
+					},
+				},
+			},
+		},
+	}
+)
+
+func TestSetTCPAccessLog(t *testing.T) {
+	b := newAccessLogBuilder()
+
+	env := newTestEnviroment()
+
+	cases := []struct {
+		name     string
+		push     *model.PushContext
+		proxy    *model.Proxy
+		tcp      *tcp.TcpProxy
+		class    networking.ListenerClass
+		expected *tcp.TcpProxy
+	}{
+		{
+			name:  "telemetry",
+			push:  env.PushContext,
+			proxy: &model.Proxy{ConfigNamespace: "default", Metadata: &model.NodeMetadata{Labels: map[string]string{"app": "test"}}},
+			tcp:   &tcp.TcpProxy{},
+			class: networking.ListenerClassSidecarInbound,
+			expected: &tcp.TcpProxy{
+				AccessLog: []*accesslog.AccessLog{
+					{
+						Name:       wellknown.FileAccessLog,
+						ConfigType: &accesslog.AccessLog_TypedConfig{TypedConfig: protoconv.MessageToAny(defaultJSONLabelsOut)},
+					},
+				},
+			},
+		},
+		{
+			name:  "without-telemetry",
+			push:  env.PushContext,
+			proxy: &model.Proxy{ConfigNamespace: "default", Metadata: &model.NodeMetadata{Labels: map[string]string{"app": "without-telemetry"}}},
+			tcp:   &tcp.TcpProxy{},
+			class: networking.ListenerClassSidecarInbound,
+			expected: &tcp.TcpProxy{
+				AccessLog: []*accesslog.AccessLog{
+					{
+						Name:       wellknown.FileAccessLog,
+						ConfigType: &accesslog.AccessLog_TypedConfig{TypedConfig: protoconv.MessageToAny(defaultOut)},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			b.setTCPAccessLog(tc.push, tc.proxy, tc.tcp, tc.class)
+			assert.Equal(t, tc.expected, tc.tcp)
+		})
+
+	}
+}
+
+func TestSetHttpAccessLog(t *testing.T) {
+	b := newAccessLogBuilder()
+
+	env := newTestEnviroment()
+
+	cases := []struct {
+		name     string
+		push     *model.PushContext
+		proxy    *model.Proxy
+		hcm      *httppb.HttpConnectionManager
+		class    networking.ListenerClass
+		expected *httppb.HttpConnectionManager
+	}{
+		{
+			name:  "telemetry",
+			push:  env.PushContext,
+			proxy: &model.Proxy{ConfigNamespace: "default", Metadata: &model.NodeMetadata{Labels: map[string]string{"app": "test"}}},
+			hcm:   &httppb.HttpConnectionManager{},
+			class: networking.ListenerClassSidecarInbound,
+			expected: &httppb.HttpConnectionManager{
+				AccessLog: []*accesslog.AccessLog{
+					{
+						Name:       wellknown.FileAccessLog,
+						ConfigType: &accesslog.AccessLog_TypedConfig{TypedConfig: protoconv.MessageToAny(defaultJSONLabelsOut)},
+					},
+				},
+			},
+		},
+		{
+			name:  "without-telemetry",
+			push:  env.PushContext,
+			proxy: &model.Proxy{ConfigNamespace: "default", Metadata: &model.NodeMetadata{Labels: map[string]string{"app": "without-telemetry"}}},
+			hcm:   &httppb.HttpConnectionManager{},
+			class: networking.ListenerClassSidecarInbound,
+			expected: &httppb.HttpConnectionManager{
+				AccessLog: []*accesslog.AccessLog{
+					{
+						Name:       wellknown.FileAccessLog,
+						ConfigType: &accesslog.AccessLog_TypedConfig{TypedConfig: protoconv.MessageToAny(defaultOut)},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			b.setHTTPAccessLog(tc.push, tc.proxy, tc.hcm, tc.class)
+			assert.Equal(t, tc.expected, tc.hcm)
+		})
+
+	}
+}
+
+func TestSetListenerAccessLog(t *testing.T) {
+	b := newAccessLogBuilder()
+
+	env := newTestEnviroment()
+
+	cases := []struct {
+		name     string
+		push     *model.PushContext
+		proxy    *model.Proxy
+		listener *listener.Listener
+		class    networking.ListenerClass
+		expected *listener.Listener
+	}{
+		{
+			name:     "telemetry",
+			push:     env.PushContext,
+			proxy:    &model.Proxy{ConfigNamespace: "default", Metadata: &model.NodeMetadata{Labels: map[string]string{"app": "test"}}},
+			listener: &listener.Listener{},
+			class:    networking.ListenerClassSidecarInbound,
+			expected: &listener.Listener{
+				AccessLog: []*accesslog.AccessLog{
+					{
+						Name: wellknown.FileAccessLog,
+						Filter: &accesslog.AccessLogFilter{
+							FilterSpecifier: &accesslog.AccessLogFilter_ResponseFlagFilter{
+								ResponseFlagFilter: &accesslog.ResponseFlagFilter{Flags: []string{"NR"}},
+							},
+						},
+						ConfigType: &accesslog.AccessLog_TypedConfig{TypedConfig: protoconv.MessageToAny(defaultJSONLabelsOut)},
+					},
+				},
+			},
+		},
+		{
+			name:     "without-telemetry",
+			push:     env.PushContext,
+			proxy:    &model.Proxy{ConfigNamespace: "default", Metadata: &model.NodeMetadata{Labels: map[string]string{"app": "without-telemetry"}}},
+			listener: &listener.Listener{},
+			class:    networking.ListenerClassSidecarInbound,
+			expected: &listener.Listener{
+
+				AccessLog: []*accesslog.AccessLog{
+					{
+						Name: wellknown.FileAccessLog,
+						Filter: &accesslog.AccessLogFilter{
+							FilterSpecifier: &accesslog.AccessLogFilter_ResponseFlagFilter{
+								ResponseFlagFilter: &accesslog.ResponseFlagFilter{Flags: []string{"NR"}},
+							},
+						},
+						ConfigType: &accesslog.AccessLog_TypedConfig{TypedConfig: protoconv.MessageToAny(defaultOut)},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			b.setListenerAccessLog(tc.push, tc.proxy, tc.listener, tc.class)
+			assert.Equal(t, tc.expected, tc.listener)
+		})
+
+	}
+}
