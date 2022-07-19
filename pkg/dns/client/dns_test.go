8c1e603f2900e970c12d4672526fbfdffd2ee14b
John Howard <howardjohn@google.com>
2022-07-19 02:12:19 +0000

    Mitigate TestDNS flakes (#39971)
    Two issues:
    * Port conflict on 15053. Pretty simple, bind to port 0
    * We want to test servers supporting TCP+UDP on one port, but we cannot
      atomically reserve a free tcp and udp port. Currently we reserve UDP
    first then bind to TCP. This just swaps the order. In practice we have
    less UDP listeners, so much less chance of conflict. This dropped flakes
    from 1/10 to 1/1000 on my machine

--- pkg/dns/client/dns_test.go
+++ pkg/dns/client/dns_test.go
@@ -29,19 +29,17 @@ import (
 	"istio.io/istio/pkg/test"
 )
 
-var testAgentDNSAddr = "127.0.0.1:15053"
-
 func TestDNSForwardParallel(t *testing.T) {
-	initDNS(t, true)
-	testDNS(t)
+	d := initDNS(t, true)
+	testDNS(t, d)
 }
 
 func TestDNS(t *testing.T) {
-	initDNS(t, false)
-	testDNS(t)
+	d := initDNS(t, false)
+	testDNS(t, d)
 }
 
-func testDNS(t *testing.T) {
+func testDNS(t *testing.T, d *LocalDNSServer) {
 	testCases := []struct {
 		name                     string
 		host                     string
@@ -260,6 +258,10 @@ func testDNS(t *testing.T) {
 			Net:     "tcp",
 		},
 	}
+	addresses := []string{
+		d.dnsProxies[0].Address(),
+		d.dnsProxies[1].Address(),
+	}
 	currentID := atomic.NewInt32(0)
 	oldID := dns.Id
 	dns.Id = func() uint16 {
@@ -267,6 +269,7 @@ func testDNS(t *testing.T) {
 	}
 	defer func() { dns.Id = oldID }()
 	for i := range clients {
+		addr := addresses[i]
 		for _, tt := range testCases {
 			// Test is for explicit network
 			if (strings.HasPrefix(tt.name, "udp") || strings.HasPrefix(tt.name, "tcp")) && !strings.HasPrefix(tt.name, clients[i].Net) {
@@ -286,7 +289,7 @@ func testDNS(t *testing.T) {
 					currentID.Store(int32(tt.id))
 					defer func() { currentID.Store(0) }()
 				}
-				res, _, err := clients[i].Exchange(m, testAgentDNSAddr)
+				res, _, err := clients[i].Exchange(m, addr)
 				if res != nil {
 					t.Log("size: ", len(res.Answer))
 				}
@@ -328,15 +331,15 @@ func testDNS(t *testing.T) {
 //   docker run -v $PWD:$PWD -w $PWD --network host quay.io/ssro/dnsperf dnsperf -p 15053 -d input -c 100 -l 30
 // where `input` contains dns queries to run, such as `echo.default. A`
 func BenchmarkDNS(t *testing.B) {
-	initDNS(t, false)
+	s := initDNS(t, false)
 	t.Run("via-agent-cache-miss", func(b *testing.B) {
-		bench(b, testAgentDNSAddr, "www.bing.com.")
+		bench(b, s.dnsProxies[0].Address(), "www.bing.com.")
 	})
 	t.Run("via-agent-cache-hit-fqdn", func(b *testing.B) {
-		bench(b, testAgentDNSAddr, "www.google.com.")
+		bench(b, s.dnsProxies[0].Address(), "www.google.com.")
 	})
 	t.Run("via-agent-cache-hit-cname", func(b *testing.B) {
-		bench(b, testAgentDNSAddr, "www.google.com.ns1.svc.cluster.local.")
+		bench(b, s.dnsProxies[0].Address(), "www.google.com.ns1.svc.cluster.local.")
 	})
 }
 
@@ -437,8 +440,28 @@ func makeUpstream(t test.Failer, responses map[string]string) string {
 		}
 	})
 	up := make(chan struct{})
-	server := &dns.Server{
+
+	tcp := &dns.Server{
 		Addr:              "127.0.0.1:0",
+		Net:               "tcp",
+		Handler:           mux,
+		NotifyStartedFunc: func() { close(up) },
+	}
+	go func() {
+		if err := tcp.ListenAndServe(); err != nil {
+			log.Warnf("listen error: %v", err)
+		}
+	}()
+	select {
+	case <-time.After(time.Second * 10):
+		t.Fatalf("setup timeout")
+	case <-up:
+	}
+
+	// Setup UDP server on same port
+	up = make(chan struct{})
+	server := &dns.Server{
+		Addr:              tcp.Listener.Addr().String(),
 		Net:               "udp",
 		Handler:           mux,
 		NotifyStartedFunc: func() { close(up) },
@@ -458,19 +481,6 @@ func makeUpstream(t test.Failer, responses map[string]string) string {
 	t.Cleanup(func() { _ = server.Shutdown() })
 	server.Addr = server.PacketConn.LocalAddr().String()
 
-	// Setup TCP server on same port
-	up = make(chan struct{})
-	tcp := &dns.Server{
-		Addr:              server.Addr,
-		Net:               "tcp",
-		Handler:           mux,
-		NotifyStartedFunc: func() { close(up) },
-	}
-	go func() {
-		if err := tcp.ListenAndServe(); err != nil {
-			log.Warnf("listen error: %v", err)
-		}
-	}()
 	select {
 	case <-time.After(time.Second * 10):
 		t.Fatalf("setup timeout")
@@ -484,7 +494,7 @@ func makeUpstream(t test.Failer, responses map[string]string) string {
 
 func initDNS(t test.Failer, forwardToUpstreamParallel bool) *LocalDNSServer {
 	srv := makeUpstream(t, map[string]string{"www.bing.com.": "1.1.1.1"})
-	testAgentDNS, err := NewLocalDNSServer("ns1", "ns1.svc.cluster.local", "localhost:15053", forwardToUpstreamParallel)
+	testAgentDNS, err := NewLocalDNSServer("ns1", "ns1.svc.cluster.local", "localhost:0", forwardToUpstreamParallel)
 	if err != nil {
 		t.Fatal(err)
 	}
