8d58fbee0caf9027d35350358e2d4dca740b75cd
Rama Chavali <rama.rao@salesforce.com>
2022-07-20 02:54:13 +0530

    handle non wildcard resources during reconnect (#39960)
    * handle non wildcard resources in resource warming duing reconnect
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * warming dependencies
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * remove test case
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * remove lds/rds dependency
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * comments
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/model/context.go
+++ pilot/pkg/model/context.go
@@ -356,8 +356,10 @@ type WatchedResource struct {
 	NonceNacked string
 
 	// AlwaysRespond, if true, will ensure that even when a request would otherwise be treated as an
-	// ACK, it will be responded to. Typically, this should be set to 'false' after response; keeping it
-	// true would likely result in an endless loop.
+	// ACK, it will be responded to. This typically happens when a proxy reconnects to another instance of
+	// Istiod. In that case, Envoy expects us to respond to EDS/RDS/SDS requests to finish warming of
+	// clusters/listeners.
+	// Typically, this should be set to 'false' after response; keeping it true would likely result in an endless loop.
 	AlwaysRespond bool
 
 	// LastSent tracks the time of the generated push, to determine the time it takes the client to ack.
