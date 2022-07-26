ddf1605597b2100a834171c22c65cd101d534c91
Patrick <patrickjiang0530@gmail.com>
2022-07-26 23:36:10 +0800

    add benchmark
    Signed-off-by: Patrick <patrickjiang0530@gmail.com>

--- pilot/cmd/pilot-agent/status/server_test.go
+++ pilot/cmd/pilot-agent/status/server_test.go
@@ -523,6 +523,90 @@ func TestStatsError(t *testing.T) {
 	}
 }
 
+func initServerWithStats(t *testing.B) *Server {
+
+	appOpenMetrics := `# TYPE jvm info
+# HELP jvm VM version info
+jvm_info{runtime="OpenJDK Runtime Environment",vendor="AdoptOpenJDK",version="16.0.1+9"} 1.0
+# TYPE jmx_config_reload_success counter
+# HELP jmx_config_reload_success Number of times configuration have successfully been reloaded.
+jmx_config_reload_success_total 0.0
+jmx_config_reload_success_created 1.623984612719E9
+# EOF
+`
+	appText004 := `# HELP jvm_info VM version info
+# TYPE jvm_info gauge
+jvm_info{runtime="OpenJDK Runtime Environment",vendor="AdoptOpenJDK",version="16.0.1+9",} 1.0
+# HELP jmx_config_reload_failure_created Number of times configuration have failed to be reloaded.
+# TYPE jmx_config_reload_failure_created gauge
+jmx_config_reload_failure_created 1.624025983489E9
+`
+	envoy := `# TYPE my_metric counter
+my_metric{} 0
+
+# TYPE my_other_metric counter
+my_other_metric{} 0
+`
+	for i := 0; i < 100; i++ {
+		envoy = envoy + "\n#TYPE my_other_metric_" + strconv.Itoa(i) + " counter\nmy_other_metric_" + strconv.Itoa(i) + " 0"
+	}
+
+	envoyServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if _, err := w.Write([]byte(envoy)); err != nil {
+			t.Fatalf("write failed: %v", err)
+		}
+	}))
+	t.Cleanup(envoyServer.Close)
+	app := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		format := expfmt.NegotiateIncludingOpenMetrics(r.Header)
+		var negotiatedMetrics string
+		if format == expfmt.FmtOpenMetrics {
+			negotiatedMetrics = appOpenMetrics
+			w.Header().Set("Content-Type", string(expfmt.FmtOpenMetrics))
+		} else {
+			negotiatedMetrics = appText004
+			w.Header().Set("Content-Type", string(expfmt.FmtText))
+		}
+		if _, err := w.Write([]byte(negotiatedMetrics)); err != nil {
+			t.Fatalf("write failed: %v", err)
+		}
+	}))
+	t.Cleanup(app.Close)
+	envoyPort, err := strconv.Atoi(strings.Split(envoyServer.URL, ":")[2])
+	if err != nil {
+		t.Fatal(err)
+	}
+	server := &Server{
+		prometheus: &PrometheusScrapeConfiguration{
+			Port: strings.Split(app.URL, ":")[2],
+		},
+		envoyStatsPort: envoyPort,
+	}
+	return server
+}
+
+func BenchmarkStats(t *testing.B) {
+	server := initServerWithStats(t)
+	t.Run("stats-fmttext", func(t *testing.B) {
+		for i := 0; i < t.N; i++ {
+			req := &http.Request{}
+			req.Header = make(http.Header)
+			req.Header.Add("Accept", string(expfmt.FmtText))
+			rec := httptest.NewRecorder()
+			server.handleStats(rec, req)
+		}
+	})
+	t.Run("stats-fmtopenmetrics", func(t *testing.B) {
+		for i := 0; i < t.N; i++ {
+			req := &http.Request{}
+			req.Header = make(http.Header)
+			req.Header.Add("Accept", string(expfmt.FmtOpenMetrics))
+			rec := httptest.NewRecorder()
+			server.handleStats(rec, req)
+		}
+	})
+}
+
 func TestAppProbe(t *testing.T) {
 	// Starts the application first.
 	listener, err := net.Listen("tcp", ":0")
