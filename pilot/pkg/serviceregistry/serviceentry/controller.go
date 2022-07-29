0a0b3dd97bfafd3d407af38d6a0b3dea63d4ffb6
Greg Hanson <gregory.hanson@solo.io>
2022-07-29 12:19:08 -0400

    workload instance cause stale CDS clusters of type STRICT_DNS (#39947)
    * workload instance cause stale CDS clusters with of type STRICT_DNS
    
    * added release note
    
    * fewer full push triggers
    
    * code review comments for release note
    
    * extend logic for DNS_ROUND_ROBIN
    
    * update trigger reason to EndpointUpdate
    
    * add unit tests

--- pilot/pkg/serviceregistry/serviceentry/controller.go
+++ pilot/pkg/serviceregistry/serviceentry/controller.go
@@ -493,6 +493,8 @@ func (s *Controller) WorkloadInstanceHandler(wi *model.WorkloadInstance, event m
 
 	instances := []*model.ServiceInstance{}
 	instancesDeleted := []*model.ServiceInstance{}
+	configsUpdated := map[model.ConfigKey]struct{}{}
+	fullPush := false
 	for _, cfg := range cfgs {
 		se := cfg.Spec.(*networking.ServiceEntry)
 		if se.WorkloadSelector == nil || !labels.Instance(se.WorkloadSelector.Labels).SubsetOf(wi.Endpoint.Labels) {
@@ -515,6 +517,20 @@ func (s *Controller) WorkloadInstanceHandler(wi *model.WorkloadInstance, event m
 		} else {
 			s.serviceInstances.updateServiceEntryInstancesPerConfig(seNamespacedName, key, instance)
 		}
+		// If serviceentry's resolution is DNS, make a full push
+		// TODO: maybe cds?
+		if (se.Resolution == networking.ServiceEntry_DNS || se.Resolution == networking.ServiceEntry_DNS_ROUND_ROBIN) &&
+			se.WorkloadSelector != nil {
+
+			fullPush = true
+			for _, inst := range instance {
+				configsUpdated[model.ConfigKey{
+					Kind:      kind.ServiceEntry,
+					Name:      string(inst.Service.Hostname),
+					Namespace: cfg.Namespace,
+				}] = struct{}{}
+			}
+		}
 	}
 	if len(instancesDeleted) > 0 {
 		s.serviceInstances.deleteInstances(key, instancesDeleted)
@@ -528,6 +544,20 @@ func (s *Controller) WorkloadInstanceHandler(wi *model.WorkloadInstance, event m
 	s.mutex.Unlock()
 
 	s.edsUpdate(instances)
+
+	// ServiceEntry with WorkloadEntry results in STRICT_DNS cluster with hardcoded endpoints
+	// need to update CDS to refresh endpoints
+	// https://github.com/istio/istio/issues/39505
+	if fullPush {
+		log.Debugf("Full push triggered during event %s for workload instance (%s/%s) in namespace %s", event,
+			wi.Kind, wi.Endpoint.Address, wi.Namespace)
+		pushReq := &model.PushRequest{
+			Full:           true,
+			ConfigsUpdated: configsUpdated,
+			Reason:         []model.TriggerReason{model.EndpointUpdate},
+		}
+		s.XdsUpdater.ConfigUpdate(pushReq)
+	}
 }
 
 func (s *Controller) Provider() provider.ID {
