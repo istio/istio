f32526f48ff3872a7240466a0ab579ff845fb377
Sam Naser <samnaser@google.com>
2022-08-02 10:50:53 -0700

    [release-1.15] Automated branching step 4 (#40227)

--- common/Makefile.common.mk
+++ common/Makefile.common.mk
@@ -91,7 +91,7 @@ mirror-licenses: mod-download-go
 	@license-lint --mirror
 
 TMP := $(shell mktemp -d -u)
-UPDATE_BRANCH ?= "master"
+UPDATE_BRANCH ?= "release-1.15"
 
 update-common:
 	@mkdir -p $(TMP)
