1f6bba119abd60c9b1cffa55f8bcc1614a7e10b7
Anubhav <anubhavaeron@gmail.com>
2022-08-02 09:00:01 -0700

    Use absolute path when adding file watcher (#40137)
    * Use absolute path when adding file watcher
    
    * add test case which checks for absolute path
    
    * add testdata for TestTryAddFileWatcher test

--- security/pkg/nodeagent/cache/secretcache.go
+++ security/pkg/nodeagent/cache/secretcache.go
@@ -343,7 +343,11 @@ func (sc *SecretManagerClient) tryAddFileWatcher(file string, resourceName strin
 	// avoid processing duplicate events for the same file.
 	sc.certMutex.Lock()
 	defer sc.certMutex.Unlock()
-	file = filepath.Clean(file)
+	file, err := filepath.Abs(file)
+	if err != nil {
+		cacheLog.Errorf("%v: error finding absolute path of %s, retrying watches [%s] %v", resourceName, file, err)
+		return err
+	}
 	key := FileCert{
 		ResourceName: resourceName,
 		Filename:     file,
