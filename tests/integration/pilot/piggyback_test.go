2d0e53dd54fb742c577657138ce51ac0c399ca73
John Howard <howardjohn@google.com>
2022-08-10 07:42:39 -0700

    fmt

--- tests/integration/pilot/piggyback_test.go
+++ tests/integration/pilot/piggyback_test.go
@@ -22,9 +22,9 @@ import (
 	"testing"
 
 	xdsapi "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
-	"istio.io/istio/pkg/test/util/retry"
 
 	"istio.io/istio/pkg/test/framework"
+	"istio.io/istio/pkg/test/util/retry"
 	"istio.io/istio/pkg/util/protomarshal"
 )
 
