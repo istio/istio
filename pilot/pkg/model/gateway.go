5c96eb94085fab9e51f5492a759e302bf526e6ee
Istio Automation <istio-testing-bot@google.com>
2022-08-15 07:33:45 -0700

    [release-1.14] fix gateway with different bind to have same host (#40461)
    * fix gateway with different bind to have same host
    
    * add release note
    
    * address comments
    
    Co-authored-by: youchen <youchen@ebay.com>

--- pilot/pkg/model/gateway.go
+++ pilot/pkg/model/gateway.go
@@ -142,7 +142,7 @@ func MergeGateways(gateways []gatewayWithInstances, proxy *Proxy, ps *PushContex
 	gatewayNameForServer := make(map[*networking.Server]string)
 	verifiedCertificateReferences := sets.New()
 	http3AdvertisingRoutes := sets.New()
-	tlsHostsByPort := map[uint32]sets.Set{} // port -> host set
+	tlsHostsByPort := map[uint32]map[string]string{} // port -> host/bind map
 	autoPassthrough := false
 
 	log.Debugf("MergeGateways: merging %d gateways", len(gateways))
@@ -189,10 +189,14 @@ func MergeGateways(gateways []gatewayWithInstances, proxy *Proxy, ps *PushContex
 					// Envoy will reject config that has multiple filter chain matches with the same matching rules.
 					// To avoid this, we need to make sure we don't have duplicated hosts, which will become
 					// SNI filter chain matches.
+
+					// When there is Bind specified in the Gateway, the listener is built per IP instead of
+					// sharing one wildcard listener. So different Gateways can
+					// have same host as long as they have different Bind.
 					if tlsHostsByPort[resolvedPort] == nil {
-						tlsHostsByPort[resolvedPort] = sets.New()
+						tlsHostsByPort[resolvedPort] = map[string]string{}
 					}
-					if duplicateHosts := CheckDuplicates(s.Hosts, tlsHostsByPort[resolvedPort]); len(duplicateHosts) != 0 {
+					if duplicateHosts := CheckDuplicates(s.Hosts, s.Bind, tlsHostsByPort[resolvedPort]); len(duplicateHosts) != 0 {
 						log.Debugf("skipping server on gateway %s, duplicate host names: %v", gatewayName, duplicateHosts)
 						RecordRejectedConfig(gatewayName)
 						continue
@@ -415,17 +419,17 @@ func GetSNIHostsForServer(server *networking.Server) []string {
 
 // CheckDuplicates returns all of the hosts provided that are already known
 // If there were no duplicates, all hosts are added to the known hosts.
-func CheckDuplicates(hosts []string, knownHosts sets.Set) []string {
+func CheckDuplicates(hosts []string, bind string, knownHosts map[string]string) []string {
 	var duplicates []string
 	for _, h := range hosts {
-		if knownHosts.Contains(h) {
+		if existingBind, ok := knownHosts[h]; ok && bind == existingBind {
 			duplicates = append(duplicates, h)
 		}
 	}
 	// No duplicates found, so we can mark all of these hosts as known
 	if len(duplicates) == 0 {
 		for _, h := range hosts {
-			knownHosts.Insert(h)
+			knownHosts[h] = bind
 		}
 	}
 	return duplicates
