71ea88cfeb2fd20b94d3ed7656b3d7179b95b21e
John Howard <howardjohn@google.com>
2022-08-17 08:37:11 -0700

    Fix ProxyConfig merge impacting shared state (#40494)
    The issue was we were overriding things by setting them to pointers.
    These pointers are then shared, and when `proto.merge` is used they are
    all mutated.
    
    Instead, take deepcopies and add a test

--- pilot/pkg/model/proxy_config.go
+++ pilot/pkg/model/proxy_config.go
@@ -16,6 +16,7 @@ package model
 
 import (
 	"google.golang.org/protobuf/proto"
+	wrappers "google.golang.org/protobuf/types/known/wrapperspb"
 
 	"istio.io/api/annotation"
 	meshconfig "istio.io/api/mesh/v1alpha1"
@@ -134,10 +135,10 @@ func mergeWithPrecedence(pcs ...*meshconfig.ProxyConfig) *meshconfig.ProxyConfig
 		// telemetry code does with shallowMerge?
 		proto.Merge(merged, pcs[i])
 		if pcs[i].GetConcurrency() != nil {
-			merged.Concurrency = pcs[i].GetConcurrency()
+			merged.Concurrency = wrappers.Int32(pcs[i].GetConcurrency().GetValue())
 		}
 		if pcs[i].GetImage() != nil {
-			merged.Image = pcs[i].GetImage()
+			merged.Image = pcs[i].GetImage().DeepCopy()
 		}
 	}
 	return merged
