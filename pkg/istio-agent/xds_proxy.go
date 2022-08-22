a1bf454f7439ff65e89a754cc3db0535458a4343
Ingwon Song <102102227+ingwonsong@users.noreply.github.com>
2022-08-22 11:10:14 -0700

    Add Tap gRPC handler to Istio Agent. (#40566)
    * Add Tap GRPC handler to Istio Agent.
    
    * Fix lint errors
    
    * Add the integration test

--- pkg/istio-agent/xds_proxy.go
+++ pkg/istio-agent/xds_proxy.go
@@ -31,6 +31,8 @@ import (
 
 	discovery "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
 	"go.uber.org/atomic"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 	google_rpc "google.golang.org/genproto/googleapis/rpc/status"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
@@ -870,14 +872,27 @@ func (p *XdsProxy) makeTapHandler() func(w http.ResponseWriter, req *http.Reques
 func (p *XdsProxy) initDebugInterface(port int) error {
 	p.tapResponseChannel = make(chan *discovery.DiscoveryResponse)
 
+	tapGrpcHandler, err := NewTapGrpcHandler(p)
+	if err != nil {
+		log.Errorf("failed to start Tap XDS Proxy: %v", err)
+	}
+
 	httpMux := http.NewServeMux()
 	handler := p.makeTapHandler()
 	httpMux.HandleFunc("/debug/", handler)
 	httpMux.HandleFunc("/debug", handler) // For 1.10 Istiod which uses istio.io/debug
 
+	mixedHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.ProtoMajor == 2 && strings.HasPrefix(r.Header.Get("content-type"), "application/grpc") {
+			tapGrpcHandler.ServeHTTP(w, r)
+			return
+		}
+		httpMux.ServeHTTP(w, r)
+	})
+
 	p.httpTapServer = &http.Server{
 		Addr:        fmt.Sprintf("localhost:%d", port),
-		Handler:     httpMux,
+		Handler:     h2c.NewHandler(mixedHandler, &http2.Server{}),
 		IdleTimeout: 90 * time.Second, // matches http.DefaultTransport keep-alive timeout
 		ReadTimeout: 30 * time.Second,
 	}
