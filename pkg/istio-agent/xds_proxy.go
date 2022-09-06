13363a21aad6c65e222048c1ca11d0dd48e68f14
John Howard <howardjohn@google.com>
2022-09-06 14:22:32 -0700

    agent: fix regression in SAN check for agent (#40832)
    We have the error check inverted, so it is never used

--- pkg/istio-agent/xds_proxy.go
+++ pkg/istio-agent/xds_proxy.go
@@ -726,7 +726,7 @@ func (p *XdsProxy) getTLSDialOption(agent *Agent) (grpc.DialOption, error) {
 		MinVersion: tls.VersionTLS12,
 	}
 
-	if host, _, err := net.SplitHostPort(agent.proxyConfig.DiscoveryAddress); err != nil {
+	if host, _, err := net.SplitHostPort(agent.proxyConfig.DiscoveryAddress); err == nil {
 		config.ServerName = host
 	}
 	// For debugging on localhost (with port forward)
