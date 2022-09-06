986c7c727f7100effded7bad2025f299bedc7930
xin.li <xin.li@daocloud.io>
2022-09-06 19:52:21 +0800

    improve profile-list_test.go
    Signed-off-by: xin.li <xin.li@daocloud.io>

--- operator/cmd/mesh/profile-list_test.go
+++ operator/cmd/mesh/profile-list_test.go
@@ -38,7 +38,7 @@ func TestProfileList(t *testing.T) {
 		t.Fatalf("failed to execute istioctl profile command: %v", err)
 	}
 	output := out.String()
-	expectedProfiles := []string{"default", "demo", "empty", "minimal", "openshift", "preview", "external"}
+	expectedProfiles := []string{"default", "demo", "empty", "minimal", "openshift", "preview", "external", "remote"}
 	for _, prof := range expectedProfiles {
 		g.Expect(output).To(gomega.ContainSubstring(prof))
 	}
@@ -58,7 +58,7 @@ func TestProfileListByurl(t *testing.T) {
 		t.Fatalf("failed to execute istioctl profile command: %v", err)
 	}
 	output := out.String()
-	expectedProfiles := []string{"default", "demo", "empty", "minimal", "openshift", "preview", "external"}
+	expectedProfiles := []string{"default", "demo", "empty", "minimal", "openshift", "preview", "external", "remote"}
 	for _, prof := range expectedProfiles {
 		g.Expect(output).To(gomega.ContainSubstring(prof))
 	}
