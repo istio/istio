745d7c19618b2199c8b129952819f6584fdb3d2c
Eric Van Norman <ericvn@us.ibm.com>
2022-09-09 14:41:22 -0500

    Quick fix to get builds working on a Mac

--- bin/init.sh
+++ bin/init.sh
@@ -175,10 +175,8 @@ download_envoy_if_necessary "${ISTIO_ENVOY_LINUX_RELEASE_URL}" "$ISTIO_ENVOY_LIN
 download_envoy_if_necessary "${ISTIO_ENVOY_CENTOS_RELEASE_URL}" "$ISTIO_ENVOY_CENTOS_LINUX_RELEASE_PATH" "${SIDECAR}-centos"
 
 if [[ "$GOOS_LOCAL" == "darwin" ]]; then
-  # Download and extract the Envoy macOS release binary
-  download_envoy_if_necessary "${ISTIO_ENVOY_MACOS_RELEASE_URL}" "$ISTIO_ENVOY_MACOS_RELEASE_PATH" "${SIDECAR}"
-  ISTIO_ENVOY_NATIVE_PATH=${ISTIO_ENVOY_MACOS_RELEASE_PATH}
-else
+  # There is no longer an Istio built Envoy binary available. Copy the Linux binary as the Mac binary was
+  # very old and likely no one was really using it (at least temporarily).
   ISTIO_ENVOY_NATIVE_PATH=${ISTIO_ENVOY_LINUX_RELEASE_PATH}
 fi
 
