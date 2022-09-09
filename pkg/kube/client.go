588f58179cae4cb883a6abe6ff9e596f54367bc3
John Howard <howardjohn@google.com>
2022-09-09 07:49:44 -0700

    Fix delete opts

--- pkg/kube/client.go
+++ pkg/kube/client.go
@@ -1020,12 +1020,19 @@ func (c *client) applyYAMLFile(namespace string, dryRun bool, file string) error
 			return err
 		}
 	}
-
+	dcmd := kubectlDelete.NewCmdDelete(c.clientFactory, streams)
 	opts.DeleteOptions = &kubectlDelete.DeleteOptions{
 		DynamicClient:   c.dynamic,
 		IOStreams:       streams,
 		FilenameOptions: flags.DeleteFlags.FileNameFlags.ToOptions(),
 	}
+	if err := opts.DeleteOptions.Complete(c.clientFactory, nil, dcmd); err != nil {
+		return fmt.Errorf("delete.Complete: %v", err)
+	}
+
+	// opts.DeleteOptions.DynamicClient = c.dynamic
+	// opts.DeleteOptions.IOStreams = streams
+	// opts.DeleteOptions.FilenameOptions = flags.DeleteFlags.FileNameFlags.ToOptions()
 
 	opts.OpenAPISchema, _ = c.clientFactory.OpenAPISchema()
 
@@ -1108,19 +1115,25 @@ func (c *client) deleteFile(namespace string, dryRun bool, file string) error {
 	fileOpts := resource.FilenameOptions{
 		Filenames: []string{file},
 	}
-
-	opts := kubectlDelete.DeleteOptions{
-		FilenameOptions:   fileOpts,
-		CascadingStrategy: metav1.DeletePropagationBackground,
-		GracePeriod:       -1,
-		IgnoreNotFound:    true,
-		WaitForDeletion:   true,
-		WarnClusterScope:  enforceNamespace,
-		DynamicClient:     c.dynamic,
-		DryRunVerifier:    resource.NewQueryParamVerifier(c.dynamic, c.discoveryClient, resource.QueryParamDryRun),
-
-		IOStreams: streams,
+	dcmd := kubectlDelete.NewCmdDelete(c.clientFactory, streams)
+	opts := &kubectlDelete.DeleteOptions{
+		DynamicClient:   c.dynamic,
+		IOStreams:       streams,
+		FilenameOptions: fileOpts,
 	}
+
+	if err := opts.Complete(c.clientFactory, nil, dcmd); err != nil {
+		return fmt.Errorf("delete.Complete: %v", err)
+	}
+
+	opts.CascadingStrategy = metav1.DeletePropagationBackground
+	opts.GracePeriod = -1
+	opts.IgnoreNotFound = true
+	opts.WaitForDeletion = true
+	opts.WarnClusterScope = enforceNamespace
+	opts.DynamicClient = c.dynamic
+	opts.DryRunVerifier = resource.NewQueryParamVerifier(c.dynamic, c.discoveryClient, resource.QueryParamDryRun)
+
 	if dryRun {
 		opts.DryRunStrategy = util.DryRunServer
 	}
