4b75ec9a6758d1d6bfbdf18d2eb64c735e7cc901
fatedier <fatedier@gmail.com>
2022-09-22 22:38:38 +0800

    do not change default idleTimeout for passthrough cluster

--- pilot/pkg/networking/core/v1alpha3/listener_test.go
+++ pilot/pkg/networking/core/v1alpha3/listener_test.go
@@ -2609,8 +2609,8 @@ func TestAppendListenerFallthroughRouteForCompleteListener(t *testing.T) {
 					},
 				},
 			},
-			hostname:    util.PassthroughCluster,
-			idleTimeout: durationpb.New(0 * time.Second),
+			hostname: util.PassthroughCluster,
+			// idleTimeout shouldn't be set, will use default value in envoy
 		},
 	}
 	for _, tt := range tests {
@@ -2636,6 +2636,9 @@ func TestAppendListenerFallthroughRouteForCompleteListener(t *testing.T) {
 			}
 			if tt.idleTimeout != nil && !reflect.DeepEqual(tcpProxy.IdleTimeout, tt.idleTimeout) {
 				t.Errorf("Expected IdleTimeout %s but got %s\n", tt.idleTimeout, tcpProxy.IdleTimeout)
+			}
+			if tt.idleTimeout == nil && tcpProxy.IdleTimeout != nil {
+				t.Errorf("Expected no IdleTimeout set, but got %s\n", tcpProxy.IdleTimeout.AsDuration())
 			}
 		})
 	}
