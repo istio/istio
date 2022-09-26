e7fba967b3f2cfaf24ae199fb7ce6cae9129780a
able.lv <able.lv@airwallex.com>
2022-09-27 01:00:27 +0800

    fix error in GetKubernetesVersion

--- pkg/kube/client.go
+++ pkg/kube/client.go
@@ -591,7 +591,7 @@ func (c *client) GetKubernetesVersion() (*kubeVersion.Info, error) {
 	}
 	// Initial attempt failed, retry on each call to this function
 	v, err := clientWithTimeout.Discovery().ServerVersion()
-	if err != nil {
+	if err == nil {
 		c.version = v
 	}
 	return c.version, err
