75e81a155ef150b8f6e1c669fc2abe003cbbf566
xiaopeng <hanxiaop8@outlook.com>
2022-09-27 14:50:38 +0800

    fix unit test

--- operator/cmd/mesh/profile-list_test.go
+++ operator/cmd/mesh/profile-list_test.go
@@ -16,11 +16,14 @@ package mesh
 
 import (
 	"bytes"
+	"os"
+	"os/exec"
 	"path/filepath"
 	"testing"
 
 	"github.com/onsi/gomega"
-
+	"istio.io/istio/operator/pkg/util/httpserver"
+	"istio.io/istio/operator/pkg/util/tgz"
 	"istio.io/istio/pkg/test/env"
 )
 
@@ -46,14 +49,33 @@ func TestProfileList(t *testing.T) {
 
 func TestProfileListByurl(t *testing.T) {
 	g := gomega.NewWithT(t)
-	args := []string{"profile", "list", "--dry-run", "--manifests", "https://github.com/istio/istio/releases/download/1.15.0/istio-1.15.0-linux-amd64.tar.gz"}
+	tempChars, err := os.MkdirTemp("/tmp/", "profilelist-*")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tempChars)
+	manifestsPath := filepath.Join(tempChars, "istio", "istio-1.15.0", "manifests")
+	mkCmd := exec.Command("mkdir", "-p", manifestsPath)
+	if err = mkCmd.Run(); err != nil {
+		t.Fatal(err)
+	}
+	cpCmd := exec.Command("cp", "-r", string(liveCharts), manifestsPath)
+	if err = cpCmd.Run(); err != nil {
+		t.Fatal(err)
+	}
+	if err = tgz.Create(filepath.Join(tempChars, "istio"), filepath.Join(tempChars, "istio-1.15.0-linux.tar.gz")); err != nil {
+		t.Fatal(err)
+	}
+	svr := httpserver.NewServer(tempChars)
+	defer svr.Close()
+	args := []string{"profile", "list", "--dry-run", "--manifests", svr.URL() + "/" + "istio-1.15.0-linux.tar.gz"}
 
 	rootCmd := GetRootCmd(args)
 	var out bytes.Buffer
 	rootCmd.SetOut(&out)
 	rootCmd.SetErr(&out)
 
-	err := rootCmd.Execute()
+	err = rootCmd.Execute()
 	if err != nil {
 		t.Fatalf("failed to execute istioctl profile command: %v", err)
 	}
