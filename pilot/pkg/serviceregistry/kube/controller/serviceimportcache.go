663b570ad2db8fe6c52b2310312238fc49ce5172
Zhonghu Xu <xuzhonghu@huawei.com>
2022-09-29 11:10:37 +0800

    lint

--- pilot/pkg/serviceregistry/kube/controller/serviceimportcache.go
+++ pilot/pkg/serviceregistry/kube/controller/serviceimportcache.go
@@ -21,7 +21,6 @@ import (
 	"strings"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/cache"
