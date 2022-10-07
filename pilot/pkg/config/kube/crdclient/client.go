a4b970178d0c2c2ec6b20ec0a1f7889ec377ab3e
John Howard <howardjohn@google.com>
2022-10-06 19:11:24 -0700

    Fix issue with GVK in event handlers

--- pilot/pkg/config/kube/crdclient/client.go
+++ pilot/pkg/config/kube/crdclient/client.go
@@ -273,7 +273,7 @@ func (cl *Client) SyncAll() {
 	cl.beginSync.Store(true)
 	wg := sync.WaitGroup{}
 	for _, h := range cl.allKinds() {
-		handlers := cl.handlers[h.clusterGvk]
+		handlers := cl.handlers[h.preferredGvk]
 		if len(handlers) == 0 {
 			continue
 		}
@@ -288,7 +288,7 @@ func (cl *Client) SyncAll() {
 					scope.Warnf("New Object can not be converted to runtime Object %v, is type %T", object, object)
 					continue
 				}
-				currConfig := TranslateObject(currItem, h.clusterGvk, h.client.domainSuffix)
+				currConfig := TranslateObject(currItem, h.preferredGvk, h.client.domainSuffix)
 				for _, f := range handlers {
 					f(config.Config{}, currConfig, model.EventAdd)
 				}
