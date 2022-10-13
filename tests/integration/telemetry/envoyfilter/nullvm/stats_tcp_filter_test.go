bfffa73ee5fd97c835b0929573f4510116300647
zirain <hejianpeng2@huawei.com>
2022-10-14 00:30:22 +0800

    telemetry e2e: move most of code out of `stats/prometheus` (#40799)
    * move util_prometheus to util
    
    Signed-off-by: hejianpeng <hejianpeng2@huawei.com>
    
    * move customizemetrics to envoyfilter
    
    Signed-off-by: hejianpeng <hejianpeng2@huawei.com>
    
    * move test out of stats/prometheus
    
    Signed-off-by: hejianpeng <hejianpeng2@huawei.com>
    
    * testdata path error
    
    * rename tests/integration/telemetry/stats/prometheus/stats.go to tests/integration/telemetry/common/stats.go
    
    * fix lint
    
    * fix lint
    
    Signed-off-by: hejianpeng <hejianpeng2@huawei.com>

--- tests/integration/telemetry/envoyfilter/nullvm/stats_tcp_filter_test.go
+++ tests/integration/telemetry/envoyfilter/nullvm/stats_tcp_filter_test.go
@@ -15,18 +15,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package wasm
+package nullvm
 
 import (
 	"testing"
 
-	common "istio.io/istio/tests/integration/telemetry/stats/prometheus"
+	"istio.io/istio/tests/integration/telemetry/common"
 )
 
-func TestWASMTcpMetric(t *testing.T) { // nolint:interfacer
+func TestTcpMetric(t *testing.T) {
 	common.TestStatsTCPFilter(t, "observability.telemetry.stats.prometheus.tcp")
 }
 
-func TestWASMGatewayTCP(t *testing.T) {
+func TestGatewayTCP(t *testing.T) {
 	common.TestStatsGatewayServerTCPFilter(t, "observability.telemetry.stats.prometheus.tcp")
 }
