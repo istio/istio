eacfa4ed278613631fed8c4df4565f658e2d0017
qiuqunfeng <qunfengqiu@gmail.com>
2022-10-19 00:52:23 +0800

    fix lint err

--- security/pkg/pki/ca/ca.go
+++ security/pkg/pki/ca/ca.go
@@ -57,7 +57,7 @@ const (
 	TLSSecretRootCertFile = "ca.crt"
 	// The standard key size to use when generating an RSA private key
 	rsaKeySize = 2048
-	//ExternalCASecret stores the key/cert of self-signed CA from external Istiod.
+	// ExternalCASecret stores the key/cert of self-signed CA from external Istiod.
 	ExternalCASecret = "cacerts"
 )
 
