c188c57b76643fde0ca549273f917ce86273843a
Eric Van Norman <ericvn@us.ibm.com>
2022-10-20 16:35:49 -0500

    Manual make update-common

--- common/scripts/setup_env.sh
+++ common/scripts/setup_env.sh
@@ -73,7 +73,7 @@ fi
 
 # Build image to use
 if [[ "${IMAGE_VERSION:-}" == "" ]]; then
-  IMAGE_VERSION=master-6363b97c6bc60b7a3a8d52c5e08322d8394149e2
+  IMAGE_VERSION=release-1.16-dea5a1a3719b9ef5963569304e2210e0c92a5ce4
 fi
 if [[ "${IMAGE_NAME:-}" == "" ]]; then
   IMAGE_NAME=build-tools
