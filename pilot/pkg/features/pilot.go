7d040f930232da433b13efdbefbbc74cf44db39d
Faseela K <faseela.k@est.tech>
2022-11-04 10:22:30 +0100

    Default value for validationwebhookconfig seems incorrect
    Signed-off-by: Faseela K <faseela.k@est.tech>

--- pilot/pkg/features/pilot.go
+++ pilot/pkg/features/pilot.go
@@ -448,7 +448,7 @@ var (
 	InjectionWebhookConfigName = env.Register("INJECTION_WEBHOOK_CONFIG_NAME", "istio-sidecar-injector",
 		"Name of the mutatingwebhookconfiguration to patch, if istioctl is not used.").Get()
 
-	ValidationWebhookConfigName = env.Register("VALIDATION_WEBHOOK_CONFIG_NAME", "istio-istio-system",
+	ValidationWebhookConfigName = env.Register("VALIDATION_WEBHOOK_CONFIG_NAME", "istio-validator-istio-system",
 		"Name of the validatingwebhookconfiguration to patch. Empty will skip using cluster admin to patch.").Get()
 
 	SpiffeBundleEndpoints = env.Register("SPIFFE_BUNDLE_ENDPOINTS", "",
