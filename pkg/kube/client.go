f5a4d3221736b534fa7ab29f62dca031f28b4374
Zhonghu Xu <xuzhonghu@huawei.com>
2022-11-08 12:26:43 +0800

    Fix GetKubernetesVersion

--- pkg/kube/client.go
+++ pkg/kube/client.go
@@ -552,7 +552,7 @@ func (c *client) GetKubernetesVersion() (*kubeVersion.Info, error) {
 	}
 	// Initial attempt failed, retry on each call to this function
 	v, err := c.Discovery().ServerVersion()
-	if err != nil {
+	if err == nil {
 		c.version = v
 	}
 	return c.version, err
