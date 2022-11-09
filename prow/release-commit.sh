81f138f90705ca7f32fe4e0d5cc720af4d85ebba
jacob-delgado <jacob.delgado@volunteers.acasi.info>
2022-11-09 12:19:44 -0700

    run UPDATE_BRANCH=release-1.15 ./bin/update_deps.sh (#41890)

--- prow/release-commit.sh
+++ prow/release-commit.sh
@@ -36,7 +36,7 @@ DOCKER_HUB=${DOCKER_HUB:-gcr.io/istio-testing}
 GCS_BUCKET=${GCS_BUCKET:-istio-build/dev}
 
 # Use a pinned version in case breaking changes are needed
-BUILDER_SHA=afa9dcb38ad78133d7aaeed1ec5b6612edeae552
+BUILDER_SHA=bc695912a68da1b1cb3ee4843ea8320931212545
 
 # Reference to the next minor version of Istio
 # This will create a version like 1.4-alpha.sha
