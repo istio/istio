791648f73e05ab85cb358a56d81c244da489b466
cobb-tx <33242758+cobb-tx@users.noreply.github.com>
2022-11-11 04:37:08 +0800

    feat:clean-iptables consider InboundInterceptionMode (#41431)
    * feat:clean-iptables case InboundInterceptionMode
    
    Signed-off-by: cobb-tx <464693850@qq.com>
    
    * feat:clean-iptables case InboundInterceptionMode
    
    Signed-off-by: cobb-tx <464693850@qq.com>
    
    * feat:clean-iptables case InboundInterceptionMode
    
    Signed-off-by: cobb-tx <464693850@qq.com>
    
    * feat:clean-iptables case InboundInterceptionMode
    
    Signed-off-by: cobb-tx <464693850@qq.com>
    
    * feat:clean-iptables case InboundInterceptionMode
    
    Signed-off-by: cobb-tx <464693850@qq.com>
    
    * feat:clean-iptables case InboundInterceptionMode
    
    Signed-off-by: cobb-tx <464693850@qq.com>
    
    * feat:clean-iptables case InboundInterceptionMode
    
    Signed-off-by: cobb-tx <464693850@qq.com>
    
    * feat:clean-iptables case InboundInterceptionMode
    
    Signed-off-by: cobb-tx <464693850@qq.com>
    
    * Update releasenotes/notes/41431.yaml
    
    Signed-off-by: cobb-tx <464693850@qq.com>
    Co-authored-by: John Howard <howardjohn@google.com>

--- tools/istio-clean-iptables/pkg/cmd/root.go
+++ tools/istio-clean-iptables/pkg/cmd/root.go
@@ -57,13 +57,15 @@ var rootCmd = &cobra.Command{
 
 func constructConfig() *config.Config {
 	cfg := &config.Config{
-		DryRun:             viper.GetBool(constants.DryRun),
-		ProxyUID:           viper.GetString(constants.ProxyUID),
-		ProxyGID:           viper.GetString(constants.ProxyGID),
-		RedirectDNS:        viper.GetBool(constants.RedirectDNS),
-		CaptureAllDNS:      viper.GetBool(constants.CaptureAllDNS),
-		OwnerGroupsInclude: viper.GetString(constants.OwnerGroupsInclude.Name),
-		OwnerGroupsExclude: viper.GetString(constants.OwnerGroupsExclude.Name),
+		DryRun:                  viper.GetBool(constants.DryRun),
+		ProxyUID:                viper.GetString(constants.ProxyUID),
+		ProxyGID:                viper.GetString(constants.ProxyGID),
+		RedirectDNS:             viper.GetBool(constants.RedirectDNS),
+		CaptureAllDNS:           viper.GetBool(constants.CaptureAllDNS),
+		OwnerGroupsInclude:      viper.GetString(constants.OwnerGroupsInclude.Name),
+		OwnerGroupsExclude:      viper.GetString(constants.OwnerGroupsExclude.Name),
+		InboundInterceptionMode: viper.GetString(constants.IstioInboundInterceptionMode.Name),
+		InboundTProxyMark:       viper.GetString(constants.IstioInboundTproxyMark.Name),
 	}
 
 	// TODO: Make this more configurable, maybe with an allowlist of users to be captured for output instead of a denylist.
@@ -133,6 +135,16 @@ func bindFlags(cmd *cobra.Command, args []string) {
 		handleError(err)
 	}
 	viper.SetDefault(constants.OwnerGroupsExclude.Name, constants.OwnerGroupsExclude.DefaultValue)
+
+	if err := viper.BindEnv(constants.IstioInboundInterceptionMode.Name); err != nil {
+		handleError(err)
+	}
+	viper.SetDefault(constants.IstioInboundInterceptionMode.Name, constants.IstioInboundInterceptionMode.DefaultValue)
+
+	if err := viper.BindEnv(constants.IstioInboundTproxyMark.Name); err != nil {
+		handleError(err)
+	}
+	viper.SetDefault(constants.IstioInboundTproxyMark.Name, constants.IstioInboundTproxyMark.DefaultValue)
 }
 
 // https://github.com/spf13/viper/issues/233.
@@ -148,6 +160,11 @@ func init() {
 		"Specify the GID of the user for which the redirection is not applied. (same default value as -u param)")
 
 	rootCmd.Flags().Bool(constants.RedirectDNS, dnsCaptureByAgent, "Enable capture of dns traffic by istio-agent")
+
+	rootCmd.Flags().StringP(constants.InboundInterceptionMode, "m", "",
+		"The mode used to redirect inbound connections to Envoy, either \"REDIRECT\" or \"TPROXY\"")
+
+	rootCmd.Flags().StringP(constants.InboundTProxyMark, "t", "", "")
 }
 
 func GetCommand() *cobra.Command {
