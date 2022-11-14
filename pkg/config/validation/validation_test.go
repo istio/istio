fc92ce952ec8e4f579b732bd7094de52ed023794
Faseela K <faseela.k@est.tech>
2022-11-14 10:55:49 +0100

    Fix unit test failures
    Signed-off-by: Faseela K <faseela.k@est.tech>

--- pkg/config/validation/validation_test.go
+++ pkg/config/validation/validation_test.go
@@ -1530,7 +1530,7 @@ func TestValidateTlsOptions(t *testing.T) {
 			&networking.ServerTLSSettings{
 				Mode:           networking.ServerTLSSettings_SIMPLE,
 				CredentialName: "sds-name",
-				CipherSuites:   []string{"P-256"},
+				EcdhCurves:     []string{"P-256"},
 			},
 			"", "",
 		},
@@ -1539,7 +1539,7 @@ func TestValidateTlsOptions(t *testing.T) {
 			&networking.ServerTLSSettings{
 				Mode:           networking.ServerTLSSettings_SIMPLE,
 				CredentialName: "sds-name",
-				CipherSuites:   []string{"P-256", "P-256"},
+				EcdhCurves:     []string{"P-256", "P-256"},
 			},
 			"", "P-256",
 		},
