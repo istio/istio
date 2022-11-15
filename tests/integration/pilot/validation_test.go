83fa07462a67c7cf3b8a372dc9a9b72789f036d6
John Howard <howardjohn@google.com>
2022-11-15 08:52:46 -0800

    fix tests

--- tests/integration/pilot/validation_test.go
+++ tests/integration/pilot/validation_test.go
@@ -85,11 +85,6 @@ func TestValidation(t *testing.T) {
 			}
 
 			for _, cluster := range t.Clusters().Configs() {
-
-				ns := namespace.NewOrFail(t, t, namespace.Config{
-					Prefix: "validation",
-				})
-
 				for i := range dataset {
 					d := dataset[i]
 
@@ -103,6 +98,10 @@ func TestValidation(t *testing.T) {
 						if err != nil {
 							t.Fatalf("Unable to load test data: %v", err)
 						}
+
+						ns := namespace.NewOrFail(t, t, namespace.Config{
+							Prefix: "validation",
+						})
 
 						applyFiles := t.WriteYAMLOrFail(t, "apply", ym)
 						dryRunErr := cluster.ApplyYAMLFilesDryRun(ns.Name(), applyFiles...)
