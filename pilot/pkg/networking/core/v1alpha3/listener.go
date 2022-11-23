4a37d9852b44073bb0a6f7741c8468642d82fc41
John Howard <howardjohn@google.com>
2022-11-23 07:13:53 -0800

    fix test

--- pilot/pkg/networking/core/v1alpha3/listener.go
+++ pilot/pkg/networking/core/v1alpha3/listener.go
@@ -129,8 +129,15 @@ func BuildListenerTLSContext(serverTLSSettings *networking.ServerTLSSettings,
 		},
 	}
 
-	// TODO(https://github.com/envoyproxy/envoy/issues/23809) support this in Envoy
 	ctx.RequireClientCertificate = proto.BoolFalse
+	if serverTLSSettings.Mode == networking.ServerTLSSettings_MUTUAL ||
+		serverTLSSettings.Mode == networking.ServerTLSSettings_ISTIO_MUTUAL {
+		ctx.RequireClientCertificate = proto.BoolTrue
+	}
+	if transportProtocol == istionetworking.TransportProtocolQUIC {
+		// TODO(https://github.com/envoyproxy/envoy/issues/23809) support this in Envoy
+		ctx.RequireClientCertificate = proto.BoolFalse
+	}
 	credentialSocketExist := false
 	if proxy.Metadata != nil && proxy.Metadata.Raw[secconst.CredentialMetaDataName] == "true" {
 		credentialSocketExist = true
