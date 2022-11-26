4e2ab1671001189a14b76c3788b4591b2718d287
hejianpeng <hejianpeng2@huawei.com>
2022-11-26 17:38:00 +0800

    revert

--- pkg/wasm/cache.go
+++ pkg/wasm/cache.go
@@ -444,9 +444,7 @@ func (c *LocalFileCache) purge() {
 					for downloadURL := range m.referencingURLs {
 						delete(c.checksums, downloadURL)
 					}
-					c.mux.Lock()
 					delete(c.modules, k)
-					c.mux.Unlock()
 					wasmLog.Debugf("successfully removed stale Wasm module %v", m.modulePath)
 				}
 			}
