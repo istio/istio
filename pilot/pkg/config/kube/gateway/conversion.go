bd0ee076c629b2110c1841242ce2ab02cd78fea9
Frank Budinsky <frankb@ca.ibm.com>
2022-11-28 11:32:11 -0500

    gateway-api: fix hostname gateway addresses (#42125)
    * gateway-api: fix hostname gateway addresses
    
    * tweak naming
    
    * add service type
    
    * fix tests
    
    * address review comments
    
    * lint

--- pilot/pkg/config/kube/gateway/conversion.go
+++ pilot/pkg/config/kube/gateway/conversion.go
@@ -16,6 +16,7 @@ package gateway
 
 import (
 	"fmt"
+	"net"
 	"sort"
 	"strings"
 
@@ -1418,17 +1419,17 @@ func convertGateways(r ConfigContext) ([]config.Config, map[parentKey]map[k8s.Se
 		}
 
 		// TODO: we lose address if servers is empty due to an error
-		internal, external, warnings := r.Context.ResolveGatewayInstances(obj.Namespace, gatewayServices, servers)
+		internal, external, pending, warnings := r.Context.ResolveGatewayInstances(obj.Namespace, gatewayServices, servers)
 		if len(skippedAddresses) > 0 {
 			warnings = append(warnings, fmt.Sprintf("Only Hostname is supported, ignoring %v", skippedAddresses))
 		}
 		if len(warnings) > 0 {
 			var msg string
-			if len(internal) > 0 {
+			if len(internal) != 0 {
 				msg = fmt.Sprintf("Assigned to service(s) %s, but failed to assign to all requested addresses: %s",
 					humanReadableJoin(internal), strings.Join(warnings, "; "))
 			} else {
-				msg = fmt.Sprintf("failed to assign to any requested addresses: %s", strings.Join(warnings, "; "))
+				msg = fmt.Sprintf("Failed to assign to any requested addresses: %s", strings.Join(warnings, "; "))
 			}
 			gatewayConditions[string(k8s.GatewayConditionReady)].error = &ConfigError{
 				Reason:  string(k8s.GatewayReasonAddressNotAssigned),
@@ -1449,8 +1450,13 @@ func convertGateways(r ConfigContext) ([]config.Config, map[parentKey]map[k8s.Se
 			if len(addressesToReport) == 0 {
 				// There are no external addresses, so report the internal ones
 				// TODO: should we always report both?
-				addressesToReport = internal
 				addrType = k8s.HostnameAddressType
+				for _, hostport := range internal {
+					svchost, _, _ := net.SplitHostPort(hostport)
+					if !contains(pending, svchost) && !contains(addressesToReport, svchost) {
+						addressesToReport = append(addressesToReport, svchost)
+					}
+				}
 			}
 			gs.Addresses = make([]k8s.GatewayAddress, 0, len(addressesToReport))
 			for _, addr := range addressesToReport {
@@ -1795,6 +1801,15 @@ func humanReadableJoin(ss []string) string {
 	default:
 		return strings.Join(ss[:len(ss)-1], ", ") + ", and " + ss[len(ss)-1]
 	}
+}
+
+func contains(ss []string, s string) bool {
+	for _, str := range ss {
+		if str == s {
+			return true
+		}
+	}
+	return false
 }
 
 // NamespaceNameLabel represents that label added automatically to namespaces is newer Kubernetes clusters
