467f9ba4e18d2c5a55d13756ca9d6d0800d9d26b
Faseela K <k.faseela@gmail.com>
2022-12-07 18:30:15 +0100

    Revert "[release-1.16] Fix ENABLE_ENHANCED_RESOURCE_SCOPING bug (#42188) (#42333)" (#42345)
    This reverts commit aac29c4d92c637f7e32347918a134915f7b21f00.

--- pilot/pkg/serviceregistry/kube/controller/controller.go
+++ pilot/pkg/serviceregistry/kube/controller/controller.go
@@ -233,8 +233,7 @@ type Controller struct {
 	imports serviceImportCache
 	pods    *PodCache
 
-	handlers                   model.ControllerHandlers
-	namespaceDiscoveryHandlers []func(ns string, event model.Event)
+	handlers model.ControllerHandlers
 
 	// This is only used for test
 	stop chan struct{}
@@ -1405,9 +1404,4 @@ func (c *Controller) AppendServiceHandler(f func(*model.Service, model.Event)) {
 // AppendWorkloadHandler implements a service catalog operation
 func (c *Controller) AppendWorkloadHandler(f func(*model.WorkloadInstance, model.Event)) {
 	c.handlers.AppendWorkloadHandler(f)
-}
-
-// AppendNamespaceDiscoveryHandlers register handlers on namespace selected/deselected by discovery selectors change.
-func (c *Controller) AppendNamespaceDiscoveryHandlers(f func(string, model.Event)) {
-	c.namespaceDiscoveryHandlers = append(c.namespaceDiscoveryHandlers, f)
 }
