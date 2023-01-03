9c6e00dfb33777a0708aae6cc941bc161bbe0836
Istio Automation <istio-testing-bot@google.com>
2023-01-03 07:09:35 -0800

    [release-1.15] istioctl analyze revision (#42552)
    * istioctl analyze revision
    
    * added issue
    
    * Update releasenotes/notes/istioctl-analyze-revision.yaml
    
    Co-authored-by: Eric Van Norman <ericvn@us.ibm.com>
    
    Co-authored-by: Nick Nellis <neni0701@gmail.com>
    Co-authored-by: Nick <nick.nellis@solo.io>
    Co-authored-by: Eric Van Norman <ericvn@us.ibm.com>

--- istioctl/cmd/analyze.go
+++ istioctl/cmd/analyze.go
@@ -86,6 +86,9 @@ func Analyze() *cobra.Command {
 		Example: `  # Analyze the current live cluster
   istioctl analyze
 
+  # Analyze the current live cluster for a specific revision
+  istioctl analyze --revision 1-16
+
   # Analyze the current live cluster, simulating the effect of applying additional yaml files
   istioctl analyze a.yaml b.yaml my-app-config/
 
@@ -188,7 +191,7 @@ func Analyze() *cobra.Command {
 				if err != nil {
 					return err
 				}
-				sa.AddRunningKubeSource(k)
+				sa.AddRunningKubeSourceWithRevision(k, revision)
 			}
 
 			// If we explicitly specify mesh config, use it.
@@ -316,6 +319,8 @@ func Analyze() *cobra.Command {
 		"Process directory arguments recursively. Useful when you want to analyze related manifests organized within the same directory.")
 	analysisCmd.PersistentFlags().BoolVar(&ignoreUnknown, "ignore-unknown", false,
 		"Don't complain about un-parseable input documents, for cases where analyze should run only on k8s compliant inputs.")
+	analysisCmd.PersistentFlags().StringVarP(&revision, "revision", "", "default",
+		"analyze a specific revision deployed.")
 	return analysisCmd
 }
 
