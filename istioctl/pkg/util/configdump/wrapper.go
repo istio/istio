45154cc61469ef9ea06eb432811ab076b980f8f9
Yossi Mesika <yossi.mesika@solo.io>
2023-01-14 04:41:27 +0200

    Dependency guard jsonpb and protojson packages (#42760)
    * Dependency guard jsonpb and protojson packages
    
    * Using protojson for handling global types
    
    * Lint
    
    * Minor text changes

--- istioctl/pkg/util/configdump/wrapper.go
+++ istioctl/pkg/util/configdump/wrapper.go
@@ -15,15 +15,15 @@
 package configdump
 
 import (
-	"bytes"
 	"reflect"
 	"strings"
 
 	admin "github.com/envoyproxy/go-control-plane/envoy/admin/v3"
-	"github.com/golang/protobuf/jsonpb"
 	legacyproto "github.com/golang/protobuf/proto" // nolint: staticcheck
 	emptypb "github.com/golang/protobuf/ptypes/empty"
 	exprpb "google.golang.org/genproto/googleapis/api/expr/v1alpha1"
+
+	"istio.io/istio/pkg/util/protomarshal"
 )
 
 // nonstrictResolver is an AnyResolver that ignores unknown proto messages
@@ -54,21 +54,13 @@ type Wrapper struct {
 
 // MarshalJSON is a custom marshaller to handle protobuf pain
 func (w *Wrapper) MarshalJSON() ([]byte, error) {
-	buffer := &bytes.Buffer{}
-	err := (&jsonpb.Marshaler{}).Marshal(buffer, w)
-	if err != nil {
-		return nil, err
-	}
-	return buffer.Bytes(), nil
+	return protomarshal.Marshal(w)
 }
 
 // UnmarshalJSON is a custom unmarshaller to handle protobuf pain
 func (w *Wrapper) UnmarshalJSON(b []byte) error {
 	cd := &admin.ConfigDump{}
-	err := (&jsonpb.Unmarshaler{
-		AllowUnknownFields: true,
-		AnyResolver:        &envoyResolver,
-	}).Unmarshal(bytes.NewReader(b), cd)
+	err := protomarshal.UnmarshalAllowUnknownWithAnyResolver(&envoyResolver, b, cd)
 	*w = Wrapper{cd}
 	return err
 }
