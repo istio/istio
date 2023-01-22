7061ef8d5625ad7ca34f9cf012f5ff64bd1534e3
Rama Chavali <rama.rao@salesforce.com>
2023-01-22 14:13:25 +0530

    use authn filter constant every where
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/security/model/authentication.go
+++ pilot/pkg/security/model/authentication.go
@@ -56,11 +56,6 @@ const (
 	// EnvoyJwtFilterName is the name of the Envoy JWT filter. This should be the same as the name defined
 	// in https://github.com/envoyproxy/envoy/blob/v1.9.1/source/extensions/filters/http/well_known_names.h#L48
 	EnvoyJwtFilterName = "envoy.filters.http.jwt_authn"
-
-	// AuthnFilterName is the name for the Istio AuthN filter. This should be the same
-	// as the name defined in
-	// https://github.com/istio/proxy/blob/master/src/envoy/http/authn/http_filter_factory.cc#L30
-	AuthnFilterName = "istio_authn"
 )
 
 var SDSAdsConfig = &core.ConfigSource{
