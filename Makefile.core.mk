a7b3176da81c00e076c469077e8e9b432145a05a
Istio Automation <istio-testing-bot@google.com>
2023-01-25 19:06:33 +0000

    Update BASE_VERSION to 1.16-2023-01-25T19-01-45

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -38,7 +38,7 @@ SHELL := /bin/bash -o pipefail
 export VERSION ?= 1.16-dev
 
 # Base version of Istio image to use
-BASE_VERSION ?= 1.16-2023-01-20T04-22-09
+BASE_VERSION ?= 1.16-2023-01-25T19-01-45
 
 export GO111MODULE ?= on
 export GOPROXY ?= https://proxy.golang.org
