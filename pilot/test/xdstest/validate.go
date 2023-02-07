e12fafbf90452ef6469c7582c6799790b88f48f3
Cecilia Bernardi <cbernardi@expediagroup.com>
2023-02-07 11:47:52 +0100

    linting imports

--- pilot/test/xdstest/validate.go
+++ pilot/test/xdstest/validate.go
@@ -15,14 +15,16 @@
 package xdstest
 
 import (
+	"strings"
+	"testing"
+
 	cluster "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
 	endpoint "github.com/envoyproxy/go-control-plane/envoy/config/endpoint/v3"
 	listener "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
 	route "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
+
 	xdsfilters "istio.io/istio/pilot/pkg/xds/filters"
 	"istio.io/istio/pkg/util/sets"
-	"strings"
-	"testing"
 )
 
 func ValidateListeners(t testing.TB, ls []*listener.Listener) {
