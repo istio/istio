e6b2e0982390e44425a68b21cbe416140ba565de
Jackie Elliott <64559656+jaellio@users.noreply.github.com>
2023-02-07 15:57:35 -0800

    Fix Custom CA Integration using K8s CSR (#42980)
    * Updates SignWithCertChain to check MeshConfig for the root ca
    before the cert chain. Also, updated the error handling when
    FindRootCertFromCertificateChainBytes. SignWithCertChain should
    not return an error when FindRootCertFromCertificateChainBytes
    fails.
    
    Signed-off-by: jaellio <jaellio@microsoft.com>
    
    * Add unit tests
    
    Signed-off-by: jaellio <jaellio@microsoft.com>
    
    * Revert ordering of MeshConfig and cert chain check.
    
    Signed-off-by: jaellio <jaellio@microsoft.com>
    
    * Add leaf cert file generation to generate-workload.go and update
    unit tests.
    
    Signed-off-by: jaellio <jaellio@microsoft.com>
    
    * Update leaf cert test to expect fail
    
    Signed-off-by: jaellio <jaellio@microsoft.com>
    
    * Fix small typos
    
    Signed-off-by: jaellio <jaellio@microsoft.com>
    
    * Adds additional unit test with root and intermediate CA in the
    MeshConfig and the leaf in the cert chain.
    
    Signed-off-by: jaellio <jaellio@microsoft.com>
    
    * Adds period and workload name to leaf cert description
    
    Signed-off-by: jaellio <jaellio@microsoft.com>
    
    ---------
    
    Signed-off-by: jaellio <jaellio@microsoft.com>

--- security/pkg/pki/ra/k8s_ra.go
+++ security/pkg/pki/ra/k8s_ra.go
@@ -127,7 +127,7 @@ func (r *KubernetesRA) SignWithCertChain(csrPEM []byte, certOpts ca.CertOpts) ([
 	if len(r.GetCAKeyCertBundle().GetRootCertPem()) == 0 {
 		rootCertFromCertChain, err = util.FindRootCertFromCertificateChainBytes(cert)
 		if err != nil {
-			return nil, raerror.NewError(raerror.CSRError, fmt.Errorf("failed to find root cert from signed cert-chain (%v)", err.Error()))
+			pkiRaLog.Infof("failed to find root cert from signed cert-chain (%v)", err.Error())
 		}
 		rootCertFromMeshConfig, err = r.GetRootCertFromMeshConfig(certSigner)
 		if err != nil {
@@ -142,7 +142,7 @@ func (r *KubernetesRA) SignWithCertChain(csrPEM []byte, certOpts ca.CertOpts) ([
 			return nil, raerror.NewError(raerror.CSRError, fmt.Errorf("failed to find root cert from either signed cert-chain or mesh config"))
 		}
 		if verifyErr := util.VerifyCertificate(nil, cert, possibleRootCert, nil); verifyErr != nil {
-			return nil, raerror.NewError(raerror.CSRError, fmt.Errorf("root cert from signed cert-chain is invalid %v ", verifyErr))
+			return nil, raerror.NewError(raerror.CSRError, fmt.Errorf("root cert from signed cert-chain is invalid (%v)", verifyErr))
 		}
 		if !bytes.Equal(possibleRootCert, rootCertFromCertChain) {
 			respCertChain = append(respCertChain, string(possibleRootCert))
