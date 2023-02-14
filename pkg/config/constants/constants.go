8951ac382c28225e8d5f632a8da6dc4115180566
John Howard <howardjohn@google.com>
2023-02-14 09:26:13 -0800

    gen

--- pkg/config/constants/constants.go
+++ pkg/config/constants/constants.go
@@ -151,7 +151,7 @@ const (
 	// load balancer, such as an Istio Gateway, is terminating the TLS.
 	CertProviderNone = "none"
 
-	GatewayNameLabel             = "istio.io/gateway-name"
+	GatewayNameLabel                  = "istio.io/gateway-name"
 	ManagedGatewayLabel               = "gateway.istio.io/managed"
 	WaypointServiceAccount            = "istio.io/for-service-account"
 	WaypointGatewayClassName          = "istio-waypoint"
