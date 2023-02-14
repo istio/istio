75d8f0ae166a672a996584cdcf964fa71eaf5a36
dwq <41563853+dddddai@users.noreply.github.com>
2023-02-14 22:40:42 +0800

    NDS: merge name info by hostname (#43152)
    * merge name info by hostname
    
    * fix
    
    * add a test case
    
    * add a link

--- pkg/dns/server/name_table.go
+++ pkg/dns/server/name_table.go
@@ -114,21 +114,37 @@ func BuildNameTable(cfg Config) *dnsProto.NameTable {
 			}
 		}
 
-		nameInfo := &dnsProto.NameTable_NameInfo{
-			Ips:      addressList,
-			Registry: string(svc.Attributes.ServiceRegistry),
+		if ni, f := out.Table[hostName.String()]; !f {
+			nameInfo := &dnsProto.NameTable_NameInfo{
+				Ips:      addressList,
+				Registry: string(svc.Attributes.ServiceRegistry),
+			}
+			if svc.Attributes.ServiceRegistry == provider.Kubernetes &&
+				!strings.HasSuffix(hostName.String(), "."+constants.DefaultClusterSetLocalDomain) {
+				// The agent will take care of resolving a, a.ns, a.ns.svc, etc.
+				// No need to provide a DNS entry for each variant.
+				//
+				// NOTE: This is not done for Kubernetes Multi-Cluster Services (MCS) hosts, in order
+				// to avoid conflicting with the entries for the regular (cluster.local) service.
+				nameInfo.Namespace = svc.Attributes.Namespace
+				nameInfo.Shortname = svc.Attributes.Name
+			}
+			out.Table[hostName.String()] = nameInfo
+		} else if provider.ID(ni.Registry) != provider.Kubernetes {
+			// 2 possible cases:
+			// 1. If the SE has multiple addresses(vips) specified, merge the ips
+			// 2. If the previous SE is a decorator of the k8s service, give precedence to the k8s service
+			if svc.Attributes.ServiceRegistry == provider.Kubernetes {
+				ni.Ips = addressList
+				ni.Registry = string(provider.Kubernetes)
+				if !strings.HasSuffix(hostName.String(), "."+constants.DefaultClusterSetLocalDomain) {
+					ni.Namespace = svc.Attributes.Namespace
+					ni.Shortname = svc.Attributes.Name
+				}
+			} else {
+				ni.Ips = append(ni.Ips, addressList...)
+			}
 		}
-		if svc.Attributes.ServiceRegistry == provider.Kubernetes &&
-			!strings.HasSuffix(hostName.String(), "."+constants.DefaultClusterSetLocalDomain) {
-			// The agent will take care of resolving a, a.ns, a.ns.svc, etc.
-			// No need to provide a DNS entry for each variant.
-			//
-			// NOTE: This is not done for Kubernetes Multi-Cluster Services (MCS) hosts, in order
-			// to avoid conflicting with the entries for the regular (cluster.local) service.
-			nameInfo.Namespace = svc.Attributes.Namespace
-			nameInfo.Shortname = svc.Attributes.Name
-		}
-		out.Table[hostName.String()] = nameInfo
 	}
 	return out
 }
