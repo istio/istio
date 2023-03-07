dc999fcfd46631f03d4269ec3277c2e5eb23eff7
Xiaopeng Han <hanxiaop8@outlook.com>
2023-03-07 11:03:45 +0800

    [release-1.16] Manual cherry-pick of https://github.com/istio/istio/pull/43652 (#43683)
    * resolve conflict
    
    * add releasenotes
    
    * manual revise releasenotes

--- pkg/config/analysis/analyzers/analyzers_test.go
+++ pkg/config/analysis/analyzers/analyzers_test.go
@@ -366,6 +366,14 @@ var testGrid = []testCase{
 		expected:   []message{},
 	},
 	{
+		name:       "serviceWithNoSelector",
+		inputFiles: []string{"testdata/deployment-service-no-selector.yaml"},
+		analyzer:   &deployment.ServiceAssociationAnalyzer{},
+		expected: []message{
+			{msg.DeploymentRequiresServiceAssociated, "Deployment default/helloworld-v2"},
+		},
+	},
+	{
 		name: "regexes",
 		inputFiles: []string{
 			"testdata/virtualservice_regexes.yaml",
