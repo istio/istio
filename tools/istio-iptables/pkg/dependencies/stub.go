43048b8acd7ca199a5dc160ed01991ecd5e74b96
Marko Lukša <marko.luksa@gmail.com>
2023-03-15 19:59:19 +0100

    Remove OutputPath option from iptables

--- tools/istio-iptables/pkg/dependencies/stub.go
+++ tools/istio-iptables/pkg/dependencies/stub.go
@@ -15,11 +15,16 @@
 package dependencies
 
 import (
+	"fmt"
 	"io"
+	"os"
 	"strings"
 
+	"istio.io/pkg/env"
 	"istio.io/pkg/log"
 )
+
+var DryRunFilePath = env.Register("DRY_RUN_FILE_PATH", "", "If provided, StdoutStubDependencies will write the input from stdin to the given file.")
 
 // StdoutStubDependencies implementation of interface Dependencies, which is used for testing
 type StdoutStubDependencies struct{}
@@ -27,15 +32,31 @@ type StdoutStubDependencies struct{}
 // RunOrFail runs a command and panics, if it fails
 func (s *StdoutStubDependencies) RunOrFail(cmd string, stdin io.ReadSeeker, args ...string) {
 	log.Infof("%s %s", cmd, strings.Join(args, " "))
+
+	path := DryRunFilePath.Get()
+	if path != "" {
+		// Print the input into the given output file.
+		f, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0o644)
+		if err != nil {
+			panic(fmt.Errorf("unable to open dry run output file %v: %v", path, err))
+		}
+
+		defer f.Close()
+		if stdin != nil {
+			if _, err = io.Copy(f, stdin); err != nil {
+				panic(fmt.Errorf("unable to write dry run output file: %v", err))
+			}
+		}
+	}
 }
 
 // Run runs a command
 func (s *StdoutStubDependencies) Run(cmd string, stdin io.ReadSeeker, args ...string) error {
-	log.Infof("%s %s", cmd, strings.Join(args, " "))
+	s.RunOrFail(cmd, stdin, args...)
 	return nil
 }
 
 // RunQuietlyAndIgnore runs a command quietly and ignores errors
 func (s *StdoutStubDependencies) RunQuietlyAndIgnore(cmd string, stdin io.ReadSeeker, args ...string) {
-	log.Infof("%s %s", cmd, strings.Join(args, " "))
+	s.RunOrFail(cmd, stdin, args...)
 }
