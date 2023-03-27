122053494433c52986b65b7e6a28cebbbc04bd32
John Howard <howardjohn@google.com>
2023-03-27 14:01:23 -0700

    litn

--- tools/commonfiles-postprocess.sh
+++ tools/commonfiles-postprocess.sh
@@ -14,5 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# shellcheck disable=SC2016
 yq ea '. as $item ireduce ({}; . *d $item )' ./common/config/.golangci.yml ./tools/golangci-override.yaml > ./common/config/.golangci.yml.tmp
 mv ./common/config/.golangci.yml.tmp ./common/config/.golangci.yml
