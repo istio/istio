1bf0b540cbe05ef18871f01d23112692a3be3886
John Howard <howardjohn@google.com>
2023-03-28 11:06:35 -0700

    lint

--- pilot/pkg/config/kube/gateway/conversion.go
+++ pilot/pkg/config/kube/gateway/conversion.go
@@ -1500,7 +1500,8 @@ func reportGatewayStatus(
 		},
 	}
 	if len(internal) > 0 {
-		gatewayConditions[string(k8sbeta.GatewayReasonProgrammed)].message = fmt.Sprintf("Resource programmed, assigned to service(s) %s", humanReadableJoin(internal))
+		msg := fmt.Sprintf("Resource programmed, assigned to service(s) %s", humanReadableJoin(internal))
+		gatewayConditions[string(k8sbeta.GatewayReasonProgrammed)].message = msg
 	}
 
 	if len(warnings) > 0 {
