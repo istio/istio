8ba9a1946738283416938670478cbf056e55f3c8
dddddai <dddwq@foxmail.com>
2023-04-03 10:23:32 +0800

    test retry period

--- pilot/pkg/model/network_test.go
+++ pilot/pkg/model/network_test.go
@@ -38,6 +38,7 @@ import (
 
 func TestGatewayHostnames(t *testing.T) {
 	test.SetForTest(t, &model.MinGatewayTTL, 30*time.Millisecond)
+	test.SetForTest(t, &model.DNSRetryPeriod, 30*time.Millisecond)
 
 	gwHost := "test.gw.istio.io"
 	workingDNSServer := newFakeDNSServer(":15353", 1, sets.New(gwHost))
