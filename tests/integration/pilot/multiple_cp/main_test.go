9abce9ee5320d93e6a4cea9d1410187c3e710dc1
Faseela K <faseela.k@est.tech>
2023-04-05 12:47:28 +0200

    add build tag
    Signed-off-by: Faseela K <faseela.k@est.tech>

--- tests/integration/pilot/multiple_cp/main_test.go
+++ tests/integration/pilot/multiple_cp/main_test.go
@@ -1,3 +1,6 @@
+//go:build integ
+// +build integ
+
 // Copyright Istio Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package revisions
+package multiple_cp
 
 import (
 	"fmt"
