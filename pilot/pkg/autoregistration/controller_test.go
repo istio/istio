432dc6e8ff36f7753ac4821c28e3954ca4f54103
Cecilia Bernardi <cbernardi@expediagroup.com>
2023-04-06 10:15:31 +0100

    added 5 more millis

--- pilot/pkg/autoregistration/controller_test.go
+++ pilot/pkg/autoregistration/controller_test.go
@@ -45,7 +45,7 @@ import (
 func init() {
 	features.WorkloadEntryAutoRegistration = true
 	features.WorkloadEntryHealthChecks = true
-	features.WorkloadEntryCleanupGracePeriod = 20 * time.Millisecond
+	features.WorkloadEntryCleanupGracePeriod = 25 * time.Millisecond
 }
 
 var (
