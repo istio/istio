142e51efbe36bfc8b888c154d1b6f30ba22cc1c9
Jacob Delgado <jacob.delgado@volunteers.acasi.info>
2023-04-11 18:07:13 +0000

    Update release prow

--- prow/release-commit.sh
+++ prow/release-commit.sh
@@ -36,7 +36,7 @@ DOCKER_HUB=${DOCKER_HUB:-gcr.io/istio-testing}
 GCS_BUCKET=${GCS_BUCKET:-istio-build/dev}
 
 # Use a pinned version in case breaking changes are needed
-BUILDER_SHA=7e53365ecb188b417c74f6daf69f410094d5a3fd
+BUILDER_SHA=49103b5e8fa57db9167608e529fb6c9080427d70
 
 # Reference to the next minor version of Istio
 # This will create a version like 1.4-alpha.sha
