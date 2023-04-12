33d43b2759f62d7fbf522828bd5061e0b8ccee66
Rajat Sharma <rajat.shrma94@gmail.com>
2023-04-12 16:06:46 +0200

    Fix ServiceEntry WorkloadInstanceHandler for label change in Pods (#42922)
    * Fix ServiceEntry WorkloadInstanceHandler for label change in Pods
    
    For a label change in WorkloadInstance, which results in a mismatch with a previously matching ServiceEntry, the whole update event is skipped. Which results in the WorkloadInstance not getting removed from the ServiceEntry.
    This fix changes the behaviour for such cases to handle those updates similar to a delete event and clean up the WorkloadInstance from the list.
    
    Fixes [#42921]
    
    * use DeepEquals to avoid SubsetOf iteration
    
    * gocritic linting fix
    
    * go back to the first approach
    
    * fix workloadInstanceDiff
    
    * fix linting errors
    
    * change test cases to table format
    
    * fix testcase for new Event type
    
    * fix Event Type
    
    * go fmt
    
    * remove wi diff

--- pilot/pkg/serviceregistry/serviceentry/controller_test.go
+++ pilot/pkg/serviceregistry/serviceentry/controller_test.go
@@ -1159,6 +1159,172 @@ func TestServiceDiscoveryWorkloadInstance(t *testing.T) {
 	})
 }
 
+func TestServiceDiscoveryWorkloadInstanceChangeLabel(t *testing.T) {
+	store, sd, events := initServiceDiscovery(t)
+
+	type expectedProxyInstances struct {
+		instancesWithSA []*model.ServiceInstance
+		address         string
+	}
+
+	type testWorkloadInstance struct {
+		name                   string
+		namespace              string
+		address                string
+		labels                 map[string]string
+		serviceAccount         string
+		tlsmode                string
+		expectedProxyInstances []expectedProxyInstances
+	}
+
+	t.Run("service entry", func(t *testing.T) {
+		// Add just the ServiceEntry with selector. We should see no instances
+		createConfigs([]*config.Config{selector}, store, t)
+		instances := []*model.ServiceInstance{}
+		expectProxyInstances(t, sd, instances, "2.2.2.2")
+		expectServiceInstances(t, sd, selector, 0, instances)
+		expectEvents(t, events,
+			Event{Type: "service", ID: "selector.com", Namespace: selector.Namespace},
+			Event{Type: "eds cache", ID: "selector.com", Namespace: selector.Namespace},
+			Event{Type: "xds full"})
+	})
+
+	cases := []struct {
+		name      string
+		instances []testWorkloadInstance
+	}{
+		{
+			name: "change label removing all",
+			instances: []testWorkloadInstance{
+				{
+					name:           selector.Name,
+					namespace:      selector.Namespace,
+					address:        "2.2.2.2",
+					labels:         map[string]string{"app": "wle"},
+					serviceAccount: "default",
+					tlsmode:        model.IstioMutualTLSModeLabel,
+					expectedProxyInstances: []expectedProxyInstances{
+						{
+							instancesWithSA: []*model.ServiceInstance{
+								makeInstanceWithServiceAccount(selector, "2.2.2.2", 444, selector.Spec.(*networking.ServiceEntry).Ports[0], map[string]string{"app": "wle"}, "default"),
+								makeInstanceWithServiceAccount(selector, "2.2.2.2", 445, selector.Spec.(*networking.ServiceEntry).Ports[1], map[string]string{"app": "wle"}, "default"),
+							},
+							address: "2.2.2.2",
+						},
+					},
+				},
+				{
+					name:           selector.Name,
+					namespace:      selector.Namespace,
+					address:        "2.2.2.2",
+					labels:         map[string]string{"app": "wle2"},
+					serviceAccount: "default",
+					tlsmode:        model.IstioMutualTLSModeLabel,
+					expectedProxyInstances: []expectedProxyInstances{
+						{
+							instancesWithSA: []*model.ServiceInstance{}, // The instance labels don't match the se anymore, so adding this wi removes 2 instances
+							address:         "2.2.2.2",
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "change label removing all",
+			instances: []testWorkloadInstance{
+				{
+					name:           selector.Name,
+					namespace:      selector.Namespace,
+					address:        "2.2.2.2",
+					labels:         map[string]string{"app": "wle"},
+					serviceAccount: "default",
+					tlsmode:        model.IstioMutualTLSModeLabel,
+					expectedProxyInstances: []expectedProxyInstances{
+						{
+							instancesWithSA: []*model.ServiceInstance{
+								makeInstanceWithServiceAccount(selector, "2.2.2.2", 444, selector.Spec.(*networking.ServiceEntry).Ports[0], map[string]string{"app": "wle"}, "default"),
+								makeInstanceWithServiceAccount(selector, "2.2.2.2", 445, selector.Spec.(*networking.ServiceEntry).Ports[1], map[string]string{"app": "wle"}, "default"),
+							},
+							address: "2.2.2.2",
+						},
+					},
+				},
+				{
+					name:           "another-name",
+					namespace:      selector.Namespace,
+					address:        "3.3.3.3",
+					labels:         map[string]string{"app": "wle"},
+					serviceAccount: "default",
+					tlsmode:        model.IstioMutualTLSModeLabel,
+					expectedProxyInstances: []expectedProxyInstances{
+						{
+							instancesWithSA: []*model.ServiceInstance{
+								makeInstanceWithServiceAccount(selector, "2.2.2.2", 444, selector.Spec.(*networking.ServiceEntry).Ports[0], map[string]string{"app": "wle"}, "default"),
+								makeInstanceWithServiceAccount(selector, "2.2.2.2", 445, selector.Spec.(*networking.ServiceEntry).Ports[1], map[string]string{"app": "wle"}, "default"),
+							},
+							address: "2.2.2.2",
+						},
+						{
+							instancesWithSA: []*model.ServiceInstance{
+								makeInstanceWithServiceAccount(selector, "3.3.3.3", 444, selector.Spec.(*networking.ServiceEntry).Ports[0], map[string]string{"app": "wle"}, "default"),
+								makeInstanceWithServiceAccount(selector, "3.3.3.3", 445, selector.Spec.(*networking.ServiceEntry).Ports[1], map[string]string{"app": "wle"}, "default"),
+							},
+							address: "3.3.3.3",
+						},
+					},
+				},
+				{
+					name:           selector.Name,
+					namespace:      selector.Namespace,
+					address:        "2.2.2.2",
+					labels:         map[string]string{"app": "wle2"},
+					serviceAccount: "default",
+					tlsmode:        model.IstioMutualTLSModeLabel,
+					expectedProxyInstances: []expectedProxyInstances{
+						{
+							instancesWithSA: []*model.ServiceInstance{
+								makeInstanceWithServiceAccount(selector, "3.3.3.3", 444, selector.Spec.(*networking.ServiceEntry).Ports[0], map[string]string{"app": "wle"}, "default"),
+								makeInstanceWithServiceAccount(selector, "3.3.3.3", 445, selector.Spec.(*networking.ServiceEntry).Ports[1], map[string]string{"app": "wle"}, "default"),
+							},
+							address: "3.3.3.3",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, testCase := range cases {
+		t.Run(testCase.name, func(t *testing.T) {
+			for _, instance := range testCase.instances {
+
+				wi := &model.WorkloadInstance{
+					Name:      instance.name,
+					Namespace: instance.namespace,
+					Endpoint: &model.IstioEndpoint{
+						Address:        instance.address,
+						Labels:         instance.labels,
+						ServiceAccount: spiffe.MustGenSpiffeURI(selector.Name, instance.serviceAccount),
+						TLSMode:        instance.tlsmode,
+					},
+				}
+
+				callInstanceHandlers([]*model.WorkloadInstance{wi}, sd, model.EventAdd, t)
+
+				totalInstances := []*model.ServiceInstance{}
+				for _, expectedProxyInstance := range instance.expectedProxyInstances {
+					expectProxyInstances(t, sd, expectedProxyInstance.instancesWithSA, expectedProxyInstance.address)
+					totalInstances = append(totalInstances, expectedProxyInstance.instancesWithSA...)
+				}
+
+				expectServiceInstances(t, sd, selector, 0, totalInstances)
+				expectEvents(t, events,
+					Event{Type: "eds", ID: selector.Spec.(*networking.ServiceEntry).Hosts[0], Namespace: selector.Namespace, EndpointCount: len(totalInstances)})
+			}
+		})
+	}
+}
+
 func expectProxyInstances(t testing.TB, sd *Controller, expected []*model.ServiceInstance, ip string) {
 	t.Helper()
 	// The system is eventually consistent, so add some retries
