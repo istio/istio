49adc1f35c7a5999c39aadf2d6d89199f3c699ac
Istio Automation <istio-testing-bot@google.com>
2023-04-20 09:48:18 -0700

    [release-1.17] add validation for empty prefix header match (#44455)
    * add validation for empty prefix header match
    
    * fix linter issues
    
    * update error message + add a release note
    
    * add "prefix" to the error message
    
    ---------
    
    Co-authored-by: Peter Jausovec <peterj@users.noreply.github.com>
    Co-authored-by: Peter Jausovec <peter.jausovec@solo.io>

--- pkg/config/validation/validation_test.go
+++ pkg/config/validation/validation_test.go
@@ -2827,6 +2827,16 @@ func TestValidateHTTPRoute(t *testing.T) {
 				},
 			}},
 		}, valid: false},
+		{name: "empty prefix header match", route: &networking.HTTPRoute{
+			Route: []*networking.HTTPRouteDestination{{
+				Destination: &networking.Destination{Host: "foo.bar"},
+			}},
+			Match: []*networking.HTTPMatchRequest{{
+				Headers: map[string]*networking.StringMatch{
+					"emptyprefix": {MatchType: &networking.StringMatch_Prefix{Prefix: ""}},
+				},
+			}},
+		}, valid: false},
 		{name: "nil match", route: &networking.HTTPRoute{
 			Route: []*networking.HTTPRouteDestination{{
 				Destination: &networking.Destination{Host: "foo.bar"},
