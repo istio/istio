0794599ad79401f6f2751c1667789c0298dca823
akshayjnambiar <akshayjnambiar@google.com>
2023-04-20 11:25:28 +0000

    disable automount SA token only on tests with min istio revisions >= 1.16
    Testing multiple istio versions involves older istio versions which doesn't support sidecars with disable automount SA token. This was enabled form 1.16 onwards.

--- pkg/test/framework/components/echo/common/deployment/echos.go
+++ pkg/test/framework/components/echo/common/deployment/echos.go
@@ -159,6 +159,11 @@ func (c *Config) fillDefaults(ctx resource.Context) error {
 func (c *Config) DefaultEchoConfigs(t resource.Context) []echo.Config {
 	var defaultConfigs []echo.Config
 
+	disableAutomountSAToken := true
+	if t.Settings().Revisions.Minimum() < "1.16" {
+		disableAutomountSAToken = false
+	}
+
 	a := echo.Config{
 		Service:                 ASvc,
 		ServiceAccount:          true,
@@ -166,7 +171,7 @@ func (c *Config) DefaultEchoConfigs(t resource.Context) []echo.Config {
 		Subsets:                 []echo.SubsetConfig{{}},
 		Locality:                "region.zone.subzone",
 		IncludeExtAuthz:         c.IncludeExtAuthz,
-		DisableAutomountSAToken: true,
+		DisableAutomountSAToken: disableAutomountSAToken,
 	}
 
 	b := echo.Config{
