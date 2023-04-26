601a7c7e925a671f1001daea468a03d0aef9c87e
John Howard <howardjohn@google.com>
2023-04-26 14:28:09 -0700

    Remove legacy SIDECAR_IGNORE_PORT_IN_HOST_MATCH feature flag

--- pilot/pkg/features/pilot.go
+++ pilot/pkg/features/pilot.go
@@ -728,8 +728,6 @@ var (
 	LocalClusterSecretWatcher = env.Register("LOCAL_CLUSTER_SECRET_WATCHER", false,
 		"If enabled, the cluster secret watcher will watch the namespace of the external cluster instead of config cluster").Get()
 
-	SidecarIgnorePort = env.Register("SIDECAR_IGNORE_PORT_IN_HOST_MATCH", true, "If enabled, port will not be used in vhost domain matches.").Get()
-
 	EnableEnhancedResourceScoping = env.Register("ENABLE_ENHANCED_RESOURCE_SCOPING", false,
 		"If enabled, meshConfig.discoverySelectors will limit the CustomResource configurations(like Gateway,VirtualService,DestinationRule,Ingress, etc)"+
 			"that can be processed by pilot. This will also restrict the root-ca certificate distribution.").Get()
