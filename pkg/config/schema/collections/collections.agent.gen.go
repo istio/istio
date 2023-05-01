9ba1b1c3cf08b836fbe6e6ee173b9deb4867565a
John Howard <howardjohn@google.com>
2023-05-01 10:38:49 -0700

    fix build

--- pkg/config/schema/collections/collections.agent.gen.go
+++ pkg/config/schema/collections/collections.agent.gen.go
@@ -331,8 +331,8 @@ var (
 		MustAdd(WorkloadGroup).
 		Build()
 
-	// PilotGatewayAPI() contains only collections used by Pilot, including the full Gateway API.
-	PilotGatewayAPI() = collection.NewSchemasBuilder().
+	// pilotGatewayAPI contains only collections used by Pilot, including the full Gateway API.
+	pilotGatewayAPI = collection.NewSchemasBuilder().
 			MustAdd(AuthorizationPolicy).
 			MustAdd(DestinationRule).
 			MustAdd(EnvoyFilter).
