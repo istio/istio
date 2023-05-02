88a189ecb2054adc01b27d5a1ba74dd1ae178fc7
Paul Merrison <paul@tetrate.io>
2023-05-02 14:30:41 +0000

    update_deps output priot to beta0 build

--- prow/release-commit.sh
+++ prow/release-commit.sh
@@ -36,7 +36,7 @@ DOCKER_HUB=${DOCKER_HUB:-gcr.io/istio-testing}
 GCS_BUCKET=${GCS_BUCKET:-istio-build/dev}
 
 # Use a pinned version in case breaking changes are needed
-BUILDER_SHA=a85593d26d52fcd77032bde5438cc04e7e9cb81c
+BUILDER_SHA=3c9c07b6b632ce0cf91ccf5de2bc103a54ae2b47
 
 # Reference to the next minor version of Istio
 # This will create a version like 1.4-alpha.sha
