bb716748dc399b8c4e9fe75bdd601dfacb5d7fb3
John Howard <howardjohn@google.com>
2023-05-22 10:51:15 -0700

    istioctl: remove experimental `add-to-mesh`, `remove-from-mesh` and `kube-uninject` (#44967)
    Rationale:
    * These have been experimental since inception many many years ago with
      no path to promotion
    * They promote bad practices; usage of automatic injection is strongly
      preferred
    * The VM part uses legacy VM model not documented/recommended anymore
    * It doesn't really work. Uninjecting is not feasible; we just attempt
      to guess what was injected and what wasn't which is not possible to do
    correctly.
    
    As a result, I recommend removing these entirely.

--- tests/integration/pilot/main_test.go
+++ tests/integration/pilot/main_test.go
@@ -20,12 +20,9 @@ package pilot
 import (
 	"testing"
 
-	"istio.io/istio/pkg/config/protocol"
 	"istio.io/istio/pkg/test/framework"
-	"istio.io/istio/pkg/test/framework/components/echo"
 	"istio.io/istio/pkg/test/framework/components/echo/common/deployment"
 	"istio.io/istio/pkg/test/framework/components/istio"
-	"istio.io/istio/pkg/test/framework/components/namespace"
 	"istio.io/istio/pkg/test/framework/resource"
 )
 
@@ -53,20 +50,4 @@ func TestMain(m *testing.M) {
 			return nil
 		}).
 		Run()
-}
-
-func echoConfig(ns namespace.Instance, name string) echo.Config {
-	return echo.Config{
-		Service:   name,
-		Namespace: ns,
-		Ports: []echo.Port{
-			{
-				Name:     "http",
-				Protocol: protocol.HTTP,
-				// We use a port > 1024 to not require root
-				WorkloadPort: 8090,
-			},
-		},
-		Subsets: []echo.SubsetConfig{{}},
-	}
 }
