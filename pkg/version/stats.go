4603a20a05e720761df6c8d45a908ef94ede009d
Xiaopeng Han <hanxiaop8@outlook.com>
2023-05-24 22:06:41 +0800

    update pkg copyright (#45084)

--- pkg/version/stats.go
+++ pkg/version/stats.go
@@ -1,7 +1,7 @@
 //go:build !linux
 // +build !linux
 
-// Copyright 2018 Istio Authors
+// Copyright Istio Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
