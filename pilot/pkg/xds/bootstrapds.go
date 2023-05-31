da1bb40a52d6a69162a50779d1fd13411fe06b23
zengyuxing007 <newday.jesse@gmail.com>
2023-05-31 16:13:04 +0800

    bootstapds: Static listeners support HTTP/1.0

--- pilot/pkg/xds/bootstrapds.go
+++ pilot/pkg/xds/bootstrapds.go
@@ -17,7 +17,11 @@ package xds
 import (
 	"bytes"
 	"fmt"
+	envoycorev3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	hcm "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
+	"github.com/envoyproxy/go-control-plane/pkg/wellknown"
 	"io"
+	"istio.io/istio/pilot/pkg/features"
 
 	bootstrapv3 "github.com/envoyproxy/go-control-plane/envoy/config/bootstrap/v3"
 	discovery "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
@@ -39,6 +43,11 @@ type BootstrapGenerator struct {
 
 var _ model.XdsResourceGenerator = &BootstrapGenerator{}
 
+// if enableFlag is "1" indicates that AcceptHttp_10 is enabled.
+func enableHTTP10(enableFlag string) bool {
+	return enableFlag == "1"
+}
+
 // Generate returns a bootstrap discovery response.
 func (e *BootstrapGenerator) Generate(proxy *model.Proxy, w *model.WatchedResource, req *model.PushRequest) (model.Resources, model.XdsLogDetails, error) {
 	// The model.Proxy information is incomplete, re-parse the discovery request.
@@ -57,6 +66,48 @@ func (e *BootstrapGenerator) Generate(proxy *model.Proxy, w *model.WatchedResour
 	if err = protomarshal.Unmarshal(buf.Bytes(), bs); err != nil {
 		log.Warnf("failed to unmarshal bootstrap from JSON %q: %v", buf.String(), err)
 	}
+	if features.HTTP10 || enableHTTP10(proxy.Metadata.HTTP10) {
+		log.Infof("http10 enabled")
+		if bs.StaticResources != nil {
+			for _, listener := range bs.StaticResources.Listeners {
+				if listener != nil {
+					for _, filterChain := range listener.FilterChains {
+						if filterChain != nil {
+							for _, filter := range filterChain.Filters {
+								if filter != nil {
+									if filter.Name == wellknown.HTTPConnectionManager {
+										httpProxy := &hcm.HttpConnectionManager{}
+										filter.GetTypedConfig().TypeUrl = "type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager"
+										err = filter.GetTypedConfig().UnmarshalTo(httpProxy)
+										if err != nil {
+											log.Warnf("try to deal with bootstrap static resources for proxy[%s], failed to unmarshal filter [%s->%s] to hcm,errMsg: [%s]", proxy.ID, listener.GetAddress().String(), filter.Name, err.Error())
+											continue
+										}
+										if httpProxy.HttpProtocolOptions != nil {
+											httpProxy.HttpProtocolOptions.AcceptHttp_10 = true
+										} else {
+											httpProxy.HttpProtocolOptions = &envoycorev3.Http1ProtocolOptions{
+												AcceptHttp_10: true,
+											}
+										}
+										err = filter.GetTypedConfig().MarshalFrom(httpProxy)
+										if err != nil {
+											log.Warnf("after add http10 option, hcm filter failed to marshal from it ")
+											continue
+										} else {
+											log.Infof("Due to enabled http10 for [%s], the filter [%s->%s] under bootstrap static resources be added http10 option successfully",
+												proxy.ID, listener.GetAddress().String(), filter.GetName())
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
 	bs = e.applyPatches(bs, proxy, req.Push)
 	return model.Resources{
 		&discovery.Resource{
