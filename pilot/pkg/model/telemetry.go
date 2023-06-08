f685638298ae05a5a27c4a0907157ef81447cb82
lei-tang <32078630+lei-tang@users.noreply.github.com>
2023-06-08 13:32:05 -0700

    Check the disabled status when adding a log provider (#45366)
    * Check the disabled status when adding a log provider
    
    By checking the disabled status when adding a log provider, this PR fixes an issue where disabling a log provider through Istio telemetry API would not work. Otherwise, a disabled log provider may still be added to the log configuration and cause the disabling to not work as expected. The test case in this PR verifies that with the fix this PR, a disabled log provider will not be added to the log configuration.
    
    * Update the variable name

--- pilot/pkg/model/telemetry.go
+++ pilot/pkg/model/telemetry.go
@@ -516,7 +516,7 @@ func (t *Telemetries) telemetryFilters(proxy *Proxy, class networking.ListenerCl
 		if p == nil {
 			continue
 		}
-		_, logging := tml[k]
+		loggingCfg, logging := tml[k]
 		mertricCfg, metrics := tmm[k]
 
 		mertricCfg.RotationInterval = rotationInterval
@@ -525,7 +525,7 @@ func (t *Telemetries) telemetryFilters(proxy *Proxy, class networking.ListenerCl
 		cfg := telemetryFilterConfig{
 			Provider:      p,
 			metricsConfig: mertricCfg,
-			AccessLogging: logging,
+			AccessLogging: logging && !loggingCfg.Disabled,
 			Metrics:       metrics,
 			LogsFilter:    tml[p.Name].Filter,
 			NodeType:      proxy.Type,
