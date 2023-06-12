85d31dfe270bb043bc0e237336f5c59e27c8ed93
rokkiter <101091030+rokkiter@users.noreply.github.com>
2023-06-12 18:08:11 +0800

    cniAmbientConfigDir put in to config

--- cni/pkg/config/config.go
+++ cni/pkg/config/config.go
@@ -74,6 +74,9 @@ type InstallConfig struct {
 	// Whether ambient is enabled
 	AmbientEnabled bool
 
+	// Directories into which to copy the CNI ambient config
+	CNIAmbientConfigDir string
+
 	// Whether ebpf is enabled
 	EbpfEnabled bool
 
