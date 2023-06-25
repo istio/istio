2c7c09c87bc75ce94a66b86a1b37d482c3b744de
rokkiter <101091030+rokkiter@users.noreply.github.com>
2023-06-25 18:32:20 +0800

    refactor copy to merge with return a new map

--- pkg/maps/maps.go
+++ pkg/maps/maps.go
@@ -40,10 +40,13 @@ func Keys[M ~map[K]V, K comparable, V any](m M) []K {
 	return maps.Keys(m)
 }
 
-// Copy copies all key/value pairs in src adding them to dst.
-// When a key in src is already present in dst,
-// the value in dst will be overwritten by the value associated
-// with the key in src.
-func Copy[M1 ~map[K]V, M2 ~map[K]V, K comparable, V any](dst M1, src M2) {
-	maps.Copy(dst, src)
+// Merge create a new map merge all key/value pairs from base and override.
+// When a key in override is already present in base,
+// the value in base will be overwritten by the value associated
+// with the key in override.
+func Merge[M1 ~map[K]V, M2 ~map[K]V, K comparable, V any](base M1, override M2) M1 {
+	dst := make(M1)
+	maps.Copy(dst, base)
+	maps.Copy(dst, override)
+	return dst
 }
