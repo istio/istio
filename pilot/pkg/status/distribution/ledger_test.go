f88a31c8bab2bc039483d5485cc9cf3016cddc43
shirleyyLi <shirleyy000.li@gmail.com>
2023-07-10 00:27:55 -0700

    add ledger test

---
+++ pilot/pkg/status/distribution/ledger_test.go
@@ -0,0 +1,61 @@
+package distribution
+
+import (
+	"testing"
+	"time"
+
+	. "github.com/onsi/gomega"
+
+	"istio.io/istio/pkg/config"
+	"istio.io/istio/pkg/ledger"
+)
+
+func TestTryLedgerPutAndDelete(t *testing.T) {
+	RegisterTestingT(t)
+	l := ledger.Make(time.Minute)
+	resources := []config.Config{
+		{
+			Meta: config.Meta{
+				Namespace:  "default",
+				Name:       "foo",
+				Generation: 1,
+			},
+		},
+		{
+			Meta: config.Meta{
+				Namespace:  "default",
+				Name:       "bar",
+				Generation: 2,
+			},
+		},
+		{
+			Meta: config.Meta{
+				Namespace:  "alternate",
+				Name:       "boo",
+				Generation: 3,
+			},
+		},
+	}
+	var result []string
+	for _, res := range resources {
+		tryLedgerPut(l, res)
+
+		r, err := l.Get(res.Key())
+		if err != nil {
+			t.Errorf("ledger get %s error %v", res.Key(), err)
+		}
+		result = append(result, r)
+	}
+
+	Expect(result[0]).To(Equal("1"))
+	Expect(result[1]).To(Equal("2"))
+	Expect(result[2]).To(Equal("3"))
+
+	tryLedgerDelete(l, resources[0])
+	time.Sleep(2 * time.Minute)
+	result1, err := l.Get(resources[0].Key())
+	if err != nil {
+		t.Errorf("ledger get %s error %v", resources[0].Key(), err)
+	}
+	Expect(result1).To(Equal(""))
+}
