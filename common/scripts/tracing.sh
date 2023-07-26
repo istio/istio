c170c751c16ea9cc575c3c5ad3a255ac0c4b4b76
Eric Van Norman <ericvn@us.ibm.com>
2023-07-26 10:33:58 -0500

    Fix url

--- common/scripts/tracing.sh
+++ common/scripts/tracing.sh
@@ -35,9 +35,15 @@ function tracing::extract_prow_trace() {
 
 function _genattrs() {
   # No upstream standard, so copy from https://github.com/jenkinsci/opentelemetry-plugin/blob/master/docs/job-traces.md
+  if [[ -n "${PULL_NUMBER}" ]]
+  then
+    url="https://prow.istio.io/view/gs/istio-prow/pr-logs/pull/${REPO_OWNER}_${REPO_NAME}/${PULL_NUMBER:-none}/${JOB_NAME}/${BUILD_ID},"
+  else
+    url="https://prow.istio.io/view/gs/istio-prow/pr-logs/${JOB_NAME}/${BUILD_ID},"
+  fi
   echo "ci.pipeline.id=${JOB_NAME},"\
     "ci.pipeline.type=${JOB_TYPE},"\
-    "ci.pipeline.run.url=https://prow.istio.io/view/gs/istio-prow/pr-logs/pull/${REPO_OWNER}_${REPO_NAME}/${PULL_NUMBER:-none}/${JOB_NAME}/${BUILD_ID},"\
+    "ci.pipeline.run.url=${url}"\
     "ci.pipeline.run.number=${BUILD_ID},"\
     "ci.pipeline.run.id=${PROW_JOB_ID},"\
     "ci.pipeline.run.repo=${REPO_OWNER}/${REPO_NAME},"\
