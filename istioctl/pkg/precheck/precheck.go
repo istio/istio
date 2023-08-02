87a84a8eda5ae2533d9dd11f50ff9c025993d9d5
Xiaopeng Han <hanxiaop8@outlook.com>
2023-08-02 23:34:39 +0800

    Revise precheck messages output default threshold, and add threshold control option (#46073)
    * fix precheck info messages
    
    * add releasenotes

--- istioctl/pkg/precheck/precheck.go
+++ istioctl/pkg/precheck/precheck.go
@@ -58,6 +58,7 @@ import (
 func Cmd(ctx cli.Context) *cobra.Command {
 	var opts clioptions.ControlPlaneOptions
 	var skipControlPlane bool
+	outputThreshold := formatting.MessageThreshold{Level: diag.Warning}
 	var msgOutputFormat string
 	// cmd represents the upgradeCheck command
 	cmd := &cobra.Command{
@@ -89,11 +90,18 @@ func Cmd(ctx cli.Context) *cobra.Command {
 			msgs.Add(nsmsgs...)
 			// Print all the messages to stdout in the specified format
 			msgs = msgs.SortedDedupedCopy()
+			outputMsgs := diag.Messages{}
+			for _, m := range msgs {
+				if m.Type.Level().IsWorseThanOrEqualTo(outputThreshold.Level) {
+					outputMsgs = append(outputMsgs, m)
+				}
+			}
 			output, err := formatting.Print(msgs, msgOutputFormat, false)
 			if err != nil {
 				return err
 			}
-			if len(msgs) == 0 {
+
+			if len(outputMsgs) == 0 {
 				fmt.Fprintf(cmd.ErrOrStderr(), color.New(color.FgGreen).Sprint("✔")+" No issues found when checking the cluster. Istio is safe to install or upgrade!\n"+
 					"  To get started, check out https://istio.io/latest/docs/setup/getting-started/\n")
 			} else {
@@ -110,6 +118,8 @@ See %s for more information about causes and resolutions.`, url.ConfigAnalysis)
 		},
 	}
 	cmd.PersistentFlags().BoolVar(&skipControlPlane, "skip-controlplane", false, "skip checking the control plane")
+	cmd.PersistentFlags().Var(&outputThreshold, "output-threshold",
+		fmt.Sprintf("The severity level of precheck at which to display messages. Valid values: %v", diag.GetAllLevelStrings()))
 	cmd.PersistentFlags().StringVarP(&msgOutputFormat, "output", "o", formatting.LogFormat,
 		fmt.Sprintf("Output format: one of %v", formatting.MsgOutputFormatKeys))
 	opts.AttachControlPlaneFlags(cmd)
