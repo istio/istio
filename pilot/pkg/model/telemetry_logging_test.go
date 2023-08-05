3800a38477dec86df4edb1e64b9038163534b882
zirain <zirain2009@gmail.com>
2023-08-05 22:28:42 +0800

    accesslog: disable builtin resource labels

--- pilot/pkg/model/telemetry_logging_test.go
+++ pilot/pkg/model/telemetry_logging_test.go
@@ -955,6 +955,7 @@ func TestBuildOpenTelemetryAccessLogConfig(t *testing.T) {
 					TransportApiVersion:     core.ApiVersion_V3,
 					FilterStateObjectsToLog: envoyWasmStateToLog,
 				},
+				DisableBuiltinLabels: true,
 				Body: &otlpcommon.AnyValue{
 					Value: &otlpcommon.AnyValue_StringValue{
 						StringValue: EnvoyTextLogFormat,
@@ -987,6 +988,7 @@ func TestBuildOpenTelemetryAccessLogConfig(t *testing.T) {
 					TransportApiVersion:     core.ApiVersion_V3,
 					FilterStateObjectsToLog: envoyWasmStateToLog,
 				},
+				DisableBuiltinLabels: true,
 				Body: &otlpcommon.AnyValue{
 					Value: &otlpcommon.AnyValue_StringValue{
 						StringValue: EnvoyTextLogFormat,
