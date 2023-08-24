c0069802cb8101d4f7f627ef9ead532cfe763bf5
Yossi Mesika <yossi.mesika@solo.io>
2023-08-24 15:19:19 +0300

    Lint fix

--- pilot/pkg/serviceregistry/kube/controller/ambientindex_workloadentry_test.go
+++ pilot/pkg/serviceregistry/kube/controller/ambientindex_workloadentry_test.go
@@ -302,9 +302,9 @@ func TestAmbientIndex_InlinedWorkloadEntries(t *testing.T) {
 	test.SetForTest(t, &features.EnableAmbientControllers, true)
 	s := newAmbientTestServer(t, testC, testNW)
 
-	s.addServiceEntry(t, "se.istio.io", []string{"240.240.23.45"}, "name1", testNS, map[string]string{"app": "a"}, true)
-	s.assertWorkloads(t, "", workloadapi.WorkloadStatus_HEALTHY, "name1")
-	s.assertEvent(t, s.seIPXdsName("name1", "127.0.0.1"), "ns1/se.istio.io")
+	s.addServiceEntry(t, "se.istio.io", []string{"240.240.23.45"}, "se1", testNS, map[string]string{"app": "a"}, true)
+	s.assertWorkloads(t, "", workloadapi.WorkloadStatus_HEALTHY, "se1")
+	s.assertEvent(t, s.seIPXdsName("se1", "127.0.0.1"), "ns1/se.istio.io")
 
 	s.addPolicy(t, "selector", "ns1", map[string]string{"app": "a"}, gvk.AuthorizationPolicy, nil)
 	assert.Equal(t,
@@ -312,7 +312,7 @@ func TestAmbientIndex_InlinedWorkloadEntries(t *testing.T) {
 		[]string{"ns1/selector"})
 
 	_ = s.cfg.Delete(gvk.AuthorizationPolicy, "selector", "ns1", nil)
-	s.assertEvent(t, s.wleXdsName("name1"))
+	s.assertEvent(t, s.wleXdsName("se1"))
 	assert.Equal(t,
 		s.lookup(s.addrXdsName("127.0.0.1"))[0].GetWorkload().GetAuthorizationPolicies(),
 		nil)
