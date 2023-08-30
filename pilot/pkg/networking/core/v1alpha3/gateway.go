65ee971a8b57889c8e2126a51ade3f9668dc2bb8
Zhonghu Xu <xuzhonghu@huawei.com>
2023-08-30 23:04:08 +0800

    Remove Istio 1.17 version check (#46588)
    * remove 1.17 check
    
    * update
    
    * update
    
    * remove options.istio-1.17-check

--- pilot/pkg/networking/core/v1alpha3/gateway.go
+++ pilot/pkg/networking/core/v1alpha3/gateway.go
@@ -240,11 +240,7 @@ func (configgen *ConfigGeneratorImpl) buildGatewayTCPBasedFilterChains(
 	mergedGateway *model.MergedGateway,
 	tlsHostsByPort map[uint32]map[string]string,
 ) {
-	var tcpAuthFilters []*listener.Filter
-	if util.IsIstioVersionGE117(builder.node.IstioVersion) {
-		tcpAuthFilters = append(tcpAuthFilters, xdsfilters.IstioNetworkAuthenticationFilter)
-	}
-
+	tcpAuthFilters := []*listener.Filter{xdsfilters.IstioNetworkAuthenticationFilter}
 	if p.IsHTTP() {
 		// We have a list of HTTP servers on this port. Build a single listener for the server port.
 		port := &networking.Port{Number: port.Number, Protocol: port.Protocol}
@@ -273,9 +269,7 @@ func (configgen *ConfigGeneratorImpl) buildGatewayTCPBasedFilterChains(
 				// For network filters such as mysql, mongo, etc., we need the filter codec upfront. Data from this
 				// codec is used by RBAC later.
 				var tcpAuthFilters []*listener.Filter
-				if util.IsIstioVersionGE117(builder.node.IstioVersion) {
-					tcpAuthFilters = append(tcpAuthFilters, xdsfilters.IstioNetworkAuthenticationFilter)
-				}
+				tcpAuthFilters = append(tcpAuthFilters, xdsfilters.IstioNetworkAuthenticationFilter)
 				tcpAuthFilters = append(tcpAuthFilters, builder.authzCustomBuilder.BuildTCP()...)
 				tcpAuthFilters = append(tcpAuthFilters, builder.authzBuilder.BuildTCP()...)
 				// This is the case of TCP or PASSTHROUGH.
