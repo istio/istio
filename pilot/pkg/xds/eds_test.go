75f8b4c1b8438115b38965a8378cb6c783e91cbe
Rama Chavali <rama.rao@salesforce.com>
2023-09-08 16:53:08 +0530

    send initial unready endpoints when SendUnhealthyEndpoints is enabled
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/xds/eds_test.go
+++ pilot/pkg/xds/eds_test.go
@@ -399,8 +399,8 @@ func TestEDSUnhealthyEndpoints(t *testing.T) {
 		}
 	}
 
-	// Validate that we do not send initial unhealthy endpoints.
-	validateEndpoints(false, nil, nil)
+	// Validate that we do  send initial unhealthy endpoints.
+	validateEndpoints(true, nil, []string{"10.0.0.53:53"})
 	adscon.WaitClear()
 
 	// Set additional unhealthy endpoint and validate Eds update is not triggered.
@@ -420,8 +420,8 @@ func TestEDSUnhealthyEndpoints(t *testing.T) {
 			},
 		})
 
-	// Validate that endpoint is not pushed.
-	validateEndpoints(false, nil, nil)
+	// Validate that endpoint is pushed.
+	validateEndpoints(true, nil, []string{"10.0.0.53:53", "10.0.0.54:53"})
 
 	// Change the status of endpoint to Healthy and validate Eds is pushed.
 	s.MemRegistry.SetEndpoints("unhealthy.svc.cluster.local", "",
