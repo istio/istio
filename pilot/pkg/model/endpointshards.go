b52a510c0d5a8f9e5b54f8f8bb0db2f679f11c18
Rama Chavali <rama.rao@salesforce.com>
2023-09-08 21:08:55 +0530

    send initial unready endpoints when SendUnhealthyEndpoints is enabled (#46906)
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/model/endpointshards.go
+++ pilot/pkg/model/endpointshards.go
@@ -288,44 +288,48 @@ func (e *EndpointIndex) UpdateServiceEndpoints(
 	newIstioEndpoints := istioEndpoints
 	if features.SendUnhealthyEndpoints.Load() {
 		oldIstioEndpoints := ep.Shards[shard]
-		newIstioEndpoints = make([]*IstioEndpoint, 0, len(istioEndpoints))
-
-		// Check if new Endpoints are ready to be pushed. This check
-		// will ensure that if a new pod comes with a non ready endpoint,
-		// we do not unnecessarily push that config to Envoy.
-		// Please note that address is not a unique key. So this may not accurately
-		// identify based on health status and push too many times - which is ok since its an optimization.
-		emap := make(map[string]*IstioEndpoint, len(oldIstioEndpoints))
-		nmap := make(map[string]*IstioEndpoint, len(newIstioEndpoints))
-		// Add new endpoints only if they are ever ready once to shards
-		// so that full push does not send them from shards.
-		for _, oie := range oldIstioEndpoints {
-			emap[oie.Address] = oie
-		}
-		for _, nie := range istioEndpoints {
-			nmap[nie.Address] = nie
-		}
 		needPush := false
-		for _, nie := range istioEndpoints {
-			if oie, exists := emap[nie.Address]; exists {
-				// If endpoint exists already, we should push if it's health status changes.
-				if oie.HealthStatus != nie.HealthStatus {
+		if oldIstioEndpoints == nil {
+			// If there are no old endpoints, we should push with incoming endpoints as there is nothing to compare.
+			needPush = true
+		} else {
+			newIstioEndpoints = make([]*IstioEndpoint, 0, len(istioEndpoints))
+			// Check if new Endpoints are ready to be pushed. This check
+			// will ensure that if a new pod comes with a non ready endpoint,
+			// we do not unnecessarily push that config to Envoy.
+			// Please note that address is not a unique key. So this may not accurately
+			// identify based on health status and push too many times - which is ok since its an optimization.
+			emap := make(map[string]*IstioEndpoint, len(oldIstioEndpoints))
+			nmap := make(map[string]*IstioEndpoint, len(newIstioEndpoints))
+			// Add new endpoints only if they are ever ready once to shards
+			// so that full push does not send them from shards.
+			for _, oie := range oldIstioEndpoints {
+				emap[oie.Address] = oie
+			}
+			for _, nie := range istioEndpoints {
+				nmap[nie.Address] = nie
+			}
+			for _, nie := range istioEndpoints {
+				if oie, exists := emap[nie.Address]; exists {
+					// If endpoint exists already, we should push if it's health status changes.
+					if oie.HealthStatus != nie.HealthStatus {
+						needPush = true
+					}
+					newIstioEndpoints = append(newIstioEndpoints, nie)
+				} else {
+					// If the endpoint does not exist in shards that means it is a
+					// new endpoint. Always send new endpoints even if they are not healthy.
+					// This is OK since we disable panic threshold when SendUnhealthyEndpoints is enabled.
+					needPush = true
+					newIstioEndpoints = append(newIstioEndpoints, nie)
+				}
+			}
+			// Next, check for endpoints that were in old but no longer exist. If there are any, there is a
+			// removal so we need to push an update.
+			for _, oie := range oldIstioEndpoints {
+				if _, f := nmap[oie.Address]; !f {
 					needPush = true
 				}
-				newIstioEndpoints = append(newIstioEndpoints, nie)
-			} else if nie.HealthStatus == Healthy {
-				// If the endpoint does not exist in shards that means it is a
-				// new endpoint. Only send if it is healthy to avoid pushing endpoints
-				// that are not ready to start with.
-				needPush = true
-				newIstioEndpoints = append(newIstioEndpoints, nie)
-			}
-		}
-		// Next, check for endpoints that were in old but no longer exist. If there are any, there is a
-		// removal so we need to push an update.
-		for _, oie := range oldIstioEndpoints {
-			if _, f := nmap[oie.Address]; !f {
-				needPush = true
 			}
 		}
 
