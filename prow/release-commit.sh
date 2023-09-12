d7fd78ee1824566752ae97f2aeaec139ffc9bbf9
Jonh Wendell <jwendell@redhat.com>
2023-09-12 09:26:03 -0400

    Update deps and simplify update_deps.sh getSha()

--- prow/release-commit.sh
+++ prow/release-commit.sh
@@ -36,7 +36,7 @@ docker run --rm --privileged "${DOCKER_HUB}/qemu-user-static" --reset -p yes
 export ISTIO_DOCKER_QEMU=true
 
 # Use a pinned version in case breaking changes are needed
-BUILDER_SHA=15adf1102ef73aa9b06a154728734faab14a85f4
+BUILDER_SHA=00f7e078c8f40211b0c450a0e48e9bb27c99b556
 
 # Reference to the next minor version of Istio
 # This will create a version like 1.4-alpha.sha
