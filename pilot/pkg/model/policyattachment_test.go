c33c03b8787976cbc49b61b70a6eaf049c64e2d4
Keith Mattix II <keithmattix@microsoft.com>
2023-09-14 22:12:43 +0000

    Fix test
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>

--- pilot/pkg/model/policyattachment_test.go
+++ pilot/pkg/model/policyattachment_test.go
@@ -150,7 +150,7 @@ func TestGetPolicyMatcher(t *testing.T) {
 				selector: sampleGatewaySelector,
 			},
 			opts:                 sampleGateway,
-			expected:             policyMatchSelector,
+			expected:             policyMatchIgnore,
 			policyAttachmentOnly: true,
 		},
 		{
