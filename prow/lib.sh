9acf4ad409748461351a9aad5e9719cdec2e3238
Faseela K <faseela.k@est.tech>
2023-09-21 12:47:17 +0200

    revert post submit tests enabled
    Signed-off-by: Faseela K <faseela.k@est.tech>

--- prow/lib.sh
+++ prow/lib.sh
@@ -119,16 +119,16 @@ function build_images() {
 
   # use ubuntu:jammy to test vms by default
   nonDistrolessTargets="docker.app docker.app_sidecar_ubuntu_jammy docker.ext-authz "
-  if [[ "${JOB_TYPE:-presubmit}" == "presubmit" ]]; then
+  if [[ "${JOB_TYPE:-presubmit}" == "postsubmit" ]]; then
     # We run tests across all VM types only in postsubmit
     nonDistrolessTargets+="docker.app_sidecar_ubuntu_bionic docker.app_sidecar_debian_11 "
     # TODO(https://github.com/istio/istio/issues/38224)
 #    nonDistrolessTargets+="docker.app_sidecar_rockylinux_8 "
   fi
-  if [[ "${SELECT_TEST}" == "test.integration.operator.kube" || "${SELECT_TEST}" == "test.integration.kube" || "${JOB_TYPE:-postsubmit}" == "presubmit" ]]; then
+  if [[ "${SELECT_TEST}" == "test.integration.operator.kube" || "${SELECT_TEST}" == "test.integration.kube" || "${JOB_TYPE:-postsubmit}" == "postsubmit" ]]; then
     targets+="docker.operator "
   fi
-  if [[ "${SELECT_TEST}" == "test.integration.ambient.kube" || "${SELECT_TEST}" == "test.integration.kube" || "${JOB_TYPE:-postsubmit}" == "presubmit" ]]; then
+  if [[ "${SELECT_TEST}" == "test.integration.ambient.kube" || "${SELECT_TEST}" == "test.integration.kube" || "${JOB_TYPE:-postsubmit}" == "postsubmit" ]]; then
     targets+="docker.ztunnel "
   fi
   targets+="docker.install-cni "
