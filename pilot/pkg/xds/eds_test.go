b2a3e3eabed0fe8ed265ec175985927bb472a5ee
zhlsunshine <huailong.zhang@intel.com>
2023-10-12 17:05:21 +0800

    fix unit test cases for TestEDSUnhealthyEndpoints

--- pilot/pkg/xds/eds_test.go
+++ pilot/pkg/xds/eds_test.go
@@ -407,7 +407,13 @@ func TestEDSUnhealthyEndpoints(t *testing.T) {
 	s.MemRegistry.SetEndpoints("unhealthy.svc.cluster.local", "",
 		[]*model.IstioEndpoint{
 			{
-				Addresses:       []string{"10.0.0.53", "10.0.0.54"},
+				Addresses:       []string{"10.0.0.53"},
+				EndpointPort:    53,
+				ServicePortName: "tcp-dns",
+				HealthStatus:    model.UnHealthy,
+			},
+			{
+				Addresses:       []string{"10.0.0.54"},
 				EndpointPort:    53,
 				ServicePortName: "tcp-dns",
 				HealthStatus:    model.UnHealthy,
@@ -421,7 +427,13 @@ func TestEDSUnhealthyEndpoints(t *testing.T) {
 	s.MemRegistry.SetEndpoints("unhealthy.svc.cluster.local", "",
 		[]*model.IstioEndpoint{
 			{
-				Addresses:       []string{"10.0.0.53", "10.0.0.54"},
+				Addresses:       []string{"10.0.0.53"},
+				EndpointPort:    53,
+				ServicePortName: "tcp-dns",
+				HealthStatus:    model.Healthy,
+			},
+			{
+				Addresses:       []string{"10.0.0.54"},
 				EndpointPort:    53,
 				ServicePortName: "tcp-dns",
 				HealthStatus:    model.Healthy,
