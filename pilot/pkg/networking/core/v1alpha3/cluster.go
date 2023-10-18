1562b2c300d2e89b43d47855e9e7ae9910a95dde
Jacob Delgado <jacob.delgado@volunteers.acasi.info>
2023-10-18 21:06:04 +0000

    Add release note

--- pilot/pkg/networking/core/v1alpha3/cluster.go
+++ pilot/pkg/networking/core/v1alpha3/cluster.go
@@ -603,6 +603,22 @@ func buildInboundClustersFromSidecar(cb *ClusterBuilder, proxy *model.Proxy,
 				if endpointAddress == "" {
 					endpointAddress = model.LocalhostIPv6AddressPrefix
 				}
+			} else if features.EnableDualStack {
+				if hostIP == model.LocalhostAddressPrefix {
+					for _, host := range actualLocalHosts {
+						if netutil.IsIPv4Address(host) {
+							endpointAddress = host
+							break
+						}
+					}
+				} else if hostIP == model.LocalhostIPv6AddressPrefix {
+					for _, host := range actualLocalHosts {
+						if netutil.IsIPv6Address(host) {
+							endpointAddress = host
+							break
+						}
+					}
+				}
 			} else if hostIP == model.LocalhostAddressPrefix || hostIP == model.LocalhostIPv6AddressPrefix {
 				endpointAddress = actualLocalHosts[0]
 			}
