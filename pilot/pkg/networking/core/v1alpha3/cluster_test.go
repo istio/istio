c42e16178d577128addfea304f2ebebfdebdbcf5
Jacob Delgado <jacob.delgado@volunteers.acasi.info>
2023-10-18 22:37:56 +0000

    Fix unit test issues

--- pilot/pkg/networking/core/v1alpha3/cluster_test.go
+++ pilot/pkg/networking/core/v1alpha3/cluster_test.go
@@ -1697,7 +1697,16 @@ func TestBuildInboundClustersPortLevelCircuitBreakerThresholds(t *testing.T) {
 }
 
 func TestDualStackInboundClustersDefaultEndpoint(t *testing.T) {
-	dsProxy := &dualStackProxy
+	dsProxy := model.Proxy{
+		Type:        model.SidecarProxy,
+		IPAddresses: []string{"1.1.1.1", "1111:2222::1"},
+		ID:          "v0.default",
+		DNSDomain:   "default.example.org",
+		Metadata: &model.NodeMetadata{
+			Namespace: "not-default",
+		},
+		ConfigNamespace: "not-default",
+	}
 
 	inboundFilter := func(c *cluster.Cluster) bool {
 		return strings.HasPrefix(c.Name, "inbound|")
@@ -1714,7 +1723,7 @@ func TestDualStackInboundClustersDefaultEndpoint(t *testing.T) {
 		{
 			name:            "dual stack disabled, defaultEndpoint set to [::1]:7073",
 			isDual:          false,
-			proxy:           dsProxy,
+			proxy:           &dsProxy,
 			defaultEndpoint: "[::1]:7073",
 			expectedAddr:    "127.0.0.1",
 			expectedPort:    7073,
@@ -1722,7 +1731,7 @@ func TestDualStackInboundClustersDefaultEndpoint(t *testing.T) {
 		{
 			name:            "dual stack enabled, defaultEndpoint set to 127.0.0.1:7072",
 			isDual:          true,
-			proxy:           dsProxy,
+			proxy:           &dsProxy,
 			defaultEndpoint: "127.0.0.1:7072",
 			expectedAddr:    "127.0.0.1",
 			expectedPort:    7072,
@@ -1730,7 +1739,7 @@ func TestDualStackInboundClustersDefaultEndpoint(t *testing.T) {
 		{
 			name:            "dual stack enabled, defaultEndpoint set to [::1]:7073",
 			isDual:          true,
-			proxy:           dsProxy,
+			proxy:           &dsProxy,
 			defaultEndpoint: "[::1]:7073",
 			expectedAddr:    "::1",
 			expectedPort:    7073,
