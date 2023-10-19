861abfbc050c5be41154054853fe70336a851ce9
John Howard <howardjohn@google.com>
2023-10-19 14:20:09 -0700

    Bump go.mod dependencies, mostly otel (#47466)

--- pkg/monitoring/monitoring.go
+++ pkg/monitoring/monitoring.go
@@ -25,7 +25,6 @@ import (
 	otelprom "go.opentelemetry.io/otel/exporters/prometheus"
 	api "go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/sdk/metric"
-	"go.opentelemetry.io/otel/sdk/metric/aggregation"
 
 	"istio.io/istio/pkg/log"
 	"istio.io/istio/pkg/maps"
@@ -289,7 +288,7 @@ func (d *metrics) toHistogramViews() []metric.Option {
 		// for each histogram metric (i.e. those with bounds), set up a view explicitly defining those buckets.
 		v := metric.WithView(metric.NewView(
 			metric.Instrument{Name: name},
-			metric.Stream{Aggregation: aggregation.ExplicitBucketHistogram{
+			metric.Stream{Aggregation: metric.AggregationExplicitBucketHistogram{
 				Boundaries: def.Bounds,
 			}},
 		))
