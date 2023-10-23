1ca9603d37c4b006a9e5a22e37dcadeff76e0879
xiaopeng <hanxiaop8@outlook.com>
2023-10-23 10:58:58 +0800

    allow missing eds in file configdump

--- istioctl/pkg/writer/envoy/configdump/endpoint.go
+++ istioctl/pkg/writer/envoy/configdump/endpoint.go
@@ -141,9 +141,12 @@ func (c *ConfigWriter) retrieveSortedEndpointsSlice(filter EndpointFilter) ([]*e
 	if c.configDump == nil {
 		return nil, fmt.Errorf("config writer has not been primed")
 	}
-	dump, err := c.configDump.GetEndpointsConfigDump()
+	dump, err := c.configDump.GetEndpointsConfigDump(c.AllowMissingEds)
 	if err != nil {
 		return nil, err
+	}
+	if dump == nil {
+		return nil, nil
 	}
 	endpoints := make([]*endpoint.ClusterLoadAssignment, 0, len(dump.DynamicEndpointConfigs))
 	for _, e := range dump.GetDynamicEndpointConfigs() {
