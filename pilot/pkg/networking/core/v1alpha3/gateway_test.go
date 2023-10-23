istio/istio

commit 0fe60f8045ba380b331bc448ad1b6ae87238059e

fix gateway bind with same port and different protocol (#43690)
* fix gateway bind with same port and different protocol

* address comments

pilot/pkg/model/gateway.go
@@ -132,6 +132,7 @@ const DisableGatewayPortTranslationLabel = "experimental.istio.io/disable-gatewa
 // If servers with different protocols attempt to listen on the same port, one of the protocols will be chosen at random.
 func MergeGateways(gateways []gatewayWithInstances, proxy *Proxy, ps *PushContext) *MergedGateway {
 	gatewayPorts := make(map[uint32]bool)
+	nonPlainTextGatewayPortsBindMap := map[uint32]sets.String{}
 	mergedServers := make(map[ServerPort]*MergedServers)
 	mergedQUICServers := make(map[ServerPort]*MergedServers)
 	serverPorts := make([]ServerPort, 0)
@@ -216,7 +217,7 @@ func MergeGateways(gateways []gatewayWithInstances, proxy *Proxy, ps *PushContex
 					//    for each server (as each server ends up as a separate http connection manager due to filter chain match)
 					// 3. No for everything else.
 					if current, exists := plainTextServers[resolvedPort]; exists {
-						if !canMergeProtocols(serverProtocol, protocol.Parse(current.Protocol)) {
+						if !canMergeProtocols(serverProtocol, protocol.Parse(current.Protocol)) && current.Bind == serverPort.Bind {
 							log.Infof("skipping server on gateway %s port %s.%d.%s: conflict with existing server %d.%s",
 								gatewayConfig.Name, s.Port.Name, resolvedPort, s.Port.Protocol, serverPort.Number, serverPort.Protocol)
 							RecordRejectedConfig(gatewayName)
@@ -268,9 +269,19 @@ func MergeGateways(gateways []gatewayWithInstances, proxy *Proxy, ps *PushContex
 							}
 							serversByRouteName[routeName] = []*networking.Server{s}
 						}
-
+						// build the port bind map for none plain text protocol, thus can avoid protocol conflict if it's different bind
+						if bindsPortMap, ok := nonPlainTextGatewayPortsBindMap[resolvedPort]; ok && !bindsPortMap.Contains(serverPort.Bind) {
+							bindsPortMap.Insert(serverPort.Bind)
+						} else {
+							bindsPortMap := sets.String{}
+							bindsPortMap.Insert(serverPort.Bind)
+							nonPlainTextGatewayPortsBindMap[resolvedPort] = bindsPortMap
+						}
+						// If the bind/port combination is not being used as non-plaintext, they are different
+						// listeners and won't get conflicted even with same port different protocol
+						// i.e 0.0.0.0:443:GRPC/1.0.0.1:443:GRPC/1.0.0.2:443:HTTPS they are not conflicted, otherwise
 						// We have another TLS server on the same port. Can differentiate servers using SNI
-						if s.Tls == nil {
+						if s.Tls == nil && !nonPlainTextGatewayPortsBindMap[resolvedPort].Contains(serverPort.Bind) {
 							log.Warnf("TLS server without TLS options %s %s", gatewayName, s.String())
 							continue
 						}

pilot/pkg/networking/core/v1alpha3/gateway_test.go
@@ -2352,6 +2352,114 @@ func TestBuildGatewayListeners(t *testing.T) {
 			[]string{"0.0.0.0_443", "0.0.0.0_9443"},
 		},
 		{
+			"gateway with HTTPS/GRPC servers with bind",
+			&pilot_model.Proxy{},
+			[]config.Config{
+				{
+					Meta: config.Meta{Name: "gateway1", Namespace: "testns", GroupVersionKind: gvk.Gateway},
+					Spec: &networking.Gateway{
+						Servers: []*networking.Server{
+							{
+								Port:  &networking.Port{Name: "https", Number: 443, Protocol: "HTTPS"},
+								Hosts: []string{"*"},
+								Bind:  "10.0.0.1",
+								Tls:   &networking.ServerTLSSettings{CredentialName: "test", Mode: networking.ServerTLSSettings_SIMPLE},
+							},
+						},
+					},
+				},
+				{
+					Meta: config.Meta{Name: "gateway2", Namespace: "testns", GroupVersionKind: gvk.Gateway},
+					Spec: &networking.Gateway{
+						Servers: []*networking.Server{
+							{
+								Port:  &networking.Port{Name: "grpc", Number: 443, Protocol: "GRPC"},
+								Bind:  "10.0.0.2",
+								Hosts: []string{"*"},
+							},
+						},
+					},
+				},
+				{
+					Meta: config.Meta{Name: "gateway3", Namespace: "testns", GroupVersionKind: gvk.Gateway},
+					Spec: &networking.Gateway{
+						Servers: []*networking.Server{
+							{
+								Port:  &networking.Port{Name: "grpc", Number: 443, Protocol: "GRPC"},
+								Hosts: []string{"grpc.example.org"},
+							},
+						},
+					},
+				},
+			},
+			[]config.Config{
+				{
+					Meta: config.Meta{Name: uuid.NewString(), Namespace: uuid.NewString(), GroupVersionKind: gvk.VirtualService},
+					Spec: &networking.VirtualService{
+						Gateways: []string{"testns/gateway1"},
+						Hosts:    []string{"*"},
+						Http: []*networking.HTTPRoute{
+							{
+								Route: []*networking.HTTPRouteDestination{
+									{
+										Destination: &networking.Destination{
+											Host: "example.org",
+											Port: &networking.PortSelector{
+												Number: 80,
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				{
+					Meta: config.Meta{Name: uuid.NewString(), Namespace: uuid.NewString(), GroupVersionKind: gvk.VirtualService},
+					Spec: &networking.VirtualService{
+						Gateways: []string{"testns/gateway2"},
+						Hosts:    []string{"*"},
+						Http: []*networking.HTTPRoute{
+							{
+								Route: []*networking.HTTPRouteDestination{
+									{
+										Destination: &networking.Destination{
+											Host: "grpc.example.org",
+											Port: &networking.PortSelector{
+												Number: 80,
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				{
+					Meta: config.Meta{Name: uuid.NewString(), Namespace: uuid.NewString(), GroupVersionKind: gvk.VirtualService},
+					Spec: &networking.VirtualService{
+						Gateways: []string{"testns/gateway3"},
+						Hosts:    []string{"grpc.example.org"},
+						Http: []*networking.HTTPRoute{
+							{
+								Route: []*networking.HTTPRouteDestination{
+									{
+										Destination: &networking.Destination{
+											Host: "grpc.example.org",
+											Port: &networking.PortSelector{
+												Number: 80,
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			[]string{"10.0.0.1_443", "10.0.0.2_443", "0.0.0.0_443"},
+		},
+		{
 			"gateway with multiple HTTPS servers with bind and same host",
 			&pilot_model.Proxy{},
 			[]config.Config{

releasenotes/notes/43688.yaml
@@ -1,0 +1,9 @@
+apiVersion: release-notes/v2
+kind: bug-fix
+area: traffic-management
+issue:
+  - https://github.com/istio/istio/issues/43688
+releaseNotes:
+  - |
+    **Fixed** an issue that when there are different Binds specified in the Gateways with the same port and different protocols, listeners are not generated correctly.
+

commit ff22ac249bee7574a1f7e02daeebfe3131c736be

Skip+Warn instead of NACK on invalid TLS gateway
Fixes https://github.com/istio/istio/issues/47511
Regression from https://github.com/istio/istio/pull/43690

pilot/pkg/model/gateway.go
@@ -267,19 +267,20 @@ func MergeGateways(gateways []gatewayWithInstances, proxy *Proxy, ps *PushContex
 							serversByRouteName[routeName] = []*networking.Server{s}
 						}
 						// build the port bind map for none plain text protocol, thus can avoid protocol conflict if it's different bind
-						if bindsPortMap, ok := nonPlainTextGatewayPortsBindMap[resolvedPort]; ok && !bindsPortMap.Contains(serverPort.Bind) {
-							bindsPortMap.Insert(serverPort.Bind)
+						var newBind bool
+						if bindsPortMap, ok := nonPlainTextGatewayPortsBindMap[resolvedPort]; ok {
+							newBind = !bindsPortMap.InsertContains(serverPort.Bind)
 						} else {
-							bindsPortMap := sets.String{}
-							bindsPortMap.Insert(serverPort.Bind)
-							nonPlainTextGatewayPortsBindMap[resolvedPort] = bindsPortMap
+							nonPlainTextGatewayPortsBindMap[resolvedPort] = sets.New(serverPort.Bind)
+							newBind = true
 						}
 						// If the bind/port combination is not being used as non-plaintext, they are different
 						// listeners and won't get conflicted even with same port different protocol
 						// i.e 0.0.0.0:443:GRPC/1.0.0.1:443:GRPC/1.0.0.2:443:HTTPS they are not conflicted, otherwise
 						// We have another TLS server on the same port. Can differentiate servers using SNI
-						if s.Tls == nil && !nonPlainTextGatewayPortsBindMap[resolvedPort].Contains(serverPort.Bind) {
+						if s.Tls == nil && !newBind {
 							log.Warnf("TLS server without TLS options %s %s", gatewayName, s.String())
+							RecordRejectedConfig(gatewayName)
 							continue
 						}
 						if mergedServers[serverPort] == nil {

pilot/pkg/networking/core/v1alpha3/gateway_test.go
@@ -2899,6 +2899,67 @@ func TestBuildGatewayListeners(t *testing.T) {
 			[]string{"10.0.0.1_443", "10.0.0.2_443", "0.0.0.0_443"},
 		},
 		{
+			"gateway with HTTPS/TCP invalid configuration",
+			&pilot_model.Proxy{},
+			[]config.Config{
+				{
+					Meta: config.Meta{Name: "gateway1", Namespace: "testns", GroupVersionKind: gvk.Gateway},
+					Spec: &networking.Gateway{
+						Servers: []*networking.Server{
+							{
+								Port:  &networking.Port{Name: "https", Number: 443, Protocol: "HTTPS"},
+								Hosts: []string{"*.1.example.com"},
+								Tls:   &networking.ServerTLSSettings{CredentialName: "test", Mode: networking.ServerTLSSettings_SIMPLE},
+							},
+							{
+								Port:  &networking.Port{Name: "tcp", Number: 443, Protocol: "TCP"},
+								Hosts: []string{"*.1.example.com"},
+							},
+						},
+					},
+				},
+				{
+					Meta: config.Meta{Name: "gateway2", Namespace: "testns", GroupVersionKind: gvk.Gateway},
+					Spec: &networking.Gateway{
+						Servers: []*networking.Server{
+							{
+								Port:  &networking.Port{Name: "https", Number: 443, Protocol: "HTTPS"},
+								Hosts: []string{"*.2.example.com"},
+								Tls:   &networking.ServerTLSSettings{CredentialName: "test", Mode: networking.ServerTLSSettings_SIMPLE},
+							},
+							{
+								Port:  &networking.Port{Name: "tcp", Number: 443, Protocol: "TCP"},
+								Hosts: []string{"*.2.example.com"},
+							},
+						},
+					},
+				},
+			},
+			[]config.Config{
+				{
+					Meta: config.Meta{Name: uuid.NewString(), Namespace: uuid.NewString(), GroupVersionKind: gvk.VirtualService},
+					Spec: &networking.VirtualService{
+						Gateways: []string{"testns/gateway1"},
+						Hosts:    []string{"*"},
+						Tcp: []*networking.TCPRoute{{
+							Route: []*networking.RouteDestination{{Destination: &networking.Destination{Host: "example.com"}}},
+						}},
+					},
+				},
+				{
+					Meta: config.Meta{Name: uuid.NewString(), Namespace: uuid.NewString(), GroupVersionKind: gvk.VirtualService},
+					Spec: &networking.VirtualService{
+						Gateways: []string{"testns/gateway2"},
+						Hosts:    []string{"*"},
+						Tcp: []*networking.TCPRoute{{
+							Route: []*networking.RouteDestination{{Destination: &networking.Destination{Host: "example.com"}}},
+						}},
+					},
+				},
+			},
+			[]string{"0.0.0.0_443"},
+		},
+		{
 			"gateway with multiple HTTPS servers with bind and same host",
 			&pilot_model.Proxy{},
 			[]config.Config{