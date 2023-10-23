cc26bd5f223ad3a5824f1df09dcc13e8bcbb5eaa
xiaopeng <hanxiaop8@outlook.com>
2023-10-23 10:58:58 +0800

    allow missing eds in file configdump

--- istioctl/pkg/writer/envoy/configdump/configdump.go
+++ istioctl/pkg/writer/envoy/configdump/configdump.go
@@ -30,8 +30,9 @@ import (
 
 // ConfigWriter is a writer for processing responses from the Envoy Admin config_dump endpoint
 type ConfigWriter struct {
-	Stdout     io.Writer
-	configDump *configdump.Wrapper
+	Stdout          io.Writer
+	configDump      *configdump.Wrapper
+	AllowMissingEds bool
 }
 
 // includeConfigType is a flag to indicate whether to include the config type in the output
