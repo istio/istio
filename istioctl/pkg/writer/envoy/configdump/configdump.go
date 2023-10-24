0e00fdff544c4d4fd1f5166977a30c695544b385
xiaopeng <hanxiaop8@outlook.com>
2023-10-24 15:31:57 +0800

    Revert "allow missing eds in file configdump"
    This reverts commit cc26bd5f223ad3a5824f1df09dcc13e8bcbb5eaa.

--- istioctl/pkg/writer/envoy/configdump/configdump.go
+++ istioctl/pkg/writer/envoy/configdump/configdump.go
@@ -30,9 +30,8 @@ import (
 
 // ConfigWriter is a writer for processing responses from the Envoy Admin config_dump endpoint
 type ConfigWriter struct {
-	Stdout          io.Writer
-	configDump      *configdump.Wrapper
-	AllowMissingEds bool
+	Stdout     io.Writer
+	configDump *configdump.Wrapper
 }
 
 // includeConfigType is a flag to indicate whether to include the config type in the output
