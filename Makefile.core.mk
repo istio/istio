ab23f2c3951f64d58846dd36b71e8a14c916e189
Istio Automation <istio-testing-bot@google.com>
2023-10-25 14:41:27 -0700

    Update BASE_VERSION to 1.18-2023-10-25T20-52-18 (#47592)

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -38,7 +38,7 @@ SHELL := /bin/bash -o pipefail
 export VERSION ?= 1.18-dev
 
 # Base version of Istio image to use
-BASE_VERSION ?= 1.18-2023-10-12T19-01-45
+BASE_VERSION ?= 1.18-2023-10-25T20-52-18
 ISTIO_BASE_REGISTRY ?= gcr.io/istio-release
 
 export GO111MODULE ?= on
