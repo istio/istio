istio/istio

commit 81ad7b8fcaee61319bddd367a1ba15ba1e3811cb

do not push when unrelated service labels and annotations change (#42617)
* do not push when unrelated service labels and annotations change

Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

* rm comment

Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

* compare services

Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

* lable compare

Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

* fix ut

Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

* remove config compare

Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

* remove config compare test

Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

* remove label and add flag

Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

* compile

* implement Equals

Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

pilot/pkg/model/service.go
@@ -30,6 +30,7 @@ import (
 
 	endpoint "github.com/envoyproxy/go-control-plane/envoy/config/endpoint/v3"
 	"github.com/mitchellh/copystructure"
+	"golang.org/x/exp/maps"
 
 	"istio.io/api/label"
 	"istio.io/istio/pilot/pkg/serviceregistry/provider"
@@ -637,6 +638,50 @@ func (s *ServiceAttributes) DeepCopy() ServiceAttributes {
 	return out
 }
 
+// Equals checks whether the attributes are equal from the passed in service.
+func (s *ServiceAttributes) Equals(other *ServiceAttributes) bool {
+	if s == nil {
+		return other == nil
+	}
+	if other == nil {
+		return s == nil
+	}
+
+	if !maps.Equal(s.Labels, other.Labels) {
+		return false
+	}
+
+	if !maps.Equal(s.LabelSelectors, other.LabelSelectors) {
+		return false
+	}
+
+	if !maps.Equal(s.ExportTo, other.ExportTo) {
+		return false
+	}
+
+	if len(s.ClusterExternalAddresses.Addresses) != len(other.ClusterExternalAddresses.Addresses) {
+		return false
+	}
+
+	for k, v1 := range s.ClusterExternalAddresses.Addresses {
+		if v2, ok := other.ClusterExternalAddresses.Addresses[k]; !ok || !stringSliceEqual(v1, v2) {
+			return false
+		}
+	}
+
+	if len(s.ClusterExternalPorts) != len(other.ClusterExternalPorts) {
+		return false
+	}
+
+	for k, v1 := range s.ClusterExternalPorts {
+		if v2, ok := s.ClusterExternalPorts[k]; !ok || !maps.Equal(v1, v2) {
+			return false
+		}
+	}
+	return s.Name == other.Name && s.Namespace == other.Namespace && s.NodeLocal == other.NodeLocal &&
+		s.ServiceRegistry == other.ServiceRegistry && s.Type == other.Type
+}
+
 // ServiceDiscovery enumerates Istio service instances.
 // nolint: lll
 type ServiceDiscovery interface {
@@ -737,6 +782,35 @@ func (ports PortList) GetByPort(num int) (*Port, bool) {
 		}
 	}
 	return nil, false
+}
+
+func (p *Port) Equals(other *Port) bool {
+	if p == nil {
+		return other == nil
+	}
+	if other == nil {
+		return p == nil
+	}
+	return p.Name == other.Name && p.Port == other.Port && p.Protocol == other.Protocol
+}
+
+func (ports PortList) Equals(other PortList) bool {
+	if len(ports) != len(other) {
+		return false
+	}
+	for _, p1 := range ports {
+		found := false
+		for _, p2 := range other {
+			if p1.Equals(p2) {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return false
+		}
+	}
+	return true
 }
 
 // External predicate checks whether the service is external
@@ -906,6 +980,39 @@ func (s *Service) DeepCopy() *Service {
 	}
 	out.ClusterVIPs = s.ClusterVIPs.DeepCopy()
 	return &out
+}
+
+// Equals compares two service objects.
+func (s *Service) Equals(other *Service) bool {
+	if s == nil {
+		return other == nil
+	}
+	if other == nil {
+		return s == nil
+	}
+
+	if !s.Attributes.Equals(&other.Attributes) {
+		return false
+	}
+
+	if !s.Ports.Equals(other.Ports) {
+		return false
+	}
+	if !stringSliceEqual(s.ServiceAccounts, other.ServiceAccounts) {
+		return false
+	}
+
+	if len(s.ClusterVIPs.Addresses) != len(other.ClusterVIPs.Addresses) {
+		return false
+	}
+	for k, v1 := range s.ClusterVIPs.Addresses {
+		if v2, ok := other.ClusterVIPs.Addresses[k]; !ok || !stringSliceEqual(v1, v2) {
+			return false
+		}
+	}
+
+	return s.DefaultAddress == other.DefaultAddress && s.AutoAllocatedIPv4Address == other.AutoAllocatedIPv4Address &&
+		s.AutoAllocatedIPv6Address == other.AutoAllocatedIPv6Address && s.Hostname == other.Hostname && s.MeshExternal == other.MeshExternal
 }
 
 // DeepCopy creates a clone of IstioEndpoint.

pilot/pkg/model/service_test.go
@@ -278,6 +278,214 @@ func TestWorkloadInstanceEqual(t *testing.T) {
 	}
 }
 
+func TestServicesEqual(t *testing.T) {
+	cases := []struct {
+		first    *Service
+		other    *Service
+		shouldEq bool
+		name     string
+	}{
+		{
+			first:    nil,
+			other:    &Service{},
+			shouldEq: false,
+			name:     "first nil services",
+		},
+		{
+			first:    &Service{},
+			other:    nil,
+			shouldEq: false,
+			name:     "other nil services",
+		},
+		{
+			first:    nil,
+			other:    nil,
+			shouldEq: true,
+			name:     "both nil services",
+		},
+		{
+			first:    &Service{},
+			other:    &Service{},
+			shouldEq: true,
+			name:     "two empty services",
+		},
+		{
+			first: &Service{
+				Ports: port7000,
+			},
+			other: &Service{
+				Ports: port7442,
+			},
+			shouldEq: false,
+			name:     "different ports",
+		},
+		{
+			first: &Service{
+				Ports: twoMatchingPorts,
+			},
+			other: &Service{
+				Ports: twoMatchingPorts,
+			},
+			shouldEq: true,
+			name:     "matching ports",
+		},
+		{
+			first: &Service{
+				ServiceAccounts: []string{"sa1"},
+			},
+			other: &Service{
+				ServiceAccounts: []string{"sa1"},
+			},
+			shouldEq: true,
+			name:     "matching service accounts",
+		},
+		{
+			first: &Service{
+				ServiceAccounts: []string{"sa1"},
+			},
+			other: &Service{
+				ServiceAccounts: []string{"sa2"},
+			},
+			shouldEq: false,
+			name:     "different service accounts",
+		},
+		{
+			first: &Service{
+				ClusterVIPs: AddressMap{
+					Addresses: map[cluster.ID][]string{
+						"cluster-1": {"c1-vip1,c1-vip2"},
+						"cluster-2": {"c2-vip1,c2-vip2"},
+					},
+				},
+			},
+			other: &Service{
+				ClusterVIPs: AddressMap{
+					Addresses: map[cluster.ID][]string{
+						"cluster-1": {"c1-vip1,c1-vip2"},
+						"cluster-2": {"c2-vip1,c2-vip2"},
+					},
+				},
+			},
+			shouldEq: true,
+			name:     "matching cluster VIPs",
+		},
+		{
+			first: &Service{
+				ClusterVIPs: AddressMap{
+					Addresses: map[cluster.ID][]string{
+						"cluster-1": {"c1-vip1,c1-vip2"},
+					},
+				},
+			},
+			other: &Service{
+				ClusterVIPs: AddressMap{
+					Addresses: map[cluster.ID][]string{
+						"cluster-1": {"c1-vip1,c1-vip2"},
+						"cluster-2": {"c2-vip1,c2-vip2"},
+					},
+				},
+			},
+			shouldEq: false,
+			name:     "different cluster VIPs",
+		},
+		{
+			first: &Service{
+				Attributes: ServiceAttributes{
+					Name:      "test",
+					Namespace: "testns",
+					Labels: map[string]string{
+						"label-1": "value-1",
+					},
+				},
+			},
+			other: &Service{
+				Attributes: ServiceAttributes{
+					Name:      "test",
+					Namespace: "testns",
+					Labels: map[string]string{
+						"label-1": "value-1",
+					},
+				},
+			},
+			shouldEq: true,
+			name:     "same service attributes",
+		},
+		{
+			first: &Service{
+				Attributes: ServiceAttributes{
+					Name:      "test",
+					Namespace: "testns",
+					Labels: map[string]string{
+						"label-1": "value-1",
+					},
+				},
+			},
+			other: &Service{
+				Attributes: ServiceAttributes{
+					Name:      "test",
+					Namespace: "testns",
+					Labels: map[string]string{
+						"label-1": "value-2",
+					},
+				},
+			},
+			shouldEq: false,
+			name:     "different service attributes",
+		},
+		{
+			first: &Service{
+				ClusterVIPs: AddressMap{
+					Addresses: map[cluster.ID][]string{
+						"cluster-1": {"c1-vip1,c1-vip2"},
+					},
+				},
+				ServiceAccounts: []string{"sa-1", "sa-2"},
+				Ports:           twoMatchingPorts,
+				Attributes: ServiceAttributes{
+					Name:      "test",
+					Namespace: "testns",
+					Labels: map[string]string{
+						"label-1": "value-1",
+					},
+				},
+			},
+			other: &Service{
+				ClusterVIPs: AddressMap{
+					Addresses: map[cluster.ID][]string{
+						"cluster-1": {"c1-vip1,c1-vip2"},
+						"cluster-2": {"c2-vip1,c2-vip2"},
+					},
+				},
+				Ports:           twoMatchingPorts,
+				ServiceAccounts: []string{"sa-1", "sa-2"},
+				Attributes: ServiceAttributes{
+					Name:      "test",
+					Namespace: "testns",
+					Labels: map[string]string{
+						"label-1": "value-2",
+					},
+				},
+			},
+			shouldEq: false,
+			name:     "service with just label change",
+		},
+	}
+
+	for _, testCase := range cases {
+		t.Run("ServicesEqual: "+testCase.name, func(t *testing.T) {
+			isEq := testCase.first.Equals(testCase.other)
+			if isEq != testCase.shouldEq {
+				t.Errorf(
+					"equality of %v , and %v are not equal expected %t",
+					testCase.first,
+					testCase.other,
+					testCase.shouldEq,
+				)
+			}
+		})
+	}
+}
+
 func BenchmarkBuildSubsetKey(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		_ = BuildSubsetKey(TrafficDirectionInbound, "v1", "someHost", 80)

pilot/pkg/serviceregistry/aggregate/controller_test.go
@@ -323,7 +323,7 @@ func TestAddRegistry(t *testing.T) {
 		if clusterID == "cluster2" {
 			counter = registry2Counter
 		}
-		ctrl.AppendServiceHandlerForCluster(clusterID, func(service *model.Service, event model.Event) {
+		ctrl.AppendServiceHandlerForCluster(clusterID, func(_, curr *model.Service, event model.Event) {
 			counter.Add(1)
 		})
 		ctrl.AddRegistry(r)
@@ -332,12 +332,12 @@ func TestAddRegistry(t *testing.T) {
 		t.Fatalf("Expected length of the registries slice should be 2, got %d", l)
 	}
 
-	registries[0].Controller.(*mock.Controller).OnServiceEvent(mock.HelloService, model.EventAdd)
-	registries[1].Controller.(*mock.Controller).OnServiceEvent(mock.WorldService, model.EventAdd)
+	registries[0].Controller.(*mock.Controller).OnServiceEvent(nil, mock.HelloService, model.EventAdd)
+	registries[1].Controller.(*mock.Controller).OnServiceEvent(nil, mock.WorldService, model.EventAdd)
 
 	ctrl.DeleteRegistry(registries[1].Cluster(), registries[1].Provider())
 	ctrl.UnRegisterHandlersForCluster(registries[1].Cluster())
-	registries[0].Controller.(*mock.Controller).OnServiceEvent(mock.HelloService, model.EventAdd)
+	registries[0].Controller.(*mock.Controller).OnServiceEvent(nil, mock.HelloService, model.EventAdd)
 
 	if registry1Counter.Load() != 3 {
 		t.Errorf("cluster1 expected 3 event, but got %d", registry1Counter.Load())

pilot/pkg/serviceregistry/kube/controller/controller_test.go
@@ -1851,7 +1851,7 @@ func TestController_ExternalNameService(t *testing.T) {
 			deleteWg := sync.WaitGroup{}
 			controller, fx := NewFakeControllerWithOptions(t, FakeControllerOptions{
 				Mode: mode,
-				ServiceHandler: func(_ *model.Service, e model.Event) {
+				ServiceHandler: func(_, _ *model.Service, e model.Event) {
 					if e == model.EventDelete {
 						deleteWg.Done()
 					}
@@ -2782,7 +2782,7 @@ func TestKubeEndpointsControllerOnEvent(t *testing.T) {
 		t.Run(EndpointModeNames[tc.mode], func(t *testing.T) {
 			controller, _ := NewFakeControllerWithOptions(t, FakeControllerOptions{Mode: tc.mode})
 
-			if err := controller.endpoints.onEvent(tc.tombstone, model.EventDelete); err != nil {
+			if err := controller.endpoints.onEvent(nil, tc.tombstone, model.EventDelete); err != nil {
 				t.Errorf("unexpected error: %v", err)
 			}
 		})

pilot/pkg/bootstrap/server.go
@@ -840,13 +840,19 @@ func (s *Server) cachesSynced() bool {
 func (s *Server) initRegistryEventHandlers() {
 	log.Info("initializing registry event handlers")
 	// Flush cached discovery responses whenever services configuration change.
-	serviceHandler := func(svc *model.Service, _ model.Event) {
-		pushReq := &model.PushRequest{
-			Full:           true,
-			ConfigsUpdated: sets.New(model.ConfigKey{Kind: kind.ServiceEntry, Name: string(svc.Hostname), Namespace: svc.Attributes.Namespace}),
-			Reason:         []model.TriggerReason{model.ServiceUpdate},
+	serviceHandler := func(prev, curr *model.Service, event model.Event) {
+		needsPush := true
+		if event == model.EventUpdate {
+			needsPush = serviceUpdateNeedsPush(prev, curr)
 		}
-		s.XDSServer.ConfigUpdate(pushReq)
+		if needsPush {
+			pushReq := &model.PushRequest{
+				Full:           true,
+				ConfigsUpdated: sets.New(model.ConfigKey{Kind: kind.ServiceEntry, Name: string(curr.Hostname), Namespace: curr.Attributes.Namespace}),
+				Reason:         []model.TriggerReason{model.ServiceUpdate},
+			}
+			s.XDSServer.ConfigUpdate(pushReq)
+		}
 	}
 	s.ServiceController().AppendServiceHandler(serviceHandler)
 
@@ -1333,4 +1339,14 @@ func (s *Server) serveHTTP() error {
 	}()
 	s.httpAddr = httpListener.Addr().String()
 	return nil
+}
+
+func serviceUpdateNeedsPush(prev, curr *model.Service) bool {
+	if !features.EnableOptimizedServicePush {
+		return true
+	}
+	if prev == nil {
+		return true
+	}
+	return !prev.Equals(curr)
 }

pilot/pkg/features/pilot.go
@@ -693,6 +693,9 @@ var (
 
 	EnableDualStack = env.RegisterBoolVar("ISTIO_DUAL_STACK", false,
 		"If enabled, pilot will configure clusters/listeners/routes for dual stack capability.").Get()
+
+	EnableOptimizedServicePush = env.RegisterBoolVar("ISTIO_ENABLE_OPTIMIZED_SERVICE_PUSH", true,
+		"If enabled, Istiod will not push changes on arbitraty annotation change.").Get()
 )
 
 // EnableEndpointSliceController returns the value of the feature flag and whether it was actually specified.

pilot/pkg/model/controller.go
@@ -20,6 +20,8 @@ import (
 	"istio.io/istio/pkg/cluster"
 )
 
+type ServiceHandler func(*Service, *Service, Event)
+
 // Controller defines an event controller loop.  Proxy agent registers itself
 // with the controller loop and receives notifications on changes to the
 // service topology or changes to the configuration artifacts.
@@ -39,7 +41,7 @@ type Controller interface {
 	// For per cluster handlers, they should be registered by the `AppendXXXHandlerForCluster` interface.
 
 	// AppendServiceHandler notifies about changes to the service catalog.
-	AppendServiceHandler(f func(*Service, Event))
+	AppendServiceHandler(f ServiceHandler)
 
 	// AppendWorkloadHandler notifies about changes to workloads. This differs from InstanceHandler,
 	// which deals with service instances (the result of a merge of Service and Workload)
@@ -57,7 +59,7 @@ type AggregateController interface {
 	Controller
 	// AppendServiceHandlerForCluster is similar to Controller.AppendServiceHandler,
 	// but it is used to store the handler from a specific cluster.
-	AppendServiceHandlerForCluster(clusterID cluster.ID, f func(*Service, Event))
+	AppendServiceHandlerForCluster(clusterID cluster.ID, f ServiceHandler)
 	// AppendWorkloadHandlerForCluster is similar to Controller.AppendWorkloadHandler,
 	// but it is used to store the handler from a specific cluster.
 	AppendWorkloadHandlerForCluster(clusterID cluster.ID, f func(*WorkloadInstance, Event))
@@ -67,14 +69,14 @@ type AggregateController interface {
 // ControllerHandlers is a utility to help Controller implementations manage their lists of handlers.
 type ControllerHandlers struct {
 	mutex            sync.RWMutex
-	serviceHandlers  []func(*Service, Event)
+	serviceHandlers  []ServiceHandler
 	workloadHandlers []func(*WorkloadInstance, Event)
 }
 
-func (c *ControllerHandlers) AppendServiceHandler(f func(*Service, Event)) {
+func (c *ControllerHandlers) AppendServiceHandler(f ServiceHandler) {
 	// Copy on write.
 	c.mutex.Lock()
-	handlers := make([]func(*Service, Event), 0, len(c.serviceHandlers)+1)
+	handlers := make([]ServiceHandler, 0, len(c.serviceHandlers)+1)
 	handlers = append(handlers, c.serviceHandlers...)
 	handlers = append(handlers, f)
 	c.serviceHandlers = handlers
@@ -91,7 +93,7 @@ func (c *ControllerHandlers) AppendWorkloadHandler(f func(*WorkloadInstance, Eve
 	c.mutex.Unlock()
 }
 
-func (c *ControllerHandlers) GetServiceHandlers() []func(*Service, Event) {
+func (c *ControllerHandlers) GetServiceHandlers() []ServiceHandler {
 	c.mutex.RLock()
 	defer c.mutex.RUnlock()
 	// Return a shallow copy of the array
@@ -105,9 +107,9 @@ func (c *ControllerHandlers) GetWorkloadHandlers() []func(*WorkloadInstance, Eve
 	return c.workloadHandlers
 }
 
-func (c *ControllerHandlers) NotifyServiceHandlers(svc *Service, event Event) {
+func (c *ControllerHandlers) NotifyServiceHandlers(prev, curr *Service, event Event) {
 	for _, f := range c.GetServiceHandlers() {
-		f(svc, event)
+		f(prev, curr, event)
 	}
 }
 

pilot/pkg/serviceregistry/aggregate/controller.go
@@ -76,9 +76,9 @@ func (c *Controller) addRegistry(registry serviceregistry.Instance, stop <-chan 
 	// Observe the registry for events.
 	registry.AppendNetworkGatewayHandler(c.NotifyGatewayHandlers)
 	registry.AppendServiceHandler(c.handlers.NotifyServiceHandlers)
-	registry.AppendServiceHandler(func(service *model.Service, event model.Event) {
+	registry.AppendServiceHandler(func(prev, curr *model.Service, event model.Event) {
 		for _, handlers := range c.getClusterHandlers() {
-			handlers.NotifyServiceHandlers(service, event)
+			handlers.NotifyServiceHandlers(prev, curr, event)
 		}
 	})
 }
@@ -345,7 +345,7 @@ func (c *Controller) HasSynced() bool {
 	return true
 }
 
-func (c *Controller) AppendServiceHandler(f func(*model.Service, model.Event)) {
+func (c *Controller) AppendServiceHandler(f model.ServiceHandler) {
 	c.handlers.AppendServiceHandler(f)
 }
 
@@ -355,7 +355,7 @@ func (c *Controller) AppendWorkloadHandler(f func(*model.WorkloadInstance, model
 	// c.handlers.AppendWorkloadHandler(f)
 }
 
-func (c *Controller) AppendServiceHandlerForCluster(id cluster.ID, f func(*model.Service, model.Event)) {
+func (c *Controller) AppendServiceHandlerForCluster(id cluster.ID, f model.ServiceHandler) {
 	c.storeLock.Lock()
 	defer c.storeLock.Unlock()
 	handler, ok := c.handlersByCluster[id]

commit a308d5cac7f74bdad51630c12031fed5264f6d4b

Actually run through TestServicesDiff cases and fix regression
This PR to actually run through service comparison test cases, and
ensure model.Service.Resolution is taken into account when comparing
model.Services, which is a regression introduced in
https://github.com/istio/istio/commit/81ad7b8fcae

Also add test coverage for service comparison cases.

pilot/pkg/model/service.go
@@ -1241,7 +1241,8 @@ func (s *Service) Equals(other *Service) bool {
 	}
 
 	return s.DefaultAddress == other.DefaultAddress && s.AutoAllocatedIPv4Address == other.AutoAllocatedIPv4Address &&
-		s.AutoAllocatedIPv6Address == other.AutoAllocatedIPv6Address && s.Hostname == other.Hostname && s.MeshExternal == other.MeshExternal
+		s.AutoAllocatedIPv6Address == other.AutoAllocatedIPv6Address && s.Hostname == other.Hostname &&
+		s.Resolution == other.Resolution && s.MeshExternal == other.MeshExternal
 }
 
 // DeepCopy creates a clone of IstioEndpoint.

pilot/pkg/model/service_test.go
@@ -22,6 +22,7 @@ import (
 	fuzz "github.com/google/gofuzz"
 
 	"istio.io/istio/pkg/cluster"
+	"istio.io/istio/pkg/config/constants"
 	"istio.io/istio/pkg/config/host"
 	"istio.io/istio/pkg/config/labels"
 	"istio.io/istio/pkg/config/visibility"
@@ -514,6 +515,42 @@ func TestServicesEqual(t *testing.T) {
 			},
 			shouldEq: false,
 			name:     "different internal traffic policies",
+		},
+		{
+			first:    &Service{Hostname: host.Name("foo.com")},
+			other:    &Service{Hostname: host.Name("foo1.com")},
+			shouldEq: false,
+			name:     "different hostname",
+		},
+		{
+			first:    &Service{DefaultAddress: constants.UnspecifiedIPv6},
+			other:    &Service{DefaultAddress: constants.UnspecifiedIP},
+			shouldEq: false,
+			name:     "different default address",
+		},
+		{
+			first:    &Service{AutoAllocatedIPv4Address: "240.240.0.100"},
+			other:    &Service{AutoAllocatedIPv4Address: "240.240.0.101"},
+			shouldEq: false,
+			name:     "different auto allocated IPv4 addresses",
+		},
+		{
+			first:    &Service{AutoAllocatedIPv6Address: "2001:2::f0f0:e351"},
+			other:    &Service{AutoAllocatedIPv6Address: "2001:2::f0f1:e351"},
+			shouldEq: false,
+			name:     "different auto allocated IPv6 addresses",
+		},
+		{
+			first:    &Service{Resolution: ClientSideLB},
+			other:    &Service{Resolution: Passthrough},
+			shouldEq: false,
+			name:     "different resolution",
+		},
+		{
+			first:    &Service{MeshExternal: true},
+			other:    &Service{MeshExternal: false},
+			shouldEq: false,
+			name:     "different mesh external setting",
 		},
 	}
 

pilot/pkg/serviceregistry/serviceentry/controller_test.go
@@ -1607,9 +1607,6 @@ func TestServicesDiff(t *testing.T) {
 	}
 
 	for _, tt := range cases {
-		if tt.name != "same config with additional endpoint" {
-			continue
-		}
 		t.Run(tt.name, func(t *testing.T) {
 			as := convertServices(*tt.current)
 			bs := convertServices(*tt.new)