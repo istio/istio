92b452303d55a263305a5038b9a15d82f415bb0f
zirain <zirain2009@gmail.com>
2023-11-14 11:39:48 +0800

    fix cacert_rotation TestReachability flake

--- tests/integration/security/cacert_rotation/main_test.go
+++ tests/integration/security/cacert_rotation/main_test.go
@@ -131,7 +131,8 @@ func TestReachability(t *testing.T) {
 				}
 
 				return false
-			}, retry.Timeout(time.Minute), retry.Delay(1*time.Second))
+			}, retry.Timeout(2*time.Minute), // Need more time for waiting
+				retry.Delay(1*time.Second))
 
 			// Verify traffic works between a and b after cert rotation
 			echotest.New(t, fromAndTo).
