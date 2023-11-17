777676ca6f4756c45d81a993d2d0bc52d212f5de
Li Chun <lichun823@gmail.com>
2023-11-17 14:59:22 +0800

    Wrap IsPodEnrolled for iptables and ebpf mode & minor change for some log level
    Signed-off-by: Li Chun <chun2.li@intel.com>

--- cni/pkg/ambient/net_linux.go
+++ cni/pkg/ambient/net_linux.go
@@ -1326,6 +1326,16 @@ func (s *Server) getEnrolledIPSets() sets.String {
 	return pods
 }
 
+func (s *Server) IsPodEnrolledInDp(pod *corev1.Pod) bool {
+	switch s.redirectMode {
+	case IptablesMode:
+		return IsPodInIpset(pod)
+	case EbpfMode:
+		return s.ebpfServer.IsPodIPEnrolled(pod.Status.PodIP)
+	}
+	return false
+}
+
 func addTProxyMarkRule() error {
 	// Set up tproxy marks
 	var rules []*netlink.Rule
