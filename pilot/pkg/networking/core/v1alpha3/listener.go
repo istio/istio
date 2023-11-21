istio/istio

commit 9b3a1fb4dc799fd78d6d35284e59211241841626

Always enable listener.ContinueOnListenerFiltersTimeout (#46235)
* correctly distinguish listener_filter_timeout nil with 0

* ut

* fix ut

* Fix error

* Fix error

* always enable continue on fillistener ter timeout

* fmt

pilot/pkg/networking/core/v1alpha3/listener.go
@@ -27,6 +27,7 @@ import (
 	envoyquicv3 "github.com/envoyproxy/go-control-plane/envoy/extensions/transport_sockets/quic/v3"
 	auth "github.com/envoyproxy/go-control-plane/envoy/extensions/transport_sockets/tls/v3"
 	"github.com/envoyproxy/go-control-plane/pkg/wellknown"
+	"google.golang.org/protobuf/types/known/durationpb"
 	wrappers "google.golang.org/protobuf/types/known/wrapperspb"
 
 	meshconfig "istio.io/api/mesh/v1alpha1"
@@ -1221,13 +1222,14 @@ func buildListener(opts buildListenerOpts, trafficDirection core.TrafficDirectio
 
 		res = &listener.Listener{
 			// TODO: need to sanitize the opts.bind if its a UDS socket, as it could have colons, that envoy doesn't like
-			Name:                    getListenerName(opts.bind, opts.port.Port, istionetworking.TransportProtocolTCP),
-			Address:                 util.BuildAddress(opts.bind, uint32(opts.port.Port)),
-			TrafficDirection:        trafficDirection,
-			ListenerFilters:         listenerFilters,
-			FilterChains:            filterChains,
-			BindToPort:              bindToPort,
-			ConnectionBalanceConfig: connectionBalance,
+			Name:                             getListenerName(opts.bind, opts.port.Port, istionetworking.TransportProtocolTCP),
+			Address:                          util.BuildAddress(opts.bind, uint32(opts.port.Port)),
+			TrafficDirection:                 trafficDirection,
+			ListenerFilters:                  listenerFilters,
+			FilterChains:                     filterChains,
+			BindToPort:                       bindToPort,
+			ConnectionBalanceConfig:          connectionBalance,
+			ContinueOnListenerFiltersTimeout: true,
 		}
 		// add extra addresses for the listener
 		if features.EnableDualStack && len(opts.extraBind) > 0 {
@@ -1236,9 +1238,6 @@ func buildListener(opts buildListenerOpts, trafficDirection core.TrafficDirectio
 
 		if opts.proxy.Type != model.Router {
 			res.ListenerFiltersTimeout = opts.push.Mesh.ProtocolDetectionTimeout
-			if res.ListenerFiltersTimeout != nil {
-				res.ContinueOnListenerFiltersTimeout = true
-			}
 		}
 	case istionetworking.TransportProtocolQUIC:
 		// TODO: switch on TransportProtocolQUIC is in too many places now. Once this is a bit
@@ -1258,6 +1257,7 @@ func buildListener(opts buildListenerOpts, trafficDirection core.TrafficDirectio
 				QuicOptions:            &listener.QuicProtocolOptions{},
 				DownstreamSocketConfig: &core.UdpSocketConfig{},
 			},
+			ContinueOnListenerFiltersTimeout: true,
 		}
 		// add extra addresses for the listener
 		if features.EnableDualStack && len(opts.extraBind) > 0 {
@@ -1608,8 +1608,7 @@ func removeListenerFilterTimeout(listeners []*listener.Listener) {
 		}
 
 		if !hasHTTPInspector && l.TrafficDirection == core.TrafficDirection_OUTBOUND {
-			l.ListenerFiltersTimeout = nil
-			l.ContinueOnListenerFiltersTimeout = false
+			l.ListenerFiltersTimeout = durationpb.New(0)
 		}
 	}
 }

pilot/pkg/networking/core/v1alpha3/listener_inbound.go
@@ -164,9 +164,10 @@ func (lb *ListenerBuilder) buildInboundHBONEListeners() []*listener.Listener {
 	// Now we have top level listener... but we must have an internal listener for each standard filter chain
 	// 1 listener per port; that listener will do protocol detection.
 	l := &listener.Listener{
-		Name:              MainInternalName,
-		ListenerSpecifier: &listener.Listener_InternalListener{InternalListener: &listener.Listener_InternalListenerConfig{}},
-		TrafficDirection:  core.TrafficDirection_INBOUND,
+		Name:                             MainInternalName,
+		ListenerSpecifier:                &listener.Listener_InternalListener{InternalListener: &listener.Listener_InternalListenerConfig{}},
+		TrafficDirection:                 core.TrafficDirection_INBOUND,
+		ContinueOnListenerFiltersTimeout: true,
 	}
 
 	inboundChainConfigs := lb.buildInboundChainConfigs()
@@ -271,9 +272,10 @@ func (lb *ListenerBuilder) buildInboundListener(name string, addresses []string,
 	}
 	address := util.BuildAddress(addresses[0], tPort)
 	l := &listener.Listener{
-		Name:             name,
-		Address:          address,
-		TrafficDirection: core.TrafficDirection_INBOUND,
+		Name:                             name,
+		Address:                          address,
+		TrafficDirection:                 core.TrafficDirection_INBOUND,
+		ContinueOnListenerFiltersTimeout: true,
 	}
 	if features.EnableDualStack && len(addresses) > 1 {
 		// add extra addresses for the listener
@@ -294,7 +296,6 @@ func (lb *ListenerBuilder) buildInboundListener(name string, addresses []string,
 	l.FilterChains = chains
 	l.ListenerFilters = populateListenerFilters(lb.node, l, bindToPort)
 	l.ListenerFiltersTimeout = lb.push.Mesh.GetProtocolDetectionTimeout()
-	l.ContinueOnListenerFiltersTimeout = true
 	return l
 }
 

pilot/pkg/networking/core/v1alpha3/listener_test.go
@@ -1371,13 +1371,15 @@ func testOutboundListenerFilterTimeout(t *testing.T, services ...*model.Service)
 			t.Fatalf("expected %d listeners, found %d", 2, len(listeners))
 		}
 
-		if listeners[0].ContinueOnListenerFiltersTimeout {
-			t.Fatalf("expected timeout disabled, found ContinueOnListenerFiltersTimeout %v",
-				listeners[0].ContinueOnListenerFiltersTimeout)
+		if listeners[0].ListenerFiltersTimeout == nil ||
+			(listeners[0].ListenerFiltersTimeout.GetSeconds() != 0 && listeners[0].ListenerFiltersTimeout.GetNanos() != 0) {
+			t.Fatalf("expected timeout disabled, found ContinueOnListenerFiltersTimeout %v, ListenerFiltersTimeout %v",
+				listeners[0].ContinueOnListenerFiltersTimeout,
+				listeners[0].ListenerFiltersTimeout)
 		}
-
-		if !listeners[1].ContinueOnListenerFiltersTimeout || listeners[1].ListenerFiltersTimeout == nil {
-			t.Fatalf("expected timeout enabled, found ContinueOnListenerFiltersTimeout %v, ListenerFiltersTimeout %v",
+		if listeners[1].ListenerFiltersTimeout == nil ||
+			(listeners[0].ListenerFiltersTimeout.GetSeconds() != 0 && listeners[0].ListenerFiltersTimeout.GetNanos() != 0) {
+			t.Fatalf("expected timeout disabled , found ContinueOnListenerFiltersTimeout %v, ListenerFiltersTimeout %v",
 				listeners[1].ContinueOnListenerFiltersTimeout,
 				listeners[1].ListenerFiltersTimeout)
 		}
@@ -1415,9 +1417,8 @@ func testOutboundListenerConflict(t *testing.T, services ...*model.Service) {
 			verifyHTTPFilterChainMatch(t, listeners[0].FilterChains[0])
 			verifyListenerFilters(t, listeners[0].ListenerFilters)
 
-			if !listeners[0].ContinueOnListenerFiltersTimeout || listeners[0].ListenerFiltersTimeout == nil {
-				t.Fatalf("expected timeout, found ContinueOnListenerFiltersTimeout %v, ListenerFiltersTimeout %v",
-					listeners[0].ContinueOnListenerFiltersTimeout,
+			if listeners[0].ListenerFiltersTimeout.GetSeconds() != 5 {
+				t.Fatalf("expected timeout 5s, found  ListenerFiltersTimeout %v",
 					listeners[0].ListenerFiltersTimeout)
 			}
 
@@ -1442,7 +1443,7 @@ func testOutboundListenerConflict(t *testing.T, services ...*model.Service) {
 			verifyHTTPFilterChainMatch(t, http)
 			verifyListenerFilters(t, listeners[0].ListenerFilters)
 
-			if !listeners[0].ContinueOnListenerFiltersTimeout || listeners[0].ListenerFiltersTimeout == nil {
+			if listeners[0].ListenerFiltersTimeout == nil {
 				t.Fatalf("expected timeout, found ContinueOnListenerFiltersTimeout %v, ListenerFiltersTimeout %v",
 					listeners[0].ContinueOnListenerFiltersTimeout,
 					listeners[0].ListenerFiltersTimeout)
@@ -2279,6 +2280,7 @@ func TestHttpProxyListener_Tracing(t *testing.T) {
 					MaxPathTagLength: tc.in.MaxPathTagLength,
 					Sampling:         tc.in.Sampling,
 				},
+				DiscoveryAddress: "istiod.istio-system.svc:15012",
 			}
 			listeners := buildListeners(t, TestOptions{MeshConfig: m}, nil)
 			httpProxy := xdstest.ExtractListener("127.0.0.1_15007", listeners)
@@ -2465,9 +2467,12 @@ func buildOutboundListeners(t *testing.T, proxy *model.Proxy, sidecarConfig *con
 	virtualService *config.Config, services ...*model.Service,
 ) []*listener.Listener {
 	t.Helper()
+	m := mesh.DefaultMeshConfig()
+	m.ProtocolDetectionTimeout = durationpb.New(5 * time.Second)
 	cg := NewConfigGenTest(t, TestOptions{
 		Services:       services,
 		ConfigPointers: []*config.Config{sidecarConfig, virtualService},
+		MeshConfig:     m,
 	})
 	listeners := NewListenerBuilder(proxy, cg.env.PushContext()).buildSidecarOutboundListeners(cg.SetupProxy(proxy), cg.env.PushContext())
 	xdstest.ValidateListeners(t, listeners)

commit 6f8056795f711bb53dabdd9153384004983fa3ad

Do not allow listener filter timeout on gateway
This fixes an edge case where a client connects to a gateway and takes
>15s to finish the TLS handshake. Regression from #46235. Note this PR
is not simply reverting that; the code has changed since that PR, and
this codepath is now exclusive to gateways (at the time, it was also
used for outbound)

pilot/pkg/networking/core/v1alpha3/listener.go
@@ -1115,13 +1115,17 @@ func buildGatewayListener(opts gatewayListenerOpts, transport istionetworking.Tr
 
 		res = &listener.Listener{
 			// TODO: need to sanitize the opts.bind if its a UDS socket, as it could have colons, that envoy doesn't like
-			Name:                             getListenerName(opts.bind, opts.port, istionetworking.TransportProtocolTCP),
-			Address:                          util.BuildAddress(opts.bind, uint32(opts.port)),
-			TrafficDirection:                 core.TrafficDirection_OUTBOUND,
-			ListenerFilters:                  listenerFilters,
-			FilterChains:                     filterChains,
-			ConnectionBalanceConfig:          connectionBalance,
-			ContinueOnListenerFiltersTimeout: true,
+			Name:                    getListenerName(opts.bind, opts.port, istionetworking.TransportProtocolTCP),
+			Address:                 util.BuildAddress(opts.bind, uint32(opts.port)),
+			TrafficDirection:        core.TrafficDirection_OUTBOUND,
+			ListenerFilters:         listenerFilters,
+			FilterChains:            filterChains,
+			ConnectionBalanceConfig: connectionBalance,
+			// For Gateways, we want no timeout. We should wait indefinitely for the TLS if we are sniffing.
+			// The timeout is useful for sidecars, where we may operate on server first traffic; for gateways if we have listener filters
+			// we know those filters are required.
+			ContinueOnListenerFiltersTimeout: false,
+			ListenerFiltersTimeout:           durationpb.New(0),
 		}
 		// add extra addresses for the listener
 		if features.EnableDualStack && len(opts.extraBind) > 0 {
@@ -1145,7 +1149,11 @@ func buildGatewayListener(opts gatewayListenerOpts, transport istionetworking.Tr
 				QuicOptions:            &listener.QuicProtocolOptions{},
 				DownstreamSocketConfig: &core.UdpSocketConfig{},
 			},
-			ContinueOnListenerFiltersTimeout: true,
+			// For Gateways, we want no timeout. We should wait indefinitely for the TLS if we are sniffing.
+			// The timeout is useful for sidecars, where we may operate on server first traffic; for gateways if we have listener filters
+			// we know those filters are required.
+			ContinueOnListenerFiltersTimeout: false,
+			ListenerFiltersTimeout:           durationpb.New(0),
 		}
 		// add extra addresses for the listener
 		if features.EnableDualStack && len(opts.extraBind) > 0 {