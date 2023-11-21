dd079baaaa8d549fcc320f352f5e5a00ed3db5ab
Istio Automation <istio-testing-bot@google.com>
2023-11-20 20:31:12 -0800

    [release-1.20] fix cacert_rotation TestReachability flake (#47954)
    * fix cacert_rotation TestReachability flake
    
    * increase timeout
    
    ---------
    
    Co-authored-by: zirain <zirain2009@gmail.com>

--- tests/integration/security/cacert_rotation/main_test.go
+++ tests/integration/security/cacert_rotation/main_test.go
@@ -131,7 +131,8 @@ func TestReachability(t *testing.T) {
 				}
 
 				return false
-			}, retry.Timeout(time.Minute), retry.Delay(1*time.Second))
+			}, retry.Timeout(5*time.Minute), // Need more time for waiting, see https://github.com/istio/istio/pull/47839#issuecomment-1809510888
+				retry.Delay(1*time.Second))
 
 			// Verify traffic works between a and b after cert rotation
 			echotest.New(t, fromAndTo).
