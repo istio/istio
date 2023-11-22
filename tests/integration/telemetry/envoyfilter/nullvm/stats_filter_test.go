2d53526cfc584d960d05f5ef07f228061d08d37a
John Howard <howardjohn@google.com>
2023-11-21 16:11:43 -0800

    enable ipv6 tests for telemetry

--- tests/integration/telemetry/envoyfilter/nullvm/stats_filter_test.go
+++ tests/integration/telemetry/envoyfilter/nullvm/stats_filter_test.go
@@ -43,7 +43,6 @@ func TestGRPCStats(t *testing.T) {
 func TestMain(m *testing.M) {
 	framework.NewSuite(m).
 		Label(label.CustomSetup).
-		Label(label.IPv4). // https://github.com/istio/istio/issues/35915
 		Setup(istio.Setup(common.GetIstioInstance(), setupConfig)).
 		Setup(common.TestSetup).
 		Run()
