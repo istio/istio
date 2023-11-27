91f3517d21fc7823684db40a6657ad75270bac66
Xiaopeng Han <hanxiaop8@outlook.com>
2023-11-28 01:55:37 +0800

    Drop support for policy v1beta1 (#48019)
    * drop support for policy v1beta1
    
    * add releasenotes

--- operator/pkg/controller/istiocontrolplane/istiocontrolplane_controller.go
+++ operator/pkg/controller/istiocontrolplane/istiocontrolplane_controller.go
@@ -108,18 +108,13 @@ func watchedResources(version *kubeversion.Info) []schema.GroupVersionKind {
 		{Group: "rbac.authorization.k8s.io", Version: "v1", Kind: name.ClusterRoleStr},
 		{Group: "rbac.authorization.k8s.io", Version: "v1", Kind: name.ClusterRoleBindingStr},
 		{Group: "apiextensions.k8s.io", Version: "v1", Kind: name.CRDStr},
+		{Group: "policy", Version: "v1", Kind: name.PDBStr},
 	}
 	// autoscaling v2 API is available on >=1.23
 	if kube.IsKubeAtLeastOrLessThanVersion(version, autoscalingV2MinK8SVersion, true) {
 		res = append(res, schema.GroupVersionKind{Group: "autoscaling", Version: "v2", Kind: name.HPAStr})
 	} else {
 		res = append(res, schema.GroupVersionKind{Group: "autoscaling", Version: "v2beta2", Kind: name.HPAStr})
-	}
-	// policy/v1 is available on >=1.21
-	if kube.IsKubeAtLeastOrLessThanVersion(version, pdbV1MinK8SVersion, true) {
-		res = append(res, schema.GroupVersionKind{Group: "policy", Version: "v1", Kind: name.PDBStr})
-	} else {
-		res = append(res, schema.GroupVersionKind{Group: "policy", Version: "v1beta1", Kind: name.PDBStr})
 	}
 	return res
 }
