264a6729878ba58b562b51e57bc062af92fda408
Zhonghu Xu <xuzhonghu@huawei.com>
2023-11-30 10:49:14 +0800

    make discoverySelector work on gateway deployment controller (#47946)
    * Apply namespace discovery selector to gateway deployment controller
    
    * update
    
    * ut
    
    * address comment
    
    * release note

--- pilot/pkg/config/kube/gateway/deploymentcontroller_test.go
+++ pilot/pkg/config/kube/gateway/deploymentcontroller_test.go
@@ -43,9 +43,11 @@ import (
 	"istio.io/istio/pkg/config/schema/gvk"
 	"istio.io/istio/pkg/config/schema/gvr"
 	"istio.io/istio/pkg/kube"
+	"istio.io/istio/pkg/kube/controllers"
 	"istio.io/istio/pkg/kube/inject"
 	"istio.io/istio/pkg/kube/kclient"
 	"istio.io/istio/pkg/kube/kclient/clienttest"
+	"istio.io/istio/pkg/kube/namespace"
 	istiolog "istio.io/istio/pkg/log"
 	"istio.io/istio/pkg/revisions"
 	"istio.io/istio/pkg/test"
@@ -53,9 +55,11 @@ import (
 	"istio.io/istio/pkg/test/util/assert"
 	"istio.io/istio/pkg/test/util/file"
 	"istio.io/istio/pkg/test/util/retry"
+	"istio.io/istio/pkg/util/sets"
 )
 
 func TestConfigureIstioGateway(t *testing.T) {
+	discoveryNamespacesFilter := &fakeDiscoveryNamespacesFilter{namespaces: sets.New("default")}
 	defaultNamespace := &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "default"}}
 	customClass := &v1beta1.GatewayClass{
 		ObjectMeta: metav1.ObjectMeta{
@@ -88,11 +92,13 @@ func TestConfigureIstioGateway(t *testing.T) {
 	}
 	proxyConfig := model.GetProxyConfigs(store, mesh.DefaultMeshConfig())
 	tests := []struct {
-		name    string
-		gw      v1beta1.Gateway
-		objects []runtime.Object
-		pcs     *model.ProxyConfigs
-		values  string
+		name                     string
+		gw                       v1beta1.Gateway
+		objects                  []runtime.Object
+		pcs                      *model.ProxyConfigs
+		values                   string
+		discoveryNamespaceFilter namespace.DiscoveryNamespacesFilter
+		ignore                   bool
 	}{
 		{
 			name: "simple",
@@ -105,7 +111,23 @@ func TestConfigureIstioGateway(t *testing.T) {
 					GatewayClassName: defaultClassName,
 				},
 			},
-			objects: defaultObjects,
+			objects:                  defaultObjects,
+			discoveryNamespaceFilter: discoveryNamespacesFilter,
+		},
+		{
+			name: "simple",
+			gw: v1beta1.Gateway{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "default",
+					Namespace: "default",
+				},
+				Spec: v1alpha2.GatewaySpec{
+					GatewayClassName: defaultClassName,
+				},
+			},
+			objects:                  defaultObjects,
+			discoveryNamespaceFilter: &fakeDiscoveryNamespacesFilter{namespaces: sets.New("non-default")},
+			ignore:                   true,
 		},
 		{
 			name: "manual-sa",
@@ -119,7 +141,8 @@ func TestConfigureIstioGateway(t *testing.T) {
 					GatewayClassName: defaultClassName,
 				},
 			},
-			objects: defaultObjects,
+			objects:                  defaultObjects,
+			discoveryNamespaceFilter: discoveryNamespacesFilter,
 		},
 		{
 			name: "manual-ip",
@@ -137,7 +160,8 @@ func TestConfigureIstioGateway(t *testing.T) {
 					}},
 				},
 			},
-			objects: defaultObjects,
+			objects:                  defaultObjects,
+			discoveryNamespaceFilter: discoveryNamespacesFilter,
 		},
 		{
 			name: "cluster-ip",
@@ -159,7 +183,8 @@ func TestConfigureIstioGateway(t *testing.T) {
 					}},
 				},
 			},
-			objects: defaultObjects,
+			objects:                  defaultObjects,
+			discoveryNamespaceFilter: discoveryNamespacesFilter,
 		},
 		{
 			name: "multinetwork",
@@ -179,7 +204,8 @@ func TestConfigureIstioGateway(t *testing.T) {
 					}},
 				},
 			},
-			objects: defaultObjects,
+			objects:                  defaultObjects,
+			discoveryNamespaceFilter: discoveryNamespacesFilter,
 		},
 		{
 			name: "waypoint",
@@ -269,7 +295,7 @@ func TestConfigureIstioGateway(t *testing.T) {
 			go tw.Run(stop)
 			d := NewDeploymentController(
 				client, cluster.ID(features.ClusterName), env, testInjectionConfig(t, tt.values), func(fn func()) {
-				}, tw, "")
+				}, tw, "", tt.discoveryNamespaceFilter)
 			d.patcher = func(gvr schema.GroupVersionResource, name string, namespace string, data []byte, subresources ...string) error {
 				b, err := yaml.JSONToYAML(data)
 				if err != nil {
@@ -283,8 +309,12 @@ func TestConfigureIstioGateway(t *testing.T) {
 			go d.Run(stop)
 			kube.WaitForCacheSync("test", stop, d.queue.HasSynced)
 
-			resp := timestampRegex.ReplaceAll(buf.Bytes(), []byte("lastTransitionTime: fake"))
-			util.CompareContent(t, resp, filepath.Join("testdata", "deployment", tt.name+".yaml"))
+			if tt.ignore {
+				assert.Equal(t, buf.String(), "")
+			} else {
+				resp := timestampRegex.ReplaceAll(buf.Bytes(), []byte("lastTransitionTime: fake"))
+				util.CompareContent(t, resp, filepath.Join("testdata", "deployment", tt.name+".yaml"))
+			}
 		})
 	}
 }
@@ -299,7 +329,7 @@ func TestVersionManagement(t *testing.T) {
 	})
 	tw := revisions.NewTagWatcher(c, "default")
 	env := &model.Environment{}
-	d := NewDeploymentController(c, "", env, testInjectionConfig(t, ""), func(fn func()) {}, tw, "")
+	d := NewDeploymentController(c, "", env, testInjectionConfig(t, ""), func(fn func()) {}, tw, "", nil)
 	reconciles := atomic.NewInt32(0)
 	wantReconcile := int32(0)
 	expectReconciled := func() {
@@ -430,3 +460,37 @@ metadata:
     gateway.istio.io/controller-version: "%d"
 `, version)
 }
+
+type fakeDiscoveryNamespacesFilter struct {
+	namespaces sets.String
+}
+
+func (d *fakeDiscoveryNamespacesFilter) Filter(obj any) bool {
+	if ns, ok := obj.(string); ok {
+		return d.namespaces.Contains(ns)
+	}
+
+	// When an object is deleted, obj could be a DeletionFinalStateUnknown marker item.
+	object := controllers.ExtractObject(obj)
+	if object == nil {
+		return false
+	}
+	ns := object.GetNamespace()
+	if _, ok := object.(*corev1.Namespace); ok {
+		ns = object.GetName()
+	}
+	// permit if object resides in a namespace labeled for discovery
+	return d.namespaces.Contains(ns)
+}
+
+func (d *fakeDiscoveryNamespacesFilter) SelectorsChanged(
+	discoverySelectors []*metav1.LabelSelector,
+) {
+}
+
+// GetMembers returns member namespaces
+func (d *fakeDiscoveryNamespacesFilter) GetMembers() sets.String {
+	return d.namespaces
+}
+
+func (d *fakeDiscoveryNamespacesFilter) AddHandler(f func(ns string, event model.Event)) {}
