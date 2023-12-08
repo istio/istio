4a0ea8797546c51ebeec33381b78c9642f9c1fde
John Howard <howardjohn@google.com>
2023-12-08 11:16:59 -0800

    helm: initial support for profiles
    Part of https://github.com/istio/istio/issues/47838

--- operator/cmd/mesh/manifest_shared_test.go
+++ operator/cmd/mesh/manifest_shared_test.go
@@ -314,7 +314,7 @@ func runManifestCommand(command string, filenames []string, flags string, chartS
 		filters := []string{}
 		filters = append(filters, fileSelect...)
 		// Everything needs these
-		filters = append(filters, "templates/_affinity.tpl", "templates/_helpers.tpl")
+		filters = append(filters, "templates/_affinity.tpl", "templates/_helpers.tpl", "templates/zzz_profile.yaml")
 		args += " --filter " + strings.Join(filters, ",")
 	}
 	args += " --set installPackagePath=" + string(chartSource)
