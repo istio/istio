d3141f901d5a90d84e44e70669efe7000956cac4
Xiaopeng Han <hanxiaop8@outlook.com>
2023-12-15 15:13:37 +0300

    Fix uninstall with files doesn't prune things correctly (#47961)
    * fix uninstall with file
    
    * fix lint
    
    * add releasenotes
    
    * add integration test
    
    * change custom test to before purge
    
    * use canary revision instead
    
    * debug what's left
    
    * revise test

--- operator/pkg/helmreconciler/prune_test.go
+++ operator/pkg/helmreconciler/prune_test.go
@@ -15,18 +15,13 @@
 package helmreconciler
 
 import (
-	"context"
 	_ "embed"
-	"os"
-	"path/filepath"
 	"sync"
 	"testing"
 
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/yaml"
 
@@ -37,12 +32,7 @@ import (
 	"istio.io/istio/operator/pkg/util/clog"
 	"istio.io/istio/operator/pkg/util/progress"
 	"istio.io/istio/pkg/kube"
-	"istio.io/istio/pkg/test/env"
 	"istio.io/istio/pkg/test/util/assert"
-)
-
-const (
-	testRevision = "test"
 )
 
 var (
@@ -51,60 +41,6 @@ var (
 	//go:embed testdata/iop-test-gw-2.yaml
 	iopTestGwData2 []byte
 )
-
-func TestHelmReconciler_DeleteControlPlaneByManifest(t *testing.T) {
-	t.Run("deleteControlPlaneByManifest", func(t *testing.T) {
-		cl := fake.NewClientBuilder().WithInterceptorFuncs(interceptorFunc).Build()
-		df := filepath.Join(env.IstioSrc, "manifests/profiles/default.yaml")
-		iopStr, err := os.ReadFile(df)
-		if err != nil {
-			t.Fatal(err)
-		}
-		iop := &v1alpha1.IstioOperator{}
-		if err := yaml.UnmarshalStrict(iopStr, iop); err != nil {
-			t.Fatal(err)
-		}
-		iop.Spec.Revision = testRevision
-		iop.Spec.InstallPackagePath = filepath.Join(env.IstioSrc, "manifests")
-
-		h := &HelmReconciler{
-			client:     cl,
-			kubeClient: kube.NewFakeClientWithVersion("24"),
-			opts: &Options{
-				ProgressLog: progress.NewLog(),
-				Log:         clog.NewDefaultLogger(),
-			},
-			iop:           iop,
-			countLock:     &sync.Mutex{},
-			prunedKindSet: map[schema.GroupKind]struct{}{},
-		}
-		manifestMap, err := h.RenderCharts()
-		if err != nil {
-			t.Fatalf("failed to render manifest: %v", err)
-		}
-		applyResourcesIntoCluster(t, h, manifestMap)
-		if err := h.DeleteControlPlaneByManifests(manifestMap, testRevision, false); err != nil {
-			t.Fatalf("HelmReconciler.DeleteControlPlaneByManifests() error = %v", err)
-		}
-		for _, gvk := range append(h.NamespacedResources(), ClusterCPResources...) {
-			receiver := &unstructured.Unstructured{}
-			receiver.SetGroupVersionKind(schema.GroupVersionKind{Group: gvk.Group, Version: gvk.Version, Kind: gvk.Kind})
-			objKey := client.ObjectKey{Namespace: "istio-system", Name: "istiod-test"}
-			if gvk.Kind == name.MutatingWebhookConfigurationStr {
-				objKey.Name = "istio-sidecar-injector-test"
-			}
-			// List does not work well here as that requires adding all resource types to the fake client scheme
-			if err := h.client.Get(context.TODO(), objKey, receiver); err != nil {
-				// the error is expected because we expect resources do not exist any more in the cluster
-				t.Logf(err.Error())
-			}
-			obj := receiver.Object
-			if obj["spec"] != nil {
-				t.Errorf("got resource: %s/%s from the cluster, expected to be deleted", receiver.GetKind(), receiver.GetName())
-			}
-		}
-	})
-}
 
 func applyResourcesIntoCluster(t *testing.T, h *HelmReconciler, manifestMap name.ManifestMap) {
 	for cn, ms := range manifestMap.Consolidated() {
