85da05d551be89d61ee329f40b606cf461275975
John Howard <howardjohn@google.com>
2024-01-08 16:57:52 -0800

    bump release builder

--- prow/release-commit.sh
+++ prow/release-commit.sh
@@ -36,7 +36,7 @@ docker run --rm --privileged "${DOCKER_HUB}/qemu-user-static" --reset -p yes
 export ISTIO_DOCKER_QEMU=true
 
 # Use a pinned version in case breaking changes are needed
-BUILDER_SHA=aa112ec6b6eff14eb35c72e5727865f380f34a86
+BUILDER_SHA=d18e5e5379bc8012855c8bcf7b00f6a64adbd12b
 
 # Reference to the next minor version of Istio
 # This will create a version like 1.4-alpha.sha
