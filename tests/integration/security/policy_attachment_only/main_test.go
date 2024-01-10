b05224af8792d06fe6da3915054d681ba0f4737e
Wenhao Li <69884412+liwenhao0810@users.noreply.github.com>
2024-01-09 17:47:52 -0800

    add customsetup label for policy attachment only test (#48720)
    * add customsetup label for policy attachment only test
    
    * add import"

--- tests/integration/security/policy_attachment_only/main_test.go
+++ tests/integration/security/policy_attachment_only/main_test.go
@@ -26,6 +26,7 @@ import (
 	"istio.io/istio/pkg/test/framework/components/istio"
 	"istio.io/istio/pkg/test/framework/components/jwt"
 	"istio.io/istio/pkg/test/framework/components/namespace"
+	"istio.io/istio/pkg/test/framework/label"
 	"istio.io/istio/pkg/test/framework/resource"
 )
 
@@ -46,6 +47,7 @@ var (
 func TestMain(m *testing.M) {
 	framework.
 		NewSuite(m).
+		Label(label.CustomSetup).
 		Setup(istio.Setup(&i, func(c resource.Context, cfg *istio.Config) {
 			if !c.Settings().EnableDualStack {
 				cfg.ControlPlaneValues = `
