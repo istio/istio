9c06bc7ace31034be20d2dd12ecbe5007bfba97a
Aryan Gupta <mraryan.ag@gmail.com>
2024-01-23 23:37:23 +0000

    [release-1.21] Automated branching step 4

--- prow/release-commit.sh
+++ prow/release-commit.sh
@@ -104,6 +104,6 @@ release-builder validate --release "${WORK_DIR}/out"
 
 if [[ -z "${DRY_RUN:-}" ]]; then
   release-builder publish --release "${WORK_DIR}/out" \
-    --gcsbucket "${GCS_BUCKET}" --gcsaliases "${TAG},${NEXT_VERSION}-dev,latest" \
-    --dockerhub "${DOCKER_HUB}" --dockertags "${TAG},${VERSION},${NEXT_VERSION}-dev,latest"
+    --gcsbucket "${GCS_BUCKET}" --gcsaliases "${TAG},${NEXT_VERSION}-dev" \
+    --dockerhub "${DOCKER_HUB}" --dockertags "${TAG},${VERSION},${NEXT_VERSION}-dev"
 fi
