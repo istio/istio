05e7bf42929e004302508b886942dc2100063f71
Istio Automation <istio-testing-bot@google.com>
2024-01-31 13:43:37 -0800

    Update BASE_VERSION to 1.21-2024-01-31T20-49-14 (#49121)

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -49,7 +49,7 @@ endif
 export VERSION
 
 # Base version of Istio image to use
-BASE_VERSION ?= master-2024-01-18T19-01-03
+BASE_VERSION ?= 1.21-2024-01-31T20-49-14
 ISTIO_BASE_REGISTRY ?= gcr.io/istio-release
 
 export GO111MODULE ?= on
