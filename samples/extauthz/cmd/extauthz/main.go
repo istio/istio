406e2c34aeab3b903bbcfe5fbdccd4363166d258
Jacek Ewertowski <jewertow@redhat.com>
2024-02-14 17:29:08 +0100

    Typo fixes
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>

--- samples/extauthz/cmd/extauthz/main.go
+++ samples/extauthz/cmd/extauthz/main.go
@@ -360,7 +360,7 @@ func main() {
 }
 
 func returnIfNotTooLong(body string) string {
-	// Maximum size of a header accepted by Envoy is 60MiB, so when the request body is bigger than 60MB,
+	// Maximum size of a header accepted by Envoy is 60KiB, so when the request body is bigger than 60KB,
 	// we don't return it in a response header to avoid rejecting it by Envoy and returning 431 to the client
 	if len(body) > 60000 {
 		return "<too-long>"
