istio/istio

commit 98d927a3f9fb92be01dda64504c51b5057ed0b7c

filter service instances based on service creation for round robin  (#41831)
* filter service instances based on service creattion for round robin

Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

* merge master

Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

* do not add instance

Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

* comments

Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

* init

Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

* fix ut

Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

* add comments

Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

* add test

Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

* lint

Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

pilot/pkg/serviceregistry/serviceentry/store.go
@@ -18,6 +18,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 
 	"istio.io/istio/pilot/pkg/model"
+	"istio.io/istio/pkg/util/sets"
 )
 
 // stores all the service instances from SE, WLE and pods
@@ -27,6 +28,9 @@ type serviceInstancesStore struct {
 	instances map[instancesKey]map[configKey][]*model.ServiceInstance
 	// instances only for serviceentry
 	instancesBySE map[types.NamespacedName]map[configKey][]*model.ServiceInstance
+	// instancesByHost tells whether the host has instances.
+	// This is used to validate that we only have one instance for DNS_ROUNDROBIN_LB.
+	instancesByHost sets.Set[string]
 }
 
 func (s *serviceInstancesStore) getByIP(ip string) []*model.ServiceInstance {
@@ -51,6 +55,8 @@ func (s *serviceInstancesStore) getByKey(key instancesKey) []*model.ServiceInsta
 
 func (s *serviceInstancesStore) deleteInstances(key configKey, instances []*model.ServiceInstance) {
 	for _, i := range instances {
+		ikey := makeInstanceKey(i)
+		s.instancesByHost.Delete(ikey.hostname.String())
 		delete(s.instances[makeInstanceKey(i)], key)
 		delete(s.ip2instance, i.Endpoint.Address)
 	}
@@ -60,9 +66,20 @@ func (s *serviceInstancesStore) deleteInstances(key configKey, instances []*mode
 func (s *serviceInstancesStore) addInstances(key configKey, instances []*model.ServiceInstance) {
 	for _, instance := range instances {
 		ikey := makeInstanceKey(instance)
+		// For DNSRoundRobinLB resolution type, check if service instances already exist and do not add
+		// if it already exist. This can happen if two Service Entries are created with same host name,
+		// resolution as DNS_ROUND_ROBIN and with same/different endpoints.
+		if instance.Service.Resolution == model.DNSRoundRobinLB &&
+			s.instancesByHost.Contains(ikey.hostname.String()) {
+			log.Warnf("skipping service %s from service entry %s with DnsRoundRobinLB. A service entry with the same host "+
+				"already exists. Only one locality lb end point is allowed for DnsRoundRobinLB services.",
+				ikey.hostname, key.name+"/"+key.namespace)
+			continue
+		}
 		if _, f := s.instances[ikey]; !f {
 			s.instances[ikey] = map[configKey][]*model.ServiceInstance{}
 		}
+		s.instancesByHost.Insert(ikey.hostname.String())
 		s.instances[ikey][key] = append(s.instances[ikey][key], instance)
 		s.ip2instance[instance.Endpoint.Address] = append(s.ip2instance[instance.Endpoint.Address], instance)
 	}

pilot/pkg/serviceregistry/serviceentry/store_test.go
@@ -23,13 +23,15 @@ import (
 	networking "istio.io/api/networking/v1alpha3"
 	"istio.io/istio/pilot/pkg/model"
 	"istio.io/istio/pkg/config/constants"
+	"istio.io/istio/pkg/util/sets"
 )
 
 func TestServiceInstancesStore(t *testing.T) {
 	store := serviceInstancesStore{
-		ip2instance:   map[string][]*model.ServiceInstance{},
-		instances:     map[instancesKey]map[configKey][]*model.ServiceInstance{},
-		instancesBySE: map[types.NamespacedName]map[configKey][]*model.ServiceInstance{},
+		ip2instance:     map[string][]*model.ServiceInstance{},
+		instances:       map[instancesKey]map[configKey][]*model.ServiceInstance{},
+		instancesBySE:   map[types.NamespacedName]map[configKey][]*model.ServiceInstance{},
+		instancesByHost: sets.Set[string]{},
 	}
 	instances := []*model.ServiceInstance{
 		makeInstance(selector, "1.1.1.1", 444, selector.Spec.(*networking.ServiceEntry).Ports[0], nil, PlainText),
@@ -133,5 +135,54 @@ func TestServiceStore(t *testing.T) {
 	}
 	if store.allocateNeeded {
 		t.Errorf("expected no allocate needed")
+	}
+}
+
+// Tests that when multiple service entries with "DNSRounbRobinLB" resolution type
+// are created with different/same endpoints, we only consider the first service because
+// Envoy's LogicalDNS type of cluster does not allow more than one locality LB Enpoint.
+func TestServiceInstancesForDnsRoundRobinLB(t *testing.T) {
+	store := serviceInstancesStore{
+		ip2instance:     map[string][]*model.ServiceInstance{},
+		instances:       map[instancesKey]map[configKey][]*model.ServiceInstance{},
+		instancesBySE:   map[types.NamespacedName]map[configKey][]*model.ServiceInstance{},
+		instancesByHost: sets.Set[string]{},
+	}
+	instances := []*model.ServiceInstance{
+		makeInstance(dnsRoundRobinLBSE1, "1.1.1.1", 444, selector.Spec.(*networking.ServiceEntry).Ports[0], nil, PlainText),
+	}
+	cKey := configKey{
+		namespace: "dns",
+		name:      "dns-round-robin-1",
+	}
+	// Add instance related to first Service Entry and validate they are added correctly.
+	store.addInstances(cKey, instances)
+	gotInstances := store.getByKey(instancesKey{
+		hostname:  "example.com",
+		namespace: "dns",
+	})
+	expected := []*model.ServiceInstance{
+		makeInstance(dnsRoundRobinLBSE1, "1.1.1.1", 444, selector.Spec.(*networking.ServiceEntry).Ports[0], nil, PlainText),
+	}
+	if !reflect.DeepEqual(gotInstances, expected) {
+		t.Errorf("got unexpected instances : %v", gotInstances)
+	}
+
+	// Add instance related to second Service Entry and validate it is not ignored.
+	instances = []*model.ServiceInstance{
+		makeInstance(dnsRoundRobinLBSE2, "2.2.2.2", 444, selector.Spec.(*networking.ServiceEntry).Ports[0], nil, PlainText),
+	}
+	cKey = configKey{
+		namespace: "dns",
+		name:      "dns-round-robin-2",
+	}
+	store.addInstances(cKey, instances)
+
+	gotInstances = store.getByKey(instancesKey{
+		hostname:  "example.com",
+		namespace: "dns",
+	})
+	if !reflect.DeepEqual(gotInstances, expected) {
+		t.Errorf("got unexpected instances : %v", gotInstances)
 	}
 }

pilot/pkg/serviceregistry/serviceentry/controller.go
@@ -164,9 +164,10 @@ func newController(store model.ConfigStore, xdsUpdater model.XDSUpdater, options
 		XdsUpdater: xdsUpdater,
 		store:      store,
 		serviceInstances: serviceInstancesStore{
-			ip2instance:   map[string][]*model.ServiceInstance{},
-			instances:     map[instancesKey]map[configKey][]*model.ServiceInstance{},
-			instancesBySE: map[types.NamespacedName]map[configKey][]*model.ServiceInstance{},
+			ip2instance:     map[string][]*model.ServiceInstance{},
+			instances:       map[instancesKey]map[configKey][]*model.ServiceInstance{},
+			instancesBySE:   map[types.NamespacedName]map[configKey][]*model.ServiceInstance{},
+			instancesByHost: sets.New[string](),
 		},
 		workloadInstances: workloadinstances.NewIndex(),
 		services: serviceStore{

pilot/pkg/serviceregistry/serviceentry/conversion_test.go
@@ -445,6 +445,41 @@ var dnsSelector = &config.Config{
 	},
 }
 
+// Service Entry with DNSRoundRobinLB
+var dnsRoundRobinLBSE1 = &config.Config{
+	Meta: config.Meta{
+		GroupVersionKind:  gvk.ServiceEntry,
+		Name:              "dns-round-robin-1",
+		Namespace:         "dns",
+		CreationTimestamp: GlobalTime,
+		Labels:            map[string]string{label.SecurityTlsMode.Name: model.IstioMutualTLSModeLabel},
+	},
+	Spec: &networking.ServiceEntry{
+		Hosts: []string{"example.com"},
+		Ports: []*networking.Port{
+			{Number: 445, Name: "http-445", Protocol: "http"},
+		},
+		Resolution: networking.ServiceEntry_DNS_ROUND_ROBIN,
+	},
+}
+
+var dnsRoundRobinLBSE2 = &config.Config{
+	Meta: config.Meta{
+		GroupVersionKind:  gvk.ServiceEntry,
+		Name:              "dns-round-robin-2",
+		Namespace:         "dns",
+		CreationTimestamp: GlobalTime,
+		Labels:            map[string]string{label.SecurityTlsMode.Name: model.IstioMutualTLSModeLabel},
+	},
+	Spec: &networking.ServiceEntry{
+		Hosts: []string{"example.com"},
+		Ports: []*networking.Port{
+			{Number: 445, Name: "http-445", Protocol: "http"},
+		},
+		Resolution: networking.ServiceEntry_DNS_ROUND_ROBIN,
+	},
+}
+
 func createWorkloadEntry(name, namespace string, spec *networking.WorkloadEntry) *config.Config {
 	return &config.Config{
 		Meta: config.Meta{

commit 961d6b9e18f82b26b005fe8e40e711ba63812480

Fix creation of new DNS_ROUND_ROBIN entries
Fixes https://github.com/istio/istio/issues/49315

Broken in https://github.com/istio/istio/pull/41831

pilot/pkg/serviceregistry/serviceentry/store.go
@@ -22,7 +22,7 @@ import (
 )
 
 type hostPort struct {
-	host string
+	host instancesKey
 	port int
 }
 
@@ -64,7 +64,7 @@ func (s *serviceInstancesStore) getByKey(key instancesKey) []*model.ServiceInsta
 func (s *serviceInstancesStore) deleteInstanceKeys(key configKey, instances []*model.ServiceInstance) {
 	for _, i := range instances {
 		ikey := makeInstanceKey(i)
-		s.instancesByHostAndPort.Delete(hostPort{ikey.hostname.String(), i.ServicePort.Port})
+		s.instancesByHostAndPort.Delete(hostPort{ikey, i.ServicePort.Port})
 		oldInstances := s.instances[ikey][key]
 		delete(s.instances[ikey], key)
 		if len(s.instances[ikey]) == 0 {
@@ -73,7 +73,7 @@ func (s *serviceInstancesStore) deleteInstanceKeys(key configKey, instances []*m
 		delete(s.ip2instance, i.Endpoint.Address)
 		// Cleanup stale IPs, if the IPs changed
 		for _, oi := range oldInstances {
-			s.instancesByHostAndPort.Delete(hostPort{ikey.hostname.String(), oi.ServicePort.Port})
+			s.instancesByHostAndPort.Delete(hostPort{ikey, oi.ServicePort.Port})
 			delete(s.ip2instance, oi.Endpoint.Address)
 		}
 	}
@@ -83,7 +83,7 @@ func (s *serviceInstancesStore) deleteInstanceKeys(key configKey, instances []*m
 func (s *serviceInstancesStore) addInstances(key configKey, instances []*model.ServiceInstance) {
 	for _, instance := range instances {
 		ikey := makeInstanceKey(instance)
-		hostPort := hostPort{ikey.hostname.String(), instance.ServicePort.Port}
+		hostPort := hostPort{ikey, instance.ServicePort.Port}
 		// For DNSRoundRobinLB resolution type, check if service instances already exist and do not add
 		// if it already exist. This can happen if two Service Entries are created with same host name,
 		// resolution as DNS_ROUND_ROBIN and with same/different endpoints.

pilot/pkg/serviceregistry/serviceentry/store_test.go
@@ -23,6 +23,8 @@ import (
 	networking "istio.io/api/networking/v1alpha3"
 	"istio.io/istio/pilot/pkg/model"
 	"istio.io/istio/pkg/config/constants"
+	"istio.io/istio/pkg/ptr"
+	"istio.io/istio/pkg/test/util/assert"
 	"istio.io/istio/pkg/util/sets"
 )
 
@@ -142,6 +144,8 @@ func TestServiceStore(t *testing.T) {
 // are created with different/same endpoints, we only consider the first service because
 // Envoy's LogicalDNS type of cluster does not allow more than one locality LB Endpoint.
 func TestServiceInstancesForDnsRoundRobinLB(t *testing.T) {
+	otherNs := ptr.Of(dnsRoundRobinLBSE1.DeepCopy())
+	otherNs.Namespace = "other"
 	store := serviceInstancesStore{
 		ip2instance:            map[string][]*model.ServiceInstance{},
 		instances:              map[instancesKey]map[configKey][]*model.ServiceInstance{},
@@ -158,17 +162,32 @@ func TestServiceInstancesForDnsRoundRobinLB(t *testing.T) {
 	}
 	// Add instance related to first Service Entry and validate they are added correctly.
 	store.addInstances(cKey, instances)
-	gotInstances := store.getByKey(instancesKey{
-		hostname:  "example.com",
-		namespace: "dns",
-	})
+
+	store.addInstances(
+		configKey{namespace: otherNs.Namespace, name: otherNs.Name},
+		[]*model.ServiceInstance{
+			makeInstance(otherNs, "1.1.1.1", 444, otherNs.Spec.(*networking.ServiceEntry).Ports[0], nil, PlainText),
+			makeInstance(otherNs, "1.1.1.1", 445, otherNs.Spec.(*networking.ServiceEntry).Ports[1], nil, PlainText),
+		},
+	)
+
 	expected := []*model.ServiceInstance{
 		makeInstance(dnsRoundRobinLBSE1, "1.1.1.1", 444, dnsRoundRobinLBSE1.Spec.(*networking.ServiceEntry).Ports[0], nil, PlainText),
 		makeInstance(dnsRoundRobinLBSE1, "1.1.1.1", 445, dnsRoundRobinLBSE1.Spec.(*networking.ServiceEntry).Ports[1], nil, PlainText),
 	}
-	if !reflect.DeepEqual(gotInstances, expected) {
-		t.Errorf("got unexpected instances : %v", gotInstances)
+	assert.Equal(t, store.getByKey(instancesKey{
+		hostname:  "example.com",
+		namespace: "dns",
+	}), expected)
+
+	otherNsExpected := []*model.ServiceInstance{
+		makeInstance(otherNs, "1.1.1.1", 444, otherNs.Spec.(*networking.ServiceEntry).Ports[0], nil, PlainText),
+		makeInstance(otherNs, "1.1.1.1", 445, otherNs.Spec.(*networking.ServiceEntry).Ports[1], nil, PlainText),
 	}
+	assert.Equal(t, store.getByKey(instancesKey{
+		hostname:  "example.com",
+		namespace: otherNs.Namespace,
+	}), otherNsExpected)
 
 	// Add instance related to second Service Entry and validate it is ignored.
 	instances = []*model.ServiceInstance{
@@ -180,11 +199,12 @@ func TestServiceInstancesForDnsRoundRobinLB(t *testing.T) {
 	}
 	store.addInstances(cKey, instances)
 
-	gotInstances = store.getByKey(instancesKey{
+	assert.Equal(t, store.getByKey(instancesKey{
 		hostname:  "example.com",
 		namespace: "dns",
-	})
-	if !reflect.DeepEqual(gotInstances, expected) {
-		t.Errorf("got unexpected instances : %v", gotInstances)
-	}
+	}), expected)
+	assert.Equal(t, store.getByKey(instancesKey{
+		hostname:  "example.com",
+		namespace: otherNs.Namespace,
+	}), otherNsExpected)
 }