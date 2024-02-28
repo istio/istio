74837530840e4d7e935afe1d51b35a88629e7ccb
Zhonghu Xu <xuzhonghu@huawei.com>
2024-02-28 15:43:08 +0800

    address comments

--- pilot/pkg/xds/delta.go
+++ pilot/pkg/xds/delta.go
@@ -341,6 +341,9 @@ func (s *DiscoveryServer) shouldRespondDelta(con *Connection, request *discovery
 	// because Istiod is restarted or Envoy disconnects and reconnects.
 	// We should always respond with the current resource names.
 	if previousInfo == nil {
+		con.proxy.Lock()
+		defer con.proxy.Unlock()
+
 		if len(request.InitialResourceVersions) > 0 {
 			deltaLog.Debugf("ADS:%s: RECONNECT %s %s resources:%v", stype, con.conID, request.ResponseNonce, len(request.InitialResourceVersions))
 		} else {
@@ -348,11 +351,11 @@ func (s *DiscoveryServer) shouldRespondDelta(con *Connection, request *discovery
 		}
 
 		res, wildcard := deltaWatchedResources(nil, request)
-		con.proxy.AddOrUpdateWatchedResource(&model.WatchedResource{
+		con.proxy.WatchedResources[request.TypeUrl] = &model.WatchedResource{
 			TypeUrl:       request.TypeUrl,
 			ResourceNames: res,
 			Wildcard:      wildcard,
-		})
+		}
 		// For all EDS requests that we have already responded with in the same stream let us
 		// force the response. It is important to respond to those requests for Envoy to finish
 		// warming of those resources(Clusters).
@@ -364,13 +367,9 @@ func (s *DiscoveryServer) shouldRespondDelta(con *Connection, request *discovery
 		// 5. We should respond to the EDS request with Endpoints to let Envoy finish cluster warming.
 		// Refer to https://github.com/envoyproxy/envoy/issues/13009 for more details.
 		for _, dependent := range warmingDependencies(request.TypeUrl) {
-			con.proxy.UpdateWatchedResource(dependent, func(wr *model.WatchedResource) *model.WatchedResource {
-				if wr == nil {
-					return nil
-				}
-				wr.AlwaysRespond = true
-				return wr
-			})
+			if dwr, exists := con.proxy.WatchedResources[dependent]; exists {
+				dwr.AlwaysRespond = true
+			}
 		}
 		return true
 	}
