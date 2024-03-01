istio/istio

commit 9ca112c308c2c4ab5d595d6ba052a88212dd00f7

kube: make discovery selectors automagically handle updates (#49262)
For https://github.com/istio/istio/issues/44675

Rather than handle this in each controller, automatically handle it in
kclient.

pkg/kube/multicluster/cluster.go
@@ -19,10 +19,14 @@ import (
 	"time"
 
 	"go.uber.org/atomic"
+	corev1 "k8s.io/api/core/v1"
 
 	"istio.io/istio/pilot/pkg/features"
 	"istio.io/istio/pkg/cluster"
+	"istio.io/istio/pkg/config/mesh"
 	"istio.io/istio/pkg/kube"
+	"istio.io/istio/pkg/kube/kclient"
+	filter "istio.io/istio/pkg/kube/namespace"
 	"istio.io/istio/pkg/log"
 )
 
@@ -44,7 +48,7 @@ type Cluster struct {
 
 // Run starts the cluster's informers and waits for caches to sync. Once caches are synced, we mark the cluster synced.
 // This should be called after each of the handlers have registered informers, and should be run in a goroutine.
-func (r *Cluster) Run(handlers []handler) {
+func (r *Cluster) Run(mesh mesh.Watcher, handlers []handler) {
 	if features.RemoteClusterTimeout > 0 {
 		time.AfterFunc(features.RemoteClusterTimeout, func() {
 			if !r.initialSync.Load() {
@@ -54,6 +58,10 @@ func (r *Cluster) Run(handlers []handler) {
 			r.initialSyncTimeout.Store(true)
 		})
 	}
+	// Build a namespace watcher. This must have no filter, since this is our input to the filter itself.
+	namespaces := kclient.New[*corev1.Namespace](r.Client)
+	filter := filter.NewDiscoveryNamespacesFilter(namespaces, mesh, r.stop)
+	kube.SetObjectFilter(r.Client, filter)
 
 	if !r.Client.RunAndWait(r.stop) {
 		log.Warnf("remote cluster %s failed to sync", r.ID)

pkg/kube/multicluster/secretcontroller.go
@@ -33,7 +33,6 @@ import (
 	"istio.io/istio/pkg/kube"
 	"istio.io/istio/pkg/kube/controllers"
 	"istio.io/istio/pkg/kube/kclient"
-	filter "istio.io/istio/pkg/kube/namespace"
 	"istio.io/istio/pkg/log"
 	"istio.io/istio/pkg/monitoring"
 )
@@ -83,12 +82,10 @@ type Controller struct {
 	secrets         kclient.Client[*corev1.Secret]
 	configOverrides []func(*rest.Config)
 
-	namespaces kclient.Client[*corev1.Namespace]
+	cs *ClusterStore
 
-	DiscoveryNamespacesFilter filter.DiscoveryNamespacesFilter
-	cs                        *ClusterStore
-
-	handlers []handler
+	meshWatcher mesh.Watcher
+	handlers    []handler
 }
 
 // NewController returns a new secret controller
@@ -134,11 +131,9 @@ func NewController(kubeclientset kube.Client, namespace string, clusterID cluste
 		cs:              newClustersStore(),
 		secrets:         secrets,
 		configOverrides: configOverrides,
+		meshWatcher:     meshWatcher,
 	}
 
-	namespaces := kclient.New[*corev1.Namespace](kubeclientset)
-	controller.namespaces = namespaces
-	controller.DiscoveryNamespacesFilter = filter.NewDiscoveryNamespacesFilter(namespaces, meshWatcher.Mesh().GetDiscoverySelectors())
 	// Queue does NOT retry. The only error that can occur is if the kubeconfig is
 	// malformed. This is a static analysis that cannot be resolved by retry. Actual
 	// connectivity issues would result in HasSynced returning false rather than an
@@ -175,13 +170,6 @@ func (c *Controller) registerHandler(h handler) {
 
 // Run starts the controller until it receives a message over stopCh
 func (c *Controller) Run(stopCh <-chan struct{}) error {
-	// Normally, we let informers start after all controllers. However, in this case we need namespaces to start and sync
-	// first, so we have DiscoveryNamespacesFilter ready to go. This avoids processing objects that would be filtered during startup.
-	c.namespaces.Start(stopCh)
-	// Wait for namespace informer synced, which implies discovery filter is synced as well
-	if !kube.WaitForCacheSync("namespace", stopCh, c.namespaces.HasSynced) {
-		return fmt.Errorf("failed to sync namespaces")
-	}
 	// run handlers for the config cluster; do not store this *Cluster in the ClusterStore or give it a SyncTimeout
 	// this is done outside the goroutine, we should block other Run/startFuncs until this is registered
 	c.configClusterSyncers = c.handleAdd(c.configCluster)
@@ -248,6 +236,7 @@ func DefaultBuildClientsFromConfig(kubeConfig []byte, clusterID cluster.ID, conf
 	if features.WorkloadEntryCrossCluster {
 		clients = kube.EnableCrdWatcher(clients)
 	}
+
 	return clients, nil
 }
 
@@ -312,7 +301,7 @@ func (c *Controller) addSecret(name types.NamespacedName, s *corev1.Secret) erro
 		callback(remoteCluster)
 		logger.Infof("finished callback for cluster and starting to sync")
 		c.cs.Store(secretKey, remoteCluster.ID, remoteCluster)
-		go remoteCluster.Run(c.handlers)
+		go remoteCluster.Run(c.meshWatcher, c.handlers)
 	}
 
 	log.Infof("Number of remote clusters: %d", c.cs.Len())

pilot/pkg/bootstrap/configcontroller.go
@@ -98,7 +98,7 @@ func (s *Server) initConfigController(args *PilotArgs) error {
 			leaderelection.
 				NewLeaderElection(args.Namespace, args.PodName, leaderelection.IngressController, args.Revision, s.kubeClient).
 				AddRunFunction(func(leaderStop <-chan struct{}) {
-					ingressSyncer := ingress.NewStatusSyncer(s.environment.Watcher, s.kubeClient, args.RegistryOptions.KubeOptions)
+					ingressSyncer := ingress.NewStatusSyncer(s.environment.Watcher, s.kubeClient)
 					// Start informers again. This fixes the case where informers for namespace do not start,
 					// as we create them only after acquiring the leader lock
 					// Note: stop here should be the overall pilot stop, NOT the leader election stop. We are
@@ -172,13 +172,9 @@ func (s *Server) initK8SConfigStore(args *PilotArgs) error {
 					AddRunFunction(func(leaderStop <-chan struct{}) {
 						// We can only run this if the Gateway CRD is created
 						if s.kubeClient.CrdWatcher().WaitForCRD(gvr.KubernetesGateway, leaderStop) {
-							nsFilter := args.RegistryOptions.KubeOptions.DiscoveryNamespacesFilter
-							if !features.EnableEnhancedResourceScoping {
-								nsFilter = nil
-							}
 							tagWatcher := revisions.NewTagWatcher(s.kubeClient, args.Revision)
 							controller := gateway.NewDeploymentController(s.kubeClient, s.clusterID, s.environment,
-								s.webhookInfo.getWebhookConfig, s.webhookInfo.addHandler, tagWatcher, args.Revision, nsFilter)
+								s.webhookInfo.getWebhookConfig, s.webhookInfo.addHandler, tagWatcher, args.Revision)
 							// Start informers again. This fixes the case where informers for namespace do not start,
 							// as we create them only after acquiring the leader lock
 							// Note: stop here should be the overall pilot stop, NOT the leader election stop. We are
@@ -352,9 +348,6 @@ func (s *Server) makeKubeConfigController(args *PilotArgs) *crdclient.Client {
 		Revision:     args.Revision,
 		DomainSuffix: args.RegistryOptions.KubeOptions.DomainSuffix,
 		Identifier:   "crd-controller",
-	}
-	if args.RegistryOptions.KubeOptions.DiscoveryNamespacesFilter != nil {
-		opts.NamespacesFilter = args.RegistryOptions.KubeOptions.DiscoveryNamespacesFilter.Filter
 	}
 	return crdclient.New(s.kubeClient, opts)
 }

pilot/pkg/bootstrap/istio_ca.go
@@ -34,7 +34,6 @@ import (
 	securityModel "istio.io/istio/pilot/pkg/security/model"
 	"istio.io/istio/pkg/config/constants"
 	"istio.io/istio/pkg/env"
-	"istio.io/istio/pkg/kube/namespace"
 	"istio.io/istio/pkg/log"
 	"istio.io/istio/pkg/security"
 	"istio.io/istio/security/pkg/cmd"
@@ -53,7 +52,6 @@ type caOptions struct {
 	Namespace        string
 	Authenticators   []security.Authenticator
 	CertSignerDomain string
-	DiscoveryFilter  namespace.DiscoveryFilter
 }
 
 // Based on istio_ca main - removing creation of Secrets with private keys in all namespaces and install complexity.
@@ -142,7 +140,7 @@ var (
 // 'hostlist' must be non-empty - but is not used since CA Server will start on existing
 // grpc server. Adds client cert auth and kube (sds enabled)
 func (s *Server) initCAServer(ca caserver.CertificateAuthority, opts *caOptions) {
-	caServer, startErr := caserver.New(ca, maxWorkloadCertTTL.Get(), opts.Authenticators, opts.DiscoveryFilter, s.multiclusterController)
+	caServer, startErr := caserver.New(ca, maxWorkloadCertTTL.Get(), opts.Authenticators, s.multiclusterController)
 	if startErr != nil {
 		log.Fatalf("failed to create istio ca server: %v", startErr)
 	}

pilot/pkg/bootstrap/server.go
@@ -34,6 +34,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/reflection"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/rest"
 
 	meshconfig "istio.io/api/mesh/v1alpha1"
@@ -66,7 +67,9 @@ import (
 	istiokeepalive "istio.io/istio/pkg/keepalive"
 	kubelib "istio.io/istio/pkg/kube"
 	"istio.io/istio/pkg/kube/inject"
+	"istio.io/istio/pkg/kube/kclient"
 	"istio.io/istio/pkg/kube/multicluster"
+	"istio.io/istio/pkg/kube/namespace"
 	"istio.io/istio/pkg/log"
 	"istio.io/istio/pkg/network"
 	"istio.io/istio/pkg/security"
@@ -265,6 +268,14 @@ func NewServer(args *PilotArgs, initFuncs ...func(*Server)) (*Server, error) {
 
 	s.initMeshConfiguration(args, s.fileWatcher)
 	spiffe.SetTrustDomain(s.environment.Mesh().GetTrustDomain())
+	// Setup Kubernetes watch filters
+	// Because this relies on meshconfig, it needs to be outside initKubeClient
+	if s.kubeClient != nil {
+		// Build a namespace watcher. This must have no filter, since this is our input to the filter itself.
+		namespaces := kclient.New[*corev1.Namespace](s.kubeClient)
+		filter := namespace.NewDiscoveryNamespacesFilter(namespaces, s.environment.Watcher, s.internalStop)
+		s.kubeClient = kubelib.SetObjectFilter(s.kubeClient, filter)
+	}
 
 	s.initMeshNetworks(args, s.fileWatcher)
 	s.initMeshHandlers(configGen.MeshConfigChanged)
@@ -277,7 +288,6 @@ func NewServer(args *PilotArgs, initFuncs ...func(*Server)) (*Server, error) {
 	caOpts := &caOptions{
 		TrustDomain:      s.environment.Mesh().TrustDomain,
 		Namespace:        args.Namespace,
-		DiscoveryFilter:  args.RegistryOptions.KubeOptions.GetFilter(),
 		ExternalCAType:   ra.CaExternalType(externalCaType),
 		CertSignerDomain: features.CertSignerDomain,
 	}
@@ -1105,10 +1115,6 @@ func (s *Server) initControllers(args *PilotArgs) error {
 
 	s.initSDSServer()
 
-	if features.EnableEnhancedResourceScoping {
-		// setup namespace filter
-		args.RegistryOptions.KubeOptions.DiscoveryNamespacesFilter = s.multiclusterController.DiscoveryNamespacesFilter
-	}
 	if err := s.initConfigController(args); err != nil {
 		return fmt.Errorf("error initializing config controller: %v", err)
 	}

pilot/pkg/bootstrap/servicecontroller.go
@@ -77,7 +77,6 @@ func (s *Server) initKubeRegistry(args *PilotArgs) (err error) {
 	args.RegistryOptions.KubeOptions.SystemNamespace = args.Namespace
 	args.RegistryOptions.KubeOptions.MeshServiceController = s.ServiceController()
 	// pass namespace to k8s service registry
-	args.RegistryOptions.KubeOptions.DiscoveryNamespacesFilter = s.multiclusterController.DiscoveryNamespacesFilter
 	kubecontroller.NewMulticluster(args.PodName,
 		s.kubeClient.Kube(),
 		args.RegistryOptions.ClusterRegistriesNamespace,

pilot/pkg/config/kube/crdclient/client.go
@@ -51,6 +51,8 @@ import (
 	"istio.io/istio/pkg/log"
 	"istio.io/istio/pkg/maps"
 	"istio.io/istio/pkg/queue"
+	"istio.io/istio/pkg/slices"
+	"istio.io/istio/pkg/util/sets"
 )
 
 var scope = log.RegisterScope("kube", "Kubernetes client messages")
@@ -83,16 +85,14 @@ type Client struct {
 	logger           *log.Scope
 
 	// namespacesFilter is only used to initiate filtered informer.
-	namespacesFilter func(obj interface{}) bool
-	filtersByGVK     map[config.GroupVersionKind]kubetypes.Filter
+	filtersByGVK map[config.GroupVersionKind]kubetypes.Filter
 }
 
 type Option struct {
-	Revision         string
-	DomainSuffix     string
-	Identifier       string
-	NamespacesFilter func(obj interface{}) bool
-	FiltersByGVK     map[config.GroupVersionKind]kubetypes.Filter
+	Revision     string
+	DomainSuffix string
+	Identifier   string
+	FiltersByGVK map[config.GroupVersionKind]kubetypes.Filter
 }
 
 var _ model.ConfigStoreController = &Client{}
@@ -123,7 +123,6 @@ func NewForSchemas(client kube.Client, opts Option, schemas collection.Schemas) 
 		handlers:         map[config.GroupVersionKind][]model.EventHandler{},
 		client:           client,
 		logger:           scope.WithLabels("controller", opts.Identifier),
-		namespacesFilter: opts.NamespacesFilter,
 		filtersByGVK:     opts.FiltersByGVK,
 	}
 
@@ -344,17 +343,17 @@ func (cl *Client) addCRD(name string) {
 		cl.logger.Debugf("added resource that already exists: %v", resourceGVK)
 		return
 	}
-	filter := cl.filtersByGVK[resourceGVK]
-	objectFilter := filter.ObjectFilter
-	filter.ObjectFilter = func(t any) bool {
-		if objectFilter != nil && !objectFilter(t) {
-			return false
-		}
-		if cl.namespacesFilter != nil && !cl.namespacesFilter(t) {
-			return false
-		}
-		return config.LabelsInRevision(t.(controllers.Object).GetLabels(), cl.revision)
+
+	// We need multiple filters:
+	// 1. Is it in this revision?
+	// 2. Does it match the discovery selector?
+	// 3. Does it have a special per-type object filter?
+	var extraFilter func(obj any) bool
+	if of, f := cl.filtersByGVK[resourceGVK]; f && of.ObjectFilter != nil {
+		extraFilter = of.ObjectFilter.Filter
 	}
+	filter := kubetypes.Filter{ObjectFilter: composeFilters(kube.FilterIfEnhancedFilteringEnabled(cl.client), cl.inRevision, extraFilter)}
+
 	var kc kclient.Untyped
 	if s.IsBuiltin() {
 		kc = kclient.NewUntypedInformer(cl.client, gvr, filter)
@@ -393,6 +392,45 @@ func (cl *Client) addCRD(name string) {
 	})
 
 	cl.kinds[resourceGVK] = kc
+}
+
+// composedFilter offers a way to join multiple different object filters into a single one
+type composedFilter struct {
+	// The primary filter, which has a handler. Optional
+	filter kubetypes.DynamicObjectFilter
+	// Secondary filters (no handler allowed)
+	extra []func(obj any) bool
+}
+
+func (f composedFilter) Filter(obj any) bool {
+	for _, filter := range f.extra {
+		if !filter(obj) {
+			return false
+		}
+	}
+	if f.filter != nil {
+		return f.filter.Filter(obj)
+	}
+	return true
+}
+
+func (f composedFilter) AddHandler(fn func(selected, deselected sets.String)) {
+	if f.filter != nil {
+		f.filter.AddHandler(fn)
+	}
+}
+
+func composeFilters(filter kubetypes.DynamicObjectFilter, extra ...func(obj any) bool) kubetypes.DynamicObjectFilter {
+	return composedFilter{
+		filter: filter,
+		extra: slices.FilterInPlace(extra, func(f func(obj any) bool) bool {
+			return f != nil
+		}),
+	}
+}
+
+func (cl *Client) inRevision(obj any) bool {
+	return config.LabelsInRevision(obj.(controllers.Object).GetLabels(), cl.revision)
 }
 
 func (cl *Client) onEvent(resourceGVK config.GroupVersionKind, old controllers.Object, curr controllers.Object, event model.Event) {

pilot/pkg/config/kube/crdclient/client_test.go
@@ -27,6 +27,7 @@ import (
 	"istio.io/api/meta/v1alpha1"
 	"istio.io/api/networking/v1alpha3"
 	clientnetworkingv1alpha3 "istio.io/client-go/pkg/apis/networking/v1alpha3"
+	"istio.io/istio/pilot/pkg/features"
 	"istio.io/istio/pilot/pkg/model"
 	"istio.io/istio/pkg/config"
 	"istio.io/istio/pkg/config/schema/collection"
@@ -36,23 +37,23 @@ import (
 	"istio.io/istio/pkg/kube"
 	"istio.io/istio/pkg/kube/controllers"
 	"istio.io/istio/pkg/kube/kclient/clienttest"
+	"istio.io/istio/pkg/kube/kubetypes"
 	"istio.io/istio/pkg/slices"
 	"istio.io/istio/pkg/test"
 	"istio.io/istio/pkg/test/util/assert"
 	"istio.io/istio/pkg/test/util/retry"
 )
 
-func makeClient(t *testing.T, schemas collection.Schemas, f ...func(o Option) Option) (model.ConfigStoreController, kube.CLIClient) {
+func makeClient(t *testing.T, schemas collection.Schemas, f kubetypes.DynamicObjectFilter) (model.ConfigStoreController, kube.CLIClient) {
 	fake := kube.NewFakeClient()
+	if f != nil {
+		kube.SetObjectFilter(fake, f)
+	}
 	for _, s := range schemas.All() {
 		clienttest.MakeCRD(t, fake, s.GroupVersionResource())
 	}
 	stop := test.NewStop(t)
-	var o Option
-	for _, fn := range f {
-		o = fn(o)
-	}
-	config := New(fake, o)
+	config := New(fake, Option{})
 	go config.Run(stop)
 	fake.RunAndWait(stop)
 	kube.WaitForCacheSync("test", stop, config.HasSynced)
@@ -78,7 +79,7 @@ func createResource(t *testing.T, store model.ConfigStoreController, r resource.
 // Ensure that the client can run without CRDs present
 func TestClientNoCRDs(t *testing.T) {
 	schema := collection.NewSchemasBuilder().MustAdd(collections.Sidecar).Build()
-	store, _ := makeClient(t, schema)
+	store, _ := makeClient(t, schema, nil)
 	retry.UntilOrFail(t, store.HasSynced, retry.Timeout(time.Second))
 	r := collections.VirtualService
 	configMeta := config.Meta{
@@ -102,21 +103,19 @@ func TestClientNoCRDs(t *testing.T) {
 
 // Ensure that the client can run without CRDs present, but then added later
 func TestClientDelayedCRDs(t *testing.T) {
+	test.SetForTest(t, &features.EnableEnhancedResourceScoping, true)
 	// ns1 is allowed, ns2 is not
-	applyFilter := func(o Option) Option {
-		o.NamespacesFilter = func(obj interface{}) bool {
-			// When an object is deleted, obj could be a DeletionFinalStateUnknown marker item.
-			object := controllers.ExtractObject(obj)
-			if object == nil {
-				return false
-			}
-			ns := object.GetNamespace()
-			return ns == "ns1"
+	f := kubetypes.NewStaticObjectFilter(func(obj interface{}) bool {
+		// When an object is deleted, obj could be a DeletionFinalStateUnknown marker item.
+		object := controllers.ExtractObject(obj)
+		if object == nil {
+			return false
 		}
-		return o
-	}
+		ns := object.GetNamespace()
+		return ns == "ns1"
+	})
 	schema := collection.NewSchemasBuilder().MustAdd(collections.Sidecar).Build()
-	store, fake := makeClient(t, schema, applyFilter)
+	store, fake := makeClient(t, schema, f)
 	retry.UntilOrFail(t, store.HasSynced, retry.Timeout(time.Second))
 	r := collections.VirtualService
 
@@ -159,7 +158,7 @@ func TestClientDelayedCRDs(t *testing.T) {
 
 // CheckIstioConfigTypes validates that an empty store can do CRUD operators on all given types
 func TestClient(t *testing.T) {
-	store, _ := makeClient(t, collections.PilotGatewayAPI().Union(collections.Kube))
+	store, _ := makeClient(t, collections.PilotGatewayAPI().Union(collections.Kube), nil)
 	configName := "test"
 	configNamespace := "test-ns"
 	timeout := retry.Timeout(time.Millisecond * 200)

commit 40ee4e9d10a68b1d92b9a94fce6b4528bce0235b

multicluster: fix and test filter creation
We need to create the discovery filter before we call components.

Regression from https://github.com/istio/istio/pull/49262, which has
not shipped yet, so no backport is needed.

pkg/kube/multicluster/cluster.go
@@ -48,7 +48,7 @@ type Cluster struct {
 
 // Run starts the cluster's informers and waits for caches to sync. Once caches are synced, we mark the cluster synced.
 // This should be called after each of the handlers have registered informers, and should be run in a goroutine.
-func (r *Cluster) Run(mesh mesh.Watcher, handlers []handler) {
+func (r *Cluster) Run(mesh mesh.Watcher, handlers []handler, buildComponents func(cluster *Cluster)) {
 	if features.RemoteClusterTimeout > 0 {
 		time.AfterFunc(features.RemoteClusterTimeout, func() {
 			if !r.initialSync.Load() {
@@ -59,9 +59,12 @@ func (r *Cluster) Run(mesh mesh.Watcher, handlers []handler) {
 		})
 	}
 	// Build a namespace watcher. This must have no filter, since this is our input to the filter itself.
+	// This must be done before we build components, so they can access the filter.
 	namespaces := kclient.New[*corev1.Namespace](r.Client)
 	filter := filter.NewDiscoveryNamespacesFilter(namespaces, mesh, r.stop)
 	kube.SetObjectFilter(r.Client, filter)
+
+	buildComponents(r)
 
 	if !r.Client.RunAndWait(r.stop) {
 		log.Warnf("remote cluster %s failed to sync", r.ID)

pkg/kube/multicluster/secretcontroller.go
@@ -298,10 +298,12 @@ func (c *Controller) addSecret(name types.NamespacedName, s *corev1.Secret) erro
 			errs = multierror.Append(errs, err)
 			continue
 		}
-		callback(remoteCluster)
-		logger.Infof("finished callback for cluster and starting to sync")
+		// We run cluster async so we do not block, as this requires actually connecting to the cluster and loading configuration.
 		c.cs.Store(secretKey, remoteCluster.ID, remoteCluster)
-		go remoteCluster.Run(c.meshWatcher, c.handlers)
+		go func() {
+			remoteCluster.Run(c.meshWatcher, c.handlers, callback)
+			logger.Infof("finished callback for cluster")
+		}()
 	}
 
 	log.Infof("Number of remote clusters: %d", c.cs.Len())

pkg/kube/multicluster/secretcontroller_test.go
@@ -25,12 +25,14 @@ import (
 	klabels "k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/rest"
 
+	meshconfig "istio.io/api/mesh/v1alpha1"
 	"istio.io/istio/pkg/cluster"
 	"istio.io/istio/pkg/config/mesh"
 	"istio.io/istio/pkg/kube"
 	"istio.io/istio/pkg/kube/controllers"
 	"istio.io/istio/pkg/kube/kclient"
 	"istio.io/istio/pkg/kube/kclient/clienttest"
+	"istio.io/istio/pkg/kube/namespace"
 	"istio.io/istio/pkg/slices"
 	"istio.io/istio/pkg/test"
 	"istio.io/istio/pkg/test/util/assert"
@@ -213,6 +215,67 @@ func TestShutdown(t *testing.T) {
 	// We should *not* shutdown anything else
 	// In theory we could, but we only shut down the controller when the entire application is closing so we don't bother
 	assert.Equal(t, map[string]bool{"config": false, "c1": false, "c0": true}, fetchClosed())
+}
+
+// TestObjectFilter tests that when a component is created, it should have access to the objectfilter.
+// This ensures we do not load everything, then later filter it out.
+func TestObjectFilter(t *testing.T) {
+	stop := test.NewStop(t)
+	clientWithNamespace := func() kube.Client {
+		return kube.NewFakeClient(
+			&v1.Namespace{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:   "allowed",
+					Labels: map[string]string{"kubernetes.io/metadata.name": "allowed"},
+				},
+			},
+			&v1.Namespace{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:   "not-allowed",
+					Labels: map[string]string{"kubernetes.io/metadata.name": "not-allowed"},
+				},
+			})
+	}
+	tc := testController{
+		client: clientWithNamespace(),
+		t:      t,
+	}
+	mesh := mesh.NewFixedWatcher(&meshconfig.MeshConfig{
+		DiscoverySelectors: []*metav1.LabelSelector{
+			{
+				MatchLabels: map[string]string{
+					"kubernetes.io/metadata.name": "allowed",
+				},
+			},
+		},
+	})
+
+	// For primary cluster, we need to set it up ourselves.
+	namespaces := kclient.New[*v1.Namespace](tc.client)
+	filter := namespace.NewDiscoveryNamespacesFilter(namespaces, mesh, stop)
+	tc.client = kube.SetObjectFilter(tc.client, filter)
+
+	tc.secrets = clienttest.NewWriter[*v1.Secret](t, tc.client)
+	tc.controller = NewController(tc.client, secretNamespace, "config", mesh)
+	tc.controller.ClientBuilder = func(kubeConfig []byte, c cluster.ID, configOverrides ...func(*rest.Config)) (kube.Client, error) {
+		return clientWithNamespace(), nil
+	}
+
+	tc.component = BuildMultiClusterComponent(tc.controller, func(cluster *Cluster) testHandler {
+		// Filter must immediately work!
+		assert.Equal(t, cluster.Client.ObjectFilter() != nil, true, "cluster "+cluster.ID.String())
+		assert.Equal(t, cluster.Client.ObjectFilter().Filter("allowed"), true)
+		assert.Equal(t, cluster.Client.ObjectFilter().Filter("not-allowed"), false)
+		return testHandler{
+			ID:     cluster.ID,
+			Closed: atomic.NewBool(false),
+			Synced: atomic.NewBool(true),
+		}
+	})
+	tc.AddSecret("s0", "c0")
+	tc.AddSecret("s1", "c1")
+	tc.Run(stop)
+	retry.UntilOrFail(t, tc.controller.HasSynced, retry.Timeout(2*time.Second))
 }
 
 type informerHandler[T controllers.ComparableObject] struct {