istio/istio

commit 2c8dcd45a39964c6270653acd7605427004cae2e

Promote experimental proxy-status command to regular, and remove regular `proxy-status` command (#47740)
* remove experimental proxy-status command

* add releasenotes

* remove experimental ps tests

* Revert "remove experimental proxy-status command"

This reverts commit fbd0ab85a81ccee2e49db0dfdeb8515763be76cd.

* promote experimental proxy-status to regular

* remove dup test

* add stable filter

* hidden unstable flags

* make the test more robust

istioctl/pkg/writer/pilot/status.go
@@ -19,7 +19,6 @@ import (
 	"fmt"
 	"io"
 	"sort"
-	"strings"
 	"text/tabwriter"
 
 	discovery "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
@@ -27,20 +26,9 @@ import (
 
 	"istio.io/istio/istioctl/pkg/multixds"
 	"istio.io/istio/pilot/pkg/model"
-	"istio.io/istio/pilot/pkg/xds"
 	xdsresource "istio.io/istio/pilot/pkg/xds/v3"
 	"istio.io/istio/pkg/log"
 )
-
-// StatusWriter enables printing of sync status using multiple []byte Istiod responses
-type StatusWriter struct {
-	Writer io.Writer
-}
-
-type writerStatus struct {
-	pilot string
-	xds.SyncStatus
-}
 
 // XdsStatusWriter enables printing of sync status using multiple xdsapi.DiscoveryResponse Istiod responses
 type XdsStatusWriter struct {
@@ -61,99 +49,7 @@ type xdsWriterStatus struct {
 	extensionconfigStatus string
 }
 
-// PrintAll takes a slice of Pilot syncz responses and outputs them using a tabwriter
-func (s *StatusWriter) PrintAll(statuses map[string][]byte) error {
-	w, fullStatus, err := s.setupStatusPrint(statuses)
-	if err != nil {
-		return err
-	}
-	for _, status := range fullStatus {
-		if err := statusPrintln(w, status); err != nil {
-			return err
-		}
-	}
-	return w.Flush()
-}
-
-// PrintSingle takes a slice of Pilot syncz responses and outputs them using a tabwriter filtering for a specific pod
-func (s *StatusWriter) PrintSingle(statuses map[string][]byte, proxyName string) error {
-	w, fullStatus, err := s.setupStatusPrint(statuses)
-	if err != nil {
-		return err
-	}
-	for _, status := range fullStatus {
-		if strings.Contains(status.ProxyID, proxyName) {
-			if err := statusPrintln(w, status); err != nil {
-				return err
-			}
-		}
-	}
-	return w.Flush()
-}
-
-func (s *StatusWriter) setupStatusPrint(statuses map[string][]byte) (*tabwriter.Writer, []*writerStatus, error) {
-	w := new(tabwriter.Writer).Init(s.Writer, 0, 9, 5, ' ', 0)
-	_, _ = fmt.Fprintln(w, "NAME\tCLUSTER\tCDS\tLDS\tEDS\tRDS\tECDS\tISTIOD\tVERSION")
-	fullStatus := make([]*writerStatus, 0, len(statuses))
-	for pilot, status := range statuses {
-		var ss []*writerStatus
-		err := json.Unmarshal(status, &ss)
-		if err != nil {
-			return nil, nil, err
-		}
-		for _, s := range ss {
-			s.pilot = pilot
-		}
-		fullStatus = append(fullStatus, ss...)
-	}
-	sort.Slice(fullStatus, func(i, j int) bool {
-		if fullStatus[i].ClusterID != fullStatus[j].ClusterID {
-			return fullStatus[i].ClusterID < fullStatus[j].ClusterID
-		}
-		return fullStatus[i].ProxyID < fullStatus[j].ProxyID
-	})
-	return w, fullStatus, nil
-}
-
-func statusPrintln(w io.Writer, status *writerStatus) error {
-	clusterSynced := xdsStatus(status.ClusterSent, status.ClusterAcked, status.ProxyType)
-	listenerSynced := xdsStatus(status.ListenerSent, status.ListenerAcked, status.ProxyType)
-	routeSynced := xdsStatus(status.RouteSent, status.RouteAcked, status.ProxyType)
-	endpointSynced := xdsStatus(status.EndpointSent, status.EndpointAcked, status.ProxyType)
-	extensionconfigSynced := xdsStatus(status.ExtensionConfigSent, status.ExtensionConfigAcked, status.ProxyType)
-	version := status.IstioVersion
-	if version == "" {
-		// If we can't find an Istio version (talking to a 1.1 pilot), fallback to the proxy version
-		// This is misleading, as the proxy version isn't always the same as the Istio version,
-		// but it is better than not providing any information.
-		version = status.ProxyVersion + "*"
-	}
-	_, _ = fmt.Fprintf(w, "%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\n",
-		status.ProxyID, status.ClusterID,
-		clusterSynced, listenerSynced, endpointSynced, routeSynced, extensionconfigSynced,
-		status.pilot, version)
-	return nil
-}
-
 const ignoredStatus = "IGNORED"
-
-func xdsStatus(sent, acked string, typ model.NodeType) string {
-	if sent == "" {
-		if typ == model.Ztunnel {
-			return ignoredStatus
-		}
-		return "NOT SENT"
-	}
-	if sent == acked {
-		return "SYNCED"
-	}
-	// acked will be empty string when there is never Acknowledged
-	if acked == "" {
-		return "STALE (Never Acknowledged)"
-	}
-	// Since the Nonce changes to uuid, so there is no more any time diff info
-	return "STALE"
-}
 
 // PrintAll takes a slice of Istiod syncz responses and outputs them using a tabwriter
 func (s *XdsStatusWriter) PrintAll(statuses map[string]*discovery.DiscoveryResponse) error {
@@ -176,7 +72,8 @@ func (s *XdsStatusWriter) setupStatusPrint(drs map[string]*discovery.DiscoveryRe
 	// Gather the statuses before printing so they may be sorted
 	var fullStatus []*xdsWriterStatus
 	mappedResp := map[string]string{}
-	var w *tabwriter.Writer
+	w := new(tabwriter.Writer).Init(s.Writer, 0, 8, 5, ' ', 0)
+	_, _ = fmt.Fprintln(w, "NAME\tCLUSTER\tCDS\tLDS\tEDS\tRDS\tECDS\tISTIOD\tVERSION")
 	for id, dr := range drs {
 		for _, resource := range dr.Resources {
 			switch resource.TypeUrl {
@@ -209,9 +106,6 @@ func (s *XdsStatusWriter) setupStatusPrint(drs map[string]*discovery.DiscoveryRe
 				if len(fullStatus) == 0 {
 					return nil, nil, fmt.Errorf("no proxies found (checked %d istiods)", len(drs))
 				}
-
-				w = new(tabwriter.Writer).Init(s.Writer, 0, 8, 5, ' ', 0)
-				_, _ = fmt.Fprintln(w, "NAME\tCLUSTER\tCDS\tLDS\tEDS\tRDS\tECDS\tISTIOD\tVERSION")
 
 				sort.Slice(fullStatus, func(i, j int) bool {
 					return fullStatus[i].proxyID < fullStatus[j].proxyID

istioctl/cmd/root.go
@@ -158,12 +158,14 @@ debug and diagnose their Istio mesh.
 	}
 
 	xdsBasedTroubleshooting := []*cobra.Command{
+		// TODO(hanxiaop): I think experimental version still has issues, so we keep the old version for now.
 		version.XdsVersionCommand(ctx),
+		// TODO(hanxiaop): this is kept for some releases in case someone is using it.
 		proxystatus.XdsStatusCommand(ctx),
 	}
-	debugBasedTroubleshooting := []*cobra.Command{
+	troubleshootingCommands := []*cobra.Command{
 		version.NewVersionCommand(ctx),
-		proxystatus.StatusCommand(ctx),
+		proxystatus.StableXdsStatusCommand(ctx),
 	}
 	var debugCmdAttachmentPoint *cobra.Command
 	if viper.GetBool("PREFER-EXPERIMENTAL") {
@@ -182,7 +184,7 @@ debug and diagnose their Istio mesh.
 	for _, c := range xdsBasedTroubleshooting {
 		experimentalCmd.AddCommand(c)
 	}
-	for _, c := range debugBasedTroubleshooting {
+	for _, c := range troubleshootingCommands {
 		debugCmdAttachmentPoint.AddCommand(c)
 	}
 

istioctl/pkg/cli/mock_client.go
@@ -18,6 +18,8 @@ import (
 	"context"
 	"fmt"
 
+	"google.golang.org/grpc/credentials"
+
 	"istio.io/istio/pkg/kube"
 )
 
@@ -71,4 +73,9 @@ func (c MockClient) EnvoyDoWithPort(ctx context.Context, podName, podNamespace, 
 		return nil, fmt.Errorf("unable to retrieve Pod: pods %q not found", podName)
 	}
 	return results, nil
+}
+
+func (c MockClient) CreatePerRPCCredentials(_ context.Context, _, _ string, _ []string, _ int64,
+) (credentials.PerRPCCredentials, error) {
+	return nil, nil
 }

istioctl/pkg/multixds/gather.go
@@ -91,6 +91,8 @@ func RequestAndProcessXds(dr *discovery.DiscoveryRequest, centralOpts clioptions
 	return mergeShards(responses)
 }
 
+var GetXdsResponse = xds.GetXdsResponse
+
 // nolint: lll
 func queryEachShard(all bool, dr *discovery.DiscoveryRequest, istioNamespace string, kubeClient kube.CLIClient, centralOpts clioptions.CentralControlPlaneOptions) ([]*discovery.DiscoveryResponse, error) {
 	labelSelector := centralOpts.XdsPodLabel
@@ -130,7 +132,7 @@ func queryEachShard(all bool, dr *discovery.DiscoveryRequest, istioNamespace str
 		}
 		defer fw.Close()
 		xdsOpts.Xds = fw.Address()
-		response, err := xds.GetXdsResponse(dr, istioNamespace, tokenServiceAccount, xdsOpts, dialOpts)
+		response, err := GetXdsResponse(dr, istioNamespace, tokenServiceAccount, xdsOpts, dialOpts)
 		if err != nil {
 			return nil, fmt.Errorf("could not get XDS from discovery pod %q: %v", pod.Name, err)
 		}

istioctl/pkg/proxystatus/proxystatus.go
@@ -36,98 +36,6 @@ import (
 
 var configDumpFile string
 
-func StatusCommand(ctx cli.Context) *cobra.Command {
-	var opts clioptions.ControlPlaneOptions
-
-	statusCmd := &cobra.Command{
-		Use:   "proxy-status [<type>/]<name>[.<namespace>]",
-		Short: "Retrieves the synchronization status of each Envoy in the mesh [kube only]",
-		Long: `
-Retrieves last sent and last acknowledged xDS sync from Istiod to each Envoy in the mesh
-
-`,
-		Example: `  # Retrieve sync status for all Envoys in a mesh
-  istioctl proxy-status
-
-  # Retrieve sync diff for a single Envoy and Istiod
-  istioctl proxy-status istio-egressgateway-59585c5b9c-ndc59.istio-system
-
-  # Retrieve sync diff between Istiod and one pod under a deployment
-  istioctl proxy-status deployment/productpage-v1
-
-  # Write proxy config-dump to file, and compare to Istio control plane
-  kubectl port-forward -n istio-system istio-egressgateway-59585c5b9c-ndc59 15000 &
-  curl localhost:15000/config_dump > cd.json
-  istioctl proxy-status istio-egressgateway-59585c5b9c-ndc59.istio-system --file cd.json
-`,
-		Aliases: []string{"ps"},
-		Args: func(cmd *cobra.Command, args []string) error {
-			if (len(args) == 0) && (configDumpFile != "") {
-				cmd.Println(cmd.UsageString())
-				return fmt.Errorf("--file can only be used when pod-name is specified")
-			}
-			return nil
-		},
-		RunE: func(c *cobra.Command, args []string) error {
-			kubeClient, err := ctx.CLIClientWithRevision(opts.Revision)
-			if err != nil {
-				return err
-			}
-			if len(args) > 0 {
-				podName, ns, err := ctx.InferPodInfoFromTypedResource(args[0], ctx.Namespace())
-				if err != nil {
-					return err
-				}
-				if ambient.IsZtunnelPod(kubeClient, podName, ns) {
-					_, _ = fmt.Fprintf(c.OutOrStdout(),
-						"Sync diff is not available for ztunnel pod %s.%s\n", podName, ns)
-					return nil
-				}
-				var envoyDump []byte
-				if configDumpFile != "" {
-					envoyDump, err = readConfigFile(configDumpFile)
-				} else {
-					path := "config_dump"
-					envoyDump, err = kubeClient.EnvoyDo(context.TODO(), podName, ns, "GET", path)
-				}
-				if err != nil {
-					return err
-				}
-
-				path := fmt.Sprintf("debug/config_dump?proxyID=%s.%s", podName, ns)
-				istiodDumps, err := kubeClient.AllDiscoveryDo(context.TODO(), ctx.IstioNamespace(), path)
-				if err != nil {
-					return err
-				}
-				c, err := compare.NewComparator(c.OutOrStdout(), istiodDumps, envoyDump)
-				if err != nil {
-					return err
-				}
-				return c.Diff()
-			}
-			queryStr := "debug/syncz"
-			if ctx.Namespace() != "" {
-				queryStr += "?namespace=" + ctx.Namespace()
-			}
-			statuses, err := kubeClient.AllDiscoveryDo(context.TODO(), ctx.IstioNamespace(), queryStr)
-			if err != nil {
-				return err
-			}
-			sw := pilot.StatusWriter{Writer: c.OutOrStdout()}
-			return sw.PrintAll(statuses)
-		},
-		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
-			return completion.ValidPodsNameArgs(cmd, ctx, args, toComplete)
-		},
-	}
-
-	opts.AttachControlPlaneFlags(statusCmd)
-	statusCmd.PersistentFlags().StringVarP(&configDumpFile, "file", "f", "",
-		"Envoy config dump JSON file")
-
-	return statusCmd
-}
-
 func readConfigFile(filename string) ([]byte, error) {
 	file := os.Stdin
 	if filename != "-" {
@@ -149,6 +57,23 @@ func readConfigFile(filename string) ([]byte, error) {
 	return data, nil
 }
 
+func StableXdsStatusCommand(ctx cli.Context) *cobra.Command {
+	cmd := XdsStatusCommand(ctx)
+	unstableFlags := []string{"xds-via-agents", "xds-via-agents-limit"}
+	cmd.PreRunE = func(cmd *cobra.Command, args []string) error {
+		for _, flag := range unstableFlags {
+			if cmd.PersistentFlags().Changed(flag) {
+				return fmt.Errorf("--%s is experimental. Use `istioctl experimental ps --%s`", flag, flag)
+			}
+		}
+		return nil
+	}
+	for _, flag := range unstableFlags {
+		_ = cmd.PersistentFlags().MarkHidden(flag)
+	}
+	return cmd
+}
+
 func XdsStatusCommand(ctx cli.Context) *cobra.Command {
 	var opts clioptions.ControlPlaneOptions
 	var centralOpts clioptions.CentralControlPlaneOptions
@@ -161,28 +86,31 @@ func XdsStatusCommand(ctx cli.Context) *cobra.Command {
 Retrieves last sent and last acknowledged xDS sync from Istiod to each Envoy in the mesh
 `,
 		Example: `  # Retrieve sync status for all Envoys in a mesh
-  istioctl x proxy-status
+  istioctl proxy-status
+
+  # Retrieve sync status for Envoys in a specific namespace
+  istioctl proxy-status --namespace foo
 
   # Retrieve sync diff for a single Envoy and Istiod
-  istioctl x proxy-status istio-egressgateway-59585c5b9c-ndc59.istio-system
+  istioctl proxy-status istio-egressgateway-59585c5b9c-ndc59.istio-system
 
   # SECURITY OPTIONS
 
   # Retrieve proxy status information directly from the control plane, using token security
   # (This is the usual way to get the proxy-status with an out-of-cluster control plane.)
-  istioctl x ps --xds-address istio.cloudprovider.example.com:15012
+  istioctl ps --xds-address istio.cloudprovider.example.com:15012
 
   # Retrieve proxy status information via Kubernetes config, using token security
   # (This is the usual way to get the proxy-status with an in-cluster control plane.)
-  istioctl x proxy-status
+  istioctl proxy-status
 
   # Retrieve proxy status information directly from the control plane, using RSA certificate security
   # (Certificates must be obtained before this step.  The --cert-dir flag lets istioctl bypass the Kubernetes API server.)
-  istioctl x ps --xds-address istio.example.com:15012 --cert-dir ~/.istio-certs
+  istioctl ps --xds-address istio.example.com:15012 --cert-dir ~/.istio-certs
 
   # Retrieve proxy status information via XDS from specific control plane in multi-control plane in-cluster configuration
   # (Select a specific control plane in an in-cluster canary Istio configuration.)
-  istioctl x ps --xds-label istio.io/rev=default
+  istioctl ps --xds-label istio.io/rev=default
 `,
 		Aliases: []string{"ps"},
 		RunE: func(c *cobra.Command, args []string) error {

istioctl/pkg/proxystatus/proxystatus_test.go
@@ -16,24 +16,36 @@ package proxystatus
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"net/http"
 	"strings"
 	"testing"
 
+	discovery "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
 	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/cli-runtime/pkg/resource"
 	"k8s.io/client-go/rest/fake"
 	cmdtesting "k8s.io/kubectl/pkg/cmd/testing"
 	cmdutil "k8s.io/kubectl/pkg/cmd/util"
 
+	"istio.io/api/label"
 	"istio.io/istio/istioctl/pkg/cli"
+	"istio.io/istio/istioctl/pkg/clioptions"
+	"istio.io/istio/istioctl/pkg/multixds"
+	"istio.io/istio/istioctl/pkg/xds"
 	"istio.io/istio/pilot/test/util"
 	"istio.io/istio/pkg/kube"
+	"istio.io/istio/pkg/test/util/assert"
 )
 
 type execTestCase struct {
-	args []string
+	args     []string
+	revision string
+	noIstiod bool
 
 	// Typically use one of the three
 	expectedOutput string // Expected constant output
@@ -45,7 +57,13 @@ type execTestCase struct {
 
 func TestProxyStatus(t *testing.T) {
 	cases := []execTestCase{
-		{ // case 0
+		{ // case 0, with no Isitod instance
+			args:           []string{},
+			noIstiod:       true,
+			expectedOutput: "Error: no running Istio pods in \"istio-system\"\n",
+			wantException:  true,
+		},
+		{ // case 1, with Istiod instance
 			args:           []string{},
 			expectedString: "NAME     CLUSTER     CDS     LDS     EDS     RDS     ECDS     ISTIOD",
 		},
@@ -68,16 +86,45 @@ func TestProxyStatus(t *testing.T) {
 		{ // case 6: new --revision argument
 			args:           strings.Split("--revision canary", " "),
 			expectedString: "NAME     CLUSTER     CDS     LDS     EDS     RDS     ECDS     ISTIOD",
+			revision:       "canary",
 		},
 		{ // case 7: supplying type that doesn't select pods should fail
 			args:          strings.Split("serviceaccount/sleep", " "),
 			wantException: true,
 		},
 	}
+	multixds.GetXdsResponse = func(_ *discovery.DiscoveryRequest, _ string, _ string, _ clioptions.CentralControlPlaneOptions, _ []grpc.DialOption,
+	) (*discovery.DiscoveryResponse, error) {
+		return &discovery.DiscoveryResponse{}, nil
+	}
+	t.Cleanup(func() {
+		multixds.GetXdsResponse = xds.GetXdsResponse
+	})
 
 	for i, c := range cases {
 		t.Run(fmt.Sprintf("case %d %s", i, strings.Join(c.args, " ")), func(t *testing.T) {
-			verifyExecTestOutput(t, StatusCommand(cli.NewFakeContext(nil)), c)
+			ctx := cli.NewFakeContext(&cli.NewFakeContextOption{
+				IstioNamespace: "istio-system",
+			})
+			if !c.noIstiod {
+				client, err := ctx.CLIClientWithRevision(c.revision)
+				assert.NoError(t, err)
+				_, err = client.Kube().CoreV1().Pods("istio-system").Create(context.TODO(), &corev1.Pod{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "istiod-test",
+						Namespace: "istio-system",
+						Labels: map[string]string{
+							"app":                 "istiod",
+							label.IoIstioRev.Name: c.revision,
+						},
+					},
+					Status: corev1.PodStatus{
+						Phase: corev1.PodRunning,
+					},
+				}, metav1.CreateOptions{})
+				assert.NoError(t, err)
+			}
+			verifyExecTestOutput(t, XdsStatusCommand(ctx), c)
 		})
 	}
 }

commit 721127126430f07e374a60a81b08817718131d04

istioctl: fix issue causing ps header for internal-debug
Regression from https://github.com/istio/istio/pull/47740. I added a
test

istioctl/pkg/internaldebug/internal-debug.go
@@ -15,6 +15,7 @@
 package internaldebug
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"strings"
@@ -27,7 +28,6 @@ import (
 	"istio.io/istio/istioctl/pkg/clioptions"
 	"istio.io/istio/istioctl/pkg/multixds"
 	"istio.io/istio/istioctl/pkg/util"
-	"istio.io/istio/istioctl/pkg/writer/pilot"
 	v3 "istio.io/istio/pilot/pkg/xds/v3"
 	"istio.io/istio/pkg/kube"
 )
@@ -141,7 +141,7 @@ By default it will use the default serviceAccount from (istio-system) namespace 
 			if err != nil {
 				return err
 			}
-			sw := pilot.XdsStatusWriter{
+			sw := DebugWriter{
 				Writer:                 c.OutOrStdout(),
 				InternalDebugAllIstiod: internalDebugAllIstiod,
 			}
@@ -166,3 +166,34 @@ By default it will use the default serviceAccount from (istio-system) namespace 
 }
 
 var internalDebugAllIstiod bool
+
+type DebugWriter struct {
+	Writer                 io.Writer
+	Namespace              string
+	InternalDebugAllIstiod bool
+}
+
+func (s *DebugWriter) PrintAll(drs map[string]*discovery.DiscoveryResponse) error {
+	// Gather the statuses before printing so they may be sorted
+	mappedResp := map[string]string{}
+	for id, dr := range drs {
+		for _, resource := range dr.Resources {
+			if s.InternalDebugAllIstiod {
+				mappedResp[id] = string(resource.Value) + "\n"
+			} else {
+				_, _ = s.Writer.Write(resource.Value)
+				_, _ = s.Writer.Write([]byte("\n"))
+			}
+		}
+	}
+	if len(mappedResp) > 0 {
+		mresp, err := json.MarshalIndent(mappedResp, "", "  ")
+		if err != nil {
+			return err
+		}
+		_, _ = s.Writer.Write(mresp)
+		_, _ = s.Writer.Write([]byte("\n"))
+	}
+
+	return nil
+}

istioctl/pkg/writer/pilot/status.go
@@ -74,53 +74,40 @@ func (s *XdsStatusWriter) setupStatusPrint(drs map[string]*discovery.DiscoveryRe
 	mappedResp := map[string]string{}
 	w := new(tabwriter.Writer).Init(s.Writer, 0, 8, 5, ' ', 0)
 	_, _ = fmt.Fprintln(w, "NAME\tCLUSTER\tCDS\tLDS\tEDS\tRDS\tECDS\tISTIOD\tVERSION")
-	for id, dr := range drs {
+	for _, dr := range drs {
 		for _, resource := range dr.Resources {
-			switch resource.TypeUrl {
-			case "type.googleapis.com/envoy.service.status.v3.ClientConfig":
-				clientConfig := xdsstatus.ClientConfig{}
-				err := resource.UnmarshalTo(&clientConfig)
-				if err != nil {
-					return nil, nil, fmt.Errorf("could not unmarshal ClientConfig: %w", err)
-				}
-				meta, err := model.ParseMetadata(clientConfig.GetNode().GetMetadata())
-				if err != nil {
-					return nil, nil, fmt.Errorf("could not parse node metadata: %w", err)
-				}
-				if s.Namespace != "" && meta.Namespace != s.Namespace {
-					continue
-				}
-				cds, lds, eds, rds, ecds := getSyncStatus(&clientConfig)
-				cp := multixds.CpInfo(dr)
-				fullStatus = append(fullStatus, &xdsWriterStatus{
-					proxyID:               clientConfig.GetNode().GetId(),
-					clusterID:             meta.ClusterID.String(),
-					istiodID:              cp.ID,
-					istiodVersion:         meta.IstioVersion,
-					clusterStatus:         cds,
-					listenerStatus:        lds,
-					routeStatus:           rds,
-					endpointStatus:        eds,
-					extensionconfigStatus: ecds,
-				})
-				if len(fullStatus) == 0 {
-					return nil, nil, fmt.Errorf("no proxies found (checked %d istiods)", len(drs))
-				}
-
-				sort.Slice(fullStatus, func(i, j int) bool {
-					return fullStatus[i].proxyID < fullStatus[j].proxyID
-				})
-			default:
-				for _, resource := range dr.Resources {
-					if s.InternalDebugAllIstiod {
-						mappedResp[id] = string(resource.Value) + "\n"
-					} else {
-						_, _ = s.Writer.Write(resource.Value)
-						_, _ = s.Writer.Write([]byte("\n"))
-					}
-				}
-				fullStatus = nil
+			clientConfig := xdsstatus.ClientConfig{}
+			err := resource.UnmarshalTo(&clientConfig)
+			if err != nil {
+				return nil, nil, fmt.Errorf("could not unmarshal ClientConfig: %w", err)
 			}
+			meta, err := model.ParseMetadata(clientConfig.GetNode().GetMetadata())
+			if err != nil {
+				return nil, nil, fmt.Errorf("could not parse node metadata: %w", err)
+			}
+			if s.Namespace != "" && meta.Namespace != s.Namespace {
+				continue
+			}
+			cds, lds, eds, rds, ecds := getSyncStatus(&clientConfig)
+			cp := multixds.CpInfo(dr)
+			fullStatus = append(fullStatus, &xdsWriterStatus{
+				proxyID:               clientConfig.GetNode().GetId(),
+				clusterID:             meta.ClusterID.String(),
+				istiodID:              cp.ID,
+				istiodVersion:         meta.IstioVersion,
+				clusterStatus:         cds,
+				listenerStatus:        lds,
+				routeStatus:           rds,
+				endpointStatus:        eds,
+				extensionconfigStatus: ecds,
+			})
+			if len(fullStatus) == 0 {
+				return nil, nil, fmt.Errorf("no proxies found (checked %d istiods)", len(drs))
+			}
+
+			sort.Slice(fullStatus, func(i, j int) bool {
+				return fullStatus[i].proxyID < fullStatus[j].proxyID
+			})
 		}
 	}
 	if len(mappedResp) > 0 {

istioctl/pkg/internaldebug/internal-debug_test.go
@@ -1,0 +1,164 @@
+// Copyright Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package internaldebug
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+	"testing"
+
+	discovery "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
+	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/cli-runtime/pkg/resource"
+	"k8s.io/client-go/rest/fake"
+	cmdtesting "k8s.io/kubectl/pkg/cmd/testing"
+	cmdutil "k8s.io/kubectl/pkg/cmd/util"
+
+	"istio.io/api/label"
+	"istio.io/istio/istioctl/pkg/cli"
+	"istio.io/istio/istioctl/pkg/clioptions"
+	"istio.io/istio/istioctl/pkg/multixds"
+	"istio.io/istio/istioctl/pkg/xds"
+	"istio.io/istio/pilot/test/util"
+	"istio.io/istio/pkg/kube"
+	"istio.io/istio/pkg/test/util/assert"
+)
+
+type execTestCase struct {
+	args     []string
+	revision string
+	noIstiod bool
+
+	// Typically use one of the three
+	expectedOutput string // Expected constant output
+	expectedString string // String output is expected to contain
+	goldenFilename string // Expected output stored in golden file
+
+	wantException bool
+}
+
+func TestInternalDebug(t *testing.T) {
+	cases := []execTestCase{
+		{ // case 0, no args
+			args:           []string{},
+			noIstiod:       true,
+			expectedOutput: "Error: debug type is required\n",
+			wantException:  true,
+		},
+		{ // case 1, no istiod
+			args:           []string{"adsz"},
+			noIstiod:       true,
+			expectedOutput: "Error: no running Istio pods in \"istio-system\"\n",
+			wantException:  true,
+		},
+		{ // case 2, with Istiod instance
+			args:           []string{"adsz"},
+			expectedString: "",
+		},
+	}
+	multixds.GetXdsResponse = func(_ *discovery.DiscoveryRequest, _ string, _ string, _ clioptions.CentralControlPlaneOptions, _ []grpc.DialOption,
+	) (*discovery.DiscoveryResponse, error) {
+		return &discovery.DiscoveryResponse{}, nil
+	}
+	t.Cleanup(func() {
+		multixds.GetXdsResponse = xds.GetXdsResponse
+	})
+
+	for i, c := range cases {
+		t.Run(fmt.Sprintf("case %d %s", i, strings.Join(c.args, " ")), func(t *testing.T) {
+			ctx := cli.NewFakeContext(&cli.NewFakeContextOption{
+				IstioNamespace: "istio-system",
+			})
+			if !c.noIstiod {
+				client, err := ctx.CLIClientWithRevision(c.revision)
+				assert.NoError(t, err)
+				_, err = client.Kube().CoreV1().Pods("istio-system").Create(context.TODO(), &corev1.Pod{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "istiod-test",
+						Namespace: "istio-system",
+						Labels: map[string]string{
+							"app":                 "istiod",
+							label.IoIstioRev.Name: c.revision,
+						},
+					},
+					Status: corev1.PodStatus{
+						Phase: corev1.PodRunning,
+					},
+				}, metav1.CreateOptions{})
+				assert.NoError(t, err)
+			}
+			verifyExecTestOutput(t, DebugCommand(ctx), c)
+		})
+	}
+}
+
+func verifyExecTestOutput(t *testing.T, cmd *cobra.Command, c execTestCase) {
+	t.Helper()
+
+	var out bytes.Buffer
+	cmd.SetArgs(c.args)
+	cmd.SilenceUsage = true
+	cmd.SetOut(&out)
+	cmd.SetErr(&out)
+
+	fErr := cmd.Execute()
+	output := out.String()
+
+	if c.expectedOutput != "" && c.expectedOutput != output {
+		t.Fatalf("Unexpected output for 'istioctl %s'\n got: %q\nwant: %q", strings.Join(c.args, " "), output, c.expectedOutput)
+	}
+
+	if c.expectedString != "" && !strings.Contains(output, c.expectedString) {
+		t.Fatalf("Output didn't match for '%s %s'\n got %v\nwant: %v", cmd.Name(), strings.Join(c.args, " "), output, c.expectedString)
+	}
+
+	if c.goldenFilename != "" {
+		util.CompareContent(t, []byte(output), c.goldenFilename)
+	}
+
+	if c.wantException {
+		if fErr == nil {
+			t.Fatalf("Wanted an exception for 'istioctl %s', didn't get one, output was %q",
+				strings.Join(c.args, " "), output)
+		}
+	} else {
+		if fErr != nil {
+			t.Fatalf("Unwanted exception for 'istioctl %s': %v", strings.Join(c.args, " "), fErr)
+		}
+	}
+}
+
+func init() {
+	cli.MakeKubeFactory = func(k kube.CLIClient) cmdutil.Factory {
+		tf := cmdtesting.NewTestFactory()
+		_, _, codec := cmdtesting.NewExternalScheme()
+		tf.UnstructuredClient = &fake.RESTClient{
+			NegotiatedSerializer: resource.UnstructuredPlusDefaultContentConfig().NegotiatedSerializer,
+			Resp: &http.Response{
+				StatusCode: http.StatusOK,
+				Header:     cmdtesting.DefaultHeader(),
+				Body: cmdtesting.ObjBody(codec,
+					cmdtesting.NewInternalType("", "", "foo")),
+			},
+		}
+		return tf
+	}
+}