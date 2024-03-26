97d4de9ee8d915b9a52409df3cdd53d135c27197
Istio Automation <istio-testing-bot@google.com>
2024-03-25 20:06:14 -0700

    push xDS when SE TargetPort is changed (#50096)
    Co-authored-by: dddddai <dddwq@foxmail.com>

--- pilot/pkg/serviceregistry/serviceentry/controller_test.go
+++ pilot/pkg/serviceregistry/serviceentry/controller_test.go
@@ -371,6 +371,42 @@ func TestServiceDiscoveryServiceUpdate(t *testing.T) {
 			Event{Type: "xds full", ID: "*.google.com"})
 	})
 
+	t.Run("change target port", func(t *testing.T) {
+		// Change the target port
+		targetPortChanged := func() *config.Config {
+			c := httpStaticOverlayUpdated.DeepCopy()
+			c.Spec.(*networking.ServiceEntry).Ports[0].TargetPort = 33333
+			return &c
+		}()
+		createConfigs([]*config.Config{targetPortChanged}, store, t)
+
+		// Endpoint ports should be changed
+		instances := append(baseInstances,
+			makeInstance(httpStaticOverlay, "5.5.5.5", 33333, httpStaticOverlay.Spec.(*networking.ServiceEntry).Ports[0], map[string]string{"overlay": "bar"}, PlainText),
+			makeInstance(httpStaticOverlay, "6.6.6.6", 33333, httpStaticOverlay.Spec.(*networking.ServiceEntry).Ports[0], map[string]string{"other": "bar"}, PlainText))
+		expectServiceInstances(t, sd, targetPortChanged, 0, instances)
+
+		// Expect a full push, as the target port has changed
+		expectEvents(t, events,
+			Event{Type: "service", ID: "*.google.com", Namespace: httpStaticOverlayUpdated.Namespace},
+			Event{Type: "eds cache", ID: "*.google.com", Namespace: httpStaticOverlayUpdated.Namespace},
+			Event{Type: "xds full", ID: "*.google.com"})
+
+		// Restore the target port
+		createConfigs([]*config.Config{httpStaticOverlayUpdated}, store, t)
+
+		// Endpoint ports should be changed
+		instances = append(baseInstances,
+			makeInstance(httpStaticOverlay, "5.5.5.5", 4567, httpStaticOverlay.Spec.(*networking.ServiceEntry).Ports[0], map[string]string{"overlay": "bar"}, PlainText),
+			makeInstance(httpStaticOverlay, "6.6.6.6", 4567, httpStaticOverlay.Spec.(*networking.ServiceEntry).Ports[0], map[string]string{"other": "bar"}, PlainText))
+		expectServiceInstances(t, sd, targetPortChanged, 0, instances)
+		// Expect a full push, as the target port has changed
+		expectEvents(t, events,
+			Event{Type: "service", ID: "*.google.com", Namespace: httpStaticOverlayUpdated.Namespace},
+			Event{Type: "eds cache", ID: "*.google.com", Namespace: httpStaticOverlayUpdated.Namespace},
+			Event{Type: "xds full", ID: "*.google.com"})
+	})
+
 	t.Run("change host", func(t *testing.T) {
 		// same as httpStaticOverlayUpdated but with an additional host
 		httpStaticHost := func() *config.Config {
