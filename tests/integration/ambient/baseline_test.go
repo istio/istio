bf9fd6127f5bebf2f0024362f3c687849f390749
Ian Rudie <ian.rudie@solo.io>
2024-04-18 16:45:42 -0500

    tests for workload-attached-waypoint policies (#50464)
    * basic tests for wl waypoint
    
    Signed-off-by: Ian Rudie <ian.rudie@solo.io>
    
    * add to-wl waypoint logic to TestL7JWT
    
    Signed-off-by: Ian Rudie <ian.rudie@solo.io>
    
    * move skip for self call outside of override check logic
    
    Signed-off-by: Ian Rudie <ian.rudie@solo.io>
    
    * update skip message for self call
    
    Signed-off-by: Ian Rudie <ian.rudie@solo.io>
    
    ---------
    
    Signed-off-by: Ian Rudie <ian.rudie@solo.io>

--- tests/integration/ambient/baseline_test.go
+++ tests/integration/ambient/baseline_test.go
@@ -847,14 +847,6 @@ func TestAuthorizationL4(t *testing.T) {
 				t.Skip("https://github.com/istio/istio/issues/43238")
 			}
 
-			if src.Config().ZTunnelCaptured() && dst.Config().HasWorkloadAddressedWaypointProxy() {
-				// this case should bypass waypoints because traffic is svc addressed but
-				// presently a ztunnel bug will drop this traffic because it doesn't differentiate
-				// between svc and wl addressed traffic when determining if the connection
-				// should have gone through a waypoint.
-				t.Skip("TODO: open an issue to address this ztunnel issue")
-			}
-
 			overrideCheck := func(src echo.Instance, dst echo.Instance, opt *echo.CallOptions) {
 				switch {
 				case src.Config().IsUncaptured() && dst.Config().HasAnyWaypointProxy():
@@ -1134,14 +1126,6 @@ func TestAuthorizationL7(t *testing.T) {
 				t.Skip("https://github.com/istio/istio/issues/43238")
 			}
 
-			if dst.Config().HasWorkloadAddressedWaypointProxy() {
-				// this case should bypass waypoints because traffic is svc addressed but
-				// presently a ztunnel bug will drop this traffic because it doesn't differentiate
-				// between svc and wl addressed traffic when determining if the connection
-				// should have gone through a waypoint.
-				t.Skip("TODO: open an issue to address this ztunnel issue")
-			}
-
 			policySpec := `
   rules:
   - to:
@@ -1187,7 +1171,7 @@ func TestAuthorizationL7(t *testing.T) {
 `
 			// for most cases just use the normal policy spec
 			policySpecWL := policySpec
-			if dst.Config().HasServiceAddressedWaypointProxy() {
+			if dst.Config().HasAnyWaypointProxy() {
 				// for svc addressed traffic we want the WL policy to allow Waypoint -> Workload
 				policySpecWL = `
   rules:
@@ -1196,11 +1180,18 @@ func TestAuthorizationL7(t *testing.T) {
         principals: ["cluster.local/ns/{{.Namespace}}/sa/{{.WaypointName}}"]
 `
 			}
+			waypointName := "none"
+			switch {
+			case dst.Config().HasServiceAddressedWaypointProxy():
+				waypointName = dst.Config().ServiceWaypointProxy
+			case dst.Config().HasWorkloadAddressedWaypointProxy():
+				waypointName = dst.Config().WorkloadWaypointProxy
+			}
 			t.ConfigIstio().Eval(apps.Namespace.Name(), map[string]string{
 				"Destination":  dst.Config().Service,
 				"Source":       src.Config().Service,
 				"Namespace":    apps.Namespace.Name(),
-				"WaypointName": dst.Config().ServiceWaypointProxy,
+				"WaypointName": waypointName,
 			}, `
 apiVersion: security.istio.io/v1beta1
 kind: AuthorizationPolicy
@@ -1248,10 +1239,17 @@ spec:
 				case dst.Config().IsUncaptured() && !dst.Config().HasSidecar():
 					// No destination means no RBAC to apply. Make sure we do not accidentally reject
 					opt.Check = check.OK()
-				case !dst.Config().HasServiceAddressedWaypointProxy() && !dst.Config().HasSidecar():
+				case !dst.Config().HasAnyWaypointProxy() && !dst.Config().HasSidecar():
 					// Only waypoint proxy can handle L7 policies
 					opt.Check = CheckDeny
+				case dst.Config().HasWorkloadAddressedWaypointProxy() && !dst.Config().HasServiceAddressedWaypointProxy():
+					// send traffic to the workload instead of the service so it will redirect to the WL waypoint
+					opt.Address = dst.MustWorkloads().Addresses()[0]
+					opt.Port = echo.Port{ServicePort: ports.All().MustForName(opt.Port.Name).WorkloadPort}
 				}
+			}
+			if src == dst {
+				t.Skip("self call is not captured, L7 features will not work")
 			}
 			t.NewSubTest("simple deny").Run(func(t framework.TestContext) {
 				opt := opt.DeepCopy()
@@ -1343,8 +1341,18 @@ func TestL7JWT(t *testing.T) {
 				t.Skip("https://github.com/istio/istio/issues/43238")
 			}
 
-			if !dst.Config().HasServiceAddressedWaypointProxy() {
+			if !dst.Config().HasAnyWaypointProxy() {
 				t.Skip("L7 JWT is only for waypoints")
+			}
+
+			switch {
+			case dst.Config().HasWorkloadAddressedWaypointProxy() && !dst.Config().HasServiceAddressedWaypointProxy():
+				// send traffic to the workload instead of the service so it will redirect to the WL waypoint
+				opt.Address = dst.MustWorkloads().Addresses()[0]
+				opt.Port = echo.Port{ServicePort: ports.All().MustForName(opt.Port.Name).WorkloadPort}
+				if src == dst {
+					t.Skip("self call is not captured, L7 features will not work")
+				}
 			}
 
 			t.ConfigIstio().New().EvalFile(apps.Namespace.Name(), map[string]any{
