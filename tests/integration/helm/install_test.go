57a52ef1ab177e93f268cc44b6cf136484d9330f
Keith Mattix II <keithmattix@microsoft.com>
2024-04-22 17:00:40 -0500

    [Release Channels] Implement with a ValidatingAdmissionPolicy (#50358)
    * Add ValidatingAdmissionPolicy for stable channel implementation
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>
    
    * Make VAPs work with revisions and stub out some tests
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>
    
    * Add tests for default and revisioned VAP
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>
    
    * Simplify expression
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>
    
    * Remove kind customizations
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>
    
    * VAP is v1 now
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>
    
    * Typo
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>
    
    ---------
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>

--- tests/integration/helm/install_test.go
+++ tests/integration/helm/install_test.go
@@ -21,6 +21,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"k8s.io/apimachinery/pkg/types"
@@ -42,14 +43,14 @@ global:
 `
 	framework.
 		NewTest(t).
-		Run(setupInstallation(overrideValuesStr, false, DefaultNamespaceConfig))
+		Run(setupInstallation(overrideValuesStr, false, DefaultNamespaceConfig, ""))
 }
 
 // TestAmbientInstall tests Istio ambient profile installation using Helm
 func TestAmbientInstall(t *testing.T) {
 	framework.
 		NewTest(t).
-		Run(setupInstallation(ambientProfileOverride, true, DefaultNamespaceConfig))
+		Run(setupInstallation(ambientProfileOverride, true, DefaultNamespaceConfig, ""))
 }
 
 func TestAmbientInstallMultiNamespace(t *testing.T) {
@@ -82,12 +83,108 @@ func TestAmbientInstallMultiNamespace(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			framework.
 				NewTest(t).
-				Run(setupInstallation(ambientProfileOverride, true, tt.nsConfig))
+				Run(setupInstallation(ambientProfileOverride, true, tt.nsConfig, ""))
 		})
 	}
 }
 
-func setupInstallation(overrideValuesStr string, isAmbient bool, config NamespaceConfig) func(t framework.TestContext) {
+// TestReleaseChannels tests that non-stable CRDs and fields get blocked
+// by the default ValidatingAdmissionPolicy
+func TestReleaseChannels(t *testing.T) {
+	overrideValuesStr := `
+global:
+  hub: %s
+  tag: %s
+  variant: %q
+profile: stable
+`
+
+	framework.
+		NewTest(t).
+		Run(setupInstallationWithCustomCheck(overrideValuesStr, false, DefaultNamespaceConfig, func(t framework.TestContext) {
+			// Try to apply an EnvoyFilter (it should be rejected)
+			expectedErrorPrefix := `%s "sample" is forbidden: ValidatingAdmissionPolicy 'stable-channel-default-policy.istio.io' ` +
+				`with binding 'stable-channel-default-policy-binding.istio.io' denied request`
+			err := t.ConfigIstio().Eval("default", nil, sampleEnvoyFilter).Apply()
+			if err == nil {
+				t.Errorf("Did not receive an error while applying sample EnvoyFilter with stable admission policy")
+			} else {
+				msg := fmt.Sprintf(expectedErrorPrefix, "envoyfilters.networking.istio.io")
+				if !strings.Contains(err.Error(), msg) {
+					t.Errorf("Expected error %q to contain %q", err.Error(), msg)
+				}
+			}
+
+			// Now test field-level blocks with Telemetry
+			err = t.ConfigIstio().Eval("default", nil, extendedTelemetry).Apply()
+			if err == nil {
+				t.Error("Did not receive an error while applying extended Telemetry resource with stable admission policy")
+			} else {
+				msg := fmt.Sprintf(expectedErrorPrefix, "telemetries.telemetry.istio.io")
+				if !strings.Contains(err.Error(), msg) {
+					t.Errorf("Expected error %q to contain %q", err.Error(), msg)
+				}
+			}
+		}, ""))
+}
+
+// TestRevisionedReleaseChannels tests that non-stable CRDs and fields get blocked
+// by the revisioned ValidatingAdmissionPolicy
+func TestRevisionedReleaseChannels(t *testing.T) {
+	overrideValuesStr := `
+global:
+  hub: %s
+  tag: %s
+  variant: %q
+profile: stable
+revision: 1-x
+defaultRevision: ""
+`
+	revision := "1-x"
+	framework.
+		NewTest(t).
+		Run(setupInstallationWithCustomCheck(overrideValuesStr, false, DefaultNamespaceConfig, func(t framework.TestContext) {
+			// Try to apply an EnvoyFilter (it should be rejected)
+			expectedErrorPrefix := `%s "sample" is forbidden: ValidatingAdmissionPolicy 'stable-channel-policy-1-x-istio-system.istio.io' ` +
+				`with binding 'stable-channel-policy-binding-1-x-istio-system.istio.io' denied request`
+			err := t.ConfigIstio().Eval("default", nil, fmt.Sprintf(revisionedSampleEnvoyFilter, revision)).Apply()
+			if err == nil {
+				t.Errorf("Did not receive an error while applying sample EnvoyFilter with stable admission policy")
+			} else {
+				msg := fmt.Sprintf(expectedErrorPrefix, "envoyfilters.networking.istio.io")
+				if !strings.Contains(err.Error(), msg) {
+					t.Errorf("Expected error %q to contain %q", err.Error(), msg)
+				}
+			}
+
+			// Now test field-level blocks with Telemetry
+			err = t.ConfigIstio().Eval("default", nil, fmt.Sprintf(revisionedExtendedTelemetry, revision)).Apply()
+			if err == nil {
+				t.Error("Did not receive an error while applying extended Telemetry resource with stable admission policy")
+			} else {
+				msg := fmt.Sprintf(expectedErrorPrefix, "telemetries.telemetry.istio.io")
+				if !strings.Contains(err.Error(), msg) {
+					t.Errorf("Expected error %q to contain %q", err.Error(), msg)
+				}
+			}
+		}, revision))
+}
+
+func setupInstallation(overrideValuesStr string, isAmbient bool, config NamespaceConfig, revision string) func(t framework.TestContext) {
+	return baseSetup(overrideValuesStr, isAmbient, config, func(t framework.TestContext) {
+		sanitycheck.RunTrafficTest(t, t)
+	}, revision)
+}
+
+func setupInstallationWithCustomCheck(overrideValuesStr string, isAmbient bool, config NamespaceConfig,
+	check func(t framework.TestContext), revision string,
+) func(t framework.TestContext) {
+	return baseSetup(overrideValuesStr, isAmbient, config, check, revision)
+}
+
+func baseSetup(overrideValuesStr string, isAmbient bool, config NamespaceConfig,
+	check func(t framework.TestContext), revision string,
+) func(t framework.TestContext) {
 	return func(t framework.TestContext) {
 		workDir, err := t.CreateTmpDirectory("helm-install-test")
 		if err != nil {
@@ -111,12 +208,13 @@ func setupInstallation(overrideValuesStr string, isAmbient bool, config Namespac
 				}
 			}
 		})
+
 		InstallIstio(t, cs, h, overrideValuesFile, "", true, isAmbient, config)
 
-		VerifyInstallation(t, cs, config, true, isAmbient)
-		verifyValidation(t)
+		VerifyInstallation(t, cs, config, true, isAmbient, revision)
+		verifyValidation(t, revision)
 
-		sanitycheck.RunTrafficTest(t, t)
+		check(t)
 		t.Cleanup(func() {
 			DeleteIstio(t, h, cs, config, isAmbient)
 		})
