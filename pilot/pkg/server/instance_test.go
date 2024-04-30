3823a53473cb04c88fe7fa554dcc13390f1a0296
John Howard <howardjohn@google.com>
2024-04-30 16:02:39 -0700

    Fix test flake in TestRunComponentsAfterStart (#50767)

--- pilot/pkg/server/instance_test.go
+++ pilot/pkg/server/instance_test.go
@@ -23,6 +23,7 @@ import (
 	"go.uber.org/atomic"
 
 	"istio.io/istio/pilot/pkg/server"
+	"istio.io/istio/pkg/test"
 )
 
 func TestStartWithError(t *testing.T) {
@@ -43,7 +44,7 @@ func TestStartWithNoError(t *testing.T) {
 	g := NewWithT(t)
 
 	inst := server.New()
-	c := newFakeComponent(0)
+	c := newFakeComponent(0, test.NewStop(t))
 	inst.RunComponent("fake", c.Run)
 
 	stop := newReclosableChannel()
@@ -55,6 +56,8 @@ func TestStartWithNoError(t *testing.T) {
 func TestRunComponentsAfterStart(t *testing.T) {
 	longDuration := 10 * time.Second
 	shortDuration := 10 * time.Millisecond
+	// Just used to make sure we do not leak goroutines
+	stop := test.NewStop(t)
 	cases := []struct {
 		name  string
 		c     *fakeComponent
@@ -65,7 +68,7 @@ func TestRunComponentsAfterStart(t *testing.T) {
 			name: "RunComponent",
 			// Use a large duration - it will not complete before the end of the test.
 			// This is used to verify that we don't wait for it while shutting down.
-			c:     newFakeComponent(longDuration),
+			c:     newFakeComponent(longDuration, stop),
 			async: false,
 			wait:  false,
 		},
@@ -73,13 +76,13 @@ func TestRunComponentsAfterStart(t *testing.T) {
 			name: "RunComponentAsync",
 			// Use a large duration - it will not complete before the end of the test.
 			// This is used to verify that we don't wait for it while shutting down.
-			c:     newFakeComponent(longDuration),
+			c:     newFakeComponent(longDuration, stop),
 			async: true,
 			wait:  false,
 		},
 		{
 			name:  "RunComponentAsyncAndWait",
-			c:     newFakeComponent(shortDuration),
+			c:     newFakeComponent(shortDuration, stop),
 			async: true,
 			wait:  true,
 		},
@@ -94,8 +97,8 @@ func TestRunComponentsAfterStart(t *testing.T) {
 			inst := server.New()
 			g.Expect(inst.Start(stop.c)).To(BeNil())
 
+			component := c.c.Run
 			go func() {
-				component := c.c.Run
 				if c.async {
 					if c.wait {
 						inst.RunComponentAsyncAndWait("test", component)
@@ -149,13 +152,15 @@ type fakeComponent struct {
 	started   *atomic.Bool
 	completed *atomic.Bool
 	d         time.Duration
+	stop      chan struct{}
 }
 
-func newFakeComponent(d time.Duration) *fakeComponent {
+func newFakeComponent(d time.Duration, stop chan struct{}) *fakeComponent {
 	return &fakeComponent{
 		started:   atomic.NewBool(false),
 		completed: atomic.NewBool(false),
 		d:         d,
+		stop:      stop,
 	}
 }
 
@@ -163,7 +168,7 @@ func (c *fakeComponent) Run(stop <-chan struct{}) error {
 	c.started.Store(true)
 	select {
 	case <-time.After(c.d):
-	case <-stop:
+	case <-c.stop: // ignore incoming stop; for test purposes we use our own stop
 	}
 	c.completed.Store(true)
 	return nil
