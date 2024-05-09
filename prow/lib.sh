8f9f58bc073968436e7239bd85ac9add5d1d6f7d
John Howard <howardjohn@google.com>
2024-05-09 05:26:52 -0700

    Use new VM base images (#50927)

--- prow/lib.sh
+++ prow/lib.sh
@@ -118,12 +118,10 @@ function build_images() {
   targets="docker.pilot docker.proxyv2 "
 
   # use ubuntu:jammy to test vms by default
-  nonDistrolessTargets="docker.app docker.app_sidecar_ubuntu_jammy docker.ext-authz "
+  nonDistrolessTargets="docker.app docker.app_sidecar_ubuntu_noble docker.ext-authz "
   if [[ "${JOB_TYPE:-presubmit}" == "postsubmit" ]]; then
     # We run tests across all VM types only in postsubmit
-    nonDistrolessTargets+="docker.app_sidecar_ubuntu_bionic docker.app_sidecar_debian_11 "
-    # TODO(https://github.com/istio/istio/issues/38224)
-#    nonDistrolessTargets+="docker.app_sidecar_rockylinux_8 "
+    nonDistrolessTargets+="docker.app_sidecar_ubuntu_bionic docker.app_sidecar_debian_12 docker.app_sidecar_rockylinux_9 "
   fi
   if [[ "${SELECT_TEST}" == "test.integration.operator.kube" || "${SELECT_TEST}" == "test.integration.kube" || "${JOB_TYPE:-postsubmit}" == "postsubmit" ]]; then
     targets+="docker.operator "
