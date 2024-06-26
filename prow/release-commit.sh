4801bd05840a9b1ff65785a8383c885f280d48b3
zirain <zirain2009@gmail.com>
2024-06-26 13:11:33 +0800

    update deps

--- prow/release-commit.sh
+++ prow/release-commit.sh
@@ -36,7 +36,7 @@ docker run --rm --privileged "${DOCKER_HUB}/qemu-user-static" --reset -p yes
 export ISTIO_DOCKER_QEMU=true
 
 # Use a pinned version in case breaking changes are needed
-BUILDER_SHA=38c3f703303351081c391bb03634a68e7ecc653e
+BUILDER_SHA=f11602637a9b7df8bf9908e4ce1289bb2b6a6563
 
 # Reference to the next minor version of Istio
 # This will create a version like 1.4-alpha.sha
