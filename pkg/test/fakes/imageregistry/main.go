80a9cb367dff578e1e6fb82d4027554752dbf8b7
Jacek Ewertowski <jewertow@redhat.com>
2024-07-01 16:03:55 +0200

    Use default client
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>

--- pkg/test/fakes/imageregistry/main.go
+++ pkg/test/fakes/imageregistry/main.go
@@ -97,12 +97,8 @@ func (h *Handler) getFirstLayerURL(imageName string, tag string) (string, error)
 	}
 
 	var t *http.Transport
-	if *scheme == "https" {
-		t = remote.DefaultTransport.(*http.Transport).Clone()
-		t.TLSClientConfig = &tls.Config{InsecureSkipVerify: true} // nolint: gosec // test only code
-	} else {
-		t = &http.Transport{}
-	}
+	t = remote.DefaultTransport.(*http.Transport).Clone()
+	t.TLSClientConfig = &tls.Config{InsecureSkipVerify: true} // nolint: gosec // test only code
 	desc, err := remote.Get(ref, remote.WithTransport(t))
 	if err != nil {
 		return "", fmt.Errorf("could not get the description: %v", err)
