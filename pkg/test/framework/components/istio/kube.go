8d9ea384648e699753e5720af04784bcbcf19539
Sridhar Gaddam <sgaddam@redhat.com>
2024-07-09 00:35:29 +0530

    Enable dualStack cfg for istiod control plane in dualStack tests (#51844)
    Currently, in our integration tests, when "enableDualStack=true"
    we configure the ingress/egress gateways as dualStack but are
    not configuring the istiod service as a dualStack service.
    This PR updates the istiod configuration to use a dualStack
    istiod control plane for dualStack integration tests.
    
    Signed-off-by: Sridhar Gaddam <sgaddam@redhat.com>

--- pkg/test/framework/components/istio/kube.go
+++ pkg/test/framework/components/istio/kube.go
@@ -622,6 +622,7 @@ func commonInstallArgs(ctx resource.Context, cfg Config, c cluster.Cluster, defa
 
 	if ctx.Settings().EnableDualStack {
 		args.AppendSet("values.pilot.env.ISTIO_DUAL_STACK", "true")
+		args.AppendSet("values.pilot.ipFamilyPolicy", string(corev1.IPFamilyPolicyRequireDualStack))
 		args.AppendSet("meshConfig.defaultConfig.proxyMetadata.ISTIO_DUAL_STACK", "true")
 		args.AppendSet("values.gateways.istio-ingressgateway.ipFamilyPolicy", string(corev1.IPFamilyPolicyRequireDualStack))
 		args.AppendSet("values.gateways.istio-egressgateway.ipFamilyPolicy", string(corev1.IPFamilyPolicyRequireDualStack))
