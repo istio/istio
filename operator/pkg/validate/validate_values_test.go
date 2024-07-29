2bf3767905a7e9d4ef08726e4e6d8cd1a5aac762
John Howard <howardjohn@google.com>
2024-07-29 15:23:13 -0700

    Flatten CNI values as well (#52395)
    Followup to https://github.com/istio/istio/pull/52036

--- operator/pkg/validate/validate_values_test.go
+++ operator/pkg/validate/validate_values_test.go
@@ -206,7 +206,7 @@ func TestValidateValuesFromValuesYAMLs(t *testing.T) {
 	valuesYAML := ""
 	var allFiles []string
 	manifestDir := filepath.Join(repoRootDir, "manifests/charts")
-	for _, sd := range []string{"base", "gateways", "istio-cni"} {
+	for _, sd := range []string{"base", "gateways"} {
 		dir := filepath.Join(manifestDir, sd)
 		files, err := util.FindFiles(dir, yamlFileFilter)
 		if err != nil {
