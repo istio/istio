da89d8b0eb40906c97f54f5f85daa23cf9d2c865
Costin Manolache <costin@google.com>
2024-07-30 20:16:53 +0000

    Change the name based on feedback, and set it to false to see the impact on tests (will change back)
    Change-Id: Ifd4627e4d111fbe678c2ef3a3955dd1678a354cc

--- pilot/pkg/features/pilot.go
+++ pilot/pkg/features/pilot.go
@@ -120,7 +120,7 @@ var (
 	WorkloadEntryCrossCluster = env.Register("PILOT_ENABLE_CROSS_CLUSTER_WORKLOAD_ENTRY", true,
 		"If enabled, pilot will read WorkloadEntry from other clusters, selectable by Services in that cluster.").Get()
 
-	CentralIstiodAccess = env.Register("PILOT_CENTRAL_ISTIOD", true,
+	CentralIstiodAccess = env.Register("PILOT_REMOTE_CLUSER_AUTH", false,
 		"If disabled, pilot will reject CLUSTER_ID meta and reject XDS, certificates and secret requests for different clusters.").Get()
 
 	WasmRemoteLoadConversion = env.Register("ISTIO_AGENT_ENABLE_WASM_REMOTE_LOAD_CONVERSION", true,
