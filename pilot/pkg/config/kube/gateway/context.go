7654e8d2924f5f02689af5aa89f97aca4f824b9b
Zhonghu Xu <xuzhonghu@huawei.com>
2024-07-30 14:53:37 +0800

    update

--- pilot/pkg/config/kube/gateway/context.go
+++ pilot/pkg/config/kube/gateway/context.go
@@ -88,7 +88,9 @@ func (gc GatewayContext) ResolveGatewayInstances(
 			instances := gc.ps.ServiceEndpointsByPort(svc, port, nil)
 			if len(instances) > 0 {
 				foundInternal.Insert(fmt.Sprintf("%s:%d", g, port))
-				foundInternalIP.InsertAll(svc.GetAllAddressesForProxy(&model.Proxy{Metadata: &model.NodeMetadata{ClusterID: gc.cluster}})...)
+				dummyProxy := &model.Proxy{Metadata: &model.NodeMetadata{ClusterID: gc.cluster}}
+				dummyProxy.SetIPMode(model.Dual)
+				foundInternalIP.InsertAll(svc.GetAllAddressesForProxy(dummyProxy)...)
 				if svc.Attributes.ClusterExternalAddresses.Len() > 0 {
 					// Fetch external IPs from all clusters
 					svc.Attributes.ClusterExternalAddresses.ForEach(func(c cluster.ID, externalIPs []string) {
