e69bc1ba734554cac1b7eb376a0af0e3f9b81a33
John Howard <john.howard@solo.io>
2024-08-02 13:28:26 -0700

    more

--- operator/pkg/helmreconciler/prune_test.go
+++ operator/pkg/helmreconciler/prune_test.go
@@ -15,7 +15,14 @@
 package helmreconciler
 
 import (
+	"context"
 	_ "embed"
+	"errors"
+	"fmt"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/interceptor"
 	"testing"
 
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
@@ -24,10 +31,6 @@ import (
 	"istio.io/api/label"
 	"istio.io/istio/operator/pkg/apis/istio/v1alpha1"
 	"istio.io/istio/operator/pkg/name"
-	"istio.io/istio/operator/pkg/object"
-	"istio.io/istio/operator/pkg/util/clog"
-	"istio.io/istio/operator/pkg/util/progress"
-	"istio.io/istio/pkg/kube"
 	"istio.io/istio/pkg/test/util/assert"
 )
 
@@ -38,67 +41,77 @@ var (
 	iopTestGwData2 []byte
 )
 
-func applyResourcesIntoCluster(t *testing.T, h *HelmReconciler, manifestMap name.ManifestMap) {
-	for cn, ms := range manifestMap.Consolidated() {
-		objects, err := object.ParseK8sObjectsFromYAMLManifest(ms)
-		if err != nil {
-			t.Fatalf("failed parse k8s objects from yaml: %v", err)
+// TODO: move to shared
+var interceptorFunc = interceptor.Funcs{Patch: func(
+	ctx context.Context,
+	clnt client.WithWatch,
+	obj client.Object,
+	patch client.Patch,
+	opts ...client.PatchOption,
+) error {
+	// Apply patches are supposed to upsert, but fake client fails if the object doesn't exist,
+	// if an apply patch occurs for an object that doesn't yet exist, create it.
+	if patch.Type() != types.ApplyPatchType {
+		return clnt.Patch(ctx, obj, patch, opts...)
+	}
+	check, ok := obj.DeepCopyObject().(client.Object)
+	if !ok {
+		return errors.New("could not check for object in fake client")
+	}
+	if err := clnt.Get(ctx, client.ObjectKeyFromObject(obj), check); kerrors.IsNotFound(err) {
+		if err := clnt.Create(ctx, check); err != nil {
+			return fmt.Errorf("could not inject object creation for fake: %w", err)
 		}
-		for _, obj := range objects {
-			obju := obj.UnstructuredObject()
-			if err := h.applyLabelsAndAnnotations(obju, cn); err != nil {
-				t.Errorf("failed to apply label and annotations: %v", err)
-			}
-			if err := h.ApplyObject(obj.UnstructuredObject()); err != nil {
-				t.Errorf("HelmReconciler.ApplyObject() error = %v", err)
-			}
+	} else if err != nil {
+		return err
+	}
+	obj.SetResourceVersion(check.GetResourceVersion())
+	return clnt.Update(ctx, obj)
+}}
+
+
+// TODO
+func TestGetPrunedResources(t *testing.T) {
+	cl := fake.NewClientBuilder().WithInterceptorFuncs(interceptorFunc).Build()
+	// init two custom gateways with revision
+	gateways := [][]byte{iopTestGwData1, iopTestGwData2}
+	for i, data := range gateways {
+		iop := &v1alpha1.IstioOperator{}
+		err := yaml.UnmarshalStrict(data, iop)
+		assert.NoError(t, err)
+		_ = i
+		//h := &HelmReconciler{
+		//	client:     cl,
+		//	kubeClient: kube.NewFakeClientWithVersion("24"),
+		//	opts: &Options{
+		//		ProgressLog: progress.NewLog(),
+		//		Log:         clog.NewDefaultLogger(),
+		//	},
+		//	iop: iop,
+		//}
+		//if i == 0 {
+		//	h1 = h
+		//}
+		//manifestMap, err := h.RenderCharts()
+		//if err != nil {
+		//	t.Fatalf("failed to render manifest: %v", err)
+		//}
+		//applyResourcesIntoCluster(t, h, manifestMap)
+	}
+	// delete one iop: iop-test-gw-1, get its pruned resources
+	componentName := string(name.IngressComponentName)
+	resources, err := GetPrunedResources(cl, "name", "ns", "rev", false)
+	assert.NoError(t, err)
+	assert.Equal(t, true, len(resources) > 0)
+	// check resources, only associated with iop-test-gw-1 istiooperator CR,
+	// otherwise, the resources of all IngressGateways components will be deleted.
+	// See https://github.com/istio/istio/issues/40577 for more details.
+	for _, uslist := range resources {
+		for _, u := range uslist.Items {
+			assert.Equal(t, "rev", u.GetLabels()[label.IoIstioRev.Name])
+			assert.Equal(t, componentName, u.GetLabels()[IstioComponentLabelStr])
+			assert.Equal(t, "name", u.GetLabels()[OwningResourceName])
+			assert.Equal(t, "ns", u.GetLabels()[OwningResourceNamespace])
 		}
 	}
-}
-
-func TestHelmReconciler_GetPrunedResources(t *testing.T) {
-	t.Run("get gateway pruned resources", func(t *testing.T) {
-		var h1 *HelmReconciler
-		cl := fake.NewClientBuilder().WithInterceptorFuncs(interceptorFunc).Build()
-		// init two custom gateways with revision
-		gateways := [][]byte{iopTestGwData1, iopTestGwData2}
-		for i, data := range gateways {
-			iop := &v1alpha1.IstioOperator{}
-			err := yaml.UnmarshalStrict(data, iop)
-			assert.NoError(t, err)
-			h := &HelmReconciler{
-				client:     cl,
-				kubeClient: kube.NewFakeClientWithVersion("24"),
-				opts: &Options{
-					ProgressLog: progress.NewLog(),
-					Log:         clog.NewDefaultLogger(),
-				},
-				iop: iop,
-			}
-			if i == 0 {
-				h1 = h
-			}
-			manifestMap, err := h.RenderCharts()
-			if err != nil {
-				t.Fatalf("failed to render manifest: %v", err)
-			}
-			applyResourcesIntoCluster(t, h, manifestMap)
-		}
-		// delete one iop: iop-test-gw-1, get its pruned resources
-		componentName := string(name.IngressComponentName)
-		resources, err := h1.GetPrunedResources(h1.iop.Spec.Revision, false, componentName)
-		assert.NoError(t, err)
-		assert.Equal(t, true, len(resources) > 0)
-		// check resources, only associated with iop-test-gw-1 istiooperator CR,
-		// otherwise, the resources of all IngressGateways components will be deleted.
-		// See https://github.com/istio/istio/issues/40577 for more details.
-		for _, uslist := range resources {
-			for _, u := range uslist.Items {
-				assert.Equal(t, h1.iop.Spec.Revision, u.GetLabels()[label.IoIstioRev.Name])
-				assert.Equal(t, componentName, u.GetLabels()[IstioComponentLabelStr])
-				assert.Equal(t, h1.iop.GetName(), u.GetLabels()[OwningResourceName])
-				assert.Equal(t, h1.iop.GetNamespace(), u.GetLabels()[OwningResourceNamespace])
-			}
-		}
-	})
 }
