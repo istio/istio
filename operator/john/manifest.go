dd0dfe699a446f7735dcc504e0850c3c16a130a7
John Howard <john.howard@solo.io>
2024-08-05 10:54:27 -0700

    all pilot patches working

--- operator/john/manifest.go
+++ operator/john/manifest.go
@@ -7,16 +7,10 @@ import (
 	"os"
 	"strings"
 
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/util/strategicpatch"
-	"k8s.io/client-go/kubernetes/scheme"
-	"sigs.k8s.io/yaml"
-
 	"istio.io/istio/manifests"
 	"istio.io/istio/operator/pkg/apis/istio/v1alpha1"
 	"istio.io/istio/operator/pkg/validate"
 	"istio.io/istio/pkg/kube"
-	"istio.io/istio/pkg/log"
 	pkgversion "istio.io/istio/pkg/version"
 )
 
@@ -63,106 +57,6 @@ func GenerateManifest(files []string, setFlags []string, force bool, filter []st
 	// TODO: set components based on profile
 	// TODO: ValuesEnablementPathMap? This enables the ingress or egress
 	return allManifests, nil
-}
-
-func postProcess(comp Component, spec ComponentSpec, manifests []Manifest) ([]Manifest, error) {
-	if spec.Kubernetes == nil {
-		return manifests, nil
-	}
-	type Patch struct {
-		Kind, Name string
-		Patch      string
-	}
-	rn := comp.ResourceName
-	rt := comp.ResourceType
-	patches := map[string]Patch{
-		"affinity":            {Kind: rt, Name: rn, Patch: `{"spec":{"template":{"spec":{"affinity":%s}}}}`},
-		"env":                 {Kind: rt, Name: rn, Patch: fmt.Sprintf(`{"spec":{"template":{"spec":{"containers":[{"name":%q, "env": %%s}]}}}}`, comp.ContainerName)},
-		"hpaSpec":             {Kind: "HorizontalPodAutoscaler", Name: rn, Patch: `{"spec":%s}`},
-		"imagePullPolicy":     {Kind: rt, Name: rn, Patch: fmt.Sprintf(`{"spec":{"template":{"spec":{"containers":[{"name":%q, "imagePullPolicy": %%s}]}}}}`, comp.ContainerName)},
-		"nodeSelector":        {Kind: rt, Name: rn, Patch: `{"spec":{"template":{"spec":{"nodeSelector":%s}}}}`},
-		"podDisruptionBudget": {Kind: "PodDisruptionBudget", Name: rn, Patch: `{"spec":%s}`},
-		"podAnnotations":      {Kind: rt, Name: rn, Patch: `{"spec":{"template":{"metadata":{"annotations":%s}}}}`},
-		"priorityClassName":   {Kind: rt, Name: rn, Patch: `{"spec":{"template":{"spec":{"priorityClassName":%s}}}}`},
-		"readinessProbe":      {Kind: rt, Name: rn, Patch: fmt.Sprintf(`{"spec":{"template":{"spec":{"containers":[{"name":%q, "readinessProbe": %%s}]}}}}`, comp.ContainerName)},
-		"replicaCount":        {Kind: rt, Name: rn, Patch: `{"spec":{"replicas":%s}}`},
-		"resources":           {Kind: rt, Name: rn, Patch: fmt.Sprintf(`{"spec":{"template":{"spec":{"containers":[{"name":%q, "resources": %%s}]}}}}`, comp.ContainerName)},
-		"strategy":            {Kind: rt, Name: rn, Patch: `{"spec":{"strategy":%s}}`},
-		"tolerations":         {Kind: rt, Name: rn, Patch: `{"spec":{"template":{"spec":{"tolerations":%s}}}}`},
-		"serviceAnnotations":  {Kind: "Service", Name: rn, Patch: `{"metadata":{"annotations":%s}}`},
-		"service":             {Kind: "Service", Name: rn, Patch: `{"spec"::%s}`},
-		"securityContext":     {Kind: rt, Name: rn, Patch: `{"spec":{"template":{"spec":{"securityContext":%s}}}}`},
-	}
-	needPatching := map[int][]string{}
-	for field, k := range patches {
-		v, ok := spec.Raw.GetPath("k8s." + field)
-		if !ok {
-			log.Errorf("howardjohn: %v", field)
-			continue
-		}
-		inner, err := json.Marshal(v)
-		if err != nil {
-			return nil, err
-		}
-		patch := fmt.Sprintf(k.Patch, inner)
-		// Find which manifests need the patch
-		for idx, m := range manifests {
-			if k.Kind == m.GetKind() && k.Name == m.GetName() {
-				needPatching[idx] = append(needPatching[idx], patch)
-			}
-		}
-	}
-
-
-	for _, o := range spec.Kubernetes.Overlays {
-		for idx, m := range manifests {
-			if o.Kind != m.GetKind() {
-				continue
-			}
-			// While patches have ApiVersion, this is ignored for legacy compatibility
-			if o.Name != m.GetName() {
-				continue
-			}
-			for _,p := range o.Patches {
-				patch := MakePatch(p.Value, p.Path)
-				needPatching[idx] = append(needPatching[idx], patch)
-			}
-		}
-	}
-
-	for idx, patches := range needPatching {
-		m := manifests[idx]
-		baseJSON, err := yaml.YAMLToJSON([]byte(m.Content))
-		if err != nil {
-			return nil, err
-		}
-		typed, err := scheme.Scheme.New(m.GroupVersionKind())
-		if err != nil {
-			return nil, err
-		}
-
-		for _, patch := range patches {
-			newBytes, err := strategicpatch.StrategicMergePatch(baseJSON, []byte(patch), typed)
-			if err != nil {
-				return nil, err
-			}
-			baseJSON = newBytes
-		}
-		us := &unstructured.Unstructured{}
-		if err := json.Unmarshal(baseJSON, us); err != nil {
-			return nil, err
-		}
-		yml, err := yaml.Marshal(us)
-		if err != nil {
-			return nil, err
-		}
-		// Rebuild our manifest
-		manifests[idx] = Manifest{
-			Unstructured: us,
-			Content:      string(yml),
-		}
-	}
-	return manifests, nil
 }
 
 func hubTagOverlay() []string {
