4761bb3919a9adb79432c333ad082a6d4d9b062f
John Howard <john.howard@solo.io>
2024-08-27 10:25:01 -0700

    Fix new test setup

--- tools/istio-iptables/pkg/capture/run_test.go
+++ tools/istio-iptables/pkg/capture/run_test.go
@@ -17,16 +17,20 @@ package capture
 import (
 	"bytes"
 	"net/netip"
+	"os"
 	"os/exec"
 	"path/filepath"
 	"reflect"
 	"strings"
+	"sync"
 	"testing"
 
+	// Create a new mount namespace.
+	"github.com/howardjohn/unshare-go/mountns"
 	// Create a new network namespace. This will have the 'lo' interface ready but nothing else.
 	_ "github.com/howardjohn/unshare-go/netns"
 	// Create a new user namespace. This will map the current UID/GID to 0.
-	_ "github.com/howardjohn/unshare-go/userns"
+	"github.com/howardjohn/unshare-go/userns"
 
 	testutil "istio.io/istio/pilot/test/util"
 	"istio.io/istio/pkg/test/util/assert"
@@ -352,6 +356,7 @@ func TestSeparateV4V6(t *testing.T) {
 }
 
 func TestIdempotentEquivalentRerun(t *testing.T) {
+	setup(t)
 	commonCases := getCommonTestCases()
 	ext := &dep.RealDependencies{
 		HostFilesystemPodNetwork: false,
@@ -359,7 +364,7 @@ func TestIdempotentEquivalentRerun(t *testing.T) {
 	}
 	iptVer, err := ext.DetectIptablesVersion(false)
 	if err != nil {
-		t.Fatalf("Can't detect iptables version")
+		t.Fatalf("Can't detect iptables version: %v", err)
 	}
 
 	ipt6Ver, err := ext.DetectIptablesVersion(true)
@@ -407,7 +412,25 @@ func TestIdempotentEquivalentRerun(t *testing.T) {
 	}
 }
 
+var initialized = &sync.Once{}
+
+func setup(t *testing.T) {
+	initialized.Do(func() {
+		// Setup group namespace so iptables --gid-owner will work
+		assert.NoError(t, userns.WriteGroupMap(map[uint32]uint32{userns.OriginalGID(): 0}))
+		// Istio iptables expects to find a non-localhost IP in some interface
+		assert.NoError(t, exec.Command("ip", "addr", "add", "240.240.240.240/32", "dev", "lo").Run())
+		// Put a new file we have permission to access over xtables.lock
+		xtables := filepath.Join(t.TempDir(), "xtables.lock")
+		_, err := os.Create(xtables)
+		assert.NoError(t, err)
+		_ = os.Mkdir("/run", 0o777)
+		_ = mountns.BindMount(xtables, "/run/xtables.lock")
+	})
+}
+
 func TestIdempotentUnequaledRerun(t *testing.T) {
+	setup(t)
 	commonCases := getCommonTestCases()
 	ext := &dep.RealDependencies{
 		HostFilesystemPodNetwork: false,
