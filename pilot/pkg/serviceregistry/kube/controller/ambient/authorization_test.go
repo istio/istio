3bef708c68bc98d2a284fa5984e67c124be68e11
Ian Rudie <ian.rudie@solo.io>
2024-09-09 15:57:38 -0400

    license for new testing file, break up long line
    Signed-off-by: Ian Rudie <ian.rudie@solo.io>

--- pilot/pkg/serviceregistry/kube/controller/ambient/authorization_test.go
+++ pilot/pkg/serviceregistry/kube/controller/ambient/authorization_test.go
@@ -1,3 +1,17 @@
+// Copyright Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package ambient
 
 import (
@@ -166,8 +180,10 @@ func TestConvertAuthorizationPolicyStatus(t *testing.T) {
 				},
 			},
 			expectStatusMessage: &model.StatusMessage{
-				Reason:  "UnsupportedValue",
-				Message: "ztunnel does not support HTTP rules (methods, request.auth.presenter, requestPrincipals require HTTP parsing), in ambient mode you must use waypoint proxy to enforce HTTP rules. Allow rules with HTTP attributes will be empty and never match. This is more restrictive than requested.",
+				Reason: "UnsupportedValue",
+				Message: "ztunnel does not support HTTP rules (methods, request.auth.presenter, requestPrincipals require HTTP parsing), in ambient" +
+					" mode you must use waypoint proxy to enforce HTTP rules. Allow rules with HTTP attributes will be empty and never match." +
+					" This is more restrictive than requested.",
 			},
 		},
 	}
