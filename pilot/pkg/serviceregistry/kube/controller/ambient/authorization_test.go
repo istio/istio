26b2ca6a73b8bb1170fe86e4f96d922fb9c1a3b4
Ian Rudie <ian.rudie@solo.io>
2024-09-09 15:30:25 -0400

    add unit testing for authzpolicy status
    Signed-off-by: Ian Rudie <ian.rudie@solo.io>

---
+++ pilot/pkg/serviceregistry/kube/controller/ambient/authorization_test.go
@@ -0,0 +1,181 @@
+package ambient
+
+import (
+	"testing"
+
+	"istio.io/api/security/v1beta1"
+	apiv1beta1 "istio.io/api/type/v1beta1"
+	securityclient "istio.io/client-go/pkg/apis/security/v1"
+	"istio.io/istio/pilot/pkg/model"
+	"istio.io/istio/pkg/test/util/assert"
+)
+
+const (
+	rootns string = "test-system"
+)
+
+func TestConvertAuthorizationPolicyStatus(t *testing.T) {
+	testCases := []struct {
+		name                string
+		inputAuthzPol       *securityclient.AuthorizationPolicy
+		expectStatusMessage *model.StatusMessage
+	}{
+		{
+			name: "no status - targetRef is not for ztunnel",
+			inputAuthzPol: &securityclient.AuthorizationPolicy{
+				Spec: v1beta1.AuthorizationPolicy{
+					TargetRef: &apiv1beta1.PolicyTargetReference{
+						Kind: "Service",
+						Name: "test-svc",
+					},
+					Rules: []*v1beta1.Rule{},
+				},
+			},
+			expectStatusMessage: nil,
+		},
+		{
+			name: "no status - policy is fine",
+			inputAuthzPol: &securityclient.AuthorizationPolicy{
+				Spec: v1beta1.AuthorizationPolicy{
+					Rules: []*v1beta1.Rule{
+						{
+							From: []*v1beta1.Rule_From{
+								{
+									Source: &v1beta1.Source{
+										Principals: []string{
+											"trust/ns/echo-1234/",
+										},
+									},
+								},
+							},
+							To: []*v1beta1.Rule_To{
+								{
+									Operation: &v1beta1.Operation{
+										Ports: []string{
+											"http",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			expectStatusMessage: nil,
+		},
+		{
+			name: "unsupported AUDIT action",
+			inputAuthzPol: &securityclient.AuthorizationPolicy{
+				Spec: v1beta1.AuthorizationPolicy{
+					Action: v1beta1.AuthorizationPolicy_AUDIT,
+					Rules: []*v1beta1.Rule{
+						{
+							From: []*v1beta1.Rule_From{
+								{
+									Source: &v1beta1.Source{
+										Principals: []string{
+											"trust/ns/echo-1234/",
+										},
+									},
+								},
+							},
+							To: []*v1beta1.Rule_To{
+								{
+									Operation: &v1beta1.Operation{
+										Ports: []string{
+											"http",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			expectStatusMessage: &model.StatusMessage{
+				Reason:  "UnsupportedValue",
+				Message: "ztunnel does not support the AUDIT action",
+			},
+		},
+		{
+			name: "unsupported CUSTOM action",
+			inputAuthzPol: &securityclient.AuthorizationPolicy{
+				Spec: v1beta1.AuthorizationPolicy{
+					Action: v1beta1.AuthorizationPolicy_CUSTOM,
+					Rules: []*v1beta1.Rule{
+						{
+							From: []*v1beta1.Rule_From{
+								{
+									Source: &v1beta1.Source{
+										Principals: []string{
+											"trust/ns/echo-1234/",
+										},
+									},
+								},
+							},
+							To: []*v1beta1.Rule_To{
+								{
+									Operation: &v1beta1.Operation{
+										Ports: []string{
+											"http",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			expectStatusMessage: &model.StatusMessage{
+				Reason:  "UnsupportedValue",
+				Message: "ztunnel does not support the CUSTOM action",
+			},
+		},
+		{
+			name: "unsupported HTTP methods",
+			inputAuthzPol: &securityclient.AuthorizationPolicy{
+				Spec: v1beta1.AuthorizationPolicy{
+					Rules: []*v1beta1.Rule{
+						{
+							From: []*v1beta1.Rule_From{
+								{
+									Source: &v1beta1.Source{
+										RequestPrincipals: []string{
+											"example.com/sub-1",
+										},
+									},
+								},
+							},
+							To: []*v1beta1.Rule_To{
+								{
+									Operation: &v1beta1.Operation{
+										Methods: []string{
+											"GET",
+										},
+									},
+								},
+							},
+							When: []*v1beta1.Condition{
+								{
+									Key:    "request.auth.presenter",
+									Values: []string{"presenter.example.com"},
+								},
+							},
+						},
+					},
+				},
+			},
+			expectStatusMessage: &model.StatusMessage{
+				Reason:  "UnsupportedValue",
+				Message: "ztunnel does not support HTTP rules (methods, request.auth.presenter, requestPrincipals require HTTP parsing), in ambient mode you must use waypoint proxy to enforce HTTP rules. Allow rules with HTTP attributes will be empty and never match. This is more restrictive than requested.",
+			},
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			_, outStatusMessage := convertAuthorizationPolicy(rootns, test.inputAuthzPol)
+			assert.Equal(t, test.expectStatusMessage, outStatusMessage)
+		})
+	}
+}
