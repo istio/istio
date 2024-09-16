7486418e8fd046152fb2053e3bc41d2527b95a3d
Jackie Elliott <jaellio@microsoft.com>
2024-09-16 15:26:26 +0000

    Explicitly set internal addresses in Http Connection Manager when PILOT_SIDECAR_USE_REMOTE_ADDRESS is set to true for the sidecar.
    Signed-off-by: Jackie Elliott <jaellio@microsoft.com>

--- pilot/pkg/networking/core/listener_builder.go
+++ pilot/pkg/networking/core/listener_builder.go
@@ -421,7 +421,10 @@ func (lb *ListenerBuilder) buildHTTPConnectionManager(httpOpts *httpListenerOpts
 	connectionManager.HttpFilters = filters
 	connectionManager.RequestIdExtension = requestidextension.BuildUUIDRequestIDExtension(reqIDExtensionCtx)
 
-	if features.EnableHCMInternalNetworks && lb.push.Networks != nil {
+	// If UseRemoteAddress is set, we must set the internal address config in preparation for envoy
+	// internal addresses defaulting to empty set. Currently, the internal addresses defaulted to
+	// all private IPs but this will change in the future.
+	if (features.EnableHCMInternalNetworks || features.UseRemoteAddress) && lb.push.Networks != nil {
 		for _, internalnetwork := range lb.push.Networks.Networks {
 			iac := &hcm.HttpConnectionManager_InternalAddressConfig{}
 			for _, ne := range internalnetwork.Endpoints {
