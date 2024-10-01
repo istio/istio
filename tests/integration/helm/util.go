istio/istio

commit f4c37d06dcd3653fb1918f8974ff0a283c1fced3

Default to always templating CRDs in the `base` chart. (#53318)
* Switch to default-templating CRDs (i.e. Helm no longer treats them specially)

Signed-off-by: Benjamin Leggett <benjamin.leggett@solo.io>

* Make this a bit smarter

Signed-off-by: Benjamin Leggett <benjamin.leggett@solo.io>

* Relnote

Signed-off-by: Benjamin Leggett <benjamin.leggett@solo.io>

* Don't need this anymore

Signed-off-by: Benjamin Leggett <benjamin.leggett@solo.io>

* use upgradeNote

Signed-off-by: Benjamin Leggett <benjamin.leggett@solo.io>

* Revert "Don't need this anymore"

This reverts commit f476f1f02858d46fd78f7de5870df86fc74415e5.

* Fix tests properly and also add better upgradeNote

Signed-off-by: Benjamin Leggett <benjamin.leggett@solo.io>

* reformat relnote

Signed-off-by: Benjamin Leggett <benjamin.leggett@solo.io>

* Lint

Signed-off-by: Benjamin Leggett <benjamin.leggett@solo.io>

* geez

Signed-off-by: Benjamin Leggett <benjamin.leggett@solo.io>

* Missed some tests.

Signed-off-by: Benjamin Leggett <benjamin.leggett@solo.io>

---------

Signed-off-by: Benjamin Leggett <benjamin.leggett@solo.io>

releasenotes/notes/53318.yaml
@@ -1,0 +1,44 @@
+apiVersion: release-notes/v2
+kind: feature
+area: installation
+issue:
+  - 43204
+releaseNotes:
+- |
+  **Fixed** templated CRD installation in the `base` chart by default. Previously this only worked under certain conditions,
+  and when certain install flags were used, could result in CRDs that could only be upgraded via manual `kubectl` intervention.
+  **Deprecated** `Values.base.enableCRDTemplates`. This option now defaults to `true` and will be removed
+  in a future release. Until then, the legacy behavior can be enabled by setting this to `false`
+upgradeNotes:
+- title: Istio CRDs are templated by default and can be installed and upgraded via `helm install istio-base`
+  content: |
+    This changes how CRDs are upgraded.
+    Previously, we recommended and documented:
+
+    - Install: `helm install istio-base`
+    - Upgrade: `kubectl apply -f manifests/charts/base/files/crd-all.gen.yaml` or similar.
+    - Uninstall: `kubectl get crd -oname | grep --color=never 'istio.io' | xargs kubectl delete`
+
+    This change allows:
+
+    - Install: `helm install istio-base`
+    - Upgrade: `helm upgrade istio-base`
+    - Uninstall: `kubectl get crd -oname | grep --color=never 'istio.io' | xargs kubectl delete`
+
+    Previously this only worked under certain conditions, and when certain install flags were used,
+    could result in non-Helm-upgradable CRDs being generated that required manual intervention to fix.
+
+    As a necessary consequence of this, the labels on the CRDs are changed to be consistent with other Helm-installed resources.
+
+    If you previously installed or upgraded CRDs with `kubectl apply` and not Helm, you can continue to do so.
+
+    If you previously installed CRDs with `helm install istio-base` OR `kubectl apply`, you can begin safely upgrading Istio CRDs
+    with `helm upgrade istio-base` from this and all subsequent releases
+    after running the below kubectl commands as a one-time migration:
+
+      - `kubectl label crds -l chart=istio "app.kubernetes.io/managed-by=Helm"`
+      - `kubectl annotate crds -l chart=istio "meta.helm.sh/release-name=istio-base"`
+      - `kubectl annotate crds -l chart=istio "meta.helm.sh/release-namespace=istio-system"` (replace with actual istio namespace)
+
+    If desired, the legacy non-Helm-upgradable CRDs and labels can be generated by setting `Values.base.enableCRDTemplates=false`
+    during `helm install base`, but this option will be removed in a future release.

tests/integration/helm/upgrade/util.go
@@ -45,20 +45,34 @@ const (
 	latestRevisionTag = "latest"
 )
 
+// TODO BML this relabeling/reannotating is only required if the previous release is =< 1.23,
+// and should be dropped once 1.24 is released.
+func adoptPre123CRDResourcesIfNeeded() {
+	requiredAdoptionLabels := []string{"app.kubernetes.io/managed-by=Helm"}
+	requiredAdoptionAnnos := []string{"meta.helm.sh/release-name=istio-base", "meta.helm.sh/release-namespace=istio-system"}
+
+	for _, labelToAdd := range requiredAdoptionLabels {
+		execCmd := fmt.Sprintf("kubectl label crds -l chart=istio %v", labelToAdd)
+		_, err := shell.Execute(false, execCmd)
+		if err != nil {
+			scopes.Framework.Infof("couldn't relabel CRDs for Helm adoption: %s. Likely not needed for this release", labelToAdd)
+		}
+	}
+
+	for _, annoToAdd := range requiredAdoptionAnnos {
+		execCmd := fmt.Sprintf("kubectl annotate crds -l chart=istio %v", annoToAdd)
+		_, err := shell.Execute(false, execCmd)
+		if err != nil {
+			scopes.Framework.Infof("couldn't reannotate CRDs for Helm adoption: %s. Likely not needed for this release", annoToAdd)
+		}
+	}
+}
+
 // upgradeCharts upgrades Istio using Helm charts with the provided
 // override values file to the latest charts in $ISTIO_SRC/manifests
 func upgradeCharts(ctx framework.TestContext, h *helm.Helm, overrideValuesFile string, nsConfig helmtest.NamespaceConfig, isAmbient bool) {
-	execCmd := fmt.Sprintf(
-		"kubectl apply -n %v -f %v",
-		helmtest.IstioNamespace,
-		filepath.Join(helmtest.ManifestsChartPath, helmtest.BaseChart, helmtest.CRDsFolder))
-	_, err := shell.Execute(false, execCmd)
-	if err != nil {
-		ctx.Fatalf("couldn't run kubectl apply on crds folder: %v", err)
-	}
-
 	// Upgrade base chart
-	err = h.UpgradeChart(helmtest.BaseReleaseName, filepath.Join(helmtest.ManifestsChartPath, helmtest.BaseChart),
+	err := h.UpgradeChart(helmtest.BaseReleaseName, filepath.Join(helmtest.ManifestsChartPath, helmtest.BaseChart),
 		nsConfig.Get(helmtest.BaseReleaseName), overrideValuesFile, helmtest.Timeout, "--skip-crds")
 	if err != nil {
 		ctx.Fatalf("failed to upgrade istio %s chart", helmtest.BaseReleaseName)
@@ -164,6 +178,9 @@ func performInPlaceUpgradeFunc(previousVersion string, isAmbient bool) func(fram
 		sanitycheck.RunTrafficTestClientServer(t, oldClient, oldServer)
 
 		overrideValuesFile = helmtest.GetValuesOverrides(t, s.Image.Hub, s.Image.Tag, s.Image.Variant, "", isAmbient)
+
+		adoptPre123CRDResourcesIfNeeded()
+
 		upgradeCharts(t, h, overrideValuesFile, nsConfig, isAmbient)
 		helmtest.VerifyInstallation(t, cs, nsConfig, true, isAmbient, "")
 
@@ -201,6 +218,9 @@ func performCanaryUpgradeFunc(nsConfig helmtest.NamespaceConfig, previousVersion
 		sanitycheck.RunTrafficTestClientServer(t, oldClient, oldServer)
 
 		overrideValuesFile = helmtest.GetValuesOverrides(t, s.Image.Hub, s.Image.Tag, s.Image.Variant, canaryTag, false)
+
+		adoptPre123CRDResourcesIfNeeded()
+
 		helmtest.InstallIstioWithRevision(t, cs, h, "", canaryTag, overrideValuesFile, true, false)
 		helmtest.VerifyInstallation(t, cs, helmtest.DefaultNamespaceConfig, false, false, "")
 
@@ -263,6 +283,9 @@ func performRevisionTagsUpgradeFunc(previousVersion string) func(framework.TestC
 		// helm upgrade istio-base ../manifests/charts/base --namespace istio-system -f values.yaml
 		// helm install istiod-latest ../manifests/charts/istio-control/istio-discovery -f values.yaml
 		overrideValuesFile = helmtest.GetValuesOverrides(t, s.Image.Hub, s.Image.Tag, s.Image.Variant, latestRevisionTag, false)
+
+		adoptPre123CRDResourcesIfNeeded()
+
 		helmtest.InstallIstioWithRevision(t, cs, h, "", latestRevisionTag, overrideValuesFile, true, false)
 		helmtest.VerifyInstallation(t, cs, helmtest.DefaultNamespaceConfig, false, false, "")
 

Makefile.core.mk
@@ -341,7 +341,7 @@ copy-templates:
 	cp manifests/charts/istio-control/istio-discovery/templates/configmap.yaml manifests/charts/istiod-remote/templates
 	cp manifests/charts/istio-control/istio-discovery/templates/_helpers.tpl manifests/charts/istiod-remote/templates
 	cp manifests/charts/istio-control/istio-discovery/templates/zzy_descope_legacy.yaml manifests/charts/istiod-remote/templates
-	sed -e '1 i {{- if .Values.global.configCluster }}' -e '$$ a {{- end }}' manifests/charts/base/crds/crd-all.gen.yaml > manifests/charts/istiod-remote/templates/crd-all.gen.yaml
+	sed -e '1 i {{- if .Values.global.configCluster }}' -e '$$ a {{- end }}' manifests/charts/base/files/crd-all.gen.yaml > manifests/charts/istiod-remote/templates/crd-all.gen.yaml
 	sed -e '1 i {{- if .Values.global.configCluster }}' -e '$$ a {{- end }}' manifests/charts/base/templates/defaultrevision-validatingadmissionpolicy.yaml > manifests/charts/istiod-remote/templates/defaultrevision-validatingadmissionpolicy.yaml
 	sed -e '1 i {{- if .Values.global.configCluster }}' -e '$$ a {{- end }}' manifests/charts/istio-control/istio-discovery/templates/validatingadmissionpolicy.yaml > manifests/charts/istiod-remote/templates/validatingadmissionpolicy.yaml
 	sed -e '1 i {{- if .Values.global.configCluster }}' -e '$$ a {{- end }}' manifests/charts/base/templates/defaultrevision-validatingwebhookconfiguration.yaml > manifests/charts/istiod-remote/templates/defaultrevision-validatingwebhookconfiguration.yaml

bin/update_crds.sh
@@ -53,8 +53,8 @@ if [ ! -f "${API_TMP}/kubernetes/customresourcedefinitions.gen.yaml" ]; then
   echo "Generated Custom Resource Definitions file does not exist in the commit SHA ${SHA}. Not updating the CRD file."
   exit
 fi
-rm -f "${ROOTDIR}/manifests/charts/base/crds/crd-all.gen.yaml"
-cp "${API_TMP}/kubernetes/customresourcedefinitions.gen.yaml" "${ROOTDIR}/manifests/charts/base/crds/crd-all.gen.yaml"
+rm -f "${ROOTDIR}/manifests/charts/base/files/crd-all.gen.yaml"
+cp "${API_TMP}/kubernetes/customresourcedefinitions.gen.yaml" "${ROOTDIR}/manifests/charts/base/files/crd-all.gen.yaml"
 cp "${API_TMP}"/tests/testdata/* "${ROOTDIR}/pkg/config/validation/testdata/crds"
 
 cd "${ROOTDIR}"

manifests/charts/base/templates/crds.yaml
@@ -1,13 +1,21 @@
+# TODO enableCRDTemplates is now defaulted to true as we
+# want to always self-manage CRD upgrades via plain templates,
+# so we should remove this flag after a few releases
 {{- if .Values.base.enableCRDTemplates }}
-{{- if .Values.base.excludedCRDs }}
-{{- range $crd := .Files.Get "crds/crd-all.gen.yaml"|splitList "\n---\n"}}
+{{- $replacement := include "istio.labels" . | fromYaml}}
+{{- range $crd := .Files.Get "files/crd-all.gen.yaml"|splitList "\n---\n"}}
 {{- $name := (index ($crd |fromYaml) "metadata" "name") }}
 {{- if not (has $name $.Values.base.excludedCRDs)}}
-{{$crd}}
+{{- $asDict := ($crd | fromYaml) }}
+# If we are templating these CRDs, we want to wipe out the "static"/legacy
+# labels and replace them with the standard templated istio ones.
+# This allows the continued use of `kubectl apply -f crd-all.gen.yaml`
+# without any templating+the old labels, if desired.
+{{- $_ := set $asDict.metadata "labels" $replacement }}
+{{$asDict | toYaml }}
 ---
 {{- end }}
 {{- end }}
 {{- else }}
-{{ .Files.Get "crds/crd-all.gen.yaml" }}
-{{- end }}
+{{ .Files.Get "files/crd-all.gen.yaml" }}
 {{- end }}

manifests/charts/base/values.yaml
@@ -20,8 +20,13 @@ _internal_defaults_do_not_set:
     # Example: `excludedCRDs: ["envoyfilters.networking.istio.io"]`.
     # Note: when installing with `istioctl`, `enableIstioConfigCRDs=false` must also be set.
     excludedCRDs: []
-    # Used for helm2 to add the CRDs to templates.
-    enableCRDTemplates: false
+    # Helm (as of V3) does not support upgrading CRDs, because it is not universally
+    # safe for them to support this.
+    # Istio as a project enforces certain backwards-compat guarantees that allow us
+    # to safely upgrade CRDs in spite of this, so we default to self-managing CRDs
+    # as standard K8S resources in Helm, and disable Helm's CRD management. See also:
+    # https://helm.sh/docs/chart_best_practices/custom_resource_definitions/#method-2-separate-charts
+    enableCRDTemplates: true
 
     # Validation webhook configuration url
     # For example: https://$remotePilotAddress:15017/validate

manifests/charts/ztunnel/templates/_helpers.tpl
@@ -1,10 +1,0 @@
-{{- define "istio-labels" }}
-    app.kubernetes.io/name: ztunnel
-    {{- if .Release.Service }}
-    app.kubernetes.io/managed-by: {{ .Release.Service }}
-    {{- end }}
-    {{- if .Release.Name}}
-    app.kubernetes.io/instance: {{ .Release.Name }}
-    {{- end }}
-    app.kubernetes.io/part-of: istio
-{{- end }}

commit c61d58cdefb0d350084742561fc56b09c12fe376

Fix postsubmit fails caused by https://github.com/istio/istio/pull/53318
Signed-off-by: Benjamin Leggett <benjamin.leggett@solo.io>

releasenotes/notes/53318.yaml
@@ -36,9 +36,9 @@ upgradeNotes:
     with `helm upgrade istio-base` from this and all subsequent releases
     after running the below kubectl commands as a one-time migration:
 
-      - `kubectl label crds -l chart=istio "app.kubernetes.io/managed-by=Helm"`
-      - `kubectl annotate crds -l chart=istio "meta.helm.sh/release-name=istio-base"`
-      - `kubectl annotate crds -l chart=istio "meta.helm.sh/release-namespace=istio-system"` (replace with actual istio namespace)
+      - `kubectl label crds -l chart=istio -l app.kubernetes.io/part-of=istio "app.kubernetes.io/managed-by=Helm"`
+      - `kubectl annotate crds -l chart=istio -l app.kubernetes.io/part-of=istio "meta.helm.sh/release-name=istio-base"` (replace with actual `istio-base` Helm releasename)
+      - `kubectl annotate crds -l chart=istio -l app.kubernetes.io/part-of=istio "meta.helm.sh/release-namespace=istio-system"` (replace with actual istio namespace)
 
     If desired, the legacy non-Helm-upgradable CRDs and labels can be generated by setting `Values.base.enableCRDTemplates=false`
     during `helm install base`, but this option will be removed in a future release.

tests/integration/helm/main_test.go
@@ -21,6 +21,7 @@ import (
 	"testing"
 
 	"istio.io/istio/pkg/test/framework"
+	"istio.io/istio/pkg/test/framework/resource"
 )
 
 func TestMain(m *testing.M) {
@@ -28,5 +29,15 @@ func TestMain(m *testing.M) {
 	framework.
 		NewSuite(m).
 		RequireSingleCluster().
+		Setup(adoptCRDsFromPreviousTests).
 		Run()
+}
+
+// TODO BML this relabeling/reannotating is only required if the previous release is =< 1.23,
+// and should be dropped once 1.24 is released.
+//
+// Needed as other tests do not clean up CRDs and in this suite that can create issues
+func adoptCRDsFromPreviousTests(ctx resource.Context) error {
+	adoptPre123CRDResourcesIfNeeded()
+    return nil
 }

tests/integration/helm/upgrade/util.go
@@ -32,7 +32,6 @@ import (
 	"istio.io/istio/pkg/test/helm"
 	kubetest "istio.io/istio/pkg/test/kube"
 	"istio.io/istio/pkg/test/scopes"
-	"istio.io/istio/pkg/test/shell"
 	"istio.io/istio/pkg/test/util/retry"
 	helmtest "istio.io/istio/tests/integration/helm"
 	"istio.io/istio/tests/util/sanitycheck"
@@ -44,29 +43,6 @@ const (
 	canaryTag         = "canary"
 	latestRevisionTag = "latest"
 )
-
-// TODO BML this relabeling/reannotating is only required if the previous release is =< 1.23,
-// and should be dropped once 1.24 is released.
-func adoptPre123CRDResourcesIfNeeded() {
-	requiredAdoptionLabels := []string{"app.kubernetes.io/managed-by=Helm"}
-	requiredAdoptionAnnos := []string{"meta.helm.sh/release-name=istio-base", "meta.helm.sh/release-namespace=istio-system"}
-
-	for _, labelToAdd := range requiredAdoptionLabels {
-		execCmd := fmt.Sprintf("kubectl label crds -l chart=istio %v", labelToAdd)
-		_, err := shell.Execute(false, execCmd)
-		if err != nil {
-			scopes.Framework.Infof("couldn't relabel CRDs for Helm adoption: %s. Likely not needed for this release", labelToAdd)
-		}
-	}
-
-	for _, annoToAdd := range requiredAdoptionAnnos {
-		execCmd := fmt.Sprintf("kubectl annotate crds -l chart=istio %v", annoToAdd)
-		_, err := shell.Execute(false, execCmd)
-		if err != nil {
-			scopes.Framework.Infof("couldn't reannotate CRDs for Helm adoption: %s. Likely not needed for this release", annoToAdd)
-		}
-	}
-}
 
 // upgradeCharts upgrades Istio using Helm charts with the provided
 // override values file to the latest charts in $ISTIO_SRC/manifests
@@ -179,7 +155,7 @@ func performInPlaceUpgradeFunc(previousVersion string, isAmbient bool) func(fram
 
 		overrideValuesFile = helmtest.GetValuesOverrides(t, s.Image.Hub, s.Image.Tag, s.Image.Variant, "", isAmbient)
 
-		adoptPre123CRDResourcesIfNeeded()
+		helmtest.adoptPre123CRDResourcesIfNeeded()
 
 		upgradeCharts(t, h, overrideValuesFile, nsConfig, isAmbient)
 		helmtest.VerifyInstallation(t, cs, nsConfig, true, isAmbient, "")
@@ -219,7 +195,7 @@ func performCanaryUpgradeFunc(nsConfig helmtest.NamespaceConfig, previousVersion
 
 		overrideValuesFile = helmtest.GetValuesOverrides(t, s.Image.Hub, s.Image.Tag, s.Image.Variant, canaryTag, false)
 
-		adoptPre123CRDResourcesIfNeeded()
+		helmtest.adoptPre123CRDResourcesIfNeeded()
 
 		helmtest.InstallIstioWithRevision(t, cs, h, "", canaryTag, overrideValuesFile, true, false)
 		helmtest.VerifyInstallation(t, cs, helmtest.DefaultNamespaceConfig, false, false, "")
@@ -284,7 +260,7 @@ func performRevisionTagsUpgradeFunc(previousVersion string) func(framework.TestC
 		// helm install istiod-latest ../manifests/charts/istio-control/istio-discovery -f values.yaml
 		overrideValuesFile = helmtest.GetValuesOverrides(t, s.Image.Hub, s.Image.Tag, s.Image.Variant, latestRevisionTag, false)
 
-		adoptPre123CRDResourcesIfNeeded()
+		helmtest.adoptPre123CRDResourcesIfNeeded()
 
 		helmtest.InstallIstioWithRevision(t, cs, h, "", latestRevisionTag, overrideValuesFile, true, false)
 		helmtest.VerifyInstallation(t, cs, helmtest.DefaultNamespaceConfig, false, false, "")

tests/integration/helm/util.go
@@ -40,6 +40,7 @@ import (
 	"istio.io/istio/pkg/test"
 	"istio.io/istio/pkg/test/env"
 	"istio.io/istio/pkg/test/framework"
+	"istio.io/istio/pkg/test/shell"
 	"istio.io/istio/pkg/test/framework/components/cluster"
 	"istio.io/istio/pkg/test/framework/components/cluster/kube"
 	"istio.io/istio/pkg/test/helm"
@@ -557,4 +558,27 @@ func verifyValidation(ctx framework.TestContext, revision string) {
 		rejected := err != nil
 		return rejected
 	})
+}
+
+// TODO BML this relabeling/reannotating is only required if the previous release is =< 1.23,
+// and should be dropped once 1.24 is released.
+func adoptPre123CRDResourcesIfNeeded() {
+	requiredAdoptionLabels := []string{"app.kubernetes.io/managed-by=Helm"}
+	requiredAdoptionAnnos := []string{"meta.helm.sh/release-name=istio-base", "meta.helm.sh/release-namespace=istio-system"}
+
+	for _, labelToAdd := range requiredAdoptionLabels {
+		execCmd := fmt.Sprintf("kubectl label crds -l chart=istio -l app.kubernetes.io/part-of=istio %v", labelToAdd)
+		_, err := shell.Execute(false, execCmd)
+		if err != nil {
+			scopes.Framework.Infof("couldn't relabel CRDs for Helm adoption: %s. Likely not needed for this release", labelToAdd)
+		}
+	}
+
+	for _, annoToAdd := range requiredAdoptionAnnos {
+		execCmd := fmt.Sprintf("kubectl annotate crds -l -l -l app.kubernetes.io/part-of=istio chart=istio %v", annoToAdd)
+		_, err := shell.Execute(false, execCmd)
+		if err != nil {
+			scopes.Framework.Infof("couldn't reannotate CRDs for Helm adoption: %s. Likely not needed for this release", annoToAdd)
+		}
+	}
 }