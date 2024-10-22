4e20964cf739f28be2c8e1377a9a9e81dbec652c
John Howard <howardjohn@google.com>
2024-10-21 17:35:56 -0700

    addons: bump versions, including Kiali 2.0 (#53592)
    Get this in for 1.24

--- manifests/addons/gen.sh
+++ manifests/addons/gen.sh
@@ -26,8 +26,8 @@ ADDONS="${WD}/../../samples/addons"
 DASHBOARDS="${WD}/dashboards"
 mkdir -p "${ADDONS}"
 TMP=$(mktemp -d)
-LOKI_VERSION=${LOKI_VERSION:-"6.7.1"}
-GRAFANA_VERSION=${GRAFANA_VERSION:-"8.3.5"}
+LOKI_VERSION=${LOKI_VERSION:-"6.18.0"}
+GRAFANA_VERSION=${GRAFANA_VERSION:-"8.5.8"}
 
 # Set up kiali
 {
@@ -44,7 +44,7 @@ helm3 template kiali-server \
 # Set up prometheus
 helm3 template prometheus prometheus \
   --namespace istio-system \
-  --version 25.24.1 \
+  --version 25.27.0 \
   --repo https://prometheus-community.github.io/helm-charts \
   -f "${WD}/values-prometheus.yaml" \
   > "${ADDONS}/prometheus.yaml"
