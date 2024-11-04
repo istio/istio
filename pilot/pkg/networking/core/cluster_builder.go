2f5185293ff3248bc4d486acc603c0a151bce2ba
Steven Jin Xuan <sjinxuan@microsoft.com>
2024-11-04 13:39:15 -0500

    Undo DNS jitter

--- pilot/pkg/networking/core/cluster_builder.go
+++ pilot/pkg/networking/core/cluster_builder.go
@@ -301,7 +301,6 @@ func (cb *ClusterBuilder) buildCluster(name string, discoveryType cluster.Cluste
 
 	switch discoveryType {
 	case cluster.Cluster_STRICT_DNS, cluster.Cluster_LOGICAL_DNS:
-		c.DnsJitter = durationpb.New(features.DNSJitterDurationEnv)
 		if networkutil.AllIPv4(cb.proxyIPAddresses) {
 			// IPv4 only
 			c.DnsLookupFamily = cluster.Cluster_V4_ONLY
