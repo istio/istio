d5b83283c0f32ec5ceec182496429597786be392
John Howard <john.howard@solo.io>
2024-11-25 14:26:00 -0800

    lint

--- pilot/pkg/serviceregistry/kube/controller/ambient/statusqueue/queue.go
+++ pilot/pkg/serviceregistry/kube/controller/ambient/statusqueue/queue.go
@@ -151,7 +151,7 @@ func Register[T StatusWriter](q *StatusQueue, name string, col krt.Collection[T]
 			log.Debugf("processing snapshot")
 			for _, obj := range col.List() {
 				q.queue.Add(statusItem{
-					Key:      string(krt.GetKey(obj)),
+					Key:      krt.GetKey(obj),
 					Reporter: name,
 				})
 			}
@@ -163,7 +163,7 @@ func Register[T StatusWriter](q *StatusQueue, name string, col krt.Collection[T]
 				}
 				for _, o := range events {
 					ol := o.Latest()
-					key := string(krt.GetKey(ol))
+					key := krt.GetKey(ol)
 					log.Debugf("registering key for processing: %s", key)
 					q.queue.Add(statusItem{
 						Key:      key,
