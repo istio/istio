bbdc5121161546b991adf53cde3901b5cacf93b8
Ian Rudie <ian.rudie@solo.io>
2024-11-27 13:29:21 -0800

    just remove the long key
    Signed-off-by: Ian Rudie <ian.rudie@solo.io>

--- pilot/pkg/xds/waypoint_test.go
+++ pilot/pkg/xds/waypoint_test.go
@@ -307,8 +307,7 @@ spec:
 }
 
 func TestWaypointRequestAuth(t *testing.T) {
-	// jwks from https://datatracker.ietf.org/doc/html/rfc7517#appendix-A.1
-	// nolint: lll
+	// jwks mostly from https://datatracker.ietf.org/doc/html/rfc7517#appendix-A.1
 	requestAuthn := `apiVersion: security.istio.io/v1
 kind: RequestAuthentication
 metadata:
@@ -329,13 +328,7 @@ spec:
               "x":"MKBCTNIcKUSDii11ySs3526iDZ8AiTo7Tu6KPAqv7D4",
               "y":"4Etl6SRW2YiLUrN5vfvVHuhp7x8PxltmWWlbbM4IFyM",
               "use":"enc",
-              "kid":"1"},
-
-              {"kty":"RSA",
-              "n": "0vx7agoebGcQSuuPiLJXZptN9nndrQmbXEps2aiAFbWhM78LhWx4cbbfAAtVT86zwu1RK7aPFFxuhDR1L6tSoc_BJECPebWKRXjBZCiFV4n3oknjhMstn64tZ_2W-5JsGY4Hc5n9yBXArwl93lqt7_RN5w6Cf0h4QyQ5v-65YGjQR0_FDW2QvzqY368QQMicAtaSqzs8KJZgnYb9c7d0zgdAZHzu6qMQvRL5hajrn1n91CbOpbISD08qNLyrdkt-bFTWhAI4vMQFh6WeZu0fM4lFd2NcRwr3XPksINHaQ-G_xBniIqbw0Ls1jF44-csFCur-kEgU8awapJzKnqDKgw",
-              "e":"AQAB",
-              "alg":"RS256",
-              "kid":"2011-04-29"}
+              "kid":"1"}
             ]
           }`
 	d, proxy := setupWaypointTest(t,
