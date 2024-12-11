d271248a7f966d6e47669e1ad5414d00ad777763
Ben Leggett <854255+bleggett@users.noreply.github.com>
2024-12-11 13:25:01 -0500

    Prep for iptables idempotency by making ipset ops (safely) idempotent by doubling-down on comments (#54217)
    * Prep for iptables idempotency by making ipset ops (safely) idempotent by doubling-down on comments
    
    Signed-off-by: Benjamin Leggett <benjamin.leggett@solo.io>
    
    * Genfix
    
    Signed-off-by: Benjamin Leggett <benjamin.leggett@solo.io>
    
    ---------
    
    Signed-off-by: Benjamin Leggett <benjamin.leggett@solo.io>

--- cni/pkg/nodeagent/server.go
+++ cni/pkg/nodeagent/server.go
@@ -357,7 +357,22 @@ func (s *meshDataplane) syncHostIPSets(ambientPods []*corev1.Pod) error {
 // 3. update ipsets accordingly
 // 4. return the ones we added successfully, and errors for any we couldn't (dupes)
 //
-// Dupe IPs should be considered an IPAM error and should never happen.
+// For each set (v4, v6), each pod IP can appear exactly once.
+//
+// Note that for adds, because we have 2 potential sources of async adds (CNI plugin and informer)
+// we want this to be an upsert.
+//
+// This is important to make sure reconciliation and overlapping events do not cause problems -
+// adds can come from two sources, but removes can only come from one source (informer).
+//
+// Ex:
+// Pod UID "A", IP 1.1.1.1
+// Pod UID "B", IP 1.1.1.1
+//
+// Add for UID "B", IP 1.1.1.1 is handled before Remove for UID "A", IP 1.1.1.1
+// -> we no longer have an entry for either, which is bad (pod fails healthchecks)
+//
+// So "add" always overwrites, and remove only removes if the pod IP AND the pod UID match.
 func (s *meshDataplane) addPodToHostNSIpset(pod *corev1.Pod, podIPs []netip.Addr) ([]netip.Addr, error) {
 	log := log.WithLabels("ns", pod.Namespace, "name", pod.Name)
 	// Add the pod UID as an ipset entry comment, so we can (more) easily find and delete
@@ -376,11 +391,7 @@ func (s *meshDataplane) addPodToHostNSIpset(pod *corev1.Pod, podIPs []netip.Addr
 		// set it to true, but in theory that could mask weird scenarios where K8S triggers events out of order ->
 		// an add(sameIPreused) then delete(originalIP).
 		// Which will result in the new pod starting to fail healthchecks.
-		//
-		// Since we purge on restart of CNI, and remove pod IPs from the set on every pod removal/deletion,
-		// we _shouldn't_ get any overwrite/overlap, unless something is wrong and we are asked to add
-		// a pod by an IP we already have in the set (which will give an error, which we want).
-		if err := s.hostsideProbeIPSet.AddIP(pip, ipProto, podUID, false); err != nil {
+		if err := s.hostsideProbeIPSet.AddIP(pip, ipProto, podUID, true); err != nil {
 			ipsetAddrErrs = append(ipsetAddrErrs, err)
 			log.Errorf("failed adding pod to ipset %s with ip %s: %v",
 				s.hostsideProbeIPSet.Prefix, pip, err)
@@ -392,13 +403,21 @@ func (s *meshDataplane) addPodToHostNSIpset(pod *corev1.Pod, podIPs []netip.Addr
 	return addedIps, errors.Join(ipsetAddrErrs...)
 }
 
+// removePodFromHostNSIpset will remove (v4, v6) pod IPs from the host IP set(s).
+// Note that unlike when we add the IP to the set, on removal we will simply
+// skip removing the IP if the IP matches, but the UID comment does not match our pod.
 func removePodFromHostNSIpset(pod *corev1.Pod, hostsideProbeSet *ipset.IPSet) error {
+	podUID := string(pod.ObjectMeta.UID)
+	log := log.WithLabels("ns", pod.Namespace, "name", pod.Name, "podUID", podUID, "ipset", hostsideProbeSet.Prefix)
+
 	podIPs := util.GetPodIPsIfPresent(pod)
 	for _, pip := range podIPs {
-		if err := hostsideProbeSet.ClearEntriesWithIP(pip); err != nil {
+		if uidMismatch, err := hostsideProbeSet.ClearEntriesWithIPAndComment(pip, podUID); err != nil {
 			return err
+		} else if uidMismatch != "" {
+			log.Warnf("pod ip %s could not be removed from ipset, found entry with pod UID %s instead", pip, uidMismatch)
 		}
-		log.Debugf("removed pod name %s with UID %s from host ipset %s by ip %s", pod.Name, pod.UID, hostsideProbeSet.Prefix, pip)
+		log.Debugf("removed pod from host ipset by ip %s", pip)
 	}
 
 	return nil
