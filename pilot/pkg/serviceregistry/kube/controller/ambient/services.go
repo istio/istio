dd63825efde529e38b666c5c242c42bcf99831bc
John Howard <john.howard@solo.io>
2024-12-11 09:26:05 -0800

    ambient: fix DNS resolution of ExternalName services with DNS proxy
    Today, we send an ~empty service over WDS. When the DNS request comes
    in, we match to the Service, see there is no VIP, and return NXDOMAIN.
    
    With this change, we just don't send the service at all. This mimics the
    behavior of sidecar DNS proxy, and gives the expected behavior: the DNS
    request is forwarded as request.

--- pilot/pkg/serviceregistry/kube/controller/ambient/services.go
+++ pilot/pkg/serviceregistry/kube/controller/ambient/services.go
@@ -58,6 +58,14 @@ func (a *index) serviceServiceBuilder(
 	namespaces krt.Collection[*v1.Namespace],
 ) krt.TransformationSingle[*v1.Service, model.ServiceInfo] {
 	return func(ctx krt.HandlerContext, s *v1.Service) *model.ServiceInfo {
+		if s.Spec.Type == v1.ServiceTypeExternalName {
+			// ExternalName services are not implemented by ambient (but will still work).
+			// The DNS requests will forward to the upstream DNS server, then Ztunnel can handle the request based on the target
+			// hostname.
+			// In theory we could add support for native 'DNS alias' into Ztunnel's DNS proxy. This would give the same behavior
+			// but let the DNS proxy handle it instead of forwarding upstream. However, at this time we do not do so.
+			return nil
+		}
 		portNames := map[int32]model.ServicePortName{}
 		for _, p := range s.Spec.Ports {
 			portNames[p.Port] = model.ServicePortName{
