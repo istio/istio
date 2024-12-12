e7cb41ea792dae70dadb19694bdfbedd02cfbcd6
Mitch Connors <mitchconnors@gmail.com>
2024-12-12 21:18:53 +0000

    fix tagwatcher with revisions
    Signed-off-by: Mitch Connors <mitchconnors@gmail.com>

--- pkg/revisions/tag_watcher_test.go
+++ pkg/revisions/tag_watcher_test.go
@@ -44,6 +44,7 @@ func TestTagWatcher(t *testing.T) {
 	track.WaitOrdered("revision")
 	assert.Equal(t, tw.GetMyTags(), sets.New("revision"))
 
+	whs.Create(makeRevision("revision"))
 	whs.Create(makeTag("revision", "tag-foo"))
 	track.WaitOrdered("revision,tag-foo")
 	assert.Equal(t, tw.GetMyTags(), sets.New("revision", "tag-foo"))
@@ -73,6 +74,18 @@ func makeTag(revision string, tg string) *admissionregistrationv1.MutatingWebhoo
 			Labels: map[string]string{
 				label.IoIstioRev.Name: revision,
 				label.IoIstioTag.Name: tg,
+			},
+		},
+	}
+}
+
+func makeRevision(revision string) *admissionregistrationv1.MutatingWebhookConfiguration {
+	return &admissionregistrationv1.MutatingWebhookConfiguration{
+		TypeMeta: metav1.TypeMeta{},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: revision,
+			Labels: map[string]string{
+				label.IoIstioRev.Name: revision,
 			},
 		},
 	}
