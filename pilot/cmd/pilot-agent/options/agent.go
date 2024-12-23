b87ed3dd63be487d7253f62da8d9328f938a0278
Keith Mattix II <keithmattix@microsoft.com>
2024-12-23 23:36:58 +0000

    Add tests
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>

--- pilot/cmd/pilot-agent/options/agent.go
+++ pilot/cmd/pilot-agent/options/agent.go
@@ -67,7 +67,6 @@ func NewAgentOptions(proxy *ProxyArgs, cfg *meshconfig.ProxyConfig, sds istioage
 		ProxyNamespace:              PodNamespaceVar.Get(),
 		ProxyDomain:                 proxy.DNSDomain,
 		IstiodSAN:                   istiodSAN.Get(),
-		MetadataDiscovery:           enableWDSEnv,
 		SDSFactory:                  sds,
 		WorkloadIdentitySocketFile:  workloadIdentitySocketFile,
 	}
