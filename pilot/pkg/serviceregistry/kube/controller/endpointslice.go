52b58c367c66aea400f87e3ed817e0b8fadc7423
Rama Chavali <rama.rao@salesforce.com>
2024-12-24 15:22:29 +0530

    change log level to debug
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/serviceregistry/kube/controller/endpointslice.go
+++ pilot/pkg/serviceregistry/kube/controller/endpointslice.go
@@ -114,7 +114,7 @@ func (esc *endpointSliceController) onEventInternal(_, ep *v1.EndpointSlice, eve
 	if svc != nil && !serviceNeedsPush(svc) {
 		return
 	}
-	log.Infof("triggering EDS push for %s %s in namespace %s", name, event, namespace)
+	log.Debugf("triggering EDS push for %s %s in namespace %s", name, event, namespace)
 
 	hostnames := esc.c.hostNamesForNamespacedName(namespacedName)
 	// Trigger EDS push for all hostnames.
