44bcb4719038e34b773736716dddc9e88613e495
Keith Mattix II <keithmattix@microsoft.com>
2025-01-06 17:25:00 -0600

    Fix Ambient Peer Authn flake (#54566)
    * Peer authn flake fix
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>
    
    * Add release note
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>
    
    ---------
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>

--- tests/integration/ambient/baseline_test.go
+++ tests/integration/ambient/baseline_test.go
@@ -723,9 +723,6 @@ func TestPeerAuthentication(t *testing.T) {
 	framework.NewTest(t).Run(func(t framework.TestContext) {
 		applyDrainingWorkaround(t)
 		runTestContext(t, func(t framework.TestContext, src echo.Instance, dst echo.Instance, opt echo.CallOptions) {
-			if opt.Scheme != scheme.TCP {
-				return
-			}
 			// Ensure we don't get stuck on old connections with old RBAC rules. This causes 45s test times
 			// due to draining.
 			opt.NewConnectionPerRequest = true
@@ -824,7 +821,7 @@ spec:
     18080:
       mode: PERMISSIVE
     19090:
-      mode: STRICT
+      mode: PERMISSIVE
         `).ApplyOrFail(t)
 				opt = opt.DeepCopy()
 				// Should pass for all workloads, in or out of mesh, targeting this port
@@ -836,7 +833,7 @@ spec:
 apiVersion: security.istio.io/v1
 kind: PeerAuthentication
 metadata:
-  name: global-strict
+  name: global-permissive
 spec:
   mtls:
     mode: PERMISSIVE
@@ -858,7 +855,6 @@ spec:
       mode: STRICT
     19090:
       mode: STRICT
-
         `).ApplyOrFail(t)
 				opt = opt.DeepCopy()
 				if !src.Config().HasProxyCapabilities() && dst.Config().HasProxyCapabilities() {
