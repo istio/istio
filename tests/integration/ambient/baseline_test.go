f89737935e7b455d14876453d1095ca01518f4dd
Keith Mattix II <keithmattix@microsoft.com>
2025-01-08 22:48:38 +0000

    Fix broken test revealed by the fix

--- tests/integration/ambient/baseline_test.go
+++ tests/integration/ambient/baseline_test.go
@@ -791,6 +791,8 @@ spec:
   portLevelMtls:
     18080:
       mode: PERMISSIVE
+		19090:
+      mode: PERMISSIVE
 				`).ApplyOrFail(t)
 				opt := opt.DeepCopy()
 				// Should pass for all workloads, in or out of mesh, targeting this port
