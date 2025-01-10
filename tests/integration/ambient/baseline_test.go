90275a98cfcf9f94acf4c9cf616eb18a6ed40f39
Keith Mattix II <keithmattix@microsoft.com>
2025-01-09 20:20:05 -0600

    [release-1.23] Fix Ambient Peer Authn flake (#54566) (#54617)
    * Fix Ambient Peer Authn flake (#54566)
    
    * Peer authn flake fix
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>
    
    * Add release note
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>
    
    ---------
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>
    
    * Sort portlevel mtls before converting
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>
    
    ---------
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>

--- tests/integration/ambient/baseline_test.go
+++ tests/integration/ambient/baseline_test.go
@@ -723,9 +723,6 @@ func TestPeerAuthentication(t *testing.T) {
 	framework.NewTest(t).Run(func(t framework.TestContext) {
 		applyDrainingWorkaround(t)
 		runTestContext(t, func(t framework.TestContext, src echo.Instance, dst echo.Instance, opt echo.CallOptions) {
-			if opt.Scheme != scheme.TCP {
-				return
-			}
 			// Ensure we don't get stuck on old connections with old RBAC rules. This causes 45s test times
 			// due to draining.
 			opt.NewConnectionPerRequest = true
@@ -825,6 +822,8 @@ spec:
   portLevelMtls:
     18080:
       mode: PERMISSIVE
+    19090:
+      mode: PERMISSIVE
         `).ApplyOrFail(t)
 				opt := opt.DeepCopy()
 				// Should pass for all workloads, in or out of mesh, targeting this port
@@ -836,7 +835,7 @@ spec:
 apiVersion: security.istio.io/v1
 kind: PeerAuthentication
 metadata:
-  name: global-strict
+  name: global-permissive
 spec:
   mtls:
     mode: PERMISSIVE
@@ -856,6 +855,8 @@ spec:
       app: "{{ .Destination }}"
   portLevelMtls:
     18080:
+      mode: STRICT
+    19090:
       mode: STRICT
         `).ApplyOrFail(t)
 				opt := opt.DeepCopy()
