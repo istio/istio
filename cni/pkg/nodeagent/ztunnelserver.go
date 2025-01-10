aa4da9ee43147b8c344481e802f901f1e516dce8
Benjamin Leggett <benjamin.leggett@solo.io>
2025-01-10 14:12:16 -0500

    Add a better log line for "we could write, but we timed out waiting for a valid msg"
    Signed-off-by: Benjamin Leggett <benjamin.leggett@solo.io>

--- cni/pkg/nodeagent/ztunnelserver.go
+++ cni/pkg/nodeagent/ztunnelserver.go
@@ -231,6 +231,8 @@ func (z *ztunnelServer) handleConn(ctx context.Context, conn *ZtunnelConnection)
 				if strings.Contains(err.Error(), "sendmsg: broken pipe") {
 					log.Error("ztunnel connection broken/unwritable, disposing of this connection")
 					return err
+				} else {
+					log.Warnf("timed out waiting for valid ztunnel response: %s", err)
 				}
 				// - we could write, but ztunnel responded with an `ack` error (in which case, this conn is not dead)
 				log.Errorf("ztunnel responded with an ack error: err %v ackErr %s", err, resp.GetAck().GetError())
