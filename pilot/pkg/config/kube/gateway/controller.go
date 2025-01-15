f19511ea3d1b54a107450e9c6177fc4a5451dd67
Mitch Connors <mitchconnors@gmail.com>
2025-01-15 22:23:38 +0000

    fix lint error

--- pilot/pkg/config/kube/gateway/controller.go
+++ pilot/pkg/config/kube/gateway/controller.go
@@ -181,7 +181,7 @@ func (c *Controller) Reconcile(ps *model.PushContext) error {
 		log.Debugf("reconcile complete in %v", time.Since(t0))
 	}()
 	gatewayClass := c.cache.List(gvk.GatewayClass, metav1.NamespaceAll)
-	all_gateways := c.cache.List(gvk.KubernetesGateway, metav1.NamespaceAll)
+	allGateways := c.cache.List(gvk.KubernetesGateway, metav1.NamespaceAll)
 	httpRoute := c.cache.List(gvk.HTTPRoute, metav1.NamespaceAll)
 	grpcRoute := c.cache.List(gvk.GRPCRoute, metav1.NamespaceAll)
 	tcpRoute := c.cache.List(gvk.TCPRoute, metav1.NamespaceAll)
@@ -191,7 +191,7 @@ func (c *Controller) Reconcile(ps *model.PushContext) error {
 
 	// Filter down to gateways currently in this tag/revision
 	gateway := []config.Config{}
-	for _, gw := range all_gateways {
+	for _, gw := range allGateways {
 		if c.tagWatcher.IsMine(gw.ToObjectMeta()) {
 			gateway = append(gateway, gw)
 		}
