b1847cd24a1605046fb4cbb41c1481e19e6b80be
John Mazzitelli <mazz@redhat.com>
2025-02-03 10:25:49 -0500

    bump Kiali to v2.5 for the Istio 1.25 release (#54950)

--- manifests/addons/gen.sh
+++ manifests/addons/gen.sh
@@ -33,8 +33,8 @@ GRAFANA_VERSION=${GRAFANA_VERSION:-"8.6.3"}
 {
 helm3 template kiali-server \
   --namespace istio-system \
-  --version 2.3.0 \
-  --set deployment.image_version=v2.3 \
+  --version 2.5.0 \
+  --set deployment.image_version=v2.5 \
   --include-crds \
   kiali-server \
   --repo https://kiali.org/helm-charts \
