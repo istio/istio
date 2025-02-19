8059460d2fc3eefff5b2e8912428bbf5fe824e8e
Benjamin Leggett <benjamin.leggett@solo.io>
2025-02-19 17:56:01 -0500

    Needs unique ones here too
    Signed-off-by: Benjamin Leggett <benjamin.leggett@solo.io>

--- cni/pkg/nodeagent/net_test.go
+++ cni/pkg/nodeagent/net_test.go
@@ -66,7 +66,7 @@ func getTestFixureWithIptablesConfig(ctx context.Context, fakeDeps *dependencies
 
 	ztunnelServer := &fakeZtunnel{}
 
-	netServer := newNetServer(ztunnelServer, podNsMap, podIptC, NewPodNetnsProcFinder(fakeFs(false)))
+	netServer := newNetServer(ztunnelServer, podNsMap, podIptC, NewPodNetnsProcFinder(fakeFs(true)))
 
 	netServer.netnsRunner = func(fdable NetnsFd, toRun func() error) error {
 		return toRun()
