bfcb2f1a9f210f53dfae42b3a986983b26a0e670
Mike Morris <1149913+mikemorris@users.noreply.github.com>
2025-02-20 11:23:29 -0500

    update bin/update_deps.sh and bin/update_ztunnel.sh to release-1.25 branch and run scripts

--- bin/update_deps.sh
+++ bin/update_deps.sh
@@ -16,7 +16,7 @@
 
 set -exo pipefail
 
-UPDATE_BRANCH=${UPDATE_BRANCH:-"master"}
+UPDATE_BRANCH=${UPDATE_BRANCH:-"release-1.25"}
 
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOTDIR=$(dirname "${SCRIPTPATH}")
@@ -35,10 +35,10 @@ go get -u "istio.io/api@${UPDATE_BRANCH}"
 go get -u "istio.io/client-go@${UPDATE_BRANCH}"
 go mod tidy
 
-sed -i "s/^BUILDER_SHA=.*\$/BUILDER_SHA=$(getSha release-builder)/" prow/release-commit.sh
+sed -i'.bak' -e "s/^BUILDER_SHA=.*\$/BUILDER_SHA=$(getSha release-builder)/" prow/release-commit.sh
 chmod +x prow/release-commit.sh
-sed -i '/PROXY_REPO_SHA/,/lastStableSHA/ { s/"lastStableSHA":.*/"lastStableSHA": "'"$(getSha proxy)"'"/  }; /ZTUNNEL_REPO_SHA/,/lastStableSHA/ { s/"lastStableSHA":.*/"lastStableSHA": "'"$(getSha ztunnel)"'"/  }' istio.deps
+sed -i'.bak' -e '/PROXY_REPO_SHA/,/lastStableSHA/ { s/"lastStableSHA":.*/"lastStableSHA": "'"$(getSha proxy)"'"/  }; /ZTUNNEL_REPO_SHA/,/lastStableSHA/ { s/"lastStableSHA":.*/"lastStableSHA": "'"$(getSha ztunnel)"'"/  }' istio.deps
 
 # shellcheck disable=SC1001
 LATEST_DISTROLESS_SHA256=$(crane digest cgr.dev/chainguard/static | awk -F\: '{print $2}')
-sed -i -E "s/sha256:[a-z0-9]+/sha256:${LATEST_DISTROLESS_SHA256}/g" docker/Dockerfile.distroless
+sed -i'.bak' -E "s/sha256:[a-z0-9]+/sha256:${LATEST_DISTROLESS_SHA256}/g" docker/Dockerfile.distroless
