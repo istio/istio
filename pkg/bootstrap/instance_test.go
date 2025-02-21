63d5de4fd0d2ec81b3f40d0a86f574b9b0d8337a
Leonardo Sarra <8293810+leosarra@users.noreply.github.com>
2025-02-21 19:55:53 +0100

    Remove ENABLE_DEFERRED_CLUSTER_CREATION flag and code paths (#55206)

--- pkg/bootstrap/instance_test.go
+++ pkg/bootstrap/instance_test.go
@@ -43,7 +43,6 @@ import (
 
 	"istio.io/api/annotation"
 	meshconfig "istio.io/api/mesh/v1alpha1"
-	"istio.io/istio/pilot/pkg/features"
 	"istio.io/istio/pilot/test/util"
 	"istio.io/istio/pkg/bootstrap/platform"
 	"istio.io/istio/pkg/model"
@@ -79,21 +78,20 @@ var (
 // REFRESH_GOLDEN=true go test ./pkg/bootstrap/...
 func TestGolden(t *testing.T) {
 	cases := []struct {
-		base                          string
-		envVars                       map[string]string
-		annotations                   map[string]string
-		sdsUDSPath                    string
-		sdsTokenPath                  string
-		expectLightstepAccessToken    bool
-		stats                         stats
-		checkLocality                 bool
-		stsPort                       int
-		platformMeta                  map[string]string
-		setup                         func()
-		teardown                      func()
-		check                         func(got *bootstrap.Bootstrap, t *testing.T)
-		compliancePolicy              string
-		enableDefferedClusterCreation bool
+		base                       string
+		envVars                    map[string]string
+		annotations                map[string]string
+		sdsUDSPath                 string
+		sdsTokenPath               string
+		expectLightstepAccessToken bool
+		stats                      stats
+		checkLocality              bool
+		stsPort                    int
+		platformMeta               map[string]string
+		setup                      func()
+		teardown                   func()
+		check                      func(got *bootstrap.Bootstrap, t *testing.T)
+		compliancePolicy           string
 	}{
 		{
 			base: "xdsproxy",
@@ -181,10 +179,6 @@ func TestGolden(t *testing.T) {
 			base: "all",
 		},
 		{
-			base:                          "deferred_cluster_creation",
-			enableDefferedClusterCreation: true,
-		},
-		{
 			base: "stats_inclusion",
 			annotations: map[string]string{
 				"sidecar.istio.io/statsInclusionPrefixes": "prefix1,prefix2,http.{pod_ip}_",
@@ -242,7 +236,6 @@ func TestGolden(t *testing.T) {
 
 	for _, c := range cases {
 		t.Run("Bootstrap-"+c.base, func(t *testing.T) {
-			test.SetForTest(t, &features.EnableDeferredClusterCreation, c.enableDefferedClusterCreation)
 			out := t.TempDir()
 			if c.setup != nil {
 				c.setup()
