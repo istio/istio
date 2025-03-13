22caff9436da709d35f715f3d0163da398381be0
John Howard <john.howard@solo.io>
2025-03-13 08:57:17 -0700

    Bump modules and minimum go version
    Start requiring Go 1.24.
    
    We don't use any new features of it yet in this PR, but can in others
    (generic type alias, etc).
    
    RSA key size min was introduced, so workaround it in tests (we could
    also change the key size, but it takes multiple seconds to generate
    large RSA keys with -race)

--- security/pkg/server/ca/authenticate/oidc_test.go
+++ security/pkg/server/ca/authenticate/oidc_test.go
@@ -11,6 +11,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+// Allow a smaller key since large keys + race detector is pretty slow
+//
+//go:debug rsa1024min=0
 package authenticate
 
 import (
