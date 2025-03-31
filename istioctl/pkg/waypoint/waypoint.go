7101392292572445c1488331376bd155391c5902
my-git9 <xin.li@daocloud.io>
2025-03-31 10:19:44 +0800

    istioctl: improve waypoint delete example (#55733)
    Signed-off-by: xin.li <xin.li@daocloud.io>

--- istioctl/pkg/waypoint/waypoint.go
+++ istioctl/pkg/waypoint/waypoint.go
@@ -320,7 +320,7 @@ func Cmd(ctx cli.Context) *cobra.Command {
 		Use:   "delete",
 		Short: "Delete a waypoint configuration",
 		Long:  "Delete a waypoint configuration from the cluster",
-		Example: `# Delete a waypoint by name, which can obtain from istioctl waypoint list
+		Example: `  # Delete a waypoint by name, which can obtain from istioctl waypoint list
   istioctl waypoint delete waypoint-name --namespace default
 
   # Delete several waypoints by name
