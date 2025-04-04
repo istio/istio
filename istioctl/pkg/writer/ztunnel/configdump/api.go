603b32655aa6d138b148d789971422e516663923
John Howard <howardjohn@google.com>
2025-04-04 04:36:47 -0700

    istioctl: include protocol in connection dump (#55720)
    * istioctl: include protocol in connection dump
    
    Goes with https://github.com/istio/ztunnel/pull/1496
    
    * lint

--- istioctl/pkg/writer/ztunnel/configdump/api.go
+++ istioctl/pkg/writer/ztunnel/configdump/api.go
@@ -158,12 +158,14 @@ type InboundConnection struct {
 	Src         string `json:"src"`
 	OriginalDst string `json:"originalDst"`
 	ActualDst   string `json:"actualDst"`
+	Protocol    string `json:"protocol"`
 }
 
 type OutboundConnection struct {
 	Src         string `json:"src"`
 	OriginalDst string `json:"originalDst"`
 	ActualDst   string `json:"actualDst"`
+	Protocol    string `json:"protocol"`
 }
 
 type WorkloadConnection struct {
