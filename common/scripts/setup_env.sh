32ae0ef5a424a009a8ae78d85afeb1ff94610f79
istio-testing <istio-testing-bot@google.com>
2025-04-18 09:30:46 +0000

    Automator: update common-files@release-1.26 in istio/istio@release-1.26

--- common/scripts/setup_env.sh
+++ common/scripts/setup_env.sh
@@ -75,7 +75,7 @@ fi
 TOOLS_REGISTRY_PROVIDER=${TOOLS_REGISTRY_PROVIDER:-gcr.io}
 PROJECT_ID=${PROJECT_ID:-istio-testing}
 if [[ "${IMAGE_VERSION:-}" == "" ]]; then
-  IMAGE_VERSION=master-fcd42145fc132acd1e8f607e9e7aca15058e9fb9
+  IMAGE_VERSION=release-1.26-fcd42145fc132acd1e8f607e9e7aca15058e9fb9
 fi
 if [[ "${IMAGE_NAME:-}" == "" ]]; then
   IMAGE_NAME=build-tools
