8c6df171f214aa820671d272ac42bf412ccd5515
Sebastian Schepens <sebastian.schepens@mercadolibre.com>
2025-04-23 11:58:08 -0300

    krt memory controller

--- pilot/pkg/config/memory/controller.go
+++ pilot/pkg/config/memory/controller.go
@@ -22,46 +22,58 @@ import (
 	"istio.io/istio/pilot/pkg/model"
 	"istio.io/istio/pkg/config"
 	"istio.io/istio/pkg/config/schema/collection"
-	"istio.io/istio/pkg/slices"
+	"istio.io/istio/pkg/kube"
+	"istio.io/istio/pkg/kube/controllers"
+	"istio.io/istio/pkg/kube/krt"
 )
+
+type Options struct {
+	// processes events synchronously and requires manual sync
+	Sync           bool
+	SkipValidation bool
+	KrtDebugger    *krt.DebugHandler
+}
 
 // Controller is an implementation of ConfigStoreController.
 type Controller struct {
-	monitor     Monitor
-	configStore model.ConfigStore
-	hasSynced   func() bool
-
-	// If meshConfig.DiscoverySelectors are specified, the namespacesFilter tracks the namespaces this controller watches.
-	namespacesFilter func(obj interface{}) bool
+	hasSynced func() bool
+	store     *Store
+	handlers  []krt.HandlerRegistration
 }
 
 // NewController return an implementation of ConfigStoreController
-// This is a client-side monitor that dispatches events as the changes are being
-// made on the client.
-func NewController(cs model.ConfigStore) *Controller {
-	out := &Controller{
-		configStore: cs,
-		monitor:     NewMonitor(cs),
+func NewController(store *Store) *Controller {
+	fmt.Println("new memory controller")
+	return &Controller{
+		store: store,
 	}
-	return out
-}
-
-// NewSyncController return an implementation of model.ConfigStoreController which processes events synchronously
-func NewSyncController(cs model.ConfigStore) *Controller {
-	out := &Controller{
-		configStore: cs,
-		monitor:     NewSyncMonitor(cs),
-	}
-
-	return out
 }
 
 func (c *Controller) RegisterHasSyncedHandler(cb func() bool) {
 	c.hasSynced = cb
 }
 
+func (c *Controller) WaitUntilSynced(stop <-chan struct{}) bool {
+	return kube.WaitForCacheSync("memory", stop, c.HasSynced)
+}
+
 func (c *Controller) RegisterEventHandler(kind config.GroupVersionKind, f model.EventHandler) {
-	c.monitor.AppendEventHandler(kind, f)
+	c.store.mutex.Lock()
+	defer c.store.mutex.Unlock()
+	if kindStore, ok := c.store.data[kind]; ok {
+		c.handlers = append(c.handlers, kindStore.collection.RegisterBatch(func(o []krt.Event[config.Config]) {
+			for _, event := range o {
+				switch event.Event {
+				case controllers.EventAdd:
+					f(config.Config{}, *event.New, model.Event(event.Event))
+				case controllers.EventUpdate:
+					f(*event.Old, *event.New, model.Event(event.Event))
+				case controllers.EventDelete:
+					f(config.Config{}, *event.Old, model.Event(event.Event))
+				}
+			}
+		}, false))
+	}
 }
 
 // HasSynced return whether store has synced
@@ -71,56 +83,48 @@ func (c *Controller) HasSynced() bool {
 	if c.hasSynced != nil {
 		return c.hasSynced()
 	}
+
+	if !c.store.hasSynced() {
+		fmt.Println("syncer not synced")
+		return false
+	}
+
+	for _, handler := range c.handlers {
+		if !handler.HasSynced() {
+			fmt.Println("handler not synced")
+			return false
+		}
+	}
+
 	return true
 }
 
 func (c *Controller) Run(stop <-chan struct{}) {
-	c.monitor.Run(stop)
+	fmt.Println("memory controller run")
+	// marked as synced on run to allow clients to store data before the store is marked as synced
+	c.store.syncer.MarkSynced()
+	<-stop
+	close(c.store.stop)
 }
 
 func (c *Controller) Schemas() collection.Schemas {
-	return c.configStore.Schemas()
+	return c.store.schemas
 }
 
 func (c *Controller) Get(kind config.GroupVersionKind, key, namespace string) *config.Config {
-	if c.namespacesFilter != nil && !c.namespacesFilter(namespace) {
-		return nil
-	}
-	return c.configStore.Get(kind, key, namespace)
+	return c.store.Get(kind, key, namespace)
 }
 
-func (c *Controller) Create(config config.Config) (revision string, err error) {
-	if revision, err = c.configStore.Create(config); err == nil {
-		c.monitor.ScheduleProcessEvent(ConfigEvent{
-			config: config,
-			event:  model.EventAdd,
-		})
-	}
-	return
+func (c *Controller) Create(cfg config.Config) (revision string, err error) {
+	return c.store.Create(cfg)
 }
 
-func (c *Controller) Update(config config.Config) (newRevision string, err error) {
-	oldconfig := c.configStore.Get(config.GroupVersionKind, config.Name, config.Namespace)
-	if newRevision, err = c.configStore.Update(config); err == nil {
-		c.monitor.ScheduleProcessEvent(ConfigEvent{
-			old:    *oldconfig,
-			config: config,
-			event:  model.EventUpdate,
-		})
-	}
-	return
+func (c *Controller) Update(cfg config.Config) (newRevision string, err error) {
+	return c.store.Update(cfg)
 }
 
-func (c *Controller) UpdateStatus(config config.Config) (newRevision string, err error) {
-	oldconfig := c.configStore.Get(config.GroupVersionKind, config.Name, config.Namespace)
-	if newRevision, err = c.configStore.UpdateStatus(config); err == nil {
-		c.monitor.ScheduleProcessEvent(ConfigEvent{
-			old:    *oldconfig,
-			config: config,
-			event:  model.EventUpdate,
-		})
-	}
-	return
+func (c *Controller) UpdateStatus(cfg config.Config) (newRevision string, err error) {
+	return c.store.UpdateStatus(cfg)
 }
 
 func (c *Controller) Patch(orig config.Config, patchFn config.PatchFunc) (newRevision string, err error) {
@@ -131,36 +135,14 @@ func (c *Controller) Patch(orig config.Config, patchFn config.PatchFunc) (newRev
 	default:
 		return "", fmt.Errorf("unsupported merge type: %s", typ)
 	}
-	if newRevision, err = c.configStore.Patch(cfg, patchFn); err == nil {
-		c.monitor.ScheduleProcessEvent(ConfigEvent{
-			old:    orig,
-			config: cfg,
-			event:  model.EventUpdate,
-		})
-	}
-	return
+
+	return c.store.Patch(cfg, patchFn)
 }
 
 func (c *Controller) Delete(kind config.GroupVersionKind, key, namespace string, resourceVersion *string) error {
-	if config := c.Get(kind, key, namespace); config != nil {
-		if err := c.configStore.Delete(kind, key, namespace, resourceVersion); err != nil {
-			return err
-		}
-		c.monitor.ScheduleProcessEvent(ConfigEvent{
-			config: *config,
-			event:  model.EventDelete,
-		})
-		return nil
-	}
-	return fmt.Errorf("delete: config %v/%v/%v does not exist", kind, namespace, key)
+	return c.store.Delete(kind, key, namespace, resourceVersion)
 }
 
 func (c *Controller) List(kind config.GroupVersionKind, namespace string) []config.Config {
-	configs := c.configStore.List(kind, namespace)
-	if c.namespacesFilter != nil {
-		return slices.Filter(configs, func(config config.Config) bool {
-			return c.namespacesFilter(config)
-		})
-	}
-	return configs
+	return c.store.List(kind, namespace)
 }
