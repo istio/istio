d4317ed79ab68e3dba4329068dc54c6f7a5a4918
Sebastian Schepens <sebastian.schepens@mercadolibre.com>
2025-04-24 12:31:06 -0300

    extend logs

--- pilot/pkg/xds/discovery.go
+++ pilot/pkg/xds/discovery.go
@@ -415,12 +415,7 @@ func debounce(ch chan *model.PushRequest, stopCh <-chan struct{}, opts DebounceO
 func configsUpdated(req *model.PushRequest) string {
 	configs := ""
 	for key := range req.ConfigsUpdated {
-		configs += key.String()
-		break
-	}
-	if len(req.ConfigsUpdated) > 1 {
-		more := " and " + strconv.Itoa(len(req.ConfigsUpdated)-1) + " more configs"
-		configs += more
+		configs += key.String() + ", "
 	}
 	return configs
 }
