82bee2b455527f406cd7dbd884316070f1232a21
Tyler Schade <tylerschade99@gmail.com>
2025-05-01 22:51:38 -0400

    Add configurable client timeout to istioctl commands (#56020)
    * Add configurable timeout to istioctl commands
    
    Signed-off-by: tjons <tylerschade99@gmail.com>
    
    * fix typo where 15 second timeout was reduced to 5
    
    Signed-off-by: tjons <tylerschade99@gmail.com>
    
    * error if kubeclient-timeout is an invalid duration
    
    Signed-off-by: tjons <tylerschade99@gmail.com>
    
    * fix error messages to use the name of the flag
    
    Signed-off-by: tjons <tylerschade99@gmail.com>
    
    * actually fix error message
    
    Signed-off-by: tjons <tylerschade99@gmail.com>
    
    ---------
    
    Signed-off-by: tjons <tylerschade99@gmail.com>

--- pkg/kube/client.go
+++ pkg/kube/client.go
@@ -479,14 +479,21 @@ func newClientInternal(clientFactory *clientFactory, opts ...ClientOption) (*cli
 		return nil, err
 	}
 
-	c.http = &http.Client{
-		Timeout: time.Second * 15,
+	c.http = &http.Client{}
+	if c.config != nil && c.config.Timeout != 0 {
+		c.http.Timeout = c.config.Timeout
+	} else {
+		c.http.Timeout = time.Second * 15
 	}
+
 	var clientWithTimeout kubernetes.Interface
 	clientWithTimeout = c.kube
 	restConfig := c.RESTConfig()
 	if restConfig != nil {
-		restConfig.Timeout = time.Second * 5
+		if restConfig.Timeout == 0 {
+			restConfig.Timeout = time.Second * 5
+		}
+
 		kubeClient, err := kubernetes.NewForConfig(restConfig)
 		if err == nil {
 			clientWithTimeout = kubeClient
@@ -541,6 +548,20 @@ func WithRevision(revision string) ClientOption {
 	return func(c CLIClient) CLIClient {
 		client := c.(*client)
 		client.revision = revision
+		return client
+	}
+}
+
+// WithTimeout sets the timeout for the client.
+func WithTimeout(timeout time.Duration) ClientOption {
+	return func(c CLIClient) CLIClient {
+		client := c.(*client)
+		if client.config == nil {
+			client.config = &rest.Config{}
+		}
+
+		client.config.Timeout = timeout
+
 		return client
 	}
 }
