30896df6f134c68bcb5e46b2a7baf22d94b991af
Sebastian Schepens <sebastian.schepens@mercadolibre.com>
2025-05-06 09:41:25 -0300

    add more logs

--- pilot/pkg/status/resourcelock.go
+++ pilot/pkg/status/resourcelock.go
@@ -204,6 +204,7 @@ func (wp *WorkerPool) maybeAddWorker() {
 			wp.lock.Unlock()
 			// work should be done without holding the lock
 			cfg := wp.get(target)
+			scope.Infof("retrieved config for status update %s: %+v", target, cfg)
 			if cfg != nil {
 				// Check that generation matches
 				if strconv.FormatInt(cfg.Generation, 10) == target.Generation {
@@ -215,6 +216,8 @@ func (wp *WorkerPool) maybeAddWorker() {
 					}
 					cfg.Status = sm.Unwrap()
 					wp.write(cfg)
+				} else {
+					scope.Infof("skipping status update for %s because generation does not match: %s != %s", target, cfg.Generation, target.Generation)
 				}
 			}
 			wp.lock.Lock()
