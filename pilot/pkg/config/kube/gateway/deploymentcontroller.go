384d95771f3419c7de2c3852caf1ab6e6361a034
irenezhong2861 <irenezhong@microsoft.com>
2025-05-21 23:12:16 +0000

    update native sidecar default value

--- pilot/pkg/config/kube/gateway/deploymentcontroller.go
+++ pilot/pkg/config/kube/gateway/deploymentcontroller.go
@@ -439,6 +439,7 @@ func (d *DeploymentController) configureIstioGateway(log *istiolog.Scope, gw gat
 
 		KubeVersion:               kube.GetVersionAsInt(d.client),
 		Revision:                  d.revision,
+		NativeSidecars:            features.EnableNativeSidecars.Get(),
 		ServiceType:               serviceType,
 		ProxyUID:                  proxyUID,
 		ProxyGID:                  proxyGID,
@@ -871,6 +872,7 @@ type TemplateInput struct {
 	ClusterID                 string
 	KubeVersion               int
 	Revision                  string
+	NativeSidecars            bool
 	ProxyUID                  int64
 	ProxyGID                  int64
 	CompliancePolicy          string
