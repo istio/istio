69483c9b95d5a5fed1d820aed53b32ed83f5bf6f
my-git9 <xin.li@daocloud.io>
2025-05-30 11:08:37 +0800

    Support customizing the timeout of istioctl waypoint status (#56454)
    * Support customizing the timeout of istioctl waypoint status
    
    Signed-off-by: xin.li <xin.li@daocloud.io>
    
    * add waypoint timeout flags for waypoint apply
    
    Signed-off-by: xin.li <xin.li@daocloud.io>
    
    ---------
    
    Signed-off-by: xin.li <xin.li@daocloud.io>

--- istioctl/pkg/waypoint/waypoint.go
+++ istioctl/pkg/waypoint/waypoint.go
@@ -49,6 +49,8 @@ var (
 	waitReady     bool
 	allNamespaces bool
 
+	waypointTimeout time.Duration
+
 	deleteAll bool
 
 	trafficType       = ""
@@ -282,7 +284,7 @@ func Cmd(ctx cli.Context) *cobra.Command {
 					if programmed {
 						break
 					}
-					if time.Since(startTime) > waitTimeout {
+					if time.Since(startTime) > waypointTimeout {
 						return errorWithMessage("timed out while waiting for waypoint", gwc, err)
 					}
 				}
@@ -456,7 +458,9 @@ func Cmd(ctx cli.Context) *cobra.Command {
 
 	waypointApplyCmd.Flags().StringVarP(&revision, "revision", "r", "", "The revision to label the waypoint with")
 	waypointApplyCmd.Flags().BoolVarP(&waitReady, "wait", "w", false, "Wait for the waypoint to be ready")
+	waypointApplyCmd.Flags().DurationVar(&waypointTimeout, "waypoint-timeout", waitTimeout, "Timeout for waiting for waypoint ready")
 	waypointGenerateCmd.Flags().StringVarP(&revision, "revision", "r", "", "The revision to label the waypoint with")
+	waypointStatusCmd.Flags().DurationVar(&waypointTimeout, "waypoint-timeout", waitTimeout, "Timeout for retrieving status for waypoint")
 	waypointCmd.AddCommand(waypointGenerateCmd)
 	waypointCmd.AddCommand(waypointDeleteCmd)
 	waypointCmd.AddCommand(waypointListCmd)
@@ -624,7 +628,8 @@ func printWaypointStatus(ctx cli.Context, w *tabwriter.Writer, kubeClient kube.C
 			if programmed {
 				break
 			}
-			if time.Since(startTime) > waitTimeout {
+
+			if time.Since(startTime) > waypointTimeout {
 				return errorWithMessage("timed out while retrieving status for waypoint", gwc, err)
 			}
 		}
