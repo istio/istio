02c0d1cfb35e97a58bcaef956026f8546f806a6e
Sridhar Gaddam <sgaddam@redhat.com>
2025-06-03 00:22:09 +0530

    support native nftables flag while using istio-init container
    Signed-off-by: Sridhar Gaddam <sgaddam@redhat.com>
    Co-authored-by: Yuanlin Xu <yuanlin.xu@redhat.com>

--- tools/istio-iptables/pkg/constants/constants.go
+++ tools/istio-iptables/pkg/constants/constants.go
@@ -87,6 +87,7 @@ const (
 	Reconcile                 = "reconcile"
 	CleanupOnly               = "cleanup-only"
 	ForceApply                = "force-apply"
+	NativeNftables            = "native-nftables"
 )
 
 // Environment variables that deliberately have no equivalent command-line flags.
