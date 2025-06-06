48382fe2de44c5b9b18b2ec458362ea388d9da85
Sebastian Schepens <sebastian.schepens@mercadolibre.com>
2025-06-06 17:25:32 -0300

    krt fake store

--- pilot/pkg/model/fake_store.go
+++ pilot/pkg/model/fake_store.go
@@ -16,92 +16,96 @@ package model
 
 import (
 	"errors"
+	"sync/atomic"
 
 	"istio.io/istio/pkg/config"
 	"istio.io/istio/pkg/config/schema/collection"
 	"istio.io/istio/pkg/config/schema/collections"
-	"istio.io/istio/pkg/maps"
+	"istio.io/istio/pkg/kube"
+	"istio.io/istio/pkg/kube/controllers"
+	"istio.io/istio/pkg/kube/krt"
 )
 
 type FakeStore struct {
-	store map[config.GroupVersionKind]map[string]map[string]config.Config
+	store  map[config.GroupVersionKind]nsStore
+	stop   chan struct{}
+	synced *atomic.Bool
+}
+
+type nsStore struct {
+	collection krt.StaticCollection[config.Config]
+	index      krt.Index[string, config.Config]
+	handlers   []krt.HandlerRegistration
 }
 
 func NewFakeStore() *FakeStore {
-	f := FakeStore{
-		store: make(map[config.GroupVersionKind]map[string]map[string]config.Config),
+	stop := make(chan struct{})
+	f := &FakeStore{
+		store:  make(map[config.GroupVersionKind]nsStore),
+		stop:   stop,
+		synced: &atomic.Bool{},
 	}
-	return &f
+
+	for _, s := range collections.Pilot.All() {
+		opts := krt.NewOptionsBuilder(f.stop, "fake-store", krt.GlobalDebugHandler)
+		collection := krt.NewStaticCollection[config.Config](f, nil, opts.WithName(s.Kind())...)
+		nsConfigs := nsStore{
+			collection: collection,
+			index:      krt.NewNamespaceIndex(collection),
+		}
+		f.store[s.GroupVersionKind()] = nsConfigs
+	}
+
+	return f
 }
 
-var _ ConfigStore = (*FakeStore)(nil)
+var _ ConfigStoreController = (*FakeStore)(nil)
 
 func (s *FakeStore) Schemas() collection.Schemas {
 	return collections.Pilot
 }
 
 func (s *FakeStore) Get(typ config.GroupVersionKind, name, namespace string) *config.Config {
-	nsConfigs := s.store[typ]
-	if nsConfigs == nil {
+	nsConfigs, ok := s.store[typ]
+	if !ok {
 		return nil
 	}
 
-	configs := nsConfigs[namespace]
-	if configs == nil {
-		return nil
+	key := name
+	if namespace != "" {
+		key = namespace + "/" + name
 	}
-
-	if config, f := configs[name]; f {
-		return &config
-	}
-
-	return nil
+	return nsConfigs.collection.GetKey(key)
 }
 
 func (s *FakeStore) List(typ config.GroupVersionKind, namespace string) []config.Config {
-	nsConfigs := s.store[typ]
-	if nsConfigs == nil {
+	data, exists := s.store[typ]
+	if !exists {
 		return nil
 	}
-	var res []config.Config
+
 	if namespace == NamespaceAll {
-		for _, configs := range nsConfigs {
-			for _, cfg := range configs {
-				res = append(res, cfg)
-			}
-		}
-		return res
+		return data.collection.List()
 	}
 
-	return maps.Values(nsConfigs[namespace])
+	return data.index.Lookup(namespace)
 }
 
 func (s *FakeStore) Create(cfg config.Config) (revision string, err error) {
-	nsConfigs := s.store[cfg.GroupVersionKind]
-	if nsConfigs == nil {
-		nsConfigs = make(map[string]map[string]config.Config)
-		s.store[cfg.GroupVersionKind] = nsConfigs
+	nsConfigs, ok := s.store[cfg.GroupVersionKind]
+	if ok {
+		nsConfigs.collection.UpdateObject(cfg)
 	}
 
-	configs := nsConfigs[cfg.Namespace]
-	if configs == nil {
-		configs = make(map[string]config.Config)
-		nsConfigs[cfg.Namespace] = configs
-	}
-
-	configs[cfg.Name] = cfg
 	return "", nil
 }
 
 func (s *FakeStore) Update(cfg config.Config) (newRevision string, err error) {
-	nsConfigs := s.store[cfg.GroupVersionKind]
-	if nsConfigs != nil {
-		configs := nsConfigs[cfg.Namespace]
-		if configs != nil {
-			if _, f := configs[cfg.Name]; f {
-				configs[cfg.Name] = cfg
-				return "", nil
-			}
+	nsConfigs, ok := s.store[cfg.GroupVersionKind]
+	if ok {
+		if obj := nsConfigs.collection.GetKey(krt.GetKey(cfg)); obj != nil {
+			nsConfigs.collection.UpdateObject(cfg)
+			return "", nil
 		}
 	}
 
@@ -115,16 +119,61 @@ func (*FakeStore) Patch(orig config.Config, patchFn config.PatchFunc) (string, e
 }
 
 func (s *FakeStore) Delete(typ config.GroupVersionKind, name, namespace string, rv *string) error {
-	nsConfigs := s.store[typ]
-	if nsConfigs == nil {
+	nsConfigs, ok := s.store[typ]
+	if !ok {
 		return nil
 	}
 
-	configs := nsConfigs[namespace]
-	if configs == nil {
-		return nil
+	key := name
+	if namespace != "" {
+		key = namespace + "/" + name
 	}
-
-	delete(configs, name)
+	nsConfigs.collection.DeleteObject(key)
 	return nil
+}
+
+func (s *FakeStore) RegisterEventHandler(kind config.GroupVersionKind, handler EventHandler) {
+	if nsConfigs, ok := s.store[kind]; ok {
+		nsConfigs.handlers = append(
+			nsConfigs.handlers,
+			nsConfigs.collection.RegisterBatch(func(evs []krt.Event[config.Config]) {
+				for _, event := range evs {
+					switch event.Event {
+					case controllers.EventAdd:
+						handler(config.Config{}, *event.New, EventAdd)
+					case controllers.EventUpdate:
+						handler(*event.Old, *event.New, EventUpdate)
+					case controllers.EventDelete:
+						handler(config.Config{}, *event.Old, EventDelete)
+					}
+				}
+			}, false),
+		)
+	}
+}
+
+func (s *FakeStore) Run(stop <-chan struct{}) {
+	s.synced.Store(true)
+	<-stop
+	close(s.stop)
+}
+
+func (s *FakeStore) HasSynced() bool {
+	if !s.synced.Load() {
+		return false
+	}
+
+	for _, nsConfigs := range s.store {
+		for _, handler := range nsConfigs.handlers {
+			if !handler.HasSynced() {
+				return false
+			}
+		}
+	}
+
+	return s.synced.Load()
+}
+
+func (s *FakeStore) WaitUntilSynced(stop <-chan struct{}) bool {
+	return kube.WaitForCacheSync("memory", stop, s.HasSynced)
 }
