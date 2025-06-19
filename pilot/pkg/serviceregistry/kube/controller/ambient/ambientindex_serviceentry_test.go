c9e2a7a30a0197cc3758bcaf6c1eeff108817a7c
Vikas Choudhary (vikasc) <choudharyvikas16@gmail.com>
2025-06-19 14:54:10 +0530

    [release-1.26] Ambient: In ambient index, filter configs by revision (#56477) (#56689)
    * Ambient: In ambient index, filter configs by revision (#56477)
    
    * In ambient index, filter configs by revision
    
    * release notes
    
    * Address feedback
    
    Signed-off-by: Vikas Choudhary (vikasc) <choudharyvikas16@gmail.com>
    
    * Update releasenotes/notes/56477.yaml
    
    ---------
    
    Signed-off-by: Vikas Choudhary (vikasc) <choudharyvikas16@gmail.com>
    Co-authored-by: Daniel Hawton <daniel@hawton.org>
    Signed-off-by: Vikas Choudhary (vikasc) <choudharyvikas16@gmail.com>

--- pilot/pkg/serviceregistry/kube/controller/ambient/ambientindex_serviceentry_test.go
+++ pilot/pkg/serviceregistry/kube/controller/ambient/ambientindex_serviceentry_test.go
@@ -27,7 +27,7 @@ import (
 )
 
 func TestAmbientIndexDuplicates(t *testing.T) {
-	s := newAmbientTestServer(t, testC, testNW)
+	s := newAmbientTestServer(t, testC, testNW, "")
 	s.addWorkloadEntries(t, "140.140.0.10", "name0", "sa1", map[string]string{"app": "a"})
 	s.addPods(t, "140.140.0.10", "pod0", "sa1", map[string]string{"app": "a"}, nil, true, corev1.PodRunning)
 	s.addWorkloadEntries(t, "140.140.0.10", "name1", "sa1", map[string]string{"app": "a"})
@@ -37,7 +37,7 @@ func TestAmbientIndexDuplicates(t *testing.T) {
 }
 
 func TestAmbientIndex_ServiceEntry(t *testing.T) {
-	s := newAmbientTestServer(t, testC, testNW)
+	s := newAmbientTestServer(t, testC, testNW, "")
 
 	// test code path where service entry creates a workload entry via `ServiceEntry.endpoints`
 	// and the inlined WE has a port override
@@ -417,7 +417,7 @@ func TestAmbientIndex_ServiceEntry_DisableK8SServiceSelectWorkloadEntries(t *tes
 	s := newAmbientTestServerWithFlags(t, testC, testNW, FeatureFlags{
 		DefaultAllowFromWaypoint:              features.DefaultAllowFromWaypoint,
 		EnableK8SServiceSelectWorkloadEntries: false,
-	})
+	}, "")
 
 	s.addPods(t, "140.140.0.10", "pod1", "sa1", map[string]string{"app": "a"}, nil, true, corev1.PodRunning)
 	s.assertEvent(t, s.podXdsName("pod1"))
