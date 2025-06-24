3f52daeeff19880a522e81dbd76df54f3dacffc9
nilekh <1626598+nilekhc@users.noreply.github.com>
2025-06-24 11:23:43 -0700

    test: adds integration test for CRL
    Signed-off-by: nilekh <1626598+nilekhc@users.noreply.github.com>

--- pilot/pkg/features/pilot.go
+++ pilot/pkg/features/pilot.go
@@ -282,12 +282,12 @@ var (
 	EnableLazySidecarEvaluation = env.Register("ENABLE_LAZY_SIDECAR_EVALUATION", true,
 		"If enabled, pilot will only compute sidecar resources when actually used").Get()
 
-	// EnableCACRL ToDo (nilekh): remove this feature flag once https://github.com/istio/istio/issues/56529 is closed
+	// EnableCACRL ToDo (nilekh): remove this feature flag once it's stable
 	EnableCACRL = env.Register(
 		"PILOT_ENABLE_CA_CRL",
-		false, // Default value (false = feature disabled by default)
-		"If set to true, Istio will watch for ca-crl.pem file in /etc/cacerts directory "+
-			"and will distribute crl data in each namespace in the cluster for proxies to consume. ",
+		true, // Default value (true = feature enabled by default)
+		"If set to false, Istio will not watch for the ca-crl.pem file in the /etc/cacerts directory "+
+			"and will not distribute CRL data to namespaces for proxies to consume.",
 	).Get()
 )
 
