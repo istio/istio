55ea856868cf398a3463393d655bda7704f6b6af
Istio Automation <istio-testing-bot@google.com>
2025-07-08 19:41:30 -0400

    [release-1.27] Support native sidecar by default (#56918)
    * update native sidecar default value
    
    * update webhook and injection
    
    * update golden files
    
    * release notes
    
    * fix test
    
    * update golden file
    
    * comments and updates
    
    * lint
    
    * update testcases
    
    * lint
    
    * lint
    
    * remove finding of pods by IP (#56502)
    
    * remove finding of pods by IP
    
    * fix test
    
    * add feature flag and release note
    
    * comments and updates
    
    * update testcases
    
    * update native sidecar default value and related comment updates
    
    * update feature file
    
    * update golden files
    
    * update golden files for openshift
    
    * update openshift golden files
    
    * update golden file
    
    * update default settings
    
    * lint
    
    ---------
    
    Co-authored-by: irenezhong2861 <irenezhong@microsoft.com>
    Co-authored-by: sschepens <sebastian.schepens@mercadolibre.com>

--- pilot/pkg/config/kube/gateway/deploymentcontroller.go
+++ pilot/pkg/config/kube/gateway/deploymentcontroller.go
@@ -474,6 +474,13 @@ func (d *DeploymentController) configureIstioGateway(log *istiolog.Scope, gw gat
 		}
 	}
 
+	var nativeSidecarModeEnabled bool
+	if features.EnableNativeSidecars == features.NativeSidecarModeDisabled {
+		nativeSidecarModeEnabled = false
+	} else {
+		nativeSidecarModeEnabled = true
+	}
+
 	input := TemplateInput{
 		Gateway:        &gw,
 		DeploymentName: model.GetOrDefault(gw.Annotations[annotation.GatewayNameOverride.Name], defaultName),
@@ -483,6 +490,7 @@ func (d *DeploymentController) configureIstioGateway(log *istiolog.Scope, gw gat
 
 		KubeVersion:               kube.GetVersionAsInt(d.client),
 		Revision:                  d.revision,
+		NativeSidecars:            nativeSidecarModeEnabled,
 		ServiceType:               serviceType,
 		ProxyUID:                  proxyUID,
 		ProxyGID:                  proxyGID,
@@ -915,6 +923,7 @@ type TemplateInput struct {
 	ClusterID                 string
 	KubeVersion               int
 	Revision                  string
+	NativeSidecars            bool
 	ProxyUID                  int64
 	ProxyGID                  int64
 	CompliancePolicy          string
