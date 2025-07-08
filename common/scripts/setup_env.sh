abac7dc02ac27c92b6ef7e49828a96bdd25c192c
Xeonacid <h.dwwwwww@gmail.com>
2025-07-08 15:55:22 +0800

    Support riscv64 compilation

--- common/scripts/setup_env.sh
+++ common/scripts/setup_env.sh
@@ -49,6 +49,8 @@ elif [[ ${LOCAL_ARCH} == s390x ]]; then
     TARGET_ARCH=s390x
 elif [[ ${LOCAL_ARCH} == ppc64le ]]; then
     TARGET_ARCH=ppc64le
+elif [[ ${LOCAL_ARCH} == riscv64 ]]; then
+    export TARGET_ARCH=riscv64
 else
     echo "This system's architecture, ${LOCAL_ARCH}, isn't supported"
     exit 1
