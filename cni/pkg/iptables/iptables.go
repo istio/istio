b208a0729ad0d89d9c39e93bd301507f45ef5dc8
Xscaper- <62665519+Xscaperrr@users.noreply.github.com>
2025-07-12 16:35:34 +0800

    Change host iptables rule addition from Append to Insert to ensure Istio's rules take precedence (#56414)
    * use "insert" instead of "append" to avoid the impact from existing rules on the Host
    
    (cherry picked from commit fb2e1efa9c81b973827c124fb9f9cb688ab10394)
    
    * add release-note
    
    * update testdata
    
    * Update releasenotes/notes/56414.yaml
    
    Co-authored-by: Sridhar Gaddam <sgaddam@redhat.com>
    
    * Update cni/pkg/iptables/iptables.go
    
    Co-authored-by: Keith Mattix II <keithmattix2@gmail.com>
    
    ---------
    
    Co-authored-by: Sridhar Gaddam <sgaddam@redhat.com>
    Co-authored-by: Keith Mattix II <keithmattix2@gmail.com>

--- cni/pkg/iptables/iptables.go
+++ cni/pkg/iptables/iptables.go
@@ -651,9 +651,9 @@ func (cfg *IptablesConfigurator) AppendHostRules() *builder.IptablesRuleBuilder 
 	iptablesBuilder := builder.NewIptablesRuleBuilder(ipbuildConfig(cfg.cfg))
 
 	// For easier cleanup, insert a jump into an owned chain
-	// -A POSTROUTING -p tcp -j ISTIO_POSTRT
-	iptablesBuilder.AppendRule(
-		"POSTROUTING", "nat",
+	// -I POSTROUTING 1 -p tcp -j ISTIO_POSTRT
+	iptablesBuilder.InsertRule(
+		"POSTROUTING", "nat", 1,
 		"-j", ChainHostPostrouting,
 	)
 
