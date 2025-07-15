cc22b5ecc50af0c297fce5e697cfff52166819f7
German Robayo Paz <germanrobayo33@gmail.com>
2025-07-15 22:00:50 +0000

    revert postsubmit changes

--- prow/lib.sh
+++ prow/lib.sh
@@ -112,20 +112,20 @@ function buildx-create() {
 }
 
 function build_images() {
-  # SELECT_TEST="${1}"
+  SELECT_TEST="${1}"
 
   # Build just the images needed for tests
   targets="docker.pilot docker.proxyv2 "
 
   # use ubuntu:jammy to test vms by default
   nonDistrolessTargets="docker.app docker.app_sidecar_ubuntu_noble docker.ext-authz "
-  # if [[ "${JOB_TYPE:-presubmit}" == "postsubmit" ]]; then
+  if [[ "${JOB_TYPE:-presubmit}" == "postsubmit" ]]; then
     # We run tests across all VM types only in postsubmit
-  nonDistrolessTargets+="docker.app_sidecar_ubuntu_bionic docker.app_sidecar_debian_12 docker.app_sidecar_rockylinux_9 "
-  # fi
-  # if [[ "${SELECT_TEST}" == "test.integration.ambient.kube" || "${SELECT_TEST}" == "test.integration.kube"  || "${SELECT_TEST}" == "test.integration.helm.kube" || "${JOB_TYPE:-postsubmit}" == "postsubmit" ]]; then
-  targets+="docker.ztunnel "
-  # fi
+    nonDistrolessTargets+="docker.app_sidecar_ubuntu_bionic docker.app_sidecar_debian_12 docker.app_sidecar_rockylinux_9 "
+  fi
+  if [[ "${SELECT_TEST}" == "test.integration.ambient.kube" || "${SELECT_TEST}" == "test.integration.kube"  || "${SELECT_TEST}" == "test.integration.helm.kube" || "${JOB_TYPE:-postsubmit}" == "postsubmit" ]]; then
+    targets+="docker.ztunnel "
+  fi
   targets+="docker.install-cni "
   # Integration tests are always running on local architecture (no cross compiling), so find out what that is.
   arch="linux/amd64"
@@ -166,7 +166,7 @@ function setup_kind_registry() {
       cat <<EOF | docker exec -i "${node}" cp /dev/stdin "${KIND_REGISTRY_DIR}/hosts.toml"
 [host."http://${KIND_REGISTRY_NAME}:5000"]
 EOF
-
+    
       kubectl annotate node "${node}" "kind.x-k8s.io/registry=kind-registry:${KIND_REGISTRY_PORT}" --overwrite;
     done
 
