f2f01b07d9536af187033a61f519093229551876
Yuanlin Xu <yuanlin.xu@redhat.com>
2025-07-26 12:00:39 -0400

    Update BASE_VERSION to master-2025-07-25T19-02-36
    Signed-off-by: Yuanlin Xu <yuanlin.xu@redhat.com>

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -49,7 +49,7 @@ endif
 export VERSION
 
 # Base version of Istio image to use
-BASE_VERSION ?= master-2025-07-14T12-39-09
+BASE_VERSION ?= master-2025-07-25T19-02-36
 ISTIO_BASE_REGISTRY ?= gcr.io/istio-release
 
 export GO111MODULE ?= on
