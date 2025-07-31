cae5ed2f79e01039fe81d70b2a191fbc5a3debd9
zirain <zirain2009@gmail.com>
2025-07-31 15:48:09 +0800

    [release-1.27] update deps

--- go.mod
+++ go.mod
@@ -94,8 +94,8 @@ require (
 	gopkg.in/yaml.v2 v2.4.0
 	gopkg.in/yaml.v3 v3.0.1
 	helm.sh/helm/v3 v3.17.3
-	istio.io/api v1.26.0-alpha.0.0.20250708022530-f8c0a53baaf9
-	istio.io/client-go v1.26.0-alpha.0.0.20250708023131-44cd7c661bad
+	istio.io/api v1.27.0-beta.0
+	istio.io/client-go v1.27.0-beta.0
 	k8s.io/api v0.33.2
 	k8s.io/apiextensions-apiserver v0.33.2
 	k8s.io/apimachinery v0.33.2
--- prow/release-commit.sh
+++ prow/release-commit.sh
@@ -37,7 +37,7 @@ docker run --rm --privileged "${DOCKER_HUB}/qemu-user-static" --reset -p yes
 export ISTIO_DOCKER_QEMU=true
 
 # Use a pinned version in case breaking changes are needed
-BUILDER_SHA=c9e9a66dea0545fbd8290efbbbdb1f78a2813583
+BUILDER_SHA=92c321187eaecaf39c086d7e1b6a24e18b7a0f75
 
 # Reference to the next minor version of Istio
 # This will create a version like 1.4-alpha.sha
