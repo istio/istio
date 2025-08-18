2dccb7799c8597a87f2228406074c201a6bb3516
xin.li <xin.li@daocloud.io>
2025-08-18 21:22:40 +0800

    addons: Bump addons version
    Signed-off-by: xin.li <xin.li@daocloud.io>

--- manifests/addons/gen.sh
+++ manifests/addons/gen.sh
@@ -26,7 +26,7 @@ ADDONS="${WD}/../../samples/addons"
 DASHBOARDS="${WD}/dashboards"
 mkdir -p "${ADDONS}"
 TMP=$(mktemp -d)
-LOKI_VERSION=${LOKI_VERSION:-"6.34.0"}
+LOKI_VERSION=${LOKI_VERSION:-"6.36.1"}
 GRAFANA_VERSION=${GRAFANA_VERSION:-"9.2.2"}
 
 # Set up kiali
@@ -44,7 +44,7 @@ helm3 template kiali-server \
 # Set up prometheus
 helm3 template prometheus prometheus \
   --namespace istio-system \
-  --version 27.29.0 \
+  --version 27.30.0 \
   --repo https://prometheus-community.github.io/helm-charts \
   -f "${WD}/values-prometheus.yaml" \
   > "${ADDONS}/prometheus.yaml"
