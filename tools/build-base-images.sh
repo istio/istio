097b7a03ba50098133f72379e362782cb871163c
Yuanlin Xu <yuanlin.xu@redhat.com>
2025-08-19 09:02:58 -0400

    Add expectedFiles nft in build-base-images
    Signed-off-by: Yuanlin Xu <yuanlin.xu@redhat.com>

--- tools/build-base-images.sh
+++ tools/build-base-images.sh
@@ -80,6 +80,7 @@ unexpectedFiles="$(
 )"
 expectedFiles=(
   "usr/bin/xtables-legacy-multi"
+  "usr/bin/nft"
 )
 for want in "${expectedFiles[@]}"; do
   if ! grep -q "${want}" <<<"${exefiles}"; then
