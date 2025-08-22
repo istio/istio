7527e45ee0256ece9af12b26c5344cb17d43d5b3
Istio Automation <istio-testing-bot@google.com>
2025-08-22 09:54:43 -0400

    fix TestHandlerEnqueueFunction setup (#57451)
    If the add event occurs before the tag watcher has synced, you will have
    2 reconcile events (informer and tag watcher).
    
    Co-authored-by: Michael Fraenkel <michael.fraenkel@solo.io>

--- pilot/pkg/config/kube/gateway/deploymentcontroller_test.go
+++ pilot/pkg/config/kube/gateway/deploymentcontroller_test.go
@@ -1005,7 +1005,7 @@ func TestHandlerEnqueueFunction(t *testing.T) {
 			d.queue = controllers.NewQueue("fake gateway queue",
 				controllers.WithReconciler(dummyReconcile))
 			go d.queue.Run(stop)
-			kube.WaitForCacheSync("test", stop, d.queue.HasSynced, d.gateways.HasSynced)
+			kube.WaitForCacheSync("test", stop, d.queue.HasSynced, d.gateways.HasSynced, tw.HasSynced)
 
 			switch tt.event.Event {
 			case controllers.EventAdd:
