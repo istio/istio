e91d99347e4e42d512f52d3b3aa3eb663cbe1f41
Shmuel Kallner <kallner@il.ibm.com>
2025-09-09 18:55:47 +0300

    Fix shadow service issues when an InferencePool has more than one TargetPort (#57624)
    * Added the NAme parameter to ports created for the InferencePool shadow service
    
    Signed-off-by: Shmuel Kallner <kallner@il.ibm.com>
    
    * Simply use strconv to convert int to string
    
    Signed-off-by: Shmuel Kallner <kallner@il.ibm.com>
    
    ---------
    
    Signed-off-by: Shmuel Kallner <kallner@il.ibm.com>

--- pilot/pkg/config/kube/gateway/inferencepool_collection.go
+++ pilot/pkg/config/kube/gateway/inferencepool_collection.go
@@ -509,6 +509,7 @@ func translateShadowServiceToService(existingLabels map[string]string, shadow sh
 	dummyPort := int32(54321) // Dummy port, not used for anything
 	for i, port := range shadow.targetPorts {
 		ports = append(ports, corev1.ServicePort{
+			Name:       "port" + strconv.Itoa(i),
 			Protocol:   corev1.ProtocolTCP,
 			Port:       dummyPort + int32(i),
 			TargetPort: intstr.FromInt(int(port.port)),
