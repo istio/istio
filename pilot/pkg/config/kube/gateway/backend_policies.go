e7478435954c47960af50fcff4c4c7b7004b9f54
Jacek Ewertowski <jacek.ewertowski1@gmail.com>
2025-09-10 11:22:31 +0200

    Revert unintentional comment removal
    Signed-off-by: Jacek Ewertowski <jacek.ewertowski1@gmail.com>

--- pilot/pkg/config/kube/gateway/backend_policies.go
+++ pilot/pkg/config/kube/gateway/backend_policies.go
@@ -124,6 +124,8 @@ func DestinationRuleCollection(
 	ancestorCollection := ancestors.AsCollection(append(opts.WithName("AncestorBackend"), TypedNamespacedNameIndexCollectionFunc)...)
 	tlsPolicyStatus, backendTLSPolicies := BackendTLSPolicyCollection(tlsPolicies, ancestorCollection, references, domainSuffix, opts)
 	status.RegisterStatus(c.status, tlsPolicyStatus, GetStatus)
+
+	// We need to merge these by hostname into a single DR
 	allPolicies := krt.JoinCollection([]krt.Collection[BackendPolicy]{backendTrafficPolicies, backendTLSPolicies})
 	byTargetAndHost := krt.NewIndex(allPolicies, "targetAndHost", func(o BackendPolicy) []TargetWithHost {
 		return []TargetWithHost{{Target: o.Target, Host: o.Host}}
