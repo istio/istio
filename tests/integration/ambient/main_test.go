d40827f8e28b4d6df5eb40d30c76861a24aa4891
Mitch Connors <mitchconnors@gmail.com>
2025-09-10 23:36:06 +0000

    initial work on ambient mc integ tests

--- tests/integration/ambient/main_test.go
+++ tests/integration/ambient/main_test.go
@@ -124,7 +124,7 @@ type EchoDeployments struct {
 	MockExternal echo.Instances
 
 	// WaypointProxies by
-	WaypointProxies map[string]ambient.WaypointProxy
+	WaypointProxies map[string]ambient.Waypoints
 }
 
 // TestMain defines the entrypoint for pilot tests using a standard Istio installation.
@@ -384,7 +384,7 @@ func SetupApps(t resource.Context, i istio.Instance, apps *EchoDeployments) erro
 	}
 
 	if apps.WaypointProxies == nil {
-		apps.WaypointProxies = make(map[string]ambient.WaypointProxy)
+		apps.WaypointProxies = make(map[string]ambient.Waypoints)
 	}
 
 	for _, echo := range echos {
