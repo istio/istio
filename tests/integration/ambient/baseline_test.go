05a34d92e68a5d338700e54c161de7f38a1add76
Mitch Connors <mitchconnors@gmail.com>
2025-09-29 10:44:18 -0700

    unlabel at cleanup

--- tests/integration/ambient/baseline_test.go
+++ tests/integration/ambient/baseline_test.go
@@ -2756,7 +2756,7 @@ func runAllCallsTest(t *testing.T, f func(t framework.TestContext, src echo.Inst
 			t.Cleanup(func() {
 				// cleanup services which other tests expect to be local
 				for _, app := range apps.Mesh {
-					labelService(t, app.ServiceName(), "istio.io/global", "false", app.Config().Cluster)
+					unlabelService(t, app.ServiceName(), "istio.io/global", "true", app.Config().Cluster)
 				}
 			})
 		}
@@ -3572,6 +3572,16 @@ func labelService(t framework.TestContext, svcName, k, v string, cls cluster.Clu
 	if v == "" {
 		patchData = fmt.Sprintf(`{"metadata":{"labels": {%q: null}}}`, k)
 	}
+	s := cls.Kube().CoreV1().Services(apps.Namespace.Name())
+	_, err := s.Patch(context.Background(), svcName, types.StrategicMergePatchType, []byte(patchData), patchOpts)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func unlabelService(t framework.TestContext, svcName, k, v string, cls cluster.Cluster) {
+	patchOpts := metav1.PatchOptions{}
+	patchData := fmt.Sprintf(`{"metadata":{"labels":{"$patch": "delete", %q: %q}}}`, k, v)
 	s := cls.Kube().CoreV1().Services(apps.Namespace.Name())
 	_, err := s.Patch(context.Background(), svcName, types.StrategicMergePatchType, []byte(patchData), patchOpts)
 	if err != nil {
