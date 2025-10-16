99277cd6b7f4f44b0a6628464cb7b8ba340c5553
John Mazzitelli <mazz@redhat.com>
2025-10-16 18:49:08 -0400

    bump kiali addon to v2.17.0 (#58009)

--- manifests/addons/gen.sh
+++ manifests/addons/gen.sh
@@ -33,8 +33,8 @@ GRAFANA_VERSION=${GRAFANA_VERSION:-"9.2.2"}
 {
 helm3 template kiali-server \
   --namespace istio-system \
-  --version 2.16.0 \
-  --set deployment.image_version=v2.16 \
+  --version 2.17.0 \
+  --set deployment.image_version=v2.17 \
   --include-crds \
   kiali-server \
   --repo https://kiali.org/helm-charts \
