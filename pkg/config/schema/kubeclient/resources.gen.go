4c224e21b54ccd55e5678eabf26ef5d110122ac8
John Howard <john.howard@solo.io>
2025-10-22 08:21:06 -0700

    gen

--- pkg/config/schema/kubeclient/resources.gen.go
+++ pkg/config/schema/kubeclient/resources.gen.go
@@ -6,7 +6,6 @@ import (
 	"context"
 	"fmt"
 
-	"istio.io/istio/pkg/typemap"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -17,6 +16,7 @@ import (
 	"istio.io/istio/pkg/kube/informerfactory"
 	ktypes "istio.io/istio/pkg/kube/kubetypes"
 	"istio.io/istio/pkg/ptr"
+	"istio.io/istio/pkg/typemap"
 
 	k8sioapiadmissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	k8sioapiappsv1 "k8s.io/api/apps/v1"
