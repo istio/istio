ff5d6b66e89fb66c8e647a08ad59f3b2ea9f449c
Daniel Grimm <dgrimm@redhat.com>
2025-11-07 13:58:16 +0100

    Add integration test that uses EPP mock

--- prow/lib.sh
+++ prow/lib.sh
@@ -118,7 +118,7 @@ function build_images() {
   targets="docker.pilot docker.proxyv2 "
 
   # use ubuntu:jammy to test vms by default
-  nonDistrolessTargets="docker.app docker.app_sidecar_ubuntu_noble docker.ext-authz "
+  nonDistrolessTargets="docker.app docker.app_sidecar_ubuntu_noble docker.ext-authz docker.endpoint-picker "
   if [[ "${JOB_TYPE:-presubmit}" == "postsubmit" ]]; then
     # We run tests across all VM types only in postsubmit
     nonDistrolessTargets+="docker.app_sidecar_ubuntu_bionic docker.app_sidecar_debian_12 docker.app_sidecar_rockylinux_9 "
