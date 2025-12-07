b1d0bba5f939e62604c0b8c53048ff1661c4c59f
Craig Ringer <craig.ringer@enterprisedb.com>
2025-12-08 12:30:54 +1300

    telemetry: fix stats eviction interval format
    The annotation sidecar.istio.io/statsEvictionInterval added in #57736 is
    incorrectly translated to Envoy bootstrap configuration
    stats_eviction_interval if it is set to a value >= 60s.
    
    Istio will translate an annotation value like "120s" or "2m" into an
    Envoy stats_eviction_interval option of "2m0s". But Envoy expects a
    protocol buffers Duration type that only supports a "s" suffix so
    parsing fails with an error like:
    
        error `Unable to parse JSON as proto (INVALID_ARGUMENT: invalid JSON in envoy.config.bootstrap.v3.Bootstrap @ stats_eviction_interval:   message google.protobuf.Duration, near  39:30 (offset 4137): duration must end with a single 's'): {
    
    Fix by adding a new envoyDurationConverter that emits values in the
    desired format.
    
    Also change the option to use a protocol buffers duration type for
    consistency with existing ConnectTimeout option and its
    newDurationOption helper.
    
    Fixes istio/istio#58500

--- pkg/bootstrap/option/instance.go
+++ pkg/bootstrap/option/instance.go
@@ -123,8 +123,15 @@ func newOptionOrSkipIfZero(name Name, value any) *instance {
 	return newOption(name, value)
 }
 
+// Create an option with a time.Duration-compatible stringified format (hours, minutes, seconds, etc)
 func newDurationOption(name Name, value *durationpb.Duration) *instance {
 	return newOptionOrSkipIfZero(name, value).withConvert(durationConverter(value))
+}
+
+// Create an option with a protobuf Duration-compatible stringified format
+// (seconds and nanoseconds) as accepted by Envoy's protobuf json parser
+func newEnvoyDurationOption(name Name, value *durationpb.Duration) *instance {
+	return newOptionOrSkipIfZero(name, value).withConvert(envoyDurationConverter(value))
 }
 
 func newTCPKeepaliveOption(name Name, value *networkingAPI.ConnectionPoolSettings_TCPSettings_TcpKeepalive) *instance {
