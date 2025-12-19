d6458115ee124a144e143914affc5d3c0e835eea
Jacob Delgado <j.delgado@f5.com>
2025-12-19 16:16:56 -0700

    update common-files

--- common/scripts/setup_env.sh
+++ common/scripts/setup_env.sh
@@ -77,7 +77,7 @@ fi
 TOOLS_REGISTRY_PROVIDER=${TOOLS_REGISTRY_PROVIDER:-gcr.io}
 PROJECT_ID=${PROJECT_ID:-istio-testing}
 if [[ "${IMAGE_VERSION:-}" == "" ]]; then
-  IMAGE_VERSION=master-b60a991c89f8f5d55cbe5c0952a4b84da8ef33f4
+  IMAGE_VERSION=master-de1539f06db13a41873250de1c77892314172016
 fi
 if [[ "${IMAGE_NAME:-}" == "" ]]; then
   IMAGE_NAME=build-tools
