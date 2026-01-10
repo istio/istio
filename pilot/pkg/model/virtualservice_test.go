0e949f4d72c1a7559fc81e6a1a14dc57af607a11
Rama Chavali <rama.rao@salesforce.com>
2026-01-10 13:51:11 +0530

    update test
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- pilot/pkg/model/virtualservice_test.go
+++ pilot/pkg/model/virtualservice_test.go
@@ -2382,106 +2382,15 @@ func buildHTTPService(hostname string, v visibility.Instance, ip, namespace stri
 }
 
 func BenchmarkSelectVirtualServices(b *testing.B) {
-	// Create test data with varying sizes
-	sizes := []struct {
-		name        string
-		numServices int
-		numVS       int
-	}{
-		{"small", 10, 10},
-		{"medium", 50, 100},
-		{"large", 100, 500},
-	}
-
-	for _, size := range sizes {
-		b.Run(size.name, func(b *testing.B) {
-			// Build services
-			services := make([]*Service, 0, size.numServices)
-			for i := 0; i < size.numServices; i++ {
-				hostname := fmt.Sprintf("service-%d.default.svc.cluster.local", i)
-				services = append(services, buildHTTPService(hostname, visibility.Public, wildcardIP, "default", 8080))
-			}
-
-			// Build hostsByNamespace
-			hostsByNamespace := make(map[string]hostClassification)
-			for _, svc := range services {
-				ns := svc.Attributes.Namespace
-				if _, exists := hostsByNamespace[ns]; !exists {
-					hostsByNamespace[ns] = hostClassification{exactHosts: sets.New[host.Name](), allHosts: make([]host.Name, 0)}
-				}
-				hc := hostsByNamespace[ns]
-				hc.allHosts = append(hc.allHosts, svc.Hostname)
-				hostsByNamespace[ns] = hc
-				if !svc.Hostname.IsWildCarded() {
-					hostsByNamespace[ns].exactHosts.Insert(svc.Hostname)
-				}
-			}
-
-			// Build virtual services
-			virtualServices := make([]config.Config, 0, size.numVS)
-			for i := 0; i < size.numVS; i++ {
-				// Mix of matching and non-matching hosts
-				var vsHost string
-				if i%2 == 0 && size.numServices > 0 {
-					vsHost = fmt.Sprintf("service-%d.default.svc.cluster.local", i%size.numServices)
-				} else {
-					vsHost = fmt.Sprintf("external-%d.example.com", i)
-				}
-				vs := config.Config{
-					Meta: config.Meta{
-						GroupVersionKind: gvk.VirtualService,
-						Name:             fmt.Sprintf("vs-%d", i),
-						Namespace:        "default",
-					},
-					Spec: &networking.VirtualService{
-						Hosts:    []string{vsHost},
-						Gateways: []string{"mesh"},
-						Http: []*networking.HTTPRoute{
-							{
-								Route: []*networking.HTTPRouteDestination{
-									{
-										Destination: &networking.Destination{
-											Host: "destination.default.svc.cluster.local",
-											Port: &networking.PortSelector{Number: 80},
-										},
-										Weight: 100,
-									},
-								},
-							},
-						},
-					},
-				}
-				virtualServices = append(virtualServices, vs)
-			}
-
-			index := virtualServiceIndex{
-				publicByGateway: map[string][]config.Config{
-					constants.IstioMeshGateway: virtualServices,
-				},
-				privateByNamespaceAndGateway: map[types.NamespacedName][]config.Config{},
-				exportedToNamespaceByGateway: map[types.NamespacedName][]config.Config{},
-			}
-
-			b.Run("optimized", func(b *testing.B) {
-				b.ReportAllocs()
-				for i := 0; i < b.N; i++ {
-					_ = SelectVirtualServices(index, "default", hostsByNamespace)
-				}
-			})
-		})
-	}
-}
-
-// BenchmarkSelectVirtualServicesProduction simulates production scenario where
-// SelectVirtualServices is called once per sidecar scope (per namespace/workload).
-// This mimics convertSidecarScopes calling it for many namespaces.
-func BenchmarkSelectVirtualServicesProduction(b *testing.B) {
 	scenarios := []struct {
 		name          string
 		numNamespaces int
 		numVSPerNS    int
 		numServices   int
 	}{
+		{"10ns_10vs_10svc", 1, 10, 10},
+		{"1ns_100vs_50svc", 1, 100, 50},
+		{"1ns_500vs_100svc", 1, 500, 100},
 		{"50ns_100vs_500svc", 50, 100, 500},
 		{"100ns_200vs_1000svc", 100, 200, 1000},
 		{"200ns_500vs_2000svc", 200, 500, 2000},
@@ -2503,11 +2412,10 @@ func BenchmarkSelectVirtualServicesProduction(b *testing.B) {
 				services = append(services, buildHTTPService(hostname, visibility.Public, wildcardIP, ns, 8080))
 			}
 
-			// Build hostsByNamespace for each namespace (simulating wildcard import */*)
+			// Build hostsByNamespace for each namespace
 			hostsByNamespacePerNS := make([]map[string]hostClassification, sc.numNamespaces)
 			for i, ns := range namespaces {
 				hostsByNamespace := make(map[string]hostClassification)
-				// Add wildcard namespace entry (simulates "*/*" import)
 				hc := hostClassification{
 					exactHosts: sets.New[host.Name](),
 					allHosts:   make([]host.Name, 0, sc.numServices),
@@ -2520,7 +2428,6 @@ func BenchmarkSelectVirtualServicesProduction(b *testing.B) {
 				}
 				hostsByNamespace["*"] = hc
 
-				// Also add the namespace's own hosts
 				nsHc := hostClassification{
 					exactHosts: sets.New[host.Name](),
 					allHosts:   make([]host.Name, 0),
@@ -2541,14 +2448,13 @@ func BenchmarkSelectVirtualServicesProduction(b *testing.B) {
 			virtualServices := make([]config.Config, 0, sc.numVSPerNS*sc.numNamespaces)
 			for i := 0; i < sc.numVSPerNS*sc.numNamespaces; i++ {
 				ns := namespaces[i%sc.numNamespaces]
-				// Some VS match services, some don't
 				var vsHost string
 				if i%3 == 0 && sc.numServices > 0 {
 					svcIdx := i % sc.numServices
 					svcNs := namespaces[svcIdx%sc.numNamespaces]
 					vsHost = fmt.Sprintf("service-%d.%s.svc.cluster.local", svcIdx, svcNs)
 				} else if i%3 == 1 {
-					vsHost = fmt.Sprintf("*.%s.svc.cluster.local", ns) // wildcard
+					vsHost = fmt.Sprintf("*.%s.svc.cluster.local", ns)
 				} else {
 					vsHost = fmt.Sprintf("external-%d.example.com", i)
 				}
@@ -2591,7 +2497,6 @@ func BenchmarkSelectVirtualServicesProduction(b *testing.B) {
 			b.ResetTimer()
 
 			for i := 0; i < b.N; i++ {
-				// Simulate calling SelectVirtualServices for each namespace (like convertSidecarScopes does)
 				for j, ns := range namespaces {
 					_ = SelectVirtualServices(index, ns, hostsByNamespacePerNS[j])
 				}
