c019c9fc482535b4c76f0cb07c15d50488131e11
Christian Rohmann <christian.rohmann@inovex.de>
2026-01-10 17:42:06 +0100

    move sidecar annotation `statsCompression` to proxyConfig parameter
    * remove `sidecar.istio.io/statsCompression` annotation
    * add option `statsCompression` (default true)
    
    Compressing metrics reduces the size of the responses by 90%.
    Since modern CPUs handle compression extremely efficiently, there
    is virtually no downside in allowing metrics scrapers to ask for
    compressed responses. In case no compression is wanted, a client
    like Prometheus can just request with an appropriate `Accept-Encoding`
    header (or none).
    
    Fixes: #48051
    xref: #30987 #47997
    Signed-off-by: Christian Rohmann <christian.rohmann@inovex.de>
    
    fixup

--- pkg/bootstrap/instance_test.go
+++ pkg/bootstrap/instance_test.go
@@ -203,30 +203,6 @@ func TestGolden(t *testing.T) {
 			},
 		},
 		{
-			base: "stats_compression_gzip",
-			annotations: map[string]string{
-				"sidecar.istio.io/statsCompression": "gzip",
-			},
-		},
-		{
-			base: "stats_compression_brotli",
-			annotations: map[string]string{
-				"sidecar.istio.io/statsCompression": "brotli",
-			},
-		},
-		{
-			base: "stats_compression_zstd",
-			annotations: map[string]string{
-				"sidecar.istio.io/statsCompression": "zstd",
-			},
-		},
-		{
-			base: "stats_compression_unknown",
-			annotations: map[string]string{
-				"sidecar.istio.io/statsCompression": "unknown",
-			},
-		},
-		{
 			base: "stats_flush_interval",
 			annotations: map[string]string{
 				"sidecar.istio.io/statsFlushInterval": "10s",
@@ -265,6 +241,9 @@ func TestGolden(t *testing.T) {
 			envVars: map[string]string{
 				GlobalDownstreamMaxConnections: "20000",
 			},
+		},
+		{
+			base: "stats_compression",
 		},
 	}
 
