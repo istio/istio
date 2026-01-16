aeaca86b9d833e22550b64db66037cd87cc942bf
Gustavo Meira <grnmeira@users.noreply.github.com>
2026-01-16 22:58:21 +0000

    adding EW gateways for remote EPs in ingress gateway (#58777)
    * adding EW gateways for remote EPs in ingress gateway
    
    * suggestions from PR
    
    * fix in unit tests and suggestions from PR
    
    * Update pilot/pkg/networking/core/cluster.go
    
    Co-authored-by: Keith Mattix II <keithmattix2@gmail.com>
    
    * more suggestions from PR
    
    * fixing unit tests: back with the legacy flag
    
    * adding comment
    
    * more suggestions from PR
    
    * Update pilot/pkg/model/context.go
    
    Co-authored-by: Keith Mattix II <keithmattix2@gmail.com>
    
    * fixing conditions for EnableHBONESend
    
    * fixing default ep generation
    
    * fixing logic, again
    
    ---------
    
    Co-authored-by: Keith Mattix II <keithmattix2@gmail.com>

--- pilot/pkg/xds/endpoints/endpoint_builder.go
+++ pilot/pkg/xds/endpoints/endpoint_builder.go
@@ -946,16 +946,6 @@ func isEastWestGateway(node *model.Proxy) bool {
 	return isManagedGateway && controller == constants.ManagedGatewayEastWestControllerLabel
 }
 
-// Duplicated from networkin/core/waypoint to avoid circular dependency
-func isWaypointProxy(node *model.Proxy) bool {
-	if node == nil || node.Type != model.Waypoint {
-		return false
-	}
-	controller, isManagedGateway := node.Labels[label.GatewayManaged.Name]
-
-	return isManagedGateway && controller == constants.ManagedGatewayMeshControllerLabel
-}
-
 func isSidecarProxy(node *model.Proxy) bool {
 	return node != nil && node.Type == model.SidecarProxy
 }
