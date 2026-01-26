6cf5458796803c95746aa570f2faf66d58ddc0da
John Mazzitelli <mazz@redhat.com>
2026-01-26 15:41:44 -0500

    bump kiali addon to v2.21.0 Kiali v2.21.0 is the appropriate version for the next Istio release. This PR bumps up the Kiali addon to that version.

--- manifests/addons/gen.sh
+++ manifests/addons/gen.sh
@@ -33,8 +33,8 @@ GRAFANA_VERSION=${GRAFANA_VERSION:-"9.2.2"}
 {
 helm template kiali-server \
   --namespace istio-system \
-  --version 2.20.0 \
-  --set deployment.image_version=v2.20 \
+  --version 2.21.0 \
+  --set deployment.image_version=v2.21 \
   --include-crds \
   kiali-server \
   --repo https://kiali.org/helm-charts \
