52fb1822e1561a3c3ca6007cc145005b1d84afc2
Jaiveer Katariya <jkatariya@microsoft.com>
2026-01-27 20:32:38 +0000

    pilot ignore resources

--- pkg/test/framework/components/istio/kube.go
+++ pkg/test/framework/components/istio/kube.go
@@ -643,6 +643,11 @@ func commonInstallArgs(ctx resource.Context, cfg Config, c cluster.Cluster, defa
 		args.AppendSet("values.pilot.env.PILOT_GATEWAY_API_DEFAULT_GATEWAYCLASS_NAME", cfg.GatewayClassName)
 	}
 
+	// When running in meshless mode, tell Pilot to ignore all Istio CRDs and only process Gateway API resources.
+	if ctx.Settings().Meshless {
+		args.AppendSet("values.pilot.env.PILOT_IGNORE_RESOURCES", "*.istio.io")
+	}
+
 	return args
 }
 
