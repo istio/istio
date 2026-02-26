b06336b79c22a2a0eb0f4e4fea53d4ed6968af2c
David Jumani <dj.davidjumani1994@gmail.com>
2026-02-26 14:52:24 -0500

    unset mapper

--- tests/integration/ambient/gateway_conformance_test.go
+++ tests/integration/ambient/gateway_conformance_test.go
@@ -139,10 +139,10 @@ func TestGatewayConformance(t *testing.T) {
 				}
 			}
 
-			mapper, _ := gatewayConformanceInputs.Client.UtilFactory().ToRESTMapper()
+			// mapper, _ := gatewayConformanceInputs.Client.UtilFactory().ToRESTMapper()
 			clientOptions := client.Options{
 				Scheme: kube.IstioScheme,
-				Mapper: mapper,
+				// Mapper: mapper,
 			}
 			c, err := client.New(gatewayConformanceInputs.Client.RESTConfig(), clientOptions)
 			if err != nil {
