132653b8a557adbcde25a104df32ffa66710849d
Mike Morris <mikemorris@users.noreply.github.com>
2026-03-05 12:57:55 -0500

    Update skipped tests in Gateway API conformance test

--- tests/integration/ambient/gateway_conformance_test.go
+++ tests/integration/ambient/gateway_conformance_test.go
@@ -70,7 +70,7 @@ var skippedTests = map[string]string{
 	"BackendTLSPolicyConflictResolution": "https://github.com/istio/istio/issues/57817",
 
 	// The following tests were added in v1.5.0
-	"GatewayBackendClientCertificateFeature":                     "TODO",
+	"GatewayTLSBackendClientCertificate":                         "TODO",
 	"GatewayFrontendInvalidDefaultClientCertificateValidation":   "TODO",
 	"GatewayInvalidTLSBackendConfiguration":                      "TODO",
 	"GatewayTLSBackendClientCertificate":                         "TODO",
