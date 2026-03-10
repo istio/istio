41c2ba1d32845548c743ac4029d690ef7149bf78
sschepens <sebastian.schepens@mercadolibre.com>
2026-03-10 09:55:55 -0300

    extend delta awareness in cds

--- pilot/pkg/xds/cds.go
+++ pilot/pkg/xds/cds.go
@@ -29,18 +29,15 @@ type CdsGenerator struct {
 
 var _ model.XdsDeltaResourceGenerator = &CdsGenerator{}
 
-// Map of all configs that do not impact CDS
-var skippedCdsConfigs = sets.New(
-	kind.Gateway,
-	kind.WorkloadEntry,
-	kind.WorkloadGroup,
-	kind.AuthorizationPolicy,
-	kind.RequestAuthentication,
-	kind.Secret,
-	kind.Telemetry,
-	kind.WasmPlugin,
-	kind.ProxyConfig,
-	kind.DNSName,
+// Map of all configs that impact CDS
+// CDS is also affected by other global resources, but these always trigger a Forced push.
+var cdsAffectingConfigs = sets.New(
+	kind.ServiceEntry,
+	kind.DestinationRule,
+	kind.PeerAuthentication,
+	kind.VirtualService,
+	kind.EnvoyFilter,
+	kind.Sidecar,
 )
 
 // Map all configs that impact CDS for gateways when `PILOT_FILTER_GATEWAY_CLUSTER_CONFIG = true`.
@@ -92,7 +89,7 @@ func cdsNeedsPush(req *model.PushRequest, proxy *model.Proxy) (*model.PushReques
 			}
 		}
 
-		if _, f := skippedCdsConfigs[config.Kind]; !f {
+		if cdsAffectingConfigs.Contains(config.Kind) {
 			relevantUpdates.Insert(config)
 		} else {
 			// we filtered a config
