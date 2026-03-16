4d695b8c302581fa4070728209b0b4fef44ad045
Petr McAllister <petr.mcallister@gmail.com>
2026-03-16 09:34:20 -0700

    fix webhook failurePolicy conflict on helm upgrade (with SSA) (#59367)
    * fix server-side apply error during upgrade
    
    Signed-off-by: Petr McAllister <petr.mcallister@gmail.com>
    
    * release notes rewording
    
    Signed-off-by: Petr McAllister <petr.mcallister@gmail.com>
    
    * add control via values.yaml
    
    Signed-off-by: Petr McAllister <petr.mcallister@gmail.com>
    
    ---------
    
    Signed-off-by: Petr McAllister <petr.mcallister@gmail.com>

--- operator/pkg/helm/helm_test.go
+++ operator/pkg/helm/helm_test.go
@@ -15,18 +15,28 @@
 package helm
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"sort"
+	"strconv"
 	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/require"
+	"helm.sh/helm/v3/pkg/chartutil"
+	"helm.sh/helm/v3/pkg/engine"
 	"sigs.k8s.io/yaml"
 
+	"istio.io/istio/istioctl/pkg/install/k8sversion"
+	"istio.io/istio/manifests"
+	"istio.io/istio/operator/pkg/manifest"
 	operatortest "istio.io/istio/operator/pkg/test"
+	"istio.io/istio/operator/pkg/util"
 	"istio.io/istio/operator/pkg/values"
 	tutil "istio.io/istio/pilot/test/util"
+	"istio.io/istio/pkg/test/util/yml"
 )
 
 type testCase struct {
@@ -35,6 +45,58 @@ type testCase struct {
 	namespace   string
 	chartName   string
 	diffSelect  string
+	isUpgrade   bool
+}
+
+func renderWithOptions(releaseName, namespace, directory string, iop values.Map, isUpgrade bool) ([]manifest.Manifest, util.Errors, error) {
+	vals, _ := iop.GetPathMap("spec.values")
+	installPackagePath := iop.GetPathString("spec.installPackagePath")
+	f := manifests.BuiltinOrDir(installPackagePath)
+	path := pathJoin("charts", directory)
+	chrt, err := loadChart(f, path)
+	if err != nil {
+		return nil, nil, fmt.Errorf("load chart: %v", err)
+	}
+
+	options := chartutil.ReleaseOptions{
+		Name:      releaseName,
+		Namespace: namespace,
+		IsUpgrade: isUpgrade,
+		IsInstall: !isUpgrade,
+	}
+
+	caps := *chartutil.DefaultCapabilities
+	operatorVersion, _ := chartutil.ParseKubeVersion("1." + strconv.Itoa(k8sversion.MinK8SVersion) + ".0")
+	caps.KubeVersion = *operatorVersion
+
+	helmVals, err := chartutil.ToRenderValues(chrt, vals, options, &caps)
+	if err != nil {
+		return nil, nil, fmt.Errorf("converting values: %v", err)
+	}
+
+	files, err := engine.Render(chrt, helmVals)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var warnings Warnings
+	keys := make([]string, 0, len(files))
+	for k, v := range files {
+		if strings.HasSuffix(k, NotesFileNameSuffix) {
+			warnings = extractWarnings(v)
+			continue
+		}
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	results := make([]string, 0, len(keys))
+	for _, k := range keys {
+		results = append(results, yml.SplitString(files[k])...)
+	}
+
+	mfs, err := manifest.Parse(results)
+	return mfs, warnings, err
 }
 
 func TestRender(t *testing.T) {
@@ -137,6 +199,28 @@ func TestRender(t *testing.T) {
 			chartName:   "gateway",
 			diffSelect:  "PodDisruptionBudget:*:istio-ingress",
 		},
+		{
+			desc:        "istiod-webhook-install",
+			releaseName: "istiod",
+			namespace:   "istio-system",
+			chartName:   "istio-control/istio-discovery",
+			diffSelect:  "ValidatingWebhookConfiguration:*:istio-validator-istio-system",
+		},
+		{
+			desc:        "istiod-webhook-upgrade",
+			releaseName: "istiod",
+			namespace:   "istio-system",
+			chartName:   "istio-control/istio-discovery",
+			diffSelect:  "ValidatingWebhookConfiguration:*:istio-validator-istio-system",
+			isUpgrade:   true,
+		},
+		{
+			desc:        "istiod-webhook-failure-policy",
+			releaseName: "istiod",
+			namespace:   "istio-system",
+			chartName:   "istio-control/istio-discovery",
+			diffSelect:  "ValidatingWebhookConfiguration:*:istio-validator-istio-system",
+		},
 	}
 
 	for _, tc := range cases {
@@ -151,7 +235,7 @@ func TestRender(t *testing.T) {
 			if err := yaml.Unmarshal(data, &vals); err != nil {
 				t.Fatalf("error %s: %s", err, inPath)
 			}
-			m, _, err := Render(tc.releaseName, tc.namespace, tc.chartName, vals, nil)
+			m, _, err := renderWithOptions(tc.releaseName, tc.namespace, tc.chartName, vals, tc.isUpgrade)
 			require.NoError(t, err)
 
 			b := strings.Builder{}
