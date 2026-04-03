a2c66900683e2ddd0357030de1f27b9563c958ee
Michael Fraenkel <michael.fraenkel@solo.io>
2026-04-02 18:45:07 -0600

    Gateways render once the push context is loaded (#59711)
    Do not attempt to render when the proxy configs have not be initialized.
    The ProxyConfig will bounce between {} and some content. This will cause
    any existing Gateway to have its deployment re-configured twice.

--- pilot/pkg/config/kube/gateway/deploymentcontroller_test.go
+++ pilot/pkg/config/kube/gateway/deploymentcontroller_test.go
@@ -627,6 +627,7 @@ metadata:
 			stop := test.NewStop(t)
 			env := newTestEnv()
 			env.PushContext().ProxyConfigs = tt.pcs
+			env.PushContext().InitDone.Store(true)
 			tw := revisions.NewTagWatcher(client, "", "istio-system")
 			go tw.Run(stop)
 			d := NewDeploymentController(client, cluster.ID(features.ClusterName), env, testInjectionConfig(t, tt.values), func(fn func()) {
@@ -705,6 +706,7 @@ func TestMeshGatewayReconciliation(t *testing.T) {
 
 	stop := test.NewStop(t)
 	gws := clienttest.Wrap(t, d.gateways)
+	env.PushContext().InitDone.Store(true)
 	go tw.Run(stop)
 	go d.Run(stop)
 	c.RunAndWait(stop)
@@ -787,6 +789,7 @@ func TestVersionManagement(t *testing.T) {
 	}
 	stop := test.NewStop(t)
 	gws := clienttest.Wrap(t, d.gateways)
+	env.PushContext().InitDone.Store(true)
 	go tw.Run(stop)
 	go d.Run(stop)
 	c.RunAndWait(stop)
