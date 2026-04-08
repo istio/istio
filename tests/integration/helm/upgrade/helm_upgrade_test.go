a50a640d401ecad106a2b7ab5d9edeab1d9a60a5
Petr McAllister <petr.mcallister@gmail.com>
2026-04-08 08:28:04 -0700

    address PR comment
    Signed-off-by: Petr McAllister <petr.mcallister@gmail.com>

--- tests/integration/helm/upgrade/helm_upgrade_test.go
+++ tests/integration/helm/upgrade/helm_upgrade_test.go
@@ -118,7 +118,7 @@ func TestZtunnelFromPreviousMinorRelease(t *testing.T) {
 func TestInPlaceUpgradeWebhookFailurePolicy(t *testing.T) {
 	framework.
 		NewTest(t).
-		Run(performInPlaceUpgradeWithFailurePolicyFunc(previousSupportedVersion))
+		Run(performInPlaceUpgradeWithFailurePolicy(previousSupportedVersion))
 }
 
 func TestAmbientStableRevisionLabelsGatewayStatus(t *testing.T) {
