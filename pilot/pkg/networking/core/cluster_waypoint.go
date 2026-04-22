a3b8764ca9cfb56d837375ca3d8d553f3f359551
Yun Huang Yong <yun.huang.yong@octopus.com>
2026-04-22 22:23:42 +1000

    set HBONE window sizes using feature flags (env vars)

--- pilot/pkg/networking/core/cluster_waypoint.go
+++ pilot/pkg/networking/core/cluster_waypoint.go
@@ -557,8 +557,8 @@ func (cb *ClusterBuilder) h2connectUpgrade() map[string]*anypb.Any {
 				ProtocolConfig: &http.HttpProtocolOptions_ExplicitHttpConfig_Http2ProtocolOptions{
 					Http2ProtocolOptions: &core.Http2ProtocolOptions{
 						AllowConnect:                true,
-						InitialStreamWindowSize:     cb.req.Push.Mesh.HboneInitialStreamWindowSize,
-						InitialConnectionWindowSize: cb.req.Push.Mesh.HboneInitialConnectionWindowSize,
+						InitialStreamWindowSize:     features.HBONEInitialStreamWindowSize,
+						InitialConnectionWindowSize: features.HBONEInitialConnectionWindowSize,
 					},
 				},
 			}},
@@ -586,8 +586,8 @@ func (cb *ClusterBuilder) h2connectUpgradeWithNoPooling() map[string]*anypb.Any 
 				ProtocolConfig: &http.HttpProtocolOptions_ExplicitHttpConfig_Http2ProtocolOptions{
 					Http2ProtocolOptions: &core.Http2ProtocolOptions{
 						AllowConnect:                true,
-						InitialStreamWindowSize:     cb.req.Push.Mesh.HboneInitialStreamWindowSize,
-						InitialConnectionWindowSize: cb.req.Push.Mesh.HboneInitialConnectionWindowSize,
+						InitialStreamWindowSize:     features.HBONEInitialStreamWindowSize,
+						InitialConnectionWindowSize: features.HBONEInitialConnectionWindowSize,
 					},
 				},
 			}},
