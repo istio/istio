26a793e9d5501877ac70c76df5f20f6d34f611d1
Francisco Herrera <fjglira@gmail.com>
2026-04-22 20:40:18 +0200

    test: add memory null and mixed resource test cases for injection (#59134) (#59947)
    Adding extra test scenarios to increase coverage after merging #58824
    
    Signed-off-by: Francisco Herrera <fjglira@gmail.com>

--- pkg/kube/inject/inject_test.go
+++ pkg/kube/inject/inject_test.go
@@ -564,6 +564,16 @@ func TestInjection(t *testing.T) {
 			inFilePath: "proxy-resources-zero.iop.yaml",
 		},
 		{
+			in:         "proxy-resources-memory-null.yaml",
+			want:       "proxy-resources-memory-null.yaml.injected",
+			inFilePath: "proxy-resources-memory-null.iop.yaml",
+		},
+		{
+			in:         "proxy-resources-mixed-null.yaml",
+			want:       "proxy-resources-mixed-null.yaml.injected",
+			inFilePath: "proxy-resources-mixed-null.iop.yaml",
+		},
+		{
 			in:         "sidecar-spire.yaml",
 			want:       "sidecar-spire.yaml.injected",
 			inFilePath: "spire-template.iop.yaml",
