0753e142c1c0744c784d49324ad1f5c960930220
MohammadHosein Bahmani <mhossein.bahmani@divar.ir>
2026-04-27 19:09:26 +0330

    Replace cmp.Or with seperate if statements

--- pilot/pkg/model/push_context.go
+++ pilot/pkg/model/push_context.go
@@ -2032,12 +2032,14 @@ func sortConfigBySelectorAndCreationTime(configs []config.Config) []config.Confi
 			return +1
 		}
 
-		// ordered by creation time, name, namespace
-		return cmp.Or(
-            a.CreationTimestamp.Compare(b.CreationTimestamp),
-            cmp.Compare(a.Name, b.Name),
-            cmp.Compare(a.Namespace, b.Namespace),
-        )
+		// If priority is the same or neither has priority, fallback to creation time ordering
+		if r := a.CreationTimestamp.Compare(b.CreationTimestamp); r != 0 {
+			return r // -1 means i is older than j, so it should be before than j, so return -1.
+		}
+		if r := cmp.Compare(a.Name, b.Name); r != 0 {
+			return r
+		}
+		return cmp.Compare(a.Namespace, b.Namespace)
 	})
 	return configs
 }
