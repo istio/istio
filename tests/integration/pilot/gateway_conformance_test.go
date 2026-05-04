ad2443f8538ab400739533d3f8c38ed26d21292c
jgreeer <jacksongreer@microsoft.com>
2026-05-04 21:28:17 +0000

    add listnerSet skip back to pilot

--- tests/integration/pilot/gateway_conformance_test.go
+++ tests/integration/pilot/gateway_conformance_test.go
@@ -83,6 +83,10 @@ var skippedTests = map[string]string{
 	"HTTPRouteCORS":                                   "TODO",
 	"HTTPRouteHTTPSListenerDetectMisdirectedRequests": "TODO",
 
+	"ListenerSetHostnameConflict": "TODO",
+	"ListenerSetProtocolConflict": "TODO",
+	"ListenerSetReferenceGrant":   "TODO",
+
 	"MeshHTTPRoute303Redirect": "TODO",
 	"MeshHTTPRoute307Redirect": "TODO",
 	"MeshHTTPRoute308Redirect": "TODO",
