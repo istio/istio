564316e9f0a4f8f005a8230198c8ba7a7090103a
Istio Automation <istio-testing-bot@google.com>
2026-06-08 04:01:28 -0700

    Automator: update go-control-plane in istio/istio@master (#60493)

--- pkg/config/xds/filter_types.gen.go
+++ pkg/config/xds/filter_types.gen.go
@@ -345,6 +345,7 @@ import (
 	_ "github.com/envoyproxy/go-control-plane/envoy/extensions/retry/priority/previous_priorities/v3"
 	_ "github.com/envoyproxy/go-control-plane/envoy/extensions/router/cluster_specifiers/lua/v3"
 	_ "github.com/envoyproxy/go-control-plane/envoy/extensions/router/cluster_specifiers/matcher/v3"
+	_ "github.com/envoyproxy/go-control-plane/envoy/extensions/stat_sinks/dynamic_modules/v3"
 	_ "github.com/envoyproxy/go-control-plane/envoy/extensions/stat_sinks/graphite_statsd/v3"
 	_ "github.com/envoyproxy/go-control-plane/envoy/extensions/stat_sinks/open_telemetry/v3"
 	_ "github.com/envoyproxy/go-control-plane/envoy/extensions/stat_sinks/wasm/v3"
