cba4502808839db7d1431d5558ab9c7ce1d721b7
Eric Bishop <ericbish.dev@gmail.com>
2026-07-10 17:00:13 +0000

    remove more skips
    Signed-off-by: Eric Bishop <ericbish.dev@gmail.com>

--- tests/integration/pilot/agentgateway/gateway_conformance_test.go
+++ tests/integration/pilot/agentgateway/gateway_conformance_test.go
@@ -74,17 +74,12 @@ var skippedTests = map[string]string{
 	"GatewayFrontendClientCertificateValidation":                 "TODO",
 	"GatewayInvalidFrontendClientCertificateValidation":          "TODO",
 
-	"HTTPRouteCORS": "TODO",
 	"HTTPRouteHTTPSListenerDetectMisdirectedRequests": "TODO",
 
 	"ListenerSetReferenceGrant": "TODO",
 
 	// The following tests were modified between v1.4.0 && v1.5.0
 	"BackendTLSPolicy": "TODO",
-
-	"GatewayWithAttachedRoutesWithPort8080": "TODO",
-
-	"MeshGRPCRouteWeight": "TODO",
 
 	// The following tests were added in v1.5.0
 	"TLSRouteTerminateSimpleSameNamespace":  "TODO",
