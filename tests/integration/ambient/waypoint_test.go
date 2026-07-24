9fb1d46b770ee7d412a59e97f0c7761a650794aa
Mads Jensen <atombrella@users.noreply.github.com>
2026-07-24 21:36:51 +0200

    Update to networking.istio.io/v1 where possible in tests.
    These all use v1 in the documentation:
    WorkloadEntry, WorkloadGroup, ServiceEntry,
     DestinationRule, VirtualService, Gateway

--- tests/integration/ambient/waypoint_test.go
+++ tests/integration/ambient/waypoint_test.go
@@ -974,7 +974,7 @@ spec:
       protocol: HTTP
     hosts: ["*"]
 ---
-apiVersion: networking.istio.io/v1alpha3
+apiVersion: networking.istio.io/v1
 kind: VirtualService
 metadata:
   name: route
@@ -1017,7 +1017,7 @@ spec:
 			}
 			t.ConfigIstio().Eval(apps.Namespace.Name(), map[string]string{
 				"Destination": apps.ServiceAddressedWaypoint.ServiceName(),
-			}, `apiVersion: networking.istio.io/v1alpha3
+			}, `apiVersion: networking.istio.io/v1
 kind: Gateway
 metadata:
   name: gateway
@@ -1031,7 +1031,7 @@ spec:
       protocol: HTTP
     hosts: ["*"]
 ---
-apiVersion: networking.istio.io/v1alpha3
+apiVersion: networking.istio.io/v1
 kind: VirtualService
 metadata:
   name: route
