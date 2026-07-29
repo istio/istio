24b8d9eff391ab16a8e3f1cf9945e7e4d7991e65
istio-testing <istio-testing-bot@google.com>
2026-07-29 14:48:41 +0000

    Automator: update common-files@release-1.31 in istio/istio@release-1.31

--- common/Makefile.common.mk
+++ common/Makefile.common.mk
@@ -92,7 +92,7 @@ mirror-licenses: mod-download-go
 	@license-lint --mirror
 
 TMP := $(shell mktemp -d -u)
-UPDATE_BRANCH ?= "release-1.31"
+UPDATE_BRANCH ?= "master"
 
 BUILD_TOOLS_ORG ?= "istio"
 
