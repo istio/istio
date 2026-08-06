1196c1630f9644caaf7be216346f232d1994ae2a
Jacek Ewertowski <jacek.ewertowski1@gmail.com>
2026-08-06 11:40:13 +0200

    Update deps branch to release-1.31
    Update `UPDATE_BRANCH` in update_deps.sh and update_ztunnel.sh
    to release-1.31 and run update_deps.sh.
    
    Step 6 in https://github.com/istio/istio/issues/60658.
    
    Signed-off-by: Jacek Ewertowski <jacek.ewertowski1@gmail.com>

--- prow/release-commit.sh
+++ prow/release-commit.sh
@@ -39,7 +39,7 @@ docker run --rm --privileged "${DOCKER_HUB}/qemu-user-static" --reset -p yes
 export ISTIO_DOCKER_QEMU=true
 
 # Use a pinned version in case breaking changes are needed
-BUILDER_SHA=2241d0a237035dac7879e2d7f08027a12d855a49
+BUILDER_SHA=7ec6fdf3245617e1cf49be7916fda80aa29d7b5b
 
 # Reference to the next minor version of Istio
 # This will create a version like 1.30.0-alpha.<sha>
