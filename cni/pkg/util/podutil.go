d53ce9ebb10753ca48cf4e3e1c279ce7ded2881c
Ian Rudie <ian.rudie@solo.io>
2026-08-10 01:38:06 -0400

    CNI restore hostnet filter (#61168)
    * add host network filter back to our matching logic
    
    Signed-off-by: Ian Rudie <ian.rudie@solo.io>
    
    * testing
    
    Signed-off-by: Ian Rudie <ian.rudie@solo.io>
    
    * wording tweaks
    
    Signed-off-by: Ian Rudie <ian.rudie@solo.io>
    
    * releasenote
    
    Signed-off-by: Ian Rudie <ian.rudie@solo.io>
    
    ---------
    
    Signed-off-by: Ian Rudie <ian.rudie@solo.io>

--- cni/pkg/util/podutil.go
+++ cni/pkg/util/podutil.go
@@ -52,7 +52,7 @@ var annotationRemovePatch = []byte(fmt.Sprintf(
 //
 // That is, have we annotated it after successfully setting up iptables rules AND sending it to a node proxy instance.
 //
-// If you just want to know if the pod _should be_ configured for traffic redirection, see PodRedirectionEnabled
+// If you just want to know if the pod _should be_ configured for traffic redirection, see CompiledEnablementSelectors.Matches
 func PodFullyEnrolled(pod *corev1.Pod) bool {
 	if pod != nil {
 		return pod.GetAnnotations()[annotation.AmbientRedirection.Name] == constants.AmbientRedirectionEnabled
@@ -68,7 +68,7 @@ func PodFullyEnrolled(pod *corev1.Pod) bool {
 //
 // Pods like this still need to undergo the removal process (to potentially undo the redirection).
 //
-// If you just want to know if the pod _should be_ configured for traffic redirection, see PodRedirectionEnabled
+// If you just want to know if the pod _should be_ configured for traffic redirection, see CompiledEnablementSelectors.Matches
 func PodPartiallyEnrolled(pod *corev1.Pod) bool {
 	if pod != nil {
 		return pod.GetAnnotations()[annotation.AmbientRedirection.Name] == constants.AmbientRedirectionPending
