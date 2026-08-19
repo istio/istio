423f108b5459fd9fd93929e2004861f2f14e3492
Sebastian Schepens <sebastian.schepens@mercadolibre.com>
2026-08-19 04:34:32 -0300

    [release-1.31] cherry-pick Ambient multi cluster fixes (#61305)
    * ambient: fix global nodes per-cluster collection leaking (#61255)
    
    * ambient: fix global nodes per-cluster collection leaking
    
    * add release note
    
    * ambient: make ambient index sync when remote clusters sync (#61296)
    
    * ambient: make ambient index sync when remote clusters sync
    
    * de-flake ambient multicluster leak test
    
    * add release note
    
    * fixes
    
    * avoid dropping cluster collections during cluster updates
    
    * use has synced
    
    * lint
    
    * skip timedout clusters
    
    * rename
    
    * triggerRecomputeOnSync should wait for clusters to be fully initially synced
    
    * slight improvement
    
    * comments
    
    * also trigger recomputes for timedout clusters to avoid losing them

--- pilot/pkg/serviceregistry/ambient/ambientindex_test.go
+++ pilot/pkg/serviceregistry/ambient/ambientindex_test.go
@@ -1583,7 +1583,7 @@ func TestDefaultAllowWaypointPolicy(t *testing.T) {
 			s := newAmbientTestServerWithFlags(t, testC, testNW, FeatureFlags{
 				DefaultAllowFromWaypoint:              true,
 				EnableK8SServiceSelectWorkloadEntries: features.EnableK8SServiceSelectWorkloadEntries,
-			}, "")
+			}, "", true)
 			setupPolicyTest(t, s)
 
 			t.Run("policy with service accounts", func(t *testing.T) {
@@ -2308,7 +2308,7 @@ func newAmbientTestServer(t *testing.T, clusterID cluster.ID, networkID network.
 	return newAmbientTestServerWithFlags(t, clusterID, networkID, FeatureFlags{
 		DefaultAllowFromWaypoint:              features.DefaultAllowFromWaypoint,
 		EnableK8SServiceSelectWorkloadEntries: features.EnableK8SServiceSelectWorkloadEntries,
-	}, revision)
+	}, revision, true)
 }
 
 func newAmbientTestServerFromOptions(t *testing.T, networkID network.ID, options Options, runClient bool) *ambientTestServer {
@@ -2412,7 +2412,14 @@ func newAmbientTestServerFromOptions(t *testing.T, networkID network.ID, options
 	return a
 }
 
-func newAmbientTestServerWithFlags(t *testing.T, clusterID cluster.ID, networkID network.ID, flags FeatureFlags, revision string) *ambientTestServer {
+func newAmbientTestServerWithFlags(
+	t *testing.T,
+	clusterID cluster.ID,
+	networkID network.ID,
+	flags FeatureFlags,
+	revision string,
+	runClient bool,
+) *ambientTestServer {
 	up := xdsfake.NewFakeXDS()
 	up.SplitEvents = true
 	cl := kubeclient.NewFakeClient()
@@ -2474,7 +2481,7 @@ func newAmbientTestServerWithFlags(t *testing.T, clusterID cluster.ID, networkID
 		}
 	}
 
-	return newAmbientTestServerFromOptions(t, networkID, o, true)
+	return newAmbientTestServerFromOptions(t, networkID, o, runClient)
 }
 
 func dumpOnFailure(t *testing.T, debugger *krt.DebugHandler) {
