a826f39d78abc50f1c217d4722d6b0b1a798150d
Jackson Greer <jacksongreer@microsoft.com>
2026-08-28 16:55:18 -0400

    re-enable mesh httproute 307 redirect (#61462)

--- tests/integration/ambient/gateway_conformance_test.go
+++ tests/integration/ambient/gateway_conformance_test.go
@@ -69,9 +69,6 @@ var skippedTests = map[string]string{
 	// The following tests were added in v1.5.0
 	"HTTPRouteHTTPSListenerDetectMisdirectedRequests": "TODO",
 
-	// Fixed upstream, waiting for new gateway api release to pick up fix
-	"MeshHTTPRoute307Redirect": "TODO",
-
 	// The following tests were added in v1.6.0
 	"GatewayListenerUnsupportedProtocol": "TODO",
 }
