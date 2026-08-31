bea4dbdb385682e68819caa24abff0cb42e8f74a
Prabhjot <psb@tetrate.io>
2026-08-31 16:09:28 -0600

    sanitize kubeconfig from secret
    Signed-off-by: Prabhjot <psb@tetrate.io>

--- istioctl/pkg/analyze/analyze_test.go
+++ istioctl/pkg/analyze/analyze_test.go
@@ -19,10 +19,14 @@ import (
 	"testing"
 
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 
 	"istio.io/istio/istioctl/pkg/cli"
 	"istio.io/istio/istioctl/pkg/util/testutil"
 	"istio.io/istio/pkg/config/analysis/diag"
+	"istio.io/istio/pkg/kube/multicluster"
 )
 
 func TestErrorOnIssuesFound(t *testing.T) {
@@ -76,6 +80,108 @@ func TestSkipPodsInFiles(t *testing.T) {
 	}
 	analyze := Analyze(cli.NewFakeContext(nil))
 	testutil.VerifyOutput(t, analyze, c)
+}
+
+// TestGetClientsRejectsUnsafeMultiClusterSecret guards against pswg#23: a multicluster
+// secret in istio-system is untrusted input, and a kubeconfig exec plugin in it must never
+// reach client construction, since client-go would run it as a local process.
+func TestGetClientsRejectsUnsafeMultiClusterSecret(t *testing.T) {
+	g := NewWithT(t)
+
+	maliciousKubeconfig := []byte(`
+apiVersion: v1
+kind: Config
+clusters:
+- name: remote
+  cluster:
+    server: https://remote.example.com
+contexts:
+- name: remote-context
+  context:
+    cluster: remote
+    user: attacker
+current-context: remote-context
+users:
+- name: attacker
+  user:
+    exec:
+      apiVersion: client.authentication.k8s.io/v1
+      command: /bin/sh
+      args: ["-c", "touch /tmp/pwned-by-istioctl"]
+      interactiveMode: Never
+`)
+
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "remote-cluster-secret",
+			Namespace: "istio-system",
+			Labels: map[string]string{
+				multicluster.MultiClusterSecretLabel: "true",
+			},
+		},
+		Data: map[string][]byte{
+			"remote-cluster": maliciousKubeconfig,
+		},
+	}
+
+	ctx := cli.NewFakeContext(&cli.NewFakeContextOption{
+		IstioNamespace: "istio-system",
+		Objects:        []runtime.Object{secret},
+	})
+
+	_, err := getClients(ctx)
+
+	g.Expect(err).To(HaveOccurred())
+	g.Expect(err.Error()).To(ContainSubstring("exec is not allowed"))
+}
+
+// TestGetClientsAllowsLegitimateTokenSecret makes sure the sanitization added for pswg#23
+// doesn't reject the kind of secret istioctl create-remote-secret actually produces.
+func TestGetClientsAllowsLegitimateTokenSecret(t *testing.T) {
+	g := NewWithT(t)
+
+	legitKubeconfig := []byte(`
+apiVersion: v1
+kind: Config
+clusters:
+- name: remote
+  cluster:
+    server: https://127.0.0.1:6443
+    insecure-skip-tls-verify: true
+contexts:
+- name: remote-context
+  context:
+    cluster: remote
+    user: remote-sa
+current-context: remote-context
+users:
+- name: remote-sa
+  user:
+    token: some-bearer-token
+`)
+
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "remote-cluster-secret",
+			Namespace: "istio-system",
+			Labels: map[string]string{
+				multicluster.MultiClusterSecretLabel: "true",
+			},
+		},
+		Data: map[string][]byte{
+			"remote-cluster": legitKubeconfig,
+		},
+	}
+
+	ctx := cli.NewFakeContext(&cli.NewFakeContextOption{
+		IstioNamespace: "istio-system",
+		Objects:        []runtime.Object{secret},
+	})
+
+	clients, err := getClients(ctx)
+
+	g.Expect(err).ToNot(HaveOccurred())
+	g.Expect(clients).To(HaveLen(2)) // local fake client + the sanitized remote one
 }
 
 func TestRunSpecificAnalyzer(t *testing.T) {
