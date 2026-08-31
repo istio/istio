f7c5277b6ff1b47122e52bf3334966786eb22a75
zirain <zirain2009@gmail.com>
2026-08-31 17:27:37 +0800

    fix
    Signed-off-by: zirain <zirain2009@gmail.com>

--- pilot/pkg/networking/core/cluster_traffic_policy.go
+++ pilot/pkg/networking/core/cluster_traffic_policy.go
@@ -514,6 +514,11 @@ func applyOutlierDetection(service *model.Service, c *cluster.Cluster, outlier *
 		v := e.GetValue()
 
 		out.FailurePercentageThreshold = &wrapperspb.UInt32Value{Value: v}
+
+		if v > 0 {
+			v = 100
+		}
+		out.EnforcingFailurePercentage = &wrapperspb.UInt32Value{Value: v}
 	}
 	if e := outlier.ConsecutiveGatewayErrors; e != nil {
 		v := e.GetValue()
