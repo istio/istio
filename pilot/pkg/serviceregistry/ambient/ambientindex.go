a8ef57371d0c5c964fad54796928a65cdb58914d
Michael Fraenkel <michael.fraenkel@solo.io>
2026-09-04 08:41:00 -0600

    Reduce AuthorizationPolicy scanning by indexing on namespace (#61256) (#61424)
    * Reduce AuthorizationPolicy scanning by indexing on namespace
    
    Only look at AuthorizationPolicies that are in the same namespace and
    root namespace. This drops the number of policies that are examined for
    every pod, WorkloadEntry and ServiceEntry.
    
    * create the index we need
    
    The Authorization must be present to get the namespace.
    A LabelSelector is required so we match exactly.

--- pilot/pkg/serviceregistry/ambient/ambientindex.go
+++ pilot/pkg/serviceregistry/ambient/ambientindex.go
@@ -252,6 +252,7 @@ func New(options Options) Index {
 		Waypoints,
 		opts,
 	)
+	authPoliciesByNs := selectingWorkloadAuthzByNs(AuthorizationPolicies)
 	serviceEntryVisibility := model.ServiceEntryVisibilityCollection(a.meshConfig.AsCollection(), opts)
 
 	// these are workloadapi-style services combined from kube services and service entries
@@ -342,7 +343,7 @@ func New(options Options) Index {
 		Pods,
 		NodeLocality,
 		a.meshConfig,
-		AuthorizationPolicies,
+		authPoliciesByNs,
 		PeerAuthsByNs,
 		Waypoints,
 		WorkloadServices,
@@ -917,6 +918,18 @@ func PushXdsAddress[T any](xds model.XDSUpdater, f func(T) string, waypointRef f
 			Reason:           model.NewReasonStats(model.AmbientUpdate),
 		})
 	}
+}
+
+func selectingWorkloadAuthzByNs(c krt.Collection[model.WorkloadAuthorization]) krt.Index[string, model.WorkloadAuthorization] {
+	return krt.NewIndex(c, "selectingWorklodAuthorizationsByNs", func(wa model.WorkloadAuthorization) []string {
+		if wa.Authorization == nil {
+			return nil // filter policy which are invalid
+		}
+		if wa.GetLabelSelector() == nil {
+			return nil
+		}
+		return []string{wa.Authorization.Namespace}
+	})
 }
 
 type MeshConfig = meshwatcher.MeshConfigResource
