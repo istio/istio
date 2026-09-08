99a60454e3a5942ff40d3da1afe8d60d172614a0
Steven Jin Xuan <sjinxuan@microsoft.com>
2026-09-08 14:40:26 -0400

    Exclude Service Entries

--- tests/integration/security/util/reachability/context.go
+++ tests/integration/security/util/reachability/context.go
@@ -180,7 +180,8 @@ func Run(testCases []TestCase, t framework.TestContext) {
 
 								// TODO(https://github.com/istio/istio/issues/37629) go back to converge
 								opts.Retry.Options = []retry.Option{retry.Converge(1)}
-								opts.Timeout = retry.DefaultTimeout
+								// TODO(https://github.com/istio/istio/issues/37629) go back to 5s
+								opts.Timeout = time.Second * 10
 
 								expectSuccess := c.ExpectSuccess(from, opts)
 								expectMTLS := c.ExpectMTLS(from, opts)
