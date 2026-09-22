5471f52b4ba8954f6a1d51f8da5047a7db6eefc3
Francisco Herrera <fjglira@gmail.com>
2026-09-22 17:10:59 +0200

    Updating manually the BASE_VERSION
    Signed-off-by: Francisco Herrera <fjglira@gmail.com>

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -49,7 +49,7 @@ endif
 export VERSION
 
 # Base version of Istio image to use
-BASE_VERSION ?= master-2026-08-30T19-01-37
+BASE_VERSION ?= master-2026-09-21T19-03-00
 ISTIO_BASE_REGISTRY ?= docker.io/istio
 
 export GO111MODULE ?= on
