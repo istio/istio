5e65d2cbc1306c01db17666eeaa344016a1d0b06
Petr McAllister <petr.mcallister@gmail.com>
2026-09-23 10:11:54 -0700

    Restore skipped eds check (#61811) (#61846)
    * restore skipped configs to the eds partial check
    
    
    
    * release note
    
    
    
    ---------
    
    
    (cherry picked from commit 8fd30e7f749d62cf2b035e77ce9db1f4473a2f70)
    
    Signed-off-by: Ian Rudie <ian.rudie@solo.io>
    Signed-off-by: Petr McAllister <petr.mcallister@gmail.com>
    Co-authored-by: Ian Rudie <ian.rudie@solo.io>

--- pilot/pkg/xds/eds.go
+++ pilot/pkg/xds/eds.go
@@ -163,6 +163,12 @@ func canSendPartialFullPushes(req *model.PushRequest) bool {
 	}
 
 	for cfg := range req.ConfigsUpdated {
+		// Skipped kinds do not affect EDS and may be coalesced with an update that does.
+		// TODO(ilrudie): Address can affect waypoint endpoint tunnel metadata. We cannot determine
+		// which services are affected, so Address updates must conservatively trigger full EDS.
+		if skippedEdsConfigs.Contains(cfg.Kind) && cfg.Kind != kind.Address {
+			continue
+		}
 		if !deltaAwareEdsConfigs.Contains(cfg.Kind) {
 			return false
 		}
